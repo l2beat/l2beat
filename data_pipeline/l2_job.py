53843244d4ae91be367f9d3699f88eeeeca0b9bb
pk <piotr.m.klis@gmail.com>
2021-03-28 22:18:26 +0200

    updated config and job

--- data_pipeline/l2_job.py
+++ data_pipeline/l2_job.py
@@ -1,6 +1,7 @@
 from tools.storage import create_storage
 from tools.pull_data import pull_l2_data
 import sys
+import os
 import json
 
 
@@ -9,8 +10,9 @@ def run_l2beat(config=None):
     with open(sys.argv[1]) as json_data:
         config = json.load(json_data)
 
+    parent_directory = os.path.dirname(os.getcwd())
     # create object to store data
-    output_object = create_storage(path=config['output'])
+    output_object = create_storage(path=parent_directory + config['output'])
 
     # get the earliest bridge deploy date
     config_l2s = [l2 for l2 in config['l2s'].keys()]
@@ -28,7 +30,8 @@ if sys.argv and len(sys.argv) == 2:
     
     output = run_l2beat(config=config)
 
-    with open(config['output'], 'w') as output_file:
+    parent_directory = os.path.dirname(os.getcwd())
+    with open(parent_directory + config['output'], 'w') as output_file:
         json.dump(output, output_file)
 
 else:
