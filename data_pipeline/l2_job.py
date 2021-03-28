af7397a1fa9f1aae044bed3e5cad51269307aa35
pk <piotr.m.klis@gmail.com>
2021-03-29 00:06:19 +0200

    path to output upgrade

--- data_pipeline/l2_job.py
+++ data_pipeline/l2_job.py
@@ -5,14 +5,10 @@ import os
 import json
 
 
-def run_l2beat(config=None):
+def run_l2beat(config=None, output_file_path=None):
 
-    with open(sys.argv[1]) as json_data:
-        config = json.load(json_data)
-
-    parent_directory = os.path.dirname(os.getcwd())
     # create object to store data
-    output_object = create_storage(path=parent_directory + config['output'])
+    output_object = create_storage(path=output_file_path)
 
     # get the earliest bridge deploy date
     config_l2s = [l2 for l2 in config['l2s'].keys()]
@@ -28,10 +24,12 @@ if sys.argv and len(sys.argv) == 2:
     with open(sys.argv[1]) as json_data:
         config = json.load(json_data)
     
-    output = run_l2beat(config=config)
+    parent_dir = os.path.dirname(os.getcwd())
+    output_file_path = os.path.join(parent_dir, config['output'])
 
-    parent_directory = os.path.dirname(os.getcwd())
-    with open(parent_directory + config['output'], 'w') as output_file:
+    output = run_l2beat(config=config, output_file_path=output_file_path)
+
+    with open(output_file_path, 'w') as output_file:
         json.dump(output, output_file)
 
 else:
