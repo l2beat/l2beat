f1f3eb66423ce608c36b8c4095a39c969ae96817
Kris Kaczor <chris@kaczor.io>
2021-04-06 17:18:05 +0200

    Add logo (#20)

--- frontend/components/Navigation.tsx
+++ frontend/components/Navigation.tsx
@@ -1,14 +1,20 @@
 import GitHubIcon from '@material-ui/icons/GitHub'
+import cx from 'classnames'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 import styles from './Navigation.module.scss'
 
 const HomePath = '/'
 export const Navigation = () => {
+  const router = useRouter()
+
   return (
     <header className={styles.container}>
       <Link href={HomePath}>
-        <h1 className={styles.logo}>l2beat</h1>
+        <h1 className={styles.logo}>
+          L2Beat<div className={cx(styles.logoEmoji, router.asPath === '/' && styles['animate-beat'])}>💓</div>
+        </h1>
       </Link>
       <nav title="Main navigation" className={styles.linksWrapper}>
         <div className={styles.link}>
