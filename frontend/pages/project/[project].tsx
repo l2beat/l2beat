e85a82be9b235be7e83348cbc65b26526f935b23
Damian Paszkowski <paszko.damian@gmail.com>
2021-04-13 10:50:05 +0200

    remove more button (#34)

--- frontend/pages/project/[project].tsx
+++ frontend/pages/project/[project].tsx
@@ -95,15 +95,26 @@ export default function Project(props: ReturnType<typeof getStaticProps>['props'
           </div>
           <List>
             <Item title="Technology" content={props.projectMetadata.technology} />
+
+            <Item title="Technology details" content={props.projectMetadata['technology-details']} />
+
             {Object.keys(props.projectMetadata['more-info']).map((key) => (
               <Item title={key} content={<ContentWithTooltip {...props.projectMetadata['more-info'][key]} />} />
             ))}
+
+            {props.projectMetadata.notes && (
+              <Item
+                title="Notes"
+                className={styles.notes}
+                content={<ContentWithTooltip {...props.projectMetadata.notes} />}
+              />
+            )}
 
             <Item
               title="Tracked bridges"
               content={props.projectConfig.bridges.map((bridge: any) => (
                 <li>
-                  <EtherscanLink address={bridge.address} />
+                  <EtherscanLink address={bridge.address} /> - {bridge.tokens.map((t: string) => t).join(', ')}
                 </li>
               ))}
             />
