a6aea36f14e8a45a84482f95cbd2f224442ce41b
Damian Paszkowski <paszko.damian@gmail.com>
2021-04-14 08:52:29 +0200

    serve image

--- frontend/pages/api/og.ts
+++ frontend/pages/api/og.ts
@@ -1,7 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import { NextApiRequest, NextApiResponse } from 'next'
+import fs from 'fs'
+import path from 'path'
+
+const filePath = path.resolve(process.cwd(), 'public/optimism-og.png')
+const imageBuffer = fs.readFileSync(filePath)
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.redirect('/optimism-og.png')
+  res.setHeader('Content-Type', 'image/png')
+  res.send(imageBuffer)
 }
