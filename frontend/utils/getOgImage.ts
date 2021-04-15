9fab1d7b8a2ed25df995641d75ae3a3f311da669
Damian Paszkowski <paszko.damian@gmail.com>
2021-04-15 21:56:49 +0200

    lint

--- frontend/utils/getOgImage.ts
+++ frontend/utils/getOgImage.ts
@@ -1,5 +1,6 @@
 import captureWebsite from 'capture-website'
 import memoize from 'lodash/memoize'
+
 import { APP_URL } from './constants'
 export function getImage_(project?: string) {
   return captureWebsite.buffer(`${APP_URL}/og/${project || ''}`, { width: 1200, height: 620 })
