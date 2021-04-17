e5cbbfce76e5139b1be7b499f47570d6b26a424f
Damian Paszkowski <paszko.damian@gmail.com>
2021-04-17 22:54:50 +0200

    comment out

--- frontend/scripts/generateOgImages.ts
+++ frontend/scripts/generateOgImages.ts
@@ -9,25 +9,25 @@ async function sleep(time: number) {
     return new Promise((resolve) => setTimeout(() => resolve(undefined), time))
 }
 
-async function wait(attempts = 10) {
-    let attempt = 0
-    while (attempt < attempts) {
-        try {
-            console.log(`Checking server: attempt ${attempt + 1}`)
-            const response = await axios.get("http://localhost:3000")
-            if (response.status === 200) {
-                console.log('Server responded with 200')
-                return;
-            }
-            console.log(`Server responded with ${response.status}`)
-        } catch {
+// async function wait(attempts = 10) {
+//     let attempt = 0
+//     while (attempt < attempts) {
+//         try {
+//             console.log(`Checking server: attempt ${attempt + 1}`)
+//             const response = await axios.get("http://localhost:3000")
+//             if (response.status === 200) {
+//                 console.log('Server responded with 200')
+//                 return;
+//             }
+//             console.log(`Server responded with ${response.status}`)
+//         } catch {
 
-        }
-        attempt = attempt + 1
-        await sleep(3000)
-    }
-    throw new Error(`Couldn't connect to server!`)
-}
+//         }
+//         attempt = attempt + 1
+//         await sleep(3000)
+//     }
+//     throw new Error(`Couldn't connect to server!`)
+// }
 
 function clearAndCreateDirectory() {
     const ogPath = join(process.cwd(), 'public', 'og')
