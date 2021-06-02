83a5b943a300149ce27fe60200f4427e3177ba29
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-06-02 09:41:22 +0200

    Cachce BlockInfo results

--- backend/src/services/BlockInfo.ts
+++ backend/src/services/BlockInfo.ts
@@ -3,12 +3,26 @@ import { providers } from 'ethers'
 import { SimpleDate } from './SimpleDate'
 
 export class BlockInfo {
+  private maxBlocks = new Map<string, Promise<number>>()
+  private blockDates = new Map<number, Promise<SimpleDate>>()
+
   constructor(
     private bigQuery: BigQuery,
     private provider: providers.Provider
   ) {}
 
   async getMaxBlock(date: SimpleDate) {
+    const key = date.toString()
+    const cached = this.maxBlocks.get(key)
+    if (cached) {
+      return cached
+    }
+    const promise = this._getMaxBlock(date)
+    this.maxBlocks.set(key, promise)
+    return promise
+  }
+
+  private async _getMaxBlock(date: SimpleDate) {
     const [rows] = await this.bigQuery.query({
       query: `
         SELECT MAX(number) as block
@@ -21,6 +35,16 @@ export class BlockInfo {
   }
 
   async getBlockDate(block: number) {
+    const cached = this.blockDates.get(block)
+    if (cached) {
+      return cached
+    }
+    const promise = this._getBlockDate(block)
+    this.blockDates.set(block, promise)
+    return promise
+  }
+
+  private async _getBlockDate(block: number) {
     const { timestamp } = await this.provider.getBlock(block)
     return SimpleDate.fromUnixTimestamp(timestamp)
   }
