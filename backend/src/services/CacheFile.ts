1119836484404fcf723bb1696eecb19bc01a2f1b
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-06-11 17:33:09 +0200

    Always read precomputed.json

--- backend/src/services/CacheFile.ts
+++ backend/src/services/CacheFile.ts
@@ -5,10 +5,11 @@ const CACHE_FILE_PATH = 'cache/cache.json'
 
 export class CacheFile {
   read() {
-    const contents = fs.existsSync(CACHE_FILE_PATH)
+    const precomputed = fs.readFileSync(PRECOMPUTED_FILE_PATH, 'utf-8')
+    const cache = fs.existsSync(CACHE_FILE_PATH)
       ? fs.readFileSync(CACHE_FILE_PATH, 'utf-8')
-      : fs.readFileSync(PRECOMPUTED_FILE_PATH, 'utf-8')
-    return JSON.parse(contents)
+      : '{}'
+    return { ...JSON.parse(cache), ...JSON.parse(precomputed) }
   }
 
   write(data: unknown) {
