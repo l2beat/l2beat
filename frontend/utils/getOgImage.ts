bd686bd1900beb57c8e4bb62ab1728f4d12a263b
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-06-11 08:51:44 +0200

    Generate og images directly to out

--- frontend/utils/getOgImage.ts
+++ frontend/utils/getOgImage.ts
@@ -1,12 +1,12 @@
 import captureWebsite from 'capture-website'
 import { join } from 'path'
 
-import { OG_FILES_DIR } from './constants'
+export const OG_FILES_DIR = join(process.cwd(), 'out', 'og')
 
 export async function generateImage(project: string) {
   console.log(`Generating: ${project}`)
 
-  return captureWebsite.file(`http://localhost:8080/og/${project}`, join(OG_FILES_DIR, `${project}.png`), {
+  return captureWebsite.file(`http://localhost:8080/og/${project}.html`, join(OG_FILES_DIR, `${project}.png`), {
     width: 1200,
     height: 630,
     launchOptions: {
