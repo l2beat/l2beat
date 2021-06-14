6f45fb1047357de836378b266157f0a710b9e64b
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-06-14 12:04:58 +0200

    Store precomputed as zip (#92)

--- backend/src/services/CacheFile.ts
+++ backend/src/services/CacheFile.ts
@@ -1,12 +1,15 @@
 import fs from 'fs'
+import AdmZip from 'adm-zip'
 
-const PRECOMPUTED_FILE_PATH = 'cache/precomputed.json'
+const PRECOMPUTED_FILE_PATH = 'cache/precomputed.zip'
 const CACHE_FILE_PATH = 'cache/cache.json'
 const VERSION = 1
 
 export class CacheFile {
   read() {
-    const precomputed = fs.readFileSync(PRECOMPUTED_FILE_PATH, 'utf-8')
+    const zip = new AdmZip(PRECOMPUTED_FILE_PATH)
+    const precomputed = zip.readAsText('precomputed.json')
+
     const cache = fs.existsSync(CACHE_FILE_PATH)
       ? fs.readFileSync(CACHE_FILE_PATH, 'utf-8')
       : '{}'
@@ -28,10 +31,14 @@ export class CacheFile {
   }
 
   writePrecomputed(data: unknown) {
-    fs.writeFileSync(
-      PRECOMPUTED_FILE_PATH,
-      JSON.stringify(data, null, 2),
-      'utf-8'
-    )
+    const content = JSON.stringify(data)
+    const zip = new AdmZip()
+    zip.addFile('precomputed.json', Buffer.from(content))
+    const entry = zip.getEntry('precomputed.json')
+    if (entry) {
+      // ensure deterministic output
+      entry.header.time = new Date('1999-12-31T23:59:99.999Z')
+    }
+    zip.writeZip(PRECOMPUTED_FILE_PATH)
   }
 }
