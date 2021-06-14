576cdbd8bb409f9f92edb1f93836ae1cb3892b14
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-06-14 09:13:37 +0200

    Verify coingecko ids and token metadata (#90)

--- config/test/tokens.test.ts
+++ config/test/tokens.test.ts
@@ -1,5 +1,6 @@
 import { expect } from 'chai'
-import { utils } from 'ethers'
+import { Contract, providers, utils } from 'ethers'
+import fetch from 'node-fetch'
 import { tokenList } from '../src/tokens'
 
 describe('tokens', () => {
@@ -24,5 +25,116 @@ describe('tokens', () => {
     const names = tokenList.map((x) => x.name)
     const sorted = [...names].sort((a, b) => a.localeCompare(b))
     expect(names).to.deep.equal(sorted)
+  })
+
+  describe('metadata is correct', () => {
+    const MULTICALL_ADDRESS = '0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441'
+    const ABI = [
+      'function aggregate(tuple(address target, bytes callData)[] calls) view returns (uint256 blockNumber, bytes[] returnData)',
+      'function name() view returns (string)',
+      'function symbol() view returns (string)',
+      'function decimals() view returns (uint8)',
+    ]
+    const CODER = new utils.Interface(ABI)
+    const NAME_CALL = CODER.encodeFunctionData('name')
+    const SYMBOL_CALL = CODER.encodeFunctionData('symbol')
+    const DECIMALS = CODER.encodeFunctionData('decimals')
+
+    interface Metadata {
+      name: string
+      symbol: string
+      decimals: number
+    }
+    const results: Record<string, Metadata> = {}
+
+    before('fetch metadata', async () => {
+      const provider = new providers.AlchemyProvider('mainnet')
+      const contract = new Contract(MULTICALL_ADDRESS, CODER, provider)
+      const calls = tokenList.flatMap((x) =>
+        !x.address
+          ? []
+          : [
+              [x.address, NAME_CALL],
+              [x.address, SYMBOL_CALL],
+              [x.address, DECIMALS],
+            ]
+      )
+      const [, data] = await contract.functions.aggregate(calls)
+      for (let i = 0; i < calls.length; i += 3) {
+        const nameResult = data[i]
+        const symbolResult = data[i + 1]
+        const decimalsResult = data[i + 2]
+        results[calls[i][0]] = {
+          name: decodeString(nameResult),
+          symbol: decodeString(symbolResult),
+          decimals: decodeNumber(decimalsResult),
+        }
+      }
+    })
+
+    function decodeString(hex: string) {
+      try {
+        return CODER.decodeFunctionResult('name', hex)[0] as string
+      } catch {
+        // This is a special case because MKR is broken and encodes name and
+        // symbol as bytes32 null terminated strings
+        let result = ''
+        for (let i = 2; i < hex.length; i += 2) {
+          const byte = hex[i] + hex[i + 1]
+          if (byte === '00') {
+            break
+          }
+          result += String.fromCharCode(parseInt(byte, 16))
+        }
+        return result
+      }
+    }
+
+    function decodeNumber(hex: string) {
+      return CODER.decodeFunctionResult('decimals', hex)[0] as number
+    }
+
+    for (const token of tokenList) {
+      it(token.symbol, () => {
+        if (!token.address) {
+          return
+        }
+        expect(token.name).to.equal(results[token.address].name)
+        expect(token.symbol).to.equal(results[token.address].symbol)
+        expect(token.decimals).to.equal(results[token.address].decimals)
+      })
+    }
+  })
+
+  describe("coingeckoId's are correct", () => {
+    const API_URL =
+      'https://api.coingecko.com/api/v3/coins/list?include_platform=true'
+    const coingeckoData: Record<string, any> = {}
+    before('fetches coingecko ids', async function () {
+      this.timeout(10_000)
+      const data = await fetch(API_URL).then((res) => res.json())
+      for (const record of data) {
+        coingeckoData[record.id] = record
+      }
+    })
+
+    for (const token of tokenList) {
+      it(token.coingeckoId, () => {
+        const record = coingeckoData[token.coingeckoId]
+        expect(record).not.to.equal(undefined)
+        const address = record.platforms.ethereum
+        if (!address) {
+          if (token.coingeckoId === 'binancecoin') {
+            expect(token.address).to.equal(
+              '0xB8c77482e45F1F44dE1745F52C74426C631bDD52'
+            )
+          } else {
+            expect(token.coingeckoId).to.equal('ethereum')
+          }
+        } else {
+          expect(token.address?.toLowerCase()).to.equal(address)
+        }
+      })
+    }
   })
 })
