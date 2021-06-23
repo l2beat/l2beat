ac90e679fd2cbf6875f80b6e1aa0f82d1cf76511
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-06-23 15:54:16 +0200

    Add features to Hermez

--- config/src/projects/hermez.ts
+++ config/src/projects/hermez.ts
@@ -1,3 +1,4 @@
+import { Features, featuresByComparison, risk, Technologies } from '../features'
 import { Project } from './Project'
 
 export const hermez: Project = {
@@ -80,5 +81,60 @@ export const hermez: Project = {
         link: 'https://blog.hermez.io/hermez-network-mainnet-launch/',
       },
     ],
+    features: featuresByComparison(Technologies.ZkRollup, [
+      Features.Generality.Specific({
+        pointers: ['https://docs.hermez.io/#/about/scalability?id=background'],
+      }),
+      Features.Withdrawal.Proved({
+        description:
+          'Additonally if funds exceed certain treshold the withdrawal is delayed to prevent attacks. In case an attack is determined to occur the funds go to governance instead.',
+        pointers: [
+          'https://docs.hermez.io/#/developers/glossary?id=exit-tree',
+          'https://docs.hermez.io/#/developers/protocol/withdrawal-delayer/withdrawal-delayer',
+        ],
+        risks: [
+          risk(
+            'Funds can be stolen',
+            'The governance can claim an attack is underway and confiscate user funds.'
+          ),
+        ],
+      }),
+      Features.State.ValidityProofs({
+        pointers: ['https://docs.hermez.io/#/about/security?id=zk-proofs'],
+      }),
+      Features.Settlement.Delayed({
+        description:
+          'In case of Hermez the delay comes from the attack protection placed on withdrawals.',
+        pointers: [
+          'https://docs.hermez.io/#/developers/protocol/withdrawal-delayer/withdrawal-delayer',
+        ],
+      }),
+      Features.Cryptography.SNARKS({
+        pointers: [
+          'https://docs.hermez.io/#/about/security?id=multi-party-computation-for-the-trusted-setup',
+        ],
+      }),
+      Features.Data.OnChain({
+        pointers: [
+          'https://docs.hermez.io/#/about/scalability?id=zero-knowledge-rollups',
+        ],
+      }),
+      Features.SourceCode.Public({
+        pointers: ['https://github.com/hermeznetwork/contracts'],
+      }),
+      Features.Upgradeability.Upgradeable({
+        pointers: [
+          'https://etherscan.io/address/0xa68d85df56e733a06443306a095646317b5fa633',
+        ],
+      }),
+      Features.Sequencer.Decentralized({
+        pointers: ['https://docs.hermez.io/#/about/model?id=auction'],
+      }),
+      Features.ForceTxs.Any({
+        pointers: [
+          'https://docs.hermez.io/#/faq/end-users?id=can-coordinators-censor-transactions',
+        ],
+      }),
+    ]),
   },
 }
