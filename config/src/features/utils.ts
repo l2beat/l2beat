9c86499cf7ad1dc483ecd551048d973bb123fc77
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-06-23 15:25:53 +0200

    Add Arbitrum features

--- config/src/features/utils.ts
+++ config/src/features/utils.ts
@@ -1,4 +1,5 @@
 import { Feature, Risk } from '../projects'
+import { Features } from './features'
 
 export interface FeatureOptions {
   pointers?: string[]
@@ -23,4 +24,26 @@ export function feature(name: string, description: string, ...risks: Risk[]) {
 
 export function risk(type: Risk['type'], description: string): Risk {
   return { type, description }
+}
+
+const groups: Record<string, string> = {}
+for (const [feature, options] of Object.entries(Features)) {
+  for (const option of Object.values(options)) {
+    groups[option().name] = feature
+  }
+}
+
+export function featuresByComparison(
+  technology: Feature[],
+  features: Feature[]
+) {
+  return features.map((feature) => {
+    const techFeature = technology.find(
+      (f) => groups[f.name] === groups[feature.name]
+    )
+    if (techFeature && techFeature.name !== feature.name) {
+      return { ...feature, overrides: techFeature }
+    }
+    return feature
+  })
 }
--- config/src/projects/arbitrum.ts
+++ config/src/projects/arbitrum.ts
@@ -1,3 +1,4 @@
+import { Features, featuresByComparison, Technologies } from '../features'
 import { Project } from './Project'
 
 export const arbitrum: Project = {
@@ -65,5 +66,69 @@ export const arbitrum: Project = {
         tooltip: 'Automatic EVM -> AVM translation happens under the hood.',
       },
     ],
+    features: featuresByComparison(Technologies.OptimisticRollup, [
+      Features.Generality.Any({
+        pointers: [
+          'https://developer.offchainlabs.com/docs/rollup_basics#arbitrum-rollup',
+        ],
+      }),
+      Features.Withdrawal.Proved({
+        pointers: [
+          'https://developer.offchainlabs.com/docs/l1_l2_messages#l2-to-l1-messages-lifecycle',
+        ],
+      }),
+      Features.State.FraudProofs({
+        pointers: [
+          'https://developer.offchainlabs.com/docs/rollup_basics#executing-and-securing-the-chain',
+        ],
+      }),
+      Features.Settlement.Delayed({
+        pointers: [
+          'https://developer.offchainlabs.com/docs/inside_arbitrum#rules-for-confirming-or-rejecting-rollup-blocks',
+        ],
+      }),
+      Features.Data.OnChain({
+        pointers: [
+          'https://developer.offchainlabs.com/docs/rollup_basics#arbitrum-rollup',
+        ],
+      }),
+      Features.SourceCode.Public({
+        pointers: [
+          'https://github.com/OffchainLabs/arbitrum',
+          'https://github.com/OffchainLabs/arb-os',
+        ],
+      }),
+      Features.Upgradeability.Upgradeable({
+        pointers: [
+          'https://etherscan.io/address/0xc12ba48c781f6e392b49db2e25cd0c28cd77531a#code',
+          'https://etherscan.io/address/0x011B6E24FfB0B5f5fCc564cf4183C5BBBc96D515#code',
+        ],
+      }),
+      Features.Ownership.EOA({
+        description:
+          'The owner of the bridge is the rollup contract and the owner of the rollup is an EOA.',
+        pointers: [
+          'https://etherscan.io/address/0x1c7d91ccbdbf378bac0f074678b09cb589184e4e',
+        ],
+      }),
+      Features.SmartContracts.EVM({
+        description:
+          'Arbitrum has developed its own virtual machine called AVM.',
+        pointers: [
+          'https://developer.offchainlabs.com/docs/inside_arbitrum#avm-the-arbitrum-virtual-machine',
+        ],
+      }),
+      Features.Sequencer.Centralized({
+        pointers: [
+          'https://developer.offchainlabs.com/docs/inside_arbitrum#validators',
+          'https://developer.offchainlabs.com/docs/inside_arbitrum#if-the-sequencer-is-malicious',
+        ],
+      }),
+      Features.ForceTxs.Any({
+        pointers: [
+          'https://developer.offchainlabs.com/docs/rollup_basics#aggregating-transactions',
+        ],
+      }),
+    ]),
   },
 }
