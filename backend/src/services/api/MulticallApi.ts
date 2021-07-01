27d48c5b8f8143fbd204455eb9b8778d67c1733c
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-07-01 15:56:27 +0200

    Add multicall batches

--- backend/src/services/api/MulticallApi.ts
+++ backend/src/services/api/MulticallApi.ts
@@ -1,6 +1,7 @@
 import { utils } from 'ethers'
 import { MULTICALL } from '../../constants'
 import { AsyncCache } from '../AsyncCache'
+import { Logger } from '../Logger'
 import { AlchemyApi } from './AlchemyApi'
 
 const MULTICALL_ABI = new utils.Interface([
@@ -18,7 +19,11 @@ export interface MulticallResponse {
 }
 
 export class MulticallApi {
-  constructor(private alchemyApi: AlchemyApi, private asyncCache: AsyncCache) {}
+  constructor(
+    private alchemyApi: AlchemyApi,
+    private asyncCache: AsyncCache,
+    private logger: Logger
+  ) {}
 
   async multicall(
     requests: Record<string, MulticallRequest>,
@@ -38,29 +43,63 @@ export class MulticallApi {
       }
     }
     if (unknown.length > 0) {
-      const callData = MULTICALL_ABI.encodeFunctionData('aggregate', [
-        unknown.map(([, request]) => [request.address, request.data]),
-      ])
-      const returnData = await this.alchemyApi.call(
-        MULTICALL,
-        callData,
-        blockNumber
-      )
-      const [, result] = MULTICALL_ABI.decodeFunctionResult(
-        'aggregate',
-        returnData
-      )
-      for (const [i, [key, request]] of unknown.entries()) {
-        this.asyncCache.set(
-          ['multicall', blockNumber, request.address, request.data],
-          result[i],
-          (x) => x
-        )
-        known.push([key, dataToResponse(result[i])])
+      const requests = unknown.map((x) => x[1])
+      const results = await this.callInBatches(requests, blockNumber)
+      for (const [i, [key]] of unknown.entries()) {
+        known.push([key, results[i]])
       }
     }
     return Object.fromEntries(known)
   }
+
+  private async callInBatches(
+    requests: MulticallRequest[],
+    blockNumber: number
+  ) {
+    const batches = toBatches(requests, 200)
+    const batchedResults = await Promise.all(
+      batches.map((batch, i) =>
+        this.executeBatch(batch, `${i + 1}/${batches.length}`, blockNumber)
+      )
+    )
+    return batchedResults.flat()
+  }
+
+  private async executeBatch(
+    requests: MulticallRequest[],
+    batchId: string,
+    blockNumber: number
+  ) {
+    const callData = MULTICALL_ABI.encodeFunctionData('aggregate', [
+      requests.map((request) => [request.address, request.data]),
+    ])
+    const returnData = await this.alchemyApi.call(
+      MULTICALL,
+      callData,
+      blockNumber
+    )
+    this.logger.log(
+      `fetched batch ${batchId} (${requests.length} calls) @ ${blockNumber}`
+    )
+    const decoded = MULTICALL_ABI.decodeFunctionResult('aggregate', returnData)
+    const result: string[] = decoded[1]
+    for (const [i, request] of requests.entries()) {
+      this.asyncCache.set(
+        ['multicall', blockNumber, request.address, request.data],
+        result[i],
+        (x) => x
+      )
+    }
+    return result.map(dataToResponse)
+  }
+}
+
+function toBatches<T>(items: T[], batchSize: number): T[][] {
+  const batches: T[][] = []
+  for (let i = 0; i < items.length; i += batchSize) {
+    batches.push(items.slice(i, i + batchSize))
+  }
+  return batches
 }
 
 function dataToResponse(data: string): MulticallResponse {
