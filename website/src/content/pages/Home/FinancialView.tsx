8b5ccea5a86f711e0624ce016999e76c384e3fbd
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-07-13 17:37:44 +0200

    Make table responsive

--- website/src/content/pages/Home/FinancialView.tsx
+++ website/src/content/pages/Home/FinancialView.tsx
@@ -7,45 +7,50 @@ interface Props {
 
 export function FinancialView({ items }: Props) {
   return (
-    <table className="financials">
-      <thead className="financials__header">
-        <tr>
-          <th>No.</th>
-          <th>Name</th>
-          <th>Total Value Locked</th>
-          <th>1 day change</th>
-          <th>7 day change</th>
-          <th>Market share</th>
-        </tr>
-      </thead>
-      <tbody className="financials__body">
-        {items.map((project, i) => (
-          <tr key={i}>
-            <td>{i + 1}.</td>
-            <td>
-              <a
-                className="financials__name"
-                href={`/projects/${project.slug}`}
-              >
-                <img
-                  className="financials__icon"
-                  src={`/icons/${project.slug}.png`}
-                  alt={`${project.name} logo`}
-                />
-                {project.name}
-              </a>
-            </td>
-            <td>{project.tvl}</td>
-            <td>
-              <PercentChange value={project.oneDayChange} />
-            </td>
-            <td>
-              <PercentChange value={project.sevenDayChange} />
-            </td>
-            <td>{project.marketShare}</td>
+    <div className="financials">
+      <table className="financials__table">
+        <thead className="financials__header">
+          <tr>
+            <th>No.</th>
+            <th>Name</th>
+            <th>
+              <span data-wide>Total Value Locked</span>
+              <span data-narrow>TVL</span>
+            </th>
+            <th>1 day change</th>
+            <th>7 day change</th>
+            <th>Market share</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody className="financials__body">
+          {items.map((project, i) => (
+            <tr key={i}>
+              <td>{i + 1}.</td>
+              <td>
+                <a
+                  className="financials__name"
+                  href={`/projects/${project.slug}`}
+                >
+                  <img
+                    className="financials__icon"
+                    src={`/icons/${project.slug}.png`}
+                    alt={`${project.name} logo`}
+                  />
+                  {project.name}
+                </a>
+              </td>
+              <td>{project.tvl}</td>
+              <td>
+                <PercentChange value={project.oneDayChange} />
+              </td>
+              <td>
+                <PercentChange value={project.sevenDayChange} />
+              </td>
+              <td>{project.marketShare}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
