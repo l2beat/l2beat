e30c242c7dcc39bf31118d48c626e7c1d1f5e500
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-07-14 15:24:44 +0200

    Minify js & css

--- website/rollup.config.js
+++ website/rollup.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'rollup'
 import typescript from '@rollup/plugin-typescript'
+import { terser } from 'rollup-plugin-terser'
 
 export default defineConfig({
   input: './src/scripts/index.ts',
   output: {
     file: 'build/scripts/main.js',
     format: 'iife',
-    sourcemap: true,
   },
-  plugins: [typescript({ module: 'ESNext' })],
+  plugins: [typescript({ module: 'ESNext' }), terser()],
 })
