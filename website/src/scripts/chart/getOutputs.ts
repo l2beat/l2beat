ab42dab6da2dd00b366e2dabf60c2735dc674ff2
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-07-14 14:08:39 +0200

    Simplify element querying

--- website/src/scripts/chart/getOutputs.ts
+++ website/src/scripts/chart/getOutputs.ts
@@ -1,12 +1,9 @@
+import { makeQuery } from './query'
+
 export type Outputs = ReturnType<typeof getOutputs>
 
 export function getOutputs(chart: Element) {
-  const $ = <T extends HTMLElement>(selector: string) =>
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    chart.querySelector<T>(selector)!
-  const $$ = <T extends HTMLElement>(selector: string) =>
-    Array.from(chart.querySelectorAll<T>(selector))
-
+  const { $, $$ } = makeQuery(chart)
   return {
     canvas: $<HTMLCanvasElement>('.chart__canvas'),
     loader: $('.chart__loader'),
