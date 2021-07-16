17ffebe43b64eb43f57e9a1488ec5a6a19f81a49
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-07-16 16:07:35 +0200

    Add real FAQ

--- website/src/content/pages/Faq/FaqPage.tsx
+++ website/src/content/pages/Faq/FaqPage.tsx
@@ -1,11 +1,10 @@
-import { Footer, Header, Navbar, Page } from '../../common'
+import { Footer, Navbar, Page } from '../../common'
 import { FaqPageProps } from './getFaqPageProps'
 
 export function FaqPage(props: FaqPageProps) {
   return (
     <Page metadata={props.metadata}>
       <Navbar />
-      <Header title={props.title} titleLength="very-long" />
       <article
         className="faq"
         dangerouslySetInnerHTML={{ __html: props.content }}
