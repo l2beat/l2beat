1147d92a588d46a7c2f981e5babcf957c2f7d04c
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-07-19 11:59:51 +0200

    Replace old website with new (#135)
    * Remove frontend
    
    * Deploy new website to netlify

--- frontend/utils/findProjectBridges.ts
+++
@@ -1,8 +0,0 @@
-import { projects } from '@l2beat/config'
-import { assert } from 'ts-essentials'
-
-export function findProjectBridges(name: string) {
-  const bridges = projects.find((x) => x.name.toLowerCase() === name.toLowerCase())?.bridges
-  assert(bridges, `Couldn't find ${name} in projects config`)
-  return bridges
-}
--- frontend/utils/getProjects.ts
+++
@@ -1,5 +0,0 @@
-import { projects } from '@l2beat/config'
-
-export function getProjects() {
-  return projects.map(({ name }) => name.toLowerCase())
-}
