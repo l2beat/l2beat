ef582bb389321689c2e0a75764b5ce95257696b4
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-08-16 09:18:42 +0200

    Improve meta descriptions (#183)

--- website/src/content/pages/Home/props/getPageMetadata.ts
+++ website/src/content/pages/Home/props/getPageMetadata.ts
@@ -4,7 +4,7 @@ export function getPageMetadata(): PageMetadata {
   return {
     title: 'L2BEAT – The state of the layer two ecosystem',
     description:
-      'L2BEAT is a analytics and research website about Ethereum layer 2 scaling.',
+      'L2BEAT is an analytics and research website about Ethereum layer 2 scaling. Here you will find in depth comparison of major protocols live on Ethereum today.',
     image: 'https://l2beat.com/meta-images/overview.png',
     url: 'https://l2beat.com/',
   }
