69815a53c299adb1fdddbee2afda6503e6b7e29e
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-08-18 14:39:42 +0200

    Update website organisation (#189)

--- website/src/content/pages/Project/props/getTechnologyOverview.ts
+++ website/src/content/pages/Project/props/getTechnologyOverview.ts
@@ -2,12 +2,16 @@ import {
   Project,
   ProjectContract,
   ProjectReference,
+  ProjectTechnology,
   ProjectTechnologyChoice,
 } from '@l2beat/config'
 import { TechnologyContract } from '../view/ContractsSection'
 import { TechnologyReference } from '../view/ReferencesSection'
 import { TechnologyOverviewProps } from '../view/TechnologyOverview'
-import { TechnologyChoice } from '../view/TechnologySection'
+import {
+  TechnologyChoice,
+  TechnologySectionProps,
+} from '../view/TechnologySection'
 import { getEditLink, getIssueLink } from './links'
 
 export function getTechnologyOverview(
@@ -19,43 +23,90 @@ export function getTechnologyOverview(
   }
 
   const references: TechnologyReference[] = []
-  const technologies: TechnologyChoice[] = []
-  const withdrawals: TechnologyChoice[] = []
-  const contracts: TechnologyContract[] = []
 
   function addReference(reference: ProjectReference) {
-    const id = references.length + 1
-    references.push({ id, text: reference.text, href: reference.href })
-    return id
-  }
-
-  function addTechnologyChoice(
-    into: TechnologyChoice[],
-    id: string,
-    item?: ProjectTechnologyChoice
-  ) {
-    if (item) {
-      const risks = item.risks.map((risk) => ({
-        referenceIds: (risk.references ?? []).map(addReference),
-        text: `${risk.category} ${risk.text}`,
-      }))
-
-      const issueTitle = `Problem: ${project.name} - ${item.name}`
-
-      into.push({
-        id,
-        name: item.name,
-        editLink: getEditLink(project),
-        issueLink: getIssueLink(issueTitle),
-        description: item.description ?? '',
-        missingInfo: item.description === null,
-        referenceIds: item.references.map(addReference),
-        risks,
-      })
+    const index = references.findIndex((x) => x.href === reference.href)
+    if (index !== -1) {
+      return index + 1
+    } else {
+      const id = references.length + 1
+      references.push({ id, text: reference.text, href: reference.href })
+      return id
     }
   }
 
-  function addContract(item: ProjectContract) {
+  function makeTechnologyChoice(
+    id: string,
+    item: ProjectTechnologyChoice
+  ): TechnologyChoice {
+    const risks = item.risks.map((risk) => ({
+      referenceIds: (risk.references ?? []).map(addReference),
+      text: `${risk.category} ${risk.text}`,
+    }))
+
+    const issueTitle = `Problem: ${project.name} - ${item.name}`
+
+    return {
+      id,
+      name: item.name,
+      editLink: getEditLink(project),
+      issueLink: getIssueLink(issueTitle),
+      description: item.description ?? '',
+      missingInfo: item.description === null,
+      referenceIds: item.references.map(addReference),
+      risks,
+    }
+  }
+
+  function makeSections(tech: ProjectTechnology) {
+    const technology: TechnologySectionProps = {
+      id: 'technology',
+      title: 'Technology',
+      items: [
+        makeTechnologyChoice('state-correctness', tech.stateCorrectness),
+        tech.newCryptography &&
+          makeTechnologyChoice('new-cryptography', tech.newCryptography),
+        makeTechnologyChoice('data-availability', tech.dataAvailability),
+      ].filter(noUndefined),
+    }
+
+    const operator: TechnologySectionProps = {
+      id: 'operator',
+      title: 'Operator',
+      items: [
+        makeTechnologyChoice('operator', tech.operator),
+        makeTechnologyChoice('force-transactions', tech.forceTransactions),
+      ],
+    }
+
+    const withdrawals: TechnologySectionProps = {
+      id: 'withdrawals',
+      title: 'Withdrawals',
+      items: [
+        ...tech.exitMechanisms.map((x, i) =>
+          makeTechnologyChoice(`exit-mechanisms-${i + 1}`, x)
+        ),
+        tech.massExit && makeTechnologyChoice('mass-exit', tech.massExit),
+      ].filter(noUndefined),
+    }
+
+    const other: TechnologySectionProps = {
+      id: 'other-considerations',
+      title: 'Other considerations',
+      items: [
+        tech.additionalPrivacy &&
+          makeTechnologyChoice('additional-privacy', tech.additionalPrivacy),
+        tech.smartContracts &&
+          makeTechnologyChoice('smart-contracts', tech.smartContracts),
+      ].filter(noUndefined),
+    }
+
+    return [technology, operator, withdrawals, other].filter(
+      (x) => x.items.length > 0
+    )
+  }
+
+  function makeTechnologyContract(item: ProjectContract): TechnologyContract {
     const links = []
 
     const upgradable = !!item.upgradeability
@@ -94,51 +145,37 @@ export function getTechnologyOverview(
       })
     }
 
-    contracts.push({
+    return {
       name: item.name,
       address: item.address,
       description: item.description,
       links,
-    })
+    }
   }
 
-  const addTechnology = (id: string, item?: ProjectTechnologyChoice) =>
-    addTechnologyChoice(technologies, id, item)
+  function makeContractSection(tech: ProjectTechnology) {
+    const contracts = tech.contracts.addresses.map(makeTechnologyContract)
 
-  addTechnology('state-correctness', tech.stateCorrectness)
-  addTechnology('data-availability', tech.dataAvailability)
-  addTechnology('new-cryptography', tech.newCryptography)
-  addTechnology('mass-exit', tech.massExit)
-  addTechnology('additional-privacy', tech.additionalPrivacy)
-  addTechnology('smart-contracts', tech.smartContracts)
-  addTechnology('operator', tech.operator)
-  addTechnology('force-transactions', tech.forceTransactions)
+    const risks = tech.contracts.risks.map((risk) => ({
+      referenceIds: (risk.references ?? []).map(addReference),
+      text: `${risk.category} ${risk.text}`,
+    }))
 
-  const addWithdrawal = (id: string, item?: ProjectTechnologyChoice) =>
-    addTechnologyChoice(withdrawals, id, item)
-
-  for (const [i, item] of tech.exitMechanisms.entries()) {
-    addWithdrawal(`exit-mechanisms-${i + 1}`, item)
-  }
-
-  for (const item of tech.contracts.addresses) {
-    addContract(item)
-  }
-
-  const contractRisks = tech.contracts.risks.map((risk) => ({
-    referenceIds: (risk.references ?? []).map(addReference),
-    text: `${risk.category} ${risk.text}`,
-  }))
-
-  return {
-    technologySection: { items: technologies },
-    referencesSection: { items: references },
-    withdrawalsSection: { items: withdrawals },
-    contractsSection: {
+    return {
       editLink: getEditLink(project),
       issueLink: getIssueLink(`Problem: ${project.name} - Contracts`),
       contracts,
-      risks: contractRisks,
-    },
+      risks,
+    }
   }
+
+  return {
+    sections: makeSections(tech),
+    contractsSection: makeContractSection(tech),
+    referencesSection: { items: references },
+  }
+}
+
+function noUndefined<T>(x: T | undefined): x is T {
+  return x !== undefined
 }
--- website/src/content/pages/Project/view/TechnologyOverview.tsx
+++ website/src/content/pages/Project/view/TechnologyOverview.tsx
@@ -1,35 +1,19 @@
 import { ContractsSection, ContractsSectionProps } from './ContractsSection'
 import { ReferencesSection, ReferencesSectionProps } from './ReferencesSection'
-import { TechnologyChoice, TechnologySection } from './TechnologySection'
+import { TechnologySection, TechnologySectionProps } from './TechnologySection'
 
 export interface TechnologyOverviewProps {
-  technologySection: TechnologySectionProps
-  withdrawalsSection: WithdrawalSectionProps
+  sections: TechnologySectionProps[]
   contractsSection: ContractsSectionProps
   referencesSection: ReferencesSectionProps
-}
-
-export interface TechnologySectionProps {
-  items: TechnologyChoice[]
-}
-
-export interface WithdrawalSectionProps {
-  items: TechnologyChoice[]
 }
 
 export function TechnologyOverview(props: TechnologyOverviewProps) {
   return (
     <>
-      <TechnologySection
-        title="Technology"
-        id="technology"
-        {...props.technologySection}
-      />
-      <TechnologySection
-        title="Withdrawals"
-        id="withdrawals"
-        {...props.withdrawalsSection}
-      />
+      {props.sections.map((section) => (
+        <TechnologySection key={section.id} {...section} />
+      ))}
       <ContractsSection {...props.contractsSection} />
       <ReferencesSection {...props.referencesSection} />
     </>
