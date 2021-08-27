93ae8d648ce4a07a7079af1835d15e7bcc3747dd
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-08-27 11:34:57 +0200

    Enable the new risk framework

--- website/src/content/config.ts
+++ website/src/content/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   __DEV__showTechnologiesPage: false,
-  __DEV__showNewDetails: false,
-  __DEV__showRiskView: false,
-  __DEV__showNewUpdateBanner: false,
+  __DEV__showNewDetails: true,
+  __DEV__showRiskView: true,
+  __DEV__showNewUpdateBanner: true,
 }
