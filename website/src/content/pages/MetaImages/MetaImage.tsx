4827cbcb10993021d7bd6ae6390da058f2cfe394
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-08-27 08:50:42 +0200

    Add new update banner

--- website/src/content/pages/MetaImages/MetaImage.tsx
+++ website/src/content/pages/MetaImages/MetaImage.tsx
@@ -1,6 +1,7 @@
 import cx from 'classnames'
 import React from 'react'
 import { Chart, Header, Logo, Page } from '../../common'
+import { config } from '../../config'
 import { MetaImageProps } from './getMetaImageProps'
 
 export function MetaImage(props: MetaImageProps) {
@@ -16,6 +17,9 @@ export function MetaImage(props: MetaImageProps) {
         }}
       />
       <div className={cx('Meta', !props.name ? 'overview' : 'project')}>
+        {config.__DEV__showNewUpdateBanner && (
+          <div className="Meta-Update">New Update Out Now!</div>
+        )}
         <Header
           title={props.name ?? 'Overview'}
           icon={props.icon}
