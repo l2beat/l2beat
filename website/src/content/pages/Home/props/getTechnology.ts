4e878d72b51bc3103034eee6af69497bcdf4132b
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-08-30 09:41:34 +0200

    Fix type issue

--- website/src/content/pages/Home/props/getTechnology.ts
+++ website/src/content/pages/Home/props/getTechnology.ts
@@ -1,7 +1,7 @@
 import { Project } from '@l2beat/config'
 
 export function getTechnology(project: Project) {
-  const name = project.details.technologyName
+  const name = project.details.technology.category.name
   switch (name) {
     case 'Optimistic Rollup':
       return { abbreviation: 'ORU', name }
@@ -14,5 +14,4 @@ export function getTechnology(project: Project) {
     case 'State Pools':
       return { abbreviation: 'STP', name }
   }
-  return { abbreviation: '???', name }
 }
