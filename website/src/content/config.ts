ac883e4badef7d1c86f6607ecbb7b82dcae56683
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-09-01 15:59:21 +0200

    Show experimental stats (#222)

--- website/src/content/config.ts
+++ website/src/content/config.ts
@@ -1,5 +1,4 @@
 export const config = {
   __DEV__showTechnologiesPage: false,
   __DEV__showNewUpdateBanner: true,
-  __DEV__showExperimentalStats: false,
 }
--- website/src/content/pages/Project/props/getLinkSection.ts
+++ website/src/content/pages/Project/props/getLinkSection.ts
@@ -9,15 +9,37 @@ export function getLinkSection(
   l2Data: L2Data
 ): LinkSectionProps {
   const experimental = l2Data.experimental[project.name]
+
+  const tokens = project.bridges
+    .flatMap((x) => x.tokens)
+    .filter((x, i, a) => a.indexOf(x) === i)
+
+  const showNoEth = tokens.includes('ETH')
+  const showInflowsOutflows = tokens.length !== 1 || tokens[0] !== 'ETH'
+
+  const stats: LinkSectionProps['experimentalStats7d'] = {
+    visible: true,
+    showExcludingEth: showNoEth,
+    inflows: showInflowsOutflows
+      ? formatUSD(experimental.usdIn7DayNoEth)
+      : undefined,
+    outflows: showInflowsOutflows
+      ? formatUSD(experimental.usdOut7DayNoEth)
+      : undefined,
+    batchCount: experimental.batchCount7d,
+    transactionCount: experimental.messageCount7d,
+  }
+
+  stats.visible =
+    stats.inflows !== undefined ||
+    stats.outflows !== undefined ||
+    stats.batchCount !== undefined ||
+    stats.transactionCount !== undefined
+
   return {
     links: project.details.links,
     name: project.name,
     icon: `/icons/${project.slug}.png`,
-    experimentalStats7d: {
-      inflowsNoEth: formatUSD(experimental.usdIn7DayNoEth),
-      outflowsNoEth: formatUSD(experimental.usdOut7DayNoEth),
-      batchCount: experimental.batchCount7d,
-      transactionCount: experimental.messageCount7d,
-    },
+    experimentalStats7d: stats,
   }
 }
