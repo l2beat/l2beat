b303de9ec1cca9b570b4641c78b42da367fa8ed4
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-09-06 12:48:16 +0200

    Add toke listing guidelines

--- packages/config/src/tokens.ts
+++ packages/config/src/tokens.ts
@@ -1,3 +1,30 @@
+/*
+
+                         === IMPORTANT REQUIREMENTS ===
+                        Please read before adding tokens
+
+L2BEAT cannot and will not track every possible token. Adding a large number of
+obscure coins will only introduce noise and unnecessary work while not providing
+visible benefits.
+
+Because of this we will enforce the following requirements for adding a token to
+this list:
+
+1. The token MUST be locked inside one of the projects found in `./projects` as
+   reported by etherscan.io
+2. The token MUST satisfy at least one of the following:
+  2.1. The token is associated with the project and marked as `associatedToken`
+  2.2. The token is in the top 300 of coins as reported by coingecko.com
+3. The token SHOULD be listed on Uniswap V2 or V3 with sufficient liquidity. If
+   this is not satisfied getting the token price will be problematic. In the
+   future more exchanges will be supported, e.g. SushiSwap.
+
+Please also note that for the time being we (incorrectly) treat the token symbol
+as the identifier. If your token symbol clashes with one of the existing tokens
+you are out of luck. We will fix this in the future.
+
+*/
+
 export interface TokenInfo {
   /** Token name as dictated by the token contract */
   name: string
