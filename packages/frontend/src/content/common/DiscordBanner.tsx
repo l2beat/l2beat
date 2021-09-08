ea924fa0c6d3021e750f6ffe17a523ee761e3ad7
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-09-08 11:12:25 +0200

    Add Discord invite banner

---
+++ packages/frontend/src/content/common/DiscordBanner.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { OutLink } from './OutLink'
+
+export function DiscordBanner() {
+  return (
+    <OutLink className="DiscordBanner" href="https://discord.gg/eaVKXPmtWk">
+      We have a community Discord server. Join us:{' '}
+      <span>discord.gg/eaVKXPmtWk</span>
+    </OutLink>
+  )
+}
