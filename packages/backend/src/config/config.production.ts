8ebcb60eee83a3095c443c49809e1843b8fe50e6
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-09-16 16:43:59 +0200

    Use env variables in config

--- packages/backend/src/config/config.production.ts
+++ packages/backend/src/config/config.production.ts
@@ -1,10 +1,11 @@
 import { LogLevel } from '../services/Logger'
 import { Config } from './Config'
+import { getEnv } from './getEnv'
 
 export function getProductionConfig(): Config {
   return {
     name: 'Backend/Production',
-    port: 3000,
     logLevel: LogLevel.INFO,
+    port: getEnv.integer('PORT'),
   }
 }
