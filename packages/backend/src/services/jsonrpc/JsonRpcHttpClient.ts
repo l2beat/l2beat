3ab9297726f42b2b05fd1b34e3aeb5066e4db67d
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-09-20 17:39:34 +0200

    Add JsonRpcHttpClient

---
+++ packages/backend/src/services/jsonrpc/JsonRpcHttpClient.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '../HttpClient'
+import { Logger } from '../Logger'
+import { JsonRpcClient } from './JsonRpcClient'
+import { JsonRpcRequest } from './types'
+
+export class JsonRpcHttpClient extends JsonRpcClient {
+  constructor(
+    private url: string,
+    private httpClient: HttpClient,
+    private logger: Logger
+  ) {
+    super()
+    this.logger = this.logger.withName('JsonRpcHttpClient')
+  }
+
+  async execute(request: JsonRpcRequest | JsonRpcRequest[]): Promise<unknown> {
+    const name = Array.isArray(request)
+      ? `batch(${request.length})#${request[0].id}`
+      : `${request.method}#${request.id}`
+    this.logger.debug(`> ${name}`)
+    const res = await this.httpClient.fetch(this.url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(request),
+    })
+    this.logger.debug(`< ${res.status} ${name}`)
+    if (!res.ok) {
+      throw new Error(`Http error ${res.status}: ${await res.text()}`)
+    }
+    try {
+      return await res.json()
+    } catch {
+      throw new TypeError('Invalid JSON received')
+    }
+  }
+}
