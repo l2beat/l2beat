d1dc3e451ed21b06b35bc4c2329f0cc74db949a4
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-09-26 11:58:38 +0200

    Add a better retry algorithm

---
+++ packages/backend/src/services/utils/retry.ts
@@ -0,0 +1,30 @@
+export interface RetryOptions {
+  minTimeout: number
+  maxTimeout?: number
+  maxRetryCount: number
+  onError?: (error: unknown) => void
+}
+
+export async function retry<T>(
+  fn: () => Promise<T>,
+  options: RetryOptions
+): Promise<T> {
+  return new Promise<T>((resolve, reject) => {
+    let callCount = 0
+    const call = () => fn().then(resolve, onError)
+    const onError = (e: unknown) => {
+      options.onError?.(e)
+      callCount++
+      if (callCount > options.maxRetryCount) {
+        reject(e)
+      } else {
+        const ms = Math.min(
+          options.maxTimeout ?? Infinity,
+          options.minTimeout * 2 ** (callCount - 1)
+        )
+        setTimeout(call, ms)
+      }
+    }
+    call()
+  })
+}
