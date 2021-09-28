530b30293a5f6f1c68ab60db0f7e83c92eec8a23
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-09-28 16:40:54 +0200

    Add asBigIntFromString

---
+++ packages/backend/test/services/etherscan/asBigIntFromString.test.ts
@@ -0,0 +1,27 @@
+import { expect } from 'chai'
+
+import { asBigIntFromString } from '../../../src/services/etherscan/asBigIntFromString'
+
+describe('asBigIntFromString', () => {
+  const cases = [
+    { value: '1', expected: 1n },
+    { value: '-1', expected: -1n },
+    { value: '0', expected: 0n },
+    { value: '12345678901234567890', expected: 12345678901234567890n },
+    { value: '', error: 'Value must represent a base 10 integer' },
+    { value: '0x1234', error: 'Value must represent a base 10 integer' },
+    { value: 'foo', error: 'Value must represent a base 10 integer' },
+  ]
+  for (const { value, error, expected } of cases) {
+    if (error) {
+      it(`throws for ${value}`, () => {
+        expect(() => asBigIntFromString(value)).to.throw(TypeError, error)
+      })
+    } else if (expected !== undefined) {
+      it(`reads ${value} as ${expected}`, () => {
+        const result = asBigIntFromString(value)
+        expect(result).to.equal(expected)
+      })
+    }
+  }
+})
