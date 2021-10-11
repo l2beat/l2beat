86c2402c08c6b8a97d0e587dfcce9daccb89ea01
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-10-11 17:46:54 +0200

    Add BlockNumberUpdater

---
+++ packages/backend/src/core/BlockNumberUpdater.ts
@@ -0,0 +1,93 @@
+import { UnixTime } from '../model/UnixTime'
+import {
+  BlockNumberRecord,
+  IBlockNumberRepository,
+} from '../peripherals/database/BlockNumberRepository'
+import { IEtherscanClient } from '../peripherals/etherscan'
+import { createEventEmitter } from '../tools/EventEmitter'
+import { JobQueue } from '../tools/JobQueue'
+import { Logger } from '../tools/Logger'
+import { ISafeBlockService } from './SafeBlockService'
+
+interface BlockNumberEvents {
+  newBlocks: BlockNumberRecord[]
+}
+
+export interface IBlockNumberUpdater {
+  getBlockList(): BlockNumberRecord[]
+  onNewBlocks(fn: (blocks: BlockNumberRecord[]) => void): () => void
+}
+
+export class BlockNumberUpdater implements IBlockNumberUpdater {
+  private events = createEventEmitter<BlockNumberEvents>()
+  private blocks: BlockNumberRecord[] = []
+  private timestamps = new Set<number>()
+  private jobQueue: JobQueue
+  private nextTimestamp: UnixTime
+
+  constructor(
+    private minTimestamp: UnixTime,
+    private safeBlockService: ISafeBlockService,
+    private etherscanClient: IEtherscanClient,
+    private blockNumberRepository: IBlockNumberRepository,
+    private logger: Logger
+  ) {
+    if (!minTimestamp.toStartOf('hour').equals(minTimestamp)) {
+      throw new Error('minTimestamp must be aligned to full hours')
+    }
+    this.nextTimestamp = minTimestamp
+    this.logger = this.logger.for(this)
+    this.jobQueue = new JobQueue({ maxConcurrentJobs: 20 }, this.logger)
+  }
+
+  getBlockList() {
+    return [...this.blocks]
+  }
+
+  onNewBlocks(fn: (blocks: BlockNumberRecord[]) => void) {
+    this.events.on('newBlocks', fn)
+    return () => this.events.off('newBlocks', fn)
+  }
+
+  async start() {
+    const known = await this.blockNumberRepository.getAll()
+    this.addBlocks(known.filter((x) => x.timestamp.gte(this.minTimestamp)))
+
+    const safeBlock = this.safeBlockService.getSafeBlock()
+    this.addNewJobs(safeBlock.timestamp)
+
+    return this.safeBlockService.onNewSafeBlock((safeBlock) => {
+      this.addNewJobs(safeBlock.timestamp)
+    })
+  }
+
+  private addBlocks(blocks: BlockNumberRecord[]) {
+    this.blocks.push(...blocks)
+    for (const { timestamp } of blocks) {
+      this.timestamps.add(timestamp.toNumber())
+    }
+    this.events.emit('newBlocks', blocks)
+  }
+
+  private async addNewJobs(maxTimestamp: UnixTime) {
+    while (this.nextTimestamp.lt(maxTimestamp)) {
+      const jobTimestamp = this.nextTimestamp
+      if (!this.timestamps.has(jobTimestamp.toNumber())) {
+        this.jobQueue.add({
+          name: jobTimestamp.toString(),
+          execute: () => this.getBlockNumber(jobTimestamp),
+        })
+      }
+      this.nextTimestamp = this.nextTimestamp.add(1, 'hours')
+    }
+  }
+
+  private async getBlockNumber(timestamp: UnixTime) {
+    const blockNumber = await this.etherscanClient.getBlockNumberAtOrBefore(
+      timestamp
+    )
+    const block = { timestamp, blockNumber }
+    await this.blockNumberRepository.add(block)
+    this.addBlocks([block])
+  }
+}
