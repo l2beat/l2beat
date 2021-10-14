7a0ffe5ac457955204e499f92188faa07ba9758c
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-10-14 18:09:35 +0200

    Add new row types

--- packages/backend/src/peripherals/database/types.ts
+++ packages/backend/src/peripherals/database/types.ts
@@ -6,7 +6,23 @@ declare module 'knex/types/tables' {
     block_number: number
   }
 
+  interface ExchangePriceRow {
+    block_number: number
+    asset_id: string
+    exchange: string
+    liquidity: string
+    price: string
+  }
+
+  interface AggregatePriceRow {
+    block_number: number
+    asset_id: string
+    price_usd: string
+  }
+
   interface Tables {
     block_numbers: BlockNumberRow
+    exchange_prices: ExchangePriceRow
+    aggregate_prices: AggregatePriceRow
   }
 }
