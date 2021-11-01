5c73a8997ca43b7d707ee9e506f474ef24816ded
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-11-01 16:36:22 +0100

    Remove technologies

--- packages/frontend/src/content/config.ts
+++ packages/frontend/src/content/config.ts
@@ -2,6 +2,5 @@ export const config = {
   twitterLink: 'https://twitter.com/l2beatcom',
   discordLink: 'https://discord.gg/eaVKXPmtWk',
   githubLink: 'https://github.com/l2beat/l2beat',
-  __DEV__showTechnologiesPage: false,
   __DEV__showNewUpdateBanner: false,
 }
