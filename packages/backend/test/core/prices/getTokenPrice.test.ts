cb233edc2aa5348d0205837bafef6ae173221c67
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-11-22 15:16:33 +0100

    Add getTokenPrice

---
+++ packages/backend/test/core/prices/getTokenPrice.test.ts
@@ -0,0 +1,132 @@
+import { expect } from 'chai'
+
+import { getTokenPrice } from '../../../src/core/prices/getTokenPrice'
+import { EthereumAddress, Exchange, Token } from '../../../src/model'
+import { ExchangePriceRecord } from '../../../src/peripherals/database/ExchangePriceRepository'
+
+describe('getTokenPrice', () => {
+  describe('for market tokens', () => {
+    describe('single record', () => {
+      const expectedPrice = 200n * 10n ** 18n
+      const etherPrice = 4_000n * 10n ** 18n
+      const token: Token = {
+        id: 'aaa-token',
+        symbol: 'AAA',
+        decimals: 18,
+        address: EthereumAddress('0x' + 'a'.repeat(40)),
+        priceStrategy: { type: 'market' },
+      }
+      const testCases: ExchangePriceRecord[] = [
+        {
+          blockNumber: 123n,
+          assetId: 'aaa-token',
+          exchange: Exchange.uniswapV1(),
+          liquidity: 100n * 10n ** 18n,
+          price: 10n ** 18n / 20n,
+        },
+        {
+          blockNumber: 123n,
+          assetId: 'aaa-token',
+          exchange: Exchange.uniswapV2('dai'),
+          liquidity: 100n * 10n ** 18n,
+          price: 200n * 10n ** 18n,
+        },
+        {
+          blockNumber: 123n,
+          assetId: 'aaa-token',
+          exchange: Exchange.uniswapV2('usdc'),
+          liquidity: 100n * 10n ** 18n,
+          price: 200n * 10n ** 6n,
+        },
+        {
+          blockNumber: 123n,
+          assetId: 'aaa-token',
+          exchange: Exchange.uniswapV2('usdt'),
+          liquidity: 100n * 10n ** 18n,
+          price: 200n * 10n ** 6n,
+        },
+        {
+          blockNumber: 123n,
+          assetId: 'aaa-token',
+          exchange: Exchange.uniswapV2('weth'),
+          liquidity: 100n * 10n ** 18n,
+          price: 10n ** 18n / 20n,
+        },
+        {
+          blockNumber: 123n,
+          assetId: 'aaa-token',
+          exchange: Exchange.uniswapV3('weth', 3000),
+          liquidity: 100n * 10n ** 18n,
+          price: 10n ** 18n / 20n,
+        },
+      ]
+
+      for (const record of testCases) {
+        it(`${record.exchange.name} - ${record.assetId}`, () => {
+          const price = getTokenPrice(token, [record], etherPrice)
+          expect(price).to.equal(expectedPrice)
+        })
+      }
+    })
+
+    it('chooses the most liquid record', () => {
+      const token: Token = {
+        id: 'aaa-token',
+        symbol: 'AAA',
+        decimals: 18,
+        address: EthereumAddress('0x' + 'a'.repeat(40)),
+        priceStrategy: { type: 'market' },
+      }
+      const records: ExchangePriceRecord[] = [
+        {
+          blockNumber: 123n,
+          assetId: 'aaa-token',
+          exchange: Exchange.uniswapV2('dai'),
+          liquidity: 100n * 10n ** 18n,
+          price: 201n * 10n ** 18n,
+        },
+        {
+          blockNumber: 123n,
+          assetId: 'aaa-token',
+          exchange: Exchange.uniswapV2('usdc'),
+          liquidity: 300n * 10n ** 18n,
+          price: 202n * 10n ** 6n,
+        },
+        {
+          blockNumber: 123n,
+          assetId: 'aaa-token',
+          exchange: Exchange.uniswapV2('usdt'),
+          liquidity: 200n * 10n ** 18n,
+          price: 203n * 10n ** 6n,
+        },
+      ]
+
+      const price = getTokenPrice(token, records, 0n)
+      expect(price).to.equal(202n * 10n ** 18n)
+    })
+  })
+
+  it('works for constant price tokens', () => {
+    const token: Token = {
+      id: 'aaa-token',
+      symbol: 'AAA',
+      decimals: 18,
+      address: EthereumAddress('0x' + 'a'.repeat(40)),
+      priceStrategy: { type: 'constant', value: 1234n },
+    }
+    const price = getTokenPrice(token, [], 0n)
+    expect(price).to.equal(1234n)
+  })
+
+  it('works for ether price tokens', () => {
+    const token: Token = {
+      id: 'aaa-token',
+      symbol: 'AAA',
+      decimals: 18,
+      address: EthereumAddress('0x' + 'a'.repeat(40)),
+      priceStrategy: { type: 'ether' },
+    }
+    const price = getTokenPrice(token, [], 5678n)
+    expect(price).to.equal(5678n)
+  })
+})
