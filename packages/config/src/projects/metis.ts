311e323675f81ec74d1007866a462a446989075b
Pavel Sinelnikov <pavel.sinelnikov@outlook.com>
2021-11-23 22:26:55 -0500

    Add Metis

---
+++ packages/config/src/projects/metis.ts
@@ -0,0 +1,208 @@
+import {
+  CONTRACTS,
+  DATA_AVAILABILITY,
+  EXITS,
+  FORCE_TRANSACTIONS,
+  OPERATOR,
+  RISK_VIEW,
+} from './common'
+import { Project } from './types'
+
+export const metis: Project = {
+  name: 'Metis',
+  slug: 'metis',
+  bridges: [
+    {
+      address: '0x3980c9ed79d2c191A89E02Fa3529C60eD6e9c04b',
+      sinceBlock: 13627429,
+      tokens: ['METIS'],
+    },
+  ],
+  details: {
+    description:
+      'Metis is an EVM-equivalent Optimistic Rollup chain. It aims to be fast, simple, and secure.',
+    purpose: 'Universal',
+    links: {
+      websites: ['https://www.metis.io'],
+      apps: [],
+      documentation: ['https://docs.metis.io'],
+      explorers: ['https://andromeda-explorer.metis.io'],
+      repositories: ['https://github.com/MetisProtocol'],
+      socialMedia: [
+        'https://metisdao.medium.com',
+        'https://twitter.com/MetisDAO',
+        'https://discord.gg/RqfEJZXnxd',
+        'https://youtube.com/MetisDAO',
+        'https://t.me/MetisDAO',
+      ],
+    },
+    riskView: {
+      stateValidation: {
+        value: 'Proofs disabled',
+        description:
+          'Currently the system permits invalid state roots. More details in project overview.',
+        sentiment: 'bad',
+      },
+      dataAvailability: RISK_VIEW.DATA_ON_CHAIN,
+      upgradeability: RISK_VIEW.UPGRADABLE_NO,
+      sequencerFailure: RISK_VIEW.SEQUENCER_TRANSACT_L1,
+      validatorFailure: RISK_VIEW.VALIDATOR_WHITELISTED_BLOCKS,
+    },
+    technology: {
+      category: {
+        name: 'Optimistic Rollup',
+      },
+      stateCorrectness: {
+        name: 'Fraud proofs are disabled',
+        description:
+          'Metis will use fraud proofs to enforce state correctness. This feature is currently disabled and the system permits invalid state roots.',
+        risks: [
+          {
+            category: 'Funds can be stolen if',
+            text: 'an invalid state root is submitted to the system.',
+            isCritical: true,
+          },
+        ],
+        references: [
+          {
+            text: '',
+            href: '',
+          },
+        ],
+      },
+      dataAvailability: {
+        ...DATA_AVAILABILITY.ON_CHAIN_CANONICAL,
+        references: [
+          {
+            text: '',
+            href: '',
+          },
+        ],
+      },
+      operator: {
+        ...OPERATOR.CENTRALIZED_SEQUENCER,
+        references: [
+          {
+            text: '',
+            href: '',
+          },
+        ],
+      },
+      forceTransactions: {
+        ...FORCE_TRANSACTIONS.CANONICAL_ORDERING,
+        references: [
+          {
+            text: '',
+            href: '',
+          },
+        ],
+      },
+      exitMechanisms: [
+        {
+          ...EXITS.REGULAR('optimistic', 'merkle proof'),
+          references: [
+            {
+              text: '',
+              href: '',
+            },
+          ],
+          risks: [
+            {
+              ...EXITS.RISK_CENTRALIZED_VALIDATOR,
+              references: [
+                {
+                  text: '',
+                  href: '',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+      smartContracts: {
+        name: 'EVM compatible smart contracts are supported',
+        description:
+          'Metis uses the Optimistic Virtual Machine (OVM) 2.0 to execute transactions. This is similar to the EVM, but is independent from it and allows fraud proofs to be executed.',
+        risks: [
+          {
+            category: 'Funds can be lost if',
+            text: 'there are mistakes in the highly complex OVM implementation.',
+          },
+        ],
+        references: [
+          {
+            text: '',
+            href: '',
+          }
+        ],
+      },
+      contracts: {
+        addresses: [
+          {
+            name: 'BondManager',
+            address: '0xf51B9C9a1c12e7E48BEC15DC358D0C1f0d7Eb3be',
+          },
+          {
+            name: 'CanonicalTransactionChain',
+            address: '0x56a76bcC92361f6DF8D75476feD8843EdC70e1C9',
+          },
+          {
+            name: 'ChainStorageContainer-CTC-batches',
+            address: '0x38473Feb3A6366757A249dB2cA4fBB2C663416B7',
+          },
+          {
+            name: 'ChainStorageContainer-CTC-queue',
+            address: '0xA91Ea6F5d1EDA8e6686639d6C88b309cF35D2E57',
+          },
+          {
+            name: 'ChainStorageContainer-SCC-batches',
+            address: '0x10739F09f6e62689c0aA8A1878816de9e166d6f9',
+          },
+          {
+            name: 'L1StandardBridge_for_verification_only',
+            address: '0x101500214981e7A5Ad2334D8404eaF365C2c3113',
+          },
+          {
+            name: 'AddressManager',
+            address: '0x918778e825747a892b17C66fe7D24C618262867d',
+          },
+          {
+            name: 'MVM_DiscountOracle',
+            address: '0xC8953ca384b4AdC8B1b11B030Afe2F05471664b0',
+          },
+          {
+            name: 'MVM_L2ChainManagerOnL1_for_verification_only',
+            address: '0x9E2E3be85df5Ca63DE7674BA64ffD564075f3B48',
+          },
+          {
+            name: 'MVM_Verifier',
+            address: '0x9Ed4739afd706122591E75F215208ecF522C0Fd3',
+          },
+          {
+            name: 'OVM_L1CrossDomainMessenger',
+            address: '0x8bF439ef7167023F009E24b21719Ca5f768Ecb36',
+          },
+          {
+            name: 'Proxy__MVM_ChainManager',
+            address: '0xf3d58D1794f2634d6649a978f2dc093898FEEBc0',
+          },
+          {
+            name: 'Proxy__OVM_L1CrossDomainMessenger',
+            address: '0x081D1101855bD523bA69A9794e0217F0DB6323ff',
+          },
+          {
+            name: 'Proxy__OVM_L1StandardBridge',
+            address: '0x3980c9ed79d2c191A89E02Fa3529C60eD6e9c04b',
+          },
+          {
+            name: 'OVM_StateCommitmentChain',
+            address: '0xf209815E595Cdf3ed0aAF9665b1772e608AB9380',
+          },
+          
+        ],
+        risks: [],
+      },
+    },
+    news: [],
+  },
+}
--- packages/config/src/tokens.ts
+++ packages/config/src/tokens.ts
@@ -375,6 +375,13 @@ export const tokenList: TokenInfo[] = [
     sinceBlock: 7130380,
   },
   {
+    name: 'Metis Token',
+    symbol: 'METIS',
+    address: '0x9e32b13ce7f2e80a01932b42553652e053d6ed8e',
+    decimals: 18,
+    sinceBlock: 11434969,
+  },
+  {
     name: 'OKB',
     symbol: 'OKB',
     address: '0x75231F58b43240C9718Dd58B4967c5114342a86c',
