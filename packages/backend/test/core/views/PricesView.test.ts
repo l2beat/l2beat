9d562171c2303040d6539d8e704b08d97933e051
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-11-25 16:19:29 +0100

    Add PricesView

---
+++ packages/backend/test/core/views/PricesView.test.ts
@@ -0,0 +1,57 @@
+import { expect } from 'chai'
+
+import { PricesView } from '../../../src/core/views/PricesView'
+import { AssetId, Exchange } from '../../../src/model'
+import { AggregatePriceRepository } from '../../../src/peripherals/database/AggregatePriceRepository'
+import { ExchangePriceRepository } from '../../../src/peripherals/database/ExchangePriceRepository'
+import { mock } from '../../mock'
+
+describe('PricesView', () => {
+  it('returns transformed aggregate prices', async () => {
+    const aggregatePriceRepository = mock<AggregatePriceRepository>({
+      async getAllByAssetId(assetId) {
+        expect(assetId).to.equal(AssetId.DAI)
+        return [
+          { blockNumber: 1n, priceUsd: 2n },
+          { blockNumber: 3n, priceUsd: 4n },
+        ]
+      },
+    })
+    const exchangePriceRepository = mock<ExchangePriceRepository>()
+    const pricesView = new PricesView(
+      exchangePriceRepository,
+      aggregatePriceRepository
+    )
+    expect(await pricesView.getPriceHistory(AssetId.DAI)).to.deep.equal([
+      { blockNumber: '1', priceUsd: '2' },
+      { blockNumber: '3', priceUsd: '4' },
+    ])
+  })
+
+  it('returns transformed exchange prices', async () => {
+    const aggregatePriceRepository = mock<AggregatePriceRepository>()
+    const exchangePriceRepository = mock<ExchangePriceRepository>({
+      async getAllByAssetIdAndExchange(assetId, exchange) {
+        expect(assetId).to.equal(AssetId.DAI)
+        expect(exchange).to.deep.equal(Exchange.uniswapV2('weth'))
+        return [
+          { blockNumber: 1n, price: 2n, liquidity: 3n },
+          { blockNumber: 4n, price: 5n, liquidity: 6n },
+        ]
+      },
+    })
+    const pricesView = new PricesView(
+      exchangePriceRepository,
+      aggregatePriceRepository
+    )
+    expect(
+      await pricesView.getPriceHistoryOnExchange(
+        AssetId.DAI,
+        Exchange.uniswapV2('weth')
+      )
+    ).to.deep.equal([
+      { blockNumber: '1', price: '2', liquidity: '3' },
+      { blockNumber: '4', price: '5', liquidity: '6' },
+    ])
+  })
+})
