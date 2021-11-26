5f326ac4d70b2bfdbfa647317e8fbacbd02a2a10
Tim Haines <tmhaines@gmail.com>
2021-11-26 13:34:52 +1300

    Fix trivial typo "a" -> "an" on about section

--- packages/frontend/src/content/pages/Home/view/About.tsx
+++ packages/frontend/src/content/pages/Home/view/About.tsx
@@ -7,7 +7,7 @@ export function About() {
     <section className="About">
       <Heading level={2} id="about" title="About L2BEAT" />
       <p>
-        L2BEAT is a analytics and research website about Ethereum layer two (L2)
+        L2BEAT is an analytics and research website about Ethereum layer two (L2)
         scaling. We provide a comparison of the various Ethereum L2 systems
         available today.
       </p>
