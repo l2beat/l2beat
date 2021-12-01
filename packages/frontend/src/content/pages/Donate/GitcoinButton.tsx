550dc6ebd4259531ea2823feeee70218f2665ac5
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-12-01 19:52:07 +0100

    Add Gitcoin grant information (#345)

---
+++ packages/frontend/src/content/pages/Donate/GitcoinButton.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { OutLink } from '../../common'
+
+export function GitcoinButton() {
+  return (
+    <OutLink
+      className="GitcoinButton"
+      href="https://gitcoin.co/grants/3857/l2beat"
+    >
+      Donate using Gitcoin
+    </OutLink>
+  )
+}
