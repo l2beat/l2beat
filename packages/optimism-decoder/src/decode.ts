d05db67cf8a3b9bc870fabbba783651f81e97ed0
Bartek Kiepuszewski <bkiepuszewski@gmail.com>
2021-12-17 14:12:29 +0100

    Add ByteReader

--- packages/optimism-decoder/src/decode.ts
+++ packages/optimism-decoder/src/decode.ts
@@ -1,11 +1,12 @@
 import { ethers } from 'ethers'
 
+import { ByteReader } from './ByteReader'
 import { FourBytesApi } from './FourBytesApi'
 import { add0x, remove0x, trimLong } from './utils'
 
 interface BatchContext {
-  numSequencedTransactions: number
-  numSubsequentQueueTransactions: number
+  sequencerTxCount: number
+  queueTxCount: number
   timestamp: number
   blockNumber: number
 }
@@ -14,67 +15,51 @@ interface AppendSequencerBatchParams {
   shouldStartAtElement: number // 5 bytes -- starts at batch
   totalElementsToAppend: number // 3 bytes -- total_elements_to_append
   contexts: BatchContext[] // total_elements[fixed_size[]]
-  transactions: string[] // total_size_bytes[],total_size_bytes[]
+  transactions: string[] // total_size_bytes[], total_size_bytes[]
 }
 
 export async function decodeSequencerBatch(
   kind: string,
-  b: string,
+  data: string,
   fourBytesApi: FourBytesApi
 ): Promise<AppendSequencerBatchParams> {
   console.log('Decoding', kind, 'L1 Sequencer transaction batch...')
+  const reader = new ByteReader(data)
 
-  b = remove0x(b)
-  const methodName = b.slice(0, 8)
-  const shouldStartAtElement = b.slice(8, 18)
-  const totalElementsToAppend = b.slice(18, 24)
-  const contextHeader = b.slice(24, 30)
-  const contextCount = parseInt(contextHeader, 16)
+  const methodName = reader.getBytes(4)
+  const shouldStartAtElement = reader.getNumber(5)
+  const totalElementsToAppend = reader.getNumber(3)
+  const contextCount = reader.getNumber(3)
 
-  console.log('MethodName:', '0x' + methodName)
-  console.log(
-    'Should start at Element:',
-    shouldStartAtElement,
-    parseInt(shouldStartAtElement, 16)
-  )
-  console.log('Total Elements to Append:', parseInt(totalElementsToAppend, 16))
-  console.log('ContextHeader:', contextHeader, contextCount)
+  console.log('MethodName:', methodName)
+  console.log('Should start at Element:', shouldStartAtElement)
+  console.log('Total Elements to Append:', totalElementsToAppend)
+  console.log('contextCount:', contextCount)
 
-  let offset = 30
   const contexts = []
   for (let i = 0; i < contextCount; i++) {
-    const numSequencedTransactions = b.slice(offset, offset + 6)
-    offset += 6
-    const numSubsequentQueueTransactions = b.slice(offset, offset + 6)
-    offset += 6
-    const timestamp = b.slice(offset, offset + 10)
-    offset += 10
-    const blockNumber = b.slice(offset, offset + 10)
-    offset += 10
+    const sequencerTxCount = reader.getNumber(3)
+    const queueTxCount = reader.getNumber(3)
+    const timestamp = reader.getNumber(5)
+    const blockNumber = reader.getNumber(5)
     contexts.push({
-      numSequencedTransactions: parseInt(numSequencedTransactions, 16),
-      numSubsequentQueueTransactions: parseInt(
-        numSubsequentQueueTransactions,
-        16
-      ),
-      timestamp: parseInt(timestamp, 16),
-      blockNumber: parseInt(blockNumber, 16),
+      sequencerTxCount,
+      queueTxCount,
+      timestamp,
+      blockNumber,
     })
   }
 
   const transactions = []
   for (const context of contexts) {
     console.log('Block:', context.blockNumber, 'Timestamp:', context.timestamp)
-    for (let i = 0; i < context.numSequencedTransactions; i++) {
-      const size = b.slice(offset, offset + 6)
-      offset += 6
-      const raw = b.slice(offset, offset + parseInt(size, 16) * 2)
-      const t = ethers.utils.parseTransaction(add0x(raw))
-      const methodHash = t.data.slice(0, 10)
+    for (let i = 0; i < context.sequencerTxCount; i++) {
+      const size = reader.getNumber(3)
+      const raw = reader.getBytes(size)
+      const parsed = ethers.utils.parseTransaction(add0x(raw))
+      const methodHash = parsed.data.slice(0, 10)
       const methodSignature = await fourBytesApi.getMethodSignature(methodHash)
       transactions.push(add0x(raw))
-      offset += raw.length
-
       console.log('  ', trimLong(add0x(raw)), methodHash, methodSignature)
     }
   }
@@ -82,10 +67,9 @@ export async function decodeSequencerBatch(
   console.log('Decoded', transactions.length, 'transactions')
   console.log('Done decoding...')
 
-  //console.log(ethers.utils.parseTransaction(transactions[0]))
   return {
-    shouldStartAtElement: parseInt(shouldStartAtElement, 16),
-    totalElementsToAppend: parseInt(totalElementsToAppend, 16),
+    shouldStartAtElement,
+    totalElementsToAppend,
     contexts,
     transactions,
   }
