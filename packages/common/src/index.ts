91ba40dd4b55c2a5eee65f6756769587cdb014c0
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-12-27 12:48:02 +0100

    Add AddressAnalyzer

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,3 +1,4 @@
+export * from './core'
 export * from './model'
 export * from './peripherals'
 export * from './tools'
---
+++ packages/common/test/core/AddressAnalyzer.test.ts
@@ -0,0 +1,63 @@
+import { expect } from 'chai'
+import { providers } from 'ethers'
+import {
+  AddressAnalyzer,
+  EthereumAddress,
+  EtherscanClient,
+  mock,
+} from '../../src'
+
+describe('AddressAnalyzer', () => {
+  it('can detect an EOA', async () => {
+    const provider = mock<providers.Provider>({
+      getCode: async () => '0x',
+    })
+    const etherscanClient = mock<EtherscanClient>({
+      getContractSource: async () => ({} as any),
+    })
+    const addressAnalyzer = new AddressAnalyzer(provider, etherscanClient)
+    const result = await addressAnalyzer.analyze(
+      EthereumAddress('0x11223344556677889900aabbccddeeff11223344')
+    )
+    expect(result).to.deep.equal({ type: 'EOA', name: '<EOA 11223344>' })
+  })
+
+  it('can detect an unverified contract', async () => {
+    const provider = mock<providers.Provider>({
+      getCode: async () => '0x112233',
+    })
+    const etherscanClient = mock<EtherscanClient>({
+      getContractSource: async () =>
+        ({ ABI: 'Contract source code not verified' } as any),
+    })
+    const addressAnalyzer = new AddressAnalyzer(provider, etherscanClient)
+    const result = await addressAnalyzer.analyze(
+      EthereumAddress('0x11223344556677889900aabbccddeeff11223344')
+    )
+    expect(result).to.deep.equal({
+      type: 'Contract',
+      verified: false,
+      name: '<Unverified 11223344>',
+    })
+  })
+
+  it('can detect a verified contract', async () => {
+    const provider = mock<providers.Provider>({
+      getCode: async () => '0x112233',
+    })
+    const etherscanClient = mock<EtherscanClient>({
+      getContractSource: async () =>
+        ({ ABI: '[{"type":"string"}]', ContractName: 'Foo' } as any),
+    })
+    const addressAnalyzer = new AddressAnalyzer(provider, etherscanClient)
+    const result = await addressAnalyzer.analyze(
+      EthereumAddress('0x11223344556677889900aabbccddeeff11223344')
+    )
+    expect(result).to.deep.equal({
+      type: 'Contract',
+      verified: true,
+      name: 'Foo',
+      abi: [{ type: 'string' }],
+    })
+  })
+})
