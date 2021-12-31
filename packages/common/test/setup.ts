5b636538fb1dd2fa63653a9bf6a6285389370bfe
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-12-31 13:24:55 +0100

    Remove chai and chai-as-promised

--- packages/common/test/setup.ts
+++ packages/common/test/setup.ts
@@ -1,10 +1,6 @@
-import chai from 'chai'
-import chaiAsPromised from 'chai-as-promised'
 import waitForExpect from 'wait-for-expect'
 
 process.env.NODE_ENV = 'test'
-
-chai.use(chaiAsPromised)
 
 waitForExpect.defaults.timeout = 1500
 waitForExpect.defaults.interval = 10
