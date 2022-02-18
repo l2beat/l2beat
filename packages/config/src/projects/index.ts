a4851371a558633fe89878d63ea3387a2c7a1aec
Antoni Pawlak <antoni.pawlak@protonmail.com>
2022-02-18 16:24:49 +0100

    ZKSpace rebranding

--- packages/config/src/projects/index.ts
+++ packages/config/src/projects/index.ts
@@ -16,8 +16,8 @@ import { optimism } from './optimism'
 import { sorare } from './sorare'
 import { starknet } from './starknet'
 import { Project } from './types'
+import { zkspace } from './zkspace'
 import { zkswap } from './zkswap'
-import { zkswapv2 } from './zkswapv2'
 import { zksync } from './zksync'
 
 export * from './types'
@@ -41,6 +41,6 @@ export const projects: Project[] = [
   sorare,
   starknet,
   zkswap,
-  zkswapv2,
+  zkspace,
   zksync,
 ]
