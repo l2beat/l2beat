8cb90a580c86056e114f450ede4eee072da1cfda
Bartek Kiepuszewski <bkiepuszewski@gmail.com>
2022-03-15 09:18:55 +0100

    Add optimism check (#420)
    * Initial commit
    
    * Format printout
    
    * Add boba metis and nahmii configs
    
    * Add expected addresses
    
    * Fix format
    
    * Add main bridge info
    
    * Fix format
    
    * Fix small error
    
    * Fix boba deployment info|
    
    * Fix for linter
    
    * Fix format
    
    * Add parameters description
    
    * Analyse main bridge

---
+++ packages/optimism-check/src/config/Config.ts
@@ -0,0 +1,46 @@
+export type Parameter = ConstantParameter | FixedParameter | VariableParameter
+
+export interface ConstantParameter {
+  type: 'constant'
+  name: string
+  description?: string
+  value: string | number
+}
+
+export interface FixedParameter {
+  type: 'fixed'
+  name: string
+  description?: string
+  abi: string
+}
+
+export interface VariableParameter {
+  type: 'variable'
+  name: string
+  description?: string
+  abi: string
+}
+
+export interface ContractDescription {
+  expectedAddress?: string
+  dependencies?: string[]
+  parameters?: Parameter[]
+}
+
+export interface Contracts {
+  [name: string]: ContractDescription
+}
+
+export interface MainBridgeConfig {
+  type: string
+  proxyAddress: string
+  implementation: string
+  parameters?: Parameter[]
+}
+
+export interface Config {
+  mainBridge: MainBridgeConfig
+  libAddressManager: string
+  startingPoints: string[]
+  contracts: Contracts
+}
