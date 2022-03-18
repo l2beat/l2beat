c00bde62efe0626bcd39fc2815d61a643ad11b34
Antoni Pawlak <antoni.pawlak@protonmail.com>
2022-03-18 10:54:34 +0100

    Refactor BlockNumberUpdater (#425)
    * Remove SafeBlockService
    
    * Refactor BlockNumberUpdater,
    
    * Remove unused code

--- packages/backend/src/Application.ts
+++ packages/backend/src/Application.ts
@@ -3,17 +3,9 @@ import { Logger } from '@l2beat/common'
 import { ApiServer } from './api/ApiServer'
 import { BlocksController } from './api/controllers/BlocksController'
 import { createBlocksRouter } from './api/routers/BlocksRouter'
-import { createStatusRouter } from './api/routers/StatusRouter'
 import { Config } from './config'
-import { BlockNumberUpdater } from './core/BlockNumberUpdater'
-import { SafeBlockService } from './core/SafeBlockService'
-import { StatusService } from './core/StatusService'
 import { BlockNumberRepository } from './peripherals/database/BlockNumberRepository'
 import { DatabaseService } from './peripherals/database/DatabaseService'
-import { AlchemyHttpClient } from './peripherals/ethereum/AlchemyHttpClient'
-import { EthereumClient } from './peripherals/ethereum/EthereumClient'
-import { EtherscanClient } from './peripherals/etherscan'
-import { HttpClient } from './peripherals/HttpClient'
 
 export class Application {
   start: () => Promise<void>
@@ -29,50 +21,12 @@ export class Application {
     const databaseService = new DatabaseService(knex, logger)
     const blockNumberRepository = new BlockNumberRepository(knex, logger)
 
-    const httpClient = new HttpClient()
-    const alchemyHttpClient = new AlchemyHttpClient(
-      config.alchemyApiKey,
-      httpClient,
-      logger
-    )
-    const ethereumClient = new EthereumClient(alchemyHttpClient)
-    const etherscanClient = new EtherscanClient(
-      config.etherscanApiKey,
-      httpClient,
-      logger
-    )
-
-    /* - - - - - CORE - - - - - */
-
-    const safeBlockService = new SafeBlockService(
-      config.core.safeBlockRefreshIntervalMs,
-      config.core.safeBlockBlockOffset,
-      ethereumClient,
-      logger
-    )
-    const blockNumberUpdater = new BlockNumberUpdater(
-      config.core.minBlockTimestamp,
-      safeBlockService,
-      etherscanClient,
-      blockNumberRepository,
-      logger
-    )
-
-    const statusService = new StatusService({
-      alchemyHttpClient,
-      blockNumberUpdater,
-      databaseService,
-      etherscanClient,
-      safeBlockService,
-    })
-
     /* - - - - - API - - - - - */
 
     const blocksController = new BlocksController(blockNumberRepository)
 
     const apiServer = new ApiServer(config.port, logger, [
       createBlocksRouter(blocksController),
-      createStatusRouter(statusService),
     ])
 
     /* - - - - - START - - - - - */
@@ -83,9 +37,6 @@ export class Application {
       await databaseService.migrateToLatest()
 
       await apiServer.listen()
-
-      await safeBlockService.start()
-      await blockNumberUpdater.start()
 
       logger.for(this).info('Started')
     }
--- packages/backend/test/core/BlockNumberUpdater.test.ts
+++ packages/backend/test/core/BlockNumberUpdater.test.ts
@@ -1,232 +1,101 @@
 import { Logger, mock, UnixTime } from '@l2beat/common'
-import { expect } from 'earljs'
-import waitForExpect from 'wait-for-expect'
+import { expect, mockFn } from 'earljs'
 
 import { BlockNumberUpdater } from '../../src/core/BlockNumberUpdater'
-import { SafeBlock, SafeBlockService } from '../../src/core/SafeBlockService'
-import {
-  BlockNumberRecord,
-  BlockNumberRepository,
-} from '../../src/peripherals/database/BlockNumberRepository'
-import { EtherscanClient } from '../../src/peripherals/etherscan'
+import { BlockNumberRepository } from '../../src/peripherals/database/BlockNumberRepository'
+import { EtherscanClient } from '../../src/peripherals/etherscan/EtherscanClient'
 
 describe(BlockNumberUpdater.name, () => {
-  it('requires minTimestamp to be hour aligned', () => {
-    const time = UnixTime.now().toStartOf('hour').add(1, 'seconds')
-    expect(
-      () =>
-        new BlockNumberUpdater(
-          time,
-          mock<SafeBlockService>(),
-          mock<EtherscanClient>(),
-          mock<BlockNumberRepository>(),
-          Logger.SILENT
-        )
-    ).toThrow(Error, 'minTimestamp must be aligned to full hours')
-  })
+  describe(BlockNumberUpdater.prototype.update.name, () => {
+    const START = UnixTime.fromDate(new Date('2021-09-07T00:00:00Z'))
+    const START_BN = 1000n
 
-  it('only considers blocks after minTimestamp', async () => {
-    const minTimestamp = UnixTime.fromDate(new Date('2021-09-07T00:00:00Z'))
-    const safeBlockService = mock<SafeBlockService>({
-      getSafeBlock() {
-        return {
-          blockNumber: 610n,
-          timestamp: minTimestamp.add(2, 'hours').add(5, 'minutes'),
-        }
-      },
-      onNewSafeBlock: () => () => {},
-    })
-    const blockNumberRepository = mock<BlockNumberRepository>({
-      async getAll() {
-        return [
-          { blockNumber: 300n, timestamp: minTimestamp.add(-1, 'hours') },
-          { blockNumber: 400n, timestamp: minTimestamp },
-          { blockNumber: 500n, timestamp: minTimestamp.add(1, 'hours') },
-          { blockNumber: 600n, timestamp: minTimestamp.add(2, 'hours') },
-        ]
-      },
+    it('all known blocks', async () => {
+      const etherscan = mock<EtherscanClient>({
+        getBlockNumberAtOrBefore: mockFn().returns([]),
+      })
+      const blockNumberRepository = mock<BlockNumberRepository>({
+        getAll: mockFn().returns([
+          { timestamp: START, blockNumber: START_BN },
+          { timestamp: START.add(1, 'hours'), blockNumber: START_BN + 100n },
+          { timestamp: START.add(2, 'hours'), blockNumber: START_BN + 200n },
+        ]),
+        add: mockFn().returns([]),
+      })
+      const timestamps = [START, START.add(1, 'hours'), START.add(2, 'hours')]
+      const blockNumberUpdater = new BlockNumberUpdater(
+        etherscan,
+        blockNumberRepository,
+        Logger.SILENT
+      )
+
+      const result = await blockNumberUpdater.update(timestamps)
+
+      expect(result).toEqual([START_BN, START_BN + 100n, START_BN + 200n])
+      expect(etherscan.getBlockNumberAtOrBefore).toHaveBeenCalledExactlyWith([])
+      expect(blockNumberRepository.add).toHaveBeenCalledExactlyWith([])
     })
 
-    const blockNumberUpdater = new BlockNumberUpdater(
-      minTimestamp,
-      safeBlockService,
-      mock<EtherscanClient>(),
-      blockNumberRepository,
-      Logger.SILENT
-    )
+    it('all unknown blocks', async () => {
+      const etherscan = mock<EtherscanClient>({
+        getBlockNumberAtOrBefore: mockFn()
+          .returnsOnce(START_BN)
+          .returnsOnce(START_BN + 100n)
+          .returnsOnce(START_BN + 200n),
+      })
+      const blockNumberRepository = mock<BlockNumberRepository>({
+        getAll: mockFn().returns([]),
+        add: mockFn().returns([]),
+      })
+      const timestamps = [START, START.add(1, 'hours'), START.add(2, 'hours')]
+      const blockNumberUpdater = new BlockNumberUpdater(
+        etherscan,
+        blockNumberRepository,
+        Logger.SILENT
+      )
 
-    await blockNumberUpdater.start()
-    const blocks = blockNumberUpdater.getBlockList()
-    expect(blocks).toEqual([
-      { blockNumber: 400n, timestamp: minTimestamp },
-      { blockNumber: 500n, timestamp: minTimestamp.add(1, 'hours') },
-      { blockNumber: 600n, timestamp: minTimestamp.add(2, 'hours') },
-    ])
-  })
+      const result = await blockNumberUpdater.update(timestamps)
 
-  it('can fetch all unknown blocks', async () => {
-    const minTimestamp = UnixTime.fromDate(new Date('2021-09-07T00:00:00Z'))
-    const safeBlockService = mock<SafeBlockService>({
-      getSafeBlock() {
-        return {
-          blockNumber: 610n,
-          timestamp: minTimestamp.add(2, 'hours').add(5, 'minutes'),
-        }
-      },
-      onNewSafeBlock: () => () => {},
+      expect(result).toEqual([START_BN, START_BN + 100n, START_BN + 200n])
+      expect(etherscan.getBlockNumberAtOrBefore).toHaveBeenCalledExactlyWith([
+        [START],
+        [START.add(1, 'hours')],
+        [START.add(2, 'hours')],
+      ])
+      expect(blockNumberRepository.add).toHaveBeenCalledExactlyWith([
+        [{ timestamp: START, blockNumber: START_BN }],
+        [{ timestamp: START.add(1, 'hours'), blockNumber: START_BN + 100n }],
+        [{ timestamp: START.add(2, 'hours'), blockNumber: START_BN + 200n }],
+      ])
     })
-    const etherscanClient = mock<EtherscanClient>({
-      async getBlockNumberAtOrBefore(timestamp) {
-        if (timestamp.equals(minTimestamp)) {
-          return 400n
-        } else if (timestamp.equals(minTimestamp.add(1, 'hours'))) {
-          return 500n
-        } else if (timestamp.equals(minTimestamp.add(2, 'hours'))) {
-          return 600n
-        }
-        return -1n
-      },
+    it('mixed: known and unknown blocks', async () => {
+      const etherscan = mock<EtherscanClient>({
+        getBlockNumberAtOrBefore: mockFn().returnsOnce(START_BN + 200n),
+      })
+
+      const blockNumberRepository = mock<BlockNumberRepository>({
+        getAll: mockFn().returns([
+          { timestamp: START, blockNumber: START_BN },
+          { timestamp: START.add(1, 'hours'), blockNumber: START_BN + 100n },
+        ]),
+        add: mockFn().returns([]),
+      })
+      const timestamps = [START, START.add(1, 'hours'), START.add(2, 'hours')]
+      const blockNumberUpdater = new BlockNumberUpdater(
+        etherscan,
+        blockNumberRepository,
+        Logger.SILENT
+      )
+
+      const result = await blockNumberUpdater.update(timestamps)
+
+      expect(result).toEqual([START_BN, START_BN + 100n, START_BN + 200n])
+      expect(etherscan.getBlockNumberAtOrBefore).toHaveBeenCalledExactlyWith([
+        [START.add(2, 'hours')],
+      ])
+      expect(blockNumberRepository.add).toHaveBeenCalledExactlyWith([
+        [{ timestamp: START.add(2, 'hours'), blockNumber: START_BN + 200n }],
+      ])
     })
-    const blockNumberRepository = mock<BlockNumberRepository>({
-      async getAll() {
-        return []
-      },
-      async add() {},
-    })
-
-    const blockNumberUpdater = new BlockNumberUpdater(
-      minTimestamp,
-      safeBlockService,
-      etherscanClient,
-      blockNumberRepository,
-      Logger.SILENT
-    )
-
-    await blockNumberUpdater.start()
-
-    await waitForExpect(() => {
-      expect(blockNumberUpdater.getBlockList().length).toEqual(3)
-    })
-
-    const blocks = blockNumberUpdater.getBlockList()
-    expect(blocks).toEqual([
-      { blockNumber: 400n, timestamp: minTimestamp },
-      { blockNumber: 500n, timestamp: minTimestamp.add(1, 'hours') },
-      { blockNumber: 600n, timestamp: minTimestamp.add(2, 'hours') },
-    ])
-  })
-
-  it('emits events when new blocks are fetched', async () => {
-    const minTimestamp = UnixTime.fromDate(new Date('2021-09-07T00:00:00Z'))
-    const events: BlockNumberRecord[][] = []
-    const safeBlockService = mock<SafeBlockService>({
-      getSafeBlock() {
-        return {
-          blockNumber: 610n,
-          timestamp: minTimestamp.add(2, 'hours').add(5, 'minutes'),
-        }
-      },
-      onNewSafeBlock: () => () => {},
-    })
-    const etherscanClient = mock<EtherscanClient>({
-      async getBlockNumberAtOrBefore(timestamp) {
-        if (timestamp.equals(minTimestamp)) {
-          return 400n
-        } else if (timestamp.equals(minTimestamp.add(1, 'hours'))) {
-          return 500n
-        } else if (timestamp.equals(minTimestamp.add(2, 'hours'))) {
-          return 600n
-        }
-        return -1n
-      },
-    })
-    const blockNumberRepository = mock<BlockNumberRepository>({
-      async getAll() {
-        return []
-      },
-      async add() {},
-    })
-
-    const blockNumberUpdater = new BlockNumberUpdater(
-      minTimestamp,
-      safeBlockService,
-      etherscanClient,
-      blockNumberRepository,
-      Logger.SILENT
-    )
-
-    await blockNumberUpdater.start()
-    blockNumberUpdater.onNewBlocks((event) => events.push(event))
-
-    await waitForExpect(() => {
-      expect(blockNumberUpdater.getBlockList().length).toEqual(3)
-    })
-
-    expect(events).toEqual([
-      [{ blockNumber: 400n, timestamp: minTimestamp }],
-      [{ blockNumber: 500n, timestamp: minTimestamp.add(1, 'hours') }],
-      [{ blockNumber: 600n, timestamp: minTimestamp.add(2, 'hours') }],
-    ])
-  })
-
-  it('can fetch new blocks after safe block updates', async () => {
-    const minTimestamp = UnixTime.fromDate(new Date('2021-09-07T00:00:00Z'))
-    let callback: ((safeBlock: SafeBlock) => void) | undefined
-    const safeBlockService = mock<SafeBlockService>({
-      getSafeBlock() {
-        return {
-          blockNumber: 410n,
-          timestamp: minTimestamp.add(5, 'minutes'),
-        }
-      },
-      onNewSafeBlock: (cb) => {
-        callback = cb
-        return () => {}
-      },
-    })
-    const etherscanClient = mock<EtherscanClient>({
-      async getBlockNumberAtOrBefore(timestamp) {
-        if (timestamp.equals(minTimestamp.add(1, 'hours'))) {
-          return 500n
-        }
-        return -1n
-      },
-    })
-    const blockNumberRepository = mock<BlockNumberRepository>({
-      async getAll() {
-        return [{ blockNumber: 400n, timestamp: minTimestamp }]
-      },
-      async add() {},
-    })
-
-    const blockNumberUpdater = new BlockNumberUpdater(
-      minTimestamp,
-      safeBlockService,
-      etherscanClient,
-      blockNumberRepository,
-      Logger.SILENT
-    )
-
-    await blockNumberUpdater.start()
-
-    const earlyBlocks = blockNumberUpdater.getBlockList()
-    expect(earlyBlocks).toEqual([
-      { blockNumber: 400n, timestamp: minTimestamp },
-    ])
-
-    callback?.({
-      blockNumber: 510n,
-      timestamp: minTimestamp.add(1, 'hours').add(5, 'minutes'),
-    })
-
-    await waitForExpect(() => {
-      expect(blockNumberUpdater.getBlockList().length).toEqual(2)
-    })
-
-    const lateBlocks = blockNumberUpdater.getBlockList()
-    expect(lateBlocks).toEqual([
-      { blockNumber: 400n, timestamp: minTimestamp },
-      { blockNumber: 500n, timestamp: minTimestamp.add(1, 'hours') },
-    ])
   })
 })
