37eb2f66a6e40ce734c5c3b107b465d5171b3915
Antoni Pawlak <antoni.pawlak@protonmail.com>
2022-05-05 14:54:30 +0200

    add ReportRepository tests

--- packages/backend/src/peripherals/database/ReportRepository.ts
+++ packages/backend/src/peripherals/database/ReportRepository.ts
@@ -11,9 +11,43 @@ export interface ReportRecord {
   ethTVL: bigint
 }
 
+export interface ReportWithBalance extends ReportRecord {
+  balance: bigint
+}
+
 export class ReportRepository {
   constructor(private knex: Knex, private logger: Logger) {
     this.logger = this.logger.for(this)
+  }
+
+  async getDaily(): Promise<ReportWithBalance[]> {
+    const rows = await this.knex('reports')
+      .select(
+        'reports.block_number',
+        'reports.unix_timestamp',
+        'reports.bridge_address',
+        'reports.asset_id',
+        'reports.usd_tvl',
+        'reports.eth_tvl',
+        'asset_balances.balance'
+      )
+      .leftJoin('asset_balances', function () {
+        this.on('asset_balances.block_number', '=', 'reports.block_number')
+          .andOn('asset_balances.asset_id', '=', 'reports.asset_id')
+          .andOn('asset_balances.holder_address', '=', 'reports.bridge_address')
+      })
+
+    return rows
+      .map((r) => ({
+        ...toRecord(r),
+        balance: r.balance ? BigInt(r.balance) : BigInt(0),
+      }))
+      .filter((r) => r.timestamp.isFull('day'))
+      .sort((a, b) => {
+        if (a.timestamp.lt(b.timestamp)) return -1
+        else if (a.timestamp.equals(b.timestamp)) return 0
+        else return 1
+      })
   }
 
   async getAll(): Promise<ReportRecord[]> {
--- packages/backend/test/peripherals/database/ReportRepository.test.ts
+++ packages/backend/test/peripherals/database/ReportRepository.test.ts
@@ -2,6 +2,10 @@ import { AssetId, EthereumAddress, Logger, UnixTime } from '@l2beat/common'
 import { expect } from 'earljs'
 
 import {
+  BalanceRecord,
+  BalanceRepository,
+} from '../../../src/peripherals/database/BalanceRepository'
+import {
   ReportRecord,
   ReportRepository,
 } from '../../../src/peripherals/database/ReportRepository'
@@ -10,16 +14,18 @@ import { setupDatabaseTestSuite } from './setup'
 describe(ReportRepository.name, () => {
   const { knex } = setupDatabaseTestSuite()
   const repository = new ReportRepository(knex, Logger.SILENT)
+  const balanceRepository = new BalanceRepository(knex, Logger.SILENT)
 
   const START_BLOCK_NUMBER = 123456n
   const MOCK_BRIDGE = EthereumAddress(
     '0x011B6E24FfB0B5f5fCc564cf4183C5BBBc96D515'
   )
+  const MOCK_BRIDGE_2 = EthereumAddress.random()
   const MOCK_ASSET = AssetId('dai-dai-stablecoin')
   const MOCK_USD_TVL = 100000000n
   const MOCK_ETH_TVL = 100000n
 
-  const START = UnixTime.now()
+  const START = UnixTime.now().toStartOf('day')
 
   const DATA: ReportRecord[] = [
     {
@@ -31,6 +37,14 @@ describe(ReportRepository.name, () => {
       ethTVL: MOCK_ETH_TVL,
     },
     {
+      blockNumber: START_BLOCK_NUMBER,
+      timestamp: START,
+      bridge: MOCK_BRIDGE_2,
+      asset: MOCK_ASSET,
+      usdTVL: 1000n,
+      ethTVL: 1n,
+    },
+    {
       blockNumber: START_BLOCK_NUMBER + 100n,
       timestamp: START.add(1, 'hours'),
       bridge: MOCK_BRIDGE,
@@ -40,9 +54,125 @@ describe(ReportRepository.name, () => {
     },
   ]
 
+  const BALANCE_DATA: BalanceRecord[] = [
+    {
+      blockNumber: START_BLOCK_NUMBER,
+      holderAddress: MOCK_BRIDGE,
+      assetId: MOCK_ASSET,
+      balance: 100000000n,
+    },
+    {
+      blockNumber: START_BLOCK_NUMBER,
+      holderAddress: MOCK_BRIDGE_2,
+      assetId: MOCK_ASSET,
+      balance: 1000n,
+    },
+    {
+      blockNumber: START_BLOCK_NUMBER + 100n,
+      holderAddress: MOCK_BRIDGE,
+      assetId: MOCK_ASSET,
+      balance: 100000000n,
+    },
+  ]
+
   beforeEach(async () => {
+    await balanceRepository.deleteAll()
     await repository.deleteAll()
     await repository.addOrUpdate(DATA)
+    await balanceRepository.addOrUpdate(BALANCE_DATA)
+  })
+
+  describe(ReportRepository.prototype.getDaily.name, () => {
+    it('filters data to get only full days', async () => {
+      const result = await repository.getDaily()
+
+      expect(result).toBeAnArrayWith(
+        { ...DATA[0], balance: 100000000n },
+        { ...DATA[1], balance: 1000n }
+      )
+      expect(result).toBeAnArrayOfLength(2)
+    })
+
+    it('returns sorted data', async () => {
+      const unsortedData = [
+        {
+          blockNumber: START_BLOCK_NUMBER + 1000n,
+          timestamp: START.add(1, 'days'),
+          bridge: MOCK_BRIDGE,
+          asset: MOCK_ASSET,
+          usdTVL: MOCK_USD_TVL,
+          ethTVL: MOCK_ETH_TVL,
+        },
+        {
+          blockNumber: START_BLOCK_NUMBER - 1000n,
+          timestamp: START.add(-1, 'days'),
+          bridge: MOCK_BRIDGE,
+          asset: MOCK_ASSET,
+          usdTVL: MOCK_USD_TVL,
+          ethTVL: MOCK_ETH_TVL,
+        },
+        {
+          blockNumber: START_BLOCK_NUMBER - 2000n,
+          timestamp: START.add(-2, 'days'),
+          bridge: MOCK_BRIDGE,
+          asset: MOCK_ASSET,
+          usdTVL: MOCK_USD_TVL,
+          ethTVL: MOCK_ETH_TVL,
+        },
+      ]
+
+      const balanceData = [
+        {
+          blockNumber: START_BLOCK_NUMBER + 1000n,
+          holderAddress: MOCK_BRIDGE,
+          assetId: MOCK_ASSET,
+          balance: 100000000n,
+        },
+        {
+          blockNumber: START_BLOCK_NUMBER - 1000n,
+          holderAddress: MOCK_BRIDGE,
+          assetId: MOCK_ASSET,
+          balance: 100000000n,
+        },
+        {
+          blockNumber: START_BLOCK_NUMBER - 2000n,
+          holderAddress: MOCK_BRIDGE,
+          assetId: MOCK_ASSET,
+          balance: 100000000n,
+        },
+      ]
+
+      await repository.addOrUpdate(unsortedData)
+      await balanceRepository.addOrUpdate(balanceData)
+
+      const result = await repository.getDaily()
+
+      const bridgeExpected = [-2, -1, 0, 1].map((offset) => ({
+        blockNumber: START_BLOCK_NUMBER + BigInt(offset) * 1000n,
+        timestamp: START.add(offset, 'days'),
+        bridge: MOCK_BRIDGE,
+        asset: MOCK_ASSET,
+        usdTVL: MOCK_USD_TVL,
+        ethTVL: MOCK_ETH_TVL,
+        balance: 100000000n,
+      }))
+
+      const bridgeTwoExpected = {
+        blockNumber: START_BLOCK_NUMBER,
+        timestamp: START,
+        bridge: MOCK_BRIDGE_2,
+        asset: MOCK_ASSET,
+        usdTVL: 1000n,
+        ethTVL: 1n,
+        balance: 1000n,
+      }
+
+      expect(result).toEqual([
+        ...bridgeExpected.slice(0, 3),
+        bridgeTwoExpected,
+        bridgeExpected[3],
+      ])
+    })
   })
 
   describe(ReportRepository.prototype.addOrUpdate.name, () => {
@@ -64,7 +194,7 @@ describe(ReportRepository.name, () => {
       const result = await repository.getAll()
 
       expect(result).toBeAnArrayWith(...DATA, newData)
-      expect(result).toBeAnArrayOfLength(3)
+      expect(result).toBeAnArrayOfLength(4)
     })
 
     it('update row', async () => {
@@ -81,8 +211,8 @@ describe(ReportRepository.name, () => {
 
       const result = await repository.getAll()
 
-      expect(result).toBeAnArrayWith(modifiedData, DATA[1])
-      expect(result).toBeAnArrayOfLength(2)
+      expect(result).toBeAnArrayWith(modifiedData, DATA[1], DATA[2])
+      expect(result).toBeAnArrayOfLength(3)
     })
   })
 
@@ -90,7 +220,7 @@ describe(ReportRepository.name, () => {
     const results = await repository.getAll()
 
     expect(results).toBeAnArrayWith(...DATA)
-    expect(results).toBeAnArrayOfLength(2)
+    expect(results).toBeAnArrayOfLength(3)
   })
 
   it(ReportRepository.prototype.deleteAll.name, async () => {
