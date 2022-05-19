e5c00c3ee893dbfd64c1b43dd5fdf846c4930a62
Bartek Kiepuszewski <bkiepuszewski@gmail.com>
2022-05-19 11:06:40 +0200

    Fix hop starting block

--- packages/config/src/projects/hopBridge.ts
+++ packages/config/src/projects/hopBridge.ts
@@ -7,32 +7,32 @@ export const hopBridge: Project = {
   bridges: [
     {
       address: '0x3666f603Cc164936C1b87e207F36BEBa4AC5f18a',
-      sinceBlock: 14559298,
+      sinceBlock: 12650032,
       tokens: ['USDC'],
     },
     {
       address: '0x3d4Cc8A61c7528Fd86C55cfe061a78dCBA48EDd1',
-      sinceBlock: 14559298,
+      sinceBlock: 13226217,
       tokens: ['DAI'],
     },
     {
       address: '0x3E4a3a4796d16c0Cd582C382691998f7c06420B6',
-      sinceBlock: 14559298,
+      sinceBlock: 12860139,
       tokens: ['USDT'],
     },
     {
       address: '0xb8901acB165ed027E32754E0FFe830802919727f',
-      sinceBlock: 14559298,
+      sinceBlock: 13331564,
       tokens: ['ETH'],
     },
     {
       address: '0x22B1Cbb8D98a01a3B71D034BB899775A76Eb1cc2',
-      sinceBlock: 14559298,
+      sinceBlock: 12969385,
       tokens: ['MATIC'],
     },
     {
       address: '0xb98454270065A31D71Bf635F6F7Ee6A518dFb849',
-      sinceBlock: 14559298,
+      sinceBlock: 13476113,
       tokens: ['WBTC'],
     },
   ],
