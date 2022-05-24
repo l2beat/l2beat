7346682a92ba946f07b7087428684876c00756bf
Antoni Pawlak <antoni.pawlak@pm.me>
2022-05-24 11:51:48 +0200

    update migration with default value

--- packages/backend/src/peripherals/database/migrations/008_add_balance.ts
+++ packages/backend/src/peripherals/database/migrations/008_add_balance.ts
@@ -2,7 +2,7 @@ import { Knex } from 'knex'
 
 export async function up(knex: Knex) {
   await knex.schema.alterTable('reports', function (table) {
-    table.decimal('balance', 80, 0).notNullable()
+    table.decimal('balance', 80, 0).defaultTo(0).notNullable()
   })
 }
 
