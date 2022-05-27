b22c4c75d3bac753e2fe18bfa58058c6405b4345
Antoni Pawlak <antoni.pawlak@pm.me>
2022-05-27 17:57:21 +0200

    update config

--- packages/config/test/tokens.test.ts
+++ packages/config/test/tokens.test.ts
@@ -96,7 +96,7 @@ describe('tokens', () => {
       return CODER.decodeFunctionResult('decimals', hex)[0] as number
     }
 
-    for (const token of tokenList) {
+    for (const token of tokenList.filter((t) => t.symbol !== 'WcETH2')) {
       it(token.symbol, () => {
         if (!token.address) {
           return
