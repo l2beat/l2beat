dc4c9307ff9cb8890995ca1ecb5ea55f8e73c0ec
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-06-01 16:51:27 +0200

    Fix price logic

--- packages/backend/src/old/services/StatCollector.ts
+++ packages/backend/src/old/services/StatCollector.ts
@@ -1,15 +1,13 @@
 import { SimpleDate } from '@l2beat/common'
 import { TokenInfo } from '@l2beat/config'
-import { BigNumber } from 'ethers'
 
 import { ProjectInfo } from '../../model'
-import { TEN_TO_18 } from '../constants'
+import { addOptimismToken } from './addOptimismToken'
 import { ArbitrumStatChecker, ArbitrumStats } from './ArbitrumStatChecker'
 import { BalanceChecker } from './balances'
 import { TVLAnalysis } from './balances/model'
 import { FlowChecker, Flows } from './FlowChecker'
 import { PriceService } from './prices'
-import { PriceSnapshot } from './prices/model'
 import { ProjectDates } from './ProjectDates'
 
 export interface Stats {
@@ -46,8 +44,8 @@ export class StatCollector {
         fetchedPrices,
         date
       )
-      const wrapped = wrapEntryWithOptimism(date, fetchedPrices, entry)
-      tvlEntries.push(wrapped)
+      addOptimismToken(entry, date, fetchedPrices)
+      tvlEntries.push(entry)
     }
 
     const flows = await this.flowChecker.getFlows(projects, tvlEntries)
@@ -58,39 +56,4 @@ export class StatCollector {
 
     return { tvlEntries, flows, arbitrum }
   }
-}
-
-function wrapEntryWithOptimism(
-  date: SimpleDate,
-  fetchedPrices: PriceSnapshot,
-  entry: TVLAnalysis
-) {
-  if (date.isAfter(SimpleDate.fromString('2022-05-30'))) {
-    const opPrice =
-      fetchedPrices.token[
-        '0x4200000000000000000000000000000000000042'
-      ]
-
-    if (opPrice) {
-      const opBalance = BigNumber.from(214748364).mul(TEN_TO_18)
-      const opUsd = opBalance.mul(opPrice)
-      const opEth = opUsd.div(fetchedPrices.eth)
-
-
-
-      entry.projects['Optimism'].tokens['OP'] = {
-        balance: 214748364,
-        usd: opUsd,
-      }
-      entry.projects['Optimism'].TVL = {
-        usd: entry.projects['Optimism'].TVL.usd + opUsd,
-        eth: entry.projects['Optimism'].TVL.eth + opUsd / fetchedPrices.eth.toNumber() ,
-      }
-      entry.TVL = {
-        usd: entry.TVL.usd + opUsd,
-        eth: entry.TVL.eth,
-      }
-    }
-  }
-  return entry
 }
