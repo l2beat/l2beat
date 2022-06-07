7c4de2f333122274b2c5a7b7e4036dc3b0327336
sin6pi7 <jac.zyla@gmail.com>
2022-06-07 11:56:34 +0200

    Refactor ReportRouter

--- packages/backend/src/api/routers/ReportRouter.ts
+++ packages/backend/src/api/routers/ReportRouter.ts
@@ -6,11 +6,12 @@ export function createReportRouter(reportController: ReportController) {
   const router = new Router()
 
   router.get('/api/data', async (ctx) => {
-    ctx.body = await reportController.getDaily()
-
-    if (ctx.body === undefined) {
-      ctx.throw(404)
+    const data = await reportController.getDaily()
+    if (!data) {
+      ctx.status = 404
+      return
     }
+    ctx.body = data
   })
 
   return router
