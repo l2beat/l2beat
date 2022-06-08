eda31e77a178524b53f57985c0a76f1970b838ef
Bartek Kiepuszewski <bkiepuszewski@gmail.com>
2022-06-08 10:19:24 +0200

    Resolve ChugSplashProxy in optimism-history script (#440)
    * Resolve ChugSplashProxy in optimism-history script
    
    * remove unused var
    
    * add Metis 2.0 tx type to decoder
    
    * Fix linter
    
    Co-authored-by: Piotr Szlachciak <szlachciak.piotr@gmail.com>

--- packages/optimism-decoder/src/analyze.ts
+++ packages/optimism-decoder/src/analyze.ts
@@ -4,6 +4,7 @@ const ctcMapping: Record<string, string | undefined> = {
   '0x5E4e65926BA27467555EB562121fac00D24E9dD2': 'Optimism OVM 2.0',
   '0x4BF681894abEc828B212C906082B444Ceb2f6cf6': 'Optimism OVM 1.0',
   '0x56a76bcC92361f6DF8D75476feD8843EdC70e1C9': 'Metis',
+  '0x6A1DB7d799FBA381F2a518cA859ED30cB8E1d41a': 'Metis 2.0',
   '0xfBd2541e316948B259264c02f370eD088E04c3Db': 'Boba Network',
 }
 
