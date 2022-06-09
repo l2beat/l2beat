1f5c53ac9a99ee059c09c80bba1711cfe68f6076
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-06-09 18:58:39 +0200

    Begin migration to tailwindcss (#470)
    * Add tailwindcss
    
    * Fix inconsistencies

---
+++ packages/frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type import("tailwindcss/types").Config */
+module.exports = {
+  content: ['./src/**/*.{html,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'Arial', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
