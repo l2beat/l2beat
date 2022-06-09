cbfb81e550c49e29780f21cc0f789416c68715de
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-06-09 18:01:53 +0200

    Fix inconsistencies

--- packages/frontend/src/content/common/head/Head.tsx
+++ packages/frontend/src/content/common/head/Head.tsx
@@ -19,8 +19,8 @@ export function Head(props: Props) {
     <head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="stylesheet" href="/styles/main.css" />
       <link rel="stylesheet" href="/styles/style.css" />
+      <link rel="stylesheet" href="/styles/main.css" />
       {props.includeMetaImageStyles && (
         <link rel="stylesheet" href="/styles/meta-image.css" />
       )}
