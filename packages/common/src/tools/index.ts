82dd1cf766dc18d03651988cf64e6a91afae3ce7
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-06-20 18:33:30 +0200

    Remove old code (#505)
    * Remove unused files
    
    * Remove unused dependencies

--- packages/common/src/tools/index.ts
+++ packages/common/src/tools/index.ts
@@ -1,7 +1,5 @@
-export * from './EventEmitter'
 export * from './JobQueue'
 export * from './Logger'
 export * from './mock'
 export * from './RateLimiter'
 export * from './RequestTracker'
-export * from './retry'
