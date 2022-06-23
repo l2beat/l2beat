a697f79e7e6636e0a4019ae04f5df73661c1724a
Antoni Pawlak <antoni.pawlak@pm.me>
2022-06-23 12:03:16 +0200

    add status folder

---
+++ packages/backend/src/api/controllers/status/Status.ts
@@ -0,0 +1,6 @@
+export interface Status {
+  name: string
+  timestamp: number | undefined
+  value: string | undefined
+  isSynced: boolean
+}
---
+++ packages/backend/src/api/controllers/status/StatusController.ts
@@ -0,0 +1,119 @@
+import { EthereumAddress, UnixTime } from '@l2beat/common'
+
+import { PriceUpdater } from '../../../core/PriceUpdater'
+import { ProjectInfo } from '../../../model'
+import { Token } from '../../../model/Token'
+import { BalanceRepository } from '../../../peripherals/database/BalanceRepository'
+import { ReportRepository } from '../../../peripherals/database/ReportRepository'
+import { Status } from './Status'
+import { renderStatusPage } from './view/StatusPage'
+
+export class StatusController {
+  constructor(
+    private priceUpdater: PriceUpdater,
+    private balanceRepository: BalanceRepository,
+    private reportsRepository: ReportRepository,
+    private tokens: Token[],
+    private projects: ProjectInfo[],
+  ) {}
+
+  async getPricesStatus() {
+    const latestByToken = await this.priceUpdater.getStatus()
+
+    const statuses = this.tokens
+      .map((token): Status => {
+        const latest = latestByToken.get(token.id)
+
+        return {
+          name: token.coingeckoId.toString(),
+          timestamp: latest?.timestamp.toNumber(),
+          value: latest?.priceUsd.toString(),
+          isSynced: isSynced(latest?.timestamp),
+        }
+      })
+      .sort((a, b) => Number(a.isSynced) - Number(b.isSynced))
+
+    return renderStatusPage({ title: 'Prices', statuses })
+  }
+
+  async getBalancesStatus() {
+    const holderLatest = await this.balanceRepository.getLatestPerHolder()
+    return this.projects.flatMap(({ bridges, name }) =>
+      bridges.map(({ address }) => ({
+        name,
+        address,
+        tokens:
+          holderLatest.get(EthereumAddress(address))?.map((latest) => ({
+            assetId: latest.assetId,
+            balance: latest.balance.toString(),
+            blockNumber: latest.blockNumber.toString(),
+            timestamp: unixTimeToString(latest.timestamp),
+            syncStatus: getSyncStatus(latest.timestamp),
+          })) ?? [],
+      })),
+    )
+  }
+
+  async getReportsStatus() {
+    const bridgeLatest = await this.reportsRepository.getLatestPerBridge()
+    return this.projects.flatMap(({ bridges, name }) =>
+      bridges.map(({ address }) => ({
+        name,
+        address,
+        tokens:
+          bridgeLatest.get(EthereumAddress(address))?.map((latest) => ({
+            assetId: latest.asset,
+            balance: latest.balance.toString(),
+            usd: latest.balanceUsd.toString(),
+            eth: latest.balanceEth.toString(),
+            blockNumber: latest.blockNumber.toString(),
+            timestamp: unixTimeToString(latest.timestamp),
+            syncStatus: getSyncStatus(latest.timestamp),
+          })) ?? [],
+      })),
+    )
+  }
+}
+
+function isSynced(timestamp: UnixTime | undefined) {
+  const now = UnixTime.now().add(-1, 'hours').toStartOf('hour')
+  return !!timestamp && now.equals(timestamp)
+}
+
+const unixTimeToString = (date: UnixTime) => {
+  return date.toDate().toString().slice(4, 21)
+}
+
+const SECONDS_PER_HOUR = 60 * 60
+const getSyncStatus = (
+  date: UnixTime,
+): {
+  isSynced: boolean
+  message: string
+} => {
+  const now = UnixTime.now().add(-1, 'hours').toStartOf('hour')
+
+  const diff = now.toNumber() - date.toNumber()
+
+  if (diff === 0) {
+    return { isSynced: true, message: '✔' }
+  }
+
+  if (diff > 0 && diff < 24 * SECONDS_PER_HOUR) {
+    return {
+      isSynced: false,
+      message: `out of sync for ${diff / SECONDS_PER_HOUR} hour(s)`,
+    }
+  }
+
+  if (diff >= 24 * SECONDS_PER_HOUR) {
+    return {
+      isSynced: false,
+      message: `out of sync for ${Math.floor(
+        diff / (24 * SECONDS_PER_HOUR),
+      )} day(s)`,
+    }
+  }
+
+  return { isSynced: false, message: '? price synced ahead of time' }
+}
