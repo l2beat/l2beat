f7b30bd7df11a5222aab34c769fbe92213a13800
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-07-13 10:50:31 +0200

    Add discovery mode to update-monitor (#545)
    * Add discovery mode based on arbitrum
    
    * Add json output to DiscoveryEngine
    
    * Discover zkSwap1&2 and zkSpace

--- packages/update-monitor/src/common/eip1967.ts
+++ packages/update-monitor/src/common/eip1967.ts
@@ -7,7 +7,7 @@ export const EIP1967_IMPLEMENTATION_SLOT =
   '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc'
 
 export async function getEip1967Implementation(
-  provider: providers.JsonRpcProvider,
+  provider: providers.Provider,
   contract: Contract | string,
 ) {
   const address = typeof contract === 'string' ? contract : contract.address
@@ -23,7 +23,7 @@ export const EIP1967_ADMIN_SLOT =
   '0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103'
 
 export async function getEip1967Admin(
-  provider: providers.JsonRpcProvider,
+  provider: providers.Provider,
   contract: Contract | string,
 ) {
   const address = typeof contract === 'string' ? contract : contract.address
