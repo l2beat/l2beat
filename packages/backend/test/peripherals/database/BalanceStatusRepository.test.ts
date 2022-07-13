4217f9504cee4b1d0e1f7a45ca70a84cf078fb9f
Antoni Pawlak <antoni.pawlak@pm.me>
2022-07-13 14:36:32 +0200

    add test for balances

--- packages/backend/test/peripherals/database/BalanceStatusRepository.test.ts
+++ packages/backend/test/peripherals/database/BalanceStatusRepository.test.ts
@@ -49,4 +49,13 @@ describe(BalanceStatusRepository.name, () => {
     const timestamps = await repository.getByConfigHash(HASH_ONE)
     expect(timestamps).toEqual([TIME_ONE])
   })
+
+  it(BalanceStatusRepository.prototype.getFromTo.name, async () => {
+    await repository.add({ configHash: HASH_ONE, timestamp: TIME_ONE })
+    await repository.add({ configHash: HASH_ONE, timestamp: TIME_TWO })
+
+    const result = await repository.getFromTo(TIME_THREE, TIME_TWO)
+
+    expect(result).toEqual([{ configHash: HASH_ONE, timestamp: TIME_TWO }])
+  })
 })
