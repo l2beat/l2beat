8b57cb02a40e81a6fc4fbe630ff2e9e60b3d7757
sin6pi7 <jac.zyla@gmail.com>
2022-07-14 09:28:07 +0200

    Update Aztec Connect

--- packages/config/src/projects/aztecconnect.ts
+++ packages/config/src/projects/aztecconnect.ts
@@ -25,7 +25,7 @@ export const aztecconnect: Project = {
   details: {
     description:
       'Aztec Connect is an open source layer 2 network that aims to bring scalability and privacy to Ethereum. It strives to enable affordable, private crypto payments via zero-knowledge proofs. Additionally it allows to deposit funds into a variety of DeFi Protocols such as LiDo, Element.Fi, etc.',
-    purpose: 'Private payments',
+    purpose: 'Private DeFi',
     links: {
       websites: ['https://aztec.network/'],
       apps: ['https://zk.money'],
