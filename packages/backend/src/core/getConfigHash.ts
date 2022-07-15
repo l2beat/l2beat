9827997e6bb99c40007b3865273ecef61196366e
sin6pi7 <jac.zyla@gmail.com>
2022-07-15 15:36:00 +0200

    Add possibility to force update the config

--- packages/backend/src/core/getConfigHash.ts
+++ packages/backend/src/core/getConfigHash.ts
@@ -1,9 +1,13 @@
 import { Hash256 } from '@l2beat/common'
+import { FORCED_UPDATE_ID } from '@l2beat/config'
 import { createHash } from 'crypto'
 
 import { ProjectInfo } from '../model'
 
-export function getConfigHash(projects: ProjectInfo[]): Hash256 {
+export function getConfigHash(
+  projects: ProjectInfo[],
+  forcedUpdateId = FORCED_UPDATE_ID,
+): Hash256 {
   const entries = []
   for (const { projectId, bridges } of projects) {
     for (const { tokens, address, sinceTimestamp } of bridges) {
@@ -18,6 +22,9 @@ export function getConfigHash(projects: ProjectInfo[]): Hash256 {
       }
     }
   }
-  const string = JSON.stringify(entries.map((x) => JSON.stringify(x)).sort())
+  const string = JSON.stringify([
+    entries.map((x) => JSON.stringify(x)).sort(),
+    forcedUpdateId,
+  ])
   return Hash256('0x' + createHash('sha256').update(string).digest('hex'))
 }
--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,2 +1,3 @@
 export * from './projects'
 export * from './tokens'
+export const FORCED_UPDATE_ID = 0
