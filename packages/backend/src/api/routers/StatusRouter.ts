098c5ac93167870547d98b329b4dd2337fa5658a
Antoni Pawlak <antoni.pawlak@pm.me>
2022-07-15 12:05:05 +0200

    add param parser to router

--- packages/backend/src/api/routers/StatusRouter.ts
+++ packages/backend/src/api/routers/StatusRouter.ts
@@ -1,39 +1,47 @@
 import Router from '@koa/router'
 import { UnixTime } from '@l2beat/common'
-import { ParsedUrlQuery } from 'querystring'
+import { z } from 'zod'
 
+import { stringAs } from '../../tools/types'
 import { StatusController } from '../controllers/status/StatusController'
+import { withTypedContext } from './types'
+
+const paramsParser = z.object({
+  query: z.object({
+    from: stringAs((s) => new UnixTime(+s)).optional(),
+    to: stringAs((s) => new UnixTime(+s)).optional(),
+  }),
+})
 
 export function createStatusRouter(statusController: StatusController) {
   const router = new Router()
 
-  router.get('/status/prices', async (ctx) => {
-    const { from, to } = getParams(ctx.query)
+  router.get(
+    '/status/prices',
+    withTypedContext(paramsParser, async (ctx) => {
+      const { from, to } = ctx.query
 
-    ctx.body = await statusController.getPricesStatus(from, to)
-  })
+      ctx.body = await statusController.getPricesStatus(from, to)
+    }),
+  )
 
-  router.get('/status/balances', async (ctx) => {
-    const { from, to } = getParams(ctx.query)
+  router.get(
+    '/status/balances',
+    withTypedContext(paramsParser, async (ctx) => {
+      const { from, to } = ctx.query
 
-    ctx.body = await statusController.getBalancesStatus(from, to)
-  })
+      ctx.body = await statusController.getBalancesStatus(from, to)
+    }),
+  )
 
-  router.get('/status/reports', async (ctx) => {
-    const { from, to } = getParams(ctx.query)
+  router.get(
+    '/status/reports',
+    withTypedContext(paramsParser, async (ctx) => {
+      const { from, to } = ctx.query
 
-    ctx.body = await statusController.getReportsStatus(from, to)
-  })
+      ctx.body = await statusController.getReportsStatus(from, to)
+    }),
+  )
 
   return router
-}
-
-function getParams(query: ParsedUrlQuery): {
-  from: UnixTime | undefined
-  to: UnixTime | undefined
-} {
-  const from = query.from ? new UnixTime(+query.from) : undefined
-  const to = query.to ? new UnixTime(+query.to) : undefined
-
-  return { from, to }
 }
