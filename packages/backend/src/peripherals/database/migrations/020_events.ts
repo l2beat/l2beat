88b4861b2c96376a64a2268aa1264d548b1ed763
Antoni Pawlak <antoni.pawlak@pm.me>
2022-08-08 15:49:27 +0200

    add migration

---
+++ packages/backend/src/peripherals/database/migrations/020_events.ts
@@ -0,0 +1,28 @@
+/*
+                      ====== IMPORTANT NOTICE ======
+
+DO NOT EDIT OR RENAME THIS FILE
+
+This is a migration file. Once created the file should not be renamed or edited,
+because migrations are only run once on the production server. 
+
+If you find that something was incorrectly set up in the `up` function you
+should create a new migration file that fixes the issue.
+
+*/
+
+import { Knex } from 'knex'
+
+export async function up(knex: Knex) {
+  await knex.schema.createTable('events', function (table) {
+    table.integer('block_number').notNullable()
+    table.bigInteger('unix_timestamp').notNullable()
+    table.string('project_id').notNullable()
+    table.string('event_name').notNullable()
+    table.primary(['block_number', 'project_id', 'event_name'])
+  })
+}
+
+export async function down(knex: Knex) {
+  await knex.schema.dropTable('events')
+}
