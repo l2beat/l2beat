1ff8912c351d95a342d25098018c6d60ee1845bc
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-08-24 14:09:21 +0200

    Add financial and risk view stories

---
+++ packages/frontend/src/stories/pages/home/FinancialView.stories.tsx
@@ -0,0 +1,124 @@
+import React, { useEffect } from 'react'
+
+import { Page } from '../../../components/Page'
+import { Tooltip } from '../../../components/Tooltip'
+import { FinancialView as FinancialViewComponent } from '../../../pages/Home/view/FinancialView/FinancialView'
+import { configureTooltips } from '../../../scripts/configureTooltips'
+import { formatLargeNumber } from '../../../utils'
+
+export default {
+  title: 'Pages/Home/FinancialView',
+  controls: { hideNoControlsWarning: true },
+  layout: 'fullscreen',
+}
+
+export function FinancialView() {
+  useEffect(() => {
+    configureTooltips()
+  }, [])
+  return (
+    <>
+      <Page>
+        <FinancialViewComponent
+          items={[
+            {
+              name: 'Forktimism',
+              provider: 'Optimism',
+              slug: 'optimism',
+              tvl: formatLargeNumber(2_740_000_000),
+              tvlBreakdown: {
+                warning: 'Some random warning',
+                warningSeverity: 'warning',
+                label: 'The tooltip label',
+                empty: false,
+                associated: 0.4,
+                ether: 0.2,
+                stable: 0.2,
+                other: 0.2,
+              },
+              oneDayChange: '+3.45%',
+              sevenDayChange: '-54.2%',
+              marketShare: '50.42%',
+              purpose: 'Universal',
+              technology: {
+                abbreviation: 'ORU',
+                name: 'Optimistic Rollup',
+              },
+            },
+            {
+              name: 'Arbitrage',
+              slug: 'arbitrum',
+              tvl: formatLargeNumber(2_740_000_000),
+              tvlBreakdown: {
+                warning: 'Some random warning',
+                warningSeverity: 'warning',
+                label: 'The tooltip label',
+                empty: false,
+                associated: 0.4,
+                ether: 0.2,
+                stable: 0.2,
+                other: 0.2,
+              },
+              oneDayChange: '+3.45%',
+              sevenDayChange: '-54.2%',
+              marketShare: '50.42%',
+              purpose: 'Universal',
+              technology: {
+                abbreviation: 'ORU',
+                name: 'Optimistic Rollup',
+              },
+            },
+            {
+              name: 'StorkCommerce',
+              provider: 'StarkEx',
+              slug: 'starknet',
+              tvl: formatLargeNumber(2_740_000_000),
+              tvlBreakdown: {
+                warning: 'Some random warning',
+                warningSeverity: 'warning',
+                label: 'The tooltip label',
+                empty: false,
+                associated: 0.4,
+                ether: 0.2,
+                stable: 0.2,
+                other: 0.2,
+              },
+              oneDayChange: '+3.45%',
+              sevenDayChange: '-54.2%',
+              marketShare: '50.42%',
+              purpose: 'Universal',
+              technology: {
+                abbreviation: 'ORU',
+                name: 'Optimistic Rollup',
+              },
+            },
+            {
+              name: 'zk.download',
+              slug: 'zksync',
+              tvl: formatLargeNumber(2_740_000_000),
+              tvlBreakdown: {
+                warning: 'Some random warning',
+                warningSeverity: 'warning',
+                label: 'The tooltip label',
+                empty: false,
+                associated: 0.4,
+                ether: 0.2,
+                stable: 0.2,
+                other: 0.2,
+              },
+              oneDayChange: '+3.45%',
+              sevenDayChange: '-54.2%',
+              marketShare: '50.42%',
+              purpose: 'Universal',
+              technology: {
+                abbreviation: 'ORU',
+                name: 'Optimistic Rollup',
+              },
+            },
+          ]}
+        />
+      </Page>
+      <Tooltip />
+    </>
+  )
+}
---
+++ packages/frontend/src/stories/pages/home/RiskView.stories.tsx
@@ -0,0 +1,85 @@
+import { ProjectRiskViewEntry } from '@l2beat/config'
+import React, { useEffect } from 'react'
+
+import { Page } from '../../../components/Page'
+import { Tooltip } from '../../../components/Tooltip'
+import { RiskView as RiskViewComponent } from '../../../pages/Home/view/RiskView/RiskView'
+import { configureTooltips } from '../../../scripts/configureTooltips'
+
+export default {
+  title: 'Pages/Home/RiskView',
+  controls: { hideNoControlsWarning: true },
+  layout: 'fullscreen',
+}
+
+export function RiskView() {
+  useEffect(() => {
+    configureTooltips()
+  }, [])
+  return (
+    <>
+      <Page>
+        <RiskViewComponent
+          className="!block"
+          items={[
+            {
+              name: 'Forktimism',
+              provider: 'Optimism',
+              slug: 'optimism',
+              stateValidation: risk('short', 'warning'),
+              dataAvailability: risk('medium', 'warning'),
+              upgradeability: risk('medium', 'bad'),
+              sequencerFailure: risk('long'),
+              validatorFailure: risk('short', 'bad'),
+            },
+            {
+              name: 'Arbitrage',
+              slug: 'arbitrum',
+              stateValidation: risk('long'),
+              dataAvailability: risk('medium'),
+              upgradeability: risk('short', 'bad'),
+              sequencerFailure: risk('short'),
+              validatorFailure: risk('long', 'warning'),
+            },
+            {
+              name: 'StorkCommerce',
+              provider: 'StarkEx',
+              slug: 'starknet',
+              stateValidation: risk('short'),
+              dataAvailability: risk('medium'),
+              upgradeability: risk('long'),
+              sequencerFailure: risk('medium', 'bad'),
+              validatorFailure: risk('long', 'warning'),
+            },
+            {
+              name: 'zk.download',
+              slug: 'zksync',
+              stateValidation: risk('medium', 'bad'),
+              dataAvailability: risk('medium'),
+              upgradeability: risk('long'),
+              sequencerFailure: risk('long', 'warning'),
+              validatorFailure: risk('short'),
+            },
+          ]}
+        />
+      </Page>
+      <Tooltip />
+    </>
+  )
+}
+
+function risk(
+  length: 'short' | 'medium' | 'long',
+  sentiment?: 'warning' | 'bad',
+): ProjectRiskViewEntry {
+  return {
+    value:
+      length === 'short'
+        ? 'Some value'
+        : length === 'medium'
+        ? 'Medium text entry'
+        : 'Quite a longer value',
+    description: 'Some longer description of the thing',
+    sentiment,
+  }
+}
