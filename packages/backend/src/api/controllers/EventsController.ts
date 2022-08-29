0ec53a1a00ecdcfc1a530553d73740145f5324ac
Antoni Pawlak <antoni.pawlak@pm.me>
2022-08-29 17:20:25 +0200

    hourly properly working

--- packages/backend/src/api/controllers/EventsController.ts
+++ packages/backend/src/api/controllers/EventsController.ts
@@ -1,8 +1,14 @@
 import { Logger } from '@l2beat/common'
-import { ApiEvents } from '@l2beat/types'
+import { ApiEvents, UnixTime } from '@l2beat/types'
 
 import { ProjectInfo } from '../../model'
 import { EventRepository } from '../../peripherals/database/EventRepository'
+
+interface ProjectEntry {
+  hourly: Record<number, number[]>
+  sixHourly: Record<number, number[]>
+  daily: Record<number, number[]>
+}
 
 export class EventsController {
   constructor(
@@ -13,57 +19,52 @@ export class EventsController {
     this.logger = this.logger.for(this)
   }
 
+  //todo
+  //ensure data in config is sorted
+  //make a cleaner type for project entry and only at the end transpile it to EventCharts
+  //add 'from' to getter from repository
+  //reason about minimalFrom for  the hourly sixhourly and daily
   async getMain(): Promise<ApiEvents> {
     const main: ApiEvents = {
       projects: {},
     }
 
     for (const project of this.projects) {
+      const eventNames = project.events.map((e) => e.name)
+
       // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-      if (main.projects[project.projectId.toString()] === undefined) {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-        main.projects[project.projectId.toString()] = {
-          hourly: { types: ['timestamp', []], data: [] },
-          sixHourly: { types: ['timestamp', []], data: [] },
-          daily: { types: ['timestamp', []], data: [] },
+      const projectEntry: ProjectEntry = {
+        hourly: {},
+        sixHourly: {},
+        daily: {},
+      }
+
+      const hourly = await this.eventsRepository.getByProject(
+        project.projectId,
+        'hourly',
+      )
+
+      for (const { timestamp, count } of hourly) {
+        if (projectEntry.hourly[timestamp.toNumber()]) {
+          projectEntry.hourly[timestamp.toNumber()].push(count)
+        } else {
+          projectEntry.hourly[timestamp.toNumber()] = [count]
         }
       }
-      for (const event of project.events) {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-        main.projects[project.projectId.toString()]?.hourly.types[1]?.push(
-          event.name,
-        )
 
-        const hourly = await this.eventsRepository.getByProjectAndName(
-          project.projectId,
-          event.name,
-          'hourly',
-        )
+      const data: [UnixTime, number[]][] = []
 
-        for (const hour of hourly) {
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-          const id = // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            main.projects[project.projectId.toString()]?.hourly.data.findIndex(
-              (h) =>
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-                h[0].equals(hour.timestamp),
-            )
-          if (id !== -1) {
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-            main.projects[project.projectId.toString()]?.hourly.data[
-              id as number
-            ][1].push(hour.count)
-          } else {
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-            main.projects[project.projectId.toString()]?.hourly.data.push([
-              hour.timestamp,
-              [hour.count],
-            ])
-          }
-        }
+      for (const key in projectEntry.hourly) {
+        data.push([new UnixTime(Number(key)), projectEntry.hourly[key]])
+      }
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+      main.projects[project.projectId.toString()] = {
+        hourly: { types: ['timestamp', eventNames], data: data },
+        sixHourly: { types: ['timestamp', eventNames], data: [] },
+        daily: { types: ['timestamp', eventNames], data: [] },
       }
     }
-
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return main
   }
