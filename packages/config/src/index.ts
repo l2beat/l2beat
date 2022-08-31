e8a3fa227feb78cae208a61744ebbc84a2611627
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-08-31 10:41:30 +0200

    Fix

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -2,6 +2,7 @@ import { bridges } from './bridges'
 import { bridge } from './bridges/types/bridge'
 import { projects as scalingProjects } from './projects'
 
+export * from './projects'
 export * from './tokens'
 
 export const projects = [...scalingProjects, ...bridges.map(bridge)]
