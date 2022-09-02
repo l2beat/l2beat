eb66c748d8ee7ecd9a077dc0ef1f6bda63e4444b
gzeon <95478735+gzeoneth@users.noreply.github.com>
2022-09-02 23:15:24 +0800

    fix: arbitrum one nitro rollup address

--- packages/update-monitor/src/projects/arbitrum/constants.ts
+++ packages/update-monitor/src/projects/arbitrum/constants.ts
@@ -1,5 +1,5 @@
 export const addresses = {
-  rollupAddress: '0xC12BA48c781F6e392B49Db2E25Cd0c28cD77531A',
+  rollupAddress: '0x5ef0d09d1e6204141b4d37530808ed19f60fba35',
   l1GatewayRouter: '0x72Ce9c846789fdB6fC1f34aC4AD25Dd9ef7031ef',
   l1ERC20Gateway: '0xa3A7B6F88361F48403514059F1F16C8E78d60EeC',
 }
