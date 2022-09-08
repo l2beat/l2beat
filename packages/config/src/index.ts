31d6691841c66f75b9fd722f34087c09efc4b6a8
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-09-08 08:47:45 +0200

    Make the build pass

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,8 +1,3 @@
-import { bridges } from './bridges'
-import { bridge } from './bridges/types/bridge'
-import { projects as scalingProjects } from './projects'
-
+export * from './bridges'
 export * from './projects'
 export * from './tokens'
-
-export const projects = [...scalingProjects, ...bridges.map(bridge)]
