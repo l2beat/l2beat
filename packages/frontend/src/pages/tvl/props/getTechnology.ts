25f9effeac3a8375473ea6147563ce53f328bc67
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-09-08 11:00:18 +0200

    Revert changes in frontend code

--- packages/frontend/src/pages/tvl/props/getTechnology.ts
+++ packages/frontend/src/pages/tvl/props/getTechnology.ts
@@ -2,6 +2,18 @@ import { Layer2 } from '@l2beat/config'
 
 export function getTechnology(project: Layer2) {
   const name = project.details.technology.category.name
-  // TODO: refactor and remove abbreviation
-  return { abbreviation: '-', name }
+  switch (name) {
+    case 'Optimistic Rollup':
+      return { abbreviation: 'ORU', name }
+    case 'ZK Rollup':
+      return { abbreviation: 'ZKR', name }
+    case 'Plasma':
+      return { abbreviation: 'PLA', name }
+    case 'Validium':
+      return { abbreviation: 'VAL', name }
+    case 'State Pools':
+      return { abbreviation: 'STP', name }
+    case 'Optimistic Chain':
+      return { abbreviation: 'OPC', name }
+  }
 }
