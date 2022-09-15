87b5a757e15298260da940412e41c92133418294
sin6pi7 <jac.zyla@gmail.com>
2022-09-15 11:32:23 +0200

    Remove comment

--- packages/frontend/src/stories/components/Navbar.stories.tsx
+++ packages/frontend/src/stories/components/Navbar.stories.tsx
@@ -3,7 +3,6 @@ import React, { useEffect } from 'react'
 import { configureDarkThemeToggle } from '../../components/navbar/configureDarkThemeToggle'
 import { configureSidebarMenu } from '../../components/navbar/configureSidebarMenu'
 import { Navbar as NavbarComponent } from '../../components/navbar/Navbar'
-// import { Page } from '../../components/Page'
 
 export default {
   title: 'Components/Navbar',
