b3fd26bef8ea3007c15c42678ed47c9be073af0c
sin6pi7 <jac.zyla@gmail.com>
2022-09-15 11:30:40 +0200

    Bring back banner colors

--- packages/frontend/src/components/navbar/Banner.tsx
+++ packages/frontend/src/components/navbar/Banner.tsx
@@ -9,7 +9,7 @@ export function Banner() {
   }
   return (
     <OutLink
-      className="block text-center bg-[#0B4737] text-white text-opacity-70 text-sm font-bold px-4 py-1.5 -mx-4 lg:mx-0"
+      className="block text-center bg-[#2a363b] text-white text-sm font-bold px-4 py-1.5 -mx-4 lg:mx-0"
       href="https://twitter.com/VitalikButerin/status/1570306185391378434"
     >
       🎉 Ethereum 🐼 Merge is DONE! 🎉
