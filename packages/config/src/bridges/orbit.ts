64b3868ed9be5aa49fd86765725e41df009219f3
Bartek Kiepuszewski <bkiepuszewski@gmail.com>
2022-09-15 11:35:49 +0200

    Add Orbit to Bridges (#642)
    * Add Orbit
    
    * Lint Fix
    
    * Add icon

---
+++ packages/config/src/bridges/orbit.ts
@@ -0,0 +1,42 @@
+import { ProjectId, UnixTime } from '@l2beat/types'
+
+import { Bridge } from './types'
+
+export const orbit: Bridge = {
+  id: ProjectId('orbit'),
+  name: 'Orbit Bridge',
+  slug: 'orbit',
+  type: 'Lock-Mint',
+  validation: 'Liquidity Network',
+  destination: ['KLAYTN'], //TODO: add more chains
+  //TODO: Originally for KLAYTN, Orbit Bridge now supports multiple chains and has Liquidity Network
+  links: {
+    websites: ['https://bridge.orbitchain.io/'],
+  },
+  risks: {
+    validation: {
+      value: 'External',
+      description: 'MultiSig',
+      sentiment: 'bad',
+    },
+    sourceUpgradeability: {
+      value: 'YES',
+      description: '',
+      sentiment: 'bad',
+    },
+    destinationToken: {
+      value: 'KToken',
+      description: 'Info not available yet',
+      sentiment: 'warning',
+    },
+  },
+  escrows: [
+    {
+      address: '0x1Bf68A9d1EaEe7826b3593C20a0ca93293cb489a',
+      sinceTimestamp: new UnixTime(1603950507),
+      tokens: ['ETH', 'USDT', 'ORC', 'DAI', 'USDC', 'WBTC', 'HANDY', 'MATIC'], // TODO: there are more tokens
+    },
+  ],
+
+  connections: [],
+}
