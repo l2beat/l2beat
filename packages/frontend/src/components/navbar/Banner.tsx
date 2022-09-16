0a1e8051420d3b492715629e78ecdea19883b1e9
Antoni Pawlak <antoni.pawlak@pm.me>
2022-09-16 14:25:27 +0200

    fix styling

--- packages/frontend/src/components/navbar/Banner.tsx
+++ packages/frontend/src/components/navbar/Banner.tsx
@@ -9,7 +9,7 @@ export function Banner() {
   }
   return (
     <OutLink
-      className="block text-center bg-[#048104] text-white font-bold px-4 py-1 -mx-4 lg:mx-0 mb-4"
+      className="block text-center bg-[#048104] text-white text-sm font-bold px-4 py-1.5"
       href="https://gitcoin.co/grants/3857/l2beat"
     >
       Gitcoin round 15 is live! Support L2BEAT by donating!
