4d5ce2ed36dc5d81eb6c60fc2aec8ff1aeada5f2
Antoni Pawlak <antoni.pawlak@pm.me>
2022-09-16 12:46:56 +0200

    move getMintimestamps to utils

---
+++ packages/backend/src/api/controllers/utils/getHourlyMinTimestamp.ts
@@ -0,0 +1,6 @@
+import { UnixTime } from '@l2beat/types';
+
+
+export function getHourlyMinTimestamp(now?: UnixTime) {
+  return (now ?? UnixTime.now()).add(-7, 'days');
+}
---
+++ packages/backend/src/api/controllers/utils/getSixHourlyMinTimestamp.ts
@@ -0,0 +1,6 @@
+import { UnixTime } from '@l2beat/types';
+
+
+export function getSixHourlyMinTimestamp(now?: UnixTime) {
+  return (now ?? UnixTime.now()).add(-90, 'days');
+}
