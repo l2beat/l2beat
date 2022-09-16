77d44634ef15f8728c297033fec364b4ded39b0c
Antoni Pawlak <antoni.pawlak@pm.me>
2022-09-16 15:51:46 +0200

    getEventChart refactor and tests

--- packages/backend/src/api/controllers/events/EventsController.ts
+++ packages/backend/src/api/controllers/events/EventsController.ts
@@ -1,12 +1,10 @@
-import { ApiEvents, EventChart, EventChartPoint, UnixTime } from '@l2beat/types'
+import { ApiEvents } from '@l2beat/types'
 
 import { Clock } from '../../../core/Clock'
 import { Project } from '../../../model'
-import {
-  EventRecordAggregated,
-  EventRepository,
-} from '../../../peripherals/database/EventRepository'
+import { EventRepository } from '../../../peripherals/database/EventRepository'
 import { getHourlyMinTimestamp } from '../utils/getHourlyMinTimestamp'
+import { getEventChart } from './getEventChart'
 import { renderShowcasePage } from './ShowcasePage'
 
 export class EventController {
@@ -57,33 +55,5 @@ export class EventController {
     const events = await this.getEvents()
 
     return renderShowcasePage({ events })
-  }
-}
-
-function getEventChart(
-  records: EventRecordAggregated[],
-  eventNames: string[],
-): EventChart {
-  //Record<timestamp, Record<eventName, count>>
-  const entries: Record<number, Record<string, number> | undefined> = {}
-
-  for (const { timestamp, name, count } of records) {
-    const entry = entries[timestamp.toNumber()] ?? {}
-    entry[name] = count
-    entries[timestamp.toNumber()] = entry
-  }
-
-  const data: EventChartPoint[] = []
-
-  for (const key in entries) {
-    data.push([
-      new UnixTime(Number(key)),
-      ...eventNames.map((e) => entries[key]?.[e] ?? 0),
-    ])
-  }
-
-  return {
-    types: ['timestamp', ...eventNames],
-    data,
   }
 }
