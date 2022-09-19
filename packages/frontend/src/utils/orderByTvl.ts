8ee6069045e1bcee731f4e61c216753ef8d44f83
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-09-19 14:12:23 +0200

    Add tests

--- packages/frontend/src/utils/orderByTvl.ts
+++ packages/frontend/src/utils/orderByTvl.ts
@@ -2,7 +2,7 @@ import { ApiMain, ProjectId } from '@l2beat/types'
 
 export function orderByTvl<T extends { id: ProjectId }>(
   projects: T[],
-  apiMain: ApiMain,
+  apiMain: Pick<ApiMain, 'projects'>,
 ): T[] {
   const getTvl = (project: T) =>
     apiMain.projects[project.id.toString()]?.charts.hourly.data.at(-1)?.[1] ?? 0
