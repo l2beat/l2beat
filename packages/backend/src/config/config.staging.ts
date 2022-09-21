6d8ce4d41362accaec1890b2428d33bf5b010ec7
Krzysztof Kaczor <krzkaczor@gmail.com>
2022-09-21 12:44:36 +0200

    Add staging config support

---
+++ packages/backend/src/config/config.staging.ts
@@ -0,0 +1,41 @@
+import { LogLevel } from '@l2beat/common'
+import { bridges, layer2s, tokenList } from '@l2beat/config'
+import { UnixTime } from '@l2beat/types'
+
+import { bridgeToProject, layer2ToProject } from '../model'
+import { Config } from './Config'
+import { getEnv } from './getEnv'
+
+export function getStagingConfig(): Config {
+  const name = 'Backend/Staging'
+  return {
+    name,
+    logger: {
+      logLevel: getEnv.integer('LOG_LEVEL', LogLevel.INFO),
+      format: 'json',
+    },
+    port: getEnv.integer('PORT'),
+    coingeckoApiKey: getEnv('COINGECKO_API_KEY'),
+    alchemyApiKey: getEnv('ALCHEMY_API_KEY'),
+    etherscanApiKey: getEnv('ETHERSCAN_API_KEY'),
+    databaseConnection: {
+      connectionString: getEnv('DATABASE_URL'),
+      ssl: { rejectUnauthorized: false },
+      application_name: name,
+    },
+    core: {
+      minBlockTimestamp: UnixTime.fromDate(new Date('2019-11-14T00:00:00Z')),
+      safeBlockRefreshIntervalMs: 5 * 60 * 1000,
+      safeTimeOffsetSeconds: 60 * 60,
+    },
+    tokens: tokenList.map((token) => ({
+      ...token,
+      priceStrategy: { type: 'market' },
+    })),
+    projects: layer2s.map(layer2ToProject).concat(bridges.map(bridgeToProject)),
+    syncEnabled: !getEnv.boolean('SYNC_DISABLED', false),
+    freshStart: false,
+    eventsSyncEnabled: true,
+    transactionCountSyncEnabled: false,
+  }
+}
