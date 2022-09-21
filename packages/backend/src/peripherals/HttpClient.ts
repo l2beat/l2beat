4c4a4ffbe9bc32be874f68b646d5b15bd2f7ef0e
Michał Sobieraj-Jakubiec <38423054+michalsidzej@users.noreply.github.com>
2022-09-21 10:49:06 +0200

    Deduplicate http client (#655)

--- packages/backend/src/peripherals/HttpClient.ts
+++
@@ -1,7 +0,0 @@
-import fetch, { RequestInit, Response } from 'node-fetch'
-
-export class HttpClient {
-  fetch(url: string, init?: RequestInit): Promise<Response> {
-    return fetch(url, init)
-  }
-}
