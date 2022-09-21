5f03715c4e4849bfc19e7b2b85dca4baa491b4af
Michal <michalsidzej@gmail.com>
2022-09-21 10:41:32 +0200

    Create unique task queue

---
+++ packages/common/src/tools/queue/UniqueTaskQueue.ts
@@ -0,0 +1,24 @@
+import { TaskQueue } from './TaskQueue'
+
+export class UniqueTaskQueue<T> extends TaskQueue<T> {
+  private taskSet: Set<T> = new Set()
+
+  addIfEmpty(task: T): void {
+    this.addUnique(task, super.addIfEmpty.bind(this))
+  }
+
+  addToBack(task: T): void {
+    this.addUnique(task, super.addToBack.bind(this))
+  }
+
+  addToFront(task: T): void {
+    this.addUnique(task, super.addToFront.bind(this))
+  }
+
+  private addUnique(task: T, method: (task: T) => void) {
+    if (!this.taskSet.has(task)) {
+      this.taskSet.add(task)
+      method(task)
+    }
+  }
+}
