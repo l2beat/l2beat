ea618e214e9ed53506a8799f2f2132857a2414da
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-09-22 14:38:37 +0200

    Fix tests in config

--- packages/config/test/projects.test.ts
+++ packages/config/test/projects.test.ts
@@ -4,33 +4,43 @@ import { expect } from 'earljs'
 import { bridges, layer2s } from '../src'
 
 describe('projects', () => {
-  describe('each slug is unique', () => {
-    const slugs = new Set<string>()
-
+  describe('every slug is valid', () => {
     for (const project of [...layer2s, ...bridges]) {
-      it(project.name, () => {
-        expect(slugs.has(project.slug)).toEqual(false)
-        slugs.add(project.slug)
+      it(project.display.slug, () => {
+        expect(project.display.slug).toEqual(
+          expect.stringMatching(/^[a-z\d]+$/),
+        )
       })
     }
   })
 
-  describe('each id is unique', () => {
+  describe('every slug is unique', () => {
+    const slugs = new Set<string>()
+
+    for (const project of [...layer2s, ...bridges]) {
+      it(project.display.name, () => {
+        expect(slugs.has(project.display.slug)).toEqual(false)
+        slugs.add(project.display.slug)
+      })
+    }
+  })
+
+  describe('every id is unique', () => {
     const ids = new Set<ProjectId>()
 
     for (const project of [...layer2s, ...bridges]) {
-      it(project.name, () => {
+      it(project.display.name, () => {
         expect(ids.has(project.id)).toEqual(false)
         ids.add(project.id)
       })
     }
   })
 
-  describe('each escrow is unique', () => {
+  describe('every escrow is unique', () => {
     const addresses = new Set<EthereumAddress>()
 
     for (const project of [...layer2s, ...bridges]) {
-      for (const { address } of project.escrows) {
+      for (const { address } of project.config.escrows) {
         it(address, () => {
           expect(addresses.has(EthereumAddress(address))).toEqual(false)
           addresses.add(EthereumAddress(address))
