5ac47b2dbf30e011b9bd7990d77a51d03d6bf455
Michał Sobieraj-Jakubiec <michalsidzej@gmail.com>
2022-09-22 13:44:01 +0200

    Add StarkexClient

---
+++ packages/backend/src/peripherals/starkex/StarkexClient.ts
@@ -0,0 +1,82 @@
+import { getErrorMessage, HttpClient, Logger } from '@l2beat/common'
+import { json } from '@l2beat/types'
+
+import { parseStarkexApiResponse } from './parseStarkexApiResponse'
+
+export type starkexProduct =
+  | 'dydx'
+  | 'sorare'
+  | 'immutable'
+  | 'myria'
+  | 'diversify'
+
+export class StarkexClient {
+  constructor(
+    private starkexApiUrl: string,
+    private starkexApiKey: string,
+    private httpClient: HttpClient,
+    private logger: Logger,
+  ) {
+    this.logger = this.logger.for(this)
+  }
+  // Rate limit?
+
+  // TODO: create BaseClient for this and EtherscanClient to avoid duplication
+  async getDailyCount(day: number, product: starkexProduct): Promise<number> {
+    const body = {
+      day_start: day,
+      day_end: day + 1,
+      product,
+      tx_type: '_all',
+      token_id: '_all',
+    }
+
+    const response = await this.call(body)
+    return response.count
+  }
+
+  async call(body: json) {
+    const start = Date.now()
+    const { httpResponse, error } = await this.httpClient
+      .fetch(this.starkexApiUrl, {
+        method: 'POST',
+        headers: {
+          accept: 'application/json',
+          'Content-Type': 'application/json',
+          'x-api-key': this.starkexApiKey,
+        },
+        body: JSON.stringify(body),
+        timeout: 10_000,
+      })
+      .then(
+        (httpResponse) => ({ httpResponse, error: undefined }),
+        (error: unknown) => ({ httpResponse: undefined, error }),
+      )
+    const timeMs = Date.now() - start
+
+    if (!httpResponse) {
+      const message = getErrorMessage(error)
+      this.recordError(timeMs, message)
+      throw error
+    }
+
+    const text = await httpResponse.text()
+
+    if (!httpResponse.ok) {
+      this.recordError(timeMs, text)
+      throw new Error(
+        `Server responded with non-2XX result: ${httpResponse.status} ${httpResponse.statusText}`,
+      )
+    }
+
+    const starkexApiResponse = parseStarkexApiResponse(text)
+
+    this.logger.debug({ type: 'success', timeMs })
+
+    return starkexApiResponse
+  }
+
+  private recordError(timeMs: number, message: string) {
+    this.logger.debug({ type: 'error', message, timeMs })
+  }
+}
