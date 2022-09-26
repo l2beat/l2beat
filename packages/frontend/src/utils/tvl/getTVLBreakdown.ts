53a9d61910c0e6606c03bf1118a3f4b63f84a42c
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-09-26 18:08:30 +0200

    Add more content to bridge tvl page (#678)

---
+++ packages/frontend/src/utils/tvl/getTVLBreakdown.ts
@@ -0,0 +1,101 @@
+import { safeGetTokenByAssetId } from '@l2beat/config'
+import { Token } from '@l2beat/types'
+
+import { TVLBreakdownProps } from '../../components/TVLBreakdown'
+import { formatPercent } from '../utils'
+
+export function getTvlBreakdown(
+  name: string,
+  associatedTokens: string[],
+  total: number,
+  tokens: Token[],
+): TVLBreakdownProps {
+  const partial = getPartialTVLBreakdown(associatedTokens, total, tokens)
+  return {
+    ...partial,
+    label: getTVLBreakdownLabel(partial, associatedTokens),
+    ...getTvlWarning(partial, name, associatedTokens),
+  }
+}
+
+function getPartialTVLBreakdown(
+  associatedTokens: string[],
+  total: number,
+  tokens: Token[],
+) {
+  let associated = 0
+  let ether = 0
+  let stable = 0
+  let other = 0
+
+  for (const { assetId, tvl } of tokens) {
+    const token = safeGetTokenByAssetId(assetId)
+    if (!token) {
+      other += tvl
+    } else if (associatedTokens.includes(token.symbol)) {
+      associated += tvl
+    } else if (token.category === 'ether') {
+      ether += tvl
+    } else if (token.category === 'stablecoin') {
+      stable += tvl
+    } else {
+      other += tvl
+    }
+  }
+
+  const empty = total === 0
+  return {
+    empty,
+    associated: empty ? 0 : associated / total,
+    ether: empty ? 0 : ether / total,
+    stable: empty ? 0 : stable / total,
+    other: empty ? 0 : other / total,
+  }
+}
+
+function getTVLBreakdownLabel(
+  breakdown: ReturnType<typeof getPartialTVLBreakdown>,
+  associatedTokens: string[],
+) {
+  if (breakdown.empty) {
+    return 'No tokens'
+  }
+  const toLabel = (text: string, x: number) =>
+    x === 0 ? '' : `${text} – ${(x * 100).toFixed(2)}%`
+
+  return [
+    toLabel(associatedTokens.join(' and '), breakdown.associated),
+    toLabel('Ether', breakdown.ether),
+    toLabel('Stablecoins', breakdown.stable),
+    toLabel('Other', breakdown.other),
+  ]
+    .filter((x) => x !== '')
+    .join('\n')
+}
+
+function getTvlWarning(
+  breakdown: ReturnType<typeof getPartialTVLBreakdown>,
+  name: string,
+  associatedTokens: string[],
+) {
+  let warning: string | undefined
+  if (breakdown.associated > 0.1) {
+    const percent = formatPercent(breakdown.associated)
+    if (associatedTokens.length === 1) {
+      const what = `The ${associatedTokens[0]} token associated with ${name}`
+      warning = `${what} accounts for ${percent} of the TVL!`
+    } else {
+      const joined = associatedTokens.join(' and ')
+      const what = `The ${joined} tokens associated with ${name}`
+      warning = `${what} account for ${percent} of the TVL!`
+    }
+  }
+  let warningSeverity: 'bad' | 'warning' | 'info' =
+    breakdown.associated > 0.9 ? 'bad' : 'warning'
+  if (name === 'Layer2.Finance') {
+    warning =
+      'The TVL is calculated incorrectly because it does not account for the assets locked in DeFi.'
+    warningSeverity = 'info'
+  }
+  return { warning, warningSeverity }
+}
