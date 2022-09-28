2540484bb15ce719ea415d0f7c417a9e6c4a7724
sin6pi7 <jac.zyla@gmail.com>
2022-09-28 12:23:46 +0200

    Rename in types

---
+++ packages/types/src/api/ActivityApi.ts
@@ -0,0 +1,22 @@
+import z from 'zod'
+
+import { branded } from '../branded'
+import { UnixTime } from '../UnixTime'
+
+export const ActivityApiChartPoint = z.tuple([
+  branded(z.number(), (n) => new UnixTime(n)),
+  z.number().int(),
+])
+export type ActivityApiChartPoint = z.infer<typeof ActivityApiChartPoint>
+
+export const ActivityApiChart = z.object({
+  types: z.tuple([z.literal('timestamp'), z.literal('daily tx count')]),
+  data: z.array(ActivityApiChartPoint),
+})
+export type ActivityApiChart = z.infer<typeof ActivityApiChart>
+
+export const ActivityApiResponse = z.object({
+  combined: ActivityApiChart,
+  projects: z.record(z.string(), z.optional(ActivityApiChart)),
+})
+export type ActivityApiResponse = z.infer<typeof ActivityApiResponse>
---
+++ packages/types/src/api/index.ts
@@ -0,0 +1,3 @@
+export * from './ActivityApi'
+export * from './EventsApi'
+export * from './TvlApi'
