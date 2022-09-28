9bc9e97ea2a217e41f3436d0b7f456ed11b5ff94
Michał Sobieraj-Jakubiec <38423054+michalsidzej@users.noreply.github.com>
2022-09-28 14:59:04 +0200

    Zksync transaction count (#686)
    * Add ZksyncClient
    
    * Add ZksyncTransactionRepository
    
    * Get last finalized block from api
    
    * Add getMissingRanges to repository
    
    * Add zksyncTransactionUpdater
    
    * Add functionality to Application.ts
    
    * Add distinct filter on getMissingRanges query
    
    * Start sync from block 1
    
    * Skip block 427
    
    * Enable transaction list pagination
    
    * Use ZOD's safeParse
    
    * Add RateLimiter to ZksyncClient
    
    * Improve handling pagination
    
    * Save block 427 in DB
    
    * Degrade logging level
    
    * Filter transactions without blockIndex
    
    * Check if paginated transactions match

---
+++ packages/backend/src/peripherals/zksync/ZksyncClient.ts
@@ -0,0 +1,142 @@
+import {
+  getErrorMessage,
+  HttpClient,
+  Logger,
+  RateLimiter,
+} from '@l2beat/common'
+import { UnixTime } from '@l2beat/types'
+
+import {
+  ZksyncBlocksResultSchema,
+  ZksyncResponse,
+  ZksyncTransactionResultSchema,
+} from './schemas'
+
+interface Transaction {
+  txHash: string
+  blockIndex: number
+  createdAt: UnixTime
+}
+
+const PAGE_LIMIT = 100
+
+export class ZksyncClient {
+  constructor(private httpClient: HttpClient, private logger: Logger) {
+    this.logger = logger.for(this)
+    const rateLimiter = new RateLimiter({
+      callsPerMinute: 3000,
+    })
+    this.call = rateLimiter.wrap(this.call.bind(this))
+  }
+
+  async getLatestBlock() {
+    const result = await this.call('blocks/lastFinalized')
+
+    const parsed = ZksyncBlocksResultSchema.safeParse(result)
+
+    if (!parsed.success) {
+      throw new TypeError('Invalid Zksync block schema')
+    }
+
+    return parsed.data.blockNumber
+  }
+
+  async getTransactionsInBlock(blockNumber: number) {
+    const firstPage = await this.getTransactionsPage(blockNumber, 'latest')
+
+    if (firstPage.list.length === 0) {
+      throw new Error('Transactions list empty!')
+    }
+
+    let transactions = firstPage.list
+    const count = firstPage.pagination.count
+    while (count - transactions.length > 0) {
+      const lastTx = transactions.at(-1)
+      if (!lastTx) {
+        throw new Error('Programmer error: Transactions list empty!')
+      }
+
+      const nextPage = await this.getTransactionsPage(
+        blockNumber,
+        lastTx.txHash,
+      )
+
+      if (nextPage.list[0].txHash !== lastTx.txHash) {
+        throw new Error('Invalid Zksync first transaction')
+      }
+
+      transactions = transactions.concat(nextPage.list.slice(1))
+    }
+
+    const filteredTransactions = transactions.filter(
+      (t): t is Transaction => t.blockIndex !== null,
+    )
+
+    return filteredTransactions
+  }
+
+  private async getTransactionsPage(blockNumber: number, from: string) {
+    const result = await this.call(`blocks/${blockNumber}/transactions`, {
+      from,
+      limit: PAGE_LIMIT.toString(),
+      direction: 'older',
+    })
+
+    const parsed = ZksyncTransactionResultSchema.safeParse(result)
+
+    if (!parsed.success) {
+      throw new TypeError('Invalid Zksync transactions schema')
+    }
+
+    return parsed.data
+  }
+
+  async call(path: string, params?: Record<string, string>) {
+    const query = new URLSearchParams(params)
+    const url = `https://api.zksync.io/api/v0.2/${path}?${query.toString()}`
+
+    const start = Date.now()
+    const { httpResponse, error } = await this.httpClient
+      .fetch(url, { timeout: 20_000 })
+      .then(
+        (httpResponse) => ({ httpResponse, error: undefined }),
+        (error: unknown) => ({ httpResponse: undefined, error }),
+      )
+    const timeMs = Date.now() - start
+
+    if (!httpResponse) {
+      const message = getErrorMessage(error)
+      this.recordError(path, timeMs, message)
+      throw error
+    }
+
+    const text = await httpResponse.text()
+
+    if (!httpResponse.ok) {
+      this.recordError(path, timeMs, text)
+      throw new Error(`Http error ${httpResponse.status}: ${text}`)
+    }
+
+    const json: unknown = JSON.parse(text)
+    const zksyncResponse = ZksyncResponse.safeParse(json)
+
+    if (!zksyncResponse.success) {
+      const message = 'Invalid Zksync response.'
+      this.recordError(path, timeMs, message)
+      throw new TypeError(message)
+    }
+
+    if (zksyncResponse.data.status !== 'success') {
+      this.recordError(path, timeMs, zksyncResponse.data.error.message)
+      throw new Error(zksyncResponse.data.error.message)
+    }
+
+    this.logger.debug({ type: 'success', timeMs, path })
+
+    return zksyncResponse.data.result
+  }
+
+  private recordError(path: string, timeMs: number, message: string) {
+    this.logger.debug({ type: 'error', message, timeMs, path })
+  }
+}
---
+++ packages/backend/test/core/transaction-count/ZksyncTransactionUpdater.test.ts
@@ -0,0 +1,129 @@
+import { Logger, mock } from '@l2beat/common'
+import { UnixTime } from '@l2beat/types'
+import { expect, mockFn } from 'earljs'
+import waitForExpect from 'wait-for-expect'
+
+import { Clock } from '../../../src/core/Clock'
+import { ZksyncTransactionUpdater } from '../../../src/core/transaction-count/ZksyncTransactionUpdater'
+import { ZksyncTransactionRepository } from '../../../src/peripherals/database/ZksyncTransactionRepository'
+import { ZksyncClient } from '../../../src/peripherals/zksync'
+
+describe(ZksyncTransactionUpdater.name, () => {
+  describe(ZksyncTransactionUpdater.prototype.start.name, () => {
+    it('skips known blocks', async () => {
+      const zksyncClient = mock<ZksyncClient>({
+        getLatestBlock: async () => 5,
+        getTransactionsInBlock: async () => [],
+      })
+      const zksyncTransactionRepository = mock<ZksyncTransactionRepository>({
+        getMissingRanges: async () => [
+          [-Infinity, -1],
+          [2, 3],
+          [5, Infinity],
+        ],
+        addMany: async () => 0,
+      })
+      const clock = mock<Clock>({
+        onNewHour: (callback) => {
+          callback(UnixTime.now())
+          return () => {}
+        },
+      })
+      const zksyncTransactionUpdater = new ZksyncTransactionUpdater(
+        zksyncClient,
+        zksyncTransactionRepository,
+        clock,
+        Logger.SILENT,
+      )
+      zksyncTransactionUpdater.start()
+
+      await waitForExpect(() => {
+        expect(zksyncClient.getTransactionsInBlock).toHaveBeenCalledExactlyWith(
+          [[2], [5]],
+        )
+      })
+    })
+  })
+
+  describe(ZksyncTransactionUpdater.prototype.update.name, () => {
+    it('does not query the same blocks multiple times', async () => {
+      const zksyncClient = mock<ZksyncClient>({
+        getTransactionsInBlock: async () => [],
+        getLatestBlock: async () => 5,
+      })
+      const zksyncTransactionRepository = mock<ZksyncTransactionRepository>({
+        getMissingRanges: async () => [
+          [-Infinity, -1],
+          [2, 3],
+          [5, Infinity],
+        ],
+        addMany: async () => 0,
+      })
+      const clock = mock<Clock>({
+        onNewHour: (callback) => {
+          callback(UnixTime.now())
+          return () => {}
+        },
+      })
+      const zksyncTransactionUpdater = new ZksyncTransactionUpdater(
+        zksyncClient,
+        zksyncTransactionRepository,
+        clock,
+        Logger.SILENT,
+      )
+      await zksyncTransactionUpdater.update()
+      await zksyncTransactionUpdater.update()
+
+      await waitForExpect(() => {
+        expect(zksyncClient.getTransactionsInBlock).toHaveBeenCalledExactlyWith(
+          [[2], [5]],
+        )
+      })
+    })
+  })
+
+  describe(ZksyncTransactionUpdater.prototype.updateBlock.name, () => {
+    it('downloads and saves transactions to DB', async () => {
+      const transactions1 = [{ blockIndex: 0, createdAt: new UnixTime(0) }]
+      const transactions2 = [
+        { blockIndex: 1, createdAt: new UnixTime(2) },
+        { blockIndex: 0, createdAt: new UnixTime(1) },
+      ]
+      const zksyncClient = mock<ZksyncClient>({
+        getTransactionsInBlock: mockFn()
+          .resolvesToOnce(transactions1)
+          .resolvesToOnce(transactions2),
+      })
+      const zksyncTransactionRepository = mock<ZksyncTransactionRepository>({
+        addMany: async () => 0,
+      })
+      const clock = mock<Clock>()
+      const zksyncTransactionUpdater = new ZksyncTransactionUpdater(
+        zksyncClient,
+        zksyncTransactionRepository,
+        clock,
+        Logger.SILENT,
+      )
+
+      await zksyncTransactionUpdater.updateBlock(1)
+      await zksyncTransactionUpdater.updateBlock(2)
+
+      expect(zksyncTransactionRepository.addMany).toHaveBeenCalledExactlyWith([
+        [
+          transactions1.map((transaction) => ({
+            blockIndex: transaction.blockIndex,
+            blockNumber: 1,
+            timestamp: transaction.createdAt,
+          })),
+        ],
+        [
+          transactions2.map((transaction) => ({
+            blockIndex: transaction.blockIndex,
+            blockNumber: 2,
+            timestamp: transaction.createdAt,
+          })),
+        ],
+      ])
+    })
+  })
+})
