43cfee8839ebc49caeb224836a7c813157dd7f40
sin6pi7 <jac.zyla@gmail.com>
2022-09-28 13:27:38 +0200

    Bring back /api/main as main source

--- packages/frontend/src/build/fetchTvlApi.ts
+++ packages/frontend/src/build/fetchTvlApi.ts
@@ -7,7 +7,7 @@ export async function fetchTvlApi(
   apiUrl: string,
   skipCache = false,
 ): Promise<TvlApiResponse> {
-  const url = apiUrl + '/api/tvl'
+  const url = apiUrl + '/api/main' // change to /api/tvl once backend can serve that
 
   if (!skipCache) {
     const cached = await readCachedData(url)
