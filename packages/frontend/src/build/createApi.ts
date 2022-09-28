bfe522cbcc515bb6950cbb9d32464ec81f03f11d
Antoni Pawlak <antoni.pawlak@pm.me>
2022-09-28 11:20:12 +0200

    remove unused code

--- packages/frontend/src/build/createApi.ts
+++ packages/frontend/src/build/createApi.ts
@@ -61,9 +61,3 @@ function getTps(txCount: number): number {
 
   return +(txCount / SECONDS_IN_A_DAY).toFixed(2)
 }
-
-function getTps(txCount: number): number {
-  const SECONDS_IN_A_DAY = 24 * 60 * 60
-
-  return +(txCount / SECONDS_IN_A_DAY).toFixed(2)
-}
