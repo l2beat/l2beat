c31b031d529d192d4d514b0637fba648c4ed08cf
Antoni Pawlak <antoni.pawlak@pm.me>
2022-09-29 12:49:16 +0200

    Add activity table (#696)
    * chart works with sample data
    
    * revert generic chart idea
    
    * get data from api
    
    * hide part of chart UI
    
    * fix post merge errors
    
    * fix formatting
    
    * add requested changes
    
    * refactor api naming
    
    * refactor
    
    * remove unused code
    
    * remove unused code
    
    * change count to tps in header
    
    * change count to tps in chart
    
    * add table skeleton
    
    * add real data to table
    
    * fix linter error
    
    * fix post merge error
    
    * remove unused code
    
    * remove not needed code
    
    * fix weekly count
    
    * fix type errors
    
    * refactor getProps
    
    * refactor logic of getProps
    
    * tests for getTpsDaily
    
    * add tests for weekly change
    
    * add tests for weekly count

--- packages/frontend/src/pages/scaling-activity/props/getActivityView.ts
+++ packages/frontend/src/pages/scaling-activity/props/getActivityView.ts
@@ -1,5 +1,36 @@
-import { ActivityViewProps } from '../view/ActivityView'
+import { Layer2 } from '@l2beat/config'
+import { ActivityApiResponse } from '@l2beat/types'
 
-export function getActivityView(): ActivityViewProps {
-  return {}
+import { getTpsDaily } from '../../../utils/activity/getTpsDaily'
+import { getTpsWeeklyChange } from '../../../utils/activity/getTpsWeeklyChange'
+import { getTransactionWeeklyCount } from '../../../utils/activity/getTransactionWeeklyCount'
+import { ActivityViewEntry, ActivityViewProps } from '../view/ActivityView'
+
+export function getActivityView(
+  projects: Layer2[],
+  apiActivity: ActivityApiResponse,
+): ActivityViewProps {
+  return {
+    items: projects
+      .map((x) => getActivityViewEntry(x, apiActivity))
+      .sort((a, b) => +b.tpsDaily - +a.tpsDaily),
+  }
+}
+
+export function getActivityViewEntry(
+  project: Layer2,
+  apiActivity: ActivityApiResponse,
+): ActivityViewEntry {
+  const data = apiActivity.projects[project.id.toString()]?.data
+  const tpsDaily = getTpsDaily(data)
+  const tpsWeeklyChange = getTpsWeeklyChange(data)
+  const transactionsWeeklyCount = getTransactionWeeklyCount(data)
+
+  return {
+    name: project.display.name,
+    slug: project.display.slug,
+    tpsDaily: tpsDaily?.toString() ?? '',
+    tpsWeeklyChange,
+    transactionsWeeklyCount: transactionsWeeklyCount?.toString() ?? '',
+  }
 }
--- packages/frontend/src/pages/scaling-activity/view/ActivityView.tsx
+++ packages/frontend/src/pages/scaling-activity/view/ActivityView.tsx
@@ -1,10 +1,48 @@
 import classNames from 'classnames'
 import React from 'react'
 
+import { PercentChange } from '../../../components'
+import { Column, TableView } from '../../../components/TableView'
+
 export interface ActivityViewProps {
+  items: ActivityViewEntry[]
   className?: string
 }
 
-export function ActivityView({ className }: ActivityViewProps) {
-  return <section className={classNames('ActivityView', className)}></section>
+export interface ActivityViewEntry {
+  name: string
+  slug: string
+  tpsDaily: string
+  tpsWeeklyChange: string
+  transactionsWeeklyCount: string
+}
+
+export function ActivityView({ items, className }: ActivityViewProps) {
+  const columns: Column<ActivityViewEntry>[] = [
+    {
+      name: 'Name',
+      getValue: (project) => project.name,
+    },
+    {
+      name: 'TPS',
+      alignRight: true,
+      getValue: (project) => project.tpsDaily,
+    },
+    {
+      name: '7d Change',
+      alignRight: true,
+      getValue: (project) => <PercentChange value={project.tpsWeeklyChange} />,
+    },
+    {
+      name: '7d Count',
+      alignRight: true,
+      getValue: (project) => project.transactionsWeeklyCount,
+    },
+  ]
+
+  return (
+    <section className={classNames('ActivityView', className)}>
+      <TableView items={items} columns={columns} />
+    </section>
+  )
 }
