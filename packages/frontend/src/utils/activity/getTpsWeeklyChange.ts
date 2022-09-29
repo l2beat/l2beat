eed5213b25a32124d4622c013264897e2432a531
Antoni Pawlak <antoni.pawlak@pm.me>
2022-09-29 12:18:40 +0200

    tests for getTpsDaily

--- packages/frontend/src/utils/activity/getTpsWeeklyChange.ts
+++ packages/frontend/src/utils/activity/getTpsWeeklyChange.ts
@@ -3,6 +3,8 @@ import { ActivityApiChartPoint } from '@l2beat/types'
 import { getPercentageChange } from '../utils'
 import { getTpsDaily } from './getTpsDaily'
 
+const SECONDS_IN_A_DAY = 24 * 60 * 60
+
 export function getTpsWeeklyChange(
   data: ActivityApiChartPoint[] | undefined,
 ): string {
@@ -10,12 +12,10 @@ export function getTpsWeeklyChange(
     return ''
   }
 
-  const SECONDS_IN_A_DAY = 24 * 60 * 60
-
   const tps = getTpsDaily(data)
   const tpsSevenDaysAgo = (data.at(-7)?.[1] ?? 0) / SECONDS_IN_A_DAY
 
-  if(!tps || !tpsSevenDaysAgo ) {
+  if (!tps || !tpsSevenDaysAgo) {
     return ''
   }
 
---
+++ packages/frontend/test/utils/activity/getTpsDaily.test.ts
@@ -0,0 +1,43 @@
+import { ActivityApiChartPoint, UnixTime } from '@l2beat/types'
+import { expect } from 'earljs'
+
+import { getTpsDaily } from '../../../src/utils/activity/getTpsDaily'
+
+const SECONDS_IN_A_DAY = 24 * 60 * 60
+
+describe(getTpsDaily.name, () => {
+  it('calculates correctly', () => {
+    const data: ActivityApiChartPoint[] = [
+      [new UnixTime(0), 0],
+      [new UnixTime(1), 1],
+      [new UnixTime(2), SECONDS_IN_A_DAY],
+    ]
+
+    const result = getTpsDaily(data)
+
+    expect(result).toEqual(1)
+  })
+  it('returns undefined if data not defined', () => {
+    const data = undefined
+
+    const result = getTpsDaily(data)
+
+    expect(result).toEqual(undefined)
+  })
+  it('returns undefined if data is empty', () => {
+    const result = getTpsDaily([])
+
+    expect(result).toEqual(undefined)
+  })
+  it('rounds correctly', () => {
+    const data: ActivityApiChartPoint[] = [
+      [new UnixTime(0), 0],
+      [new UnixTime(1), 1],
+      [new UnixTime(2), SECONDS_IN_A_DAY * 1.456],
+    ]
+
+    const result = getTpsDaily(data)
+
+    expect(result).toEqual(1.46)
+  })
+})
