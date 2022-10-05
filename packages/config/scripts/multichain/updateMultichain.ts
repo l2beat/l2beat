fa940be05db7f7157a5e2b75f9c7dd8aa892c3d1
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-10-05 11:24:54 +0200

    Add Multichain types

--- packages/config/scripts/multichain/updateMultichain.ts
+++ packages/config/scripts/multichain/updateMultichain.ts
@@ -1,54 +1,53 @@
-import { writeFile } from "fs/promises"
-import fetch from "node-fetch"
-import { tokenList } from "@l2beat/config";
+import { writeFile } from 'fs/promises'
+import fetch from 'node-fetch'
+import { tokenList } from '@l2beat/config'
+import { MultichainApiResponse } from './MultichainApiResponse'
 
-main();
+main()
 async function main() {
-  const res = await fetch(
-    "https://bridgeapi.anyswap.exchange/v4/tokenlistv4/1"
-  );
-  const json = await res.json();
+  const res = await fetch('https://bridgeapi.anyswap.exchange/v4/tokenlistv4/1')
+  const json: MultichainApiResponse = await res.json()
 
-  const escrows = [];
+  const escrows = []
 
-  for (const [key, token] of Object.entries<any>(json)) {
-    for (const [id, chain] of Object.entries<any>(token.destChains)) {
-      for (const [hash, spec] of Object.entries<any>(chain)) {
-        if (spec.type === "swapin") {
+  for (const [key, token] of Object.entries(json)) {
+    for (const [id, chain] of Object.entries(token.destChains)) {
+      for (const [hash, spec] of Object.entries(chain)) {
+        if (spec.type === 'swapin') {
           escrows.push({
             tokenName: token.name,
             tokenSymbol: token.symbol,
             tokenAddress: token.address,
             chainId: id,
             escrowAddress: spec.DepositAddress,
-          });
+          })
         }
       }
     }
   }
 
-  const groupedEscrowMap = new Map();
+  const groupedEscrowMap = new Map()
   for (const escrow of escrows) {
     const groupedEntry = groupedEscrowMap.get(escrow.escrowAddress) ?? {
       chainId: escrow.chainId,
       tokens: [],
-    };
-    groupedEscrowMap.set(escrow.escrowAddress, groupedEntry);
+    }
+    groupedEscrowMap.set(escrow.escrowAddress, groupedEntry)
     if (escrow.chainId !== groupedEntry.chainId) {
-      console.log({ escrow, groupedEntry });
-      throw new Error("Duplicate chainId");
+      console.log({ escrow, groupedEntry })
+      throw new Error('Duplicate chainId')
     }
     const token = tokenList.find(
-      (x) => x.address?.toLowerCase() === escrow.tokenAddress.toLowerCase()
-    );
+      (x) => x.address?.toLowerCase() === escrow.tokenAddress.toLowerCase(),
+    )
     if (token) {
-      groupedEntry.tokens.push(token.symbol);
+      groupedEntry.tokens.push(token.symbol)
     } else {
       groupedEntry.tokens.push({
         unknown: true,
         name: escrow.tokenName,
         address: escrow.tokenAddress,
-      });
+      })
     }
   }
 
@@ -57,8 +56,8 @@ async function main() {
       address,
       chainId: entry.chainId,
       tokens: entry.tokens,
-    })
-  );
+    }),
+  )
 
-  writeFile("escrows.json", JSON.stringify(groupedEscrows, null, 2));
+  writeFile('escrows.json', JSON.stringify(groupedEscrows, null, 2))
 }
