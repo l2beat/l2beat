1d794d4b880eef98f271c802d837a5b671a1ed36
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-10-05 19:49:02 +0200

    Filter using tokenList

--- packages/config/scripts/multichain/generateFinalConfig.ts
+++ packages/config/scripts/multichain/generateFinalConfig.ts
@@ -1,7 +1,30 @@
+import { tokenList } from '../../src'
 import { IntermediateConfig } from './generateIntermediateConfig'
 
 export function generateFinalConfig(config: IntermediateConfig) {
+  const destinations = config.chains.map((x) => x.name).filter(noUndefined)
+
+  const escrows = config.escrows
+    .map((escrow) => ({
+      ...escrow,
+      tokens: escrow.tokens
+        .map((address) =>
+          address === 'ETH'
+            ? 'ETH'
+            : tokenList.find((y) => y.address === address)?.symbol,
+        )
+        .filter(noUndefined),
+    }))
+    .filter((escrow) => escrow.tokens.length > 0)
+
+  // TODO: figure out sinceTimestamp
+
   return {
-    final: true,
+    destinations,
+    escrows,
   }
+}
+
+function noUndefined<T>(value: T | undefined): value is T {
+  return value !== undefined
 }
