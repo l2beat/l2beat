50f9fdfde0161c5d5495c0b2169844f7e66e9a9d
Bartek Kiepuszewski <bkiepuszewski@gmail.com>
2022-10-06 08:47:51 +0200

    Add near rainbow bridge info (#749)
    * add near bridge info
    
    * remove comment
    
    * fix format
    
    Co-authored-by: Piotr Szlachciak <szlachciak.piotr@gmail.com>

--- packages/config/src/bridges/near.ts
+++ packages/config/src/bridges/near.ts
@@ -1,5 +1,6 @@
 import { ProjectId, UnixTime } from '@l2beat/types'
 
+import { CONTRACTS } from '../layer2s/common'
 import { Bridge } from './types'
 
 export const near: Bridge = {
@@ -9,8 +10,14 @@ export const near: Bridge = {
     name: 'Rainbow Bridge',
     slug: 'near',
     links: {
-      websites: ['https://near.org/bridge/'],
+      websites: ['https://near.org/'],
+      explorers: ['https://explorer.near.org/', 'https://aurorascan.dev/'],
+      apps: ['https://rainbowbridge.app/'],
+      repositories: ['https://github.com/aurora-is-near/rainbow-bridge'],
+      socialMedia: ['https://twitter.com/auroraisnear'],
     },
+    description:
+      'Rainbow bridge is an light client based bridge between NEAR/AURORA and Ethereum that allows for asset and data movement between these chains. For better gas efficiency from NEAR to Ethereum it leverages optimistic validation which adds some trust assumption and latency.',
   },
   config: {
     associatedTokens: ['AURORA'],
@@ -24,16 +31,14 @@ export const near: Bridge = {
           'AURORA',
           'USDT',
           'WBTC',
-          // PLY,
-          // OCT
+          //'PLY',
+          //'OCT',
+          //'BSTN',
+          'WOO',
+          'FRAX',
         ],
       },
     ],
-  },
-  technology: {
-    canonical: true,
-    category: 'Lock-Mint',
-    destination: ['Near', 'Aurora'],
   },
   riskView: {
     validation: {
@@ -48,5 +53,109 @@ export const near: Bridge = {
         'Bridge cannot be upgraded but 3/6 Admin Multisig can move all funds out of the bridge via admin functions with no warning.',
       sentiment: 'bad',
     },
+    destinationToken: {
+      value: '???',
+      description:
+        'Tokens minted on AURORA do not appear to be verified on aurorascan.dev.',
+      sentiment: 'bad',
+    },
   },
+  technology: {
+    canonical: true,
+    category: 'Lock-Mint',
+    destination: ['Near', 'Aurora'],
+    principleOfOperation: {
+      name: 'Principle of operation',
+      description:
+        'Rainbow is a Lock-Mint bridge that locks tokens in the escrow contracts on Ethereum and mints tokens on the Aurora or NEAR network. When bridging back to Ethereum tokens are burned on Aurora / NEAR and then released from the escrow on Ethereum.',
+      references: [],
+      risks: [],
+    },
+    validation: {
+      name: 'Both inbound and outbound transfers are verified by the light client',
+      description:
+        'Near Rainbow bridge implements light client for both inbound and outbound transfers. For inbound transfers, checkpoints of near state are submitted every 4 hours. \
+        These are optimistically assumed to be signed by 2/3 of Near Validators. The signatures are not immediately verified by Ethereum due to a different signature scheme \
+        used on NEAR and - as a result - very high gas cost on Ethereum. If signatures are found to be invalid, checkpoints can be permissionlessly challenged. \
+        Users can withdraw funds by submitting a merkle proof of a burn event against the checkpoint. \
+        For outbound transfers, Ethereum light client is implemented on NEAR and a merkle proof of a lock event must be presented.',
+      references: [],
+      risks: [
+        {
+          category: 'Funds can be stolen if',
+          text: 'incorrect checkpoint is submitted and nobody challenges it.',
+        },
+        {
+          category: 'Funds can be stolen if',
+          text: 'bridge administrator removes funds from the bridge escrow.',
+          isCritical: true,
+        },
+      ],
+      isIncomplete: true,
+    },
+    destinationToken: {
+      name: 'Destination tokens are not verified',
+      description:
+        'Tokens transferred end up as ERC20 tokens on AURORA or NEAR and they do not appear to be verified.',
+      references: [],
+      risks: [
+        {
+          category: 'Funds can be stolen if',
+          text: 'destination token contract is maliciously upgraded.',
+          isCritical: true,
+        },
+      ],
+      isIncomplete: true,
+    },
+  },
+  contracts: {
+    addresses: [
+      {
+        address: '0x23Ddd3e3692d1861Ed57EDE224608875809e127f',
+        name: 'ERC20Locker',
+        description: 'Escrow contract for ERC20 tokens.',
+      },
+      {
+        address: '0x051AD3F020274910065Dcb421629cd2e6E5b46c4',
+        name: 'NearProver',
+        description: 'Contract verifying merkle proofs, used for withdrawals.',
+      },
+      {
+        address: '0x3FEFc5A4B1c02f21cBc8D3613643ba0635b9a873', // new near bridge
+        name: 'NearBridge (new)',
+        description: 'Contract storing Near state checkpoints.',
+      },
+      {
+        address: '0x3be7Df8dB39996a837041bb8Ee0dAdf60F767038', // old near bridge
+        name: 'NearBridge (old)',
+        description: 'Contract storing Near state checkpoints.',
+      },
+    ],
+    risks: [CONTRACTS.UPGRADE_NO_DELAY_RISK],
+  },
+  permissions: [
+    {
+      accounts: [
+        {
+          address: '0x2468603819Bf09Ed3Fb6f3EFeff24B1955f3CDE1 ',
+          type: 'MultiSig',
+        },
+      ],
+      name: 'Aurora MultiSig',
+      description:
+        'Can pause and reconfigure the bridge with no delay, can remove all tokens from Escrow via adminTransfer() method.',
+    },
+    {
+      accounts: [
+        { address: '0x0c406517c7B2f86d5935fB0a78511b7498B94413', type: 'EOA' },
+        { address: '0x21458c08e9a0Df8cf18132def9fEA0620962618E', type: 'EOA' },
+        { address: '0x2B5948a65BE418a54880501cfaA0969AC3fAb40c', type: 'EOA' },
+        { address: '0x8ddFfe05453d3b50774fb3938fD85Ecd1CC5E955', type: 'EOA' },
+        { address: '0xC3E34337B6505d503DcA3e99303Ad8BE6DB05984', type: 'EOA' },
+        { address: '0xD127323856C3703f94Dc1de18985cdCa2a8bA2b5', type: 'EOA' },
+      ],
+      name: 'MultiSig Participants',
+      description: 'Participants of the 3/6 Aurora MultiSig.',
+    },
+  ],
 }
