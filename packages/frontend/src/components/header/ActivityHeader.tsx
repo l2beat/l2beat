36040c1bc24a07aeb850b088b1e13e874e849c21
Michal <michalsidzej@gmail.com>
2022-10-13 21:59:54 +0200

    Show proper scaling factor description

--- packages/frontend/src/components/header/ActivityHeader.tsx
+++ packages/frontend/src/components/header/ActivityHeader.tsx
@@ -11,7 +11,7 @@ export function ActivityHeader(props: ActivityHeaderProps) {
         <h1 className="font-bold text-3xl mb-1">Activity</h1>
         <p className="font-bold text-3xl text-right">
           <span className="hidden md:inline text-sm md:text-2xl">
-            Speedup factor:{' '}
+            Scaling factor:{' '}
           </span>
           {props.scalingFactor}
         </p>
@@ -21,7 +21,13 @@ export function ActivityHeader(props: ActivityHeaderProps) {
           Transactions per second
         </p>
         <p className="text-gray-500 dark:text-gray-600 text-xs md:text-base text-right w-full md:w-auto">
-          More txs on Ethereum because of L2s
+          More Ethereum txs through L2s{' '}
+          <span
+            className="Tooltip"
+            title="Last week (L2 txs + ETH txs) / ETH txs"
+          >
+            [info icon]
+          </span>
         </p>
       </div>
       <hr className="md:hidden mt-2 border-gray-300 dark:border-gray-850" />
