4e8e6221ae6c75dd7da39adcf3d6f21af081f3d5
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-10-15 23:41:35 +0200

    Deduplicate code

--- packages/frontend/src/components/chart/configure/update/updateMouseMoved.ts
+++ packages/frontend/src/components/chart/configure/update/updateMouseMoved.ts
@@ -1,17 +1,14 @@
 import { Effect } from '../effects/effects'
 import { MouseMovedMessage } from '../messages'
 import { State } from '../state/State'
+import { getHoverIndex } from './view/getHoverIndex'
 
 export function updateMouseMoved(
   state: State,
   message: MouseMovedMessage,
 ): [State, Effect[]] {
-  let showHoverAtIndex: number | undefined
   const points = state.view.chart?.points.length
-  if (points) {
-    // This only works if the points are evenly distributed
-    showHoverAtIndex = Math.round(message.mouseX * (points - 1))
-  }
+  const showHoverAtIndex = getHoverIndex(message.mouseX, points)
   const newState: State = {
     ...state,
     controls: {
--- packages/frontend/src/components/chart/configure/update/view/getHoverIndex.ts
+++ packages/frontend/src/components/chart/configure/update/view/getHoverIndex.ts
@@ -1,5 +1,8 @@
-export function getHoverIndex(mouseX: number | undefined, points: number) {
-  if (mouseX && points > 0) {
+export function getHoverIndex(
+  mouseX: number | undefined,
+  points: number | undefined,
+) {
+  if (mouseX && points) {
     // This only works if the points are evenly distributed
     return Math.round(mouseX * (points - 1))
   }
