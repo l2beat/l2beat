77d7ea25f74e811f7846bd04f6fcc65f7258dfe7
Michal <michalsidzej@gmail.com>
2022-10-17 12:46:14 +0200

    Refactor

--- packages/frontend/src/components/chart/RangeControls.tsx
+++ packages/frontend/src/components/chart/RangeControls.tsx
@@ -9,20 +9,22 @@ export function RangeControls({
   days: number
   type: 'tvl' | 'activity'
 }) {
-  const options = [
-    {
-      value: '7D',
-      checked: days === 7,
-      className: type === 'activity' ? '!hidden' : undefined,
-    },
-    { value: '30D', checked: days === 30 },
-    { value: '90D', className: '!hidden sm:!block' },
-    { value: '180D', className: '!hidden sm:!block' },
-    { value: '1Y' },
-    { value: 'MAX' },
-  ]
-
   return (
-    <RadioGroup role="chart-range-controls" name="range" options={options} />
+    <RadioGroup
+      role="chart-range-controls"
+      name="range"
+      options={[
+        {
+          value: '7D',
+          checked: days === 7,
+          className: type === 'activity' ? '!hidden' : undefined,
+        },
+        { value: '30D', checked: days === 30 },
+        { value: '90D', className: '!hidden sm:!block' },
+        { value: '180D', className: '!hidden sm:!block' },
+        { value: '1Y' },
+        { value: 'MAX' },
+      ]}
+    />
   )
 }
