18559f0c4790413cc2ee510f1e6342ca84396f93
Michal <michalsidzej@gmail.com>
2022-10-17 12:45:21 +0200

    Add hidden to 7d range control in activity chart

--- packages/frontend/src/components/chart/RangeControls.tsx
+++ packages/frontend/src/components/chart/RangeControls.tsx
@@ -10,16 +10,17 @@ export function RangeControls({
   type: 'tvl' | 'activity'
 }) {
   const options = [
+    {
+      value: '7D',
+      checked: days === 7,
+      className: type === 'activity' ? '!hidden' : undefined,
+    },
     { value: '30D', checked: days === 30 },
     { value: '90D', className: '!hidden sm:!block' },
     { value: '180D', className: '!hidden sm:!block' },
     { value: '1Y' },
     { value: 'MAX' },
   ]
-
-  if (type !== 'activity') {
-    options.unshift({ value: '7D', checked: days === 7 })
-  }
 
   return (
     <RadioGroup role="chart-range-controls" name="range" options={options} />
