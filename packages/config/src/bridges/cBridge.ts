4ba4664c2b335fab62a91d1718ab6f226ee4642b
Antoni Pawlak <antoni.pawlak@pm.me>
2022-10-18 14:14:19 +0200

    add desc and links

--- packages/config/src/bridges/cBridge.ts
+++ packages/config/src/bridges/cBridge.ts
@@ -6,10 +6,21 @@ export const cBridge: Bridge = {
   type: 'bridge',
   id: ProjectId('cbridge'),
   display: {
-    name: 'Celer V2 cBridge',
+    name: 'cBridge V2',
     slug: 'cbridge',
+    description:
+      'Celer is a general messaging bridge, token-bridge and a liquidity network that leverages the "state-guardian" aka SGN thats operated by validators runnning on tendermint engine to perform cross-chain transactions.',
     links: {
-      websites: ['https://hop.exchange/'],
+      websites: ['https://www.celer.network/'],
+      apps: ['https://cbridge.celer.network/'],
+      explorers: ['https://cbridge-analytics.celer.network/'],
+      documentation: ['https://cbridge-docs.celer.network/'],
+      repositories: ['https://github.com/celer-network'],
+      socialMedia: [
+        'https://discord.gg/uGx4fjQ',
+        'https://t.me/celernetwork',
+        'https://twitter.com/CelerNetwork',
+      ],
     },
   },
   config: {
