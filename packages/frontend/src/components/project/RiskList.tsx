f94097628fd04a1deb56a5b0ea3672cb5a69aee6
Michal <michalsidzej@gmail.com>
2022-10-19 18:37:51 +0200

    Restyle RiskList

--- packages/frontend/src/components/project/RiskList.tsx
+++ packages/frontend/src/components/project/RiskList.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { ShieldBadIcon, ShieldWarnIcon } from '../icons'
+import { ShieldIcon } from '../icons'
 
 export interface RiskList {
   risks: TechnologyRisk[]
@@ -13,21 +13,23 @@ export interface TechnologyRisk {
 
 export function RiskList({ risks }: RiskList) {
   return (
-    <ul className="RiskList">
+    <ul className="bg-red-600 rounded-lg p-4">
       {risks.map((risk, i) => (
-        <li className="RiskList-Item" key={i}>
-          {risk.isCritical ? (
-            <>
-              <ShieldBadIcon />
-              {risk.text.slice(0, -1)} <strong>(CRITICAL)</strong>
-              {risk.text.slice(-1)}
-            </>
-          ) : (
-            <>
-              <ShieldWarnIcon />
-              {risk.text}
-            </>
-          )}
+        <li className="mt-2 first:mt-0 flex gap-3" key={i}>
+          <ShieldIcon className="inline-block fill-red-700 dark:fill-red-300" />
+          <p>
+            {risk.isCritical ? (
+              <>
+                {risk.text.slice(0, -1)}{' '}
+                <strong className="text-red-700 dark:text-red-300">
+                  (CRITICAL)
+                </strong>
+                {risk.text.slice(-1)}
+              </>
+            ) : (
+              risk.text
+            )}
+          </p>
         </li>
       ))}
     </ul>
