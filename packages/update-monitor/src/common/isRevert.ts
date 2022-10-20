210a83c0ef631702c53db40280378a84cfc6ccdf
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-10-20 14:33:41 +0200

    Handle edge cases

--- packages/update-monitor/src/common/isRevert.ts
+++ packages/update-monitor/src/common/isRevert.ts
@@ -4,6 +4,8 @@ export function isRevert(e: unknown) {
   }
   return (
     e.message.includes('invalid opcode: INVALID') ||
+    e.message.includes('CALL_EXCEPTION') ||
+    e.message.includes('revert') ||
     e.message.includes('reverted')
   )
 }
