ba083f1f4d2426ba71bf349e7dd7217919242548
Michał Sobieraj-Jakubiec <38423054+michalsidzej@users.noreply.github.com>
2022-10-24 14:02:01 +0200

    Add Eth chart line icon (#850)
    * Add Eth line icon
    
    * Pass fill color as a css class

--- packages/frontend/src/components/chart/EthereumActivityToggle.tsx
+++ packages/frontend/src/components/chart/EthereumActivityToggle.tsx
@@ -2,6 +2,7 @@ import cx from 'classnames'
 import React from 'react'
 
 import { Checkbox } from '../Checkbox'
+import { EthereumLineIcon } from '../icons/chart/EthereumLineIcon'
 
 interface EthereumActivityToggleProps {
   showToggle: boolean
@@ -13,7 +14,12 @@ export function EthereumActivityToggle(props: EthereumActivityToggleProps) {
       className={cx(!props.showToggle && 'hidden')}
       role="toggle-ethereum-activity"
       id="ethereum-activity"
-      label="ETH Mainnet Transaction"
+      label={
+        <span className="flex items-center gap-2">
+          <EthereumLineIcon className="w-2.5 h-1.5 hidden sm:inline-block fill-blue-500" />
+          ETH Mainnet Transactions
+        </span>
+      }
       defaultChecked={props.showToggle}
     />
   )
