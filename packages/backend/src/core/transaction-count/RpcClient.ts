d8d33da6aa0a41fdc784ae6f1eda41e4a6f4a216
Michał Sobieraj-Jakubiec <38423054+michalsidzej@users.noreply.github.com>
2022-10-26 14:49:58 +0200

    Implement binary search to look for the newest wanted block (#872)
    * Implement getBlockNumberAtOrBefore
    
    * Use previous lastBlock as a start parameter for b_search
    
    * Use Math.floor

--- packages/backend/src/core/transaction-count/RpcClient.ts
+++ packages/backend/src/core/transaction-count/RpcClient.ts
@@ -1,5 +1,8 @@
+import { UnixTime } from '@l2beat/types'
+
 export interface RpcClient {
   getBlockNumber(): Promise<number>
+  getBlockNumberAtOrBefore(timestamp: UnixTime, start?: number): Promise<number>
   getBlock(number: number): Promise<{
     number: number
     timestamp: number
