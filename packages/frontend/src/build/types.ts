0c8da4d1880dec86bc0d3994308f17701a59782c
Antoni Pawlak <antoni.pawlak@pm.me>
2022-10-26 11:44:52 +0200

    remove unused code

--- packages/frontend/src/build/types.ts
+++
@@ -1,7 +0,0 @@
-import { ActivityApiResponse, TvlApiResponse } from '@l2beat/types'
-
-export interface PagesData {
-  tvlApiResponse: TvlApiResponse
-  activityApiResponse: ActivityApiResponse
-  verificationStatus: Record<string, boolean>
-}
