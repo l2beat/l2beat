327faa358103067b57c0987cc628e5c77f9659d1
sin6pi7 <jac.zyla@gmail.com>
2022-10-27 18:05:07 +0200

    Update backend production config

--- packages/backend/src/config/config.production.ts
+++ packages/backend/src/config/config.production.ts
@@ -37,6 +37,32 @@ export function getProductionConfig(): Config {
     freshStart: false,
     tvlReportSync: true,
     eventsSync: false,
-    transactionCountSync: false,
+    transactionCountSync: {
+      starkexApiKey: getEnv('STARKEX_API_KEY'),
+      starkexApiDelayHours: 5,
+      zkSyncWorkQueueWorkers: 100,
+      starkexWorkQueueWorkers: 10,
+      starkexCallsPerMinute: 200,
+      loopringWorkQueueWorkers: 10,
+      loopringCallsPerMinute: 200,
+      rpc: {
+        workQueueLimit: 200_000,
+        workQueueWorkers: getEnv.integer('ACTIVITY_RPC_WORKERS'),
+        projects: {
+          ethereum: {
+            callsPerMinute: getEnv.integer('ACTIVITY_ETHEREUM_CALLS'),
+            url: getEnv('ACTIVITY_ETHEREUM_URL'),
+          },
+          optimism: {
+            callsPerMinute: getEnv.integer('ACTIVITY_OPTIMISM_CALLS'),
+            url: getEnv('ACTIVITY_OPTIMISM_URL'),
+          },
+          arbitrum: {
+            callsPerMinute: getEnv.integer('ACTIVITY_ARBITRUM_CALLS'),
+            url: getEnv('ACTIVITY_ARBITRUM_URL'),
+          },
+        },
+      },
+    },
   }
 }
