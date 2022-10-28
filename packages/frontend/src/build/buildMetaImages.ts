c708a2753ffb2a706b0b3db0da0e3e506e8a76b6
Michal <michalsidzej@gmail.com>
2022-10-28 12:12:46 +0200

    Check if meta image is not 404 page

--- packages/frontend/src/build/buildMetaImages.ts
+++ packages/frontend/src/build/buildMetaImages.ts
@@ -4,12 +4,18 @@ import { mkdirpSync } from 'fs-extra'
 import { Server } from 'http'
 import puppeteer from 'puppeteer'
 
+import { getConfig } from './config'
+
 main().catch((e) => {
   console.error(e)
   process.exit(1)
 })
 
 async function main() {
+  const env = process.env.DEPLOYMENT_ENV ?? 'production'
+  console.log(`Using config for ${env}`)
+  const config = getConfig(env)
+
   // ensure that meta-images directory exists
   mkdirpSync('build/meta-images')
 
@@ -21,7 +27,11 @@ async function main() {
 
   const slugs = [...layer2s, ...bridges]
     .map((x) => x.display.slug)
-    .concat('overview-scaling', 'overview-bridges', 'overview-scaling-activity')
+    .concat('overview-scaling', 'overview-bridges')
+
+  if (config.features.activity) {
+    slugs.concat('overview-scaling-activity')
+  }
 
   const browser = await puppeteer.launch({
     args: ['--no-sandbox'],
@@ -35,15 +45,34 @@ async function main() {
       height: 314,
       deviceScaleFactor: 2,
     })
-    const path = `build/meta-images/${slug}.png`
-    const url = `http://localhost:1234/meta-images/${slug}`
+    const imagePath = `build/meta-images/${slug}.png`
+    const urlPath = `meta-images/${slug}`
+    const url = `http://localhost:1234/${urlPath}`
     await page.goto(url, { waitUntil: 'networkidle0' })
-    await page.screenshot({ path })
-    console.log(`Captured ${path}`)
+
+    if (await is404(page, urlPath)) {
+      const string = `Meta image for ${slug} did not build properly!`
+      throw new Error(string)
+    }
+
+    await page.screenshot({ path: imagePath })
+    console.log(`Captured ${imagePath}`)
   }
 
   await browser.close()
   await new Promise<void>((resolve, reject) =>
     server.close((err) => (err ? reject(err) : resolve())),
   )
+}
+
+async function is404(page: puppeteer.Page, urlPath: string) {
+  const body = await page.$('body')
+  const innerHTML = await body?.getProperty('innerHTML')
+  const value = await innerHTML?.jsonValue()
+
+  if (typeof value !== 'string') {
+    throw new Error('Value should be string!')
+  }
+
+  return value.includes(`Cannot GET /${urlPath}`)
 }
