2a2413299b6d1234a39ed71f637224b3ea562b33
Bartek Kiepuszewski <bkiepuszewski@gmail.com>
2022-10-29 20:18:27 +0200

    add Aptos bridge

---
+++ packages/update-monitor/src/projects/aptosBridge/index.ts
@@ -0,0 +1,30 @@
+import { providers } from 'ethers'
+
+import { DiscoveryEngine } from '../../discovery/DiscoveryEngine'
+import { ProjectParameters } from '../../types'
+import { addresses } from './constants'
+import { getHopBridgeParams } from './contracts/aptosBridge'
+
+export const APTOS_NAME = 'aptos'
+
+/*
+export async function getHopParameters(
+  provider: providers.JsonRpcProvider,
+): Promise<ProjectParameters> {
+  const parameters: ProjectParameters = {
+    name: APTOS_NAME,
+    contracts: await Promise.all([
+      getHopBridgeParams(provider, addresses.usdcBridge, 'USDC Bridge'),
+      getHopBridgeParams(provider, addresses.usdtBridge, 'USDT Bridge'),
+      getHopBridgeParams(provider, addresses.daiBridge, 'DAI Bridge'),
+      getHopBridgeParams(provider, addresses.ethBridge, 'ETH Bridge'),
+      getHopBridgeParams(provider, addresses.maticBridge, 'MATIC Bridge'),
+      getHopBridgeParams(provider, addresses.wbtcBridge, 'WBTC Bridge'),
+    ]),
+  }
+  return parameters
+}*/
+
+export async function discoverAptos(discoveryEngine: DiscoveryEngine) {
+  await discoveryEngine.discover(APTOS_NAME, [addresses.aptosBridge])
+}
