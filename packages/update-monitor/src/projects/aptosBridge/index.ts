c97ec2ce141a4d92d223d10cb9f90a1fe502b8a9
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-10-30 10:44:53 +0100

    Remove commented out code

--- packages/update-monitor/src/projects/aptosBridge/index.ts
+++ packages/update-monitor/src/projects/aptosBridge/index.ts
@@ -3,24 +3,6 @@ import { addresses } from './constants'
 
 export const APTOS_NAME = 'aptos'
 
-/*
-export async function getHopParameters(
-  provider: providers.JsonRpcProvider,
-): Promise<ProjectParameters> {
-  const parameters: ProjectParameters = {
-    name: APTOS_NAME,
-    contracts: await Promise.all([
-      getHopBridgeParams(provider, addresses.usdcBridge, 'USDC Bridge'),
-      getHopBridgeParams(provider, addresses.usdtBridge, 'USDT Bridge'),
-      getHopBridgeParams(provider, addresses.daiBridge, 'DAI Bridge'),
-      getHopBridgeParams(provider, addresses.ethBridge, 'ETH Bridge'),
-      getHopBridgeParams(provider, addresses.maticBridge, 'MATIC Bridge'),
-      getHopBridgeParams(provider, addresses.wbtcBridge, 'WBTC Bridge'),
-    ]),
-  }
-  return parameters
-}*/
-
 export async function discoverAptos(discoveryEngine: DiscoveryEngine) {
   await discoveryEngine.discover(APTOS_NAME, [addresses.aptosBridge])
 }
