fd499907a68752fdfcb0a6134e6466d7721b2ead
Antoni Pawlak <antoni.pawlak@pm.me>
2022-11-04 15:24:02 +0100

    inline styles inside cx

--- packages/frontend/src/components/project/WarningBar.tsx
+++ packages/frontend/src/components/project/WarningBar.tsx
@@ -10,22 +10,22 @@ export interface WarningBarProps {
 }
 
 export function WarningBar({ color, text, isCritical }: WarningBarProps) {
-  const background = color === 'red' ? 'bg-red-600' : 'bg-yellow-300'
-  const iconFill =
-    color === 'red'
-      ? 'fill-red-700 dark:fill-red-300'
-      : 'fill-yellow-700 dark:fill-yellow-300'
-
   return (
     <div
       className={cx(
         'flex gap-3 mt-4 first:mt-0 md:mt-6 p-4',
         'bg-opacity-20 rounded-lg',
-        background,
+        color === 'red' ? 'bg-red-600' : 'bg-yellow-300',
       )}
     >
       <span>
-        <ShieldIcon className={cx(iconFill)} />
+        <ShieldIcon
+          className={cx(
+            color === 'red'
+              ? 'fill-red-700 dark:fill-red-300'
+              : 'fill-yellow-700 dark:fill-yellow-300',
+          )}
+        />
       </span>
       <p>
         {isCritical ? (
