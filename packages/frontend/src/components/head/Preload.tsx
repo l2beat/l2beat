26b439be32f8172829eb9fdcebd312cb104aa092
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-11-10 17:26:18 +0100

    Fix preload font urls

--- packages/frontend/src/components/head/Preload.tsx
+++ packages/frontend/src/components/head/Preload.tsx
@@ -12,14 +12,21 @@ export function Preload(props: Props) {
       )}
       <link
         rel="preload"
-        href="/fonts/Roboto/roboto-v27-latin-regular.woff2"
+        href="/fonts/Roboto/roboto-v30-latin-regular.woff2"
         as="font"
         type="font/woff2"
         crossOrigin="anonymous"
       />
       <link
         rel="preload"
-        href="/fonts/Roboto/roboto-v27-latin-700.woff2"
+        href="/fonts/Roboto/roboto-v30-latin-500.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin="anonymous"
+      />
+      <link
+        rel="preload"
+        href="/fonts/Roboto/roboto-v30-latin-700.woff2"
         as="font"
         type="font/woff2"
         crossOrigin="anonymous"
