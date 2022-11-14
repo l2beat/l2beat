5c261e98ea598920af51ee8f3e5c168bf7024039
Michal <michalsidzej@gmail.com>
2022-11-14 18:50:19 +0100

    Fix badge size

--- packages/frontend/src/components/icons/NewItemIcon.tsx
+++ packages/frontend/src/components/icons/NewItemIcon.tsx
@@ -5,7 +5,8 @@ export function NewItemIcon({ className }: { className?: string }) {
   return (
     <span
       className={cx(
-        'bg-yellow-200 text-purple-700 px-1 rounded-sm text-2xs md:text-lg font-bold',
+        'bg-yellow-200 text-purple-700 px-1 rounded-sm',
+        'text-2xs md:text-sm leading-tight font-bold',
         className,
       )}
     >
