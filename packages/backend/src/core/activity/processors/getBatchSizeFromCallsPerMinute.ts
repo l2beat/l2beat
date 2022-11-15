08ce8d994f148e17918e89125032b7f9817acb61
sin6pi7 <jac.zyla@gmail.com>
2022-11-15 12:35:07 +0100

    Batch size calculations explicit

--- packages/backend/src/core/activity/processors/getBatchSizeFromCallsPerMinute.ts
+++ packages/backend/src/core/activity/processors/getBatchSizeFromCallsPerMinute.ts
@@ -1,3 +1,12 @@
-export function getBatchSizeFromCallsPerMinute(callsPerMinute: number): number {
-  return Math.floor(callsPerMinute / 60)
+import { assert } from '@l2beat/common'
+
+export function getBatchSizeFromCallsPerMinute(
+  callsPerMinute: number,
+  batchDurationSeconds = 60,
+): number {
+  assert(
+    callsPerMinute >= batchDurationSeconds,
+    `callsPerMinute=${callsPerMinute} must be greater or equal to batchDurationSeconds=${batchDurationSeconds}`,
+  )
+  return Math.floor(callsPerMinute / batchDurationSeconds)
 }
