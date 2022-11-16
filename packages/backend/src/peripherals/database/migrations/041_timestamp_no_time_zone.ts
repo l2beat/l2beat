3b3f65529477d0d273ed1aa6db1ab470dcf61dcb
Michal <michalsidzej@gmail.com>
2022-11-16 12:42:28 +0100

    Fix formatting

--- packages/backend/src/peripherals/database/migrations/041_timestamp_no_time_zone.ts
+++ packages/backend/src/peripherals/database/migrations/041_timestamp_no_time_zone.ts
@@ -1,10 +1,14 @@
 /*
                       ====== IMPORTANT NOTICE ======
+
 DO NOT EDIT OR RENAME THIS FILE
+
 This is a migration file. Once created the file should not be renamed or edited,
 because migrations are only run once on the production server. 
+
 If you find that something was incorrectly set up in the `up` function you
 should create a new migration file that fixes the issue.
+
 */
 
 import { Knex } from 'knex'
