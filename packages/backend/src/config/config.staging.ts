aabb631078d74479258fbd52a5545ed51642beed
Jacek <jac.zyla@gmail.com>
2022-11-17 17:11:54 +0100

    Update configs (#971)

--- packages/backend/src/config/config.staging.ts
+++ packages/backend/src/config/config.staging.ts
@@ -13,7 +13,6 @@ export function getStagingConfig(cli: CliParameters): Config {
   return {
     ...productionConfig,
     name: 'Backend/Staging',
-    syncEnabled: true,
     activityV2: {
       starkexApiKey: getEnv('STARKEX_API_KEY'),
       starkexApiDelayHours: getEnv.integer('STARKEX_API_DELAY_HOURS', 12),
