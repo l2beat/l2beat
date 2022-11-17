50a6ed7c445ba1090f6ad95093eff9dfa64a0d3f
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-11-17 11:44:30 +0100

    Add internal handlers (#964)

--- packages/backend/src/core/discovery/getParameters.ts
+++ packages/backend/src/core/discovery/getParameters.ts
@@ -1,25 +1,20 @@
-import { Bytes, EthereumAddress } from '@l2beat/types'
-import { BigNumber, utils } from 'ethers'
+import { EthereumAddress } from '@l2beat/types'
+import { utils } from 'ethers'
 
 import { DiscoveryOptions } from './DiscoveryOptions'
+import { Handler, HandlerResult } from './handlers/Handler'
+import { LimitedArrayHandler } from './handlers/LimitedArrayHandler'
+import { SimpleMethodHandler } from './handlers/SimpleMethodHandler'
 import { DiscoveryProvider } from './provider/DiscoveryProvider'
-import { ContractValue } from './types'
-import { readArray } from './utils/array'
-import { isRevert } from './utils/isRevert'
-
-export interface Parameter {
-  name: string
-  value: ContractValue
-}
 
 export async function getParameters(
   abiEntries: string[],
   address: EthereumAddress,
   provider: DiscoveryProvider,
   options: DiscoveryOptions,
-): Promise<Parameter[]> {
+): Promise<HandlerResult[]> {
   const abi = new utils.Interface(abiEntries)
-  const functions = Object.entries(abi.functions)
+  const viewFunctions = Object.entries(abi.functions)
     .map((x) => x[1])
     .filter((x) => x.stateMutability === 'view' || x.constant)
     .filter(
@@ -28,83 +23,17 @@ export async function getParameters(
           (y) => y === x.name,
         ),
     )
-  const simpleFunctions = functions.filter((x) => x.inputs.length === 0)
-  const arrayFunctions = functions.filter(
-    (x) => x.inputs.length === 1 && x.inputs[0].type === 'uint256',
+
+  const handlers: Handler[] = [
+    ...viewFunctions
+      .filter((x) => x.inputs.length === 0)
+      .map((x) => new SimpleMethodHandler(x)),
+    ...viewFunctions
+      .filter((x) => x.inputs.length === 1 && x.inputs[0].type === 'uint256')
+      .map((x) => new LimitedArrayHandler(x)),
+  ]
+
+  return Promise.all(
+    handlers.map((h) => h.execute(provider, address, abiEntries)),
   )
-
-  const values = await Promise.all([
-    ...simpleFunctions.map((x) =>
-      getRegularParameter(address, abi, x, provider),
-    ),
-    ...arrayFunctions.map((x) => getArrayParameter(address, abi, x, provider)),
-  ])
-  return values.filter((x): x is Parameter => x !== undefined)
-}
-
-async function getRegularParameter(
-  address: EthereumAddress,
-  abi: utils.Interface,
-  method: utils.FunctionFragment,
-  provider: DiscoveryProvider,
-): Promise<Parameter | undefined> {
-  try {
-    const callData = Bytes.fromHex(abi.encodeFunctionData(method, []))
-    const result = await provider.call(address, callData)
-    console.log(`Called ${address.toString()}.${method.name}()`)
-    const decoded = abi.decodeFunctionResult(method, result.toString())
-    const mapped = decoded.map(toContractValue)
-    return {
-      name: method.name,
-      value: mapped.length === 1 ? mapped[0] : mapped,
-    }
-  } catch (e) {
-    if (isRevert(e)) {
-      return undefined
-    }
-    console.error(`Failed to call ${address.toString()}.${method.name}()`)
-    throw e
-  }
-}
-
-async function getArrayParameter(
-  address: EthereumAddress,
-  abi: utils.Interface,
-  method: utils.FunctionFragment,
-  provider: DiscoveryProvider,
-): Promise<Parameter> {
-  const results = await readArray(async (i) => {
-    const callData = Bytes.fromHex(abi.encodeFunctionData(method, [i]))
-    const result = await provider.call(address, callData)
-    console.log(`Called ${address.toString()}.${method.name}(${i})`)
-    const decoded = abi.decodeFunctionResult(method, result.toString())
-    const mapped = decoded.map(toContractValue)
-    return mapped.length === 1 ? mapped[0] : mapped
-  })
-  return {
-    name: method.name,
-    value: toContractValue(results),
-  }
-}
-
-export function toContractValue(value: unknown): ContractValue {
-  if (Array.isArray(value)) {
-    return value.map(toContractValue)
-  }
-  if (
-    typeof value === 'string' ||
-    typeof value === 'number' ||
-    typeof value === 'boolean'
-  ) {
-    return value
-  }
-  if (BigNumber.isBigNumber(value)) {
-    if (value.gt(Number.MAX_SAFE_INTEGER.toString())) {
-      return value.toString()
-    } else {
-      return value.toNumber()
-    }
-  }
-  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-  return `${value}`
 }
---
+++ packages/backend/test/core/discovery/handlers/LimitedArrayHandler.test.ts
@@ -0,0 +1,106 @@
+import { mock } from '@l2beat/common'
+import { Bytes, EthereumAddress } from '@l2beat/types'
+import { expect } from 'earljs'
+
+import { LimitedArrayHandler } from '../../../../src/core/discovery/handlers/LimitedArrayHandler'
+import { DiscoveryProvider } from '../../../../src/core/discovery/provider/DiscoveryProvider'
+
+describe(LimitedArrayHandler.name, () => {
+  const method = 'function owners(uint256 index) view returns (address)'
+  const signature = '0x025e7c27'
+
+  it('calls the passed method n times', async () => {
+    const address = EthereumAddress.random()
+    const owners = [
+      EthereumAddress.random(),
+      EthereumAddress.random(),
+      EthereumAddress.random(),
+    ]
+
+    const provider = mock<DiscoveryProvider>({
+      async call(passedAddress, data) {
+        expect(passedAddress).toEqual(address)
+
+        const index = data.get(35)
+
+        expect(data).toEqual(
+          Bytes.fromHex(signature + index.toString().padStart(64, '0')),
+        )
+
+        return Bytes.fromHex('00'.repeat(12)).concat(
+          Bytes.fromHex(owners[index].toString()),
+        )
+      },
+    })
+
+    const handler = new LimitedArrayHandler(method, 3)
+    expect(handler.name).toEqual('owners')
+
+    const result = await handler.execute(provider, address)
+    expect(result as unknown).toEqual({
+      name: 'owners',
+      value: owners,
+      error: 'Too many values. Update configuration explore fully',
+    })
+  })
+
+  it('handles a revert', async () => {
+    const address = EthereumAddress.random()
+    const owners = [
+      EthereumAddress.random(),
+      EthereumAddress.random(),
+      EthereumAddress.random(),
+    ]
+
+    const provider = mock<DiscoveryProvider>({
+      async call(_, data) {
+        const index = data.get(35)
+
+        if (index === 2) {
+          throw new Error('Error during execution: revert')
+        }
+
+        return Bytes.fromHex('00'.repeat(12)).concat(
+          Bytes.fromHex(owners[index].toString()),
+        )
+      },
+    })
+
+    const handler = new LimitedArrayHandler(method, 3)
+    const result = await handler.execute(provider, address)
+    expect(result as unknown).toEqual({
+      name: 'owners',
+      value: owners.slice(0, 2),
+    })
+  })
+
+  it('handles other errors', async () => {
+    const address = EthereumAddress.random()
+    const owners = [
+      EthereumAddress.random(),
+      EthereumAddress.random(),
+      EthereumAddress.random(),
+    ]
+
+    const provider = mock<DiscoveryProvider>({
+      async call(_, data) {
+        const index = data.get(35)
+
+        if (index === 2) {
+          throw new Error('foo bar')
+        }
+
+        return Bytes.fromHex('00'.repeat(12)).concat(
+          Bytes.fromHex(owners[index].toString()),
+        )
+      },
+    })
+
+    const handler = new LimitedArrayHandler(method, 3)
+    const result = await handler.execute(provider, address)
+    expect(result as unknown).toEqual({
+      name: 'owners',
+      error: 'foo bar',
+    })
+  })
+})
