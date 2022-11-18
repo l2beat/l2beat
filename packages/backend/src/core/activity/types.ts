9482e02b46c3226ed74a6ee34337ea27300d1236
Michal <michalsidzej@gmail.com>
2022-11-18 14:17:22 +0100

    Log lagging projects in ActivityV2

--- packages/backend/src/core/activity/types.ts
+++ packages/backend/src/core/activity/types.ts
@@ -1,6 +1,8 @@
-import { UnixTime } from '@l2beat/types'
+import { ProjectId, UnixTime } from '@l2beat/types'
 
 export interface DailyTransactionCount {
   count: number
   timestamp: UnixTime
 }
+
+export type DailyTransactionCountMap = Map<ProjectId, DailyTransactionCount[]>
