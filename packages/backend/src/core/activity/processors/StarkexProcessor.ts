567d63a7bd240be7b850d08af2baef5c6c851854
Michał Sobieraj-Jakubiec <38423054+michalsidzej@users.noreply.github.com>
2022-11-23 11:13:34 +0100

    Enable starkex to repeat last 7 days (#982)
    Co-authored-by: Jacek <jac.zyla@gmail.com>

--- packages/backend/src/core/activity/processors/StarkexProcessor.ts
+++ packages/backend/src/core/activity/processors/StarkexProcessor.ts
@@ -1,13 +1,14 @@
-import { Logger, promiseAllPlus } from '@l2beat/common'
+import { assert, Logger, promiseAllPlus } from '@l2beat/common'
 import { StarkexTransactionApiV2 } from '@l2beat/config'
 import { ProjectId, UnixTime } from '@l2beat/types'
+import { Knex } from 'knex'
 import { range } from 'lodash'
 
 import { StarkexTransactionCountRepository } from '../../../peripherals/database/activity-v2/StarkexCountRepository'
 import { SequenceProcessorRepository } from '../../../peripherals/database/SequenceProcessorRepository'
 import { StarkexClient } from '../../../peripherals/starkex'
 import { Clock } from '../../Clock'
-import { SequenceProcessor } from '../../SequenceProcessor'
+import { ALL_PROCESSED_EVENT, SequenceProcessor } from '../../SequenceProcessor'
 import { getBatchSizeFromCallsPerMinute } from './getBatchSizeFromCallsPerMinute'
 
 export interface StarkexProcessorOptions extends StarkexTransactionApiV2 {
@@ -25,33 +26,54 @@ export function createStarkexProcessor(
   options: StarkexProcessorOptions,
 ): SequenceProcessor {
   const batchSize = getBatchSizeFromCallsPerMinute(options.singleStarkexCPM)
+  const startDay = options.sinceTimestamp.toStartOf('day').toDays()
+  const processRange = async (
+    from: number,
+    to: number,
+    trx?: Knex.Transaction,
+  ) => {
+    const queries = range(from, to + 1).map((day) => async () => {
+      const count = await starkexClient.getDailyCount(day, options.product)
 
-  return new SequenceProcessor(
+      return {
+        count,
+        timestamp: UnixTime.fromDays(day),
+        projectId: projectId,
+      }
+    })
+
+    const counts = await promiseAllPlus(queries, logger)
+    await starkexRepository.addOrUpdateMany(counts, trx)
+  }
+
+  const processor = new SequenceProcessor(
     projectId.toString(),
     logger,
     sequenceProcessorRepository,
     {
       batchSize,
-      startFrom: options.sinceTimestamp.toStartOf('day').toDays(),
+      startFrom: startDay,
       // eslint-disable-next-line @typescript-eslint/require-await
       getLatest: async () =>
         getStarkexLastDay(clock.getLastHour(), options.starkexApiDelayHours),
-      processRange: async (from, to, trx) => {
-        const queries = range(from, to + 1).map((day) => async () => {
-          const count = await starkexClient.getDailyCount(day, options.product)
-
-          return {
-            count,
-            timestamp: UnixTime.fromDays(day),
-            projectId: projectId,
-          }
-        })
-
-        const counts = await promiseAllPlus(queries, logger)
-        await starkexRepository.addOrUpdateMany(counts, trx)
-      },
+      processRange,
     },
   )
+
+  const handleAllProcessedEvent = async () => {
+    const state = await sequenceProcessorRepository.getById(processor.id)
+    assert(state)
+    await processRange(
+      state.lastProcessed - options.resyncLastDays,
+      state.lastProcessed,
+    )
+  }
+
+  processor.on(ALL_PROCESSED_EVENT, () => {
+    handleAllProcessedEvent().catch(logger.error.bind(logger))
+  })
+
+  return processor
 }
 function getStarkexLastDay(timestamp: UnixTime, hoursDelay: number) {
   return timestamp
