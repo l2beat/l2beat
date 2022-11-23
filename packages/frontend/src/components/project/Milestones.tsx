aa9a1d5831ade3ee1cf659f0e15642849e380538
Antoni Pawlak <antoni.pawlak@pm.me>
2022-11-23 14:32:02 +0100

    pixel perfect aligned icon

--- packages/frontend/src/components/project/Milestones.tsx
+++ packages/frontend/src/components/project/Milestones.tsx
@@ -2,6 +2,8 @@ import { Milestone } from '@l2beat/config'
 import cx from 'classnames'
 import React from 'react'
 
+import { MilestoneIcon } from '../icons/symbols/MilestoneIcon'
+
 export interface MilestonesProps {
   milestones: Milestone[]
   className?: string
@@ -13,9 +15,15 @@ export function Milestones({ milestones, className }: MilestonesProps) {
       <div className="mb-6">
         <span className="text-2xl font-bold">Project Milestones</span>
       </div>
-      <div className="absolute left-4 h-[100%]">
-        <div className="h-[70%] w-[1px] bg-green-500 " />
+      <div className="absolute left-[15.4px] h-[100%]">
+        <div className="h-[70%] w-[1px] bg-green-500 mt-1" />
         <div className="h-[30%] w-[1px] bg-gradient-to-b from-green-500 to-black" />
+      </div>
+      <div className="relative">
+        <MilestoneIcon className="fill-green-800 stroke-green-500 absolute left-[6px]" />
+        {/* <div>Title</div> */}
+        {/* <div>Date</div> */}
+        {/* <div>Description</div> */}
       </div>
     </div>
   )
