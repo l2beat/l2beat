515cd43073bc56f105640e89334a140279aa3677
Antoni Pawlak <antoni.pawlak@pm.me>
2022-11-28 16:42:46 +0100

    fix formatting

--- packages/config/src/layer2s/zksync.ts
+++ packages/config/src/layer2s/zksync.ts
@@ -347,13 +347,15 @@ export const zksync: Layer2 = {
       name: 'zkSync 1.0 launch',
       link: 'https://blog.matter-labs.io/zksync-is-live-bringing-trustless-scalable-payments-to-ethereum-9c634b3e6823',
       date: new Date('2020-06-18'),
-      description: 'zkSync is live, bringing trustless, scalable payments to Ethereum using zkRollup technology.'
+      description:
+        'zkSync is live, bringing trustless, scalable payments to Ethereum using zkRollup technology.',
     },
     {
       name: 'zkEVM alpha',
       link: 'https://blog.matter-labs.io/baby-alpha-has-arrived-5b10798bc623',
       date: new Date('2022-10-28'),
-      description: 'Deployment of the system to mainnet, for the testing purposes.'
+      description:
+        'Deployment of the system to mainnet, for the testing purposes.',
     },
   ],
 }
