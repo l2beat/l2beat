ae248294f2a6b9b97d2d661409bb022329016dd1
Jacek <jac.zyla@gmail.com>
2022-11-28 16:40:36 +0100

    Use single activity endpoint (#1024)

--- packages/backend/src/api/routers/ActivityRouter.ts
+++ packages/backend/src/api/routers/ActivityRouter.ts
@@ -5,7 +5,7 @@ import { ActivityController } from '../controllers/activity/ActivityController'
 export function createActivityRouter(activityController: ActivityController) {
   const router = new Router()
 
-  router.get(['/api/activity', '/api/activity/v2'], async (ctx) => {
+  router.get('/api/activity', async (ctx) => {
     const data = await activityController.getActivity()
     ctx.body = data
   })
