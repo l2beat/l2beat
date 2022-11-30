8a405b5c131772fc5eaabc7c8e54d1b24d1c3f14
Antoni Pawlak <antoni.pawlak@pm.me>
2022-11-30 13:32:03 +0100

    fix sorting

--- packages/frontend/src/components/project/Milestones.tsx
+++ packages/frontend/src/components/project/Milestones.tsx
@@ -35,7 +35,7 @@ export function Milestones({ milestones, className }: MilestonesProps) {
         </div>
         <div className="ml-10 mt-6">
           {milestones
-            .sort((a, b) => Number(b.date) - Number(a.date))
+            .sort((a, b) => Number(new Date(b.date)) - Number(new Date(a.date)))
             .map((milestone, i) => (
               <div key={i} className="pb-7">
                 <MilestoneIcon className="absolute left-1.5 fill-green-200 stroke-green-400 dark:fill-green-800 dark:stroke-green-500" />
