97df58505076cb8646b5b07853793b267adba251
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-12-05 16:47:11 +0100

    Fix linter

--- packages/update-monitor/src/projects/metis/contracts/getBondManager.ts
+++ packages/update-monitor/src/projects/metis/contracts/getBondManager.ts
@@ -14,7 +14,6 @@ export async function getBondManager(
 
   const addressManager: string = await bondManager.libAddressManager()
 
-
   return {
     name: 'BondManager',
     address: bondManager.address,
