26a4bf9f0c61b92d4c245a71a06074a33721dbf7
Antoni Pawlak <antoni.pawlak@pm.me>
2022-12-06 13:06:22 +0100

    html as template string

--- packages/frontend/src/components/chart/configure/render/renderMilestones.ts
+++ packages/frontend/src/components/chart/configure/render/renderMilestones.ts
@@ -11,30 +11,37 @@ export function renderMilestones(state: State, milestones: HTMLElement) {
     if (!point.milestone) {
       continue
     }
-    const x = point.x * width - iconHeight / 2
-    innerHtml += `<div class="absolute top-[-10px] z-50" style="left: ${x}px">${milestoneIcon}</div>`
+    const x = point.x * width
+    innerHtml += getMilestoneHtml(x)
   }
   milestones.innerHTML = innerHtml
 }
 
+function getMilestoneHtml(x: number) {
+  return `
+  <div class="absolute z-50" 
+        style="left: ${x + offset}px; top: ${offset}px">
+    <svg
+      width="${iconHeight}"
+      height="${iconHeight}"
+      view-box="0 0 ${iconHeight} ${iconHeight}"
+      role="img"
+      aria-label="Milestone icon"
+    >      
+      <rect
+        fill="#34762F"
+        stroke="#5BFF4D"
+        x="9.89941"
+        y="1.41421"
+        width="12"
+        height="12"
+        rx="1"
+        transform="rotate(45 9.89941 1.41421)"
+        stroke-width="2"
+      /> 
+    <svg>
+  </div>`
+}
+
 const iconHeight = 20
-const milestoneIcon = `
-<svg
-width="${iconHeight}"
-height="${iconHeight}"
-view-box="0 0 ${iconHeight} ${iconHeight}"
-role="img"
-aria-label="Milestone icon"
->      
-  <rect
-  fill="#34762F"
-  stroke="#5BFF4D"
-  x="9.89941"
-  y="1.41421"
-  width="12"
-  height="12"
-  rx="1"
-  transform="rotate(45 9.89941 1.41421)"
-  stroke-width="2"
-  /> 
-<svg>`
+const offset = -(iconHeight / 2)
