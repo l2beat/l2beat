87155f0e2df083ae8c491130dcdb3816cdbf29e1
Antoni Pawlak <antoni.pawlak@pm.me>
2022-12-08 16:07:07 +0100

    add more

--- packages/frontend/src/build/config/common.ts
+++ packages/frontend/src/build/config/common.ts
@@ -24,9 +24,29 @@ export const common: Omit<Config, 'backend'> = {
   milestones: [
     {
       name: 'Rollup centric roadmap for Ethereum',
-      description: '',
       date: '2020-10-02T00:00:00Z',
       link: 'https://ethereum-magicians.org/t/a-rollup-centric-ethereum-roadmap/4698',
     },
+    {
+      name: 'Optimism launch',
+      link: 'https://medium.com/ethereum-optimism/all-gas-no-brakes-8b0f32afd466',
+      date: '2021-12-16T00:00:00Z',
+    },
+    {
+      name: 'Arbitrum launch',
+      link: 'https://twitter.com/arbitrum/status/1432817424752128008',
+      date: '2021-08-31T00:00:00Z',
+    },
+    {
+      name: 'zkEMV day on ETHcc',
+      link: 'https://twitter.com/sandeepnailwal/status/1549868882638389248',
+      date: '2022-07-20T00:00:00Z',
+    },
+    {
+      name: 'Aave on Optimism',
+      link: 'https://governance.aave.com/t/launch-aave-v3-on-optimism/6871',
+      date: '2022-01-11T00:00:00Z'
+    }
+    
   ],
 }
