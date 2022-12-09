e77af9b3913d7cd5ae0fe798f52aa6e5e23a3c33
Antoni Pawlak <antoni.pawlak@pm.me>
2022-12-09 13:12:00 +0100

    hover closest

--- packages/frontend/src/components/chart/configure/update/view/getMilestoneHover.ts
+++ packages/frontend/src/components/chart/configure/update/view/getMilestoneHover.ts
@@ -1,6 +1,7 @@
 import { Milestone } from '@l2beat/config'
 
 const ICON_HEIGHT = 25
+const MAX_TRIGGER_PROXIMITY = 0.014
 
 export function getMilestoneHover(
   mouseX: number,
@@ -10,13 +11,24 @@ export function getMilestoneHover(
   if (points === undefined) {
     return undefined
   }
-  for (const [index, point] of points.entries()) {
-    if (point.milestone) {
-      const milestoneProximity = Math.abs(point.x - mouseX)
-      if (mouseY <= ICON_HEIGHT && milestoneProximity < 0.015) {
-        return index
+  if (mouseY <= ICON_HEIGHT) {
+    let minProximity = Infinity
+    let indexMinProximity = 0
+
+    for (const [index, point] of points.entries()) {
+      if (point.milestone) {
+        const milestoneProximity = Math.abs(point.x - mouseX)
+        if (milestoneProximity < minProximity) {
+          minProximity = milestoneProximity
+          indexMinProximity = index
+        }
       }
     }
+
+    if (minProximity < MAX_TRIGGER_PROXIMITY) {
+      return indexMinProximity
+    }
   }
+
   return undefined
 }
