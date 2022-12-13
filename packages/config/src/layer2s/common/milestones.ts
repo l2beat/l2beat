9e6b7fd3fc5a3046ddc9204c8f51d47e87aac537
Antoni Pawlak <antoni.pawlak@pm.me>
2022-12-13 11:07:44 +0100

    update names

--- packages/config/src/layer2s/common/milestones.ts
+++ packages/config/src/layer2s/common/milestones.ts
@@ -12,7 +12,7 @@ export const MILESTONES = {
 
 export const HOMEPAGE_MILESTONES: Milestone[] = [
   {
-    name: 'ZK rollup (payments)',
+    name: 'First zkRollup (for payments)',
     date: '2020-06-18T00:00:00Z',
     link: 'https://blog.matter-labs.io/zksync-is-live-bringing-trustless-scalable-payments-to-ethereum-9c634b3e6823',
     description:
@@ -26,42 +26,42 @@ export const HOMEPAGE_MILESTONES: Milestone[] = [
       'Rollups are considered a scaling solution for the near and mid-term future of Ethereum.',
   },
   {
-    name: 'Optimistic Rollup (payments)',
+    name: 'First trustless Optimistic Rollup (for payments)',
     date: '2020-12-31T00:00:00Z',
     link: 'https://twitter.com/fuellabs_/status/1344707195250896899',
     description:
       'Fuel v1 is live, bringing payments scalability using Optimistic Rollup technology and UTXO model.',
   },
   {
-    name: 'Optimistic Rollup (universal)',
+    name: 'First generalized Optimistic Rollup with fraud proofs',
     link: 'https://medium.com/ethereum-optimism/mainnet-soft-launch-7cacc0143cd5',
     date: '2021-01-16T00:00:00Z',
     description:
       'Optimism soft launch is live, bringing first universal rollup based on optimistic architecture.',
   },
   {
-    name: 'ZK Rollup (anonymous payments)',
+    name: 'First privacy-focused Zk Rollup (for payments)',
     link: 'https://medium.com/aztec-protocol/launching-aztec-2-0-rollup-ac7db8012f4b',
     date: '2021-03-15T00:00:00Z',
     description:
       'Aztec is live, bringing private and scalable transactions using ZK Rollup technology.',
   },
   {
-    name: 'Public Optimistic Rollup (universal)',
+    name: 'First permissionless, generalized Optimistic Rollup',
     link: 'https://offchain.medium.com/mainnet-for-everyone-27ce0f67c85e',
     date: '2021-08-31T00:00:00Z',
     description:
       'Arbitrum removed whitelist, becoming first publicly open universal rollup based on optimistic architecture.',
   },
   {
-    name: 'ZK Rollup (universal)',
+    name: 'First generalized zkRollup',
     link: 'https://medium.com/starkware/starknet-alpha-now-on-mainnet-4cf35efd1669',
     date: '2021-11-29T00:00:00Z',
     description:
       'StarkNet Alpha is live, bringing first universal rollup based on ZK technology.',
   },
   {
-    name: 'Optimistic Chain (universal)',
+    name: 'First Optimistic Chain',
     link: 'https://metisdao.medium.com/decentralized-storage-goes-live-da876dc6eb70',
     date: '2022-04-12T00:00:00Z',
     description:
