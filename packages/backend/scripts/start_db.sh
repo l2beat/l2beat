5acb25b3e87b88b594cb074e4c8a116b6e1b8bec
Krzysztof Kaczor <krzkaczor@gmail.com>
2022-12-14 11:16:15 +0100

    Silence output

--- packages/backend/scripts/start_db.sh
+++ packages/backend/scripts/start_db.sh
@@ -7,7 +7,7 @@ name=l2beat_postgres
 if [ ! "$(docker ps -q -f name=${name})" ]; then
     if [ "$(docker ps -aq -f status=exited -f name=${name})" ]; then
         echo "${name} container exists. Restarting..."
-        docker start l2beat_postgres
+        docker start l2beat_postgres >/dev/null
     else
         echo "${name} container doesn't exist. Creating it and setting up databases."
         docker run -d --name=l2beat_postgres -p 5432:5432 -e POSTGRES_PASSWORD=password postgres:14
