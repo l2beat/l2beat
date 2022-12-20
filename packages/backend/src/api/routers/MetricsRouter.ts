a80aad1a4094d9ad34a8a89d8051865b9bc038a1
Michal <michalsidzej@gmail.com>
2022-12-20 18:51:47 +0100

    Local development without auth

--- packages/backend/src/api/routers/MetricsRouter.ts
+++ packages/backend/src/api/routers/MetricsRouter.ts
@@ -2,6 +2,7 @@ import Router from '@koa/router'
 import auth from 'basic-auth'
 
 import { Config } from '../../config'
+import { MetricsAuthConfig } from '../../config/Config'
 import { Metrics } from '../../Metrics'
 
 export function createMetricsRouter(config: Config, metrics: Metrics) {
@@ -10,7 +11,11 @@ export function createMetricsRouter(config: Config, metrics: Metrics) {
   router.get('/metrics', async (ctx) => {
     const credentials = auth(ctx.req)
 
-    if (!credentials || !check(config, credentials.name, credentials.pass)) {
+    if (
+      config.metricsAuth &&
+      (!credentials ||
+        !check(config.metricsAuth, credentials.name, credentials.pass))
+    ) {
       ctx.status = 401
       ctx.set('WWW-Authenticate', 'Basic realm="metrics"')
       return
@@ -22,11 +27,11 @@ export function createMetricsRouter(config: Config, metrics: Metrics) {
   return router
 }
 
-function check(config: Config, name: string, pass: string) {
+function check(metricsAuth: MetricsAuthConfig, name: string, pass: string) {
   let valid = true
 
-  valid = name === config.metrics.user && valid
-  valid = pass === config.metrics.pass && valid
+  valid = name === metricsAuth.user && valid
+  valid = pass === metricsAuth.pass && valid
 
   return valid
 }
