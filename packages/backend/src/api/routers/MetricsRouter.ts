88275a6d0e1f6a794133f627a94cf354838605f6
Michal <michalsidzej@gmail.com>
2022-12-20 15:36:22 +0100

    Add auth to /metrics endpoint

--- packages/backend/src/api/routers/MetricsRouter.ts
+++ packages/backend/src/api/routers/MetricsRouter.ts
@@ -1,13 +1,32 @@
 import Router from '@koa/router'
+import auth from 'basic-auth'
 
+import { Config } from '../../config'
 import { Metrics } from '../../Metrics'
 
-export function createMetricsRouter(metrics: Metrics) {
+export function createMetricsRouter(config: Config, metrics: Metrics) {
   const router = new Router()
 
   router.get('/metrics', async (ctx) => {
+    const credentials = auth(ctx.req)
+
+    if (!credentials || !check(config, credentials.name, credentials.pass)) {
+      ctx.status = 401
+      ctx.set('WWW-Authenticate', 'Basic realm="metrics"')
+      return
+    }
+
     ctx.body = await metrics.getMetrics()
   })
 
   return router
+}
+
+function check(config: Config, name: string, pass: string) {
+  let valid = true
+
+  valid = name === config.metrics.user && valid
+  valid = pass === config.metrics.pass && valid
+
+  return valid
 }
