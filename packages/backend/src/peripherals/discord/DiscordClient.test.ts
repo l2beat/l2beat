16f5693904f4372e01e5bd6feee8f15824abf0b2
Antoni Pawlak <antoni.pawlak@pm.me>
2022-12-20 14:33:42 +0100

    sneding message

--- packages/backend/src/peripherals/discord/DiscordClient.test.ts
+++ packages/backend/src/peripherals/discord/DiscordClient.test.ts
@@ -2,13 +2,18 @@ import { HttpClient } from '@l2beat/common'
 
 import { DiscordClient } from './DiscordClient'
 
+const DISCORD_TOKEN =
+  'MTA1NDM5NzcyMTcwMTc5Nzk2OQ.GpKDU-.gnofHu8RO4bYZWquHCuTzV80SfSIj8PCXW47HQ'
+
+const CHANNEL_ID = '932763329498337374'
+
 describe(DiscordClient.name, () => {
   describe(DiscordClient.prototype.sendMessage.name, () => {
     it('is called', async () => {
       const http = new HttpClient()
-      const discord = new DiscordClient(http)
+      const discord = new DiscordClient(http, DISCORD_TOKEN, CHANNEL_ID)
 
-      await discord.createCommand()
+      await discord.sendMessage("test message")
     })
   })
 })
--- packages/backend/src/peripherals/discord/DiscordClient.ts
+++ packages/backend/src/peripherals/discord/DiscordClient.ts
@@ -1,61 +1,46 @@
 import { HttpClient } from '@l2beat/common'
 import { RequestInit } from 'node-fetch'
 
-const DISCORD_TOKEN =
-  'MTA1NDM5NzcyMTcwMTc5Nzk2OQ.GpKDU-.gnofHu8RO4bYZWquHCuTzV80SfSIj8PCXW47HQ'
-const APP_ID = '1054397721701797969'
-const PUBLIC_KEY =
-  '941c5e89a9b8f4758ab131fbe37fece55fa7c9fbe0dcf9fd46b5043e15ea9a32'
-const GUILD_ID = '932763329498337371'
-
 export class DiscordClient {
-  constructor(private readonly httpClient: HttpClient) {}
+  constructor(
+    private readonly httpClient: HttpClient,
+    private readonly discordToken: string,
+    private readonly channelId: string,
+  ) {}
 
-  sendMessage() {}
-
-  async createCommand() {
-    const endpoint = `applications/${APP_ID}/guilds/${GUILD_ID}/commands`
-    const commandBody = {
-      name: 'test',
-      description: 'description',
-      type: 1,
+  async sendMessage(message: string) {
+    const endpoint = `channels/${this.channelId}/messages`
+    const body = {
+      content: message,
     }
 
     try {
       const res = await this.discordRequest(endpoint, {
         method: 'POST',
-        // @ts-expect-error
-        body: commandBody,
+        //@ts-expect-error
+        body,
       })
-      console.log(await res.json())
     } catch (err) {
-      console.log('Error installing commands', err)
+      console.log('Error sending dsicord message', err)
     }
   }
 
   async discordRequest(endpoint: string, options: RequestInit) {
-    // append endpoint to root API URL
     const url = 'https://discord.com/api/v10/' + endpoint
-    // Stringify payloads
     if (options.body) options.body = JSON.stringify(options.body)
-    // Use node-fetch to make requests
     const res = await this.httpClient.fetch(url, {
       headers: {
-        Authorization: `Bot ${DISCORD_TOKEN}`,
+        Authorization: `Bot ${this.discordToken}`,
         'Content-Type': 'application/json; charset=UTF-8',
-        'User-Agent':
-          'DiscordBot (https://github.com/discord/discord-example-app, 1.0.0)',
       },
       ...options,
     })
-    // throw API errors
     if (!res.ok) {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       const data = await res.json()
       console.log(res.status)
       throw new Error(JSON.stringify(data))
     }
-    // return original response
     return res
   }
 }
