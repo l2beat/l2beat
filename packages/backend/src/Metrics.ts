a1cb55693df859b6b477e8e7646602a87742869b
Michal <michalsidzej@gmail.com>
2022-12-21 16:16:13 +0100

    Add explicit return types

--- packages/backend/src/Metrics.ts
+++ packages/backend/src/Metrics.ts
@@ -12,28 +12,36 @@ import {
 } from 'prom-client'
 
 export class Metrics {
-  init() {
+  init(): void {
     collectDefaultMetrics()
   }
 
-  async getMetrics() {
+  async getMetrics(): Promise<string> {
     // we could also not use the global register, but that doesn't matter so much
     return await register.metrics()
   }
 
-  createCounter(configuration: CounterConfiguration<string>) {
-    return new Counter(configuration)
+  createCounter<T extends string>(
+    configuration: CounterConfiguration<T>,
+  ): Counter<T> {
+    return new Counter<T>(configuration)
   }
 
-  createGauge(configuration: GaugeConfiguration<string>) {
-    return new Gauge(configuration)
+  createGauge<T extends string>(
+    configuration: GaugeConfiguration<T>,
+  ): Gauge<T> {
+    return new Gauge<T>(configuration)
   }
 
-  createHistogram(configuration: HistogramConfiguration<string>) {
-    return new Histogram(configuration)
+  createHistogram<T extends string>(
+    configuration: HistogramConfiguration<T>,
+  ): Histogram<T> {
+    return new Histogram<T>(configuration)
   }
 
-  createSummary(configuration: SummaryConfiguration<string>) {
-    return new Summary(configuration)
+  createSummary<T extends string>(
+    configuration: SummaryConfiguration<T>,
+  ): Summary<T> {
+    return new Summary<T>(configuration)
   }
 }
