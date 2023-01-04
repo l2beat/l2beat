92ed671a4116e4f4882bc16cc9c955c96b617927
Antoni Pawlak <antoni.pawlak@pm.me>
2023-01-04 15:21:29 +0100

    fix tests

--- packages/backend/src/core/PriceUpdater.test.ts
+++ packages/backend/src/core/PriceUpdater.test.ts
@@ -153,10 +153,34 @@ describe(PriceUpdater.name, () => {
       expect(
         coingeckoQueryService.getUsdPriceHistory,
       ).toHaveBeenCalledExactlyWith([
-        [tokens[0].coingeckoId, HOUR_09.add(-7, 'days'), HOUR_09, 'hourly'],
-        [tokens[1].coingeckoId, HOUR_13.add(-7, 'days'), HOUR_13, 'hourly'],
-        [tokens[2].coingeckoId, HOUR_09.add(-7, 'days'), HOUR_13, 'hourly'],
-        [tokens[0].coingeckoId, HOUR_13.add(-7, 'days'), HOUR_13, 'hourly'],
+        [
+          tokens[0].coingeckoId,
+          HOUR_09.add(-7, 'days'),
+          HOUR_09,
+          'hourly',
+          undefined,
+        ],
+        [
+          tokens[1].coingeckoId,
+          HOUR_13.add(-7, 'days'),
+          HOUR_13,
+          'hourly',
+          undefined,
+        ],
+        [
+          tokens[2].coingeckoId,
+          HOUR_09.add(-7, 'days'),
+          HOUR_13,
+          'hourly',
+          undefined,
+        ],
+        [
+          tokens[0].coingeckoId,
+          HOUR_13.add(-7, 'days'),
+          HOUR_13,
+          'hourly',
+          undefined,
+        ],
       ])
     })
   })
@@ -192,7 +216,7 @@ describe(PriceUpdater.name, () => {
         expect(
           coingeckoQueryService.getUsdPriceHistory,
         ).toHaveBeenCalledExactlyWith([
-          [TOKEN_COINGECKO_ID, HOUR_09.add(-7, 'days'), HOUR_13, 'hourly'],
+          [TOKEN_COINGECKO_ID, HOUR_09.add(-7, 'days'), HOUR_13, 'hourly', undefined],
         ])
       })
     })
@@ -209,7 +233,7 @@ describe(PriceUpdater.name, () => {
         expect(
           coingeckoQueryService.getUsdPriceHistory,
         ).toHaveBeenCalledExactlyWith([
-          [TOKEN_COINGECKO_ID, HOUR_09.add(-7, 'days'), HOUR_09, 'hourly'],
+          [TOKEN_COINGECKO_ID, HOUR_09.add(-7, 'days'), HOUR_09, 'hourly', undefined],
         ])
       })
 
@@ -219,7 +243,7 @@ describe(PriceUpdater.name, () => {
         expect(
           coingeckoQueryService.getUsdPriceHistory,
         ).toHaveBeenCalledExactlyWith([
-          [TOKEN_COINGECKO_ID, HOUR_13.add(-7, 'days'), HOUR_13, 'hourly'],
+          [TOKEN_COINGECKO_ID, HOUR_13.add(-7, 'days'), HOUR_13, 'hourly', undefined],
         ])
       })
 
@@ -235,8 +259,8 @@ describe(PriceUpdater.name, () => {
         expect(
           coingeckoQueryService.getUsdPriceHistory,
         ).toHaveBeenCalledExactlyWith([
-          [TOKEN_COINGECKO_ID, HOUR_09.add(-7, 'days'), HOUR_09, 'hourly'],
-          [TOKEN_COINGECKO_ID, HOUR_13.add(-7, 'days'), HOUR_13, 'hourly'],
+          [TOKEN_COINGECKO_ID, HOUR_09.add(-7, 'days'), HOUR_09, 'hourly', undefined],
+          [TOKEN_COINGECKO_ID, HOUR_13.add(-7, 'days'), HOUR_13, 'hourly', undefined],
         ])
       })
 
@@ -283,6 +307,7 @@ describe(PriceUpdater.name, () => {
           from.add(-7, 'days'),
           from.add(2, 'hours'),
           'hourly',
+          undefined
         ],
       ])
 
