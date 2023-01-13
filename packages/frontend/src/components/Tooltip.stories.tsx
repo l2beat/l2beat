f3a7206660ba0d237b1cb8c0832c41dc4a82e1a7
torztomasz <tomasz.torz@l2beat.com>
2023-01-13 14:38:03 +0100

    Fix missing tooltip on reg-viz

--- packages/frontend/src/components/Tooltip.stories.tsx
+++ packages/frontend/src/components/Tooltip.stories.tsx
@@ -8,16 +8,23 @@ export default {
 }
 
 export function Tooltip() {
-  const ref = useRef<HTMLSpanElement>(null)
+  const tooltipRef = useRef<HTMLSpanElement>(null)
   useEffect(() => {
     configureTooltips()
-    // trigger the display of the tooltip
-    ref.current?.dispatchEvent(new MouseEvent('mouseenter'))
+    // show tooltip
+    tooltipRef.current?.dispatchEvent(new MouseEvent('mouseenter'))
+    // remove all event listeners, leaving them will cause reg-viz to remove tooltip
+    document.body.replaceWith(document.body.cloneNode(true))
+    tooltipRef.current?.replaceWith(tooltipRef.current.cloneNode(true))
   }, [])
 
   return (
-    <div className="m-4">
-      <span ref={ref} className="Tooltip inline-block" title="Tooltip content">
+    <div className="m-4 ml-32">
+      <span
+        ref={tooltipRef}
+        className="Tooltip inline-block"
+        title="Et sunt qui cupidatat minim aliqua occaecat labore elit. Reprehenderit cupidatat culpa aliqua mollit. Adipisicing tempor reprehenderit laborum enim aliquip Lorem excepteur."
+      >
         <span>Element with tooltip</span>
       </span>
       <TooltipComponent />
