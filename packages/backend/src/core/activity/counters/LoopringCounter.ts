41ff227e0a0e667d0cb2fcb574db026ad45a655e
Michał Sobieraj-Jakubiec <38423054+michalsidzej@users.noreply.github.com>
2023-01-19 09:46:56 +0100

    Activity metrics (#1115)

--- packages/backend/src/core/activity/counters/LoopringCounter.ts
+++ packages/backend/src/core/activity/counters/LoopringCounter.ts
@@ -3,6 +3,7 @@ import { LoopringTransactionApi } from '@l2beat/config'
 import { ProjectId } from '@l2beat/types'
 import { range } from 'lodash'
 
+import { Metrics } from '../../../Metrics'
 import { BlockTransactionCountRepository } from '../../../peripherals/database/activity/BlockTransactionCountRepository'
 import { SequenceProcessorRepository } from '../../../peripherals/database/SequenceProcessorRepository'
 import { LoopringClient } from '../../../peripherals/loopring'
@@ -17,6 +18,7 @@ export function createLoopringCounter(
   blockRepository: BlockTransactionCountRepository,
   sequenceProcessorRepository: SequenceProcessorRepository,
   logger: Logger,
+  metrics: Metrics,
   transactionApi: LoopringTransactionApi,
 ): TransactionCounter {
   const callsPerMinute = transactionApi.callsPerMinute
@@ -26,6 +28,7 @@ export function createLoopringCounter(
   const processor = new SequenceProcessor(
     projectId.toString(),
     logger,
+    metrics,
     sequenceProcessorRepository,
     {
       batchSize,
