3b8672df9f8c9e5a3f1b129ab5fff2ac805df4a1
torztomasz <tomasz.torz@l2beat.com>
2023-01-20 11:52:00 +0100

    add type tests for CheckConvention

--- packages/backend/src/peripherals/database/shared/BaseRepository.test.ts
+++ packages/backend/src/peripherals/database/shared/BaseRepository.test.ts
@@ -1,5 +1,6 @@
 import { Logger } from '@l2beat/common'
 import { expect } from 'earljs'
+import { describe } from 'mocha'
 
 import { Metrics } from '../../../Metrics'
 import { setupDatabaseTestSuite } from '../../../test/database'
@@ -123,5 +124,106 @@ describe(BaseRepository.name, () => {
       expect((dummyRepository.getAll as any).wrapped).toEqual(true)
       expect((dummyRepository.refresh as any).wrapped).toEqual(true)
     })
+    /* eslint-disable @typescript-eslint/no-unused-vars */
+    describe.skip('checkConvention', () => {
+      describe('GetMethod', () => {
+        it('should show error if returning wrong type', () => {
+          class DummyRepository extends BaseRepository {
+            constructor(database: Database, logger: Logger, metrics: Metrics) {
+              super(database, logger, metrics)
+              // @ts-expect-error get method should return array
+              this.autoWrap<CheckConvention<DummyRepository>>(this)
+            }
+
+            async getAll(): Promise<number> {
+              return 1
+            }
+          }
+        })
+      })
+
+      describe('AddMethod', () => {
+        it('should show error if convention is wrong', () => {
+          class DummyRepository extends BaseRepository {
+            constructor(database: Database, logger: Logger, metrics: Metrics) {
+              super(database, logger, metrics)
+              // @ts-expect-error add method should take only one argument
+              this.autoWrap<CheckConvention<DummyRepository>>(this)
+            }
+
+            async addWithTooMuchArguments(
+              a: string,
+              b: string,
+            ): Promise<number> {
+              return 1
+            }
+          }
+        })
+      })
+
+      describe('AddManyMethod', () => {
+        it('should show error if return type is wrong', () => {
+          class DummyRepository extends BaseRepository {
+            constructor(database: Database, logger: Logger, metrics: Metrics) {
+              super(database, logger, metrics)
+              // @ts-expect-error addMany method should return array or number
+              this.autoWrap<CheckConvention<DummyRepository>>(this)
+            }
+
+            async addMany(a: string[]): Promise<string> {
+              return ''
+            }
+          }
+        })
+
+        it('should show error if argument is of wrong type', () => {
+          class DummyRepository extends BaseRepository {
+            constructor(database: Database, logger: Logger, metrics: Metrics) {
+              super(database, logger, metrics)
+              // @ts-expect-error addMany method should take array as argument
+              this.autoWrap<CheckConvention<DummyRepository>>(this)
+            }
+
+            async addManyWithWrongArgumentType(a: string): Promise<number> {
+              return 1
+            }
+          }
+        })
+
+        it('should show error if function takes too much arguments', () => {
+          class DummyRepository extends BaseRepository {
+            constructor(database: Database, logger: Logger, metrics: Metrics) {
+              super(database, logger, metrics)
+              // @ts-expect-error addMany method should take only one argument
+              this.autoWrap<CheckConvention<DummyRepository>>(this)
+            }
+
+            async addManyWithTooMuchArguments(
+              a: string[],
+              b: string,
+            ): Promise<number> {
+              return 1
+            }
+          }
+        })
+      })
+
+      describe('DeleteMethod', () => {
+        it('should show error if return type is wrong', () => {
+          class DummyRepository extends BaseRepository {
+            constructor(database: Database, logger: Logger, metrics: Metrics) {
+              super(database, logger, metrics)
+              // @ts-expect-error delete method should return number
+              this.autoWrap<CheckConvention<DummyRepository>>(this)
+            }
+
+            async deleteOne(): Promise<number[]> {
+              return [1]
+            }
+          }
+        })
+      })
+    })
+    /* eslint-enable @typescript-eslint/no-unused-vars */
   })
 })
