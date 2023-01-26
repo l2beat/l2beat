9d949c2eb36ff8980e23a11fe340952b40afed76
antooni <antoni.pawlak@protonmail.com>
2023-01-26 11:12:42 +0100

    remove frontpae mielstone

--- packages/config/src/layer2s/common/milestones.ts
+++ packages/config/src/layer2s/common/milestones.ts
@@ -12,13 +12,6 @@ export const MILESTONES = {
 
 export const HOMEPAGE_MILESTONES: Milestone[] = [
   {
-    name: 'First zkRollup (DEX)',
-    date: '2019-12-04T00:00:00Z',
-    link: 'https://medium.com/loopring-protocol/loopring-deployed-protocol-3-0-on-ethereum-a33103c9e5bf',
-    description:
-      'Loopring is live, launching the first zkRollup DEX protocol on Ethereum mainnet.',
-  },
-  {
     name: 'First StarkEx Validium',
     date: '2020-06-03T00:00:00Z',
     link: 'https://medium.com/starkware/starks-over-mainnet-b83e63db04c0',
