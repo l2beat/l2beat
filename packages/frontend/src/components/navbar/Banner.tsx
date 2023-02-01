763cfb11900e9ee6a6f40eace63a612f24a6a081
Michal <michalsidzej@gmail.com>
2023-02-01 17:39:02 +0100

    Updade color scheme

--- packages/frontend/src/components/navbar/Banner.tsx
+++ packages/frontend/src/components/navbar/Banner.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 export function Banner() {
   return (
     <a
-      className="block text-center bg-pink-200 dark:bg-purple-700 text-xs font-medium px-4 py-1.5"
+      className="block text-center bg-pink-900 text-white text-xs font-medium px-4 py-1.5"
       href="https://l2beat.notion.site/We-are-hiring-Work-at-L2BEAT-e4e637265ae94c5db7dfa2de336b940f"
       target="_blank"
     >
-      We are hiring! 
+      We are hiring!
       <span className="underline ml-2">Join L2BEAT &rarr;</span>
     </a>
   )
