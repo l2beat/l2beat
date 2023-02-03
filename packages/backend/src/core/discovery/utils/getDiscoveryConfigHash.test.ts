f81140b96107709b8484fb34bf9701b0e6c0c180
antooni <antoni.pawlak@protonmail.com>
2023-02-03 10:33:55 +0000

    sorting works

--- packages/backend/src/core/discovery/utils/getDiscoveryConfigHash.test.ts
+++ packages/backend/src/core/discovery/utils/getDiscoveryConfigHash.test.ts
@@ -3,10 +3,14 @@ import { expect } from 'earljs'
 
 import { hashJson } from '../../../tools/hashJson'
 import { DiscoveryConfig } from '../DiscoveryConfig'
-import { getDiscoveryConfigHash } from './getDiscoveryConfigHash'
+import {
+  getDiscoveryConfigEntries,
+  getDiscoveryConfigHash,
+} from './getDiscoveryConfigHash'
 
-const ADDRESS_A = EthereumAddress.random()
-const ADDRESS_B = EthereumAddress.random()
+const ADDRESS_A = EthereumAddress('0xc186fA914353c44b2E33eBE05f21846F1048bEda')
+const ADDRESS_B = EthereumAddress('0xB524735356985D2f267FA010D681f061DfF03715')
+const ADDRESS_C = EthereumAddress('0x592349F7DeDB2b75f9d4F194d4b7C16D82E507Dc')
 
 describe(getDiscoveryConfigHash.name, () => {
   it('correctly generates hash for given config', () => {
@@ -29,5 +33,57 @@ describe(getDiscoveryConfigHash.name, () => {
     const expected = hashJson(JSON.stringify(config))
 
     expect(result).toEqual(expected)
+  })
+
+  it.only(getDiscoveryConfigEntries.name, () => {
+    const config: DiscoveryConfig = {
+      name: 'a',
+      initialAddresses: [ADDRESS_A],
+      maxDepth: 1,
+      maxAddresses: 1,
+      overrides: {
+        [ADDRESS_B.toString()]: {
+          ignoreInWatchMode: ['a', 'b'],
+          ignoreMethods: ['c', 'd'],
+          ignoreDiscovery: false,
+          proxyType: 'call implementation proxy',
+          fields: {},
+        },
+        [ADDRESS_C.toString()]: {
+          ignoreInWatchMode: ['a', 'b'],
+          ignoreMethods: ['c', 'd'],
+          ignoreDiscovery: false,
+          proxyType: 'call implementation proxy',
+          fields: {},
+        },
+      },
+    }
+
+    const expected = {
+      initialAddresses: [ADDRESS_A.toString()],
+      maxAddresses: 1,
+      maxDepth: 1,
+      name: 'a',
+      overrides: {
+        [ADDRESS_C.toString()]: {
+          fields: {},
+          ignoreDiscovery: false,
+          ignoreInWatchMode: ['a', 'b'],
+          ignoreMethods: ['c', 'd'],
+          proxyType: 'call implementation proxy',
+        },
+        [ADDRESS_B.toString()]: {
+          fields: {},
+          ignoreDiscovery: false,
+          ignoreInWatchMode: ['a', 'b'],
+          ignoreMethods: ['c', 'd'],
+          proxyType: 'call implementation proxy',
+        },
+      },
+    }
+
+    const result = getDiscoveryConfigEntries(config)
+
+    expect(result).toEqual(JSON.stringify(expected))
   })
 })
--- packages/backend/src/core/discovery/utils/getDiscoveryConfigHash.ts
+++ packages/backend/src/core/discovery/utils/getDiscoveryConfigHash.ts
@@ -4,5 +4,41 @@ import { hashJson } from '../../../tools/hashJson'
 import { DiscoveryConfig } from '../DiscoveryConfig'
 
 export function getDiscoveryConfigHash(config: DiscoveryConfig): Hash256 {
-  return hashJson(JSON.stringify(config))
+  return hashJson(getDiscoveryConfigEntries(config))
+}
+
+export function getDiscoveryConfigEntries(config: DiscoveryConfig): string {
+  const not_sorted = config
+  console.log(not_sorted)
+
+  const sorted = Object.keys(not_sorted)
+    .sort()
+    .reduce((acc, key) => {
+      if (key === 'overrides') {
+        const not_sorted2 = not_sorted[key]
+
+        const sorted2 = Object.keys(not_sorted2)
+          .sort()
+          .reduce((acc, key) => {
+            const not_sorted3 = not_sorted2[key]
+            const sorted3 = Object.keys(not_sorted3)
+            .sort()
+            .reduce((acc, key) => ({
+                ...acc, [key]: not_sorted3[key]
+            }), {})
+
+            return {
+              ...acc,
+              [key]: sorted3,
+            }
+          }, {})
+
+        return { ...acc, [key]: sorted2 }
+      } else {
+        return { ...acc, [key]: not_sorted[key] }
+      }
+    }, {})
+
+  console.log(sorted) //{a: true, b: false}
+  return JSON.stringify(sorted)
 }
