3b634c5b3f2625d2204280222866f0dcf033420a
Michał Sobieraj-Jakubiec <michalsidzej@gmail.com>
2023-02-03 14:36:13 +0100

    Remove scripts (#1172)

--- packages/backend/scripts/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './cast'
-export * from './compare'
-export * from './eip1967'
-export * from './nutberry'
-export * from './reference'
-export * from './starkware'
-export * from './zeppelinos'
