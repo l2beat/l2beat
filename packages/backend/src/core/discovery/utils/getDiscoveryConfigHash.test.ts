40fcb605e2ef1965333b1eeceff8a73d91cae307
antooni <antoni.pawlak@protonmail.com>
2023-02-07 09:10:42 +0000

    add test for config generation

--- packages/backend/src/core/discovery/utils/getDiscoveryConfigHash.test.ts
+++ packages/backend/src/core/discovery/utils/getDiscoveryConfigHash.test.ts
@@ -12,25 +12,34 @@ const ADDRESS_A = EthereumAddress('0xc186fA914353c44b2E33eBE05f21846F1048bEda')
 const ADDRESS_B = EthereumAddress('0xB524735356985D2f267FA010D681f061DfF03715')
 const ADDRESS_C = EthereumAddress('0x592349F7DeDB2b75f9d4F194d4b7C16D82E507Dc')
 
+const CONFIG: DiscoveryConfig = {
+  name: 'a',
+  initialAddresses: [ADDRESS_A],
+  maxAddresses: 1,
+  maxDepth: 1,
+  overrides: {
+    [ADDRESS_B.toString()]: {
+      ignoreInWatchMode: ['b', 'a'],
+      ignoreMethods: ['d', 'c'],
+      ignoreDiscovery: false,
+    },
+  },
+}
+
 describe(getDiscoveryConfigHash.name, () => {
   it('correctly generates hash for given config', () => {
-    const config: DiscoveryConfig = {
-      name: 'a',
-      initialAddresses: [ADDRESS_A],
-      maxAddresses: 1,
-      maxDepth: 1,
-      overrides: {
-        [ADDRESS_B.toString()]: {
-          ignoreInWatchMode: ['a', 'b'],
-          ignoreMethods: ['c', 'd'],
-          ignoreDiscovery: false,
-        },
-      },
-    }
+    const result = getDiscoveryConfigHash(CONFIG)
 
-    const result = getDiscoveryConfigHash(config)
+    const expected =
+      '0x800ad499ae413c85dbfd965e91cb0120fd62b38efd4a921995268110f9e238ed'
 
-    const expected = hashJson(getDiscoveryConfigEntries(config))
+    expect(result).toEqual(expected)
+  })
+
+  it('uses entries sorting algorithm', () => {
+    const result = getDiscoveryConfigHash(CONFIG)
+
+    const expected = hashJson(getDiscoveryConfigEntries(CONFIG))
 
     expect(result).toEqual(expected)
   })
