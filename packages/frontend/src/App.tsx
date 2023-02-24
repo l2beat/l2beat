fdedf1e90a2696b0096ee2eef96445ed1bb7e02e
Kris Kaczor <krzkaczor@gmail.com>
2023-02-24 15:32:40 +0400

    Refactor layout of the main screen (l2beat/protocolbeat#18)

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import '@total-typescript/ts-reset'
 
-import classNames from 'classnames'
+import cx from 'classnames'
 import { useState } from 'react'
 
 import { deleteNode } from './api/delete'
@@ -21,6 +21,7 @@ export function App() {
   const [loading, setLoading] = useState<Record<string, boolean>>({})
   const selectedIds = useStore((state) => state.selectedNodeIds)
   const selectedNodes = nodes.filter((x) => selectedIds.includes(x.id))
+  const showSidebar = selectedNodes.length > 0
 
   function markLoading(id: string, value: boolean) {
     setLoading((loading) => ({ ...loading, [id]: value }))
@@ -58,47 +59,62 @@ export function App() {
   }
 
   return (
-    <div className="grid h-full w-full grid-cols-[1fr,_400px] grid-rows-[64px,_1fr]">
-      <div className="flex h-full w-full items-center justify-center">
-        <button
-          className={classNames(
-            'rounded bg-blue-500 py-2 px-4 font-bold text-white',
-            !loading.global && 'hover:bg-blue-700',
-          )}
-          type="button"
-          disabled={loading.global}
-          onClick={() => void showPrompt()}
-        >
-          Discover!
-          {loading.global && '🔄'}
-        </button>
-        <p className="ml-2">Contracts loaded: {nodes.length}</p>
-        <button
-          className="ml-2	text-2xl"
-          type="button"
-          disabled={loading.global}
-          onClick={clear}
-          title="Clear"
-        >
-          🚮
-        </button>
-      </div>
-
-      <div className="row-span-2 bg-white p-2 drop-shadow-xl">
-        <Sidebar
-          selectedNodes={selectedNodes}
-          onDeleteNodes={deleteNodeAction}
-        />
-      </div>
-
-      <div className="flex h-full w-full items-center justify-center gap-4 p-2">
+    <div
+      className={cx(
+        'grid h-full w-full grid-rows-[_1fr]',
+        showSidebar ? 'grid-cols-[1fr,_400px]' : 'grid-cols-[1fr]',
+      )}
+    >
+      <div className="relative flex h-full w-full items-center justify-center">
         <Viewport
           nodes={nodes}
           loading={loading}
           // eslint-disable-next-line @typescript-eslint/no-misused-promises
           onDiscover={discoverContract}
         />
+
+        <div className="absolute top-0 w-full p-2">
+          <div className="flex flex-row content-center items-center	 justify-between">
+            <div className="ml-2">Contracts loaded: {nodes.length}</div>
+
+            <div>
+              <button
+                className={cx(
+                  'rounded bg-blue-500 py-2 px-4 font-bold text-white',
+                  !loading.global && 'hover:bg-blue-700',
+                )}
+                type="button"
+                disabled={loading.global}
+                onClick={() => void showPrompt()}
+              >
+                Discover!
+                {loading.global && '🔄'}
+              </button>
+            </div>
+
+            <div>
+              <button
+                className="ml-2	text-2xl"
+                type="button"
+                disabled={loading.global}
+                onClick={clear}
+                title="Clear"
+              >
+                🚮
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
+
+      {showSidebar && (
+        <div className="row-span-2 bg-white p-2 drop-shadow-xl">
+          <Sidebar
+            selectedNodes={selectedNodes}
+            onDeleteNodes={deleteNodeAction}
+          />
+        </div>
+      )}
     </div>
   )
 }
