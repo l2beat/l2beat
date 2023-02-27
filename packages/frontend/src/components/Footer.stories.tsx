5737f666e9b40a00836df80b06057e092eaf440d
Michał Sobieraj-Jakubiec <michalsidzej@gmail.com>
2023-02-27 11:06:44 +0100

    Add linkedIn link to Socials (#1247)

--- packages/frontend/src/components/Footer.stories.tsx
+++ packages/frontend/src/components/Footer.stories.tsx
@@ -14,6 +14,7 @@ export function Footer() {
         twitterLink="#"
         discordLink="#"
         githubLink="#"
+        linkedinLink="#"
         mediumLink="#"
         youTubeLink="#"
       />
