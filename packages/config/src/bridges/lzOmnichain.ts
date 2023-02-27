056637cb3114a7cf432b3ac071298f57bf8c448c
antooni <antoni.pawlak@protonmail.com>
2023-02-27 16:27:40 +0100

    Add omnichain tokens list

--- packages/config/src/bridges/lzOmnichain.ts
+++ packages/config/src/bridges/lzOmnichain.ts
@@ -1,8 +1,9 @@
-import { EthereumAddress, ProjectId, UnixTime } from '@l2beat/shared'
+import { EthereumAddress, ProjectId } from '@l2beat/shared'
 
 import { CONTRACTS } from '../layer2s/common'
 import { ProjectDiscovery } from '../layer2s/common/ProjectDiscovery'
 import { RISK_VIEW } from './common'
+import { OMNICHAN_ESCROWS } from './lzOmnichain.escrows'
 import { Bridge } from './types'
 
 const discovery = new ProjectDiscovery('lzOmnichain')
@@ -13,6 +14,8 @@ export const lzOmnichain: Bridge = {
   display: {
     name: 'Omnichain (LayerZero)',
     slug: 'omnichain',
+    warning:
+      'The security parameters of each individual token must be individually assessed, and can be changed by the developers. Omnichain tokens are are in the early stages of development, use at your own risk.',
     links: {
       websites: ['https://layerzero.network/'],
       repositories: [
@@ -86,66 +89,16 @@ export const lzOmnichain: Bridge = {
     },
   },
   config: {
-    escrows: [
-      {
-        address: EthereumAddress('0xAf5191B0De278C7286d6C7CC6ab6BB8A73bA2Cd6'),
-        sinceTimestamp: new UnixTime(1647504559),
-        tokens: ['STG'],
-      },
-      {
-        address: EthereumAddress('0x4fa745fccc04555f2afa8874cd23961636cdf982'),
-        sinceTimestamp: new UnixTime(1657699079),
-        tokens: ['agEUR'],
-      },
-      {
-        address: EthereumAddress('0x1A36E24D61BC1aDa68C21C2Da1aD53EaB8E03e55'),
-        sinceTimestamp: new UnixTime(1663003197),
-        tokens: ['BOBA'],
-      },
-      {
-        address: EthereumAddress('0xb0003eb166654f7e57c0463f8d1a438eb238c490'),
-        sinceTimestamp: new UnixTime(1661794745),
-        tokens: ['BOBA'],
-      },
-      {
-        address: EthereumAddress('0x6f537839714761388b6d7ed61bc09579d5da2f41'),
-        sinceTimestamp: new UnixTime(1660932033),
-        tokens: ['BOBA'],
-      },
-      {
-        address: EthereumAddress('0xee381e476b4335b8584a2026f3e845edac2c69de'),
-        sinceTimestamp: new UnixTime(1663802015),
-        tokens: ['LINK'],
-      },
-      {
-        address: EthereumAddress('0x4F52b41a778761bd2EEa5b7b7ed8cBDAA02cEF3E'),
-        sinceTimestamp: new UnixTime(1665428735),
-        tokens: ['USDC'],
-      },
-      {
-        address: EthereumAddress('0xb5D4D94b82722EAFe067887a2FB596Bb37B3AAd2'),
-        sinceTimestamp: new UnixTime(1669580819),
-        tokens: ['MATIC'],
-      },
-      {
-        address: EthereumAddress('0x07b6eb9d3be334098e5af77185344bb3a34e0017'),
-        sinceTimestamp: new UnixTime(1669581635),
-        tokens: ['CRV'],
-      },
-      {
-        address: EthereumAddress('0xab6Ebe9472e2e4B6FE720Dad16701F32ab905CC6'),
-        sinceTimestamp: new UnixTime(1666769735),
-        tokens: ['WBTC'],
-      },
-      {
-        address: EthereumAddress('0x242d6e16653b30c830c1918b5cc23d27253b2d26'),
-        sinceTimestamp: new UnixTime(1669583471),
-        tokens: ['UNI'],
-      },
-    ],
+    escrows: OMNICHAN_ESCROWS,
   },
   contracts: {
     addresses: [
+      {
+        multipleAddresses: OMNICHAN_ESCROWS.map((e) => e.address),
+        name: 'Omnichain Tokens (OFT)',
+        description:
+          'Contracts using LayerZero smart contracts to transfer tokens between chains. The implementation details may vary between each individual omnichain token and must be individually assessed. LayerZero as a framework to build omnichain application does not provide any base security as applications can define their own security settings, however applications and tokens choosing the default security settings will leverage security provided by default Oracle, Relayer, Verification Library and Proof Library. Default settings are managed by LayerZero team.',
+      },
       {
         address: EthereumAddress('0x902F09715B6303d4173037652FA7377e5b98089E'),
         name: 'Default LayerZero Relayer',
