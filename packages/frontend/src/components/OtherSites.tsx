a5a9d3826b624da3eb3391ec6cdabee0fe431abc
vitali-l2beat <vitali.litvinovich@l2beat.com>
2023-03-05 23:12:50 +0300

    change l2fees.info bar

--- packages/frontend/src/components/OtherSites.tsx
+++ packages/frontend/src/components/OtherSites.tsx
@@ -4,10 +4,26 @@ import { OutLink } from './OutLink'
 
 export function OtherSites() {
   return (
-    <div className="OtherSites-L2Fees">
-      Ethereum Layer 1 is expensive. How much does it cost to use Layer 2? 💸
-      <OutLink className="underline" allowReferrer href="https://l2fees.info/">
-        Find out on L2 Fees
+    <div className="
+      flex flex-row justify-between items-center
+      mt-[32px] px-[40px] py-[24px]
+      rounded-[12px] 
+      dark:bg-gradient-to-r from-[#7E41CC] via-[#FF46C0] to-[#EE2C01] to-[#FF46C0] bg-purple-700
+    ">
+      <div className="flex flex-col justify-between text-white text-2xl">
+        <span>Ethereum Layer 1 is expensive.</span>
+        <span>How much does it cost to use Layer 2?</span>
+      </div>
+      <OutLink className="
+        flex items-center 
+        px-[48px] py-[19px]
+        rounded-[8px]
+        text-lg
+        dark:bg-gradient-to-r from-[#7E41CC] to-[#FF46C0] bg-white" 
+      allowReferrer href="https://l2fees.info/">
+        <span>
+          Find out on L2 Fees
+        </span>
       </OutLink>
     </div>
   )
