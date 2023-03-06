ddedd1c5d568316f33572db4fbf74bce5ccc898a
vitali-l2beat <vitali.litvinovich@l2beat.com>
2023-03-06 11:39:55 +0100

    run prettier on the modified file

--- packages/frontend/src/components/otherSites/OtherSites.tsx
+++ packages/frontend/src/components/otherSites/OtherSites.tsx
@@ -4,27 +4,30 @@ import { OutLink } from '../OutLink'
 
 export function OtherSites() {
   return (
-    <div className="
-      flex flex-row justify-between items-center
-      mt-[32px] px-[40px] py-[24px]
-      rounded-[12px] 
-      bg-gradient-to-r from-[#7E41CC99] via-[#FF46C099] to-[#EE2C0199]
+    <div
+      className="
+      mt-[32px] flex flex-row items-center
+      justify-between rounded-[12px] bg-gradient-to-r
+      from-[#7E41CC99] 
+      via-[#FF46C099] to-[#EE2C0199] px-[40px] py-[24px]
       dark:from-purple-700 dark:to-purple-700
-    ">
-      <div className="flex flex-col justify-between text-white text-2xl">
+    "
+    >
+      <div className="flex flex-col justify-between text-2xl text-white">
         <span>Ethereum Layer 1 is expensive.</span>
         <span>How much does it cost to use Layer 2?</span>
       </div>
-      <OutLink className="
+      <OutLink
+        className="
         flex items-center 
-        px-[48px] py-[19px]
-        rounded-[8px]
-        text-lg
-        bg-white dark:bg-gradient-to-r from-[#7E41CC] to-[#FF46C0]"
-        allowReferrer href="https://l2fees.info/">
-        <span>
-          Find out on L2 Fees
-        </span>
+        rounded-[8px] bg-white
+        from-[#7E41CC]
+        to-[#FF46C0]
+        px-[48px] py-[19px] text-lg dark:bg-gradient-to-r"
+        allowReferrer
+        href="https://l2fees.info/"
+      >
+        <span>Find out on L2 Fees</span>
       </OutLink>
     </div>
   )
