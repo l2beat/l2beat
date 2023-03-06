4d809c876bdc19b43ec442afb8c8ba73a311a7f1
vitali-l2beat <vitali.litvinovich@l2beat.com>
2023-03-06 19:27:43 +0300

    L2B-940 - add mobile landing

--- packages/frontend/src/components/other-sites/OtherSites.tsx
+++ packages/frontend/src/components/other-sites/OtherSites.tsx
@@ -6,21 +6,21 @@ export function OtherSites() {
   return (
     <div
       className="
-      mt-8 flex flex-row items-center
-      justify-between rounded-xl bg-gradient-to-r
-      from-purple-100/40
-      via-pink-100/40 to-red-200/40
-      px-10 py-6
+      mt-8 flex flex-col items-center justify-between
+      rounded-xl bg-gradient-to-r from-purple-100/40
+      via-pink-100/40
+      to-red-200/40 px-10
+      py-6 md:flex-row
     "
     >
-      <div className="flex flex-col justify-between text-lg text-white">
-        <span>Ethereum Layer 1 is expensive. How much does it cost to use Layer 2? 💸</span>
+      <div className="text-center text-lg text-white md:text-left">
+        Ethereum Layer 1 is expensive. How much does it cost to use Layer 2? 💸
       </div>
       <OutLink
         className="
-        flex items-center min-w-[90px]
-        ml-4 text-lg font-bold
-        text-center self-center underline"
+        mt-3 ml-0 min-w-[90px] self-center text-center
+        text-lg font-bold
+        underline md:mt-0 md:ml-4"
         allowReferrer
         href="https://l2fees.info/"
       >
