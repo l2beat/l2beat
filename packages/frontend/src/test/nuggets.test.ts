2875c3f605478f409a2c43c64dd98110f9f90b87
vitali-l2beat <vitali.litvinovich@l2beat.com>
2023-03-07 12:28:03 +0100

    L2B-939 – Run prettier on nuggets test

--- packages/frontend/src/test/nuggets.test.ts
+++ packages/frontend/src/test/nuggets.test.ts
@@ -2,16 +2,16 @@ import { KnowledgeNugget, layer2s } from '@l2beat/config'
 import { expect } from 'earljs'
 
 const knowledgeNuggets = layer2s.reduce<KnowledgeNugget[]>((nuggets, l2) => {
-    nuggets.push(...(l2.knowledgeNuggets ?? []));
-    return nuggets;
-}, []);
+  nuggets.push(...(l2.knowledgeNuggets ?? []))
+  return nuggets
+}, [])
 
 describe('knowledgeNuggets', () => {
-    describe('title fits character limit', () => {
-        knowledgeNuggets.forEach(nugget => {
-            it(nugget.title, () => {
-                expect(nugget.title.length).toBeLessThanOrEqualTo(40);
-            })
-        });
-    });
+  describe('title fits character limit', () => {
+    knowledgeNuggets.forEach((nugget) => {
+      it(nugget.title, () => {
+        expect(nugget.title.length).toBeLessThanOrEqualTo(40)
+      })
+    })
+  })
 })
