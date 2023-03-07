c9f04dba876ae3828e19de0ef420acbf37b09b26
Michal <michalsidzej@gmail.com>
2023-03-07 08:46:27 +0100

    Add big Rosette component

--- packages/frontend/src/components/Rosette.stories.tsx
+++ packages/frontend/src/components/Rosette.stories.tsx
@@ -1,20 +1,59 @@
 import { Story } from '@storybook/react'
 import React from 'react'
 
-import { RosetteProps, SmallRosette } from './Rosette'
+import {
+  BigRosette as BigRosetteComponent,
+  RosetteProps,
+  SmallRosette as SmallRosetteComponent,
+} from './Rosette'
 
 export default {
   title: 'Components/Rosette',
+  argTypes: {
+    sequencerFailure: {
+      options: ['bad', 'warning', 'fine'],
+      control: { type: 'select' },
+    },
+    stateValidation: {
+      options: ['bad', 'warning', 'fine'],
+      control: { type: 'select' },
+    },
+    dataAvailability: {
+      options: ['bad', 'warning', 'fine'],
+      control: { type: 'select' },
+    },
+    upgradeability: {
+      options: ['bad', 'warning', 'fine'],
+      control: { type: 'select' },
+    },
+    validatorFailure: {
+      options: ['bad', 'warning', 'fine'],
+      control: { type: 'select' },
+    },
+  },
 }
 
 type TemplateProps = RosetteProps['risks']
 
-function Template(risks: TemplateProps) {
-  return <SmallRosette risks={risks} />
+function SmallTemplate(risks: TemplateProps) {
+  return <SmallRosetteComponent risks={risks} />
 }
 
-export const Rosette: Story<TemplateProps> = Template.bind({})
-Rosette.args = {
+export const SmallRosette: Story<TemplateProps> = SmallTemplate.bind({})
+SmallRosette.args = {
+  sequencerFailure: 'bad',
+  stateValidation: 'warning',
+  dataAvailability: 'fine',
+  upgradeability: 'bad',
+  validatorFailure: 'warning',
+}
+
+function BigTemplate(risks: TemplateProps) {
+  return <BigRosetteComponent risks={risks} />
+}
+
+export const BigRosette: Story<TemplateProps> = BigTemplate.bind({})
+BigRosette.args = {
   sequencerFailure: 'bad',
   stateValidation: 'warning',
   dataAvailability: 'fine',
