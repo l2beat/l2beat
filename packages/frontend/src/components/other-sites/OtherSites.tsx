aa8d5092f4c10663d9a298c6067c6ad9f6d9ba41
vitali-l2beat <vitali.litvinovich@l2beat.com>
2023-03-07 17:47:01 +0300

    L2B-940 - change mobile landing

--- packages/frontend/src/components/other-sites/OtherSites.tsx
+++ packages/frontend/src/components/other-sites/OtherSites.tsx
@@ -1,3 +1,4 @@
+import cx from 'classnames'
 import React from 'react'
 
 import { OutLink } from '../OutLink'
@@ -5,22 +6,36 @@ import { OutLink } from '../OutLink'
 export function OtherSites() {
   return (
     <div
-      className="
-      mt-8 flex flex-col items-center justify-between
-      rounded-xl bg-gradient-to-r from-purple-100/40
-      via-pink-100/40
-      to-red-200/40 px-10
-      py-6 md:flex-row
-    "
+      className={cx(
+        'mt-8 flex flex-col items-center justify-between md:flex-row',
+        'rounded-0 bg-gradient-to-r from-purple-100/40 md:rounded-xl',
+        'via-pink-100/40',
+        'to-red-200/40 px-10',
+        'py-6 md:flex-row',
+      )}
     >
-      <div className="text-center text-lg font-semibold md:text-left">
+      <span
+        className={cx(
+          'flex flex-col text-center text-lg font-semibold',
+          'md:hidden md:flex-row md:text-left',
+        )}
+      >
+        <span className="w-max self-center text-pink-200 md:text-white">
+          Ethereum Layer 1 is expensive.
+        </span>
+        <span className="md:flex md:flex-row">
+          How much does it cost to use Layer 2?
+        </span>
+      </span>
+      <span className="hidden text-lg font-semibold md:block">
         Ethereum Layer 1 is expensive. How much does it cost to use Layer 2? 💸
-      </div>
+      </span>
       <OutLink
-        className="
-        mt-3 ml-0 min-w-[90px] text-center
-        text-lg font-bold
-        underline md:mt-0 md:ml-4"
+        className={cx(
+          'mt-3 ml-0 text-center',
+          'text-lg font-bold',
+          'underline md:mt-0 md:ml-2',
+        )}
         allowReferrer
         href="https://l2fees.info/"
       >
