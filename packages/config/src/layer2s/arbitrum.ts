bf811f32c31764c89b39281a47b78b1ebf132b70
vitali-l2beat <vitali.litvinovich@l2beat.com>
2023-03-07 14:27:10 +0100

    L2B-939 – Correct arbitrum knowledge nugget title

--- packages/config/src/layer2s/arbitrum.ts
+++ packages/config/src/layer2s/arbitrum.ts
@@ -520,7 +520,7 @@ export const arbitrum: Layer2 = {
       thumbnailUrl: NUGGETS.BARTEK_TWITTER_THUMBNAIL,
     },
     {
-      title: 'Arbitrum is down - but does it work?',
+      title: 'Arbitrum is down... or is it?',
       url: 'https://twitter.com/bkiepuszewski/status/1438445910191710211',
       thumbnailUrl: NUGGETS.BARTEK_TWITTER_THUMBNAIL,
     },
