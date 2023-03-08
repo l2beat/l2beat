9ec49072ef6908c82b8e8aa8e0bc4fc4efe9cd8f
Michal <michalsidzej@gmail.com>
2023-03-08 11:01:30 +0100

    Implement light mode

--- packages/frontend/src/components/rosette/Rosette.tsx
+++ packages/frontend/src/components/rosette/Rosette.tsx
@@ -68,7 +68,12 @@ function BigRosetteIcon({ risks }: RosetteProps) {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <circle cx="90.8408" cy="90" r="90" fill="#323539" />
+      <circle
+        cx="90.8408"
+        cy="90"
+        r="90"
+        className="fill-gray-100 dark:fill-gray-825"
+      />
       <path
         d="M80.8589 98.2024C83.4853 97.3765 86.1548 99.3485 86.1374 102.102L85.7291 166.681C85.7139 169.077 83.649 170.912 81.288 170.504C73.3126 169.126 56.0884 165.178 42.7447 155.321C28.2279 144.597 19.9876 129.784 16.6906 122.825C15.6954 120.724 16.8492 118.33 19.0669 117.632L80.8589 98.2024Z"
         className={sentimentToClassName(risks.sequencerFailure)}
@@ -100,6 +105,6 @@ function sentimentToClassName(sentiment?: Sentiment): string {
     case 'warning':
       return 'fill-yellow-200'
     case undefined:
-      return 'fill-gray-750'
+      return 'fill-gray-400 dark:fill-gray-750'
   }
 }
