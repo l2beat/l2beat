be2ae7a3461887da4b62ba8b4d739b54fb6b94b5
vitali-l2beat <vitali.litvinovich@l2beat.com>
2023-03-08 11:14:04 +0300

    L2B-940 - redesign bar for light theme

--- packages/frontend/src/components/other-sites/OtherSites.tsx
+++ packages/frontend/src/components/other-sites/OtherSites.tsx
@@ -7,7 +7,7 @@ export function OtherSites() {
   return (
     <div
       className={cx(
-        'mt-8 flex flex-col items-center justify-between md:flex-row',
+        '-ml-4 mt-8 flex w-screen flex-col items-center justify-between md:-ml-0 md:w-full md:flex-row',
         'rounded-0 bg-gradient-to-r from-purple-100/40 md:rounded-xl',
         'via-pink-100/40',
         'to-red-200/40 px-10',
@@ -16,14 +16,19 @@ export function OtherSites() {
     >
       <span
         className={cx(
-          'flex flex-col text-center text-lg font-semibold',
-          'md:hidden md:flex-row md:text-left',
+          'flex flex-col text-center font-semibold',
+          'md:hidden md:text-left',
         )}
       >
-        <span className="w-max self-center text-pink-200 md:text-white">
-          Ethereum Layer 1 is expensive.
+        <span
+          className={cx(
+            'self-center text-pink-900 dark:text-pink-200',
+            'text-sm font-semibold tracking-wider',
+          )}
+        >
+          ETHEREUM LAYER 1 IS EXPENSIVE
         </span>
-        <span className="md:flex md:flex-row">
+        <span className="mt-1 text-lg font-bold">
           How much does it cost to use Layer 2?
         </span>
       </span>
