331770b52f9eb3281ebf76db98d77a8cc6662f3d
Michal <michalsidzej@gmail.com>
2023-03-08 08:50:36 +0100

    Add Rosette Tooltip

--- packages/frontend/src/components/rosette/TooltipPopup.tsx
+++ packages/frontend/src/components/rosette/TooltipPopup.tsx
@@ -1,7 +1,8 @@
+import cx from 'classnames'
 import React from 'react'
 
 import { BigRosette } from './Rosette'
-import { RiskSentiments, RiskValues } from './types'
+import { RiskSentiments, RiskValue, RiskValues } from './types'
 
 export interface RosetteTooltipProps {
   riskSummary: RiskSentiments
@@ -14,10 +15,55 @@ export function RosetteTooltipPopup({
 }: RosetteTooltipProps) {
   return (
     <div className="w-88 flex flex-col gap-4">
-      <span className="font-bold">
-        <span className="mr-2">Current score</span>
+      <span className="text-base font-bold">
+        <span className="mr-2">Risk analysis</span>
       </span>
-      <BigRosette risks={riskSummary} />
+      <div className="flex gap-4">
+        <div className="-ml-3">
+          <BigRosette risks={riskSummary} />
+        </div>
+        <div>
+          <RiskValueComponent
+            title="Sequencer failure"
+            risk={riskValues.sequencerFailure}
+          />
+          <RiskValueComponent
+            title="State validation"
+            risk={riskValues.stateValidation}
+          />
+          <RiskValueComponent
+            title="Data availability"
+            risk={riskValues.dataAvailability}
+          />
+          <RiskValueComponent
+            title="Upgradeability"
+            risk={riskValues.upgradeability}
+          />
+          <RiskValueComponent
+            title="Validator failure"
+            risk={riskValues.validatorFailure}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+interface RiskValueProps {
+  title: string
+  risk: RiskValue
+}
+
+function RiskValueComponent({ title, risk }: RiskValueProps) {
+  const sentimentColor =
+    risk.sentiment &&
+    (risk.sentiment === 'bad' ? 'text-red-500' : 'text-yellow-200')
+  return (
+    <div>
+      <span className="mb-1 block text-[10px] uppercase">{title}</span>
+      <span className={cx(sentimentColor, 'mb-4 block text-base font-medium')}>
+        {risk.value}
+      </span>
     </div>
   )
 }
--- packages/frontend/src/components/rosette/types.ts
+++ packages/frontend/src/components/rosette/types.ts
@@ -11,7 +11,7 @@ export interface RosetteProps {
   risks: RiskSentiments
 }
 
-interface RiskValue {
+export interface RiskValue {
   value: string
   sentiment?: Sentiment
 }
