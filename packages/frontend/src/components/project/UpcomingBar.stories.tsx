4338d56df22bcaef79addb22462948ac1e13f269
antooni <antoni.pawlak@protonmail.com>
2023-03-09 12:29:03 +0100

    Add storybook

---
+++ packages/frontend/src/components/project/UpcomingBar.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { UpcomingBar as UpcomingBarComponent } from './UpcomingBar'
+
+export default {
+  title: 'Components/Project/UpcomingBar',
+}
+
+export function UpcomingBar() {
+  return (
+    <div className="p-4">
+      <UpcomingBarComponent />
+    </div>
+  )
+}
