4f196291639c9b4d9f7aad006a1dfe7d11f835b9
Michal <michalsidzej@gmail.com>
2023-03-14 14:17:05 +0100

    Fix lint

--- packages/frontend/src/scripts/configureDropdowns.ts
+++ packages/frontend/src/scripts/configureDropdowns.ts
@@ -10,21 +10,21 @@ export function configureDropdowns() {
   const dropdowns = $$('.Dropdown')
 
   for (const dropdown of dropdowns) {
-    const button = dropdown.querySelector('.Dropdown-Button')!
-    const items = dropdown.querySelector('.Dropdown-Items')!
-    const arrow = dropdown.querySelector('.Dropdown-Arrow')!
+    const button = dropdown.querySelector('.Dropdown-Button')
+    const items = dropdown.querySelector('.Dropdown-Items')
+    const arrow = dropdown.querySelector('.Dropdown-Arrow')
 
     document.body.addEventListener('click', (e) => {
       if (e.currentTarget !== dropdown) {
-        items.classList.add('hidden')
-        arrow.classList.remove('rotate-180')
+        items?.classList.add('hidden')
+        arrow?.classList.remove('rotate-180')
       }
     })
 
-    button.addEventListener('click', (e) => {
+    button?.addEventListener('click', (e) => {
       e.stopPropagation()
-      items.classList.toggle('hidden')
-      arrow.classList.toggle('rotate-180')
+      items?.classList.toggle('hidden')
+      arrow?.classList.toggle('rotate-180')
     })
   }
 }
