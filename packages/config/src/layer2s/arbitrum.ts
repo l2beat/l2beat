0e03049059fa82ee777b4155ccc835a1ef2a3040
Michal <michalsidzej@gmail.com>
2023-03-20 13:24:55 +0100

    Change arbitrum dao risk

--- packages/config/src/layer2s/arbitrum.ts
+++ packages/config/src/layer2s/arbitrum.ts
@@ -212,7 +212,7 @@ export const arbitrum: Layer2 = {
       name: 'Arbitrum DAO is in charge of upgrades',
       description:
         'Arbitrum DAO allows $ARB token holders to propose and vote on changes to the organization and the technologies it governs. The governance smart contracts are implemented on Arbitrum One rollup chain. The DAO can upgrade the Arbitrum One contracts on L2 with 3 days delay and - using L2 --> L1 Governance Relay, update contracts on L1 with additional 3 day delay + 7 days delay for all L2 --> L1 messages (in total a delay of 13 days). The Security Council can upgrade the contracts without any delay. It can also cancel any upgrades initiated by the DAO.',
-      risks: [CONTRACTS.UPGRADE_NO_DELAY_RISK],
+      risks: [CONTRACTS.UPGRADE_WITH_DELAY_RISK('13 days')],
       references: [
         {
           text: 'Arbitrum DAO',
