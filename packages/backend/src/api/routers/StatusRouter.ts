da98d850dfb3b991a8f835289591314022a2b5aa
antooni <antoni.pawlak@protonmail.com>
2023-03-21 14:43:57 +0100

    update Router lint

--- packages/backend/src/api/routers/StatusRouter.ts
+++ packages/backend/src/api/routers/StatusRouter.ts
@@ -50,14 +50,15 @@ export function createStatusRouter(statusController: StatusController) {
 
   router.get('/status/discovery', async (ctx) => {
     ctx.body = await statusController.getDiscoveryDashboard()
-  }),
-    router.get(
-      '/status/discovery/:project',
-      withTypedContext(paramsParser, async (ctx) => {
-        const { project } = ctx.params
-        ctx.body = await statusController.getDiscoveryConfigStatus(project)
-      }),
-    )
+  })
+
+  router.get(
+    '/status/discovery/:project',
+    withTypedContext(paramsParser, async (ctx) => {
+      const { project } = ctx.params
+      ctx.body = await statusController.getDiscoveryConfigStatus(project)
+    }),
+  )
 
   return router
 }
