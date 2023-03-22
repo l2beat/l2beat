c5459382050df3f05cbe03b144064bf7d357b106
antooni <antoni.pawlak@protonmail.com>
2023-03-22 08:53:13 +0100

    refactor getAddresses

--- packages/backend/src/api/controllers/status/discovery/props/utils/getAddresses.ts
+++ packages/backend/src/api/controllers/status/discovery/props/utils/getAddresses.ts
@@ -1,65 +1,56 @@
-import { ContractParameters, EthereumAddress } from '@l2beat/shared'
+import {
+  assertUnreachable,
+  ContractParameters,
+  EthereumAddress,
+  UpgradeabilityParameters,
+} from '@l2beat/shared'
 
 export function getAddresses(contract: ContractParameters): EthereumAddress[] {
-  const addresses: EthereumAddress[] = [contract.address]
-  if (contract.upgradeability.type === 'immutable') {
-    return addresses
+  const addresses = gatherAddressesFromUpgradeability(contract.upgradeability)
+
+  return [contract.address, ...addresses]
+}
+
+// TODO: Move to shared
+// This function is duplicated in packages/config/scripts/checkVerifiedContracts/addresses.ts
+export function gatherAddressesFromUpgradeability(
+  item: UpgradeabilityParameters,
+): EthereumAddress[] {
+  const result: EthereumAddress[] = []
+
+  switch (item.type) {
+    case 'EIP1967 proxy':
+    case 'ZeppelinOS proxy':
+    case 'StarkWare diamond':
+    case 'resolved delegate proxy':
+    case 'call implementation proxy':
+    case 'EIP897 proxy':
+      result.push(item.implementation)
+      break
+    case 'StarkWare proxy':
+      result.push(item.implementation)
+      if (item.callImplementation) {
+        result.push(item.callImplementation)
+      }
+      break
+    case 'Arbitrum proxy':
+      result.push(item.adminImplementation)
+      result.push(item.userImplementation)
+      break
+    case 'new Arbitrum proxy':
+      result.push(item.adminImplementation)
+      result.push(item.userImplementation)
+      result.push(item.implementation)
+      break
+    case 'immutable':
+    case 'gnosis safe':
+    case 'EIP2535 diamond proxy':
+      // Ignoring types because no (admin/user)implementation included in them
+      break
+    default:
+      // This code triggers a typescript compile-time error if not all cases have been covered
+      assertUnreachable(item)
   }
 
-  if (contract.upgradeability.type === 'gnosis safe') {
-    addresses.push(contract.upgradeability.masterCopy)
-    return addresses
-  }
-
-  if (
-    contract.upgradeability.type === 'EIP1967 proxy' ||
-    contract.upgradeability.type === 'ZeppelinOS proxy' ||
-    contract.upgradeability.type === 'EIP897 proxy' ||
-    contract.upgradeability.type === 'call implementation proxy'
-  ) {
-    addresses.push(contract.upgradeability.implementation)
-    return addresses
-  }
-
-  if (contract.upgradeability.type === 'StarkWare proxy') {
-    addresses.push(contract.upgradeability.implementation)
-    if (contract.upgradeability.callImplementation) {
-      addresses.push(contract.upgradeability.callImplementation)
-    }
-    return addresses
-  }
-
-  if (contract.upgradeability.type === 'StarkWare diamond') {
-    addresses.push(contract.upgradeability.implementation)
-    addresses.push(...Object.values(contract.upgradeability.facets))
-    return addresses
-  }
-
-  if (contract.upgradeability.type === 'Arbitrum proxy') {
-    addresses.push(contract.upgradeability.adminImplementation)
-    addresses.push(contract.upgradeability.userImplementation)
-    return addresses
-  }
-
-  if (contract.upgradeability.type === 'new Arbitrum proxy') {
-    addresses.push(contract.upgradeability.implementation)
-    addresses.push(contract.upgradeability.adminImplementation)
-    addresses.push(contract.upgradeability.userImplementation)
-    return addresses
-  }
-
-  if (contract.upgradeability.type === 'resolved delegate proxy') {
-    addresses.push(contract.upgradeability.implementation)
-    return addresses
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-  if (contract.upgradeability.type === 'EIP2535 diamond proxy') {
-    addresses.push(...Object.values(contract.upgradeability.facets))
-    return addresses
-  }
-
-  throw new Error(
-    'Unhandled upgradeability type: ' + JSON.stringify(contract.upgradeability),
-  )
+  return result
 }
