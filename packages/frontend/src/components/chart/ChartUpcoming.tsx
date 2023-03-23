a57ec842c7d6d1add1bb10518c3dad9c5c0bd43f
Antoni Pawlak <antoni.pawlak@pm.me>
2023-03-23 11:02:51 +0100

    Update packages/frontend/src/components/chart/ChartUpcoming.tsx
    Co-authored-by: Michał Sobieraj-Jakubiec <michalsidzej@gmail.com>

--- packages/frontend/src/components/chart/ChartUpcoming.tsx
+++ packages/frontend/src/components/chart/ChartUpcoming.tsx
@@ -53,7 +53,7 @@ export function ChartUpcoming({ mobileFull: fullWidth = false }: ChartProps) {
             />
             <YAxisLabels className="blur-sm" />
             <div className="absolute top-0 z-50 flex  h-full w-full items-center justify-center">
-              <div className="flex h-[20%] w-[50%] items-center justify-center rounded-lg bg-gray-200 text-gray-700 dark:bg-gray-800 dark:text-gray-300">
+              <div className="flex h-[20%] w-1/2 items-center justify-center rounded-lg bg-gray-200 text-gray-700 dark:bg-gray-800 dark:text-gray-300">
                 Coming soon
               </div>
             </div>
