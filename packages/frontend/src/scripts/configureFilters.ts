a6e3e4eee062d109b6469791448204183c8b7ee7
antooni <antoni.pawlak@protonmail.com>
2023-03-23 15:06:59 +0100

    save filter state in local storage

--- packages/frontend/src/scripts/configureFilters.ts
+++ packages/frontend/src/scripts/configureFilters.ts
@@ -1,28 +1,39 @@
 export function configureFilters() {
-  const combinedCheckbox =
-    document.querySelector<HTMLInputElement>('#combined-bridges')
-  const archivedCheckbox =
-    document.querySelector<HTMLInputElement>('#archived-projects')
-  const upcomingCheckbox =
-    document.querySelector<HTMLInputElement>('#upcoming-rollups')
-
-  const archived = Array.from(document.querySelectorAll('[data-archived]'))
-  const upcoming = Array.from(document.querySelectorAll('[data-upcoming]'))
-  const L2s = Array.from(document.querySelectorAll('[data-layer2]'))
+  const archived = Array.from(
+    document.querySelectorAll<HTMLElement>('[data-archived]'),
+  )
+  const upcoming = Array.from(
+    document.querySelectorAll<HTMLElement>('[data-upcoming]'),
+  )
+  const L2s = Array.from(
+    document.querySelectorAll<HTMLElement>('[data-layer2]'),
+  )
   const bridgesOnlyCells = Array.from(
-    document.querySelectorAll('[data-bridges-only-cell]'),
+    document.querySelectorAll<HTMLElement>('[data-bridges-only-cell]'),
   )
   const combinedOnlyCells = Array.from(
-    document.querySelectorAll('[data-combined-only-cell]'),
+    document.querySelectorAll<HTMLElement>('[data-combined-only-cell]'),
   )
 
   const archivedBridges = archived.filter((x) => !L2s.includes(x))
   const archivedL2s = archived.filter((x) => L2s.includes(x))
   const activeL2s = L2s.filter((x) => !archivedL2s.includes(x))
 
+  const combinedCheckbox =
+    document.querySelector<HTMLInputElement>('#combined-bridges')
+  const archivedCheckbox = setupArchivedCheckbox(archived)
+  const upcomingCheckbox = setupUpcomingCheckbox(upcoming)
+
+  // this call ensures that the numbers are rendered correctly on page load
+  // this needs to be called after the checkboxes are set up
   renderNumbers()
 
   archivedCheckbox?.addEventListener('change', () => {
+    localStorage.setItem(
+      'l2beat-filter-archived',
+      archivedCheckbox.checked.toString(),
+    )
+
     const l2sVisible = !combinedCheckbox || combinedCheckbox.checked
 
     archivedL2s.forEach((x) => {
@@ -56,11 +67,50 @@ export function configureFilters() {
   })
 
   upcomingCheckbox?.addEventListener('change', () => {
+    localStorage.setItem(
+      'l2beat-filter-upcoming',
+      upcomingCheckbox.checked.toString(),
+    )
+
     upcoming.forEach((x) => {
       x.classList.toggle('hidden', !upcomingCheckbox.checked)
     })
     renderNumbers()
   })
+}
+
+function setupArchivedCheckbox(archived: HTMLElement[]) {
+  const saved = localStorage.getItem('l2beat-filter-archived')
+
+  const archivedCheckbox =
+    document.querySelector<HTMLInputElement>('#archived-projects')
+
+  if (archivedCheckbox) {
+    archivedCheckbox.checked = saved === 'true' ? true : false
+
+    archived.forEach((x) => {
+      x.classList.toggle('hidden', !archivedCheckbox.checked)
+    })
+  }
+
+  return archivedCheckbox
+}
+
+function setupUpcomingCheckbox(upcoming: HTMLElement[]) {
+  const saved = localStorage.getItem('l2beat-filter-upcoming')
+
+  const upcomingCheckbox =
+    document.querySelector<HTMLInputElement>('#upcoming-rollups')
+
+  if (upcomingCheckbox) {
+    upcomingCheckbox.checked = saved === 'true' ? true : false
+
+    upcoming.forEach((x) => {
+      x.classList.toggle('hidden', !upcomingCheckbox.checked)
+    })
+  }
+
+  return upcomingCheckbox
 }
 
 // this function is currently placed in the same file as configureFilters()
