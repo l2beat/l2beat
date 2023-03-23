879b3cfc2cea3156450ba33a5ff398bb9f0bbf56
Michal <michalsidzej@gmail.com>
2023-03-23 15:21:55 +0100

    Update arbitrum token circulating supply

--- packages/backend/src/core/reports/custom/arbitrum.ts
+++ packages/backend/src/core/reports/custom/arbitrum.ts
@@ -8,14 +8,15 @@ export const ARB_TOKEN_SINCE_TIMESTAMP = UnixTime.fromDate(
 )
 export const ARB_TOKEN_ID = AssetId('arbitrum')
 const ARB_TOKEN_DECIMALS = 18
-// This is the circulating supply of OP as given by Coingecko.
+// This is the circulating supply of ARB as given by Coingecko.
 // The value is obtained by looking at how many tokens have been designated
-// to be distributed in the Optimism's airdrop.
+// to be distributed in the Arbitrum's airdrop.
 // Our policy is to keep this value in sync with Coingecko.
-// https://www.coingecko.com/en/coins/optimism
-// https://optimistic.etherscan.io/token/0x4200000000000000000000000000000000000042?a=0x2a82ae142b2e62cb7d10b55e323acb1cab663a26
+// https://www.coingecko.com/en/coins/arbitrum
+// https://docs.arbitrum.foundation/airdrop-eligibility-distribution#initial-token-allocation--airdrop-distribution
+
 export const ARB_TOKEN_BALANCE =
-  214_748_364n * 10n ** BigInt(ARB_TOKEN_DECIMALS)
+  1_275_000_000n * 10n ** BigInt(ARB_TOKEN_DECIMALS)
 
 export const addArbTokenReport = createAddCustomTokenReport(
   ARB_TOKEN_ID,
