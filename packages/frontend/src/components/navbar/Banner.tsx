3011b7d00425d97646df39eaecb7f5fb774089fc
torztomasz <tomasz.torz@l2beat.com>
2023-03-25 01:15:06 +0100

    Change banner link to tally.xyz

--- packages/frontend/src/components/navbar/Banner.tsx
+++ packages/frontend/src/components/navbar/Banner.tsx
@@ -6,7 +6,7 @@ export function Banner() {
   return (
     <a
       className="flex items-center justify-center bg-blue-800 px-4 py-1.5 text-center text-xs font-medium text-white"
-      href="https://forum.arbitrum.foundation/t/delegate-application-template/31/934"
+      href="https://www.tally.xyz/profile/l2beatcom.eth?governanceId=eip155:42161:0xf07DeD9dC292157749B6Fd268E37DF6EA38395B9"
       target="_blank"
     >
       We're striving to become an Arbitrum DAO delegate.{' '}
