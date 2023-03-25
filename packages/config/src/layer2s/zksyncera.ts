a83601e0c16d49a863255182e7852df9d18fe3df
Michał Sobieraj-Jakubiec <michalsidzej@gmail.com>
2023-03-25 17:19:36 +0100

    Speedup zksyncera activity sync (#1330)

--- packages/config/src/layer2s/zksyncera.ts
+++ packages/config/src/layer2s/zksyncera.ts
@@ -69,6 +69,7 @@ export const zksyncera: Layer2 = {
       type: 'rpc',
       startBlock: 1,
       url: 'https://mainnet.era.zksync.io',
+      callsPerMinute: 1500,
       excludeFromActivityApi: true, // excluding until it's synced
     },
   },
