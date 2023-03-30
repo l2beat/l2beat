effbc82f8bf41f4dcab72e2382ae6500cf1338f3
antooni <antoni.pawlak@protonmail.com>
2023-03-30 09:53:43 +0200

    refactor getHardcoded

--- packages/backend/src/api/controllers/status/discovery/props/utils/getHardcoded.ts
+++ packages/backend/src/api/controllers/status/discovery/props/utils/getHardcoded.ts
@@ -3,6 +3,9 @@ import { readdirSync, readFileSync } from 'fs'
 const BRIDGES_PATH = '../config/src/bridges/'
 const LAYER2_PATH = '../config/src/layer2s/'
 
+const ETHEREUM_ADDRESS_REGEX = /0x[a-fA-F0-9]{40}/g
+const PROJECT_ID_REGEX = /ProjectId\('(.*)'\)/
+
 export function getHardcoded(): Record<string, number> {
   const hardcoded: Record<string, number> = {}
 
@@ -16,10 +19,10 @@ export function getHardcoded(): Record<string, number> {
     )
     .forEach((bridgeFile) => {
       const contents = readFileSync(`${BRIDGES_PATH}${bridgeFile}`, 'utf-8')
-      const count = [...contents.matchAll(/0x[a-fA-F0-9]{40}/g)].length
-      const id = contents.match(/ProjectId\('(.*)'\)/)
+      const count = [...contents.matchAll(ETHEREUM_ADDRESS_REGEX)].length
+      const id = contents.match(PROJECT_ID_REGEX)
 
-      if (id && id[1]) {
+      if (id?.[1]) {
         hardcoded[id[1]] = count
       }
     })
@@ -34,10 +37,10 @@ export function getHardcoded(): Record<string, number> {
     .forEach((layer2File) => {
       const contents = readFileSync(`${LAYER2_PATH}${layer2File}`, 'utf-8')
 
-      const count = [...contents.matchAll(/0x[a-fA-F0-9]{40}/g)].length
-      const id = contents.match(/ProjectId\('(.*)'\)/)
+      const count = [...contents.matchAll(ETHEREUM_ADDRESS_REGEX)].length
+      const id = contents.match(PROJECT_ID_REGEX)
 
-      if (id && id[1]) {
+      if (id?.[1]) {
         hardcoded[id[1]] = count
       }
     })
