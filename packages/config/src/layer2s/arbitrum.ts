20375e14d3ed4871735165ed43dbc8e8ffae064c
antooni <antoni.pawlak@protonmail.com>
2023-04-03 18:01:46 +0200

    remove dead import

--- packages/config/src/layer2s/arbitrum.ts
+++ packages/config/src/layer2s/arbitrum.ts
@@ -1,4 +1,4 @@
-import { EthereumAddress, ProjectId, UnixTime } from '@l2beat/shared'
+import { ProjectId, UnixTime } from '@l2beat/shared'
 
 import { ProjectDiscovery } from '../discovery/ProjectDiscovery'
 import {
