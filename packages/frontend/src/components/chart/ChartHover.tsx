ecbfecf045ece7a30448e2d8f556886d7fac5d4f
Michal <michalsidzej@gmail.com>
2023-04-03 13:26:42 +0200

    Update chart tooltip & control colors

--- packages/frontend/src/components/chart/ChartHover.tsx
+++ packages/frontend/src/components/chart/ChartHover.tsx
@@ -29,7 +29,7 @@ export function ChartHover() {
         data-role="chart-hover-contents"
         className={cx(
           'absolute z-41 rounded-lg py-2 px-3 text-right text-2xs md:py-3 md:px-4 md:text-xs',
-          'bg-gray-100 shadow-[0_4px_8px_rgba(0,0,0,0.25)] dark:bg-gray-800',
+          'bg-gray-100 shadow-[0_4px_8px_rgba(0,0,0,0.25)] dark:bg-gray-750',
           'select-none',
           'flex flex-col items-start justify-center',
         )}
