1a125b8317dcb71e92ef83e1f6bff9cf6e2f34f9
Michal <michalsidzej@gmail.com>
2023-04-05 12:56:31 +0200

    Fix sorare upgrade delay

--- packages/config/src/layer2s/sorare.ts
+++ packages/config/src/layer2s/sorare.ts
@@ -1,6 +1,12 @@
 import { EthereumAddress, ProjectId, UnixTime } from '@l2beat/shared'
 
 import { ProjectDiscovery } from '../discovery/ProjectDiscovery'
+import { getProxyGovernance } from '../discovery/starkware/getProxyGovernance'
+import {
+  delayDescriptionFromSeconds,
+  delayDescriptionFromString,
+} from '../utils/delayDescription'
+import { formatSeconds } from '../utils/formatSeconds'
 import {
   CONTRACTS,
   DATA_AVAILABILITY,
@@ -17,6 +23,12 @@ import {
 import { Layer2 } from './types'
 
 const discovery = new ProjectDiscovery('sorare')
+
+const delaySeconds = discovery.getContractUpgradeabilityParam(
+  'StarkExchange',
+  'upgradeDelay',
+)
+const delay = formatSeconds(delaySeconds)
 
 export const sorare: Layer2 = {
   type: 'layer2',
@@ -60,7 +72,7 @@ export const sorare: Layer2 = {
   riskView: makeBridgeCompatible({
     stateValidation: RISK_VIEW.STATE_ZKP_ST,
     dataAvailability: RISK_VIEW.DATA_EXTERNAL_DAC,
-    upgradeability: RISK_VIEW.UPGRADE_DELAY('14 days'),
+    upgradeability: RISK_VIEW.UPGRADE_DELAY(delay),
     sequencerFailure: RISK_VIEW.SEQUENCER_STARKEX_SPOT,
     validatorFailure: RISK_VIEW.VALIDATOR_ESCAPE_STARKEX_NFT,
     destinationToken: RISK_VIEW.CANONICAL,
@@ -78,18 +90,7 @@ export const sorare: Layer2 = {
   },
   contracts: {
     addresses: [
-      {
-        name: 'StarkExchange',
-        address: EthereumAddress('0xF5C9F957705bea56a7e806943f98F7777B995826'),
-        upgradeability: {
-          type: 'StarkWare proxy',
-          implementation: EthereumAddress(
-            '0xB8563AD5aF1F79dd04937BE8B572318c8e6f43AC',
-          ),
-          upgradeDelay: 1209600,
-          isFinal: false,
-        },
-      },
+      discovery.getMainContractDetails('StarkExchange'),
       {
         name: 'Committee',
         address: EthereumAddress('0x879cD57975d596004863D30c59d579ef78BBbe32'),
@@ -98,19 +99,15 @@ export const sorare: Layer2 = {
       },
       SHARP_VERIFIER_CONTRACT,
     ],
-    risks: [CONTRACTS.UPGRADE_WITH_DELAY_RISK('14 days')],
+    risks: [CONTRACTS.UPGRADE_WITH_DELAY_RISK(delay)],
   },
   permissions: [
     {
       name: 'Governors',
-      accounts: discovery
-        .getContractValue<string[]>('StarkExchange', 'GOVERNORS')
-        .map((governor) => ({
-          address: EthereumAddress(governor),
-          type: 'EOA',
-        })),
+      accounts: getProxyGovernance(discovery, 'StarkExchange'),
       description:
-        'Can upgrade implementation of the system, potentially gaining access to all funds stored in the bridge. Currently there is no delay before the upgrade, so the users will not have time to migrate.',
+        'Can upgrade implementation of the system, potentially gaining access to all funds stored in the bridge. ' +
+        delayDescriptionFromString(delay),
     },
     {
       name: 'Data Availability Committee',
@@ -154,16 +151,17 @@ export const sorare: Layer2 = {
         },
       ],
       description:
-        'Can upgrade implementation of SHARP Verifier, potentially with code approving fraudulent state. Currently there is no delay before the upgrade, so the users will not have time to migrate.',
+        'Can upgrade implementation of SHARP Verifier, potentially with code approving fraudulent state. ' +
+        // @todo
+        // This should be coming from discovery, but it's not available yet.
+        // because sorare discovery is not detecting the starkware diamond
+        delayDescriptionFromSeconds(2419200),
     },
     {
       name: 'Operators',
       accounts: discovery
         .getContractValue<string[]>('StarkExchange', 'OPERATORS')
-        .map((operator) => ({
-          address: EthereumAddress(operator),
-          type: 'EOA',
-        })),
+        .map(discovery.formatPermissionedAccount.bind(discovery)),
       description:
         'Allowed to update state of the system. When Operator is down the state cannot be updated.',
     },
