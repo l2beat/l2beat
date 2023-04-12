2e66063ba3eb366eb73373be7448c12688f2f762
antooni <antoni.pawlak@protonmail.com>
2023-04-12 10:00:20 +0200

    move gatherAddresses to shared

---
+++ packages/shared/src/utils/gatherAddressesFromUpgradeability.ts
@@ -0,0 +1,50 @@
+// TODO: Move to shared
+
+import { assertUnreachable } from '../tools'
+import { EthereumAddress, UpgradeabilityParameters } from '../types'
+
+export function gatherAddressesFromUpgradeability(
+  item: UpgradeabilityParameters,
+): EthereumAddress[] {
+  const result: EthereumAddress[] = []
+
+  switch (item.type) {
+    case 'EIP1967 proxy':
+    case 'ZeppelinOS proxy':
+    case 'StarkWare diamond':
+    case 'resolved delegate proxy':
+    case 'call implementation proxy':
+    case 'EIP897 proxy':
+      result.push(item.implementation)
+      break
+    case 'StarkWare proxy':
+      result.push(item.implementation)
+      if (item.callImplementation) {
+        result.push(item.callImplementation)
+      }
+      break
+    case 'Arbitrum proxy':
+      result.push(item.adminImplementation)
+      result.push(item.userImplementation)
+      break
+    case 'new Arbitrum proxy':
+      result.push(item.adminImplementation)
+      result.push(item.userImplementation)
+      result.push(item.implementation)
+      break
+    case 'gnosis safe':
+      result.push(item.masterCopy)
+      break
+    case 'EIP2535 diamond proxy':
+      result.push(...item.facets)
+      break
+    case 'immutable':
+      // Ignoring types because no (admin/user)implementation included in them
+      break
+    default:
+      // This code triggers a typescript compile-time error if not all cases have been covered
+      assertUnreachable(item)
+  }
+
+  return result
+}
