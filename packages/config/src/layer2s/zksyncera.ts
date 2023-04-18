33717bce8376077e22cc027d9701c7858147f2ad
antooni <antoni.pawlak@protonmail.com>
2023-04-18 15:50:27 +0200

    update risks

--- packages/config/src/layer2s/zksyncera.ts
+++ packages/config/src/layer2s/zksyncera.ts
@@ -78,7 +78,8 @@ export const zksyncera: Layer2 = {
   },
   riskView: makeBridgeCompatible({
     stateValidation: {
-      ...RISK_VIEW.STATE_ZKP_SN,
+      value: 'ZK proofs',
+      description: 'todo',
       references: [
         'https://etherscan.io/address/0x3dB52cE065f728011Ac6732222270b3F2360d919#code#F5#L89',
         'https://etherscan.io/address/0x389a081BCf20e5803288183b929F08458F1d863D#code#F10#L254',
@@ -90,9 +91,9 @@ export const zksyncera: Layer2 = {
       contracts: ['ValidatorTimelock', 'DiamondProxy', 'Verifier'],
     },
     dataAvailability: {
-      ...RISK_VIEW.DATA_ON_CHAIN,
+      value: 'On chain (SD)',
       description:
-        'All of the data (state diffs) needed for proof construction is published on chain.',
+        'All of the data (SD = state diffs) needed for proof construction is published on chain.',
       references: [
         'https://etherscan.io/address/0x3dB52cE065f728011Ac6732222270b3F2360d919#code#F5#L71',
         'https://etherscan.io/address/0x389a081BCf20e5803288183b929F08458F1d863D#code#F10#L149',
@@ -103,7 +104,7 @@ export const zksyncera: Layer2 = {
       contracts: ['ValidatorTimelock', 'DiamondProxy'],
     },
     upgradeability: {
-      ...VALUES.ZKSYNC_2.UPGRADEABILITY_STRING,
+      ...VALUES.ZKSYNC_2.getUpgradeabilityRisk(),
       references: [
         'https://etherscan.io/address/0x2a2d6010202B93E727b61a60dfC1d5CF2707c1CE#code#F8#L121',
         'https://etherscan.io/address/0x2a2d6010202B93E727b61a60dfC1d5CF2707c1CE#code#F6#L51',
@@ -111,7 +112,7 @@ export const zksyncera: Layer2 = {
       contracts: ['DiamondProxy'],
     },
     sequencerFailure: {
-      ...RISK_VIEW.SEQUENCER_TRANSACT_L1,
+      value: 'Transact using L1',
       description:
         'L2 transactions can be forced through L1 by adding them to append only queue on L1, which is processed sequentially by Sequencer, meaning that the individual user cannot be censored. At the moment there is no mechanism that forces L2 Sequencer to empty the L1 queue.',
       sentiment: 'warning',
@@ -124,8 +125,9 @@ export const zksyncera: Layer2 = {
       contracts: ['DiamondProxy'],
     },
     validatorFailure: {
-      value: 'In development',
-      description: 'Currently, there is no generic escape hatch.',
+      value: 'No mechanism',
+      description:
+        'Only whitelisted validators can update the state on L1, so in the event of failure the withdrawals are blocked. ',
       sentiment: 'bad',
     },
     destinationToken: RISK_VIEW.NATIVE_AND_CANONICAL(),
@@ -243,6 +245,7 @@ export const zksyncera: Layer2 = {
       description:
         'This actor is allowed to propose, revert and execute L2 blocks on L1.',
     },
+    VALUES.ZKSYNC_2.getSecurityCouncil(),
   ],
   milestones: [
     {
