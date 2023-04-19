17da02135f4bc6262ac0c2f1b09b7d8bf86a0332
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2023-04-19 12:58:09 +0200

    Add tests for AddressAnalyzer

--- packages/backend/src/core/discovery/handlers/getValuesAndErrors.test.ts
+++ packages/backend/src/core/discovery/handlers/getValuesAndErrors.test.ts
@@ -31,4 +31,36 @@ describe(getValuesAndErrors.name, () => {
       },
     })
   })
+
+  it('returns only errors', () => {
+    expect(
+      getValuesAndErrors([
+        { field: 'd', error: 'Error 1' },
+        { field: 'e', error: 'Error 2' },
+      ]),
+    ).toEqual({
+      values: undefined,
+      errors: {
+        d: 'Error 1',
+        e: 'Error 2',
+      },
+    })
+  })
+
+  it('returns only values', () => {
+    expect(
+      getValuesAndErrors([
+        { field: 'a', value: 1 },
+        { field: 'b', value: 2 },
+        { field: 'c', value: 3 },
+      ]),
+    ).toEqual({
+      values: {
+        a: 1,
+        b: 2,
+        c: 3,
+      },
+      errors: undefined,
+    })
+  })
 })
