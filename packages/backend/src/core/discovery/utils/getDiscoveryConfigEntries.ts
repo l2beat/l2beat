a1a2efa6e4388523810913ef0df0dd9c4c1ae577
Michal <michalsidzej@gmail.com>
2023-04-20 13:06:53 +0200

    Fix sanity check

--- packages/backend/src/core/discovery/utils/getDiscoveryConfigEntries.ts
+++ packages/backend/src/core/discovery/utils/getDiscoveryConfigEntries.ts
@@ -15,6 +15,7 @@ export function getDiscoveryConfigEntries(
 const MAX_SEMANTIC_NEST_LEVEL = 2
 
 function deepSortByKeys(object: Record<string, unknown>, nestLevel = 0) {
+  object = { ...object }
   return Object.keys(object)
     .sort()
     .reduce(function (acc: Record<string, unknown>, key) {
@@ -22,7 +23,8 @@ function deepSortByKeys(object: Record<string, unknown>, nestLevel = 0) {
 
       if (isObject(entry)) {
         if (isArray(entry)) {
-          acc[key] = entry.sort()
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+          acc[key] = [...entry].sort()
         } else {
           if (nestLevel >= MAX_SEMANTIC_NEST_LEVEL) {
             acc[key] = entry
