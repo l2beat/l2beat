2ecd377662103b6ebecd3f544b3408a8848e82eb
torztomasz <tomasz.torz@l2beat.com>
2023-04-26 09:39:32 +0200

    change key in ProjectDetails

--- packages/frontend/src/pages/scaling-projects/view/ProjectDetails.tsx
+++ packages/frontend/src/pages/scaling-projects/view/ProjectDetails.tsx
@@ -25,28 +25,34 @@ export interface ProjectDetailsProps {
 export function ProjectDetails(props: ProjectDetailsProps) {
   return (
     <div className="px-4 md:px-0">
-      {props.items.map((item) => {
+      {props.items.map((item, index) => {
         switch (item.type) {
           case 'ChartSection':
-            return <ChartSection key={item.type} {...item.props} />
+            return <ChartSection key={item.props.id} {...item.props} />
           case 'MilestonesSection':
-            return <MilestonesSection key={item.type} {...item.props} />
+            return <MilestonesSection key={item.props.id} {...item.props} />
           case 'KnowledgeNuggetsSection':
-            return <KnowledgeNuggetsSection key={item.type} {...item.props} />
+            return (
+              <KnowledgeNuggetsSection key={item.props.id} {...item.props} />
+            )
           case 'DescriptionSection':
-            return <DescriptionSection key={item.type} {...item.props} />
+            return <DescriptionSection key={item.props.id} {...item.props} />
           case 'RiskAnalysisSection':
-            return <RiskAnalysis key={item.type} {...item.props} />
+            return <RiskAnalysis key={item.props.id} {...item.props} />
           case 'TechnologyIncompleteNote':
-            return <TechnologyIncomplete key={item.type} {...item.props} />
+            return (
+              <TechnologyIncomplete key={item.type + index} {...item.props} />
+            )
           case 'TechnologySection':
-            return <TechnologySection key={item.type} {...item.props} />
+            return <TechnologySection key={item.props.id} {...item.props} />
           case 'PermissionsSection':
-            return <PermissionsSection key={item.type} {...item.props} />
+            return <PermissionsSection key={item.props.id} {...item.props} />
           case 'ContractsSection':
-            return <ContractsSection key={item.type} {...item.props} />
+            return <ContractsSection key={item.props.id} {...item.props} />
           case 'UpcomingDisclaimer':
-            return <UpcomingDisclaimer key={item.type} className="mt-6" />
+            return (
+              <UpcomingDisclaimer key={item.type + index} className="mt-6" />
+            )
           default:
             assertUnreachable(item)
         }
