9727badb2d1f669db047c1b9544ec1c3e133c6e3
torztomasz <tomasz.torz@l2beat.com>
2023-05-04 12:35:28 +0200

    use 999 z index for mobile side menu

--- packages/frontend/tailwind.config.js
+++ packages/frontend/tailwind.config.js
@@ -108,9 +108,10 @@ module.exports = {
       10: '10', // Hoverable dropdown menu, Chart logo and Y axis, Borders (TvlActivityToggle and DesktopTabs)
       20: '20', // Chart canvas and loader, Items (DesktopTabs, TvlActivityToggle)
       30: '30', // Chart hover line
-      40: '40', // Mobile project navigation, Mobile side menu shadow, Milestones, Chart hover line point (squares and circles)
+      40: '40', // Mobile project navigation, Milestones, Chart hover line point (squares and circles)
       41: '41', // Chart hover content
-      50: '50', // Tooltip, Chart "Coming soon" disclaimer, Mobile side menu
+      50: '50', // Tooltip, Chart "Coming soon" disclaimer
+      999: '999', // Mobile side menu
     },
     extend: {
       fontFamily: {
