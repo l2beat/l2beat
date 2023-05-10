7881482c117f72b603ddb13b883f72cda84fe86d
torztomasz <tomasz.torz@l2beat.com>
2023-05-10 11:05:50 +0200

    add possibility to add icon to tab

--- packages/frontend/src/components/TabNavigation.tsx
+++ packages/frontend/src/components/TabNavigation.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, ReactNode } from 'react'
 import { renderToString } from 'react-dom/server'
 
 import { HorizontalSeparator } from './HorizontalSeparator'
@@ -11,21 +11,23 @@ interface Tab {
   id: string
   name: string
   content: ReactElement
+  icon?: ReactNode
 }
 
 export function TabNavigation({ tabs }: TabsProps) {
   return (
     <div className="TabNavigation">
       <div className="relative">
-        <div className="TabNavigationTabsContainer space-x-8">
+        <div className="TabNavigationTabsContainer flex space-x-8">
           {tabs.map((tab) => (
             <a
-              className="TabNavigationTab group relative inline-block py-3 px-1 text-base font-semibold"
+              className="TabNavigationTab group relative flex items-center py-3 px-1 text-base font-semibold"
               key={tab.id}
               href={`#${tab.id}`}
               data-content={renderToString(tab.content)}
             >
-              {tab.name}
+              {tab.icon && <span className="mr-1.5">{tab.icon}</span>}
+              <span>{tab.name}</span>
               <span className="absolute bottom-0 left-0 block h-1 w-full rounded-t-sm bg-gray-850 opacity-0 transition-all duration-300 group-hover:opacity-80" />
             </a>
           ))}
