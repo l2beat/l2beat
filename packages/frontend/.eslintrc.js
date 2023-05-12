b4bf4f3ace44a8f7c836ef86c734a10183a77e09
Michal <michalsidzej@gmail.com>
2023-05-12 09:18:33 +0200

    Add a rule to ban imports from @l2beat/services inside frontend

--- packages/frontend/.eslintrc.js
+++ packages/frontend/.eslintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
   extends: '../../.eslintrc.json',
+  overrides: [
+    {
+      files: ['*'],
+      excludedFiles: ['**/build/**/*.ts'],
+      rules: {
+        'no-restricted-imports': ['error', { paths: ['@l2beat/services'] }],
+      },
+    },
+  ],
 }
