24e2146a9ef4ebac6d26e69f5866ea63af96a918
Michal <michalsidzej@gmail.com>
2023-05-15 07:15:05 +0200

    Extract aligning logic

--- packages/frontend/src/build/createApi.test.ts
+++ packages/frontend/src/build/createApi.test.ts
@@ -1,159 +1,109 @@
 import { ActivityApiChart, UnixTime } from '@l2beat/shared'
 import { expect } from 'earl'
 
-import { getActivityChart } from './createApi'
+import { alignActivityData } from './createApi'
 
 const lastDay = UnixTime.now().toStartOf('day')
 
-describe(getActivityChart.name, () => {
+describe(alignActivityData.name, () => {
   it('should return the correct chart', () => {
-    const apiChart: ActivityApiChart = {
-      types: ['timestamp', 'daily tx count'],
-      data: [
+    const apiChartData: ActivityApiChart['data'] = [
+      [lastDay.add(-2, 'days'), 1],
+      [lastDay.add(-1, 'days'), 2],
+      [lastDay, 3],
+    ]
+
+    const ethChartData: ActivityApiChart['data'] = [
+      [lastDay.add(-2, 'days'), 4],
+      [lastDay.add(-1, 'days'), 5],
+      [lastDay, 6],
+    ]
+
+    const result = alignActivityData(apiChartData, ethChartData)
+
+    expect(result).toEqual([
+      [lastDay.add(-2, 'days').toNumber(), 1, 4],
+      [lastDay.add(-1, 'days').toNumber(), 2, 5],
+      [lastDay.toNumber(), 3, 6],
+    ])
+  })
+
+  describe('more eth data than layer2 data', () => {
+    it('cut front', () => {
+      const apiChartData: ActivityApiChart['data'] = [
         [lastDay.add(-2, 'days'), 1],
         [lastDay.add(-1, 'days'), 2],
-        [lastDay, 3],
-      ],
-    }
+      ]
 
-    const ethChart: ActivityApiChart = {
-      types: ['timestamp', 'daily tx count'],
-      data: [
+      const ethChartData: ActivityApiChart['data'] = [
         [lastDay.add(-2, 'days'), 4],
         [lastDay.add(-1, 'days'), 5],
         [lastDay, 6],
-      ],
-    }
+      ]
 
-    const result = getActivityChart(apiChart, ethChart)
+      const result = alignActivityData(apiChartData, ethChartData)
 
-    expect(result).toEqual({
-      daily: {
-        types: ['timestamp', 'transactions', 'ethereumTransactions'],
-        data: [
-          [lastDay.add(-2, 'days').toNumber(), 1, 4],
-          [lastDay.add(-1, 'days').toNumber(), 2, 5],
-          [lastDay.toNumber(), 3, 6],
-        ],
-      },
-    })
-  })
-
-  describe('should align eth chart to api chart', () => {
-    it('cut front', () => {
-      const apiChart: ActivityApiChart = {
-        types: ['timestamp', 'daily tx count'],
-        data: [
-          [lastDay.add(-2, 'days'), 1],
-          [lastDay.add(-1, 'days'), 2],
-        ],
-      }
-
-      const ethChart: ActivityApiChart = {
-        types: ['timestamp', 'daily tx count'],
-        data: [
-          [lastDay.add(-2, 'days'), 4],
-          [lastDay.add(-1, 'days'), 5],
-          [lastDay, 6],
-        ],
-      }
-
-      const result = getActivityChart(apiChart, ethChart)
-
-      expect(result).toEqual({
-        daily: {
-          types: ['timestamp', 'transactions', 'ethereumTransactions'],
-          data: [
-            [lastDay.add(-2, 'days').toNumber(), 1, 4],
-            [lastDay.add(-1, 'days').toNumber(), 2, 5],
-          ],
-        },
-      })
+      expect(result).toEqual([
+        [lastDay.add(-2, 'days').toNumber(), 1, 4],
+        [lastDay.add(-1, 'days').toNumber(), 2, 5],
+      ])
     })
 
     it('cut back', () => {
-      const apiChart: ActivityApiChart = {
-        types: ['timestamp', 'daily tx count'],
-        data: [
-          [lastDay.add(-1, 'days'), 2],
-          [lastDay, 3],
-        ],
-      }
+      const apiChartData: ActivityApiChart['data'] = [
+        [lastDay.add(-1, 'days'), 2],
+        [lastDay, 3],
+      ]
 
-      const ethChart: ActivityApiChart = {
-        types: ['timestamp', 'daily tx count'],
-        data: [
-          [lastDay.add(-2, 'days'), 4],
-          [lastDay.add(-1, 'days'), 5],
-          [lastDay, 6],
-        ],
-      }
+      const ethChartData: ActivityApiChart['data'] = [
+        [lastDay.add(-2, 'days'), 4],
+        [lastDay.add(-1, 'days'), 5],
+        [lastDay, 6],
+      ]
 
-      const result = getActivityChart(apiChart, ethChart)
+      const result = alignActivityData(apiChartData, ethChartData)
 
-      expect(result).toEqual({
-        daily: {
-          types: ['timestamp', 'transactions', 'ethereumTransactions'],
-          data: [
-            [lastDay.add(-1, 'days').toNumber(), 2, 5],
-            [lastDay.toNumber(), 3, 6],
-          ],
-        },
-      })
+      expect(result).toEqual([
+        [lastDay.add(-1, 'days').toNumber(), 2, 5],
+        [lastDay.toNumber(), 3, 6],
+      ])
     })
   })
 
-  describe('if eth data is not present', () => {
+  describe('more layer2 data than eth data', () => {
     it('throw when eth delayed', () => {
-      const apiChart: ActivityApiChart = {
-        types: ['timestamp', 'daily tx count'],
-        data: [
-          [lastDay.add(-2, 'days'), 1],
-          [lastDay.add(-1, 'days'), 2],
-          [lastDay, 3],
-        ],
-      }
+      const apiChartData: ActivityApiChart['data'] = [
+        [lastDay.add(-2, 'days'), 1],
+        [lastDay.add(-1, 'days'), 2],
+        [lastDay, 3],
+      ]
 
-      const ethChart: ActivityApiChart = {
-        types: ['timestamp', 'daily tx count'],
-        data: [
-          [lastDay.add(-2, 'days'), 4],
-          [lastDay.add(-1, 'days'), 5],
-        ],
-      }
+      const ethChartData: ActivityApiChart['data'] = [
+        [lastDay.add(-2, 'days'), 4],
+        [lastDay.add(-1, 'days'), 5],
+      ]
 
-      expect(() => getActivityChart(apiChart, ethChart)).toThrow()
+      expect(() => alignActivityData(apiChartData, ethChartData)).toThrow()
     })
 
     it('cut back', () => {
-      const apiChart: ActivityApiChart = {
-        types: ['timestamp', 'daily tx count'],
-        data: [
-          [lastDay.add(-2, 'days'), 1],
-          [lastDay.add(-1, 'days'), 2],
-          [lastDay, 3],
-        ],
-      }
+      const apiChartData: ActivityApiChart['data'] = [
+        [lastDay.add(-2, 'days'), 1],
+        [lastDay.add(-1, 'days'), 2],
+        [lastDay, 3],
+      ]
 
-      const ethChart: ActivityApiChart = {
-        types: ['timestamp', 'daily tx count'],
-        data: [
-          [lastDay.add(-1, 'days'), 5],
-          [lastDay, 6],
-        ],
-      }
+      const ethChartData: ActivityApiChart['data'] = [
+        [lastDay.add(-1, 'days'), 5],
+        [lastDay, 6],
+      ]
 
-      const result = getActivityChart(apiChart, ethChart)
+      const result = alignActivityData(apiChartData, ethChartData)
 
-      expect(result).toEqual({
-        daily: {
-          types: ['timestamp', 'transactions', 'ethereumTransactions'],
-          data: [
-            [lastDay.add(-1, 'days').toNumber(), 2, 5],
-            [lastDay.toNumber(), 3, 6],
-          ],
-        },
-      })
+      expect(result).toEqual([
+        [lastDay.add(-1, 'days').toNumber(), 2, 5],
+        [lastDay.toNumber(), 3, 6],
+      ])
     })
   })
 })
