2dae297eef1309021c9aef120e8d5a13b9600cdc
Kris Kaczor <krzkaczor@gmail.com>
2023-05-16 20:02:33 +0400

    Avoid exiting the app too early (#1483)

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -3,17 +3,15 @@ import { handleCli } from './cli/handleCli'
 import { getConfig } from './config'
 import { flushErrors, reportError } from './tools/ErrorReporter'
 
-main()
-  .catch((e) => {
-    console.error(e)
-    reportError(e)
+main().catch((e) => {
+  console.error(e)
+  reportError(e)
 
-    // Need to flush errors to sentry before exiting otherwise they will be lost
-    return flushErrors()
-  })
-  .finally(() => {
+  // Need to flush errors to sentry before exiting otherwise they will be lost
+  return flushErrors().finally(() => {
     process.exit(1)
   })
+})
 
 async function main() {
   const cli = handleCli()
