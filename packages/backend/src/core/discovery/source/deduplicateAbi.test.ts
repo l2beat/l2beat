f93964ca8408e0b02f90895053c51398d6a70383
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2023-05-16 16:24:12 +0200

    Fix deduplication of view and pure functions (#1480)

--- packages/backend/src/core/discovery/source/deduplicateAbi.test.ts
+++ packages/backend/src/core/discovery/source/deduplicateAbi.test.ts
@@ -27,9 +27,37 @@ describe(deduplicateAbi.name, () => {
     expect(result).toEqual(['constructor(uint a, uint b)'])
   })
 
+  it('only keeps the first function', () => {
+    const result = deduplicateAbi([
+      'function foo(uint a, uint b)',
+      'function foo(uint c, uint d)',
+    ])
+
+    expect(result).toEqual(['function foo(uint a, uint b)'])
+  })
+
   it('filters out invalid strings', () => {
     const result = deduplicateAbi(['function foo()', 'foo bar baz', '12345'])
 
     expect(result).toEqual(['function foo()'])
+  })
+
+  it('prefers view or pure functions even if they are late', () => {
+    // We added this because we care more about view or pure functions
+    // Some proxies defined similar functions but skipped the modifiers
+
+    const result = deduplicateAbi([
+      'function foo(uint a, uint b)',
+      'function foo(uint c, uint d) view returns (uint)',
+      'function foo(uint e, uint f) pure returns (uint)',
+      'function bar(uint a, uint b)',
+      'function bar(uint c, uint d) pure returns (uint)',
+      'function bar(uint e, uint f) view returns (uint)',
+    ])
+
+    expect(result).toEqual([
+      'function bar(uint e, uint f) view returns (uint)',
+      'function foo(uint e, uint f) pure returns (uint)',
+    ])
   })
 })
