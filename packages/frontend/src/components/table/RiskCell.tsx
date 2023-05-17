d3dd31b8d57a4680deed211ba19801bc4bc30912
Michal <michalsidzej@gmail.com>
2023-05-17 20:58:49 +0200

    Remove badge from RiskCell

--- packages/frontend/src/components/table/RiskCell.tsx
+++ packages/frontend/src/components/table/RiskCell.tsx
@@ -2,7 +2,7 @@ import { ProjectRiskViewEntry } from '@l2beat/config'
 import cx from 'classnames'
 import React from 'react'
 
-import { Badge } from '../badge/Badge'
+import { sentimentToTextColor } from '../../utils/risks/color'
 import { UpcomingBadge } from '../badge/UpcomingBadge'
 import { NoInfoCell } from './NoInfoCell'
 
@@ -25,23 +25,9 @@ export function RiskCell({ item }: Props) {
       className={cx(item.description !== '' && 'Tooltip')}
       title={item.description !== '' ? item.description : undefined}
     >
-      {item.sentiment !== 'bad' && item.sentiment !== 'warning' ? (
-        <span
-          className={cx(item.description !== '' && 'Tooltip')}
-          title={item.description !== '' ? item.description : undefined}
-        >
-          {item.value}
-        </span>
-      ) : (
-        <Badge
-          className={cx(item.description !== '' && 'Tooltip')}
-          type={item.sentiment === 'bad' ? 'error' : 'warning'}
-          title={item.description !== '' ? item.description : undefined}
-          oneSize
-        >
-          {item.value}
-        </Badge>
-      )}
+      <span className={cx('font-medium', sentimentToTextColor(item.sentiment))}>
+        {item.value}
+      </span>
     </div>
   )
 }
