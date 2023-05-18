92f61b24548c7b98143cd10295670414aba6cdd5
Michal <michalsidzej@gmail.com>
2023-05-18 10:25:20 +0200

    Add Eternal Storage proxy

--- packages/config/src/bridges/pulseChain.ts
+++ packages/config/src/bridges/pulseChain.ts
@@ -2,6 +2,7 @@ import { ProjectId, UnixTime } from '@l2beat/shared'
 
 import { ProjectDiscovery } from '../discovery/ProjectDiscovery'
 import { Bridge } from './types'
+import { RISK_VIEW } from './common'
 
 const discovery = new ProjectDiscovery('pulseChain')
 
@@ -18,15 +19,11 @@ export const pulseChain: Bridge = {
   },
   config: {
     escrows: [
-      // not using discovery.getEscrowDetails as we do not support the proxy here yet.
-      {
-        address: discovery.getAddressFromValue(
-          'WETHOmnibridgeRouter',
-          'bridge',
-        ),
+      discovery.getEscrowDetails({
+        identifier: 'Escrow',
         sinceTimestamp: new UnixTime(1684137600),
         tokens: '*',
-      },
+      }),
     ],
   },
   technology: {
