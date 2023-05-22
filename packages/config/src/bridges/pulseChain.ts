87d418b73e88ad922144afde9d7a9ff1bcf5b23f
Bartek Kiepuszewski <bkiepuszewski@gmail.com>
2023-05-22 15:21:58 +0200

    added risks

--- packages/config/src/bridges/pulseChain.ts
+++ packages/config/src/bridges/pulseChain.ts
@@ -7,15 +7,33 @@ import { Bridge } from './types'
 
 const discovery = new ProjectDiscovery('pulseChain')
 
+const reqNumberOfSigs = discovery.getContractValue<number>(
+  'BridgeValidators',
+  'requiredSignatures',
+)
+
+const numOfValidators = discovery.getContractValue<number>(
+  'BridgeValidators',
+  'validatorCount',
+)
+
 export const pulseChain: Bridge = {
   type: 'bridge',
   id: ProjectId('pulseChain'),
   display: {
     name: 'PulseChain',
     slug: 'pulsechain',
-    description: '',
+    description:
+      'Bridge used to transfer assets from Ethereum to PulseChain. Transfers are validated by set of trusted Validators.',
     links: {
-      websites: ['https://pulsechain.com/'],
+      websites: [
+        'https://pulsechain.com/',
+        'https://bridge.pulsechain.com/#/bridge',
+      ],
+      socialMedia: [
+        'https://twitter.com/PulseChainCom',
+        'https://t.me/PulseChainCom',
+      ],
     },
   },
   config: {
@@ -31,6 +49,35 @@ export const pulseChain: Bridge = {
     // not sure about this
     category: 'Token Bridge',
     destination: ['PulseChain'],
+    validation: {
+      name: 'Validation',
+      description:
+        'PulseChain Bridge stores a number of Validators which need to sign off every message coming from the PulseChain. Once quorum of signatures is \
+        reached, message (e.g. withdrawal request) can be relayed to a destination contract',
+      references: [],
+      risks: [
+        {
+          category: 'Users can be censored if',
+          text: 'validators decide to not pass selected messages between chains.',
+          isCritical: true,
+        },
+        {
+          category: 'Funds can be stolen if',
+          text: 'validators relay a fake message to a destination chain to mint more tokens than there are locked on Ethereum thus preventing some existing holders from being able to bring their funds back to Ethereum.',
+          isCritical: true,
+        },
+        {
+          category: 'Funds can be stolen if',
+          text: 'validators relay a fake message to Ethereum chain allowing a user to withdraw tokens from Ethereum escrow when equivalent amount of tokens has not been deposited and burned on destination chain.',
+          isCritical: true,
+        },
+        {
+          category: 'Funds can be frozen if',
+          text: "validators don't relay messages between chains.",
+        },
+      ],
+      isIncomplete: true,
+    },
   },
   riskView: {
     validatedBy: {
@@ -68,4 +115,38 @@ export const pulseChain: Bridge = {
     ],
     risks: [CONTRACTS.UPGRADE_NO_DELAY_RISK],
   },
+  permissions: [
+    {
+      name: 'Validators',
+      description: `Permissioned set of validators that can sign off any arbitrary message from PulseChain including withdrawal request. ${reqNumberOfSigs} / ${numOfValidators} signatures\
+        are required.`,
+      accounts: discovery.getPermissionedAccountsList(
+        'BridgeValidators',
+        'validatorList',
+      ),
+    },
+    {
+      name: 'Owner of Validators contract',
+      description:
+        'Owner of Validators contract keeping a list of current Validators. Can add/remove Validators.',
+      accounts: [
+        discovery.formatPermissionedAccount(
+          discovery.getContractValue<string>('BridgeValidators', 'owner'),
+        ),
+      ],
+    },
+    {
+      name: 'Upgradeability Owner of main bridge contract',
+      description:
+        'Owner of the main bridge contract, able to upgrade the contract with no notice.',
+      accounts: [
+        discovery.formatPermissionedAccount(
+          discovery.getContractValue<string>(
+            'ForeignOmnibridge',
+            'upgradeabilityOwner',
+          ),
+        ),
+      ],
+    },
+  ],
 }
