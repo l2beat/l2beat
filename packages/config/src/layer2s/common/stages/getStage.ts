2b86958c17d84d0bc8d9ae4817d55d76b35b0690
antooni <antoni.pawlak@protonmail.com>
2023-06-06 13:13:02 +0200

    update stages requirements

--- packages/config/src/layer2s/common/stages/getStage.ts
+++ packages/config/src/layer2s/common/stages/getStage.ts
@@ -22,6 +22,22 @@ export const getStage = createGetStage({
         positive: 'There exists a rollup node for the project.',
         negative: 'There is no rollup node for the project.',
       },
+      nodeSoftwareProgram: {
+        positive: 'The rollup node is a software program.',
+        negative: 'The rollup node is not a software program.',
+      },
+      nodeOpenSource: {
+        positive: 'The rollup node source code is open source.',
+        negative: 'The rollup node source code is not open source.',
+      },
+      nodeComputesStateBasedOnL1: {
+        positive: 'Computes the L2 state based on Ethereum L1 data.',
+        negative: 'Does not compute the L2 state based on Ethereum L1 data.',
+      },
+      nodeDetectsDisagreement: {
+        positive: 'Detects when L2 state disagrees with Ethereum L1.',
+        negative: 'Does not detect when L2 state disagrees with Ethereum L1.',
+      },
     },
   },
   stage1: {
@@ -77,17 +93,35 @@ export const getStage = createGetStage({
         negative:
           'Users cannot exit the system without cooperation from Operators.',
       },
-      isSecurityCouncil: {
-        positive: 'The project has Security Council passing the requirements.',
+      securityCouncilMultisig: {
+        positive: 'The security council is an onchain multisig.',
+        negative: 'The security council is not an onchain multisig.',
+      },
+      securityCouncilAtLeast8: {
+        positive: 'The security council has at least 8 members.',
+        negative: 'The security council has less than 8 members.',
+      },
+      securityCouncilMultisigThreshold: {
+        positive: 'Requires at least 75% threshold for reaching consensus.',
+        negative: 'Requires less than 75% threshold for reaching consensus.',
+      },
+      securityCouncilMembersOutsideOrganization: {
+        positive:
+          'At least 50% of the security council members are outside the organization.',
         negative:
-          'The project does not have Security Council passing the requirements.',
+          'Less than 50% of the security council members are outside the organization.',
+      },
+      securityCouncilMembersOutsideOrganizationPseudonymous: {
+        positive:
+          'The outside members are publicly known, but can be pseudonymous.',
+        negative: 'The outside members are not publicly known.',
       },
     },
   },
   stage2: {
     name: 'Stage 2',
     items: {
-      proofSystemOverridenOnlyInCaseOfABug: {
+      proofSystemOverriddenOnlyInCaseOfABug: {
         positive:
           'Proof system can only be overridden if a bug is detected by the Ethereum L1 contracts.',
         negative:
