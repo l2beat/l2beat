f4de5e528a2aab282588c4b8722d27b7278e39c0
antooni <antoni.pawlak@protonmail.com>
2023-06-07 08:21:25 +0200

    improve badge story

--- packages/frontend/src/components/stages/StageBadge.stories.tsx
+++ packages/frontend/src/components/stages/StageBadge.stories.tsx
@@ -1,3 +1,4 @@
+import { Story } from '@storybook/react'
 import React from 'react'
 
 import { StageBadge } from './StageBadge'
@@ -6,10 +7,31 @@ export default {
   title: 'Components/Stages',
 }
 
-export function Badge() {
+interface TemplateProps {
+  category: 'Stage 0' | 'Stage 1' | 'Stage 2' | undefined
+  small?: boolean
+}
+
+function Template(props: TemplateProps) {
   return (
     <div className="m-4 ml-32">
-      <StageBadge category="Stage 1" />
+      <StageBadge category={props.category} small={props.small} />
     </div>
   )
+}
+
+export const Badge: Story<TemplateProps> = Template.bind({})
+Badge.argTypes = {
+  category: {
+    control: 'radio',
+    options: ['Stage 0', 'Stage 1', 'Stage 2', undefined],
+  },
+  small: {
+    control: 'radio',
+    options: [true, false],
+  },
+}
+Badge.args = {
+  category: 'Stage 0',
+  small: false,
 }
