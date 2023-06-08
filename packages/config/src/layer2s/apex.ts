5255a1616c99662fdaeaf53c619bb48992e1c67d
Luca Donno <donnoh99@gmail.com>
2023-06-08 08:34:01 +0200

    change prop fail apex

--- packages/config/src/layer2s/apex.ts
+++ packages/config/src/layer2s/apex.ts
@@ -96,7 +96,7 @@ export const apex: Layer2 = {
     upgradeability: RISK_VIEW.UPGRADE_DELAY_SECONDS(delaySeconds),
     sequencerFailure:
       RISK_VIEW.FORCE_VIA_L1_STARKEX_PERPETUAL(freezeGracePeriod),
-    proposerFailure: RISK_VIEW.VALIDATOR_ESCAPE_STARKEX_PERPETUAL,
+    proposerFailure: RISK_VIEW.USE_ESCAPE_HATCH_MP_AVGPRICE,
     validatedBy: RISK_VIEW.VALIDATED_BY_ETHEREUM,
     destinationToken: RISK_VIEW.CANONICAL_USDC,
   }),
