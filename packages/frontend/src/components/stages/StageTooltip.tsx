188c43dbd0d104ce807ef52a28bbd380f6b25559
Michal <michalsidzej@gmail.com>
2023-06-15 10:41:07 +0200

    Implement under review tooltip

--- packages/frontend/src/components/stages/StageTooltip.tsx
+++ packages/frontend/src/components/stages/StageTooltip.tsx
@@ -12,33 +12,40 @@ export interface StageTooltipProps {
 export function StageTooltip({ item }: StageTooltipProps) {
   if (!item) return null
 
-  // TODO: implement under review tooltip
-  if (item.stage === 'UnderReview') {
-    return <span>Under Review 🕵️‍♀️</span>
-  }
-
   return (
     <div className="flex max-w-[300px] flex-col gap-4 py-1">
       <span>
         <StageBadge stage={item.stage} className="font-medium" />
+        <span className="ml-2 inline-block font-medium">
+          {getStageName(item.stage)}
+        </span>
       </span>
-      {item.missing && (
-        <div className="text-sm">
-          <span className="mb-2 block leading-tight">
-            Items missing for{' '}
-            <span className={getColorClassName(item.missing.nextStage)}>
-              {item.missing.nextStage}
+      {item.stage === 'UnderReview' ? (
+        <>
+          Projects under review might present uncompleted information & data.
+          <br />
+          L2BEAT Team is working to research & validate content before
+          publishing.
+        </>
+      ) : (
+        item.missing && (
+          <div className="text-sm">
+            <span className="mb-2 block leading-tight">
+              Items missing for{' '}
+              <span className={getColorClassName(item.missing.nextStage)}>
+                {item.missing.nextStage}
+              </span>
             </span>
-          </span>
-          <ul className="list-none space-y-2">
-            {item.missing.requirements.map((requirement, i) => (
-              <li className="flex gap-1.5" key={i}>
-                <MissingIcon className="relative top-0.5 inline-block shrink-0" />
-                {requirement}
-              </li>
-            ))}
-          </ul>
-        </div>
+            <ul className="list-none space-y-2">
+              {item.missing.requirements.map((requirement, i) => (
+                <li className="flex gap-1.5" key={i}>
+                  <MissingIcon className="relative top-0.5 inline-block shrink-0" />
+                  {requirement}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )
       )}
       <StageDisclaimer
         text="Please mind, stages do not reflect rollup security"
@@ -46,6 +53,22 @@ export function StageTooltip({ item }: StageTooltipProps) {
       />
     </div>
   )
+}
+
+function getStageName(stage: Stage | 'UnderReview') {
+  switch (stage) {
+    case 'UnderReview':
+      return 'Stage under review'
+    case 'Stage 0':
+      return 'Full training wheels'
+    case 'Stage 1':
+      return 'Limited training wheels'
+    case 'Stage 2':
+      return 'No training wheels'
+    default:
+      return ''
+    // assertUnreachable(stage)
+  }
 }
 
 function getColorClassName(stage: Stage) {
