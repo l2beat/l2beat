e38f7eec3783893c02e1403fa45fc6fbcaa76293
Wojciech Korzeniowski <korzeniowski.wojciech@gmail.com>
2023-06-22 00:06:19 +0200

    Make navbar sticky to the top

--- packages/frontend/src/components/navbar/Navbar.tsx
+++ packages/frontend/src/components/navbar/Navbar.tsx
@@ -51,7 +51,10 @@ export function Navbar(props: NavbarProps) {
         socialLinks={props.socialLinks}
       />
       {props.showBanner && <Banner />}
-      <div className="h-14 border-b border-gray-200 text-base dark:border-gray-850 lg:h-16">
+      <div
+        className="fixed top-0 z-70 h-14 w-full border-b border-gray-200 text-base dark:border-gray-850 lg:h-16"
+        style={{ backgroundColor: 'inherit' }}
+      >
         <nav className="relative mx-auto box-border flex h-full max-w-[1780px] items-center justify-between px-4 lg:px-12">
           <ul className="flex h-full items-center">
             <li className="mr-4 lg:mr-8">
@@ -130,6 +133,7 @@ export function Navbar(props: NavbarProps) {
           </div>
         </nav>
       </div>
+      <div className="h-14" />
     </>
   )
 }
