8f20d7177e8736f3e536809a6a95ad850ebe2b26
Antoni Pawlak <antoni.pawlak@pm.me>
2023-06-22 11:10:18 +0200

    Update packages/frontend/src/components/ScalingLegend.tsx
    Co-authored-by: Luca Donno <30298476+lucadonnoh@users.noreply.github.com>

--- packages/frontend/src/components/ScalingLegend.tsx
+++ packages/frontend/src/components/ScalingLegend.tsx
@@ -14,7 +14,7 @@ export function ScalingLegend() {
         <p className="flex gap-1">
           <OptimismIcon className="relative -top-0.5 min-w-[24px]" />
           <span>&ndash;</span>
-          <span>This project is based on OP stack&apos;s code base.</span>
+          <span>This project is based on OP Stack&apos;s code base.</span>
         </p>
         <p className="flex gap-1">
           <ZkSyncIcon className="relative -top-0.5 min-w-[24px]" />
