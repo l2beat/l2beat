d4dd06c60c84358f1f2bdd06cc0a6198791f9094
torztomasz <tomasz.torz@l2beat.com>
2023-06-27 12:20:10 +0200

    refactor polygon pos config add implementation for polygon proxy add implementation for polygon extension proxy
    Co-authored-by: Piotr Szlachciak <sz-piotr@users.noreply.github.com>
    Co-authored-by: Mateusz Radomski <mateuszradomski@users.noreply.github.com>

--- packages/discovery/src/config/config.discovery.ts
+++ packages/discovery/src/config/config.discovery.ts
@@ -15,7 +15,7 @@ export function getDiscoveryCliConfig(cli: CliParameters): DiscoveryCliConfig {
   return {
     invert: invertEnabled && {
       file: cli.file,
-      useMermaidMarkup: false,
+      useMermaidMarkup: true,
     },
     discovery: discoveryEnabled && {
       project: cli.project,
