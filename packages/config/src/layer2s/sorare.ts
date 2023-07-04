a65ebc2ba0f7f8a7e3d18301977ea688a91305fe
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2023-07-04 12:50:23 +0200

    Fix Etherscan links (#1625)

--- packages/config/src/layer2s/sorare.ts
+++ packages/config/src/layer2s/sorare.ts
@@ -88,13 +88,13 @@ export const sorare: Layer2 = {
         {
           contract: 'StarkExchange',
           references: [
-            'https://etherscan.io/address/0x67e198743BC19fa4757720eDd0e769f8291e1F1D#code#F13#L188',
+            'https://etherscan.io/address/0x67e198743BC19fa4757720eDd0e769f8291e1F1D#code#F14#L188',
           ],
         },
         {
           contract: 'Committee',
           references: [
-            'https://etherscan.io/address/0x879cD57975d596004863D30c59d579ef78BBbe32#code#F1#L60',
+            'https://etherscan.io/address/0x879cD57975d596004863D30c59d579ef78BBbe32#code#F1#L65',
           ],
         },
       ],
