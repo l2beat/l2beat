46d47ddacac031068db24d1a011bb413d97e9d57
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2023-07-05 13:12:34 +0200

    Add paused contracts warning for Poly Network (#1635)

--- packages/config/src/bridges/polynetwork.ts
+++ packages/config/src/bridges/polynetwork.ts
@@ -7,12 +7,21 @@ import { Bridge } from './types'
 
 const discovery = new ProjectDiscovery('polynetwork')
 
+const isPaused = discovery.getContractValue<boolean>(
+  'EthCrossChainManager',
+  'paused',
+)
+const warningText = isPaused
+  ? 'The bridge is currently paused due to an attack occurred on July 2nd, resulting from stolen or misused private keys. For more information, read the postmortem here: https://dedaub.com/blog/poly-chain-hack-postmortem'
+  : ''
+
 export const polynetwork: Bridge = {
   type: 'bridge',
   id: ProjectId('polynetwork'),
   display: {
     name: 'Poly Bridge',
     slug: 'polynetwork',
+    warning: warningText,
     links: {
       websites: ['https://bridge.poly.network/', 'https://poly.network/'],
       apps: ['https://bridge.poly.network/'],
@@ -168,10 +177,14 @@ export const polynetwork: Bridge = {
         'Lock Proxy 5',
         'Escrow and proxy contract for the Bridge.',
       ),
-      discovery.getContractDetails(
-        'EthCrossChainManager',
-        'Contract responsible for building cross-chain messages and validating incoming messages, including Merkle proofs.',
-      ),
+      discovery.getContractDetails('EthCrossChainManager', {
+        description:
+          'Contract responsible for building cross-chain messages and validating incoming messages, including Merkle proofs.',
+        pausable: {
+          paused: discovery.getContractValue('EthCrossChainManager', 'paused'),
+          pausableBy: ['EthCrossChainManager'],
+        },
+      }),
       //DUPLICATES???
       discovery.getContractDetails(
         'EthCrossChainData',
@@ -185,7 +198,10 @@ export const polynetwork: Bridge = {
         description:
           'Used to proxy requests from LockProxy to EthCrossChainManager.',
         pausable: {
-          paused: discovery.getContractValue('EthCrossChainManager', 'paused'),
+          paused: discovery.getContractValue(
+            'EthCrossChainManagerProxy',
+            'paused',
+          ),
           pausableBy: ['EthCrossChainManager'],
         },
       }),
