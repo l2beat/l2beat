6fea9e9a5d7c31e576dcf9697955c1ffb985d7d6
Michal <michalsidzej@gmail.com>
2023-07-05 11:54:22 +0200

    Fix bug in isProjectUnderReview logic

--- packages/frontend/src/utils/project/isProjectUnderReview.ts
+++ packages/frontend/src/utils/project/isProjectUnderReview.ts
@@ -1,12 +1,19 @@
-import { Bridge, BridgeRiskView, Layer2, Layer2RiskView } from '@l2beat/config'
+import {
+  Bridge,
+  BridgeRiskView,
+  Layer2,
+  Layer2RiskView,
+  ProjectRiskViewEntry,
+} from '@l2beat/config'
 
+// this function should be updated when new sections that can be under review are added
 export function isProjectUnderReview(project: Layer2 | Bridge): boolean {
   return (
     project.isUnderReview ??
     project.technology.isUnderReview ??
     project.contracts?.isUnderReview ??
-    isAnyRiskUnderReview(project.riskView) ??
-    ((project.type === 'layer2' && project.stage?.stage === 'UnderReview') ||
+    ((isAnyRiskUnderReview(project.riskView) ?? false) ||
+      (project.type === 'layer2' && project.stage?.stage === 'UnderReview') ||
       project.permissions === 'UnderReview')
   )
 }
@@ -15,6 +22,9 @@ export function isAnyRiskUnderReview(
   riskView?: Layer2RiskView | BridgeRiskView,
 ) {
   return (
-    riskView && Object.values(riskView).some((value) => value === 'UnderReview')
+    riskView &&
+    Object.values(riskView as Record<string, ProjectRiskViewEntry>).some(
+      (risk) => risk.sentiment === 'UnderReview',
+    )
   )
 }
