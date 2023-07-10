07953b0fbb6f250fcbce4462163ef2badaad99b7
Artem Payvin <payvinartem@gmail.com>
2023-07-10 17:46:51 +0100

    Add comma

--- packages/config/src/bridges/multichain.ts
+++ packages/config/src/bridges/multichain.ts
@@ -188,7 +188,7 @@ export const multichain: Bridge = {
       name: 'Contracts hacked for $130M',
       date: '2023-07-07T00:00:00Z',
       link: 'https://blockworks.co/news/multichain-anyswap-exploit',
-    }
+    },
   ],
   knowledgeNuggets: [
     {
