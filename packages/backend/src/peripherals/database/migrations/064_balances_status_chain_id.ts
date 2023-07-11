19fd6da43d1cf0cce9b710ed3d7401fc504b9602
Michał Podsiadły <michal.podsiadly@l2beat.com>
2023-07-11 12:42:49 +0200

    chore: fix migration indices

--- packages/backend/src/peripherals/database/migrations/064_balances_status_chain_id.ts
+++ packages/backend/src/peripherals/database/migrations/064_balances_status_chain_id.ts
@@ -18,16 +18,25 @@ export async function up(knex: Knex) {
   await knex.schema.alterTable('balances_status', function (table) {
     table.integer('chain_id').notNullable().defaultTo(Number(ChainId.ETHEREUM))
 
+    table.dropIndex(['config_hash'])
+    table.dropIndex(['unix_timestamp'])
+
+    table.index(['chain_id', 'config_hash'])
+
     table.dropPrimary()
-    table.primary(['chain_id', 'unix_timestamp', 'config_hash'])
+    table.primary(['chain_id', 'unix_timestamp'])
   })
 }
 
 export async function down(knex: Knex) {
   await knex.schema.alterTable('balances_status', function (table) {
-    table.dropPrimary()
-    table.primary(['config_hash', 'unix_timestamp'])
-
     table.dropColumn('chain_id')
+
+    table.dropIndex(['chain_id', 'config_hash'])
+    table.dropPrimary()
+
+    table.primary(['config_hash', 'unix_timestamp'])
+    table.index(['unix_timestamp'])
+    table.index(['config_hash'])
   })
 }
