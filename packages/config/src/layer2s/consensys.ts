2defc487ce396b97e18af467053897f76dcc4e9e
Bartek Kiepuszewski <bkiepuszewski@gmail.com>
2023-07-13 23:40:35 +0200

    more config

--- packages/config/src/layer2s/consensys.ts
+++ packages/config/src/layer2s/consensys.ts
@@ -1,7 +1,19 @@
-import { ProjectId } from '@l2beat/shared-pure'
+import { EthereumAddress, ProjectId, UnixTime } from '@l2beat/shared-pure'
 import { ProjectDiscovery } from '../discovery/ProjectDiscovery'
-import { CONTRACTS, TECHNOLOGY, UPCOMING_RISK_VIEW, RISK_VIEW } from './common'
+import {
+  CONTRACTS,
+  STATE_CORRECTNESS,
+  DATA_AVAILABILITY,
+  FRONTRUNNING_RISK,
+  RISK_CENTRALIZED_VALIDATOR,
+  FORCE_TRANSACTIONS,
+  TECHNOLOGY,
+  UPCOMING_RISK_VIEW,
+  RISK_VIEW,
+  makeBridgeCompatible,
+} from './common'
 import { Layer2 } from './types'
+import { getStage } from './common/stages/getStage'
 
 const discovery = new ProjectDiscovery('linea')
 
@@ -29,7 +41,13 @@ export const linea: Layer2 = {
     },
   },
   config: {
-    escrows: [],
+    escrows: [
+      discovery.getEscrowDetails({
+        address: EthereumAddress('0xd19d4B5d358258f05D7B411E21A1460D11B0876F'),
+        sinceTimestamp: new UnixTime(1689159923),
+        tokens: ['ETH'],
+      }),
+    ],
   },
   riskView: makeBridgeCompatible({
     stateValidation: {
@@ -63,7 +81,65 @@ export const linea: Layer2 = {
     destinationToken: RISK_VIEW.NATIVE_AND_CANONICAL(),
     validatedBy: RISK_VIEW.VALIDATED_BY_ETHEREUM,
   }),
-  technology: TECHNOLOGY.UPCOMING,
+  stage: getStage({
+    stage0: {
+      callsItselfRollup: true,
+      stateRootsPostedToL1: true,
+      dataAvailabilityOnL1: true,
+      rollupNodeOpenSource: true,
+    },
+    stage1: {
+      stateVerificationOnL1: true,
+      fraudProofSystemAtLeast5Outsiders: null,
+      usersHave7DaysToExit: false,
+      usersCanExitWithoutCooperation: false,
+      securityCouncilProperlySetUp: [
+        false,
+        'Security Council members are not publicly known.',
+      ],
+    },
+    stage2: {
+      proofSystemOverriddenOnlyInCaseOfABug: false,
+      fraudProofSystemIsPermissionless: null,
+      delayWith30DExitWindow: false,
+    },
+  }),
+  technology: {
+    stateCorrectness: STATE_CORRECTNESS.VALIDITY_PROOFS,
+    dataAvailability: DATA_AVAILABILITY.ON_CHAIN_CANONICAL,
+    operator: {
+      name: 'The system has a centralized sequencer',
+      description:
+        'Only a trusted sequencer is allowed to submit transaction batches. A mechanism for users to submit their own batches is currently disabled.',
+      risks: [
+        FRONTRUNNING_RISK,
+        {
+          category: 'Funds can be frozen if',
+          text: 'the sequencer refuses to include an exit transaction.',
+          isCritical: true,
+        },
+      ],
+    },
+    forceTransactions: FORCE_TRANSACTIONS.SEQUENCER_NO_MECHANISM,
+    exitMechanisms: RISK_CENTRALIZED_VALIDATOR,
+  },
+  permissions: [
+    ...discovery.getMultisigPermission(
+      'AdminMultisig',
+      'Admin of the linea rollup',
+    ),
+    {
+      name: 'Operator',
+      accounts: [
+        discovery.getPermissionedAccount(
+          'zkEVM',
+          'accessControl.OPERATOR_ROLE.members',
+        ),
+      ],
+      description:
+        'Its sole purpose and ability is to submit transaction batches and proofs.',
+    },
+  ],
   contracts: CONTRACTS.EMPTY,
   milestones: [
     {
