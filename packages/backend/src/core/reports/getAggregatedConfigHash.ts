3eb58de84360ed14c2dc06a94000d5731d167b1d
Mateusz Radomski <mateusz.radomski@l2beat.com>
2023-07-14 16:25:20 +0200

    tests

--- packages/backend/src/core/reports/getAggregatedConfigHash.ts
+++ packages/backend/src/core/reports/getAggregatedConfigHash.ts
@@ -1,12 +1,13 @@
 import { hashJson } from '@l2beat/shared-pure'
+import { sortBy } from 'lodash'
 
 import { AssetUpdater } from '../assets'
 
 const AGGREGATED_REPORT_LOGIC_VERSION = 0
 
 export function getAggregatedConfigHash(updaters: AssetUpdater[]) {
-  const chainIds = updaters.map((x) => x.getChainId)
-  const configHashes = updaters.map((x) => x.getConfigHash)
+  const chainIds = sortBy(updaters.map((x) => x.getChainId()))
+  const configHashes = sortBy(updaters.map((x) => x.getConfigHash()))
 
   return hashJson([
     chainIds.map(Number),
