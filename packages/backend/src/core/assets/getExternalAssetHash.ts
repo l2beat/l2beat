e83a7052a4a90a5baac3649830bd1d741414b47d
Mateusz Radomski <mateusz.radomski@l2beat.com>
2023-07-14 12:04:05 +0200

    Add ExternalAsset config

---
+++ packages/backend/src/core/assets/getExternalAssetHash.ts
@@ -0,0 +1,6 @@
+import { Layer2ExternalAsset } from "@l2beat/config";
+import { Hash256 } from "@l2beat/shared-pure";
+
+export function getExternalAssetHash(asset: Layer2ExternalAsset): Hash256 {
+     
+}
