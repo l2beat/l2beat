c041fef6f65943579e8312b11f36cf3ae43e1e52
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2023-07-17 22:14:47 +0200

    Add Base config (#1674)

--- packages/config/src/layer2s/base.ts
+++ packages/config/src/layer2s/base.ts
@@ -1,7 +1,26 @@
-import { ProjectId } from '@l2beat/shared-pure'
+import { EthereumAddress, ProjectId, UnixTime } from '@l2beat/shared-pure'
 
-import { CONTRACTS, TECHNOLOGY, UNDER_REVIEW_RISK_VIEW } from './common'
+import { ProjectDiscovery } from '../discovery/ProjectDiscovery'
+import { HARDCODED } from '../discovery/values/hardcoded'
+import {
+  CONTRACTS,
+  DATA_AVAILABILITY,
+  EXITS,
+  FORCE_TRANSACTIONS,
+  makeBridgeCompatible,
+  NUGGETS,
+  OPERATOR,
+  RISK_VIEW,
+} from './common'
+import { getStage } from './common/stages/getStage'
 import { Layer2 } from './types'
+
+const discovery = new ProjectDiscovery('base')
+
+const upgradesProxy = {
+  upgradableBy: ['ProxyAdmin'],
+  upgradeDelay: 'No Delay',
+}
 
 export const base: Layer2 = {
   type: 'layer2',
@@ -9,6 +28,8 @@ export const base: Layer2 = {
   display: {
     name: 'Base',
     slug: 'base',
+    warning:
+      'Fraud proof system is currently under development. Users need to trust the block proposer to submit correct L1 state roots.',
     description:
       'Base is an Optimistic Rollup that has been developed on the Ethereum network, utilizing OP Stack technology.',
     purpose: 'Universal',
@@ -26,14 +47,290 @@ export const base: Layer2 = {
         'https://base.mirror.xyz/',
       ],
     },
+    activityDataSource: 'Blockchain RPC',
   },
   config: {
-    escrows: [],
+    escrows: [
+      discovery.getEscrowDetails({
+        address: EthereumAddress('0x49048044D57e1C92A77f79988d21Fa8fAF74E97e'),
+        sinceTimestamp: new UnixTime(1686793895),
+        tokens: ['ETH'],
+        description: 'Main entry point for users depositing ETH.',
+        ...upgradesProxy,
+      }),
+      discovery.getEscrowDetails({
+        address: EthereumAddress('0x3154Cf16ccdb4C6d922629664174b904d80F2C35'),
+        sinceTimestamp: new UnixTime(1686793895),
+        tokens: '*',
+        description:
+          'Main entry point for users depositing ERC20 token that do not require custom gateway.',
+        ...upgradesProxy,
+      }),
+    ],
+    transactionApi: {
+      type: 'rpc',
+      startBlock: 1,
+      url: 'developer-access-mainnet.base.org',
+      callsPerMinute: 1500,
+    },
   },
-  stage: {
-    stage: 'UnderReview',
+  riskView: makeBridgeCompatible({
+    stateValidation: {
+      value: 'In development',
+      description:
+        'Currently the system permits invalid state roots. More details in project overview.',
+      sentiment: 'bad',
+    },
+    dataAvailability: {
+      ...RISK_VIEW.DATA_ON_CHAIN,
+      sources: [
+        {
+          contract: 'OptimismPortal',
+          references: [
+            'https://etherscan.io/address/0x5FB30336A8d0841cf15d452afA297cB6D10877D7#code#F1#L434',
+          ],
+        },
+      ],
+    },
+    upgradeability: {
+      ...RISK_VIEW.UPGRADABLE_YES,
+      sources: [
+        {
+          contract: 'OptimismPortal',
+          references: [
+            'https://etherscan.io/address/0x49048044D57e1C92A77f79988d21Fa8fAF74E97e',
+          ],
+        },
+      ],
+    },
+    sequencerFailure: {
+      ...RISK_VIEW.SEQUENCER_SELF_SEQUENCE(
+        HARDCODED.OPTIMISM.SEQUENCING_WINDOW_SECONDS,
+      ),
+      sources: [
+        {
+          contract: 'OptimismPortal',
+          references: [
+            'https://etherscan.io/address/0x5FB30336A8d0841cf15d452afA297cB6D10877D7#code#F1#L434',
+          ],
+        },
+      ],
+    },
+    proposerFailure: {
+      ...RISK_VIEW.PROPOSER_CANNOT_WITHDRAW,
+      sources: [
+        {
+          contract: 'L2OutputOracle',
+          references: [
+            'https://etherscan.io/address/0x7237343c2A746Aa2940E5E4Fbd53eaFBF3049DcA#code#F1#L186',
+          ],
+        },
+      ],
+    },
+    destinationToken: RISK_VIEW.NATIVE_AND_CANONICAL(),
+    validatedBy: RISK_VIEW.VALIDATED_BY_ETHEREUM,
+  }),
+  stage: getStage({
+    stage0: {
+      callsItselfRollup: true,
+      stateRootsPostedToL1: true,
+      dataAvailabilityOnL1: true,
+      rollupNodeOpenSource: true,
+    },
+    stage1: {
+      stateVerificationOnL1: false,
+      fraudProofSystemAtLeast5Outsiders: null,
+      usersHave7DaysToExit: false,
+      usersCanExitWithoutCooperation: false,
+      securityCouncilProperlySetUp: null,
+    },
+    stage2: {
+      proofSystemOverriddenOnlyInCaseOfABug: null,
+      fraudProofSystemIsPermissionless: null,
+      delayWith30DExitWindow: false,
+    },
+  }),
+  technology: {
+    stateCorrectness: {
+      name: 'Fraud proofs are in development',
+      description:
+        'Ultimately, Base will use interactive fraud proofs to enforce state correctness. This feature is currently in development and the system permits invalid state roots.',
+      risks: [
+        {
+          category: 'Funds can be stolen if',
+          text: 'an invalid state root is submitted to the system.',
+          isCritical: true,
+        },
+      ],
+      references: [
+        {
+          text: 'L2OutputOracle.sol#L141 - Etherscan source code, deleteL2Outputs function',
+          href: 'https://etherscan.io/address/0x7237343c2A746Aa2940E5E4Fbd53eaFBF3049DcA#code#F1#L141',
+        },
+      ],
+    },
+    dataAvailability: {
+      ...DATA_AVAILABILITY.ON_CHAIN_CANONICAL,
+      references: [
+        {
+          text: 'BatchInbox - Etherscan address',
+          href: 'https://etherscan.io/address/0xff00000000000000000000000000000000008453',
+        },
+        {
+          text: 'OptimismPortal.sol#L434 - Etherscan source code, depositTransaction function',
+          href: 'https://etherscan.io/address/0x5FB30336A8d0841cf15d452afA297cB6D10877D7#code#F1#L434',
+        },
+      ],
+    },
+    operator: {
+      ...OPERATOR.CENTRALIZED_OPERATOR,
+      references: [
+        {
+          text: 'L2OutputOracle.sol#L30 - Etherscan source code, CHALLENGER address',
+          href: 'https://etherscan.io/address/0x7237343c2A746Aa2940E5E4Fbd53eaFBF3049DcA#code#F1#L30',
+        },
+        {
+          text: 'L2OutputOracle.sol#L35 - Etherscan source code, PROPOSER address',
+          href: 'https://etherscan.io/address/0x7237343c2A746Aa2940E5E4Fbd53eaFBF3049DcA#code#F1#L35',
+        },
+        {
+          text: 'Decentralizing the sequencer - OP Stack docs',
+          href: 'https://community.optimism.io/docs/protocol/#decentralizing-the-sequencer',
+        },
+      ],
+    },
+    forceTransactions: {
+      ...FORCE_TRANSACTIONS.CANONICAL_ORDERING,
+      references: [
+        {
+          text: 'OptimismPortal.sol#L434 - Etherscan source code, depositTransaction function',
+          href: 'https://etherscan.io/address/0x5FB30336A8d0841cf15d452afA297cB6D10877D7#code#F1#L434',
+        },
+      ],
+    },
+    exitMechanisms: [
+      {
+        ...EXITS.REGULAR('optimistic', 'merkle proof'),
+        references: [
+          {
+            text: 'OptimismPortal.sol#L242 - Etherscan source code, proveWithdrawalTransaction function',
+            href: 'https://etherscan.io/address/0x5FB30336A8d0841cf15d452afA297cB6D10877D7#code#F1#L242',
+          },
+          {
+            text: 'OptimismPortal.sol#325 - Etherscan source code, finalizeWithdrawalTransaction function',
+            href: 'https://etherscan.io/address/0x5FB30336A8d0841cf15d452afA297cB6D10877D7#code#F1#L325',
+          },
+          {
+            text: 'L2OutputOracle.sol#L185 - Etherscan source code, PROPOSER check',
+            href: 'https://etherscan.io/address/0x7237343c2A746Aa2940E5E4Fbd53eaFBF3049DcA#code#F1#L185',
+          },
+        ],
+        risks: [EXITS.RISK_CENTRALIZED_VALIDATOR],
+      },
+    ],
+    smartContracts: {
+      name: 'EVM compatible smart contracts are supported',
+      description:
+        'Base is pursuing the EVM Equivalence model. No changes to smart contracts are required regardless of the language they are written in, i.e. anything deployed on L1 can be deployed on Base.',
+      risks: [],
+      references: [
+        {
+          text: 'Introducing EVM Equivalence',
+          href: 'https://medium.com/ethereum-optimism/introducing-evm-equivalence-5c2021deb306',
+        },
+      ],
+    },
   },
-  riskView: UNDER_REVIEW_RISK_VIEW,
-  technology: TECHNOLOGY.UNDER_REVIEW,
-  contracts: CONTRACTS.UNDER_REVIEW,
+  permissions: [
+    ...discovery.getMultisigPermission(
+      'AdminMultisig',
+      'This address is the owner of the ProxyAdmin. It can upgrade the bridge implementation potentially gaining access to all funds.',
+    ),
+    ...discovery.getMultisigPermission(
+      'ChallengerMultisig',
+      "This address is the permissioned challenger of the system. It can delete non finalized roots without going through the fault proof process. It is also designated as a Guardian of the OptimismPortal, meaning it can halt withdrawals. It's the owner of SystemConfig, which allows to update the sequencer address.",
+    ),
+    {
+      name: 'ProxyAdmin',
+      accounts: [discovery.getPermissionedAccount('AddressManager', 'owner')],
+      description:
+        'Admin of the OptimismPortal, L1ERC721Bridge, L2OutputOracle, OptimismMintableERC20Factory, L1StandardBridge, AddressManager, SystemConfig proxies. It’s controlled by the AdminMultisig.',
+    },
+    {
+      name: 'Sequencer',
+      accounts: [
+        discovery.getPermissionedAccount('SystemConfig', 'batcherHash'),
+      ],
+      description: 'Central actor allowed to commit L2 transactions to L1.',
+    },
+    {
+      name: 'Proposer',
+      accounts: [
+        discovery.getPermissionedAccount('L2OutputOracle', 'PROPOSER'),
+      ],
+      description: 'Central actor allowed to post new L2 state roots to L1.',
+    },
+    {
+      name: 'Challenger',
+      accounts: [
+        discovery.getPermissionedAccount('L2OutputOracle', 'CHALLENGER'),
+      ],
+      description: 'Central actor allowed to challenge L2 state roots.',
+    },
+  ],
+  contracts: {
+    addresses: [
+      discovery.getContractDetails('L2OutputOracle', {
+        description:
+          'The L2OutputOracle contract contains a list of proposed state roots which Proposers assert to be a result of block execution. Currently only the PROPOSER address can submit new state roots.',
+        ...upgradesProxy,
+      }),
+      discovery.getContractDetails('OptimismPortal', {
+        description:
+          'The OptimismPortal contract is the main entry point to deposit funds from L1 to L2. It also allows to prove and finalize withdrawals.',
+        ...upgradesProxy,
+      }),
+      discovery.getContractDetails('SystemConfig', {
+        description:
+          'It contains configuration parameters such as the Sequencer address, the L2 gas limit and the unsafe block signer address.',
+        ...upgradesProxy,
+      }),
+      discovery.getContractDetails('L1ERC721Bridge', {
+        description:
+          'The L1ERC721Bridge contract is the main entry point to deposit ERC721 tokens from L1 to L2.',
+        ...upgradesProxy,
+      }),
+      discovery.getContractDetails('L1CrossDomainMessenger', {
+        description:
+          "The L1 Cross Domain Messenger contract sends messages from L1 to L2, and relays messages from L2 onto L1. In the event that a message sent from L1 to L2 is rejected for exceeding the L2 epoch gas limit, it can be resubmitted via this contract's replay function.",
+        ...upgradesProxy,
+      }),
+    ],
+    risks: [CONTRACTS.UPGRADE_NO_DELAY_RISK],
+  },
+  milestones: [
+    {
+      name: 'Base Mainnet Launch',
+      link: 'https://base.mirror.xyz/hwNwqXHVoLlO8s4DZppog4DfGvM34tigaDjOWuEJQfY',
+      date: '2023-07-13T00:00:00.00Z',
+      description: 'Base is live on mainnet.',
+    },
+  ],
+  knowledgeNuggets: [
+    {
+      title: 'How Optimism compresses data',
+      url: 'https://twitter.com/bkiepuszewski/status/1508740414492323840?s=20&t=vMgR4jW1ssap-A-MBsO4Jw',
+      thumbnail: NUGGETS.THUMBNAILS.L2BEAT_03,
+    },
+    {
+      title: 'Bedrock Explainer',
+      url: 'https://community.optimism.io/docs/developers/bedrock/explainer/',
+      thumbnail: NUGGETS.THUMBNAILS.OPTIMISM_04,
+    },
+    {
+      title: 'Modular Rollup Theory',
+      url: 'https://www.youtube.com/watch?v=jnVjhp41pcc',
+      thumbnail: NUGGETS.THUMBNAILS.MODULAR_ROLLUP,
+    },
+  ],
 }
