f4071b708b91a1f1988d6101c956b072646b31d4
Michał Podsiadły <michal.podsiadly@l2beat.com>
2023-07-19 17:05:55 +0200

    chore: remove cx from floating banner

--- packages/frontend/src/components/floating-banner/FloatingBannerCross.tsx
+++ packages/frontend/src/components/floating-banner/FloatingBannerCross.tsx
@@ -1,21 +1,10 @@
-import cx from 'classnames'
 import React from 'react'
 
 import { CrossIcon } from '../icons/Cross'
 
 export function FloatingBannerCross() {
   return (
-    <div
-      className={cx(
-        'FloatingBanner-Cross',
-        'absolute',
-        'top-0',
-        'right-0',
-        'cursor-pointer',
-        'mt-4',
-        'mr-4',
-      )}
-    >
+    <div className="FloatingBanner-Cross absolute top-0 right-0 mt-4 mr-4 cursor-pointer">
       <CrossIcon className="fill-gray-550 dark:fill-gray-50" />
     </div>
   )
