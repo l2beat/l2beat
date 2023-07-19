0e78ec78d797f357a201e1bd76c5a73bdb0a2d5d
Michał Podsiadły <michal.podsiadly@l2beat.com>
2023-07-19 11:59:51 +0200

    chore: remember closing floating banner

--- packages/frontend/src/scripts/configureFloatingBanner.ts
+++ packages/frontend/src/scripts/configureFloatingBanner.ts
@@ -3,11 +3,22 @@ import { makeQuery } from './query'
 export function configureFloatingBanner() {
   const { $$ } = makeQuery(document.body)
 
+  const hasClosedBanner =
+    localStorage.getItem('floatingBannerClosed') === 'true'
+
   if (!document.querySelector('.FloatingBanner')) {
     return
   }
 
   const floatingBanners = $$('.FloatingBanner')
+
+  if (hasClosedBanner) {
+    floatingBanners.forEach((banner) => {
+      banner.classList.add('hidden')
+    })
+
+    return
+  }
 
   for (const banner of floatingBanners) {
     const cross = banner.querySelector('.FloatingBanner-Cross')
@@ -17,4 +28,6 @@ export function configureFloatingBanner() {
       banner.classList.toggle('hidden')
     })
   }
+
+  localStorage.setItem('floatingBannerClosed', 'true')
 }
