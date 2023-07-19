e07d5f70436773bf71177f2fe77fbe6a2dac8e4e
Michał Podsiadły <michal.podsiadly@l2beat.com>
2023-07-19 11:29:33 +0200

    feat: multisig report page

---
+++ packages/frontend/src/pages/multisig-report/view/MultisigReportPage.tsx
@@ -0,0 +1,86 @@
+import cx from 'classnames'
+import React from 'react'
+
+import { Footer, FooterProps, Navbar, NavbarProps } from '../../../components'
+import { LimitedAnnouncementBanner } from '../../../components/announcement/AnnouncementBanner'
+import {
+  L2BeatLogo,
+  PolygonLogo,
+} from '../../../components/announcement/logos/AnnouncementLogos'
+import { DownloadButton } from '../../../components/download-button/DownloadButton'
+import { PageContent } from '../../../components/PageContent'
+
+export interface MultisigReportPageProps {
+  navbar: NavbarProps
+  footer: FooterProps
+  showActivity: boolean
+  multisigReportUrl: string
+}
+
+export function MultisigReportPage(props: MultisigReportPageProps) {
+  return (
+    <>
+      <Navbar {...props.navbar} />
+      <PageContent>
+        <main className={cx('flex', 'flex-col', 'md:items-center')}>
+          <LimitedAnnouncementBanner />
+          <DownloadButton fileUrl={props.multisigReportUrl}>
+            Download the report
+          </DownloadButton>
+          <div className="mt-12 mb-6 w-full leading-7">
+            <h2
+              className={cx(
+                'text-2xl',
+                'md:text-4xl',
+                'font-medium',
+                'mb-3',
+                'md:mb-5',
+              )}
+            >
+              About the report
+            </h2>
+            <div>
+              In recent years the Ethereum L2s ecosystem has grown tremendously
+              in terms of the number of available systems, the number of
+              different system architectures, and their complexity. As a direct
+              consequence, a deeper understanding of the management mechanisms
+              of these systems has grown increasingly important. Almost all
+              present-day L2s are equipped with some form of upgrade mechanism,
+              whether a code implementation upgrade or system parameters update.
+              Given that these systems are in their relative infancy and
+              continue to evolve, it's reasonable to expect that these upgrade
+              mechanisms will remain an essential aspect of L2s for the
+              foreseeable future.
+            </div>
+            <div className={cx('mt-6')}>
+              This report is a snapshot of our current knowledge as of June
+              2023. In the report, we delve into various aspects of system
+              upgradeability. We start with why and when we need Layer 2s to be
+              upgradable, both from a user’s and an operator’s perspective. We
+              go through different aspects that need to be considered when
+              thinking about code upgradeability and managing system parameters,
+              and we finish with our recommendations regarding the design of the
+              systems architecture and safety measures. Attached at the end, you
+              will find our analysis of 8 existing L2s and how they approached
+              upgradeability and management. For every project, we focus
+              primarily on the technical and organizational aspects, leaving
+              away considerations regarding the governance structures and
+              processes.
+            </div>
+          </div>
+          <div className="flex w-full space-x-4">
+            <L2BeatLogo />
+            <PolygonLogo />
+          </div>
+          <DownloadButton
+            fileUrl={props.multisigReportUrl}
+            className={cx('visible', 'md:invisible', 'mt-12')}
+          >
+            Download the report
+          </DownloadButton>
+        </main>
+      </PageContent>
+      <Footer {...props.footer} />
+    </>
+  )
+}
