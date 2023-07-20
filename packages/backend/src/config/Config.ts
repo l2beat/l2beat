551fd630be0314a16eb23d4b0aadc3eb0ec8e006
Michał Podsiadły <67391475+sdlyy@users.noreply.github.com>
2023-07-20 13:24:17 +0200

    Migrate Arbitrum to QuickNode & rate limit the provider (#1699)

--- packages/backend/src/config/Config.ts
+++ packages/backend/src/config/Config.ts
@@ -65,7 +65,7 @@ export interface EthereumTvlConfig {
 }
 
 export interface ArbitrumTvlConfig {
-  readonly alchemyApiKey: string
+  readonly providerUrl: string
   readonly arbiscanApiKey: string
   readonly minBlockTimestamp: UnixTime
 }
