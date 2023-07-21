a5a20d77c8ce42e40f0c039f2e4a4cd99d0d8964
Michał Sobieraj-Jakubiec <michalsidzej@gmail.com>
2023-07-21 12:05:05 +0200

    Hoverable risk cells on mobile (#1707)

--- packages/frontend/src/components/table/props/getScalingTableColumns.tsx
+++ packages/frontend/src/components/table/props/getScalingTableColumns.tsx
@@ -218,29 +218,34 @@ export function getScalingRiskColumns() {
     {
       name: 'State validation',
       tooltip: 'How is the validity of the system state checked?',
+      noHrefMobile: true,
       getValue: (project) => <RiskCell item={project.stateValidation} />,
     },
     {
       name: 'Data availability',
       tooltip: 'Is the data needed to reconstruct the state available?',
+      noHrefMobile: true,
       getValue: (project) => <RiskCell item={project.dataAvailability} />,
     },
     {
       name: 'Upgradeability',
       tooltip:
         'Are the Ethereum contracts upgradeable? Note that the delay itself might not be enough to ensure that users can withdraw their funds in the case of a malicious and censoring operator.',
+      noHrefMobile: true,
       getValue: (project) => <RiskCell item={project.upgradeability} />,
     },
     {
       name: 'Sequencer failure',
       tooltip:
         "Sequencer is an entity responsible for constructing L2 blocks and deciding on the ordering of user's L2 transactions. What happens if it is offline or censors individual user?",
+      noHrefMobile: true,
       getValue: (project) => <RiskCell item={project.sequencerFailure} />,
     },
     {
       name: 'Proposer failure',
       tooltip:
         'Proposer is an entity responsible for submitting L2 state to Ethereum (optionally, along with the zkProof). What happens if it is offline?',
+      noHrefMobile: true,
       getValue: (project) => <RiskCell item={project.proposerFailure} />,
     },
   ]
