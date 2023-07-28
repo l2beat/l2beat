2dc0b10ca91fdf0f37bca2020471a8ce2957c456
Mateusz Radomski <mateusz.radomski@l2beat.com>
2023-07-28 13:00:48 +0200

    Enable detailed TVL on staging

--- packages/frontend/src/build/config/config.staging.ts
+++ packages/frontend/src/build/config/config.staging.ts
@@ -6,6 +6,7 @@ export function getStagingConfig(): Config {
     ...common,
     features: {
       ...common.features,
+      detailedTvl: true,
       buildAllProjectPages: true,
     },
     backend: {
