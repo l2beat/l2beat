656405152156a6a1a62b2f5d7149fc55ef951407
Mateusz Radomski <mateusz.radomski@l2beat.com>
2023-08-01 14:10:58 +0200

    remove sanityCheck skip

--- packages/frontend/src/build/api/sanityCheck.ts
+++ packages/frontend/src/build/api/sanityCheck.ts
@@ -76,7 +76,6 @@ export function checkIfDelayedTvl(
   allProjects: TvlProjectData[],
   now: UnixTime,
 ) {
-  return
   const delayedProjects = allProjects
     .map(([name, charts]) => {
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
