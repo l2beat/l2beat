279ab5481aa5c91a445d816d4f1328f604dad9d7
Sahil Kamra <sahilkamra2@gmail.com>
2023-08-01 18:59:14 +0530

    Polygon Upcoming Projects (#1717)
    Co-authored-by: Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
    Co-authored-by: lucadonnoh <donnoh99@gmail.com>
    Co-authored-by: antooni <antoni.pawlak@protonmail.com>
    Co-authored-by: Antoni Pawlak <antoni.pawlak@pm.me>

---
+++ packages/config/src/layer2s/xchain.ts
@@ -0,0 +1,38 @@
+import { ProjectId } from '@l2beat/shared-pure'
+
+import { CONTRACTS, TECHNOLOGY, UPCOMING_RISK_VIEW } from './common'
+import { Layer2 } from './types'
+
+export const xchain: Layer2 = {
+  isUpcoming: true,
+  type: 'layer2',
+  id: ProjectId('xchain'),
+  display: {
+    name: 'XCHAIN',
+    slug: 'xchain',
+    description:
+      'XCHAIN is an upcoming validium leveraging Polygon zkEVM Supernets.',
+    purpose: 'DEX Chain',
+    category: 'Validium',
+    links: {
+      websites: ['https://idex.io/'],
+      apps: ['https://exchange.idex.io/polygon/trading/IDEX-USDC/'],
+      documentation: [
+        'https://medium.com/idex/the-all-new-idex-a-quick-recap-b27df4cd981b',
+      ],
+      explorers: [],
+      repositories: [],
+      socialMedia: [
+        'https://twitter.com/idexio',
+        'https://discord.gg/idex',
+        'https://t.me/IDEXChat',
+      ],
+    },
+  },
+  config: {
+    escrows: [],
+  },
+  riskView: UPCOMING_RISK_VIEW,
+  technology: TECHNOLOGY.UPCOMING,
+  contracts: CONTRACTS.EMPTY,
+}
