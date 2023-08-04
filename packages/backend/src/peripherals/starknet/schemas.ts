d7d1079cb8e487d4580e80949b263e8b844759b4
Michał Sobieraj-Jakubiec <michalsidzej@gmail.com>
2023-08-04 11:53:12 +0200

    Rewrite StarknetClient to use reliable RPC

--- packages/backend/src/peripherals/starknet/schemas.ts
+++ packages/backend/src/peripherals/starknet/schemas.ts
@@ -4,7 +4,11 @@ export type StarknetGetBlockResponseBodySchema = z.infer<
   typeof StarknetGetBlockResponseBodySchema
 >
 export const StarknetGetBlockResponseBodySchema = z.object({
-  block_number: z.number().int(),
-  timestamp: z.number().int(),
-  transactions: z.array(z.unknown()),
+  jsonrpc: z.literal('2.0'),
+  id: z.number().int(),
+  result: z.object({
+    block_number: z.number().int(),
+    timestamp: z.number().int(),
+    transactions: z.array(z.string()),
+  }),
 })
