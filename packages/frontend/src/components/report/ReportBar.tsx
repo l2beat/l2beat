faee52c7d568d47fc5d64b2b90389864d7a056f2
Michał Podsiadły <michal.podsiadly@l2beat.com>
2023-08-08 15:09:43 +0200

    chore: spacing and block adjustments

--- packages/frontend/src/components/report/ReportBar.tsx
+++ packages/frontend/src/components/report/ReportBar.tsx
@@ -5,16 +5,14 @@ import { ArrowRightIcon } from '../icons'
 export function ReportBar() {
   return (
     <a
-      className="flex flex-col items-center justify-center space-x-5 bg-[#2C7265] p-2 text-center text-xs font-bold text-white dark:bg-[#487A85] md:flex-row md:text-sm"
+      className="flex flex-col items-center justify-center bg-[#2C7265] p-2 text-center text-xs font-bold text-white dark:bg-[#487A85] md:flex-row md:space-x-5 md:text-sm"
       href="https://warsaw.l2beat.com"
       target="_blank"
     >
       <p>Join us at L2Warsaw</p>
-      <ArrowRightIcon className="invisible ml-1 inline-block fill-current md:visible" />
-      <span>
-        <span className="underline decoration-solid underline-offset-2">
-          warsaw.l2beat.com
-        </span>
+      <ArrowRightIcon className="ml-1 hidden fill-current md:inline-block" />
+      <span className="underline decoration-solid underline-offset-2">
+        warsaw.l2beat.com
       </span>
     </a>
   )
