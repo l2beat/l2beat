86ad08f64d8f30fd4909ba7a1f6672f554f52512
Michał Podsiadły <michal.podsiadly@l2beat.com>
2023-08-08 15:33:37 +0200

    feat: cleanup

--- packages/frontend/src/components/l2warsaw/FloatingBanner.tsx
+++ packages/frontend/src/components/l2warsaw/FloatingBanner.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 import { CloseIcon } from '../icons/CloseIcon'
 
-export function ReportFloatingBanner() {
+export function FloatingBanner() {
   return (
-    <div className="ReportFloatingBanner fixed bottom-0 left-0 z-110 m-4 flex overflow-hidden rounded-md bg-gray-100 dark:bg-zinc-800">
+    <div className="FloatingBanner fixed bottom-0 left-0 z-110 m-4 flex overflow-hidden rounded-md bg-gray-100 pr-4 dark:bg-zinc-800">
       <Close />
       <div className="flex max-w-[182px] bg-[#2C7265] py-2 px-3 md:px-5 md:py-4">
         <img src={'/images/announcements/l2-warsaw-floating.png'} />
@@ -13,7 +13,7 @@ export function ReportFloatingBanner() {
         <div className="mb-2 text-xs font-medium uppercase text-gray-550 dark:text-gray-50 md:text-sm">
           Upcoming event
         </div>
-        <div className="mb-1 text-xl font-semibold leading-tight tracking-normal text-black dark:text-white md:text-2xl">
+        <div className="text-xl font-semibold leading-tight tracking-normal text-black dark:text-white md:text-2xl">
           L2Warsaw
         </div>
         <div className="mb-4 text-xs md:text-base">
@@ -33,7 +33,7 @@ export function ReportFloatingBanner() {
 
 function Close() {
   return (
-    <div className="ReportFloatingBanner-Close absolute top-0 right-0 mt-4 mr-4 cursor-pointer">
+    <div className="FloatingBanner-Close absolute top-0 right-0 mt-4 mr-4 cursor-pointer">
       <CloseIcon className="fill-gray-550 dark:fill-gray-50" />
     </div>
   )
--- packages/frontend/src/components/l2warsaw/TopBar.tsx
+++ packages/frontend/src/components/l2warsaw/TopBar.tsx
@@ -2,17 +2,19 @@ import React from 'react'
 
 import { ArrowRightIcon } from '../icons'
 
-export function ReportBar() {
+export function TopBar() {
   return (
     <a
       className="flex flex-col items-center justify-center bg-[#2C7265] p-2 text-center text-xs font-bold text-white md:flex-row md:space-x-5 md:text-sm"
       href="https://warsaw.l2beat.com"
       target="_blank"
     >
-      <p>Join us at L2Warsaw</p>
-      <ArrowRightIcon className="ml-1 hidden fill-current md:inline-block" />
-      <span className="underline decoration-solid underline-offset-2">
-        warsaw.l2beat.com
+      <p>Join us for L2Warsaw on August 30th! </p>
+      <span>
+        <span className="underline decoration-solid underline-offset-2">
+          warsaw.l2beat.com
+        </span>
+        <ArrowRightIcon className="ml-1 inline-block fill-current" />
       </span>
     </a>
   )
