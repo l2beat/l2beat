3e42b5c584068a6afcaf2c8f0ea86a244bef35ef
Michał Podsiadły <michal.podsiadly@l2beat.com>
2023-08-08 15:10:15 +0200

    chore: single bg bar

--- packages/frontend/src/components/report/ReportBar.tsx
+++ packages/frontend/src/components/report/ReportBar.tsx
@@ -5,7 +5,7 @@ import { ArrowRightIcon } from '../icons'
 export function ReportBar() {
   return (
     <a
-      className="flex flex-col items-center justify-center bg-[#2C7265] p-2 text-center text-xs font-bold text-white dark:bg-[#487A85] md:flex-row md:space-x-5 md:text-sm"
+      className="flex flex-col items-center justify-center bg-[#2C7265] p-2 text-center text-xs font-bold text-white md:flex-row md:space-x-5 md:text-sm"
       href="https://warsaw.l2beat.com"
       target="_blank"
     >
