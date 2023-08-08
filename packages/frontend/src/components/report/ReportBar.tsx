2b5b57b97b4f6f16bfe883280b61f1d6e0148696
Michał Podsiadły <michal.podsiadly@l2beat.com>
2023-08-08 15:06:27 +0200

    chore: bar bgs

--- packages/frontend/src/components/report/ReportBar.tsx
+++ packages/frontend/src/components/report/ReportBar.tsx
@@ -5,12 +5,12 @@ import { ArrowRightIcon } from '../icons'
 export function ReportBar() {
   return (
     <a
-      className="flex flex-col items-center justify-center space-x-5 bg-indigo-500 p-2 text-center text-xs font-bold text-white dark:bg-indigo-500 md:flex-row md:text-sm"
+      className="flex flex-col items-center justify-center space-x-5 bg-[#2C7265] p-2 text-center text-xs font-bold text-white dark:bg-[#487A85] md:flex-row md:text-sm"
       href="https://warsaw.l2beat.com"
       target="_blank"
     >
       <p>Join us at L2Warsaw</p>
-      <ArrowRightIcon className="ml-1 inline-block fill-current" />
+      <ArrowRightIcon className="invisible ml-1 inline-block fill-current md:visible" />
       <span>
         <span className="underline decoration-solid underline-offset-2">
           warsaw.l2beat.com
