4154e76761d6cfb750600043c415b19e6cb6c07a
Mateusz Radomski <mateusz.radomski@l2beat.com>
2023-08-10 14:41:02 +0200

    ylff

--- packages/frontend/src/utils/project/getChart.ts
+++ packages/frontend/src/utils/project/getChart.ts
@@ -21,7 +21,11 @@ export function getChart(
     tvlEndpoint: `/api/${project.display.slug}-tvl.json`,
     detailedTvlEndpoint: `/api/${project.display.slug}-detailed-tvl.json`,
     activityEndpoint: `/api/activity/${project.display.slug}.json`,
-    tokens: getTokens(project.id, tvlApiResponse, config?.features.detailedTvl ?? false),
+    tokens: getTokens(
+      project.id,
+      tvlApiResponse,
+      config?.features.detailedTvl ?? false,
+    ),
     hasActivity:
       config?.features.activity &&
       !!activityApiResponse?.projects[project.id.toString()],
