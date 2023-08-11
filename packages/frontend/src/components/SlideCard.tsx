9c293a8cdd0a3c62f7435e81f0098bb491e17d3e
Mateusz Radomski <mateusz.radomski@l2beat.com>
2023-08-11 11:37:48 +0200

    ylff

--- packages/frontend/src/components/SlideCard.tsx
+++ packages/frontend/src/components/SlideCard.tsx
@@ -72,7 +72,9 @@ function SlideCardContent(props: SlideCardContentProps) {
       </div>
       <div className="flex h-[12%] w-full items-center justify-center bg-gray-200 dark:bg-[#373B41] ">
         <div className="flex h-[6%] items-center justify-center rounded-md bg-pink-900 p-[6%] px-4">
-          <SlideCardToggle type="close"> { props.closeButtonText ?? "Close the overlay" }</SlideCardToggle>
+          <SlideCardToggle type="close">
+            {props.closeButtonText ?? 'Close the overlay'}
+          </SlideCardToggle>
         </div>
       </div>
     </div>
