e56bb8ee408cd411413a471efd32eb265a2c8226
Mateusz Radomski <mateusz.radomski@l2beat.com>
2023-08-11 08:39:45 +0200

    Make chart loader visible

--- packages/frontend/src/components/chart/ChartLoader.tsx
+++ packages/frontend/src/components/chart/ChartLoader.tsx
@@ -10,7 +10,7 @@ export function ChartLoader() {
       viewBox="0 0 100 100"
       preserveAspectRatio="xMidYMid"
     >
-      <circle cx="30" cy="50" fill="var(--gradient-1)" r="20">
+      <circle cx="30" cy="50" fill="var(--second-gradient-1)" r="20">
         <animate
           attributeName="cx"
           repeatCount="indefinite"
@@ -30,7 +30,7 @@ export function ChartLoader() {
           begin="0s"
         />
       </circle>
-      <circle cx="30" cy="50" fill="var(--gradient-1)" r="20">
+      <circle cx="30" cy="50" fill="var(--second-gradient-1)" r="20">
         <animate
           attributeName="cx"
           repeatCount="indefinite"
