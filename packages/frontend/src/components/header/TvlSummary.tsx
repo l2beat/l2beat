8161d4cc066e1fbdd6067a62f73eced33d57b40c
Mateusz Radomski <mateusz.radomski@l2beat.com>
2023-08-19 07:13:25 +0200

    TvlSummary changes for mobile

--- packages/frontend/src/components/header/TvlSummary.tsx
+++ packages/frontend/src/components/header/TvlSummary.tsx
@@ -3,6 +3,7 @@ import { renderToStaticMarkup } from 'react-dom/server'
 
 import { formatUSD } from '../../utils/utils'
 import { UpcomingBadge } from '../badge/UpcomingBadge'
+import { BridgedIcon, ExternalIcon, NativeIcon } from '../icons'
 import { PercentChange } from '../PercentChange'
 
 export interface TvlStats {
@@ -19,23 +20,6 @@ export interface TvlSummaryProps {
 }
 
 export function TvlSummary(props: TvlSummaryProps) {
-  const tvlStats = props.stats
-    ? [
-        {
-          label: 'Canonically Bridged',
-          value: formatUSD(props.stats.cbv),
-        },
-        {
-          label: 'Externally Bridged',
-          value: formatUSD(props.stats.ebv),
-        },
-        {
-          label: 'Natively Minted',
-          value: formatUSD(props.stats.nmv),
-        },
-      ]
-    : []
-
   const usage = props.stats
     ? {
         cbv: ((props.stats.cbv / props.stats.tvl) * 100).toFixed(),
@@ -44,10 +28,33 @@ export function TvlSummary(props: TvlSummaryProps) {
       }
     : undefined
 
+  const tvlStats = props.stats
+    ? [
+        {
+          label: 'Canonically Bridged',
+          value: formatUSD(props.stats.cbv),
+          usage: usage?.cbv,
+          icon: <BridgedIcon className="h-[8px] w-[8px]" />,
+        },
+        {
+          label: 'Externally Bridged',
+          value: formatUSD(props.stats.ebv),
+          usage: usage?.ebv,
+          icon: <ExternalIcon className="h-[10px] w-[10px]" />,
+        },
+        {
+          label: 'Natively Minted',
+          value: formatUSD(props.stats.nmv),
+          usage: usage?.nmv,
+          icon: <NativeIcon className="h-[8px] w-[8px]" />,
+        },
+      ]
+    : []
+
   return props.detailedTvlEnabled ? (
     <div className="w-full bg-gray-100 p-4 dark:bg-zinc-800 md:flex md:w-[30%] md:flex-col md:gap-3 md:rounded-lg md:px-6 md:py-5">
       <div className="flex w-full items-baseline justify-between md:gap-2">
-        <span className="text-lg font-bold text-white text-gray-500 dark:text-gray-600 md:hidden md:text-xs md:font-normal">
+        <span className="text-lg font-medium md:hidden md:text-xs md:font-normal md:text-gray-500 md:dark:text-gray-600">
           Value Locked
         </span>
         <span className="hidden text-lg font-bold text-white text-gray-500 dark:text-gray-600 md:block md:text-xs md:font-normal">
@@ -72,7 +79,7 @@ export function TvlSummary(props: TvlSummaryProps) {
 
       {usage && (
         <div
-          className="Tooltip flex h-1 w-full flex-wrap my-3 md:my-0"
+          className="Tooltip my-3 flex h-1 w-full flex-wrap md:my-0"
           title={renderToStaticMarkup(<BreakdownTooltip usage={usage} />)}
         >
           <div
@@ -101,10 +108,20 @@ export function TvlSummary(props: TvlSummaryProps) {
           <div className="h-1/2">
             {tvlStats.map((s, i) => (
               <div key={i} className="flex w-full items-end justify-between">
-                <span className="text-xs text-gray-500 dark:text-gray-600">
-                  {s.label}
+                <div className="flex items-center gap-1">
+                  <div className="flex h-[10px] w-[10px] items-center justify-center md:hidden">
+                    {s.icon}
+                  </div>
+                  <span className="text-xs text-gray-500 dark:text-gray-600">
+                    {s.label}
+                  </span>
+                </div>
+                <span className="text-base font-semibold">
+                  {s.value}
+                  <span className="text-base font-normal text-gray-500 md:hidden">
+                    {` (${s.usage}%)`}
+                  </span>
                 </span>
-                <span className="text-base font-bold">{s.value}</span>
               </div>
             ))}
           </div>
