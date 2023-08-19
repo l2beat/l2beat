8cde346229b5810ef0d7db0e664e7fb5387e0056
Mateusz Radomski <mateusz.radomski@l2beat.com>
2023-08-19 08:00:45 +0200

    Close SlideCard if tap clicked outside the scrolling region

--- packages/frontend/src/scripts/configureSlideCard.ts
+++ packages/frontend/src/scripts/configureSlideCard.ts
@@ -25,6 +25,10 @@ export function configureSlideCards() {
       slideCard.dispatchEvent(new Event('close-slidecard'))
     })
 
+    content.addEventListener('click', () => {
+      slideCard.dispatchEvent(new Event('close-slidecard'))
+    })
+
     slideCard.addEventListener('close-slidecard', () => {
       content.classList.add('translate-y-full')
       document.body.classList.remove('w-screen', 'h-screen', 'overflow-hidden')
