da47bd2785f025471d2b96fbb2b1296fe8533d96
Maciej Zygmunt <maciekzygmunt@interia.pl>
2023-08-25 13:40:31 +0200

    update technology tooltip text

--- packages/frontend/src/components/table/props/getScalingTableColumns.tsx
+++ packages/frontend/src/components/table/props/getScalingTableColumns.tsx
@@ -220,7 +220,7 @@ export function getUpcomingScalingTvlColumns() {
     {
       name: 'Technology',
       tooltip:
-        'Type of this Layer 2. Determines data availability and proof system used.',
+        'Type of this Layer 2. Determines data availability and proof system used.<br>ZK Rollups = Validity Proofs + onchain data<br>Optimistic Rollups = Fraud Proofs + onchain data<br>Validiums = Validity Proofs + offchain data<br>Optimiums = Fraud Proofs + offchain data',
       shortName: 'Tech',
       getValue: (project) => (
         <TechnologyCell provider={project.provider}>
@@ -262,7 +262,7 @@ export function getArchivedScalingTvlColumns(detailedTvlEnabled: boolean) {
     {
       name: 'Technology',
       tooltip:
-        'Type of this Layer 2. Determines data availability and proof system used.',
+        'Type of this Layer 2. Determines data availability and proof system used.<br>ZK Rollups = Validity Proofs + onchain data<br>Optimistic Rollups = Fraud Proofs + onchain data<br>Validiums = Validity Proofs + offchain data<br>Optimiums = Fraud Proofs + offchain data',
       shortName: 'Tech',
       getValue: (project) => (
         <TechnologyCell provider={project.provider}>
