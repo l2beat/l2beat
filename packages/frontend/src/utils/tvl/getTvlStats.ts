7cb59c18ce437b20b99327d7d8158a42de4a875f
Maciej Zygmunt <maciekzygmunt@interia.pl>
2023-08-25 12:34:49 +0200

    minor fix

--- packages/frontend/src/utils/tvl/getTvlStats.ts
+++ packages/frontend/src/utils/tvl/getTvlStats.ts
@@ -20,7 +20,7 @@ export function getTvlStats(
   const tvl = aggregate.at(-1)?.[1] ?? 0
   const tvlOneDayAgo = aggregate.at(-25)?.[1] ?? 0
   // This assumes that hourly data spans exactly 7 days
-  const tvlSevenDaysAgo = aggregate[0][1]
+  const tvlSevenDaysAgo = aggregate.at(0)?.[1] ?? 0
 
   return {
     tvl,
