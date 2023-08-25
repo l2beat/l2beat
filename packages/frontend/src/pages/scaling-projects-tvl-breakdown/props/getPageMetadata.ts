b2cace3061fa99e8ecb38954ff0cc66c9848c83a
Maciej Zygmunt <maciekzygmunt@interia.pl>
2023-08-25 09:15:49 +0200

    change description metadata

--- packages/frontend/src/pages/scaling-projects-tvl-breakdown/props/getPageMetadata.ts
+++ packages/frontend/src/pages/scaling-projects-tvl-breakdown/props/getPageMetadata.ts
@@ -5,7 +5,7 @@ import { PageMetadata } from '../../Page'
 export function getPageMetadata(project: Layer2): PageMetadata {
   return {
     title: `${project.display.name} | TVL Breakdown – L2BEAT`,
-    description: `${project.display.name} project overview on L2BEAT. In depth layer 2 protocol analysis. Ethereum scaling analytics and research.`,
+    description: `${project.display.name} project TVL Breakdown overview on L2BEAT. In depth layer 2 protocol analysis. Ethereum scaling analytics and research.`,
     image: `https://l2beat.com/meta-images/${project.display.slug}.png`,
     url: `https://l2beat.com/scaling/projects/${project.display.slug}/tvl-breakdown`,
   }
