5068a790843d15fc4ac32a0d9c7ceb25e670bffe
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2023-09-08 14:12:09 +0200

    Use mock data in local and ci frontend builds (#1879)

--- packages/frontend/src/build/api/fetchActivityApi.ts
+++ packages/frontend/src/build/api/fetchActivityApi.ts
@@ -1,12 +1,46 @@
-import { ActivityApiResponse } from '@l2beat/shared-pure'
+import { layer2s as allLayer2s } from '@l2beat/config'
+import {
+  ActivityApiChart,
+  ActivityApiResponse,
+  UnixTime,
+} from '@l2beat/shared-pure'
 
 import { JsonHttpClient } from '../caching/JsonHttpClient'
+import { Config } from '../config'
 
 export async function fetchActivityApi(
-  apiUrl: string,
+  backend: Config['backend'],
   http: JsonHttpClient,
 ): Promise<ActivityApiResponse> {
-  const url = apiUrl + '/api/activity'
+  if (backend.mock) {
+    return getMockActivityApiResponse()
+  }
+  const url = backend.apiUrl + '/api/activity'
   const json = await http.fetchJson(url)
   return ActivityApiResponse.parse(json)
+}
+
+function getMockActivityApiResponse(): ActivityApiResponse {
+  const result: ActivityApiResponse = {
+    combined: getMockActivityApiChart(),
+    projects: {},
+    ethereum: getMockActivityApiChart(),
+  }
+  for (const project of allLayer2s) {
+    result.projects[project.id.toString()] = getMockActivityApiChart()
+  }
+  return result
+}
+
+function getMockActivityApiChart(): ActivityApiChart {
+  const now = UnixTime.now().toStartOf('day')
+  const chart: ActivityApiChart = {
+    types: ['timestamp', 'daily tx count'],
+    data: [],
+  }
+  for (let i = -365; i <= 0; i++) {
+    const timestamp = now.add(i, 'days')
+    chart.data.push([timestamp, 50])
+  }
+  return chart
 }
