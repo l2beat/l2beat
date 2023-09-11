3222826f5f809950da3afa221f86dcb942dbea95
Mateusz Radomski <radomski.main@protonmail.com>
2023-09-11 08:31:14 +0200

    Fix merge conflict in Aevo

--- packages/config/src/layer2s/aevo.ts
+++ packages/config/src/layer2s/aevo.ts
@@ -45,9 +45,6 @@ export const aevo: Layer2 = {
     },
     activityDataSource: 'Blockchain RPC',
   },
-  stage: {
-    stage: 'UnderReview',
-  },
   config: {
     escrows: [
       discovery.getEscrowDetails({
