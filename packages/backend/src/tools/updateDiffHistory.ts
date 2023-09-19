678dcf94f7ac832f425fb7aa50df2a1d641f8cdc
Adrian Adamiak <adrian@adamiak.net>
2023-09-19 13:38:12 +0000

    Refactor updateDiffHistory

--- packages/backend/src/tools/updateDiffHistory.ts
+++ packages/backend/src/tools/updateDiffHistory.ts
@@ -10,6 +10,47 @@ import { execSync } from 'child_process'
 import { writeFileSync } from 'fs'
 import { toUpper } from 'lodash'
 
+export async function updateDiffHistoryFile() {
+  console.log('Updating diff history file...')
+  const params = process.argv.filter((v) => !v.startsWith('-'))
+  console.log(params)
+  const chainName = params[2]
+  const projectName = params[3]
+  if (!chainName || !projectName) {
+    console.error('Pass parameters: <chainName> <projectName>')
+    process.exit(1)
+  }
+  const chainId = ChainId.fromName(chainName)
+
+  console.log(`Project: ${projectName}`)
+  const configReader = new ConfigReader()
+  const curDiscovery = await configReader.readDiscovery(projectName, chainId)
+  const config = await configReader.readConfig(projectName, chainId)
+  const discoveryFolder = `./discovery/${projectName}/${chainName}`
+  const { content: DiscoveryJsonFromMainBranch, mainBranchHash } =
+    getFileVersionOnMainBranch(`${discoveryFolder}/discovered.json`)
+  const discoveryFromMainBranch =
+    DiscoveryJsonFromMainBranch === ''
+      ? { contracts: [] }
+      : (JSON.parse(DiscoveryJsonFromMainBranch) as DiscoveryOutput)
+  const diff = diffDiscovery(
+    discoveryFromMainBranch.contracts,
+    curDiscovery.contracts,
+    config,
+  )
+
+  if (diff.length > 0) {
+    const newHistoryEntry = generateDiffHistoryMarkdown(diff, mainBranchHash)
+    const diffHistoryPath = `${discoveryFolder}/diffHistory.md`
+    const { content: historyFileFromMainBranch } =
+      getFileVersionOnMainBranch(diffHistoryPath)
+    const diffHistory = newHistoryEntry.concat('\n' + historyFileFromMainBranch)
+    writeFileSync(diffHistoryPath, diffHistory)
+  } else {
+    console.log('No changes found')
+  }
+}
+
 function getMainBranchName(): 'main' | 'master' {
   try {
     if (execSync('git show-ref --verify refs/heads/master').toString().trim()) {
@@ -108,47 +149,6 @@ function generateDiffHistoryMarkdown(
   result.push('')
 
   return result.join('\n')
-}
-
-export async function updateDiffHistoryFile() {
-  console.log('Updating diff history file...')
-  const params = process.argv.filter((v) => !v.startsWith('-'))
-  console.log(params)
-  const chainName = params[2]
-  const projectName = params[3]
-  if (!chainName || !projectName) {
-    console.error('Pass parameters: <chainName> <projectName>')
-    process.exit(1)
-  }
-  const chainId = ChainId.fromName(chainName)
-
-  console.log(`Project: ${projectName}`)
-  const configReader = new ConfigReader()
-  const curDiscovery = await configReader.readDiscovery(projectName, chainId)
-  const config = await configReader.readConfig(projectName, chainId)
-  const discoveryFolder = `./discovery/${projectName}/${chainName}`
-  const { content: DiscoveryJsonFromMainBranch, mainBranchHash } =
-    getFileVersionOnMainBranch(`${discoveryFolder}/discovered.json`)
-  const discoveryFromMainBranch =
-    DiscoveryJsonFromMainBranch === ''
-      ? { contracts: [] }
-      : (JSON.parse(DiscoveryJsonFromMainBranch) as DiscoveryOutput)
-  const diff = diffDiscovery(
-    discoveryFromMainBranch.contracts,
-    curDiscovery.contracts,
-    config,
-  )
-
-  if (diff.length > 0) {
-    const newHistoryEntry = generateDiffHistoryMarkdown(diff, mainBranchHash)
-    const diffHistoryPath = `${discoveryFolder}/diffHistory.md`
-    const { content: historyFileFromMainBranch } =
-      getFileVersionOnMainBranch(diffHistoryPath)
-    const diffHistory = newHistoryEntry.concat('\n' + historyFileFromMainBranch)
-    writeFileSync(diffHistoryPath, diffHistory)
-  } else {
-    console.log('No changes found')
-  }
 }
 
 void updateDiffHistoryFile()
