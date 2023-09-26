565ff5554a99ef23600aaeff5e00143e57fa1270
Maciej Zygmunt <maciekzygmunt@interia.pl>
2023-09-26 20:37:47 +0200

    change sponsor deck for new one

--- packages/frontend/src/pages/l2days/index.tsx
+++ packages/frontend/src/pages/l2days/index.tsx
@@ -10,7 +10,7 @@ export function getL2DaysPage() {
     page: (
       <PageWrapper {...wrapper}>
         <iframe
-          src="https://drive.google.com/file/d/1IxAQHXDb4mamvI5rYzBddmjToDp5uZiN/preview"
+          src="https://drive.google.com/file/d/1pydpwCuUJD92syjFwySTS3YjHwyfgKQ6/preview"
           className="h-screen w-full"
         />
       </PageWrapper>
