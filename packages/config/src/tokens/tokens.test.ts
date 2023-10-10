bee4220ce28246c99d2830717b26d1a15ec4ff79
antooni <antoni.pawlak@protonmail.com>
2023-10-10 13:16:04 +0200

    improve Multicall error handling

--- packages/config/src/tokens/tokens.test.ts
+++ packages/config/src/tokens/tokens.test.ts
@@ -74,8 +74,25 @@ describe('tokens', () => {
                 [x.address, DECIMALS],
               ],
         )
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-        const data: string[] = (await contract.functions.aggregate(calls))[1]
+        let data: string[] = []
+
+        try {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+          data = (await contract.functions.aggregate(calls))[1]
+        } catch (error) {
+          // @ts-expect-error Alchemy error is not typed
+          const errorBody = error.error.body
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+          const message = JSON.parse(errorBody).error.message
+
+          if (message) {
+            // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
+            throw new Error('Multicall failed: ' + message)
+          } else {
+            throw error
+          }
+        }
+
         for (let i = 0; i < calls.length; i += 3) {
           const nameResult = data[i]
           const symbolResult = data[i + 1]
