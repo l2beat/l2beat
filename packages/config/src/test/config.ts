6905dc7fdbe48e6d7e91cca677d834df536ca49c
antooni <antoni.pawlak@protonmail.com>
2023-10-10 12:51:31 +0200

    Revert "fix config issue"
    This reverts commit fefe4cf07139f14d3c01a1be31fe6842a541dba7.

--- packages/config/src/test/config.ts
+++ packages/config/src/test/config.ts
@@ -2,11 +2,15 @@ import { getEnv } from '@l2beat/backend-tools'
 
 interface Config {
   alchemyApiKey?: string
+  githubActionKey?: string
 }
 
 export const config: Config = {
   alchemyApiKey:
     // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-    getEnv().optionalString('CONFIG_ALCHEMY_API_KEY') ||
-    'mlGD422scpwVOpn3lye_swHEebbKQy0D',
+    getEnv().string(
+      'CONFIG_ALCHEMY_API_KEY',
+      'mlGD422scpwVOpn3lye_swHEebbKQy0D',
+    ),
+  githubActionKey: getEnv().optionalString('CONFIG_ALCHEMY_API_KEY'),
 }
