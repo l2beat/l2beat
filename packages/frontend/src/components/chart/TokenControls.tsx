d721b0c091f9475d04303f809658ec1b506e0f3e
torztomasz <tomasz.torz@l2beat.com>
2023-10-10 15:07:48 +0200

    adjust styles

--- packages/frontend/src/components/chart/TokenControls.tsx
+++ packages/frontend/src/components/chart/TokenControls.tsx
@@ -64,7 +64,7 @@ function TokenList({ tokens }: { tokens: TokenControl[] }) {
               </div>
               <HorizontalSeparator className="mb-4 border-gray-400 dark:border-gray-650" />
               <div
-                className="grid grid-cols-1 gap-y-3 gap-x-6 md:grid-cols-3 md:gap-y-0"
+                className="grid grid-cols-1 gap-x-6 md:grid-cols-3"
                 data-role="chart-token-controls"
               >
                 {p.tokens.map((token, j) => (
