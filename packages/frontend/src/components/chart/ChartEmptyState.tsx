b1b5890664d7f875c17a3514bbb8a376a6e63720
torztomasz <tomasz.torz@l2beat.com>
2023-10-10 15:08:51 +0200

    remove todo

--- packages/frontend/src/components/chart/ChartEmptyState.tsx
+++ packages/frontend/src/components/chart/ChartEmptyState.tsx
@@ -4,8 +4,7 @@ import { Link } from '../Link'
 
 export function ChartEmptyState() {
   return (
-    //TODO: (filter) change bg to neutral-900 after merge
-    <div className="absolute inset-0 -z-1 flex select-none flex-col items-center justify-center bg-gradient-to-t from-white opacity-0 transition-opacity duration-200 group-data-[state=empty]/chart:z-30 group-data-[state=empty]/chart:opacity-100 dark:from-black">
+    <div className="absolute inset-0 -z-1 flex select-none flex-col items-center justify-center bg-gradient-to-t from-white opacity-0 transition-opacity duration-200 group-data-[state=empty]/chart:z-30 group-data-[state=empty]/chart:opacity-100 dark:from-neutral-900">
       <span className="mb-4 text-2xl font-semibold">No results</span>
       <span className="mb-6">There are no results meeting the criteria</span>
       <Link className="ProjectFilters-ResetButton cursor-pointer">
