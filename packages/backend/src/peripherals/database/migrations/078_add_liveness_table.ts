2828a5733e832c20713ea23d260656fafcebc913
Maciej Zygmunt <maciekzygmunt@interia.pl>
2023-10-10 13:56:04 +0200

    add liveness table and livenessRepo

---
+++ packages/backend/src/peripherals/database/migrations/078_add_liveness_table.ts
@@ -0,0 +1,29 @@
+/*
+                      ====== IMPORTANT NOTICE ======
+
+DO NOT EDIT OR RENAME THIS FILE
+
+This is a migration file. Once created the file should not be renamed or edited,
+because migrations are only run once on the production server. 
+
+If you find that something was incorrectly set up in the `up` function you
+should create a new migration file that fixes the issue.
+
+*/
+
+import { Knex } from 'knex'
+
+export async function up(knex: Knex) {
+  await knex.schema.createTable('liveness', function (table) {
+    table.string('project_id').notNullable()
+    table.timestamp('timestamp').notNullable()
+    table.integer('block_number').notNullable()
+    table.string('tx_hash').primary()
+    table.string('type').notNullable()
+    table.index(['project_id', 'timestamp'])
+  })
+}
+
+export async function down(knex: Knex) {
+  await knex.schema.dropTable('liveness')
+}
