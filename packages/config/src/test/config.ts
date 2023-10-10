fefe4cf07139f14d3c01a1be31fe6842a541dba7
antooni <antoni.pawlak@protonmail.com>
2023-10-10 12:29:44 +0200

    fix config issue

--- packages/config/src/test/config.ts
+++ packages/config/src/test/config.ts
@@ -2,15 +2,11 @@ import { getEnv } from '@l2beat/backend-tools'
 
 interface Config {
   alchemyApiKey?: string
-  githubActionKey?: string
 }
 
 export const config: Config = {
   alchemyApiKey:
     // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-    getEnv().string(
-      'CONFIG_ALCHEMY_API_KEY',
-      'mlGD422scpwVOpn3lye_swHEebbKQy0D',
-    ),
-  githubActionKey: getEnv().optionalString('CONFIG_ALCHEMY_API_KEY'),
+    getEnv().optionalString('CONFIG_ALCHEMY_API_KEY') ||
+    'mlGD422scpwVOpn3lye_swHEebbKQy0D',
 }
