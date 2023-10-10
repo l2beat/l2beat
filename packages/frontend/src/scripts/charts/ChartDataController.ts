a0bae8740ee46640bf9ab7ce83f8b99d802ad1ae
torztomasz <tomasz.torz@l2beat.com>
2023-10-10 17:48:33 +0200

    experimental fetching

--- packages/frontend/src/scripts/charts/ChartDataController.ts
+++ packages/frontend/src/scripts/charts/ChartDataController.ts
@@ -36,6 +36,23 @@ export class ChartDataController {
     this.chartViewController.showLoader()
     const chartType = this.chartType
     const url = getChartUrl(chartType)
+
+    if (Array.isArray(url)) {
+      const responses = url.map((url) =>
+        fetch(url, { signal: this.abortController!.signal }),
+      )
+      void Promise.all(responses)
+        .then((responses) => Promise.all(responses.map((res) => res.json())))
+        .then((data: unknown[]) =>
+          data.map((data) => AggregateDetailedTvlResponse.parse(data)),
+        )
+        .then((data) => {
+          this.parseAndConfigure(chartType, groupAndSumData(data))
+        })
+        .finally(() => this.chartViewController.hideLoader())
+      return
+    }
+
     if (this.cache.has(url)) {
       this.parseAndConfigure(chartType, this.cache.get(url))
       this.chartViewController.hideLoader()
@@ -97,7 +114,7 @@ export function getChartUrl(chartType: ChartType) {
     case 'layer2-tvl':
     case 'layer2-detailed-tvl':
       return chartType.filteredSlugs
-        ? `/api/tvl/aggregate?projectSlugs=${chartType.filteredSlugs.join(',')}`
+        ? chartType.filteredSlugs.map((slug) => `/api/tvl/${slug}.json`)
         : '/api/tvl/scaling.json'
     case 'layer2-activity':
       return chartType.filteredSlugs
@@ -127,4 +144,145 @@ export function getTokenTvlUrl(info: TokenInfo) {
   const chainId = 'chainId' in info ? info.chainId : 1
   const type = info.type === 'regular' ? 'CBV' : info.type
   return `/api/projects/${info.projectId}/tvl/chains/${chainId}/assets/${info.assetId}/types/${type}`
+}
+
+function groupAndSumData(
+  dataArray: AggregateDetailedTvlResponse[],
+): AggregateDetailedTvlResponse {
+  const groupedData: AggregateDetailedTvlResponse = {
+    hourly: {
+      data: [],
+      types: dataArray[0].hourly.types,
+    },
+    sixHourly: {
+      data: [],
+      types: dataArray[0]?.sixHourly.types,
+    },
+    daily: {
+      data: [],
+      types: dataArray[0]?.daily.types,
+    },
+  }
+
+  const timestampIndex = groupedData.hourly.types.indexOf('timestamp')
+
+  // Create a map to group data by timestamp
+  const dailyGroupedByTimestamp = new Map<
+    number,
+    [number, number, number, number, number, number, number, number, number]
+  >()
+  const hourlyGroupedByTimestamp = new Map<
+    number,
+    [number, number, number, number, number, number, number, number, number]
+  >()
+  const sixHourlyGroupedByTimestamp = new Map<
+    number,
+    [number, number, number, number, number, number, number, number, number]
+  >()
+
+  for (const data of dataArray) {
+    for (const daily of data.daily.data) {
+      const timestamp = daily[0]
+      const valueIndices = [1, 2, 3, 4, 5, 6, 7, 8] // Indices of values to sum
+
+      const groupedDataArray = dailyGroupedByTimestamp.get(timestamp)
+
+      if (!groupedDataArray) {
+        dailyGroupedByTimestamp.set(timestamp, [
+          timestamp,
+          daily[1],
+          daily[2],
+          daily[3],
+          daily[4],
+          daily[5],
+          daily[6],
+          daily[7],
+          daily[8],
+        ])
+        continue
+      }
+
+      for (const index of valueIndices) {
+        groupedDataArray[index] += daily[index]
+      }
+
+      dailyGroupedByTimestamp.set(timestamp, groupedDataArray)
+    }
+    for (const hourly of data.hourly.data) {
+      const timestamp = hourly[0]
+      const valueIndices = [1, 2, 3, 4, 5, 6, 7, 8] // Indices of values to sum
+
+      const groupedDataArray = hourlyGroupedByTimestamp.get(timestamp)
+
+      if (!groupedDataArray) {
+        hourlyGroupedByTimestamp.set(timestamp, [
+          timestamp,
+          hourly[1],
+          hourly[2],
+          hourly[3],
+          hourly[4],
+          hourly[5],
+          hourly[6],
+          hourly[7],
+          hourly[8],
+        ])
+        continue
+      }
+
+      for (const index of valueIndices) {
+        groupedDataArray[index] += hourly[index]
+      }
+
+      hourlyGroupedByTimestamp.set(timestamp, groupedDataArray)
+    }
+    for (const sixHourly of data.sixHourly.data) {
+      const timestamp = sixHourly[0]
+      const valueIndices = [1, 2, 3, 4, 5, 6, 7, 8] // Indices of values to sum
+
+      const groupedDataArray = sixHourlyGroupedByTimestamp.get(timestamp)
+
+      if (!groupedDataArray) {
+        sixHourlyGroupedByTimestamp.set(timestamp, [
+          timestamp,
+          sixHourly[1],
+          sixHourly[2],
+          sixHourly[3],
+          sixHourly[4],
+          sixHourly[5],
+          sixHourly[6],
+          sixHourly[7],
+          sixHourly[8],
+        ])
+        continue
+      }
+
+      for (const index of valueIndices) {
+        groupedDataArray[index] += sixHourly[index]
+      }
+
+      sixHourlyGroupedByTimestamp.set(timestamp, groupedDataArray)
+    }
+  }
+
+  // Convert the map back to an array
+  const hourlyData = Array.from(hourlyGroupedByTimestamp.values())
+
+  // Sort the array by timestamp
+
+  // Convert the map back to an array
+  const dailyData = Array.from(dailyGroupedByTimestamp.values())
+
+  // Sort the array by timestamp
+
+  // Convert the map back to an array
+  const sixHourlyData = Array.from(sixHourlyGroupedByTimestamp.values())
+
+  // Sort the array by timestamp
+
+  // Extract data back into the result object
+  groupedData.hourly.data = hourlyData
+  groupedData.sixHourly.data = sixHourlyData // You can modify this part as needed
+  groupedData.daily.data = dailyData // You can modify this part as needed
+
+  return groupedData
 }
