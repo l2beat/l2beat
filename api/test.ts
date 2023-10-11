3410af25cd4464dcd30461b5a2b14b859efef662
torztomasz <tomasz.torz@l2beat.com>
2023-10-11 10:49:10 +0200

    test

---
+++ api/test.ts
@@ -0,0 +1,9 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
+// eslint-disable-next-line import/no-default-export
+export default function handler(
+  request: VercelRequest,
+  response: VercelResponse
+) {
+  response.status(200).send("Hello, world!");
+}
