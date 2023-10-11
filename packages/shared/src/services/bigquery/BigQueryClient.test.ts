511e67f578b3d77455e7ce13ab29d30636d7697a
Maciej Zygmunt <maciekzygmunt@interia.pl>
2023-10-11 14:47:28 +0200

    Liveness refactor (#2030)
    Co-authored-by: Antoni Pawlak <antoni.pawlak@pm.me>

--- packages/shared/src/services/bigquery/BigQueryClient.test.ts
+++ packages/shared/src/services/bigquery/BigQueryClient.test.ts
@@ -1,97 +1,122 @@
+import { EthereumAddress, UnixTime } from '@l2beat/shared-pure'
 import { expect, mockFn, mockObject } from 'earl'
 
-import { BigQueryClient } from './BigQueryClient'
+import {
+  BigQueryClient,
+  FunctionCallQueryParams,
+  TransferQueryParams,
+} from './BigQueryClient'
 import { BigQueryProvider } from './BigQueryProvider'
 import { BigQueryWrapper } from './BigQueryWrapper'
-import { Method, Transfer } from './config'
-
-const transfersToQuery: Transfer[] = [
-  {
-    from_address: '0x1234567890abcdef',
-    to_address: '0xabcdef1234567890',
-  },
-  {
-    from_address: '0xabcdef1234567890',
-    to_address: '0x1234567890abcdef',
-  },
-]
-
-const methodsToQuery: Method[] = [
-  {
-    name: 'method1',
-    address: '0x1234567890abcdef',
-    selector: '0xabcdef',
-  },
-  {
-    name: 'method2',
-    address: '0xabcdef1234567890',
-    selector: '0x123456',
-  },
-]
 
 describe('BigQueryClient', () => {
   it('should return valid data for transfers', async () => {
-    const transfers = [
+    const transfersToQuery: TransferQueryParams[] = [
+      {
+        from: EthereumAddress.random(),
+        to: EthereumAddress.random(),
+      },
+      {
+        from: EthereumAddress.random(),
+        to: EthereumAddress.random(),
+      },
+    ]
+
+    const bigQueryResponse = [
       {
         block_number: 12345,
-        to_address: '0x1234567890abcdef',
-        block_timestamp: '2022-01-01T00:00:00Z',
+        from_address: transfersToQuery[0].from.toLocaleLowerCase(),
+        to_address: transfersToQuery[0].to.toLocaleLowerCase(),
+        block_timestamp: { value: '2022-01-01T00:00:00Z' },
         transaction_hash: '0xabcdef1234567890',
       },
       {
         block_number: 12346,
-        to_address: '0xabcdef1234567890',
-        block_timestamp: '2022-01-01T00:01:00Z',
+        from_address: transfersToQuery[1].from.toLocaleLowerCase(),
+        to_address: transfersToQuery[1].to.toLocaleLowerCase(),
+        block_timestamp: { value: '2022-01-01T00:01:00Z' },
         transaction_hash: '0x1234567890abcdef',
       },
     ]
     const bigQuery = mockObject<BigQueryWrapper>({
       createQueryJob: mockFn().resolvesToOnce([
         {
-          getQueryResults: async () => [transfers],
+          getQueryResults: async () => [bigQueryResponse],
         },
       ]),
     })
     const queryProvider = new BigQueryProvider(bigQuery)
-    const results = await new BigQueryClient(queryProvider).makeTransfersQuery(
+
+    const results = await new BigQueryClient(queryProvider).getTransfers(
       transfersToQuery,
-      '2023-10-01 00:00:00 UTC',
-      '2023-10-01 01:00:00 UTC',
+      UnixTime.fromDate(new Date('2022-01-01T00:00:00Z')),
+      UnixTime.fromDate(new Date('2022-01-01T01:00:00Z')),
     )
-    expect(results).toEqual(transfers)
+
+    const expected = bigQueryResponse.map((transfer) => ({
+      ...transfer,
+      from_address: EthereumAddress(transfer.from_address),
+      to_address: EthereumAddress(transfer.to_address),
+      block_timestamp: UnixTime.fromDate(
+        new Date(transfer.block_timestamp.value),
+      ),
+    }))
+
+    expect(results).toEqual(expected)
   })
 
-  it('should return valid data for methods', async () => {
-    const methods = [
+  it('should return valid data for function calls', async () => {
+    const functionCallsToQuery: FunctionCallQueryParams[] = [
+      {
+        address: EthereumAddress.random(),
+        selector: '0xabcdef',
+      },
+      {
+        address: EthereumAddress.random(),
+        selector: '0x123456',
+      },
+    ]
+
+    const bigQueryResponse = [
       {
         block_number: 12345,
         input: 'some input',
-        to_address: '0x1234567890abcdef',
-        block_timestamp: '2022-01-01T00:00:00Z',
+        to_address: functionCallsToQuery[0].address.toLocaleLowerCase(),
+        block_timestamp: { value: '2022-01-01T00:00:00Z' },
         transaction_hash: '0xabcdef1234567890',
       },
       {
         block_number: 12346,
         input: 'some other input',
-        to_address: '0xabcdef1234567890',
-        block_timestamp: '2022-01-01T00:01:00Z',
+        to_address: functionCallsToQuery[1].address.toLocaleLowerCase(),
+        block_timestamp: { value: '2022-01-01T00:01:00Z' },
         transaction_hash: '0x1234567890abcdef',
       },
     ]
+
     const bigQuery = mockObject<BigQueryWrapper>({
       createQueryJob: mockFn().resolvesToOnce([
         {
-          getQueryResults: async () => [methods],
+          getQueryResults: async () => [bigQueryResponse],
         },
       ]),
     })
     const queryProvider = new BigQueryProvider(bigQuery)
-    const results = await new BigQueryClient(queryProvider).makeMethodsQuery(
-      methodsToQuery,
-      '2023-10-01 00:00:00 UTC',
-      '2023-10-01 01:00:00 UTC',
+    const results = await new BigQueryClient(queryProvider).getFunctionCalls(
+      functionCallsToQuery,
+      UnixTime.fromDate(new Date('2022-01-01T00:00:00Z')),
+      UnixTime.fromDate(new Date('2022-01-01T01:00:00Z')),
     )
-    expect(results).toEqual(methods)
+
+    const expected = bigQueryResponse.map((method) => ({
+      ...method,
+      to_address: EthereumAddress(method.to_address),
+      block_timestamp: UnixTime.fromDate(
+        new Date(method.block_timestamp.value),
+      ),
+    }))
+
+    expect(results).toEqual(expected)
   })
 
   it('should throw an error if the query fails', async () => {
@@ -106,10 +131,10 @@ describe('BigQueryClient', () => {
     })
     const queryProvider = new BigQueryProvider(bigQuery)
     await expect(
-      new BigQueryClient(queryProvider).makeTransfersQuery(
-        transfersToQuery,
-        '2023-10-01 00:00:00 UTC',
-        '2023-10-01 01:00:00 UTC',
+      new BigQueryClient(queryProvider).getTransfers(
+        [],
+        UnixTime.fromDate(new Date('2022-01-01T00:00:00Z')),
+        UnixTime.fromDate(new Date('2022-01-01T01:00:00Z')),
       ),
     ).toBeRejectedWith('Failed to fetch query results.')
   })
