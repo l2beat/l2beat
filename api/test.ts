983c270009cb92d6d16666a07acb48042394ee72
torztomasz <tomasz.torz@l2beat.com>
2023-10-11 11:21:36 +0200

    xd

--- api/test.ts
+++ api/test.ts
@@ -1,12 +1,9 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { readFileSync } from "fs";
 
 // eslint-disable-next-line import/no-default-export
 export default function handler(
   request: VercelRequest,
   response: VercelResponse
 ) {
-  const x = readFileSync("../packages/frontend/build/api/acrossv2-tvl.json");
-  const json: unknown = JSON.parse(x.toString());
-  response.status(200).json(json);
+  response.status(200).json({ path: __dirname });
 }
