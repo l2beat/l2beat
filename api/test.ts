b1005fc39629b93b13c2b71dfe5849f3f6b33694
torztomasz <tomasz.torz@l2beat.com>
2023-10-11 12:40:34 +0200

    use node-fetch

--- api/test.ts
+++ api/test.ts
@@ -2,6 +2,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import type { VercelRequest, VercelResponse } from "@vercel/node";
+import fetch from "node-fetch";
 // eslint-disable-next-line import/no-default-export
 export default async function handler(
   request: VercelRequest,
