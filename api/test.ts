67ec0d2fd8ae8268e0dc76fb4ae1ab36c58ebbf5
torztomasz <tomasz.torz@l2beat.com>
2023-10-11 11:12:39 +0200

    read project json from api

--- api/test.ts
+++ api/test.ts
@@ -1,9 +1,14 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
+import { readFileSync } from "fs";
 
 // eslint-disable-next-line import/no-default-export
 export default function handler(
   request: VercelRequest,
   response: VercelResponse
 ) {
-  response.status(200).send("Hello, world!");
+  const x = readFileSync(
+    "../packages/frontend/src/build/api/tvl/arbitrum.json"
+  );
+  const json: unknown = JSON.parse(x.toString());
+  response.status(200).json(json);
 }
