eedcdb820eebb653f3c2c2dff518e28e1c3dfd51
antooni <antoni.pawlak@protonmail.com>
2023-10-12 20:10:14 +0200

    refactor configs

--- packages/backend/src/core/liveness/types/LivenessConfig.ts
+++ packages/backend/src/core/liveness/types/LivenessConfig.ts
@@ -2,17 +2,18 @@ import { FunctionCallQueryParams, TransferQueryParams } from '@l2beat/shared'
 import { LivenessType, ProjectId, UnixTime } from '@l2beat/shared-pure'
 
 export interface LivenessFunctionCall extends FunctionCallQueryParams {
+  projectId: ProjectId
   type: LivenessType
   untilTimestamp?: UnixTime
 }
 
 export interface LivenessTransfer extends TransferQueryParams {
+  projectId: ProjectId
   type: LivenessType
   untilTimestamp?: UnixTime
 }
 
 export interface LivenessConfig {
-  projectId: ProjectId
-  transfers?: LivenessTransfer[]
-  functionCalls?: LivenessFunctionCall[]
+  transfers: LivenessTransfer[]
+  functionCalls: LivenessFunctionCall[]
 }
