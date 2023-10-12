d6716bc1664193e7a1e3d6199ac9105e43ea80f9
Amin Latifi <a.latifi.al@gmail.com>
2023-10-12 15:25:32 +0330

    Added more index to activity view

--- packages/backend/src/peripherals/database/migrations/079_speed_up_activity_view.ts
+++ packages/backend/src/peripherals/database/migrations/079_speed_up_activity_view.ts
@@ -13,15 +13,20 @@ should create a new migration file that fixes the issue.
 
 import { Knex } from 'knex'
 
-const indexName = 'activity_daily_count_view_timestamp_index'
+const timestampIndexName = 'activity_daily_count_view_timestamp_index'
+const projectTimestampIndexName = 'activity_daily_count_view_project_timestamp_index'
 const materializedViewName = 'activity.daily_count_view'
 
 export async function up(knex: Knex) {
   await knex.schema.raw(
-    ` CREATE INDEX IF NOT EXISTS ${indexName} ON ${materializedViewName} (unix_timestamp);`,
+    ` CREATE INDEX IF NOT EXISTS ${timestampIndexName} ON ${materializedViewName} (unix_timestamp);`,
   )
+  await knex.schema.raw(
+      `CREATE INDEX IF NOT EXISTS ${projectTimestampIndexName} ON ${materializedViewName} (project_id, unix_timestamp);`
+  );
 }
 
 export async function down(knex: Knex) {
-  await knex.schema.raw(`DROP INDEX IF EXISTS ${indexName}`)
+  await knex.schema.raw(`DROP INDEX IF EXISTS ${timestampIndexName}`)
+  await knex.schema.raw(`DROP INDEX IF EXISTS ${projectTimestampIndexName}`)
 }
