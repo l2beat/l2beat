882f9d6d1e18ab69029f915a70c79dad60dcfa0b
gregKlk <147268523+gregKlk@users.noreply.github.com>
2023-10-12 12:10:18 +0200

    Add canto as upcoming (#2028)
    Co-authored-by: Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
    Co-authored-by: Luca Donno <donnoh99@gmail.com>

--- packages/config/src/layer2s/index.ts
+++ packages/config/src/layer2s/index.ts
@@ -7,6 +7,7 @@ import { aztecconnect } from './aztecconnect'
 import { base } from './base'
 import { bobanetwork } from './bobanetwork'
 import { brine } from './brine'
+import { canto } from './canto'
 import { canvasconnect } from './canvasconnect'
 import { capx } from './capx'
 import { HOMEPAGE_MILESTONES } from './common'
@@ -67,6 +68,7 @@ export const layer2s: Layer2[] = [
   base,
   bobanetwork,
   brine,
+  canto,
   capx,
   canvasconnect,
   honeypot,
