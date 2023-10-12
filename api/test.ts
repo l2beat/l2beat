2c789fb3a9badd1153d4b9faddf6e8d70a96e06a
torztomasz <tomasz.torz@l2beat.com>
2023-10-12 12:50:06 +0200

    readdir

--- api/test.ts
+++ api/test.ts
@@ -7,17 +7,59 @@ export default function handler(
   request: VercelRequest,
   response: VercelResponse
 ) {
-  const currentDir = readdirSync("./");
-  const currentDir2 = readdirSync(__dirname);
-  const prevDir = readdirSync("../");
-  const prevPrevDir = readdirSync("../../");
-  const prevPrevPrevDir = readdirSync("../../../");
+  const apiDir = readdirSync("./api");
+  const adm = readdirSync("../adm");
+  const cache = readdirSync("../cache");
+  const db = readdirSync("../db");
+  const empty = readdirSync("../empty");
+  const games = readdirSync("../games");
+  const gopher = readdirSync("../gopher");
+  const kerberos = readdirSync("../kerberos");
+  const lang = readdirSync("../lang");
+  const lib = readdirSync("../lib");
+  const local = readdirSync("../local");
+  const lock = readdirSync("../lock");
+  const log = readdirSync("../log");
+  const mail = readdirSync("../mail");
+  const nis = readdirSync("../nis");
+  const opt = readdirSync("../opt");
+  const preserve = readdirSync("../preserve");
+  const rapid = readdirSync("../rapid");
+  const run = readdirSync("../run");
+  const runtime = readdirSync("../runtime");
+  const spool = readdirSync("../spool");
+  const task = readdirSync("../task");
+  const telemetry = readdirSync("../telemetry");
+  const tmp = readdirSync("../tmp");
+  const tracer = readdirSync("../tracer");
+  const yp = readdirSync("../yp");
 
   response.status(200).json({
-    currentDir,
-    currentDir2,
-    prevDir,
-    prevPrevDir,
-    prevPrevPrevDir,
+    apiDir,
+    adm,
+    cache,
+    db,
+    empty,
+    games,
+    gopher,
+    kerberos,
+    lang,
+    lib,
+    local,
+    lock,
+    log,
+    mail,
+    nis,
+    opt,
+    preserve,
+    rapid,
+    run,
+    runtime,
+    spool,
+    task,
+    telemetry,
+    tmp,
+    tracer,
+    yp,
   });
 }
