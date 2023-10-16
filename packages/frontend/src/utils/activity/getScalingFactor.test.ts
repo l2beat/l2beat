3362463cd768b75ff878c115d3b624c2f932c468
torztomasz <tomasz.torz@l2beat.com>
2023-10-16 14:38:11 +0200

    fix frontend tests

--- packages/frontend/src/utils/activity/getScalingFactor.test.ts
+++ packages/frontend/src/utils/activity/getScalingFactor.test.ts
@@ -1,4 +1,4 @@
-import { ActivityApiResponse, UnixTime } from '@l2beat/shared-pure'
+import { ActivityApiResponse } from '@l2beat/shared-pure'
 import { expect } from 'earl'
 
 import { getScalingFactor } from './getScalingFactor'
@@ -8,31 +8,20 @@ const ONE_TPS = 24 * 60 * 60
 describe(getScalingFactor.name, () => {
   it('calculates correctly', () => {
     const apiResponse: ActivityApiResponse = {
-      ethereum: {
-        types: ['timestamp', 'daily tx count'],
-        data: [
-          [new UnixTime(0), ONE_TPS],
-          [new UnixTime(1), ONE_TPS],
-          [new UnixTime(2), ONE_TPS],
-          [new UnixTime(3), ONE_TPS],
-          [new UnixTime(4), ONE_TPS],
-          [new UnixTime(5), ONE_TPS],
-          [new UnixTime(6), ONE_TPS],
-          [new UnixTime(7), ONE_TPS],
-        ],
-      },
       combined: {
-        types: ['timestamp', 'daily tx count'],
-        data: [
-          [new UnixTime(0), ONE_TPS],
-          [new UnixTime(1), ONE_TPS],
-          [new UnixTime(2), ONE_TPS],
-          [new UnixTime(3), ONE_TPS],
-          [new UnixTime(4), ONE_TPS],
-          [new UnixTime(5), ONE_TPS],
-          [new UnixTime(6), ONE_TPS],
-          [new UnixTime(7), ONE_TPS],
-        ],
+        daily: {
+          types: ['timestamp', 'transactions', 'ethereumTransactions'],
+          data: [
+            [0, ONE_TPS, ONE_TPS],
+            [1, ONE_TPS, ONE_TPS],
+            [2, ONE_TPS, ONE_TPS],
+            [3, ONE_TPS, ONE_TPS],
+            [4, ONE_TPS, ONE_TPS],
+            [5, ONE_TPS, ONE_TPS],
+            [6, ONE_TPS, ONE_TPS],
+            [7, ONE_TPS, ONE_TPS],
+          ],
+        },
       },
       projects: {},
     }
