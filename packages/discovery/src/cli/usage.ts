50f3857232f5fa06006631ac58378d517beb4fd0
Mateusz Radomski <33978857+mateuszradomski@users.noreply.github.com>
2023-10-16 12:15:10 +0200

    Align description and fix color in the help menu (l2beat/tools#65)
    * Align the help menu
    
    * Fix chalk not coloring one line

--- packages/discovery/src/cli/usage.ts
+++ packages/discovery/src/cli/usage.ts
@@ -3,12 +3,12 @@ import chalk from 'chalk'
 const usage = `Usage:
     yarn start ...................................... run the server application
     yarn discover [chain] [project] ................. run discovery on a specific system
-    yarn discover [chain] [project] --dry-run ..... check simulated discovery bot output
-    yarn discover [chain] [project] --dev ..... run on the same block number as in .json
-    yarn invert [chain] [project] ..................... print addresses and their functions
-    yarn invert [chain] [project] --mermaid .................... print mermaid graph markup
-    yarn discover:single [chain] [address]........ run a discovery on the address (no config needed, useful for experimenting)
-    yarn <start|discover> --help .......................... display this message
+    yarn discover [chain] [project] --dry-run ....... check simulated discovery bot output
+    yarn discover [chain] [project] --dev ........... run on the same block number as in .json
+    yarn invert [chain] [project] ................... print addresses and their functions
+    yarn invert [chain] [project] --mermaid ......... print mermaid graph markup
+    yarn discover:single [chain] [address] .......... run a discovery on the address (no config needed, useful for experimenting)
+    yarn <start|discover> --help .................... display this message
 
     supported chains: checkout config.discovery.ts
 `
