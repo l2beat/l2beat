deeac27fd33fa82304ced06f13582a4ebed278d4
Amin Latifi <a.latifi.al@gmail.com>
2023-10-17 22:47:50 +0330

    Removed a comment line

--- packages/backend/discovery/ConfigReader.test.ts
+++ packages/backend/discovery/ConfigReader.test.ts
@@ -1,7 +1,6 @@
 import { ChainId, ConfigReader } from '@l2beat/discovery'
 import { expect } from 'earl'
 import path from 'path'
-// import { ConfigReader } from './ConfigReader'
 
 describe(ConfigReader.name, () => {
   // FIXME: This is a temporary hack to make the tests while configs are in backend
