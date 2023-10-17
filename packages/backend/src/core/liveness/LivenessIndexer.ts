d3a9c05fc081db107105015159a713d0fdfd916d
antooni <antoni.pawlak@protonmail.com>
2023-10-17 12:39:44 +0200

    add safeHeight handling

--- packages/backend/src/core/liveness/LivenessIndexer.ts
+++ packages/backend/src/core/liveness/LivenessIndexer.ts
@@ -1,7 +1,11 @@
+import { Logger } from '@l2beat/backend-tools'
 import { BigQueryClient } from '@l2beat/shared'
-import { UnixTime } from '@l2beat/shared-pure'
+import { Hash256, hashJson, UnixTime } from '@l2beat/shared-pure'
+import { ChildIndexer } from '@l2beat/uif'
 
+import { IndexerStateRepository } from '../../peripherals/database/IndexerStateRepository'
 import { LivenessRecord } from '../../peripherals/database/LivenessRepository'
+import { HourlyIndexer } from './HourlyIndexer'
 import {
   LivenessConfig,
   LivenessFunctionCall,
@@ -13,8 +17,46 @@ import {
   transformTransfersQueryResult,
 } from './utils'
 
-export class LivenessIndexer {
-  constructor(private readonly bigQueryClient: BigQueryClient) {}
+const LOGIC_VERSION = 0
+const CONFIG_HASH = hashJson('LIVENESS_CONFIG_HASH' + LOGIC_VERSION.toString())
+
+export class LivenessIndexer extends ChildIndexer {
+  private readonly indexerId = 'liveness_indexer'
+
+  constructor(
+    logger: Logger,
+    parentIndexer: HourlyIndexer,
+    private readonly bigQueryClient: BigQueryClient,
+    private readonly stateRepository: IndexerStateRepository,
+    private readonly minTimestamp: UnixTime,
+  ) {
+    super(logger, [parentIndexer])
+  }
+
+  override async start(): Promise<void> {
+    // TODO
+  }
+
+  override async update(from: number, to: number): Promise<number> {
+    // TODO
+  }
+
+  // override async invalidate(targetHeight: number): Promise<number> {
+  //   // TODO
+  // }
+
+  override async getSafeHeight(): Promise<number> {
+    const height = await this.stateRepository.getSafeHeight(this.indexerId)
+    return height ?? this.minTimestamp.toNumber()
+  }
+
+  override async setSafeHeight(height: number): Promise<void> {
+    await this.stateRepository.addOrUpdate({
+      indexerId: this.indexerId,
+      configHash: CONFIG_HASH,
+      safeHeight: height,
+    })
+  }
 
   async getLivenessData(
     config: LivenessConfig,
