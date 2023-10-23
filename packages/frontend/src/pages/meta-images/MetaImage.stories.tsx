f2b67e8769a7d20fa501c8958a8683d5e7fe2ae6
torztomasz <tomasz.torz@l2beat.com>
2023-10-23 13:39:11 +0200

    fix metaimage stories

--- packages/frontend/src/pages/meta-images/MetaImage.stories.tsx
+++ packages/frontend/src/pages/meta-images/MetaImage.stories.tsx
@@ -1,16 +1,35 @@
-import { ProjectId } from '@l2beat/shared-pure'
-import { Meta, Story } from '@storybook/react'
+import { Meta, StoryFn } from '@storybook/react'
 import React, { useEffect } from 'react'
 
 import { configureCharts } from '../../scripts/charts'
 import { ActivityMetaImage } from './ActivityMetaImage'
+import { DetailedTvlMetaImage } from './DetailedTvlMetaImage'
 import { TvlMetaImage } from './TvlMetaImage'
 
 export default {
   title: 'Other/MetaImage',
 } as Meta
 
-function Template() {
+export function TvlOverview() {
+  useEffect(() => {
+    configureCharts()
+  })
+
+  return (
+    <div className="meta flex items-center justify-center">
+      <div className="relative h-[314px] min-h-[314px] w-[600px] min-w-[600px] overflow-hidden rounded-lg shadow-2xl">
+        <TvlMetaImage
+          tvl="$1.34 B"
+          sevenDayChange="+3.45%"
+          chartType={{ type: 'layer2-tvl' }}
+          fake
+        />
+      </div>
+    </div>
+  )
+}
+
+export function ProjectOverview() {
   useEffect(() => {
     configureCharts()
   })
@@ -37,14 +56,56 @@ function Template() {
   )
 }
 
-export const Project: Story = Template.bind({})
-Project.args = {
-  projectId: ProjectId('nova'),
+export function DetailedTvlOverview() {
+  useEffect(() => {
+    configureCharts()
+  })
+
+  return (
+    <div className="meta flex items-center justify-center">
+      <div className="relative h-[314px] min-h-[314px] w-[600px] min-w-[600px] overflow-hidden rounded-lg shadow-2xl">
+        <DetailedTvlMetaImage
+          tvl="$1.34 B"
+          sevenDayChange="+3.45%"
+          chartType={{ type: 'layer2-detailed-tvl' }}
+          fake
+        />
+      </div>
+    </div>
+  )
 }
 
-export const TvlOverview: Story = Template.bind({})
+export function DetailedProjectOverview() {
+  useEffect(() => {
+    configureCharts()
+  })
+  const project = {
+    display: {
+      name: 'Arbitrum One',
+      slug: 'arbitrum',
+    },
+  }
 
-export const ActivityOverview: Story = () => {
+  return (
+    <div className="meta flex items-center justify-center">
+      <div className="relative h-[314px] min-h-[314px] w-[600px] min-w-[600px] overflow-hidden rounded-lg shadow-2xl">
+        <DetailedTvlMetaImage
+          tvl="$1.34 B"
+          sevenDayChange="+3.45%"
+          name={project.display.name}
+          icon={project.display.slug && `/icons/${project.display.slug}.png`}
+          chartType={{
+            type: 'project-detailed-tvl',
+            slug: project.display.slug,
+          }}
+          fake
+        />
+      </div>
+    </div>
+  )
+}
+
+export const ActivityOverview: StoryFn = () => {
   useEffect(() => {
     configureCharts()
   })
