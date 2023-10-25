92aca7fbe5741924390f723900ea9138b0f1f32f
torztomasz <tomasz.torz@l2beat.com>
2023-10-25 22:30:03 +0200

    remove turbo snap stuff

--- packages/frontend/.storybook/main.ts
+++ packages/frontend/.storybook/main.ts
@@ -1,6 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-vite'
 import { mergeConfig } from 'vite'
-import turbosnap from 'vite-plugin-turbosnap'
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.tsx'],
@@ -18,19 +17,11 @@ const config: StorybookConfig = {
     storyStoreV7: true,
   },
   staticDirs: ['../src/static', './static'],
-  async viteFinal(config, { configType }) {
+  async viteFinal(config) {
     return mergeConfig(config, {
       optimizeDeps: {
         include: ['@l2beat/config', '@l2beat/shared-pure'],
       },
-      plugins:
-        configType === 'PRODUCTION'
-          ? [
-              turbosnap({
-                rootDir: config.root ?? process.cwd(),
-              }),
-            ]
-          : [],
     })
   },
 }
