de8a528b2fbbfb3ad31274f1669671774e9208bf
torztomasz <tomasz.torz@l2beat.com>
2023-10-25 20:29:11 +0200

    add turbosnap support

--- packages/frontend/.storybook/main.ts
+++ packages/frontend/.storybook/main.ts
@@ -1,5 +1,6 @@
 import type { StorybookConfig } from '@storybook/react-vite'
 import { mergeConfig } from 'vite'
+import turbosnap from 'vite-plugin-turbosnap'
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.tsx'],
@@ -17,11 +18,19 @@ const config: StorybookConfig = {
     storyStoreV7: true,
   },
   staticDirs: ['../src/static', './static'],
-  async viteFinal(config) {
+  async viteFinal(config, { configType }) {
     return mergeConfig(config, {
       optimizeDeps: {
         include: ['@l2beat/config', '@l2beat/shared-pure'],
       },
+      plugins:
+        configType === 'PRODUCTION'
+          ? [
+              turbosnap({
+                rootDir: process.cwd(),
+              }),
+            ]
+          : [],
     })
   },
 }
