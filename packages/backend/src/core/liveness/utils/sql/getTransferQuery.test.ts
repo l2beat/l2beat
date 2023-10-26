2cfe1a8d2fe7155a9c1de81fbea51cd27ed791d1
Maciej Zygmunt <maciekzygmunt@interia.pl>
2023-10-26 14:16:36 +0200

    refactor getTransferQuery

--- packages/backend/src/core/liveness/utils/sql/getTransferQuery.test.ts
+++ packages/backend/src/core/liveness/utils/sql/getTransferQuery.test.ts
@@ -1,12 +1,27 @@
-import { EthereumAddress, UnixTime } from '@l2beat/shared-pure'
+import { EthereumAddress, ProjectId, UnixTime } from '@l2beat/shared-pure'
 import { expect } from 'earl'
 
+import { LivenessTransfer } from '../../types/LivenessConfig'
 import { getTransferQuery } from './getTransferQuery'
 
 describe('getTransferQuery', () => {
   it('should return valid SQL query', () => {
-    const senders = [EthereumAddress.random(), EthereumAddress.random()]
-    const receivers = [EthereumAddress.random(), EthereumAddress.random()]
+    const config: LivenessTransfer[] = [
+      {
+        projectId: ProjectId('project-1'), // irrelevant
+        from: EthereumAddress.random(),
+        to: EthereumAddress.random(),
+        type: 'DA', // irrelevant
+        sinceTimestamp: new UnixTime(0), // irrelevant
+      },
+      {
+        projectId: ProjectId('project-2'), // irrelevant
+        from: EthereumAddress.random(),
+        to: EthereumAddress.random(),
+        type: 'DA', // irrelevant
+        sinceTimestamp: new UnixTime(0), // irrelevant
+      },
+    ]
     const startTimestamp = UnixTime.fromDate(new Date('2022-01-01T00:00:00Z'))
     const endTimestamp = UnixTime.fromDate(new Date('2022-01-02T00:00:00Z'))
 
@@ -30,22 +45,17 @@ describe('getTransferQuery', () => {
         .toISOString()}')`,
       'AND ',
       '(',
-      `(from_address = LOWER('${senders[0].toLocaleLowerCase()}')`,
-      `AND to_address = LOWER('${receivers[0].toLocaleLowerCase()}'))`,
+      `(from_address = LOWER('${config[0].from.toLocaleLowerCase()}')`,
+      `AND to_address = LOWER('${config[0].to.toLocaleLowerCase()}'))`,
       'OR',
-      `(from_address = LOWER('${senders[1].toLocaleLowerCase()}')`,
-      `AND to_address = LOWER('${receivers[1].toLocaleLowerCase()}'))`,
+      `(from_address = LOWER('${config[1].from.toLocaleLowerCase()}')`,
+      `AND to_address = LOWER('${config[1].to.toLocaleLowerCase()}'))`,
       ')',
       'ORDER BY ',
       'block_timestamp ASC;',
     ].join('\n')
 
-    const result = getTransferQuery(
-      senders,
-      receivers,
-      startTimestamp,
-      endTimestamp,
-    )
+    const result = getTransferQuery(config, startTimestamp, endTimestamp)
     expect(result).toEqual(expectedQuery)
   })
 })
