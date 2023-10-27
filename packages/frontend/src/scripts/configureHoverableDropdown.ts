52a1066bff3eeb104005596b72aa0abe7284a429
torztomasz <tomasz.torz@l2beat.com>
2023-10-27 10:16:08 +0200

    check if scripts affect turbosnap

--- packages/frontend/src/scripts/configureHoverableDropdown.ts
+++ packages/frontend/src/scripts/configureHoverableDropdown.ts
@@ -5,11 +5,11 @@ export function configureHoverableDropdown() {
     document.querySelectorAll<HTMLElement>('.HoverableDropdown')
 
   const show = (menu: HTMLElement) => {
-    menu.classList.remove('opacity-0', 'pointer-events-none')
+    menu.classList.remove()
   }
 
   const hide = (menu: HTMLElement) => {
-    menu.classList.add('opacity-0', 'pointer-events-none')
+    menu.classList.add()
   }
 
   hoverableDropdowns.forEach((hoverableDropdown) => {
