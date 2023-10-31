75cdadb2ee48fb16133d92e562f4fe4e980bc158
zzq0826 <770166635@qq.com>
2023-10-31 09:37:08 +0800

    Update starknet.ts
    Replace the expired Discord link.

--- packages/config/src/layer2s/starknet.ts
+++ packages/config/src/layer2s/starknet.ts
@@ -161,7 +161,7 @@ export const starknet: Layer2 = {
       explorers: ['https://voyager.online/', 'https://starkscan.co/'],
       repositories: ['https://github.com/starkware-libs'],
       socialMedia: [
-        'https://discord.gg/uJ9HZTUk2Y',
+        'https://starknet.io/discord',
         'https://twitter.com/StarkWareLtd',
         'https://medium.com/starkware',
         'https://starkware.co/',
