4c2e2584113c50980d117844e0d3e6fecb1ed3ff
Luca Donno <donnoh99@gmail.com>
2023-11-02 08:32:55 +0100

    Add Paradex (full)

--- packages/config/src/layer2s/paradex.ts
+++ packages/config/src/layer2s/paradex.ts
@@ -1,10 +1,50 @@
-import { EthereumAddress, ProjectId } from '@l2beat/shared-pure'
+import {
+  EthereumAddress,
+  formatLargeNumberShared,
+  ProjectId,
+} from '@l2beat/shared-pure'
 
 import { ProjectDiscovery } from '../discovery/ProjectDiscovery'
-import { CONTRACTS, TECHNOLOGY, UNDER_REVIEW_RISK_VIEW } from './common'
+import { formatSeconds } from '../utils/formatSeconds'
+import {
+  CONTRACTS,
+  makeBridgeCompatible,
+  TECHNOLOGY,
+  UNDER_REVIEW_RISK_VIEW,
+} from './common'
 import { Layer2 } from './types'
+import { RISK_VIEW, STATE_ZKP_ST } from './common/riskView'
 
 const discovery = new ProjectDiscovery('paradex')
+const verifierAddress = discovery.getAddressFromValue('Paradex', 'verifier')
+
+const upgradeDelaySeconds = discovery.getContractUpgradeabilityParam(
+  'Paradex',
+  'upgradeDelay',
+)
+
+const escrowUSDCDelaySeconds = discovery.getContractUpgradeabilityParam(
+  'USDC Bridge',
+  'upgradeDelay',
+)
+
+const minDelay = Math.min(upgradeDelaySeconds, escrowUSDCDelaySeconds)
+
+function formatMaxTotalBalanceString(
+  ticker: string,
+  maxTotalBalance: number,
+  decimals: number,
+) {
+  return `The current bridge cap is ${formatLargeNumberShared(
+    maxTotalBalance / 10 ** decimals,
+  )} ${ticker}.`
+}
+
+const escrowUSDCMaxTotalBalanceString = formatMaxTotalBalanceString(
+  'USDC',
+  discovery.getContractValue<number>('USDC Bridge', 'maxTotalBalance'),
+  6,
+)
 
 export const paradex: Layer2 = {
   isUnderReview: true,
@@ -34,11 +74,25 @@ export const paradex: Layer2 = {
       discovery.getEscrowDetails({
         address: EthereumAddress('0xE3cbE3A636AB6A754e9e41B12b09d09Ce9E53Db3'),
         tokens: ['USDC'],
+        upgradableBy: ['USDC Escrow owner'],
+        upgradeDelay: formatSeconds(escrowUSDCDelaySeconds),
         description: 'Paradex USDC Escrow.',
       }),
     ],
   },
-  riskView: UNDER_REVIEW_RISK_VIEW,
+  riskView: makeBridgeCompatible({
+    stateValidation: {
+      ...RISK_VIEW.STATE_ZKP_ST,
+      sources: [
+        {
+          contract: 'Paradex',
+          references: [
+            'https://etherscan.io/address/0xA964D693cd45FCBe4303524E0EFe0988cfF5ed08#code#F1#L218',
+          ],
+        },
+      ],
+    },
+  }),
   technology: TECHNOLOGY.UNDER_REVIEW,
   contracts: CONTRACTS.UNDER_REVIEW,
 }
