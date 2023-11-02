50aafc661e7ba8ab3ef8c6ba8398403a4241c64d
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2023-11-02 15:12:10 +0100

    Update packages/config/src/layer2s/zksyncera.ts

--- packages/config/src/layer2s/zksyncera.ts
+++ packages/config/src/layer2s/zksyncera.ts
@@ -188,7 +188,7 @@ export const zksyncera: Layer2 = {
         callsItselfRollup: true,
         stateRootsPostedToL1: true,
         dataAvailabilityOnL1: true,
-        rollupNodeSourceAvailable: 'UnderReview',
+        rollupNodeSourceAvailable: true,
       },
       stage1: {
         stateVerificationOnL1: true,
