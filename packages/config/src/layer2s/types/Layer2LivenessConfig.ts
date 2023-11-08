0914e59dfbfa3abdb6930dc7e12b0df6eff05f5f
Maciej Zygmunt <maciekzygmunt@interia.pl>
2023-11-08 08:16:37 +0100

    fix type

--- packages/config/src/layer2s/types/Layer2LivenessConfig.ts
+++ packages/config/src/layer2s/types/Layer2LivenessConfig.ts
@@ -8,8 +8,8 @@ export interface Layer2LivenessConfig {
 export interface FunctionCallParams {
   formula: 'functionCall'
   address: EthereumAddress
-  selector: string
-  functionSignature: `0x${string}`
+  selector: `0x${string}`
+  functionSignature: string
   sinceTimestamp: UnixTime
   untilTimestamp?: UnixTime
 }
