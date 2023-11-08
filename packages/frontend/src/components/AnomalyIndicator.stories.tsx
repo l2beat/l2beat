0cecd3de6c46558079fed5c1fd7523aa8f632701
torztomasz <tomasz.torz@l2beat.com>
2023-11-08 15:02:22 +0100

    fix anomaly indicator story

--- packages/frontend/src/components/AnomalyIndicator.stories.tsx
+++ packages/frontend/src/components/AnomalyIndicator.stories.tsx
@@ -34,48 +34,90 @@ type Story = StoryObj<typeof AnomalyIndicator>
 const anomalies: AnomalyIndicatorEntry[] = [
   {
     isAnomaly: true,
-    anomalies: [{ timestamp: 1696758992, durationInSeconds: 120 * 60 }],
+    anomalies: [
+      {
+        timestamp: 1696758992,
+        durationInSeconds: 120 * 60,
+        type: 'STATE UPDATE',
+      },
+    ],
   },
   {
     isAnomaly: true,
-    anomalies: [{ timestamp: 1697277392, durationInSeconds: 20 * 60 }],
+    anomalies: [
+      {
+        timestamp: 1697277392,
+        durationInSeconds: 20 * 60,
+        type: 'BATCH SUBMISSION',
+      },
+    ],
   },
   { isAnomaly: false },
   {
     isAnomaly: true,
-    anomalies: [{ timestamp: 1697363792, durationInSeconds: 1440 * 60 }],
+    anomalies: [
+      {
+        timestamp: 1697363792,
+        durationInSeconds: 1440 * 60,
+        type: 'STATE UPDATE',
+      },
+    ],
   },
-  { isAnomaly: false },
-  { isAnomaly: false },
-  { isAnomaly: false },
-  {
-    isAnomaly: true,
-    anomalies: [{ timestamp: 1697622992, durationInSeconds: 180 * 60 }],
-  },
-  { isAnomaly: false },
-  { isAnomaly: false },
-  { isAnomaly: false },
-  {
-    isAnomaly: true,
-    anomalies: [{ timestamp: 1698054992, durationInSeconds: 45 * 60 }],
-  },
-  { isAnomaly: false },
-  { isAnomaly: false },
-  { isAnomaly: false },
-  { isAnomaly: false },
-  { isAnomaly: false },
-  {
-    isAnomaly: true,
-    anomalies: [{ timestamp: 1698227792, durationInSeconds: 15 * 60 }],
-  },
-  { isAnomaly: false },
-  { isAnomaly: false },
   { isAnomaly: false },
   { isAnomaly: false },
   { isAnomaly: false },
   {
     isAnomaly: true,
-    anomalies: [{ timestamp: 1698663392, durationInSeconds: 75 * 60 }],
+    anomalies: [
+      {
+        timestamp: 1697622992,
+        durationInSeconds: 180 * 60,
+        type: 'STATE UPDATE',
+      },
+    ],
+  },
+  { isAnomaly: false },
+  { isAnomaly: false },
+  { isAnomaly: false },
+  {
+    isAnomaly: true,
+    anomalies: [
+      {
+        timestamp: 1698054992,
+        durationInSeconds: 45 * 60,
+        type: 'BATCH SUBMISSION',
+      },
+    ],
+  },
+  { isAnomaly: false },
+  { isAnomaly: false },
+  { isAnomaly: false },
+  { isAnomaly: false },
+  { isAnomaly: false },
+  {
+    isAnomaly: true,
+    anomalies: [
+      {
+        timestamp: 1698227792,
+        durationInSeconds: 15 * 60,
+        type: 'STATE UPDATE',
+      },
+    ],
+  },
+  { isAnomaly: false },
+  { isAnomaly: false },
+  { isAnomaly: false },
+  { isAnomaly: false },
+  { isAnomaly: false },
+  {
+    isAnomaly: true,
+    anomalies: [
+      {
+        timestamp: 1698663392,
+        durationInSeconds: 75 * 60,
+        type: 'STATE UPDATE',
+      },
+    ],
   },
   { isAnomaly: false },
   { isAnomaly: false },
