59ac66140c0372d30606cce2bda16515b023279d
antooni <antoni.pawlak@protonmail.com>
2023-11-09 17:19:00 +0100

    add dry run to query

--- packages/shared/src/services/bigquery/BigQueryClient.test.ts
+++ packages/shared/src/services/bigquery/BigQueryClient.test.ts
@@ -79,5 +79,51 @@ describe(BigQueryClient.name, () => {
         'Google BigQuery error: BigQuery error',
       )
     })
+
+    it('throws when query size exceeds the limit', async () => {
+      const QUERY_LIMIT = 1000
+      const response = {
+        metadata: {
+          statistics: {
+            query: { totalBytesProcessed: QUERY_LIMIT + 1 },
+          },
+        },
+      }
+      const bigQuery = mockObject<BigQuerySDKWrapper>({
+        createQueryJob: mockFn().resolvesToOnce([response]),
+      })
+
+      const bigQueryClient = new BigQueryClient(bigQuery, QUERY_LIMIT)
+      const sql = 'SELECT * FROM my_table'
+
+      await expect(
+        async () => await bigQueryClient.query(sql),
+      ).toBeRejectedWith(
+        'BigQuery estimate too high: ' + (QUERY_LIMIT + 1).toString(),
+      )
+    })
+  })
+
+  describe('Estimate query size', () => {
+    it('performs dry run & returns query size estimate', async () => {
+      const ESTIMATE = 1000
+      const response = {
+        metadata: {
+          statistics: {
+            query: { totalBytesProcessed: ESTIMATE },
+          },
+        },
+      }
+      const bigQuery = mockObject<BigQuerySDKWrapper>({
+        createQueryJob: mockFn().resolvesToOnce([response]),
+      })
+
+      const bigQueryClient = new BigQueryClient(bigQuery)
+      const sql = 'SELECT * FROM my_table'
+      const estimate = await bigQueryClient.estimateQuerySize(sql)
+
+      expect(bigQuery.createQueryJob).toHaveBeenCalledWith(sql, true)
+      expect(estimate).toEqual(ESTIMATE)
+    })
   })
 })
