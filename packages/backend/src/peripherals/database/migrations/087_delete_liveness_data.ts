c1c47c07a128ccc9f775378b07a5342755572071
maciekzygmunt <maciekzygmunt@interia.pl>
2023-11-21 10:55:27 +0100

    delete liveness data migration

---
+++ packages/backend/src/peripherals/database/migrations/087_delete_liveness_data.ts
@@ -0,0 +1,22 @@
+/*
+                      ====== IMPORTANT NOTICE ======
+
+DO NOT EDIT OR RENAME THIS FILE
+
+This is a migration file. Once created the file should not be renamed or edited,
+because migrations are only run once on the production server. 
+
+If you find that something was incorrectly set up in the `up` function you
+should create a new migration file that fixes the issue.
+
+*/
+
+import { Knex } from 'knex'
+
+export async function up(knex: Knex) {
+  await knex('liveness_configuration').delete()
+  await knex('liveness').delete()
+  await knex('indexer_state').delete()
+}
+
+export async function down() {}
