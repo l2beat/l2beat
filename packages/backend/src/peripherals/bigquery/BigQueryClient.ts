27df0df1e36cae59826b28cbc59d1b7bea21479e
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2023-11-23 12:48:42 +0100

    Refactor BigQuery queries (#2254)
    Co-authored-by: Maciej Zygmunt <maciekzygmunt@interia.pl>
    Co-authored-by: Antoni Pawlak <antoni.pawlak@pm.me>

---
+++ packages/backend/src/peripherals/bigquery/BigQueryClient.ts
@@ -0,0 +1,38 @@
+import { BigQuery, Query } from '@google-cloud/bigquery'
+import { RateLimiter } from '@l2beat/shared-pure'
+
+export interface BigQueryAuth {
+  // Client Email
+  clientEmail: string
+  // Consumer Private Key
+  privateKey: string
+  // Project ID
+  projectId: string
+}
+
+export class BigQueryClient {
+  private readonly bigquery: BigQuery
+  constructor(auth: BigQueryAuth) {
+    this.bigquery = new BigQuery({
+      credentials: {
+        client_email: auth.clientEmail,
+        private_key: auth.privateKey,
+      },
+      projectId: auth.projectId,
+    })
+    const rateLimiter = new RateLimiter({
+      callsPerMinute: 100,
+    })
+    this.query = rateLimiter.wrap(this.query.bind(this))
+  }
+
+  async query(query: Query | string): Promise<unknown[]> {
+    const [job] = await this.bigquery.createQueryJob(
+      typeof query === 'string'
+        ? { query, location: 'US' }
+        : { ...query, location: 'US' },
+    )
+    const [rows] = await job.getQueryResults()
+    return rows as unknown[]
+  }
+}
--- packages/shared/src/services/bigquery/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './BigQueryClient'
-export * from './BigQuerySDKWrapper'
