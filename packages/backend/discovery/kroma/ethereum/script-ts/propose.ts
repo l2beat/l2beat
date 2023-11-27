2360db9cb98b3adcc70c2df6c75c31b13730d888
Adrian Adamiak <adrian@adamiak.net>
2023-11-27 16:35:02 +0000

    Add (unfinished) kroma proposer script

---
+++ packages/backend/discovery/kroma/ethereum/script-ts/propose.ts
@@ -0,0 +1,158 @@
+import { ethers } from 'ethers'
+
+import { getContracts, KromaContracts, KromaNetwork } from './getContracts'
+import { getLogsInBatches, getRcpHostFromArgs, sleep } from './utils'
+
+const KROMA_NETWORK: KromaNetwork = 'sepolia'
+const PROPOSER_DEPOSIT = {
+  mainnet: ethers.utils.parseEther('0.2'),
+  sepolia: ethers.utils.parseEther('0.1'),
+}
+const EARLIEST_BLOCK = 4775729 // copied when writing this script
+export const GET_LOGS_MAX_RANGE = 10000
+const VALIDATOR_PUBLIC_ROUND_ADDRESS =
+  '0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF'
+const myAddr = process.env.MY_ADDRESS!
+
+async function run() {
+  console.log('Running the Kroma proposer script')
+  console.log('=================================')
+  const rpcHost = getRcpHostFromArgs()
+  const provider = new ethers.providers.JsonRpcProvider(rpcHost)
+  const wallet = new ethers.Wallet(process.env.MY_PRIVATE_KEY!, provider)
+  const contracts = getContracts(KROMA_NETWORK, wallet)
+
+  await showBasicKromaInfo(contracts)
+
+  let nextStep = 'continue'
+  while (nextStep === 'continue') {
+    nextStep = await performNextAction(contracts)
+  }
+}
+
+async function performNextAction(
+  contracts: KromaContracts,
+): Promise<'continue' | 'stop'> {
+  const proposedOutputIndex = await getProposedOutputIndex(contracts)
+
+  if (proposedOutputIndex === undefined) {
+    console.log("I haven't proposed yet.")
+    await proposeRoot(contracts)
+  } else {
+    console.log(`I have proposed output index: ${proposedOutputIndex}`)
+    return 'stop'
+  }
+
+  return 'continue'
+}
+
+async function showBasicKromaInfo(contracts: KromaContracts) {
+  const proposerCount = await contracts.validatorPool.validatorCount()
+  const curProposer = await contracts.validatorPool.nextValidator()
+  const roundDuration = await contracts.validatorPool.ROUND_DURATION()
+  console.log(`Current proposer count: ${proposerCount.toString()}`)
+  console.log(`Current proposer: ${curProposer}`)
+  console.log(`Round duration: ${roundDuration.toString()}`)
+}
+
+async function getProposedOutputIndex(
+  contracts: KromaContracts,
+): Promise<number | undefined> {
+  console.log('Checking if I already proposed by looking at Bonded events')
+  const myBondedEvents = await getLogsInBatches(
+    contracts.validatorPool,
+    contracts.validatorPool.filters.Bonded(myAddr),
+    GET_LOGS_MAX_RANGE,
+    EARLIEST_BLOCK,
+    'latest',
+  )
+
+  if (myBondedEvents.length > 1) {
+    throw new Error(
+      'Making multiple proposals is currently not supported by this script',
+    )
+  }
+
+  const event = myBondedEvents[0] as ethers.Event | undefined
+  if (event === undefined) {
+    return undefined
+  }
+  return event.args!.outputIndex as number
+}
+
+async function proposeRoot(contracts: KromaContracts) {
+  await addMyselfToProposerList(contracts)
+  await waitToBecomeProposer(contracts)
+  await submitOutputRoot(contracts)
+}
+
+async function addMyselfToProposerList(contracts: KromaContracts) {
+  if (await contracts.validatorPool.isValidator(myAddr)) {
+    console.log('I am already on the proposer list.')
+    return
+  }
+  console.log('Adding myself to proposer list.')
+  const value = PROPOSER_DEPOSIT[KROMA_NETWORK]
+  await contracts.validatorPool.deposit({ value })
+  if (!(await contracts.validatorPool.isValidator(myAddr))) {
+    throw new Error('Failed to add myself to proposer list')
+  }
+}
+
+async function waitToBecomeProposer(contracts: KromaContracts) {
+  console.log('Waiting to become proposer or a public round...')
+  let proposer = await contracts.validatorPool.nextValidator()
+
+  while (proposer !== myAddr && proposer !== VALIDATOR_PUBLIC_ROUND_ADDRESS) {
+    console.log(`${new Date().toISOString()} Current proposer: ${proposer}`)
+    await sleep(10 * 1000)
+    proposer = await contracts.validatorPool.nextValidator()
+  }
+
+  console.log(
+    proposer === myAddr ? 'I am the proposer!' : "It's a public round.",
+  )
+}
+
+async function submitOutputRoot(contracts: KromaContracts) {
+  console.log('Preparing to submit output root...')
+  const provider = contracts.validatorPool.provider
+  const nextL2BlockNumber = await contracts.l2OutputOracle.nextBlockNumber()
+  const block = await provider.getBlock('latest')
+  const blockNumber = block.number
+  const blockHash = block.hash
+
+  const expectedTimestamp = await contracts.l2OutputOracle.computeL2Timestamp(
+    nextL2BlockNumber,
+  )
+
+  const waitUntil = expectedTimestamp.toNumber() + 1 // +1 second for clock drift
+  while (Date.now() / 1000 < waitUntil) {
+    console.log(
+      `Waiting for timestamp ${expectedTimestamp.toString()} (it's ${
+        Date.now() / 1000
+      })`,
+    )
+    await sleep(1 * 1000)
+  }
+
+  const dummyOutputRoot = ethers.utils.formatBytes32String('0x1234567890')
+
+  console.log('Submitting output...')
+  const tx = await contracts.l2OutputOracle.submitL2Output(
+    dummyOutputRoot,
+    nextL2BlockNumber,
+    blockHash,
+    blockNumber,
+  )
+  console.log('Done! Waiting until transaction is mined...')
+  await tx.wait()
+  console.log('Mined')
+
+  await sleep(20 * 1000) // just to make sure we will get the Bonded event
+}
+
+run().catch((e) => {
+  console.error(e)
+  process.exit(1)
+})
