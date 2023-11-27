c0b06948065cecbe64a50cf2302d5d472d535032
antooni <antoni.pawlak@protonmail.com>
2023-11-27 12:30:38 +0100

    add better comments

--- packages/backend/src/core/SequenceProcessor.ts
+++ packages/backend/src/core/SequenceProcessor.ts
@@ -219,7 +219,9 @@ export class SequenceProcessor extends EventEmitter {
     this.state = state
   }
 
-  // WARNING: this method should be used only in tests
+  /**
+  WARNING: this method should be used only in tests
+  **/
   _TEST_ONLY_stopQueue(): void {
     this.processQueue._TEST_ONLY_stop()
     this.processQueue._TEST_ONLY_clear()
