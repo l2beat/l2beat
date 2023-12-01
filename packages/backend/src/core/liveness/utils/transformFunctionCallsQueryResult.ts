5f1534127faff99256b8b6ce87a159ba3447a4ee
maciekzygmunt <maciekzygmunt@interia.pl>
2023-12-01 17:00:02 +0100

    Refactor transformFunctionCallsQueryResult function

--- packages/backend/src/core/liveness/utils/transformFunctionCallsQueryResult.ts
+++ packages/backend/src/core/liveness/utils/transformFunctionCallsQueryResult.ts
@@ -1,4 +1,5 @@
 import { assert } from '@l2beat/shared-pure'
+import { utils } from 'ethers'
 
 import { LivenessRecord } from '../../../peripherals/database/LivenessRepository'
 import { LivenessFunctionCall } from '../types/LivenessConfig'
@@ -8,18 +9,41 @@ export function transformFunctionCallsQueryResult(
   configs: LivenessFunctionCall[],
   queryResults: BigQueryFunctionCallsResult,
 ): LivenessRecord[] {
-  const results: LivenessRecord[] = queryResults.map((r) => {
-    const config = configs.find(
-      (t) => r.input.startsWith(t.selector) && t.address === r.to_address,
-    )
+  const configsWithProgramHash = configs.filter((c) => c.programHash)
 
-    assert(config, 'Programmer error: config should not be undefined there')
+  const results: LivenessRecord[] = []
+  queryResults.forEach((r) => {
+    if (r.input.length <= 10) {
+      const config = configs.find(
+        (t) => r.input.startsWith(t.selector) && t.address === r.to_address,
+      )
 
-    return {
-      timestamp: r.block_timestamp,
-      blockNumber: r.block_number,
-      txHash: r.transaction_hash,
-      livenessConfigurationId: config.livenessConfigurationId,
+      assert(config, 'Programmer error: config should not be undefined there')
+
+      results.push({
+        timestamp: r.block_timestamp,
+        blockNumber: r.block_number,
+        txHash: r.transaction_hash,
+        livenessConfigurationId: config.livenessConfigurationId,
+      })
+    } else {
+      // Should be it hardcoded here?
+      const fnSignature =
+        'verifyProofAndRegister(uint256[] proofParams, uint256[] proof, uint256[] taskMetadata, uint256[] cairoAuxInput, uint256 cairoVerifierId)'
+      const i = new utils.Interface([`function ${fnSignature}`])
+      const decodedInput = i.decodeFunctionData(fnSignature, r.input)
+      const hashes = (decodedInput[2] as bigint[]).map((n) => n.toString())
+
+      configsWithProgramHash.forEach((config) => {
+        if (config.programHash && hashes.includes(config.programHash)) {
+          results.push({
+            timestamp: r.block_timestamp,
+            blockNumber: r.block_number,
+            txHash: r.transaction_hash,
+            livenessConfigurationId: config.livenessConfigurationId,
+          })
+        }
+      })
     }
   })
   return results
