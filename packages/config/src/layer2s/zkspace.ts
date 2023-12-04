d4e21e1cebb63b50bc555fb1d7cbdc22a9284a9b
Luca Donno <donnoh99@gmail.com>
2023-12-04 12:10:53 +0100

    Downgrade projects to Stage 0 with warning

--- packages/config/src/layer2s/zkspace.ts
+++ packages/config/src/layer2s/zkspace.ts
@@ -149,7 +149,7 @@ export const zkspace: Layer2 = {
       callsItselfRollup: true,
       stateRootsPostedToL1: true,
       dataAvailabilityOnL1: true,
-      rollupNodeSourceAvailable: 'UnderReview',
+      rollupNodeSourceAvailable: false,
     },
     stage1: {
       stateVerificationOnL1: true,
