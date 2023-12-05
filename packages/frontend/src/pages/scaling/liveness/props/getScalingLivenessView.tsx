028cf053ed415c8de9eed0e53a9e1f91b7ad0681
Tomasz Tórz <93620601+torztomasz@users.noreply.github.com>
2023-12-05 11:32:24 +0100

    Add proof submissions to liveness table (#2319)

--- packages/frontend/src/pages/scaling/liveness/props/getScalingLivenessView.tsx
+++ packages/frontend/src/pages/scaling/liveness/props/getScalingLivenessView.tsx
@@ -36,6 +36,7 @@ function getScalingLivenessViewEntry(
     explanation: project.display.livenessExplanation,
     stateUpdates: liveness?.stateUpdates,
     batchSubmissions: liveness?.batchSubmissions,
+    proofSubmissions: liveness?.proofSubmissions,
     anomalyEntries: getAnomalyEntries(liveness?.anomalies),
   }
 }
