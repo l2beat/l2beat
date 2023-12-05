c865b0aab45a20f223a97d915312d779a5485843
torztomasz <tomasz.torz@l2beat.com>
2023-12-05 12:21:09 +0100

    revert orderByTvl

--- packages/frontend/src/utils/getOrder.ts
+++ packages/frontend/src/utils/getOrder.ts
@@ -1,8 +1,3 @@
-import { Layer2 } from '@l2beat/config'
-import { TvlApiChart, TvlApiResponse } from '@l2beat/shared-pure'
-
-import { orderByTvl } from './orderByTvl'
-
 export function getSortingOrder<T extends { slug: string }>(
   items: T[],
   check: (a: T, b: T) => boolean | number,
@@ -16,14 +11,4 @@ export function getSortingOrder<T extends { slug: string }>(
     return result
   })
   return order.map((item) => item.slug)
-}
-
-export function getSortingOrderByTvl(
-  projects: Layer2[],
-  tvlApiResponse: Pick<TvlApiResponse, 'projects'>,
-  tvlType: Exclude<TvlApiChart['types'][number], 'timestamp'> = 'valueUsd',
-): string[] {
-  return orderByTvl(projects, tvlApiResponse, tvlType).map(
-    (item) => item.display.slug,
-  )
 }
--- packages/frontend/src/utils/orderByTvl.ts
+++ packages/frontend/src/utils/orderByTvl.ts
@@ -1,32 +1,18 @@
-import { ProjectId, TvlApiChart, TvlApiResponse } from '@l2beat/shared-pure'
+import { ProjectId, TvlApiResponse } from '@l2beat/shared-pure'
 
 export function orderByTvl<
   T extends { id: ProjectId; isArchived?: boolean; isUpcoming?: boolean },
->(
-  projects: T[],
-  tvlApiResponse: Pick<TvlApiResponse, 'projects'>,
-  tvlType: Exclude<TvlApiChart['types'][number], 'timestamp'> = 'valueUsd',
-): T[] {
+>(projects: T[], tvlApiResponse: Pick<TvlApiResponse, 'projects'>): T[] {
   const active = projects.filter(
     (project) => !project.isArchived && !project.isUpcoming,
   )
-
   const archived = projects.filter((project) => project.isArchived)
   const upcoming = projects.filter((project) => project.isUpcoming)
 
-  const getTvl = (project: T) => {
-    const index =
-      tvlApiResponse.projects[
-        project.id.toString()
-      ]?.charts.hourly.types.indexOf(tvlType)
-    if (!index)
-      throw new Error(
-        `Project ${project.id.toString()} does not have ${tvlType} tvl`,
-      )
-    return (tvlApiResponse.projects[
-      project.id.toString()
-    ]?.charts.hourly.data.at(-1)?.[index] ?? 0) as number
-  }
+  const getTvl = (project: T) =>
+    tvlApiResponse.projects[project.id.toString()]?.charts.hourly.data.at(
+      -1,
+    )?.[1] ?? 0
 
   const sortByTvl = (a: T, b: T) => getTvl(b) - getTvl(a)
 
