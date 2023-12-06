9d71132e817f5d7f9932f21bd7b8a751ebe61299
Radina Talanova <32281618+radinat@users.noreply.github.com>
2023-12-06 11:45:17 +0200

    Update Taiko explorers (#2331)
    Co-authored-by: Radina Talanova <radinatalanova@Radinas-MacBook-Air.local>

--- packages/config/src/layer2s/taiko.ts
+++ packages/config/src/layer2s/taiko.ts
@@ -19,10 +19,7 @@ export const taiko: Layer2 = {
       websites: ['https://taiko.xyz'],
       apps: ['https://bridge.test.taiko.xyz'],
       documentation: ['https://taiko.xyz/docs'],
-      explorers: [
-        'https://explorer.test.taiko.xyz',
-        'https://status.test.taiko.xyz',
-      ],
+      explorers: ['https://jolnir.taikoscan.network/'],
       repositories: ['https://github.com/taikoxyz'],
       socialMedia: [
         'https://twitter.com/taikoxyz',
