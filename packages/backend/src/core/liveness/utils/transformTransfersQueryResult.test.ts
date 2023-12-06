28be223ed719dd2ecf6f5692f6f74ee5933d80f9
antooni <antoni.pawlak@protonmail.com>
2023-12-06 13:35:43 +0100

    test transformTransfersQuery

--- packages/backend/src/core/liveness/utils/transformTransfersQueryResult.test.ts
+++ packages/backend/src/core/liveness/utils/transformTransfersQueryResult.test.ts
@@ -2,7 +2,7 @@ import { EthereumAddress, ProjectId, UnixTime } from '@l2beat/shared-pure'
 import { expect } from 'earl'
 
 import { LivenessRecord } from '../../../peripherals/database/LivenessRepository'
-import { LivenessTransfer } from '../types/LivenessConfig'
+import { LivenessTransfer, makeLivenessTransfer } from '../types/LivenessConfig'
 import { BigQueryTransfersResult } from '../types/model'
 import { transformTransfersQueryResult } from './transformTransfersQueryResult'
 
@@ -16,73 +16,82 @@ describe(transformTransfersQueryResult.name, () => {
     const ADDRESS_6 = EthereumAddress.random()
     const sinceTimestamp = UnixTime.now()
 
-    const config: Omit<LivenessTransfer, 'id'>[] = [
-      {
+    const config: LivenessTransfer[] = [
+      makeLivenessTransfer({
+        formula: 'transfer',
         projectId: ProjectId('project1'),
         from: ADDRESS_1,
         to: ADDRESS_2,
         type: 'STATE',
         sinceTimestamp,
-        livenessConfigurationId: 1,
-      },
-      {
+      }),
+      makeLivenessTransfer({
+        formula: 'transfer',
         projectId: ProjectId('project1'),
         from: ADDRESS_3,
         to: ADDRESS_4,
         type: 'DA',
         sinceTimestamp,
-        livenessConfigurationId: 2,
-      },
-      {
+      }),
+      makeLivenessTransfer({
+        formula: 'transfer',
         projectId: ProjectId('project2'),
         from: ADDRESS_5,
         to: ADDRESS_6,
         type: 'STATE',
         sinceTimestamp,
-        livenessConfigurationId: 3,
-      },
+      }),
     ]
+
+    const timestamp = UnixTime.fromDate(new Date('2022-01-01T01:00:00Z'))
+    const block = 1
+    const txHashes = [
+      '0x095e4e9ee709e353ad7849cf30e4dc19',
+      '0x915d9ed63e196d8c612aad5d6f5cd1ba',
+      '0x90d5e81b40d6a6fa6f34b3dc67d3fce6',
+    ]
+
     const queryResults: BigQueryTransfersResult = [
       {
-        transaction_hash: '0x095e4e9ee709e353ad7849cf30e4dc19',
-        block_number: 1,
-        block_timestamp: UnixTime.fromDate(new Date('2022-01-01T01:00:00Z')),
+        transaction_hash: txHashes[0],
+        block_number: block,
+        block_timestamp: timestamp,
         from_address: ADDRESS_1,
         to_address: ADDRESS_2,
       },
       {
-        transaction_hash: '0x915d9ed63e196d8c612aad5d6f5cd1ba',
-        block_number: 2,
-        block_timestamp: UnixTime.fromDate(new Date('2022-01-01T02:00:00Z')),
+        transaction_hash: txHashes[1],
+        block_number: block,
+        block_timestamp: timestamp,
         from_address: ADDRESS_3,
         to_address: ADDRESS_4,
       },
       {
-        transaction_hash: '0x90d5e81b40d6a6fa6f34b3dc67d3fce6',
-        block_number: 3,
-        block_timestamp: UnixTime.fromDate(new Date('2022-01-01T03:00:00Z')),
+        transaction_hash: txHashes[2],
+        block_number: block,
+        block_timestamp: timestamp,
         from_address: ADDRESS_5,
         to_address: ADDRESS_6,
       },
     ]
     const expected: LivenessRecord[] = [
       {
-        blockNumber: 1,
-        timestamp: UnixTime.fromDate(new Date('2022-01-01T01:00:00Z')),
-        txHash: '0x095e4e9ee709e353ad7849cf30e4dc19',
-        livenessId: 1,
+        txHash: txHashes[0],
+        livenessId: config[0].id,
+        blockNumber: block,
+        timestamp: timestamp,
       },
       {
-        blockNumber: 2,
-        timestamp: UnixTime.fromDate(new Date('2022-01-01T02:00:00Z')),
-        txHash: '0x915d9ed63e196d8c612aad5d6f5cd1ba',
-        livenessId: 2,
+        txHash: txHashes[1],
+        livenessId: config[1].id,
+        blockNumber: block,
+        timestamp: timestamp,
       },
       {
-        blockNumber: 3,
-        timestamp: UnixTime.fromDate(new Date('2022-01-01T03:00:00Z')),
-        txHash: '0x90d5e81b40d6a6fa6f34b3dc67d3fce6',
-        livenessId: 3,
+        txHash: txHashes[2],
+        livenessId: config[2].id,
+        blockNumber: block,
+        timestamp: timestamp,
       },
     ]
 
