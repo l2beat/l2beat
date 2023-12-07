f9a1e954b268bcced628d2d89dc5d6efb2f93b7a
Maciej Zygmunt <maciekzygmunt@interia.pl>
2023-12-07 15:07:05 +0100

    Allow for l3s to be added (#2321)
    Co-authored-by: Tomasz Tórz <torztomasz@users.noreply.github.com>

--- packages/config/src/layer2s/types/Layer2.ts
+++ packages/config/src/layer2s/types/Layer2.ts
@@ -31,6 +31,10 @@ export interface Layer2 {
   isUpcoming?: boolean
   /** Has this layer2 changed and is under review? */
   isUnderReview?: boolean
+  /** Is this a layer3? */
+  isLayer3?: boolean
+  /** ProjectId of hostChain */
+  hostChain?: ProjectId
   /** Information displayed about the layer2 on the frontend */
   display: Layer2Display
   /** Information required to calculate the stats of the layer2 */
@@ -88,6 +92,7 @@ export interface Layer2Display {
     | 'Polygon'
     | 'OVM'
     | 'Starknet'
+    | 'Arbitrum Orbit'
   /** List of links */
   links: ProjectLinks
   /** Where does the activity data come from? */
