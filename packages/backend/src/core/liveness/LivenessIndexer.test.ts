e7f73192d7a17e975826776b76a1e1ab5d134105
antooni <antoni.pawlak@protonmail.com>
2023-12-08 17:18:07 +0100

    test start function

--- packages/backend/src/core/liveness/LivenessIndexer.test.ts
+++ packages/backend/src/core/liveness/LivenessIndexer.test.ts
@@ -1,25 +1,95 @@
 import { Logger } from '@l2beat/backend-tools'
-import { UnixTime } from '@l2beat/shared-pure'
-import { expect, mockFn, mockObject } from 'earl'
+import { EthereumAddress, ProjectId, UnixTime } from '@l2beat/shared-pure'
+import { expect, mockObject } from 'earl'
+import { Knex } from 'knex'
 
+import { IndexerStateRepository } from '../../peripherals/database/IndexerStateRepository'
 import {
-  IndexerStateRecord,
-  IndexerStateRepository,
-} from '../../peripherals/database/IndexerStateRepository'
-import { LivenessConfigurationRepository } from '../../peripherals/database/LivenessConfigurationRepository'
+  LivenessConfigurationRecord,
+  LivenessConfigurationRepository,
+} from '../../peripherals/database/LivenessConfigurationRepository'
 import { LivenessRepository } from '../../peripherals/database/LivenessRepository'
 import { HourlyIndexer } from './HourlyIndexer'
 import { LivenessClient } from './LivenessClient'
 import { LivenessIndexer } from './LivenessIndexer'
+import {
+  LivenessConfigEntry,
+  makeLivenessFunctionCall,
+} from './types/LivenessConfig'
+import { LivenessId } from './types/LivenessId'
+import { diffLivenessConfigurations } from './utils/diffLivenessConfigurations'
+import { getSyncStatus } from './utils/getSyncStatus'
 
 const MIN_TIMESTAMP = UnixTime.fromDate(new Date('2023-05-01T00:00:00Z'))
+const TRX = mockObject<Knex.Transaction>({})
 
 describe(LivenessIndexer.name, () => {
-  describe(LivenessIndexer.prototype.start.name, () => {})
-
   describe(LivenessIndexer.prototype.update.name, () => {})
 
   describe(LivenessIndexer.prototype.getConfiguration.name, () => {})
+
+  describe(LivenessIndexer.prototype.start.name, () => {
+    it('initializes configurations and indexer state', async () => {
+      const runtimeEntries = getMockRuntimeConfigurations()
+
+      const removedConfig = mockObject<LivenessConfigurationRecord>({
+        id: LivenessId.random(),
+      })
+
+      const databaseEntries: LivenessConfigurationRecord[] = [
+        removedConfig,
+        toRecordWithUntilTimestamp(runtimeEntries[1], undefined),
+        // rest of the configurations would be considered "toAdd"
+      ]
+
+      const configurationRepository = getMockConfigRepository(databaseEntries)
+      const livenessRepository = getMockLivenessRepository()
+      const stateRepository = getMockStateRepository()
+      const livenessIndexer = getMockLivenessIndexer({
+        configurationRepository,
+        livenessRepository,
+        stateRepository,
+        runtimeEntries,
+      })
+
+      await livenessIndexer.start()
+
+      const { toAdd, toRemove, toTrim } = diffLivenessConfigurations(
+        runtimeEntries,
+        databaseEntries,
+      )
+
+      expect(configurationRepository.addMany).toHaveBeenOnlyCalledWith(
+        toAdd,
+        TRX,
+      )
+      expect(configurationRepository.deleteMany).toHaveBeenOnlyCalledWith(
+        toRemove,
+        TRX,
+      )
+      expect(
+        configurationRepository.setUntilTimestamp,
+      ).toHaveBeenOnlyCalledWith(toTrim[0].id, toTrim[0].untilTimestamp, TRX)
+      expect(livenessRepository.deleteAfter).toHaveBeenOnlyCalledWith(
+        toTrim[0].id,
+        toTrim[0].untilTimestamp,
+        TRX,
+      )
+
+      expect(configurationRepository.getAll).toHaveBeenCalledTimes(1)
+      expect(stateRepository.runInTransaction).toHaveBeenCalledTimes(1)
+
+      const syncStatus = getSyncStatus(databaseEntries, toAdd, MIN_TIMESTAMP)
+      expect(stateRepository.setSafeHeight).toHaveBeenOnlyCalledWith(
+        livenessIndexer.indexerId,
+        syncStatus.toNumber(),
+      )
+
+      // 1st during this.initialize() -> this.setSafeHeight()
+      // 2nd during super.start() -> this.getSafeHeight()
+      expect(stateRepository.findIndexerState).toHaveBeenCalledTimes(2)
+    })
+  })
 
   describe(LivenessIndexer.prototype.getSafeHeight.name, () => {
     it('returns safe height from DB', async () => {
@@ -57,7 +127,13 @@ describe(LivenessIndexer.name, () => {
 
   describe(LivenessIndexer.prototype.setSafeHeight.name, () => {
     it('saves safe height in the database', async () => {
+      const indexerState = {
+        indexerId: 'liveness_indexer',
+        safeHeight: 1,
+        minTimestamp: MIN_TIMESTAMP,
+      }
       const stateRepository = mockObject<IndexerStateRepository>({
+        findIndexerState: async () => indexerState,
         setSafeHeight: async () => 0, // return value is not important
       })
       const livenessIndexer = getMockLivenessIndexer({ stateRepository })
@@ -69,6 +145,43 @@ describe(LivenessIndexer.name, () => {
         'liveness_indexer',
         safeHeight,
       )
+    })
+
+    it('adds state to DB if undefined', async () => {
+      const stateRepository = mockObject<IndexerStateRepository>({
+        findIndexerState: async () => undefined,
+        add: async () => '',
+      })
+      const livenessIndexer = getMockLivenessIndexer({ stateRepository })
+
+      const safeHeight = MIN_TIMESTAMP.toNumber()
+      await livenessIndexer.setSafeHeight(safeHeight, TRX)
+
+      expect(stateRepository.add).toHaveBeenOnlyCalledWith(
+        {
+          indexerId: livenessIndexer.indexerId,
+          safeHeight: MIN_TIMESTAMP.toNumber(),
+          minTimestamp: MIN_TIMESTAMP,
+        },
+        TRX,
+      )
+    })
+
+    it('throws if minTimestamp is different than the one from DB', async () => {
+      const indexerState = {
+        indexerId: 'liveness_indexer',
+        safeHeight: 1,
+        minTimestamp: new UnixTime(0),
+      }
+      const stateRepository = mockObject<IndexerStateRepository>({
+        findIndexerState: async () => indexerState,
+        setSafeHeight: async () => 0, // return value is not important
+      })
+      const livenessIndexer = getMockLivenessIndexer({ stateRepository })
+
+      await expect(
+        async () => await livenessIndexer.setSafeHeight(111, TRX),
+      ).toBeRejectedWith('Minimum timestamp of this indexer cannot be updated')
     })
   })
 
@@ -84,11 +197,74 @@ describe(LivenessIndexer.name, () => {
   })
 })
 
+function getMockLivenessRepository() {
+  return mockObject<LivenessRepository>({
+    deleteAfter: async () => 0,
+  })
+}
+
+function getMockConfigRepository(
+  databaseEntries: LivenessConfigurationRecord[],
+) {
+  return mockObject<LivenessConfigurationRepository>({
+    addMany: async () => [],
+    deleteMany: async () => 0,
+    setUntilTimestamp: async () => 0,
+    getAll: async () => databaseEntries,
+  })
+}
+
+function getMockStateRepository(
+  indexerState = {
+    indexerId: 'liveness_indexer',
+    safeHeight: 1,
+    minTimestamp: MIN_TIMESTAMP,
+  },
+) {
+  const stateRepository = mockObject<IndexerStateRepository>({
+    findIndexerState: async () => indexerState,
+    add: async () => '',
+    setSafeHeight: async () => 0,
+    runInTransaction: async (fn) => fn(TRX),
+  })
+  return stateRepository
+}
+
+function getMockRuntimeConfigurations() {
+  return [
+    makeLivenessFunctionCall({
+      projectId: ProjectId('test'),
+      type: 'STATE',
+      formula: 'functionCall',
+      address: EthereumAddress.random(),
+      selector: '0x',
+      sinceTimestamp: MIN_TIMESTAMP,
+    }),
+    // this one has updated untilTimestamp
+    makeLivenessFunctionCall({
+      projectId: ProjectId('test2'),
+      type: 'STATE',
+      formula: 'functionCall',
+      address: EthereumAddress.random(),
+      selector: '0x',
+      sinceTimestamp: MIN_TIMESTAMP,
+      untilTimestamp: MIN_TIMESTAMP.add(1, 'days'),
+    }),
+  ]
+}
+
 function getMockLivenessIndexer(params: {
   stateRepository?: IndexerStateRepository
-  minTimestamp?: UnixTime
+  configurationRepository?: LivenessConfigurationRepository
+  livenessRepository?: LivenessRepository
+  runtimeEntries?: LivenessConfigEntry[]
 }) {
-  const { stateRepository, minTimestamp } = params
+  const {
+    stateRepository,
+    configurationRepository,
+    livenessRepository,
+    runtimeEntries,
+  } = params
 
   return new LivenessIndexer(
     Logger.SILENT,
@@ -99,9 +275,23 @@ function getMockLivenessIndexer(params: {
     }),
     mockObject<LivenessClient>({}),
     stateRepository ?? mockObject<IndexerStateRepository>({}),
-    mockObject<LivenessRepository>({}),
-    mockObject<LivenessConfigurationRepository>({}),
-    [],
-    minTimestamp ?? MIN_TIMESTAMP,
+    livenessRepository ?? mockObject<LivenessRepository>({}),
+    configurationRepository ?? mockObject<LivenessConfigurationRepository>({}),
+    runtimeEntries ?? [],
+    MIN_TIMESTAMP,
   )
+}
+
+function toRecordWithUntilTimestamp(
+  entry: LivenessConfigEntry,
+  untilTimestamp: UnixTime | undefined,
+): LivenessConfigurationRecord {
+  return {
+    id: entry.id,
+    projectId: entry.projectId,
+    type: entry.type,
+    sinceTimestamp: entry.sinceTimestamp,
+    untilTimestamp,
+    debugInfo: '',
+  }
 }
--- packages/backend/src/core/liveness/LivenessIndexer.ts
+++ packages/backend/src/core/liveness/LivenessIndexer.ts
@@ -3,10 +3,7 @@ import { UnixTime } from '@l2beat/shared-pure'
 import { ChildIndexer } from '@l2beat/uif'
 import { Knex } from 'knex'
 
-import {
-  IndexerStateRecord,
-  IndexerStateRepository,
-} from '../../peripherals/database/IndexerStateRepository'
+import { IndexerStateRepository } from '../../peripherals/database/IndexerStateRepository'
 import { LivenessConfigurationRepository } from '../../peripherals/database/LivenessConfigurationRepository'
 import { LivenessRepository } from '../../peripherals/database/LivenessRepository'
 import { HourlyIndexer } from './HourlyIndexer'
@@ -94,9 +91,6 @@ export class LivenessIndexer extends ChildIndexer {
   }
 
   private async initialize() {
-    const indexerState = await this.stateRepository.findIndexerState(
-      this.indexerId,
-    )
     const databaseEntries = await this.configurationRepository.getAll()
 
     const { toAdd, toRemove, toTrim } = diffLivenessConfigurations(
@@ -107,47 +101,15 @@ export class LivenessIndexer extends ChildIndexer {
     const syncStatus = getSyncStatus(databaseEntries, toAdd, this.minTimestamp)
 
     await this.stateRepository.runInTransaction(async (trx) => {
-      await this.initializeIndexerState(indexerState, syncStatus, trx)
-      await this.initializeConfigurations(toAdd, toTrim, toRemove, trx)
+      await this.initializeConfigurations(toAdd, toRemove, toTrim, trx)
+      await this.setSafeHeight(syncStatus.toNumber(), trx)
     })
-  }
-
-  private async initializeIndexerState(
-    indexerState: IndexerStateRecord | undefined,
-    status: UnixTime,
-    trx: Knex.Transaction,
-  ) {
-    if (indexerState === undefined) {
-      await this.stateRepository.add(
-        {
-          indexerId: this.indexerId,
-          safeHeight: this.minTimestamp.toNumber(),
-          minTimestamp: this.minTimestamp,
-        },
-        trx,
-      )
-      return
-    }
-
-    // We prevent updating the minimum timestamp of the indexer.
-    // This functionality can be added in the future if needed.
-    assert(
-      indexerState.minTimestamp &&
-        this.minTimestamp.equals(indexerState.minTimestamp),
-      'Minimum timestamp of this indexer cannot be updated',
-    )
-
-    await this.stateRepository.setSafeHeight(
-      this.indexerId,
-      status.toNumber(),
-      trx,
-    )
   }
 
   private async initializeConfigurations(
     toAdd: LivenessConfigEntry[],
-    toTrim: { id: LivenessId; untilTimestamp: UnixTime }[],
     toRemove: LivenessId[],
+    toTrim: { id: LivenessId; untilTimestamp: UnixTime }[],
     trx: Knex.Transaction,
   ) {
     await this.configurationRepository.addMany(toAdd, trx)
@@ -176,7 +138,34 @@ export class LivenessIndexer extends ChildIndexer {
     return indexerState?.safeHeight ?? this.minTimestamp.toNumber()
   }
 
-  override async setSafeHeight(height: number): Promise<void> {
+  override async setSafeHeight(
+    height: number,
+    trx?: Knex.Transaction,
+  ): Promise<void> {
+    const indexerState = await this.stateRepository.findIndexerState(
+      this.indexerId,
+    )
+
+    if (indexerState === undefined) {
+      await this.stateRepository.add(
+        {
+          indexerId: this.indexerId,
+          safeHeight: height,
+          minTimestamp: this.minTimestamp,
+        },
+        trx,
+      )
+      return
+    }
+
+    // We prevent updating the minimum timestamp of the indexer.
+    // This functionality can be added in the future if needed.
+    assert(
+      indexerState.minTimestamp &&
+        this.minTimestamp.equals(indexerState.minTimestamp),
+      'Minimum timestamp of this indexer cannot be updated',
+    )
+
     await this.stateRepository.setSafeHeight(this.indexerId, height)
   }
 
