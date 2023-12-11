33828f91585be9d734b21059fba1f76168704ffa
Luca Donno <donnoh99@gmail.com>
2023-12-11 13:33:49 +0100

    add missing rollups

--- packages/config/src/layer2s/paradex.ts
+++ packages/config/src/layer2s/paradex.ts
@@ -80,6 +80,10 @@ export const paradex: Layer2 = {
       repositories: ['https://github.com/tradeparadex'],
       socialMedia: ['https://twitter.com/tradeparadex'],
     },
+    liveness: {
+      explanation:
+        'Paradex is a ZK rollup that posts state diffs to the L1. For a transaction to be considered final, the state diffs have to be submitted and validity proof should be generated, submitted, and verified. Proofs are aggregated with other projects using SHARP and state updates have to refer to proved claims.',
+    },
   },
   config: {
     escrows: [
