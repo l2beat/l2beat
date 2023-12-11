02d4fb558ed15901b2063275961937df4c106d32
Tomasz Tórz <93620601+torztomasz@users.noreply.github.com>
2023-12-11 14:55:31 +0100

    Add liveness explanations to Layer2 projects (#2346)
    Co-authored-by: Luca Donno <donnoh99@gmail.com>

--- packages/config/src/layer2s/zkspace.ts
+++ packages/config/src/layer2s/zkspace.ts
@@ -50,6 +50,10 @@ export const zkspace: Layer2 = {
         'https://reddit.com/r/ZKSwap_Official/',
       ],
     },
+    liveness: {
+      explanation:
+        'ZK Space is a ZK rollup based on zkSync Lite’s code base that posts state diffs to the L1. For a transaction to be considered final, the state diffs have to be submitted and validity proof should be generated, submitted, and verified. ',
+    },
   },
   config: {
     associatedTokens: ['ZKS'],
