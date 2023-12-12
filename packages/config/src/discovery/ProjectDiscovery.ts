9b8c366a53cc4c59f2a1fd0add118526aa3f5733
maciekzygmunt <maciekzygmunt@interia.pl>
2023-12-12 14:37:14 +0100

    allow for adding l2 contracts to project page

--- packages/config/src/discovery/ProjectDiscovery.ts
+++ packages/config/src/discovery/ProjectDiscovery.ts
@@ -54,12 +54,18 @@ const filesystem = {
 
 export class ProjectDiscovery {
   private readonly discovery: DiscoveryOutput
+  private readonly explorer: string
   constructor(
     public readonly projectName: string,
     public readonly chainId: ChainId = ChainId.ETHEREUM,
     private readonly fs: Filesystem = filesystem,
   ) {
     this.discovery = this.getDiscoveryJson(projectName)
+    this.explorer = ChainId.getExplorer(chainId)
+    assert(
+      this.explorer,
+      `No explorer found for chainId: ${chainId.toString()}`,
+    )
   }
 
   private getDiscoveryJson(project: string): DiscoveryOutput {
@@ -97,6 +103,7 @@ export class ProjectDiscovery {
       name: contract.name,
       address: contract.address,
       upgradeability: contract.upgradeability,
+      etherscanUrl: this.explorer,
       ...descriptionOrOptions,
     }
   }
@@ -138,6 +145,7 @@ export class ProjectDiscovery {
         name: name ?? contract.name,
         description,
         upgradeability: contract.upgradeability,
+        etherscanUrl: this.explorer,
         upgradableBy,
         upgradeDelay,
       },
@@ -219,6 +227,7 @@ export class ProjectDiscovery {
           ),
         )
         .join(' '),
+      etherscanUrl: this.explorer,
     }))
   }
 
@@ -245,6 +254,7 @@ export class ProjectDiscovery {
             type: 'MultiSig',
           },
         ],
+        etherscanUrl: this.explorer,
       },
       {
         name: `${identifier} participants`,
@@ -349,11 +359,11 @@ export class ProjectDiscovery {
     if (typeof descriptionOrOptions === 'string') {
       descriptionOrOptions = { description: descriptionOrOptions }
     }
-
     return {
       address: contract.address,
       name: contract.name,
       upgradeability: contract.upgradeability,
+      etherscanUrl: this.explorer,
       ...descriptionOrOptions,
     }
   }
@@ -395,6 +405,7 @@ export class ProjectDiscovery {
           type: 'Contract',
         },
       ],
+      etherscanUrl: this.explorer,
       description,
     }
   }
