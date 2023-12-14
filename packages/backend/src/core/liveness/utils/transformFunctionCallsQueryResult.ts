5c48f84fd25c90d93c25ca7e6ec736ec6de00e37
antooni <antoni.pawlak@protonmail.com>
2023-12-14 13:08:11 +0100

    transform function result

--- packages/backend/src/core/liveness/utils/transformFunctionCallsQueryResult.ts
+++ packages/backend/src/core/liveness/utils/transformFunctionCallsQueryResult.ts
@@ -1,26 +1,56 @@
 import { assert } from '@l2beat/shared-pure'
 
 import { LivenessRecord } from '../../../peripherals/database/LivenessRepository'
-import { LivenessFunctionCall } from '../types/LivenessConfig'
+import {
+  LivenessFunctionCall,
+  LivenessSharpSubmission,
+} from '../types/LivenessConfig'
 import { BigQueryFunctionCallsResult } from '../types/model'
+import { isProgramHashProven } from './isProgramHashProven'
 
 export function transformFunctionCallsQueryResult(
-  configs: LivenessFunctionCall[],
+  functionCalls: LivenessFunctionCall[],
+  sharpSubmissions: LivenessSharpSubmission[],
   queryResults: BigQueryFunctionCallsResult,
 ): LivenessRecord[] {
-  const results: LivenessRecord[] = queryResults.map((r) => {
-    const config = configs.find(
+  return queryResults.flatMap((r) => {
+    const functionCallsConfig = functionCalls.filter(
       (t) => r.input.startsWith(t.selector) && t.address === r.to_address,
     )
 
-    assert(config, 'Programmer error: config should not be undefined there')
-
-    return {
-      timestamp: r.block_timestamp,
-      blockNumber: r.block_number,
-      txHash: r.transaction_hash,
-      livenessId: config.id,
+    if (functionCallsConfig.length > 0) {
+      assert(
+        functionCallsConfig.length === 1,
+        'There should be exactly one matching config for functions',
+      )
+      return {
+        timestamp: r.block_timestamp,
+        blockNumber: r.block_number,
+        txHash: r.transaction_hash,
+        livenessId: functionCallsConfig[0].id,
+      }
     }
+
+    // There can be more than one matching config for the projects using the same contract.
+    const sharpSubmissionsConfig = sharpSubmissions.filter(
+      (t) => r.input.startsWith(t.selector) && t.address === r.to_address,
+    )
+
+    const results: LivenessRecord[] = []
+
+    for (const config of sharpSubmissionsConfig) {
+      if (isProgramHashProven(r, config.programHashes)) {
+        results.push({
+          timestamp: r.block_timestamp,
+          blockNumber: r.block_number,
+          txHash: r.transaction_hash,
+          livenessId: config.id,
+        })
+      }
+    }
+
+    assert(results.length > 0, 'There should be at least one matching config')
+
+    return results
   })
-  return results
 }
