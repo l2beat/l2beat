653b2936017b3750319d710381a05b5ac2d4a57f
antooni <antoni.pawlak@protonmail.com>
2023-12-14 16:11:08 +0100

    refactor getFunctionCallQuery

--- packages/backend/src/core/liveness/utils/sql/getFunctionCallQuery.test.ts
+++ packages/backend/src/core/liveness/utils/sql/getFunctionCallQuery.test.ts
@@ -10,25 +10,31 @@ const SELECTOR_2 = '0x' + 'B'.repeat(8)
 const FROM = UnixTime.fromDate(new Date('2021-01-01Z'))
 const TO = UnixTime.fromDate(new Date('2021-01-02Z'))
 
-const FUNCTION_CALLS = [
+const CONFIGURATIONS = [
   {
     address: ADDRESS_1,
     selector: SELECTOR_1,
+    getFullInput: false,
   },
   {
     address: ADDRESS_2,
     selector: SELECTOR_2,
+    getFullInput: true,
   },
 ]
 
 describe(getFunctionCallQuery.name, () => {
-  it('returns valid SQL when sharpSubmissions are empty', () => {
-    const { query, params } = getFunctionCallQuery(FUNCTION_CALLS, [], FROM, TO)
+  it('returns valid SQL', () => {
+    const { query, params, types } = getFunctionCallQuery(
+      CONFIGURATIONS,
+      FROM,
+      TO,
+    )
 
     expect(query).toEqual(`
     SELECT
       block_number,
-      LEFT(input, 10) AS input,
+      CASE WHEN to_address IN UNNEST(?) THEN input ELSE LEFT(input, 10) END AS input,
       to_address,
       block_timestamp,
       transaction_hash
@@ -46,6 +52,7 @@ describe(getFunctionCallQuery.name, () => {
   `)
 
     expect(params).toEqual([
+      [ADDRESS_2.toLowerCase()],
       FROM.toDate().toISOString(),
       TO.toDate().toISOString(),
       ADDRESS_1.toLowerCase(),
@@ -53,63 +60,16 @@ describe(getFunctionCallQuery.name, () => {
       ADDRESS_2.toLowerCase(),
       SELECTOR_2.toLowerCase() + '%',
     ])
-  })
 
-  it('returns valid SQL when sharpSubmissions are not empty', () => {
-    const ADDRESS_3 = EthereumAddress.random()
-    const ADDRESS_4 = EthereumAddress.random()
-
-    const SELECTOR_3 = '0x' + 'C'.repeat(8)
-    const SELECTOR_4 = '0x' + 'D'.repeat(8)
-    const sharpSubmissionsConfig = [
-      {
-        address: ADDRESS_3,
-        selector: SELECTOR_3,
-      },
-      {
-        address: ADDRESS_4,
-        selector: SELECTOR_4,
-      },
-    ]
-    const { query, params } = getFunctionCallQuery(
-      FUNCTION_CALLS,
-      sharpSubmissionsConfig,
-      FROM,
-      TO,
-    )
-
-    expect(query).toEqual(`
-    SELECT
-      block_number,
-      CASE WHEN to_address IN (?,?) THEN input ELSE LEFT(input, 10) END AS input,
-      to_address,
-      block_timestamp,
-      transaction_hash
-    FROM
-      bigquery-public-data.crypto_ethereum.traces
-    WHERE call_type = 'call'
-    AND status = 1
-    AND block_timestamp >= TIMESTAMP(?)
-    AND block_timestamp < TIMESTAMP(?)
-    AND (
-      ${Array.from({ length: 4 })
-        .map(() => `(to_address = ? AND input LIKE ?)`)
-        .join(' OR ')}
-    )
-  `)
-
-    expect(params).toEqual([
-      [ADDRESS_3.toLowerCase(), ADDRESS_4.toLowerCase()],
-      FROM.toDate().toISOString(),
-      TO.toDate().toISOString(),
-      ADDRESS_1.toLowerCase(),
-      SELECTOR_1.toLowerCase() + '%',
-      ADDRESS_2.toLowerCase(),
-      SELECTOR_2.toLowerCase() + '%',
-      ADDRESS_3.toLowerCase(),
-      SELECTOR_3.toLowerCase() + '%',
-      ADDRESS_4.toLowerCase(),
-      SELECTOR_4.toLowerCase() + '%',
+    // @ts-expect-error BigQuery types are wrong
+    expect(types).toEqual([
+      ['STRING'],
+      'STRING',
+      'STRING',
+      'STRING',
+      'STRING',
+      'STRING',
+      'STRING',
     ])
   })
 })
--- packages/backend/src/core/liveness/utils/sql/getFunctionCallQuery.ts
+++ packages/backend/src/core/liveness/utils/sql/getFunctionCallQuery.ts
@@ -1,39 +1,33 @@
 import { Query } from '@google-cloud/bigquery'
+import { QueryParamTypes } from '@google-cloud/bigquery/build/src/bigquery'
 import { EthereumAddress, UnixTime } from '@l2beat/shared-pure'
 
 export function getFunctionCallQuery(
-  functionCalls: { address: EthereumAddress; selector: string }[],
-  sharpSubmissions: { address: EthereumAddress; selector: string }[],
+  configs: {
+    address: EthereumAddress
+    selector: string
+    getFullInput: boolean
+  }[],
   from: UnixTime,
   to: UnixTime,
 ): Query {
-  let params: (string | string[])[] = [
+  const fullInputAddresses = configs
+    .filter((c) => c.getFullInput)
+    .map((c) => c.address.toLowerCase())
+  const params = [
+    fullInputAddresses,
     from.toDate().toISOString(),
     to.toDate().toISOString(),
-    ...functionCalls.flatMap((c) => [
-      c.address.toLowerCase(),
-      c.selector.toLowerCase() + '%',
-    ]),
-    ...sharpSubmissions.flatMap((c) => [
+    ...configs.flatMap((c) => [
       c.address.toLowerCase(),
       c.selector.toLowerCase() + '%',
     ]),
   ]
 
-  if (sharpSubmissions.length > 0) {
-    params = [sharpSubmissions.map((c) => c.address.toLowerCase()), ...params]
-  }
-
   const query = `
     SELECT
       block_number,
-      ${
-        sharpSubmissions.length > 0
-          ? `CASE WHEN to_address IN (${sharpSubmissions
-              .map(() => '?')
-              .join(',')}) THEN input ELSE LEFT(input, 10) END AS input,`
-          : `LEFT(input, 10) AS input,`
-      }
+      CASE WHEN to_address IN UNNEST(?) THEN input ELSE LEFT(input, 10) END AS input,
       to_address,
       block_timestamp,
       transaction_hash
@@ -44,11 +38,17 @@ export function getFunctionCallQuery(
     AND block_timestamp >= TIMESTAMP(?)
     AND block_timestamp < TIMESTAMP(?)
     AND (
-      ${[...functionCalls, ...sharpSubmissions]
-        .map(() => `(to_address = ? AND input LIKE ?)`)
-        .join(' OR ')}
+      ${configs.map(() => `(to_address = ? AND input LIKE ?)`).join(' OR ')}
     )
   `
 
-  return { query, params }
+  // @ts-expect-error BigQuery types are wrong
+  const types: QueryParamTypes = [
+    ['STRING'],
+    'STRING',
+    'STRING',
+    ...configs.flatMap(() => ['STRING', 'STRING']),
+  ]
+
+  return { query, params, types }
 }
