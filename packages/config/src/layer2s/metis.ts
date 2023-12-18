8dca7fbbb868a180f57ac56363a82d016aa3823d
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2023-12-18 17:42:23 +0100

    Convert Manta Pacific to Optimium (#2390)

--- packages/config/src/layer2s/metis.ts
+++ packages/config/src/layer2s/metis.ts
@@ -19,7 +19,7 @@ export const metis: Layer2 = {
   display: {
     name: 'Metis Andromeda',
     slug: 'metis',
-    warning:
+    redWarning:
       'Fraud proof system is currently under development. Users need to trust block Proposer to submit correct L1 state roots. \
       Since April 2022 the transaction data is no longer kept on-chain, instead it is kept in MEMO distributed data storage system. \
       The optimistic challenge mechanism that allows Validators to force Sequencer to post missing data is not fully implemented yet.',
