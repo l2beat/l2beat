6304960af78a0697d8e0edd98ebe6731610c4aae
Luca Donno <donnoh99@gmail.com>
2023-12-19 09:14:06 +0100

    Add Lyra (under review)

---
+++ packages/backend/discovery/lyra/ethereum/diffHistory.md
@@ -0,0 +1,76 @@
+# Diff at Tue, 19 Dec 2023 08:04:26 GMT:
+
+- author: Luca Donno (<donnoh99@gmail.com>)
+- comparing to: master@
+
+## Description
+
+Provide description of changes. This section will be preserved.
+
+## Watched changes
+
+```diff
++   Status: CREATED
+    contract OptimismMintableERC20Factory (0x08Dea366F26C25a08C8D1C3568ad07d1e587136d) {
+    }
+```
+
+```diff
++   Status: CREATED
+    contract SystemConfig (0x0e4C4CDd01ceCB01070E9Fdfe7600871e4ae996e) {
+    }
+```
+
+```diff
++   Status: CREATED
+    contract L2OutputOracle (0x1145E7848c8B64c6cab86Fd6D378733385c5C3Ba) {
+    }
+```
+
+```diff
++   Status: CREATED
+    contract ProxyAdmin (0x35d5D43271548c984662d4879FBc8e041Bc1Ff93) {
+    }
+```
+
+```diff
++   Status: CREATED
+    contract LyraMultisig (0x4a4962275DF8C60a80d3a25faEc5AA7De116A746) {
+    }
+```
+
+```diff
++   Status: CREATED
+    contract L1CrossDomainMessenger (0x5456f02c08e9A018E42C39b351328E5AA864174A) {
+    }
+```
+
+```diff
++   Status: CREATED
+    contract L1StandardBridge (0x61E44dC0dae6888B5a301887732217d5725B0bFf) {
+    }
+```
+
+```diff
++   Status: CREATED
+    contract L1ERC721Bridge (0x6CC3268794c5d3E3d9d52adEfC748B59d536cb22) {
+    }
+```
+
+```diff
++   Status: CREATED
+    contract OptimismPortal (0x85eA9c11cf3D4786027F7FD08F4406b15777e5f8) {
+    }
+```
+
+```diff
++   Status: CREATED
+    contract ChallengerMultisig (0x91F4be0C264FAFA1fEd75c4440910Cba2cAd98e8) {
+    }
+```
+
+```diff
++   Status: CREATED
+    contract AddressManager (0xC845F9C4004EB35a8bde8ad89C4760a9c0e65CAB) {
+    }
+```
---
+++ packages/config/src/layer2s/lyra.ts
@@ -0,0 +1,62 @@
+import { EthereumAddress, ProjectId } from '@l2beat/shared-pure'
+
+import { ProjectDiscovery } from '../discovery/ProjectDiscovery'
+import { CONTRACTS, TECHNOLOGY, UNDER_REVIEW_RISK_VIEW } from './common'
+import { Layer2 } from './types'
+
+const discovery = new ProjectDiscovery('lyra')
+
+const upgradesProxy = {
+  upgradableBy: ['ProxyAdmin'],
+  upgradeDelay: 'No delay',
+}
+
+export const lyra: Layer2 = {
+  isUnderReview: true,
+  type: 'layer2',
+  id: ProjectId('lyra'),
+  display: {
+    name: 'Lyra',
+    slug: 'lyra',
+    description:
+      'Lyra Chain is a L2 scaling solution build using OP Stack specially for Lyra protocol - a settlement protocol for spot, perpetuals and options trading.',
+    purpose: 'Exchange',
+    category: 'Optimistic Rollup',
+    dataAvailabilityMode: 'TxData',
+    provider: 'OP Stack',
+    links: {
+      websites: ['https://lyra.finance/'],
+      apps: ['https://www.lyra.finance/portfolio'],
+      documentation: ['https://docs.lyra.finance/docs/introduction'],
+      explorers: ['https://explorer.lyra.finance/'],
+      repositories: ['https://github.com/lyra-finance/v2-core'],
+      socialMedia: [
+        'https://twitter.com/lyrafinance',
+        'https://discord.gg/Lyra',
+      ],
+    },
+  },
+  stage: {
+    stage: 'UnderReview',
+  },
+  config: {
+    escrows: [
+      discovery.getEscrowDetails({
+        address: EthereumAddress('0x85eA9c11cf3D4786027F7FD08F4406b15777e5f8'),
+        tokens: ['ETH'],
+        description: 'Main entry point for users depositing ETH.',
+        ...upgradesProxy,
+      }),
+      discovery.getEscrowDetails({
+        address: EthereumAddress('0x61E44dC0dae6888B5a301887732217d5725B0bFf'),
+        tokens: '*',
+        description:
+          'Main entry point for users depositing ERC20 token that do not require custom gateway.',
+        ...upgradesProxy,
+      }),
+    ],
+  },
+  riskView: UNDER_REVIEW_RISK_VIEW,
+  technology: TECHNOLOGY.UNDER_REVIEW,
+  contracts: CONTRACTS.UNDER_REVIEW,
+}
