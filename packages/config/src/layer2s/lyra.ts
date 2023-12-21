b566cb1a2736b94b66abef149a86548cf759d10f
Adrian Adamiak <adrian@adamiak.net>
2023-12-21 10:53:04 +0100

    New OpStack .ts template (#2412)
    Co-authored-by: Luca Donno <donnoh99@gmail.com>

--- packages/config/src/layer2s/lyra.ts
+++ packages/config/src/layer2s/lyra.ts
@@ -1,20 +1,18 @@
-import { EthereumAddress, ProjectId } from '@l2beat/shared-pure'
+import { EthereumAddress, UnixTime } from '@l2beat/shared-pure'
 
 import { ProjectDiscovery } from '../discovery/ProjectDiscovery'
-import { CONTRACTS, TECHNOLOGY, UNDER_REVIEW_RISK_VIEW } from './common'
+import { opStack } from './templates/opStack'
 import { Layer2 } from './types'
 
 const discovery = new ProjectDiscovery('lyra')
 
-const upgradesProxy = {
+const upgradeability = {
   upgradableBy: ['ProxyAdmin'],
   upgradeDelay: 'No delay',
 }
 
-export const lyra: Layer2 = {
-  isUnderReview: true,
-  type: 'layer2',
-  id: ProjectId('lyra'),
+export const lyra: Layer2 = opStack({
+  discovery,
   display: {
     name: 'Lyra',
     slug: 'lyra',
@@ -36,27 +34,50 @@ export const lyra: Layer2 = {
       ],
     },
   },
-  stage: {
-    stage: 'UnderReview',
+  upgradeability,
+  l1StandardBridgeEscrow: EthereumAddress(
+    '0x61E44dC0dae6888B5a301887732217d5725B0bFf',
+  ),
+  apiUrl: 'https://rpc.lyra.finance',
+  sequencerAddress: EthereumAddress(
+    discovery.getContractValue('SystemConfig', 'batcherHash'),
+  ),
+  inboxAddress: EthereumAddress('0x5f7f7f6DB967F0ef10BdA0678964DBA185d16c50'),
+  genesisTimestamp: new UnixTime(1700022479),
+  l2OutputOracle: discovery.getContract('L2OutputOracle'),
+  portal: discovery.getContract('OptimismPortal'),
+  // stateDerivation: DERIVATION.OPSTACK('LYRA'),
+  milestones: [
+    {
+      name: 'Lyra V2 live on mainnet',
+      link: 'https://x.com/lyrafinance/status/1735516929341980748',
+      date: '2023-12-15T00:00:00Z',
+      description: 'Lyra V2 launches on Mainnet.',
+    },
+  ],
+  knowledgeNuggets: [],
+  roleOverrides: {
+    batcherHash: 'Sequencer',
+    PROPOSER: 'Proposer',
+    GUARDIAN: 'Guardian',
+    CHALLENGER: 'Challenger',
   },
-  config: {
-    escrows: [
-      discovery.getEscrowDetails({
-        address: EthereumAddress('0x85eA9c11cf3D4786027F7FD08F4406b15777e5f8'),
-        tokens: ['ETH'],
-        description: 'Main entry point for users depositing ETH.',
-        ...upgradesProxy,
-      }),
-      discovery.getEscrowDetails({
-        address: EthereumAddress('0x61E44dC0dae6888B5a301887732217d5725B0bFf'),
-        tokens: '*',
-        description:
-          'Main entry point for users depositing ERC20 token that do not require custom gateway.',
-        ...upgradesProxy,
-      }),
-    ],
-  },
-  riskView: UNDER_REVIEW_RISK_VIEW,
-  technology: TECHNOLOGY.UNDER_REVIEW,
-  contracts: CONTRACTS.UNDER_REVIEW,
-}
+  nonTemplatePermissions: [
+    // TODO: check whether the description is correct
+    ...discovery.getMultisigPermission(
+      'LyraMultisig',
+      'This address is the owner of the following contracts: ProxyAdmin, SystemConfig. It can upgrade the bridge implementation potentially gaining access to all funds, and change the sequencer, state root proposer or any other system component (unlimited upgrade power).',
+    ),
+    ...discovery.getMultisigPermission(
+      'ChallengerMultisig',
+      'This address is the permissioned challenger of the system. It can delete non finalized roots without going through the fault proof process. It is also designated as a Guardian of the OptimismPortal, meaning it can halt withdrawals.',
+    ),
+  ],
+  nonTemplateContracts: [
+    discovery.getContractDetails('L1ERC721Bridge', {
+      description:
+        'The L1ERC721Bridge contract is the main entry point to deposit ERC721 tokens from L1 to L2.',
+      ...upgradeability,
+    }),
+  ],
+})
