dedb9d3060af1eebf573ad42c437b9f20244f712
Luca Donno <donnoh99@gmail.com>
2023-12-21 10:17:50 +0100

    fix description

--- packages/config/src/layer2s/lyra.ts
+++ packages/config/src/layer2s/lyra.ts
@@ -66,11 +66,11 @@ export const lyra: Layer2 = opStack({
     // TODO: check whether the description is correct
     ...discovery.getMultisigPermission(
       'LyraMultisig',
-      'This address is the owner of the following contracts: ProxyAdmin, SystemConfig. It is also designated as a Guardian of the OptimismPortal, meaning it can halt withdrawals. It can upgrade the bridge implementation potentially gaining access to all funds, and change the sequencer, state root proposer or any other system component (unlimited upgrade power).',
+      'This address is the owner of the following contracts: ProxyAdmin, SystemConfig. It can upgrade the bridge implementation potentially gaining access to all funds, and change the sequencer, state root proposer or any other system component (unlimited upgrade power).',
     ),
     ...discovery.getMultisigPermission(
       'ChallengerMultisig',
-      'This address is the permissioned challenger of the system. It can delete non finalized roots without going through the fault proof process.',
+      'This address is the permissioned challenger of the system. It can delete non finalized roots without going through the fault proof process. It is also designated as a Guardian of the OptimismPortal, meaning it can halt withdrawals.',
     ),
   ],
   nonTemplateContracts: [
