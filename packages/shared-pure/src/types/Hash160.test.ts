af70f1ea5113fa162e5399c91e023e30247ef1f7
Mateusz Radomski <radomski.main@protonmail.com>
2024-01-03 13:35:06 +0100

    ylff

--- packages/shared-pure/src/types/Hash160.test.ts
+++ packages/shared-pure/src/types/Hash160.test.ts
@@ -4,18 +4,15 @@ import { Hash160 } from './Hash160'
 
 describe(Hash160.name, () => {
   it('accepts lowercase hashes', () => {
-    const hash = Hash160(
-      '0xabcdabcd12345678abcdabcd12345678abcdabcd',
-    )
+    const hash = Hash160('0xabcdabcd12345678abcdabcd12345678abcdabcd')
     expect(hash).toBeA(String)
   })
 
   it('does not accept mixed case hashes', () => {
-    expect(() =>
-      Hash160(
-        '0xabcdabcd12345678ABCDABCD12345678ABCDABCD',
-      ),
-    ).toThrow(TypeError, 'Invalid Hash160')
+    expect(() => Hash160('0xabcdabcd12345678ABCDABCD12345678ABCDABCD')).toThrow(
+      TypeError,
+      'Invalid Hash160',
+    )
   })
 
   it('does not accept invalid strings', () => {
