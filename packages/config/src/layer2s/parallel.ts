20cff2ef7447f5c80964893f903c957e57aab280
hoobi <hubert.kotlinski@l2beat.com>
2024-01-03 12:31:23 +0100

    Added logo and details

--- packages/config/src/layer2s/parallel.ts
+++ packages/config/src/layer2s/parallel.ts
@@ -12,17 +12,22 @@ export const parallel: Layer2 = {
     slug: 'parallel',
     headerWarning: '',
     description:
-      'Parallel will launch an Ethereum Layer2 supporting native yield. More information coming soon.',
+      'Parallel will launch an Ethereum L2 solution utilizing Arbitrum Nitro technology. More information coming soon.',
     purpose: 'Universal, DeFi',
     category: 'Optimistic Rollup',
-    dataAvailabilityMode: 'NotApplicable',
+    dataAvailabilityMode: 'TxData',
+    provider: 'Arbitrum',
     links: {
       websites: ['https://parallel.fi'],
       apps: ['https://parallel.fi/airdrop'],
-      documentation: [],
-      explorers: [],
+      documentation: ['https://docs.parallel.fi/parallel-chain/overview'],
+      explorers: ['https://explorerl2new-surprised-harlequin-bonobo-fvcy2k9oqh.t.conduit.xyz/'], 
       repositories: [],
-      socialMedia: ['https://twitter.com/ParallelFi'],
+      socialMedia: [
+        'https://twitter.com/ParallelFi',
+        'https://discord.gg/rdjVz8zavF',
+        'https://t.me/parallelfi_community',
+      ],
     },
   },
   stage: {
