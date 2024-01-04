139ece230325d1e516f184582b561bda7f23c804
maciekzygmunt <maciekzygmunt@interia.pl>
2024-01-04 10:19:03 +0100

    add tests for icons size

--- packages/frontend/src/test/icons.test.ts
+++ packages/frontend/src/test/icons.test.ts
@@ -1,6 +1,6 @@
 import { bridges, layer2s } from '@l2beat/config'
 import { expect } from 'earl'
-import { existsSync } from 'fs'
+import { existsSync, readFileSync } from 'fs'
 import path from 'path'
 
 describe('icons', () => {
@@ -11,6 +11,22 @@ describe('icons', () => {
         `../static/icons/${project.display.slug}.png`,
       )
       expect(existsSync(iconPath)).toEqual(true)
+    })
+  }
+
+  for (const project of [...layer2s, ...bridges]) {
+    it(`${project.display.name} every icon has proper size`, () => {
+      const iconPath = path.join(
+        __dirname,
+        `../static/icons/${project.display.slug}.png`,
+      )
+
+      const buffer = readFileSync(iconPath)
+      const width = buffer.readUInt32BE(16)
+      const height = buffer.readUInt32BE(20)
+
+      expect(width).toEqual(256)
+      expect(height).toEqual(256)
     })
   }
 })
