09eee2bdd57e50a31848bf812301aed4cc7615e3
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2024-01-05 17:08:56 +0100

    Update Loopring node requirement (#2489)
    Co-authored-by: Bartek Kiepuszewski <bkiepuszewski@gmail.com>

--- packages/config/src/layer2s/loopring.ts
+++ packages/config/src/layer2s/loopring.ts
@@ -170,7 +170,7 @@ export const loopring: Layer2 = {
       callsItselfRollup: true,
       stateRootsPostedToL1: true,
       dataAvailabilityOnL1: true,
-      rollupNodeSourceAvailable: 'UnderReview',
+      rollupNodeSourceAvailable: false,
     },
     stage1: {
       stateVerificationOnL1: true,
