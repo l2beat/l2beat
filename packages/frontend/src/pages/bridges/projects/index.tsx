8dc0add8cd5ff7a30cbe2d9d5889de5409f8127a
Maciej Zygmunt <maciekzygmunt@interia.pl>
2024-01-05 12:04:09 +0100

    Remove get included projects (#2485)
    Co-authored-by: antooni <antoni.pawlak@protonmail.com>

--- packages/frontend/src/pages/bridges/projects/index.tsx
+++ packages/frontend/src/pages/bridges/projects/index.tsx
@@ -2,19 +2,12 @@ import React from 'react'
 
 import { Config } from '../../../build/config'
 import { PageWrapper } from '../../../components'
-import { getIncludedProjects } from '../../../utils/getIncludedProjects'
 import { PagesData } from '../../Page'
 import { getProps } from './props'
 import { ProjectPage } from './view/ProjectPage'
 
 export function getBridgeProjectPages(config: Config, pagesData: PagesData) {
-  const included = getIncludedProjects(
-    config.bridges,
-    pagesData.tvlApiResponse,
-    config.features.buildAllProjectPages,
-  )
-
-  return included.map((bridge) => {
+  return config.bridges.map((bridge) => {
     const { wrapper, props } = getProps(bridge, config, pagesData)
     return {
       slug: `/bridges/projects/${bridge.display.slug}`,
