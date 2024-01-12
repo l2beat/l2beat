f851fd763f925717552829da03d50048e3902b6c
antooni <antoni.pawlak@protonmail.com>
2024-01-12 13:19:16 +0100

    refactor index.ts

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,22 +1,12 @@
-import { getEnv, Logger, LoggerOptions } from '@l2beat/backend-tools'
+import Bugsnag from '@bugsnag/js'
+import { getEnv } from '@l2beat/backend-tools'
 
 import { Application } from './Application'
 import { getConfig } from './config'
-import { initializeErrorReporting, reportError } from './tools/ErrorReporter'
+import { initializeErrorReporting } from './tools/ErrorReporter'
 
-main().catch((e: unknown) => {
-  const loggerOptions: Partial<LoggerOptions> = {
-    logLevel: 'ERROR',
-    format: 'pretty',
-    colors: true,
-    reportError,
-  }
-  const logger = new Logger(loggerOptions)
-
-  logger.error(e)
-
-  // wait 10 seconds for the error to be reported
-  setTimeout(() => process.exit(1), 10_000).unref()
+main().catch(() => {
+  process.exit(1)
 })
 
 async function main() {
@@ -29,7 +19,35 @@ async function main() {
     console.log('Bugsnag integration disabled')
   }
 
-  const config = getConfig()
-  const app = new Application(config)
-  await app.start()
+  try {
+    const config = getConfig()
+    const app = new Application(config)
+    await app.start()
+  } catch (e) {
+    console.error(e)
+
+    if (bugsnagApiKey) {
+      if (typeof e === 'string') {
+        Bugsnag.notify(e, (event) => {
+          event.context = 'Backend index.ts'
+        })
+      } else if (e instanceof Error) {
+        Bugsnag.notify(e, (event) => {
+          event.context = 'Backend index.ts'
+        })
+      } else {
+        Bugsnag.notify('Unknown error', (event) => {
+          event.context = 'Backend index.ts'
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+          event.addMetadata('error', { message: e })
+        })
+      }
+
+      // wait 10 seconds for the error to be reported
+      console.log('Waiting 10 seconds for the error to be reported')
+      await new Promise((resolve) => setTimeout(resolve, 10_000))
+    }
+
+    throw e
+  }
 }
