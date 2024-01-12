fa092c7419198383762bad122548f2a0813d5726
Tomasz Tórz <93620601+torztomasz@users.noreply.github.com>
2024-01-12 13:42:46 +0100

    Rebrand Brine to tanX (#2536)

--- packages/config/src/layer2s/index.ts
+++ packages/config/src/layer2s/index.ts
@@ -11,7 +11,6 @@ import { base } from './base'
 import { blast } from './blast'
 import { bob } from './bob'
 import { bobanetwork } from './bobanetwork'
-import { brine } from './brine'
 import { canto } from './canto'
 import { canvasconnect } from './canvasconnect'
 import { capx } from './capx'
@@ -70,6 +69,7 @@ import { specular } from './specular'
 import { starknet } from './starknet'
 import { stealthchain } from './stealthchain'
 import { taiko } from './taiko'
+import { tanx } from './tanx'
 import { Layer2 } from './types'
 import { x1 } from './x1'
 import { xchain } from './xchain'
@@ -96,7 +96,6 @@ export const layer2s: Layer2[] = [
   blast,
   bob,
   bobanetwork,
-  brine,
   canto,
   capx,
   canvasconnect,
@@ -155,6 +154,7 @@ export const layer2s: Layer2[] = [
   starknet,
   stealthchain,
   taiko,
+  tanx,
   x1,
   xchain,
   zkfair,
