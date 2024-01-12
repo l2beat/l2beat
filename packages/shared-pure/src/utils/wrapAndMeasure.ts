dad1b2e74802b0ac94ca81609e97a85907b826ce
torztomasz <tomasz.torz@l2beat.com>
2024-01-12 19:47:15 +0100

    disable builds

--- packages/shared-pure/src/utils/wrapAndMeasure.ts
+++ packages/shared-pure/src/utils/wrapAndMeasure.ts
@@ -9,7 +9,6 @@ export function wrapAndMeasure<T extends unknown[], U, S extends string>(
   fn: (...args: T) => Promise<U>,
   { histogram, labels }: Metrics<S>,
 ): (...args: T) => Promise<U> {
-  console.log('sdadsadadasdas')
   return async (...args: T) => {
     const done = histogram.startTimer(labels)
     const result = await fn(...args)
