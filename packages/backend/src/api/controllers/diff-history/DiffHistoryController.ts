e63e04c0ea901075d8c278d0667e585e23f049ef
Mateusz Radomski <radomski.main@protonmail.com>
2024-01-12 12:07:17 +0100

    format and remove console.logs

--- packages/backend/src/api/controllers/diff-history/DiffHistoryController.ts
+++ packages/backend/src/api/controllers/diff-history/DiffHistoryController.ts
@@ -17,24 +17,25 @@ export class DiffHistoryController {
   }
 
   async getDiff(chainId: ChainId, project: string): Promise<string> {
-    console.time("database")
     const discoveries = await this.discoveryHistoryRepository.getProject(
       project,
       chainId,
     )
-    console.timeEnd("database")
 
-    console.time("diffing")
-    console.log(discoveries.length)
     const diffs: Record<number, DiscoveryDiff[]> = {}
     const configReader = new ConfigReader()
     const config = await configReader.readConfig(project, chainId)
-    for(let i = 0; i < discoveries.length - 1; i++) {
-        diffs[discoveries[i+1].timestamp.toNumber()] = diffDiscovery(discoveries[i].discovery.contracts, discoveries[i+1].discovery.contracts, config)
+    for (let i = 0; i < discoveries.length - 1; i++) {
+      diffs[discoveries[i + 1].timestamp.toNumber()] = diffDiscovery(
+        discoveries[i].discovery.contracts,
+        discoveries[i + 1].discovery.contracts,
+        config,
+      )
     }
 
-    const result = Object.fromEntries(Object.entries(diffs).filter(([_, z]) => z.length > 0))
-    console.timeEnd("diffing")
+    const result = Object.fromEntries(
+      Object.entries(diffs).filter(([_, z]) => z.length > 0),
+    )
 
     return JSON.stringify(result)
   }
