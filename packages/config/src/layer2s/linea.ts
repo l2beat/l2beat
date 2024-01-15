4b8c99f0237ba18966f627465c35f8be15686ca1
Michał Podsiadły <michal.podsiadly@l2beat.com>
2024-01-15 18:14:55 +0100

    docs: remove details overhead from linea

--- packages/config/src/layer2s/linea.ts
+++ packages/config/src/layer2s/linea.ts
@@ -296,7 +296,7 @@ export const linea: Layer2 = {
     ),
     discovery.contractAsPermissioned(
       discovery.getContract('Roles'),
-      `Module to the AdminMultisig. Allows to add additional members to the multisig via permissions to call functions specified by roles. 0x453B3A4b4d64B4E6f472A306c3D4Fc318C34bbA8 is able to invoke pause() on ERC20Bridge and USDCBridge. Also given address is able to invoke pauseByType() on ZkEvm contract`,
+      `Module to the AdminMultisig. Allows to add additional members to the multisig via permissions to call functions specified by roles.`,
     ),
     {
       accounts: operators,
