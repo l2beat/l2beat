5d48729d4d6d22bb0dab780b705e4565d2a5f58f
Mateusz Radomski <radomski.main@protonmail.com>
2024-01-16 14:25:15 +0100

    Simplifiy and type the diff history endpoints

--- packages/shared-pure/src/types/api/index.ts
+++ packages/shared-pure/src/types/api/index.ts
@@ -1,5 +1,6 @@
 export * from './ActivityApi'
 export * from './FinalityApi'
+export * from './DiffHistoryApi'
 export * from './LivenessApi'
 export * from './ManuallyVerifiedContrats'
 export * from './TokenTvlApi'
