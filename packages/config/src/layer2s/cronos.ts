7bb8dd7c1b9a929eae348475aeb4c5b5e3898942
Mateusz Radomski <radomski.main@protonmail.com>
2024-01-17 16:06:16 +0100

    Move Cronos to the upcoming template

--- packages/config/src/layer2s/cronos.ts
+++ packages/config/src/layer2s/cronos.ts
@@ -1,20 +1,15 @@
-import { ProjectId } from '@l2beat/shared-pure'
-
-import { CONTRACTS, TECHNOLOGY, UPCOMING_RISK_VIEW } from './common'
+import { upcoming } from './templates/upcoming'
 import { Layer2 } from './types'
 
-export const cronos: Layer2 = {
-  isUpcoming: true,
-  type: 'layer2',
-  id: ProjectId('cronos'),
+export const cronos: Layer2 = upcoming({
+  id: 'cronos',
   display: {
     name: 'Cronos zkEVM',
     slug: 'cronos',
     description:
       "Cronos zkEVM is an Ethereum Layer-2 scaling solution that leverages zkSync's ZK Stack, extending the existing portfolio of Cronos apps.",
-    purpose: 'Universal',
+    purposes: ['Universal'],
     category: 'ZK Rollup',
-    dataAvailabilityMode: 'NotApplicable',
     provider: 'ZK Stack',
     links: {
       websites: ['https://cronos.org/'],
@@ -25,13 +20,4 @@ export const cronos: Layer2 = {
       socialMedia: ['https://twitter.com/cronos_chain'],
     },
   },
-  stage: {
-    stage: 'NotApplicable',
-  },
-  config: {
-    escrows: [],
-  },
-  riskView: UPCOMING_RISK_VIEW,
-  technology: TECHNOLOGY.UPCOMING,
-  contracts: CONTRACTS.EMPTY,
-}
+})
