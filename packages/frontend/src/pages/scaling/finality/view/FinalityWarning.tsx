b36b7e8e9206db27ad55edc45903053430c6acf0
torztomasz <tomasz.torz@l2beat.com>
2024-01-18 11:33:42 +0100

    change finality warning

--- packages/frontend/src/pages/scaling/finality/view/FinalityWarning.tsx
+++ packages/frontend/src/pages/scaling/finality/view/FinalityWarning.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 
+import { Link } from '../../../../components/Link'
+
 export function FinalityWarning() {
   return (
     <p className="my-4 rounded-lg bg-yellow-500 p-2 text-center text-xs font-medium text-black">
       Please note, the values on the page reflect the time required to achieve
       ordering finality, at which point transactions cannot be reverted or
-      reordered. <br /> The actionable settlement time for these transactions
-      may be longer.
+      reordered. The actionable settlement time on the L1 (that affects your
+      ability to withdraw your funds on the L1) for these transactions may be
+      longer. In most other cases, however, this does not apply (e.g., when
+      transferring funds to an exchange).For more information on how finality
+      works for L2 transactions, check{' '}
+      <Link href="https://medium.com/l2beat/tracking-time-to-finality-of-l2-transactions-051d32f5d5ba">
+        our article
+      </Link>
+      .
     </p>
   )
 }
