4e1ee5c27b77d1fb11e860e55d6c3b6cd71e656d
Maciej Zygmunt <maciekzygmunt@interia.pl>
2024-01-18 16:47:00 +0100

    Fix text (#2619)

--- packages/frontend/src/components/table/DurationCell.tsx
+++ packages/frontend/src/components/table/DurationCell.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 import { pluralize } from '../../utils/pluralize'
 
-export function DurationCell(props: { durationInSeconds: number }) {
+export function DurationCell(props: {
+  durationInSeconds: number
+  additionalText?: string
+}) {
   const seconds = props.durationInSeconds
   const minutes = Math.floor(props.durationInSeconds / 60)
   const hours = Math.floor(minutes / 60)
@@ -11,19 +14,21 @@ export function DurationCell(props: { durationInSeconds: number }) {
   const duration =
     days > 0 ? (
       <span className="text-orange-600 dark:text-orange-500">
-        {days} {pluralize(days, 'day') + ' + L1 Finality'}
+        {days} {`${pluralize(days, 'day')} ${props.additionalText ?? ''}`}
       </span>
     ) : hours > 0 ? (
       <span className="text-yellow-700 dark:text-yellow-100">
-        {hours} {pluralize(hours, 'hour') + ' + L1 Finality'}
+        {hours} {`${pluralize(hours, 'hour')} ${props.additionalText ?? ''}`}
       </span>
     ) : minutes > 0 ? (
       <span>
-        {minutes} {pluralize(minutes, 'minute') + ' + L1 Finality'}
+        {minutes}{' '}
+        {`${pluralize(minutes, 'minute')} ${props.additionalText ?? ''}`}
       </span>
     ) : (
       <span className="text-green-300 dark:text-green-450">
-        {seconds} {pluralize(seconds, 'second') + ' + L1 Finality'}
+        {seconds}{' '}
+        {`${pluralize(seconds, 'second')} ${props.additionalText ?? ''}`}
       </span>
     )
 
