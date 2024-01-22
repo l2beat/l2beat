4ec67cd8e0e3bb69c10efb30b169eb745fc4f0a4
Michał Sobieraj-Jakubiec <michalsidzej@gmail.com>
2024-01-22 14:52:09 +0100

    fix: read manually verified contracts on the FE

--- packages/frontend/src/build/api/getVerificationStatus.ts
+++ packages/frontend/src/build/api/getVerificationStatus.ts
@@ -1,6 +1,11 @@
-import { VerificationStatus } from '@l2beat/shared-pure'
+import { parseManuallyVerifiedContracts } from '@l2beat/config'
+import {
+  ManuallyVerifiedContracts,
+  VerificationStatus,
+} from '@l2beat/shared-pure'
 import fs from 'fs'
 
+// TODO(michalsj): This should be read from the config. Skipping now as it's urgent fix
 const supportedDevIds = ['ethereum', 'arbitrum']
 
 export function getVerificationStatus(): VerificationStatus {
@@ -32,4 +37,21 @@ function getContractVerificationStatus(devId: string): unknown {
   const path = getContractVerificationPath(devId)
   const contracts = fs.readFileSync(path, 'utf8')
   return JSON.parse(contracts) as unknown
+}
+
+function getManuallyVerifiedContractsPath(devId: string) {
+  return `../config/src/verification/${devId}/manuallyVerified.jsonc`
+}
+export function getManuallyVerifiedContracts(): ManuallyVerifiedContracts {
+  const contracts: ManuallyVerifiedContracts = {}
+  for (const devId of supportedDevIds) {
+    const path = getManuallyVerifiedContractsPath(devId)
+    if (!fs.existsSync(path)) {
+      continue
+    }
+    const data = fs.readFileSync(path, 'utf8')
+    contracts[devId] = parseManuallyVerifiedContracts(data)
+  }
+
+  return contracts
 }
