0cb82c594a5c94229094946ea87ddadbef19709d
Michał Sobieraj-Jakubiec <michalsidzej@gmail.com>
2024-01-22 14:29:15 +0100

    fix: read verification status file path

--- packages/frontend/src/build/api/getVerificationStatus.ts
+++ packages/frontend/src/build/api/getVerificationStatus.ts
@@ -1,17 +1,35 @@
 import { VerificationStatus } from '@l2beat/shared-pure'
 import fs from 'fs'
 
-const PATH = '../config/src/verified.json'
+const supportedDevIds = ['ethereum', 'arbitrum']
 
 export function getVerificationStatus(): VerificationStatus {
-  if (!fs.existsSync(PATH)) {
-    return {
-      projects: {},
-      contracts: {},
-    }
-  }
+  const projects = getProjectVerificationStatus()
+  const contracts = Object.fromEntries(
+    supportedDevIds.map((devId) => [
+      devId,
+      getContractVerificationStatus(devId),
+    ]),
+  )
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const data = JSON.parse(fs.readFileSync(PATH, 'utf8'))
+  const data = {
+    projects,
+    contracts,
+  }
   return VerificationStatus.parse(data)
+}
+
+const PROJECTS_VERIFICATION_PATH = '../config/src/verification/projects.json'
+function getProjectVerificationStatus(): unknown {
+  const projects = fs.readFileSync(PROJECTS_VERIFICATION_PATH, 'utf8')
+  return JSON.parse(projects) as unknown
+}
+
+function getContractVerificationPath(devId: string) {
+  return `../config/src/verification/${devId}/verified.json`
+}
+function getContractVerificationStatus(devId: string): unknown {
+  const path = getContractVerificationPath(devId)
+  const contracts = fs.readFileSync(path, 'utf8')
+  return JSON.parse(contracts) as unknown
 }
