d49c46ee179b9958c9297f0880945f782da83082
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2024-01-23 10:43:32 +0100

    Use enums

--- packages/config/src/tokens/types.ts
+++ packages/config/src/tokens/types.ts
@@ -10,27 +10,16 @@ export type SourceEntry = z.infer<typeof SourceEntry>
 export const SourceEntry = z.object({
   symbol: z.string(),
   address: stringAs(EthereumAddress).optional(),
-  coingeckoId: stringAs((s) => CoingeckoId(s)).optional(),
-  category: z
-    .union([z.literal('ether'), z.literal('stablecoin'), z.literal('other')])
-    .optional(),
-  type: z
-    .union([z.literal('CBV'), z.literal('EBV'), z.literal('NMV')])
-    .optional(),
-  formula: z
-    .union([
-      z.literal('totalSupply'),
-      z.literal('locked'),
-      z.literal('circulatingSupply'),
-    ])
-    .optional(),
-
-  bridgedUsing: z.optional(
-    z.object({
+  coingeckoId: stringAs(CoingeckoId).optional(),
+  category: z.enum(['ether', 'stablecoin', 'other']).optional(),
+  type: z.enum(['CBV', 'EBV', 'NMV']).optional(),
+  formula: z.enum(['totalSupply', 'locked', 'circulatingSupply']).optional(),
+  bridgedUsing: z
+    .object({
       bridge: z.string(),
       slug: z.string().optional(),
-    }),
-  ),
+    })
+    .optional(),
 })
 
 export type Source = z.infer<typeof Source>
