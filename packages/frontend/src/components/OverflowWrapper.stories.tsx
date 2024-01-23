8fbb628c73872f54d8ad8f047b1f78f3f437381c
torztomasz <tomasz.torz@l2beat.com>
2024-01-23 11:58:24 +0100

    fix overflow wrapper

--- packages/frontend/src/components/OverflowWrapper.stories.tsx
+++ packages/frontend/src/components/OverflowWrapper.stories.tsx
@@ -14,7 +14,7 @@ const meta: Meta<typeof OverflowWrapper> = {
     (Story) => {
       useEffect(() => {
         configureOverflowWrappers()
-      })
+      }, [])
 
       return <Story />
     },
