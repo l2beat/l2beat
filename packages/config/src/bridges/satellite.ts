eb794876d427a130da68603637f005cd655a29bc
Bartek Kiepuszewski <bkiepuszewski@gmail.com>
2024-01-23 12:36:43 +0100

    add permissions

--- packages/config/src/bridges/satellite.ts
+++ packages/config/src/bridges/satellite.ts
@@ -5,6 +5,15 @@ import { Bridge } from './types'
 
 const discovery = new ProjectDiscovery('satellite')
 
+const mintLimiterSigners = discovery.getContractValue<string[]>(
+  'Multisig',
+  'signerAccounts',
+)
+const mintLimiterThreshold = discovery.getContractValue<number>(
+  'Multisig',
+  'signerThreshold',
+)
+
 export const satellite: Bridge = {
   type: 'bridge',
   id: ProjectId('satellite'),
@@ -106,6 +115,19 @@ export const satellite: Bridge = {
       sentiment: 'warning',
     },
   },
+  permissions: [
+    {
+      name: 'MintLimiter Multisig',
+      accounts: discovery.getPermissionedAccounts('Multisig', 'signerAccounts'),
+      description: 'Members of the Multisig that can set mint limits.',
+    },
+    {
+      name: 'Operators',
+      accounts: [],
+      description:
+        'Axelar operators are a list of Axelar validators for the current epoch that can relay messages.',
+    },
+  ],
   contracts: {
     addresses: [
       discovery.getContractDetails(
@@ -114,12 +136,15 @@ export const satellite: Bridge = {
       ),
       discovery.getContractDetails(
         'AxelarAuthWeighted',
-        'Contract verifying  Axelar network Verifier signatures.',
+        'Contract verifying  Axelar network Verifier signatures. It stores the list of operators that can relay messages. Operators can be changed by the owner of this contract. Owner is set to be the Gateway itself.',
       ),
-      discovery.getContractDetails('InterchainGovernance', '...'),
+      discovery.getContractDetails(
+        'InterchainGovernance',
+        'Governance contract that executes on-chain governance proposals from Axelar network. It is authorised to upgrade Axelar Gateway.',
+      ),
       discovery.getContractDetails(
         'Multisig',
-        'Admin Multisig setting mint limits.',
+        `Admin Multisig setting mint limits. Acts as a ${mintLimiterThreshold}-of-${mintLimiterSigners.length} multisig.`,
       ),
     ],
     risks: [],
