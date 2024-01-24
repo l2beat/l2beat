50c55fa7d1ad685c56f0ada06e01262d7eceaaff
torztomasz <tomasz.torz@l2beat.com>
2024-01-24 11:43:38 +0100

    fix interactivity-disabled colors

--- packages/frontend/src/components/chart/CurrencyControls.tsx
+++ packages/frontend/src/components/chart/CurrencyControls.tsx
@@ -7,7 +7,7 @@ export function CurrencyControls() {
     <RadioGroup
       role="chart-currency-controls"
       name="currency"
-      className="transition-colors duration-200 group-data-[interactivity-disabled]/chart:bg-gray-100 dark:group-data-[interactivity-disabled]/chart:bg-zinc-900"
+      className="transition-colors duration-200 group-data-[interactivity-disabled]/chart:bg-gray-100 dark:group-data-[interactivity-disabled]/chart:bg-zinc-700"
       optionsClassname="group-data-[interactivity-disabled]/chart:opacity-0 transition-opacity duration-200 group-data-[interactivity-disabled]/chart:pointer-events-none"
       options={[{ value: 'USD', checked: true }, { value: 'ETH' }]}
     />
