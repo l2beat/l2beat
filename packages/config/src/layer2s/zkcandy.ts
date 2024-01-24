972361f0bfd0a34779c965daf5547e1edcda63f6
Luca Donno <donnoh99@gmail.com>
2024-01-24 08:54:10 +0100

    fix tests

--- packages/config/src/layer2s/zkcandy.ts
+++ packages/config/src/layer2s/zkcandy.ts
@@ -7,17 +7,21 @@ export const zkcandy: Layer2 = upcoming({
     name: 'zkCandy',
     slug: 'zkcandy',
     description:
-      "zkCandy is an L2 Gaming Hyperchain for the next generation of GameFi - Supercharged by iCandy & zkSync ZK Stack. iCandy is the largest game developer in ANZ and SEA, publicly traded on the ASX with a world-class portfolio of award-winning titles enjoyed by millions of gamers on mobile, console, and PC, as well as ambitious projects in Web3 gaming, NFTs, and the metaverse.",
-    purposes: ['Universal','Gaming'],
+      "zkCandy is an L2 Gaming Hyperchain built on zkSync's ZK stack for the next generation of GameFi - Supercharged by iCandy, the largest game developer in ANZ and SEA.",
+    purposes: ['Universal', 'Gaming'],
     category: 'ZK Rollup',
     provider: 'ZK Stack',
     links: {
-      websites: ['https://zkcandy.io','https://www.icandy.io/'],
+      websites: ['https://zkcandy.io', 'https://icandy.io/'],
       apps: [],
       documentation: [],
       explorers: [],
       repositories: [],
-      socialMedia: ['https://twitter.com/zkCandyHQ','https://discord.gg/zkcandy','https://t.me/zkcandy'],
+      socialMedia: [
+        'https://twitter.com/zkCandyHQ',
+        'https://discord.gg/zkcandy',
+        'https://t.me/zkcandy',
+      ],
     },
   },
 })
