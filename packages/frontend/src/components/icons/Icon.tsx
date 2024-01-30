445db6e7a24e46e5296b7ed4e45772834bd22404
torztomasz <tomasz.torz@l2beat.com>
2024-01-30 08:51:06 +0100

    fix icon

--- packages/frontend/src/components/icons/Icon.tsx
+++ packages/frontend/src/components/icons/Icon.tsx
@@ -6,7 +6,8 @@ export function Icon({ className, ...rest }: SVGAttributes<SVGElement>) {
       width="24"
       height="24"
       viewBox="0 0 24 24"
-      className={className ?? 'fill-black dark:fill-white'}
+      className={className}
+      fill="currentColor"
       role="img"
       {...rest}
     />
