854addcd7989caa8e9f255dc774d2b6e473359c5
torztomasz <tomasz.torz@l2beat.com>
2024-01-31 12:37:19 +0100

    disable link stories for mobiles

--- packages/frontend/src/components/Link.stories.tsx
+++ packages/frontend/src/components/Link.stories.tsx
@@ -1,9 +1,15 @@
 import { Meta, StoryObj } from '@storybook/react'
 
+import { onlyDesktopModes } from '../../.storybook/modes'
 import { Link } from './Link'
 
 const meta: Meta<typeof Link> = {
   component: Link,
+  parameters: {
+    chromatic: {
+      modes: onlyDesktopModes,
+    },
+  },
 }
 export default meta
 type Story = StoryObj<typeof Link>
@@ -45,7 +51,7 @@ export const WithoutUnderline: Story = {
 export const Highlighted: Story = {
   args: {
     children: 'Example link',
-    //@ts-ignore
+    //@ts-expect-error data-state is not handled by the Story type
     'data-state': 'highlighted',
   },
 }
