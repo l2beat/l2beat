b8750ca94bfb0451983480df3b3d69c0754d7bb7
antooni <antoni.pawlak@protonmail.com>
2024-02-01 11:40:50 +0100

    function for tests

--- packages/backend/src/peripherals/database/shared/deleteArchivedRecords.ts
+++ packages/backend/src/peripherals/database/shared/deleteArchivedRecords.ts
@@ -1,4 +1,5 @@
 import { UnixTime } from '@l2beat/shared-pure'
+import { expect } from 'earl'
 import { Knex } from 'knex'
 
 /**
@@ -37,4 +38,63 @@ export function _TVL_ONLY_deleteSixHourlyUntil(
       .andWhereRaw(`extract(hour from "unix_timestamp") % 6 = 0`)
       .delete()
   )
+}
+
+interface Repository {
+  deleteHourlyUntil: (timestamp: UnixTime) => Promise<number>
+  deleteSixHourlyUntil: (timestamp: UnixTime) => Promise<number>
+  addMany: (records: any[]) => Promise<number>
+  getAll: () => Promise<any[]>
+}
+
+export function _TVL_ONLY_deletion_test<T>(
+  repository: Repository,
+  fakeRecord: (timestamp: UnixTime) => T,
+) {
+  it('deletes hourly records', async () => {
+    const start = UnixTime.now().toStartOf('day')
+    const until = start.add(25, 'hours')
+
+    const entries = []
+    for (let i = start.toNumber(); i <= until.toNumber(); i += UnixTime.HOUR) {
+      entries.push(fakeRecord(new UnixTime(i)))
+    }
+
+    await repository.addMany(entries)
+    await repository.deleteHourlyUntil(until)
+    const results = await repository.getAll()
+
+    expect(results).toEqualUnsorted([
+      fakeRecord(start),
+      fakeRecord(start.add(6, 'hours')),
+      fakeRecord(start.add(12, 'hours')),
+      fakeRecord(start.add(18, 'hours')),
+      fakeRecord(start.add(24, 'hours')),
+      fakeRecord(start.add(25, 'hours')),
+    ])
+  })
+
+  it('deletes six hourly records', async () => {
+    const start = UnixTime.now().toStartOf('day')
+    const until = start.add(7, 'hours')
+
+    const entries = []
+    for (let i = start.toNumber(); i <= until.toNumber(); i += UnixTime.HOUR) {
+      entries.push(fakeRecord(new UnixTime(i)))
+    }
+
+    await repository.addMany(entries)
+    await repository.deleteSixHourlyUntil(until)
+    const results = await repository.getAll()
+
+    expect(results).toEqualUnsorted([
+      fakeRecord(start),
+      fakeRecord(start.add(1, 'hours')),
+      fakeRecord(start.add(2, 'hours')),
+      fakeRecord(start.add(3, 'hours')),
+      fakeRecord(start.add(4, 'hours')),
+      fakeRecord(start.add(5, 'hours')),
+      fakeRecord(start.add(7, 'hours')),
+    ])
+  })
 }
