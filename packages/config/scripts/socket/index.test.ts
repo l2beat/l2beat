f6b06eb8a6a8309b178aae0021682bdaef2705a8
Mateusz Radomski <radomski.main@protonmail.com>
2024-02-05 12:37:45 +0100

    Fix Socket tests

--- packages/config/scripts/socket/index.test.ts
+++ packages/config/scripts/socket/index.test.ts
@@ -6,7 +6,7 @@ import { ProjectDiscovery } from '../../src/discovery/ProjectDiscovery'
 import { SocketVaults } from './schema'
 
 // FIXME: this test is not working
-describe.skip('socket', () => {
+describe('socket', () => {
   it('plugs should be up to date with the discovery output', async () => {
     const file = await readFile('src/bridges/socket-vaults.json', 'utf-8')
     const socketData = SocketVaults.parse(JSON.parse(file))
