00f67723bc22f8589c6c18512004415df6f56731
torztomasz <tomasz.torz@l2beat.com>
2024-02-06 15:40:25 +0100

    add metaimage for gov page

--- packages/frontend/src/pages/governance/index/props/getPageMetadata.ts
+++ packages/frontend/src/pages/governance/index/props/getPageMetadata.ts
@@ -4,8 +4,7 @@ export function getPageMetadata(): PageMetadata {
   return {
     title: 'Governance - L2BEAT',
     description: 'Governance - L2BEAT',
-    // TODO: (governance) some image?
-    image: '',
+    image: 'https://l2beat.com/meta-images/governance.png',
     url: 'https://l2beat.com/governance',
   }
 }
