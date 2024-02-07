189af10272ce4f6138d08d3cf51dfbec05248194
Radina Talanova <radinatalanova@Radinas-MacBook-Air.local>
2024-02-07 15:33:38 +0200

    Add OEV (upcoming)

---
+++ packages/config/src/layer2s/oev.ts
@@ -0,0 +1,32 @@
+import { upcoming } from './templates/upcoming'
+import { Layer2 } from './types'
+
+export const oev: Layer2 = upcoming({
+  id: 'oev',
+  display: {
+    name: 'OEV Network',
+    slug: 'oev',
+    description:
+      'OEV is an upcoming Validium by API3, built with Polygon CDK. It is designed to capture oracle extractable value and return it to the dApps and their users that generated it.',
+    purposes: ['Auctions'],
+    category: 'Validium',
+    provider: 'Polygon',
+    links: {
+      websites: ['https://api3.org/'],
+      apps: [
+        'https://oev-network-sepolia-testnet-bridge.eu-north-2.gateway.fm',
+      ],
+      documentation: [
+        'https://medium.com/api3/oracle-extractable-value-oev-13c1b6d53c5b',
+      ],
+      explorers: [
+        'https://oev-network-sepolia-testnet-blockscout.eu-north-2.gateway.fm',
+      ],
+      repositories: [],
+      socialMedia: [
+        'https://twitter.com/API3DAO',
+        'https://discord.com/invite/api3dao',
+      ],
+    },
+  },
+})
