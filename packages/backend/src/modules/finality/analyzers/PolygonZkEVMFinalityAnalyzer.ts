241ede5914dd8c9d22b614f8e3796cd273b6ca5e
maciekzygmunt <maciekzygmunt@interia.pl>
2024-02-13 15:31:47 +0100

    update analyzer to new BaseAnalyzer

--- packages/backend/src/modules/finality/analyzers/PolygonZkEVMFinalityAnalyzer.ts
+++ packages/backend/src/modules/finality/analyzers/PolygonZkEVMFinalityAnalyzer.ts
@@ -1,61 +1,17 @@
-import {
-  assert,
-  LivenessType,
-  notUndefined,
-  ProjectId,
-  UnixTime,
-} from '@l2beat/shared-pure'
+import { LivenessType, ProjectId, UnixTime } from '@l2beat/shared-pure'
 import { utils } from 'ethers'
 
-import { RpcClient } from '../../../peripherals/rpcclient/RpcClient'
-import { LivenessRepository } from '../../liveness/repositories/LivenessRepository'
 import { BaseAnalyzer } from './types/BaseAnalyzer'
 
 type PolygonZkEVMDecoded = [[string, string, number, number][], string]
 
-export class PolygonZkEVMFinalityAnalyzer implements BaseAnalyzer {
-  constructor(
-    private readonly provider: RpcClient,
-    private readonly livenessRepository: LivenessRepository,
-  ) {}
+export class PolygonZkEVMFinalityAnalyzer extends BaseAnalyzer {
+  getProjectId(): ProjectId {
+    return ProjectId('polygonzkevm')
+  }
 
-  async getFinalityWithGranularity(
-    from: UnixTime,
-    to: UnixTime,
-    granularity: number,
-  ) {
-    assert(to.toNumber() > from.toNumber())
-    const interval = (to.toNumber() - from.toNumber()) / granularity
-
-    const transactions = (
-      await Promise.all(
-        Array.from({ length: granularity }).map(async (_, i) => {
-          const targetTimestamp = to.add(-interval * i, 'seconds')
-          const lowerBound = targetTimestamp.add(-interval, 'seconds')
-
-          return this.livenessRepository.findTransactionWithinTimeRange(
-            ProjectId('polygonzkevm'),
-            LivenessType('DA'),
-            targetTimestamp,
-            lowerBound,
-          )
-        }),
-      )
-    ).filter(notUndefined)
-
-    if (!transactions.length) {
-      return undefined
-    }
-
-    const finalityDelays = (
-      await Promise.all(
-        transactions.map(async (transaction) => {
-          return this.getFinality(transaction)
-        }),
-      )
-    ).flat()
-
-    return finalityDelays
+  getLivenessType(): LivenessType {
+    return LivenessType('DA')
   }
 
   async getFinality(transaction: {
--- packages/backend/src/modules/finality/analyzers/types/BaseAnalyzer.ts
+++ packages/backend/src/modules/finality/analyzers/types/BaseAnalyzer.ts
@@ -1,9 +1,63 @@
-import { UnixTime } from '@l2beat/shared-pure'
+import {
+  assert,
+  LivenessType,
+  notUndefined,
+  ProjectId,
+  UnixTime,
+} from '@l2beat/shared-pure'
 
-export interface BaseAnalyzer {
-  getFinalityWithGranularity(
+import { RpcClient } from '../../../../peripherals/rpcclient/RpcClient'
+import { LivenessRepository } from '../../../liveness/repositories/LivenessRepository'
+
+export abstract class BaseAnalyzer {
+  constructor(
+    protected readonly provider: RpcClient,
+    protected readonly livenessRepository: LivenessRepository,
+  ) {}
+
+  async getFinalityWithGranularity(
     from: UnixTime,
     to: UnixTime,
     granularity: number,
-  ): Promise<number[] | undefined>
+  ) {
+    assert(to.toNumber() > from.toNumber())
+    const interval = (to.toNumber() - from.toNumber()) / granularity
+
+    const transactions = (
+      await Promise.all(
+        Array.from({ length: granularity }).map(async (_, i) => {
+          const targetTimestamp = to.add(-interval * i, 'seconds')
+          const lowerBound = targetTimestamp.add(-interval, 'seconds')
+
+          return this.livenessRepository.findTransactionWithinTimeRange(
+            this.getProjectId(),
+            this.getLivenessType(),
+            targetTimestamp,
+            lowerBound,
+          )
+        }),
+      )
+    ).filter(notUndefined)
+
+    if (!transactions.length) {
+      return undefined
+    }
+
+    const finalityDelays = (
+      await Promise.all(
+        transactions.map(async (transaction) => {
+          return this.getFinality(transaction)
+        }),
+      )
+    ).flat()
+
+    return finalityDelays
+  }
+
+  abstract getProjectId(): ProjectId
+  abstract getLivenessType(): LivenessType
+  abstract getFinality(transaction: {
+    txHash: string
+    timestamp: UnixTime
+  }): Promise<number[]>
 }
