3e973ed85701a8b5d40714d9710e9b39b6fcfdd2
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2024-02-14 09:58:39 +0100

    Split UpdateMonitor from status

--- packages/backend/src/modules/status/api/StatusController.ts
+++ packages/backend/src/modules/status/api/StatusController.ts
@@ -1,19 +1,6 @@
-import { ConfigReader, DiscoveryDiff } from '@l2beat/discovery'
-
-import { Project } from '../../../model/Project'
 import { IndexerStateRepository } from '../../../peripherals/database/repositories/IndexerStateRepository'
-import { ChainConverter } from '../../../tools/ChainConverter'
 import { Clock } from '../../../tools/Clock'
 import { LivenessConfigurationRepository } from '../../liveness/repositories/LivenessConfigurationRepository'
-import { UpdateMonitorRepository } from '../../update-monitor/repositories/UpdateMonitorRepository'
-import { getDashboardContracts } from './discovery/props/getDashboardContracts'
-import {
-  DashboardProject,
-  getDashboardProjects,
-} from './discovery/props/getDashboardProjects'
-import { getDiff } from './discovery/props/utils/getDiff'
-import { renderDashboardPage } from './discovery/view/DashboardPage'
-import { renderDashboardProjectPage } from './discovery/view/DashboardProjectPage'
 import {
   LivenessStatusPageProps,
   renderLivenessStatusPage,
@@ -22,54 +9,10 @@ import { renderStatusPagesLinksPage } from './view/StatusPagesLinksPage'
 
 export class StatusController {
   constructor(
-    private readonly updateMonitorRepository: UpdateMonitorRepository,
     private readonly clock: Clock,
-    private readonly projects: Project[],
-    private readonly configReader: ConfigReader,
     private readonly indexerStateRepository: IndexerStateRepository,
     private readonly livenessConfigurationRepository: LivenessConfigurationRepository,
-    private readonly chainConverter: ChainConverter,
   ) {}
-
-  async getDiscoveryDashboard(): Promise<string> {
-    const projects: Record<string, DashboardProject[]> = {}
-    const chains = this.configReader.readAllChains()
-    for (const chain of chains) {
-      const projectsToFill = chain === 'ethereum' ? this.projects : []
-      projects[chain] = await getDashboardProjects(
-        projectsToFill,
-        this.configReader,
-        this.updateMonitorRepository,
-        chain,
-        this.chainConverter.toChainId(chain),
-      )
-    }
-
-    return renderDashboardPage({ projects })
-  }
-
-  async getDiscoveryDashboardProject(
-    project: string,
-    chain: string,
-  ): Promise<string> {
-    const discovery = await this.configReader.readDiscovery(project, chain)
-    const config = await this.configReader.readConfig(project, chain)
-    const contracts = getDashboardContracts(discovery, config)
-
-    const diff: DiscoveryDiff[] = await getDiff(
-      this.updateMonitorRepository,
-      discovery,
-      config,
-      this.chainConverter.toChainId(chain),
-    )
-
-    return renderDashboardProjectPage({
-      chain,
-      projectName: project,
-      contracts,
-      diff,
-    })
-  }
 
   async getLivenessStatus() {
     const indexerState = await this.indexerStateRepository.findIndexerState(
--- packages/backend/src/modules/status/api/StatusRouter.ts
+++ packages/backend/src/modules/status/api/StatusRouter.ts
@@ -1,7 +1,5 @@
 import Router from '@koa/router'
-import { z } from 'zod'
 
-import { withTypedContext } from '../../../api/types'
 import { StatusController } from './StatusController'
 
 export function createStatusRouter(statusController: StatusController) {
@@ -10,28 +8,6 @@ export function createStatusRouter(statusController: StatusController) {
   router.get('/status', (ctx) => {
     ctx.body = statusController.getStatusPagesLinks()
   })
-
-  router.get('/status/discovery', async (ctx) => {
-    ctx.body = await statusController.getDiscoveryDashboard()
-  })
-
-  router.get(
-    '/status/discovery/:chain/:project',
-    withTypedContext(
-      z.object({
-        params: z.object({
-          chain: z.string(),
-          project: z.string(),
-        }),
-      }),
-      async (ctx) => {
-        ctx.body = await statusController.getDiscoveryDashboardProject(
-          ctx.params.project,
-          ctx.params.chain,
-        )
-      },
-    ),
-  )
 
   router.get('/status/liveness', async (ctx) => {
     ctx.body = await statusController.getLivenessStatus()
