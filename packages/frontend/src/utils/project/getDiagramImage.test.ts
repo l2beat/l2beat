4dc7dd9353add4359616ce68c85051f4ba3f3906
torztomasz <tomasz.torz@l2beat.com>
2024-02-19 16:35:46 +0100

    fix formatting

--- packages/frontend/src/utils/project/getDiagramImage.test.ts
+++ packages/frontend/src/utils/project/getDiagramImage.test.ts
@@ -1,14 +1,16 @@
-import { expect } from 'earl';
-import mockFs from 'mock-fs';
-import path from 'path';
+import { expect } from 'earl'
+import mockFs from 'mock-fs'
+import path from 'path'
 
-import { getDiagramImage } from "./getDiagramImage";
+import { getDiagramImage } from './getDiagramImage'
 
 describe(getDiagramImage.name, () => {
-
   it('returns the correct path if the file exists', () => {
     mockFs({
-      [path.join(__dirname, `../../static/images/upgrades-and-governance/l2beat.png`)]: 'content'
+      [path.join(
+        __dirname,
+        `../../static/images/upgrades-and-governance/l2beat.png`,
+      )]: 'content',
     })
 
     const result = getDiagramImage('upgrades-and-governance', 'l2beat')
