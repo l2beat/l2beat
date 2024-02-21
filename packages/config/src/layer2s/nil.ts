9ad9d89744180cc2ef18a728e594affd804f93fe
Aleksao998 <aleksaopacic988@gmail.com>
2024-02-21 16:27:23 +0100

    Fix format

--- packages/config/src/layer2s/nil.ts
+++ packages/config/src/layer2s/nil.ts
@@ -2,23 +2,21 @@ import { upcoming } from './templates/upcoming'
 import { Layer2 } from './types'
 
 export const nil: Layer2 = upcoming({
-    id: '=nil;',
-    display: {
-        name: '=nil;',
-        slug: 'nil',
-        description:
-            "=nil; is a zkRollup that securely scales Ethereum through zkSharding, empowering web3 developers to build scalable and composable applications.",
-        purposes: ['Universal'],
-        category: 'ZK Rollup',
-        links: {
-            websites: ['https://nil.foundation/'],
-            apps: [],
-            documentation: ['https://docs.nil.foundation/nil/'],
-            explorers: [],
-            repositories: [],
-            socialMedia: [
-                'https://twitter.com/nil_foundation',
-            ],
-        },
+  id: '=nil;',
+  display: {
+    name: '=nil;',
+    slug: 'nil',
+    description:
+      '=nil; is a zkRollup that securely scales Ethereum through zkSharding, empowering web3 developers to build scalable and composable applications.',
+    purposes: ['Universal'],
+    category: 'ZK Rollup',
+    links: {
+      websites: ['https://nil.foundation/'],
+      apps: [],
+      documentation: ['https://docs.nil.foundation/nil/'],
+      explorers: [],
+      repositories: [],
+      socialMedia: ['https://twitter.com/nil_foundation'],
     },
+  },
 })
