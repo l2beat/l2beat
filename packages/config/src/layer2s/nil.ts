f374298552953b34aa6ce3e2415efef021ea107a
Opacic Aleksa <opacicaleksa@Opacics-Mac-mini.local>
2024-02-21 14:58:32 +0100

    Add =nil; as upcoming L2 rollup

---
+++ packages/config/src/layer2s/nil.ts
@@ -0,0 +1,24 @@
+import { upcoming } from './templates/upcoming'
+import { Layer2 } from './types'
+
+export const nil: Layer2 = upcoming({
+    id: '=nil;',
+    display: {
+        name: '=nil;',
+        slug: 'nil',
+        description:
+            "=nil; is a zkRollup that securely scales Ethereum to 60,000+ TPS through zkSharding, empowering web3 developers to build scalable, secure, and composable applications.",
+        purposes: ['Universal'],
+        category: 'ZK Rollup',
+        links: {
+            websites: ['https://nil.foundation/'],
+            apps: [],
+            documentation: ['https://docs.nil.foundation/nil/'],
+            explorers: [],
+            repositories: [],
+            socialMedia: [
+                'https://twitter.com/nil_foundation',
+            ],
+        },
+    },
+})
