2a6601d833c82a9796f627cbb2834b3d24045d05
Bartek Kiepuszewski <bkiepuszewski@gmail.com>
2024-02-23 14:46:20 +0100

    small cleanup

--- packages/backend/scripts/compareOrbitChains.ts
+++ packages/backend/scripts/compareOrbitChains.ts
@@ -1,12 +1,5 @@
-import Convert from 'ansi-to-html'
-import chalk from 'chalk'
-import { execSync } from 'child_process'
-import { readFileSync } from 'fs'
-import http from 'http'
-import path from 'path'
 import { ConfigReader } from '@l2beat/discovery'
-import { bridges, layer2s } from '@l2beat/config'
-import { assert, EthereumAddress } from '@l2beat/shared-pure'
+import { assert } from '@l2beat/shared-pure'
 
 const chainMapping: Record<string, string> = {
   arbitrum: 'ethereum',
@@ -16,12 +9,18 @@ const chainMapping: Record<string, string> = {
   rari: 'arbitrum',
 }
 
-const chainIds: Record<number, string> = {
-  42161: 'arbitrum',
-  1024: 'parallel',
-  660279: 'xai',
-  20231119: 'deri',
-  1380012617: 'rari',
+function getChainName(chainId: number | undefined): string {
+  const chainIds: Record<number, string> = {
+    42161: 'arbitrum',
+    1024: 'parallel',
+    660279: 'xai',
+    20231119: 'deri',
+    1380012617: 'rari',
+  }
+  if (chainId === undefined) {
+    return 'Unknown chainId'
+  }
+  return chainIds[chainId] || `Unknown chainId: ${chainId}`
 }
 
 const descriptions: Record<string, string> = {
@@ -48,7 +47,7 @@ async function main() {
       printUsage()
       break
     case 'print':
-      await analyseOrbitChain(config)
+      await compareTwoOrbitChain(config)
       break
     case 'all':
       await analyseAllOrbitChains()
@@ -57,7 +56,9 @@ async function main() {
 }
 
 function printUsage(): void {
-  console.log('Usage: yarn analyse-orbit-chains --chains=<l2s | bridges | all>')
+  console.log(
+    'Usage: yarn analyse-orbit-chains <--chains=<firstOrbitchain, secondOrbitChain> | --all>',
+  )
 }
 
 function parseCliParameters(): Config {
@@ -79,7 +80,6 @@ function parseCliParameters(): Config {
     if (inputStr === undefined) {
       return { mode: 'help' }
     }
-    console.log(inputStr)
     const chains = inputStr.split(',')
     input = chains
   }
@@ -114,9 +114,9 @@ async function analyseAllOrbitChains(): Promise<void> {
   for (const rollup of rollups) {
     console.log(
       'Rollup:',
-      rollup?.values.chainId,
+      rollup?.values?.chainId,
       ':',
-      chainIds[rollup?.values.chainId],
+      getChainName(rollup.values.chainId),
     )
   }
   console.log()
@@ -130,10 +130,10 @@ async function analyseAllOrbitChains(): Promise<void> {
   const data = []
   for (const rollup of rollups) {
     data.push({
-      name: chainIds[rollup?.values.chainId],
-      confirmPeriodBlocks: rollup?.values.confirmPeriodBlocks,
-      extraChallengeTimeBlocks: rollup?.values.extraChallengeTimeBlocks,
-      VALIDATOR_AFK_BLOCKS: rollup?.values.VALIDATOR_AFK_BLOCKS,
+      name: getChainName(rollup?.values?.chainId),
+      confirmPeriodBlocks: rollup?.values?.confirmPeriodBlocks,
+      extraChallengeTimeBlocks: rollup?.values?.extraChallengeTimeBlocks,
+      VALIDATOR_AFK_BLOCKS: rollup?.values?.VALIDATOR_AFK_BLOCKS,
     })
   }
   console.table(data)
@@ -141,36 +141,36 @@ async function analyseAllOrbitChains(): Promise<void> {
   const data1 = []
   for (const rollup of rollups) {
     data1.push({
-      name: chainIds[rollup?.values.chainId],
-      setValidatorCount: rollup?.values.setValidatorCount,
-      stakerCount: rollup?.values.stakerCount,
+      name: getChainName(rollup?.values.chainId),
+      setValidatorCount: rollup?.values?.setValidatorCount,
+      stakerCount: rollup?.values?.stakerCount,
     })
   }
   console.table(data1)
 
-  const data3 = []
-  for (const rollup of rollups) {
-    data3.push({
-      name: chainIds[rollup?.values.chainId],
-      baseStake: rollup?.values.baseStake,
-      currentRequiredStake: rollup?.values.currentRequiredStake,
-      stakeToken: rollup?.values.stakeToken,
-    })
-  }
-  console.table(data3)
-
   const data2 = []
   for (const rollup of rollups) {
     data2.push({
-      name: chainIds[rollup?.values.chainId],
-      wasmModuleRoot: rollup?.values.wasmModuleRoot,
-      isERC20Enabled: rollup?.values.isERC20Enabled,
+      name: getChainName(rollup?.values.chainId),
+      baseStake: rollup?.values?.baseStake,
+      currentRequiredStake: rollup?.values?.currentRequiredStake,
+      stakeToken: rollup?.values?.stakeToken,
     })
   }
   console.table(data2)
+
+  const data3 = []
+  for (const rollup of rollups) {
+    data3.push({
+      name: getChainName(rollup?.values.chainId),
+      wasmModuleRoot: rollup?.values?.wasmModuleRoot,
+      isERC20Enabled: rollup?.values?.isERC20Enabled,
+    })
+  }
+  console.table(data3)
 }
 
-async function analyseOrbitChain(config: Config): Promise<void> {
+async function compareTwoOrbitChain(config: Config): Promise<void> {
   console.log('Analyzing', config.input)
   const configReader = new ConfigReader()
   const discovery1 = await configReader.readDiscovery(
@@ -185,7 +185,7 @@ async function analyseOrbitChain(config: Config): Promise<void> {
   const rollupProxy1 = discovery1.contracts.find(
     (obj) => obj.name === 'RollupProxy',
   )
-  //console.log('Rollup:', rollupProxy1)
+  // console.log('Rollup:', rollupProxy1)
 
   const rollupProxy2 = discovery2.contracts.find(
     (obj) => obj.name === 'RollupProxy',
@@ -200,71 +200,71 @@ async function analyseOrbitChain(config: Config): Promise<void> {
     },
     {
       name: 'chainId',
-      chain1: rollupProxy1.values.chainId,
-      chain2: rollupProxy2.values.chainId,
+      chain1: rollupProxy1?.values?.chainId,
+      chain2: rollupProxy2?.values?.chainId,
       description: 'Chain ID of the rollup chain',
     },
     {
       name: 'minimumAssertionPeriod',
-      chain1: rollupProxy1.values.minimumAssertionPeriod,
-      chain2: rollupProxy2.values.minimumAssertionPeriod,
+      chain1: rollupProxy1?.values?.minimumAssertionPeriod,
+      chain2: rollupProxy2?.values?.minimumAssertionPeriod,
     },
     {
       name: 'setValidatorCount',
-      chain1: rollupProxy1.values.setValidatorCount,
-      chain2: rollupProxy2.values.setValidatorCount,
+      chain1: rollupProxy1?.values?.setValidatorCount,
+      chain2: rollupProxy2?.values?.setValidatorCount,
       description: 'How many Validators are on the whitelist',
     },
     {
       name: 'stakerCount',
-      chain1: rollupProxy1.values.stakerCount,
-      chain2: rollupProxy2.values.stakerCount,
+      chain1: rollupProxy1?.values?.stakerCount,
+      chain2: rollupProxy2?.values?.stakerCount,
       description: 'How many Validators are staked and can propose state roots',
     },
     {
       name: 'baseStake',
-      chain1: rollupProxy1.values.baseStake,
-      chain2: rollupProxy2.values.baseStake,
+      chain1: rollupProxy1?.values?.baseStake,
+      chain2: rollupProxy2?.values?.baseStake,
     },
     {
       name: 'currentRequiredStake',
-      chain1: rollupProxy1.values.currentRequiredStake,
-      chain2: rollupProxy2.values.currentRequiredStake,
+      chain1: rollupProxy1?.values?.currentRequiredStake,
+      chain2: rollupProxy2?.values?.currentRequiredStake,
     },
     {
       name: 'stakeToken',
-      chain1: rollupProxy1.values.stakeToken,
-      chain2: rollupProxy2.values.stakeToken,
+      chain1: rollupProxy1?.values?.stakeToken,
+      chain2: rollupProxy2?.values?.stakeToken,
       description: '0x is ETH',
     },
     {
       name: 'confirmPeriodBlocks',
-      chain1: rollupProxy1.values.confirmPeriodBlocks,
-      chain2: rollupProxy2.values.confirmPeriodBlocks,
-      description: descriptions[confirmPeriodBlocks],
+      chain1: rollupProxy1?.values?.confirmPeriodBlocks,
+      chain2: rollupProxy2?.values?.confirmPeriodBlocks,
+      description: descriptions['confirmPeriodBlocks'],
     },
     {
       name: 'extraChallengeTimeBlocks',
-      chain1: rollupProxy1.values.extraChallengeTimeBlocks,
-      chain2: rollupProxy2.values.extraChallengeTimeBlocks,
-      description: '????  WTF is this ????',
+      chain1: rollupProxy1?.values?.extraChallengeTimeBlocks,
+      chain2: rollupProxy2?.values?.extraChallengeTimeBlocks,
+      description: descriptions['extraChallengeTimeBlocks'],
     },
     {
       name: 'VALIDATOR_AFK_BLOCKS',
-      chain1: rollupProxy1.values.VALIDATOR_AFK_BLOCKS,
-      chain2: rollupProxy2.values.VALIDATOR_AFK_BLOCKS,
-      description: descriptions[VALIDATOR_AFK_BLOCKS],
+      chain1: rollupProxy1?.values?.VALIDATOR_AFK_BLOCKS,
+      chain2: rollupProxy2?.values?.VALIDATOR_AFK_BLOCKS,
+      description: descriptions['VALIDATOR_AFK_BLOCKS'],
     },
     {
       name: 'wasmModuleRoot',
-      chain1: rollupProxy1.values.wasmModuleRoot.slice(0, 10) + '...',
-      chain2: rollupProxy2.values.wasmModuleRoot.slice(0, 10) + '...',
-      description: 'Hash of the STF. If different, different STF is applied.',
+      chain1: (rollupProxy1?.values?.wasmModuleRoot as string)?.slice(0, 10) + '...',
+      chain2: (rollupProxy2?.values?.wasmModuleRoot as string)?.slice(0, 10) + '...',
+      description: descriptions['wasmModuleRoot'],
     },
     {
       name: 'isERC20Enabled',
-      chain1: rollupProxy1.values.isERC20Enabled,
-      chain2: rollupProxy2.values.isERC20Enabled,
+      chain1: rollupProxy1?.values?.isERC20Enabled,
+      chain2: rollupProxy2?.values?.isERC20Enabled,
     },
   ]
 
