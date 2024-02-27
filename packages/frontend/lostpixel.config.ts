034ef7bb015bd98ba87e504eaa78c492929efa7c
torztomasz <tomasz.torz@l2beat.com>
2024-02-27 14:45:23 +0100

    configure lost pixel

---
+++ packages/frontend/lostpixel.config.ts
@@ -0,0 +1,10 @@
+import { CustomProjectConfig } from 'lost-pixel'
+
+export const config: CustomProjectConfig = {
+  storybookShots: {
+    storybookUrl: './storybook-static',
+  },
+
+  lostPixelProjectId: 'clsyhac3l1hlvll0e72atmqc8',
+  apiKey: '41e5a803-102b-4f82-9759-b6525d88d8b9',
+}
