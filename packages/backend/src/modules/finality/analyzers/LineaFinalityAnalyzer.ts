dd62e0abbac8929ce75e4a4139877c540e172734
maciekzygmunt <maciekzygmunt@interia.pl>
2024-02-27 15:37:09 +0100

    update decoding logic

--- packages/backend/src/modules/finality/analyzers/LineaFinalityAnalyzer.ts
+++ packages/backend/src/modules/finality/analyzers/LineaFinalityAnalyzer.ts
@@ -1,18 +1,13 @@
-import { LivenessType, UnixTime } from '@l2beat/shared-pure'
+import { assert, LivenessType, UnixTime } from '@l2beat/shared-pure'
 import { utils } from 'ethers'
 
 import { BaseAnalyzer } from './types/BaseAnalyzer'
 
-type LineaDecoded = [
-  [string, number, string, unknown[], string, unknown[]][],
-  string,
-  number,
-  string,
-]
+type LineaDecoded = [[string, string, string, number, number, string, string]]
 
 export class LineaFinalityAnalyzer extends BaseAnalyzer {
   override getLivenessType(): LivenessType {
-    return 'STATE'
+    return 'DA'
   }
 
   async getFinality(transaction: {
@@ -23,14 +18,26 @@ export class LineaFinalityAnalyzer extends BaseAnalyzer {
     const l1Timestamp = transaction.timestamp
 
     const decodedInput = this.decodeInput(tx.data)
-    const timestamps = decodedInput[0].map((x) => x[1])
+
+    const firstBlockInData = Number(decodedInput[0][3])
+    const lastBlockInData = Number(decodedInput[0][4])
+
+    const timestampsPromises = Array.from(
+      { length: lastBlockInData - firstBlockInData + 1 },
+      async (_, i) => {
+        assert(this.l2Provider, 'Linea provider is not set')
+        const block = await this.l2Provider.getBlock(firstBlockInData + i)
+        return block.timestamp
+      },
+    )
+    const timestamps = await Promise.all(timestampsPromises)
 
     return timestamps.map((l2Timestamp) => l1Timestamp.toNumber() - l2Timestamp)
   }
 
   private decodeInput(data: string) {
     const fnSignature =
-      'finalizeBlocks((bytes32,uint32,bytes[],bytes32[],bytes,uint16[])[], bytes, uint256, bytes32)'
+      'submitData((bytes32,bytes32,bytes32,uint256,uint256,bytes32,bytes))'
     const iface = new utils.Interface([`function ${fnSignature}`])
     const decodedInput = iface.decodeFunctionData(
       fnSignature,
