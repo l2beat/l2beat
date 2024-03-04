05193408d7f01100ca5bd9b51d6f42b2e62fba11
maciekzygmunt <maciekzygmunt@interia.pl>
2024-03-04 15:23:55 +0100

    add table

---
+++ packages/backend/src/peripherals/database/migrations/095_add_tracked_txs_configs_table.ts
@@ -0,0 +1,29 @@
+/*
+                      ====== IMPORTANT NOTICE ======
+
+DO NOT EDIT OR RENAME THIS FILE
+
+This is a migration file. Once created the file should not be renamed or edited,
+because migrations are only run once on the production server.
+
+If you find that something was incorrectly set up in the `up` function you
+should create a new migration file that fixes the issue.
+
+*/
+
+import { Knex } from 'knex'
+
+export async function up(knex: Knex) {
+  await knex.schema.createTable('tracked_txs_configs', function (table) {
+    table.string('project_id').notNullable()
+    table.string('debug_info').notNullable()
+    table.dateTime('since_timestamp', { useTz: false })
+    table.dateTime('until_timestamp', { useTz: false })
+    table.dateTime('last_synced_timestamp', { useTz: false })
+    table.string('config_hash').notNullable()
+  })
+}
+
+export async function down(knex: Knex) {
+  await knex.schema.dropTable('tracked_txs_configs')
+}
