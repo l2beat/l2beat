d234d2561a67a2f4c6f7674445b130c3e4de4e40
Mateusz Radomski <radomski.main@protonmail.com>
2024-03-05 15:41:10 +0100

    Add liveness config

--- packages/config/src/layer2s/blast.ts
+++ packages/config/src/layer2s/blast.ts
@@ -1,6 +1,8 @@
-import { EthereumAddress, UnixTime } from '@l2beat/shared-pure'
+import { EthereumAddress, formatSeconds, UnixTime } from '@l2beat/shared-pure'
 
 import { ProjectDiscovery } from '../discovery/ProjectDiscovery'
+import { HARDCODED } from '../discovery/values/hardcoded'
+import { OPTIMISTIC_ROLLUP_STATE_UPDATES_WARNING } from './common'
 import { opStack } from './templates/opStack'
 import { Layer2 } from './types'
 
@@ -34,6 +36,16 @@ export const blast: Layer2 = opStack({
       explorers: ['https://blastscan.io'],
       repositories: [],
       socialMedia: ['https://twitter.com/Blast_L2'],
+    },
+    liveness: {
+      warnings: {
+        stateUpdates: OPTIMISTIC_ROLLUP_STATE_UPDATES_WARNING,
+      },
+      explanation: `Zora is an Optimistic rollup that posts transaction data to the L1. For a transaction to be considered final, it has to be posted within a tx batch on L1 that links to a previous finalized batch. If the previous batch is missing, transaction finalization can be delayed up to ${formatSeconds(
+        HARDCODED.OPTIMISM.SEQUENCING_WINDOW_SECONDS,
+      )} or until it gets published. The state root gets finalized ${formatSeconds(
+        FINALIZATION_PERIOD_SECONDS,
+      )} after it has been posted.`,
     },
     finality: {
       finalizationPeriod: FINALIZATION_PERIOD_SECONDS,
