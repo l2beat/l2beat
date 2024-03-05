22857cabc15cad10c7d22c4841960a20192bf994
antooni <antoni.pawlak@protonmail.com>
2024-03-05 13:01:14 +0100

    group by chain and project

--- packages/backend/src/modules/tvl2/api/Tvl2StatusRouter.ts
+++ packages/backend/src/modules/tvl2/api/Tvl2StatusRouter.ts
@@ -1,19 +1,16 @@
 import Router from '@koa/router'
-import {
-  ChainId,
-  CoingeckoId,
-  EthereumAddress,
-  UnixTime,
-} from '@l2beat/shared-pure'
+import { CoingeckoId, EthereumAddress, UnixTime } from '@l2beat/shared-pure'
 
 import { Config } from '../../../config'
+import { ChainConverter } from '../../../tools/ChainConverter'
 
 type Token = TotalSupplyToken | CirculatingSupplyToken | EscrowBalanceToken
 
 interface EscrowBalanceToken {
   type: 'ESCROW_BALANCE'
   address: EthereumAddress | 'native'
-  chainId: ChainId
+  chain: string
+  project: string
   escrow: EthereumAddress
   sinceTimestamp: UnixTime
 }
@@ -21,18 +18,22 @@ interface EscrowBalanceToken {
 interface TotalSupplyToken {
   type: 'TOTAL_SUPPLY'
   address: EthereumAddress
-  chainId: ChainId
+  chain: string
+  project: string
   sinceTimestamp: UnixTime
 }
 
 interface CirculatingSupplyToken {
   type: 'CIRCULATING_SUPPLY'
   coingeckoId: CoingeckoId
+  project: string
   sinceTimestamp: UnixTime
 }
 
 export function createTvl2StatusRouter(config: Config) {
   const router = new Router()
+
+  const chainConverter = new ChainConverter(config.chains)
 
   const totalSupplyTokens: TotalSupplyToken[] = config.tokens
     .filter((t) => t.formula === 'totalSupply')
@@ -40,8 +41,9 @@ export function createTvl2StatusRouter(config: Config) {
       type: 'TOTAL_SUPPLY',
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       address: t.address!,
-      chainId: t.chainId,
+      chain: chainConverter.toName(t.chainId),
       sinceTimestamp: t.sinceTimestamp,
+      project: chainConverter.toName(t.chainId),
     }))
 
   const circulatingSupplyTokens: CirculatingSupplyToken[] = config.tokens
@@ -49,18 +51,22 @@ export function createTvl2StatusRouter(config: Config) {
     .map((t) => ({
       type: 'CIRCULATING_SUPPLY',
       coingeckoId: t.coingeckoId,
+      project: chainConverter.toName(t.chainId),
       sinceTimestamp: t.sinceTimestamp,
     }))
 
-  const escrows = config.projects.map((p) => p.escrows).flat()
+  const escrows = config.projects
+    .map((p) => p.escrows.map((e) => ({ ...e, project: p.projectId })))
+    .flat()
 
   const escrowBalanceTokens: EscrowBalanceToken[][] = escrows.map((e) =>
     e.tokens.map((t) => ({
       type: 'ESCROW_BALANCE',
       address: t.address ?? 'native',
-      chainId: t.chainId,
+      chain: 'ethereum',
       escrow: e.address,
       sinceTimestamp: e.sinceTimestamp,
+      project: e.project.toString(),
     })),
   )
 
@@ -70,8 +76,36 @@ export function createTvl2StatusRouter(config: Config) {
     ...escrowBalanceTokens.flat(),
   ]
 
+  const tokensByChain: Record<string, Token[]> = {}
+
+  for (const token of tokens) {
+    if (token.type === 'CIRCULATING_SUPPLY') {
+      if (tokensByChain['coingecko']) {
+        tokensByChain['coingecko'].push(token)
+      } else {
+        tokensByChain['coingecko'] = [token]
+      }
+    } else {
+      if (tokensByChain[token.chain]) {
+        tokensByChain[token.chain].push(token)
+      } else {
+        tokensByChain[token.chain] = [token]
+      }
+    }
+  }
+
+  const tokensByProject: Record<string, Token[]> = {}
+
+  for (const token of tokens) {
+    if (tokensByProject[token.project]) {
+      tokensByProject[token.project].push(token)
+    } else {
+      tokensByProject[token.project] = [token]
+    }
+  }
+
   router.get('/status/tokens', (ctx) => {
-    ctx.body = tokens
+    ctx.body = tokensByProject
   })
 
   return router
