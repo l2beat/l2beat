b0c42602239073a6821d73af2ad55522306d8622
Mateusz Radomski <33978857+mateuszradomski@users.noreply.github.com>
2024-03-07 15:23:09 +0100

    Add a tvlWarning for Blast (#2971)

--- packages/config/src/layer2s/blast.ts
+++ packages/config/src/layer2s/blast.ts
@@ -51,6 +51,10 @@ export const blast: Layer2 = opStack({
     finality: {
       finalizationPeriod: FINALIZATION_PERIOD_SECONDS,
     },
+    tvlWarning: {
+      content: 'The TVL does account for rehypothecated tokens.',
+      sentiment: 'bad',
+    },
   },
   upgradeability,
   l1StandardBridgeEscrow: EthereumAddress(
