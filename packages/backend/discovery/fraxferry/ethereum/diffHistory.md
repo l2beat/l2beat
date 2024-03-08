1c01df372f7a7386ffb9de8570d13ecd0ae9be9a
vincfurc <10850139+vincfurc@users.noreply.github.com>
2024-03-08 16:54:03 +0000

    Add frax ferry bridge config

---
+++ packages/backend/discovery/fraxferry/ethereum/diffHistory.md
@@ -0,0 +1,144 @@
+Generated with discovered.json: 0x7b440833db9731683aedaa65b24313cf4682991e
+
+# Diff at Fri, 08 Mar 2024 16:28:15 GMT:
+
+- author: vincfurc (<10850139+vincfurc@users.noreply.github.com>)
+- current block number: 19391636
+
+## Description
+
+Provide description of changes. This section will be preserved.
+
+## Initial discovery
+
+```diff
++   Status: CREATED
+    contract DepositContract (0x00000000219ab540356cBB839Cbe05303d7705Fa)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract FeeToSetter (0x18e433c7Bf8A2E1d0197CE5d8f9AFAda1A771360)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract Timelock (0x1a9C8182C09F50C8318d769245beA52c32BE35BC)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract FRAXOracleWrapper (0x2A6ddD9401B14d0443d0738B8a78fd5B99829A80)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract sfraxFerryBridge (0x2b4864c2F2A2C275C6C66B90a2ae6BE9fA9cbE47)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract FRAXShares (0x3432B6A60D23Ca0dFCa7761B7ab56459D9C964D0)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract UniswapPairOracle_USDT_WETH (0x3B11DA52030420c663d263Ad4415a8A02E5f8cf8)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract fxsFerryBridge (0x4A6d155df9Ec9A1BB3639e6B7B99E46Fb68D42f6)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract sfrxFerryBridge (0x5c5f05cF8528FFe925A2264743bFfEdbAB2b0FE3)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract UniswapV2Factory (0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract fraxFerryBridge (0x5e1D94021484642863Ea8E7Cb4F0188e56B18FEE)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract frxETH (0x5E8422345238F34275888049021821E8E08CAa1f)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract frxETHMultisig (0x8306300ffd616049FD7e4b0354a64Da835c1A81C)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract GnosisSafe (0x831822660572bd54ebaa065C2acef662a6277D40)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract Timelock (0x8412ebf45bAC1B340BbE8F318b928C466c4E39CA)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract FRAXStablecoin (0x853d955aCEf822Db058eb8505911ED77F175b99e)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract StakedFrax (0xA663B02CF0a4b149d2aD41910CB81e23e1c41c32)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract sfrxETH (0xac3E018457B222d93114458476f3E3416Abbe38F)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract TimelockMultisig (0xB1748C79709f4Ba2Dd82834B8c82D4a505003f27)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract frxETHMinter (0xbAFA44EFE7901E04E39Dad13167D089C559c1138)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract FeeTo (0xDAF819c2437a82f9e01f6586207ebF961a7f0970)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract UniswapV2Pair (0xecBa967D84fCF0405F6b32Bc45F4d36BfDBB2E81)
+    +++ description: None
+```
---
+++ packages/config/src/bridges/fraxferry.ts
@@ -0,0 +1,124 @@
+import { EthereumAddress, ProjectId, UnixTime } from '@l2beat/shared-pure'
+
+import { CONTRACTS } from '../common'
+import { Bridge } from './types'
+
+// const discovery = new ProjectDiscovery('fraxferry')
+
+export const fraxferry: Bridge = {
+  type: 'bridge',
+  id: ProjectId('fraxferry'),
+  display: {
+    name: 'Frax Ferry',
+    slug: 'fraxferry',
+    links: {
+      websites: ['https://frax.com/'],
+      explorers: [],
+      apps: ['https://mainnet.frax.com/tools/bridge/'],
+      repositories: ['https://github.com/FraxFinance/frax-solidity'],
+      socialMedia: ['https://twitter.com/fraxfinance'],
+    },
+    description:
+      'The Frax Ferry is a permissioned bridge that can be used to transfer tokens between chains.',
+    category: 'Token Bridge',
+  },
+  config: {
+    associatedTokens: ['FRAX', 'FXS', 'frxETH', 'sfrxETH'],
+    escrows: [
+      {
+        address: EthereumAddress('0x5e1D94021484642863Ea8E7Cb4F0188e56B18FEE'),
+        sinceTimestamp: new UnixTime(0),
+        tokens: ['FRAX'],
+      },
+      {
+        address: EthereumAddress('0x4A6d155df9Ec9A1BB3639e6B7B99E46Fb68D42f6'),
+        sinceTimestamp: new UnixTime(0),
+        tokens: ['FXS'],
+      },
+      {
+        address: EthereumAddress('0x5c5f05cF8528FFe925A2264743bFfEdbAB2b0FE3'),
+        sinceTimestamp: new UnixTime(0),
+        tokens: ['sfrxETH'],
+      },
+      //   {
+      //     address: EthereumAddress('0x9A576A3d39c589A861B46864C253288bcA428a6c'),
+      //     sinceTimestamp: new UnixTime(0),
+      //     tokens: ['FPI'],
+      //   },
+      //   {
+      //     address: EthereumAddress('0x958815f476cD07354c0BC034EE5077B20fD93003'),
+      //     sinceTimestamp: new UnixTime(0),
+      //     tokens: ['FPIS'],
+      //   },
+      //   {
+      //     address: EthereumAddress('0x2b4864c2F2A2C275C6C66B90a2ae6BE9fA9cbE47'),
+      //     sinceTimestamp: new UnixTime(0),
+      //     tokens: ['sFRAX'],
+      //   },
+    ],
+  },
+  riskView: {
+    validatedBy: {
+      value: 'Third Party',
+      description: 'Transfers out of the bridge are validated by...',
+      sentiment: 'bad',
+    },
+    sourceUpgradeability: {
+      value: 'Yes',
+      description: `Admin Multisig can move all funds out of the bridge via admin functions with no warning.`,
+      sentiment: 'bad',
+    },
+  },
+  technology: {
+    canonical: true,
+    destination: [
+      'Arbitrum',
+      'Optimism',
+      'Fraxtal',
+      'Boba',
+      'Polygon zkEVM',
+      'zkSync',
+    ],
+    principleOfOperation: {
+      name: 'Principle of operation',
+      description: '...',
+      references: [],
+      risks: [],
+    },
+    validation: {
+      name: 'Both inbound and outbound transfers are verified by',
+      description: '...',
+      references: [],
+      risks: [
+        {
+          category: 'Funds can be stolen if',
+          text: 'incorrect hash is submitted and nobody challenges it.',
+        },
+        {
+          category: 'Funds can be stolen if',
+          text: 'bridge administrator removes funds from the bridge escrow.',
+          isCritical: true,
+        },
+      ],
+      isIncomplete: true,
+    },
+    destinationToken: {
+      name: 'Destination tokens are not verified',
+      description: '...',
+      references: [],
+      risks: [
+        {
+          category: 'Funds can be stolen if',
+          text: 'destination token contract is maliciously upgraded.',
+          isCritical: true,
+        },
+      ],
+      isIncomplete: true,
+    },
+  },
+  contracts: {
+    addresses: [],
+    risks: [CONTRACTS.UPGRADE_NO_DELAY_RISK],
+  },
+  permissions: [],
+}
