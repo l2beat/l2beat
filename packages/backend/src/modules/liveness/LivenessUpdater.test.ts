fab4d0e3638fd768abbe26c0a9dd4495da1b0311
maciekzygmunt <maciekzygmunt@interia.pl>
2024-03-11 13:07:19 +0200

    add tests to updater
    Co-authored-by: Tomasz Tórz <torztomasz@users.noreply.github.com>

---
+++ packages/backend/src/modules/liveness/LivenessUpdater.test.ts
@@ -0,0 +1,181 @@
+import { Logger } from '@l2beat/backend-tools'
+import { EthereumAddress, ProjectId, UnixTime } from '@l2beat/shared-pure'
+import { expect, mockObject } from 'earl'
+import { Knex } from 'knex'
+
+import { TrackedTxResult } from '../tracked-txs/types/model'
+import { TrackedTxId } from '../tracked-txs/types/TrackedTxId'
+import { TrackedTxConfigEntry } from '../tracked-txs/types/TrackedTxsConfig'
+import { LivenessUpdater } from './LivenessUpdater'
+import {
+  LivenessRecord,
+  LivenessRepository,
+} from './repositories/LivenessRepository'
+
+const MIN_TIMESTAMP = UnixTime.fromDate(new Date('2023-05-01T00:00:00Z'))
+const TRX = mockObject<Knex.Transaction>({})
+
+describe(LivenessUpdater.name, () => {
+  describe(LivenessUpdater.prototype.update.name, () => {
+    it('skips update if no transactions', async () => {
+      const logger = mockObject<Logger>({
+        debug: () => undefined,
+      })
+      const livenessRepo = getMockLivenessRepository()
+      const updater = new LivenessUpdater(livenessRepo, logger)
+
+      const transactions: TrackedTxResult[] = []
+
+      await updater.update(transactions, TRX)
+
+      expect(logger.debug).toHaveBeenCalled()
+      expect(livenessRepo.addMany).not.toHaveBeenCalled()
+    })
+
+    it('calls liveness repo with correct parameters', async () => {
+      const livenessRepo = getMockLivenessRepository()
+      const updater = new LivenessUpdater(livenessRepo, Logger.SILENT)
+
+      const transactions: TrackedTxResult[] = getMockTrackedTxResults()
+      await updater.update(transactions, TRX)
+
+      expect(livenessRepo.addMany).toHaveBeenNthCalledWith(
+        1,
+        [
+          {
+            txHash: transactions[0].hash,
+            blockNumber: transactions[0].blockNumber,
+            timestamp: transactions[0].blockTimestamp,
+            trackedTxId: transactions[0].use.id,
+          },
+          {
+            txHash: transactions[1].hash,
+            blockNumber: transactions[1].blockNumber,
+            timestamp: transactions[1].blockTimestamp,
+            trackedTxId: transactions[1].use.id,
+          },
+        ],
+        TRX,
+      )
+    })
+  })
+
+  describe(LivenessUpdater.prototype.deleteAfter.name, () => {
+    it('calls liveness repo with correct parameters', async () => {
+      const livenessRepo = getMockLivenessRepository()
+      const updater = new LivenessUpdater(livenessRepo, Logger.SILENT)
+
+      const id = TrackedTxId.random()
+      await updater.deleteAfter(id, MIN_TIMESTAMP, TRX)
+
+      expect(livenessRepo.deleteAfter).toHaveBeenNthCalledWith(
+        1,
+        id,
+        MIN_TIMESTAMP,
+        TRX,
+      )
+    })
+  })
+
+  describe(LivenessUpdater.prototype.transformTransactions.name, () => {
+    it('it correctly transforms records to livenessRow', () => {
+      const updater = new LivenessUpdater(
+        getMockLivenessRepository(),
+        Logger.SILENT,
+      )
+
+      const transactions: TrackedTxResult[] = getMockTrackedTxResults()
+
+      const expected: LivenessRecord[] = [
+        {
+          txHash: transactions[0].hash,
+          blockNumber: transactions[0].blockNumber,
+          timestamp: transactions[0].blockTimestamp,
+          trackedTxId: transactions[0].use.id,
+        },
+        {
+          txHash: transactions[1].hash,
+          blockNumber: transactions[1].blockNumber,
+          timestamp: transactions[1].blockTimestamp,
+          trackedTxId: transactions[1].use.id,
+        },
+      ]
+
+      expect(updater.transformTransactions(transactions)).toEqual(expected)
+    })
+  })
+})
+
+function getMockLivenessRepository() {
+  return mockObject<LivenessRepository>({
+    deleteAfter: async () => 0,
+    runInTransaction: async (fn) => fn(TRX),
+    addMany: async () => 0,
+  })
+}
+
+function getMockTrackedTxResults(): TrackedTxResult[] {
+  return [
+    {
+      type: 'functionCall',
+      projectId: ProjectId('test'),
+      blockNumber: 1,
+      blockTimestamp: UnixTime.now(),
+      toAddress: EthereumAddress.random(),
+      input: '',
+      hash: '',
+      use: {
+        type: 'liveness',
+        subType: 'batchSubmissions',
+        id: getMockRuntimeConfigurations()[0].uses[0].id,
+      },
+    },
+    {
+      type: 'transfer',
+      use: {
+        id: getMockRuntimeConfigurations()[1].uses[0].id,
+        type: 'liveness',
+        subType: 'stateUpdates',
+      },
+      blockNumber: 1,
+      blockTimestamp: UnixTime.now(),
+      hash: '',
+      fromAddress: EthereumAddress.random(),
+      toAddress: EthereumAddress.random(),
+      projectId: ProjectId('test2'),
+    },
+  ]
+}
+
+function getMockRuntimeConfigurations(): TrackedTxConfigEntry[] {
+  return [
+    {
+      formula: 'functionCall',
+      projectId: ProjectId('test'),
+      address: EthereumAddress.random(),
+      selector: '0x',
+      sinceTimestamp: MIN_TIMESTAMP,
+      uses: [
+        {
+          type: 'liveness',
+          subType: 'batchSubmissions',
+          id: TrackedTxId.random(),
+        },
+      ],
+    },
+    {
+      formula: 'functionCall',
+      projectId: ProjectId('test2'),
+      address: EthereumAddress.random(),
+      selector: '0x',
+      sinceTimestamp: MIN_TIMESTAMP,
+      uses: [
+        {
+          type: 'liveness',
+          subType: 'stateUpdates',
+          id: TrackedTxId.random(),
+        },
+      ],
+    },
+  ]
+}
--- packages/backend/src/modules/liveness/LivenessUpdater.ts
+++ packages/backend/src/modules/liveness/LivenessUpdater.ts
@@ -2,7 +2,6 @@ import { Logger } from '@l2beat/backend-tools'
 import { UnixTime } from '@l2beat/shared-pure'
 import { Knex } from 'knex'
 
-import { TrackedTxsConfigsRepository } from '../tracked-txs/repositories/TrackedTxsConfigsRepository'
 import { TrackedTxResult } from '../tracked-txs/types/model'
 import { TrackedTxId } from '../tracked-txs/types/TrackedTxId'
 import { TxUpdaterInterface } from '../tracked-txs/types/TxUpdaterInterface'
@@ -14,7 +13,6 @@ import {
 export class LivenessUpdater implements TxUpdaterInterface {
   constructor(
     private readonly livenessRepository: LivenessRepository,
-    private readonly configurationRepository: TrackedTxsConfigsRepository,
     private readonly logger: Logger,
   ) {}
 
