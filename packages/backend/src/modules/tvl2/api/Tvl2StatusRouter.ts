401f916a94a4b7bb27e60b2fb334277fa3c0e2aa
antooni <antoni.pawlak@protonmail.com>
2024-03-12 10:19:16 +0100

    group by type

--- packages/backend/src/modules/tvl2/api/Tvl2StatusRouter.ts
+++ packages/backend/src/modules/tvl2/api/Tvl2StatusRouter.ts
@@ -13,32 +13,32 @@ export function createTvl2StatusRouter(
   const router = new Router()
 
   router.get('/status/tokens', (ctx) => {
-    const byType = groupBy(amounts, 'type')
-    const byChain = groupBy(amounts, 'chain')
-    const byProject = groupBy(amounts, 'project')
+    const pricesByType = groupBy(prices, 'type')
+
+    const amountsByType = groupBy(amounts, 'type')
+    const amountsByChain = groupBy(amounts, 'chain')
+    const amountsByProject = groupBy(amounts, 'project')
 
     ctx.body = {
       prices: {
         entries: prices.length,
         datapoints: getDatapoints(prices, clock),
+        byType: Object.entries(pricesByType).map(([type, entries]) => ({
+          type,
+          entries: entries.length,
+          datapoints: getDatapoints(entries, clock),
+        })),
       },
       amounts: {
         entries: amounts.length,
         datapoints: getDatapoints(amounts, clock),
-        chains: Object.keys(byChain).length,
-        projects: Object.keys(byProject).length,
-        totalSupplies: {
-          entries: byType['totalSupply'].length,
-          datapoints: getDatapoints(byType['totalSupply'], clock),
-        },
-        circulatingSupplies: {
-          entries: byType['circulatingSupply'].length,
-          datapoints: getDatapoints(byType['circulatingSupply'], clock),
-        },
-        escrows: {
-          entries: byType['escrow'].length,
-          datapoints: getDatapoints(byType['escrow'], clock),
-        },
+        chains: Object.keys(amountsByChain).length,
+        projects: Object.keys(amountsByProject).length,
+        byType: Object.entries(amountsByType).map(([type, entries]) => ({
+          type,
+          entries: entries.length,
+          datapoints: getDatapoints(entries, clock),
+        })),
       },
     }
   })
