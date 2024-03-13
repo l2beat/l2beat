57aea3e6a7bf20167a1a8fcb287f7bd3e7a48f61
sekuba <sekuba@users.noreply.github.com>
2024-03-13 14:03:19 +0100

    format: immutablezkevm.ts

--- packages/config/src/bridges/immutablezkevm.ts
+++ packages/config/src/bridges/immutablezkevm.ts
@@ -44,7 +44,7 @@ export const immutablezkevm: Bridge = {
     principleOfOperation: {
       name: 'Principle of Operation',
       description:
-        'Immutable zkEVM bridge makes use of Axelar network to transfer assets between Ethereum and Immutable zkEVM. A deposit starts by a user depositing tokens on the Bridge contract and then the tokens are minted on the destination chain.\n\nWithdrawals to Ethereum can be delayed by a predefined time with a flow rate mechanism that controls outflows of the bridge escrow. The ProxyAdmin or an address with the rate_control role can define so-called buckets for each token: Each bucket has a capacity and a refill rate. All withdrawals that exceed the tokens bucket capacity trigger the withdrawal queue, which delays subsequent withdrawals of *any* of the bridges\' assets for a time defined in withdrawalDelay.',
+        "Immutable zkEVM bridge makes use of Axelar network to transfer assets between Ethereum and Immutable zkEVM. A deposit starts by a user depositing tokens on the Bridge contract and then the tokens are minted on the destination chain.\n\nWithdrawals to Ethereum can be delayed by a predefined time with a flow rate mechanism that controls outflows of the bridge escrow. The ProxyAdmin or an address with the rate_control role can define so-called buckets for each token: Each bucket has a capacity and a refill rate. All withdrawals that exceed the tokens bucket capacity trigger the withdrawal queue, which delays subsequent withdrawals of *any* of the bridges' assets for a time defined in withdrawalDelay.",
       references: [],
       risks: [],
     },
