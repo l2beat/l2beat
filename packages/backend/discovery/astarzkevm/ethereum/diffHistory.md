379b0833e33d48740a4981af4e54801004be005a
vincfurc <10850139+vincfurc@users.noreply.github.com>
2024-03-18 18:06:53 +0000

    astar zkevm discovery

---
+++ packages/backend/discovery/astarzkevm/ethereum/diffHistory.md
@@ -0,0 +1,84 @@
+Generated with discovered.json: 0x27fae79059b7c7d6161b057930536cc501b0ece5
+
+# Diff at Mon, 18 Mar 2024 18:01:18 GMT:
+
+- author: vincfurc (<10850139+vincfurc@users.noreply.github.com>)
+- current block number: 19463321
+
+## Description
+
+Provide description of changes. This section will be preserved.
+
+## Initial discovery
+
+```diff
++   Status: CREATED
+    contract Permit2 (0x000000000022D473030F116dDEE9F6B43aC78BA3)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract ProxyAdmin (0x0F99738B2Fc14D77308337f3e2596b63aE7BCC4A)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract ProxyAdmin (0x1963D7b78e75A5eDfF9e5376E7A07A935Fb3d50d)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract AstarValidiumEtrog (0x1E163594e13030244DCAf4cDfC2cd0ba3206DA80)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract Bridge (0x2a3DD3EB832aF982ec71669E178424b10Dca2EDe)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract PolygonEcosystemToken (0x455e53CBB86018Ac2B8092FdCd39d8444aFFC3F6)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract PolygonRollupManager (0x5132A183E9F3CB7C848b0AAC5Ae0c4f0491B7aB2)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract PolygonZkEVMGlobalExitRootV2 (0x580bda1e7A0CFAe92Fa7F6c20A3794F169CE3CFb)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract GnosisSafe (0x6c4876Ecb5de33f76700f44d547C593065806dAC)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract PolygonDataCommittee (0x9CCD205052c732Ac1Df2cf7bf8aACC0E371eE0B0)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract Timelock (0xEf1462451C30Ea7aD8555386226059Fe837CA4EF)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract GnosisSafe (0xf98ee8c46baEa2B11e4f0450AD9D01861265F76E)
+    +++ description: None
+```
--- packages/config/src/layer2s/astarzkevm.ts
+++ packages/config/src/layer2s/astarzkevm.ts
@@ -1,8 +1,80 @@
-import { underReviewL2 } from './templates/underReview'
+import {
+  EthereumAddress,
+  formatSeconds,
+  ProjectId,
+  UnixTime,
+} from '@l2beat/shared-pure'
+
+import {
+  CONTRACTS,
+  EXITS,
+  FORCE_TRANSACTIONS,
+  FRONTRUNNING_RISK,
+  makeBridgeCompatible,
+  RISK_VIEW,
+  SEQUENCER_NO_MECHANISM,
+  STATE_CORRECTNESS,
+  TECHNOLOGY_DATA_AVAILABILITY,
+} from '../common'
+import { ProjectDiscovery } from '../discovery/ProjectDiscovery'
+import { getStage } from './common/stages/getStage'
 import { Layer2 } from './types'
 
-export const astarzkevm: Layer2 = underReviewL2({
-  id: 'astarzkevm',
+const discovery = new ProjectDiscovery('astarzkevm')
+
+const upgradeDelay = discovery.getContractValue<number>(
+  'Timelock',
+  'getMinDelay',
+)
+const upgradeDelayString = formatSeconds(upgradeDelay)
+
+const trustedAggregatorTimeout = discovery.getContractValue<number>(
+  'PolygonRollupManager',
+  'trustedAggregatorTimeout',
+)
+const trustedAggregatorTimeoutString = formatSeconds(trustedAggregatorTimeout)
+
+const pendingStateTimeout = discovery.getContractValue<number>(
+  'PolygonRollupManager',
+  'pendingStateTimeout',
+)
+const pendingStateTimeoutString = formatSeconds(pendingStateTimeout)
+const _HALT_AGGREGATION_TIMEOUT = formatSeconds(
+  discovery.getContractValue<number>(
+    'PolygonRollupManager',
+    '_HALT_AGGREGATION_TIMEOUT',
+  ),
+)
+
+const isForcedBatchDisallowed =
+  discovery.getContractValue<string>(
+    'AstarValidiumEtrog',
+    'forceBatchAddress',
+  ) !== '0x0000000000000000000000000000000000000000'
+
+const forceBatchTimeout = discovery.getContractValue<number>(
+  'AstarValidiumEtrog',
+  'forceBatchTimeout',
+)
+
+const exitWindowRisk = {
+  ...RISK_VIEW.EXIT_WINDOW(
+    upgradeDelay,
+    trustedAggregatorTimeout + pendingStateTimeout + forceBatchTimeout,
+    0,
+  ),
+  description: `Even though there is a ${upgradeDelayString} Timelock for upgrades, forced transactions are disabled. Even if they were to be enabled, user withdrawals can be censored up to ${formatSeconds(
+    trustedAggregatorTimeout + pendingStateTimeout + forceBatchTimeout,
+  )}.`,
+  warning: {
+    value: 'The Security Council can remove the delay on upgrades.',
+    sentiment: 'bad',
+  },
+} as const
+
+export const astarzkevm: Layer2 = {
+  type: 'layer2',
+  id: ProjectId('astarzkevm'),
   display: {
     name: 'Astar zkEVM',
     slug: 'astarzkevm',
@@ -27,5 +99,169 @@ export const astarzkevm: Layer2 = underReviewL2({
       ],
     },
   },
-  escrows: [],
-})
+  config: {
+    escrows: [
+      discovery.getEscrowDetails({
+        address: EthereumAddress('0x2a3DD3EB832aF982ec71669E178424b10Dca2EDe'),
+        sinceTimestamp: new UnixTime(1679653127),
+        tokens: '*',
+      }),
+    ],
+  },
+  riskView: makeBridgeCompatible({
+    stateValidation: {
+      ...RISK_VIEW.STATE_ZKP_SN,
+      sources: [
+        {
+          contract: 'PolygonRollupManager',
+          references: [
+            'https://etherscan.io/address/0x5132A183E9F3CB7C848b0AAC5Ae0c4f0491B7aB2',
+          ],
+        },
+      ],
+    },
+    dataAvailability: {
+      ...RISK_VIEW.DATA_ON_CHAIN,
+      description:
+        RISK_VIEW.DATA_ON_CHAIN.description +
+        ' Unlike most ZK rollups transactions are posted instead of state diffs.',
+      sources: [
+        {
+          contract: 'AstarValidiumEtrog',
+          references: [
+            'https://etherscan.io/address/0x519E42c24163192Dca44CD3fBDCEBF6be9130987',
+          ],
+        },
+      ],
+    },
+    exitWindow: exitWindowRisk,
+    // this will change once the isForcedBatchDisallowed is set to false inside Polygon ZkEvm contract (if they either lower timeouts or increase the timelock delay)
+    sequencerFailure: {
+      ...SEQUENCER_NO_MECHANISM(isForcedBatchDisallowed),
+      sources: [
+        {
+          contract: 'AstarValidiumEtrog',
+          references: [
+            'https://etherscan.io/address/0x519E42c24163192Dca44CD3fBDCEBF6be9130987',
+          ],
+        },
+      ],
+    },
+    proposerFailure: {
+      ...RISK_VIEW.PROPOSER_SELF_PROPOSE_ZK,
+      description:
+        RISK_VIEW.PROPOSER_SELF_PROPOSE_ZK.description +
+        ` There is a ${trustedAggregatorTimeoutString} delay for proving and a ${pendingStateTimeoutString} delay for finalizing state proven in this way. These delays can only be lowered except during the emergency state.`,
+      sources: [
+        {
+          contract: 'PolygonRollupManager',
+          references: [
+            'https://etherscan.io/address/0x5132A183E9F3CB7C848b0AAC5Ae0c4f0491B7aB2',
+            'https://etherscan.io/address/0x5132A183E9F3CB7C848b0AAC5Ae0c4f0491B7aB2',
+          ],
+        },
+      ],
+    },
+    destinationToken: RISK_VIEW.NATIVE_AND_CANONICAL(),
+    validatedBy: RISK_VIEW.VALIDATED_BY_ETHEREUM,
+  }),
+  stage: getStage(
+    {
+      stage0: {
+        callsItselfRollup: true,
+        stateRootsPostedToL1: true,
+        dataAvailabilityOnL1: true,
+        rollupNodeSourceAvailable: true,
+      },
+      stage1: {
+        stateVerificationOnL1: true,
+        fraudProofSystemAtLeast5Outsiders: null,
+        usersHave7DaysToExit: false,
+        usersCanExitWithoutCooperation: false,
+        securityCouncilProperlySetUp: [
+          false,
+          'Security Council members are not publicly known.',
+        ],
+      },
+      stage2: {
+        proofSystemOverriddenOnlyInCaseOfABug: false,
+        fraudProofSystemIsPermissionless: null,
+        delayWith30DExitWindow: false,
+      },
+    },
+    {
+      rollupNodeLink: 'https://github.com/0xPolygonHermez/zkevm-node',
+    },
+  ),
+  technology: {
+    stateCorrectness: {
+      ...STATE_CORRECTNESS.VALIDITY_PROOFS,
+      references: [
+        {
+          text: 'PolygonRollupManager.sol - Etherscan source code, _verifyAndRewardBatches function',
+          href: 'https://etherscan.io/address/0x5132A183E9F3CB7C848b0AAC5Ae0c4f0491B7aB2',
+        },
+      ],
+    },
+    dataAvailability: {
+      ...TECHNOLOGY_DATA_AVAILABILITY.ON_CHAIN_CANONICAL,
+      references: [
+        {
+          text: 'AstarValidiumEtrog.sol - Etherscan source code, sequenceBatches function',
+          href: 'https://etherscan.io/address/0x519E42c24163192Dca44CD3fBDCEBF6be9130987',
+        },
+      ],
+    },
+    operator: {
+      name: 'The system has a centralized sequencer',
+      description:
+        'Only a trusted sequencer is allowed to submit transaction batches. A mechanism for users to submit their own batches is currently disabled.',
+      risks: [
+        FRONTRUNNING_RISK,
+        {
+          category: 'Funds can be frozen if',
+          text: 'the sequencer refuses to include an exit transaction.',
+          isCritical: true,
+        },
+      ],
+      references: [
+        {
+          text: 'AstarValidiumEtrog.sol - Etherscan source code, onlyTrustedSequencer modifier',
+          href: 'https://etherscan.io/address/0x519E42c24163192Dca44CD3fBDCEBF6be9130987',
+        },
+      ],
+    },
+    forceTransactions: {
+      ...FORCE_TRANSACTIONS.SEQUENCER_NO_MECHANISM,
+      description:
+        'The mechanism for allowing users to submit their own transactions is currently disabled.',
+      references: [
+        {
+          text: 'AstarValidiumEtrog.sol - Etherscan source code, forceBatchAddress address',
+          href: 'https://etherscan.io/address/0x519E42c24163192Dca44CD3fBDCEBF6be9130987',
+        },
+      ],
+    },
+    exitMechanisms: [
+      {
+        ...EXITS.REGULAR('zk', 'merkle proof'),
+        references: [
+          {
+            text: 'PolygonZkEvmBridgeV2.sol - Etherscan source code, claimAsset function',
+            href: 'https://etherscan.io/address/0x0feb850b183c57534b56b7d56520133c8f9bdb65',
+          },
+        ],
+      },
+    ],
+  },
+  contracts: {
+    addresses: [
+      discovery.getContractDetails('Bridge', {
+        description:
+          'The escrow contract for user funds. It is mirrored on the L2 side and can be used to transfer both ERC20 assets and arbitrary messages. To transfer funds a user initiated transaction on both sides is required.',
+      }),
+    ],
+    references: [],
+    risks: [CONTRACTS.UPGRADE_WITH_DELAY_RISK(upgradeDelayString)],
+  },
+}
