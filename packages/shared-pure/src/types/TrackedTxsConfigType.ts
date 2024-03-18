7d1a826eff3d1f6a92a472eaf625d8c3530d1cb5
maciekzygmunt <maciekzygmunt@interia.pl>
2024-03-18 15:28:33 +0100

    add l2costs subtype

--- packages/shared-pure/src/types/TrackedTxsConfigType.ts
+++ packages/shared-pure/src/types/TrackedTxsConfigType.ts
@@ -1,4 +1,7 @@
 import { z } from 'zod'
 
 export type TrackedTxsConfigType = z.infer<typeof TrackedTxsConfigType>
-export const TrackedTxsConfigType = z.literal('liveness')
+export const TrackedTxsConfigType = z.union([
+  z.literal('liveness'),
+  z.literal('l2costs'),
+])
