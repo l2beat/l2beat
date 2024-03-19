c224488e8127303b574a04c0edfbbf9d720f4cba
Mateusz Radomski <radomski.main@protonmail.com>
2024-03-19 14:15:48 +0100

    Return some data from the mock

--- packages/frontend/src/build/api/fetchImplementationChangeReport.ts
+++ packages/frontend/src/build/api/fetchImplementationChangeReport.ts
@@ -1,4 +1,7 @@
-import { ImplementationChangeReportApiResponse } from '@l2beat/shared-pure'
+import {
+  EthereumAddress,
+  ImplementationChangeReportApiResponse,
+} from '@l2beat/shared-pure'
 
 import { JsonHttpClient } from '../caching/JsonHttpClient'
 import { Config } from '../config'
@@ -18,6 +21,17 @@ export async function fetchImplementationChangeReport(
 
 function getMockImplementationChangeApiResponse(): ImplementationChangeReportApiResponse {
   return {
-    projects: {},
+    projects: {
+      arbitrum: {
+        ethereum: [
+          {
+            containingContract: EthereumAddress(
+              '0xE6841D92B0C345144506576eC13ECf5103aC7f49',
+            ),
+            newImplementations: [EthereumAddress.random()],
+          },
+        ],
+      },
+    },
   }
 }
