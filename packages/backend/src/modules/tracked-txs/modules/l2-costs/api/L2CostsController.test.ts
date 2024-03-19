255057347b1c4ca716b8690af0db3d1f3a316a04
maciekzygmunt <maciekzygmunt@interia.pl>
2024-03-19 08:28:53 +0100

    add tests

--- packages/backend/src/modules/tracked-txs/modules/l2-costs/api/L2CostsController.test.ts
+++ packages/backend/src/modules/tracked-txs/modules/l2-costs/api/L2CostsController.test.ts
@@ -1,17 +1,68 @@
 import { UnixTime } from '@l2beat/shared-pure'
-import { expect, mockObject } from 'earl'
+import { expect, mockFn, mockObject } from 'earl'
 import { range } from 'lodash'
 
 import { Project } from '../../../../../model/Project'
 import { IndexerStateRepository } from '../../../../../peripherals/database/repositories/IndexerStateRepository'
 import { Clock } from '../../../../../tools/Clock'
 import { PriceRepository } from '../../../../tvl/repositories/PriceRepository'
-import { L2CostsRepository } from '../repositories/L2CostsRepository'
+import { TrackedTxId } from '../../../types/TrackedTxId'
+import {
+  L2CostsRecord,
+  L2CostsRepository,
+} from '../repositories/L2CostsRepository'
 import { DetailedTransaction, L2CostsController } from './L2CostsController'
 
 const NOW = UnixTime.now()
 
 describe(L2CostsController.name, () => {
+  describe(L2CostsController.prototype.makeTransactionCalculations.name, () => {
+    it('calculates transaction costs', async () => {
+      const controller = getMockL2CostsController({})
+
+      const results = await controller.makeTransactionCalculations(
+        getMockL2CostRecords(),
+      )
+
+      const expected = [
+        {
+          timestamp: NOW.add(-1, 'hours'),
+          calldataGasUsed: 100,
+          computeGasUsed: -21000,
+          totalGas: 100,
+          gasCost: 100,
+          calldataGasCost: 100,
+          computeGasCost: -21000,
+          totalGasCost: 100,
+          gasCostUsd: 300000,
+          totalGasCostUsd: 300000,
+          calldataGasCostUsd: 300000,
+          computeGasCostUsd: -63000000,
+          type: 2 as const,
+        },
+        {
+          timestamp: NOW.add(-2, 'hours'),
+          calldataGasUsed: 100,
+          computeGasUsed: -21000,
+          totalGas: 200,
+          gasCost: 100,
+          calldataGasCost: 100,
+          computeGasCost: -21000,
+          totalGasCost: 200,
+          gasCostUsd: 310000,
+          totalGasCostUsd: 620000,
+          calldataGasCostUsd: 310000,
+          computeGasCostUsd: -65100000,
+          type: 3 as const,
+          blobGasCost: 100,
+          blobGasUsed: 100,
+          blobGasCostUsd: 310000,
+        },
+      ]
+      expect(results).toEqualUnsorted(expected)
+    })
+  })
+
   describe(L2CostsController.prototype.sumDetails.name, () => {
     it('sums details', () => {
       const transactions = getMockDetailedTransactions(2)
@@ -71,11 +122,50 @@ function getMockL2CostsController(params: {
 
   return new L2CostsController(
     l2CostsRepository ?? mockObject<L2CostsRepository>({}),
-    priceRepository ?? mockObject<PriceRepository>({}),
+    priceRepository ??
+      mockObject<PriceRepository>({
+        findByTimestampRange: mockFn().resolvesToOnce(
+          new Map([
+            [NOW.add(-1, 'hours').toStartOf('hour').toNumber(), 3000],
+            [NOW.add(-2, 'hours').toStartOf('hour').toNumber(), 3100],
+          ]),
+        ),
+      }),
     indexerStateRepository ?? mockObject<IndexerStateRepository>({}),
     projects ?? [],
     mockObject<Clock>({}),
   )
+}
+
+function getMockL2CostRecords(): L2CostsRecord[] {
+  return [
+    {
+      txHash: '0x1',
+      timestamp: NOW.add(-1, 'hours'),
+      trackedTxId: TrackedTxId.unsafe('aaa'),
+      data: {
+        type: 2,
+        gasUsed: 100,
+        gasPrice: 1,
+        calldataLength: 100,
+        calldataGasUsed: 100,
+      },
+    },
+    {
+      txHash: '0x2',
+      timestamp: NOW.add(-2, 'hours'),
+      trackedTxId: TrackedTxId.unsafe('bbb'),
+      data: {
+        type: 3,
+        gasUsed: 100,
+        gasPrice: 1,
+        calldataLength: 100,
+        calldataGasUsed: 100,
+        blobGasPrice: 1,
+        blobGasUsed: 100,
+      },
+    },
+  ]
 }
 
 function getMockDetailedTransactions(amount: number): DetailedTransaction[] {
