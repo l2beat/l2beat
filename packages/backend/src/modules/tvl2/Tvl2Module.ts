1dc6ea5d57b8b56382efa0bc7cf0bec97e1997c9
Antoni Pawlak <antoni.pawlak@pm.me>
2024-03-19 13:17:41 +0100

    Implement PriceIndexer (#3065)
    Co-authored-by: Piotr Szlachciak <szlachciak.piotr@gmail.com>

--- packages/backend/src/modules/tvl2/Tvl2Module.ts
+++ packages/backend/src/modules/tvl2/Tvl2Module.ts
@@ -1,13 +1,27 @@
 import { Logger } from '@l2beat/backend-tools'
+import { tokenList } from '@l2beat/config'
+import {
+  CoingeckoClient,
+  CoingeckoQueryService,
+  HttpClient,
+} from '@l2beat/shared'
 
 import { Config } from '../../config'
+import { IndexerStateRepository } from '../../peripherals/database/repositories/IndexerStateRepository'
+import { Peripherals } from '../../peripherals/Peripherals'
 import { Clock } from '../../tools/Clock'
 import { ApplicationModule } from '../ApplicationModule'
+import { HourlyIndexer } from '../tracked-txs/HourlyIndexer'
 import { createTvl2StatusRouter } from './api/Tvl2StatusRouter'
+import { PriceIndexer } from './PriceIndexer'
+import { PriceRepository } from './repositories/PriceRepository'
+import { SyncOptimizer } from './SyncOptimizer'
 
 export function createTvl2Module(
   config: Config,
   logger: Logger,
+  http: HttpClient,
+  peripherals: Peripherals,
   clock: Clock,
 ): ApplicationModule | undefined {
   if (!config.tvl2) {
@@ -15,10 +29,47 @@ export function createTvl2Module(
     return
   }
 
-  const statusRouter = createTvl2StatusRouter(config.tvl2, clock)
+  const stateRepository = peripherals.getRepository(IndexerStateRepository)
+  const priceRepository = peripherals.getRepository(PriceRepository)
 
-  const start = () => {
+  const coingeckoClient = new CoingeckoClient(http, config.tvl2.coingeckoApiKey)
+  const coingeckoQueryService = new CoingeckoQueryService(coingeckoClient)
+
+  const statusRouter = createTvl2StatusRouter(config.tvl2, clock)
+  const hourlyIndexer = new HourlyIndexer(logger, clock)
+
+  const syncOptimizer = new SyncOptimizer(clock, {
+    removeHourlyAfterDays: 10,
+    removeSixHourlyAfterDays: 93,
+  })
+
+  const indexers = config.tvl2.prices.map(
+    (price) =>
+      new PriceIndexer(
+        // TODO: write it correctly
+        logger.tag(
+          `${price.chain}:${
+            tokenList.find((t) => t.address === price.address)?.symbol
+          }`,
+        ),
+        hourlyIndexer,
+        coingeckoQueryService,
+        stateRepository,
+        priceRepository,
+        price,
+        syncOptimizer,
+      ),
+  )
+
+  const start = async () => {
     logger = logger.for('Tvl2Module')
+
+    await hourlyIndexer.start()
+
+    for (const indexer of indexers) {
+      await indexer.start()
+    }
+
     logger.info('Started')
   }
 
