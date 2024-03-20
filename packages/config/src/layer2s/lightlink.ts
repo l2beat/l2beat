9511c29f5c579338a859f5afc5ef93e20695bc58
sekuba <sekuba@users.noreply.githum.com>
2024-03-20 17:17:31 +0100

    move lightlink to inReview

--- packages/config/src/layer2s/lightlink.ts
+++ packages/config/src/layer2s/lightlink.ts
@@ -1,13 +1,15 @@
-import { upcoming } from './templates/upcoming'
+import { EthereumAddress, UnixTime } from '@l2beat/shared-pure'
+
+import { underReviewL2 } from './templates/underReview'
 import { Layer2 } from './types'
 
-export const lightlink: Layer2 = upcoming({
+export const lightlink: Layer2 = underReviewL2({
   id: 'lightlink',
   display: {
     name: 'LightLink',
     slug: 'lightlink',
     description:
-      'LightLink is an Ethereum Layer 2 blockchain that lets dApps and enterprises offer users instant, gasless transactions.',
+      'LightLink is an Ethereum Layer 2 blockchain based on a proprietary stack that lets dApps and enterprises offer users instant, gasless transactions.',
     purposes: ['Universal'],
     category: 'Optimium',
     links: {
@@ -24,4 +26,16 @@ export const lightlink: Layer2 = upcoming({
       ],
     },
   },
+  escrows: [
+    {
+      address: EthereumAddress('0x3ca373F5ecB92ac762f9876f6e773082A4589995'),
+      sinceTimestamp: new UnixTime(1692155207),
+      tokens: ['ETH'],
+    },
+    {
+      address: EthereumAddress('0x63105ee97BfB22Dfe23033b3b14A4F8FED121ee9'),
+      sinceTimestamp: new UnixTime(1692155531),
+      tokens: '*',
+    },
+  ],
 })
