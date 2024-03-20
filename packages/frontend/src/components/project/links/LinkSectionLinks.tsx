ea7f35390bee3be628fd6123cd31c6566e7ca80a
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2024-03-20 09:22:51 +0100

    Fix dependency issues by recreating yarn.lock

--- packages/frontend/src/components/project/links/LinkSectionLinks.tsx
+++ packages/frontend/src/components/project/links/LinkSectionLinks.tsx
@@ -13,7 +13,7 @@ export function LinkSectionLinks({ name, links }: Props) {
     return null
   }
   return (
-    <tr className="border-t-[1px] border-gray-300 first:border-none dark:border-gray-850 md:px-6 md:first:border-solid md:dark:border-gray-650 ">
+    <tr className="border-t border-gray-300 first:border-none dark:border-gray-850 md:px-6 md:first:border-solid md:dark:border-gray-650 ">
       <th className="w-[110px] py-3 align-top font-medium text-gray-500 dark:text-gray-550 md:w-36 md:pl-6">
         {name}
       </th>
