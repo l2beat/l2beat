db65b4635c18e4c8c8d105ac9d35f06fe5a6eba6
Daniel Lumi <149794418+zk-Lumi@users.noreply.github.com>
2024-03-21 03:35:35 +0100

    Add zkLighter to upcoming projects

---
+++ packages/config/src/layer2s/zklighter.ts
@@ -0,0 +1,25 @@
+import { upcoming } from './templates/upcoming'
+import { Layer2 } from './types'
+
+export const zklighter: Layer2 = upcoming({
+  id: 'zklighter',
+  display: {
+    name: 'zkLighter',
+    slug: 'zklighter',
+    description:
+      'zkLighter is the most efficient order book infrastructure on Ethereum - low cost, low latency, verifiable matching.',
+    purposes: ['DeFi'],
+    category: 'Validium',
+    provider: 'ZK Stack',
+    links: {
+      websites: ['https://zk.lighter.xyz'],
+      apps: [],
+      documentation: ['https://zk.lighter.xyz/developers'],
+      explorers: [],
+      repositories: [],
+      socialMedia: [
+        'https://twitter.com/lighter_xyz',
+      ],
+    },
+  },
+})
