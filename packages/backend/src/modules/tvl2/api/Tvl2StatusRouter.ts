276c8e069dca581cc467b083b09e0363e1142247
antooni <antoni.pawlak@protonmail.com>
2024-03-22 17:36:04 +0100

    add status

--- packages/backend/src/modules/tvl2/api/Tvl2StatusRouter.ts
+++ packages/backend/src/modules/tvl2/api/Tvl2StatusRouter.ts
@@ -1,5 +1,5 @@
 import Router from '@koa/router'
-import { UnixTime } from '@l2beat/shared-pure'
+import { notUndefined, UnixTime } from '@l2beat/shared-pure'
 import { groupBy } from 'lodash'
 
 import { Tvl2Config } from '../../../config/Config'
@@ -7,17 +7,19 @@ import { Clock } from '../../../tools/Clock'
 import { getTargetDataPoints } from './getTargetDataPoints'
 
 export function createTvl2StatusRouter(
-  { amounts, prices }: Tvl2Config,
+  { amounts, prices, chains }: Tvl2Config,
   clock: Clock,
 ) {
   const router = new Router()
 
-  router.get('/status/tokens', (ctx) => {
+  router.get('/status/tvl2', (ctx) => {
     const pricesByType = groupBy(prices, 'type')
 
     const amountsByType = groupBy(amounts, 'type')
     const amountsByChain = groupBy(amounts, 'chain')
     const amountsByProject = groupBy(amounts, 'project')
+
+    const chainsConfig = chains.filter((c) => c.config)
 
     ctx.body = {
       prices: {
@@ -38,6 +40,24 @@ export function createTvl2StatusRouter(
           type,
           entries: entries.length,
           datapoints: getDatapoints(entries, clock),
+        })),
+      },
+      chains: {
+        chains: chainsConfig.length,
+        datapoints: getDatapoints(
+          chainsConfig.map((c) => ({
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            sinceTimestamp: c.config!.minBlockTimestamp,
+          })),
+          clock,
+        ),
+        byChain: chainsConfig.map((c) => ({
+          chain: c.chain,
+          datapoints: getDatapoints(
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            [{ sinceTimestamp: c.config!.minBlockTimestamp }],
+            clock,
+          ),
         })),
       },
     }
