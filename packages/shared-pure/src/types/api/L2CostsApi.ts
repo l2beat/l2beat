bf918c671bc71af230381c8bb8e3a679cd3287cb
maciekzygmunt <maciekzygmunt@interia.pl>
2024-03-22 14:39:26 +0100

    update api response

--- packages/shared-pure/src/types/api/L2CostsApi.ts
+++ packages/shared-pure/src/types/api/L2CostsApi.ts
@@ -1,32 +1,35 @@
 import z from 'zod'
 
-const L2CostsDetails = z
-  .object({
-    totalCost: z.number(),
-    totalGas: z.number(),
-    totalCostUsd: z.number(),
-    totalCalldataGas: z.number(),
-    totalComputeGas: z.number(),
-    totalBlobGas: z.number().optional(),
-    totalCalldataCost: z.number(),
-    totalComputeCost: z.number(),
-    totalBlobCost: z.number().optional(),
-    totalCalldataCostUsd: z.number(),
-    totalComputeCostUsd: z.number(),
-    totalBlobCostUsd: z.number().optional(),
-    totalOverheadGas: z.number(),
-    totalOverheadCost: z.number(),
-    totalOverheadCostUsd: z.number(),
-  })
-  .or(z.undefined())
+import { branded } from '../branded'
+import { UnixTime } from '../UnixTime'
+
+const L2CostsDetails = z.object({
+  totalCost: z.number(),
+  totalGas: z.number(),
+  totalCostUsd: z.number(),
+  totalCalldataGas: z.number(),
+  totalComputeGas: z.number(),
+  totalBlobGas: z.number().optional(),
+  totalCalldataCost: z.number(),
+  totalComputeCost: z.number(),
+  totalBlobCost: z.number().optional(),
+  totalCalldataCostUsd: z.number(),
+  totalComputeCostUsd: z.number(),
+  totalBlobCostUsd: z.number().optional(),
+  totalOverheadGas: z.number(),
+  totalOverheadCost: z.number(),
+  totalOverheadCostUsd: z.number(),
+})
 export type L2CostsDetails = z.infer<typeof L2CostsDetails>
 
 export const L2CostsApiProject = z.object({
-  '24h': L2CostsDetails,
-  '7d': L2CostsDetails,
-  '30d': L2CostsDetails,
-  '90d': L2CostsDetails,
-  isSynced: z.boolean(),
+  last24h: L2CostsDetails.or(z.undefined()),
+  last7d: L2CostsDetails.or(z.undefined()),
+  last30d: L2CostsDetails.or(z.undefined()),
+  last90d: L2CostsDetails.or(z.undefined()),
+  // daily: [L2CostsDetails],
+  // hourly: [L2CostsDetails],
+  syncedUntil: branded(z.number(), (n) => new UnixTime(n)),
 })
 
 export type L2CostsApiProject = z.infer<typeof L2CostsApiProject>
