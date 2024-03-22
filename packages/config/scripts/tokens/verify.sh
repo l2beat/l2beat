9c36adf33d97603c2f2165814e3f17ae9bc1468c
antooni <antoni.pawlak@protonmail.com>
2024-03-22 13:43:08 +0100

    remove diff

--- packages/config/scripts/tokens/verify.sh
+++ packages/config/scripts/tokens/verify.sh
@@ -1,22 +1,5 @@
 git checkout main 
-# git pull 
-# git switch - 
+git pull 
+git switch - 
 git checkout main -- src/tokens/generated.json 
 yarn tokens
-
-#!/bin/bash
-
-# Define the file you want to check
-FILE="src/tokens/generated.json"
-
-# Check for differences in the specified file between the current branch and the origin/main branch
-# Check for differences in the specified file between the current branch and the origin/main branch
-if git diff --quiet origin/main -- "$FILE"; then
-  # No differences in the specified file, print green tick
-  echo "✔ Tokens added using a script."
-  exit 0
-else
-  # Differences found in the specified file, print red cross
-  echo "✘ Manual override detected, please re-run yarn tokens."
-  exit 1
-fi
