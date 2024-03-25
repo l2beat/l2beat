8ae61d9baa382d64435dc96f501279300d199da6
Michał Podsiadły <michal.podsiadly@l2beat.com>
2024-03-25 13:38:18 +0100

    feat: refac scroll analyzer to prevent unnecessary l2 calls

--- packages/backend/src/modules/finality/analyzers/ScrollFinalityAnalyzer.ts
+++ packages/backend/src/modules/finality/analyzers/ScrollFinalityAnalyzer.ts
@@ -1,4 +1,4 @@
-import { assert, TrackedTxsConfigSubtype, UnixTime } from '@l2beat/shared-pure'
+import { TrackedTxsConfigSubtype, UnixTime } from '@l2beat/shared-pure'
 import { utils } from 'ethers'
 import { z } from 'zod'
 
@@ -21,78 +21,73 @@ export class ScrollFinalityAnalyzer extends BaseAnalyzer {
     txHash: string
     timestamp: UnixTime
   }): Promise<number[]> {
-    assert(this.l2Provider, 'Scroll RPC provider not defined')
-
     const tx = await this.provider.getTransaction(transaction.txHash)
     const l1Timestamp = transaction.timestamp
 
-    const decodedTransactionData = this.decodeTransaction(tx.data)
+    const decodedTransactionData = decodeTransaction(tx.data)
 
-    const blockNumbers = decodedTransactionData.chunks
-      .map((chunk) => this.decodeChunkBlockNumbers(chunk))
+    const rawBlockContexts = decodedTransactionData.chunks
+      .map(toBlockContexts)
       .flat()
 
-    const firstBlockInData = Math.max(...blockNumbers)
-    const lastBlockInData = Math.min(...blockNumbers)
+    const l2Timestamps = rawBlockContexts.map(decodeBlockContext)
 
-    const l2Blocks = await Promise.all([
-      this.l2Provider.getBlock(firstBlockInData),
-      this.l2Provider.getBlock(lastBlockInData),
-    ])
-
-    return l2Blocks.map((l2Block) => l1Timestamp.toNumber() - l2Block.timestamp)
-  }
-
-  private decodeTransaction(data: string) {
-    const signature =
-      'commitBatch(uint8 _version, bytes _parentBatchHeader, bytes[] _chunks, bytes _skippedL1MessageBitmap)'
-    const iface = new utils.Interface([`function ${signature}`])
-
-    const decodedInput = iface.decodeFunctionData(signature, data)
-
-    return ScrollBatchCommit.parse({
-      /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-      version: decodedInput._version,
-      parentBatchHeader: decodedInput._parentBatchHeader,
-      chunks: decodedInput._chunks,
-      skippedL1MessageBitmap: decodedInput._skippedL1MessageBitmap,
-      /* eslint-enable @typescript-eslint/no-unsafe-assignment */
-    })
-  }
-
-  /**
-   * @see https://github.com/scroll-tech/scroll/blob/develop/contracts/src/libraries/codec/ChunkCodec.sol#L13
-   */
-  private decodeChunkBlockNumbers(chunk: string) {
-    const BLOCK_CONTEXT_SIZE = 60 * 2 // 60 bytes
-    const BLOCK_NUMBER_SIZE = 8 * 2 // 8 bytes
-
-    const BLOCKS_IN_CHUNK_START = 2 // skip '0x'
-    const BLOCKS_IN_CHUNK_END = 4 // 1 byte
-
-    const BLOCK_CONTEXT_OFFSET = 4 // '0x' + 1 byte for block number
-
-    const amountOfBlocksInChunk = parseInt(
-      chunk.slice(BLOCKS_IN_CHUNK_START, BLOCKS_IN_CHUNK_END),
-      16,
+    return l2Timestamps.map(
+      (l2Timestamp) => l1Timestamp.toNumber() - l2Timestamp,
     )
-
-    const blockNumbers = Array.from(
-      { length: amountOfBlocksInChunk },
-      (_, i) => {
-        const blockNumberByteStart =
-          BLOCK_CONTEXT_OFFSET + i * BLOCK_CONTEXT_SIZE
-        const blockNumberByteEnd = blockNumberByteStart + BLOCK_NUMBER_SIZE
-
-        const blockNumber = parseInt(
-          chunk.slice(blockNumberByteStart, blockNumberByteEnd),
-          16,
-        )
-
-        return blockNumber
-      },
-    )
-
-    return blockNumbers
   }
+}
+
+function decodeTransaction(data: string) {
+  const signature =
+    'commitBatch(uint8 _version, bytes _parentBatchHeader, bytes[] _chunks, bytes _skippedL1MessageBitmap)'
+  const iface = new utils.Interface([`function ${signature}`])
+
+  const decodedInput = iface.decodeFunctionData(signature, data)
+
+  return ScrollBatchCommit.parse({
+    /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+    version: decodedInput._version,
+    parentBatchHeader: decodedInput._parentBatchHeader,
+    chunks: decodedInput._chunks,
+    skippedL1MessageBitmap: decodedInput._skippedL1MessageBitmap,
+    /* eslint-enable @typescript-eslint/no-unsafe-assignment */
+  })
+}
+
+/**
+ * Converts a chunk to a list of block contexts
+ * @param chunk 0x-padded hex-string
+ * @returns a list of block contexts ready for parsing
+ */
+function toBlockContexts(chunk: string) {
+  const BLOCKS_IN_CHUNK_START = 2 // skip '0x'
+  const BLOCKS_IN_CHUNK_END = 4 // 1 byte
+
+  const BLOCK_CONTEXT_SIZE = 60 * 2 // 60 bytes
+
+  const amountOfBlocksInChunk = parseInt(
+    chunk.slice(BLOCKS_IN_CHUNK_START, BLOCKS_IN_CHUNK_END),
+    16,
+  )
+
+  return Array.from({ length: amountOfBlocksInChunk }, (_, i) => {
+    const byteFrom = BLOCKS_IN_CHUNK_END + i * BLOCK_CONTEXT_SIZE
+    const byteTo = byteFrom + BLOCK_CONTEXT_SIZE
+
+    return chunk.slice(byteFrom, byteTo)
+  })
+}
+
+/**
+ * @see https://github.com/scroll-tech/scroll/blob/develop/contracts/src/libraries/codec/ChunkCodecV0.sol
+ * @see https://github.com/scroll-tech/scroll/blob/develop/contracts/src/libraries/codec/ChunkCodecV1.sol
+ */
+function decodeBlockContext(rawBlockContext: string) {
+  const TIMESTAMP_START = 8 * 2 // 8th byte inclusive
+  const TIMESTAMP_END = 16 * 2 // 16th byte inclusive
+
+  const hexTimestamp = rawBlockContext.slice(TIMESTAMP_START, TIMESTAMP_END)
+
+  return parseInt(hexTimestamp, 16)
 }
