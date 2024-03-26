aafd0d12689e460896a9e7bd3a76df7617711864
sekuba <sekuba@users.noreply.github.com>
2024-03-26 17:41:13 +0100

    fix provider

--- packages/config/src/layer3s/syndicateframe.ts
+++ packages/config/src/layer3s/syndicateframe.ts
@@ -13,7 +13,7 @@ export const syndicateframe: Layer3 = underReviewL3({
     description:
       'Syndicate Frame Chain is an OP Stack L3 on Base with Celestia underneath unlocking high throughput and extremely low gas costs for Frame developers.',
     purposes: ['Social', 'NFT'],
-    provider: 'Arbitrum Orbit',
+    provider: 'OP Stack',
     links: {
       websites: ['https://syndicate.io/blog/syndicate-frame-chain'],
       apps: [
