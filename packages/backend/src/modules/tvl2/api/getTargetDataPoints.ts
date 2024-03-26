aef99dbe1b2bd2ced072b76d1f770ff9c6da80f5
antooni <antoni.pawlak@protonmail.com>
2024-03-26 11:30:56 +0100

    fix bug in getTargetDataPoints

--- packages/backend/src/modules/tvl2/api/getTargetDataPoints.ts
+++ packages/backend/src/modules/tvl2/api/getTargetDataPoints.ts
@@ -7,36 +7,28 @@ export function getTargetDataPoints(
   clock: Clock,
 ) {
   const start = token.sinceTimestamp.toEndOf('hour')
-
   const sixHourlyBoundary = clock._TVL_ONLY_getSixHourlyDeletionBoundary()
-
   const hourlyBoundary = clock._TVL_ONLY_getHourlyDeletionBoundary()
-
   const end = clock.getLastHour()
 
   let dataPoints = 0
 
-  if (start.lt(end)) {
-    const hourlyDiff = end.toNumber() - hourlyBoundary.toNumber()
+  const hourlyDiff =
+    end.toNumber() - Math.max(hourlyBoundary.toNumber(), start.toNumber())
+  dataPoints += hourlyDiff > 0 ? Math.floor(hourlyDiff / 3600) + 1 : 0
 
-    dataPoints += hourlyDiff > 0 ? Math.floor(hourlyDiff / 3600) + 1 : 0
-  }
+  const sixHourlyDiff =
+    hourlyBoundary.toStartOf('six hours').toNumber() -
+    Math.max(
+      sixHourlyBoundary.toEndOf('six hours').toNumber(),
+      start.toNumber(),
+    )
+  dataPoints += sixHourlyDiff > 0 ? Math.floor(sixHourlyDiff / 21600) + 1 : 0
 
-  if (start.lt(hourlyBoundary)) {
-    const sixHourlyDiff =
-      hourlyBoundary.toStartOf('six hours').toNumber() -
-      sixHourlyBoundary.toEndOf('six hours').toNumber()
-
-    dataPoints += sixHourlyDiff > 0 ? Math.floor(sixHourlyDiff / 21600) + 1 : 0
-  }
-
-  if (start.lt(sixHourlyBoundary)) {
-    const dailyDiff =
-      sixHourlyBoundary.toStartOf('day').toNumber() -
-      start.toEndOf('day').toNumber()
-
-    dataPoints += dailyDiff > 0 ? Math.floor(dailyDiff / 86400) + 1 : 0
-  }
+  const dailyDiff =
+    sixHourlyBoundary.toStartOf('day').toNumber() -
+    start.toEndOf('day').toNumber()
+  dataPoints += dailyDiff > 0 ? Math.floor(dailyDiff / 86400) + 1 : 0
 
   return dataPoints
 }
