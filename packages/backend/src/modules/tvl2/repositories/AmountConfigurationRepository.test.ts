5a35b017dd8b5364e225ffc799c48dfd1554f1e0
antooni <antoni.pawlak@protonmail.com>
2024-03-27 15:18:10 +0100

    configurations

---
+++ packages/backend/src/modules/tvl2/repositories/AmountConfigurationRepository.test.ts
@@ -0,0 +1,76 @@
+import { Logger } from '@l2beat/backend-tools'
+import { EthereumAddress, ProjectId, UnixTime } from '@l2beat/shared-pure'
+import { expect } from 'earl'
+
+import { describeDatabase } from '../../../test/database'
+import {
+  AmountConfigurationRecord,
+  AmountConfigurationRepository,
+} from './AmountConfigurationRepository'
+
+describeDatabase(AmountConfigurationRepository.name, (database) => {
+  const repository = new AmountConfigurationRepository(database, Logger.SILENT)
+
+  afterEach(async () => {
+    await repository.deleteAll()
+  })
+
+  describe(AmountConfigurationRepository.prototype.addMany.name, () => {
+    it('adds new rows', async () => {
+      const newRows = [
+        {
+          ...mock({ indexerId: 'a' }),
+        },
+        {
+          ...mock({ indexerId: 'b' }),
+        },
+      ]
+      const ids = await repository.addMany(newRows)
+
+      const results = await repository.getAll()
+
+      expect(results).toEqualUnsorted(
+        newRows.map((r, i) => ({ id: ids[i], ...r })),
+      )
+    })
+
+    it('empty array', async () => {
+      await expect(repository.addMany([])).not.toBeRejected()
+    })
+
+    it('performs batch insert when more than 10k records', async () => {
+      const records: Omit<AmountConfigurationRecord, 'id'>[] = []
+      for (let i = 5; i < 15_000; i++) {
+        records.push({
+          ...mock({ indexerId: i.toString() }),
+        })
+      }
+      await expect(repository.addMany(records)).not.toBeRejected()
+    })
+  })
+
+  it(AmountConfigurationRepository.prototype.deleteAll.name, async () => {
+    await repository.addMany([mock()])
+
+    await repository.deleteAll()
+
+    const results = await repository.getAll()
+
+    expect(results).toEqual([])
+  })
+})
+
+function mock(
+  record?: Partial<AmountConfigurationRecord>,
+): Omit<AmountConfigurationRecord, 'id'> {
+  return {
+    projectId: ProjectId('project'),
+    indexerId: 'indexer',
+    chain: 'chain',
+    address: EthereumAddress.random(),
+    origin: 'native',
+    type: 'circulatingSupply',
+    includeInTotal: true,
+    ...record,
+  }
+}
---
+++ packages/backend/src/modules/tvl2/repositories/AmountConfigurationRepository.ts
@@ -0,0 +1,101 @@
+import { Logger } from '@l2beat/backend-tools'
+import { EthereumAddress, ProjectId } from '@l2beat/shared-pure'
+
+import {
+  BaseRepository,
+  CheckConvention,
+} from '../../../peripherals/database/BaseRepository'
+import { Database } from '../../../peripherals/database/Database'
+
+export interface AmountConfigurationRow {
+  id: number
+  project_id: string
+  indexer_id: string
+  chain: string
+  address: string
+  origin: string
+  type: string
+  include_in_total: boolean
+  escrow_address: string | null
+}
+
+export interface AmountConfigurationRecord {
+  id: number
+  projectId: ProjectId
+  indexerId: string
+  chain: string
+  address: EthereumAddress | 'native'
+  origin: 'canonical' | 'external' | 'native'
+  type: 'totalSupply' | 'circulatingSupply' | 'escrow'
+  includeInTotal: boolean
+  escrowAddress?: EthereumAddress
+}
+
+export class AmountConfigurationRepository extends BaseRepository {
+  constructor(database: Database, logger: Logger) {
+    super(database, logger)
+    this.autoWrap<CheckConvention<AmountConfigurationRepository>>(this)
+  }
+
+  async getAll(): Promise<AmountConfigurationRecord[]> {
+    const knex = await this.knex()
+    const rows = await knex('amounts_configurations')
+    return rows.map(toRecord)
+  }
+
+  async addMany(
+    records: Omit<AmountConfigurationRecord, 'id'>[],
+  ): Promise<number[]> {
+    const rows: Omit<AmountConfigurationRow, 'id'>[] = records.map(toRow)
+
+    const knex = await this.knex()
+    const inserted = (await knex
+      .batchInsert('amounts_configurations', rows, 5_000)
+      // @ts-expect-error knex types are wrong
+      .returning('id')) as unknown as { id: number }[]
+
+    return inserted.map((row) => row.id)
+  }
+
+  async deleteAll() {
+    const knex = await this.knex()
+    return knex('amounts_configurations').delete()
+  }
+}
+
+function toRow(
+  row: Omit<AmountConfigurationRecord, 'id'>,
+): Omit<AmountConfigurationRow, 'id'> {
+  return {
+    project_id: row.projectId.toString(),
+    indexer_id: row.indexerId,
+    chain: row.chain,
+    address: row.address === 'native' ? 'native' : row.address.toString(),
+    origin: row.origin,
+    type: row.type,
+    include_in_total: row.includeInTotal,
+    escrow_address: row.escrowAddress?.toString() ?? null,
+  }
+}
+
+function toRecord(row: AmountConfigurationRow): AmountConfigurationRecord {
+  const r: Omit<AmountConfigurationRecord, 'escrowAddress'> = {
+    id: row.id,
+    projectId: ProjectId(row.project_id),
+    indexerId: row.indexer_id,
+    chain: row.chain,
+    address: row.address === 'native' ? 'native' : EthereumAddress(row.address),
+    origin: row.origin as 'canonical' | 'external' | 'native',
+    type: row.type as 'totalSupply' | 'circulatingSupply' | 'escrow',
+    includeInTotal: row.include_in_total,
+  }
+
+  if (row.escrow_address) {
+    return {
+      ...r,
+      escrowAddress: EthereumAddress(row.escrow_address),
+    }
+  }
+
+  return r
+}
