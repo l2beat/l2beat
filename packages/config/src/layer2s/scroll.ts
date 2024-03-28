fae9c7f7e950dbcff9b95df18db08def46256665
Michał Podsiadły <67391475+sdlyy@users.noreply.github.com>
2024-03-28 14:29:32 +0100

    Add scroll finalization warning (#3217)

--- packages/config/src/layer2s/scroll.ts
+++ packages/config/src/layer2s/scroll.ts
@@ -99,6 +99,8 @@ export const scroll: Layer2 = {
         'Scroll is a ZK rollup that posts transaction data to the L1. For a transaction to be considered final, it has to be posted on L1, but the owner can revert them if the corresponding root has not yet be confirmed.',
     },
     finality: {
+      warning:
+        'Transaction data batches that have not yet been proven can be reverted.',
       finalizationPeriod: 0,
     },
   },
