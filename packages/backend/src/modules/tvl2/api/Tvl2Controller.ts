66fe6a84cdc216508130e1f544aea66d4c273851
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2024-03-29 11:23:45 +0100

    Get data in controller

--- packages/backend/src/modules/tvl2/api/Tvl2Controller.ts
+++ packages/backend/src/modules/tvl2/api/Tvl2Controller.ts
@@ -1,26 +1,52 @@
-export interface Tvl2Result {
-  projects: {
-    [projectId: string]: Tvl2ProjectResult
-  }
-  ethereumPrices: Tvl2MultiIntervalSeries
-}
-
-export interface Tvl2MultiIntervalSeries {
-  daily: Tvl2TimeSeries
-  sixHourly: Tvl2TimeSeries
-  hourly: Tvl2TimeSeries
-}
-
-export interface Tvl2TimeSeries {
-  since: number
-  offset: number
-  values: number[]
-}
-
-export interface Tvl2ProjectResult {
-  since: number // unix timestamp
-}
+import { ProjectId, UnixTime } from '@l2beat/shared-pure'
+import { AmountRepository } from '../repositories/AmountRepository'
+import { PriceRepository } from '../repositories/PriceRepository'
 
 export class Tvl2Controller {
-  async getTvl() {}
+  constructor(
+    private readonly amountRepository: AmountRepository,
+    private readonly priceRepository: PriceRepository,
+    private projects: ProjectId[],
+  ) {}
+
+  async getTvl() {
+    const now = UnixTime.now().toStartOf('hour')
+    const results: Record<
+      string,
+      { canonical: number; external: number; native: number }
+    > = {}
+    for (const project of this.projects) {
+      results[project.toString()] = await this.getProjectTvl(project, now)
+    }
+    return results
+  }
+
+  async getProjectTvl(project: ProjectId, timestamp: UnixTime) {
+    const records = await this.amountRepository.getByProjectIdAndTimestamp(
+      project,
+      timestamp,
+    )
+
+    const prices = await this.priceRepository.getByTimestamp(timestamp)
+
+    const results = {
+      canonical: 0,
+      external: 0,
+      native: 0,
+    }
+    for (const record of records) {
+      const price = prices.find(
+        (x) => x.chain === record.chain && x.address === record.address,
+      )
+      if (!price) {
+        // TODO: better error handling.
+        throw new Error(
+          `Price not found for ${record.chain} ${record.address} @ ${timestamp}`,
+        )
+      }
+      // TODO: divide by token decimals.
+      results[record.origin] += Number(record.amount) * price.priceUsd
+    }
+    return results
+  }
 }
