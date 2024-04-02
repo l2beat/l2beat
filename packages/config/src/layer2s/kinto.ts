f0b8c9a0abd88a0dd449c89e42409a137f2b8a17
sekuba <sekuba@users.noreply.github.com>
2024-04-02 17:24:56 +0200

    .ts finesse

--- packages/config/src/layer2s/kinto.ts
+++ packages/config/src/layer2s/kinto.ts
@@ -2,7 +2,6 @@ import { EthereumAddress, ProjectId, UnixTime } from '@l2beat/shared-pure'
 
 import { Layer2 } from './types'
 import { subtractOne } from '../common/assessCount'
-import { addSentimentToDataAvailability } from '../common'
 import { orbitStackL2 } from './templates/orbitStack'
 import { ProjectDiscovery } from '../discovery/ProjectDiscovery'
 
@@ -15,7 +14,7 @@ export const kinto: Layer2 = orbitStackL2({
     slug: 'kinto',
     headerWarning: '',
     description:
-      'Kinto is an Optimium with account abstraction and KYC enabled for all users, that supports both modern financial institutions and decentralized protocols.',
+      'Kinto is an Orbit stack L2 with account abstraction and KYC enabled for all users, supporting both modern financial institutions and decentralized protocols.',
     purposes: ['DeFi'],
     links: {
       websites: ['https://kinto.xyz'],
@@ -36,7 +35,7 @@ export const kinto: Layer2 = orbitStackL2({
       address: EthereumAddress('0x0f1b7bd7762662b23486320aa91f30312184f70c'),
       tokens: '*',
       description:
-        'Escrow for yielding assets that are then bridged in batches.',
+        'Escrow for yielding assets that are later bridged to the L2 in batches.',
     }),
   ],
   bridge: discovery.getContract('Bridge'),
@@ -53,21 +52,23 @@ export const kinto: Layer2 = orbitStackL2({
       'ExecutorMultisig',
       'Multisig that can execute upgrades via the UpgradeExecutor.',
     ),
-    {
-      name: 'RollupOwner',
-      accounts: discovery.getAccessControlRolePermission(
-        'UpgradeExecutor',
-        'EXECUTOR_ROLE',
-      ),
-      description: 'EOA that can execute upgrades via the UpgradeExecutor.',
-    },
+    ...discovery.getMultisigPermission(
+      'BridgerOwnerMultisig',
+      'Multisig that can upgrade the Engen escrow.',
+    ),
   ],
   milestones: [
-    {
-      name: 'Big Event',
-      link: 'https://twitter.com/ParallelFi/status/1743048283684237574',
-      date: '2024-01-05T00:00:00Z',
-      description: 'Parallel Mainnet is open for developers.',
+    // {
+    //   name: 'Mainnet final launch',
+    //   link: 'https://medium.com/mamori-finance/%EF%B8%8F-engen-update-numbers-rewards-00f96370ceac',
+    //   date: '2024-05-16T00:00:00Z',
+    //   description: 'Engen mining is completed and locked funds are deposited to the Kinto L2. Users can deposit and withdraw funds.',
+    // },
+        {
+      name: 'Kinto Mainnet Genesis',
+      link: 'https://medium.com/mamori-finance/%EF%B8%8F-kintos-launch-the-set-up-7eddfbb4bc38',
+      date: '2023-12-15T00:00:00Z',
+      description: 'Kinto mainnet is launched. Deposits by users are disabled.',
     },
   ],
 })
