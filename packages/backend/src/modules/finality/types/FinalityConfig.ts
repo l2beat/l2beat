21c58fba64236967255c588a4c6319c5c29da4be
Michał Podsiadły <michal.podsiadly@l2beat.com>
2024-04-03 16:48:49 +0200

    chore: remove empty generic

--- packages/backend/src/modules/finality/types/FinalityConfig.ts
+++ packages/backend/src/modules/finality/types/FinalityConfig.ts
@@ -2,7 +2,7 @@ import { ProjectId, UnixTime } from '@l2beat/shared-pure'
 
 import { BaseAnalyzer } from '../analyzers/types/BaseAnalyzer'
 
-export interface FinalityConfig<> {
+export interface FinalityConfig {
   projectId: ProjectId
   analyzer: BaseAnalyzer
   minTimestamp: UnixTime
