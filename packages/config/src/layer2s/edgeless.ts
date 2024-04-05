ae476a200ce77b2fe62cec0524b70f5115ce0252
sekuba <sekuba@users.noreply.github.com>
2024-04-05 10:54:36 +0200

    add telegram link

--- packages/config/src/layer2s/edgeless.ts
+++ packages/config/src/layer2s/edgeless.ts
@@ -24,6 +24,7 @@ export const edgeless: Layer2 = underReviewL2({
         'https://twitter.com/EdgelessNetwork',
         'https://discord.gg/edgeless',
         'https://paragraph.xyz/@edgeless',
+        'https://t.me/+f8yhoOg-4cNhYWEx'
       ],
     },
   },
