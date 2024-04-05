3ec5200a936b7d223e814f8b30027bc84795b9cb
hoobi <hubert.kotlinski@l2beat.com>
2024-04-05 13:49:03 +0200

    Added Swell L2 as upcoming project

---
+++ packages/config/src/layer2s/swell.ts
@@ -0,0 +1,23 @@
+import { upcomingL2 } from './templates/upcoming'
+import { Layer2 } from './types'
+
+export const swell: Layer2 = upcomingL2({
+  id: 'swell',
+  display: {
+    name: 'Swell',
+    slug: 'Swell',
+    description:
+      'Swell L2, powered by Polygon CDK,  will leverage EigenDA and the AggLayer in collaboration with AltLayer and Chainlink.',
+    purposes: ['Universal', 'Restaking'],
+    category: 'Validium',
+    provider: 'Polygon',
+    links: {
+      websites: ['https://www.swellnetwork.io/'],
+      apps: [],
+      documentation: ['https://docs.swellnetwork.io/'],
+      explorers: [],
+      repositories: ['https://github.com/SwellNetwork'],
+      socialMedia: ['https://twitter.com/swellnetworkio'],
+    },
+  },
+})
