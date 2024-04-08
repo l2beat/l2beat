0e50b0549709ce6faf4261036929c4d6138f069d
torztomasz <tomasz.torz@l2beat.com>
2024-04-08 13:58:20 +0200

    use 4 decimals for per l2 tx usd

--- packages/frontend/src/pages/scaling/costs/props/getCostsData.ts
+++ packages/frontend/src/pages/scaling/costs/props/getCostsData.ts
@@ -119,7 +119,7 @@ function getCostsDataBreakdown(
       perL2Tx: txCount
         ? {
             value: data.usdCost / txCount,
-            displayValue: formatCurrency(data.usdCost / txCount, 'usd'),
+            displayValue: formatCurrency(data.usdCost / txCount, 'usd', 4),
           }
         : undefined,
     },
