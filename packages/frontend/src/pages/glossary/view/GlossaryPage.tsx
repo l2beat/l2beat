67dadc784f05daa47145327187dc754652dacdf7
torztomasz <tomasz.torz@l2beat.com>
2024-04-10 15:12:35 +0200

    move to separate files

--- packages/frontend/src/pages/glossary/view/GlossaryPage.tsx
+++ packages/frontend/src/pages/glossary/view/GlossaryPage.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 
 import { Footer, FooterProps, Navbar, NavbarProps } from '../../../components'
-import { CopyButton } from '../../../components/CopyButton'
 import { FullPageHeader } from '../../../components/FullPageHeader'
-import { ChiliIcon } from '../../../components/icons/symbols/ChiliIcon'
 import { PageContent } from '../../../components/PageContent'
 import { ScrollToTopButton } from '../../../components/ScrollToTopButton'
-import { cn } from '../../../utils/cn'
 import { GlossaryEntry } from '../props/getGlossaryEntry'
 import { AlphabetSelector } from './AlphabetSelector'
+import { GlossaryItem } from './GlossaryItem'
+import { GlossarySideNavigation } from './GlossarySideNavigation'
 
 export interface GlossaryPageProps {
   entries: GlossaryEntry[]
@@ -47,7 +46,7 @@ export function GlossaryPage(props: GlossaryPageProps) {
         <AlphabetSelector entries={props.entries} />
       </FullPageHeader>
       <PageContent className="mt-12 flex">
-        <NavigationList entries={props.entries} />
+        <GlossarySideNavigation entries={props.entries} />
         <main className="lg:ml-16">
           {props.entries.map((entry) => (
             <GlossaryItem key={entry.id} entry={entry} />
@@ -57,60 +56,5 @@ export function GlossaryPage(props: GlossaryPageProps) {
       <ScrollToTopButton />
       <Footer {...props.footer} />
     </>
-  )
-}
-
-function GlossaryItem(props: { entry: GlossaryEntry }) {
-  return (
-    <section className="mt-12 first:mt-0" id={props.entry.id}>
-      <div className="group flex items-center gap-2 pb-4">
-        <a
-          href={`#${props.entry.id}`}
-          className="peer flex items-center gap-2 text-2xl font-bold text-gray-850 no-underline dark:text-white"
-        >
-          {props.entry.term}
-          {props.entry.isSpicy && <ChiliIcon className="size-6 shrink-0" />}
-        </a>
-        <CopyButton
-          toCopy={`https://l2beat.com/glossary#${props.entry.id}`}
-          className="md:hidden md:animate-quick-fade-in md:group-hover:block"
-        />
-      </div>
-
-      <p className="text-lg text-gray-850 dark:text-white">
-        {props.entry.definition}
-      </p>
-    </section>
-  )
-}
-
-function NavigationList(props: { entries: GlossaryEntry[] }) {
-  return (
-    <nav
-      data-role="glossary-nav-list"
-      className="sticky top-[145px] hidden max-h-[70vh] w-[246px] min-w-[246px] lg:block"
-    >
-      <div className="relative h-full">
-        <ul className="flex h-full flex-col gap-4 overflow-y-scroll pb-8 pr-6">
-          {props.entries.map((entry) => (
-            <li key={entry.id}>
-              <a
-                href={`#${entry.id}`}
-                data-role="glossary-nav-item"
-                className={cn(
-                  'flex items-center gap-1 text-xs font-medium transition-colors duration-100',
-                  'text-zinc-500 hover:text-zinc-700 dark:text-pure-white/80 dark:hover:text-pure-white',
-                  'data-[selected="true"]:text-pink-900 data-[selected="true"]:hover:text-fuchsia-700 dark:data-[selected="true"]:text-pink-200 dark:data-[selected="true"]:hover:text-[#C164E3]',
-                )}
-              >
-                <span className="line-clamp-1">{entry.term}</span>
-                {entry.isSpicy && <ChiliIcon className="shrink-0" />}
-              </a>
-            </li>
-          ))}
-        </ul>
-        <div className="pointer-events-none absolute inset-x-0 bottom-0 z-10 h-8 bg-gradient-to-t from-white via-white dark:from-neutral-900 dark:via-neutral-900" />
-      </div>
-    </nav>
   )
 }
---
+++ packages/frontend/src/pages/glossary/view/GlossarySideNavigation.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import { ChiliIcon } from '../../../components/icons/symbols/ChiliIcon'
+import { cn } from '../../../utils/cn'
+import { GlossaryEntry } from '../props/getGlossaryEntry'
+
+export function GlossarySideNavigation(props: { entries: GlossaryEntry[] }) {
+  return (
+    <nav
+      data-role="glossary-side-nav"
+      className="sticky top-[145px] hidden max-h-[70vh] w-[246px] min-w-[246px] lg:block"
+    >
+      <div className="relative h-full">
+        <ul className="flex h-full flex-col gap-4 overflow-y-scroll pb-8 pr-6">
+          {props.entries.map((entry) => (
+            <li key={entry.id}>
+              <a
+                href={`#${entry.id}`}
+                data-role="glossary-side-nav-item"
+                className={cn(
+                  'flex items-center gap-1 text-xs font-medium transition-colors duration-100',
+                  'text-zinc-500 hover:text-zinc-700 dark:text-pure-white/80 dark:hover:text-pure-white',
+                  'data-[selected="true"]:text-pink-900 data-[selected="true"]:hover:text-fuchsia-700 dark:data-[selected="true"]:text-pink-200 dark:data-[selected="true"]:hover:text-[#C164E3]',
+                )}
+              >
+                <span className="line-clamp-1">{entry.term}</span>
+                {entry.isSpicy && <ChiliIcon className="shrink-0" />}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="pointer-events-none absolute inset-x-0 bottom-0 z-10 h-8 bg-gradient-to-t from-white via-white dark:from-neutral-900 dark:via-neutral-900" />
+      </div>
+    </nav>
+  )
+}
