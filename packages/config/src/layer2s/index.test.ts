124ecd6e9fadd2da679bdf3d8337a8047a421c63
maciekzygmunt <maciekzygmunt@interia.pl>
2024-04-10 15:40:19 +0300

    add test to check multipiers

--- packages/config/src/layer2s/index.test.ts
+++ packages/config/src/layer2s/index.test.ts
@@ -2,6 +2,7 @@ import {
   assertUnreachable,
   EthereumAddress,
   gatherAddressesFromUpgradeability,
+  notUndefined,
   UnixTime,
 } from '@l2beat/shared-pure'
 import { expect } from 'earl'
@@ -119,6 +120,19 @@ describe('layer2s', () => {
             })
           }
         })
+      }
+    })
+
+    describe('every cost multiplier is in 0 to 1 range', () => {
+      for (const project of layer2s) {
+        if (project.config.trackedTxs) {
+          it(`${project.id.toString()} : has valid cost multipliers`, () => {
+            const costMultipliers = project.config.trackedTxs
+              ?.map((t) => t._hackCostMultiplier)
+              .filter(notUndefined)
+            expect(costMultipliers?.every((m) => m > 0 && m <= 1)).toEqual(true)
+          })
+        }
       }
     })
 
