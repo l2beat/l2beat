d1ccfcd707f764d8b63de41688ef9749cc9ce47d
Piotr Adamczyk <xeno@xeno.yt>
2024-04-11 11:31:12 +0200

    Fixes addressing console errors (#3366)

--- packages/frontend/src/components/head/Preload.tsx
+++ packages/frontend/src/components/head/Preload.tsx
@@ -12,21 +12,21 @@ export function Preload(props: Props) {
       ))}
       <link
         rel="preload"
-        href="/fonts/Roboto/roboto-v30-latin-regular.woff2"
+        href="/fonts/roboto/roboto-v30-latin-regular.woff2"
         as="font"
         type="font/woff2"
         crossOrigin="anonymous"
       />
       <link
         rel="preload"
-        href="/fonts/Roboto/roboto-v30-latin-500.woff2"
+        href="/fonts/roboto/roboto-v30-latin-500.woff2"
         as="font"
         type="font/woff2"
         crossOrigin="anonymous"
       />
       <link
         rel="preload"
-        href="/fonts/Roboto/roboto-v30-latin-700.woff2"
+        href="/fonts/roboto/roboto-v30-latin-700.woff2"
         as="font"
         type="font/woff2"
         crossOrigin="anonymous"
