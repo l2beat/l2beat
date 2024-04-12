2cc7d58a7d5554f3df1f3f224d02fce3ac493e6d
Michał Sobieraj-Jakubiec <michalsidzej@gmail.com>
2024-04-12 12:16:46 +0200

    feat: hook up a primitive tvl controller

--- packages/backend/src/modules/tvl2/modules/Tvl2Module.ts
+++ packages/backend/src/modules/tvl2/modules/Tvl2Module.ts
@@ -1,4 +1,5 @@
 import { Logger } from '@l2beat/backend-tools'
+import { UnixTime } from '@l2beat/shared-pure'
 
 import { Config } from '../../../config'
 import { Peripherals } from '../../../peripherals/Peripherals'
@@ -8,7 +9,11 @@ import { IndexerService } from '../../../tools/uif/IndexerService'
 import { IndexerStateRepository } from '../../../tools/uif/IndexerStateRepository'
 import { ApplicationModule } from '../../ApplicationModule'
 import { HourlyIndexer } from '../../tracked-txs/HourlyIndexer'
+import { Tvl2Controller } from '../api/Tvl2Controller'
+import { createTvl2Router } from '../api/Tvl2Router'
 import { createTvl2StatusRouter } from '../api/Tvl2StatusRouter'
+import { AmountRepository } from '../repositories/AmountRepository'
+import { PriceRepository } from '../repositories/PriceRepository'
 import { SyncOptimizer } from '../utils/SyncOptimizer'
 import { createChainModules } from './ChainModule'
 import { createCirculatingSupplyModule } from './CirculatingSupplyModule'
@@ -43,14 +48,16 @@ export function createTvl2Module(
 
   const hourlyIndexer = new HourlyIndexer(logger, clock)
 
+  const priceModule = createPriceModule(
+    config.tvl2,
+    logger,
+    peripherals,
+    hourlyIndexer,
+    syncOptimizer,
+  )
+
   const modules = [
-    createPriceModule(
-      config.tvl2,
-      logger,
-      peripherals,
-      hourlyIndexer,
-      syncOptimizer,
-    ),
+    priceModule,
     ...createChainModules(
       config.tvl2,
       peripherals,
@@ -69,11 +76,34 @@ export function createTvl2Module(
     ),
   ]
 
+  const tvlController = new Tvl2Controller(
+    peripherals.getRepository(AmountRepository),
+    peripherals.getRepository(PriceRepository),
+    {
+      get safeHeight() {
+        return Math.min(...priceModule.indexers.map((i) => i.safeHeight))
+      },
+    },
+    // this is tricky. We need to answer the question: what is the min and max timestamp.
+    // minTimestamp:
+    config.projects.map((p) => ({
+      id: p.projectId,
+      // TODO: we should have a minTimestamp for each project calculated. It should be a part of the project config.
+      minTimestamp: p.escrows
+        .map((e) => e.sinceTimestamp)
+        .reduce((a, b) => UnixTime.min(a, b)),
+      // TODO: we should know the associated indexers for each project.
+      // i.e. Optimism is not interested in ChainAmountIndexer:arbitrum (or is it?)
+      indexers: modules.map((m) => m.indexers).flat(),
+    })),
+    config.tvl2,
+  )
   const statusRouter = createTvl2StatusRouter(
     config.tvl2,
     modules.map((m) => m.indexers).flat(),
     clock,
   )
+  const tvlRouter = createTvl2Router(tvlController)
 
   const start = async () => {
     await hourlyIndexer.start()
@@ -84,7 +114,7 @@ export function createTvl2Module(
   }
 
   return {
-    routers: [statusRouter],
+    routers: [statusRouter, tvlRouter],
     start,
   }
 }
