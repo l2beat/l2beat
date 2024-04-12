14499789dc76a31fc25c0f688db8e5974fd20b14
Piotr Adamczyk <xeno@xeno.yt>
2024-04-12 18:49:28 +0200

    Fix mock

--- packages/frontend/src/build/api/fetchActivityApi.ts
+++ packages/frontend/src/build/api/fetchActivityApi.ts
@@ -1,7 +1,7 @@
 import { layer2s as allLayer2s, layer3s as allLayer3s } from '@l2beat/config'
 import {
   ActivityApiChart,
-  ActivityApiCharts,
+  ActivityApiChartsWithEstimation,
   ActivityApiResponse,
   UnixTime,
 } from '@l2beat/shared-pure'
@@ -32,7 +32,7 @@ function getMockActivityApiResponse(): ActivityApiResponse {
   return result
 }
 
-function getMockActivityApiChart(): ActivityApiCharts {
+function getMockActivityApiChart(): ActivityApiChartsWithEstimation {
   const now = UnixTime.now().toStartOf('day')
   const chart: ActivityApiChart = {
     types: ['timestamp', 'transactions', 'ethereumTransactions'],
@@ -44,5 +44,7 @@ function getMockActivityApiChart(): ActivityApiCharts {
   }
   return {
     daily: chart,
+    estimatedImpact: 0,
+    estimatedSince: now.add(1, 'days'),
   }
 }
