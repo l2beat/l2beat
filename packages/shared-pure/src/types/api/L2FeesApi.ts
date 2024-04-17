009e9a5a2a235f3d4eb02e9b26b978d7fab6538a
maciekzygmunt <maciekzygmunt@interia.pl>
2024-04-17 14:50:06 +0200

    add l2feesapi response

---
+++ packages/shared-pure/src/types/api/L2FeesApi.ts
@@ -0,0 +1,21 @@
+import z from 'zod'
+
+import { branded } from '../branded'
+import { UnixTime } from '../UnixTime'
+
+const L2FeesApiChartPoint = z.tuple([
+  branded(z.number(), (n) => new UnixTime(n)),
+  z.number(),
+])
+export type L2FeesApiChartPoint = z.infer<typeof L2FeesApiChartPoint>
+
+const L2FeesApiChart = z.object({
+  types: z.tuple([z.literal('timestamp'), z.literal('gasPrice')]),
+  data: z.array(L2FeesApiChartPoint),
+})
+export type L2FeesApiChart = z.infer<typeof L2FeesApiChart>
+
+export const L2FeesApiResponse = z.object({
+  projects: z.record(z.string(), z.optional(L2FeesApiChart)),
+})
+export type L2FeesApiResponse = z.infer<typeof L2FeesApiResponse>
