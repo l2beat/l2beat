c601c2392ef8e3448259b00c371006d324ea5ba4
Michał Podsiadły <michal.podsiadly@l2beat.com>
2024-04-17 14:43:06 +0200

    feat: cleanup configs

--- packages/backend/src/modules/fees/financials.ts
+++ packages/backend/src/modules/fees/financials.ts
@@ -6,29 +6,20 @@ import {
   EtherscanClient,
   HttpClient,
 } from '@l2beat/shared'
-<<<<<<< Updated upstream
-import { ChainId, CoingeckoId, UnixTime } from '@l2beat/shared-pure'
-=======
 import {
   assertUnreachable,
   ChainId,
   CoingeckoId,
   UnixTime,
 } from '@l2beat/shared-pure'
-import { writeFileSync } from 'fs'
->>>>>>> Stashed changes
 import { mean } from 'lodash'
 import { createPublicClient, http, PublicClient } from 'viem'
 import { arbitrum, mainnet } from 'viem/chains'
 
 import { ArbitrumFeeAnalyzer } from './ArbitrumFeeAnalyzer'
 import { EVMFeeAnalyzer } from './EVMFeeAnalyzer'
-<<<<<<< Updated upstream
 import { outputCsv } from './output/outputCsv'
-import { Fee, FeeDataPoint } from './types'
-=======
-import { Fee, FeeAnalyzer } from './types'
->>>>>>> Stashed changes
+import { Fee, FeeAnalyzer, FeeDataPoint } from './types'
 import { gweiToEth } from './utils/gasToGwei'
 
 type AnalyzerType = 'evm' | 'arbitrum'
@@ -47,8 +38,8 @@ async function main() {
   const coingecko = new CoingeckoQueryService(
     new CoingeckoClient(new HttpClient(), apiKey),
   )
-  const from = UnixTime.fromDate(new Date('2024-04-16T09:00:00Z'))
-  const to = UnixTime.fromDate(new Date('2024-04-16T10:00:00Z'))
+  const from = UnixTime.fromDate(new Date('2024-04-13T00:00:00Z'))
+  const to = UnixTime.fromDate(new Date('2024-04-14T00:00:00Z'))
 
   const ethPrice = await coingecko.getUsdPriceHistoryHourly(
     CoingeckoId('ethereum'),
@@ -91,25 +82,27 @@ async function main() {
 
   for (const c of config) {
     const points: FeeDataPoint[] = []
+    const analyzer = getAnalyzerForConfig(c)
+    const getGasPriceForTimestamp = getGasPriceProvider(
+      analyzer,
+      c.blockTimestampClient,
+    )
 
-    console.log(c.name)
+    console.log(`Fetching gas prices for ${c.name}`)
     let timestamp = from.toStartOf('hour')
 
     while (timestamp.lte(to)) {
-      const gasPriceInGwei = await getGasPriceForTimestamp(
-        c.rpc,
-        c.blockTimestampClient,
-        timestamp,
-        c.type,
-      )
-      console.log('\nTimestamp', timestamp.toDate().toISOString())
-      console.log('Aggregate:', gasPriceInGwei)
+      const gasPriceInGwei = await getGasPriceForTimestamp(timestamp)
+      console.dir({
+        timestamp: timestamp.toDate().toISOString(),
+        meanAggregate: gasPriceInGwei,
+      })
 
       const gasPriceInEth = gweiToEth(gasPriceInGwei)
 
       const price = ethPrice.find((x) => x.timestamp.equals(timestamp))
 
-      assert(price, 'lol where price')
+      assert(price, 'no price found')
 
       const gasPriceUsd = gasPriceInEth * price.value
 
@@ -124,17 +117,13 @@ async function main() {
 
     await outputCsv(points, c.name)
   }
+}
 
-  main().catch((e: unknown) => {
-    console.error(e)
-  })
-
-  async function getGasPriceForTimestamp(
-    rpc: PublicClient,
-    blockTimestampClient: EtherscanClient | BlockscoutClient,
-    timestamp: UnixTime,
-    type: AnalyzerType,
-  ) {
+function getGasPriceProvider(
+  analyzer: FeeAnalyzer,
+  blockTimestampClient: EtherscanClient | BlockscoutClient,
+) {
+  return async function (timestamp: UnixTime) {
     const fromBlock = await blockTimestampClient.getBlockNumberAtOrBefore(
       timestamp,
     )
@@ -147,26 +136,13 @@ async function main() {
 
     const d: Fee[] = []
 
-    let feeAnalyzer: FeeAnalyzer
-
-    switch (type) {
-      case 'evm':
-        feeAnalyzer = new EVMFeeAnalyzer(rpc)
-        break
-      case 'arbitrum':
-        feeAnalyzer = new ArbitrumFeeAnalyzer(rpc)
-        break
-      default:
-        assertUnreachable(type)
-    }
-
     for (
       let i = fromBlock;
       i < toBlock;
       i += Math.floor(blockDiff / granularity)
     ) {
       console.log('Getting gas price for block', i)
-      const dd = await feeAnalyzer.getData(i)
+      const dd = await analyzer.getData(i)
       d.push(dd)
     }
 
@@ -174,4 +150,17 @@ async function main() {
   }
 }
 
-void main()
+main().catch((e: unknown) => {
+  console.error(e)
+})
+
+function getAnalyzerForConfig(config: Config) {
+  switch (config.type) {
+    case 'evm':
+      return new EVMFeeAnalyzer(config.rpc)
+    case 'arbitrum':
+      return new ArbitrumFeeAnalyzer(config.rpc)
+    default:
+      assertUnreachable(config.type)
+  }
+}
