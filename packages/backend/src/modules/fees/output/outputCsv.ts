64fcca070b8450ed5e50dd00fd9a95fa7f2e78f7
Michał Podsiadły <michal.podsiadly@l2beat.com>
2024-04-17 14:20:19 +0200

    feat: csv 2

--- packages/backend/src/modules/fees/output/outputCsv.ts
+++ packages/backend/src/modules/fees/output/outputCsv.ts
@@ -6,10 +6,12 @@ export { outputCsv }
 
 async function outputCsv(points: FeeDataPoint[], name: string) {
   const delimiter = ','
-  const headers = ['timestamp', 'gasPriceUsd'].join(delimiter)
-  const body = points.map((point) =>
-    [point.timestamp, point.gasPriceUsd, point.gasPriceGwei].join(delimiter),
-  )
+  const headers = ['timestamp', 'gasPriceUsd', 'gasPriceGwei'].join(delimiter)
+  const body = points
+    .map((point) =>
+      [point.timestamp, point.gasPriceUsd, point.gasPriceGwei].join(delimiter),
+    )
+    .join('\n')
 
-  return writeFile(`./${name}.csv`, [headers, body].join('\n'))
+  return writeFile(`./${name}.csv`, `${headers}\n${body}`)
 }
