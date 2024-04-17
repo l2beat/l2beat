20d8099024ffd65e81b98da4a102dabbf123755e
Michał Podsiadły <michal.podsiadly@l2beat.com>
2024-04-17 13:37:30 +0200

    feat: csv xd

--- packages/backend/src/modules/fees/financials.ts
+++ packages/backend/src/modules/fees/financials.ts
@@ -1,10 +1,11 @@
-import { getEnv } from '@l2beat/backend-tools'
+import { assert, getEnv } from '@l2beat/backend-tools'
 import {
   CoingeckoClient,
   CoingeckoQueryService,
   HttpClient,
 } from '@l2beat/shared'
 import { CoingeckoId, UnixTime } from '@l2beat/shared-pure'
+import { writeFileSync } from 'fs'
 import { mean } from 'lodash'
 import { createPublicClient, http, PublicClient } from 'viem'
 import { mainnet } from 'viem/chains'
@@ -12,6 +13,7 @@ import { mainnet } from 'viem/chains'
 import { EVMFeeAnalyzer } from './EVMFeenalyzer'
 import { getBlockByTimestamp } from './getBlockByTimestamp'
 import { Fee } from './types'
+import { gweiToEth } from './utils/gasToGwei'
 
 interface Config {
   name: string
@@ -25,8 +27,8 @@ async function main() {
   const coingecko = new CoingeckoQueryService(
     new CoingeckoClient(new HttpClient(), apiKey),
   )
-  const from = UnixTime.fromDate(new Date('2024-04-13T20:00:00Z'))
-  const to = UnixTime.fromDate(new Date('2024-04-13T21:00:00Z'))
+  const from = UnixTime.fromDate(new Date('2024-04-16T09:00:00Z'))
+  const to = UnixTime.fromDate(new Date('2024-04-16T10:00:00Z'))
 
   const ethPrice = await coingecko.getUsdPriceHistoryHourly(
     CoingeckoId('ethereum'),
@@ -44,44 +46,80 @@ async function main() {
     },
   ]
 
+  type E = {
+    gasPriceUsd: number
+    timestamp: UnixTime
+  }
+
   for (const c of config) {
+    const points: E[] = []
+
     console.log(c.name)
-    let timestamp = from
+    // ??? xd
+    let timestamp = from.toStartOf('hour')
+
     while (timestamp.lte(to)) {
-      const d = await getGasPriceForTimestamp(c.rpc, timestamp)
+      const gasPriceInGwei = await getGasPriceForTimestamp(c.rpc, timestamp)
       console.log('\nTimestamp', timestamp.toDate().toISOString())
-      console.log('Aggregate:', d)
+      console.log('Aggregate:', gasPriceInGwei)
+
+      const gasPriceInEth = gweiToEth(gasPriceInGwei)
+
+      const price = ethPrice.find((x) => x.timestamp.equals(timestamp))
+
+      assert(price, 'lol where price')
+
+      const gasPriceUsd = gasPriceInEth * price.value
+
+      points.push({
+        gasPriceUsd,
+        timestamp,
+      })
+
       timestamp = timestamp.add(1, 'hours')
     }
+
+    const delimiter = ','
+    const headers = ['price', 'timestamp'].join(delimiter)
+    const body = points
+      .map((point) => [point.gasPriceUsd, +point.timestamp].join(delimiter))
+      .join('\n')
+
+    writeFileSync(`./${c.name}.csv`, [headers, body].join('\n'))
   }
-}
 
-main().catch((e: unknown) => {
-  console.error(e)
-})
+  main().catch((e: unknown) => {
+    console.error(e)
+  })
 
-async function getGasPriceForTimestamp(rpc: PublicClient, timestamp: UnixTime) {
-  console.log('Getting first block number (this may take a while)...')
-  const fromBlock = await getBlockByTimestamp(rpc, timestamp)
-  console.log('Getting last block number (this may take a while)...')
-  const toBlock = await getBlockByTimestamp(rpc, timestamp.add(1, 'hours'))
-
-  const blockDiff = toBlock - fromBlock
-  const granularity = 6
-
-  const d: Fee[] = []
-
-  const feeAnalyzer = new EVMFeeAnalyzer(rpc)
-
-  for (
-    let i = fromBlock;
-    i < toBlock;
-    i += Math.floor(blockDiff / granularity)
+  async function getGasPriceForTimestamp(
+    rpc: PublicClient,
+    timestamp: UnixTime,
   ) {
-    console.log('Getting gas price for block', i)
-    const dd = await feeAnalyzer.getData(i)
-    d.push(dd)
-  }
+    console.log('Getting first block number (this may take a while)...')
+    const fromBlock = await getBlockByTimestamp(rpc, timestamp)
+    console.log('Getting last block number (this may take a while)...')
+    const toBlock = await getBlockByTimestamp(rpc, timestamp.add(1, 'hours'))
 
-  return mean(d.map((x) => x.avgFeePerGas))
+    const blockDiff = toBlock - fromBlock
+    const granularity = 6
+
+    const d: Fee[] = []
+
+    const feeAnalyzer = new EVMFeeAnalyzer(rpc)
+
+    for (
+      let i = fromBlock;
+      i < toBlock;
+      i += Math.floor(blockDiff / granularity)
+    ) {
+      console.log('Getting gas price for block', i)
+      const dd = await feeAnalyzer.getData(i)
+      d.push(dd)
+    }
+
+    return mean(d.map((x) => x.avgFeePerGas))
+  }
 }
+
+void main()
--- packages/backend/src/modules/fees/utils/gasToGwei.ts
+++ packages/backend/src/modules/fees/utils/gasToGwei.ts
@@ -1,3 +1,7 @@
 export function gasToGwei(gas: bigint | null | undefined): number {
   return parseFloat((Number(gas) * 1e-9).toFixed(9))
 }
+
+export function gweiToEth(gwei: number): number {
+  return gwei * 1e-9
+}
