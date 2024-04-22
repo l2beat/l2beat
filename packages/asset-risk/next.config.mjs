a66b6adfd0f81ed0491f316f3667907ae171adb5
Piotr Adamczyk <xeno@xeno.yt>
2024-04-22 13:39:15 +0200

    Asset risk front-end scaffold

---
+++ packages/asset-risk/next.config.mjs
@@ -0,0 +1,14 @@
+import createBundleAnalyzer from '@next/bundle-analyzer'
+
+const withBundleAnalyzer = createBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    typedRoutes: true,
+  },
+}
+
+export default withBundleAnalyzer(nextConfig)
---
+++ packages/asset-risk/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { AddressBar } from '~/components/AddressBar'
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Input address</h1>
+      <AddressBar />
+    </main>
+  )
+}
