d5494878f4543a1a03aa3594cc82e6ddbe89af44
Piotr Adamczyk <xeno@xeno.yt>
2024-04-22 16:45:08 +0200

    Add format scripts to asset-risk

--- packages/asset-risk/tailwind.config.ts
+++ packages/asset-risk/tailwind.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   future: {
     hoverOnlyWhenSupported: true,
@@ -208,5 +208,5 @@ const config: Config = {
       },
     },
   },
-};
-export default config;
+}
+export default config
