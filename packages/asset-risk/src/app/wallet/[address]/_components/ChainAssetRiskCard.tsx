fd6450f8b29b1379a9c28e9b4be99f74aaa0d842
Piotr Adamczyk <xeno@xeno.yt>
2024-04-23 18:16:35 +0200

    Display bridge next to the token

--- packages/asset-risk/src/app/wallet/[address]/_components/ChainAssetRiskCard.tsx
+++ packages/asset-risk/src/app/wallet/[address]/_components/ChainAssetRiskCard.tsx
@@ -1,4 +1,4 @@
-import { layer2s } from '@l2beat/config'
+import { bridges, layer2s } from '@l2beat/config'
 import { groupBy } from 'lodash'
 import Image from 'next/image'
 import { formatUnits } from 'viem'
@@ -16,6 +16,7 @@ export interface ChainAssetRiskCardProps {
       decimals: number
       symbol: string
       iconUrl?: string
+      bridge?: string
     }
     balance: bigint
   }[]
@@ -56,29 +57,81 @@ export function ChainAssetRiskCard({
           <StageBadge stage={chain.stage.stage} />
         </div>
         <div className="flex flex-col gap-4 w-full">
-          {tokens.map(({ token, balance }) => (
-            <div key={token.id} className="flex flex-row gap-4 flex-grow">
-              <div className="flex-1">
-                <div className="flex flex-row gap-2 items-center">
-                  {token.iconUrl && (
-                    <div>
-                      <Image
-                        src={token.iconUrl}
-                        alt={token.name}
-                        width={24}
-                        height={24}
-                      />
-                    </div>
-                  )}
-                  <h3 className="text-lg font-bold">{token.name}</h3>
+          {tokens.map(({ token, balance }) => {
+            const bridge = bridges.find((bridge) => bridge.id === token.bridge)
+
+            const bridgeRisks =
+              bridge &&
+              (
+                Object.keys(
+                  bridge.technology,
+                ) as (keyof typeof bridge.technology)[]
+              ).flatMap((key) => {
+                if (
+                  key === 'destination' ||
+                  key === 'canonical' ||
+                  key === 'isUnderReview'
+                )
+                  return []
+                const choice = bridge.technology[key]
+                if (!choice) return []
+                return choice.risks
+              })
+
+            const groupedBridgeRisks =
+              bridgeRisks && groupBy(bridgeRisks, (risk) => risk.category)
+
+            return (
+              <div key={token.id} className="flex flex-row gap-4 flex-grow">
+                <div className="flex-1">
+                  <div className="flex flex-row gap-2 items-center">
+                    {token.iconUrl && (
+                      <div>
+                        <Image
+                          src={token.iconUrl}
+                          alt={token.name}
+                          width={24}
+                          height={24}
+                        />
+                      </div>
+                    )}
+                    <h3 className="text-lg font-bold">{token.name}</h3>
+                  </div>
+                  <p className="text-gray-500 dark:text-gray-600k">
+                    {formatUnits(balance, token.decimals)} {token.symbol}
+                  </p>
                 </div>
-                <p className="text-gray-500 dark:text-gray-600k">{token.id}</p>
+                {groupedBridgeRisks ? (
+                  <div className="flex-1 flex flex-col gap-4">
+                    <h3 className="font-bold">Bridge: {bridge.display.name}</h3>
+                    <div>
+                      {Object.entries(groupedBridgeRisks)
+                        .sort(([categoryA], [categoryB]) =>
+                          categoryA.localeCompare(categoryB),
+                        )
+                        .map(([category, risks]) => (
+                          <div key={category} className="text-sm">
+                            <h4 className="font-bold">{category}</h4>
+                            <ul className="text-xs list-disc list-inside">
+                              {risks.map((risk) => (
+                                <li
+                                  key={risk.text}
+                                  className="text-gray-500 dark:text-gray-600k"
+                                >
+                                  {risk.text}
+                                </li>
+                              ))}
+                            </ul>
+                          </div>
+                        ))}
+                    </div>
+                  </div>
+                ) : (
+                  <div className="flex-1 text-xs">Unknown bridge</div>
+                )}
               </div>
-              <div className="flex-1">
-                {formatUnits(balance, token.decimals)} {token.symbol}
-              </div>
-            </div>
-          ))}
+            )
+          })}
         </div>
         <hr className="w-full border-gray-200 dark:border-zinc-700 md:border-t" />
         <div className="flex flex-col gap-4">
