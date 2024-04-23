0038b02cbbf5727303c6e50a72928d61a51125e0
antooni <antoni.pawlak@protonmail.com>
2024-04-23 15:40:08 +0200

    update createPriceId

--- packages/backend/src/modules/tvl2/utils/createPriceId.ts
+++ packages/backend/src/modules/tvl2/utils/createPriceId.ts
@@ -2,19 +2,22 @@ import { assertUnreachable, PriceConfigEntry } from '@l2beat/shared-pure'
 import { createHash } from 'crypto'
 
 export function createPriceId(priceConfig: PriceConfigEntry): string {
-  let typeSpecificPart: string
+  const input = []
+
+  input.push(priceConfig.address.toString())
+  input.push(priceConfig.chain)
+  input.push(priceConfig.type)
+  // Since and until timestamps are not used in the ID calculation.
+
   switch (priceConfig.type) {
     case 'coingecko':
-      typeSpecificPart = `coingecko-${priceConfig.coingeckoId.toString()}`
+      input.push(priceConfig.coingeckoId.toString())
       break
 
     default:
       assertUnreachable(priceConfig.type)
   }
 
-  const fullId = `${
-    priceConfig.chain
-  }-${priceConfig.address.toString()}-${typeSpecificPart}`
-  const hash = createHash('sha1').update(fullId).digest('hex')
+  const hash = createHash('sha1').update(input.join('')).digest('hex')
   return hash.slice(0, 12)
 }
