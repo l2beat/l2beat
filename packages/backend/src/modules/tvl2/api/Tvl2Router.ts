3e7e2ee7f2f4108b1e51d294dbb60c9ed35a3d8e
Michał Sobieraj-Jakubiec <michalsidzej@gmail.com>
2024-04-25 11:31:24 +0200

    feat: /tvl/token endpoint

--- packages/backend/src/modules/tvl2/api/Tvl2Router.ts
+++ packages/backend/src/modules/tvl2/api/Tvl2Router.ts
@@ -1,5 +1,8 @@
 import Router from '@koa/router'
+import { EthereumAddress, ProjectId, stringAs } from '@l2beat/shared-pure'
+import { z } from 'zod'
 
+import { withTypedContext } from '../../../api/types'
 import { Clock } from '../../../tools/Clock'
 import { Tvl2Controller } from './Tvl2Controller'
 
@@ -11,5 +14,26 @@ export function createTvl2Router(controller: Tvl2Controller, clock: Clock) {
     ctx.body = tvl
   })
 
+  router.get(
+    '/api/tvl2/token/:project/:chain/:address',
+    withTypedContext(
+      z.object({
+        params: z.object({
+          project: stringAs(ProjectId),
+          chain: z.string(),
+          address: z.string(),
+        }),
+      }),
+      async (ctx) => {
+        const { chain, project } = ctx.params
+        const address =
+          ctx.params.address === 'native'
+            ? 'native'
+            : EthereumAddress(ctx.params.address)
+
+        ctx.body = await controller.getTokenChart({ chain, address }, project)
+      },
+    ),
+  )
   return router
 }
