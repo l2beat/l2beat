242e566f0bc6e73976b6565453a68456f9d13a42
hoobi <hubert.kotlinski@l2beat.com>
2024-04-26 17:36:38 +0200

    Edit script

--- packages/frontend/scripts/logos/tinifyLogos.ts
+++ packages/frontend/scripts/logos/tinifyLogos.ts
@@ -21,7 +21,7 @@ async function main() {
     If the limit is reached (500 contributions), create a new one for yourself at https://tinypng.com/developers.
   */
   const apiKey =
-    process.env.TINIFY_API_KEY ?? 'JMFmbQlTJLrxSyvGjLrsNtHP5CGRyTGJ'
+    process.env.TINIFY_API_KEY ?? 'tQ01Mb5MpKfKlCzCNLLpPHqvy6gMlBhF'
 
   if (!apiKey) {
     throw new Error('Missing TINIFY_API_KEY')
