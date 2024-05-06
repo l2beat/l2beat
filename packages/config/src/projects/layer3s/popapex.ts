e7e4b15a3b132fac875347125d42cf3ded33229b
Tomasz Tórz <93620601+torztomasz@users.noreply.github.com>
2024-05-06 13:21:36 +0200

    Restructure directories inside config (#3593)

---
+++ packages/config/src/projects/layer3s/popapex.ts
@@ -0,0 +1,68 @@
+import { assert, ProjectId } from '@l2beat/shared-pure'
+
+import { ProjectDiscovery } from '../../discovery/ProjectDiscovery'
+import { orbitStackL3 } from '../layer2s/templates/orbitStack'
+import { Layer3 } from './types'
+
+const discovery = new ProjectDiscovery('popapex', 'arbitrum')
+
+export const popapex: Layer3 = orbitStackL3({
+  discovery,
+  hostChain: ProjectId('arbitrum'),
+  bridge: discovery.getContract('Bridge'),
+  rollupProxy: discovery.getContract('RollupProxy'),
+  sequencerInbox: discovery.getContract('SequencerInbox'),
+  display: {
+    name: 'Proof of Play Apex',
+    shortName: 'Apex',
+    slug: 'popapex',
+    description:
+      'Proof of Play Apex is a gaming-focused L3 settling on Arbitrum using the Orbit Stack and AnyTrust DA.',
+    purposes: ['Gaming', 'NFT'],
+    links: {
+      websites: ['https://proofofplay.gg'],
+      apps: [
+        'https://bridge.arbitrum.io/?destinationChain=pop-apex&sourceChain=arbitrum-one',
+        'https://piratenation.game/',
+      ],
+      documentation: [],
+      explorers: ['https://explorer.apex.proofofplay.com'],
+      repositories: ['https://github.com/proofofplay'],
+      socialMedia: [
+        'https://twitter.com/ProofOfPlay/',
+        'https://discord.com/invite/piratenation',
+        'https://piratenation.medium.com/',
+      ],
+    },
+    activityDataSource: 'Blockchain RPC',
+  },
+  rpcUrl: 'https://rpc.apex.proofofplay.com', // chainid 70700
+  nonTemplatePermissions: [
+    ...discovery.getMultisigPermission(
+      'RollupOwnerMultisig',
+      (() => {
+        const discoveredAdminOwner = discovery.getAddressFromValue(
+          'ProxyAdmin',
+          'owner',
+        )
+        const discoveredUpgradeExecutorAddy =
+          discovery.getContract('UpgradeExecutor').address
+        const discoveredExecutor = discovery.getAccessControlField(
+          'UpgradeExecutor',
+          'EXECUTOR_ROLE',
+        ).members[0]
+        const discoveredRollupOwnerMultisig = discovery.getContract(
+          'RollupOwnerMultisig',
+        ).address
+        assert(
+          discoveredAdminOwner === discoveredUpgradeExecutorAddy &&
+            discoveredExecutor === discoveredRollupOwnerMultisig,
+          'Update the permissions section if this changes.',
+        )
+        const description =
+          'Has the executor role of the UpgradeExecutor and indirectly owns the ProxyAdmin (can upgrade the whole system).'
+        return description
+      })(),
+    ),
+  ],
+})
