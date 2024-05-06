92a02d99ea6fd08ce28372325265b1ba47e1a832
sekuba <sekuba@users.noreply.github.com>
2024-05-06 12:43:17 +0200

    Aztecconnect update: Rollup ownership renounced (#3560)

--- packages/config/src/layer2s/aztecconnect.ts
+++ packages/config/src/layer2s/aztecconnect.ts
@@ -1,18 +1,10 @@
-import {
-  assert,
-  EthereumAddress,
-  ProjectId,
-  UnixTime,
-} from '@l2beat/shared-pure'
+import { EthereumAddress, ProjectId, UnixTime } from '@l2beat/shared-pure'
 
 import {
   addSentimentToDataAvailability,
-  CONTRACTS,
   FORCE_TRANSACTIONS,
-  makeBridgeCompatible,
   NEW_CRYPTOGRAPHY,
   NUGGETS,
-  OPERATOR,
   RISK_VIEW,
   STATE_CORRECTNESS,
   TECHNOLOGY_DATA_AVAILABILITY,
@@ -23,60 +15,6 @@ import { Layer2 } from './types'
 
 const discovery = new ProjectDiscovery('aztecconnect')
 
-const escapeHatchDelaySeconds = discovery.getContractValue<number>(
-  'RollupProcessorV2',
-  'delayBeforeEscapeHatch',
-)
-
-const upgradeDelay = 0
-
-assert(escapeHatchDelaySeconds === 4294967295) // otherwise change descriptions!!
-const escapeHatchDelayApprox = 4_291_745_472
-const escapeHatchDelayString = '~136 years'
-
-function getAccessControl() {
-  const accessControl = discovery.getContractValue<
-    Record<string, { adminRole: string; members: string[] } | undefined>
-  >('RollupProcessorV2', 'accessControl')
-
-  const check = (contract: string, role: string) => {
-    assert(Object.hasOwn(accessControl, role))
-    assert(
-      accessControl[role]?.members.length === 1,
-      `${role} has more than one member. Add this member to the permissions section.`,
-    )
-    assert(
-      accessControl[role]?.members[0] ===
-        discovery.getContract(contract).address.toString(),
-      `${contract} may not have role:${role} anymore. Update the permissions section.`,
-    )
-  }
-
-  check('Aztec Multisig', 'DEFAULT_ADMIN_ROLE')
-  check('Emergency Multisig', 'EMERGENCY_ROLE')
-  check('Resume Multisig', 'RESUME_ROLE')
-  check('Lister Multisig', 'LISTER_ROLE')
-
-  return [
-    ...discovery.getMultisigPermission(
-      'Aztec Multisig',
-      'Owner of ProxyAdmin contract, which is used to upgrade RollupProcessorV2. OWNER_ROLE on RollupProcessorV2: can enable capped deposit/withdrawals, can add rollupProviders (sequencers), can change delay before escape hatch, can change the verifier contract with no delay, can change defiBridgeProxy',
-    ),
-    ...discovery.getMultisigPermission(
-      'Emergency Multisig',
-      'EMERGENCY_ROLE on RollupProcessorV2: Can pause the rollup.',
-    ),
-    ...discovery.getMultisigPermission(
-      'Resume Multisig',
-      'RESUME_ROLE on RollupProcessorV2: Can resume the rollup.',
-    ),
-    ...discovery.getMultisigPermission(
-      'Lister Multisig',
-      "LISTER_ROLE on RollupProcessorV2: Can add new tokens and bridges to the rollup. Can't remove tokens or bridges.",
-    ),
-  ]
-}
-
 export const aztecconnect: Layer2 = {
   isArchived: true,
   type: 'layer2',
@@ -84,7 +22,7 @@ export const aztecconnect: Layer2 = {
   display: {
     name: 'Zk.Money v2 (Aztec Connect)',
     slug: 'aztecconnect',
-    warning: `EOL: Aztec team announced they are going to shut down the rollup infrastructure on March 21st, 2024. The escape hatch delay has been recently increased to ${escapeHatchDelayString}, meaning that users will not be able to exit when the operator will be shut down.`,
+    warning: `EOL: Aztec team announced they are going to shut down the rollup infrastructure on March 31st, 2024. Deposits are disabled and ownership of the rollup contract is irrevocably renounced. Assets in the escrow can be manually withdrawn with the [Aztec Connect Ejector](https://github.com/AztecProtocol/aztec-connect-ejector).`,
     description:
       'Aztec Connect is an open source layer 2 network that aims to enable affordable, private crypto payments via zero-knowledge proofs.',
     purposes: ['DeFi'],
@@ -126,6 +64,7 @@ export const aztecconnect: Layer2 = {
           selector: '0xf81cccbe',
           functionSignature: 'function processRollup(bytes ,bytes _signatures)',
           sinceTimestampInclusive: new UnixTime(1654638194),
+          untilTimestampExclusive: new UnixTime(1712696939),
         },
       },
     ],
@@ -135,10 +74,9 @@ export const aztecconnect: Layer2 = {
     bridge: { type: 'Enshrined' },
     mode: 'State diffs',
   }),
-  riskView: makeBridgeCompatible({
+  riskView: {
     stateValidation: {
       ...RISK_VIEW.STATE_ZKP_SN,
-
       sources: [
         {
           contract: 'RollupProcessorV2',
@@ -157,6 +95,15 @@ export const aztecconnect: Layer2 = {
         },
       ],
     },
+    proposerFailure: {
+      ...RISK_VIEW.PROPOSER_SELF_PROPOSE_ZK,
+    },
+    sourceUpgradeability: {
+      description:
+        'The ownership of the rollup contract (ProxyAdmin) has been irrevocably renounced, which makes it immutable.',
+      sentiment: 'good',
+      value: 'Immutable',
+    },
     dataAvailability: {
       ...RISK_VIEW.DATA_ON_CHAIN,
       sources: [
@@ -168,7 +115,7 @@ export const aztecconnect: Layer2 = {
         },
       ],
     },
-    exitWindow: RISK_VIEW.EXIT_WINDOW(upgradeDelay, escapeHatchDelayApprox),
+    exitWindow: RISK_VIEW.EXIT_WINDOW_NON_UPGRADABLE,
     sequencerFailure: {
       ...RISK_VIEW.SEQUENCER_SELF_SEQUENCE_ZK(),
       sources: [
@@ -180,21 +127,9 @@ export const aztecconnect: Layer2 = {
         },
       ],
     },
-    proposerFailure: {
-      ...RISK_VIEW.PROPOSER_CANNOT_WITHDRAW,
-      description: `Only the whitelisted proposers can publish L2 state roots on L1 within ${escapeHatchDelayString} from the last posted root, so in the event of failure the withdrawals are frozen.`,
-      sources: [
-        {
-          contract: 'RollupProcessorV2',
-          references: [
-            'https://etherscan.io/address/0x7d657Ddcf7e2A5fD118dC8A6dDc3dC308AdC2728#code#F1#L697',
-          ],
-        },
-      ],
-    },
     validatedBy: RISK_VIEW.VALIDATED_BY_ETHEREUM,
     destinationToken: RISK_VIEW.NATIVE_AND_CANONICAL(),
-  }),
+  },
   stage: getStage(
     {
       stage0: {
@@ -206,14 +141,14 @@ export const aztecconnect: Layer2 = {
       stage1: {
         stateVerificationOnL1: true,
         fraudProofSystemAtLeast5Outsiders: null,
-        usersHave7DaysToExit: false,
-        usersCanExitWithoutCooperation: false,
+        usersHave7DaysToExit: true,
+        usersCanExitWithoutCooperation: true,
         securityCouncilProperlySetUp: null,
       },
       stage2: {
         proofSystemOverriddenOnlyInCaseOfABug: null,
         fraudProofSystemIsPermissionless: null,
-        delayWith30DExitWindow: false,
+        delayWith30DExitWindow: null,
       },
     },
     {
@@ -242,6 +177,8 @@ export const aztecconnect: Layer2 = {
     },
     dataAvailability: {
       ...TECHNOLOGY_DATA_AVAILABILITY.ON_CHAIN_CALLDATA,
+      description:
+        'Since EOL this is only true if the user themself runs the rollup locally and publishes the data.',
       references: [
         {
           text: 'RollupProcessorV2.sol#L686 - Etherscan source code',
@@ -250,20 +187,23 @@ export const aztecconnect: Layer2 = {
       ],
     },
     operator: {
-      ...OPERATOR.CENTRALIZED_OPERATOR,
-      description: `Only specific addresses appointed by the owner are permitted to propose new blocks during regular rollup operation. Periodically a special window is open during which anyone can propose new blocks, but only if the last root was posted more than ${escapeHatchDelayString} prior.`,
+      name: 'No operator',
+      risks: [],
+      description: `Only specific addresses appointed by the owner were permitted to propose new blocks during regular rollup operation. Now that the system is EOL, the rollup can only be processed locally by volunteers.`,
       references: [
         {
           text: 'RollupProcessorV2.sol#L692 - Etherscan source code',
           href: 'https://etherscan.io/address/0x7d657Ddcf7e2A5fD118dC8A6dDc3dC308AdC2728#code#F1#L692',
         },
+        {
+          text: 'Aztec Connect Ejector - Codespace template for running the Aztec Connect rollup.',
+          href: 'https://github.com/AztecProtocol/aztec-connect-ejector',
+        },
       ],
     },
     forceTransactions: {
       ...FORCE_TRANSACTIONS.PROPOSE_OWN_BLOCKS,
-      description:
-        FORCE_TRANSACTIONS.PROPOSE_OWN_BLOCKS.description +
-        ` Periodically the rollup opens a special window during which anyone can propose new blocks. This is only possible if the last root was posted more than ${escapeHatchDelayString} prior.`,
+      description: FORCE_TRANSACTIONS.PROPOSE_OWN_BLOCKS.description,
       references: [
         {
           text: 'RollupProcessorV2.sol#L697 - Etherscan source code',
@@ -274,17 +214,22 @@ export const aztecconnect: Layer2 = {
           href: 'https://etherscan.io/address/0x7d657Ddcf7e2A5fD118dC8A6dDc3dC308AdC2728#code#F1#L1491',
         },
       ],
-      risks: [
-        {
-          category: 'Funds can be frozen if',
-          text: 'the centralized operator censors withdrawal transactions.',
-          isCritical: true,
-        },
-      ],
+      risks: [],
     },
     exitMechanisms: [
       {
-        name: 'Regular withdraw',
+        name: 'EOL: Manual withdrawal using Aztec Connect Ejector',
+        description: `EOL: Aztec team announced they are going to shut down the rollup infrastructure on March 31st, 2024. Deposits are disabled and ownership of the rollup contract is irrevocably renounced. Assets in the escrow can be manually withdrawn with the [Aztec Connect Ejector](https://github.com/AztecProtocol/aztec-connect-ejector).`,
+        risks: [],
+        references: [
+          {
+            text: 'Aztec Connect Ejector - Codespace template for running the Aztec Connect rollup.',
+            href: 'https://github.com/AztecProtocol/aztec-connect-ejector',
+          },
+        ],
+      },
+      {
+        name: 'Regular withdraw (disabled)',
         description:
           'The user initiates the withdrawal by submitting a transaction on L2. When the block containing that transaction is proven on L1 the assets are automatically withdrawn to the user.',
         risks: [],
@@ -318,13 +263,7 @@ export const aztecconnect: Layer2 = {
   contracts: {
     addresses: [
       discovery.getContractDetails('RollupProcessorV2', {
-        description: `Main Rollup contract responsible for deposits, withdrawals and accepting transaction batches alongside a ZK proof. The escape hatch delay is currently set to ${escapeHatchDelayString})}`,
-        pausable: {
-          paused: discovery.getContractValue('RollupProcessorV2', 'paused'),
-          pausableBy: ['Emergency Multisig'],
-        },
-        upgradeDelay: 'No delay',
-        upgradableBy: ['Aztec Multisig'],
+        description: `Main Rollup contract responsible for deposits, withdrawals and accepting transaction batches alongside a ZK proof.`,
       }),
       // rollupBeneficiary is encoded in proofData. Can be set arbitrarily for each rollup.
       // https://etherscan.io/address/0x7d657Ddcf7e2A5fD118dC8A6dDc3dC308AdC2728#code#F1#L704
@@ -339,35 +278,20 @@ export const aztecconnect: Layer2 = {
         'Bridge Connector to various DeFi Bridges.',
       ),
       discovery.getContractDetails('Verifier28x32', {
-        description:
-          'Standard Plonk zkSNARK Verifier. It can be upgraded by the owner with no delay.',
-        upgradeDelay: 'No delay',
-        upgradableBy: ['Aztec Multisig'],
-        upgradeConsiderations:
-          'The verifier can be changed in the RollupProcessor contract with no delay.',
+        description: 'Standard Plonk zkSNARK Verifier.',
       }),
     ],
-    risks: [CONTRACTS.UPGRADE_NO_DELAY_RISK],
+    risks: [],
   },
   stateDerivation: {
-    nodeSoftware: `The entire stack's source code is housed in a single monorepo, which can be found [here](https://github.com/AztecProtocol/aztec-connect/tree/v2.1). For instructions on running the node, please refer to [this readme](https://github.com/AztecProtocol/aztec-connect/blob/v2.1/yarn-project/README.md).`,
+    nodeSoftware: `The entire stack's source code is housed in a single monorepo, which can be found [here](https://github.com/AztecProtocol/aztec-connect/tree/v2.1). For instructions on running the node, please refer to [this readme](https://github.com/AztecProtocol/aztec-connect/blob/v2.1/yarn-project/README.md). Since EOL the [aztec-connect-ejector](https://github.com/AztecProtocol/aztec-connect-ejector) can be used to run a rollup instance and withdraw.`,
     compressionScheme: 'No compression is used.',
     genesisState:
       'The genesis file is available [here](https://github.com/AztecProtocol/aztec-connect/blob/v2.1/yarn-project/falafel/src/environment/init/data/mainnet/accounts), and it includes accounts from [zk.money](http://zk.money) as well.',
     dataFormat:
       'The code to decode onchain data can be found [here](https://github.com/AztecProtocol/aztec-connect/blob/master/yarn-project/barretenberg.js/src/rollup_proof/rollup_proof_data.ts#L453)',
   },
-  permissions: [
-    ...getAccessControl(),
-    {
-      name: 'Rollup Providers',
-      description:
-        'Actors allowed to call the processRollup function on the RollupProcessorvV2 contract.',
-      accounts: discovery
-        .getContractValue<string[]>('RollupProcessorV2', 'rollupProviders')
-        .map((account) => discovery.formatPermissionedAccount(account)),
-    },
-  ],
+  // permissions: [],
   milestones: [
     {
       name: 'Mainnet Launch',
