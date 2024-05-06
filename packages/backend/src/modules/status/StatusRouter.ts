62063b5b8ced64bf206d6f67a19b751e3ce6135b
antooni <antoni.pawlak@protonmail.com>
2024-05-06 15:40:14 +0200

    status - filter by indexer

--- packages/backend/src/modules/status/StatusRouter.ts
+++ packages/backend/src/modules/status/StatusRouter.ts
@@ -1,5 +1,7 @@
 import Router from '@koa/router'
+import { z } from 'zod'
 
+import { withTypedContext } from '../../api/types'
 import { IndexerConfigurationRepository } from '../../tools/uif/IndexerConfigurationRepository'
 import { IndexerStateRepository } from '../../tools/uif/IndexerStateRepository'
 import { renderStatusPagesLinksPage } from './StatusPagesLinksPage'
@@ -10,19 +12,33 @@ export function createStatusRouter(
 ) {
   const router = new Router()
 
-  router.get('/status/configurations', async (ctx) => {
-    const configurations = await indexerConfigurations.getAll()
+  router.get(
+    '/status/configurations',
+    withTypedContext(
+      z.object({
+        query: z.object({
+          indexer: z.string().optional(),
+        }),
+      }),
+      async (ctx) => {
+        const configurations = ctx.query.indexer
+          ? await indexerConfigurations.getSavedConfigurations(
+              ctx.query.indexer,
+            )
+          : await indexerConfigurations.getAll()
 
-    ctx.body = {
-      configurations: configurations.map((c) => ({
-        ...c,
-        properties: JSON.parse(c.properties) as unknown as Record<
-          string,
-          unknown
-        >,
-      })),
-    }
-  })
+        ctx.body = {
+          configurations: configurations.map((c) => ({
+            ...c,
+            properties: JSON.parse(c.properties) as unknown as Record<
+              string,
+              unknown
+            >,
+          })),
+        }
+      },
+    ),
+  )
 
   router.get('/status/indexers', async (ctx) => {
     const indexers = await indexerStateRepository.getAll()
