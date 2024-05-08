23490810dc2f047c39fa1434106a0d2c4aa95e2a
sekuba <sekuba@users.noreply.github.com>
2024-05-08 14:13:33 +0200

    update .ts with asserts for challenges

--- packages/config/src/projects/layer2s/cyber.ts
+++ packages/config/src/projects/layer2s/cyber.ts
@@ -1,4 +1,4 @@
-import { UnixTime, formatSeconds } from '@l2beat/shared-pure'
+import { assert, UnixTime, formatSeconds } from '@l2beat/shared-pure'
 
 import { ProjectDiscovery } from '../../discovery/ProjectDiscovery'
 import { opStackL2 } from './templates/opStack'
@@ -6,18 +6,18 @@ import { Layer2 } from './types'
 
 const discovery = new ProjectDiscovery('cyber')
 
-const daChallengeWindow = formatSeconds(
+const daChallengeWindowBlocks = formatSeconds(
   discovery.getContractValue<number>(
     'DataAvailabilityChallenge',
     'challengeWindow',
-  ) * 12, // in blocks, to seconds
+  ),
 )
 
-const daResolveWindow = formatSeconds(
+const daResolveWindowBlocks = formatSeconds(
   discovery.getContractValue<number>(
     'DataAvailabilityChallenge',
     'resolveWindow',
-  ) * 12, // in blocks, to seconds
+  ),
 )
 
 export const cyber: Layer2 = opStackL2({
@@ -26,7 +26,7 @@ export const cyber: Layer2 = opStackL2({
     name: 'Cyber',
     slug: 'cyber',
     description:
-      'Cyber is an Ethereum layer 2 designed for social applications, based on the OP stack and utilizing EigenDA for data availability.',
+      'Cyber is a chain designed for social applications, based on the OP stack and custom data publishing.',
     redWarning:
       'Critical contracts can be upgraded by an EOA which could result with the loss of all funds.',
     purposes: ['Universal', 'Social'],
@@ -44,16 +44,31 @@ export const cyber: Layer2 = opStackL2({
     activityDataSource: 'Blockchain RPC',
   },
   daProvider: {
-    name: 'EigenDA',
+    name: 'External',
     riskView: {
       value: 'External',
-      description:
-        'Proof construction and state derivation rely on data that is NOT published onchain. Cyber uses EigenDA for data publishing and posts resulting blob keys (blob identifier hashes) to the L1.',
+      description: (() => {
+        assert(
+          daChallengeWindowBlocks === '1' && daResolveWindowBlocks === '1',
+          'The challenge window in the DataAvailabilityChallenge contract changed, please update the daProvider description',
+        )
+        const description =
+          'Proof construction and state derivation rely on data that is NOT published onchain. Cyber uses a custom data availability system without attestations and no option to challenge DA commitments posted to L1.'
+        return description
+      })(),
       sentiment: 'bad',
     },
     technology: {
-      name: 'Data required to compute fraud proof is published offchain without onchain attestations',
-      description: `Cyber relies on DA challenges for data availability. If a DA challenger finds that the data behind a tx data commitment is not available, they can submit a challenge which requires locking a bond within ${daChallengeWindow}. A challenge can be resolved by publishing the preimage data within an additional ${daResolveWindow}. In such case, a portion of the challenger bond is burned, with the exact amount estimated as the cost incurred by the resolver to publish the full data, meaning that the resolver and challenger will approximately lose the same amount of funds. The system is not secure if the malicious sequencer is able to outspend the altruistic challengers. If instead, after a challenge, the preimage data is not published, the chain reorgs to the last fully derivable state.`,
+      name: 'Data is published offchain without a usable challenging mechanism',
+      description: (() => {
+        assert(
+          daChallengeWindowBlocks === '1' && daResolveWindowBlocks === '1',
+          'The challenge window in the DataAvailabilityChallenge contract changed, please update the technology section',
+        )
+        const description =
+          'Cyber uses a minimal data availability system in which the sequencer only publishes data commitments (hashes) to Ethereum L1. These are not attested to nor are users able to challenge them. The data essential for potential fraud- and bridge proofs can thus be withheld by the sequencer at any time.'
+        return description
+      })(),
       references: [
         {
           text: 'OP Plasma specification',
@@ -61,21 +76,17 @@ export const cyber: Layer2 = opStackL2({
         },
         {
           text: 'Universal Plasma and DA Challenges - Ethresear.ch',
-          href: 'https://ethresear.ch/t/universal-plasma-and-da-challenges/18629https://redstone.xyz/docs',
+          href: 'https://ethresear.ch/t/universal-plasma-and-da-challenges/18629',
         },
       ],
       risks: [
         {
           category: 'Funds can be stolen if',
-          text: 'the sequencer is malicious and is able to economically outspend the altruistic challengers.',
-        },
-        {
-          category: 'Funds can be stolen if',
-          text: 'there is no challenger willing to challenge unavailable data commitments.',
+          text: 'the centralized sequencer is malicious.',
         },
       ],
     },
-    bridge: { type: 'None + DA challenges' },
+    bridge: { type: 'None' },
   },
   nonTemplatePermissions: [
     ...discovery.getMultisigPermission(
@@ -83,12 +94,10 @@ export const cyber: Layer2 = opStackL2({
       'Owner of the ProxyAdmin and the rollup system. It can upgrade the bridge implementation potentially gaining access to all funds, and change any system component.',
     ),
     {
-      name: 'DataAvailabilityChallenge owner',
-      accounts: [
-        discovery.getPermissionedAccount('DataAvailabilityChallenge', 'owner'),
-      ],
+      name: 'SystemConfig Owner.',
       description:
-        'Owner of the DataAvailabilityChallenge contract. It can upgrade the contract params, potentially making the system insecure.',
+        'Account privileged to change System Config parameters such as Sequencer Address and gas limit.',
+      accounts: [discovery.getPermissionedAccount('SystemConfig', 'owner')],
     },
   ],
   nonTemplateContracts: [
@@ -100,7 +109,7 @@ export const cyber: Layer2 = opStackL2({
       description: `Upgradable contract that manages the PAUSED_SLOT, a boolean value indicating whether the Superchain is paused, and GUARDIAN_SLOT, the address of the guardian which can pause and unpause the system. The address of the guardian can only be modified by the ProxyAdmin by upgrading the SuperchainConfig contract. This contract is a fork of Optimism's superchainConfig contract and it's unrelated to the one used by the Superchain.`,
     }),
   ],
-  genesisTimestamp: new UnixTime(1713428567),
+  genesisTimestamp: new UnixTime(1713428569),
   isNodeAvailable: 'UnderReview',
   rpcUrl: 'https://cyber.alt.technology/',
 })
