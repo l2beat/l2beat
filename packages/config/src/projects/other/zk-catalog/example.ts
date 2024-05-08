37f10f29d132b86d14b2badfe88387e35915c84d
torztomasz <tomasz.torz@l2beat.com>
2024-05-08 10:09:15 +0200

    fix format

--- packages/config/src/projects/other/zk-catalog/example.ts
+++ packages/config/src/projects/other/zk-catalog/example.ts
@@ -10,7 +10,7 @@ export const example: ZkCatalogProject = {
   },
   proofVerification: {
     description:
-    'Exercitation est excepteur elit non do sit reprehenderit proident. Mollit deserunt tempor aliquip duis dolore. Aliqua cillum adipisicing magna voluptate eu consectetur Lorem eu veniam aute reprehenderit officia cillum. Consequat ea aute proident et ex adipisicing. Et consequat reprehenderit deserunt eiusmod proident mollit fugiat ad nulla. Qui nostrud est ea proident id duis veniam est pariatur dolor voluptate eu aute tempor. Esse amet aute sunt eu. Anim sit ad occaecat magna mollit quis.',
+      'Exercitation est excepteur elit non do sit reprehenderit proident. Mollit deserunt tempor aliquip duis dolore. Aliqua cillum adipisicing magna voluptate eu consectetur Lorem eu veniam aute reprehenderit officia cillum. Consequat ea aute proident et ex adipisicing. Et consequat reprehenderit deserunt eiusmod proident mollit fugiat ad nulla. Qui nostrud est ea proident id duis veniam est pariatur dolor voluptate eu aute tempor. Esse amet aute sunt eu. Anim sit ad occaecat magna mollit quis.',
     aggregation: true,
     requiredTools: [
       {
@@ -79,7 +79,7 @@ export const example: ZkCatalogProject = {
             trustedSetup: 'Example Trusted Setup 2',
           },
         ],
-      }
+      },
     ],
   },
 }
