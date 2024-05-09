9e16bafd3cc9099f6e8ea62571f27dd376fb0f1f
Mateusz Radomski <33978857+mateuszradomski@users.noreply.github.com>
2024-05-09 11:54:35 +0200

    Better heuristic for isRevert (#3631)

--- packages/discovery/src/discovery/utils/isRevert.ts
+++ packages/discovery/src/discovery/utils/isRevert.ts
@@ -1,6 +1,14 @@
+import { z } from 'zod'
+
 export function isRevert(e: unknown): boolean {
   if (!(e instanceof Error)) {
     return false
+  }
+  if ('error' in e) {
+    const parsed = ethersError.parse(e.error)
+    if (parsed.reason === 'bad response' && parsed.status !== undefined) {
+      return false
+    }
   }
   return (
     e.message.includes('invalid opcode: INVALID') ||
@@ -9,3 +17,8 @@ export function isRevert(e: unknown): boolean {
     e.message.includes('reverted')
   )
 }
+
+const ethersError = z.object({
+  reason: z.string(),
+  status: z.number().optional(),
+})
