4b43babb09d5c1e607f268b36200c058ba12b24b
Luca Donno <donnoh99@gmail.com>
2024-05-10 09:37:22 +0200

    add other verifiers

--- packages/config/src/projects/other/zk-catalog/worldcoin.ts
+++ packages/config/src/projects/other/zk-catalog/worldcoin.ts
@@ -28,7 +28,7 @@ export const worldcoin: ZkCatalogProject = {
             proofSystem: 'Groth16',
             mainArithmetization: 'R1CS',
             mainPCS: 'KZG',
-            trustedSetup: 'PPTAU 14',
+            trustedSetup: 'PPTAU 14 + circuit specific',
             link: 'https://github.com/semaphore-protocol/semaphore/blob/main/packages/circuits/src/semaphore.circom',
           },
         ],
@@ -64,6 +64,75 @@ export const worldcoin: ZkCatalogProject = {
             name: 'RegisterVerifierSize100',
             proofSystem: 'Groth16',
             mainArithmetization: 'R1CS',
+            trustedSetup: '',
+            mainPCS: 'KZG',
+          },
+        ],
+      },
+      {
+        name: 'Size 600 Register',
+        description: 'Register verifier of size 600.',
+        contractAddress: EthereumAddress(
+          '0xFC1c26E964F791f81a33F49D91f79456891AA1c1',
+        ),
+        verified: 'no',
+        subVerifiers: [
+          {
+            name: 'RegisterVerifierSize600',
+            proofSystem: 'Groth16',
+            mainArithmetization: 'R1CS',
+            trustedSetup: '',
+            mainPCS: 'KZG',
+          },
+        ],
+      },
+      {
+        name: 'Size 1200 Register',
+        description: 'Register verifier of size 1200.',
+        contractAddress: EthereumAddress(
+          '0xE44c83b9e1971A24EC698829297A0C4026B0CeF9',
+        ),
+        verified: 'no',
+        subVerifiers: [
+          {
+            name: 'RegisterVerifierSize1200',
+            proofSystem: 'Groth16',
+            mainArithmetization: 'R1CS',
+            trustedSetup: '',
+            mainPCS: 'KZG',
+          },
+        ],
+      },
+      {
+        name: 'Size 10 Delete',
+        description: 'Delete verifier of size 10.',
+        contractAddress: EthereumAddress(
+          '0xCA7d6822b9c6913B1A1416cE30eF14c4e7f0bFb1',
+        ),
+        verified: 'no',
+        subVerifiers: [
+          {
+            name: 'DeleteVerifierSize10',
+            proofSystem: 'Groth16',
+            mainArithmetization: 'R1CS',
+            trustedSetup: '',
+            mainPCS: 'KZG',
+          },
+        ],
+      },
+      {
+        name: 'Size 100 Delete',
+        description: 'Delete verifier of size 100.',
+        contractAddress: EthereumAddress(
+          '0x43B68ccBa7FC726540768fD1537c3179283140ed',
+        ),
+        verified: 'no',
+        subVerifiers: [
+          {
+            name: 'DeleteVerifierSize100',
+            proofSystem: 'Groth16',
+            mainArithmetization: 'R1CS',
+            trustedSetup: '',
             mainPCS: 'KZG',
           },
         ],
