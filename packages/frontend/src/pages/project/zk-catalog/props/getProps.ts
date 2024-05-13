3abb94acf06ae637150db4c1def4a8dd0ffcb648
torztomasz <tomasz.torz@l2beat.com>
2024-05-13 12:28:38 +0200

    fix

--- packages/frontend/src/pages/project/zk-catalog/props/getProps.ts
+++ packages/frontend/src/pages/project/zk-catalog/props/getProps.ts
@@ -10,6 +10,7 @@ import {
   ZkCatalogProjectPageProps,
 } from '../view/ZkCatalogProjectPage'
 import { getPageMetadata } from './getPageMetadata'
+import { hasTrustedSetup } from './hasTrustedSetup'
 
 export function getProps(
   project: Layer2 | Layer3 | ZkCatalogProject,
@@ -59,10 +60,4 @@ function getZkCatalogProjectDetails(
     description: descriptionEntry.content,
     ...project.stateValidation.proofVerification,
   }
-}
-
-function hasTrustedSetup(verifiers: ZkCatalogProjectDetails['verifiers']) {
-  return verifiers.some((verifier) =>
-    verifier.subVerifiers.some((subVerifier) => !!subVerifier.trustedSetup),
-  )
 }
