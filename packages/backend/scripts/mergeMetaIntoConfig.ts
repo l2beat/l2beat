dc877a307f1b26f9b1d41854795c0012f6cbec51
Adrian Adamiak <adrian@adamiak.net>
2024-05-14 19:42:12 +0000

    Add removed types to the script

--- packages/backend/scripts/mergeMetaIntoConfig.ts
+++ packages/backend/scripts/mergeMetaIntoConfig.ts
@@ -1,11 +1,9 @@
 /* eslint-disable  @typescript-eslint/no-non-null-assertion */
 import { existsSync, readFileSync, readdirSync, writeFileSync } from 'fs'
 import { join } from 'path'
-import {
-  ConfigReader,
-  DiscoveryMeta,
-  isEmptyValueMeta,
-} from '@l2beat/discovery'
+import { isEmpty } from 'lodash'
+import { z } from 'zod'
+import { ConfigReader } from '@l2beat/discovery'
 import { RawDiscoveryConfig } from '@l2beat/discovery'
 import { parse, stringify } from 'comment-json'
 import { rimraf } from 'rimraf'
@@ -150,5 +148,48 @@ async function main() {
     await transformConfig(configPath)
   }
 }
+
+export const ValueType = z.enum([
+  'CODE_CHANGE',
+  'L2',
+  'EXTERNAL',
+  'RISK_PARAMETER',
+  'PERMISSION',
+])
+
+export type ValueMeta = z.infer<typeof ValueMeta>
+export const ValueMeta = z.object({
+  description: z.string().nullable().optional(),
+  severity: z.enum(['HIGH', 'MEDIUM', 'LOW']).nullable().optional(),
+  type: z
+    .union([ValueType, z.array(ValueType)])
+    .nullable()
+    .optional(),
+})
+
+export function isEmptyValueMeta(value: ValueMeta): boolean {
+  return (
+    isEmpty(value) ||
+    (value.description === null &&
+      value.severity === null &&
+      value.type === null)
+  )
+}
+
+export type ContractMeta = z.infer<typeof ContractMeta>
+export const ContractMeta = z.object({
+  ['$schema']: z.string().optional(),
+  name: z.string(),
+  extends: z.string().optional(),
+  description: z.string().optional(),
+  values: z.record(z.string(), ValueMeta).optional(),
+})
+
+export type DiscoveryMeta = z.infer<typeof DiscoveryMeta>
+export const DiscoveryMeta = z.object({
+  ['$schema']: z.string().optional(),
+  contracts: z.array(ContractMeta),
+  _templatesWereInlined: z.boolean().optional(),
+})
 
 main()
