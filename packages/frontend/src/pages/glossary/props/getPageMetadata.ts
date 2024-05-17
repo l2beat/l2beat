cf6498d339a075296e717008e17a69561c236726
Tomasz Tórz <93620601+torztomasz@users.noreply.github.com>
2024-05-17 10:36:49 +0200

    Update glossary page metadata (#3730)

--- packages/frontend/src/pages/glossary/props/getPageMetadata.ts
+++ packages/frontend/src/pages/glossary/props/getPageMetadata.ts
@@ -2,7 +2,7 @@ export function getPageMetadata() {
   return {
     title: 'L2BEAT - Glossary',
     description: "A glossary of terms for Ethereum's Layer 2 ecosystem",
-    image: 'https://l2beat.coxm/meta-images/pages/og-glossary.png',
+    image: 'https://l2beat.com/meta-images/pages/og-glossary.png',
     url: 'https://l2beat.com/glossary',
   }
 }
