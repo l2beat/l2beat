7a103286809bea1c22480a4d7447c67d4bd08eb9
torztomasz <tomasz.torz@l2beat.com>
2024-05-17 11:16:04 +0200

    make page titles consistent

--- packages/frontend/src/pages/glossary/props/getPageMetadata.ts
+++ packages/frontend/src/pages/glossary/props/getPageMetadata.ts
@@ -1,6 +1,6 @@
 export function getPageMetadata() {
   return {
-    title: 'L2BEAT - Glossary',
+    title: 'Glossary - L2BEAT',
     description: "A glossary of terms for Ethereum's Layer 2 ecosystem",
     image: 'https://l2beat.com/meta-images/pages/og-glossary.png',
     url: 'https://l2beat.com/glossary',
