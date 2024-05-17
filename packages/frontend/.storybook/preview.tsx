2c91c6b90f39ea11fb0cc69db39edf6e8c9abe2e
Piotr Adamczyk <xeno@xeno.yt>
2024-05-17 16:01:50 +0200

    Move page build context wrapper to storybook

--- packages/frontend/.storybook/preview.tsx
+++ packages/frontend/.storybook/preview.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import '../src/styles/style.css'
 import { allModes } from './modes'
 import { viewports } from './viewports'
+import { withPageBuildContext } from '../src/components/navbar/navigationContext'
 
 export const decorators = [
   withThemeByDataAttribute({
@@ -13,11 +14,55 @@ export const decorators = [
     defaultTheme: 'light',
     attributeName: 'class',
   }),
-  (Story) => (
-    <div className="m-4">
-      <Story />
-    </div>
-  ),
+  (Story) =>
+    withPageBuildContext(
+      {
+        config: {
+          features: {
+            activity: true,
+            liveness: true,
+            finality: true,
+            banner: true,
+            tvlBreakdown: true,
+            implementationChange: true,
+            gitcoinOption: true,
+            hiringBadge: true,
+            buildAllProjectPages: true,
+            governancePage: true,
+            costsPage: true,
+            zkCatalog: true,
+            tvl2: true,
+            glossary: true,
+          },
+          links: {
+            twitter: 'test',
+            discord: 'test',
+            github: 'test',
+            linkedin: 'test',
+            youTube: 'test',
+            medium: 'test',
+            forum: 'test',
+            multisigReport: 'test',
+          },
+          backend: {
+            apiUrl: '',
+            mock: true,
+            skipCache: false,
+          },
+          layer2s: [],
+          layer3s: [],
+          bridges: [],
+          zkCatalogProjects: [],
+          milestones: [],
+        },
+        path: '',
+      },
+      () => (
+        <div className="m-4">
+          <Story />
+        </div>
+      ),
+    ),
 ]
 
 export const parameters = {
--- packages/frontend/src/components/navbar/NavWrapper.stories.tsx
+++ packages/frontend/src/components/navbar/NavWrapper.stories.tsx
@@ -3,7 +3,6 @@ import { useEffect } from 'react'
 import React from 'react'
 import { configureThemeToggle } from '../../scripts/configureThemeToggle'
 import { NavWrapper } from './NavWrapper'
-import { withPageBuildContext } from './navigationContext'
 
 const meta: Meta<typeof NavWrapper> = {
   component: NavWrapper,
@@ -13,50 +12,7 @@ const meta: Meta<typeof NavWrapper> = {
       useEffect(() => {
         configureThemeToggle()
       }, [])
-      return withPageBuildContext(
-        {
-          config: {
-            features: {
-              activity: true,
-              liveness: true,
-              finality: true,
-              banner: true,
-              tvlBreakdown: true,
-              implementationChange: true,
-              gitcoinOption: true,
-              hiringBadge: true,
-              buildAllProjectPages: true,
-              governancePage: true,
-              costsPage: true,
-              zkCatalog: true,
-              tvl2: true,
-              glossary: true,
-            },
-            links: {
-              twitter: 'test',
-              discord: 'test',
-              github: 'test',
-              linkedin: 'test',
-              youTube: 'test',
-              medium: 'test',
-              forum: 'test',
-              multisigReport: 'test',
-            },
-            backend: {
-              apiUrl: '',
-              mock: true,
-              skipCache: false,
-            },
-            layer2s: [],
-            layer3s: [],
-            bridges: [],
-            zkCatalogProjects: [],
-            milestones: [],
-          },
-          path: '',
-        },
-        () => <Story />,
-      )
+      return <Story />
     },
   ],
   args: {},
