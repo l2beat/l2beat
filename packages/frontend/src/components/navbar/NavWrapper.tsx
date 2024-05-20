3dd23f95699d272235c0dbb495735378eff26334
Piotr Adamczyk <xeno@xeno.yt>
2024-05-20 18:09:15 +0200

    Add "tabs" under mobile navbar

--- packages/frontend/src/components/navbar/NavWrapper.tsx
+++ packages/frontend/src/components/navbar/NavWrapper.tsx
@@ -182,35 +182,161 @@ function MobileNavBarLink({
   )
 }
 
+type NavbarLinkGroups = {
+  title: string
+  links: {
+    title: string
+    icon: (props: React.SVGProps<SVGSVGElement>) => React.JSX.Element
+    href: string
+    enabled: boolean
+  }[]
+}[]
+
 /**
  * Mobile navigation bar that is shown on the very top on small screens.
  */
-function MobileNavBar() {
+function MobileNavBar({ links }: { links: NavbarLinkGroups }) {
+  const { path } = usePageBuildContext()
+  const currentGroup = links.find((g) =>
+    path.startsWith(`/${g.links[0].href.split('/')[1]}`),
+  )
   return (
-    <div className="xl:hidden h-16 px-3.5 relative flex justify-between flex-row gap-8 border-b border-gray-200 dark:border-gray-850 items-stretch">
-      {/* Left side */}
-      <div className="flex flex-row gap-4">
-        <div className="py-4">
-          <Logo className="h-8 w-auto" />
+    <div className="xl:hidden">
+      <div className="h-16 px-3.5 relative flex justify-between flex-row gap-8 border-b border-gray-200 dark:border-gray-850 items-stretch">
+        {/* Left side */}
+        <div className="flex flex-row gap-4">
+          <div className="py-4">
+            <Logo className="h-8 w-auto" />
+          </div>
+          <ul className="flex flex-row">
+            <MobileNavBarLink
+              title="Scaling"
+              href="/scaling/summary"
+              activeBehavior={(path) => path.startsWith('/scaling')}
+            />
+            <MobileNavBarLink
+              title="Bridges"
+              href="/bridges/summary"
+              activeBehavior={(path) => path.startsWith('/bridges')}
+            />
+          </ul>
         </div>
-        <ul className="flex flex-row">
-          <MobileNavBarLink
-            title="Scaling"
-            href="/scaling/summary"
-            activeBehavior={(path) => path.startsWith('/scaling')}
-          />
-          <MobileNavBarLink
-            title="Bridges"
-            href="/bridges/summary"
-            activeBehavior={(path) => path.startsWith('/bridges')}
-          />
-        </ul>
+        {/* Right side */}
+        <div className="flex flex-row items-center">
+          <button data-role="sidenav-mobile-toggle">
+            <MenuOpenIcon className="h-6 w-6" />
+          </button>
+        </div>
       </div>
-      {/* Right side */}
-      <div className="flex flex-row items-center">
-        <button data-role="sidenav-mobile-toggle">
-          <MenuOpenIcon className="h-6 w-6" />
-        </button>
+      {currentGroup && (
+        <div className="overflow-x-scroll w-full border-b border-gray-200 dark:border-gray-850 text-center">
+          <div className="inline-flex flex-row gap-2 px-4 py-2 m-auto">
+            {currentGroup.links
+              .filter((link) => link.enabled)
+              .map((link) => (
+                <a href={link.href}>
+                  <div className="rounded-[4px] border border-[#AB3BD2] text-xs font-semibold px-4 py-2.5 text-nowrap m-auto">
+                    {link.title}
+                  </div>
+                </a>
+              ))}
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export function Sidenav({ links }: { links: NavbarLinkGroups }) {
+  const { config } = usePageBuildContext()
+  const sharedSizeClasses = 'w-full xl:w-[240px] 2xl:w-[280px]'
+
+  return (
+    <div
+      className={cn(
+        'flex-shrink-0 xl:static absolute flex flex-col items-stretch group translate-x-full data-[open=true]:translate-x-0 xl:transform-none xl:sidenav-collapsed:w-20 z-100',
+        sharedSizeClasses,
+      )}
+      data-role="sidenav"
+    >
+      <div
+        className={cn(
+          'bg-[#E6E7EC] dark:bg-[#1E1C21] dark:border-r dark:border-gray-850 xl:dark:border-none flex flex-col h-screen xl:fixed xl:sidenav-collapsed:w-20 overflow-x-hidden',
+          sharedSizeClasses,
+        )}
+      >
+        <div
+          className={cn(
+            'xl:px-6 px-3.5 py-4 xl:py-[1.125rem] overflow-y-auto overflow-x-hidden flex-1 flex flex-col gap-8',
+            sharedSizeClasses,
+          )}
+          data-role="sidenav-collapse-content"
+        >
+          <div className="flex flex-row justify-between items-center">
+            <Logo className="h-8 w-auto block xl:sidenav-collapsed:hidden" />
+            <LogoSmall className="h-8 w-auto hidden xl:sidenav-collapsed:block" />
+            <div className="xl:sidenav-collapsed:hidden flex flex-row gap-4">
+              <DarkThemeToggle />
+              <button className="xl:hidden" data-role="sidenav-mobile-toggle">
+                <MenuCloseIcon className="h-6 w-6" />
+              </button>
+            </div>
+          </div>
+          <nav className="flex flex-col gap-6 flex-1">
+            {links.map((group) => (
+              <NavLinkGroup key={group.title} title={group.title}>
+                {group.links.map(
+                  (link) =>
+                    link.enabled && (
+                      <NavLink
+                        title={link.title}
+                        icon={link.icon}
+                        href={link.href}
+                      />
+                    ),
+                )}
+              </NavLinkGroup>
+            ))}
+
+            <div className="h-px w-full bg-gray-300 dark:bg-gray-850 xl:sidenav-collapsed:hidden" />
+            <NavSmallLinkGroup>
+              <NavSmallLink title="Forum" href={config.links.forum} />
+              {config.features.zkCatalog && (
+                <NavSmallLink title="ZK Catalog" href="/zk-catalog" />
+              )}
+              <NavSmallLink title="Donate" href={'/donate'} />
+              {config.features.governancePage ? (
+                <NavSmallLink title="Governance" href={'/governance'} />
+              ) : (
+                <NavSmallLink
+                  title="Governance"
+                  href="https://l2beat.notion.site/Delegate-your-votes-to-L2BEAT-8ffc452bed9a431cb158d1e4e19839e3"
+                />
+              )}
+              {config.features.glossary && (
+                <NavSmallLink title="Glossary" href="/glossary" />
+              )}
+              <NavSmallLink href="https://l2beat.notion.site/We-are-hiring-Work-at-L2BEAT-e4e637265ae94c5db7dfa2de336b940f">
+                Jobs
+                {config.features.hiringBadge && (
+                  <HiringBadge className="ml-1 py-0.5 rounded-sm" />
+                )}
+              </NavSmallLink>
+              <NavSmallLink title="FAQ" href="/faq" />
+            </NavSmallLinkGroup>
+          </nav>
+        </div>
+        <div
+          className="p-6 border-t border-gray-300 dark:border-gray-850 hidden xl:block xl:sidenav-collapsed:ml-1"
+          data-role="sidenav-collapse-toggle-container"
+        >
+          <button
+            className="select-none cursor-pointer"
+            data-role="sidenav-collapse-toggle"
+          >
+            <NavCollapseIcon className="sidenav-collapsed:rotate-180" />
+          </button>
+        </div>
       </div>
     </div>
   )
@@ -219,145 +345,83 @@ function MobileNavBar() {
 export function NavWrapper({ children }: { children: ReactNode }) {
   const { config } = usePageBuildContext()
 
-  const sharedSizeClasses = 'w-full xl:w-[240px] 2xl:w-[280px]'
+  const groups: NavbarLinkGroups = [
+    {
+      title: 'Scaling',
+      links: [
+        {
+          title: 'Summary',
+          icon: SummaryIcon,
+          href: '/scaling/summary',
+          enabled: true,
+        },
+        {
+          title: 'Value Locked',
+          icon: TvlIcon,
+          href: '/scaling/tvl',
+          enabled: true,
+        },
+        {
+          title: 'Risk Analysis',
+          icon: RiskIcon,
+          href: '/scaling/risk',
+          enabled: true,
+        },
+        {
+          title: 'Data Availability',
+          icon: DataAvailabilityIcon,
+          href: '/scaling/data-availability',
+          enabled: true,
+        },
+        {
+          title: 'Liveness',
+          icon: LivenessIcon,
+          href: '/scaling/liveness',
+          enabled: config.features.liveness,
+        },
+        {
+          title: 'Finality',
+          icon: FinalityIcon,
+          href: '/scaling/finality',
+          enabled: config.features.finality,
+        },
+        {
+          title: 'Activity',
+          icon: ActivityIcon,
+          href: '/scaling/activity',
+          enabled: config.features.activity,
+        },
+        {
+          title: 'Costs',
+          icon: CostsIcon,
+          href: '/scaling/costs',
+          enabled: config.features.costsPage,
+        },
+      ],
+    },
+    {
+      title: 'Bridges',
+      links: [
+        {
+          title: 'Summary',
+          icon: SummaryIcon,
+          href: '/bridges/summary',
+          enabled: true,
+        },
+        {
+          title: 'Risk Analysis',
+          icon: RiskIcon,
+          href: '/bridges/risk',
+          enabled: true,
+        },
+      ],
+    },
+  ]
 
   return (
     <div className="flex flex-col xl:flex-row relative overflow-x-hidden">
-      <MobileNavBar />
-      <div
-        className={cn(
-          'flex-shrink-0 xl:static absolute flex flex-col items-stretch group translate-x-full data-[open=true]:translate-x-0 xl:transform-none xl:sidenav-collapsed:w-20 z-100',
-          sharedSizeClasses,
-        )}
-        data-role="sidenav"
-      >
-        <div
-          className={cn(
-            'bg-[#E6E7EC] dark:bg-[#1E1C21] dark:border-r dark:border-gray-850 xl:dark:border-none flex flex-col h-screen xl:fixed xl:sidenav-collapsed:w-20 overflow-x-hidden',
-            sharedSizeClasses,
-          )}
-        >
-          <div
-            className={cn(
-              'xl:px-6 px-3.5 py-4 xl:py-[1.125rem] overflow-y-auto overflow-x-hidden flex-1 flex flex-col gap-8',
-              sharedSizeClasses,
-            )}
-            data-role="sidenav-collapse-content"
-          >
-            <div className="flex flex-row justify-between items-center">
-              <Logo className="h-8 w-auto block xl:sidenav-collapsed:hidden" />
-              <LogoSmall className="h-8 w-auto hidden xl:sidenav-collapsed:block" />
-              <div className="xl:sidenav-collapsed:hidden flex flex-row gap-4">
-                <DarkThemeToggle />
-                <button className="xl:hidden" data-role="sidenav-mobile-toggle">
-                  <MenuCloseIcon className="h-6 w-6" />
-                </button>
-              </div>
-            </div>
-            <nav className="flex flex-col gap-6 flex-1">
-              <NavLinkGroup title="Scaling">
-                <NavLink
-                  title="Summary"
-                  icon={SummaryIcon}
-                  href="/scaling/summary"
-                />
-                <NavLink
-                  title="Value Locked"
-                  icon={TvlIcon}
-                  href="/scaling/tvl"
-                />
-                <NavLink
-                  title="Risk Analysis"
-                  icon={RiskIcon}
-                  href="/scaling/risk"
-                />
-                <NavLink
-                  title="Data Availability"
-                  icon={DataAvailabilityIcon}
-                  href="/scaling/data-availability"
-                />
-                {config.features.liveness && (
-                  <NavLink
-                    title="Liveness"
-                    icon={LivenessIcon}
-                    href="/scaling/liveness"
-                  />
-                )}
-                {config.features.finality && (
-                  <NavLink
-                    title="Finality"
-                    icon={FinalityIcon}
-                    href="/scaling/finality"
-                  />
-                )}
-                {config.features.activity && (
-                  <NavLink
-                    title="Activity"
-                    icon={ActivityIcon}
-                    href="/scaling/activity"
-                  />
-                )}
-                {config.features.costsPage && (
-                  <NavLink
-                    title="Costs"
-                    icon={CostsIcon}
-                    href="/scaling/costs"
-                  />
-                )}
-              </NavLinkGroup>
-              <NavLinkGroup title="Bridges">
-                <NavLink
-                  title="Summary"
-                  icon={SummaryIcon}
-                  href="/bridges/summary"
-                />
-                <NavLink
-                  title="Risk Analysis"
-                  icon={RiskIcon}
-                  href="/bridges/risk"
-                />
-              </NavLinkGroup>
-              <div className="h-px w-full bg-gray-300 dark:bg-gray-850 xl:sidenav-collapsed:hidden" />
-              <NavSmallLinkGroup>
-                <NavSmallLink title="Forum" href={config.links.forum} />
-                {config.features.zkCatalog && (
-                  <NavSmallLink title="ZK Catalog" href="/zk-catalog" />
-                )}
-                <NavSmallLink title="Donate" href={'/donate'} />
-                {config.features.governancePage ? (
-                  <NavSmallLink title="Governance" href={'/governance'} />
-                ) : (
-                  <NavSmallLink
-                    title="Governance"
-                    href="https://l2beat.notion.site/Delegate-your-votes-to-L2BEAT-8ffc452bed9a431cb158d1e4e19839e3"
-                  />
-                )}
-                {config.features.glossary && (
-                  <NavSmallLink title="Glossary" href="/glossary" />
-                )}
-                <NavSmallLink href="https://l2beat.notion.site/We-are-hiring-Work-at-L2BEAT-e4e637265ae94c5db7dfa2de336b940f">
-                  Jobs
-                  {config.features.hiringBadge && (
-                    <HiringBadge className="ml-1 py-0.5 rounded-sm" />
-                  )}
-                </NavSmallLink>
-                <NavSmallLink title="FAQ" href="/faq" />
-              </NavSmallLinkGroup>
-            </nav>
-          </div>
-          <div
-            className="p-6 border-t border-gray-300 dark:border-gray-850 hidden xl:block xl:sidenav-collapsed:ml-1"
-            data-role="sidenav-collapse-toggle-container"
-          >
-            <button
-              className="select-none cursor-pointer"
-              data-role="sidenav-collapse-toggle"
-            >
-              <NavCollapseIcon className="sidenav-collapsed:rotate-180" />
-            </button>
-          </div>
-        </div>
-      </div>
+      <MobileNavBar links={groups} />
+      <Sidenav links={groups} />
       <div className="flex-1">{children}</div>
     </div>
   )
