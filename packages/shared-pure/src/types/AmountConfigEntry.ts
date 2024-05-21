772f3a0039c9830608ca50474065cdccecc5811a
antooni <antoni.pawlak@protonmail.com>
2024-05-21 15:40:08 +0200

    base

--- packages/shared-pure/src/types/AmountConfigEntry.ts
+++ packages/shared-pure/src/types/AmountConfigEntry.ts
@@ -3,10 +3,12 @@
 DO NOT MODIFY THIS FILE WITHOUT MODIFYING THE "createAmountId" FUNCTION
 */
 
-import { CoingeckoId } from './CoingeckoId'
-import { EthereumAddress } from './EthereumAddress'
+import { z } from 'zod'
 import { ProjectId } from './ProjectId'
 import { UnixTime } from './UnixTime'
+import { branded } from './branded'
+import { EthereumAddress } from './EthereumAddress'
+import { CoingeckoId } from './CoingeckoId'
 
 export type AmountConfigEntry =
   | TotalSupplyEntry
@@ -30,13 +32,16 @@ export interface EscrowEntry extends AmountConfigBase {
   escrowAddress: EthereumAddress
 }
 
-export interface AmountConfigBase {
-  chain: string
-  project: ProjectId
-  source: 'canonical' | 'external' | 'native'
-  sinceTimestamp: UnixTime
-  untilTimestamp?: UnixTime
-  includeInTotal: boolean
-  decimals: number
-  symbol: string
-}
+export const AmountConfigBase = z.object({
+  chain: z.string(),
+  project: branded(z.string(), ProjectId),
+  source: z.enum(['canonical', 'external', 'native']),
+  sinceTimestamp: branded(z.number(), (n) => new UnixTime(n)),
+  untilTimestamp: branded(z.number().optional(), (n) =>
+    n !== undefined ? new UnixTime(n) : undefined,
+  ),
+  includeInTotal: z.boolean(),
+  decimals: z.number(),
+  symbol: z.string(),
+})
+export type AmountConfigBase = z.infer<typeof AmountConfigBase>
