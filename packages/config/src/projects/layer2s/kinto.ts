661e8e57df4c6186d16a05c3ed72fb5bde5860a0
sekuba <sekuba@users.noreply.github.com>
2024-05-22 17:45:41 +0200

    Update info after verified Bridger contract

--- packages/config/src/projects/layer2s/kinto.ts
+++ packages/config/src/projects/layer2s/kinto.ts
@@ -109,13 +109,7 @@ export const kinto: Layer2 = orbitStackL2({
       address: EthereumAddress('0x0f1b7bd7762662b23486320aa91f30312184f70c'),
       tokens: '*',
       description:
-        "Bridger escrow that swaps deposited assets to 'L2 final assets' that are later bridged to the L2 in batches.",
-    }),
-    discovery.getEscrowDetails({
-      address: EthereumAddress('0x7870D5398DB488c669B406fBE57b8d05b6A35e42'),
-      tokens: '*',
-      description:
-        'Main entry point for users depositing ERC20 tokens. Upon depositing, on L2 a generic, "wrapped" token will be minted.',
+        "Bridger gateway that can swap assets to 'L2 final assets' before bridging them to the L2.",
     }),
   ],
   isNodeAvailable: false,
@@ -136,15 +130,14 @@ export const kinto: Layer2 = orbitStackL2({
     ),
     ...discovery.getMultisigPermission(
       'BridgerOwnerMultisig',
-      'Multisig that can upgrade the Engen escrow.',
+      'Multisig that can upgrade the Bridger gateway contract. It also owns the Socket contracts used as escrows for bridged assets.',
     ),
-    // Contract currently unverified
-    // {
-    //   name: 'Bridger Sender Account',
-    //   description:
-    //     'EOA privileged to call `bridgeDeposits()` on the Bridger L1 escrow to send escrowed funds to the L2. It can also deposit tokens into the escrow itself by submitting signatures.',
-    //   accounts: [discovery.getPermissionedAccount('Bridger', 'senderAccount')],
-    // },
+    {
+      name: 'Bridger Sender Account',
+      description:
+        'EOA privileged to call `depositBySig()` on the Bridger gateway to deposit assets to the L2 using pre-signed transactions from users.',
+      accounts: [discovery.getPermissionedAccount('Bridger', 'senderAccount')],
+    },
   ],
   milestones: [
     {
