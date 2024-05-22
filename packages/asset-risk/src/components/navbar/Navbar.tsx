987b058e596769e1f61b10db9c4f145f87cda0d9
maciekzygmunt <maciekzygmunt@interia.pl>
2024-05-22 08:54:33 +0200

    add wallet info to navbar

--- packages/asset-risk/src/components/navbar/Navbar.tsx
+++ packages/asset-risk/src/components/navbar/Navbar.tsx
@@ -1,15 +1,15 @@
 import Link from 'next/link'
-import React from 'react'
 
 import { Logo } from '../Logo'
 import { DarkThemeToggle } from './DarkThemeToggle'
 import { InputWallet } from './InputWallet'
 import { ScannerName } from './ScannerName'
+import { WalletInfo } from './WalletInfo'
 
 export function Navbar() {
   return (
     <>
-      <div className="h-14 border-b border-[#272A3133] text-base dark:border-gray-850 lg:h-16">
+      <div className="h-14 border-b border-[#272A3133] min-w-fit text-base dark:border-gray-850 lg:h-16">
         <nav className="relative mx-auto box-border flex h-full max-w-[1780px] items-center justify-between px-4 lg:px-8">
           <ul className="flex h-full items-center">
             <li className="mr-4 lg:mr-8">
@@ -23,6 +23,7 @@ export function Navbar() {
           </ul>
           <div className="flex items-center gap-3">
             <InputWallet />
+            <WalletInfo />
             <DarkThemeToggle />
           </div>
         </nav>
---
+++ packages/asset-risk/src/components/navbar/WalletInfo.tsx
@@ -0,0 +1,26 @@
+'use client'
+import { useAccount, useEnsName } from 'wagmi'
+import { formatAddress } from '~/utils/formatAddress'
+import { ChangeWallet } from '../ChangeWallet'
+
+export function WalletInfo() {
+  const { isConnected, address } = useAccount()
+  const { data: ensAddress } = useEnsName({
+    address,
+  })
+
+  if (!isConnected) return null
+
+  return (
+    <div className="flex items-center gap-3">
+      <span className="font-medium text-sm text-black">
+        {ensAddress
+          ? ensAddress
+          : address
+            ? formatAddress('0x8aEb2A694589a6ACaD9716d23198ed3784d4C50e')
+            : null}
+      </span>
+      <ChangeWallet />
+    </div>
+  )
+}
