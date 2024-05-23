5164644ffe7c16e986263c8a6adabf5d7ef15723
Piotr Adamczyk <xeno@xeno.yt>
2024-05-23 15:06:25 +0200

    Fix fallback typings

--- packages/frontend2/svgr.d.ts
+++ packages/frontend2/svgr.d.ts
@@ -5,6 +5,7 @@ declare module '*.svg' {
 }
 
 declare module '*.svg?url' {
-  const content: string
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const content: any
   export default content
 }
