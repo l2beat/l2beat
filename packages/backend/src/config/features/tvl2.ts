3d3fe9ce925bdd372e8eaf1f9e65f0eb9160fd6e
Michał Sobieraj-Jakubiec <michalsidzej@gmail.com>
2024-05-23 17:10:40 +0200

    fix: L3s tvl categories

--- packages/backend/src/config/features/tvl2.ts
+++ packages/backend/src/config/features/tvl2.ts
@@ -143,7 +143,7 @@ function getAmountsConfig(
           ),
           escrowAddress: escrow.address,
           project: project.projectId,
-          source: toSource(token.type),
+          source: 'canonical',
           includeInTotal: escrow.includeInTotal ?? true,
           decimals: token.decimals,
           symbol: token.symbol,
