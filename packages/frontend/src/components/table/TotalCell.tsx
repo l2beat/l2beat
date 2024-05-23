6d4b0a028dfd005b8801d9660747b156fa16dafe
torztomasz <tomasz.torz@l2beat.com>
2024-05-23 12:18:48 +0200

    use one tooltip

--- packages/frontend/src/components/table/TotalCell.tsx
+++ packages/frontend/src/components/table/TotalCell.tsx
@@ -4,7 +4,6 @@ import {
   ScalingL2SummaryViewEntry,
   ScalingL3SummaryViewEntry,
 } from '../../pages/scaling/summary/types'
-import { Callout } from '../Callout'
 import { WarningBar } from '../WarningBar'
 import { Badge } from '../badge/Badge'
 import { TokenBreakdown } from '../breakdown/TokenBreakdown'
@@ -24,68 +23,48 @@ export function TotalCell({ project }: TotalCellProps) {
   return (
     <div>
       {project.tvl ? (
-        <div className="flex flex-col items-end">
-          <div className="flex items-center gap-1">
-            {project.tvlWarnings?.length && project.tvl ? (
-              <Tooltip>
-                <TooltipTrigger>
+        <Tooltip>
+          <TooltipTrigger>
+            <div className="flex flex-col items-end">
+              <div className="flex items-center gap-1">
+                {project.tvlWarnings?.length && project.tvl ? (
                   <RoundedWarningIcon
                     className="size-4"
                     sentiment={anyBadWarnings ? 'bad' : 'warning'}
                   />
-                </TooltipTrigger>
-                <TooltipContent>
-                  {project.tvlWarnings.map((warning, i) => (
-                    <Callout
-                      key={`tvl-warning-${i}`}
-                      icon={
-                        <RoundedWarningIcon
-                          className="size-5"
-                          sentiment={warning.sentiment}
-                        />
-                      }
-                      body={warning.content}
-                    />
-                  ))}
-                </TooltipContent>
-              </Tooltip>
-            ) : null}
-            <NumberCell
-              className="font-bold"
-              tooltip={project.tvlWarnings ? undefined : project.tvlTooltip}
-            >
-              {project.tvl?.displayValue}
-            </NumberCell>
-            <NumberCell signed className="ml-1 !text-base font-medium">
-              {project.sevenDayChange}
-            </NumberCell>
-          </div>
-          {project.tvlBreakdown ? (
-            <Tooltip>
-              <TooltipTrigger>
+                ) : null}
+                <NumberCell
+                  className="font-bold"
+                  tooltip={project.tvlWarnings ? undefined : project.tvlTooltip}
+                >
+                  {project.tvl?.displayValue}
+                </NumberCell>
+                <NumberCell signed className="ml-1 !text-base font-medium">
+                  {project.sevenDayChange}
+                </NumberCell>
+              </div>
+              {project.tvlBreakdown ? (
                 <TokenBreakdown
                   {...project.tvlBreakdown}
                   className="h-[3px] w-[180px]"
                 />
-              </TooltipTrigger>
-              <TooltipContent>
-                {project.tvlBreakdown.label}
-                {project.tvlBreakdown.warning && (
-                  <WarningBar
-                    className="mt-2"
-                    text={project.tvlBreakdown.warning}
-                    icon={RoundedWarningIcon}
-                    color={
-                      project.tvlBreakdown.warningSeverity === 'warning'
-                        ? 'yellow'
-                        : 'red'
-                    }
-                  />
-                )}
-              </TooltipContent>
-            </Tooltip>
-          ) : null}
-        </div>
+              ) : null}
+            </div>
+          </TooltipTrigger>
+          <TooltipContent>
+            <div className="space-y-2">
+              {project.tvlBreakdown ? project.tvlBreakdown.label : null}
+              {project.tvlWarnings?.map((warning, i) => (
+                <WarningBar
+                  key={`tvl-warning-${i}`}
+                  icon={RoundedWarningIcon}
+                  text={warning.content}
+                  color={warning.sentiment === 'warning' ? 'yellow' : 'red'}
+                />
+              ))}
+            </div>
+          </TooltipContent>
+        </Tooltip>
       ) : (
         <Badge type="gray" className="mx-auto translate-x-[11.5px]">
           Coming soon
