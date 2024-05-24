52c60eebc2d028dfb3ea431e55361b6545654167
Piotr Adamczyk <xeno@xeno.yt>
2024-05-24 15:27:07 +0200

    Bring back FLAGS_SECRET

--- packages/frontend2/src/app/.well-known/vercel/flags/route.ts
+++ packages/frontend2/src/app/.well-known/vercel/flags/route.ts
@@ -2,6 +2,9 @@ import { type ApiData, verifyAccess } from '@vercel/flags'
 import { type NextRequest, NextResponse } from 'next/server'
 
 export async function GET(request: NextRequest) {
+  const access = await verifyAccess(request.headers.get('Authorization'))
+  if (!access) return NextResponse.json(null, { status: 401 })
+
   return NextResponse.json<ApiData>({
     definitions: {
       'asset-risks': {
