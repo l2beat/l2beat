6a9bd94fb9fa65cd6f5c397b93b2d3e3d3529e80
Piotr Adamczyk <xeno@xeno.yt>
2024-05-24 15:29:18 +0200

    Rename `enableAssetRisks` to `showAssetRisks`

--- packages/frontend2/src/app/asset-risks/page.tsx
+++ packages/frontend2/src/app/asset-risks/page.tsx
@@ -1,11 +1,11 @@
 import { notFound } from 'next/navigation'
-import { enableAssetRisks } from '~/flags'
+import { showAssetRisks } from '~/flags'
 
 // Needed for the flag override without middleware
 export const dynamic = 'force-dynamic'
 
 export default async function Page() {
-  const enabled = await enableAssetRisks()
+  const enabled = await showAssetRisks()
 
   if (!enabled) {
     return notFound()
