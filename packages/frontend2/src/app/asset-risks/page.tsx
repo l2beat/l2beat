c5115631b884aaaf3daac3fd45bf2e007be7e613
Piotr Adamczyk <xeno@xeno.yt>
2024-05-24 15:03:43 +0200

    Add force-dynamic to PoC page

--- packages/frontend2/src/app/asset-risks/page.tsx
+++ packages/frontend2/src/app/asset-risks/page.tsx
@@ -1,6 +1,9 @@
 import { notFound } from 'next/navigation'
 import { enableAssetRisks } from '~/flags'
 
+// Needed for the flag override without middleware
+export const dynamic = 'force-dynamic'
+
 export default async function Page() {
   const enabled = await enableAssetRisks()
 
