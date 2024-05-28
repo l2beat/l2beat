73cf34ec9dcbf279dcdc2c72969190db910a3d86
vincfurc <10850139+vincfurc@users.noreply.github.com>
2024-05-28 09:49:10 +0100

    Taiko add escrow and update description (#3903)

--- packages/config/src/projects/layer2s/taiko.ts
+++ packages/config/src/projects/layer2s/taiko.ts
@@ -8,7 +8,7 @@ export const taiko: Layer2 = underReviewL2({
     name: 'Taiko',
     slug: 'taiko',
     description:
-      'Taiko is a decentralized, Ethereum-equivalent ZK Rollup that has been developed on the Ethereum network. At present, Taiko is deployed on the Sepolia testnet for further testing and optimization.',
+      'Taiko is an Ethereum-equivalent ZK Rollup on the Ethereum network. Taiko combines based sequencing and a contestation mechanism with multi-proofs.',
     purposes: ['Universal'],
     category: 'ZK Rollup',
     links: {
@@ -24,12 +24,37 @@ export const taiko: Layer2 = underReviewL2({
       ],
       rollupCodes: 'https://rollup.codes/taiko',
     },
+    activityDataSource: 'Blockchain RPC',
   },
+  associatedTokens: ['TKO'],
   escrows: [
     {
+      // ETH bridge
       address: EthereumAddress('0xd60247c6848B7Ca29eDdF63AA924E53dB6Ddd8EC'),
+      sinceTimestamp: new UnixTime(1714550603),
+      tokens: ['ETH'],
+    },
+    {
+      // ERC20 vault
+      address: EthereumAddress('0x996282cA11E5DEb6B5D122CC3B9A1FcAAD4415Ab'),
       sinceTimestamp: new UnixTime(1714550603),
       tokens: '*',
     },
   ],
+  transactionApi: {
+    type: 'rpc',
+    defaultUrl: 'https://rpc.mainnet.taiko.xyz',
+    defaultCallsPerMinute: 500,
+    startBlock: 1,
+  },
+  chainConfig: {
+    name: 'taiko',
+    chainId: 167000,
+    explorerUrl: 'https://taikoscan.io',
+    explorerApi: {
+      url: 'https://taikoscan.io/api',
+      type: 'etherscan',
+    },
+    minTimestampForTvl: new UnixTime(1716620627),
+  },
 })
