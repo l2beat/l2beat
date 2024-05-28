c8f17124245c3a9389cbf8ceb56e7f60a5cb926a
Piotr Adamczyk <xeno@xeno.yt>
2024-05-28 13:46:01 +0200

    Fix frontend2 build

--- packages/frontend2/src/app/_components/out-link.tsx
+++ packages/frontend2/src/app/_components/out-link.tsx
@@ -1,5 +1,5 @@
 import React, { type ComponentProps } from 'react'
-import { CustomLink } from './CustomLink'
+import { CustomLink } from './custom-link'
 
 export function OutLink(props: ComponentProps<typeof CustomLink>) {
   return <CustomLink target="_blank" rel="noreferrer noopener" {...props} />
