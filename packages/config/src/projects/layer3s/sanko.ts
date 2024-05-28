02dc831360c6c1dc07a51c34e937c50dc3210054
sekuba <sekuba@users.noreply.github.com>
2024-05-28 12:49:30 +0200

    Add sanko chain full (#3899)
    Co-authored-by: Luca Donno <30298476+lucadonnoh@users.noreply.github.com>

---
+++ packages/config/src/projects/layer3s/sanko.ts
@@ -0,0 +1,66 @@
+import { EthereumAddress, ProjectId, UnixTime } from '@l2beat/shared-pure'
+import { ProjectDiscovery } from '../../discovery/ProjectDiscovery'
+import { orbitStackL3 } from '../layer2s/templates/orbitStack'
+import { Layer3 } from './types'
+
+const discovery = new ProjectDiscovery('sanko', 'arbitrum')
+
+export const sanko: Layer3 = orbitStackL3({
+  discovery,
+  hostChain: ProjectId('arbitrum'),
+  display: {
+    name: 'Sanko',
+    slug: 'sanko',
+    redWarning:
+      'Critical contracts can be upgraded by an EOA which could result in the loss of all funds.',
+    description:
+      'Sanko is an NFT and gaming-focused Orbit stack L3 on Arbitrum with AnyTrust DA and DMT as its native token, created by Sanko GameCorp.',
+    purposes: ['Gaming', 'NFT'],
+    links: {
+      websites: ['https://sanko.xyz/'],
+      apps: ['https://sanko.xyz/bridge'],
+      documentation: ['https://sanko-1.gitbook.io/sanko-mainnet-docs/'],
+      explorers: ['https://sanko-mainnet.calderaexplorer.xyz/'],
+      repositories: [],
+      socialMedia: [
+        'https://x.com/SankoGameCorp',
+        'https://discord.gg/Cnz62Vfa2C',
+        'https://t.me/sankogamecorp',
+      ],
+    },
+  },
+  nativeToken: 'DMT',
+  associatedTokens: ['DMT'],
+  rpcUrl: 'https://sanko.calderachain.xyz/http',
+  bridge: discovery.getContract('Bridge'),
+  rollupProxy: discovery.getContract('RollupProxy'),
+  sequencerInbox: discovery.getContract('SequencerInbox'),
+  nonTemplateEscrows: [
+    discovery.getEscrowDetails({
+      address: EthereumAddress('0xb4951c0C41CFceB0D195A95FE66280457A80a990'),
+      sinceTimestamp: new UnixTime(1712958611),
+      tokens: '*',
+    }),
+  ],
+  nonTemplatePermissions: [
+    {
+      name: 'Sanko Admin EOA',
+      chain: 'arbitrum',
+      accounts: [
+        {
+          address: discovery.getAccessControlField(
+            'UpgradeExecutor',
+            'EXECUTOR_ROLE',
+          ).members[0],
+          type: 'EOA',
+        },
+      ],
+      description:
+        "EOA address that can upgrade the rollup's smart contract system (via UpgradeExecutor) and gain access to all funds.",
+    },
+    ...discovery.getMultisigPermission(
+      'Caldera Multisig',
+      'Can execute upgrades via the UpgradeExecutor.',
+    ),
+  ],
+})
