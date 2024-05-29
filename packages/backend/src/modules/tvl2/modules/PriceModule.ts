46c792d3d31fb911796bfb593d3bb124de59ab6e
antooni <antoni.pawlak@protonmail.com>
2024-05-29 09:22:05 +0200

    decode using zod

--- packages/backend/src/modules/tvl2/modules/PriceModule.ts
+++ packages/backend/src/modules/tvl2/modules/PriceModule.ts
@@ -1,11 +1,6 @@
 import { Logger } from '@l2beat/backend-tools'
 import { CoingeckoClient, CoingeckoQueryService } from '@l2beat/shared'
-import {
-  CoingeckoId,
-  CoingeckoPriceConfigEntry,
-  EthereumAddress,
-  UnixTime,
-} from '@l2beat/shared-pure'
+import { CoingeckoId, CoingeckoPriceConfigEntry } from '@l2beat/shared-pure'
 import { groupBy } from 'lodash'
 
 import { Tvl2Config } from '../../../config/Config'
@@ -101,25 +96,6 @@ function encode(value: CoingeckoPriceConfigEntry): string {
   })
 }
 
-// TODO: validate the config with zod
 function decode(value: string): CoingeckoPriceConfigEntry {
-  const obj = JSON.parse(value) as {
-    address: string
-    chain: string
-    sinceTimestamp: number
-    untilTimestamp?: number
-    type: string
-    coingeckoId: string
-  }
-
-  return {
-    address: obj.address === 'native' ? 'native' : EthereumAddress(obj.address),
-    chain: obj.chain,
-    sinceTimestamp: new UnixTime(obj.sinceTimestamp),
-    ...({
-      untilTimestamp: obj.untilTimestamp && new UnixTime(obj.untilTimestamp),
-    } ?? {}),
-    type: obj.type,
-    coingeckoId: CoingeckoId(obj.coingeckoId),
-  } as CoingeckoPriceConfigEntry
+  return CoingeckoPriceConfigEntry.parse(JSON.parse(value))
 }
