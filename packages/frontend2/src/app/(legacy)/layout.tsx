d6477ee02718c4451bc516db01f2730bca07c207
torztomasz <tomasz.torz@l2beat.com>
2024-05-29 13:03:00 +0200

    use padding instead margin for main

--- packages/frontend2/src/app/(legacy)/layout.tsx
+++ packages/frontend2/src/app/(legacy)/layout.tsx
@@ -4,7 +4,7 @@ import { NavLayout } from '../_components/nav/nav-layout'
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <NavLayout logoLink="/" legacyNav>
-      <main className="min-h-screen mb-20">{children}</main>
+      <main className="min-h-screen pb-20">{children}</main>
       <Footer />
     </NavLayout>
   )
