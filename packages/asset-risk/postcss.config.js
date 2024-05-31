078aba228a10612b1375029b14a45e8a8ba36bcb
maciekzygmunt <maciekzygmunt@interia.pl>
2024-05-31 14:15:12 +0300

    remove asset-risks

--- packages/asset-risk/postcss.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-  },
-}
-
-// biome-ignore lint/style/noDefaultExport: this is a config file
-export default config
