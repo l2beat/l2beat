40fed533aa61202332c16532d591b11b684d2221
antooni <antoni.pawlak@protonmail.com>
2024-05-31 11:04:28 +0200

    move to syncOptimizer

--- packages/backend/src/modules/tvl2/utils/SyncOptimizer.test.ts
+++ packages/backend/src/modules/tvl2/utils/SyncOptimizer.test.ts
@@ -5,6 +5,29 @@ import { Clock } from '../../../tools/Clock'
 import { SyncOptimizer } from './SyncOptimizer'
 
 describe(SyncOptimizer.name, () => {
+  describe(SyncOptimizer.prototype.shouldTimestampBeSynced.name, () => {
+    const now = UnixTime.fromDate(new Date('2023-05-01T00:00:00Z'))
+
+    const clock = mockObject<Clock>({
+      getLastHour: () => now,
+    })
+
+    const syncOptimizer = new SyncOptimizer(clock, {
+      removeHourlyAfterDays: 10,
+      removeSixHourlyAfterDays: 93,
+    })
+    it('return true if timestamp should be synced', () => {
+      const result = syncOptimizer.shouldTimestampBeSynced(now.add(-1, 'hours'))
+      expect(result).toEqual(true)
+    })
+
+    it('return false if timestamp should not be synced', () => {
+      const result = syncOptimizer.shouldTimestampBeSynced(
+        now.add(365, 'days').add(-1, 'hours'),
+      )
+      expect(result).toEqual(true)
+    })
+  })
   describe(SyncOptimizer.prototype.getTimestampToSync.name, () => {
     const MIN_TIMESTAMP = UnixTime.fromDate(new Date('2023-05-01T01:01:01Z'))
     const SIX_HOURLY_CUTOFF = 10
@@ -68,27 +91,61 @@ describe(SyncOptimizer.name, () => {
     })
   })
 
-  describe(SyncOptimizer.prototype.shouldTimestampBeSynced.name, () => {
-    const now = UnixTime.fromDate(new Date('2023-05-01T00:00:00Z'))
-
-    const clock = mockObject<Clock>({
-      getLastHour: () => now,
+  describe(SyncOptimizer.prototype.getTimestampsToSync.name, () => {
+    const MIN_TIMESTAMP = UnixTime.fromDate(new Date('2023-05-01T00:00:00Z'))
+    const SIX_HOURLY_CUTOFF = 10
+    const DAILY_CUTOFF = 93
+    const OPTIONS = {
+      removeHourlyAfterDays: SIX_HOURLY_CUTOFF,
+      removeSixHourlyAfterDays: DAILY_CUTOFF,
+    }
+    const LAST_HOUR = MIN_TIMESTAMP.add(365, 'days')
+    const CLOCK = mockObject<Clock>({
+      getLastHour: () => LAST_HOUR,
     })
 
-    const syncOptimizer = new SyncOptimizer(clock, {
-      removeHourlyAfterDays: 10,
-      removeSixHourlyAfterDays: 93,
-    })
-    it('return true if timestamp should be synced', () => {
-      const result = syncOptimizer.shouldTimestampBeSynced(now.add(-1, 'hours'))
-      expect(result).toEqual(true)
-    })
+    it('only daily timestamps range', () => {
+      const syncOptimizer = new SyncOptimizer(CLOCK, OPTIONS)
 
-    it('return false if timestamp should not be synced', () => {
-      const result = syncOptimizer.shouldTimestampBeSynced(
-        now.add(365, 'days').add(-1, 'hours'),
+      // hourly timestamp older than daily cutoff
+      const start = LAST_HOUR.add(-(DAILY_CUTOFF + 7), 'days')
+      const timestampToSync = syncOptimizer.getTimestampsToSync(
+        start.toNumber(),
+        start.add(365, 'days').toNumber(),
+        7,
       )
-      expect(result).toEqual(true)
+
+      expect(timestampToSync).toEqual([
+        start,
+        start.add(1, 'days'),
+        start.add(2, 'days'),
+        start.add(3, 'days'),
+        start.add(4, 'days'),
+        start.add(5, 'days'),
+        start.add(6, 'days'),
+      ])
+    })
+
+    it('only sixHourly timestamps range', () => {
+      const syncOptimizer = new SyncOptimizer(CLOCK, OPTIONS)
+
+      // hourly timestamp older than daily cutoff
+      const start = LAST_HOUR.add(-(SIX_HOURLY_CUTOFF + 7), 'days')
+      const timestampToSync = syncOptimizer.getTimestampsToSync(
+        start.toNumber(),
+        start.add(365, 'days').toNumber(),
+        7,
+      )
+
+      expect(timestampToSync).toEqual([
+        start,
+        start.add(6, 'hours'),
+        start.add(12, 'hours'),
+        start.add(18, 'hours'),
+        start.add(24, 'hours'),
+        start.add(30, 'hours'),
+        start.add(36, 'hours'),
+      ])
     })
   })
 })
