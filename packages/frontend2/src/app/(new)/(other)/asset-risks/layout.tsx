e1355e112207799066896d7f126563c13fbd513a
Piotr Adamczyk <xeno@xeno.yt>
2024-06-03 11:16:43 +0200

    Use asset risks feature flag in respective layout

--- packages/frontend2/src/app/(new)/(other)/asset-risks/layout.tsx
+++ packages/frontend2/src/app/(new)/(other)/asset-risks/layout.tsx
@@ -1,11 +1,21 @@
 import { type Metadata } from 'next'
 import { Web3Provider } from './_components/web3-provider'
+import { showAssetRisks } from '~/flags'
+import { notFound } from 'next/navigation'
 
 export const metadata: Metadata = {
   title: 'L2BEAT – Asset Risks',
   description: 'Get your asset risks report for your L2 assets.',
 }
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({
+  children,
+}: { children: React.ReactNode }) {
+  const enabled = await showAssetRisks()
+
+  if (!enabled) {
+    return notFound()
+  }
+
   return <Web3Provider>{children}</Web3Provider>
 }
