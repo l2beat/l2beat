21ace83d3c1e0cf39226f1b43abae1ffdb065ba5
torztomasz <tomasz.torz@l2beat.com>
2024-06-03 14:59:57 +0200

    fix wrapper

--- packages/frontend/src/components/ExcludeAssociatedTokensWrapper.tsx
+++ packages/frontend/src/components/ExcludeAssociatedTokensWrapper.tsx
@@ -16,7 +16,7 @@ ExcludeAssociatedTokensWrapper.TokensIncludedView = function IncludedView({
   children,
 }: Props) {
   return (
-    <div className="group-data-[data-associated-tokens-excluded]:hidden">
+    <div className="group-data-[associated-tokens-excluded]:hidden">
       {children}
     </div>
   )
@@ -26,7 +26,7 @@ ExcludeAssociatedTokensWrapper.TokensExcludedView = function ExcludedView({
   children,
 }: Props) {
   return (
-    <div className="hidden group-data-[data-associated-tokens-excluded]:inline">
+    <div className="hidden group-data-[associated-tokens-excluded]:inline">
       {children}
     </div>
   )
