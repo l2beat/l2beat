a076e48e8bc06e6126883200e20d496edeaf5574
torztomasz <tomasz.torz@l2beat.com>
2024-06-03 10:04:57 +0200

    intitial page with metadata

---
+++ packages/frontend2/src/app/(legacy)/glossary/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Glossary - L2BEAT',
+  description: "A glossary of terms for Ethereum's Layer 2 ecosystem",
+  openGraph: {
+    url: '/glossary',
+  },
+}
+
+export default function Page() {
+  return <div></div>
+}
