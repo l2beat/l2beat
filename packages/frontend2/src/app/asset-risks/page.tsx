264bb5f72d568e9399a823bde6ec7eb15895dea1
maciekzygmunt <maciekzygmunt@interia.pl>
2024-06-04 10:35:27 +0200

    remove navbar

--- packages/frontend2/src/app/asset-risks/page.tsx
+++ packages/frontend2/src/app/asset-risks/page.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import AssetRiskLogo from './_assets/asset-risk-logo.svg?url'
 import { AddressForm } from './_components/address-form'
+import { Footer } from './_components/footer'
 
 export default function AssetsRiskPage() {
   return (
-    <main className="md:pt-6 md:pr-6 flex bg-[#E6E7EC] flex-col justify-between pt-4 pr-4 min-h-screen">
-      <div className="flex flex-col rounded-t-xl flex-1 min-h-[calc(100vh-120px)] px-5 bg-white dark:bg-zinc-900 justify-center items-center">
+    <main className="md:p-6 flex bg-[#E6E7EC] flex-col justify-between p-4 min-h-screen">
+      <div className="flex flex-col rounded-xl flex-1 min-h-[calc(100vh-120px)] px-5 bg-white dark:bg-zinc-900 justify-center items-center">
         <div className="flex flex-col gap-14 items-center w-[min(77vw,400px)] min-w-[260px]">
           <Image
             src={AssetRiskLogo}
@@ -17,6 +18,7 @@ export default function AssetsRiskPage() {
           <AddressForm />
         </div>
       </div>
+      <Footer />
     </main>
   )
 }
