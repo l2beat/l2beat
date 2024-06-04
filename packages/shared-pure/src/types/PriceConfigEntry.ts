38ab828b457d6eb1eb5ef32f28ef7ecad5f6ab24
antooni <antoni.pawlak@protonmail.com>
2024-06-04 17:06:49 +0200

    get rid of zod types

--- packages/shared-pure/src/types/PriceConfigEntry.ts
+++ packages/shared-pure/src/types/PriceConfigEntry.ts
@@ -1,34 +1,25 @@
 /*
                 ====== IMPORTANT NOTICE ======
 DO NOT MODIFY THIS FILE WITHOUT MODIFYING THE "createPriceId" FUNCTION
-
-KEEP IN MIND THAT THIS TYPE WILL BE SERIALIZED AND STORED IN THE DATABASE
-WHEN ADDING A NEW FIELD MAKE SURE TO WRITE A DB MIGRATION OR MAKE FIELD OPTIONAL
 */
-import { z } from 'zod'
+
 import { AssetId } from './AssetId'
 import { CoingeckoId } from './CoingeckoId'
 import { EthereumAddress } from './EthereumAddress'
 import { UnixTime } from './UnixTime'
-import { branded, stringAs } from './branded'
-
-export const PriceConfigBase = z.object({
-  assetId: stringAs(AssetId),
-  address: z.union([stringAs(EthereumAddress), z.literal('native')]),
-  chain: z.string(),
-  sinceTimestamp: branded(z.number(), (n) => new UnixTime(n)),
-  untilTimestamp: branded(z.number().optional(), (n) =>
-    n !== undefined ? new UnixTime(n) : undefined,
-  ),
-})
-export type PriceConfigBase = z.infer<typeof PriceConfigBase>
-
-export const CoingeckoPriceConfigEntry = PriceConfigBase.extend({
-  type: z.literal('coingecko'),
-  coingeckoId: stringAs(CoingeckoId),
-})
-export type CoingeckoPriceConfigEntry = z.infer<
-  typeof CoingeckoPriceConfigEntry
->
 
 export type PriceConfigEntry = CoingeckoPriceConfigEntry
+
+interface PriceConfigBase {
+  // for backward compatibility
+  assetId: AssetId
+  address: EthereumAddress | 'native'
+  chain: string
+  sinceTimestamp: UnixTime
+  untilTimestamp?: UnixTime
+}
+
+export interface CoingeckoPriceConfigEntry extends PriceConfigBase {
+  type: 'coingecko'
+  coingeckoId: CoingeckoId
+}
