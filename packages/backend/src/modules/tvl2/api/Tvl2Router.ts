d7c732a9f7351fe1f8428200d85fb98f471e9406
antooni <antoni.pawlak@protonmail.com>
2024-06-04 09:35:59 +0200

    stringAsBoolean

--- packages/backend/src/modules/tvl2/api/Tvl2Router.ts
+++ packages/backend/src/modules/tvl2/api/Tvl2Router.ts
@@ -1,5 +1,10 @@
 import Router from '@koa/router'
-import { EthereumAddress, ProjectId, stringAs } from '@l2beat/shared-pure'
+import {
+  EthereumAddress,
+  ProjectId,
+  stringAs,
+  stringAsBoolean,
+} from '@l2beat/shared-pure'
 import { z } from 'zod'
 
 import { withTypedContext } from '../../../api/types'
@@ -14,13 +19,13 @@ export function createTvl2Router(controller: Tvl2Controller, clock: Clock) {
     withTypedContext(
       z.object({
         query: z.object({
-          excludeAssociatedTokens: z.string().optional(),
+          excludeAssociatedTokens: stringAsBoolean(false),
         }),
       }),
       async (ctx) => {
         // If this endpoint is too slow and aggregation layer is to be implemented,
         // remember to add "isAssociated" to createValueId.ts
-        if (ctx.query.excludeAssociatedTokens === 'true') {
+        if (ctx.query.excludeAssociatedTokens) {
           const excluded = await controller.getExcludedTvl(
             clock.getLastHour().add(-1, 'hours'),
           )
@@ -41,7 +46,7 @@ export function createTvl2Router(controller: Tvl2Controller, clock: Clock) {
       z.object({
         query: z.object({
           projectSlugs: z.string(),
-          excludeAssociatedTokens: z.string().optional(),
+          excludeAssociatedTokens: stringAsBoolean(false),
         }),
       }),
       async (ctx) => {
@@ -49,12 +54,10 @@ export function createTvl2Router(controller: Tvl2Controller, clock: Clock) {
           .split(',')
           .map((slug) => slug.trim())
 
-        const excludeAssociated = ctx.query.excludeAssociatedTokens === 'true'
-
         const tvl = await controller.getAggregatedTvl(
           clock.getLastHour().add(-1, 'hours'),
           projectSlugs,
-          excludeAssociated,
+          ctx.query.excludeAssociatedTokens,
         )
         ctx.body = tvl
       },
