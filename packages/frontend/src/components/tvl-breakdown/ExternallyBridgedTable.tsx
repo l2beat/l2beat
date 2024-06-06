8a48c656294802dfcac5f2f41000f42f9d5252f5
maciekzygmunt <maciekzygmunt@interia.pl>
2024-06-06 09:00:49 +0200

    hide tables

--- packages/frontend/src/components/tvl-breakdown/ExternallyBridgedTable.tsx
+++ packages/frontend/src/components/tvl-breakdown/ExternallyBridgedTable.tsx
@@ -14,7 +14,7 @@ export function ExternallyBridgedTable(props: ExternallyBridgedTableProps) {
     return acc + Number(token.usdValue)
   }, 0)
 
-  return (
+  return props.tokens.length === 0 ? null : (
     <div className="flex flex-col px-4 md:px-0">
       <h2 className="mb-3 ml-1 mt-12 text-xl font-bold md:mb-4 md:ml-2 md:text-2xl">
         Externally Bridged Value
