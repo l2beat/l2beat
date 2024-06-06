a101c2a386ad34f2c4200f51b6ece40fd22be3a5
maciekzygmunt <maciekzygmunt@interia.pl>
2024-06-06 09:20:30 +0200

    small refactor

--- packages/frontend/src/components/tvl-breakdown/NativelyMintedTable.tsx
+++ packages/frontend/src/components/tvl-breakdown/NativelyMintedTable.tsx
@@ -14,7 +14,11 @@ export function NativelyMintedTable(props: NativelyMintedTableProps) {
     return acc + Number(token.usdValue)
   }, 0)
 
-  return props.tokens.length === 0 ? null : (
+  if (props.tokens.length === 0) {
+    return null
+  }
+
+  return (
     <div className="flex flex-col px-4 md:px-0">
       <h2 className="mb-3 ml-1 mt-12 text-xl font-bold md:mb-4 md:ml-2 md:text-2xl">
         Natively Minted Value
