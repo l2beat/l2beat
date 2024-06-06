79b1fad1bc495a547e35565c733a03b33bb248a0
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2024-06-06 16:20:41 +0200

    Turn Kroma state validation red because not safe (#4058)

--- packages/config/src/projects/layer2s/kroma.ts
+++ packages/config/src/projects/layer2s/kroma.ts
@@ -210,7 +210,7 @@ export const kroma: Layer2 = {
       description:
         RISK_VIEW.STATE_FP_INT_ZK.description +
         " The challenge protocol can be subject to delay attacks and can fail under certain conditions. The current system doesn't use posted L2 txs batches on L1 as inputs to prove a fault, meaning that DA is not enforced.",
-      sentiment: 'warning',
+      sentiment: 'bad',
       secondLine: `${formatSeconds(finalizationPeriod)} challenge period`,
     },
     dataAvailability: {
