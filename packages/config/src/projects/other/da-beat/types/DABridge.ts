264f30af80bad3e1b67b582d06dd2b01b12320fd
Michał Podsiadły <michal.podsiadly@l2beat.com>
2024-06-06 13:43:57 +0200

    feat: further example typings

--- packages/config/src/projects/other/da-beat/types/DABridge.ts
+++ packages/config/src/projects/other/da-beat/types/DABridge.ts
@@ -1,141 +1,164 @@
-import { ChainId, EthereumAddress, ProjectId } from '@l2beat/shared-pure'
-import { ScalingProjectContractSingleAddress } from '../../../../common'
+import { ChainId, EthereumAddress, ProjectId } from '@l2beat/shared-pure';
+import { ScalingProjectContractSingleAddress } from '../../../../common';
 
-export type DABridge = {
-  /**
-   * The name of the data availability bridge
-   */
-  name: string
+export type DABridge = NoDABridge | OnChainDABridge;
 
+export type NoDABridge = CommonDABridge & {
+  type: 'NoBridge';
+};
+
+export type OnChainDABridge = CommonDABridge & {
+  type: 'OnChain';
   /**
    * The chain ID of the data availability bridge
    */
-  chain: ChainId
-
-  /**
-   * Description of the data availability bridge
-   */
-  description: string
-
-  /**
-   * List of projects given bridge is being used in
-   */
-  usedIn: ProjectId[]
+  chain: ChainId;
 
   /**
    * Data about related permissions - preferably from discovery
    */
-  permissions: Permissions[]
+  permissions: Permissions[];
 
   /**
    * Data about the contracts used in the bridge - preferably from discovery
    */
-  contracts: ScalingProjectContractSingleAddress
+  contracts: ScalingProjectContractSingleAddress;
 
   /**
    * Risks related to given data availability bridge
    */
-  risks: DABridgeRisks
+  risks: DABridgeRisks;
 
   /**
    * Exit window - TBD
    * @unit seconds
    */
-  exitWindow: ExitWindow
+  exitWindow: ExitWindow;
 
   /**
    * Accessability - TBD
    */
-  accessability: Accessability
-}
+  accessability: Accessability;
+};
+
+type CommonDABridge = {
+  display: {
+    /**
+     * The name of the data availability bridge
+     */
+    name: string;
+
+    /**
+     * Slug of the data availability bridge
+     */
+    slug: string;
+
+    /**
+     * Description of the data availability bridge
+     */
+    description: string;
+
+    /**
+     * The logo of the data availability bridge.
+     * Will be resolved automatically by the frontend.
+     * @override if the logo has different name
+     */
+    logo?: string;
+  };
+
+  /**
+   * List of projects given bridge is being used in
+   */
+  usedIn: ProjectId[];
+};
 
 type DABridgeRisks = {
-  attestations: AttestationSecurity & { description: string }
-}
+  attestations: AttestationSecurity & { description: string };
+};
 
 type AttestationSecurity =
   | {
-      type: 'SigVerified'
+      type: 'SigVerified';
     }
   | {
-      type: 'SigVerifiedZK'
+      type: 'SigVerifiedZK';
     }
   | {
-      type: 'SigVerifiedSignersNotTracked'
+      type: 'SigVerifiedSignersNotTracked';
     }
   | {
-      type: 'NotSatisfied'
+      type: 'NotSatisfied';
     }
   | {
-      type: 'NoBridge'
+      type: 'NoBridge';
     }
   | {
-      type: 'CommitmentFrequencySatisfied'
-    }
+      type: 'CommitmentFrequencySatisfied';
+    };
 
 type ExitWindow =
   | {
-      type: 'ImmutableOrSCWithDelay'
+      type: 'ImmutableOrSCWithDelay';
     }
   | {
-      type: 'EOAWithDelay'
+      type: 'EOAWithDelay';
     }
   | {
-      type: 'SCInsufficientDelay'
+      type: 'SCInsufficientDelay';
     }
   | {
-      type: 'NoDelay'
+      type: 'NoDelay';
     }
   | {
-      type: 'NoBridge'
-    }
+      type: 'NoBridge';
+    };
 
 type Accessability =
   | {
-      type: 'Enshrined'
+      type: 'Enshrined';
     }
-  | { type: 'NotEnshrined' }
+  | { type: 'NotEnshrined' };
 
 type Permissions = {
   /**
    * List of the accounts
    */
-  accounts: PermissionedAccount[]
+  accounts: PermissionedAccount[];
 
   /**
    * Name of this group
    */
-  name: string
+  name: string;
 
   /**
    * Description of the permissions
    */
-  description: string
+  description: string;
 
   /**
    * Name of the chain of these addresses. Optional for backwards compatibility
    */
-  chain?: string
+  chain?: string;
 
   /**
    * List of source code permalinks and useful materials
    */
-  references?: Reference[]
-}
+  references?: Reference[];
+};
 
 interface PermissionedAccount {
-  address: EthereumAddress
-  type: 'EOA' | 'MultiSig' | 'Contract'
+  address: EthereumAddress;
+  type: 'EOA' | 'MultiSig' | 'Contract';
 }
 
 interface Reference {
   /**
    * Short text describing link contents
    */
-  text: string
+  text: string;
 
   /**
    * URL of the link, preferably https
    */
-  href: string
+  href: string;
 }
--- packages/config/src/projects/other/da-beat/types/DALayer.ts
+++ packages/config/src/projects/other/da-beat/types/DALayer.ts
@@ -1,70 +1,85 @@
-import { DABridge } from './DABridge'
+import { ProjectId } from '@l2beat/shared-pure';
+import { DABridge } from './DABridge';
 
-export type DALayerType = 'Public blockchain' | 'DAC'
+export type DALayerType = 'Public blockchain' | 'DAC';
 
 export type DALayer = {
-  /**
-   * The name of the data availability layer.
-   */
-  name: string
+  display: {
+    /**
+     * The name of the data availability layer.
+     */
+    name: string;
+
+    /**
+     * Slug of the data availability bridge
+     */
+    slug: string;
+
+    /**
+     * A short description of the data availability layer.
+     */
+    description: string;
+
+    /**
+     * The logo of the data availability layer.
+     * Will be resolved automatically by the frontend.
+     * @override if the logo has different name
+     */
+    logo?: string;
+  };
+
+  bridges: DABridge[];
 
   /**
-   * A short description of the data availability layer.
+   * List of projects given da layer is being used in
    */
-  description: string
-
-  /**
-   * The logo of the data availability layer.
-   */
-  logo: string
-
-  daBridges: DABridge[]
+  usedIn: ProjectId[];
 
   /**
    * The type of the data availability layer.
    * @see DALayerType
    */
-  type: DALayerType
+  type: DALayerType;
 
   /**
    * The duration of the data storage.
    * @unit seconds
    */
-  storageDuration: number
+  storageDuration: number;
 
   /**
    * Risks associated with the data availability layer.
    * @see DALayerRisks
    */
-  risks: DALayerRisks
-}
+  risks: DALayerRisks;
+};
 
 export type DALayerRisks = {
-  economicSecurity: EconomicSecurityRisk
-  fraudDetection: FraudDetectionRisk
-}
+  economicSecurity: EconomicSecurityRisk;
+  fraudDetection: FraudDetectionRisk;
+};
 
 export type EconomicSecurityRisk =
   | {
-      type: 'EconomicSecurity'
+      type: 'OnchainQuantifiable';
     }
   | {
-      type: 'OffchainVerifiable'
+      type: 'OffchainVerifiable';
     }
   | {
-      type: 'Unknown'
-    }
+      type: 'Unknown';
+    };
 
 export type FraudDetectionRisk =
   | {
-      type: 'DAS with block reconstruction'
+      type: 'DAS with block reconstruction';
     }
   | {
-      type: 'DAS with no block reconstruction'
+      type: 'DAS with no block reconstruction';
     }
   | {
-      type: 'Erasure coding proof'
+      type: 'Erasure coding proof';
     }
   | {
-      type: 'No fraud detection'
-    }
+      type: 'No fraud detection';
+    };
