aa241aa881206a1e2d96720717d4d44cc3cceb1c
torztomasz <tomasz.torz@l2beat.com>
2024-06-06 13:09:35 +0200

    add documentation

--- packages/frontend2/src/app/utils/merge-refs.tsx
+++ packages/frontend2/src/app/utils/merge-refs.tsx
@@ -1,3 +1,16 @@
+/**
+ * A function that merges React refs into one.
+ * Supports both functions and ref objects created using createRef() and useRef().
+ *
+ * Usage:
+ * ```tsx
+ * <div ref={mergeRefs(ref1, ref2, ref3)} />
+ * ```
+ *
+ * @param {(React.Ref<T> | undefined)[]} inputRefs Array of refs
+ * @returns {React.Ref<T> | React.RefCallback<T>} Merged refs
+ */
+
 export function mergeRefs<T>(
   ...inputRefs: (React.Ref<T> | undefined)[]
 ): React.Ref<T> | React.RefCallback<T> {
