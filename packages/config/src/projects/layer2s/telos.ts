28287da4ae69f5ae3ea7980c88031a879cc71bce
Hubert Kotliński <75430263+Huberacht@users.noreply.github.com>
2024-06-07 09:52:24 +0000

    Added Telos as upcoming project

---
+++ packages/config/src/projects/layer2s/telos.ts
@@ -0,0 +1,26 @@
+import { upcomingL2 } from './templates/upcoming'
+import { Layer2 } from './types'
+
+export const telos: Layer2 = upcomingL2({
+  id: 'telos',
+  display: {
+    name: 'Telos',
+    slug: 'telos',
+    description:
+      'Telos L2 will leverage the SNARKtor decentralized protocol for scalable SNARKs verification unlocking massively scalable users data protecting applications, regulator compliance and trustless interoperability.',
+    purposes: ['Universal'],
+    category: 'ZK Rollup',
+    links: {
+      websites: ['https://zkevm.telos.net/'],
+      apps: [],
+      documentation: ['https://docs.telos.net/overview/what-is-telos/introduction/'],
+      explorers: [],
+      repositories: ['https://github.com/telosnetwork'],
+      socialMedia: [
+        'https://x.com/HelloTelos',
+        'https://discord.com/invite/telos',
+        'https://www.youtube.com/@TheTelosNetwork',
+      ],
+    },
+  },
+})
