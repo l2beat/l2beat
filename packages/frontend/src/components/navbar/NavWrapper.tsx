8c25ab339b06bc10bd3b65102a4fc8508b59e7f3
Piotr Adamczyk <xeno@xeno.yt>
2024-06-10 12:48:33 +0200

    Fix formatting & lint

--- packages/frontend/src/components/navbar/NavWrapper.tsx
+++ packages/frontend/src/components/navbar/NavWrapper.tsx
@@ -1,8 +1,9 @@
-import React, { ReactNode, SVGAttributes } from "react";
-import { usePageBuildContext } from "../../build/pageBuildContext";
-import { cn } from "../../utils/cn";
-import { Logo } from "../Logo";
-import { LogoSmall } from "../LogoSmall";
+import React, { ReactNode, SVGAttributes } from 'react'
+import { usePageBuildContext } from '../../build/pageBuildContext'
+import { cn } from '../../utils/cn'
+import { Logo } from '../Logo'
+import { LogoSmall } from '../LogoSmall'
+import { OverflowWrapper } from '../OverflowWrapper'
 import {
   ActivityIcon,
   MenuCloseIcon,
@@ -10,23 +11,22 @@ import {
   RiskIcon,
   SummaryIcon,
   TvlIcon,
-} from "../icons";
-import { Icon } from "../icons/Icon";
-import { CostsIcon } from "../icons/pages/CostsIcon";
-import { DataAvailabilityIcon } from "../icons/pages/DataAvailabilityIcon";
-import { FinalityIcon } from "../icons/pages/FinalityIcon";
-import { LivenessIcon } from "../icons/pages/LivenessIcon";
-import { DarkThemeToggle } from "./DarkThemeToggle";
-import { HiringBadge } from "./HiringBadge";
-import { SocialLinks } from "./SocialLinks";
-import { OverflowWrapper } from "../OverflowWrapper";
+} from '../icons'
+import { Icon } from '../icons/Icon'
+import { CostsIcon } from '../icons/pages/CostsIcon'
+import { DataAvailabilityIcon } from '../icons/pages/DataAvailabilityIcon'
+import { FinalityIcon } from '../icons/pages/FinalityIcon'
+import { LivenessIcon } from '../icons/pages/LivenessIcon'
+import { DarkThemeToggle } from './DarkThemeToggle'
+import { HiringBadge } from './HiringBadge'
+import { SocialLinks } from './SocialLinks'
 
 function NavLinkGroup({
   title,
   children,
 }: {
-  title: string;
-  children?: ReactNode;
+  title: string
+  children?: ReactNode
 }) {
   return (
     <div className="flex flex-col gap-2">
@@ -36,37 +36,37 @@ function NavLinkGroup({
       <div className="mt-[12px] hidden h-px w-8 bg-slate-600 xl:sidenav-collapsed:block dark:bg-zinc-500" />
       <ul className="flex flex-col gap-0.5">{children}</ul>
     </div>
-  );
+  )
 }
 
 function NavLink({
   icon: Icon,
   title,
   href,
-  activeBehavior = "exact",
+  activeBehavior = 'exact',
 }: {
   icon: (
-    props: React.SVGProps<SVGSVGElement> | SVGAttributes<SVGElement>
-  ) => JSX.Element;
-  title: string;
-  href: string;
-  activeBehavior?: "exact" | "prefix" | ((path: string) => boolean);
+    props: React.SVGProps<SVGSVGElement> | SVGAttributes<SVGElement>,
+  ) => JSX.Element
+  title: string
+  href: string
+  activeBehavior?: 'exact' | 'prefix' | ((path: string) => boolean)
 }) {
-  const { path } = usePageBuildContext();
+  const { path } = usePageBuildContext()
 
   const active =
-    activeBehavior === "exact"
+    activeBehavior === 'exact'
       ? path === href
-      : activeBehavior === "prefix"
+      : activeBehavior === 'prefix'
         ? path.startsWith(href)
-        : activeBehavior(path);
+        : activeBehavior(path)
 
   return (
     <a href={href}>
       <li
         className={cn(
-          "flex items-center gap-[0.625rem] rounded-[0.25rem] p-1.5 transition-colors duration-300 ease-out xl:sidenav-collapsed:h-8 xl:sidenav-collapsed:w-8 xl:sidenav-collapsed:justify-center dark:hover:bg-[#272A2F] hover:bg-[#F1D6FF] xl:sidenav-collapsed:p-0",
-          active && "bg-[#d3d5d9] dark:bg-[#393C43]"
+          'flex items-center gap-[0.625rem] rounded-[0.25rem] p-1.5 transition-colors duration-300 ease-out xl:sidenav-collapsed:h-8 xl:sidenav-collapsed:w-8 xl:sidenav-collapsed:justify-center dark:hover:bg-[#272A2F] hover:bg-[#F1D6FF] xl:sidenav-collapsed:p-0',
+          active && 'bg-[#d3d5d9] dark:bg-[#393C43]',
         )}
       >
         <Icon />
@@ -75,7 +75,7 @@ function NavLink({
         </span>
       </li>
     </a>
-  );
+  )
 }
 
 function NavSmallLinkGroup({ children }: { children: ReactNode }) {
@@ -83,49 +83,49 @@ function NavSmallLinkGroup({ children }: { children: ReactNode }) {
     <ul className="ml-1 flex flex-col gap-2 xl:sidenav-collapsed:hidden">
       {children}
     </ul>
-  );
+  )
 }
 
 function NavSmallLink({
   title,
   href,
   children,
-  activeBehavior = "exact",
+  activeBehavior = 'exact',
 }: {
-  href: string;
-  activeBehavior?: "exact" | "prefix" | ((path: string) => boolean);
+  href: string
+  activeBehavior?: 'exact' | 'prefix' | ((path: string) => boolean)
 } & (
   | { title: string; children?: undefined }
   | { children: ReactNode; title?: undefined }
 )) {
-  const { path } = usePageBuildContext();
+  const { path } = usePageBuildContext()
 
   const active =
-    activeBehavior === "exact"
+    activeBehavior === 'exact'
       ? path === href
-      : activeBehavior === "prefix"
+      : activeBehavior === 'prefix'
         ? path.startsWith(href)
-        : activeBehavior(path);
+        : activeBehavior(path)
 
   return (
-    <a href={href} target={href.startsWith("http") ? "_blank" : undefined}>
+    <a href={href} target={href.startsWith('http') ? '_blank' : undefined}>
       <li
         className={cn(
-          "font-medium text-xs text-zinc-800 leading-none transition-colors duration-300 ease-out dark:hover:text-gray-400 dark:text-white hover:text-zinc-500",
+          'font-medium text-xs text-zinc-800 leading-none transition-colors duration-300 ease-out dark:hover:text-gray-400 dark:text-white hover:text-zinc-500',
           active &&
-            "text-pink-900 dark:hover:text-pink-200 dark:text-pink-200 hover:text-pink-900"
+            'text-pink-900 dark:hover:text-pink-200 dark:text-pink-200 hover:text-pink-900',
         )}
       >
         {children ?? title}
       </li>
     </a>
-  );
+  )
 }
 
 function NavCollapseIcon({ className, ...props }: SVGAttributes<SVGElement>) {
   return (
     <Icon
-      className={cn("stroke-[#525C6A] dark:stroke-[#D3D5D9]", className)}
+      className={cn('stroke-[#525C6A] dark:stroke-[#D3D5D9]', className)}
       width="24"
       height="24"
       viewBox="0 0 24 24"
@@ -136,7 +136,7 @@ function NavCollapseIcon({ className, ...props }: SVGAttributes<SVGElement>) {
       <path d="M16.5 8L12.0633 12.4367C12.0284 12.4716 12.0284 12.5284 12.0633 12.5633L16.5 17" />
       <path d="M8 22L8 2" />
     </Icon>
-  );
+  )
 }
 
 /*
@@ -152,29 +152,29 @@ function NewItemBadge() {
 function MobileNavBarLink({
   title,
   href,
-  activeBehavior = "exact",
+  activeBehavior = 'exact',
 }: {
-  href: string;
-  activeBehavior?: "exact" | "prefix" | ((path: string) => boolean);
+  href: string
+  activeBehavior?: 'exact' | 'prefix' | ((path: string) => boolean)
 } & (
   | { title: string; children?: undefined }
   | { children: ReactNode; title?: undefined }
 )) {
-  const { path } = usePageBuildContext();
+  const { path } = usePageBuildContext()
 
   const active =
-    activeBehavior === "exact"
+    activeBehavior === 'exact'
       ? path === href
-      : activeBehavior === "prefix"
+      : activeBehavior === 'prefix'
         ? path.startsWith(href)
-        : activeBehavior(path);
+        : activeBehavior(path)
 
   return (
     <a href={href}>
       <li
         className={cn(
-          "relative flex h-full flex-col justify-center px-2 font-medium text-base md:px-4 md:text-lg",
-          active && "text-pink-900 dark:text-pink-200"
+          'relative flex h-full flex-col justify-center px-2 font-medium text-base md:px-4 md:text-lg',
+          active && 'text-pink-900 dark:text-pink-200',
         )}
       >
         {title}
@@ -183,34 +183,34 @@ function MobileNavBarLink({
         )}
       </li>
     </a>
-  );
+  )
 }
 
 type NavbarLinkGroups = {
-  title: string;
+  title: string
   links: {
-    title: string;
-    icon: (props: React.SVGProps<SVGSVGElement>) => React.JSX.Element;
-    href: string;
-    enabled: boolean;
-  }[];
-}[];
+    title: string
+    icon: (props: React.SVGProps<SVGSVGElement>) => React.JSX.Element
+    href: string
+    enabled: boolean
+  }[]
+}[]
 
 function getMainLink(path: string, links: NavbarLinkGroups) {
   return (
-    links.find((g) => path.startsWith(`/${g.links[0].href.split("/")[1]}`))
-      ?.links[0].href ?? "/"
-  );
+    links.find((g) => path.startsWith(`/${g.links[0].href.split('/')[1]}`))
+      ?.links[0].href ?? '/'
+  )
 }
 
 /**
  * Mobile navigation bar that is shown on the very top on small screens.
  */
 function MobileNavBar({ links }: { links: NavbarLinkGroups }) {
-  const { path } = usePageBuildContext();
+  const { path } = usePageBuildContext()
   const currentGroup = links.find((g) =>
-    path.startsWith(`/${g.links[0].href.split("/")[1]}`)
-  );
+    path.startsWith(`/${g.links[0].href.split('/')[1]}`),
+  )
   return (
     <div className="xl:hidden">
       <div className="relative flex h-16 flex-row items-stretch justify-between gap-8 border-gray-200 border-b px-3.5 dark:border-gray-850">
@@ -225,12 +225,12 @@ function MobileNavBar({ links }: { links: NavbarLinkGroups }) {
             <MobileNavBarLink
               title="Scaling"
               href="/scaling/summary"
-              activeBehavior={(path) => path.startsWith("/scaling")}
+              activeBehavior={(path) => path.startsWith('/scaling')}
             />
             <MobileNavBarLink
               title="Bridges"
               href="/bridges/summary"
-              activeBehavior={(path) => path.startsWith("/bridges")}
+              activeBehavior={(path) => path.startsWith('/bridges')}
             />
           </ul>
         </div>
@@ -250,11 +250,11 @@ function MobileNavBar({ links }: { links: NavbarLinkGroups }) {
                 <a href={link.href} key={link.href}>
                   <div
                     className={cn(
-                      "m-auto whitespace-nowrap rounded-[4px] border border-[#AB3BD2] px-4 py-[0.53125rem] font-semibold text-xs leading-none",
+                      'm-auto whitespace-nowrap rounded-[4px] border border-[#AB3BD2] px-4 py-[0.53125rem] font-semibold text-xs leading-none',
                       link.href === path &&
-                        "border-0 bg-[linear-gradient(90deg,_#7E41CC_0%,_#FF46C0_100%)] px-[calc(1rem_+_1px)] py-[calc(0.53125rem_+_1px)] text-white"
+                        'border-0 bg-[linear-gradient(90deg,_#7E41CC_0%,_#FF46C0_100%)] px-[calc(1rem_+_1px)] py-[calc(0.53125rem_+_1px)] text-white',
                     )}
-                    data-selected={link.href === path ? "true" : "false"}
+                    data-selected={link.href === path ? 'true' : 'false'}
                   >
                     {link.title}
                   </div>
@@ -264,52 +264,52 @@ function MobileNavBar({ links }: { links: NavbarLinkGroups }) {
         </OverflowWrapper>
       )}
     </div>
-  );
+  )
 }
 
 function Divider({ className }: { className?: string }) {
   return (
     <div
       className={cn(
-        "h-px w-full bg-gray-300 xl:sidenav-collapsed:hidden dark:bg-gray-850",
-        className
+        'h-px w-full bg-gray-300 xl:sidenav-collapsed:hidden dark:bg-gray-850',
+        className,
       )}
     />
-  );
+  )
 }
 
 export function Sidenav({
   links,
   legacyNav,
 }: {
-  links: NavbarLinkGroups;
-  legacyNav: boolean;
+  links: NavbarLinkGroups
+  legacyNav: boolean
 }) {
-  const { config, path } = usePageBuildContext();
+  const { config, path } = usePageBuildContext()
   const sharedSizeClasses = cn(
-    "h-[100dvh] h-screen w-full 2xl:w-[280px] xl:w-[240px]",
-    legacyNav && "xl:hidden xl:sidenav-collapsed:hidden"
-  );
+    'h-[100dvh] h-screen w-full 2xl:w-[280px] xl:w-[240px]',
+    legacyNav && 'xl:hidden xl:sidenav-collapsed:hidden',
+  )
 
   return (
     <div
       className={cn(
-        "group absolute z-999 flex flex-shrink-0 translate-x-full flex-col items-stretch transition-all duration-300 xl:static xl:sidenav-collapsed:w-20 data-[open=true]:translate-x-0 xl:transform-none",
-        sharedSizeClasses
+        'group absolute z-999 flex flex-shrink-0 translate-x-full flex-col items-stretch transition-all duration-300 xl:static xl:sidenav-collapsed:w-20 data-[open=true]:translate-x-0 xl:transform-none',
+        sharedSizeClasses,
       )}
       data-role="sidenav"
     >
       <div
         className={cn(
-          "flex flex-col overflow-x-clip bg-[#E6E7EC] transition-all xl:fixed xl:sidenav-collapsed:w-20 dark:border-gray-850 xl:dark:border-r dark:border-r-0 dark:bg-[#1E1C21]",
-          sharedSizeClasses
+          'flex flex-col overflow-x-clip bg-[#E6E7EC] transition-all xl:fixed xl:sidenav-collapsed:w-20 dark:border-gray-850 xl:dark:border-r dark:border-r-0 dark:bg-[#1E1C21]',
+          sharedSizeClasses,
         )}
         data-role="sidenav-inner"
       >
         <div
           className={cn(
-            "flex flex-1 flex-col gap-8 overflow-y-auto overflow-x-clip px-3.5 py-4 xl:px-6 xl:py-[1.125rem]",
-            sharedSizeClasses
+            'flex flex-1 flex-col gap-8 overflow-y-auto overflow-x-clip px-3.5 py-4 xl:px-6 xl:py-[1.125rem]',
+            sharedSizeClasses,
           )}
           data-role="sidenav-collapse-content"
         >
@@ -337,7 +337,7 @@ export function Sidenav({
                         icon={link.icon}
                         href={link.href}
                       />
-                    )
+                    ),
                 )}
               </NavLinkGroup>
             ))}
@@ -348,9 +348,9 @@ export function Sidenav({
               {config.features.zkCatalog && (
                 <NavSmallLink title="ZK Catalog" href="/zk-catalog" />
               )}
-              <NavSmallLink title="Donate" href={"/donate"} />
+              <NavSmallLink title="Donate" href={'/donate'} />
               {config.features.governancePage ? (
-                <NavSmallLink title="Governance" href={"/governance"} />
+                <NavSmallLink title="Governance" href={'/governance'} />
               ) : (
                 <NavSmallLink
                   title="Governance"
@@ -388,7 +388,7 @@ export function Sidenav({
         </div>
       </div>
     </div>
-  );
+  )
 }
 
 function LegacyNavBarLink({
@@ -400,33 +400,33 @@ function LegacyNavBarLink({
   | { title: string; children?: never }
   | { title?: never; children: React.ReactNode }
 )) {
-  const { path } = usePageBuildContext();
+  const { path } = usePageBuildContext()
   return (
     <li className="h-full">
       <a
         className={cn(
-          "flex h-full items-center font-medium",
-          large ? "px-2 text-base md:px-4 md:text-lg" : "px-2",
+          'flex h-full items-center font-medium',
+          large ? 'px-2 text-base md:px-4 md:text-lg' : 'px-2',
           path === href &&
-            "border-current border-b-2 pt-0.5 text-pink-900 dark:text-pink-200"
+            'border-current border-b-2 pt-0.5 text-pink-900 dark:text-pink-200',
         )}
         href={href}
-        target={href.startsWith("http") ? "_blank" : undefined}
+        target={href.startsWith('http') ? '_blank' : undefined}
       >
         {children || title}
       </a>
     </li>
-  );
+  )
 }
 
 function LegacyNavBar({ links }: { links: NavbarLinkGroups }) {
-  const { path, config } = usePageBuildContext();
+  const { path, config } = usePageBuildContext()
   return (
     <div className="hidden h-[4.25rem] border-gray-200 border-b text-base xl:block dark:border-gray-850">
       <nav className="relative mx-auto box-border flex h-full max-w-[1780px] items-center justify-between px-6">
         <ul className="flex items-center py-4">
           <li className="mr-8">
-            <a href={path.startsWith("/bridges") ? "/bridges/summary" : "/"}>
+            <a href={path.startsWith('/bridges') ? '/bridges/summary' : '/'}>
               <Logo className="h-8 w-auto" />
             </a>
           </li>
@@ -449,9 +449,9 @@ function LegacyNavBar({ links }: { links: NavbarLinkGroups }) {
             {config.features.zkCatalog && (
               <LegacyNavBarLink title="ZK Catalog" href="/zk-catalog" />
             )}
-            <LegacyNavBarLink title="Donate" href={"/donate"} />
+            <LegacyNavBarLink title="Donate" href={'/donate'} />
             {config.features.governancePage ? (
-              <LegacyNavBarLink title="Governance" href={"/governance"} />
+              <LegacyNavBarLink title="Governance" href={'/governance'} />
             ) : (
               <LegacyNavBarLink
                 title="Governance"
@@ -472,96 +472,96 @@ function LegacyNavBar({ links }: { links: NavbarLinkGroups }) {
         </div>
       </nav>
     </div>
-  );
+  )
 }
 
 export function NavWrapper({
   children,
   legacyNav,
 }: {
-  children: ReactNode;
-  legacyNav?: boolean;
+  children: ReactNode
+  legacyNav?: boolean
 }) {
-  const { config } = usePageBuildContext();
+  const { config } = usePageBuildContext()
 
   const groups: NavbarLinkGroups = [
     {
-      title: "Scaling",
+      title: 'Scaling',
       links: [
         {
-          title: "Summary",
+          title: 'Summary',
           icon: SummaryIcon,
-          href: "/scaling/summary",
+          href: '/scaling/summary',
           enabled: true,
         },
         {
-          title: "Value Locked",
+          title: 'Value Locked',
           icon: TvlIcon,
-          href: "/scaling/tvl",
+          href: '/scaling/tvl',
           enabled: true,
         },
         {
-          title: "Risk Analysis",
+          title: 'Risk Analysis',
           icon: RiskIcon,
-          href: "/scaling/risk",
+          href: '/scaling/risk',
           enabled: true,
         },
         {
-          title: "Data Availability",
+          title: 'Data Availability',
           icon: DataAvailabilityIcon,
-          href: "/scaling/data-availability",
+          href: '/scaling/data-availability',
           enabled: true,
         },
         {
-          title: "Liveness",
+          title: 'Liveness',
           icon: LivenessIcon,
-          href: "/scaling/liveness",
+          href: '/scaling/liveness',
           enabled: config.features.liveness,
         },
         {
-          title: "Finality",
+          title: 'Finality',
           icon: FinalityIcon,
-          href: "/scaling/finality",
+          href: '/scaling/finality',
           enabled: config.features.finality,
         },
         {
-          title: "Activity",
+          title: 'Activity',
           icon: ActivityIcon,
-          href: "/scaling/activity",
+          href: '/scaling/activity',
           enabled: config.features.activity,
         },
         {
-          title: "Costs",
+          title: 'Costs',
           icon: CostsIcon,
-          href: "/scaling/costs",
+          href: '/scaling/costs',
           enabled: config.features.costsPage,
         },
       ],
     },
     {
-      title: "Bridges",
+      title: 'Bridges',
       links: [
         {
-          title: "Summary",
+          title: 'Summary',
           icon: SummaryIcon,
-          href: "/bridges/summary",
+          href: '/bridges/summary',
           enabled: true,
         },
         {
-          title: "Risk Analysis",
+          title: 'Risk Analysis',
           icon: RiskIcon,
-          href: "/bridges/risk",
+          href: '/bridges/risk',
           enabled: true,
         },
       ],
     },
-  ];
+  ]
 
   return (
     <div
       className={cn(
-        "relative flex flex-col overflow-x-clip xl:flex-row",
-        legacyNav && "xl:flex-col"
+        'relative flex flex-col overflow-x-clip xl:flex-row',
+        legacyNav && 'xl:flex-col',
       )}
     >
       {!!legacyNav && <LegacyNavBar links={groups} />}
@@ -569,5 +569,5 @@ export function NavWrapper({
       <Sidenav links={groups} legacyNav={!!legacyNav} />
       <div className="flex-1 overflow-x-clip">{children}</div>
     </div>
-  );
+  )
 }
--- packages/frontend2/src/app/_components/nav/mobile-nav-tabs.tsx
+++ packages/frontend2/src/app/_components/nav/mobile-nav-tabs.tsx
@@ -1,19 +1,19 @@
-"use client";
+'use client'
 
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { cn } from "~/utils/cn";
-import { type NavGroup } from "./types";
-import { OverflowWrapper } from "../overflow-wrapper";
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { cn } from '~/utils/cn'
+import { OverflowWrapper } from '../overflow-wrapper'
+import { type NavGroup } from './types'
 
 /**
  * Second navbar displayed under the main navbar on mobile.
  */
 export function MobileNavTabs({ groups }: { groups: NavGroup[] }) {
-  const pathname = usePathname();
+  const pathname = usePathname()
   const currentGroup = groups.find((g) =>
-    pathname.startsWith(`/${g.links[0]?.href.split("/")[1]}`)
-  );
+    pathname.startsWith(`/${g.links[0]?.href.split('/')[1]}`),
+  )
 
   return (
     currentGroup && (
@@ -25,9 +25,9 @@ export function MobileNavTabs({ groups }: { groups: NavGroup[] }) {
               <Link href={link.href} key={link.href}>
                 <div
                   className={cn(
-                    "rounded-[4px] border border-[#AB3BD2] text-xs font-semibold px-4 py-[0.53125rem] whitespace-nowrap m-auto leading-none",
+                    'rounded-[4px] border border-[#AB3BD2] text-xs font-semibold px-4 py-[0.53125rem] whitespace-nowrap m-auto leading-none',
                     link.href === pathname &&
-                      "bg-[linear-gradient(90deg,_#7E41CC_0%,_#FF46C0_100%)] text-white border-0 px-[calc(1rem_+_1px)] py-[calc(0.53125rem_+_1px)]"
+                      'bg-[linear-gradient(90deg,_#7E41CC_0%,_#FF46C0_100%)] text-white border-0 px-[calc(1rem_+_1px)] py-[calc(0.53125rem_+_1px)]',
                   )}
                 >
                   {link.title}
@@ -37,5 +37,5 @@ export function MobileNavTabs({ groups }: { groups: NavGroup[] }) {
         </div>
       </OverflowWrapper>
     )
-  );
+  )
 }
