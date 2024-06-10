c6f3e22dbb812451b825287451c1f94ced7ed5c4
Mateusz Radomski <33978857+mateuszradomski@users.noreply.github.com>
2024-06-10 20:42:19 +0200

    Remove stale Optimism twitter account (#4107)

--- packages/config/src/projects/layer2s/optimism.ts
+++ packages/config/src/projects/layer2s/optimism.ts
@@ -40,7 +40,6 @@ export const optimism: Layer2 = opStackL2({
       repositories: ['https://github.com/ethereum-optimism/optimism'],
       socialMedia: [
         'https://optimism.mirror.xyz/',
-        'https://twitter.com/OptimismFND',
         'https://twitter.com/OPLabsPBC',
         'https://youtube.com/playlist?list=PLX_rXoLYCf5HqTWygUfoMfzRirGz5lekH',
         'https://twitch.tv/optimismpbc',
