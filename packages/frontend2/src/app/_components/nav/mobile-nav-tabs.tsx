cc35d4d601ee2cdd7db1845927fbae0da72c4866
Piotr Adamczyk <xeno@xeno.yt>
2024-06-10 12:46:19 +0200

    Use OverflowWrapper for tab menu

--- packages/frontend2/src/app/_components/nav/mobile-nav-tabs.tsx
+++ packages/frontend2/src/app/_components/nav/mobile-nav-tabs.tsx
@@ -1,32 +1,33 @@
-'use client'
+"use client";
 
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import { cn } from '~/utils/cn'
-import { type NavGroup } from './types'
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "~/utils/cn";
+import { type NavGroup } from "./types";
+import { OverflowWrapper } from "../overflow-wrapper";
 
 /**
  * Second navbar displayed under the main navbar on mobile.
  */
 export function MobileNavTabs({ groups }: { groups: NavGroup[] }) {
-  const pathname = usePathname()
+  const pathname = usePathname();
   const currentGroup = groups.find((g) =>
-    pathname.startsWith(`/${g.links[0]?.href.split('/')[1]}`),
-  )
+    pathname.startsWith(`/${g.links[0]?.href.split("/")[1]}`)
+  );
 
   return (
     currentGroup && (
-      <div className="overflow-x-scroll w-full border-b border-gray-200 dark:border-gray-850 text-center scrollbar-hide">
-        <div className="inline-flex flex-row gap-2 px-4 py-2 m-auto">
+      <OverflowWrapper>
+        <div className="flex flex-row gap-2 px-4 py-2">
           {currentGroup.links
             .filter((link) => link.enabled)
             .map((link) => (
               <Link href={link.href} key={link.href}>
                 <div
                   className={cn(
-                    'rounded-[4px] border border-[#AB3BD2] text-xs font-semibold px-4 py-[0.53125rem] whitespace-nowrap m-auto leading-none',
+                    "rounded-[4px] border border-[#AB3BD2] text-xs font-semibold px-4 py-[0.53125rem] whitespace-nowrap m-auto leading-none",
                     link.href === pathname &&
-                      'bg-[linear-gradient(90deg,_#7E41CC_0%,_#FF46C0_100%)] text-white border-0 px-[calc(1rem_+_1px)] py-[calc(0.53125rem_+_1px)]',
+                      "bg-[linear-gradient(90deg,_#7E41CC_0%,_#FF46C0_100%)] text-white border-0 px-[calc(1rem_+_1px)] py-[calc(0.53125rem_+_1px)]"
                   )}
                 >
                   {link.title}
@@ -34,7 +35,7 @@ export function MobileNavTabs({ groups }: { groups: NavGroup[] }) {
               </Link>
             ))}
         </div>
-      </div>
+      </OverflowWrapper>
     )
-  )
+  );
 }
