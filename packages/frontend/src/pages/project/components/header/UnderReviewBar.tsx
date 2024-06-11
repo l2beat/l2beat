cc3c80307b6e04783d2e828906bcfbf4690e13fe
maciekzygmunt <maciekzygmunt@interia.pl>
2024-06-11 16:29:26 +0200

    fix mobile view

--- packages/frontend/src/pages/project/components/header/UnderReviewBar.tsx
+++ packages/frontend/src/pages/project/components/header/UnderReviewBar.tsx
@@ -4,7 +4,7 @@ import { UnderReviewIcon } from '../../../../components/icons'
 
 export function UnderReviewBar({ text }: { text: string }) {
   return (
-    <div className="flex w-full justify-center rounded-lg bg-yellow-700/20 py-2.5 md:px-0">
+    <div className="flex w-full justify-center rounded-lg bg-yellow-700/20 px-2 py-2.5 md:px-0">
       <span className="flex flex-row">
         <span className="flex items-center">
           <UnderReviewIcon />
