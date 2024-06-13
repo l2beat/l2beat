7c4d2c2fce583b24d1966067d384988449e7d1dc
Vikas Bhansali <ensdeployer@gmail.com>
2024-06-13 18:55:36 +0800

    use Link component

--- packages/frontend/src/components/BridgesMvpWarning.tsx
+++ packages/frontend/src/components/BridgesMvpWarning.tsx
@@ -1,19 +1,13 @@
 import React from 'react'
 import { common } from '../build/config/common'
+import { Link } from './Link'
 
 export function BridgesMvpWarning() {
   return (
     <p className="my-4 rounded-lg bg-yellow-500 p-2 text-center font-medium text-base text-black">
       L2BEAT Bridges is a work in progress. You might find incomplete research
       or inconsistent naming. Join our{' '}
-      <a
-        className="font-medium underline"
-        href={common.links.discord}
-        target="_blank"
-      >
-        Discord
-      </a>{' '}
-      to suggest improvements!
+      <Link href={common.links.discord}>Discord</Link> to suggest improvements!
     </p>
   )
 }
