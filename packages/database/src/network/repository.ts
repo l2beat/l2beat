31f07fffe17c557ffbc12753a0cdc52256053db9
Michał Podsiadły <michal.podsiadly@l2beat.com>
2024-06-13 20:26:07 +0200

    feat: relation resolution

--- packages/database/src/network/repository.ts
+++ packages/database/src/network/repository.ts
@@ -1,4 +1,8 @@
 import { PostgresDatabase } from '../kysely'
+import { toRecord as toNetworkExplorerRecord } from '../network-explorer/entity'
+import { selectNetworkExplorer } from '../network-explorer/select'
+import { toRecord as toNetworkRpcRecord } from '../network-rpc/entity'
+import { selectNetworkRpc } from '../network-rpc/select'
 import { Network, toRecord, toRow } from './entity'
 import { joinExplorer, joinRpc } from './join'
 import { selectNetwork } from './select'
@@ -15,16 +19,19 @@ export class NetworkRepository {
     return rows.map(toRecord)
   }
 
-  // Add mappers here
   async findManyWithConfigs() {
     const rows = await this.db
       .selectFrom('Network')
       .innerJoin(...joinExplorer)
       .innerJoin(...joinRpc)
-      .select(selectNetwork)
+      .select([...selectNetwork, ...selectNetworkExplorer, ...selectNetworkRpc])
       .execute()
 
-    return rows.map(toRecord)
+    return rows.map((row) => ({
+      ...toRecord(row),
+      explorer: toNetworkExplorerRecord(row),
+      rpc: toNetworkRpcRecord(row),
+    }))
   }
 
   async findWithCoingecko() {
