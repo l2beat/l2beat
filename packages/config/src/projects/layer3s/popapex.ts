537b14297d331d7c0c0ad653fc9c95653626c5ff
sekuba <sekuba@users.noreply.github.com>
2024-06-13 16:20:48 +0200

    Update display shortName for popapex and syndicateframe (#4125)
    Co-authored-by: sekuba <29250140+sekuba@users.noreply.github.com>

--- packages/config/src/projects/layer3s/popapex.ts
+++ packages/config/src/projects/layer3s/popapex.ts
@@ -15,7 +15,6 @@ export const popapex: Layer3 = orbitStackL3({
   sequencerInbox: discovery.getContract('SequencerInbox'),
   display: {
     name: 'Proof of Play Apex',
-    shortName: 'Apex',
     slug: 'popapex',
     description:
       'Proof of Play Apex is a gaming-focused L3 settling on Arbitrum using the Orbit Stack and AnyTrust DA.',
