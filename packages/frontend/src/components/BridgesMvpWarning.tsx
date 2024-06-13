5f12585ced1b0bb7a5785323abbd8ebae99bb83f
Vikas Bhansali <ensdeployer@gmail.com>
2024-06-13 10:54:07 +0800

    improve BridgesMvpWarning ux

--- packages/frontend/src/components/BridgesMvpWarning.tsx
+++ packages/frontend/src/components/BridgesMvpWarning.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { common } from '../build/config/common'
 
 export function BridgesMvpWarning() {
   return (
     <p className="my-4 rounded-lg bg-yellow-500 p-2 text-center font-medium text-base text-black">
       L2BEAT Bridges is a work in progress. You might find incomplete research
-      or inconsistent naming. Join our discord to suggest improvements!
+      or inconsistent naming. Join our{' '}
+      <a
+        className="font-medium underline"
+        href={common.links.discord}
+        target="_blank"
+      >
+        Discord
+      </a>{' '}
+      to suggest improvements!
     </p>
   )
 }
