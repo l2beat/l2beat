aac667e67e7ac150f7b426fcdb0186915340c47a
torztomasz <tomasz.torz@l2beat.com>
2024-06-17 13:25:42 +0200

    add initial page with tabs

---
+++ packages/frontend2/src/app/(new)/scaling/layout.tsx
@@ -0,0 +1,14 @@
+import { ContentWrapper } from '~/app/_components/content-wrapper'
+import { Footer } from '~/app/_components/footer'
+import { NavLayout } from '~/app/_components/nav/nav-layout'
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <NavLayout logoLink="/scaling/summary">
+      <div className="min-h-screen">
+        <ContentWrapper className="mt-16">{children}</ContentWrapper>
+      </div>
+      <Footer />
+    </NavLayout>
+  )
+}
---
+++ packages/frontend2/src/app/(new)/scaling/summary/page.tsx
@@ -0,0 +1,49 @@
+import { OverflowWrapper } from '~/app/_components/overflow-wrapper'
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from '~/app/_components/tabs'
+import ActiveIcon from '~/icons/active.svg'
+import ArchivedIcon from '~/icons/archived.svg'
+import Layer3sIcon from '~/icons/layer3s.svg'
+import UpcomingIcon from '~/icons/upcoming.svg'
+
+export default function Page() {
+  return (
+    <div>
+      <Tabs defaultValue="active" className="w-full">
+        <OverflowWrapper>
+          <TabsList>
+            <TabsTrigger value="active" className="gap-1.5">
+              <ActiveIcon />
+              <span className="md:hidden">Active</span>
+              <span className="hidden md:inline">Active projects</span>
+            </TabsTrigger>
+            <TabsTrigger value="layer3s" className="gap-1.5">
+              <Layer3sIcon />
+              <span className="md:hidden">Layer 3s</span>
+              <span className="hidden md:inline">Layer 3 projects</span>
+            </TabsTrigger>
+            <TabsTrigger value="upcoming" className="gap-1.5">
+              <UpcomingIcon />
+              <span className="md:hidden">Upcoming</span>
+              <span className="hidden md:inline">Upcoming projects</span>
+            </TabsTrigger>
+            <TabsTrigger value="archived" className="gap-1.5">
+              <ArchivedIcon />
+              <span className="md:hidden">Archived</span>
+              <span className="hidden md:inline">Archived projects</span>
+            </TabsTrigger>
+          </TabsList>
+        </OverflowWrapper>
+
+        <TabsContent value="active">Active Layer2s</TabsContent>
+        <TabsContent value="layer3s">Active Layer3s</TabsContent>
+        <TabsContent value="upcoming">Upcoming Layer2s</TabsContent>
+        <TabsContent value="archived">Archived Layer2s</TabsContent>
+      </Tabs>
+    </div>
+  )
+}
