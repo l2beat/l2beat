f0ab96c9d9abd4933edd3706e4cb6f6038a0eb16
Mateusz Radomski <radomski.main@protonmail.com>
2024-06-19 14:53:36 +0200

    Port LightLinkProxy

--- packages/discovery/src/discovery/proxies/manual/LightLinkProxy.ts
+++ packages/discovery/src/discovery/proxies/manual/LightLinkProxy.ts
@@ -1,86 +1,42 @@
 import { ProxyDetails } from '@l2beat/discovery-types'
 import { assert, EthereumAddress } from '@l2beat/shared-pure'
 
-import { utils } from 'ethers'
-import { callMethod } from '../../handlers/utils/callMethod'
-import { toFunctionFragment } from '../../handlers/utils/toFunctionFragment'
-import { DiscoveryProvider } from '../../provider/DiscoveryProvider'
 import { getImplementation } from '../auto/Eip1967Proxy'
+import { IProvider } from '../../provider/IProvider'
 
 async function getRegistryAddress(
-  provider: DiscoveryProvider,
+  provider: IProvider,
   address: EthereumAddress,
-  blockNumber: number,
 ): Promise<EthereumAddress> {
-  const registryFragment = toFunctionFragment(
-    'function bridgeRegistry() view returns (address)',
-  )
-  const registry = await callMethod(
-    provider,
-    address,
-    registryFragment,
-    [],
-    blockNumber,
-  )
-  if (registry.value === undefined) {
+  const registry = await provider.callMethod<string>(address, 'function bridgeRegistry() view returns (address)', [])
+  if (registry === undefined) {
     // Assume we're the registry
     return address
   }
 
-  return EthereumAddress(registry.value.toString())
+  return EthereumAddress(registry)
 }
 
 async function getAdminMultisig(
-  provider: DiscoveryProvider,
+  provider: IProvider,
   address: EthereumAddress,
-  blockNumber: number,
 ): Promise<EthereumAddress> {
-  const registry = await getRegistryAddress(provider, address, blockNumber)
-  const multisigFragment = toFunctionFragment(
-    'function getMultisig() view returns (address)',
-  )
+  const registry = await getRegistryAddress(provider, address)
+  const multisig = await provider.callMethod<string>(registry, 'function getMultisig() view returns (address)', [])
+  assert(multisig !== undefined, 'Multisig not found')
 
-  const multisig = await callMethod(
-    provider,
-    registry,
-    multisigFragment,
-    [],
-    blockNumber,
-  )
-  assert(multisig.value !== undefined, 'Multisig not found')
-
-  return EthereumAddress(multisig.value.toString())
-}
-
-export async function getOwner(
-  provider: DiscoveryProvider,
-  address: EthereumAddress,
-  blockNumber: number,
-): Promise<EthereumAddress> {
-  const iface = new utils.Interface(['function owner() view returns (address)'])
-  const result = await callMethod(
-    provider,
-    address,
-    iface.getFunction('owner'),
-    [],
-    blockNumber,
-  )
-  if (result.value !== undefined) {
-    return EthereumAddress(result.value.toString())
-  }
-  return EthereumAddress.ZERO
+  return EthereumAddress(multisig.toString())
 }
 
 export async function getLightLinkProxy(
-  provider: DiscoveryProvider,
+  provider: IProvider,
   address: EthereumAddress,
-  blockNumber: number,
 ): Promise<ProxyDetails | undefined> {
-  const implementation = await getImplementation(provider, address, blockNumber)
+  const implementation = await getImplementation(provider, address)
   if (implementation === EthereumAddress.ZERO) {
     return
   }
-  const admin = await getAdminMultisig(provider, address, blockNumber)
+  const admin = await getAdminMultisig(provider, address)
   return {
     implementations: [implementation],
     relatives: [admin],
