d0c2a32a66eb1ea8b6c47f2aa615a0da576cb8ca
torztomasz <tomasz.torz@l2beat.com>
2024-06-19 09:25:22 +0200

    add console.log

--- packages/frontend2/src/content/get-collection.ts
+++ packages/frontend2/src/content/get-collection.ts
@@ -56,6 +56,7 @@ function getDataCollection<T extends DataCollectionKey>(
   key: T,
 ): DataCollectionEntry<T>[] {
   const collection = collections[key]
+  console.log(path.join(process.cwd(), DIR_PATH, key))
   const fileNames = readdirSync(path.join(process.cwd(), DIR_PATH, key))
 
   const parsedFiles = fileNames
@@ -92,6 +93,7 @@ function getContentCollection<T extends ContentCollectionKey>(
   key: T,
 ): ContentCollectionEntry<T>[] {
   const contentEntry = collections[key]
+  console.log(path.join(process.cwd(), DIR_PATH, key))
   const fileNames = readdirSync(path.join(process.cwd(), DIR_PATH, key))
   const parsedFiles = fileNames
     .filter((fileName) => fileName.endsWith(contentEntry.extension))
