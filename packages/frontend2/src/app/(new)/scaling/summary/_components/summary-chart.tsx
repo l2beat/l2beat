878ac96dff48bacc5090a09872b843aaeeed579e
torztomasz <tomasz.torz@l2beat.com>
2024-06-20 13:31:14 +0200

    add radio group

--- packages/frontend2/src/app/(new)/scaling/summary/_components/summary-chart.tsx
+++ packages/frontend2/src/app/(new)/scaling/summary/_components/summary-chart.tsx
@@ -2,6 +2,7 @@
 
 import { Chart } from '~/app/_components/chart/chart'
 import { type ChartColumn } from '~/app/_components/chart/chart-context'
+import { RadioGroup, RadioGroupItem } from '~/app/_components/radio-group'
 import { formatTimestamp } from '~/utils/dates'
 import { formatCurrency, formatCurrencyExactValue } from '~/utils/format'
 
@@ -17,23 +18,33 @@ interface Props {
 }
 export function SummaryChart({ columns }: Props) {
   return (
-    <Chart
-      columns={columns}
-      valuesStyle={[
-        {
-          fill: 'signature gradient',
-          line: 'signature gradient',
-          point: 'circle',
-        },
-      ]}
-      formatYAxisLabel={(value: number) =>
-        // Pass UNIT from controls
-        formatCurrency(value, 'usd', { showLessThanMinimum: false })
-      }
-      range={[1687039200, 1718661600]}
-      useLogScale={false}
-      renderHoverContents={(data) => <ChartHover data={data} />}
-    />
+    <>
+      <RadioGroup defaultValue="option-one">
+        <RadioGroupItem value="option-one" id="option-one">
+          USD
+        </RadioGroupItem>
+        <RadioGroupItem value="option-two" id="option-two">
+          ETH
+        </RadioGroupItem>
+      </RadioGroup>
+      <Chart
+        columns={columns}
+        valuesStyle={[
+          {
+            fill: 'signature gradient',
+            line: 'signature gradient',
+            point: 'circle',
+          },
+        ]}
+        formatYAxisLabel={(value: number) =>
+          // Pass UNIT from controls
+          formatCurrency(value, 'usd', { showLessThanMinimum: false })
+        }
+        range={[1687039200, 1718661600]}
+        useLogScale={false}
+        renderHoverContents={(data) => <ChartHover data={data} />}
+      />
+    </>
   )
 }
 
