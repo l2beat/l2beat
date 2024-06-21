6810eb5f2c62e2f3a2bc72577f554c5cc82d6b57
AK-Cza <a.k.czaplicka@gmail.com>
2024-06-21 11:13:58 +0000

    Added genso as new project

---
+++ packages/config/src/projects/layer3s/genso.ts
@@ -0,0 +1,35 @@
+import { ProjectId } from '@l2beat/shared-pure'
+
+import { underReviewL3 } from '../layer2s/templates/underReview'
+import { Layer3 } from './types'
+
+export const genso: Layer3 = underReviewL3({
+  id: 'genso',
+  hostChain: ProjectId('arbitrum'),
+  display: {
+    name: 'Genso Meta',
+    slug: 'genso meta',
+    category: 'Optimium',
+    description:
+      'Genso Meta is migrating from Polygon to Arbitrum Orbit gaming chain',
+    purposes: ['Gaming'],
+    provider: 'Arbitrum',
+    links: {
+      websites: ['https://genso.game/en'],
+      apps: [
+        'https://bridge.arbitrum.io/?destinationChain=arbitrum-one&sourceChain=ethereum',
+      ],
+      documentation: [''],
+      explorers: [],
+      repositories: [''],
+      socialMedia: [
+        'https://x.com/genso_meta',
+        'https://t.me/gensometamain',
+        'https://discord.com/invite/gensometaverse',
+        'https://line.me/R/ti/p/%40846fieqo',
+      ],
+    },
+    activityDataSource: 'Blockchain RPC',
+  },
+  rpcUrl: '',
+})
