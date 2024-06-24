970132ac689c0b433960bc6ed221a446666de513
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2024-06-24 12:05:30 +0200

    Update packages/config/src/projects/layer3s/genso.ts

--- packages/config/src/projects/layer3s/genso.ts
+++ packages/config/src/projects/layer3s/genso.ts
@@ -8,7 +8,7 @@ export const genso: Layer3 = underReviewL3({
   hostChain: ProjectId('arbitrum'),
   display: {
     name: 'Genso Meta',
-    slug: 'genso meta',
+    slug: 'gensometa',
     category: 'Optimium',
     description:
       'Genso Meta is migrating from Polygon to Arbitrum Orbit gaming chain',
