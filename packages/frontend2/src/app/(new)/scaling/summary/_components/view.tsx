a88c464fc0204fdc5c1b332e18127ce644177a15
torztomasz <tomasz.torz@l2beat.com>
2024-06-25 13:19:05 +0200

    extract part of scaling page to client component

---
+++ packages/frontend2/src/app/(new)/scaling/summary/_components/view.tsx
@@ -0,0 +1,100 @@
+'use client'
+import React, { useState } from 'react'
+import { TabCountBadge } from '~/app/_components/badge/tab-count-badge'
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from '~/app/_components/tabs'
+import { ScalingLegend } from './table/layer2s/legend'
+import { TvlChart } from '~/app/_components/chart/tvl-chart'
+import { HorizontalSeparator } from '~/app/_components/horizontal-separator'
+import { Checkbox } from '~/app/_components/checkbox'
+import { OverflowWrapper } from '~/app/_components/overflow-wrapper'
+import { SummaryArchivedTable } from './table/archived.tsx/summary-archived.table'
+import { SummaryLayer2sTable } from './table/layer2s/summary-layer2s-table'
+import { SummaryLayer3sTable } from './table/layer3s/summary-layer3s-table'
+import { SummaryUpcomingTable } from './table/upcoming/summary-upcoming-table'
+import {
+  type ScalingSummaryLayer2sEntry,
+  type ScalingSummaryLayer3sEntry,
+} from '~/server/features/scaling/types'
+import { type TvlCharts } from '~/server/features/scaling/get-tvl'
+import { type Milestone } from '@l2beat/config'
+
+interface Props {
+  layer2s: ScalingSummaryLayer2sEntry[]
+  layer3s: ScalingSummaryLayer3sEntry[]
+  layer2sTvl: TvlCharts
+  milestones: Milestone[]
+}
+
+export function View({ layer2s, layer3s, layer2sTvl, milestones }: Props) {
+  const [excludeAssociatedTokens, setExcludeAssociatedTokens] = useState(false)
+
+  const layer2sProjects = layer2s.filter(
+    (item) => !item.isArchived && !item.isUpcoming,
+  )
+  const layer3sProjects = layer3s.filter(
+    (item) => !item.isArchived && !item.isUpcoming,
+  )
+  const upcomingProjects = [...layer2s, ...layer3s].filter(
+    (item) => item.isUpcoming,
+  )
+  const archivedProjects = layer2s.filter((item) => item.isArchived)
+
+  return (
+    <>
+      <TvlChart data={layer2sTvl} milestones={milestones} />
+      <HorizontalSeparator className="my-4 md:my-6" />
+      <div className="flex items-center space-x-2 mb-2">
+        <Checkbox
+          id="exclude-associated-tokens"
+          onCheckedChange={(checked) => setExcludeAssociatedTokens(!!checked)}
+        >
+          Exclude associated tokens
+        </Checkbox>
+      </div>
+      <Tabs defaultValue="layer2s" className="w-full">
+        <OverflowWrapper>
+          <TabsList>
+            <TabsTrigger value="layer2s" className="gap-1.5">
+              <span className="md:hidden">Layer2s</span>
+              <span className="hidden md:inline">Layer 2 projects</span>
+              <TabCountBadge>{layer2sProjects.length}</TabCountBadge>
+            </TabsTrigger>
+            <TabsTrigger value="layer3s" className="gap-1.5">
+              <span className="md:hidden">Layer 3s</span>
+              <span className="hidden md:inline">Layer 3 projects</span>
+              <TabCountBadge>{layer3sProjects.length}</TabCountBadge>
+            </TabsTrigger>
+            <TabsTrigger value="upcoming" className="gap-1.5">
+              <span className="md:hidden">Upcoming</span>
+              <span className="hidden md:inline">Upcoming projects</span>
+              <TabCountBadge>{upcomingProjects.length}</TabCountBadge>
+            </TabsTrigger>
+            <TabsTrigger value="archived" className="gap-1.5">
+              <span className="md:hidden">Archived</span>
+              <span className="hidden md:inline">Archived projects</span>
+              <TabCountBadge>{archivedProjects.length}</TabCountBadge>
+            </TabsTrigger>
+          </TabsList>
+        </OverflowWrapper>
+        <TabsContent value="layer2s">
+          <SummaryLayer2sTable items={layer2sProjects} />
+          <ScalingLegend />
+        </TabsContent>
+        <TabsContent value="layer3s">
+          <SummaryLayer3sTable items={layer3sProjects} />
+        </TabsContent>
+        <TabsContent value="upcoming">
+          <SummaryUpcomingTable items={upcomingProjects} />
+        </TabsContent>
+        <TabsContent value="archived">
+          <SummaryArchivedTable items={archivedProjects} />
+        </TabsContent>
+      </Tabs>
+    </>
+  )
+}
--- packages/frontend2/src/app/(new)/scaling/summary/page.tsx
+++ packages/frontend2/src/app/(new)/scaling/summary/page.tsx
@@ -1,17 +1,3 @@
-import { HOMEPAGE_MILESTONES } from '@l2beat/config'
-import { OverflowWrapper } from '~/app/_components/overflow-wrapper'
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from '~/app/_components/tabs'
-import ActiveIcon from '~/icons/active.svg'
-import ArchivedIcon from '~/icons/archived.svg'
-import Layer3sIcon from '~/icons/layer3s.svg'
-import UpcomingIcon from '~/icons/upcoming.svg'
-import { TvlChart } from '../../../_components/chart/tvl-chart'
-import { SummaryLayer2sTable } from './_components/table/layer2s/summary-layer2s-table'
 import { getDefaultMetadata } from '~/utils/get-default-metadata'
 
 export const metadata = getDefaultMetadata({
@@ -21,78 +7,23 @@ export const metadata = getDefaultMetadata({
 })
 import { getScalingSummaryEntries } from '~/server/features/scaling/get-scaling-summary-entries'
 import { getTvl } from '~/server/features/scaling/get-tvl'
-import { HorizontalSeparator } from '~/app/_components/horizontal-separator'
-import { SummaryLayer3sTable } from './_components/table/layer3s/summary-layer3s-table'
-import { SummaryUpcomingTable } from './_components/table/upcoming/summary-upcoming-table'
-import { SummaryArchivedTable } from './_components/table/archived.tsx/summary-archived.table'
-import { ScalingLegend } from './_components/table/layer2s/legend'
 import { OtherSites } from '~/app/_components/other-sites'
 import { About } from '~/app/_components/about'
-import { TabCountBadge } from '~/app/_components/badge/tab-count-badge'
+import { View } from './_components/view'
+import { HOMEPAGE_MILESTONES } from '@l2beat/config'
 
 export default async function Page() {
   const tvl = await getTvl()
   const { layer2s, layer3s } = await getScalingSummaryEntries(tvl)
 
-  const layer2sProjects = layer2s.filter(
-    (item) => !item.isArchived && !item.isUpcoming,
-  )
-  const layer3sProjects = layer3s.filter(
-    (item) => !item.isArchived && !item.isUpcoming,
-  )
-  const upcomingProjects = [...layer2s, ...layer3s].filter(
-    (item) => item.isUpcoming,
-  )
-  const archivedProjects = layer2s.filter((item) => item.isArchived)
-
   return (
     <div className="mb-20">
-      <TvlChart data={tvl.layers2s} milestones={HOMEPAGE_MILESTONES} />
-      <HorizontalSeparator className="my-4 md:my-6" />
-      <Tabs defaultValue="layer2s" className="w-full">
-        <OverflowWrapper>
-          <TabsList>
-            <TabsTrigger value="layer2s" className="gap-1.5">
-              <ActiveIcon />
-              <span className="md:hidden">Layer2s</span>
-              <span className="hidden md:inline">Layer 2 projects</span>
-              <TabCountBadge>{layer2sProjects.length}</TabCountBadge>
-            </TabsTrigger>
-            <TabsTrigger value="layer3s" className="gap-1.5">
-              <Layer3sIcon />
-              <span className="md:hidden">Layer 3s</span>
-              <span className="hidden md:inline">Layer 3 projects</span>
-              <TabCountBadge>{layer3sProjects.length}</TabCountBadge>
-            </TabsTrigger>
-            <TabsTrigger value="upcoming" className="gap-1.5">
-              <UpcomingIcon />
-              <span className="md:hidden">Upcoming</span>
-              <span className="hidden md:inline">Upcoming projects</span>
-              <TabCountBadge>{upcomingProjects.length}</TabCountBadge>
-            </TabsTrigger>
-            <TabsTrigger value="archived" className="gap-1.5">
-              <ArchivedIcon />
-              <span className="md:hidden">Archived</span>
-              <span className="hidden md:inline">Archived projects</span>
-              <TabCountBadge>{archivedProjects.length}</TabCountBadge>
-            </TabsTrigger>
-          </TabsList>
-        </OverflowWrapper>
-
-        <TabsContent value="layer2s">
-          <SummaryLayer2sTable items={layer2sProjects} />
-          <ScalingLegend />
-        </TabsContent>
-        <TabsContent value="layer3s">
-          <SummaryLayer3sTable items={layer3sProjects} />
-        </TabsContent>
-        <TabsContent value="upcoming">
-          <SummaryUpcomingTable items={upcomingProjects} />
-        </TabsContent>
-        <TabsContent value="archived">
-          <SummaryArchivedTable items={archivedProjects} />
-        </TabsContent>
-      </Tabs>
+      <View
+        layer2s={layer2s}
+        layer3s={layer3s}
+        layer2sTvl={tvl.layers2s}
+        milestones={HOMEPAGE_MILESTONES}
+      />
       <OtherSites />
       <About />
     </div>
