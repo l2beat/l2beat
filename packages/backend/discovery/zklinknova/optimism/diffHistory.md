390a177befa7cd082d190ad5cbe4ec4026915f5b
Luca Donno <donnoh99@gmail.com>
2024-06-25 15:55:05 +0200

    op contracts

---
+++ packages/backend/discovery/zklinknova/optimism/diffHistory.md
@@ -0,0 +1,42 @@
+Generated with discovered.json: 0x9eec5861c31074b48bef384dcebebfc858b7729a
+
+# Diff at Tue, 25 Jun 2024 09:20:54 GMT:
+
+- author: Luca Donno (<donnoh99@gmail.com>)
+- current block number: 121854234
+
+## Description
+
+Provide description of changes. This section will be preserved.
+
+## Initial discovery
+
+```diff
++   Status: CREATED
+    contract GnosisSafeL2 (0x2c3FF918E3925CC3ba95f41307D1cfBEFDF93dB9)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract zkLink (0x46C8D02E93d5a03899dFa7Cf8A40A07589A3fA1b)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract L1ERC20Bridge (0x5Bd51296423A9079b931414C1De65e7057326EaA)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract ProxyAdmin (0xA688B4E1375Ed6b9129dF4959da4a271B33e50a4)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract OptimismL2Gateway (0xaD5d729291C0d6A299E370814CA6Ce1c8C25b51c)
+    +++ description: None
+```
--- packages/config/src/projects/layer3s/zklinknova.ts
+++ packages/config/src/projects/layer3s/zklinknova.ts
@@ -1,9 +1,18 @@
 import { EthereumAddress, ProjectId, UnixTime } from '@l2beat/shared-pure'
-import { underReviewL3 } from '../layer2s/templates/underReview'
 import { Layer3 } from './types'
+import { ProjectDiscovery } from '../../discovery/ProjectDiscovery'
 
-export const zklinknova: Layer3 = underReviewL3({
-  id: 'zklinknova',
+const optimismDiscovery = new ProjectDiscovery('zklinknova', 'optimism')
+const ethereumDiscovery = new ProjectDiscovery('zklinknova')
+
+const optimismUpgradability = {
+  upgradableBy: ['OptimismOwner'],
+  upgradeDelay: 'No delay',
+}
+
+export const zklinknova: Layer3 = {
+  type: 'layer3',
+  id: ProjectId('zklinknova'),
   hostChain: ProjectId('linea'),
   display: {
     name: 'zkLink Nova',
@@ -27,7 +36,6 @@ export const zklinknova: Layer3 = underReviewL3({
       ],
     },
   },
-  rpcUrl: 'https://rpc.zklink.io',
   chainConfig: {
     name: 'zklinknova',
     chainId: 810180,
@@ -38,190 +46,269 @@ export const zklinknova: Layer3 = underReviewL3({
     },
     minTimestampForTvl: new UnixTime(1709273393),
   },
-  escrows: [
-    {
-      chain: 'optimism',
-      includeInTotal: false,
-      address: EthereumAddress('0x46C8D02E93d5a03899dFa7Cf8A40A07589A3fA1b'),
-      sinceTimestamp: new UnixTime(1711092485),
-      tokens: ['ETH'],
-      source: 'external',
-      bridge: {
-        name: 'zkLink Nova Bridge from Optimism',
+  config: {
+    transactionApi: {
+      type: 'rpc',
+      defaultUrl: 'https://rpc.zklink.io',
+      defaultCallsPerMinute: 1500,
+      startBlock: 1,
+    },
+    escrows: [
+      {
+        chain: 'optimism',
+        includeInTotal: false,
+        address: EthereumAddress('0x46C8D02E93d5a03899dFa7Cf8A40A07589A3fA1b'),
+        sinceTimestamp: new UnixTime(1711092485),
+        tokens: ['ETH'],
+        source: 'external',
+        bridge: {
+          name: 'zkLink Nova Bridge from Optimism',
+        },
       },
-    },
-    {
-      chain: 'optimism',
-      includeInTotal: false,
-      address: EthereumAddress('0x5Bd51296423A9079b931414C1De65e7057326EaA'),
-      sinceTimestamp: new UnixTime(1711095511),
-      tokens: '*',
-      source: 'external',
-      bridge: {
-        name: 'zkLink Nova Bridge from Optimism',
+      {
+        chain: 'optimism',
+        includeInTotal: false,
+        address: EthereumAddress('0x5Bd51296423A9079b931414C1De65e7057326EaA'),
+        sinceTimestamp: new UnixTime(1711095511),
+        tokens: '*',
+        source: 'external',
+        bridge: {
+          name: 'zkLink Nova Bridge from Optimism',
+        },
       },
-    },
-    {
-      chain: 'linea',
-      includeInTotal: false,
-      address: EthereumAddress('0x5Cb18b6e4e6F3b46Ce646b0f4704D53724C5Df05'),
-      sinceTimestamp: new UnixTime(1709218085),
-      tokens: ['ETH'],
-    },
-    {
-      chain: 'linea',
-      includeInTotal: false,
-      address: EthereumAddress('0x62cE247f34dc316f93D3830e4Bf10959FCe630f8'),
-      sinceTimestamp: new UnixTime(1709218113),
-      tokens: '*',
-    },
-    {
-      chain: 'ethereum',
-      includeInTotal: false,
-      address: EthereumAddress('0x5fD9F73286b7E8683Bab45019C94553b93e015Cf'),
-      sinceTimestamp: new UnixTime(1709278799),
-      tokens: ['ETH'],
-      bridge: {
-        name: 'zkLink Nova Bridge from Ethereum',
+      {
+        chain: 'linea',
+        includeInTotal: false,
+        address: EthereumAddress('0x5Cb18b6e4e6F3b46Ce646b0f4704D53724C5Df05'),
+        sinceTimestamp: new UnixTime(1709218085),
+        tokens: ['ETH'],
       },
-    },
-    {
-      chain: 'ethereum',
-      includeInTotal: false,
-      address: EthereumAddress('0xAd16eDCF7DEB7e90096A259c81269d811544B6B6'),
-      sinceTimestamp: new UnixTime(1709295323),
-      tokens: '*',
-      bridge: {
-        name: 'zkLink Nova Bridge from Ethereum',
+      {
+        chain: 'linea',
+        includeInTotal: false,
+        address: EthereumAddress('0x62cE247f34dc316f93D3830e4Bf10959FCe630f8'),
+        sinceTimestamp: new UnixTime(1709218113),
+        tokens: '*',
       },
-    },
-    {
-      chain: 'mantapacific',
-      includeInTotal: false,
-      address: EthereumAddress('0xd784d7128b46b60ca7d8bdc17dcec94917455657'),
-      sinceTimestamp: new UnixTime(1709279099),
-      tokens: ['ETH'],
-      source: 'external',
-      bridge: {
-        name: 'zkLink Nova Bridge from Mantapacific',
+      {
+        chain: 'ethereum',
+        includeInTotal: false,
+        address: EthereumAddress('0x5fD9F73286b7E8683Bab45019C94553b93e015Cf'),
+        sinceTimestamp: new UnixTime(1709278799),
+        tokens: ['ETH'],
+        bridge: {
+          name: 'zkLink Nova Bridge from Ethereum',
+        },
       },
-    },
-    {
-      chain: 'mantapacific',
-      includeInTotal: false,
-      address: EthereumAddress('0x44a65dc12865a1e5249b45b4868f32b0e37168ff'),
-      sinceTimestamp: new UnixTime(1709295839),
-      tokens: '*',
-      source: 'external',
-      bridge: {
-        name: 'zkLink Nova Bridge from Mantapacific',
+      {
+        chain: 'ethereum',
+        includeInTotal: false,
+        address: EthereumAddress('0xAd16eDCF7DEB7e90096A259c81269d811544B6B6'),
+        sinceTimestamp: new UnixTime(1709295323),
+        tokens: '*',
+        bridge: {
+          name: 'zkLink Nova Bridge from Ethereum',
+        },
       },
-    },
-    // {
-    //   chain: 'mantle',
-    //   includeInTotal: false,
-    //   address: EthereumAddress('0xD784d7128B46B60Ca7d8BdC17dCEC94917455657'),
-    //   sinceTimestamp: new UnixTime(1709279309),
-    //   tokens: ['MNT'],
-    // },
-    {
-      chain: 'mantle',
-      includeInTotal: false,
-      address: EthereumAddress('0x62351b47e060c61868Ab7E05920Cb42bD9A5f2B2'),
-      sinceTimestamp: new UnixTime(1709296907),
-      tokens: '*',
-      source: 'external',
-      bridge: {
-        name: 'zkLink Nova Bridge from Mantle',
+      {
+        chain: 'mantapacific',
+        includeInTotal: false,
+        address: EthereumAddress('0xd784d7128b46b60ca7d8bdc17dcec94917455657'),
+        sinceTimestamp: new UnixTime(1709279099),
+        tokens: ['ETH'],
+        source: 'external',
+        bridge: {
+          name: 'zkLink Nova Bridge from Mantapacific',
+        },
       },
-    },
-    {
-      chain: 'zksync2',
-      includeInTotal: false,
-      address: EthereumAddress('0xaFe8C7Cf33eD0fee179DFF20ae174C660883273A'),
-      sinceTimestamp: new UnixTime(1709280600),
-      tokens: ['ETH'],
-      source: 'external',
-      bridge: {
-        name: 'zkLink Nova Bridge from ZKsync Era',
+      {
+        chain: 'mantapacific',
+        includeInTotal: false,
+        address: EthereumAddress('0x44a65dc12865a1e5249b45b4868f32b0e37168ff'),
+        sinceTimestamp: new UnixTime(1709295839),
+        tokens: '*',
+        source: 'external',
+        bridge: {
+          name: 'zkLink Nova Bridge from Mantapacific',
+        },
       },
-    },
-    {
-      chain: 'zksync2',
-      includeInTotal: false,
-      address: EthereumAddress('0xaB3DDB86072a35d74beD49AA0f9210098ebf2D08'),
-      sinceTimestamp: new UnixTime(1709297040),
-      tokens: '*',
-      source: 'external',
-      bridge: {
-        name: 'zkLink Nova Bridge from ZKsync Era',
+      // {
+      //   chain: 'mantle',
+      //   includeInTotal: false,
+      //   address: EthereumAddress('0xD784d7128B46B60Ca7d8BdC17dCEC94917455657'),
+      //   sinceTimestamp: new UnixTime(1709279309),
+      //   tokens: ['MNT'],
+      // },
+      {
+        chain: 'mantle',
+        includeInTotal: false,
+        address: EthereumAddress('0x62351b47e060c61868Ab7E05920Cb42bD9A5f2B2'),
+        sinceTimestamp: new UnixTime(1709296907),
+        tokens: '*',
+        source: 'external',
+        bridge: {
+          name: 'zkLink Nova Bridge from Mantle',
+        },
       },
-    },
-    {
-      chain: 'arbitrum',
-      includeInTotal: false,
-      address: EthereumAddress('0xFF73a1a1d27951A005eb23276dc99CB7F8d5420A'),
-      sinceTimestamp: new UnixTime(1709280428),
-      tokens: ['ETH'],
-      source: 'external',
-      bridge: {
-        name: 'zkLink Nova Bridge from Arbitrum',
+      {
+        chain: 'zksync2',
+        includeInTotal: false,
+        address: EthereumAddress('0xaFe8C7Cf33eD0fee179DFF20ae174C660883273A'),
+        sinceTimestamp: new UnixTime(1709280600),
+        tokens: ['ETH'],
+        source: 'external',
+        bridge: {
+          name: 'zkLink Nova Bridge from ZKsync Era',
+        },
       },
-    },
-    {
-      chain: 'arbitrum',
-      includeInTotal: false,
-      address: EthereumAddress('0xfB0Ad0B3C2605A7CA33d6badd0C685E11b8F5585'),
-      sinceTimestamp: new UnixTime(1709296973),
-      tokens: '*',
-      source: 'external',
-      bridge: {
-        name: 'zkLink Nova Bridge from Arbitrum',
+      {
+        chain: 'zksync2',
+        includeInTotal: false,
+        address: EthereumAddress('0xaB3DDB86072a35d74beD49AA0f9210098ebf2D08'),
+        sinceTimestamp: new UnixTime(1709297040),
+        tokens: '*',
+        source: 'external',
+        bridge: {
+          name: 'zkLink Nova Bridge from ZKsync Era',
+        },
       },
-    },
-    {
-      chain: 'blast',
-      includeInTotal: false,
-      address: EthereumAddress('0x29BA92Fe724beD5c5EBfd0099F2F64a6DC5078FD'),
-      sinceTimestamp: new UnixTime(1710417729),
-      tokens: ['ETH'],
-      source: 'external',
-      bridge: {
-        name: 'zkLink Nova Bridge from Blast',
+      {
+        chain: 'arbitrum',
+        includeInTotal: false,
+        address: EthereumAddress('0xFF73a1a1d27951A005eb23276dc99CB7F8d5420A'),
+        sinceTimestamp: new UnixTime(1709280428),
+        tokens: ['ETH'],
+        source: 'external',
+        bridge: {
+          name: 'zkLink Nova Bridge from Arbitrum',
+        },
       },
-    },
-    {
-      chain: 'blast',
-      includeInTotal: false,
-      address: EthereumAddress('0x8Df0c2bA3916bF4789c50dEc5A79b2fc719F500b'),
-      sinceTimestamp: new UnixTime(1710427013),
-      tokens: '*',
-      source: 'external',
-      bridge: {
-        name: 'zkLink Nova Bridge from Blast',
+      {
+        chain: 'arbitrum',
+        includeInTotal: false,
+        address: EthereumAddress('0xfB0Ad0B3C2605A7CA33d6badd0C685E11b8F5585'),
+        sinceTimestamp: new UnixTime(1709296973),
+        tokens: '*',
+        source: 'external',
+        bridge: {
+          name: 'zkLink Nova Bridge from Arbitrum',
+        },
       },
-    },
-    {
-      chain: 'base',
-      includeInTotal: false,
-      address: EthereumAddress('0xE473ce141b1416Fe526eb63Cf7433b7B8d7264Dd'),
-      sinceTimestamp: new UnixTime(1711095697),
-      tokens: ['ETH'],
-      source: 'external',
-      bridge: {
-        name: 'zkLink Nova Bridge from Base',
+      {
+        chain: 'blast',
+        includeInTotal: false,
+        address: EthereumAddress('0x29BA92Fe724beD5c5EBfd0099F2F64a6DC5078FD'),
+        sinceTimestamp: new UnixTime(1710417729),
+        tokens: ['ETH'],
+        source: 'external',
+        bridge: {
+          name: 'zkLink Nova Bridge from Blast',
+        },
       },
-    },
-    {
-      chain: 'base',
-      includeInTotal: false,
-      address: EthereumAddress('0x80d12A78EfE7604F00ed07aB2f16F643301674D5'),
-      sinceTimestamp: new UnixTime(1711098033),
-      tokens: '*',
-      source: 'external',
-      bridge: {
-        name: 'zkLink Nova Bridge from Base',
+      {
+        chain: 'blast',
+        includeInTotal: false,
+        address: EthereumAddress('0x8Df0c2bA3916bF4789c50dEc5A79b2fc719F500b'),
+        sinceTimestamp: new UnixTime(1710427013),
+        tokens: '*',
+        source: 'external',
+        bridge: {
+          name: 'zkLink Nova Bridge from Blast',
+        },
       },
+      {
+        chain: 'base',
+        includeInTotal: false,
+        address: EthereumAddress('0xE473ce141b1416Fe526eb63Cf7433b7B8d7264Dd'),
+        sinceTimestamp: new UnixTime(1711095697),
+        tokens: ['ETH'],
+        source: 'external',
+        bridge: {
+          name: 'zkLink Nova Bridge from Base',
+        },
+      },
+      {
+        chain: 'base',
+        includeInTotal: false,
+        address: EthereumAddress('0x80d12A78EfE7604F00ed07aB2f16F643301674D5'),
+        sinceTimestamp: new UnixTime(1711098033),
+        tokens: '*',
+        source: 'external',
+        bridge: {
+          name: 'zkLink Nova Bridge from Base',
+        },
+      },
+    ],
+  },
+  riskView: {
+    validatedBy: {
+      description: '',
+      sentiment: 'bad',
+      value: '',
     },
-  ],
-})
+    sourceUpgradeability: {
+      description: '',
+      sentiment: 'bad',
+      value: '',
+    },
+    destinationToken: {
+      description: '',
+      sentiment: 'bad',
+      value: '',
+    },
+    stateValidation: {
+      description: '',
+      sentiment: 'bad',
+      value: '',
+    },
+    dataAvailability: {
+      description: '',
+      sentiment: 'bad',
+      value: '',
+    },
+    exitWindow: {
+      description: '',
+      sentiment: 'bad',
+      value: '',
+    },
+    sequencerFailure: {
+      description: '',
+      sentiment: 'bad',
+      value: '',
+    },
+    proposerFailure: {
+      description: '',
+      sentiment: 'bad',
+      value: '',
+    },
+  },
+  technology: {},
+  contracts: {
+    addresses: [],
+    nativeAddresses: {
+      ethereum: [
+        ethereumDiscovery.getContractDetails('Arbitrator', {
+          description: '',
+        }),
+      ],
+      optimism: [
+        optimismDiscovery.getContractDetails('L1ERC20Bridge', {
+          description:
+            'Main entry point for depositing ERC20 tokens from OP Mainnet to zkLink Nova. Outgoing messages and incoming withdrawal validation is delegated to the zkLink contract.',
+          ...optimismUpgradability,
+        }),
+        optimismDiscovery.getContractDetails('zkLink', {
+          description:
+            "Main messaging contract on OP Mainnet and ETH escrow. Outgoing messages (like deposits) are sent through the OptimismL2Gateway which ultimately makes use of OP Mainnet's canonical messaging bridge to reach the Arbitrator on L1. Only whitelisted validators can sync messages with zkLink Nova, which also transfer the ETH to it via the respective canonical bridges. Incoming messages (like withdrawals) are validated on Linea first and then sent to this contract through the same path. Whitelisted validators can also relay messages to zkLink without going through the canonical bridge (fast path), which are later cross-checked with the slow path. If the check fails, the system halts.",
+          ...optimismUpgradability,
+        }),
+        optimismDiscovery.getContractDetails('OptimismL2Gateway', {
+          description:
+            "High level interface between the local zkLink contract and OP's L2CrossDomainMessenger.",
+          ...optimismUpgradability,
+        }),
+      ],
+    },
+    risks: [],
+  },
+}
