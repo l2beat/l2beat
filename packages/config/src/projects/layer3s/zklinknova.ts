42f4eea37684127380b0f2f1d532eb8d9ede0a8c
Luca Donno <donnoh99@gmail.com>
2024-06-25 16:40:30 +0200

    add arbitrum

--- packages/config/src/projects/layer3s/zklinknova.ts
+++ packages/config/src/projects/layer3s/zklinknova.ts
@@ -3,10 +3,16 @@ import { Layer3 } from './types'
 import { ProjectDiscovery } from '../../discovery/ProjectDiscovery'
 
 const optimismDiscovery = new ProjectDiscovery('zklinknova', 'optimism')
+const arbitrumDiscovery = new ProjectDiscovery('zklinknova', 'arbitrum')
 const ethereumDiscovery = new ProjectDiscovery('zklinknova')
 
 const optimismUpgradability = {
   upgradableBy: ['OptimismOwner'],
+  upgradeDelay: 'No delay',
+}
+
+const arbitrumUpgradability = {
+  upgradableBy: ['ArbitrumOwner'],
   upgradeDelay: 'No delay',
 }
 
@@ -306,6 +312,23 @@ export const zklinknova: Layer3 = {
           description:
             "High level interface between the local zkLink contract and OP's L2CrossDomainMessenger.",
           ...optimismUpgradability,
+        }),
+      ],
+      arbitrum: [
+        arbitrumDiscovery.getContractDetails('L1ERC20Bridge', {
+          description:
+            'Main entry point for depositing ERC20 tokens from Arbitrum Mainnet to zkLink Nova. Outgoing messages and incoming withdrawal validation is delegated to the zkLink contract.',
+          ...arbitrumUpgradability,
+        }),
+        arbitrumDiscovery.getContractDetails('zkLink', {
+          description:
+            "Main messaging contract on Arbitrum Mainnet and ETH escrow. Outgoing messages (like deposits) are sent through the ArbitrumL2Gateway which ultimately makes use of Arbitrum Mainnet's canonical messaging bridge to reach the Arbitrator on L1. Only whitelisted validators can sync messages with zkLink Nova, which also transfer the ETH to it via the respective canonical bridges. Incoming messages (like withdrawals) are validated on Linea first and then sent to this contract through the same path. Whitelisted validators can also relay messages to zkLink without going through the canonical bridge (fast path), which are later cross-checked with the slow path. If the check fails, the system halts.",
+          ...arbitrumUpgradability,
+        }),
+        arbitrumDiscovery.getContractDetails('ArbitrumL2Gateway', {
+          description:
+            "High level interface between the local zkLink contract and Arbitrum's L2CrossDomainMessenger.",
+          ...arbitrumUpgradability,
         }),
       ],
     },
