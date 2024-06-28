f5ae814f38fee5318a3cb086388466deada2369f
AK-Cza <a.k.czaplicka@gmail.com>
2024-06-28 11:13:03 +0000

    Added Base badges

--- packages/config/src/projects/layer2s/base.ts
+++ packages/config/src/projects/layer2s/base.ts
@@ -69,6 +69,7 @@ export const base: Layer2 = opStackL2({
       description: 'Base is live on mainnet.',
     },
   ],
+  badges: [Badge.DA.EthereumBlobs, Badge.VM.EVM, Badge.Other.L3HostChain],
   nonTemplateEscrows: [
     discovery.getEscrowDetails({
       address: EthereumAddress('0x9de443AdC5A411E83F1878Ef24C3F52C61571e72'),
