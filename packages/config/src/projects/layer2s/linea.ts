e7211af9032232bcb3a3e8c707d3edcbb963eb48
hejpiotrze <150451714+hejpiotrze@users.noreply.github.com>
2024-06-28 11:14:12 +0000

    Added Linea badges.

--- packages/config/src/projects/layer2s/linea.ts
+++ packages/config/src/projects/layer2s/linea.ts
@@ -544,4 +544,5 @@ export const linea: Layer2 = {
       link: 'https://linea.mirror.xyz/7l9gKzYzKVOxEOnReavov467Ss_fsrkGzABvbRISPMY',
     },
   ],
+  badges: [Badge.DA.EthereumBlobs, Badge.VM.EVM, Badge.Other.L3HostChain],
 }
