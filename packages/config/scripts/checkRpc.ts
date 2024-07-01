7aa1b63931f2a96104c670536d240cbef631f729
sekuba <29250140+sekuba@users.noreply.github.com>
2024-07-01 17:03:30 +0200

    revert checkrpc changes

--- packages/config/scripts/checkRpc.ts
+++ packages/config/scripts/checkRpc.ts
@@ -1,11 +1,11 @@
 import { RateLimitedProvider } from '@l2beat/discovery'
 import { providers } from 'ethers'
 
-const RPC_URL = 'https://rpc.alienxchain.io/http'
+const RPC_URL = 'https://rpc.hypr.network'
 
 // this script is here so you can stress test a RPC url.
 async function main() {
-  const callsPerMinute = 900
+  const callsPerMinute = 1500
   const provider = new providers.JsonRpcProvider(RPC_URL)
   const rateLimitedProvider = new RateLimitedProvider(provider, callsPerMinute)
 
