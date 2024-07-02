4894f60c1f3d7a498ee8fdfa670bc6f901872c10
Michał Podsiadły <67391475+sdlyy@users.noreply.github.com>
2024-07-02 16:31:20 +0200

    Blobstream template for DA-BEAT (#4351)

---
+++ packages/config/src/projects/other/da-beat/blockchain/celestia/bridges/blobstream/template.ts
@@ -0,0 +1,50 @@
+import { assert } from '@l2beat/backend-tools'
+import { capitalize } from 'lodash'
+import { chains } from '../../../../../../../'
+import {
+  DaAccessibilityRisk,
+  DaBridgeKind,
+  DaBridgeRisks,
+  OnChainDaBridge,
+} from '../../../../types'
+
+type TemplateRisks = Omit<DaBridgeRisks, 'accessibility'>
+
+type TemplateVars = Pick<
+  OnChainDaBridge,
+  'chain' | 'contracts' | 'permissions' | 'usedIn'
+> & {
+  risks: TemplateRisks
+}
+
+export function CELESTIA_BLOBSTREAM(base: TemplateVars): OnChainDaBridge {
+  const chain = chains.find((chain) => chain.name === base.chain)
+
+  assert(
+    chain,
+    `Cannot template CELESTIA_BLOBSTREAM - chain ${base.chain} not found among known chains`,
+  )
+
+  const id = `blobstream-${chain.name}`
+  const display = {
+    name: 'Blobstream',
+    slug: id,
+    description: `Celestia with Blobstream bridge on ${capitalize(chain.name)}`,
+  }
+
+  const risks = {
+    accessibility: DaAccessibilityRisk.NotEnshrined,
+    ...base.risks,
+  } satisfies DaBridgeRisks
+
+  return {
+    kind: DaBridgeKind.OnChainBridge,
+    id,
+    display,
+    risks,
+    chain: chain.name,
+    contracts: base.contracts,
+    permissions: base.permissions,
+    usedIn: base.usedIn,
+  }
+}
