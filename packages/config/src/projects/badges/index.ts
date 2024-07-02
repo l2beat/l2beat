b9681624c80e60b7e358d4b0d56c4dfa7df97113
Hubert Kotliński <75430263+Huberacht@users.noreply.github.com>
2024-07-02 09:43:17 +0000

    Fix format

--- packages/config/src/projects/badges/index.ts
+++ packages/config/src/projects/badges/index.ts
@@ -32,42 +32,48 @@ export const badges = {
   Conduit: {
     display: {
       name: 'Conduit',
-      description: 'This project was deployed via the rollup-as-a-service provider Conduit',
+      description:
+        'This project was deployed via the rollup-as-a-service provider Conduit',
     },
     type: BadgeType.RaaS,
   },
   Gelato: {
     display: {
       name: 'Gelato',
-      description: 'This project was deployed via the rollup-as-a-service provider Gelato',
+      description:
+        'This project was deployed via the rollup-as-a-service provider Gelato',
     },
     type: BadgeType.RaaS,
   },
   Karnot: {
     display: {
       name: 'Karnot',
-      description: 'This project was deployed via the rollup-as-a-service provider Karnot',
+      description:
+        'This project was deployed via the rollup-as-a-service provider Karnot',
     },
     type: BadgeType.RaaS,
   },
   StarkEx: {
     display: {
       name: 'StarkEx',
-      description: 'This project was deployed utilizing the StarkEx technology from StarkWare',
+      description:
+        'This project was deployed utilizing the StarkEx technology from StarkWare',
     },
     type: BadgeType.RaaS,
   },
   Caldera: {
     display: {
       name: 'Caldera',
-      description: 'This project was deployed via the rollup-as-a-service provider Caldera',
+      description:
+        'This project was deployed via the rollup-as-a-service provider Caldera',
     },
     type: BadgeType.RaaS,
   },
   Syndicate: {
     display: {
       name: 'Syndicate',
-      description: 'This project was deployed via the rollup-as-a-service provider Syndicate',
+      description:
+        'This project was deployed via the rollup-as-a-service provider Syndicate',
     },
     type: BadgeType.RaaS,
   },
@@ -113,56 +119,64 @@ export const badges = {
   EVM: {
     display: {
       name: 'EVM',
-      description: 'This project uses the Ethereum Virtual Machine to run its smart contracts and supports the Solidity programming language',
+      description:
+        'This project uses the Ethereum Virtual Machine to run its smart contracts and supports the Solidity programming language',
     },
     type: BadgeType.VM,
   },
   EVMLike: {
     display: {
       name: 'EVM-like',
-      description: 'This project uses the Ethereum Virtual Machine variation to run its smart contracts and supports the Solidity programming language',
+      description:
+        'This project uses the Ethereum Virtual Machine variation to run its smart contracts and supports the Solidity programming language',
     },
     type: BadgeType.VM,
   },
   CairoVM: {
     display: {
       name: 'Cairo',
-      description: 'This project uses the Cairo Virtual Machine to run its smart contracts and supports the Cairo programming language',
+      description:
+        'This project uses the Cairo Virtual Machine to run its smart contracts and supports the Cairo programming language',
     },
     type: BadgeType.VM,
   },
   FuelVM: {
     display: {
       name: 'FuelVM',
-      description: 'This project uses the Fuel Virtual Machine to run its smart contracts and supports the Sway programming language',
+      description:
+        'This project uses the Fuel Virtual Machine to run its smart contracts and supports the Sway programming language',
     },
     type: BadgeType.VM,
   },
   AztecVM: {
     display: {
       name: 'AztecVM',
-      description: 'This project uses the Aztec Virtual Machine to run its smart contracts and supports the Noir programming language',
+      description:
+        'This project uses the Aztec Virtual Machine to run its smart contracts and supports the Noir programming language',
     },
     type: BadgeType.VM,
   },
   MoveVM: {
     display: {
       name: 'MoveVM',
-      description: 'This project uses the Move Virtual Machine to run its smart contracts and supports the Move programming language',
+      description:
+        'This project uses the Move Virtual Machine to run its smart contracts and supports the Move programming language',
     },
     type: BadgeType.VM,
   },
   SolanaVM: {
     display: {
       name: 'SolanaVM',
-      description: 'This project uses the Solana Virtual Machine to run its smart contracts',
+      description:
+        'This project uses the Solana Virtual Machine to run its smart contracts',
     },
     type: BadgeType.VM,
   },
   CustomVM: {
     display: {
       name: 'Custom',
-      description: 'This project uses a custom-built virtual machine suitable for its own needs',
+      description:
+        'This project uses a custom-built virtual machine suitable for its own needs',
     },
     type: BadgeType.VM,
   },
@@ -178,7 +192,8 @@ export const badges = {
   Governance: {
     display: {
       name: 'Governance',
-      description: 'This project has a decentralized governance system that votes on proposed changes',
+      description:
+        'This project has a decentralized governance system that votes on proposed changes',
     },
     type: BadgeType.Other,
   },
