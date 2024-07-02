ad7df06b2c5511b04c8366da80d83868578fcdff
torztomasz <tomasz.torz@l2beat.com>
2024-07-02 08:39:34 +0200

    improve sorting fn

--- packages/frontend2/src/app/_components/table/sorting/functions/sentiment-sorting.ts
+++ packages/frontend2/src/app/_components/table/sorting/functions/sentiment-sorting.ts
@@ -1,36 +1,19 @@
 import { type Sentiment } from '@l2beat/shared-pure'
 
+const sentimentOrder: Sentiment[] = ['bad', 'warning', 'neutral', 'good']
+
 export function sortSentiments<TData extends { sentiment: Sentiment }>(
   rowA: TData,
   rowB: TData,
 ) {
-  const sentimentOrderValueA = getSentimentOrderValue(rowA)
-  if (sentimentOrderValueA === undefined) {
-    return -1
+  const sentimentOrderValueA = sentimentOrder.indexOf(rowA.sentiment)
+  if (sentimentOrderValueA === -1) {
+    return sentimentOrderValueA
   }
-  const sentimentOrderValueB = getSentimentOrderValue(rowB)
-  if (sentimentOrderValueB === undefined) {
-    return 1
+  const sentimentOrderValueB = sentimentOrder.indexOf(rowB.sentiment)
+  if (sentimentOrderValueB === -1) {
+    return -sentimentOrderValueA
   }
 
   return sentimentOrderValueA > sentimentOrderValueB ? 1 : -1
-}
-
-export function getSentimentOrderValue<T extends { sentiment: Sentiment }>(
-  item: T | undefined,
-) {
-  if (!item || item.sentiment === 'UnderReview') {
-    return undefined
-  }
-
-  switch (item.sentiment) {
-    case 'bad':
-      return 0
-    case 'warning':
-      return 1
-    case 'neutral':
-      return 2
-    case 'good':
-      return 3
-  }
 }
