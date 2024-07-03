701c16e0587e8497bbea0bd539e4ac988c53f828
Piotr Adamczyk <xeno@xeno.yt>
2024-07-03 11:24:56 +0200

    Fix dates in DB

--- packages/database/src/kysely/index.ts
+++ packages/database/src/kysely/index.ts
@@ -3,14 +3,18 @@ import {
   Transaction as KyselyTransaction,
   PostgresDialect,
 } from 'kysely'
-import { Pool, PoolConfig } from 'pg'
+import { Pool, PoolConfig, types } from 'pg'
 import { DB } from './generated/types'
+
+// Interpret `timestamp without time zone` as UTC
+types.setTypeParser(1114, (stringValue) => new Date(stringValue + '+0000'))
 
 export class PostgresDatabase extends Kysely<DB> {
   constructor(config?: PoolConfig) {
     super({
       dialect: new PostgresDialect({
-        pool: new Pool(config),
+        // Always parse input dates as UTC
+        pool: new Pool({ types, parseInputDatesAsUTC: true, ...config }),
       }),
     })
   }
