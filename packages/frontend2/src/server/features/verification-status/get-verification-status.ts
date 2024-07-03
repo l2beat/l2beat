cbb07a7754394c44c9f40ab0526ae2de2122335b
torztomasz <tomasz.torz@l2beat.com>
2024-07-03 14:02:33 +0200

    fix verification paths

--- packages/frontend2/src/server/features/verification-status/get-verification-status.ts
+++ packages/frontend2/src/server/features/verification-status/get-verification-status.ts
@@ -1,10 +1,11 @@
-import fs from 'fs'
 import { bridges, getChainNames, layer2s, layer3s } from '@l2beat/config'
 import { VerificationStatus } from '@l2beat/shared-pure'
+import { readFileSync } from 'fs'
 import {
   unstable_cache as cache,
   unstable_noStore as noStore,
 } from 'next/cache'
+import path from 'path'
 import { env } from '~/env'
 
 export function getVerificationStatus() {
@@ -15,7 +16,7 @@ export function getVerificationStatus() {
 const getCachedVerificationStatus = cache(
   async () => {
     const chainNames = getChainNames([...layer2s, ...layer3s, ...bridges])
-    const projects = getProjectVerificationStatus()
+    const projects = await getProjectVerificationStatus()
     const contracts = Object.fromEntries(
       chainNames.map((chain) => [chain, getContractVerificationStatus(chain)]),
     )
@@ -30,17 +31,22 @@ const getCachedVerificationStatus = cache(
   { revalidate: 60 * 60 },
 )
 
-const PROJECTS_VERIFICATION_PATH = '../config/src/verification/projects.json'
 function getProjectVerificationStatus(): unknown {
-  const projects = fs.readFileSync(PROJECTS_VERIFICATION_PATH, 'utf8')
+  const projects = readFileSync(
+    path.join(process.cwd(), '../config/src/verification/projects.json'),
+    'utf8',
+  )
   return JSON.parse(projects) as unknown
 }
 
 function getContractVerificationPath(chain: string) {
-  return `../config/src/verification/${chain}/verified.json`
+  return path.join(
+    process.cwd(),
+    `../config/src/verification/${chain}/verified.json`,
+  )
 }
 function getContractVerificationStatus(chain: string): unknown {
   const path = getContractVerificationPath(chain)
-  const contracts = fs.readFileSync(path, 'utf8')
+  const contracts = readFileSync(path, 'utf8')
   return JSON.parse(contracts) as unknown
 }
