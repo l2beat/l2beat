b1ec7c371311e1621692c7c311bb898a0a7a818b
sekuba <29250140+sekuba@users.noreply.github.com>
2024-07-05 13:03:54 +0200

    Wirex Pay Chain upcoming

---
+++ packages/config/src/projects/layer2s/wirex.ts
@@ -0,0 +1,27 @@
+import { upcomingL2 } from './templates/upcoming'
+import { Layer2 } from './types'
+
+export const prism: Layer2 = upcomingL2({
+  id: 'wirex',
+  display: {
+    name: 'Pay Chain',
+    slug: 'wirex',
+    category: 'Validium',
+    provider: 'Polygon',
+    description:
+      'Pay Chain is a Validium built on the Polygon CDK stack. It will be used as the infrastructure for the Wirex non-custodial debit cards.',
+    purposes: ['Payments'],
+    links: {
+      websites: ['https://wirexpaychain.com/'],
+      apps: ['https://presale.wirexpaychain.com/'],
+      documentation: [],
+      explorers: [],
+      repositories: [],
+      socialMedia: [
+        'https://x.com/wirexpaychain',
+        'https://discord.gg/f8UGp4dH6g',
+        'https://wirexpaychain.com/blog',
+      ],
+    },
+  },
+})
