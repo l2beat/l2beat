f07ec0ea3fdb1c31f965a1b1b5970f938dfa11a1
antooni <antoni.pawlak@protonmail.com>
2024-07-08 15:31:06 +0200

    remove console.time

--- packages/backend/src/modules/tvl/api/services/TvlService.ts
+++ packages/backend/src/modules/tvl/api/services/TvlService.ts
@@ -44,13 +44,11 @@ export class TvlService {
   ): Promise<TvlApiResponse> {
     const ethPrices =
       await this.$.pricesDataService.getEthPrices(targetTimestamp)
-    console.time('VALUES')
     const valuesByProjectByTimestamp =
       await this.$.valuesDataService.getValuesForProjects(
         projects,
         targetTimestamp,
       )
-    console.timeEnd('VALUES')
 
     const projectsMinTimestamp = projects
       .map((x) => x.minTimestamp)
@@ -186,9 +184,7 @@ export class TvlService {
       })
     }
 
-    console.time('BREAKDOWN')
     const breakdownMap = await this.getBreakdownMap(targetTimestamp)
-    console.timeEnd('BREAKDOWN')
 
     const projectData: Record<string, TvlApiProject> = {}
     // TODO: we should rethink how we use chartsMap here
