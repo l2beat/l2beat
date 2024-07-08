22c3eb8ec3ee0b275a2ea7e42a39628dcdbe1234
Tomasz Tórz <93620601+torztomasz@users.noreply.github.com>
2024-07-08 15:47:17 +0200

    Remove height for <XL screens (#4415)

--- packages/frontend2/src/app/_components/footer.tsx
+++ packages/frontend2/src/app/_components/footer.tsx
@@ -4,7 +4,7 @@ import { SocialLinks } from './social-links'
 export function Footer() {
   return (
     <footer>
-      <div className="border-t border-gray-200 h-20 dark:border-gray-850 flex flex-row items-center">
+      <div className="border-t border-gray-200 xl:h-20 max-xl:py-4 dark:border-gray-850 flex flex-row items-center">
         <div className="max-w-[1216px] mx-auto flex grid-cols-3 flex-col gap-4 px-4 md:grid md:px-12 flex-1">
           <p className="text-center text-sm font-medium md:text-left">
             Made with 💗 by the L2BEAT research team.{' '}
