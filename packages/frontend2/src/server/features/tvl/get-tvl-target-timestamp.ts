c1e4ed9267699f48faf7b80debbd954c590e87a5
Piotr Adamczyk <xeno@xeno.yt>
2024-07-08 11:50:06 +0200

    Adjust tvl target timestamp to match current one

--- packages/frontend2/src/server/features/tvl/get-tvl-target-timestamp.ts
+++ packages/frontend2/src/server/features/tvl/get-tvl-target-timestamp.ts
@@ -1,5 +1,5 @@
 import { UnixTime } from '@l2beat/shared-pure'
 
 export function getTvlTargetTimestamp() {
-  return UnixTime.now().toStartOf('hour').add(-1, 'hours')
+  return UnixTime.now().toStartOf('hour').add(-2, 'hours')
 }
