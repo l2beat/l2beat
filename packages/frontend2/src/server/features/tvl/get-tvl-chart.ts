6591eb0fb83f55f1c9e5eda3566b94bcf3731a23
Piotr Adamczyk <xeno@xeno.yt>
2024-07-09 14:43:00 +0200

    Use `noStore` in `getTvlChart`

--- packages/frontend2/src/server/features/tvl/get-tvl-chart.ts
+++ packages/frontend2/src/server/features/tvl/get-tvl-chart.ts
@@ -3,6 +3,7 @@ import { getEthPrices } from './get-eth-prices'
 import { type TvlProject, getTvlProjects } from './get-tvl-projects'
 import { getTvlValuesForProjects } from './get-tvl-values-for-projects'
 import { type TvlChartRange } from './range-utils'
+import { unstable_noStore as noStore } from 'next/cache'
 
 export async function getTvlChart({
   range,
@@ -11,6 +12,7 @@ export async function getTvlChart({
   | { type: 'all' | TvlProject['type'] }
   | { type: 'projects'; projectIds: string[] }
 )) {
+  noStore()
   const projectsFilter = () => {
     if (rest.type === 'all') {
       return () => true
