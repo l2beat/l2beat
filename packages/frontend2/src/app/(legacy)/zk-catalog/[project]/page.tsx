40a77e61d089db4b42d884eef2907b2cbce1f1b6
Michał Podsiadły <michal.podsiadly@l2beat.com>
2024-07-10 13:51:04 +0200

    docs: remove comment

--- packages/frontend2/src/app/(legacy)/zk-catalog/[project]/page.tsx
+++ packages/frontend2/src/app/(legacy)/zk-catalog/[project]/page.tsx
@@ -1,53 +1,46 @@
-import { type Metadata } from 'next'
-import { notFound } from 'next/navigation'
-import { ContentWrapper } from '~/app/_components/content-wrapper'
-import { getVerifiers } from '~/server/features/zk-catalog/get-verifiers'
-import { getDefaultMetadata } from '~/utils/get-default-metadata'
-import { ZK_CATALOG_ASK_FOR_VERIFICATION_LINK } from '../_utils/getZkCatalogView'
-import { projects } from '../_utils/projects'
-import { ZkCatalogProjectPage } from './_components/ZkCatalogProjectPage'
-import { getZkCatalogProjectDetails } from './_utils/getZkCatalogProjectDetails'
+import { type Metadata } from 'next';
+import { notFound } from 'next/navigation';
+import { ContentWrapper } from '~/app/_components/content-wrapper';
+import { getVerifiers } from '~/server/features/zk-catalog/get-verifiers';
+import { getDefaultMetadata } from '~/utils/get-default-metadata';
+import { ZK_CATALOG_ASK_FOR_VERIFICATION_LINK } from '../_utils/getZkCatalogView';
+import { projects } from '../_utils/projects';
+import { ZkCatalogProjectPage } from './_components/ZkCatalogProjectPage';
+import { getZkCatalogProjectDetails } from './_utils/getZkCatalogProjectDetails';
 
 interface Props {
   params: {
-    project: string
-  }
+    project: string;
+  };
 }
-
-// Do we want to prerender these pages?
-// export async function generateStaticParams() {
-//   return projects.map((project) => ({
-//     project: project.display.slug,
-//   }));
-// }
 
 export async function generateMetadata({
   params,
 }: Props): Promise<Metadata | null> {
-  const project = projects.find((p) => p.display.slug === params.project)
+  const project = projects.find((p) => p.display.slug === params.project);
 
   if (!project) {
-    return null
+    return null;
   }
 
   return getDefaultMetadata({
     title: `${project.display.name} - ZK Catalog`,
-  })
+  });
 }
 
 export default async function Page(props: Props) {
-  const project = projects.find((p) => p.display.slug === props.params.project)
+  const project = projects.find((p) => p.display.slug === props.params.project);
 
   if (!project) {
-    return notFound()
+    return notFound();
   }
 
-  const verifiers = await getVerifiers()
+  const verifiers = await getVerifiers();
 
   const projectDetails = {
     details: getZkCatalogProjectDetails(project, verifiers),
     askForVerificationLink: ZK_CATALOG_ASK_FOR_VERIFICATION_LINK,
-  }
+  };
 
   return (
     <ContentWrapper>
@@ -55,5 +48,5 @@ export default async function Page(props: Props) {
         <ZkCatalogProjectPage {...projectDetails} />
       </main>
     </ContentWrapper>
-  )
+  );
 }
