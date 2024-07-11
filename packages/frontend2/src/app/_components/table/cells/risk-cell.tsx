de4679774bc3c46e6cab448bff614d6289c40b98
Piotr Adamczyk <xeno@xeno.yt>
2024-07-11 10:33:41 +0200

    Update risk cells to match the old FE

--- packages/frontend2/src/app/_components/table/cells/risk-cell.tsx
+++ packages/frontend2/src/app/_components/table/cells/risk-cell.tsx
@@ -9,12 +9,15 @@ import { cn } from '~/utils/cn'
 import { sentimentToFillColor } from '~/utils/sentiment'
 import { SentimentText } from '../../sentiment-text'
 import { Tooltip, TooltipContent, TooltipTrigger } from '../../tooltip/tooltip'
+import { WarningBar } from '../../warning-bar'
 
 interface Risk {
   value: string
   sentiment: Sentiment
   description?: string
   warning?: WarningValueWithSentiment
+  secondLine?: string
+  secondSentiment?: Sentiment
 }
 
 interface Props {
@@ -23,13 +26,35 @@ interface Props {
 
 export function RiskCell({ risk }: Props) {
   const trigger = (
-    <div className="flex items-center gap-1">
-      <SentimentText sentiment={risk.sentiment}>{risk.value}</SentimentText>
-      {risk.warning && (
-        <RoundedWarningIcon
-          className={cn('size-5', sentimentToFillColor(risk.warning.sentiment))}
-        />
-      )}
+    <div className="flex flex-col">
+      <div className="flex items-center gap-1">
+        <SentimentText sentiment={risk.sentiment}>{risk.value}</SentimentText>
+        {risk.warning && (
+          <RoundedWarningIcon
+            className={cn(
+              'size-5',
+              sentimentToFillColor(risk.warning.sentiment),
+            )}
+          />
+        )}
+      </div>
+      {risk.secondLine &&
+        (risk.secondSentiment ? (
+          <SentimentText
+            sentiment={risk.secondSentiment}
+            className="block text-xs leading-none"
+          >
+            {risk.secondLine}
+          </SentimentText>
+        ) : (
+          <span
+            className={
+              '-mt-1 mb-1 text-2xs text-gray-550 md:m-0 dark:text-gray-500 md:text-xs md:leading-none'
+            }
+          >
+            {risk.secondLine}
+          </span>
+        ))}
     </div>
   )
 
@@ -38,6 +63,15 @@ export function RiskCell({ risk }: Props) {
       <Tooltip>
         <TooltipTrigger asChild>{trigger}</TooltipTrigger>
         <TooltipContent>
+          {risk.warning && (
+            <WarningBar
+              className="mb-2"
+              text={risk.warning.value}
+              icon={RoundedWarningIcon}
+              color={risk.warning.sentiment === 'bad' ? 'red' : 'yellow'}
+              ignoreMarkdown
+            />
+          )}
           {!!risk.description ? risk.description : null}
         </TooltipContent>
       </Tooltip>
