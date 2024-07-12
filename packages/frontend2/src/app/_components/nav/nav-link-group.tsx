34b80cd33c1c56e13e80330a7200bdeb5459e0a1
Piotr Adamczyk <xeno@xeno.yt>
2024-07-12 10:48:25 +0200

    Port `NavLinkGroup`

--- packages/frontend2/src/app/_components/nav/nav-link-group.tsx
+++ packages/frontend2/src/app/_components/nav/nav-link-group.tsx
@@ -9,10 +9,10 @@ export interface NavLinkGroupProps {
 export function NavLinkGroup({ title, children }: NavLinkGroupProps) {
   return (
     <div className="flex flex-col gap-2">
-      <div className="ml-1 text-slate-600 dark:text-zinc-500 text-[0.8125rem] leading-[0.8125rem] uppercase font-medium xl:sidenav-collapsed:hidden">
+      <div className="ml-1 text-slate-600 dark:text-gray-50 text-[0.9375rem] leading-[0.9375rem] uppercase font-medium xl:sidenav-collapsed:hidden tracking-tight">
         {title}
       </div>
-      <div className="h-px w-8 bg-slate-600 dark:bg-zinc-500 hidden xl:sidenav-collapsed:block mt-[12px]" />
+      <div className="h-px w-8 bg-slate-600 dark:bg-gray-50 hidden xl:sidenav-collapsed:block mt-[12px]" />
       <ul className="flex flex-col gap-0.5">{children}</ul>
     </div>
   )
