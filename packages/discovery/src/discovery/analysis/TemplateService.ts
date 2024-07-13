a82508becacb997db884366101e1f307cb80d980
Adrian Adamiak <adrian@adamiak.net>
2024-07-13 18:44:22 +0000

    Fix template similarity, move LivenessModule

--- packages/discovery/src/discovery/analysis/TemplateService.ts
+++ packages/discovery/src/discovery/analysis/TemplateService.ts
@@ -18,7 +18,7 @@ import { readJsonc } from '../utils/readJsonc'
 
 const TEMPLATES_PATH = path.join('discovery', '_templates')
 const TEMPLATE_SHAPE_FOLDER = 'shape'
-const TEMPLATE_SIMILARITY_THRESHOLD = 0.999999999 // TODO: why two identical files are not 1.0?
+const TEMPLATE_SIMILARITY_THRESHOLD = 0.999 // TODO: why two identical files are not 1.0?
 
 export class TemplateService {
   private readonly loadedTemplates: Record<string, DiscoveryContract> = {}
