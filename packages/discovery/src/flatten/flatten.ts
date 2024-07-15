c6bae99047cf03487a19e4008cfffabf520bcf2b
Mateusz Radomski <33978857+mateuszradomski@users.noreply.github.com>
2024-07-15 09:13:10 +0200

    Import dynamic interfaces and generate ones for contracts (#4456)

--- packages/discovery/src/flatten/flatten.ts
+++ packages/discovery/src/flatten/flatten.ts
@@ -2,34 +2,40 @@ import { createHash } from 'crypto'
 import { assert } from '@l2beat/backend-tools'
 
 import {
-  ByteRange,
   DeclarationFilePair,
   FileContent,
   ParsedFile,
   ParsedFilesManager,
 } from './ParsedFilesManager'
+import { generateInterfaceSourceFromContract } from './generateInterfaceSourceFromContract'
+import { FlattenOptions } from './types'
+
+type EntryType = 'inheritance' | 'dynamic'
 
 interface ContractNameFilePair {
   contractName: string
   file: ParsedFile
+  type: EntryType
 }
 
 export function flattenStartingFrom(
   rootContractName: string,
   files: FileContent[],
   remappingStrings: string[],
+  options?: FlattenOptions,
 ): string {
   const parsedFileManager = ParsedFilesManager.parseFiles(
     files,
     remappingStrings,
+    options,
   )
   const rootContract = parsedFileManager.findDeclaration(rootContractName)
-
-  let flatSource = formatSource(
-    rootContract.file.content,
-    rootContract.declaration.byteRange,
+  const relationDictionary = generateUsedFromDictionary(
+    parsedFileManager,
+    rootContract,
   )
 
+  let flatSource = formatSource(rootContract.declaration.content)
   // Depth first search
   const visited = new Set<string>()
   const stack: ContractNameFilePair[] = getStackEntries(rootContract).reverse()
@@ -49,16 +55,76 @@ export function flattenStartingFrom(
     }
     visited.add(uniqueContractId)
 
-    const { declaration: contract, file } = foundContract
-    flatSource = formatSource(file.content, contract.byteRange) + flatSource
+    const { declaration: contract } = foundContract
+    let content = contract.content
+    if (
+      entryIsPurelyDynamic(relationDictionary, foundContract) &&
+      isContract(foundContract)
+    ) {
+      content = generateInterfaceSourceFromContract(foundContract.declaration)
+    }
+    flatSource = formatSource(content) + flatSource
     stack.push(...getStackEntries(foundContract))
   }
 
   return changeLineEndingsToUnix(flatSource.trimEnd())
 }
 
-function formatSource(source: string, byteRange: ByteRange): string {
-  return source.slice(byteRange.start, byteRange.end + 1) + '\n\n'
+function entryIsPurelyDynamic(
+  relationDictionary: Record<string, string[]>,
+  contract: DeclarationFilePair,
+): boolean {
+  const uniqueContractId = getUniqueContractId(contract)
+  return (
+    relationDictionary[uniqueContractId]?.every(
+      (entry) => entry === 'dynamic',
+    ) ?? false
+  )
+}
+
+function generateUsedFromDictionary(
+  parsedFileManager: ParsedFilesManager,
+  rootContract: DeclarationFilePair,
+): Record<string, EntryType[]> {
+  const result: Record<string, EntryType[]> = {}
+  const stack: ContractNameFilePair[] = getStackEntries(rootContract).reverse()
+
+  while (stack.length > 0) {
+    const entry = stack.pop()
+    assert(entry !== undefined, 'Stack should not be empty')
+
+    const foundContract = parsedFileManager.tryFindDeclaration(
+      entry.contractName,
+      entry.file,
+    )
+    assert(foundContract, `Failed to find contract ${entry.contractName}`)
+
+    const uniqueContractId = getUniqueContractId(foundContract)
+    if (result[uniqueContractId]?.includes(entry.type)) {
+      continue
+    }
+    result[uniqueContractId] ??= []
+    result[uniqueContractId]?.push(entry.type)
+
+    const entries = getStackEntries(foundContract)
+    if (entry.type === 'dynamic') {
+      entries.forEach((e) => (e.type = 'dynamic'))
+    }
+    stack.push(...entries)
+  }
+
+  return result
+}
+
+function isContract(entry: DeclarationFilePair): boolean {
+  return (
+    entry.declaration.type === 'contract' ||
+    entry.declaration.type === 'abstract'
+  )
+}
+
+function formatSource(source: string): string {
+  return source + '\n\n'
 }
 
 function changeLineEndingsToUnix(source: string): string {
@@ -67,17 +133,34 @@ function changeLineEndingsToUnix(source: string): string {
 
 function getUniqueContractId(entry: DeclarationFilePair): string {
   const hasher = createHash('sha1')
-  const source = formatSource(entry.file.content, entry.declaration.byteRange)
-  hasher.update(source)
+  hasher.update(entry.declaration.content)
   return `0x${hasher.digest('hex')}`
 }
 
 function getStackEntries(pair: DeclarationFilePair): ContractNameFilePair[] {
-  const contractNames = pair.declaration.inheritsFrom.concat(
-    pair.declaration.referencedDeclaration,
-  )
-  return contractNames.map((contractName) => ({
-    contractName,
-    file: pair.file,
-  }))
+  const inheritanceReferences: ContractNameFilePair[] =
+    pair.declaration.inheritsFrom.map((contractName) => ({
+      contractName,
+      file: pair.file,
+      type: 'inheritance',
+    }))
+
+  const dynamicReferences: ContractNameFilePair[] =
+    pair.declaration.dynamicReferences
+      .map(
+        (contractName) =>
+          ({
+            contractName,
+            file: pair.file,
+            type: 'dynamic',
+          }) as ContractNameFilePair,
+      )
+      .filter(
+        (entry) =>
+          !inheritanceReferences.some(
+            (e) => e.contractName === entry.contractName,
+          ),
+      )
+
+  return inheritanceReferences.concat(dynamicReferences)
 }
