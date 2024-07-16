2ace80f31db0ebd8089962f1391d34b15c6dc8b6
Piotr Adamczyk <xeno@xeno.yt>
2024-07-16 13:21:07 +0200

    Enable badges on production (#4490)

--- packages/frontend/src/build/config/config.production.ts
+++ packages/frontend/src/build/config/config.production.ts
@@ -4,10 +4,6 @@ import { common } from './common'
 export function getProductionConfig(): Config {
   return {
     ...common,
-    features: {
-      ...common.features,
-      badges: false,
-    },
     backend: {
       apiUrl: 'https://api.l2beat.com',
       skipCache: true,
