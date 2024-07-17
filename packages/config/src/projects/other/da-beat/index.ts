55519b06b4b1bc769401c5142a5e4857e105b77c
Michał Podsiadły <michal.podsiadly@l2beat.com>
2024-07-17 19:57:44 +0200

    feat: include ethereum in daLayers

--- packages/config/src/projects/other/da-beat/index.ts
+++ packages/config/src/projects/other/da-beat/index.ts
@@ -1,8 +1,9 @@
 import { celestia } from './blockchain/celestia/celestia'
+import { ethereum } from './blockchain/ethereum/ethereum'
 import { dac } from './dac/layer'
 import { DaLayer } from './types/DaLayer'
 
 export * from './types'
 export * from './utils'
 
-export const daLayers: DaLayer[] = [dac, celestia]
+export const daLayers: DaLayer[] = [dac, celestia, ethereum]
