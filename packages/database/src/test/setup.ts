397c122fe438911fcb5a5038f9791506efcbefec
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2024-07-17 10:32:13 +0200

    Migrate before database tests

--- packages/database/src/test/setup.ts
+++ packages/database/src/test/setup.ts
@@ -1,1 +1,9 @@
+import { getLegacyTestDatabase } from '@l2beat/database-legacy'
+
 process.env.NODE_ENV = 'test'
+
+before(async () => {
+  const legacyDatabase = getLegacyTestDatabase()
+  await legacyDatabase?.migrateToLatest()
+  await legacyDatabase?.closeConnection()
+})
