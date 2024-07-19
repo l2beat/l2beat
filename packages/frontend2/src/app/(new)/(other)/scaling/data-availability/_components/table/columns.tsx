59584e96925de643be0ce035395a286f01af938b
Michał Podsiadły <michal.podsiadly@l2beat.com>
2024-07-19 20:08:25 +0200

    feat: sync tooltips

--- packages/frontend2/src/app/(new)/(other)/scaling/data-availability/_components/table/columns.tsx
+++ packages/frontend2/src/app/(new)/(other)/scaling/data-availability/_components/table/columns.tsx
@@ -1,13 +1,16 @@
-import { createColumnHelper } from '@tanstack/react-table'
-import Image from 'next/image'
-import { SentimentText } from '~/app/_components/sentiment-text'
-import { IndexCell } from '~/app/_components/table/cells/index-cell'
-import { ProjectNameCell } from '~/app/_components/table/cells/project-name-cell'
-import { TypeCell } from '~/app/_components/table/cells/type-cell'
-import { sortSentiments } from '~/app/_components/table/sorting/functions/sentiment-sorting'
-import { type ScalingDataAvailabilityEntry } from '~/server/features/scaling/types'
+import { createColumnHelper } from '@tanstack/react-table';
+import Image from 'next/image';
+import { SentimentText } from '~/app/_components/sentiment-text';
+import { IndexCell } from '~/app/_components/table/cells/index-cell';
+import { ProjectNameCell } from '~/app/_components/table/cells/project-name-cell';
+import {
+  TypeCell,
+  TypeColumnTooltip,
+} from '~/app/_components/table/cells/type-cell';
+import { sortSentiments } from '~/app/_components/table/sorting/functions/sentiment-sorting';
+import { type ScalingDataAvailabilityEntry } from '~/server/features/scaling/types';
 
-const columnHelper = createColumnHelper<ScalingDataAvailabilityEntry>()
+const columnHelper = createColumnHelper<ScalingDataAvailabilityEntry>();
 
 export const columns = [
   // TODO: Make sure this is centered
@@ -45,6 +48,9 @@ export const columns = [
   }),
   columnHelper.accessor('category', {
     header: 'Type',
+    meta: {
+      tooltip: <TypeColumnTooltip />,
+    },
     cell: (ctx) => (
       <TypeCell provider={ctx.row.original.provider}>
         {ctx.row.original.category}
@@ -53,8 +59,12 @@ export const columns = [
   }),
   columnHelper.accessor('dataAvailability.layer', {
     header: 'DA Layer',
+    meta: {
+      tooltip:
+        'The data availability layer where the data (transaction data or state diffs) is published.',
+    },
     cell: (ctx) => {
-      const layer = ctx.getValue()
+      const layer = ctx.getValue();
       return (
         <SentimentText
           sentiment={layer.sentiment}
@@ -62,16 +72,20 @@ export const columns = [
         >
           {layer.value}
         </SentimentText>
-      )
+      );
     },
     sortingFn: (a, b) =>
       sortSentiments(
         a.original.dataAvailability.layer,
-        b.original.dataAvailability.layer,
+        b.original.dataAvailability.layer
       ),
   }),
   columnHelper.accessor('dataAvailability.bridge', {
     header: 'DA Bridge',
+    meta: {
+      tooltip:
+        'The DA bridge used for informing Ethereum contracts if data has been made available.',
+    },
     cell: (ctx) => (
       <SentimentText
         sentiment={ctx.row.original.dataAvailability.bridge.sentiment}
@@ -83,10 +97,10 @@ export const columns = [
     sortingFn: (a, b) =>
       sortSentiments(
         a.original.dataAvailability.bridge,
-        b.original.dataAvailability.bridge,
+        b.original.dataAvailability.bridge
       ),
   }),
   columnHelper.accessor('dataAvailability.mode', {
     header: 'Type of data',
   }),
-]
+];
