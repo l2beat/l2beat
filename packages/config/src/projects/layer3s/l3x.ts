1eb0fc8f88feeff641230afa374de48ab549d86f
sekuba <29250140+sekuba@users.noreply.github.com>
2024-07-22 09:48:28 +0200

    Add L3X escrows

--- packages/config/src/projects/layer3s/l3x.ts
+++ packages/config/src/projects/layer3s/l3x.ts
@@ -1,4 +1,4 @@
-import { ProjectId } from '@l2beat/shared-pure'
+import { EthereumAddress, ProjectId, UnixTime } from '@l2beat/shared-pure'
 
 import { underReviewL3 } from '../layer2s/templates/underReview'
 import { Layer3 } from './types'
@@ -25,4 +25,20 @@ export const l3x: Layer3 = underReviewL3({
     },
     activityDataSource: 'Blockchain RPC',
   },
+  escrows: [
+    {
+      address: EthereumAddress('0x59E088d827CB7983Cd0CC64312E472D7cc8a4F44'),
+      sinceTimestamp: new UnixTime(1714618907),
+      includeInTotal: false,
+      tokens: ['ETH'],
+      chain: 'arbitrum',
+    },
+    {
+      address: EthereumAddress('0x4fF3E70f30f0394Ad62428751Fe3858740595908'),
+      sinceTimestamp: new UnixTime(1714620855),
+      includeInTotal: false,
+      tokens: '*',
+      chain: 'arbitrum',
+    },
+  ],
 })
