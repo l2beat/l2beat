33ea4326281abc80ee1faa51614b5b2e06e2ede9
torztomasz <tomasz.torz@l2beat.com>
2024-07-22 08:55:55 +0200

    adjust selector check icon

--- packages/frontend2/src/app/_components/select.tsx
+++ packages/frontend2/src/app/_components/select.tsx
@@ -115,7 +115,7 @@ const SelectItem = React.forwardRef<
   >
     <span className="absolute right-2 flex h-3.5 w-3.5 items-center justify-center">
       <SelectPrimitive.ItemIndicator>
-        <CheckIcon className="h-5 w-5 stroke-black dark:stroke-white" />
+        <CheckIcon className="h-5 w-5 stroke-[1.6px] stroke-black dark:stroke-white" />
       </SelectPrimitive.ItemIndicator>
     </span>
     <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
