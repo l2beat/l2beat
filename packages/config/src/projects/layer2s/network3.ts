fc8d567eb19f6e5e092885c4edf9e6dd4f613741
niufson <niufson@gmail.com>
2024-07-23 18:15:22 +0000

    add network3

---
+++ packages/config/src/projects/layer2s/network3.ts
@@ -0,0 +1,26 @@
+import { upcomingL2 } from './templates/upcoming'
+import { Layer2 } from './types'
+
+export const network3: Layer2 = upcomingL2({
+  id: 'network3',
+  display: {
+    name: 'Network3',
+    slug: 'network3',
+    description:
+      'An AIoT Layer2 Empowering Efficient Model Training & Validation.',
+    purposes: ['AI', 'Privacy'],
+    category: 'Validium',
+    links: {
+      websites: ['https://network3.io/'],
+      apps: [],
+      documentation: ['https://litepaper.network3.ai/~/changes/0jJxEcDKtGZ0s8Ew29KS'],
+      explorers: [],
+      repositories: [],
+      socialMedia: [
+        'https://x.com/network3_ai',
+        'https://discord.gg/q4cHgxZUCH',
+        'https://t.me/network3official'
+      ],
+    },
+  },
+})
