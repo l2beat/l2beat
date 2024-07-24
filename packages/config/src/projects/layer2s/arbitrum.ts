0c3c7073cbeaffd944bfe4ebcac8815e6196d4ff
sekuba <29250140+sekuba@users.noreply.github.com>
2024-07-24 14:22:05 +0200

    remove upgradeability from ConstHash

--- packages/config/src/projects/layer2s/arbitrum.ts
+++ packages/config/src/projects/layer2s/arbitrum.ts
@@ -358,7 +358,6 @@ export const arbitrum: Layer2 = orbitStackL2({
       l2Discovery.getContractDetails('ConstitutionHash', {
         description:
           'Keeps the current hash of the ArbitrumDAO Constitution. Settable by the owner.',
-        ...l2Upgradability,
       }),
       l2Discovery.getContractDetails('L2ProxyAdmin', {
         description:
