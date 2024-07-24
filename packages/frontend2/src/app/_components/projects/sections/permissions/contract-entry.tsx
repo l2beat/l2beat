5ab03d4bc52abc671e08037e6be106187f45af5d
Tomasz Tórz <93620601+torztomasz@users.noreply.github.com>
2024-07-24 12:15:58 +0200

    Separate upgradable by links (#4611)

--- packages/frontend2/src/app/_components/projects/sections/permissions/contract-entry.tsx
+++ packages/frontend2/src/app/_components/projects/sections/permissions/contract-entry.tsx
@@ -16,7 +16,7 @@ export interface TechnologyContract {
   addresses: TechnologyContractAddress[]
   chain: string
   description?: string
-  upgradeableBy?: string
+  upgradeableBy?: string[]
   upgradeDelay?: string
   usedInProjects?: UsedInProject[]
   upgradeConsiderations?: string
@@ -76,15 +76,19 @@ export function ContractEntry({ contract, className }: ContractEntryProps) {
               {contract.description}
             </Markdown>
           )}
-          {contract.upgradeableBy && (
-            <p className="mt-2 text-gray-850 dark:text-gray-400">
+          {contract.upgradeableBy && contract.upgradeableBy.length > 0 && (
+            <div className="mt-2 flex flex-wrap text-gray-850 dark:text-gray-400">
               <strong className="text-black dark:text-white">
                 Can be upgraded by:
-              </strong>{' '}
-              <CustomLink href={`#${contract.upgradeableBy}`}>
-                {contract.upgradeableBy}
-              </CustomLink>
-            </p>
+              </strong>
+              <div className="ml-1.5 space-x-1.5">
+                {contract.upgradeableBy.map((name) => (
+                  <CustomLink key={name} href={`#${name}`}>
+                    {name}
+                  </CustomLink>
+                ))}
+              </div>
+            </div>
           )}
           {contract.upgradeDelay && (
             <p className="mt-2 text-gray-850 dark:text-gray-400">
