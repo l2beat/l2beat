60065b73632c63fa7bf2c3019b8aad70e353bd13
torztomasz <tomasz.torz@l2beat.com>
2024-07-24 09:03:35 +0200

    sort projects by tvs

--- packages/frontend2/src/server/features/data-availability/get-da-risk-entries.ts
+++ packages/frontend2/src/server/features/data-availability/get-da-risk-entries.ts
@@ -14,7 +14,7 @@ export async function getDaRiskEntries() {
   const tvlPerProject = await getDaProjectsTvl(uniqueProjectsInUse)
   const getSumFor = pickTvlForProjects(tvlPerProject)
 
-  return daLayers.flatMap((daLayer) =>
+  const entries = daLayers.flatMap((daLayer) =>
     daLayer.bridges.map((bridge) => {
       const tvs = getSumFor(bridge.usedIn.map((project) => project.id))
       const economicSecurityData = economicSecurity[daLayer.id]
@@ -25,9 +25,12 @@ export async function getDaRiskEntries() {
         href: `/data-availability/projects/${daLayer.display.slug}/${bridge.display.slug}`,
         daBridge: toDaBridge(bridge),
         risks: getDaRisks(daLayer, bridge, tvs, economicSecurityData),
+        tvs,
       }
     }),
   )
+
+  return entries.sort((a, b) => b.tvs - a.tvs)
 }
 
 export type DaRiskEntry = Awaited<ReturnType<typeof getDaRiskEntries>>[number]
