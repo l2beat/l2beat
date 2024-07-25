c6e471e31137ff8305c591e9cedae5be7532a936
Piotr Adamczyk <xeno@xeno.yt>
2024-07-25 13:20:09 +0200

    Fix cookies

--- packages/frontend2/src/app/cookies-prefetch-poc/_components/cookies-prefetch-poc-client-component.tsx
+++ packages/frontend2/src/app/cookies-prefetch-poc/_components/cookies-prefetch-poc-client-component.tsx
@@ -1,13 +1,20 @@
 'use client'
 
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import { api } from '~/trpc/react'
 
 export function CookiesPrefetchPocClientComponent({
   initialText,
 }: { initialText: string }) {
-  const [text, setText] = useState<string>(initialText)
+  const [text, _setText] = useState<string>(initialText)
   const { data, isPending } = api.cookiesPrefetchPoC.useQuery({ text })
+  const setText = useCallback(
+    (newText: string) => {
+      _setText(newText)
+      document.cookie = `cookies-prefetch-poc-text=${newText}; max-age=3600;`
+    },
+    [_setText],
+  )
   return (
     <div>
       <input value={text} onChange={(e) => setText(e.target.value)} />
