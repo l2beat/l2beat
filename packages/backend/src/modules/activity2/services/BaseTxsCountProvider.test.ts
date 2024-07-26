4027292902b4f559db64a1b71cad4383bf5843e5
maciekzygmunt <maciekzygmunt@interia.pl>
2024-07-26 13:41:40 +0200

    add abstract base class

---
+++ packages/backend/src/modules/activity2/services/BaseTxsCountProvider.test.ts
@@ -0,0 +1,70 @@
+import { Logger } from '@l2beat/backend-tools'
+import { ActivityRecord } from '@l2beat/database'
+import { ProjectId, UnixTime } from '@l2beat/shared-pure'
+import { expect } from 'earl'
+import { range } from 'lodash'
+import { BaseTxsCountProvider } from './BaseTxsCountProvider'
+
+const START = UnixTime.fromDate(new Date('2021-01-01T00:00:00Z'))
+
+describe(BaseTxsCountProvider.name, () => {
+  describe(BaseTxsCountProvider.prototype.aggregatePerDay.name, () => {
+    it('should aggregate per day', () => {
+      const testTxsCountProvider = new TestTxsCountProvider(
+        Logger.SILENT,
+        ProjectId('a'),
+      )
+
+      const result = testTxsCountProvider.aggregatePerDay([
+        block(START, 1, 1),
+        block(START.add(1, 'hours'), 5, 2),
+        block(START.add(2, 'hours'), 2, 3),
+        block(START.add(1, 'days'), 2, 4),
+        block(START.add(1, 'days').add(1, 'hours'), 6, 5),
+      ])
+
+      expect(result).toEqual([
+        activityRecord('a', START, 8, 1, 3),
+        activityRecord('a', START.add(1, 'days'), 8, 4, 5),
+      ])
+    })
+  })
+})
+
+class TestTxsCountProvider extends BaseTxsCountProvider {
+  constructor(logger: Logger, projectId: ProjectId) {
+    super({ logger, projectId })
+  }
+
+  override getTxsCount(): Promise<ActivityRecord[]> {
+    return Promise.resolve([])
+  }
+}
+
+function block(timestamp: UnixTime, count: number, number: number) {
+  return {
+    timestamp,
+    count,
+    number,
+  }
+}
+
+export function activityRecord(
+  projectId: string,
+  timestamp: UnixTime,
+  count: number,
+  start: number = 0,
+  end: number = 10,
+) {
+  return {
+    projectId: ProjectId(projectId),
+    timestamp,
+    count,
+    start,
+    end,
+  }
+}
+
+export function transactions(count: number) {
+  return range(count).map((i) => `0x${i.toString()}`)
+}
---
+++ packages/backend/src/modules/activity2/services/BaseTxsCountProvider.ts
@@ -0,0 +1,48 @@
+import { Logger } from '@l2beat/backend-tools'
+import { ActivityRecord } from '@l2beat/database'
+import { ProjectId, UnixTime } from '@l2beat/shared-pure'
+
+interface BaseTxsCountProviderDeps {
+  logger: Logger
+  projectId: ProjectId
+}
+
+export abstract class BaseTxsCountProvider {
+  constructor(private readonly $: BaseTxsCountProviderDeps) {
+    this.$.logger = $.logger.for(this).tag($.projectId.toString())
+  }
+
+  abstract getTxsCount(from: number, to: number): Promise<ActivityRecord[]>
+
+  aggregatePerDay(
+    blocks: {
+      count: number
+      timestamp: UnixTime
+      number: number
+    }[],
+  ): ActivityRecord[] {
+    const result: ActivityRecord[] = []
+
+    for (const block of blocks) {
+      const timestamp = block.timestamp.toStartOf('day')
+      const currentCount = result.find(
+        (r) => r.timestamp.toNumber() === timestamp.toNumber(),
+      )
+
+      if (currentCount) {
+        currentCount.count += block.count
+        currentCount.start = Math.min(currentCount.start, block.number)
+        currentCount.end = Math.max(currentCount.end, block.number)
+      } else {
+        result.push({
+          projectId: this.$.projectId,
+          timestamp,
+          count: block.count,
+          start: block.number,
+          end: block.number,
+        })
+      }
+    }
+    return result
+  }
+}
