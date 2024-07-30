57bacd71ca86bb917e81aa18230cd0a23905af2e
torztomasz <tomasz.torz@l2beat.com>
2024-07-30 10:35:41 +0200

    add basic page with metadata

---
+++ packages/frontend2/src/app/(new)/(other)/scaling/costs/page.tsx
@@ -0,0 +1,11 @@
+import { getDefaultMetadata } from '~/utils/get-default-metadata'
+
+export const metadata = getDefaultMetadata({
+  openGraph: {
+    url: '/scaling/costs',
+  },
+})
+
+export default function Page() {
+  return <div></div>
+}
