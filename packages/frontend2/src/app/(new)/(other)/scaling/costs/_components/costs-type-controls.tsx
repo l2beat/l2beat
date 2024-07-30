06cc007fd4e66c9b484cdfef441a5dc63d367b92
torztomasz <tomasz.torz@l2beat.com>
2024-07-30 16:17:05 +0200

    remove comment

--- packages/frontend2/src/app/(new)/(other)/scaling/costs/_components/costs-type-controls.tsx
+++ packages/frontend2/src/app/(new)/(other)/scaling/costs/_components/costs-type-controls.tsx
@@ -3,23 +3,6 @@ import { RadioGroup, RadioGroupItem } from '~/app/_components/radio-group'
 
 export function CostsTypeControls() {
   return (
-    // <RadioGroup
-    //   role="costs-type-controls"
-    //   name="costs-type"
-    //   className="w-min"
-    //   highlighted
-    //   options={[
-    //     {
-    //       value: 'TOTAL',
-    //       checked: true,
-    //     },
-    //     {
-    //       label: 'PER L2 TX',
-    //       value: 'PER-L2-TX',
-    //       className: 'w-max',
-    //     },
-    //   ]}
-    // />
     <RadioGroup defaultValue="total">
       <RadioGroupItem value="total" variant="highlighted">
         TOTAL
