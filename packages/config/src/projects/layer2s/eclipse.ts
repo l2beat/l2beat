9b3e20b472e2b35f56ea312a081b140b8bb3dcc5
Luca Donno <donnoh99@gmail.com>
2024-07-31 12:43:18 +0200

    Add Eclipse (under review)

--- packages/config/src/projects/layer2s/eclipse.ts
+++ packages/config/src/projects/layer2s/eclipse.ts
@@ -1,7 +1,8 @@
-import { upcomingL2 } from './templates/upcoming'
+import { EthereumAddress, UnixTime } from '@l2beat/shared-pure'
+import { underReviewL2 } from './templates/underReview'
 import { Layer2 } from './types'
 
-export const eclipse: Layer2 = upcomingL2({
+export const eclipse: Layer2 = underReviewL2({
   id: 'eclipse',
   display: {
     name: 'Eclipse',
@@ -14,7 +15,7 @@ export const eclipse: Layer2 = upcomingL2({
       websites: ['https://eclipse.builders/'],
       apps: ['https://bridge.eclipse.builders/'],
       documentation: ['https://docs.eclipse.builders/'],
-      explorers: [],
+      explorers: ['https://explorer.eclipse.xyz/'],
       repositories: [],
       socialMedia: [
         'https://twitter.com/eclipsefnd',
@@ -23,4 +24,13 @@ export const eclipse: Layer2 = upcomingL2({
       ],
     },
   },
+  // rpcUrl: 'https://mainnetbeta-rpc.eclipse.xyz', custom VM, i guess it's different
+  escrows: [
+    {
+      chain: 'ethereum',
+      address: EthereumAddress('0xd7e4b67e735733ac98a88f13d087d8aac670e644'),
+      sinceTimestamp: new UnixTime(1722140987),
+      tokens: ['ETH'],
+    },
+  ],
 })
