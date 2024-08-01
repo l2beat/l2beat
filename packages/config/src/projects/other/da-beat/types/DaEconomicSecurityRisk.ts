0540a0fa1c80aa688478d053327d1cd91aac230a
Michał Podsiadły <michal.podsiadly@l2beat.com>
2024-08-01 10:58:15 +0200

    chore: shorten type value

--- packages/config/src/projects/other/da-beat/types/DaEconomicSecurityRisk.ts
+++ packages/config/src/projects/other/da-beat/types/DaEconomicSecurityRisk.ts
@@ -17,7 +17,7 @@ const OnChainNotSlashable = (token?: string) => {
   const tokenExpression = token ? ` ${token} tokens` : 'tokens'
   return {
     type: 'OnChainNotSlashable',
-    value: `Staked assets - Not Slashable`,
+    value: `Staked assets`,
     sentiment: 'bad',
     description: `Although node operators are required to stake ${tokenExpression} to become members of the DA network, there is no slashing mechanism in place for misbehaving nodes.`,
   } as const
