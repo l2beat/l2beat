c915eb45709cfe6e94a2d5c72215c632d886b600
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2024-08-01 10:30:43 +0200

    Simplify mergeConfigurations

--- packages/backend/src/tools/uif/multi/mergeConfigurations.ts
+++ packages/backend/src/tools/uif/multi/mergeConfigurations.ts
@@ -5,14 +5,6 @@ import {
   SavedConfiguration,
 } from './types'
 
-interface ConfigurationState<T> {
-  added: Configuration<T>[]
-  updated: Map<string, SavedConfiguration<T>>
-  deleted: string[]
-  trimmed: RemovalConfiguration[]
-  configurations: SavedConfiguration<T>[]
-}
-
 export interface ConfigurationsDiff<T> {
   toAdd: Configuration<T>[]
   toUpdate: SavedConfiguration<T>[]
@@ -31,212 +23,112 @@ export function mergeConfigurations<T>(
   actual: Configuration<T>[],
   serializeConfiguration: (value: T) => string,
 ): MergeResult<T> {
-  assert(actual.length > 0, 'Actual configurations should not be empty')
+  const maps = getConfigurationsAsMaps(saved, actual)
 
-  const state = initializeEmptyState<T>()
-
-  const savedMap = new Map(saved.map((c) => [c.id, c]))
-  const knownIds = new Set<string>()
+  const toAdd: Configuration<T>[] = []
+  const toUpdate: SavedConfiguration<T>[] = []
+  const toDelete: string[] = []
+  const toRemoveData: RemovalConfiguration[] = []
+  const configurations: SavedConfiguration<T>[] = []
 
   for (const c of actual) {
-    assertConfigurationValidity(knownIds, c)
-    knownIds.add(c.id)
-
-    const stored = savedMap.get(c.id)
+    const stored = maps.saved.get(c.id)
 
     if (!stored) {
-      handleNewConfiguration(state, c)
+      toAdd.push(c)
+      configurations.push({ ...c, currentHeight: null })
       continue
     }
 
-    if (stored.minHeight > c.minHeight) {
-      handleMinHeightDecrease(state, stored, c)
-      continue
-    }
-
-    if (stored.minHeight < c.minHeight) {
-      handleMinHeightIncrease(state, stored, c)
+    let currentHeight = stored.currentHeight
+    if (c.minHeight < stored.minHeight) {
+      // We remove everything because we cannot have gaps in downloaded data
+      // We will re-download everything from the beginning
+      if (stored.currentHeight !== null) {
+        toRemoveData.push({
+          id: stored.id,
+          from: stored.minHeight,
+          to: stored.currentHeight,
+        })
+      }
+      currentHeight = null
+    } else if (c.minHeight > stored.minHeight) {
+      toRemoveData.push({
+        id: stored.id,
+        from: stored.minHeight,
+        to: c.minHeight - 1,
+      })
+      if (currentHeight !== null && currentHeight < c.minHeight) {
+        currentHeight = null
+      }
     }
 
     if (c.maxHeight !== stored.maxHeight) {
-      handleMaxHeightChange(state, stored, c)
+      if (
+        c.maxHeight !== null &&
+        currentHeight !== null &&
+        c.maxHeight < currentHeight
+      ) {
+        toRemoveData.push({
+          id: stored.id,
+          from: c.maxHeight + 1,
+          to: currentHeight,
+        })
+        currentHeight = c.maxHeight
+      }
     }
 
-    const currentHeight = calculateCurrentHeight(stored, c)
+    configurations.push({ ...c, currentHeight })
 
-    state.configurations.push({ ...c, currentHeight })
-
-    if (stored.properties !== serializeConfiguration(c.properties)) {
-      setUpdated(state, c, stored.currentHeight)
+    if (
+      stored.currentHeight !== currentHeight ||
+      stored.maxHeight !== c.maxHeight ||
+      stored.minHeight !== c.minHeight ||
+      stored.properties !== serializeConfiguration(c.properties)
+    ) {
+      toUpdate.push({ ...c, currentHeight })
     }
   }
-
-  findDeleted(state, saved, knownIds, actual)
-
-  return {
-    diff: {
-      toAdd: state.added,
-      toUpdate: Array.from(state.updated.values()),
-      toDelete: state.deleted,
-      toRemoveData: state.trimmed,
-    },
-    configurations: state.configurations,
-    safeHeight: getSafeHeight(state.configurations),
-  }
-}
-
-function assertConfigurationValidity<T>(
-  knownIds: Set<string>,
-  c: Configuration<T>,
-) {
-  assert(!knownIds.has(c.id), `Configuration ${c.id} is duplicated!`)
-  assert(
-    c.maxHeight === null || c.minHeight <= c.maxHeight,
-    `Configuration ${c.id} has minHeight greater than maxHeight!`,
-  )
-}
-
-function calculateCurrentHeight<T>(
-  stored: SavedConfiguration<string>,
-  c: Configuration<T>,
-) {
-  if (stored.currentHeight === null) {
-    return null
-  }
-
-  if (stored.currentHeight < c.minHeight) {
-    return null
-  }
-
-  return Math.min(stored.currentHeight, c.maxHeight ?? stored.currentHeight)
-}
-
-function handleMaxHeightChange<T>(
-  state: ConfigurationState<T>,
-  stored: SavedConfiguration<string>,
-  c: Configuration<T>,
-) {
-  if (
-    c.maxHeight !== null &&
-    stored.currentHeight !== null &&
-    stored.currentHeight > c.maxHeight
-  ) {
-    state.trimmed.push({
-      id: stored.id,
-      from: c.maxHeight + 1,
-      to: stored.currentHeight,
-    })
-  }
-
-  const currentHeight =
-    stored.currentHeight === null
-      ? null
-      : Math.min(stored.currentHeight, c.maxHeight ?? Infinity)
-  setUpdated(state, c, currentHeight)
-}
-
-function handleMinHeightIncrease<T>(
-  state: ConfigurationState<T>,
-  stored: SavedConfiguration<string>,
-  c: Configuration<T>,
-) {
-  state.trimmed.push({
-    id: stored.id,
-    from: stored.minHeight,
-    to: c.minHeight - 1,
-  })
-  setUpdated(state, c, stored.currentHeight)
-}
-
-function handleMinHeightDecrease<T>(
-  state: ConfigurationState<T>,
-  stored: SavedConfiguration<string>,
-  c: Configuration<T>,
-) {
-  // We remove everything because we cannot have gaps in downloaded data
-  // We will re-download everything from the beginning
-  if (stored.currentHeight !== null) {
-    state.trimmed.push({
-      id: stored.id,
-      from: stored.minHeight,
-      to: stored.currentHeight,
-    })
-  }
-  state.configurations.push({ ...c, currentHeight: null })
-  setUpdated(state, c, null)
-}
-
-function handleNewConfiguration<T>(
-  state: ConfigurationState<T>,
-  c: Configuration<T>,
-) {
-  state.added.push({ ...c })
-  state.configurations.push({ ...c, currentHeight: null })
-}
-
-function initializeEmptyState<T>(): ConfigurationState<T> {
-  return {
-    added: [],
-    updated: new Map(),
-    deleted: [],
-    trimmed: [],
-    configurations: [],
-  }
-}
-
-function setUpdated<T>(
-  state: ConfigurationState<T>,
-  c: Configuration<T>,
-  targetHeight: number | null,
-) {
-  // Handle special case when minHeight get change into the future
-  // relative to current configuration state
-  const heightToSet =
-    targetHeight === null
-      ? null
-      : targetHeight < c.minHeight
-        ? null
-        : targetHeight
-
-  const existingConfig = state.updated.get(c.id)
-
-  if (!existingConfig) {
-    state.updated.set(c.id, { ...c, currentHeight: heightToSet })
-    return
-  }
-
-  // If it is already null then do nothing
-  if (existingConfig.currentHeight === null) {
-    return
-  }
-
-  if (heightToSet === null) {
-    state.updated.set(c.id, { ...c, currentHeight: heightToSet })
-  } else if (heightToSet < existingConfig.currentHeight) {
-    state.updated.set(c.id, { ...c, currentHeight: heightToSet })
-  }
-}
-
-function findDeleted<T>(
-  state: ConfigurationState<T>,
-  saved: SavedConfiguration<string>[],
-  knownIds: Set<string>,
-  actual: Configuration<T>[],
-) {
-  state.deleted = saved.filter((s) => !knownIds.has(s.id)).map((c) => c.id)
-
-  const actualMap = new Map(actual.map((c) => [c.id, c]))
 
   for (const c of saved) {
-    if (actualMap.has(c.id) || c.currentHeight === null) {
-      continue
+    if (!maps.actual.has(c.id)) {
+      toDelete.push(c.id)
+
+      if (c.currentHeight !== null) {
+        toRemoveData.push({
+          id: c.id,
+          from: c.minHeight,
+          to: c.currentHeight,
+        })
+      }
     }
-    state.trimmed.push({
-      id: c.id,
-      from: c.minHeight,
-      to: c.currentHeight,
-    })
   }
+
+  return {
+    diff: { toAdd, toUpdate, toDelete, toRemoveData },
+    configurations: configurations,
+    safeHeight: getSafeHeight(configurations),
+  }
+}
+
+function getConfigurationsAsMaps<T>(
+  saved: SavedConfiguration<string>[],
+  actual: Configuration<T>[],
+) {
+  const savedMap = new Map(saved.map((c) => [c.id, c]))
+  const actualMap = new Map()
+
+  assert(actual.length > 0, 'Actual configurations should not be empty')
+  for (const c of actual) {
+    assert(!actualMap.has(c.id), `Configuration ${c.id} is duplicated!`)
+    assert(
+      c.maxHeight === null || c.minHeight <= c.maxHeight,
+      `Configuration ${c.id} has minHeight greater than maxHeight!`,
+    )
+    actualMap.set(c.id, c)
+  }
+
+  return { saved: savedMap, actual: actualMap }
 }
 
 function getSafeHeight<T>(configurations: SavedConfiguration<T>[]) {
