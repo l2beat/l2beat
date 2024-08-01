2a66d8c48252e06729aec0581a6f13c7a949eddc
Piotr Adamczyk <xeno@xeno.yt>
2024-08-01 12:08:11 +0200

    Frontend2 chart fixes

--- packages/frontend2/src/app/_components/chart/controls/chart-time-range-controls.tsx
+++ packages/frontend2/src/app/_components/chart/controls/chart-time-range-controls.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import { useIsClient } from '~/hooks/use-is-client'
+import { type TvlChartRange } from '~/server/features/tvl/range-utils'
 import { formatRange } from '~/utils/dates'
 import { RadioGroup, RadioGroupItem } from '../../radio-group'
 import { Skeleton } from '../../skeleton'
-import { useChartContext } from '../core/chart-context'
+import { useChartLoading } from '../core/chart-loading-context'
 
 interface Props {
-  value: string
-  setValue: (range: string) => void
+  value: TvlChartRange
+  setValue: (range: TvlChartRange) => void
   range: readonly [number, number]
-  options: { value: string; label: string }[]
+  options: { value: TvlChartRange; label: string }[]
 }
 
 export function ChartTimeRangeControls({
@@ -17,29 +19,29 @@ export function ChartTimeRangeControls({
   range,
   options,
 }: Props) {
-  const { loading } = useChartContext()
-
-  if (loading) {
-    return (
-      <div className="flex flex-wrap justify-between gap-2">
-        <Skeleton className="h-8 w-40" />
-        <Skeleton className="h-8 w-[292px]" />
-      </div>
-    )
-  }
+  const loading = useChartLoading()
+  const isClient = useIsClient()
 
   return (
     <div className="flex flex-wrap-reverse justify-between gap-2">
-      <p className="flex h-8 items-center font-bold transition-opacity duration-200">
-        {formatRange(...range)}
-      </p>
-      <RadioGroup value={value} onValueChange={setValue}>
-        {options.map((option) => (
-          <RadioGroupItem key={option.value} value={option.value}>
-            {option.label}
-          </RadioGroupItem>
-        ))}
-      </RadioGroup>
+      {loading ? (
+        <Skeleton className="h-8 w-40" />
+      ) : (
+        <p className="flex h-8 items-center font-bold transition-opacity duration-200">
+          {formatRange(...range)}
+        </p>
+      )}
+      {!isClient ? (
+        <Skeleton className="h-8 w-[292px]" />
+      ) : (
+        <RadioGroup value={value} onValueChange={setValue}>
+          {options.map((option) => (
+            <RadioGroupItem key={option.value} value={option.value}>
+              {option.label}
+            </RadioGroupItem>
+          ))}
+        </RadioGroup>
+      )}
     </div>
   )
 }
--- packages/frontend2/src/app/_components/chart/core/chart-provider.tsx
+++ packages/frontend2/src/app/_components/chart/core/chart-provider.tsx
@@ -6,6 +6,8 @@ import {
   ChartHoverContextProvider,
   type ChartHoverContextProviderParams,
 } from './chart-hover-context'
+import { ChartLoadingContextProvider } from './chart-loading-context'
+import { ChartRectContextProvider } from './chart-rect-context'
 
 export interface ChartProviderProps<T>
   extends ChartContextProviderParams<T>,
@@ -18,9 +20,11 @@ export function ChartProvider<T>({
 }: ChartProviderProps<T>) {
   return (
     <ChartContextProvider {...rest}>
-      <ChartHoverContextProvider renderHoverContents={renderHoverContents}>
-        {children}
-      </ChartHoverContextProvider>
+      <ChartLoadingContextProvider>
+        <ChartHoverContextProvider renderHoverContents={renderHoverContents}>
+          <ChartRectContextProvider>{children}</ChartRectContextProvider>
+        </ChartHoverContextProvider>
+      </ChartLoadingContextProvider>
     </ChartContextProvider>
   )
 }
