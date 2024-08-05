a6cb53affb3e45fa8c3cf1ed26f9182b5e5bd1c8
sekuba <sekuba@users.noreply.github.com>
2024-08-05 15:52:37 +0200

    Move Zircuit in review (#4721)
    Co-authored-by: sekuba <29250140+sekuba@users.noreply.github.com>

--- packages/config/src/projects/layer2s/zircuit.ts
+++ packages/config/src/projects/layer2s/zircuit.ts
@@ -1,13 +1,14 @@
-import { upcomingL2 } from './templates/upcoming'
+import { EthereumAddress, UnixTime } from '@l2beat/shared-pure'
+import { underReviewL2 } from './templates/underReview'
 import { Layer2 } from './types'
 
-export const zircuit: Layer2 = upcomingL2({
+export const zircuit: Layer2 = underReviewL2({
   id: 'zircuit',
   display: {
     name: 'Zircuit',
     slug: 'zircuit',
     description:
-      'Zircuit aims to develop an universal ZK Rollup based on the Optimism Bedrock architecture, and employing AI to identify and stop malicious transactions at the sequencer level.',
+      'Zircuit is a universal ZK Rollup based on the Optimism Bedrock architecture, employing AI to identify and stop malicious transactions at the sequencer level.',
     purposes: ['Universal'],
     category: 'ZK Rollup',
     links: {
@@ -17,9 +18,27 @@ export const zircuit: Layer2 = upcomingL2({
       explorers: ['https://explorer.zircuit.com/'],
       repositories: [],
       socialMedia: [
-        'https://twitter.com/ZircuitL2',
+        'https://x.com/ZircuitL2',
         'https://discord.com/invite/zircuit',
+        'https://zircuit.com/blog',
       ],
     },
+    activityDataSource: 'Blockchain RPC',
   },
+  escrows: [
+    {
+      chain: 'ethereum',
+      address: EthereumAddress('0x17bfAfA932d2e23Bd9B909Fd5B4D2e2a27043fb1'), // OptimismPortal
+      sinceTimestamp: new UnixTime(1719936491),
+      tokens: ['ETH'],
+    },
+    {
+      chain: 'ethereum',
+      address: EthereumAddress('0x17bfAfA932d2e23Bd9B909Fd5B4D2e2a27043fb1'), // OptimismPortal
+      sinceTimestamp: new UnixTime(1719936539),
+      tokens: '*', // will escrow ZRC (no priceData on CG yet)
+    },
+  ],
+  rpcUrl: 'https://zircuit1-mainnet.p2pify.com/', // other: https://zircuit1-mainnet.liquify.com, https://zircuit-mainnet.drpc.org/
+  // Chain ID: 48900
 })
