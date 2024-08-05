f6cbcabb6a9eac917922a74ea1ff24ecbb2e85cd
sekuba <29250140+sekuba@users.noreply.github.com>
2024-08-05 16:44:07 +0200

    add zircuit.ts

--- packages/config/src/projects/layer2s/zircuit.ts
+++ packages/config/src/projects/layer2s/zircuit.ts
@@ -1,16 +1,18 @@
-import { EthereumAddress, UnixTime } from '@l2beat/shared-pure'
-import { underReviewL2 } from './templates/underReview'
+import { UnixTime } from '@l2beat/shared-pure'
+import { ProjectDiscovery } from '../../discovery/ProjectDiscovery'
+import { opStackL2 } from './templates/opStack'
 import { Layer2 } from './types'
 
-export const zircuit: Layer2 = underReviewL2({
-  id: 'zircuit',
+const discovery = new ProjectDiscovery('zircuit')
+
+export const zircuit: Layer2 = opStackL2({
+  discovery,
   display: {
     name: 'Zircuit',
     slug: 'zircuit',
     description:
       'Zircuit is a universal ZK Rollup based on the Optimism Bedrock architecture, employing AI to identify and stop malicious transactions at the sequencer level.',
     purposes: ['Universal'],
-    category: 'ZK Rollup',
     links: {
       websites: ['https://zircuit.com/'],
       apps: ['https://bridge.zircuit.com/'],
@@ -25,20 +27,18 @@ export const zircuit: Layer2 = underReviewL2({
     },
     activityDataSource: 'Blockchain RPC',
   },
-  escrows: [
-    {
-      chain: 'ethereum',
-      address: EthereumAddress('0x17bfAfA932d2e23Bd9B909Fd5B4D2e2a27043fb1'), // OptimismPortal
-      sinceTimestamp: new UnixTime(1719936491),
-      tokens: ['ETH'],
-    },
-    {
-      chain: 'ethereum',
-      address: EthereumAddress('0x386B76D9cA5F5Fb150B6BFB35CF5379B22B26dd8'), // L1StandardBridge
-      sinceTimestamp: new UnixTime(1719936539),
-      tokens: '*', // will escrow ZRC (no priceData on CG yet)
-    },
-  ],
+  genesisTimestamp: new UnixTime(1719936217),
   rpcUrl: 'https://zircuit1-mainnet.p2pify.com/', // other: https://zircuit1-mainnet.liquify.com, https://zircuit-mainnet.drpc.org/
   // Chain ID: 48900
+  usesBlobs: true,
+  isNodeAvailable: 'UnderReview',
+  useDiscoveryMetaOnly: true,
+  milestones: [
+    {
+      name: 'Zircuit Mainnet Launch',
+      link: 'https://www.zircuit.com/blog/zircuit-raises-mainnet-funding-round', // WRONG LINK
+      date: '2024-08-05T00:00:00.00Z',
+      description: 'Zircuit is live on mainnet.',
+    },
+  ],
 })
