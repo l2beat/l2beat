b4c35844e86fdccd940810210c904e485eeb602d
Radina Talanova <nt.radina@gmail.com>
2024-08-05 15:28:39 +0300

    format

--- packages/config/src/projects/layer2s/layerai.ts
+++ packages/config/src/projects/layer2s/layerai.ts
@@ -7,23 +7,17 @@ export const layerai: Layer2 = upcomingL2({
     name: 'LayerAI',
     slug: 'layerai',
     description:
-      "LayerAI is the dedicated blockchain network for data monetization within AI models. It functions as a sophisticated ecosystem strategically crafted to expedite the rise of the data economy. The LayerAI ecosystem empowers individuals to step into a transformative epoch where they can own, control, and capitalize on their most substantial digital asset – Data.",
+      'LayerAI is the dedicated blockchain network for data monetization within AI models. It functions as a sophisticated ecosystem strategically crafted to expedite the rise of the data economy. The LayerAI ecosystem empowers individuals to step into a transformative epoch where they can own, control, and capitalize on their most substantial digital asset – Data.',
     purposes: ['AI'],
     category: 'ZK Rollup',
     provider: 'Polygon',
     links: {
       websites: ['https://layerai.org/'],
-      apps: [
-        'https://kyotox.com/',
-        'https://ayervpn.com/'
-      ],
+      apps: ['https://kyotox.com/', 'https://ayervpn.com/'],
       documentation: ['https://docs.layerai.org/'],
       explorers: [],
       repositories: [],
-      socialMedia: [
-        'https://t.me/LayerAI_org',
-        'https://x.com/LayerAIorg',
-      ],
+      socialMedia: ['https://t.me/LayerAI_org', 'https://x.com/LayerAIorg'],
     },
   },
 })
