15513974825c10a6acb1d88230d7457c155de47e
Radina Talanova <nt.radina@gmail.com>
2024-08-05 16:07:15 +0300

    Fix

--- packages/config/src/projects/layer3s/unite.ts
+++ packages/config/src/projects/layer3s/unite.ts
@@ -9,7 +9,7 @@ export const unite: Layer3 = upcomingL3({
     name: 'Unite',
     slug: 'unite',
     description:
-      'Unite Blockchain is a revolutionary Layer 3 (L3) EVM-compatible blockchain, designed to transform the landscape of mobile web3 gaming. It is First L3 blockchain solution for Mass-Market Mobile Games.',
+      'Unite Blockchain is a revolutionary Layer 3 (L3) EVM-compatible blockchain, designed to transform the landscape of mobile web3 gaming. It is the first L3 blockchain solution for Mass-Market Mobile Games.',
     purposes: ['Gaming'],
     category: 'Optimistic Rollup',
     provider: 'OP Stack',
