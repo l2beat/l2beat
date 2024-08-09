016cdf0dbbe0bf93aa89eef6f49d31f3f43e9627
torztomasz <tomasz.torz@l2beat.com>
2024-08-09 08:45:55 +0200

    remove test route

--- packages/frontend2/src/app/api/test/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server'
-import { getLatestTvlUsd } from '~/server/features/tvl/get-latest-tvl-usd'
-
-export async function GET() {
-  const latestTvlValues = await getLatestTvlUsd()
-
-  return NextResponse.json(latestTvlValues)
-}
