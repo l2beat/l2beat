a11e61950bdf80cd405ba05c683d1c87505d354d
torztomasz <tomasz.torz@l2beat.com>
2024-08-09 09:19:31 +0200

    fix test

--- packages/config/src/test/projects.test.ts
+++ packages/config/src/test/projects.test.ts
@@ -1,4 +1,3 @@
-import { assert } from '@l2beat/backend-tools'
 import { EthereumAddress, ProjectId } from '@l2beat/shared-pure'
 import { expect } from 'earl'
 
@@ -164,14 +163,13 @@ describe('projects', () => {
     })
   })
 
-  it('has an activityDataSource defined if transaction data API is set', () => {
+  describe('has an activityDataSource defined if transaction data API is set', () => {
     for (const project of [...layer2s, ...layer3s]) {
-      if (project.config.transactionApi) {
-        assert(
-          project.display.activityDataSource,
-          `activityDataSource not defined for ${project.id.toString()}`,
-        )
-      }
+      it(project.display.name, () => {
+        if (project.config.transactionApi) {
+          expect(project.display.activityDataSource).toBeTruthy()
+        }
+      })
     }
   })
 })
