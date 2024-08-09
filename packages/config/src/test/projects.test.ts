c415696066c5ef173b98252dc9cf56dfaa9a9a37
torztomasz <tomasz.torz@l2beat.com>
2024-08-09 08:58:14 +0200

    Include l3s in activity data source test

--- packages/config/src/test/projects.test.ts
+++ packages/config/src/test/projects.test.ts
@@ -2,7 +2,7 @@ import { assert } from '@l2beat/backend-tools'
 import { EthereumAddress, ProjectId } from '@l2beat/shared-pure'
 import { expect } from 'earl'
 
-import { bridges, layer2s } from '../'
+import { bridges, layer2s, layer3s } from '../'
 import { checkRisk } from './helpers'
 
 describe('projects', () => {
@@ -165,7 +165,7 @@ describe('projects', () => {
   })
 
   it('has an activityDataSource defined if transaction data API is set', () => {
-    for (const project of layer2s) {
+    for (const project of [...layer2s, ...layer3s]) {
       if (project.config.transactionApi) {
         assert(
           project.display.activityDataSource,
