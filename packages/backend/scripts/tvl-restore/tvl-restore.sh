1351e1be0e33113d7e76cd8ced61da5b9a978f4b
antooni <antoni.pawlak@protonmail.com>
2024-08-09 10:18:26 +0200

    correct message

--- packages/backend/scripts/tvl-restore/tvl-restore.sh
+++ packages/backend/scripts/tvl-restore/tvl-restore.sh
@@ -1,17 +1,21 @@
+#!/bin/bash
+
 echo "Setting up data folder"
-rm -rf data 2>/dev/null && mkdir data
+rm -rf data 2>/dev/null && mkdir data &&
 
-echo "Fetching data from remote DB"
-psql $DEV_REMOTE_DB_URL_READ_ONLY -f export.sql
+echo "Clearing local TVL tables" &&
+psql $DEV_LOCAL_DB_URL -f reset-local.sql &&
 
-echo "Resetting local DB"
-psql $DEV_LOCAL_DB_URL -f reset-local.sql
+echo "Migrating DB to latest" &&
+node -r esbuild-register migrateDb.ts &&
 
-echo "Migrating DB to latest"
-node -r esbuild-register migrateDb.ts
+echo "Fetching TVL tables from remote DB" &&
+psql $DEV_REMOTE_DB_URL_READ_ONLY -f export.sql &&
 
-echo "Restoring tables"
-psql $DEV_LOCAL_DB_URL -f import.sql
+echo "Restoring TVL tables" &&
+psql $DEV_LOCAL_DB_URL -f import.sql &&
 
-echo "Removing data folder"
-rm -rf data
+echo "Removing data folder" &&
+rm -rf data &&
+
+echo "✅ TVL data restored"
