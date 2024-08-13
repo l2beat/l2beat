409d8e99bebdf6b42bb33d3ff17f154008742059
torztomasz <tomasz.torz@l2beat.com>
2024-08-13 08:44:25 +0200

    fix build

--- packages/database/src/other/anomalies/repository.ts
+++ packages/database/src/other/anomalies/repository.ts
@@ -44,6 +44,19 @@ export class AnomaliesRepository extends BaseRepository {
     return rows.map(toRecord)
   }
 
+  async getByProjectIdFrom(
+    projectId: ProjectId,
+    from: UnixTime,
+  ): Promise<AnomalyRecord[]> {
+    const rows = await this.db
+      .selectFrom('public.anomalies')
+      .select(selectAnomaly)
+      .where('project_id', '=', projectId)
+      .where('timestamp', '>=', from.toDate())
+      .execute()
+    return rows.map(toRecord)
+  }
+
   async getByProjectIdsFrom(
     projectIds: ProjectId[],
     from: UnixTime,
