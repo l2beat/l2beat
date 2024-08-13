925407501fd3d01acc73c6851abc4b5c56e2e056
Antoni Pawlak <antoni.pawlak@pm.me>
2024-08-13 12:38:50 +0200

    Add logs to RPCClient (#4781)

--- packages/backend/src/peripherals/getBlockNumberAtOrBefore.ts
+++ packages/backend/src/peripherals/getBlockNumberAtOrBefore.ts
@@ -1,3 +1,4 @@
+import { Logger } from '@l2beat/backend-tools'
 import { UnixTime } from '@l2beat/shared-pure'
 
 export async function getBlockNumberAtOrBefore(
@@ -5,10 +6,13 @@ export async function getBlockNumberAtOrBefore(
   start: number,
   end: number,
   getBlock: (number: number) => Promise<{ timestamp: number }>,
+  logger?: Logger,
 ): Promise<number> {
   while (start + 1 < end) {
     const mid = start + Math.floor((end - start) / 2)
+    if (logger) logger.info('Fetching block', { start, end, mid })
     const midBlock = await getBlock(Number(mid))
+    if (logger) logger.info('Fetched block', { block: mid })
     const midTimestamp = new UnixTime(midBlock.timestamp)
     if (midTimestamp.lte(timestamp)) {
       start = mid
