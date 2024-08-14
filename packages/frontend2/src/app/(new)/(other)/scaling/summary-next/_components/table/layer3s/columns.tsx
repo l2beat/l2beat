f585fa2be98fbca9e5b44100730478c4cf147b07
torztomasz <tomasz.torz@l2beat.com>
2024-08-14 16:37:47 +0200

    fix format

--- packages/frontend2/src/app/(new)/(other)/scaling/summary-next/_components/table/layer3s/columns.tsx
+++ packages/frontend2/src/app/(new)/(other)/scaling/summary-next/_components/table/layer3s/columns.tsx
@@ -7,8 +7,8 @@ import {
   TypeCell,
   TypeExplanationTooltip,
 } from '~/app/_components/table/cells/type-cell'
-import { TotalCell } from '../total-cell'
 import { type ScalingSummaryTableRow } from '../../../_utils/to-table-rows'
+import { TotalCell } from '../total-cell'
 
 const columnHelper = createColumnHelper<ScalingSummaryTableRow>()
 
