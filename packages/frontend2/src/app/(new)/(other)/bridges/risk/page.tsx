98d78cbd6db9da62b3653847127b3a292c748805
Michał Podsiadły <michal.podsiadly@l2beat.com>
2024-08-14 14:28:23 +0200

    chore: unify layout across scaling and bridges

--- packages/frontend2/src/app/(new)/(other)/bridges/risk/page.tsx
+++ packages/frontend2/src/app/(new)/(other)/bridges/risk/page.tsx
@@ -1,9 +1,6 @@
 import { BridgesMvpWarning } from '~/app/_components/bridges-mvp-warning'
 import { SimplePageHeader } from '~/app/_components/simple-page-header'
 import { getBridgeRiskEntries } from '~/server/features/bridges/get-bridge-risk-entries'
-import { getImplementationChangeReport } from '~/server/features/implementation-change-report/get-implementation-change-report'
-import { getLatestTvlUsd } from '~/server/features/scaling/tvl/utils/get-latest-tvl-usd'
-import { getProjectsVerificationStatuses } from '~/server/features/verification-status/get-projects-verification-statuses'
 import { getDefaultMetadata } from '~/utils/get-default-metadata'
 import { BridgesFilterContextProvider } from '../_components/bridges-filter-context'
 import { BridgesRiskTables } from './_components/table/bridges-risks-tables'
@@ -15,18 +12,7 @@ export const metadata = getDefaultMetadata({
 })
 
 export default async function Page() {
-  const [tvl, projectsVerificationStatuses, implementationChangeReport] =
-    await Promise.all([
-      getLatestTvlUsd(),
-      getProjectsVerificationStatuses(),
-      getImplementationChangeReport(),
-    ])
-
-  const entries = await getBridgeRiskEntries(
-    tvl,
-    implementationChangeReport,
-    projectsVerificationStatuses,
-  )
+  const entries = await getBridgeRiskEntries()
 
   return (
     <BridgesFilterContextProvider>
