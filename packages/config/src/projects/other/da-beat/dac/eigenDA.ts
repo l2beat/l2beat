b152d21f05875c2822a46593216222133b49186f
vincfurc <10850139+vincfurc@users.noreply.github.com>
2024-08-16 08:42:27 +0100

    eigen config

---
+++ packages/config/src/projects/other/da-beat/dac/eigenDA.ts
@@ -0,0 +1,31 @@
+import { DaEconomicSecurityRisk, DaFraudDetectionRisk } from '../types'
+import { DaLayer } from '../types/DaLayer'
+import { edgelessDac } from './bridges/edgeless'
+
+export const eigenDA: DaLayer = {
+  id: 'eigen-da',
+  type: 'DaLayer',
+  kind: 'DAC',
+  display: {
+    name: 'Eigen DA',
+    slug: 'eigenda',
+    description:
+      'EigenDA is a data availability solution built on Eigen Layer.',
+    links: {
+      websites: [],
+      documentation: [],
+      repositories: [],
+      apps: [],
+      explorers: [],
+      socialMedia: [],
+    },
+  },
+  technology: `
+  `,
+  bridges: [],
+  usedIn: [],
+  risks: {
+    economicSecurity: DaEconomicSecurityRisk.Unknown,
+    fraudDetection: DaFraudDetectionRisk.NoFraudDetection,
+  },
+}
