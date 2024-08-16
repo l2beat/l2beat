2e727154a65161a195441c9842338d759c397f75
torztomasz <tomasz.torz@l2beat.com>
2024-08-16 10:36:40 +0200

    improve chart section

--- packages/frontend2/src/app/_components/projects/sections/chart-section.tsx
+++ packages/frontend2/src/app/_components/projects/sections/chart-section.tsx
@@ -1,9 +1,15 @@
 import { type Milestone } from '@l2beat/config'
 import { ProjectSection } from './project-section'
-import { type ProjectSectionProps } from './types'
+import { type ProjectSectionId, type ProjectSectionProps } from './types'
 import { ProjectCostsChart } from '../../chart/costs/project-costs-chart'
 
+type ChartSectionId = Extract<
+  ProjectSectionId,
+  'onchain-costs' | 'tvl' | 'activity'
+>
+
 export interface ChartSectionProps extends ProjectSectionProps {
+  id: ChartSectionId
   projectId: string
   milestones: Milestone[]
 }
@@ -15,7 +21,27 @@ export function ChartSection({
 }: ChartSectionProps) {
   return (
     <ProjectSection {...sectionProps}>
-      <ProjectCostsChart milestones={milestones} projectId={projectId} />
+      <ProjectChart
+        id={sectionProps.id}
+        milestones={milestones}
+        projectId={projectId}
+      />
     </ProjectSection>
   )
+}
+
+interface ProjectChartProps {
+  id: ChartSectionId
+  projectId: string
+  milestones: Milestone[]
+}
+
+function ProjectChart({ id, projectId, milestones }: ProjectChartProps) {
+  switch (id) {
+    case 'tvl':
+    case 'activity':
+      throw new Error('Not implemented')
+    case 'onchain-costs':
+      return <ProjectCostsChart milestones={milestones} projectId={projectId} />
+  }
 }
