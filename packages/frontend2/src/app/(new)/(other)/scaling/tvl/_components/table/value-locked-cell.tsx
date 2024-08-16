04ee14d75f4a6a41834ec5e2e5f0b43f69b9b039
Piotr Adamczyk <xeno@xeno.yt>
2024-08-16 10:22:51 +0200

    Do not display empty values

--- packages/frontend2/src/app/(new)/(other)/scaling/tvl/_components/table/value-locked-cell.tsx
+++ packages/frontend2/src/app/(new)/(other)/scaling/tvl/_components/table/value-locked-cell.tsx
@@ -1,4 +1,5 @@
 import { PercentChange } from '~/app/_components/percent-change'
+import { EM_DASH } from '~/consts/characters'
 import { formatNumber } from '~/utils/format-number'
 
 export interface ValueLockedCellProps {
@@ -8,13 +9,14 @@ export interface ValueLockedCellProps {
 }
 
 export function ValueLockedCell(data: ValueLockedCellProps) {
+  const showChange = data.change !== undefined && data.value !== 0
   return (
     <div className="flex flex-col items-end">
       <div className="flex items-center gap-1">
         <span className="text-base font-bold md:text-lg">
-          ${formatNumber(data.value)}
+          {data.value === 0 ? EM_DASH : formatNumber(data.value)}
         </span>
-        {data.change !== undefined && (
+        {showChange && data.change !== undefined && (
           <PercentChange
             value={data.change}
             className="ml-1 !text-base font-medium"
