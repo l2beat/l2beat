a3b957f3dd85e5b2a91e30cb6378b4569c5ce691
Piotr Adamczyk <xeno@xeno.yt>
2024-08-19 14:21:37 +0200

    Merge old config

--- packages/frontend2/.storybook/preview.tsx
+++ packages/frontend2/.storybook/preview.tsx
@@ -4,6 +4,8 @@ import { type Preview } from '@storybook/react'
 import { withThemeByClassName } from '@storybook/addon-themes'
 
 import { roboto } from '../src/fonts'
+import { viewports } from './viewports'
+import { allModes } from './modes'
 import '../src/styles/globals.css'
 
 const withFonts = () => (Story: () => ReactNode) => {
@@ -16,11 +18,19 @@ const withFonts = () => (Story: () => ReactNode) => {
 
 const preview: Preview = {
   parameters: {
+    layout: 'fullscreen',
     controls: {
+      hideNoControlsWarning: true,
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/i,
+        date: /Date$/,
       },
+    },
+    viewport: {
+      viewports,
+    },
+    chromatic: {
+      modes: allModes,
     },
   },
   decorators: [
