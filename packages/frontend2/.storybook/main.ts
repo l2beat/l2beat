5caad4b60688b6f745c19e3ce0f10c7070d9289c
Piotr Adamczyk <xeno@xeno.yt>
2024-08-19 14:26:27 +0200

    Remove onboarding addon, add pseudo states

--- packages/frontend2/.storybook/main.ts
+++ packages/frontend2/.storybook/main.ts
@@ -12,12 +12,12 @@ function getAbsolutePath(value: string) {
 const config = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    getAbsolutePath('@storybook/addon-onboarding'),
     getAbsolutePath('@storybook/addon-links'),
     getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@chromatic-com/storybook'),
     getAbsolutePath('@storybook/addon-interactions'),
     getAbsolutePath('@storybook/addon-themes'),
+    getAbsolutePath('storybook-addon-pseudo-states'),
   ],
   /**
    * Keep in sync with the webpack configuration in next.config.js.
