e1ede39aa1f3c2c31535954f84f3a48d84f1d752
vincfurc <10850139+vincfurc@users.noreply.github.com>
2024-08-19 15:27:20 +0100

    technology

--- packages/config/src/projects/other/da-beat/dac/eigenDA.ts
+++ packages/config/src/projects/other/da-beat/dac/eigenDA.ts
@@ -21,15 +21,29 @@ export const eigenDA: DaLayer = {
     },
   },
   technology: `
+
+    ## Architecture
     EigenDA is composed by three types of off-chain entities: node operators, a disperser and a retriever.
-    EigenDA operators are a network of node operators running the EigenDA node software and registered to the EigenDA AVS in EigenLayer.
-    Operators register with the EigenDA Service Manager via the registerOperatorToAVS() function, enabling them to participate in the data availability layer. They are resposible fo holding and serving blobs data, and earn rewards for their participation in the network.
-    The disperser is the entity resposible for collecting the blobs from the sequencer, erasure coding them and generating the encoded blob's KZG commitments for each chunk. 
-    Once the chunks, KZG commitments, and KZG proofs are sent to the operators, the disperser collects signatures from them. The aggregated signatures are then uploaded to the Ethereum network as part of the data sent to the EigenDA bridge.
-    The disperser uses direct communication to EigenDA operators through unicast, rather than standard P2P gossiping. Although the disperser could be rollup-operated, it is currently a centralised entity operated by Eigen Labs.
-    Lastly, the retriever queries the EigenDA operators to retrieve blob chunks, verifies their accuracy, and reconstructs the original blob. The retriever service is hosted by Eigen Labs, but client rollups may also implement their own retriever as a sidecar to their sequencer.
+    - EigenDA **operators** are node operators running the EigenDA node software and are registered to the EigenDA AVS in EigenLayer.
+    - The **disperser** is the entity resposible for collecting the blobs from the sequencer, erasure coding them and generating the encoded blob's KZG commitments for each chunk. Although the disperser could be rollup-operated, it is currently a centralised entity operated by Eigen Labs.
+    - Lastly, the **retriever** client is responsible for querying the EigenDA operators to retrieve blob chunks, verifying their integrity and reconstructs the original blob. 
+    
+    Operators register with the EigenDAServiceManager via the registerOperatorToAVS() function, enabling them to participate in the data availability network. They are resposible for holding and serving blobs data, and earn rewards for their participation in the network.
+    The process of storing a blob on EigenDA works as follows. A sequencer submits blobs to the EigenDA Disperser, which erasure codes the blobs into chunks and generates KZG commitments and proofs for each chunk, certifying the correctness of the data. The disperser then sends the chunks, KZG commitments, and KZG proofs to the operators.
+    Multiple operators are responsible for storing chunks of the encoded data blobs and their associated KZG commitment and proof.
+    Once the chunks, KZG commitments, and KZG proofs are sent to the operators, each of them generates a signature certifying that they have stored the data. These signatures are then sent to the Disperser which aggregates them and uploads them to Ethereum by sending a transaction to the EigenDAServiceManager (the DA bridge).
     
     ![EigenDA storing/retrieving](/images/da-layer-technology/eigenda/storing-retrieving.png#center)
+
+    ## L2 Data Availability
+    The Disperser collects the operators' signatures and submits them to the EigenDAServiceManager contract via the confirmBatch() function. This submission includes a call to the BLSRegistry contract to verify signatures and check whether the required quorum of operators' stake has been achieved.
+    Threshold BLS signatures are not used. Instead, the threshold check is performed on the signers' total stake fetched by the StakeRegistry, and the stake threshold percentage to reach is provided in the batch header input data.
+
+    ![EigenDA once stored](/images/da-layer-technology/eigenda/oncestored.png#center)
+
+    The EigenDARollupUtils.sol library's verifyBlob() function can then used by scaling solutions to verify that a data blob is included within a confirmed batch in the EigenDAServiceManager. 
+
+  
 
   `,
   bridges: [eigenDAbridge],
