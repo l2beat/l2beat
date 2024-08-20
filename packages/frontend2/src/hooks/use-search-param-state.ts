ff83a96200bdeef3cfe7ae9f66e715445693aef0
Piotr Adamczyk <xeno@xeno.yt>
2024-08-20 17:23:35 +0200

    Preserve selected tab in search params (#4816)

---
+++ packages/frontend2/src/hooks/use-search-param-state.ts
@@ -0,0 +1,40 @@
+'use client'
+
+import { usePathname, useSearchParams } from 'next/navigation'
+import { useRouter } from 'next/router'
+
+/**
+ * Use state that is persisted in a search param.
+ * @param name search param name
+ * @returns array containing the current value and a function to update it
+ */
+export function useSearchParamState<T extends string | undefined = string>(
+  key: string,
+  defaultValue: T,
+): [string | undefined, (value: T) => void] {
+  const router = useRouter()
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
+  const value = searchParams.get(key) ?? defaultValue
+
+  const setValue = (value: T) => {
+    const search = new URLSearchParams(searchParams)
+
+    if (value !== defaultValue && value !== undefined) {
+      search.set(key, value)
+    } else {
+      search.delete(key)
+    }
+
+    const stringified = search.toString()
+
+    if (stringified) {
+      void router.replace(`${pathname}?${stringified}`)
+    } else {
+      void router.replace(pathname)
+    }
+  }
+
+  return [value, setValue]
+}
