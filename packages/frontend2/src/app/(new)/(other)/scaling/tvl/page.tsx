392ea623459265d2d6755eff020ce0e1623def2d
torztomasz <tomasz.torz@l2beat.com>
2024-08-20 12:54:16 +0200

    fix import

--- packages/frontend2/src/app/(new)/(other)/scaling/tvl/page.tsx
+++ packages/frontend2/src/app/(new)/(other)/scaling/tvl/page.tsx
@@ -4,13 +4,13 @@ import { HOMEPAGE_MILESTONES } from '@l2beat/config'
 import { HorizontalSeparator } from '~/app/_components/horizontal-separator'
 import { getImplementationChangeReport } from '~/server/features/implementation-change-report/get-implementation-change-report'
 import { getScalingTvlEntries } from '~/server/features/scaling/tvl/get-scaling-tvl-entries'
-import { get7dTvlBreakdown } from '~/server/features/scaling/tvl/utils/get-detailed-7d-tvl-breakdown'
 import { getProjectsVerificationStatuses } from '~/server/features/verification-status/get-projects-verification-statuses'
 import { HydrateClient, api } from '~/trpc/server'
 import { getCookie } from '~/utils/cookies/server'
 import { ScalingFilterContextProvider } from '../../_components/scaling-filter-context'
 import { ScalingTvlTable } from './_components/scaling-tvl-table'
 import { TvlChart } from './_components/tvl-chart'
+import { get7dTvlBreakdown } from '~/server/features/scaling/tvl/utils/get-7d-tvl-breakdown'
 
 export const metadata = getDefaultMetadata({
   openGraph: {
