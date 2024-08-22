d3dfef5c56c031e598f45436e46b8dbbff1e212c
Maciej Zygmunt <maciekzygmunt@interia.pl>
2024-08-22 11:27:35 +0200

    Remove old Activity code and data (#4831)

--- packages/backend/src/config/makeConfig.ts
+++ packages/backend/src/config/makeConfig.ts
@@ -12,13 +12,10 @@ import { ChainId, UnixTime } from '@l2beat/shared-pure'
 import { Config, DiscordConfig } from './Config'
 import { FeatureFlags } from './FeatureFlags'
 import {
+  getChainActivityBlockExplorerConfig,
   getChainActivityConfig,
   getProjectsWithActivity,
 } from './features/activity'
-import {
-  getChainActivityBlockExplorerConfig,
-  getProjectsWithActivity2,
-} from './features/activity2'
 import { getFinalityConfigurations } from './features/finality'
 import { getTvlConfig } from './features/tvl'
 import { getChainDiscoveryConfig } from './features/updateMonitor'
@@ -180,25 +177,6 @@ export function makeConfig(
         [],
       projects: getProjectsWithActivity()
         .filter((x) => flags.isEnabled('activity', x.id.toString()))
-        .map((x) => ({ id: x.id, config: getChainActivityConfig(env, x) })),
-    },
-    activity2: flags.isEnabled('activity2') && {
-      starkexApiKey: env.string([
-        'STARKEX_API_KEY_FOR_ACTIVITY',
-        'STARKEX_API_KEY',
-      ]),
-      starkexCallsPerMinute: env.integer(
-        [
-          'STARKEX_API_CALLS_PER_MINUTE_FOR_ACTIVITY',
-          'STARKEX_API_CALLS_PER_MINUTE',
-        ],
-        600,
-      ),
-      projectsExcludedFromAPI:
-        env.optionalString('ACTIVITY_PROJECTS_EXCLUDED_FROM_API')?.split(' ') ??
-        [],
-      projects: getProjectsWithActivity2()
-        .filter((x) => flags.isEnabled('activity2', x.id.toString()))
         .map((x) => ({
           id: x.id,
           config: getChainActivityConfig(env, x),
--- packages/database/src/activity/activity-starkex/select.ts
+++
@@ -1,7 +0,0 @@
-import { Starkex } from '../../kysely/generated/types'
-
-export const selectStarkExTransactionCount = [
-  'unix_timestamp',
-  'project_id',
-  'count',
-] as const satisfies (keyof Starkex)[]
