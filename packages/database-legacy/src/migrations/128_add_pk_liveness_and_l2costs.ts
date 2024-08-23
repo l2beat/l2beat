ab9f0d431478177f2d54bc9d16fa93419de3ca38
maciekzygmunt <maciekzygmunt@interia.pl>
2024-08-23 12:26:53 +0200

    align primary keys

---
+++ packages/database-legacy/src/migrations/128_add_pk_liveness_and_l2costs.ts
@@ -0,0 +1,34 @@
+/*
+                      ====== IMPORTANT NOTICE ======
+
+DO NOT EDIT OR RENAME THIS FILE
+
+This is a migration file. Once created the file should not be renamed or edited,
+because migrations are only run once on the production server. 
+
+If you find that something was incorrectly set up in the `up` function you
+should create a new migration file that fixes the issue.
+
+*/
+
+import { Knex } from 'knex'
+
+export async function up(knex: Knex) {
+  await knex.schema.alterTable('l2_costs', function (table) {
+    table.primary(['tx_hash', 'configuration_id'])
+  })
+
+  await knex.schema.alterTable('liveness', function (table) {
+    table.primary(['tx_hash', 'configuration_id'])
+  })
+}
+
+export async function down(knex: Knex) {
+  await knex.schema.alterTable('l2_costs', function (table) {
+    table.dropPrimary()
+  })
+
+  await knex.schema.alterTable('liveness', function (table) {
+    table.dropPrimary()
+  })
+}
