02e7c273dd65cfd0a0407aed79585cc3facfe826
maciekop-l2b <maciej.opala@l2beat.com>
2024-08-23 15:34:22 +0200

    Align prisma models to current state of db

--- packages/database/prisma/migrations/0_init/migration.sql
+++ packages/database/prisma/migrations/0_init/migration.sql
@@ -1,12 +1,3 @@
--- CreateSchema
-CREATE SCHEMA IF NOT EXISTS "public";
-
--- CreateEnum
-CREATE TYPE "public"."ExternalBridgeType" AS ENUM ('Axelar', 'LayerZeroV1', 'Orbit', 'Wormhole');
-
--- CreateEnum
-CREATE TYPE "public"."ExplorerType" AS ENUM ('Etherscan');
-
 -- CreateTable
 CREATE TABLE "public"."CurrentPrice" (
     "coingeckoId" VARCHAR(255) NOT NULL,
@@ -19,144 +10,10 @@ CREATE TABLE "public"."CurrentPrice" (
 -- CreateTable
 CREATE TABLE "public"."Stake" (
     "id" VARCHAR(255) NOT NULL,
-    "totalStake" BIGINT NOT NULL,
-    "thresholdStake" BIGINT NOT NULL,
+    "totalStake" REAL NOT NULL,
+    "thresholdStake" REAL NOT NULL,
 
     CONSTRAINT "Stake_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "public"."BridgeEscrow" (
-    "id" CHAR(21) NOT NULL,
-    "networkId" CHAR(21) NOT NULL,
-    "address" TEXT NOT NULL,
-    "externalBridgeId" CHAR(21),
-    "canonicalNetworkId" CHAR(21),
-    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "BridgeEscrow_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "public"."ExternalBridge" (
-    "id" CHAR(21) NOT NULL,
-    "name" VARCHAR(256) NOT NULL,
-    "type" "public"."ExternalBridgeType" NOT NULL,
-    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "ExternalBridge_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "public"."Deployment" (
-    "id" CHAR(21) NOT NULL,
-    "tokenId" TEXT NOT NULL,
-    "txHash" CHAR(66),
-    "blockNumber" INTEGER,
-    "timestamp" TIMESTAMP(6),
-    "from" TEXT,
-    "to" TEXT,
-    "isDeployerEoa" BOOLEAN,
-    "sourceAvailable" BOOLEAN NOT NULL,
-    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Deployment_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "public"."NetworkRpc" (
-    "id" CHAR(21) NOT NULL,
-    "networkId" CHAR(21) NOT NULL,
-    "url" TEXT NOT NULL,
-    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "NetworkRpc_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "public"."NetworkExplorer" (
-    "id" CHAR(21) NOT NULL,
-    "networkId" CHAR(21) NOT NULL,
-    "type" "public"."ExplorerType" NOT NULL,
-    "url" TEXT NOT NULL,
-    "apiKey" TEXT NOT NULL,
-    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "NetworkExplorer_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "public"."Network" (
-    "id" CHAR(21) NOT NULL,
-    "chainId" INTEGER NOT NULL,
-    "name" VARCHAR(256) NOT NULL,
-    "coingeckoId" VARCHAR(256),
-    "axelarId" VARCHAR(256),
-    "axelarGatewayAddress" CHAR(42),
-    "orbitId" VARCHAR(256),
-    "wormholeId" VARCHAR(256),
-    "layerZeroV1EndpointAddress" CHAR(42),
-    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Network_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "public"."TokenBridge" (
-    "id" CHAR(21) NOT NULL,
-    "sourceTokenId" CHAR(21) NOT NULL,
-    "targetTokenId" CHAR(21) NOT NULL,
-    "externalBridgeId" CHAR(21),
-    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "TokenBridge_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "public"."TokenMeta" (
-    "id" CHAR(21) NOT NULL,
-    "tokenId" CHAR(21) NOT NULL,
-    "externalId" VARCHAR(256),
-    "source" VARCHAR(256) NOT NULL,
-    "name" VARCHAR(256),
-    "symbol" VARCHAR(32),
-    "decimals" INTEGER,
-    "logoUrl" VARCHAR(256),
-    "contractName" VARCHAR(256),
-    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "TokenMeta_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "public"."Token" (
-    "id" CHAR(21) NOT NULL,
-    "networkId" CHAR(21) NOT NULL,
-    "address" TEXT NOT NULL,
-    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Token_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "public"."Cache" (
-    "key" TEXT NOT NULL,
-    "value" TEXT NOT NULL,
-    "chainId" INTEGER NOT NULL,
-    "blockNumber" INTEGER,
-    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Cache_pkey" PRIMARY KEY ("key")
 );
 
 -- CreateTable
@@ -164,8 +21,8 @@ CREATE TABLE "public"."activity" (
     "project_id" VARCHAR(255) NOT NULL,
     "timestamp" TIMESTAMP(6) NOT NULL,
     "count" INTEGER NOT NULL,
-    "start" INTEGER NOT NULL,
-    "end" INTEGER NOT NULL,
+    "start" INTEGER NOT NULL DEFAULT 0,
+    "end" INTEGER NOT NULL DEFAULT 0,
 
     CONSTRAINT "activity_pkey" PRIMARY KEY ("project_id","timestamp")
 );
@@ -278,7 +135,9 @@ CREATE TABLE "public"."l2_costs" (
     "calldata_gas_used" INTEGER NOT NULL,
     "calldata_length" INTEGER NOT NULL,
     "blob_gas_used" INTEGER,
-    "blob_gas_price" BIGINT
+    "blob_gas_price" BIGINT,
+
+    CONSTRAINT "l2_costs_pkey" PRIMARY KEY ("configuration_id","tx_hash")
 );
 
 -- CreateTable
@@ -296,7 +155,7 @@ CREATE TABLE "public"."liveness" (
     "tx_hash" VARCHAR(255) NOT NULL,
     "configuration_id" VARCHAR(12) NOT NULL,
 
-    CONSTRAINT "liveness_configuration_id_index" PRIMARY KEY ("configuration_id")
+    CONSTRAINT "liveness_pkey" PRIMARY KEY ("configuration_id","tx_hash")
 );
 
 -- CreateTable
@@ -359,17 +218,17 @@ CREATE TABLE "public"."values" (
     "timestamp" TIMESTAMP(6) NOT NULL,
     "project_id" VARCHAR(255) NOT NULL,
     "data_source" VARCHAR(255) NOT NULL,
-    "external" BIGINT NOT NULL,
+    "external" BIGINT,
     "external_associated" BIGINT NOT NULL DEFAULT 0,
-    "external_for_total" BIGINT NOT NULL,
+    "external_for_total" BIGINT,
     "external_associated_for_total" BIGINT NOT NULL DEFAULT 0,
-    "canonical" BIGINT NOT NULL,
+    "canonical" BIGINT,
     "canonical_associated" BIGINT NOT NULL DEFAULT 0,
-    "canonical_for_total" BIGINT NOT NULL,
+    "canonical_for_total" BIGINT,
     "canonical_associated_for_total" BIGINT NOT NULL DEFAULT 0,
-    "native" BIGINT NOT NULL,
+    "native" BIGINT,
     "native_associated" BIGINT NOT NULL DEFAULT 0,
-    "native_for_total" BIGINT NOT NULL,
+    "native_for_total" BIGINT,
     "native_associated_for_total" BIGINT NOT NULL DEFAULT 0,
     "ether" BIGINT NOT NULL DEFAULT 0,
     "stablecoin" BIGINT NOT NULL DEFAULT 0,
@@ -410,45 +269,6 @@ CREATE TABLE "public"."anomalies" (
     CONSTRAINT "anomalies_pkey" PRIMARY KEY ("timestamp","project_id","subtype")
 );
 
--- CreateTable
-CREATE TABLE "public"."_BridgeEscrowToToken" (
-    "A" CHAR(21) NOT NULL,
-    "B" CHAR(21) NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "BridgeEscrow_networkId_address_key" ON "public"."BridgeEscrow"("networkId", "address");
-
--- CreateIndex
-CREATE UNIQUE INDEX "ExternalBridge_type_key" ON "public"."ExternalBridge"("type");
-
--- CreateIndex
-CREATE UNIQUE INDEX "Deployment_tokenId_key" ON "public"."Deployment"("tokenId");
-
--- CreateIndex
-CREATE UNIQUE INDEX "NetworkExplorer_networkId_key" ON "public"."NetworkExplorer"("networkId");
-
--- CreateIndex
-CREATE UNIQUE INDEX "NetworkExplorer_networkId_type_key" ON "public"."NetworkExplorer"("networkId", "type");
-
--- CreateIndex
-CREATE UNIQUE INDEX "Network_coingeckoId_key" ON "public"."Network"("coingeckoId");
-
--- CreateIndex
-CREATE UNIQUE INDEX "TokenBridge_targetTokenId_key" ON "public"."TokenBridge"("targetTokenId");
-
--- CreateIndex
-CREATE UNIQUE INDEX "TokenMeta_tokenId_source_key" ON "public"."TokenMeta"("tokenId", "source");
-
--- CreateIndex
-CREATE UNIQUE INDEX "Token_networkId_address_key" ON "public"."Token"("networkId", "address");
-
--- CreateIndex
-CREATE INDEX "activity_project_id_index" ON "public"."activity"("project_id");
-
--- CreateIndex
-CREATE INDEX "activity_timestamp_index" ON "public"."activity"("timestamp");
-
 -- CreateIndex
 CREATE INDEX "aggregated_l2_costs_project_id_index" ON "public"."aggregated_l2_costs"("project_id");
 
@@ -459,59 +279,7 @@ CREATE INDEX "aggregated_l2_costs_timestamp_index" ON "public"."aggregated_l2_co
 CREATE INDEX "indexer_configurations_indexer_id_index" ON "public"."indexer_configurations"("indexer_id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "l2_costs_tx_hash_unique" ON "public"."l2_costs"("tx_hash");
-
--- CreateIndex
-CREATE INDEX "l2_costs_configuration_id_index" ON "public"."l2_costs"("configuration_id");
-
--- CreateIndex
 CREATE INDEX "update_notifier_block_number_index" ON "public"."update_notifier"("block_number");
 
 -- CreateIndex
 CREATE INDEX "update_notifier_project_name_index" ON "public"."update_notifier"("project_name");
-
--- CreateIndex
-CREATE UNIQUE INDEX "_BridgeEscrowToToken_AB_unique" ON "public"."_BridgeEscrowToToken"("A", "B");
-
--- CreateIndex
-CREATE INDEX "_BridgeEscrowToToken_B_index" ON "public"."_BridgeEscrowToToken"("B");
-
--- AddForeignKey
-ALTER TABLE "public"."BridgeEscrow" ADD CONSTRAINT "BridgeEscrow_networkId_fkey" FOREIGN KEY ("networkId") REFERENCES "public"."Network"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "public"."BridgeEscrow" ADD CONSTRAINT "BridgeEscrow_externalBridgeId_fkey" FOREIGN KEY ("externalBridgeId") REFERENCES "public"."ExternalBridge"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "public"."BridgeEscrow" ADD CONSTRAINT "BridgeEscrow_canonicalNetworkId_fkey" FOREIGN KEY ("canonicalNetworkId") REFERENCES "public"."Network"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "public"."Deployment" ADD CONSTRAINT "Deployment_tokenId_fkey" FOREIGN KEY ("tokenId") REFERENCES "public"."Token"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "public"."NetworkRpc" ADD CONSTRAINT "NetworkRpc_networkId_fkey" FOREIGN KEY ("networkId") REFERENCES "public"."Network"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "public"."NetworkExplorer" ADD CONSTRAINT "NetworkExplorer_networkId_fkey" FOREIGN KEY ("networkId") REFERENCES "public"."Network"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "public"."TokenBridge" ADD CONSTRAINT "TokenBridge_sourceTokenId_fkey" FOREIGN KEY ("sourceTokenId") REFERENCES "public"."Token"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "public"."TokenBridge" ADD CONSTRAINT "TokenBridge_targetTokenId_fkey" FOREIGN KEY ("targetTokenId") REFERENCES "public"."Token"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "public"."TokenBridge" ADD CONSTRAINT "TokenBridge_externalBridgeId_fkey" FOREIGN KEY ("externalBridgeId") REFERENCES "public"."ExternalBridge"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "public"."TokenMeta" ADD CONSTRAINT "TokenMeta_tokenId_fkey" FOREIGN KEY ("tokenId") REFERENCES "public"."Token"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "public"."Token" ADD CONSTRAINT "Token_networkId_fkey" FOREIGN KEY ("networkId") REFERENCES "public"."Network"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "public"."_BridgeEscrowToToken" ADD CONSTRAINT "_BridgeEscrowToToken_A_fkey" FOREIGN KEY ("A") REFERENCES "public"."BridgeEscrow"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "public"."_BridgeEscrowToToken" ADD CONSTRAINT "_BridgeEscrowToToken_B_fkey" FOREIGN KEY ("B") REFERENCES "public"."Token"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
---
+++ packages/database/src/kysely/token-db/types.ts
@@ -0,0 +1,126 @@
+// this is only temporary, to be removed when token db tables will be created
+import { Generated, Timestamp } from '../generated/types'
+import { ExplorerType, ExternalBridgeType } from './enums'
+
+export type BridgeEscrow = {
+  id: string
+  networkId: string
+  address: string
+  externalBridgeId: string | null
+  canonicalNetworkId: string | null
+  updatedAt: Generated<Timestamp>
+  createdAt: Generated<Timestamp>
+}
+export type BridgeEscrowToToken = {
+  A: string
+  B: string
+}
+export type Cache = {
+  key: string
+  value: string
+  chainId: number
+  blockNumber: number | null
+  updatedAt: Generated<Timestamp>
+  createdAt: Generated<Timestamp>
+}
+
+export type Deployment = {
+  id: string
+  tokenId: string
+  txHash: string | null
+  blockNumber: number | null
+  timestamp: Timestamp | null
+  from: string | null
+  to: string | null
+  isDeployerEoa: boolean | null
+  sourceAvailable: boolean
+  updatedAt: Generated<Timestamp>
+  createdAt: Generated<Timestamp>
+}
+export type DiscoveryCache = {
+  key: string
+  value: string
+  block_number: number
+  chain: string
+}
+export type ExternalBridge = {
+  id: string
+  name: string
+  type: ExternalBridgeType
+  updatedAt: Generated<Timestamp>
+  createdAt: Generated<Timestamp>
+}
+
+export type Network = {
+  id: string
+  chainId: number
+  name: string
+  coingeckoId: string | null
+  axelarId: string | null
+  axelarGatewayAddress: string | null
+  orbitId: string | null
+  wormholeId: string | null
+  layerZeroV1EndpointAddress: string | null
+  updatedAt: Generated<Timestamp>
+  createdAt: Generated<Timestamp>
+}
+export type NetworkExplorer = {
+  id: string
+  networkId: string
+  type: ExplorerType
+  url: string
+  apiKey: string
+  updatedAt: Generated<Timestamp>
+  createdAt: Generated<Timestamp>
+}
+export type NetworkRpc = {
+  id: string
+  networkId: string
+  url: string
+  updatedAt: Generated<Timestamp>
+  createdAt: Generated<Timestamp>
+}
+
+export type Token = {
+  id: string
+  networkId: string
+  address: string
+  updatedAt: Generated<Timestamp>
+  createdAt: Generated<Timestamp>
+}
+export type TokenBridge = {
+  id: string
+  sourceTokenId: string
+  targetTokenId: string
+  externalBridgeId: string | null
+  updatedAt: Generated<Timestamp>
+  createdAt: Generated<Timestamp>
+}
+export type TokenMeta = {
+  id: string
+  tokenId: string
+  externalId: string | null
+  source: string
+  name: string | null
+  symbol: string | null
+  decimals: number | null
+  logoUrl: string | null
+  contractName: string | null
+  updatedAt: Generated<Timestamp>
+  createdAt: Generated<Timestamp>
+}
+
+export type TokenDB = {
+  _BridgeEscrowToToken: BridgeEscrowToToken
+  'public.BridgeEscrow': BridgeEscrow
+  'public.Cache': Cache
+  'public.Deployment': Deployment
+  'public.discovery_cache': DiscoveryCache
+  'public.ExternalBridge': ExternalBridge
+  'public.Network': Network
+  'public.NetworkExplorer': NetworkExplorer
+  'public.NetworkRpc': NetworkRpc
+  'public.Token': Token
+  'public.TokenBridge': TokenBridge
+  'public.TokenMeta': TokenMeta
+}
