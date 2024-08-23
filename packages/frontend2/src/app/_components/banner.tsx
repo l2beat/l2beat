04c61d7df711d16653a764e47d39142f236afacb
Michał Podsiadły <michal.podsiadly@l2beat.com>
2024-08-23 14:57:09 +0200

    feat: do not display banner while on server

--- packages/frontend2/src/app/_components/banner.tsx
+++ packages/frontend2/src/app/_components/banner.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { useIsClient } from '~/hooks/use-is-client'
 import { useLocalStorage } from '~/hooks/use-local-storage'
 import ArrowRightIcon from '../../icons/arrow-right.svg'
 import CloseIcon from '../../icons/close.svg'
@@ -8,17 +9,18 @@ const localStorageTag = 'top-banner'
 const purpose = 'fraud-proof-wars'
 
 export function Banner() {
+  const isClient = useIsClient()
   const [isHidden, setIsHidden] = useLocalStorage(
     `${localStorageTag}-${purpose}-is-hidden`,
     false,
   )
 
-  if (isHidden) {
+  if (isHidden || !isClient) {
     return null
   }
 
   return (
-    <div className="flex w-full flex-col items-center justify-center gap-1 bg-gradient-to-r from-[#7F39B6] to-[#CD1BD3] py-1.5 text-white dark:text-white md:flex-row md:gap-3">
+    <div className="flex w-full flex-col items-center justify-center gap-1 bg-gradient-to-r from-[#7F39B6] to-[#CD1BD3] py-1.5 text-white md:flex-row md:gap-3 dark:text-white">
       <div className="absolute right-3">
         <CloseIcon
           onClick={() => setIsHidden(true)}
