d881c44d5a7e810ed34241217ce99c960ea03c2d
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2024-08-26 11:31:36 +0200

    More decoding

--- packages/tx-decoder/src/Decoded.tsx
+++ packages/tx-decoder/src/Decoded.tsx
@@ -1,7 +1,8 @@
 import { useQuery } from '@tanstack/react-query'
+import clsx from 'clsx'
 import { get4ByteSignatures } from './api/FourByte'
 import { getOpenChainSignatures } from './api/OpenChain'
-import { decode } from './decode'
+import { Value, decode } from './decode'
 
 interface DecodedProps {
   encoded: `0x${string}`
@@ -21,7 +22,7 @@ export function Decoded(props: DecodedProps) {
   })
 
   if (q1.isLoading || q2.isLoading) {
-    return 'Loading...'
+    return <div>Loading...</div>
   }
 
   const signatures = []
@@ -33,22 +34,145 @@ export function Decoded(props: DecodedProps) {
   }
   const decoded = decode(props.encoded, signatures)
 
+  const error = q1.error ?? q2.error
+  if (error) {
+    return <div>Error: {error.message}</div>
+  }
+
+  if (!decoded) {
+    return <div>Unknown</div>
+  }
+
   return (
-    <div>
-      {q1.isError && <div>Error {q1.error.message}</div>}
-      {q2.isError && <div>Error {q2.error.message}</div>}
-      {decoded?.abiItem && <div>Abi: {JSON.stringify(decoded.abiItem)}</div>}
-      {decoded?.decoded && (
-        <pre>
-          Parsed:{' '}
-          {JSON.stringify(
-            decoded.decoded,
-            (_, v) => (typeof v === 'bigint' ? `${v}n` : v),
-            2,
-          )}
-        </pre>
-      )}
-      {decoded?.extradata && <pre>Extra: {decoded.extradata}</pre>}
-    </div>
+    <>
+      <h2 className="mb-4">
+        <span className="font-bold text-lg">{decoded.name}</span>{' '}
+        <span className="pl-2 font-mono text-sm">{decoded.signature}</span>
+      </h2>
+      <ol>
+        {decoded.values.map((v, i) => (
+          <DecodedValue key={i} value={v} />
+        ))}
+      </ol>
+    </>
   )
+}
+
+interface DecodedValueProps {
+  value: Value
+}
+
+function DecodedValue({ value }: DecodedValueProps) {
+  return (
+    <li className="mb-4 last:mb-0">
+      <div>
+        <span className="text-blue-400">{value.stack.join('.')}</span>
+        <span className="pl-2 font-mono text-orange-500 text-sm">
+          {value.type}
+        </span>
+      </div>
+      {!Array.isArray(value.value) && (
+        <SimpleValue type={value.type} value={value.value} />
+      )}
+      {Array.isArray(value.value) && (
+        <ol className="my-1 rounded-sm bg-white bg-opacity-5 px-2.5 py-1">
+          {value.value.map((v, i) => (
+            <DecodedValue key={i} value={v} />
+          ))}
+        </ol>
+      )}
+    </li>
+  )
+}
+
+interface SimpleValueProps {
+  type: string
+  value: string | bigint | boolean
+}
+
+function SimpleValue({ type, value }: SimpleValueProps) {
+  if (typeof value === 'boolean') {
+    return <div className="font-mono">{value.toString()}</div>
+  }
+  if (type === 'string') {
+    return (
+      <div className="w-full break-words">
+        <span className="select-none text-white text-opacity-40">
+          {'\u201C'}
+        </span>
+        <span className="font-serif text-lg">{value.toString()}</span>
+        <span className="select-none text-white text-opacity-40">
+          {'\u201D'}
+        </span>
+      </div>
+    )
+  }
+  if (type === 'bytes' && typeof value === 'string') {
+    const data = toTxData(value)
+    return (
+      <div className="font-mono">
+        {data && <div>{data.selector}</div>}
+        <Parts value={data?.bytes ?? value.slice(2)} />
+      </div>
+    )
+  }
+  return <div className="font-mono">{value.toString()}</div>
+}
+
+function Parts({ value }: { value: string }) {
+  const bytes = toParts(value)
+  return (
+    <>
+      {bytes.map((parts, i) => (
+        <div key={i}>
+          {parts.map((p, j) => (
+            <span
+              className={clsx(p.isZero && 'text-white text-opacity-40')}
+              key={j}
+            >
+              {p.value}
+            </span>
+          ))}
+        </div>
+      ))}
+    </>
+  )
+}
+
+function toTxData(value: string) {
+  if (value.length < 10 || (value.length - 10) % 64 !== 0) {
+    return undefined
+  }
+  return {
+    selector: value.slice(2, 10),
+    bytes: value.slice(10),
+  }
+}
+
+function toParts(value: string) {
+  const length = Math.ceil(value.length / 64)
+  return Array.from({ length }).map((_, i) => {
+    const slice = value.slice(i * 64, (i + 1) * 64)
+    interface Part {
+      value: string
+      isZero: boolean
+    }
+    let part: Part = {
+      value: '',
+      isZero: false,
+    }
+    const parts: Part[] = [part]
+    for (let i = 0; i < 64; i += 2) {
+      const byte = slice.slice(i, i + 2)
+      const isZero = byte === '00'
+      if (i === 0 || isZero === part.isZero) {
+        part.value += byte
+        part.isZero = isZero
+      } else {
+        part = { value: byte, isZero }
+        parts.push(part)
+      }
+    }
+    return parts
+  })
 }
--- packages/tx-decoder/src/decode.ts
+++ packages/tx-decoder/src/decode.ts
@@ -1,6 +1,12 @@
-import { decodeFunctionData, encodeFunctionData, parseAbiItem } from 'viem'
+import {
+  AbiParameter,
+  decodeFunctionData,
+  encodeFunctionData,
+  parseAbiItem,
+} from 'viem'
 
 export function decode(data: `0x${string}`, abi: string[]) {
+  const signature = data.slice(0, 10).toLowerCase()
   for (const fn of abi) {
     try {
       const abiItem = parseAbiItem(fn)
@@ -11,19 +17,85 @@ export function decode(data: `0x${string}`, abi: string[]) {
         abi: [abiItem],
         data,
       })
+
       const encoded = encodeFunctionData({
         abi: [abiItem],
         ...decoded,
       })
-      const extradata: `0x${string}` = `0x${data.slice(encoded.length)}`
+      const extra = data.slice(encoded.length).toLowerCase()
+
+      const values = mix(abiItem.inputs, decoded.args)
+      if (extra.length > 0) {
+        values.push({
+          stack: ['extra'],
+          type: 'bytes',
+          value: `0x${extra}`,
+        })
+      }
+
       return {
-        abiItem,
-        decoded,
-        extradata,
+        name: abiItem.name,
+        signature,
+        values,
       }
     } catch (e) {
       console.error(e)
       continue
     }
   }
+}
+
+export interface Value {
+  stack: string[]
+  type: string
+  value: string | boolean | bigint | Value[]
+}
+
+function mix(
+  items: readonly AbiParameter[],
+  values: readonly unknown[],
+): Value[] {
+  return items.map((item, i) =>
+    mixValue(item, values[i], [item.name ?? i.toString()]),
+  )
+}
+
+function mixValue(item: AbiParameter, value: unknown, stack: string[]): Value {
+  if (Array.isArray(value)) {
+    const index = item.type.lastIndexOf('[')
+    const innerType = index !== -1 ? item.type.slice(0, index) : item.type
+    const innerItem = { ...item, type: innerType }
+
+    if ('components' in innerItem && index === -1) {
+      return {
+        stack,
+        type: 'tuple',
+        value: innerItem.components.map((c, i) =>
+          mixValue(c, value[i], [...stack, c.name ?? i.toString()]),
+        ),
+      }
+    }
+
+    return {
+      stack,
+      type: `array(${value.length})`,
+      value: value.map((v, i) =>
+        mixValue(innerItem, v, [...stack, i.toString()]),
+      ),
+    }
+  }
+
+  if (
+    typeof value === 'string' ||
+    typeof value === 'bigint' ||
+    typeof value === 'boolean'
+  ) {
+    return {
+      stack,
+      type: item.type,
+      value,
+    }
+  }
+
+  throw new Error()
 }
