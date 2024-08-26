e17e5c7fa8205aaf6bd0a7db833499311e0d62ef
antooni <antoni.pawlak@protonmail.com>
2024-08-26 15:21:44 +0200

    test migration

---
+++ packages/database/prisma/migrations/20240826132112_remove_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `min` on the `aggregated_liveness` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "aggregated_liveness" DROP COLUMN "min";
