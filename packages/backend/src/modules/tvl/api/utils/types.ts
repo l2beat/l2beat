aeed8ec499374ade96cb1734ef627dae35fb41ca
Michał Podsiadły <michal.podsiadly@l2beat.com>
2024-08-26 22:39:40 +0200

    Revert "chore: stip redundant types"
    This reverts commit ebc0e05175a0aeeee90bcd5dcf5cc0911d05fbb8.

--- packages/backend/src/modules/tvl/api/utils/types.ts
+++ packages/backend/src/modules/tvl/api/utils/types.ts
@@ -1,7 +1,12 @@
+import { BackendProject } from '@l2beat/config'
 import {
+  AmountConfigEntry,
   AssetId,
   CanonicalAssetBreakdownData,
   ChainId,
+  EthereumAddress,
+  ProjectId,
+  UnixTime,
 } from '@l2beat/shared-pure'
 
 export interface CanonicalAssetBreakdown {
@@ -11,4 +16,33 @@ export interface CanonicalAssetBreakdown {
   usdValue: number
   usdPrice: string
   escrows: CanonicalAssetBreakdownData['escrows']
+}
+export type AmountConfigMap = Map<
+  ProjectId,
+  (AmountConfigEntry & { configId: string })[]
+>
+export type PriceConfigIdMap = Map<
+  string,
+  { assetId: AssetId; priceId: string }
+>
+
+export interface ApiProject {
+  id: ProjectId
+  minTimestamp: UnixTime
+  type: BackendProject['type']
+  slug: string
+  sources: Map<
+    string,
+    {
+      minTimestamp: UnixTime
+    }
+  >
+}
+export interface AssociatedToken {
+  address: EthereumAddress | 'native'
+  chain: string
+  type: 'canonical' | 'external' | 'native'
+  includeInTotal: boolean
+  project: ProjectId
+  projectType: 'layers2s' | 'bridges'
 }
