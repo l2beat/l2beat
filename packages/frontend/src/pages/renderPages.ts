b69b5c2aeaf95165cd80e6f777b7a51ebcf88c98
Michał Podsiadły <michal.podsiadly@l2beat.com>
2024-08-27 19:03:42 +0200

    feat: remove old tvl-breakdown files

--- packages/frontend/src/pages/renderPages.ts
+++ packages/frontend/src/pages/renderPages.ts
@@ -8,19 +8,13 @@ import { getProjectPages } from './project/layer2'
 import { getL3sProjectPages } from './project/layer3'
 import { getActivityPage } from './scaling/activity'
 import { getMaintenanceActivityPage } from './scaling/activity/maintenance'
-import { getProjectTvlBreakdownPages } from './scaling/projects-tvl-breakdown'
 import { getSummaryPage } from './scaling/summary'
 
 export function renderPages(config: Config, pagesData: PagesData) {
   const pages: Page[] = []
 
-  const {
-    tvlApiResponse,
-    activityApiResponse,
-    verificationStatus,
-    tvlBreakdownApiResponse,
-    implementationChange,
-  } = pagesData
+  const { activityApiResponse, verificationStatus, implementationChange } =
+    pagesData
 
   pages.push(getDARiskPage())
   pages.push(getSummaryPage(config, pagesData))
@@ -41,15 +35,6 @@ export function renderPages(config: Config, pagesData: PagesData) {
     )
   } else {
     pages.push(getMaintenanceActivityPage(config))
-  }
-
-  if (config.features.tvlBreakdown && tvlBreakdownApiResponse) {
-    pages.push(
-      ...getProjectTvlBreakdownPages(config, {
-        tvlApiResponse,
-        tvlBreakdownApiResponse,
-      }),
-    )
   }
 
   outputPages(config, pages)
