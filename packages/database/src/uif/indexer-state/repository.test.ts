b881d5a92c3c79b95646bca9c979ddfe7969c066
maciekzygmunt <maciekzygmunt@interia.pl>
2024-08-27 14:58:37 +0200

    remove .only

--- packages/database/src/uif/indexer-state/repository.test.ts
+++ packages/database/src/uif/indexer-state/repository.test.ts
@@ -12,7 +12,7 @@ describeDatabase(IndexerStateRepository.name, (db) => {
   })
 
   describe(IndexerStateRepository.prototype.findByIndexerId.name, () => {
-    it.only('returns undefined if no record exists', async () => {
+    it('returns undefined if no record exists', async () => {
       const indexerState = await repository.findByIndexerId('indexer')
       expect(indexerState).toEqual(undefined)
     })
