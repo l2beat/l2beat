1e39d4f18edac8119f74bcd2b6b690fe396d057c
vincfurc <10850139+vincfurc@users.noreply.github.com>
2024-08-28 16:47:03 +0100

    risk

--- packages/config/src/projects/other/da-beat/types/DaFraudDetectionRisk.ts
+++ packages/config/src/projects/other/da-beat/types/DaFraudDetectionRisk.ts
@@ -24,7 +24,10 @@ function DasWithNoBlobsReconstruction(erasureCoding: boolean) {
     type: 'DasWithNoBlobsReconstruction',
     value: 'DAS with no blobs reconstruction',
     sentiment: erasureCoding ? 'good' : 'warning',
-    description: 'TODO',
+    description: `
+    The DA layer uses data availability sampling (DAS) to protect against data withholding attacks. However, there is no mechanism in place to reconstruct the data if some of it is withheld or the minimum number of light nodes
+    required to perform DAS is not available. This means that the data may be lost if a block producer withholds data or if there are not enough honest light nodes to perform DAS.
+    `,
   } as const
 }
 
