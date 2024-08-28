449184d4e78e8d8866631e4289b65aa44d0df894
Piotr Adamczyk <xeno@xeno.yt>
2024-08-28 12:05:54 +0200

    Filter table

--- packages/frontend2/src/app/(new)/(other)/scaling/activity/_components/table/scaling-activity-table.tsx
+++ packages/frontend2/src/app/(new)/(other)/scaling/activity/_components/table/scaling-activity-table.tsx
@@ -5,15 +5,24 @@ import { BasicTable } from '~/app/_components/table/basic-table'
 import { useTable } from '~/hooks/use-table'
 import { type ScalingActivityEntry } from '~/server/features/scaling/get-scaling-activity-entries'
 import { scalingActivityColumns } from './columns'
+import { useScalingFilter } from '~/app/(new)/(other)/_components/scaling-filter-context'
+import { useMemo } from 'react'
 
 interface Props {
   entries: ScalingActivityEntry[]
 }
 
 export function ScalingActivityTable({ entries }: Props) {
+  const filter = useScalingFilter()
+
+  const filteredEntries = useMemo(
+    () => entries.filter(filter),
+    [entries, filter],
+  )
+
   const table = useTable({
     columns: scalingActivityColumns,
-    data: entries,
+    data: filteredEntries,
     getCoreRowModel: getCoreRowModel(),
     getSortedRowModel: getSortedRowModel(),
     initialState: {
