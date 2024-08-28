8def0df59aacfd486c14165f09823f6d2b083438
Piotr Adamczyk <xeno@xeno.yt>
2024-08-28 12:28:10 +0200

    Remove activity from legacy `frontend`

--- packages/frontend/src/pages/renderPages.ts
+++ packages/frontend/src/pages/renderPages.ts
@@ -6,21 +6,13 @@ import { outputPages } from './output'
 import { getBridgeProjectPages } from './project/bridge'
 import { getProjectPages } from './project/layer2'
 import { getL3sProjectPages } from './project/layer3'
-import { getActivityPage } from './scaling/activity'
-import { getMaintenanceActivityPage } from './scaling/activity/maintenance'
 import { getProjectTvlBreakdownPages } from './scaling/projects-tvl-breakdown'
 import { getSummaryPage } from './scaling/summary'
 
 export function renderPages(config: Config, pagesData: PagesData) {
   const pages: Page[] = []
 
-  const {
-    tvlApiResponse,
-    activityApiResponse,
-    verificationStatus,
-    tvlBreakdownApiResponse,
-    implementationChange,
-  } = pagesData
+  const { tvlApiResponse, tvlBreakdownApiResponse } = pagesData
 
   pages.push(getDARiskPage())
   pages.push(getSummaryPage(config, pagesData))
@@ -30,18 +22,6 @@ export function renderPages(config: Config, pagesData: PagesData) {
   pages.push(...getBridgeProjectPages(config, pagesData))
 
   pages.push(getMultisigReportDownloadPage(config))
-
-  if (activityApiResponse) {
-    pages.push(
-      getActivityPage(config, {
-        activityApiResponse,
-        verificationStatus,
-        implementationChange,
-      }),
-    )
-  } else {
-    pages.push(getMaintenanceActivityPage(config))
-  }
 
   if (config.features.tvlBreakdown && tvlBreakdownApiResponse) {
     pages.push(
