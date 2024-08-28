bbaecab5d5af6c420ae15c1e203e1ccd25d838d3
Piotr Adamczyk <xeno@xeno.yt>
2024-08-28 13:02:56 +0200

    Remove scaling summary code from the legacy frontend

--- packages/frontend/src/pages/scaling/summary/index.tsx
+++ packages/frontend/src/pages/scaling/summary/index.tsx
@@ -1,19 +1,60 @@
 import React from 'react'
 
 import { Config } from '../../../build/config'
-import { PageWrapper } from '../../../components'
 import { PagesData } from '../../Page'
-import { getProps } from './props'
-import { ScalingSummaryPage } from './view/ScalingSummaryPage'
+import { getIncludedProjectsTvlBreakdown } from '../../../utils/getIncludedProjectsTvlBreakdown'
+import { assert } from '@l2beat/shared-pure'
 
 export function getSummaryPage(config: Config, pagesData: PagesData) {
-  const { props, wrapper } = getProps(config, pagesData)
+  assert(pagesData.tvlBreakdownApiResponse, 'Missing tvlBreakdownApiResponse')
+
+  const tvlBreakdownProjects = getIncludedProjectsTvlBreakdown(
+    [...config.layer2s, ...(config.features.layer3sTvl ? config.layer3s : [])],
+    pagesData.tvlApiResponse,
+    pagesData.tvlBreakdownApiResponse,
+  )
+
+  // NOTE(piotradamczyk): Temporary main page for legacy frontend before we port all the pages.
+
   return {
     slug: '/scaling/summary',
     page: (
-      <PageWrapper {...wrapper}>
-        <ScalingSummaryPage {...props} />
-      </PageWrapper>
+      <div>
+        <h1>Index</h1>
+        <h2>Scaling detail pages</h2>
+        <ul>
+          {[...config.layer2s, ...config.layer3s].map((project) => (
+            <a
+              href={`/scaling/projects/${project.display.slug}`}
+              key={project.display.slug}
+            >
+              <li key={project.display.slug}>{project.display.name}</li>
+            </a>
+          ))}
+        </ul>
+        <h2>Scaling TVL breakdown pages</h2>
+        <ul>
+          {tvlBreakdownProjects.map((project) => (
+            <a
+              href={`/scaling/projects/${project.display.slug}/tvl-breakdown`}
+              key={project.display.slug}
+            >
+              <li key={project.display.slug}>{project.display.name}</li>
+            </a>
+          ))}
+        </ul>
+        <h2>Bridges detail pages</h2>
+        <ul>
+          {config.bridges.map((project) => (
+            <a
+              href={`/bridges/projects/${project.display.slug}`}
+              key={project.display.slug}
+            >
+              <li key={project.display.slug}>{project.display.name}</li>
+            </a>
+          ))}
+        </ul>
+      </div>
     ),
   }
 }
