a4c2b2f79445a3ae2d8abf8375d6d1b20403a478
Piotr Adamczyk <xeno@xeno.yt>
2024-08-28 15:09:25 +0200

    Port multisig report page (#4920)

--- packages/frontend/src/pages/renderPages.ts
+++ packages/frontend/src/pages/renderPages.ts
@@ -1,6 +1,5 @@
 import { Config } from '../build/config'
 import { Page, PagesData } from './Page'
-import { getMultisigReportDownloadPage } from './multisig-report'
 import { outputPages } from './output'
 import { getBridgeProjectPages } from './project/bridge'
 import { getProjectPages } from './project/layer2'
@@ -26,8 +25,6 @@ export function renderPages(config: Config, pagesData: PagesData) {
   pages.push(...getL3sProjectPages(config, pagesData))
 
   pages.push(...getBridgeProjectPages(config, pagesData))
-
-  pages.push(getMultisigReportDownloadPage(config))
 
   if (activityApiResponse) {
     pages.push(
