eb1764119db522b444c2e3e5e5421c7834e5505f
maciekzygmunt <maciekzygmunt@interia.pl>
2024-08-28 15:39:40 +0200

    fix liveness table

--- packages/database/prisma/migrations/20240828133910_fix_long_running_queries/migration.sql
+++ packages/database/prisma/migrations/20240828133910_fix_long_running_queries/migration.sql
@@ -1,9 +1,14 @@
 /*
   Warnings:
 
+  - The primary key for the `liveness` table will be changed. If it partially fails, the table could be left without primary key constraint.
   - The primary key for the `values` table will be changed. If it partially fails, the table could be left without primary key constraint.
 
 */
+-- AlterTable
+ALTER TABLE "liveness" DROP CONSTRAINT "liveness_pkey",
+ADD CONSTRAINT "liveness_pkey" PRIMARY KEY ("configuration_id", "timestamp", "tx_hash");
+
 -- AlterTable
 ALTER TABLE "values" DROP CONSTRAINT "values_pkey",
 ADD CONSTRAINT "values_pkey" PRIMARY KEY ("project_id", "timestamp", "data_source");
