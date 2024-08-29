d5f86952e32bf3cd41f4b6c2d7d12904dde1d642
Piotr Adamczyk <xeno@xeno.yt>
2024-08-29 09:27:20 +0200

    Post-merge changes

--- packages/frontend/src/pages/scaling/summary/index.tsx
+++ packages/frontend/src/pages/scaling/summary/index.tsx
@@ -1,35 +1,27 @@
 import React from 'react'
 
-import { assert } from '@l2beat/shared-pure'
 import { Config } from '../../../build/config'
 import { getIncludedProjectsTvlBreakdown } from '../../../utils/getIncludedProjectsTvlBreakdown'
 import { PagesData } from '../../Page'
 
+// NOTE(piotradamczyk): This is a temporary main page for the legacy frontend.
 export function getSummaryPage(config: Config, pagesData: PagesData) {
-  assert(pagesData.tvlBreakdownApiResponse, 'Missing tvlBreakdownApiResponse')
-
-  const tvlBreakdownProjects = getIncludedProjectsTvlBreakdown(
-    [...config.layer2s, ...(config.features.layer3sTvl ? config.layer3s : [])],
-    pagesData.tvlApiResponse,
-    pagesData.tvlBreakdownApiResponse,
-  )
-
-  // NOTE(piotradamczyk): Temporary main page for legacy frontend before we port all the pages.
+  const tvlBreakdownProjects = pagesData.tvlBreakdownApiResponse
+    ? getIncludedProjectsTvlBreakdown(
+        [
+          ...config.layer2s,
+          ...(config.features.layer3sTvl ? config.layer3s : []),
+        ],
+        pagesData.tvlApiResponse,
+        pagesData.tvlBreakdownApiResponse,
+      )
+    : []
 
   return {
     slug: '/scaling/summary',
     page: (
       <div>
         <h1>Index of L2BEAT's legacy frontend</h1>
-        <h2>Misc. pages</h2>
-        <ul>
-          <a href="/da-risk">
-            <li>Data Availability Risk Framework</li>
-          </a>
-          <a href="/multisig-report">
-            <li>Multisig report</li>
-          </a>
-        </ul>
         <h2>Scaling detail pages</h2>
         <ul>
           {[...config.layer2s, ...config.layer3s]
@@ -43,17 +35,21 @@ export function getSummaryPage(config: Config, pagesData: PagesData) {
               </a>
             ))}
         </ul>
-        <h2>Scaling TVL breakdown pages</h2>
-        <ul>
-          {tvlBreakdownProjects.sort(byName).map((project) => (
-            <a
-              href={`/scaling/projects/${project.display.slug}/tvl-breakdown`}
-              key={project.display.slug}
-            >
-              <li key={project.display.slug}>{project.display.name}</li>
-            </a>
-          ))}
-        </ul>
+        {tvlBreakdownProjects.length > 0 && (
+          <>
+            <h2>Scaling TVL breakdown pages</h2>
+            <ul>
+              {tvlBreakdownProjects.sort(byName).map((project) => (
+                <a
+                  href={`/scaling/projects/${project.display.slug}/tvl-breakdown`}
+                  key={project.display.slug}
+                >
+                  <li key={project.display.slug}>{project.display.name}</li>
+                </a>
+              ))}
+            </ul>
+          </>
+        )}
         <h2>Bridges detail pages</h2>
         <ul>
           {config.bridges.sort(byName).map((project) => (
