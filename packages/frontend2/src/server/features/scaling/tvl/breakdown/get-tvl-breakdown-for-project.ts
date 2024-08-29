294dbbf26afe11b27e454b2379114761e1e32ad9
Michał Podsiadły <michal.podsiadly@l2beat.com>
2024-08-29 11:38:40 +0200

    feat: cache breakdown for project

--- packages/frontend2/src/server/features/scaling/tvl/breakdown/get-tvl-breakdown-for-project.ts
+++ packages/frontend2/src/server/features/scaling/tvl/breakdown/get-tvl-breakdown-for-project.ts
@@ -6,21 +6,39 @@ import {
   getTvlPricesConfig,
   toBackendProject,
 } from '@l2beat/config'
+import { UnixTime } from '@l2beat/shared-pure'
+import {
+  unstable_cache as cache,
+  unstable_noStore as noStore,
+} from 'next/cache'
 import { getTvlBreakdown } from './get-tvl-breakdown'
 
 export type ProjectTvlBreakdown = Awaited<
   ReturnType<ReturnType<typeof getTvlBreakdown>>
 >
 
-export function getTvlBreakdownForProject(project: Layer2 | Layer3) {
-  const backendProject = toBackendProject(project)
-
-  const amountsConfigs = getTvlAmountsConfigForProject(backendProject)
-  const priceConfigs = getTvlPricesConfig()
-
-  const configMapping = new ConfigMapping(priceConfigs, amountsConfigs, [
-    backendProject.projectId,
-  ])
-
-  return getTvlBreakdown({ configMapping })(backendProject.projectId)
+export function getTvlBreakdownForProject(
+  ...parameters: Parameters<typeof getCachedTvlBreakdownForProject>
+) {
+  noStore()
+  return getCachedTvlBreakdownForProject(...parameters)
 }
+
+export const getCachedTvlBreakdownForProject = cache(
+  async (project: Layer2 | Layer3) => {
+    const backendProject = toBackendProject(project)
+
+    const amountsConfigs = getTvlAmountsConfigForProject(backendProject)
+    const priceConfigs = getTvlPricesConfig()
+
+    const configMapping = new ConfigMapping(priceConfigs, amountsConfigs, [
+      backendProject.projectId,
+    ])
+
+    return getTvlBreakdown({ configMapping })(backendProject.projectId)
+  },
+  ['getCachedTvlBreakdownForProject'],
+  {
+    revalidate: 10 * UnixTime.MINUTE,
+  },
+)
