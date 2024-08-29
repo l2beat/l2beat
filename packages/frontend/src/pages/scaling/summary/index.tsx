b4b2da50e6cfefe1b7675065f33cc39284a27e44
Piotr Adamczyk <xeno@xeno.yt>
2024-08-29 09:45:07 +0200

    Remove scaling summary code from the legacy frontend (#4914)

--- packages/frontend/src/pages/scaling/summary/index.tsx
+++ packages/frontend/src/pages/scaling/summary/index.tsx
@@ -1,19 +1,74 @@
 import React from 'react'
 
 import { Config } from '../../../build/config'
-import { PageWrapper } from '../../../components'
+import { getIncludedProjectsTvlBreakdown } from '../../../utils/getIncludedProjectsTvlBreakdown'
 import { PagesData } from '../../Page'
-import { getProps } from './props'
-import { ScalingSummaryPage } from './view/ScalingSummaryPage'
 
+// NOTE(piotradamczyk): This is a temporary main page for the legacy frontend.
 export function getSummaryPage(config: Config, pagesData: PagesData) {
-  const { props, wrapper } = getProps(config, pagesData)
+  const tvlBreakdownProjects = pagesData.tvlBreakdownApiResponse
+    ? getIncludedProjectsTvlBreakdown(
+        [
+          ...config.layer2s,
+          ...(config.features.layer3sTvl ? config.layer3s : []),
+        ],
+        pagesData.tvlApiResponse,
+        pagesData.tvlBreakdownApiResponse,
+      )
+    : []
+
   return {
     slug: '/scaling/summary',
     page: (
-      <PageWrapper {...wrapper}>
-        <ScalingSummaryPage {...props} />
-      </PageWrapper>
+      <div>
+        <h1>Index of L2BEAT's legacy frontend</h1>
+        <h2>Scaling detail pages</h2>
+        <ul>
+          {[...config.layer2s, ...config.layer3s]
+            .sort(byName)
+            .map((project) => (
+              <a
+                href={`/scaling/projects/${project.display.slug}`}
+                key={project.display.slug}
+              >
+                <li key={project.display.slug}>{project.display.name}</li>
+              </a>
+            ))}
+        </ul>
+        {tvlBreakdownProjects.length > 0 && (
+          <>
+            <h2>Scaling TVL breakdown pages</h2>
+            <ul>
+              {tvlBreakdownProjects.sort(byName).map((project) => (
+                <a
+                  href={`/scaling/projects/${project.display.slug}/tvl-breakdown`}
+                  key={project.display.slug}
+                >
+                  <li key={project.display.slug}>{project.display.name}</li>
+                </a>
+              ))}
+            </ul>
+          </>
+        )}
+        <h2>Bridges detail pages</h2>
+        <ul>
+          {config.bridges.sort(byName).map((project) => (
+            <a
+              href={`/bridges/projects/${project.display.slug}`}
+              key={project.display.slug}
+            >
+              <li key={project.display.slug}>{project.display.name}</li>
+            </a>
+          ))}
+        </ul>
+      </div>
     ),
   }
+}
+
+function byName(
+  a: { display: { name: string } },
+  b: { display: { name: string } },
+) {
+  return a.display.name.localeCompare(b.display.name)
 }
