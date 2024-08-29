f23faf5d86a39b1de6031d2c6d9cf522423d7f0c
Tomasz Tórz <93620601+torztomasz@users.noreply.github.com>
2024-08-29 11:44:48 +0200

    Add stories to new frontend (#4928)
    Co-authored-by: Piotr Adamczyk <xeno@xeno.yt>

---
+++ packages/frontend2/src/app/_components/opengraph-image/project.stories.tsx
@@ -0,0 +1,19 @@
+import { type Meta, type StoryObj } from '@storybook/react'
+import { ProjectOpengraphImage } from './project'
+
+const meta = {
+  title: 'OG Images/Project',
+  component: ProjectOpengraphImage,
+} satisfies Meta<typeof ProjectOpengraphImage>
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    baseUrl: 'https://l2beat.com',
+    slug: 'arbitrum',
+    name: 'Arbitrum',
+    size: { width: 1200, height: 630 },
+  },
+}
