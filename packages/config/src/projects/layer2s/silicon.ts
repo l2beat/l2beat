c559108ec935609ba121a608962ed437147df6a3
Radina Talanova <nt.radina@gmail.com>
2024-08-30 16:41:59 +0300

    Add Silicon (under review)

---
+++ packages/config/src/projects/layer2s/silicon.ts
@@ -0,0 +1,26 @@
+import { underReviewL2 } from './templates/underReview'
+import { Layer2 } from './types'
+
+export const silicon: Layer2 = underReviewL2({
+  id: 'silicon',
+  display: {
+    name: 'Silicon',
+    slug: 'silicon',
+    description:
+      'Silicon is a Validium built on the Polygon CDK Stack, aiming to become the the social network of the future.',
+    purposes: ['Universal'],
+    category: 'Validium',
+    provider: 'Polygon',
+    links: {
+      websites: ['https://silicon.network/'],
+      apps: ['https://bridge.silicon.network/'],
+      documentation: ['https://docs.silicon.network/'],
+      explorers: ['https://scope.silicon.network'],
+      repositories: [],
+      socialMedia: ['https://x.com/0xSilicon'],
+    },
+    activityDataSource: 'Blockchain RPC',
+  },
+  rpcUrl: 'https://rpc.silicon.network',
+  //shared polygon bridge
+})
