56d051b9245759e619aa950f30b60e727c2ed9a4
vincfurc <10850139+vincfurc@users.noreply.github.com>
2024-09-02 12:59:27 +0100

    Add SP1 Blobstream to ZK catalog

---
+++ packages/config/src/projects/other/zk-catalog/blobstreamSP1.ts
@@ -0,0 +1,97 @@
+import { ChainId, EthereumAddress } from '@l2beat/shared-pure'
+import { ZkCatalogProject } from './types'
+
+export const blobstreamx: ZkCatalogProject = {
+  display: {
+    slug: 'SP1Blobstream',
+    name: 'SP1Blobstream',
+  },
+  proofVerification: {
+    aggregation: false,
+    verifiers: [
+      {
+        name: 'Blobstream SP1 Verifier (Ethereum)',
+        description:
+          'Celestia ZK light client verifying multiple blocks at once. Deployed on Ethereum mainnet.',
+        contractAddress: EthereumAddress(
+          '0xc350F063C13a3Ca21331610fe159E697a5c9c2FB',
+        ),
+        chainId: ChainId.ETHEREUM,
+        verified: 'no',
+        subVerifiers: [
+          {
+            name: 'Final wrap',
+            proofSystem: 'Plonk SNARK',
+            mainArithmetization: 'Plonk',
+            mainPCS: 'KZG',
+            trustedSetup: '?',
+          },
+          {
+            name: 'Main circuit',
+            proofSystem: 'Plonky3',
+            mainArithmetization: 'Plonk',
+            mainPCS: 'FRI',
+            trustedSetup: 'None',
+            link: '',
+          },
+        ],
+      },
+      {
+        name: 'Blobstream SP1 Verifier (Base)',
+        description:
+          'Celestia ZK light client verifying multiple blocks at once. Deployed on Base.',
+        contractAddress: EthereumAddress(
+          '0xc350F063C13a3Ca21331610fe159E697a5c9c2FB',
+        ),
+        chainId: ChainId.BASE,
+        verified: 'no',
+        subVerifiers: [
+          {
+            name: 'Final wrap',
+            proofSystem: 'Plonk SNARK',
+            mainArithmetization: 'Plonk',
+            mainPCS: 'KZG',
+            trustedSetup: '?',
+          },
+          {
+            name: 'Main circuit',
+            proofSystem: 'Plonky3',
+            mainArithmetization: 'Plonk',
+            mainPCS: 'FRI',
+            trustedSetup: 'None',
+            link: '',
+          },
+        ],
+      },
+      {
+        name: 'Blobstream SP1 Verifier (Arbitrum One)',
+        description:
+          'Celestia ZK light client verifying multiple blocks at once. Deployed on Arbitrum One.',
+        contractAddress: EthereumAddress(
+          '0xc350F063C13a3Ca21331610fe159E697a5c9c2FB',
+        ),
+        chainId: ChainId.ARBITRUM,
+        verified: 'no',
+        subVerifiers: [
+          {
+            name: 'Final wrap',
+            proofSystem: 'Plonk SNARK',
+            mainArithmetization: 'Plonk',
+            mainPCS: 'KZG',
+            trustedSetup: '?',
+          },
+          {
+            name: 'Main circuit',
+            proofSystem: 'Plonky3',
+            mainArithmetization: 'Plonk',
+            mainPCS: 'FRI',
+            trustedSetup: 'None',
+            link: '',
+          },
+        ],
+      }
+    ],
+    requiredTools: [],
+  },
+  type: 'zk-catalog',
+}
