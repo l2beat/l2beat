c394d9bc43a3de37d88c5afc195313eab8d9e5ed
torztomasz <tomasz.torz@l2beat.com>
2024-09-03 13:52:22 +0200

    add overscroll behavior to command

--- packages/frontend2/src/app/_components/command.tsx
+++ packages/frontend2/src/app/_components/command.tsx
@@ -61,7 +61,10 @@ const CommandList = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <CommandPrimitive.List
     ref={ref}
-    className={cn('max-h-[300px] overflow-y-auto overflow-x-hidden', className)}
+    className={cn(
+      'max-h-[300px] overflow-y-auto overflow-x-hidden overscroll-none',
+      className,
+    )}
     {...props}
   />
 ))
