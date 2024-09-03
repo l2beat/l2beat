71429ec09bfe1bc9e1ff01d285403a1bec38dc94
maciekzygmunt <maciekzygmunt@interia.pl>
2024-09-03 13:56:43 +0200

    diff L1 with L2s

--- packages/backend/scripts/agglayer.ts
+++ packages/backend/scripts/agglayer.ts
@@ -1,6 +1,5 @@
 import { tokenList } from '@l2beat/config/src'
 import { ChainId, EthereumAddress, notUndefined } from '@l2beat/shared-pure'
-import { formatUnits } from 'ethers/lib/utils'
 import {
   http,
   Address,
@@ -9,6 +8,7 @@ import {
   decodeFunctionResult,
   encodeFunctionData,
   formatEther,
+  formatUnits,
   parseAbi,
 } from 'viem'
 
@@ -37,27 +37,47 @@ interface Token {
 }
 
 // same address on all the chains
-const bridge = '0x2a3DD3EB832aF982ec71669E178424b10Dca2EDe' as Address
+const BRIDGE_ADDRESS = '0x2a3DD3EB832aF982ec71669E178424b10Dca2EDe' as Address
 
-const bridgeAbi = parseAbi([
+const BRIDGE_ABI = parseAbi([
   'function getTokenWrappedAddress(uint32 originNetwork, address originTokenAddress) view returns (address)',
 ])
 
-const batchSize = 100
+const BATCH_SIZE = 100
 
-const erc20Abi = parseAbi(['function totalSupply() view returns (uint256)'])
+const ERC20_ABI = parseAbi([
+  'function totalSupply() view returns (uint256)',
+  'function balanceOf(address account) view returns (uint256)',
+])
 
-const multicallAbi = parseAbi([
+const MUTLICALL_ABI = parseAbi([
   'function aggregate((address target, bytes callData)[] calls) view returns (uint256 blockNumber, bytes[] returnData)',
 ])
 
-const multicallAddress = '0xcA11bde05977b3631167028862bE2a173976CA11' as Address
+const MULTICALL_ADDRESS =
+  '0xcA11bde05977b3631167028862bE2a173976CA11' as Address
+
+// Add Ethereum configuration
+const ETHEREUM = {
+  name: 'Ethereum',
+  rpcUrl: 'https://eth.llamarpc.com',
+}
+
+const ethereumBalances: Record<string, number> = {}
+const L2sBalances: Record<string, number> = {}
 
 async function main() {
   const tokensOnEthereum = tokenList.filter(
     (token): token is typeof token & { address: EthereumAddress } =>
       token.chainId === ChainId.ETHEREUM && token.address !== undefined,
   )
+
+  console.log(`Processing ${ETHEREUM.name} balances`)
+  const ethereumClient = createPublicClient({
+    transport: http(ETHEREUM.rpcUrl),
+  })
+
+  await getEthereumBalances(ethereumClient, tokensOnEthereum)
 
   for (const chain of CHAINS) {
     console.log(`Processing ${chain.name}`)
@@ -67,21 +87,76 @@ async function main() {
     })
 
     const etherSupply = await chainClient.getBalance({
-      address: bridge as Address,
+      address: BRIDGE_ADDRESS as Address,
     })
+
+    if (etherSupply > 0n) {
+      const key = 'ETH'
+      L2sBalances[key] =
+        (L2sBalances[key] || 0) +
+        Number(formatEther(chain.premintedEth - etherSupply))
+    }
 
     console.log(`ETH: ${formatEther(chain.premintedEth - etherSupply)} ETH`)
 
     const supportsMulticall = await checkMulticallSupport(chainClient)
 
-    await processBatches(
-      chainClient,
-      tokensOnEthereum,
-      chain,
-      supportsMulticall,
-    )
+    await processBatches(chainClient, tokensOnEthereum, supportsMulticall)
 
     console.log('---')
+  }
+
+  Object.entries(L2sBalances).forEach(([symbol, balance]) => {
+    if (ethereumBalances[symbol] - balance !== 0) {
+      console.log(`Diff: ${ethereumBalances[symbol] - balance} ${symbol}`)
+    }
+  })
+}
+
+async function getEthereumBalances(
+  ethereumClient: PublicClient,
+  tokensOnEthereum: Token[],
+) {
+  const ethereumEtherSupply = await ethereumClient.getBalance({
+    address: BRIDGE_ADDRESS as Address,
+  })
+
+  ethereumBalances['ETH'] = Number(formatEther(ethereumEtherSupply))
+
+  for (let i = 0; i < tokensOnEthereum.length; i += BATCH_SIZE) {
+    const batch = tokensOnEthereum.slice(i, i + BATCH_SIZE)
+
+    const calls = batch.map((token) => ({
+      target: token.address.toString() as Address,
+      callData: encodeFunctionData({
+        abi: ERC20_ABI,
+        functionName: 'balanceOf',
+        args: [BRIDGE_ADDRESS],
+      }),
+    }))
+
+    const [, returnData] = await ethereumClient.readContract({
+      address: MULTICALL_ADDRESS,
+      abi: MUTLICALL_ABI,
+      functionName: 'aggregate',
+      args: [calls],
+    })
+
+    returnData.forEach((data, i) => {
+      const token = batch[i]
+      const bridgeBalance = decodeFunctionResult({
+        abi: ERC20_ABI,
+        functionName: 'balanceOf',
+        data,
+      })
+
+      if (bridgeBalance > 0n) {
+        const key = token.symbol
+        ethereumBalances[key] =
+          (ethereumBalances[key] || 0) +
+          Number(formatUnits(bridgeBalance, token.decimals))
+      }
+    })
   }
 }
 
@@ -90,8 +165,8 @@ async function checkMulticallSupport(
 ): Promise<boolean> {
   try {
     await chainClient.readContract({
-      address: multicallAddress,
-      abi: multicallAbi,
+      address: MULTICALL_ADDRESS,
+      abi: MUTLICALL_ABI,
       functionName: 'aggregate',
       args: [[]],
     })
@@ -104,16 +179,15 @@ async function checkMulticallSupport(
 async function processBatches(
   chainClient: PublicClient,
   tokens: Token[],
-  chain: (typeof CHAINS)[number],
   supportsMulticall: boolean,
 ) {
-  for (let i = 0; i < tokens.length; i += batchSize) {
-    const batch = tokens.slice(i, i + batchSize)
+  for (let i = 0; i < tokens.length; i += BATCH_SIZE) {
+    const batch = tokens.slice(i, i + BATCH_SIZE)
 
     if (supportsMulticall) {
       await processBatchWithMulticall(chainClient, batch)
     } else {
-      await processBatchIndividually(chainClient, batch, chain)
+      await processBatchIndividually(chainClient, batch)
     }
   }
 }
@@ -123,26 +197,26 @@ async function processBatchWithMulticall(
   batch: Token[],
 ) {
   const calls = batch.map((token) => ({
-    target: bridge,
+    target: BRIDGE_ADDRESS,
     callData: encodeFunctionData({
-      abi: bridgeAbi,
+      abi: BRIDGE_ABI,
       functionName: 'getTokenWrappedAddress',
       args: [0, token.address.toString() as Address],
     }),
   }))
 
-  const [, returnData] = await chainClient.readContract({
-    address: multicallAddress,
-    abi: multicallAbi,
+  const [, tokenAddressesReturnData] = await chainClient.readContract({
+    address: MULTICALL_ADDRESS,
+    abi: MUTLICALL_ABI,
     functionName: 'aggregate',
     args: [calls],
   })
 
-  const foundTokens = returnData
+  const foundTokens = tokenAddressesReturnData
     .map((data, i) => {
       const token = batch[i]
       const l2TokenAddress = decodeFunctionResult({
-        abi: bridgeAbi,
+        abi: BRIDGE_ABI,
         functionName: 'getTokenWrappedAddress',
         data,
       })
@@ -162,25 +236,25 @@ async function processBatchWithMulticall(
     return {
       target: data.l2TokenAddress,
       callData: encodeFunctionData({
-        abi: erc20Abi,
+        abi: ERC20_ABI,
         functionName: 'totalSupply',
       }),
     }
   })
 
-  const [, result] = await chainClient.readContract({
-    address: multicallAddress,
-    abi: multicallAbi,
+  const [, totalSupplyReturnData] = await chainClient.readContract({
+    address: MULTICALL_ADDRESS,
+    abi: MUTLICALL_ABI,
     functionName: 'aggregate',
     args: [totalSupplyCalls],
   })
 
-  const decoded = result.map((data, i) => {
+  const decoded = totalSupplyReturnData.map((data, i) => {
     const token = foundTokens[i]
     return {
       token,
       totalSupply: decodeFunctionResult({
-        abi: erc20Abi,
+        abi: ERC20_ABI,
         functionName: 'totalSupply',
         data,
       }),
@@ -189,6 +263,11 @@ async function processBatchWithMulticall(
 
   decoded.forEach((data) => {
     if (data.totalSupply > 0n) {
+      const key = data.token.symbol
+      L2sBalances[key] =
+        (L2sBalances[key] || 0) +
+        Number(formatUnits(data.totalSupply, data.token.decimals))
+
       console.log(
         `${data.token.symbol}: ${formatUnits(
           data.totalSupply,
@@ -202,13 +281,12 @@ async function processBatchWithMulticall(
 async function processBatchIndividually(
   chainClient: PublicClient,
   batch: Token[],
-  chain: (typeof CHAINS)[number],
 ) {
   for (const token of batch) {
     try {
       const l2TokenAddress = (await chainClient.readContract({
-        address: bridge,
-        abi: bridgeAbi,
+        address: BRIDGE_ADDRESS,
+        abi: BRIDGE_ABI,
         functionName: 'getTokenWrappedAddress',
         args: [0, token.address.toString() as Address],
       })) as Address
@@ -219,23 +297,19 @@ async function processBatchIndividually(
 
       const totalSupply = (await chainClient.readContract({
         address: l2TokenAddress,
-        abi: erc20Abi,
+        abi: ERC20_ABI,
         functionName: 'totalSupply',
       })) as bigint
 
       if (totalSupply > 0n) {
         console.log(
-          `${token.symbol} on ${chain.name}: ${formatUnits(
-            totalSupply,
-            token.decimals,
-          )} ${token.symbol}`,
+          `${token.symbol}: ${formatUnits(totalSupply, token.decimals)} ${
+            token.symbol
+          }`,
         )
       }
     } catch (error) {
-      console.error(
-        `Error fetching total supply for ${token.symbol} on ${chain.name}:`,
-        error,
-      )
+      console.error(`Error fetching total supply for ${token.symbol}`, error)
     }
   }
 }
