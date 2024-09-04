fb6c77a87d6cfec1e90e713b94daba782a42f2b0
torztomasz <tomasz.torz@l2beat.com>
2024-09-04 08:37:27 +0200

    add scroll to top button

--- packages/frontend2/src/app/(legacy)/scaling/projects/[slug]/layout.tsx
+++ packages/frontend2/src/app/(legacy)/scaling/projects/[slug]/layout.tsx
@@ -1,5 +1,10 @@
-import { ContentWrapper } from '~/app/_components/content-wrapper'
+import { ScrollToTopButton } from '~/app/_components/scroll-to-top-button'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <ContentWrapper>{children}</ContentWrapper>
+  return (
+    <>
+      {children}
+      <ScrollToTopButton />
+    </>
+  )
 }
