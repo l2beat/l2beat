281ee0a109a3e8054e70e2a684361837d415a858
Piotr Adamczyk <xeno@xeno.yt>
2024-09-04 12:16:37 +0200

    API adjustments

--- packages/frontend2/src/app/api/(public)/scaling/summary/route.ts
+++ packages/frontend2/src/app/api/(public)/scaling/summary/route.ts
@@ -1,14 +1,24 @@
 import { NextResponse } from 'next/server'
-import {
-  getScalingSummaryEntries,
-  type ScalingSummaryEntry,
-} from '~/server/features/scaling/summary/get-scaling-summary-entries'
+import { getScalingSummaryEntries } from '~/server/features/scaling/summary/get-scaling-summary-entries'
 
 export async function GET() {
   const entries = await getScalingSummaryEntries()
   return NextResponse.json({
-    data: entries.reduce<Record<string, ScalingSummaryEntry>>((acc, entry) => {
-      acc[entry.id] = entry
+    data: entries.reduce<Record<string, unknown>>((acc, entry) => {
+      acc[entry.id] = {
+        ...entry,
+        // Remap change to change7d
+        tvl: { ...entry.tvl, change: undefined, change7d: entry.tvl.change },
+        // For stage - only return the stage name
+        stage: entry.stage.stage,
+        // Strip out the following fields
+        entryType: undefined,
+        href: undefined,
+        isVerified: undefined,
+        hasImplementationChanged: undefined,
+        warning: undefined,
+        showProjectUnderReview: undefined,
+      }
       return acc
     }, {}),
   })
