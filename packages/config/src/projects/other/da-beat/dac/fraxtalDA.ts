dbe7769b4e452927c5e578bce624a7a8ab2b420e
vincfurc <10850139+vincfurc@users.noreply.github.com>
2024-09-04 17:45:22 +0100

    fraxtalDA

---
+++ packages/config/src/projects/other/da-beat/dac/fraxtalDA.ts
@@ -0,0 +1,51 @@
+import { ProjectDiscovery } from '../../../../discovery/ProjectDiscovery'
+import { fraxtal } from '../../../layer2s/fraxtal'
+import { NO_BRIDGE } from '../templates/no-bridge-template'
+import { DaEconomicSecurityRisk, DaFraudDetectionRisk } from '../types'
+import { DaLayer } from '../types/DaLayer'
+import { toUsedInProject } from '../utils/to-used-in-project'
+
+const discovery = new ProjectDiscovery('fraxtal')
+
+export const fraxtalDA: DaLayer = {
+  id: 'dac',
+  type: 'DaLayer',
+  kind: 'DAC',
+  display: {
+    name: 'FraxtalDA',
+    slug: 'fraxtalda',
+    description:
+      'FraxtalDA is a custom data availability solution built by the Fraxtal team.',
+      links: {
+        websites: ['https://frax.com/'],
+        apps: ['https://app.frax.finance/'],
+        documentation: ['https://docs.frax.com/'],
+        explorers: ['https://fraxscan.com/'],
+        repositories: ['https://github.com/FraxFinance'],
+        socialMedia: [
+          'https://discord.com/invite/UJVtDTFRaA',
+          'https://twitter.com/fraxfinance',
+          'https://t.me/fraxfinance',
+        ],
+      },
+  },
+  technology: `
+    ## Architecture
+    FraxtalDA is a custom data availability solution built by the Fraxtal team. 
+    The data is posted by the OP batcher to three separate locations: AWS, IPFS, and Cloudfare R2. 
+    The IPFS hash is then submitted to the on-chain inbox contract on Ethereum.
+    FraxtalDA relies on a single DA endpoint to manage data posting between the three different locations. 
+    `,
+  bridges: [NO_BRIDGE({
+    layer: 'FraxtalDA',
+    description:
+      'The risk profile in this page refers to scaling solutions that do not integrate with a data availability bridge.',
+    technology: `No DA bridge is selected. Without a DA bridge, Ethereum has no proof of data availability for this project.\n`,
+  })],
+  usedIn: [...toUsedInProject([fraxtal])],
+  risks: {
+    economicSecurity: DaEconomicSecurityRisk.OnChainNotSlashable('MNT'),
+    fraudDetection: DaFraudDetectionRisk.NoFraudDetection,
+  },
+}
+
