e1272cab4dbcca1e6e224c19bf1d49cbd70fc4cb
Piotr Adamczyk <xeno@xeno.yt>
2024-09-05 09:58:44 +0200

    Update DA Summary page layout (#4993)
    Co-authored-by: torztomasz <tomasz.torz@l2beat.com>

--- packages/frontend2/src/app/(new)/data-availability/_utils/map-risks-to-rosette-values.tsx
+++ packages/frontend2/src/app/(new)/data-availability/_utils/map-risks-to-rosette-values.tsx
@@ -1,8 +1,12 @@
 import { type DaBridgeRisks, type DaLayerRisks } from '@l2beat/config'
+import { type Sentiment, type ValueWithSentiment } from '@l2beat/shared-pure'
 import { type RosetteValue } from '~/app/_components/rosette/types'
 
 export function mapRisksToRosetteValues(
-  risks: DaBridgeRisks & DaLayerRisks,
+  risks: Record<
+    keyof (DaLayerRisks & DaBridgeRisks),
+    ValueWithSentiment<string, Sentiment> & { description?: string }
+  >,
 ): RosetteValue[] {
   const values: RosetteValue[] = [
     {
