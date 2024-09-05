1ef1233323d16a63966f1df5c65288f4756b4ea1
vincfurc <10850139+vincfurc@users.noreply.github.com>
2024-09-05 15:17:48 +0100

    Blobstream SP1 verifier update (#5013)

--- packages/config/src/projects/other/zk-catalog/SP1Blobstream.ts
+++ packages/config/src/projects/other/zk-catalog/SP1Blobstream.ts
@@ -12,7 +12,7 @@ export const SP1Blobstream: ZkCatalogProject = {
     aggregation: true,
     verifiers: [
       {
-        name: 'Blobstream SP1 Verifier (Ethereum)',
+        name: 'Blobstream SP1 Verifier (Ethereum) - Frozen',
         description:
           'Celestia ZK light client verifying multiple blocks at once. Deployed on Ethereum mainnet.',
         contractAddress: EthereumAddress(
@@ -37,7 +37,7 @@ export const SP1Blobstream: ZkCatalogProject = {
         ],
       },
       {
-        name: 'Blobstream SP1 Verifier (Base)',
+        name: 'Blobstream SP1 Verifier (Base) - Frozen',
         description:
           'Celestia ZK light client verifying multiple blocks at once. Deployed on Base.',
         contractAddress: EthereumAddress(
@@ -62,11 +62,86 @@ export const SP1Blobstream: ZkCatalogProject = {
         ],
       },
       {
-        name: 'Blobstream SP1 Verifier (Arbitrum One)',
+        name: 'Blobstream SP1 Verifier (Arbitrum One) - Frozen',
         description:
           'Celestia ZK light client verifying multiple blocks at once. Deployed on Arbitrum One.',
         contractAddress: EthereumAddress(
           '0xc350F063C13a3Ca21331610fe159E697a5c9c2FB',
+        ),
+        chainId: ChainId.ARBITRUM,
+        verified: 'no',
+        subVerifiers: [
+          {
+            name: 'Final wrap',
+            ...PROOFS.PLONKSNARK('?'),
+            link: 'https://github.com/succinctlabs/sp1-blobstream/blob/main/program/src/main.rs',
+          },
+          {
+            name: 'Main circuit',
+            proofSystem: 'Plonky3',
+            mainArithmetization: 'Plonk',
+            mainPCS: 'FRI',
+            trustedSetup: 'None',
+            link: 'https://github.com/succinctlabs/sp1',
+          },
+        ],
+      },
+      {
+        name: 'Blobstream SP1 Verifier (Ethereum)',
+        description:
+          'Celestia ZK light client verifying multiple blocks at once. Deployed on Ethereum mainnet.',
+        contractAddress: EthereumAddress(
+          '0x6B6A7Ded061567d8A56279801DEA5cFB79be5bFc',
+        ),
+        chainId: ChainId.ETHEREUM,
+        verified: 'no',
+        subVerifiers: [
+          {
+            name: 'Final wrap',
+            ...PROOFS.PLONKSNARK('?'),
+            link: 'https://github.com/succinctlabs/sp1-blobstream/blob/main/program/src/main.rs',
+          },
+          {
+            name: 'Main circuit',
+            proofSystem: 'Plonky3',
+            mainArithmetization: 'Plonk',
+            mainPCS: 'FRI',
+            trustedSetup: 'None',
+            link: 'https://github.com/succinctlabs/sp1',
+          },
+        ],
+      },
+      {
+        name: 'Blobstream SP1 Verifier (Base)',
+        description:
+          'Celestia ZK light client verifying multiple blocks at once. Deployed on Base.',
+        contractAddress: EthereumAddress(
+          '0x6B6A7Ded061567d8A56279801DEA5cFB79be5bFc',
+        ),
+        chainId: ChainId.BASE,
+        verified: 'no',
+        subVerifiers: [
+          {
+            name: 'Final wrap',
+            ...PROOFS.PLONKSNARK('?'),
+            link: 'https://github.com/succinctlabs/sp1-blobstream/blob/main/program/src/main.rs',
+          },
+          {
+            name: 'Main circuit',
+            proofSystem: 'Plonky3',
+            mainArithmetization: 'Plonk',
+            mainPCS: 'FRI',
+            trustedSetup: 'None',
+            link: 'https://github.com/succinctlabs/sp1',
+          },
+        ],
+      },
+      {
+        name: 'Blobstream SP1 Verifier (Arbitrum One)',
+        description:
+          'Celestia ZK light client verifying multiple blocks at once. Deployed on Arbitrum One.',
+        contractAddress: EthereumAddress(
+          '0x6B6A7Ded061567d8A56279801DEA5cFB79be5bFc',
         ),
         chainId: ChainId.ARBITRUM,
         verified: 'no',
