881466ddfc2326e68d6847c6197834dfaecb4ed2
vincfurc <10850139+vincfurc@users.noreply.github.com>
2024-09-05 12:37:50 +0100

    technology

--- packages/config/src/projects/other/da-beat/blockchain/memo/memo.ts
+++ packages/config/src/projects/other/da-beat/blockchain/memo/memo.ts
@@ -9,13 +9,13 @@ export const memo: DaLayer = {
   type: 'DaLayer',
   kind: 'PublicBlockchain',
   display: {
-    name: 'MemoDA',
+    name: 'Meeda',
     slug: 'memo',
-    description: `MEMO is a blockchain-based cloud storage protocol developed by MEMO Labs.`,
+    description: `Meeda (MemoDA) is a blockchain-based cloud storage protocol developed by MEMO Labs.`,
     links: {
       websites: ['https://www.memolabs.org/'],
       documentation: [
-        'https://docs.memolabs.org/#/',
+        'https://memolabs.gitbook.io/meeda',
       ],
       repositories: [
         'https://github.com/memoio',
@@ -36,6 +36,23 @@ export const memo: DaLayer = {
     unbondingPeriod: 0, 
   },
   technology: `
+    ## Architecture
+    Meeda's architecture consists of several components. 
+    Storage Nodes store blob data and generate data availability commitments, with deployment available to all users. 
+    Light Nodes are responsible for challenging storage nodes through on-chain verification, and their deployment is also open to all users. 
+    The Operator, currently deployed by the project itself, handles the erasure coding of data and data chunks distribution, as well as the aggregation of KZG commitment proofs from Storage Nodes. 
+    
+    Storage Nodes, based on seed information generated periodically on the Memo chain, submit KZG data commitments to the Operator. 
+    The Verification Contract is responsible for on-chain verification on Memo chain, and it will generate seed information through the VRF-Sampling (Verifiable Random Function) cycle to determine which files to select for verification.
+    Not all data commitments are verified on-chain, but only a subset of data is verified, with the sampling determined by the random seed produced by the Verification Contract.
+    The operator will then collect and aggregate the KZG commitments for the selected files from storage nodes, and submit the aggregate proof to the Verification Contract.
+   
+    Optimistic verification assumes that the aggregation commitment submitted by the operator node is correct and only requires checking the accuracy of the associated proof.
+    If no light node challenges the commitment within the specified validity period, the proof of availability is considered successful.
+
+    Submitting a fraud proof is expected to require multiple rounds of interaction, with pledges and rewards for both the challenger and the challenged.
+    The proving smart contracts (FileProof, ControlFileProof, ProxyFileProof) are deployed to the Memo chain and their code is unverified.
+    Their code is not public, so the logic and security of the proving contracts is not verifiable.
   `,
   bridges: [
     NO_BRIDGE({
@@ -51,6 +68,6 @@ export const memo: DaLayer = {
     fraudDetection: DaFraudDetectionRisk.NoFraudDetection,
   },
   economicSecurity: {
-    type: 'MemoDA',
+    type: 'MemoDA', //none
   },
 }
