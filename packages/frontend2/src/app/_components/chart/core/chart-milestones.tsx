e2f72254bc0bf069739d00690341c60e125e47ee
Piotr Adamczyk <xeno@xeno.yt>
2024-09-09 21:15:17 +0200

    Fix milestones display (#5044)

--- packages/frontend2/src/app/_components/chart/core/chart-milestones.tsx
+++ packages/frontend2/src/app/_components/chart/core/chart-milestones.tsx
@@ -113,18 +113,28 @@ function ChartMilestone({ x, milestone }: Props) {
           height={milestoneSize}
           viewBox={`0 0 ${milestoneSize} ${milestoneSize}`}
           role="img"
-          aria-label="Milestone icon"
+          aria-label={
+            milestone.type === 'general' ? 'Milestone icon' : 'Incident icon'
+          }
           className="fill-green-700 stroke-green-500"
         >
-          <rect
-            x="9.89941"
-            y="1.41421"
-            width="12"
-            height="12"
-            rx="1"
-            transform="rotate(45 9.89941 1.41421)"
-            strokeWidth="2"
-          />
+          {milestone.type === 'general' ? (
+            <rect
+              x="9.89941"
+              y="1.41421"
+              width="12"
+              height="12"
+              rx="1"
+              transform="rotate(45 9.89941 1.41421)"
+              strokeWidth="2"
+            />
+          ) : (
+            <path
+              className="fill-red-800 stroke-red-700"
+              d="M2.11842 14.4966L9.13637 2.46527C9.52224 1.80374 10.4781 1.80375 10.864 2.46528L17.882 14.497C18.2708 15.1637 17.7899 16.0008 17.0182 16.0008L10.0003 16.0008L10.0002 16.0008L2.98214 16.0004C2.21039 16.0004 1.72956 15.1632 2.11842 14.4966Z"
+              strokeWidth="2"
+            />
+          )}
         </svg>
       </a>
     </div>
