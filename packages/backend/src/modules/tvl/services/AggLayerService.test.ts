84030fd886b685ac0ce10729967827f425d8713b
maciekzygmunt <maciekzygmunt@interia.pl>
2024-09-09 15:27:35 +0300

    add getNativeEtherWrapped

--- packages/backend/src/modules/tvl/services/AggLayerService.test.ts
+++ packages/backend/src/modules/tvl/services/AggLayerService.test.ts
@@ -1,29 +1,32 @@
 import { Logger } from '@l2beat/backend-tools'
 import {
   AggLayerNativeEtherPreminted,
+  AggLayerNativeEtherWrapped,
+  Bytes,
   EthereumAddress,
 } from '@l2beat/shared-pure'
 import { expect, mockFn, mockObject } from 'earl'
-import { BigNumber } from 'ethers'
+import { BigNumber, utils } from 'ethers'
 import { MulticallClient } from '../../../peripherals/multicall/MulticallClient'
 import { RpcClient } from '../../../peripherals/rpcclient/RpcClient'
-import { AggLayerService } from './AggLayerService'
+import { AggLayerService, erc20Interface } from './AggLayerService'
 
 describe(AggLayerService.name, () => {
   describe(AggLayerService.prototype.getNativeEtherPreminted.name, () => {
-    it('should return the correct balance', async () => {
+    it('should fetch bridge balance and subtract it from premintedAmount', async () => {
       const mockToken: AggLayerNativeEtherPreminted =
         mockObject<AggLayerNativeEtherPreminted>({
           l2BridgeAddress: EthereumAddress.random(),
           premintedAmount: 1000n,
         })
 
+      const mockRpcClient = mockObject<RpcClient>({
+        getBalance: mockFn().resolvesTo(BigNumber.from(900)),
+      })
       const mockAggLayerService = new AggLayerService({
         logger: Logger.SILENT,
         multicallClient: mockObject<MulticallClient>({}),
-        rpcClient: mockObject<RpcClient>({
-          getBalance: mockFn().resolvesTo(BigNumber.from(900)),
-        }),
+        rpcClient: mockRpcClient,
       })
 
       const result = await mockAggLayerService.getNativeEtherPreminted(
@@ -32,6 +35,45 @@ describe(AggLayerService.name, () => {
       )
 
       expect(result).toEqual(100n)
+      expect(mockRpcClient.getBalance).toHaveBeenCalledWith(
+        mockToken.l2BridgeAddress,
+        0,
+      )
+    })
+  })
+
+  describe(AggLayerService.prototype.getNativeEtherWrapped.name, () => {
+    it('should return the correct totalSupply', async () => {
+      const token: AggLayerNativeEtherWrapped =
+        mockObject<AggLayerNativeEtherWrapped>({
+          wethAddress: EthereumAddress.random(),
+        })
+
+      const expectedSupply = 1000n
+
+      const mockRpcClient = mockObject<RpcClient>({
+        call: mockFn().resolvesTo(
+          utils.defaultAbiCoder.encode(['uint256'], [expectedSupply]),
+        ),
+      })
+      const mockAggLayerService = new AggLayerService({
+        logger: Logger.SILENT,
+        multicallClient: mockObject<MulticallClient>({}),
+        rpcClient: mockRpcClient,
+      })
+
+      const result = await mockAggLayerService.getNativeEtherWrapped(token, 0)
+
+      expect(result).toEqual(expectedSupply)
+      expect(mockRpcClient.call).toHaveBeenCalledWith(
+        {
+          to: token.wethAddress,
+          data: Bytes.fromHex(
+            erc20Interface.encodeFunctionData('totalSupply', []),
+          ),
+        },
+        0,
+      )
     })
   })
 })
--- packages/backend/src/modules/tvl/services/AggLayerService.ts
+++ packages/backend/src/modules/tvl/services/AggLayerService.ts
@@ -1,7 +1,17 @@
 import { Logger } from '@l2beat/backend-tools'
-import { AggLayerNativeEtherPreminted } from '@l2beat/shared-pure'
+import {
+  AggLayerNativeEtherPreminted,
+  AggLayerNativeEtherWrapped,
+  Bytes,
+} from '@l2beat/shared-pure'
+import { BigNumber, utils } from 'ethers'
 import { MulticallClient } from '../../../peripherals/multicall/MulticallClient'
 import { RpcClient } from '../../../peripherals/rpcclient/RpcClient'
+
+export const erc20Interface = new utils.Interface([
+  'function balanceOf(address account) view returns (uint256)',
+  'function totalSupply() view returns (uint256)',
+])
 
 export interface AggLayerServiceDependencies {
   readonly rpcClient: RpcClient
@@ -24,10 +34,31 @@ export class AggLayerService {
     )
     return BigInt(token.premintedAmount) - bridgeBalance.toBigInt()
   }
+
+  async getNativeEtherWrapped(
+    token: AggLayerNativeEtherWrapped,
+    blockNumber: number,
+  ): Promise<bigint> {
+    const response = await this.$.rpcClient.call(
+      {
+        to: token.wethAddress,
+        data: Bytes.fromHex(
+          erc20Interface.encodeFunctionData('totalSupply', []),
+        ),
+      },
+      blockNumber,
+    )
+
+    const [value] = erc20Interface.decodeFunctionResult(
+      'totalSupply',
+      response.toString(),
+    )
+
+    return (value as BigNumber).toBigInt()
+  }
 }
 
 // getData in: tokens: AggLayerL2Token[] nativeToken: AggLayerNativeEtherPreminted | AggLayerNativeEtherWrapped | undefined
 
 // getTokensData in: tokens: AggLayerL2Token[]
-// getNatieEtherPreminted in: token: AggLayerNativeEtherPreminted
 // getNativeEtherWrapped in: token: AggLayerNativeEtherWrapped
