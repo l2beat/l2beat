908ab7696b2a9d748adcfab26fa350abc4bf805f
maciekzygmunt <maciekzygmunt@interia.pl>
2024-09-10 13:43:32 +0200

    add l2tokens amount

--- packages/backend/src/modules/tvl/services/AggLayerService.ts
+++ packages/backend/src/modules/tvl/services/AggLayerService.ts
@@ -1,10 +1,12 @@
 import { Logger } from '@l2beat/backend-tools'
+import { AmountRecord } from '@l2beat/database'
 import {
   AggLayerL2Token,
   AggLayerNativeEtherPreminted,
   AggLayerNativeEtherWrapped,
   Bytes,
   EthereumAddress,
+  UnixTime,
 } from '@l2beat/shared-pure'
 import { BigNumber, utils } from 'ethers'
 import { MulticallClient } from '../../../peripherals/multicall/MulticallClient'
@@ -24,6 +26,13 @@ const BRIDGE_ADDRESS = EthereumAddress(
   '0x2a3DD3EB832aF982ec71669E178424b10Dca2EDe',
 )
 
+type AggLayerAmountConfig =
+  | AggLayerL2Token
+  | AggLayerNativeEtherPreminted
+  | AggLayerNativeEtherWrapped
+
+type Config = AggLayerAmountConfig & { id: string }
+
 export interface AggLayerServiceDependencies {
   readonly rpcClient: RpcClient
   readonly multicallClient: MulticallClient
@@ -35,12 +44,56 @@ export class AggLayerService {
     this.$.logger = $.logger.for(this)
   }
 
-  //   async getTokensData(tokens: AggLayerL2Token[], blockNumber: number) {}
+  async getL2TokensAmounts(
+    tokens: (AggLayerL2Token & { id: string })[],
+    blockNumber: number,
+    timestamp: UnixTime,
+  ) {
+    const tokensWithL2Addresses = await this.getL2TokensAddresses(
+      tokens,
+      blockNumber,
+    )
+
+    return await this.getL2TokensTotalSupply(
+      tokensWithL2Addresses,
+      blockNumber,
+      timestamp,
+    )
+  }
+
+  async getL2TokensTotalSupply(
+    tokens: (AggLayerL2Token & { address: EthereumAddress; id: string })[],
+    blockNumber: number,
+    timestamp: UnixTime,
+  ): Promise<AmountRecord[]> {
+    const encoded: MulticallRequest[] = tokens.map((token) => ({
+      address: token.address,
+      data: Bytes.fromHex(erc20Interface.encodeFunctionData('totalSupply', [])),
+    }))
+
+    const responses = await this.$.multicallClient.multicall(
+      encoded,
+      blockNumber,
+    )
+
+    return responses.map((response, index) => {
+      const token = tokens[index]
+      const [value] = erc20Interface.decodeFunctionResult(
+        'totalSupply',
+        response.data.toString(),
+      )
+      return {
+        configId: token.id,
+        timestamp,
+        amount: (value as BigNumber).toBigInt(),
+      }
+    })
+  }
 
   async getL2TokensAddresses(
-    tokens: AggLayerL2Token[],
+    tokens: (AggLayerL2Token & { id: string })[],
     blockNumber: number,
-  ): Promise<(AggLayerL2Token & { address: EthereumAddress })[]> {
+  ): Promise<(AggLayerL2Token & { address: EthereumAddress; id: string })[]> {
     const encoded: MulticallRequest[] = tokens.map((token) => ({
       address: BRIDGE_ADDRESS,
       data: Bytes.fromHex(
@@ -69,21 +122,28 @@ export class AggLayerService {
     })
   }
 
-  async getNativeEtherPreminted(
-    token: AggLayerNativeEtherPreminted,
+  async getNativeEtherPremintedAmount(
+    token: AggLayerNativeEtherPreminted & { id: string },
     blockNumber: number,
-  ): Promise<bigint> {
+    timestamp: UnixTime,
+  ): Promise<AmountRecord> {
     const bridgeBalance = await this.$.rpcClient.getBalance(
       token.l2BridgeAddress,
       blockNumber,
     )
-    return BigInt(token.premintedAmount) - bridgeBalance.toBigInt()
+    const amount = BigInt(token.premintedAmount) - bridgeBalance.toBigInt()
+    return {
+      configId: token.id,
+      amount,
+      timestamp,
+    }
   }
 
-  async getNativeEtherWrapped(
-    token: AggLayerNativeEtherWrapped,
+  async getNativeEtherWrappedAmount(
+    token: AggLayerNativeEtherWrapped & { id: string },
     blockNumber: number,
-  ): Promise<bigint> {
+    timestamp: UnixTime,
+  ): Promise<AmountRecord> {
     const response = await this.$.rpcClient.call(
       {
         to: token.wethAddress,
@@ -94,15 +154,17 @@ export class AggLayerService {
       blockNumber,
     )
 
-    const [value] = erc20Interface.decodeFunctionResult(
+    const [totalSupply] = erc20Interface.decodeFunctionResult(
       'totalSupply',
       response.toString(),
     )
 
-    return (value as BigNumber).toBigInt()
+    return {
+      configId: token.id,
+      amount: (totalSupply as BigNumber).toBigInt(),
+      timestamp,
+    }
   }
 }
 
 // getData in: tokens: AggLayerL2Token[] nativeToken: AggLayerNativeEtherPreminted | AggLayerNativeEtherWrapped | undefined
-
-// getTokensData in: tokens: AggLayerL2Token[]
