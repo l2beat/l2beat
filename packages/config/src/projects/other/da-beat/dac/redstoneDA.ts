f8aca8a945ff8fa19906eb5c00826cfc1529ce91
vincfurc <10850139+vincfurc@users.noreply.github.com>
2024-09-12 22:47:09 +0100

    desc

--- packages/config/src/projects/other/da-beat/dac/redstoneDA.ts
+++ packages/config/src/projects/other/da-beat/dac/redstoneDA.ts
@@ -1,5 +1,26 @@
 import { DaEconomicSecurityRisk, DaFraudDetectionRisk } from '../types'
 import { DaLayer } from '../types/DaLayer'
+import { ProjectDiscovery } from '../../../../discovery/ProjectDiscovery'
+import { formatSeconds } from '@l2beat/shared-pure'
+import { NO_BRIDGE } from '../templates/no-bridge-template'
+import { linkByDA } from '../utils/link-by-da'
+
+
+const discovery = new ProjectDiscovery('redstone')
+
+const daChallengeWindow = formatSeconds(
+  discovery.getContractValue<number>(
+    'DataAvailabilityChallenge',
+    'challengeWindow',
+  ) * 12, // in blocks, to seconds
+)
+
+const daResolveWindow = formatSeconds(
+  discovery.getContractValue<number>(
+    'DataAvailabilityChallenge',
+    'resolveWindow',
+  ) * 12, // in blocks, to seconds
+)
 
 export const redstoneDA: DaLayer = {
   id: 'redstone-da',
@@ -25,14 +46,25 @@ export const redstoneDA: DaLayer = {
   technology: `
     ## Data Availability Challenges
     Redstone relies on DA challenges for data availability. 
+    The DA Provider submits an input commitment on Ethereum, and users can request the data behind the commitment off-chain from the DA Provider.
     If a DA challenger finds that the data behind a tx data commitment is not available, they can submit a challenge which requires locking a bond within ${daChallengeWindow}. 
     A challenge can be resolved by publishing the preimage data within an additional ${daResolveWindow}.
     In such case, a portion of the challenger bond is burned, with the exact amount estimated as the cost incurred by the resolver to publish the full data, meaning that the resolver and challenger will approximately lose the same amount of funds.
     The system is not secure if the malicious sequencer is able to outspend the altruistic challengers. 
     If instead, after a challenge, the preimage data is not published, the chain reorgs to the last fully derivable state.
   `,
-  bridges: [],
-  usedIn: [],
+  bridges: [
+    NO_BRIDGE({
+        layer: 'RedstoneDA',
+        description:
+          'The risk profile in this page refers to scaling solutions that do not integrate with a data availability bridge.',
+        technology: `No DA bridge is selected. Without a DA bridge, Ethereum has no proof of data availability for this project.
+        However, there is a mechanism that allows users to challenge unavailability of data. \n`,
+      }),
+  ],
+  usedIn: linkByDA({
+    layer: (layer) => layer === 'RedstoneDA',
+  }),
   risks: {
     economicSecurity: DaEconomicSecurityRisk.Unknown,
     fraudDetection: DaFraudDetectionRisk.NoFraudDetection,
