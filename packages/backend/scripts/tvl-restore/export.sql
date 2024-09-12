721b6b9c1c890a1199926e9dee9ebe06bf8278bb
maciekzygmunt <maciekzygmunt@interia.pl>
2024-09-12 12:07:21 +0200

    update tvl-resore

--- packages/backend/scripts/tvl-restore/export.sql
+++ packages/backend/scripts/tvl-restore/export.sql
@@ -1,7 +1,7 @@
 -- To run it login via psql and then type: \i export.sql 
-\copy indexer_state TO './data/indexer_state.csv' CSV;
-\copy indexer_configurations TO './data/indexer_configurations.csv' CSV;
-\copy block_timestamps TO './data/block_timestamps.csv' CSV;
-\copy amounts TO './data/amounts.csv' CSV;
-\copy prices TO './data/prices.csv' CSV;
-\copy values TO './data/values.csv' CSV;
+\copy "IndexerState" TO './data/indexer_state.csv' CSV;
+\copy "IndexerConfiguration" TO './data/indexer_configurations.csv' CSV;
+\copy "BlockTimestamp" TO './data/block_timestamps.csv' CSV;
+\copy "Amount" TO './data/amounts.csv' CSV;
+\copy "Price" TO './data/prices.csv' CSV;
+\copy "Value" TO './data/values.csv' CSV;
