089297b436c551c72b19ea570866c2938e209379
Piotr Adamczyk <xeno@xeno.yt>
2024-09-12 13:18:44 +0200

    Remove legacy frontend (#5071)

--- packages/frontend/src/components/rosette/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './Rosette'
-export * from './RosetteTooltip'
--- packages/frontend/src/utils/constants.ts
+++
@@ -1,3 +0,0 @@
-// NOTE(radomski): This is not an ASCII minus character, but a UTF-8 "Em Dash"
-// Look here for more: https://www.compart.com/en/unicode/U+2014
-export const EM_DASH = '—'
