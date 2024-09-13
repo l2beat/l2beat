dbe01e2b9321ea9b13396e695ec140ed8f478342
sekuba <sekuba@users.noreply.github.com>
2024-09-13 07:45:22 +0200

    underreview era false

--- packages/config/src/projects/layer2s/zksyncera.ts
+++ packages/config/src/projects/layer2s/zksyncera.ts
@@ -130,7 +130,6 @@ export const zksyncera: Layer2 = {
     Badge.Other.L3HostChain,
     Badge.Infra.ElasticChain,
   ],
-  isUnderReview: true,
   display: {
     name: 'ZKsync Era',
     slug: 'zksync-era',
