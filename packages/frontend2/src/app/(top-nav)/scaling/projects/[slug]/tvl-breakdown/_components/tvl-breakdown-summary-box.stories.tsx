598d6054062b99a22b950f2697af083b87e0459c
torztomasz <tomasz.torz@l2beat.com>
2024-09-13 12:52:14 +0200

    fix story name

--- packages/frontend2/src/app/(top-nav)/scaling/projects/[slug]/tvl-breakdown/_components/tvl-breakdown-summary-box.stories.tsx
+++ packages/frontend2/src/app/(top-nav)/scaling/projects/[slug]/tvl-breakdown/_components/tvl-breakdown-summary-box.stories.tsx
@@ -3,32 +3,29 @@ import { type Meta, type StoryObj } from '@storybook/react'
 import { TvlBreakdownSummaryBox as TvlBreakdownSummaryBoxComponent } from './tvl-breakdown-summary-box'
 
 const meta: Meta<typeof TvlBreakdownSummaryBoxComponent> = {
+  title: 'Components/Tvl Breakdown Summary Box',
   component: TvlBreakdownSummaryBoxComponent,
 }
 export default meta
 type Story = StoryObj<typeof TvlBreakdownSummaryBoxComponent>
 
-const MOCK_DATA = {
-  tvl: {
-    value: 5470000000,
-    change: 0.029,
-  },
-  canonical: {
-    value: 2990000000,
-    change: 0.0238,
-  },
-  external: {
-    value: 2200000000,
-    change: -0.1114,
-  },
-  native: {
-    value: 280000000,
-    change: 0.1249,
-  },
-}
-
-export const Description: Story = {
+export const Default: Story = {
   args: {
-    ...MOCK_DATA,
+    tvl: {
+      value: 5470000000,
+      change: 0.029,
+    },
+    canonical: {
+      value: 2990000000,
+      change: 0.0238,
+    },
+    external: {
+      value: 2200000000,
+      change: -0.1114,
+    },
+    native: {
+      value: 280000000,
+      change: 0.1249,
+    },
   },
 }
