efe93fb70ae7be2590f15b85e8ca3801738f6f53
antooni <antoni.pawlak@protonmail.com>
2024-09-13 11:49:41 +0200

    escape special character

--- packages/backend/scripts/tvl-restore/import.sh
+++ packages/backend/scripts/tvl-restore/import.sh
@@ -1,19 +1,19 @@
 source .env 
 
 echo "IndexerState..."
-pg_restore -d $DEV_LOCAL_DB_URL -t IndexerState "./data/indexer_state.pgdump"
+pg_restore -d $DEV_LOCAL_DB_URL -t "\"IndexerState\"" "./data/indexer_state.pgdump"
 
 echo "IndexerConfiguration..."
-pg_restore -d $DEV_LOCAL_DB_URL -t IndexerConfiguration "./data/indexer_configurations.pgdump"
+pg_restore -d $DEV_LOCAL_DB_URL -t "\"IndexerConfiguration\"" "./data/indexer_configurations.pgdump"
 
 echo "BlockTimestamp..."
-pg_restore -d $DEV_LOCAL_DB_URL -t BlockTimestamp "./data/block_timestamps.pgdump"
+pg_restore -d $DEV_LOCAL_DB_URL -t "\"BlockTimestamp\"" "./data/block_timestamps.pgdump"
 
 echo "Amount..."
-pg_restore -d $DEV_LOCAL_DB_URL -t Amount "./data/amounts.pgdump"
+pg_restore -d $DEV_LOCAL_DB_URL -t "\"Amount\"" "./data/amounts.pgdump"
 
 echo "Price..."
-pg_restore -d $DEV_LOCAL_DB_URL -t Price "./data/prices.pgdump"
+pg_restore -d $DEV_LOCAL_DB_URL -t "\"Price\"" "./data/prices.pgdump"
 
 echo "Value..."
-pg_restore -d $DEV_LOCAL_DB_URL -t Value "./data/values.pgdump"
+pg_restore -d $DEV_LOCAL_DB_URL -t "\"Value\"" "./data/values.pgdump"
