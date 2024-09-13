2619cd13024479537d5cae8487c2f921d5e3723b
antooni <antoni.pawlak@protonmail.com>
2024-09-13 13:57:02 +0200

    add proper flags for all commands

--- packages/backend/scripts/tvl-restore/dump.sh
+++ packages/backend/scripts/tvl-restore/dump.sh
@@ -4,16 +4,16 @@ echo "IndexerState..."
 pg_dump -d "$DEV_REMOTE_DB_URL_READ_ONLY" -t \"IndexerState\" -a -F c -f "./data/indexer.pgdump"
 
 echo "IndexerConfiguration..."
-pg_dump -d "$DEV_REMOTE_DB_URL_READ_ONLY" -t \"IndexerConfiguration\" -a -f "./data/configuration.pgdump"
+pg_dump -d "$DEV_REMOTE_DB_URL_READ_ONLY" -t \"IndexerConfiguration\" -a -F c -f "./data/configuration.pgdump"
 
 echo "BlockTimestamp..."
-pg_dump -d "$DEV_REMOTE_DB_URL_READ_ONLY" -t \"BlockTimestamp\" -a -f "./data/block.pgdump"
+pg_dump -d "$DEV_REMOTE_DB_URL_READ_ONLY" -t \"BlockTimestamp\" -a -F c -f "./data/block.pgdump"
 
 echo "Amount..."
-pg_dump -d "$DEV_REMOTE_DB_URL_READ_ONLY" -t \"Amount\" -a -f "./data/amount.pgdump"
+pg_dump -d "$DEV_REMOTE_DB_URL_READ_ONLY" -t \"Amount\" -a -F c -f "./data/amount.pgdump"
 
 echo "Price..."
-pg_dump -d "$DEV_REMOTE_DB_URL_READ_ONLY" -t \"Price\" -a -f "./data/price.pgdump"
+pg_dump -d "$DEV_REMOTE_DB_URL_READ_ONLY" -t \"Price\" -a -F c -f "./data/price.pgdump"
 
 echo "Value..."
-pg_dump -d "$DEV_REMOTE_DB_URL_READ_ONLY" -t \"Value\" -a -f "./data/value.pgdump"
+pg_dump -d "$DEV_REMOTE_DB_URL_READ_ONLY" -t \"Value\" -a -F c -f "./data/value.pgdump"
