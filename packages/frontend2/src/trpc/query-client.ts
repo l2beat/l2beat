10ebb32070b5a21dc7cf81278daf379891e8fd3e
torztomasz <tomasz.torz@l2beat.com>
2024-09-13 13:22:28 +0200

    use unixtime

--- packages/frontend2/src/trpc/query-client.ts
+++ packages/frontend2/src/trpc/query-client.ts
@@ -1,3 +1,4 @@
+import { UnixTime } from '@l2beat/shared-pure'
 import { QueryClient, defaultShouldDehydrateQuery } from '@tanstack/react-query'
 import SuperJSON from 'superjson'
 
@@ -7,7 +8,7 @@ export const createQueryClient = () =>
       queries: {
         // With SSR, we usually want to set some default staleTime
         // above 0 to avoid refetching immediately on the client
-        staleTime: 10 * 60 * 1000,
+        staleTime: 10 * UnixTime.MINUTE,
       },
       dehydrate: {
         serializeData: SuperJSON.serialize,
