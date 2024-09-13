6cc42405ae775d55fbdd5a33e5d0ff0b10fbc1e1
torztomasz <tomasz.torz@l2beat.com>
2024-09-13 12:33:51 +0200

    stale time

--- packages/frontend2/src/trpc/query-client.ts
+++ packages/frontend2/src/trpc/query-client.ts
@@ -7,7 +7,7 @@ export const createQueryClient = () =>
       queries: {
         // With SSR, we usually want to set some default staleTime
         // above 0 to avoid refetching immediately on the client
-        staleTime: 30 * 1000,
+        staleTime: 10 * 60 * 1000,
       },
       dehydrate: {
         serializeData: SuperJSON.serialize,
