c632654bec3225c438b7eb73a5fead8899c4e327
antooni <antoni.pawlak@protonmail.com>
2024-09-13 14:17:27 +0200

    remove not needed files

--- packages/backend/scripts/tvl-restore/dump.sh
+++
@@ -1,4 +0,0 @@
-source .env
-
-echo "Dumping tables IndexerState, IndexerConfiguration, BlockTimestamp, Amount, Price, and Value..."
-pg_dump -d "$DEV_REMOTE_DB_URL_READ_ONLY" -t \"IndexerState\" -t \"IndexerConfiguration\" -t \"BlockTimestamp\" -t \"Amount\" -t \"Price\" -t \"Value\" -a -F c -f "./data/all_tables.pgdump"
--- packages/backend/scripts/tvl-restore/restore.sh
+++
@@ -1,4 +0,0 @@
-source .env 
-
-echo "IndexerState..."
-pg_restore -d $DEV_LOCAL_DB_URL "./data/all_tables.pgdump"
