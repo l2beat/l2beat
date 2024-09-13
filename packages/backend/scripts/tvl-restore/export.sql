cd6379aba9daaf9e54cbcac89869a1cc87d8173a
Antoni Pawlak <antoni.pawlak@pm.me>
2024-09-13 14:50:38 +0200

    Update tvl-restore script (#5091)

--- packages/backend/scripts/tvl-restore/export.sql
+++
@@ -1,7 +0,0 @@
--- To run it login via psql and then type: \i export.sql 
-\copy "IndexerState" TO './data/indexer_state.csv' CSV;
-\copy "IndexerConfiguration" TO './data/indexer_configurations.csv' CSV;
-\copy "BlockTimestamp" TO './data/block_timestamps.csv' CSV;
-\copy "Amount" TO './data/amounts.csv' CSV;
-\copy "Price" TO './data/prices.csv' CSV;
-\copy "Value" TO './data/values.csv' CSV;
