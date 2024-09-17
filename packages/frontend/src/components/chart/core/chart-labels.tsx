cf8ac127444d06bae9da2fbddfe0135d3a871852
torztomasz <tomasz.torz@l2beat.com>
2024-09-17 10:15:47 +0200

    better fade

--- packages/frontend/src/components/chart/core/chart-labels.tsx
+++ packages/frontend/src/components/chart/core/chart-labels.tsx
@@ -10,26 +10,32 @@ export function ChartLabels({
 }: {
   className?: string
 }) {
-  const { labels } = useChartContext()
+  const { labels, columns } = useChartContext()
+  const isLoading = useChartLoading()
   return (
     <div
       className={cn(
         'pointer-events-none relative z-25 flex h-full flex-col-reverse justify-between pt-5',
+        columns.length <= 1 && !isLoading && 'chart-fade-out-to-bottom',
         className,
       )}
     >
       {labels.map((label, i) => (
-        <ChartLabel key={i}>{label}</ChartLabel>
+        <ChartLabel key={i} isLoading={isLoading}>
+          {label}
+        </ChartLabel>
       ))}
     </div>
   )
 }
 
-function ChartLabel({ children }: { children?: ReactNode }) {
-  const loading = useChartLoading()
+function ChartLabel({
+  children,
+  isLoading,
+}: { children?: ReactNode; isLoading: boolean }) {
   return (
     <div className="relative">
-      {!loading ? (
+      {!isLoading ? (
         <span className="absolute bottom-0 left-0 pb-0.5 text-sm text-gray-500 text-opacity-50 dark:text-white dark:text-opacity-50">
           {children}
         </span>
