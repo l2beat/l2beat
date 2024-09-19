b54240c2753e98d5ef283573d99e382bf925b890
vincfurc <10850139+vincfurc@users.noreply.github.com>
2024-09-19 12:12:49 +0100

    ethereum review

--- packages/config/src/projects/other/da-beat/blockchain/ethereum/ethereum.ts
+++ packages/config/src/projects/other/da-beat/blockchain/ethereum/ethereum.ts
@@ -44,15 +44,14 @@ export const ethereum: DaLayer = {
     The Ethereum consensus protocol is organized around two key time intervals: the slot and the epoch. 
     A slot lasts 12 seconds, and an epoch comprises 32 slots, totaling 6.4 minutes. 
 
-    Validators provide attestations once per epoch. 
-    The attestation workload is spread across all 32 slots within an epoch, with committees of validators, representing 1/32 of the total validator set, making attestations in each slot.
+    Validators provide attestations once per epoch. Attesters are divided into 32 committees, and each one of them votes on one slot during an epoch.
     Each attestation contains votes for the head of the chain, which LMD GHOST uses, and votes for checkpoints, which are utilized by Casper FFG. 
-    The weight of a vote corresponds to the validator's effective balance, and this weight contributes to the overall weight of a branch in the chain.
-    Moreover, in each slot one validator is chosen to propose a block, which includes updates to the beacon state, attestations, and the execution payload containing Ethereum user transactions. 
+    The weight of a vote corresponds to the validator's effective balance which contributes to the overall weight of a branch in the chain.
+    Moreover, in each slot one validator is chosen to propose a block which includes updates to the beacon state, attestations, and the execution payload containing Ethereum user transactions. 
 
     ### Finality
     To ensure consistency among validators voting at different times within an epoch, they are required to vote for a common checkpoint, specifically the first slot of the epoch.
-    Casper FFG achieves finality through a two-round process. In the first round, each validator proposes what they believe is the best checkpoint and collects attestation from the the network about other validators' view. 
+    Casper FFG achieves finality through a two-round process. In the first round, each validator proposes what they believe is the best checkpoint and collects attestation from the network about other validators' views. 
     If 2/3 of the validators agree on the same checkpoint, it becomes justified. In the second round, validators share the justified checkpoint from the first round. If again 2/3 of the validators agree, the checkpoint is finalized.
 
     ![Casper finality](/images/da-layer-technology/ethereum/casper.png#center)\n
@@ -74,14 +73,14 @@ export const ethereum: DaLayer = {
     
     ## L2s Data Availability
 
-    L2s can post data a Ethereum by submitting type-3 transaction, which includes the standard transaction fields along with commitments (versioned hashes) to one or more blobs. Any unused space within a blob still incurs full costs, as gas is charged per blob.
+    L2s can post data a Ethereum by submitting a type-3 transaction, which includes the standard transaction fields along with commitments (versioned hashes) to one or more blobs. Any unused space within a blob still incurs full costs, as gas is charged per blob.
     To retrieve blobs, users have a limited window of 4096 epochs (~18 days) during which they can access them directly from the consensus client using the getBlobSidecars() API call. This API retrieves blobs associated with a specific block rather than a specific transaction. If blobs older than the pruning window need to be accessed, they can be retrieved from an indexer or archiver via API. 
     For transaction-specific blob retrieval, users must fetch all sidecar blobs from a block and identify the one corresponding to the desired versioned hash. <br />
     
     Optimistic rollups use blob data primarily during the process of submitting fraud proofs. 
     When a dispute arises, the rollup must provide the underlying data from a blob as part of the calldata. 
     The blob verification precompile is used to verify that the provided blob data corresponds to the versioned hash that was initially committed in the transaction. 
-    This ensures that the data being used for fraud proof verification is accurate and consistent with what was originally submitted.<br />
+    This ensures that the data being used for fraud-proof verification is accurate and consistent with what was originally submitted.<br />
 
     ZK rollups, on the other hand, use the point evaluation precompile to verify that specific points in the polynomial (represented by the blob) match the expected values. 
     This method allows ZK rollups to prove that the data used in their validity proof is consistent with the blob data committed to Ethereum.
