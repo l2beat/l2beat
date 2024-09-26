d4bfc76f47197e6fd1cbf85a9bcd0a13b5425a17
Piotr Adamczyk <xeno@xeno.yt>
2024-09-26 12:31:48 +0100

    Fix format

--- packages/frontend/src/components/table/cells/project-name-cell.tsx
+++ packages/frontend/src/components/table/cells/project-name-cell.tsx
@@ -33,62 +33,64 @@ export function ProjectNameCell({ project, className }: ProjectCellProps) {
         <span className="text-base font-bold leading-none md:text-lg">
           {project.shortName ?? project.name}
           {project.isVerified === false && (
-        <span className="pl-1.5">
-          <Tooltip>
-            <TooltipTrigger>
-              <UnverifiedIcon className="relative top-px size-4 fill-red-300" />
-            </TooltipTrigger>
-            <TooltipContent>
-              This project contains unverified contracts.
-            </TooltipContent>
-          </Tooltip>
-        </span>
-      )}
-      {project.redWarning && (
-        <span className="pl-1.5">
-          <Tooltip>
-            <TooltipTrigger>
-              <ShieldIcon className="relative top-px size-4 fill-red-300" />
-            </TooltipTrigger>
-            <TooltipContent>{project.redWarning}</TooltipContent>
-          </Tooltip>
-        </span>
-      )}
-      {(!!project.showProjectUnderReview ||
-        !!project.hasImplementationChanged) && (
-        <span className="pl-1.5">
-          <Tooltip>
-            <TooltipTrigger>
-              <UnderReviewIcon className="relative top-px size-4" />
-            </TooltipTrigger>
-            <TooltipContent>
-              {getUnderReviewText(
-                project.showProjectUnderReview,
-                project.hasImplementationChanged,
-              )}
-            </TooltipContent>
-          </Tooltip>
-        </span>
-      )}
-      {project.headerWarning && (
-        <span className="pl-1.5">
-          <Tooltip>
-            <TooltipTrigger>
-              <ShieldIcon className="relative top-px h-4 fill-yellow-700 dark:fill-yellow-300" />
-            </TooltipTrigger>
-            <TooltipContent>
-              <Markdown inline ignoreGlossary>
-                {project.headerWarning}
-              </Markdown>
-            </TooltipContent>
-          </Tooltip>
-        </span>
-      )}
-      {project.data?.syncStatus?.isSynced === false && (
-        <div className="mb-1.5 flex items-center justify-center pl-1.5">
-          <NotSyncedBadge syncedUntil={project.data?.syncStatus.syncedUntil} />
-        </div>
-      )}
+            <span className="pl-1.5">
+              <Tooltip>
+                <TooltipTrigger>
+                  <UnverifiedIcon className="relative top-px size-4 fill-red-300" />
+                </TooltipTrigger>
+                <TooltipContent>
+                  This project contains unverified contracts.
+                </TooltipContent>
+              </Tooltip>
+            </span>
+          )}
+          {project.redWarning && (
+            <span className="pl-1.5">
+              <Tooltip>
+                <TooltipTrigger>
+                  <ShieldIcon className="relative top-px size-4 fill-red-300" />
+                </TooltipTrigger>
+                <TooltipContent>{project.redWarning}</TooltipContent>
+              </Tooltip>
+            </span>
+          )}
+          {(!!project.showProjectUnderReview ||
+            !!project.hasImplementationChanged) && (
+            <span className="pl-1.5">
+              <Tooltip>
+                <TooltipTrigger>
+                  <UnderReviewIcon className="relative top-px size-4" />
+                </TooltipTrigger>
+                <TooltipContent>
+                  {getUnderReviewText(
+                    project.showProjectUnderReview,
+                    project.hasImplementationChanged,
+                  )}
+                </TooltipContent>
+              </Tooltip>
+            </span>
+          )}
+          {project.headerWarning && (
+            <span className="pl-1.5">
+              <Tooltip>
+                <TooltipTrigger>
+                  <ShieldIcon className="relative top-px h-4 fill-yellow-700 dark:fill-yellow-300" />
+                </TooltipTrigger>
+                <TooltipContent>
+                  <Markdown inline ignoreGlossary>
+                    {project.headerWarning}
+                  </Markdown>
+                </TooltipContent>
+              </Tooltip>
+            </span>
+          )}
+          {project.data?.syncStatus?.isSynced === false && (
+            <div className="mb-1.5 flex items-center justify-center pl-1.5">
+              <NotSyncedBadge
+                syncedUntil={project.data?.syncStatus.syncedUntil}
+              />
+            </div>
+          )}
         </span>
         {project.hostChain && (
           <span className="block text-[0.8125rem] font-medium leading-[0.9375rem] text-gray-500 dark:text-zinc-400">
