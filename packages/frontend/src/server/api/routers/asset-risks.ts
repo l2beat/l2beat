d21297d9b72f528babce55967775772e7f67322f
Piotr Adamczyk <xeno@xeno.yt>
2024-09-26 11:13:33 +0100

    Fix address

--- packages/frontend/src/server/api/routers/asset-risks.ts
+++ packages/frontend/src/server/api/routers/asset-risks.ts
@@ -1,12 +1,13 @@
 import { z } from 'zod'
 import { getTokensOfAddress } from '~/server/features/asset-risk/get-tokens-of-address'
 import { procedure, router } from '../trpc'
+import { getAddress } from 'viem'
 
 export const assetRisksRouter = router({
   tokens: procedure
     .input(
       z.object({
-        address: z.string().regex(/^0x[a-fA-F0-9]{40}$/),
+        address: z.string().regex(/^0x[a-fA-F0-9]{40}$/).transform((arg) => getAddress(arg)),
       }),
     )
     .query(async ({ input }) => {
