5c355e1bab0be60907e08b5a76326f052f3502e4
torztomasz <tomasz.torz@l2beat.com>
2024-09-26 09:07:32 +0100

    remove components

--- packages/frontend/src/components/table/filters/index-recalculated-on-filter.ts
+++
@@ -1,9 +0,0 @@
-import { type CellContext } from '@tanstack/react-table'
-
-export function indexRecalculatedOnFilter<T, V>(ctx: CellContext<T, V>) {
-  const index = ctx.table
-    .getFilteredRowModel()
-    .flatRows.findIndex((flatRow) => flatRow.id === ctx.row.id)
-
-  return index === -1 ? 1 : index + 1
-}
