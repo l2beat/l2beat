a2de4999806d858fc4c005e336d13e0b809839da
Tomasz Tórz <93620601+torztomasz@users.noreply.github.com>
2024-09-26 09:11:56 +0100

    Remove collapsed icon and more (#5226)

--- packages/frontend/src/components/badge/tab-count-badge.tsx
+++
@@ -1,7 +0,0 @@
-export function TabCountBadge({ children }: { children: number }) {
-  return (
-    <span className="flex items-center justify-center rounded-full bg-purple-100 px-1.5 py-0.5 text-2xs leading-none text-white md:text-xs">
-      {children}
-    </span>
-  )
-}
