eda603c0e2e8c95b3cb0e5fd0fa9e85b2e63cf34
Mateusz Radomski <radomski.main@protonmail.com>
2024-09-30 15:30:49 +0200

    EthernalStorageProxy

--- packages/discovery/src/discovery/proxies/manual/EthernalStorageProxy.ts
+++ packages/discovery/src/discovery/proxies/manual/EthernalStorageProxy.ts
@@ -1,7 +1,40 @@
-import { ProxyDetails } from '@l2beat/discovery-types'
-import { EthereumAddress } from '@l2beat/shared-pure'
+import { ContractValue, ProxyDetails } from '@l2beat/discovery-types'
+import { assert, EthereumAddress, UnixTime } from '@l2beat/shared-pure'
 
 import { IProvider } from '../../provider/IProvider'
+import { DateAddresses } from '../pastUpgrades'
+import { utils } from 'ethers'
+
+export async function getPastUpgrades(
+  provider: IProvider,
+  address: EthereumAddress,
+): Promise<DateAddresses[]> {
+  const abi = new utils.Interface([
+    'event Upgraded(uint256 version, address indexed implementation)',
+  ])
+  const logs = await provider.getLogs(address, [
+    [abi.getEventTopic('Upgraded')],
+  ])
+
+  const blockNumbers = [...new Set(logs.map((l) => l.blockNumber))]
+  const blocks = await Promise.all(
+    blockNumbers.map(
+      async (blockNumber) => await provider.getBlock(blockNumber),
+    ),
+  )
+  assert(blocks.every((b) => b !== undefined))
+  const dateMap = Object.fromEntries(
+    blocks.map((b) => [
+      b.number,
+      new UnixTime(b.timestamp).toDate().toISOString(),
+    ]),
+  )
+
+  return logs.map((l) => {
+    const implementation = abi.parseLog(l).args.implementation
+    return [dateMap[l.blockNumber] ?? 'ERROR', [implementation]]
+  })
+}
 
 export async function getEternalStorageProxy(
   provider: IProvider,
@@ -20,11 +53,14 @@ export async function getEternalStorageProxy(
   if (!implementation || !admin) {
     return undefined
   }
+  const pastUpgrades = await getPastUpgrades(provider, address)
   return {
     type: 'Eternal Storage proxy',
     values: {
       $admin: admin.toString(),
       $implementation: implementation.toString(),
+      $pastUpgrades: pastUpgrades as ContractValue,
+      $upgradeCount: pastUpgrades.length,
     },
   }
 }
