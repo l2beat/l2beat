897e712e2779edb379baebffbe60146d474926ad
torztomasz <tomasz.torz@l2beat.com>
2024-09-30 15:30:52 +0200

    remove v2 setter

--- packages/frontend/src/app/(new-side-nav)/layout.tsx
+++ packages/frontend/src/app/(new-side-nav)/layout.tsx
@@ -2,13 +2,11 @@ import React from 'react'
 import { Banner } from '~/components/banner'
 import { Footer } from '~/components/footer'
 import { NavLayout } from '~/components/nav/nav-layout'
-import { V2Setter } from './v2-setter'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <NavLayout logoLink="/scaling/summary">
-      <V2Setter />
-      <div className="min-h-screen">
+      <div className="v2 min-h-screen">
         <Banner />
         <div className="mx-auto mb-20 max-w-[1648px] md:px-6 xl:pl-0 xl:pr-6 2xl:pr-0">
           {children}
