ecdebebe0384330f54daaad0c5603e4abea275bf
Mateusz Radomski <radomski.main@protonmail.com>
2024-09-30 15:33:21 +0200

    LightLinkProxy

--- packages/discovery/src/discovery/proxies/manual/LightLinkProxy.ts
+++ packages/discovery/src/discovery/proxies/manual/LightLinkProxy.ts
@@ -1,8 +1,10 @@
-import { ProxyDetails } from '@l2beat/discovery-types'
-import { assert, EthereumAddress } from '@l2beat/shared-pure'
+import { ContractValue, ProxyDetails } from '@l2beat/discovery-types'
+import { assert, EthereumAddress, UnixTime } from '@l2beat/shared-pure'
 
 import { IProvider } from '../../provider/IProvider'
 import { getImplementation } from '../auto/Eip1967Proxy'
+import { DateAddresses } from '../pastUpgrades'
+import { utils } from 'ethers'
 
 async function getRegistryAddress(
   provider: IProvider,
@@ -36,6 +38,37 @@ async function getAdminMultisig(
   return EthereumAddress(multisig.toString())
 }
 
+export async function getPastUpgrades(
+  provider: IProvider,
+  address: EthereumAddress,
+): Promise<DateAddresses[]> {
+  const abi = new utils.Interface([
+    'event Upgraded(address indexed implementation)',
+  ])
+  const logs = await provider.getLogs(address, [
+    [abi.getEventTopic('Upgraded')],
+  ])
+
+  const blockNumbers = [...new Set(logs.map((l) => l.blockNumber))]
+  const blocks = await Promise.all(
+    blockNumbers.map(
+      async (blockNumber) => await provider.getBlock(blockNumber),
+    ),
+  )
+  assert(blocks.every((b) => b !== undefined))
+  const dateMap = Object.fromEntries(
+    blocks.map((b) => [
+      b.number,
+      new UnixTime(b.timestamp).toDate().toISOString(),
+    ]),
+  )
+
+  return logs.map((l) => {
+    const implementation = abi.parseLog(l).args.implementation
+    return [dateMap[l.blockNumber] ?? 'ERROR', [implementation]]
+  })
+}
+
 export async function getLightLinkProxy(
   provider: IProvider,
   address: EthereumAddress,
@@ -45,11 +78,14 @@ export async function getLightLinkProxy(
     return
   }
   const admin = await getAdminMultisig(provider, address)
+  const pastUpgrades = await getPastUpgrades(provider, address)
   return {
     type: 'LightLink proxy',
     values: {
       $admin: admin.toString(),
       $implementation: implementation.toString(),
+      $pastUpgrades: pastUpgrades as ContractValue,
+      $upgradeCount: pastUpgrades.length,
     },
   }
 }
