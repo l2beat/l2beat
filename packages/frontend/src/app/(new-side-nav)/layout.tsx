71964c41b89b8304f45a30e49bab96bd413ca95c
torztomasz <tomasz.torz@l2beat.com>
2024-09-30 15:58:12 +0200

    fix v2 flag

--- packages/frontend/src/app/(new-side-nav)/layout.tsx
+++ packages/frontend/src/app/(new-side-nav)/layout.tsx
@@ -5,8 +5,8 @@ import { NavLayout } from '~/components/nav/nav-layout'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <NavLayout logoLink="/scaling/summary">
-      <div className="v2 min-h-screen">
+    <NavLayout logoLink="/scaling/summary" className="v2">
+      <div className="min-h-screen">
         <Banner />
         <div className="mx-auto mb-20 max-w-[1648px] md:px-6 xl:pl-0 xl:pr-6 2xl:pr-0">
           {children}
