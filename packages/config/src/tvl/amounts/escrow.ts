05a18e45214ad779264219818ed153b068aa107b
antooni <antoni.pawlak@protonmail.com>
2024-09-30 14:48:44 +0200

    escrow

--- packages/config/src/tvl/amounts/escrow.ts
+++ packages/config/src/tvl/amounts/escrow.ts
@@ -20,38 +20,42 @@ export function getEscrowEntry(
   project: BackendProject,
 ): EscrowEntry {
   assert(chain.minTimestampForTvl, 'Chain should have minTimestampForTvl')
+
+  const address = token.address ?? 'native'
+  const chainName = chainConverter.toName(token.chainId)
+  const assetId = AssetId.create(chainName, address)
   const bridgedUsing = escrow.bridgedUsing
-  const includeInTotal = escrow.includeInTotal ?? true
+  const includeInTotal = token.excludeFromTotal
+    ? false
+    : escrow.includeInTotal ?? true
   const isAssociated = !!project.associatedTokens?.includes(token.symbol)
   const sinceTimestamp = UnixTime.max(
     UnixTime.max(chain.minTimestampForTvl, token.sinceTimestamp),
     escrow.sinceTimestamp,
   )
   const source = escrow.source ?? 'canonical'
+  const type = 'escrow'
   const untilTimestamp = getEscrowUntilTimestamp(
     token.untilTimestamp,
     escrow.untilTimestamp,
   )
 
   return {
-    address: token.address ?? 'native',
-    assetId: AssetId.create(
-      chainConverter.toName(token.chainId),
-      token.address,
-    ),
+    address,
+    assetId,
     bridgedUsing,
     category: token.category,
-    chain: chainConverter.toName(token.chainId),
+    chain: chainName,
     dataSource: chain.name,
     decimals: token.decimals,
     escrowAddress: escrow.address,
-    includeInTotal: token.excludeFromTotal ? false : includeInTotal,
+    includeInTotal,
     isAssociated,
     project: project.projectId,
     sinceTimestamp,
     source,
     symbol: token.symbol,
-    type: 'escrow',
+    type,
     untilTimestamp,
   }
 }
