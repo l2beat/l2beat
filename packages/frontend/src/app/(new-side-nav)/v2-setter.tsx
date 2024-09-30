5727cb10f425181f92a4a3a956b8fffda206c84f
torztomasz <tomasz.torz@l2beat.com>
2024-09-30 14:37:06 +0200

    fix v2 setter

--- packages/frontend/src/app/(new-side-nav)/v2-setter.tsx
+++ packages/frontend/src/app/(new-side-nav)/v2-setter.tsx
@@ -5,6 +5,10 @@ import { useEffect } from 'react'
 export function V2Setter() {
   useEffect(() => {
     document.documentElement.classList.add('v2')
+
+    return () => {
+      document.documentElement.classList.remove('v2')
+    }
   }, [])
 
   return null
