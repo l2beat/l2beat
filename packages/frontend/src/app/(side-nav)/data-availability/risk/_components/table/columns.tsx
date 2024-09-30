88cafabcf1c78ba1dc78f41b448eae0f15645645
Michał Podsiadły <michal.podsiadly@l2beat.com>
2024-09-30 12:52:17 +0200

    feat: basic tooltips

--- packages/frontend/src/app/(side-nav)/data-availability/risk/_components/table/columns.tsx
+++ packages/frontend/src/app/(side-nav)/data-availability/risk/_components/table/columns.tsx
@@ -20,6 +20,10 @@ export const columns = [
         }}
       />
     ),
+    meta: {
+      tooltip:
+        'The data availability layer where the data (transaction data or state diffs) is posted.',
+    },
   }),
   columnHelper.accessor('daBridge', {
     header: 'DA Bridge',
@@ -58,6 +62,8 @@ export const columns = [
     meta: {
       cellClassName: 'pl-8',
       headClassName: 'pl-8',
+      tooltip:
+        'The DA bridge through which Ethereum is informed that data has been made available. There might be multiple options for each layer, and L2s can choose which one to use, if any.',
     },
   }),
   columnHelper.accessor('risks.economicSecurity', {
@@ -68,6 +74,10 @@ export const columns = [
         rowA.original.risks.economicSecurity,
         rowB.original.risks.economicSecurity,
       ),
+    meta: {
+      tooltip:
+        'Shows if there are any onchain (staked assets) or offchain (reputation) guarantees that would prevent a committee from deceiving the DA bridge.',
+    },
   }),
   columnHelper.accessor('risks.fraudDetection', {
     header: 'Fraud detection',
@@ -77,20 +87,36 @@ export const columns = [
         rowA.original.risks.fraudDetection,
         rowB.original.risks.fraudDetection,
       ),
+    meta: {
+      tooltip:
+        'Shows if there are any mechanism for users to protect themselves against a malicious majority of committee members, such as validators, and recover from data withholding attack.',
+    },
   }),
   columnHelper.accessor('risks.attestations', {
     header: 'Attestation security',
     cell: (ctx) => <RiskCell risk={ctx.getValue()} emptyMode="em-dash" />,
     enableSorting: false,
+    meta: {
+      tooltip:
+        'Shows if the DA bridge can securely confirm that the data availability attestations are backed by the DA layer’s economic security, meaning that the signatures from the DA layer are accurately verified and tracked on-chain.',
+    },
   }),
   columnHelper.accessor('risks.exitWindow', {
     header: 'Exit window',
     cell: (ctx) => <RiskCell risk={ctx.getValue()} emptyMode="em-dash" />,
     enableSorting: false,
+    meta: {
+      tooltip:
+        'Shows if the DA bridge can be upgraded, and if yes - if there’s a mechanism in place for withdrawals, and the time allowed for users to exit in case of an upgrade. ',
+    },
   }),
   columnHelper.accessor('risks.accessibility', {
     header: 'Accessibility',
     cell: (ctx) => <RiskCell risk={ctx.getValue()} emptyMode="em-dash" />,
     enableSorting: false,
+    meta: {
+      tooltip:
+        'Shows if there is an additional trust assumption on the majority of committee members. It distinguishes between DA solutions that are integrated into the Ethereum protocol (enshrined) and those that are external, thus requiring an additional trust assumption.',
+    },
   }),
 ]
