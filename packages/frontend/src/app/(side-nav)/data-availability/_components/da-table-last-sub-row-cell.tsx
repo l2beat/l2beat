40a00aeaebee4dca0131f5e38c2fa329c4b9ac35
torztomasz <tomasz.torz@l2beat.com>
2024-10-01 17:59:51 +0200

    fix font size

--- packages/frontend/src/app/(side-nav)/data-availability/_components/da-table-last-sub-row-cell.tsx
+++ packages/frontend/src/app/(side-nav)/data-availability/_components/da-table-last-sub-row-cell.tsx
@@ -15,7 +15,7 @@ export function DaTableLastSubRowCell({
   return (
     <td
       className={cn(
-        'group whitespace-pre bg-n-gray-200 text-xs group-hover:bg-black/[0.1] dark:bg-white/[0.1] dark:group-hover:bg-white/[0.2] md:text-lg',
+        'group whitespace-pre bg-n-gray-200 text-xs group-hover:bg-black/[0.1] dark:bg-white/[0.1] dark:group-hover:bg-white/[0.2] md:text-base',
         firstRow && 'rounded-tr-xl',
         lastRow && 'rounded-br-xl',
         !lastRow && 'border-b border-b-gray-200 dark:border-b-zinc-700 ',
