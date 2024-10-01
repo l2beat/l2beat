44a272605826a951722a52b2a862abdf86e16896
Michał Podsiadły <67391475+sdlyy@users.noreply.github.com>
2024-10-01 11:22:55 +0200

    Sort usedIn by TVL and cap max amount of icons (#5270)

--- packages/frontend/src/app/(side-nav)/data-availability/summary/_components/table/projects-used-in.tsx
+++ packages/frontend/src/app/(side-nav)/data-availability/summary/_components/table/projects-used-in.tsx
@@ -10,12 +10,17 @@ import { cn } from '~/utils/cn'
 interface Props {
   usedIn: UsedInProject[]
   className?: string
+  maxProjects?: number
 }
 
-export function ProjectsUsedIn({ usedIn, className }: Props) {
+export function ProjectsUsedIn({ usedIn, className, maxProjects = 5 }: Props) {
+  const cappedProjects = usedIn.slice(0, maxProjects)
+
+  const restCount = usedIn.slice(maxProjects).length
+
   return (
-    <div className={cn('flex gap-1.5', className)}>
-      {usedIn.map((project) => {
+    <div className={cn('flex items-center justify-center gap-1.5', className)}>
+      {cappedProjects.map((project) => {
         return (
           <Tooltip key={project.slug}>
             <TooltipTrigger>
@@ -31,6 +36,11 @@ export function ProjectsUsedIn({ usedIn, className }: Props) {
           </Tooltip>
         )
       })}
+      {!!restCount && (
+        <span className="text-2xs text-zinc-800 dark:text-gray-50 md:ml-2">
+          +{restCount} more
+        </span>
+      )}
     </div>
   )
 }
