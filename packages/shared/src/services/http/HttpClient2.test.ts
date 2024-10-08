f7f38c166736b0f776286d4492f9b5ad94d03f9a
antooni <antoni.pawlak@protonmail.com>
2024-10-08 15:18:39 +0200

    tests

--- packages/shared/src/services/http/HttpClient2.test.ts
+++ packages/shared/src/services/http/HttpClient2.test.ts
@@ -4,22 +4,28 @@ import nock from 'nock'
 import { HttpClient2 } from './HttpClient2'
 
 describe(HttpClient2.name, () => {
-  const http = new HttpClient2()
-
   describe(HttpClient2.prototype.fetchJson.name, () => {
     it('parses json', async () => {
+      const http = new HttpClient2()
       nock('https://api')
         .get('/')
         .reply(200, JSON.stringify({ a: 1, b: 2 }))
 
       const parsed = await http.fetchJson('https://api')
-
       expect(parsed).toEqual({ a: 1, b: 2 })
     })
-    it('throws error with context', async () => {})
+
+    it('throws error with context', async () => {
+      const http = new HttpClient2({ maxRetries: 0 })
+      nock('https://api').get('/').reply(404, { message: 'info' })
+
+      await expect(
+        async () => await http.fetchJson('https://api'),
+      ).toBeRejectedWith('HTTP error: 404 Not Found {"message":"info"}')
+    })
+
     it('supports custom timeout', async function () {
       const http = new HttpClient2({ maxRetries: 0, timeoutMs: 200 })
-
       nock('https://api').get('/').delay(300).reply(200, { data: 'some data' })
 
       await expect(
@@ -27,6 +33,26 @@ describe(HttpClient2.name, () => {
       ).toBeRejected()
     })
   })
-  it('retries fetch')
-  it('supports max retry delay')
+
+  it('retries fetch', async () => {
+    const http = new HttpClient2({ maxRetries: 2, maxRetryDelayMs: 10 })
+    nock('https://api').get('/').times(2).reply(500, { message: 'info' })
+    nock('https://api').get('/').reply(200, { data: 'some data' })
+
+    const result = await http.fetchJson('https://api')
+    expect(result).toEqual({ data: 'some data' })
+  })
+
+  it('supports max retry delay', async () => {
+    const http = new HttpClient2({
+      maxRetries: 2,
+      initialRetryDelayMs: 100_000,
+      maxRetryDelayMs: 10, // if not for this test would timeout
+    })
+    nock('https://api').get('/').times(1).reply(500, { message: 'info' })
+    nock('https://api').get('/').reply(200, { data: 'some data' })
+
+    const result = await http.fetchJson('https://api')
+    expect(result).toEqual({ data: 'some data' })
+  })
 })
--- packages/shared/src/services/http/HttpClient2.ts
+++ packages/shared/src/services/http/HttpClient2.ts
@@ -1,4 +1,4 @@
-import fetch, { RequestInit } from 'node-fetch'
+import fetch, { RequestInit, Response } from 'node-fetch'
 
 interface HttpClient2Options {
   timeoutMs?: number
@@ -9,46 +9,65 @@ interface HttpClient2Options {
 }
 
 export class HttpClient2 {
-  private readonly options: Required<HttpClient2Options>
+  private readonly $: Required<HttpClient2Options>
 
-  constructor(options: HttpClient2Options = {}) {
-    this.options = {
+  constructor($: HttpClient2Options = {}) {
+    this.$ = {
       timeoutMs: 10_000,
       maxRetries: 10,
       initialRetryDelayMs: 1000,
       maxRetryDelayMs: 30000,
       statusCodesToRetry: [408, 429, 500, 502, 503, 504],
-      ...options,
+      ...$,
     }
   }
 
   async fetchJson(url: string, init?: RequestInit): Promise<unknown> {
-    let retries = 0
+    let calls = 0
     while (true) {
       try {
         const res = await fetch(url, {
           ...init,
-          timeout: this.options.timeoutMs,
+          timeout: this.$.timeoutMs,
         })
+        const { success, shouldRetry } = this.assertStatus(calls, res)
 
-        if (!res.ok) {
+        if (success) {
+          return res.json()
+        }
+
+        if (!shouldRetry) {
           const body = await res.text()
           throw new Error(`HTTP error: ${res.status} ${res.statusText} ${body}`)
         }
-
-        return res.json()
       } catch (error) {
-        if (retries >= this.options.maxRetries) {
+        if (!this.shouldRetryFetch(calls)) {
           throw error
         }
-
-        const delay = Math.min(
-          this.options.initialRetryDelayMs * Math.pow(2, retries),
-          this.options.maxRetryDelayMs,
-        )
-        await new Promise((resolve) => setTimeout(resolve, delay))
-        retries++
       }
+
+      const delay = Math.min(
+        this.$.initialRetryDelayMs * Math.pow(2, calls),
+        this.$.maxRetryDelayMs,
+      )
+      await new Promise((resolve) => setTimeout(resolve, delay))
+      calls++
     }
+  }
+
+  private assertStatus(calls: number, res: Response) {
+    if (res.ok) {
+      return { success: true }
+    }
+
+    const shouldRetry =
+      this.shouldRetryFetch(calls) &&
+      this.$.statusCodesToRetry.includes(res.status)
+
+    return { success: false, shouldRetry }
+  }
+
+  private shouldRetryFetch(calls: number): boolean {
+    return calls < this.$.maxRetries
   }
 }
