84cc8b5c9cbe6a4cbe0ff2b2178d02443d0463ea
sekuba <sekuba@users.noreply.github.com>
2024-10-09 12:56:35 +0200

    Fraxtal

--- packages/config/src/projects/layer2s/fraxtal.ts
+++ packages/config/src/projects/layer2s/fraxtal.ts
@@ -1,4 +1,4 @@
-import { UnixTime, formatSeconds } from '@l2beat/shared-pure'
+import { UnixTime } from '@l2beat/shared-pure'
 
 import { ProjectDiscovery } from '../../discovery/ProjectDiscovery'
 import { Badge } from '../badges'
@@ -6,15 +6,6 @@ import { opStackL2 } from './templates/opStack'
 import { Layer2 } from './types'
 
 const discovery = new ProjectDiscovery('fraxtal')
-
-const timelockDelay = formatSeconds(
-  discovery.getContractValue('Timelock', 'delay'),
-)
-
-const upgradeability = {
-  upgradableBy: ['ProxyAdmin'],
-  upgradeDelay: 'No delay',
-}
 
 export const fraxtal: Layer2 = opStackL2({
   badges: [Badge.DA.CustomDA],
@@ -79,44 +70,9 @@ export const fraxtal: Layer2 = opStackL2({
     },
     activityDataSource: 'Blockchain RPC',
   },
-  upgradeability,
   rpcUrl: 'https://rpc.frax.com',
   genesisTimestamp: new UnixTime(1706811599),
   isNodeAvailable: true,
-  nonTemplatePermissions: [
-    ...discovery.getMultisigPermission(
-      'FraxtalMultisig',
-      'This address is the owner of the following contracts: ProxyAdmin, SystemConfig. It is also designated as a Guardian of the FraxchainPortal, meaning it can halt withdrawals. It can upgrade the bridge implementation potentially gaining access to all funds, and change the sequencer, state root proposer or any other system component (unlimited upgrade power). This address is also the permissioned challenger of the system. It can delete non finalized roots without going through the fault proof process.',
-    ),
-    ...discovery.getMultisigPermission(
-      'frxETHMultisig',
-      'This address is the owner of the frxETHMinter contract. It can pause and unpause ETH deposits, change how much ETH is withheld from each submit() transaction and withdraw arbitrary amounts of assets from the minter contract.',
-    ),
-    ...discovery.getMultisigPermission(
-      'TimelockMultisig',
-      'This address is the owner of the timelock smart contract. It can queue, cancel, and execute transactions in the timelock (e.g., adding and removing frxETH whitelisted minters).',
-    ),
-  ],
-  nonTemplateContracts: [
-    discovery.getContractDetails('frxETH', {
-      description:
-        'Fraxtal uses Frax Ether (frxETH) as the designated gas token, allowing users to utilize frxETH to pay for blockspace.',
-    }),
-    discovery.getContractDetails('frxETHMinter', {
-      description:
-        'Authorized minter contract for frxETH, accepts user-supplied ETH and converts it to frxETH.',
-    }),
-    discovery.getContractDetails('sfrxETH', {
-      description:
-        'Vault token contract (ERC-4626) for staked frxETH. The smart contract receives frxETH tokens and mints sfrxETH tokens.',
-    }),
-    discovery.getContractDetails('Timelock', {
-      description: `Allows for time-delayed execution of transactions in the FrxETH smart contract, such as adding and removing whitelisted minters. Delay is set to ${timelockDelay}.`,
-    }),
-    discovery.getContractDetails('SuperchainConfig', {
-      description: `Upgradable contract that manages the PAUSED_SLOT, a boolean value indicating whether the Superchain is paused, and GUARDIAN_SLOT, the address of the guardian which can pause and unpause the system. The address of the guardian can only be modified by the ProxyAdmin by upgrading the SuperchainConfig contract. This contract is a fork of Optimism's superchainConfig contract and may not be utilized by other chains.`,
-    }),
-  ],
   chainConfig: {
     name: 'fraxtal',
     chainId: 252,
@@ -131,4 +87,5 @@ export const fraxtal: Layer2 = opStackL2({
   },
   nonTemplateEscrows: [],
   nonTemplateOptimismPortalEscrowTokens: ['frxETH'],
+  discoveryDrivenData: true,
 })
