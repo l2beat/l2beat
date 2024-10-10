275a18de09e65285303be0988320b3a822a860b5
Michał Podsiadły <michal.podsiadly@l2beat.com>
2024-10-10 15:19:16 +0200

    feat: verically align host chain risks bar

--- packages/frontend/src/components/host-chain-risks-warning.tsx
+++ packages/frontend/src/components/host-chain-risks-warning.tsx
@@ -12,27 +12,29 @@ export function HostChainRisksWarning({
   riskCount,
 }: HostChainRisksWarningProps) {
   const text = riskCount
-    ? 'There are ' + riskCount + ' additional risks coming from the hostchain'
-    : 'The section considers only the L3 properties. For more details please refer to'
+    ? 'There are ' + riskCount + ' additional risks coming from the hostchain '
+    : 'The section considers only the L3 properties. For more details please refer to '
 
   return (
-    <div className="w-full rounded-lg bg-gray-200 px-4 py-2 text-center dark:bg-zinc-800">
-      <span className="text-xs font-medium">{text} </span>
-      <span className="inline-flex gap-1 align-middle">
-        <Image
-          className="size-5"
-          src={`/icons/${hostChain.slug}.png`}
-          width={20}
-          height={20}
-          alt={`${hostChain.name} logo`}
-        />
-        <Link
-          href={`/scaling/projects/${hostChain.slug}`}
-          className="text-xs font-medium text-blue-700 underline hover:text-blue-550 dark:text-blue-500 dark:hover:text-blue-550"
-        >
-          {hostChain.name}
-        </Link>
-      </span>
+    <div className="flex w-full items-center rounded-lg bg-gray-200 px-4 py-2 text-xs font-medium dark:bg-zinc-800">
+      <div>
+        {text}{' '}
+        <span className="inline-block">
+          <Image
+            className="mr-1 inline-block size-5"
+            src={`/icons/${hostChain.slug}.png`}
+            width={20}
+            height={20}
+            alt={`${hostChain.name} logo`}
+          />
+          <Link
+            href={`/scaling/projects/${hostChain.slug}`}
+            className="inline-block text-xs font-medium text-blue-700 underline hover:text-blue-550 dark:text-blue-500 dark:hover:text-blue-550"
+          >
+            {hostChain.name}
+          </Link>
+        </span>
+      </div>
     </div>
   )
 }
