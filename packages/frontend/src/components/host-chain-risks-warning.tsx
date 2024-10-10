6932bfaa417b2a9306a5a3aa8f7749d1fb59eb22
Michał Podsiadły <michal.podsiadly@l2beat.com>
2024-10-10 13:43:02 +0200

    feat: center host chain risks warning bar

--- packages/frontend/src/components/host-chain-risks-warning.tsx
+++ packages/frontend/src/components/host-chain-risks-warning.tsx
@@ -12,13 +12,11 @@ export function HostChainRisksWarning({
   riskCount,
 }: HostChainRisksWarningProps) {
   const text = riskCount
-    ? 'There are ' +
-      riskCount +
-      ' additional risks coming from the hostchain - '
+    ? 'There are ' + riskCount + ' additional risks coming from the hostchain'
     : 'The section considers only the L3 properties. For more details please refer to'
 
   return (
-    <div className="w-full rounded-lg bg-gray-200 px-4 py-2 dark:bg-zinc-800">
+    <div className="w-full rounded-lg bg-gray-200 px-4 py-2 text-center dark:bg-zinc-800">
       <span className="text-xs font-medium">{text} </span>
       <span className="inline-flex gap-1 align-middle">
         <Image
