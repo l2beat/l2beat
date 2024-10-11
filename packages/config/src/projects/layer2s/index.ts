c1f003447d629232adce320c2e1e9ff0f07ceac5
carl587 <carl@clica.tech>
2024-10-12 01:26:40 +0800

    Add Turbo Protocol (#5331)
    Co-authored-by: carl587 <carl587@clica.tech>
    Co-authored-by: Radina Talanova <nt.radina@gmail.com>
    Co-authored-by: Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
    Co-authored-by: Luca Donno <donnoh99@gmail.com>

--- packages/config/src/projects/layer2s/index.ts
+++ packages/config/src/projects/layer2s/index.ts
@@ -143,6 +143,7 @@ import { ternoa } from './ternoa'
 import { thanos } from './thanos'
 import { thebinaryholdings } from './thebinaryholdings'
 import { treasure } from './treasure'
+import { turboprotocol } from './turboprotocol'
 import { Layer2 } from './types'
 import { wirex } from './wirex'
 import { witness } from './witness'
@@ -311,6 +312,7 @@ export const layer2s: Layer2[] = [
   thebinaryholdings,
   thanos,
   treasure,
+  turboprotocol,
   wirex,
   witness,
   world,
