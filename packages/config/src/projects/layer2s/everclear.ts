b4e6b3b008f7e28b320ab58495a1e0dfff780822
sekuba <sekuba@users.noreply.github.com>
2024-10-11 13:50:26 +0200

    Add Everclear (full)

--- packages/config/src/projects/layer2s/everclear.ts
+++ packages/config/src/projects/layer2s/everclear.ts
@@ -1,29 +1,48 @@
-import { UnixTime } from '@l2beat/shared-pure'
-import { upcomingL2 } from './templates/upcoming'
+import { ProjectDiscovery } from '../../discovery/ProjectDiscovery'
+import { Badge } from '../badges'
+import { orbitStackL2 } from './templates/orbitStack'
 import { Layer2 } from './types'
 
-export const everclear: Layer2 = upcomingL2({
-  id: 'everclear',
-  createdAt: new UnixTime(1717512064), // 2024-06-04T14:41:04Z
+const discovery = new ProjectDiscovery('everclear')
+
+export const everclear: Layer2 = orbitStackL2({
+  badges: [Badge.RaaS.Gelato, Badge.DA.EigenDA],
   display: {
     name: 'Everclear',
     slug: 'everclear',
+    redWarning:
+      'Critical contracts can be upgraded by an EOA which could result in the loss of all funds.',
     description:
-      'Everclear is an upcoming Layer 2 on Ethereum, built on the Orbit stack and EigenDA. It aims to coordinate settlement of liquidity between chains and solve fragmentation for modular blockchains.',
+      'Everclear is an AnyTrust Optimium on Ethereum, built on the Orbit stack. It is used as a liquidity hub (clearing layer) to solve the liquidity fragmentation between modular scaling solutions.',
     purposes: ['Interoperability'],
-    category: 'Optimium',
-    provider: 'Arbitrum',
     links: {
       websites: ['https://everclear.org'],
-      apps: [],
+      apps: ['https://bridge.gelato.network/bridge/everclear-mainnet'],
       documentation: ['https://docs.everclear.org'],
-      explorers: [],
-      repositories: [],
+      explorers: ['https://scan.everclear.org/'],
+      repositories: ['https://github.com/connext'],
       socialMedia: [
         'https://x.com/everclearorg',
         'https://discord.gg/everclear',
         'https://blog.everclear.org/',
       ],
     },
+    activityDataSource: 'Blockchain RPC',
   },
+  nonTemplatePermissions: [
+    {
+      name: 'AdminEOA',
+      accounts: discovery.getAccessControlRolePermission(
+        'UpgradeExecutor',
+        'EXECUTOR_ROLE',
+      ),
+      description:
+        'Can upgrade any project implementation via UpgradeExecutor, potentially gaining access to all funds.',
+    },
+  ],
+  rpcUrl: 'https://rpc.everclear.raas.gelato.cloud',
+  discovery,
+  bridge: discovery.getContract('Bridge'),
+  rollupProxy: discovery.getContract('RollupProxy'),
+  sequencerInbox: discovery.getContract('SequencerInbox'),
 })
