8df5cb215ae23c5e3ea0763f0b86fb5fb17f57b4
sekuba <sekuba@users.noreply.github.com>
2024-10-11 13:52:12 +0200

    remove eigenDA badge

--- packages/config/src/projects/layer2s/everclear.ts
+++ packages/config/src/projects/layer2s/everclear.ts
@@ -6,7 +6,7 @@ import { Layer2 } from './types'
 const discovery = new ProjectDiscovery('everclear')
 
 export const everclear: Layer2 = orbitStackL2({
-  badges: [Badge.RaaS.Gelato, Badge.DA.EigenDA],
+  badges: [Badge.RaaS.Gelato],
   display: {
     name: 'Everclear',
     slug: 'everclear',
