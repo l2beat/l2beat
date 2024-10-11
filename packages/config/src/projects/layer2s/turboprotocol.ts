ef7297fa22658d474907280b6418d6bec6654e86
Luca Donno <donnoh99@gmail.com>
2024-10-12 00:07:27 +0700

    time

--- packages/config/src/projects/layer2s/turboprotocol.ts
+++ packages/config/src/projects/layer2s/turboprotocol.ts
@@ -1,8 +1,10 @@
+import { UnixTime } from '@l2beat/shared-pure'
 import { upcomingL2 } from './templates/upcoming'
 import { Layer2 } from './types'
 
 export const turboprotocol: Layer2 = upcomingL2({
   id: 'turboprotocol',
+  createdAt: new UnixTime(1728666437),
   display: {
     name: 'Turbo Protocol',
     slug: 'turboprotocol',
