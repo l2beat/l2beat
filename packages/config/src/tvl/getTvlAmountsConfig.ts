dd750e73fae7604aa1d4d5fae351c9e1f86339b0
antooni <antoni.pawlak@protonmail.com>
2024-10-11 12:51:04 +0200

    refactor l1tokens function

--- packages/config/src/tvl/getTvlAmountsConfig.ts
+++ packages/config/src/tvl/getTvlAmountsConfig.ts
@@ -11,9 +11,9 @@ import { chains } from '../chains'
 import { ChainConfig } from '../common'
 import { tokenList } from '../tokens'
 import { getCirculatingSupplyEntry } from './amounts/circulatingSupply'
+import { addSharedEscrowsL1Tokens } from './amounts/custom/addSharedEscrowsL1Tokens'
 import { aggLayerEscrowToEntries } from './amounts/custom/aggLayerEscrowToEntries'
 import { elasticChainEscrowToEntries } from './amounts/custom/elasticChainEscrowToEntries'
-import { handleL1Tokens } from './amounts/custom/handleL1Tokens'
 import { getEscrowEntry } from './amounts/escrow'
 import { getPremintedEntry } from './amounts/preminted'
 import { getTotalSupplyEntry } from './amounts/totalSupply'
@@ -21,7 +21,7 @@ import { getTotalSupplyEntry } from './amounts/totalSupply'
 export function getTvlAmountsConfig(
   projects: BackendProject[],
 ): AmountConfigEntry[] {
-  let entries: AmountConfigEntry[] = []
+  const entries: AmountConfigEntry[] = []
 
   const nonZeroSupplyTokens = tokenList.filter((t) => t.supply !== 'zero')
   for (const token of nonZeroSupplyTokens) {
@@ -96,28 +96,14 @@ export function getTvlAmountsConfig(
     }
   }
 
-  const projectsWithL1Tokens = projects.filter((p) =>
-    p.escrows.some(
-      (e) =>
-        (e.sharedEscrow?.type === 'AggLayer' ||
-          e.sharedEscrow?.type === 'ElasticChian') &&
-        e.sharedEscrow?.tokensToAssignFromL1?.length &&
-        e.sharedEscrow.tokensToAssignFromL1.length > 0,
-    ),
-  )
-
-  if (projectsWithL1Tokens.length > 0) {
-    entries = handleL1Tokens(projectsWithL1Tokens, entries)
-  }
-
-  return entries
+  return addSharedEscrowsL1Tokens(projects, entries)
 }
 
 /** Lighter version of `getTvlAmountsConfig`, does not that much nor enforces full configuration compatibility  */
 export function getTvlAmountsConfigForProject(
   project: BackendProject,
 ): AmountConfigEntry[] {
-  let entries: AmountConfigEntry[] = []
+  const entries: AmountConfigEntry[] = []
 
   const nonZeroSupplyTokens = tokenList.filter((t) => t.supply !== 'zero')
 
@@ -178,17 +164,7 @@ export function getTvlAmountsConfigForProject(
     }
   }
 
-  if (
-    project.escrows.some(
-      (e) =>
-        e.sharedEscrow?.tokensToAssignFromL1?.length &&
-        e.sharedEscrow.tokensToAssignFromL1.length > 0,
-    )
-  ) {
-    entries = handleL1Tokens([project], entries)
-  }
-
-  return entries
+  return addSharedEscrowsL1Tokens([project], entries)
 }
 
 const chainConverter = new ChainConverter(
