08d6e898e40d19cacc7ea1807f6625f8b618c8b4
Luca Donno <donnoh99@gmail.com>
2024-10-11 23:33:08 +0700

    fix

--- packages/config/src/projects/layer3s/lumiterra.ts
+++ packages/config/src/projects/layer3s/lumiterra.ts
@@ -3,7 +3,7 @@ import { ProjectId } from '@l2beat/shared-pure'
 import { underReviewL3 } from '../layer2s/templates/underReview'
 import { Layer3 } from './types'
 
-export const muster: Layer3 = underReviewL3({
+export const lumiterra: Layer3 = underReviewL3({
   id: 'lumiterra',
   hostChain: ProjectId('arbitrum'),
   display: {
