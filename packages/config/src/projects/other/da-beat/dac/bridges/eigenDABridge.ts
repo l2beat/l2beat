8261dce6cedb7c0e081b0bbba3848682dafa1816
vincfurc <10850139+vincfurc@users.noreply.github.com>
2024-10-13 23:04:40 +0100

    upgradability, misc

--- packages/config/src/projects/other/da-beat/dac/bridges/eigenDABridge.ts
+++ packages/config/src/projects/other/da-beat/dac/bridges/eigenDABridge.ts
@@ -12,6 +12,11 @@ import { toUsedInProject } from '../../utils/to-used-in-project'
 const discovery = new ProjectDiscovery('eigenda')
 const eigenDiscovery = new ProjectDiscovery('shared-eigenlayer')
 
+const upgrades = {
+  upgradableBy: ['EigenDAProxyAdmin'],
+  upgradeDelay: 'No delay',
+}
+
 const quorumThresholds = discovery.getContractValue<string>(
   'EigenDAServiceManager',
   'quorumConfirmationThresholdPercentages',
@@ -19,6 +24,16 @@ const quorumThresholds = discovery.getContractValue<string>(
 
 const quorum1Threshold = parseInt(quorumThresholds.substring(2, 4), 16)
 const quorum2Threshold = parseInt(quorumThresholds.substring(4, 6), 16)
+
+const operatorSetParamsQuorum1 = discovery.getContractValue<number[]>(
+  'RegistryCoordinator',
+  'operatorSetParamsQuorum1',
+)
+
+const operatorSetParamsQuorum2 = discovery.getContractValue<number[]>(
+  'RegistryCoordinator',
+  'operatorSetParamsQuorum2',
+)
 
 const batchConfirmers = discovery.getContractValue<string[]>(
   'EigenDAServiceManager',
@@ -59,29 +74,46 @@ export const eigenDAbridge = {
   },
   contracts: {
     addresses: [
-      discovery.getContractDetails('EigenDAServiceManager', {
-        description:
-          'The EigenDAServiceManager contract is the bridge contract that accepts blob batches data availability attestations. Batches availability is attested by EigenDA operators signatures and relayed to the service manager contract by the EigenDA disperser.',
-      }),
-      discovery.getContractDetails('RegistryCoordinator', {
-        description: `Contract used by operators to register with the EigenDA AVS. The coordinator has three registries: a StakeRegistry that keeps track of operators' stakes, a BLSApkRegistry that keeps track of operators' BLS public keys and aggregate BLS public keys for each quorum, and an IndexRegistry that keeps track of an ordered list of operators for each quorum.`,
-      }),
-      eigenDiscovery.getContractDetails('PauserRegistry', {
-        description:
-          'Defines and stores pauser and unpauser roles for EigenLayer contracts and the EigenDAServiceManager.',
-      }),
-      discovery.getContractDetails('StakeRegistry', {
-        description:
-          'The StakeRegistry contract keeps track of the total stake of each operator.',
-      }),
-      discovery.getContractDetails('BLSApkRegistry', {
-        description:
-          'The BLSApkRegistry contract keeps track of the BLS public keys of each operator and the quorum aggregated keys.',
-      }),
-      discovery.getContractDetails('EjectionManager', {
-        description:
-          'The EjectionManager contract is responsible for ejecting operators from a quorum for violating the Service Legal Agreement (SLA).',
-      }),
+      {
+        ...discovery.getContractDetails('EigenDAServiceManager', {
+          description:
+            'The EigenDAServiceManager contract is the bridge contract that accepts blob batches data availability attestations. Batches availability is attested by EigenDA operators signatures and relayed to the service manager contract by the EigenDA disperser.',
+        }),
+        ...upgrades,
+      },
+      {
+        ...discovery.getContractDetails('RegistryCoordinator', {
+          description: `Contract used by operators to register with the EigenDA AVS. The coordinator has three registries: a StakeRegistry that keeps track of operators' stakes, a BLSApkRegistry that keeps track of operators' BLS public keys and aggregate BLS public keys for each quorum, and an IndexRegistry that keeps track of an ordered list of operators for each quorum.`,
+        }),
+        ...upgrades,
+      },
+      {
+        ...eigenDiscovery.getContractDetails('PauserRegistry', {
+          description:
+            'Defines and stores pauser and unpauser roles for EigenLayer contracts and the EigenDAServiceManager.',
+        }),
+      },
+      {
+        ...discovery.getContractDetails('StakeRegistry', {
+          description:
+            'The StakeRegistry contract keeps track of the total stake of each operator.',
+        }),
+        ...upgrades,
+      },
+      {
+        ...discovery.getContractDetails('BLSApkRegistry', {
+          description:
+            'The BLSApkRegistry contract keeps track of the BLS public keys of each operator and the quorum aggregated keys.',
+        }),
+        ...upgrades,
+      },
+      {
+        ...discovery.getContractDetails('EjectionManager', {
+          description:
+            'The EjectionManager contract is responsible for ejecting operators from a quorum for violating the Service Legal Agreement (SLA).',
+        }),
+        ...upgrades,
+      },
     ],
     risks: [
       {
@@ -112,8 +144,31 @@ export const eigenDAbridge = {
     ![EigenDA once stored](/images/da-layer-technology/eigenda/oncestored.png#center)
 
     Although thresholds are not enforced by the confirmBatch method, current quorum thresholds are set to ${quorum1Threshold}% of registered stake for the ETH quorum and ${quorum2Threshold}% for the EIGEN token quorum. The quorum thresholds are set on the EigenDAServiceManager contract and can be changed by the contract owner.
+    There is a maximum of ${operatorSetParamsQuorum1[0]} operators that can register for the ETH quorum and ${operatorSetParamsQuorum2[0]} for the EIGEN token quorum. Once the cap is reached, new operators must have 10% more weight than the lowest-weighted operator to join the active set. Entering the quorum is subject to the approval of the churn approver. Operators can be ejected from a quorum by the ejectors without delay should they violate the Service Legal Agreement (SLA).
   `,
   permissions: [
+    {
+      name: 'EigenDAProxyAdmin',
+      description: `The contract authorized to upgrade the core EigenDA contracts.`,
+      accounts: [
+        {
+          address: discovery.getContract('eigenDAProxyAdmin').address,
+          type: 'Contract',
+        },
+      ],
+      participants: [
+        {
+          address: EthereumAddress(
+            discovery.getContractValue<string>('eigenDAProxyAdmin', 'owner'),
+          ),
+          type: 'MultiSig',
+        },
+      ],
+    },
+    ...eigenDiscovery.getMultisigPermission(
+      'EigenLayerExecutorMultisig',
+      'The proxy contract authorized to unpause the EigenDAServiceManager contract and upgrade core contracts through the EigenDAProxyAdmin contract.',
+    ),
     ...eigenDiscovery.getMultisigPermission(
       'EigenLayerOperationsMultisig',
       'This multisig is the owner of the EigenDAServiceManager contract. It holds the power to change the contract state and upgrade the bridge.',
@@ -152,14 +207,10 @@ export const eigenDAbridge = {
         type: 'EOA',
       })),
     },
-    ...eigenDiscovery.getMultisigPermission(
-      'EigenLayerExecutorMultisig',
-      'The address authorized to unpause the EigenDAServiceManager contract.',
-    ),
   ],
   chain: ChainId.ETHEREUM,
   requiredMembers: 0, // currently 0 since threshold is not enforced
-  totalMembers: 100,
+  totalMembers: 400,
   transactionDataType: DacTransactionDataType.TransactionData,
   members: {
     type: 'unknown',
