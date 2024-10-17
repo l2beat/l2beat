002f7ee217613fe072d21df16a9835c17d04607e
vincfurc <10850139+vincfurc@users.noreply.github.com>
2024-10-17 13:35:50 +0100

    single blobstream

--- packages/config/src/projects/other/da-beat/blockchain/celestia/bridges/blobstream/index.ts
+++ packages/config/src/projects/other/da-beat/blockchain/celestia/bridges/blobstream/index.ts
@@ -1,9 +1,6 @@
-import { blobstreamArbitrum } from './SP1Blobstream/arbitrum'
-import { blobstreamBase } from './SP1Blobstream/base'
-import { blobstreamEthereum } from './SP1Blobstream/ethereum'
+
+import { SP1Blobstream } from './SP1Blobstream/SP1Blobstream'
 
 export const blobstream = [
-  blobstreamArbitrum,
-  blobstreamBase,
-  blobstreamEthereum,
+  SP1Blobstream,
 ]
