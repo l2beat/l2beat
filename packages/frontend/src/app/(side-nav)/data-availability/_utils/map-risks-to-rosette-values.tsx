b22da46ad96e1d0cb3e7d83e3293eb7b76990953
Tomasz Tórz <93620601+torztomasz@users.noreply.github.com>
2024-10-17 14:49:57 +0200

    Show warning on rosette tooltip and in the section (#5469)

--- packages/frontend/src/app/(side-nav)/data-availability/_utils/map-risks-to-rosette-values.tsx
+++ packages/frontend/src/app/(side-nav)/data-availability/_utils/map-risks-to-rosette-values.tsx
@@ -1,11 +1,18 @@
 import { type DaBridgeRisks, type DaLayerRisks } from '@l2beat/config'
-import { type Sentiment, type ValueWithSentiment } from '@l2beat/shared-pure'
+import {
+  type Sentiment,
+  type ValueWithSentiment,
+  type WarningValueWithSentiment,
+} from '@l2beat/shared-pure'
 import { type RosetteValue } from '~/components/rosette/types'
 
 export function mapRisksToRosetteValues(
   risks: Record<
     keyof (DaLayerRisks & DaBridgeRisks),
-    ValueWithSentiment<string, Sentiment> & { description?: string }
+    ValueWithSentiment<string, Sentiment> & {
+      description?: string
+      warning?: WarningValueWithSentiment
+    }
   >,
 ): RosetteValue[] {
   const values: RosetteValue[] = [
@@ -14,30 +21,35 @@ export function mapRisksToRosetteValues(
       value: risks.exitWindow.value,
       sentiment: risks.exitWindow.sentiment,
       description: risks.exitWindow.description,
+      warning: risks.exitWindow.warning,
     },
     {
       name: 'Economic security',
       value: risks.economicSecurity.value,
       sentiment: risks.economicSecurity.sentiment,
       description: risks.economicSecurity.description,
+      warning: risks.economicSecurity.warning,
     },
     {
       name: 'Accessibility',
       value: risks.accessibility.value,
       sentiment: risks.accessibility.sentiment,
       description: risks.accessibility.description,
+      warning: risks.accessibility.warning,
     },
     {
       name: 'Fraud detection',
       value: risks.fraudDetection.value,
       sentiment: risks.fraudDetection.sentiment,
       description: risks.fraudDetection.description,
+      warning: risks.fraudDetection.warning,
     },
     {
       name: 'Attestations',
       value: risks.attestations.value,
       sentiment: risks.attestations.sentiment,
       description: risks.attestations.description,
+      warning: risks.attestations.warning,
     },
   ]
 
