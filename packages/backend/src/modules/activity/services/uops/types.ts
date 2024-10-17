9a69a755ed4deb8032e2d61eb17f3a9885c8c17a
maciekzygmunt <maciekzygmunt@interia.pl>
2024-10-17 15:44:35 +0300

    initiate analyzer

--- packages/backend/src/modules/activity/services/uops/types.ts
+++ packages/backend/src/modules/activity/services/uops/types.ts
@@ -1,4 +1,5 @@
 import { providers } from 'ethers'
+import { StarknetGetBlockWithTxsResponseBodySchema } from '../../../../peripherals/starknet/schemas'
 
 export interface AnalyzedBlock {
   transactionsLength: number
@@ -6,12 +7,13 @@ export interface AnalyzedBlock {
 }
 
 export interface Analyzer {
-  analyzeBlock(rpcBlock: {
-    number: number
-    timestamp: number
-    hash: string
-    transactions: providers.TransactionResponse[]
-  }): AnalyzedBlock
+  analyzeBlock(
+    block: {
+      transactions:
+        | providers.TransactionResponse[]
+        | StarknetGetBlockWithTxsResponseBodySchema['result']['transactions']
+    },
+  ): AnalyzedBlock
 }
 
 export type Operation = StaticOperation | RecursiveOperation
