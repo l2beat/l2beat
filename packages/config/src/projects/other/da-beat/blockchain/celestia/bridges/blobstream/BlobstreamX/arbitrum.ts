852f12d6a6930b86d185b1a3e6d5bb446bbec253
vincfurc <10850139+vincfurc@users.noreply.github.com>
2024-10-18 06:13:34 +0100

    Configure DA risks to be shown in the Risk summary (#5467)

--- packages/config/src/projects/other/da-beat/blockchain/celestia/bridges/blobstream/BlobstreamX/arbitrum.ts
+++ packages/config/src/projects/other/da-beat/blockchain/celestia/bridges/blobstream/BlobstreamX/arbitrum.ts
@@ -57,6 +57,12 @@ export const blobstreamArbitrum = CELESTIA_BLOBSTREAM({
       
       By default, BlobstreamX on Arbitrum is updated by the Celestia operator at a regular cadence of 1 hour.
   `,
+    risks: [
+      {
+        category: 'Funds can be lost if',
+        text: 'the DA bridge accepts an incorrect or malicious data commitment provided by a dishonest majority of Celestia validators.',
+      },
+    ],
   },
   contracts: {
     addresses: [
@@ -86,11 +92,7 @@ export const blobstreamArbitrum = CELESTIA_BLOBSTREAM({
     risks: [
       {
         category: 'Funds can be lost if',
-        text: 'the bridge contract receives a malicious code upgrade. There is no delay on code upgrades.',
-      },
-      {
-        category: 'Funds can be lost if',
-        text: 'a dishonest majority of Celestia validators post incorrect or malicious data commitments.',
+        text: 'the bridge contract or its dependencies receive a malicious code upgrade. There is no delay on code upgrades.',
       },
     ],
   },
