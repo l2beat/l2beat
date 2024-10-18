61ef4dafa1c969d224d4f5723e40e0798546305d
Maciej Zygmunt <maciekzygmunt@interia.pl>
2024-10-18 12:54:28 +0300

    Plug in UOPS count analyzer for Starknet to providers (#5474)

--- packages/backend/src/modules/activity/services/providers/StarknetTxsCountProvider.test.ts
+++ packages/backend/src/modules/activity/services/providers/StarknetTxsCountProvider.test.ts
@@ -1,7 +1,10 @@
 import { ProjectId, UnixTime } from '@l2beat/shared-pure'
 import { expect, mockFn, mockObject } from 'earl'
+import { range } from 'lodash'
 import { StarknetClient } from '../../../../peripherals/starknet/StarknetClient'
-import { activityRecord, transactions } from '../../utils/aggregatePerDay.test'
+import { StarknetTransaction } from '../../../../peripherals/starknet/schemas'
+import { activityRecord } from '../../utils/aggregatePerDay.test'
+import { StarknetUopsAnalyzer } from '../uops/analyzers/StarknetUopsAnalyzer'
 import { StarknetTxsCountProvider } from './StarknetTxsCountProvider'
 
 const START = UnixTime.fromDate(new Date('2021-01-01T00:00:00Z'))
@@ -10,46 +13,75 @@ describe(StarknetTxsCountProvider.name, () => {
   describe(StarknetTxsCountProvider.prototype.getTxsCount.name, () => {
     it('should return txs count', async () => {
       const client = mockStarknetClient([
-        { timestamp: START, count: 1, number: 1 },
-        { timestamp: START.add(1, 'hours'), count: 2, number: 2 },
-        { timestamp: START.add(2, 'days'), count: 5, number: 3 },
+        { timestamp: START, count: 1, uopsCount: 1, number: 3000 },
+        {
+          timestamp: START.add(1, 'hours'),
+          count: 2,
+          uopsCount: 4,
+          number: 3001,
+        },
+        {
+          timestamp: START.add(2, 'days'),
+          count: 5,
+          uopsCount: 12,
+          number: 3002,
+        },
       ])
 
       const txsCountProvider = new StarknetTxsCountProvider(
         client,
         ProjectId('a'),
+        new StarknetUopsAnalyzer(),
       )
-      const result = await txsCountProvider.getTxsCount(1, 3)
+      const result = await txsCountProvider.getTxsCount(3000, 3002)
 
       expect(result).toEqual([
-        activityRecord('a', START.toStartOf('day'), 3, null, 1, 2),
+        activityRecord('a', START.toStartOf('day'), 3, 5, 3000, 3001),
         activityRecord(
           'a',
           START.add(2, 'days').toStartOf('day'),
           5,
-          null,
-          3,
-          3,
+          12,
+          3002,
+          3002,
         ),
       ])
-      expect(client.getBlock).toHaveBeenCalledTimes(3)
+      expect(client.getBlockWithTransactions).toHaveBeenCalledTimes(3)
     })
   })
 })
 
 function mockStarknetClient(
-  blocks: { timestamp: UnixTime; count: number; number: number }[],
+  blocks: {
+    timestamp: UnixTime
+    count: number
+    uopsCount: number
+    number: number
+  }[],
 ) {
   const mockGetBlock = mockFn()
-  blocks.forEach(({ timestamp, count, number }) =>
+  blocks.forEach(({ timestamp, count, uopsCount, number }) =>
     mockGetBlock.resolvesToOnce({
-      transactions: transactions(count),
+      transactions: createTransaction(count, uopsCount),
       timestamp: timestamp.toNumber(),
       number,
     }),
   )
 
   return mockObject<StarknetClient>({
-    getBlock: mockGetBlock,
+    getBlockWithTransactions: mockGetBlock,
   })
+}
+
+function createTransaction(count: number, uopsCount: number) {
+  const uopsPerTx = Math.floor(uopsCount / count)
+  const remainingUops = uopsCount % count
+  return range(count).map((i) =>
+    mockObject<StarknetTransaction>({
+      calldata: [
+        '0x' + (i < remainingUops ? uopsPerTx + 1 : uopsPerTx).toString(16),
+      ],
+      type: 'INVOKE',
+    }),
+  )
 }
