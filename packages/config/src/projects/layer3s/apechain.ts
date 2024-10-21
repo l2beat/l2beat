613750572bb72ccc4cfbac45a5e1625022f5e129
sekuba <29250140+sekuba@users.noreply.github.com>
2024-10-21 10:58:33 +0200

    Add ApeChain (full)

--- packages/config/src/projects/layer3s/apechain.ts
+++ packages/config/src/projects/layer3s/apechain.ts
@@ -1,19 +1,19 @@
-import { ProjectId, UnixTime } from '@l2beat/shared-pure'
-import { upcomingL3 } from '../layer2s/templates/upcoming'
+import { ProjectId, } from '@l2beat/shared-pure'
+import { ProjectDiscovery } from '../../discovery/ProjectDiscovery'
+import { orbitStackL3 } from '../layer2s/templates/orbitStack'
 import { Layer3 } from './types'
+import { Badge } from '../badges'
 
-export const apechain: Layer3 = upcomingL3({
-  id: 'apechain',
-  createdAt: new UnixTime(1719939717), // 2024-07-02T17:01:57Z
+const discovery = new ProjectDiscovery('apechain', 'arbitrum')
+
+export const apechain: Layer3 = orbitStackL3({
   hostChain: ProjectId('arbitrum'),
+  badges: [Badge.DA.DAC, Badge.L3ParentChain.Arbitrum, Badge.RaaS.Caldera],
   display: {
     name: 'ApeChain',
     slug: 'apechain',
     description:
       'ApeChain is an upcoming Layer 3 on Arbitrum, built on the Orbit stack. It scales $APE and supports the growth of the ApeCoin ecosystem. Powered by the $APE token and the home of assets and games in the ApeCoin ecosystem with development and growth led by Horizen Labs.',
-    purposes: ['Gaming'],
-    category: 'Optimium',
-    provider: 'Arbitrum',
     links: {
       websites: ['https://apechain.com/'],
       apps: ['https://apechain.com/portal'],
@@ -26,5 +26,19 @@ export const apechain: Layer3 = upcomingL3({
         'https://t.me/apechainofficial',
       ],
     },
+    activityDataSource: 'Blockchain RPC',
   },
+  discovery,
+  bridge: discovery.getContract('Bridge'),
+  rollupProxy: discovery.getContract('RollupProxy'),
+  sequencerInbox: discovery.getContract('SequencerInbox'),
+  rpcUrl: 'https://rpc.apechain.com/http',
+  // associatedTokens: ['APE'],
+  // nativeToken: 'APE',
+  nonTemplatePermissions: [
+    ...discovery.getMultisigPermission(
+      'ApeChainMultisig',
+      'Can upgrade any system contract and potentially steal all funds.',
+    ),
+  ],
 })
