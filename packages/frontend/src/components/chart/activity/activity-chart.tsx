638df0941bafc122293bc161438634f2e8e1e142
maciekzygmunt <maciekzygmunt@interia.pl>
2024-10-22 13:37:03 +0300

    update labels

--- packages/frontend/src/components/chart/activity/activity-chart.tsx
+++ packages/frontend/src/components/chart/activity/activity-chart.tsx
@@ -98,7 +98,7 @@ export function ActivityChart({ milestones, entries }: Props) {
               <div className="flex flex-row items-center gap-2">
                 <EthereumLineIcon className="hidden h-1.5 w-2.5 sm:inline-block" />
                 <span className="hidden md:inline">ETH Mainnet Operations</span>
-                <span className="md:hidden">ETH Txs</span>
+                <span className="md:hidden">ETH UOPS</span>
               </div>
             </Checkbox>
           </div>
