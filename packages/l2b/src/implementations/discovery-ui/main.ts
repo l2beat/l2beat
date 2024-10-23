7e21e02b71eb4fb65c171c197dfddfc39498c1cc
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2024-10-23 12:28:06 +0200

    Add simple API

--- packages/l2b/src/implementations/discovery-ui/main.ts
+++ packages/l2b/src/implementations/discovery-ui/main.ts
@@ -1,11 +1,42 @@
-import express from 'express'
 import { join } from 'path'
+import { ConfigReader } from '@l2beat/discovery'
+import express from 'express'
 
 export function runDiscoveryUi() {
   const app = express()
   const port = 2021
 
   const STATIC_ROOT = join(__dirname, '../../../../protocolbeat/build')
+  const DISCOVERY_ROOT = join(__dirname, '../../../../backend')
+  const configReader = new ConfigReader(DISCOVERY_ROOT)
+
+  app.get('/api/projects', (_req, res) => {
+    const chains = configReader.readAllChains()
+    const projectToChain = new Map<string, string[]>()
+    for (const chain of chains) {
+      const projects = configReader.readAllProjectsForChain(chain)
+      for (const project of projects) {
+        const projectChains = projectToChain.get(project) ?? []
+        projectChains.push(chain)
+        projectToChain.set(project, projectChains)
+      }
+    }
+    const projects = [...projectToChain.entries()]
+      .map(([name, chains]) => ({ name, chains }))
+      .sort((a, b) => a.name.localeCompare(b.name))
+    res.json(projects)
+  })
+
+  app.get('/api/projects/:project', (req, res) => {
+    const { project } = req.params
+    const chains = configReader.readAllChainsForProject(project)
+    const data = chains.map((chain) => ({
+      chain,
+      config: configReader.readConfig(project, chain),
+      discovery: configReader.readDiscovery(project, chain),
+    }))
+    res.json(data)
+  })
 
   app.get('/', (_req, res) => {
     res.redirect('/ui')
