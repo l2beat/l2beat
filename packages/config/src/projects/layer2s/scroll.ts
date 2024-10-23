9cc37d16a5f0b172bb41f98d8a970963e5ca4afb
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2024-10-23 17:19:06 +0700

    Add SCR to Scroll as associated (#5550)

--- packages/config/src/projects/layer2s/scroll.ts
+++ packages/config/src/projects/layer2s/scroll.ts
@@ -152,6 +152,7 @@ export const scroll: Layer2 = {
     coingeckoPlatform: 'scroll',
   },
   config: {
+    associatedTokens: ['SCR'],
     escrows: [
       discovery.getEscrowDetails({
         address: EthereumAddress('0xD8A791fE2bE73eb6E6cF1eb0cb3F36adC9B3F8f9'),
