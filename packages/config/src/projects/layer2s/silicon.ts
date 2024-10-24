7fe79cdd20e0202585d69d4a27af99d9ee231792
vincfurc <10850139+vincfurc@users.noreply.github.com>
2024-10-25 00:05:51 +0100

    fix

--- packages/config/src/projects/layer2s/silicon.ts
+++ packages/config/src/projects/layer2s/silicon.ts
@@ -135,6 +135,16 @@ export const silicon: Layer2 = polygonCDKStack({
       description:
         'Admin and ForceBatcher of the SiliconValidium contract, can set core system parameters like timeouts, sequencer, activate forced transactions, and set the DA committee members in the SiliconDAC contract.',
     },
+    {
+      name: 'SiliconDAC Upgrader',
+      accounts: [
+        discovery.formatPermissionedAccount(
+          discovery.getContractValue('DACProxyAdmin', 'owner'),
+        ),
+      ],
+      description:
+        'Can upgrade the SiliconDAC contract and thus change the data availability rules any time.',
+    },
   ],
   nonTemplateContracts: [
     discovery.getContractDetails('SiliconDAC', {
