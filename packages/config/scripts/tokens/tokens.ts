e41c9f52ca52d568cdde1d26a8555e1666d744ca
maciekzygmunt <maciekzygmunt@interia.pl>
2024-10-24 15:13:34 +0300

    handle scripts

--- packages/config/scripts/tokens/tokens.ts
+++ packages/config/scripts/tokens/tokens.ts
@@ -2,7 +2,8 @@ import { getEnv } from '@l2beat/backend-tools'
 import {
   CoinListPlatformEntry,
   CoingeckoClient,
-  HttpClient,
+  HttpClient2,
+  RetryHandler,
 } from '@l2beat/shared'
 import {
   AssetId,
@@ -170,8 +171,12 @@ async function main() {
 function getCoingeckoClient() {
   const env = getEnv()
   const coingeckoApiKey = env.optionalString('COINGECKO_API_KEY')
-  const http = new HttpClient()
-  const coingeckoClient = new CoingeckoClient(http, coingeckoApiKey)
+  const http = new HttpClient2()
+  const coingeckoClient = new CoingeckoClient(
+    http,
+    coingeckoApiKey,
+    RetryHandler.SCRIPT,
+  )
   return coingeckoClient
 }
 
