bc953b8f7d7a4f2219599e72816f02f6d92885f3
Piotr Adamczyk <xeno@xeno.yt>
2024-10-24 10:36:19 +0200

    Fix spacing in GrisiniRiskAnalysisSection

--- packages/frontend/src/components/projects/sections/grisini-risk-analysis-section.tsx
+++ packages/frontend/src/components/projects/sections/grisini-risk-analysis-section.tsx
@@ -1,11 +1,6 @@
 import { SingleGrisiniDetails } from '~/components/grisini/single-grisini-details'
 import { type GrisiniValue } from '~/components/grisini/types'
-import { RoundedWarningIcon } from '~/icons/rounded-warning'
-import { UnderReviewBadge } from '../../badge/under-review-badge'
 import { Markdown } from '../../markdown/markdown'
-import { type RosetteValue } from '../../rosette/types'
-import { SentimentText } from '../../sentiment-text'
-import { WarningBar } from '../../warning-bar'
 import { ProjectSection } from './project-section'
 import { type ProjectSectionProps } from './types'
 
@@ -25,9 +20,9 @@ export function GrisiniRiskAnalysisSection({
       ({ sentiment }) => sentiment === 'UnderReview',
     )
   return (
-    <ProjectSection {...sectionProps} isUnderReview={isUnderReview}>
+    <ProjectSection {...sectionProps} isUnderReview={isUnderReview} className='space-y-6'>
       {Object.values(grisiniValues).map((value, key) => (
-        <div key={key} className="flex flex-col gap-2 key={value.name}">
+        <div key={key} className="flex flex-col gap-2">
           <SingleGrisiniDetails {...value} />
           {value.description && (
             <Markdown className="mt-1.5 font-normal leading-snug text-black/80 dark:text-white/80 md:text-lg">
@@ -37,47 +32,5 @@ export function GrisiniRiskAnalysisSection({
         </div>
       ))}
     </ProjectSection>
-  )
-}
-
-export function SingleRisk({
-  value,
-}: {
-  value: RosetteValue
-}) {
-  return (
-    <div>
-      <h3 className="mt-6 text-sm font-medium uppercase text-zinc-800 dark:text-white md:text-lg">
-        {value.name}
-      </h3>
-      {value.sentiment === 'UnderReview' ? (
-        <span className="block">
-          {value.name} risk is currently <UnderReviewBadge />
-        </span>
-      ) : (
-        <>
-          <SentimentText
-            sentiment={value.sentiment}
-            className="block text-xl font-medium md:text-2xl"
-            vibrant
-          >
-            {value.value}
-          </SentimentText>
-          {value.warning && (
-            <WarningBar
-              className="my-2"
-              icon={RoundedWarningIcon}
-              text={value.warning.value}
-              color={value.warning.sentiment === 'bad' ? 'red' : 'yellow'}
-            />
-          )}
-          {value.description && (
-            <Markdown className="mt-1.5 font-normal leading-snug text-black/80 dark:text-white/80 md:text-lg">
-              {value.description}
-            </Markdown>
-          )}
-        </>
-      )}
-    </div>
   )
 }
