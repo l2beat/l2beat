8a01111f8ba23f16cda6e925bcd1afd066d2f63b
torztomasz <tomasz.torz@l2beat.com>
2024-10-24 12:21:26 +0200

    add upgrader

--- packages/config/src/projects/layer3s/index.test.ts
+++ packages/config/src/projects/layer3s/index.test.ts
@@ -150,12 +150,11 @@ describe('layer3s', () => {
     }
   })
 
-  describe('other category projects have proposer and challenger', () => {
+  describe('Other category projects have other details', () => {
     for (const layer3 of layer3s) {
       if (layer3.display.isOther) {
         it(layer3.display.name, () => {
-          expect(layer3.display.proposer).not.toEqual(undefined)
-          expect(layer3.display.challenger).not.toEqual(undefined)
+          expect(layer3.display.mainPermissions).not.toEqual(undefined)
         })
       }
     }
