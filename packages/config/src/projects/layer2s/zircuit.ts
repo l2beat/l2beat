f3f25c26b846d6ec907d8edcf3b4e373e4ef582f
Radina Talanova <nt.radina@gmail.com>
2024-10-25 09:49:55 +0300

    Update Zircuit tracked txs (#5576)

--- packages/config/src/projects/layer2s/zircuit.ts
+++ packages/config/src/projects/layer2s/zircuit.ts
@@ -1,4 +1,4 @@
-import { UnixTime, formatSeconds } from '@l2beat/shared-pure'
+import { EthereumAddress, UnixTime, formatSeconds } from '@l2beat/shared-pure'
 import { ScalingProjectTechnologyChoice } from '../../common'
 import { ProjectDiscovery } from '../../discovery/ProjectDiscovery'
 import { opStackL2 } from './templates/opStack'
@@ -151,6 +151,30 @@ export const zircuit: Layer2 = opStackL2({
       ...upgradeability,
     }),
   ],
+  nonTemplateTrackedTxs: [
+    {
+      uses: [
+        { type: 'liveness', subtype: 'stateUpdates' },
+        { type: 'l2costs', subtype: 'stateUpdates' },
+      ],
+      query: {
+        formula: 'functionCall',
+        address: EthereumAddress('0x92Ef6Af472b39F1b363da45E35530c24619245A4'),
+        selector: '0xa9efd6b8',
+        functionSignature:
+          'function proposeL2Output(bytes32 _outputRoot, uint256 _l2BlockNumber, bytes32 _l1Blockhash, uint256 _l1BlockNumber, bytes _proof)',
+        sinceTimestamp: new UnixTime(1720137600),
+      },
+    },
+  ],
+  finality: {
+    type: 'OPStack-blob',
+    genesisTimestamp: new UnixTime(1719936217),
+    minTimestamp: new UnixTime(1720137600),
+    l2BlockTimeSeconds: 2,
+    lag: 0,
+    stateUpdate: 'disabled', //to be enabled once state updates work
+  },
   milestones: [
     {
       name: 'Zircuit Mainnet Launch',
