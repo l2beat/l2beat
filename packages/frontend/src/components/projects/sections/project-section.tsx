de881b360f74378f34f3380c90d56ba4d7d283dd
Piotr Adamczyk <xeno@xeno.yt>
2024-10-25 09:13:00 +0200

    Fix subheaders, fix spacing

--- packages/frontend/src/components/projects/sections/project-section.tsx
+++ packages/frontend/src/components/projects/sections/project-section.tsx
@@ -20,7 +20,9 @@ export function ProjectSection(props: ExtendedProjectSectionProps) {
     <section
       id={props.id}
       className={cn(
-        !props.nested && 'mt-10 md:rounded-lg md:bg-gray-100 md:p-8 md:dark:bg-zinc-900',
+        !props.nested &&
+          'mt-10 md:rounded-lg md:bg-gray-100 md:p-8 md:dark:bg-zinc-900',
+        props.nested && 'mt-8',
         props.className,
       )}
     >
@@ -56,20 +58,35 @@ interface ProjectDetailsSectionHeaderProps {
 export function ProjectDetailsSectionHeader(
   props: ProjectDetailsSectionHeaderProps,
 ) {
+  const isSubsection =
+    props.sectionOrder !== undefined && props.sectionOrder.length > 1
   return (
     <a
       href={`#${props.id}`}
       className={cn(
         'flex items-center gap-4 md:leading-normal',
+        isSubsection && 'gap-3',
         props.className,
       )}
     >
       {props.sectionOrder && (
-        <div className="hidden size-10 items-center justify-center rounded bg-gray-200 text-[26px] font-bold tabular-nums text-gray-700 dark:bg-zinc-700 dark:text-zinc-500 md:flex">
+        <div
+          className={cn(
+            'hidden size-10 items-center justify-center rounded bg-gray-200 px-3 text-[26px] font-bold tabular-nums text-gray-700 dark:bg-zinc-700 dark:text-zinc-500 md:flex',
+            isSubsection && 'h-8 w-11 text-xl',
+          )}
+        >
           {props.sectionOrder}
         </div>
       )}
-      <span className="text-2xl font-bold md:text-4xl">{props.title}</span>
+      <span
+        className={cn(
+          'text-2xl font-bold md:text-4xl',
+          isSubsection && 'text-xl md:text-3xl',
+        )}
+      >
+        {props.title}
+      </span>
     </a>
   )
 }
