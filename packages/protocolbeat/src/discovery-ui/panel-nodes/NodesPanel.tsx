2f9ad4e735bbeabf657df8728ce7e779a8c290ad
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2024-10-28 16:18:35 +0100

    Add connections

--- packages/protocolbeat/src/discovery-ui/panel-nodes/NodesPanel.tsx
+++ packages/protocolbeat/src/discovery-ui/panel-nodes/NodesPanel.tsx
@@ -2,11 +2,12 @@ import { useQuery } from '@tanstack/react-query'
 import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { App } from '../../App'
-import { Node } from '../../store/State'
+import { Field, Node } from '../../store/State'
 import { useStore as useNodeStore } from '../../store/store'
 import { NODE_WIDTH } from '../../store/utils/constants'
 import { getChainColor } from '../../store/utils/discoveryToNodes'
 import { getProject } from '../api/api'
+import { Field as ApiField, FieldValue } from '../api/types'
 
 export function NodesPanel() {
   const { project } = useParams()
@@ -39,7 +40,7 @@ export function NodesPanel() {
           box: { x: 0, y: 0, width: NODE_WIDTH, height: 0 },
           color: baseColor,
           data: null,
-          fields: [],
+          fields: toNodeFields(contract.fields),
         }
         nodes.push(node)
       }
@@ -71,4 +72,52 @@ export function NodesPanel() {
       <App />
     </div>
   )
+}
+
+function toNodeFields(input: ApiField[]): Field[] {
+  const implementation = input.find((x) => x.name === '$implementation')
+  const banned: string[] = []
+  if (implementation?.value.type === 'address') {
+    banned.push(implementation.value.address)
+  } else if (implementation?.value.type === 'array') {
+    for (const item of implementation.value.values) {
+      if (item.type === 'address') {
+        banned.push(item.address)
+      }
+    }
+  }
+  return input.flatMap((x) => getNodeFields(x.name, x.value, banned))
+}
+
+function getNodeFields(
+  path: string,
+  value: FieldValue,
+  banned: string[],
+): Field[] {
+  if (value.type === 'object') {
+    return Object.entries(value).flatMap(([key, value]) =>
+      getNodeFields(`${path}.${key}`, value, banned),
+    )
+  } else if (value.type === 'array') {
+    return value.values.flatMap((value, i) =>
+      getNodeFields(`${path}[${i}]`, value, banned),
+    )
+  } else if (value.type === 'address') {
+    if (banned.includes(value.address)) {
+      return []
+    }
+    return [
+      {
+        name: path,
+        target: value.address,
+        box: { x: 0, y: 0, width: 0, height: 0 },
+        connection: {
+          from: { direction: 'left', x: 0, y: 0 },
+          to: { direction: 'left', x: 0, y: 0 },
+        },
+      },
+    ]
+  } else {
+    return []
+  }
 }
