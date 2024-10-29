d74acfeb4d15a418fec82ca7b34c6ed9ddb4d0c9
sekuba <29250140+sekuba@users.noreply.github.com>
2024-10-29 15:10:06 +0100

    Add Zerion Zero Network (full)

---
+++ packages/backend/discovery/zeronetwork/ethereum/diffHistory.md
@@ -0,0 +1,36 @@
+Generated with discovered.json: 0xff1c66cfc72cab38284d321b10ef03cc5878dc4c
+
+# Diff at Tue, 29 Oct 2024 14:04:33 GMT:
+
+- author: sekuba (<29250140+sekuba@users.noreply.github.com>)
+- current block number: 21071767
+
+## Description
+
+Provide description of changes. This section will be preserved.
+
+## Initial discovery
+
+```diff
++   Status: CREATED
+    contract ZeroNetworkChainAdminMultisig (0x2e5BE1479cF661eeD9F526b7926eA87F6A5dD6a9)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract Verifier (0x70F3FBf8a427155185Ec90BED8a3434203de9604)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract ZeroNetworkZkEvmAdmin (0xCA8faaF5BA885fEC8C2c8CD49bADAa7589D173b3)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract ZeroNetworkZkEvm (0xdbD849acC6bA61F461CB8A41BBaeE2D673CA02d9)
+    +++ description: None
+```
--- packages/config/src/projects/layer2s/zeronetwork.ts
+++ packages/config/src/projects/layer2s/zeronetwork.ts
@@ -1,25 +1,110 @@
-import { UnixTime } from '@l2beat/shared-pure'
-import { upcomingL2 } from './templates/upcoming'
+import { EthereumAddress, UnixTime } from '@l2beat/shared-pure'
+import { ProjectDiscovery } from '../../discovery/ProjectDiscovery'
+import { Badge } from '../badges'
+import { Upgradeability, zkStackL2 } from './templates/zkStack'
 import { Layer2 } from './types'
 
-export const zeronetwork: Layer2 = upcomingL2({
-  id: 'zeronetwork',
+const discovery = new ProjectDiscovery('zeronetwork')
+const discovery_ZKstackGovL2 = new ProjectDiscovery(
+  'shared-zk-stack',
+  'zksync2',
+)
+const bridge = discovery.getContract('L1SharedBridge')
+
+export const zeronetwork: Layer2 = zkStackL2({
+  discovery,
+  discovery_ZKstackGovL2,
+  validatorsEvents: {
+    added: 'zeroNetworkValidatorsAdded',
+    removed: 'zeroNetworkValidatorsRemoved',
+  },
+  badges: [
+    Badge.VM.EVM,
+    Badge.DA.EthereumBlobs,
+    Badge.Stack.ZKStack,
+    Badge.Infra.ElasticChain,
+  ],
   createdAt: new UnixTime(1721214420), // 2024-07-17T11:07:00Z
   display: {
-    name: 'Zero Network',
+    name: 'ZERϴ Network',
     slug: 'zeronetwork',
     description:
-      'Zero Network is an L2 by the Zerion wallet team, utilizing the ZKsync Stack and allowing certain users with Zerion DNA NFTs gasless and prioritized transactions.',
-    purposes: ['Universal'],
-    category: 'ZK Rollup',
-    provider: 'ZK Stack',
+      'ZERϴ Network is an L2 by the Zerion wallet team, utilizing the ZK stack and native account abstraction, allowing users with Zerion Wallet DNA NFTs gasless and prioritized transactions.',
     links: {
       websites: ['https://zero.network/'],
-      apps: ['https://zerion.io/'],
+      apps: ['https://bridge.zero.network/', 'https://zerion.io/'],
       documentation: ['https://docs.zero.network/'],
       explorers: ['https://explorer.zero.network/'],
       repositories: [],
-      socialMedia: ['https://x.com/zerodotnetwork'],
+      socialMedia: [
+        'https://x.com/ZEROdotnetwork',
+        'https://zero.network/blog',
+        'https://discord.com/invite/VfC24at',
+      ],
     },
   },
+  rpcUrl: 'https://rpc.zerion.io/v1/zero',
+  chainConfig: {
+    name: 'zeronetwork',
+    chainId: 543210,
+    explorerUrl: 'https://explorer.zero.network/',
+    minTimestampForTvl: new UnixTime(1729616414),
+  },
+  diamondContract: discovery.getContract('ZeroNetworkZkEvm'),
+  nonTemplateEscrows: (zkStackUpgrades: Upgradeability) => [
+    discovery.getEscrowDetails({
+      address: bridge.address,
+      tokens: [],
+      description:
+        'Shared bridge for depositing tokens to ZERϴ and other ZK stack chains.',
+      sharedEscrow: {
+        type: 'ElasticChian',
+        l2BridgeAddress: EthereumAddress(
+          '0x309429DE3621992Cb0ab8982A448c9Cc5c38405b',
+        ),
+        l2EtherAddress: EthereumAddress(
+          '0x898b3560affd6d955b1574d87ee09e46669c60ea',
+        ),
+        tokensToAssignFromL1: [],
+      },
+      ...zkStackUpgrades,
+    }),
+  ],
+  nonTemplateContracts: (zkStackUpgrades: Upgradeability) => [
+    discovery.getContractDetails('ZeroNetworkZkEvm', {
+      description:
+        'The main Rollup contract. The operator commits blocks and provides a ZK proof which is validated by the Verifier contract \
+          then processes transactions. During batch execution it processes L1 --> L2 and L2 --> L1 transactions.',
+      ...zkStackUpgrades,
+    }),
+    discovery.getContractDetails('ZeroNetworkZkEvmAdmin', {
+      description:
+        'Intermediary governance contract that has the *ChainAdmin* role in the ZERϴ zkEVM diamond contract.',
+    }),
+  ],
+  nonTemplatePermissions: [
+    ...discovery.getMultisigPermission(
+      'ZeroNetworkChainAdminMultisig',
+      'Inherits *ChainAdmin* permissions like adding/removing validators in the ValidatorTimelock, adding a TransactionFilterer that can censor transactions, upgrading the ZERϴ Diamond to a predeployed version of the ZK stack and settings its fee parameters.',
+    ),
+    {
+      name: 'ZeroNetworkOracleEOA',
+      accounts: [
+        discovery.getPermissionedAccount(
+          'ZeroNetworkZkEvmAdmin',
+          'tokenMultiplierSetter',
+        ),
+      ],
+      description: 'Can set the conversion factor for SOPH deposits to ZERϴ.',
+    },
+  ],
+  milestones: [
+    {
+      name: 'Mainnet launch',
+      link: '',
+      date: '2024-00-00T00:00:00Z',
+      description: 'ZERϴ launches their mainnet.',
+      type: 'general',
+    },
+  ],
 })
