291ba6e4be95266a44e6e781da862bc849c70988
Piotr Adamczyk <xeno@xeno.yt>
2024-10-29 11:24:28 +0100

    Fix classnames order

--- packages/frontend/src/app/(side-nav)/data-availability/summary/_components/table/projects-used-in.tsx
+++ packages/frontend/src/app/(side-nav)/data-availability/summary/_components/table/projects-used-in.tsx
@@ -26,7 +26,7 @@ export function ProjectsUsedIn({ usedIn, className, maxProjects = 5 }: Props) {
   return (
     <div
       className={cn(
-        'flex flex-row shrink-0 flex-nowrap items-center gap-1.5',
+        'flex shrink-0 flex-row flex-nowrap items-center gap-1.5',
         className,
       )}
     >
