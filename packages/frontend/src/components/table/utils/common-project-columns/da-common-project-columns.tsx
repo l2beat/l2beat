3730cf90b795f36189e7377842859c74285729e1
Michał Podsiadły <michal.podsiadly@l2beat.com>
2024-10-30 10:47:11 +0100

    feat: reuse da layer cell for main column

--- packages/frontend/src/components/table/utils/common-project-columns/da-common-project-columns.tsx
+++ packages/frontend/src/components/table/utils/common-project-columns/da-common-project-columns.tsx
@@ -1,5 +1,4 @@
 import { type ColumnHelper } from '@tanstack/react-table'
-import { ProjectNameCell } from '../../cells/project-name-cell'
 import {
   type CommonProjectColumnsEntry,
   type CommonProjectColumnsOptions,
@@ -10,22 +9,5 @@ export function getDaCommonProjectColumns<T extends CommonProjectColumnsEntry>(
   columnHelper: ColumnHelper<T>,
   opts?: CommonProjectColumnsOptions,
 ) {
-  return [
-    ...getCommonProjectColumns(columnHelper, opts),
-    columnHelper.accessor((row) => row.name, {
-      id: 'name',
-      header: 'DA Layer',
-      meta: {
-        tooltip:
-          'The data availability layer where the data (transaction data or state diffs) is posted.',
-      },
-      cell: (ctx) => (
-        <ProjectNameCell
-          project={{
-            name: ctx.getValue(),
-          }}
-        />
-      ),
-    }),
-  ] as const
+  return [...getCommonProjectColumns(columnHelper, opts)] as const
 }
