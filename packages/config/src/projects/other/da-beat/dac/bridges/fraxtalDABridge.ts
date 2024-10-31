ee9625905a0e17cde02f767aa15c34f3fc460f7c
vincfurc <10850139+vincfurc@users.noreply.github.com>
2024-10-31 12:02:06 +0000

    DAC bridge updates (#5668)

--- packages/config/src/projects/other/da-beat/dac/bridges/fraxtalDABridge.ts
+++ packages/config/src/projects/other/da-beat/dac/bridges/fraxtalDABridge.ts
@@ -1,37 +1,36 @@
 import { EthereumAddress, UnixTime } from '@l2beat/shared-pure'
 import { ProjectDiscovery } from '../../../../../discovery/ProjectDiscovery'
-import { xterio } from '../../../../layer2s/xterio'
+import { redstone } from '../../../../layer2s/redstone'
 import { DaCommitteeSecurityRisk, DaUpgradeabilityRisk } from '../../types'
 import { DaBridge } from '../../types/DaBridge'
 import { DaRelayerFailureRisk } from '../../types/DaRelayerFailureRisk'
 import { toUsedInProject } from '../../utils/to-used-in-project'
 
-const discovery = new ProjectDiscovery('xterio')
+const discovery = new ProjectDiscovery('fraxtal')
 
 const sequencerInbox = discovery.getContractValue<string>(
   'SystemConfig',
   'sequencerInbox',
 )
 
-export const xterioDABridge = {
-  id: 'xterioDABridge',
+export const fraxtalDABridge = {
+  id: 'fraxtalDABridge',
   createdAt: new UnixTime(1723022143), // 2024-04-03T10:08:59Z
   type: 'NoBridge',
   display: {
     name: 'DA Bridge',
     slug: 'da',
     description:
-      'There is EOA on Ethereum acting as a DA bridge to receive data availability commitments.',
+      'The DA bridge only stores IPFS hash commitments posted by the sequencer. It is not possible to verify blob inclusion against the data commitments onchain.',
     links: {
-      websites: ['https://xter.io/'],
-      apps: ['https://xter.io/', 'https://eth-bridge.xter.io/'],
-      documentation: ['https://stack.optimism.io/'],
-      explorers: ['https://eth.xterscan.io/'],
-      repositories: ['https://github.com/XterioTech'],
+      websites: ['https://redstone.xyz/'],
+      apps: ['https://redstone.xyz/deposit'],
+      documentation: ['https://redstone.xyz/docs'],
+      explorers: ['https://explorer.redstone.xyz/'],
+      repositories: ['https://github.com/latticexyz/redstone'],
       socialMedia: [
-        'https://x.com/XterioGames',
-        'https://discord.gg/xterio',
-        'https://medium.com/@XterioGames',
+        'https://twitter.com/redstonexyz',
+        'https://discord.com/invite/latticexyz',
       ],
     },
   },
@@ -48,17 +47,12 @@ export const xterioDABridge = {
           description:
             'The ProxyAdmin contract is the owner of SystemConfig and can change the address of the DA bridge for the system.',
         }),
-        discovery.getContractDetails('DataAvailabilityChallenge', {
-          description:
-            'The DataAvailabilityChallenge contract is used to challenge the data availability of tx data hashes. See the technology section for more details.',
-        }),
       ],
     },
     risks: [],
   },
   technology: {
-    description: `Only hashes of data batches are posted as DA commitments to an EOA on Ethereum.
-      However, there is a mechanism that allows users to challenge unavailability of data. \n`,
+    description: `There is no committee attesting to the availability of the data. For L2 chain derivation, the system relies on sequencer commitments to an L1 onchain inbox. See DA layer technology section for more details.\n`,
   },
   permissions: {
     ethereum: [
@@ -70,33 +64,18 @@ export const xterioDABridge = {
         description:
           'Central actor allowed to relay DA commitments to the DA bridge.',
       },
-      ...discovery.getMultisigPermission(
-        'RollupOwnerMultisig',
-        'Owner of the ProxyAdmin and the rollup system. It can change any system component.',
-      ),
       {
         name: 'SystemConfig owner',
         description:
           'Account privileged to change System Config parameters such as Sequencer Address and gas limit.',
         accounts: [discovery.getPermissionedAccount('SystemConfig', 'owner')],
       },
-      {
-        name: 'DataAvailabilityChallenge owner',
-        accounts: [
-          discovery.getPermissionedAccount(
-            'DataAvailabilityChallenge',
-            'owner',
-          ),
-        ],
-        description:
-          'Owner of the DataAvailabilityChallenge contract. It can upgrade the contract params, potentially making the system insecure.',
-      },
     ],
   },
-  usedIn: toUsedInProject([xterio]),
+  usedIn: toUsedInProject([redstone]),
   risks: {
-    committeeSecurity: DaCommitteeSecurityRisk.NoCommiteeSecurity(),
-    upgradeability: DaUpgradeabilityRisk.LowOrNoDelay(), // no delay
-    relayerFailure: DaRelayerFailureRisk.NoMechanism,
+    committeeSecurity: DaCommitteeSecurityRisk.NoBridge,
+    upgradeability: DaUpgradeabilityRisk.NoBridge,
+    relayerFailure: DaRelayerFailureRisk.NoBridge,
   },
 } satisfies DaBridge
