2a6121656511e2228d5e9d275d25b79036a02691
Michał Podsiadły <michal.podsiadly@l2beat.com>
2024-11-04 18:33:32 +0100

    feat: add info callouts for public/custom systems

---
+++ packages/frontend/src/app/(side-nav)/data-availability/_components/da-category-info.tsx
@@ -0,0 +1,38 @@
+import { InfoIcon } from '~/icons/info'
+
+export function PublicSystemInfo() {
+  return (
+    <Info>
+      Public DA layers are data availability solutions designed for broad,
+      general use across multiple scaling projects. These solutions aim to be
+      flexible, scalable, and accessible to a wide range of users without
+      significant modifications or integration efforts.
+    </Info>
+  )
+}
+
+export function CustomSystemInfo() {
+  return (
+    <Info>
+      Custom DA layers are data availability solutions that are tightly
+      integrated with a single scaling project and would require substantial
+      modifications for public use. They are tailored to the specific needs of
+      one project, making them less adaptable for general or broader
+      applications.
+    </Info>
+  )
+}
+
+function Info({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex flex-row md:gap-1.5 gap-1 mb-2">
+      <InfoIcon
+        variant="blue"
+        className="flex-shrink-0 md:size-[12px] size-[10px] mt-[1px]"
+      />
+      <span className="text-[11px] md:text-[13px] text-zinc-500 dark:text-secondary md:leading-tight leading-snug">
+        {children}
+      </span>
+    </div>
+  )
+}
