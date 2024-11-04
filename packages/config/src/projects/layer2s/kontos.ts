b0bd3c7f3acec657221e0d58df01648cf2efaa06
sekuba <29250140+sekuba@users.noreply.github.com>
2024-11-04 12:08:37 +0100

    Add an escrow

--- packages/config/src/projects/layer2s/kontos.ts
+++ packages/config/src/projects/layer2s/kontos.ts
@@ -1,13 +1,15 @@
+import { EthereumAddress, UnixTime } from '@l2beat/shared-pure'
 import { underReviewL2 } from './templates/underReview'
 import { Layer2 } from './types'
 
 export const kontos: Layer2 = underReviewL2({
   id: 'kontos',
+  createdAt: new UnixTime(1730718191),
   display: {
     name: 'Kontos',
     slug: 'kontos',
     description:
-      'Kontos is a zero-knowledge based layer-2 account protocol. Its architecture enables users to enjoy numerous features, including gas-less transactions, asset-less operations, and key-less with higher security.',
+      'Kontos is a zero-knowledge based layer-2 account protocol. Its architecture enables users to enjoy numerous multichain features, including gasless transactions and other smart account features.',
     purposes: ['Universal'],
     category: 'ZK Rollup',
     links: {
@@ -24,4 +26,12 @@ export const kontos: Layer2 = underReviewL2({
       ],
     },
   },
+  escrows: [
+    {
+      address: EthereumAddress('0xc08a7164F9E9d8aB66CcB67D49d6FB116b5808dD'),
+      sinceTimestamp: new UnixTime(1699939643),
+      tokens: '*',
+      chain: 'ethereum',
+    },
+  ],
 })
