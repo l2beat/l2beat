7e7fc1ff122f651c4232281d1e2e050ff51bfd87
vincfurc <10850139+vincfurc@users.noreply.github.com>
2024-11-05 12:21:16 +0000

    ref

--- packages/config/src/projects/other/da-beat/dac/fraxtalDA.ts
+++ packages/config/src/projects/other/da-beat/dac/fraxtalDA.ts
@@ -30,7 +30,7 @@ export const fraxtalDA: DaLayer = {
   technology: {
     description: `
     ## Architecture
-    FraxtalDA is a custom [data availability solution](https://docs.frax.com/fraxtal/network/data-availability) built by the Fraxtal team. 
+    FraxtalDA is a custom data availability solution built by the Fraxtal team. 
     The data is posted by the OP batcher to three separate locations: AWS, IPFS, and Cloudfare R2. 
     The IPFS hash is then submitted to the on-chain inbox contract on Ethereum.
     FraxtalDA relies on a single DA endpoint to manage data posting between the three different locations. 
@@ -38,8 +38,18 @@ export const fraxtalDA: DaLayer = {
     ![FraxtalDA](/images/da-layer-technology/fraxtalDA/FraxtalDA.png#center)
 
     The sequencer attests to data availability by posting an IPFS hash to an on-chain inbox contract on Ethereum. L2 nodes derive the L2 chain from the L1 by reading transactions commitments from this sequencer inbox.
-        When reading from the inbox, the op-node verifies that the commitment hash is a [valid IPFS CID](https://github.com/FraxFinance/fraxtal-da-follower/blob/791e849b41465e1e00377f57c8f0c49d4b13caa8/main.go#L185). If the data corresponding to the hash is missing from IPFS, the op-node will halt, preventing further derivation of the L2 chain. 
+        When reading from the inbox, the op-node verifies that the commitment hash is a valid IPFS CID. If the data corresponding to the hash is missing from IPFS, the op-node will halt, preventing further derivation of the L2 chain. 
     `,
+    references: [
+      {
+        text: 'FraxtalDA Documentation',
+        href: 'https://docs.frax.com/fraxtal/network/data-availability'
+      },
+      {
+        text: 'Fraxtal DA Follower - Source Code',
+        href: 'https://github.com/FraxFinance/fraxtal-da-follower/blob/791e849b41465e1e00377f57c8f0c49d4b13caa8/main.go'
+      }
+    ],
     risks: [
       {
         category: 'Funds can be lost if',
