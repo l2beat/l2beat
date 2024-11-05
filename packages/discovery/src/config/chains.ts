87fb3000fef7bbcd550e691afa95f7d2f6aca25d
Luca Donno <donnoh99@gmail.com>
2024-11-05 14:31:54 +0700

    Add Kinto on Kinto discovery

--- packages/discovery/src/config/chains.ts
+++ packages/discovery/src/config/chains.ts
@@ -247,4 +247,20 @@ export const chains: ChainConfig[] = [
       url: 'https://api.blastscan.io/api',
     },
   },
+  {
+    name: 'kinto',
+    chainId: 7887,
+    shortName: 'kinto',
+    multicall: getMulticall3Config(
+      218560,
+      EthereumAddress('0x2cc0188fA85FD8Ce748C7Df6066873fdcfaD95e9'),
+    ),
+    explorer: {
+      type: 'blockscout',
+      url: 'https://explorer.kinto.xyz',
+      unsupported: {
+        getContractCreation: true,
+      },
+    },
+  },
 ] as const satisfies ChainConfig[]
