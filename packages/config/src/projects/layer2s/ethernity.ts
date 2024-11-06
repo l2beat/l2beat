3421853ad86f8bc5b209abc5ed315d411a0d984c
sekuba <sekuba@users.noreply.github.com>
2024-11-06 17:29:49 +0100

    Add ethernity (full) (#5753)
    Co-authored-by: sekuba <29250140+sekuba@users.noreply.github.com>

--- packages/config/src/projects/layer2s/ethernity.ts
+++ packages/config/src/projects/layer2s/ethernity.ts
@@ -1,30 +1,62 @@
 import { UnixTime } from '@l2beat/shared-pure'
-import { upcomingL2 } from './templates/upcoming'
+import { ProjectDiscovery } from '../../discovery/ProjectDiscovery'
+import { Badge } from '../badges'
+import { opStackL2 } from './templates/opStack'
 import { Layer2 } from './types'
 
-export const ethernity: Layer2 = upcomingL2({
-  id: 'ethernity',
+const discovery = new ProjectDiscovery('ethernity')
+
+export const ethernity: Layer2 = opStackL2({
   createdAt: new UnixTime(1718182472), // 2024-06-12T08:54:32Z
+  badges: [Badge.RaaS.Gelato, Badge.Infra.Superchain],
+  additionalPurposes: ['AI'],
+  discovery,
   display: {
     name: 'Ethernity',
     slug: 'ethernity',
+    redWarning:
+      'Critical contracts can be upgraded by an EOA which could result in the loss of all funds.',
     description:
-      'Ethernity is an Ethereum L2 leveraging the OP stack and focusing on entertainment.',
-    purposes: ['Universal', 'AI'],
+      'Ethernity is a Rollup on Ethereum leveraging the OP stack. With a primary focus on the entertainment sector, Ethernity Chain aims to cultivate a vibrant ecosystem of world-class applications where developers and users alike are directly rewarded for their contributions.',
     category: 'Optimistic Rollup',
-    provider: 'OP Stack',
     links: {
       websites: ['https://ethernity.io/'],
-      apps: ['https://app.ethernity.io/'],
-      documentation: [
-        'https://drive.google.com/file/d/1o74ZBmYMIUa8QUwvLOK7KhoXP9-LCsPf/view',
+      apps: [
+        'https://bridge.gelato.network/bridge/ethernity-mainnet',
+        'https://app.ethernity.io/',
       ],
+      documentation: ['https://docs.ethernity.io/'],
       explorers: ['https://ernscan.io/'],
       repositories: [],
       socialMedia: [
         'https://x.com/EthernityChain',
+        'https://instagram.com/ethernity',
         'https://ethernitychain.medium.com/',
+        'https://t.me/ethernitychain',
+        'https://discord.com/invite/EthernityChain',
       ],
     },
+    activityDataSource: 'Blockchain RPC',
   },
+  isNodeAvailable: true,
+  usesBlobs: true,
+  rpcUrl: 'https://mainnet.ethernitychain.io',
+  genesisTimestamp: new UnixTime(1723547737),
+  milestones: [
+    // {
+    //   name: 'Ethernity Mainnet Launch',
+    //   link: 'https://debank.com/stream/2539393',
+    //   date: '2024-07-19T00:00:00Z',
+    //   description: 'Ethernity mainnet is open for users.',
+    //   type: 'general',
+    // },
+    {
+      name: 'Ethernity Permissioned Mainnet',
+      link: 'https://www.ethernity.io/blog/the-ethernity-mainnet-launch-is-approaching',
+      date: '2024-10-29T00:00:00Z',
+      description:
+        'Ethernity is accessible to early development partners and flagship apps.',
+      type: 'general',
+    },
+  ],
 })
