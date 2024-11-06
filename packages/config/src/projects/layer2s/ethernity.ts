4987a2b7ca3507b679f849357b7821f7fa33da7d
sekuba <29250140+sekuba@users.noreply.github.com>
2024-11-06 17:36:44 +0100

    Add ERN token to Ethernity

--- packages/config/src/projects/layer2s/ethernity.ts
+++ packages/config/src/projects/layer2s/ethernity.ts
@@ -38,6 +38,7 @@ export const ethernity: Layer2 = opStackL2({
     },
     activityDataSource: 'Blockchain RPC',
   },
+  associatedTokens: ['ERN'],
   isNodeAvailable: true,
   usesBlobs: true,
   rpcUrl: 'https://mainnet.ethernitychain.io',
