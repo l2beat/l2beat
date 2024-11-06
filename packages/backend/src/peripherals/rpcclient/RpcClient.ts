9b88e880cfcaa56c4d7e8befee2ed5be5e3fdf73
antooni <antoni.pawlak@protonmail.com>
2024-11-06 12:03:17 +0100

    update RpcClient type

--- packages/backend/src/peripherals/rpcclient/RpcClient.ts
+++ packages/backend/src/peripherals/rpcclient/RpcClient.ts
@@ -1,8 +1,7 @@
 import { Logger } from '@l2beat/backend-tools'
-import { Bytes, EthereumAddress, UnixTime } from '@l2beat/shared-pure'
+import { Block, Bytes, EthereumAddress, UnixTime } from '@l2beat/shared-pure'
 import { providers } from 'ethers'
 
-import { EVMBlock } from '@l2beat/shared'
 import { getBlockNumberAtOrBefore } from '../getBlockNumberAtOrBefore'
 import { RateLimitedProvider } from './RateLimitedProvider'
 import { BlockTag, CallParameters } from './types'
@@ -61,11 +60,17 @@ export class RpcClient {
     return await this.provider.getBlock(blockNumber)
   }
 
-  async getBlockWithTransactions(blockNumber: number) {
+  async getBlockWithTransactions(blockNumber: number): Promise<Block> {
     // eth_getBlockByNumber with transactions
-    return (await this.provider.getBlockWithTransactions(
-      blockNumber,
-    )) as EVMBlock
+    const block = await this.provider.getBlockWithTransactions(blockNumber)
+
+    return {
+      ...block,
+      transactions: block.transactions.map((t) => ({
+        ...t,
+        type: t.type ? t.type.toString() : undefined,
+      })),
+    }
   }
 
   async getTransaction(txHash: string) {
