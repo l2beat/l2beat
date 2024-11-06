acc293e2f2d52f3cfa312dd6d2275948050880c0
antooni <antoni.pawlak@protonmail.com>
2024-11-06 11:37:00 +0100

    fix uops-dashboard

--- packages/uops-dashboard/src/server/clients/block/BlockClient.ts
+++ packages/uops-dashboard/src/server/clients/block/BlockClient.ts
@@ -1,6 +1,6 @@
-import { Block } from "@l2beat/shared-pure"
+import { Block } from '@l2beat/shared-pure'
 
 export interface BlockClient {
-  getBlockNumber(): Promise<number>
-  getBlock(blockNumber: number): Promise<Block>
+  getLatestBlockNumber(): Promise<number>
+  getBlockWithTransactions(blockNumber: number): Promise<Block>
 }
--- packages/uops-dashboard/src/server/counters/RpcCounter.ts
+++ packages/uops-dashboard/src/server/counters/RpcCounter.ts
@@ -11,8 +11,6 @@ import {
   ENTRY_POINT_ADDRESS_0_6_0,
   ENTRY_POINT_ADDRESS_0_7_0,
   ERC4337_methods,
-  EVMBlock,
-  EVMTransaction,
   Method,
   Operation,
   SAFE_EXEC_TRANSACTION_SELECTOR,
