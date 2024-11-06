7dd9d5b81f8d726a51a938f71af7d8a277b83e29
maciekzygmunt <maciekzygmunt@interia.pl>
2024-11-06 10:00:03 +0200

    add explorer link

--- packages/frontend/src/app/(side-nav)/scaling/_components/scaling-activity-filters.tsx
+++ packages/frontend/src/app/(side-nav)/scaling/_components/scaling-activity-filters.tsx
@@ -1,5 +1,7 @@
+import Link from 'next/link'
 import { TableFilter } from '~/components/table/filters/table-filter'
 import { type ScalingActivityEntry } from '~/server/features/scaling/get-scaling-activity-entries'
+import { cn } from '~/utils/cn'
 import { useScalingFilterValues } from './scaling-filter-context'
 import { ScalingFilters } from './scaling-filters'
 
@@ -22,11 +24,28 @@ export function ScalingActivityFilters({ items, className }: Props) {
   )
 
   return (
-    <ScalingFilters
-      items={items}
-      additionalFilters={daLayerFilter}
-      className={className}
-    />
+    <div
+      className={cn(
+        'flex flex-col-reverse gap-x-4 gap-y-2 [@media(min-width:1000px)]:flex-row [@media(min-width:1000px)]:justify-between',
+        className,
+      )}
+    >
+      <ScalingFilters items={items} additionalFilters={daLayerFilter} />
+      <ExplorerButton />
+    </div>
+  )
+}
+
+function ExplorerButton() {
+  return (
+    <Link
+      href="https://uops.l2beat.com/"
+      target="_blank"
+      className="flex w-fit items-center gap-1 rounded-lg bg-gradient-to-r from-purple-100 to-pink-100 px-2 py-1 text-sm font-semibold text-white"
+    >
+      <span>🔍</span>
+      <span>UOPS Explorer</span>
+    </Link>
   )
 }
 
