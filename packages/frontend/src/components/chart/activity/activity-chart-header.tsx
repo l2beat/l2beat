6595bd34225fd9100ad60ac71610ed2b70aa8a7a
maciekzygmunt <maciekzygmunt@interia.pl>
2024-11-06 11:21:33 +0200

    update switch on mobile

--- packages/frontend/src/components/chart/activity/activity-chart-header.tsx
+++ packages/frontend/src/components/chart/activity/activity-chart-header.tsx
@@ -3,12 +3,12 @@ import {
   useActivityMetricContext,
 } from '~/app/(side-nav)/scaling/activity/_components/activity-metric-context'
 import { ActivityMetricControls } from '~/app/(side-nav)/scaling/activity/_components/activity-metric-controls'
+import { Skeleton } from '~/components/core/skeleton'
 import { InfoIcon } from '~/icons/info'
 import { type ActivityChartStats } from '~/server/features/scaling/activity/get-activity-chart-stats'
 import { countPerSecond } from '~/server/features/scaling/activity/utils/count-per-second'
 import { cn } from '~/utils/cn'
 import { formatUops } from '~/utils/number-format/format-uops'
-import { Skeleton } from '../../core/skeleton'
 import {
   Tooltip,
   TooltipContent,
@@ -27,36 +27,44 @@ export function ActivityChartHeader({ stats, range }: Props) {
   const isTps = metric === 'tps'
   return (
     <header data-role="chart-header">
-      <div className="flex items-center justify-between">
-        <div className="flex items-center gap-2">
-          <h1 className="text-xl font-bold md:text-2xl">Daily average</h1>
-          <ActivityMetricControls value={metric} onValueChange={setMetric} />
-          <SwitchInfoTooltip />
-        </div>
-        {stats !== undefined ? (
-          <p className="text-right font-bold group-data-[interactivity-disabled]/chart:pointer-events-none group-data-[interactivity-disabled]/chart:opacity-0">
-            <span className="text-xl md:text-2xl">
-              {`${formatUops(countPerSecond(stats[metric].latestProjectsTxCount))} ${isTps ? 'TPS' : 'UOPS'}`}
-            </span>
-          </p>
-        ) : (
-          <Skeleton className="my-[5px] h-5 w-28 md:my-1.5 md:h-6 md:w-[200px] lg:w-[243px]" />
-        )}
-      </div>
-
-      <div className="flex justify-between text-xs lg:text-base">
-        <ChartTimeRange range={range} />
-        {stats !== undefined ? (
-          <div className="flex items-center gap-1.5 text-right text-secondary group-data-[interactivity-disabled]/chart:pointer-events-none group-data-[interactivity-disabled]/chart:opacity-0 lg:w-auto">
-            <div>
-              <span className="max-md:hidden">Scaling factor: </span>
-              {stats[metric].scalingFactor.toFixed(2)}x
+      <div className="flex justify-between">
+        <div className="flex flex-col">
+          <div className="flex items-start max-md:mb-2 max-md:flex-col md:items-center md:gap-2">
+            <h1 className="whitespace-nowrap text-xl font-bold max-md:ml-1 md:text-2xl">
+              Daily average
+            </h1>
+            <div className="flex items-center gap-1">
+              <ActivityMetricControls
+                value={metric}
+                onValueChange={setMetric}
+              />
+              <SwitchInfoTooltip />
             </div>
-            <ScalingFactorTooltip metric={metric} />
           </div>
-        ) : (
-          <Skeleton className="h-5 w-16 md:h-6 md:w-44" />
-        )}
+          <ChartTimeRange range={range} />
+        </div>
+        <div className="flex flex-col items-end">
+          {stats !== undefined ? (
+            <p className="text-right font-bold group-data-[interactivity-disabled]/chart:pointer-events-none group-data-[interactivity-disabled]/chart:opacity-0">
+              <span className="text-xl md:text-2xl">
+                {`${formatUops(countPerSecond(stats[metric].latestProjectsTxCount))} ${isTps ? 'TPS' : 'UOPS'}`}
+              </span>
+            </p>
+          ) : (
+            <Skeleton className="my-[5px] h-5 w-28 md:my-1.5 md:h-6 md:w-[200px] lg:w-[243px]" />
+          )}
+          {stats !== undefined ? (
+            <div className="flex items-center gap-1.5 text-right text-secondary group-data-[interactivity-disabled]/chart:pointer-events-none group-data-[interactivity-disabled]/chart:opacity-0 lg:w-auto">
+              <div>
+                <span className="max-md:hidden">Scaling factor: </span>
+                {stats[metric].scalingFactor.toFixed(2)}x
+              </div>
+              <ScalingFactorTooltip metric={metric} />
+            </div>
+          ) : (
+            <Skeleton className="h-5 w-16 md:h-6 md:w-44" />
+          )}
+        </div>
       </div>
     </header>
   )
