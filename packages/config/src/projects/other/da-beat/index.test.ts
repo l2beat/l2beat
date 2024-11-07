9e02f6d213c13d5701744044cc1434844b2a40b6
Michał Podsiadły <michal.podsiadly@l2beat.com>
2024-11-07 12:15:14 +0100

    feat: temp test for dacs

--- packages/config/src/projects/other/da-beat/index.test.ts
+++ packages/config/src/projects/other/da-beat/index.test.ts
@@ -1,5 +1,7 @@
 import { expect } from 'earl'
 import { daLayers } from '.'
+import { layer2s } from '../../layer2s'
+import { layer3s } from '../../layer3s'
 
 describe('da-beat', () => {
   daLayers.forEach((layer) => {
@@ -15,6 +17,40 @@ describe('da-beat', () => {
           })
         })
       })
+    })
+  })
+
+  describe('scaling projects in sync', () => {
+    const target = [...layer2s, ...layer3s]
+      .filter(
+        (project) =>
+          !project.isUpcoming && !project.isUnderReview && !project.isArchived,
+      )
+      .filter(
+        (project) =>
+          project.display.category === 'Optimium' ||
+          project.display.category === 'Validium',
+      )
+
+    it('should have all DACs from scaling projects assigned', () => {
+      const scalingDACs = target
+        .filter((project) => project.dataAvailability?.layer.value === 'DAC')
+        .map((project) => project.id)
+
+      const daBeatDACs = daLayers.filter((daLayer) => daLayer.kind === 'DAC')
+
+      const usedInDaBeatDACs = daBeatDACs.flatMap((daLayer) =>
+        daLayer.bridges.flatMap((bridge) =>
+          bridge.usedIn.map((usedIn) => usedIn.id),
+        ),
+      )
+
+      const projectsWithoutDaBeatEntry = scalingDACs.filter(
+        (project) => !usedInDaBeatDACs.includes(project),
+      )
+
+      // Array comparison to have a better error message with actual names
+      expect(projectsWithoutDaBeatEntry).toEqual([])
     })
   })
 })
