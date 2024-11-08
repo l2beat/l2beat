0056ca5e7b75b9d900fabdb5baa8c864f09e402b
Michał Podsiadły <michal.podsiadly@l2beat.com>
2024-11-08 15:11:45 +0100

    feat: add ability to hide tooltip over used by hover feat: adjust dark mode coloring

--- packages/frontend/src/app/(side-nav)/data-availability/summary/_components/ethereum-da-entry.tsx
+++ packages/frontend/src/app/(side-nav)/data-availability/summary/_components/ethereum-da-entry.tsx
@@ -16,7 +16,7 @@ export function EthereumDaEntry({ entry }: { entry: DaSummaryEthereumEntry }) {
 
   return (
     <div className="mb-3 overflow-auto max-md:-mr-4">
-      <div className="relative flex min-w-[940px] items-center justify-between gap-3 rounded-lg bg-blue-300 px-4 py-1 dark:bg-blue-800 max-md:mr-4 md:py-3">
+      <div className="relative flex min-w-[940px] items-center justify-between gap-3 rounded-lg bg-blue-300 px-4 py-1 dark:bg-blue-950 max-md:mr-4 md:py-3">
         <Tooltip>
           <TooltipTrigger>
             <InfoIcon variant="blue" />
@@ -61,6 +61,7 @@ export function EthereumDaEntry({ entry }: { entry: DaSummaryEthereumEntry }) {
             <ProjectsUsedIn
               usedIn={entry.usedIn}
               className="text-sm font-medium"
+              noTooltip
             />
           </div>
         </div>
