f499b2a7b9acaa11db3db50c64ad83b4f5f38337
Piotr Adamczyk <xeno@xeno.yt>
2024-11-08 17:22:20 +0100

    Fix tailwind css classes

--- packages/cms/src/app/(auth)/links/page.tsx
+++ packages/cms/src/app/(auth)/links/page.tsx
@@ -52,7 +52,7 @@ export default async function Page({
             <h3 className="text-2xl font-bold tracking-tight">
               You have no links
             </h3>
-            <p className="text-muted-foreground text-sm">
+            <p className="text-sm text-muted-foreground">
               You can add a links by clicking the button below.
             </p>
             <Link href="/links/new">
