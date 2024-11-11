6bed3434a5ae6dbc3445a683c744d6a00201a94f
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2024-11-11 15:44:28 +0700

    Example token

---
+++ schema.ts
@@ -0,0 +1,41 @@
+export interface AssetEntry {
+  // [eth|arb1|oeth|base]:[hex|native]
+  // Example: ETH on Ethereum eth:native
+  // Example: USDC on Optimism: oeth:0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85
+  address: string
+
+  assetLogoUrl: string
+  chainLogoUrl: string
+  assetName: string
+  chainName: string
+  priceUsd: number
+  decimals: number
+
+  // Who is responsible for the token. For USDC it's Circle, for WETH it's None.
+  issuer: string
+
+  child?: {
+    // backreference to AssetEntry
+    address: string
+    bridgeInfo: string
+    bridgeSeverity: 'medium' | 'high' | 'low'
+  }
+}
+
+export const tokens: AssetEntry[] = [
+  {
+    address: 'base:0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913',
+    assetLogoUrl: 'https://basescan.org/token/images/centre-usdc_28.png',
+    chainLogoUrl: 'https://l2beat.com/_next/image?url=%2Ficons%2Fbase.png',
+    assetName: 'USDC',
+    chainName: 'Base',
+    priceUsd: 1,
+    decimals: 6,
+    issuer: 'Circle',
+    // child: {
+    //   address: 'eth:0x123123',
+    //   bridgeInfo: 'Canonically bridged',
+    //   bridgeSeverity: 'low'
+    // }
+  },
+]
