20e2115572892407fb916d1232327dc42eaf9452
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2024-11-11 17:03:14 +0700

    More UI work

--- packages/insight/src/Profile.tsx
+++ packages/insight/src/Profile.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { TokenEntry } from './schema'
 import { useTokens } from './useTokens'
 
 interface Props {
@@ -24,13 +25,89 @@ export function Profile(props: Props) {
           onChange={(e) => setSearch(e.target.value)}
         />
       </form>
-      <h1>Profile for {props.query}</h1>
+      <h1>Profile of {props.query}</h1>
       {response.type === 'loading' && <div>Loading</div>}
       {response.type === 'success' && (
-        <pre>
-          <code>{JSON.stringify(response.data, null, 2)}</code>
-        </pre>
+        <table>
+          <thead>
+            <tr>
+              <th />
+              <th />
+              <th className="text-right">#</th>
+              <th />
+              <th className="text-right">Balance</th>
+              <th />
+              <th className="pl-14 text-left">Token</th>
+              <th />
+              <th className="text-left">Issuer</th>
+              <th />
+              <th className="text-left">Risks</th>
+            </tr>
+          </thead>
+          <tbody>
+            {response.data.map((entry, i) => (
+              <ProfileRow entry={entry} i={i} />
+            ))}
+          </tbody>
+        </table>
       )}
     </div>
   )
+}
+
+function ProfileRow({ entry, i }: { entry: TokenEntry; i: number }) {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <tr
+      key={entry.address}
+      className="border-black border-t"
+      onClick={() => setOpen(!open)}
+    >
+      <td className="w-0">{open ? 'V' : '>'}</td>
+      <td className="w-[20%]" />
+      <td className="w-0">{i + 1}</td>
+      <td className="w-[20%]" />
+      <td className="w-0">
+        <div className="text-right">${formatNumber(entry.balanceUsd, 2)}</div>
+        <div className="text-right">{formatNumber(entry.balanceUnits, 4)}</div>
+      </td>
+      <td className="w-[20%]" />
+      <td className="flex w-min gap-4">
+        <div className="relative h-10 w-10 min-w-10">
+          <img src={entry.assetLogoUrl} className="h-10 w-10" />
+          <img
+            src={entry.chainLogoUrl}
+            className="-bottom-1.5 -right-1.5 absolute h-6 w-6"
+          />
+        </div>
+        <div className="whitespace-pre">
+          <div>
+            {entry.assetName} on {entry.chainName}
+          </div>
+          <div>{formatAddress(entry.address)}</div>
+        </div>
+      </td>
+      <td className="w-[20%]" />
+      <td className="w-0">{entry.issuer}</td>
+      <td className="w-[20%]" />
+      <td className="w-0 whitespace-pre">
+        {new Array(entry.severity.high)
+          .fill('HIGH')
+          .concat(new Array(entry.severity.medium).fill('MED'))
+          .join(' ')}
+      </td>
+    </tr>
+  )
+}
+
+function formatNumber(value: number, decimals: number) {
+  const decimalPart = value.toFixed(decimals).slice(-decimals - 1)
+  const intPart = new Intl.NumberFormat('en-US', {}).format(Math.floor(value))
+  return intPart + decimalPart
+}
+
+function formatAddress(value: string) {
+  const [chain, address] = value.split(':')
+  return `${chain}:${address?.slice(0, 6)}…${address?.slice(-4)}`
 }
--- packages/insight/src/useTokens.ts
+++ packages/insight/src/useTokens.ts
@@ -10,8 +10,27 @@ export function useTokens(query: string): UseTokensResult {
   useEffect(() => {
     setData(undefined)
     const timeout = setTimeout(() => {
-      setData([])
-    }, 1000)
+      const assset: TokenEntry = {
+        address: 'base:0x7882570840A97A490a37bd8Db9e1aE39165bfBd6',
+        assetLogoUrl:
+          'https://fe-staging.l2beat.com/_next/image?url=https%3A%2F%2Fassets.coingecko.com%2Fcoins%2Fimages%2F6319%2Fstandard%2Fusdc.png&w=64&q=75',
+        chainLogoUrl:
+          'https://l2beat.com/_next/image?url=%2Ficons%2Fbase.png&w=48&q=75',
+        assetName: 'USDC.e',
+        balanceUnits: 1234567.123456,
+        balanceUsd: 1234567.12,
+        chainName: 'Base',
+        decimals: 6,
+        issuer: 'Circle',
+        priceUsd: 1,
+        severity: {
+          low: 1,
+          high: 2,
+          medium: 1,
+        },
+      }
+      setData([assset, assset])
+    }, 100)
     return () => {
       clearTimeout(timeout)
     }
