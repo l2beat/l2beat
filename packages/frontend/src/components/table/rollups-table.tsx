2c0ba966b2208146226bdce8e8f4c0aefea4b93b
torztomasz <tomasz.torz@l2beat.com>
2024-11-12 17:18:49 +0100

    make row divider caps

--- packages/frontend/src/components/table/rollups-table.tsx
+++ packages/frontend/src/components/table/rollups-table.tsx
@@ -64,7 +64,7 @@ function RowDivider({
         colSpan={3}
         className={cn('sticky left-0 z-1 px-0 group-first:pt-1', className)}
       >
-        <div className="h-5 rounded-l bg-surface-tertiary px-2 py-1 text-2xs font-medium leading-none text-zinc-500 dark:text-n-zinc-300">
+        <div className="h-5 rounded-l bg-surface-tertiary px-2 py-1 text-2xs font-medium uppercase leading-none text-zinc-500 dark:text-n-zinc-300">
           {children}
         </div>
       </td>
