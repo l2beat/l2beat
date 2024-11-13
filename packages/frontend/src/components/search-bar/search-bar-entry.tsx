7cd7e95ba1f776747c06dba86d1a391c4135988c
Piotr Adamczyk <xeno@xeno.yt>
2024-11-13 15:17:07 +0100

    Better grouping in search (#5825)

---
+++ packages/frontend/src/components/search-bar/search-bar-entry.tsx
@@ -0,0 +1,24 @@
+import { type SearchBarCategory } from './search-bar-categories'
+
+interface SearchBarEntry {
+  category: SearchBarCategory
+  name: string
+  tags?: string[]
+  href: string
+}
+
+export type SearchBarPage = SearchBarEntry & {
+  type: 'page'
+}
+
+export type SearchBarProject = SearchBarEntry & {
+  type: 'project'
+  id: string
+  isUpcoming: boolean
+  iconUrl: string
+  createdAt: number
+  kind: 'layer2' | 'layer3' | 'bridge' | 'da' | 'zkCatalog'
+  filePrepared?: Fuzzysort.Prepared
+}
+
+export type AnySearchBarEntry = SearchBarPage | SearchBarProject
