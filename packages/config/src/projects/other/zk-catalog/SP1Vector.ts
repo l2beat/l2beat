f49151d451d8eb81c093d0070e31d791bec281d0
sekuba <29250140+sekuba@users.noreply.github.com>
2024-11-14 09:17:59 +0100

    sp1vector: New verifier

--- packages/config/src/projects/other/zk-catalog/SP1Vector.ts
+++ packages/config/src/projects/other/zk-catalog/SP1Vector.ts
@@ -43,10 +43,40 @@ export const SP1Vector: ZkCatalogProject = {
         ],
       },
       {
-        name: 'Vector SP1 Verifier',
+        name: 'Vector SP1 Verifier - Frozen',
         description: 'Avail ZK light client verifying multiple blocks at once.',
         contractAddress: EthereumAddress(
           '0x1764C29FBd94865198588f10FC75D4f6636d158d',
+        ),
+        chainId: ChainId.ETHEREUM,
+        verified: 'no',
+        subVerifiers: [
+          {
+            name: 'Main program',
+            ...PROOFS.PROGRAM,
+            link: 'https://github.com/succinctlabs/sp1-vector/blob/main/program/src/main.rs',
+          },
+          {
+            name: 'Main circuit',
+            ...PROOFS.PLONKY3,
+            link: 'https://github.com/succinctlabs/sp1/tree/dev/crates/core',
+          },
+          {
+            name: 'Recursion circuit',
+            ...PROOFS.PLONKY3,
+            link: 'https://github.com/succinctlabs/sp1/tree/dev/crates/recursion',
+          },
+          {
+            name: 'Final wrap',
+            ...PROOFS.PLONKSNARK('?'),
+          },
+        ],
+      },
+      {
+        name: 'Vector SP1 Verifier',
+        description: 'Avail ZK light client verifying multiple blocks at once.',
+        contractAddress: EthereumAddress(
+          '0xd2832Cf1fC8bA210FfABF62Db9A8781153131d16',
         ),
         chainId: ChainId.ETHEREUM,
         verified: 'no',
