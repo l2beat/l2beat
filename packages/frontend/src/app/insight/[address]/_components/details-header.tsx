0e8959503981208cde69e02cc121084de35077ab
torztomasz <tomasz.torz@l2beat.com>
2024-11-14 10:32:08 +0100

    adjust labels

--- packages/frontend/src/app/insight/[address]/_components/details-header.tsx
+++ packages/frontend/src/app/insight/[address]/_components/details-header.tsx
@@ -12,23 +12,14 @@ interface DetailsHeaderProps {
 export function DetailsHeader(props: DetailsHeaderProps) {
   const report = useReport()
 
-  const counts = report.tokens.map(
-    ({ token }) => report.chains[token.networkId]?.risks.length ?? 0,
-  )
-
-  const sum = counts.reduce((acc, risk) => acc + risk, 0)
-  const averageIssuesPerToken =
-    report.tokens.length !== 0 ? Math.round(sum / report.tokens.length) : 0
-  const leastIssues = counts.length !== 0 ? Math.min(...counts) : 0
-
   return (
     <Card className="flex flex-col gap-4 rounded-none sm:rounded-xl">
       <h1 className="font-oswald text-3xl font-bold">Wallet insights</h1>
       <p className="text-sm  text-white/80">
-        {`A total of ${sum} issues were found with an average of ${averageIssuesPerToken} issues per token. Your most valuable token has ${leastIssues} issues.`}
+        {`There were ${report.tokens.length} assets found.`}
       </p>
       <p className="text-sm  text-white/80">
-        You can check the risks associated with specific tokens by expanding the
+        You can check the the details about a specific asset by expanding the
         rows.
       </p>
       <HorizontalSeparator className="!border-[#5656C759]" />
