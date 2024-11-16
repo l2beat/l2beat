e482e7c519941034063890a2cbe28a2a4313738e
Piotr Adamczyk <xeno@xeno.yt>
2024-11-16 10:33:41 +0100

    Use ISR in scaling summary API (#5849)

--- packages/frontend/src/app/api/(public)/scaling/summary/route.ts
+++ packages/frontend/src/app/api/(public)/scaling/summary/route.ts
@@ -1,79 +1,69 @@
-import { UnixTime } from '@l2beat/shared-pure'
-import { unstable_cache as cache } from 'next/cache'
 import { NextResponse } from 'next/server'
 import { getScalingSummaryEntries } from '~/server/features/scaling/summary/get-scaling-summary-entries'
 import { getTvlChart } from '~/server/features/scaling/tvl/get-tvl-chart-data'
 
+export const dynamic = 'force-static'
+export const revalidate = 600
+
 export async function GET() {
-  return getCachedResponse()
-}
+  const entries = await getScalingSummaryEntries()
+  const items = [
+    ...entries.rollups,
+    ...entries.validiumsAndOptimiums,
+    ...(entries.others ?? []),
+  ]
 
-const getCachedResponse = cache(
-  async () => {
-    const entries = await getScalingSummaryEntries()
-    const items = [
-      ...entries.rollups,
-      ...entries.validiumsAndOptimiums,
-      ...(entries.others ?? []),
-    ]
+  const data = await getTvlChart({
+    range: '30d',
+    excludeAssociatedTokens: false,
+    filter: { type: 'layer2' },
+  })
 
-    const data = await getTvlChart({
-      range: '30d',
-      excludeAssociatedTokens: false,
-      filter: { type: 'layer2' },
-    })
-    return NextResponse.json({
-      success: true,
-      data: {
-        chart: {
-          types: ['timestamp', 'native', 'canonical', 'external', 'ethPrice'],
-          data: data.map(
-            ([timestamp, native, canonical, external, ethPrice]) => [
-              timestamp,
-              native / 100,
-              canonical / 100,
-              external / 100,
-              ethPrice / 100,
-            ],
-          ),
-        },
-        projects: Object.fromEntries(
-          items.map((entry) => {
-            return [
-              entry.id,
-              {
-                id: entry.id,
-                name: entry.name,
-                shortName: entry.shortName,
-                slug: entry.slug,
-                type: entry.type,
-                category: entry.category,
-                provider: entry.provider,
-                purposes: entry.purposes,
-                isArchived: entry.isArchived,
-                hostChain: entry.hostChain,
-                isUpcoming: entry.isUpcoming,
-                isUnderReview: !!entry.underReviewStatus,
-                badges: entry.badges.map(({ badge, kind }) => ({
-                  category: kind,
-                  name: badge,
-                })),
-                tvl: {
-                  breakdown: entry.tvl.breakdown,
-                  associatedTokens: entry.tvl.associatedTokens,
-                  change7d: entry.tvl.change,
-                },
-                stage: entry.stage.stage,
-                risks: entry.risks,
-              },
-            ]
-          }),
-        ),
+  return NextResponse.json({
+    success: true,
+    data: {
+      chart: {
+        types: ['timestamp', 'native', 'canonical', 'external', 'ethPrice'],
+        data: data.map(([timestamp, native, canonical, external, ethPrice]) => [
+          timestamp,
+          native / 100,
+          canonical / 100,
+          external / 100,
+          ethPrice / 100,
+        ]),
       },
-    })
-  },
-  ['scaling-summary-route'],
-  {
-    revalidate: 10 * UnixTime.MINUTE,
-  },
-)
+      projects: Object.fromEntries(
+        items.map((entry) => {
+          return [
+            entry.id,
+            {
+              id: entry.id,
+              name: entry.name,
+              shortName: entry.shortName,
+              slug: entry.slug,
+              type: entry.type,
+              category: entry.category,
+              provider: entry.provider,
+              purposes: entry.purposes,
+              isArchived: entry.isArchived,
+              hostChain: entry.hostChain,
+              isUpcoming: entry.isUpcoming,
+              isUnderReview: !!entry.underReviewStatus,
+              badges: entry.badges.map(({ badge, kind }) => ({
+                category: kind,
+                name: badge,
+              })),
+              tvl: {
+                breakdown: entry.tvl.breakdown,
+                associatedTokens: entry.tvl.associatedTokens,
+                change7d: entry.tvl.change,
+              },
+              stage: entry.stage.stage,
+              risks: entry.risks,
+            },
+          ]
+        }),
+      ),
+    },
+  })
+}
