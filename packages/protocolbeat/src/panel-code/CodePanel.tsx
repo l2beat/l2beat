453bc09fc27307aea07f492bf93540b5319ab2de
Mateusz Radomski <radomski.main@protonmail.com>
2024-11-19 11:43:47 +0100

    Remember fold state in code view

--- packages/protocolbeat/src/panel-code/CodePanel.tsx
+++ packages/protocolbeat/src/panel-code/CodePanel.tsx
@@ -10,6 +10,9 @@ import { useMultiViewStore } from '../multi-view/store'
 import { usePanelStore } from '../store/store'
 import { create } from './editor'
 
+import { editor } from 'monaco-editor/esm/vs/editor/editor.api'
+const viewStates: Record<string, editor.ICodeEditorViewState | null> = {}
+
 export function CodePanel() {
   const { project } = useParams()
   if (!project) {
@@ -99,7 +102,9 @@ function CodeView({ code }: { code: string }) {
 
   useEffect(() => {
     if (editor) {
+      viewStates[cyrb64(editor.getValue())] = editor.saveViewState()
       editor.setValue(code)
+      editor.restoreViewState(viewStates[cyrb64(code)] ?? null)
     }
   }, [editor, code])
 
@@ -108,4 +113,22 @@ function CodeView({ code }: { code: string }) {
   }, [editor, panels, pickedUp])
 
   return <div className="h-full w-full" ref={monacoEl} />
+}
+
+function cyrb64(str: string, seed: number = 0) {
+  let h1 = 0xdeadbeef ^ seed,
+    h2 = 0x41c6ce57 ^ seed
+  for (let i = 0, ch; i < str.length; i++) {
+    ch = str.charCodeAt(i)
+    h1 = Math.imul(h1 ^ ch, 2654435761)
+    h2 = Math.imul(h2 ^ ch, 1597334677)
+  }
+  h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507)
+  h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909)
+  h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507)
+  h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909)
+  h2 = h2 >>> 0
+  h1 = h1 >>> 0
+
+  return h2.toString(36).padStart(7, '0') + h1.toString(36).padStart(7, '0')
 }
