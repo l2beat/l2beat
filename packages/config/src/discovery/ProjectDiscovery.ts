5388653c304066287077c064c7d46b060989b3c8
Mateusz Radomski <radomski.main@protonmail.com>
2024-11-19 13:15:07 +0100

    Fix linting and formatting mistakes

--- packages/config/src/discovery/ProjectDiscovery.ts
+++ packages/config/src/discovery/ProjectDiscovery.ts
@@ -483,10 +483,7 @@ export class ProjectDiscovery {
     return result
   }
 
-  getContractValueSafeInt(
-    contractIdentifier: string,
-    key: string,
-  ): bigint {
+  getContractValueSafeInt(contractIdentifier: string, key: string): bigint {
     const result = this.getContractValueOrUndefined(contractIdentifier, key)
     assert(
       isNonNullable(result),
