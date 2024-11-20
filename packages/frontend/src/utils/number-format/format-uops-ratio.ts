47083047ba27e59c321e43710c6a3370ce2d996a
maciekzygmunt <maciekzygmunt@interia.pl>
2024-11-20 13:49:49 +0200

    fix 1.00 ratio

--- packages/frontend/src/utils/number-format/format-uops-ratio.ts
+++ packages/frontend/src/utils/number-format/format-uops-ratio.ts
@@ -1,6 +1,9 @@
 import { formatNumber } from './format-number'
 
 export function formatUopsRatio(ratio: number): string {
+  if (ratio === 1) {
+    return '1.00'
+  }
   if (ratio < 1.01) {
     return '<1.01'
   }
