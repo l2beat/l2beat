71c8879c15ed61cabe6ed4297fd02e93e3acc486
vincfurc <10850139+vincfurc@users.noreply.github.com>
2024-11-26 18:51:09 +0000

    misc

--- packages/config/src/projects/other/da-beat/blockchain/espressoDA/espressoDA.ts
+++ packages/config/src/projects/other/da-beat/blockchain/espressoDA/espressoDA.ts
@@ -3,13 +3,12 @@ import { NO_BRIDGE } from '../../templates/no-bridge-template'
 import { DaEconomicSecurityRisk } from '../../types/DaEconomicSecurityRisk'
 import { DaFraudDetectionRisk } from '../../types/DaFraudDetectionRisk'
 import { DaLayer } from '../../types/DaLayer'
-import { DasErasureCodingProof } from '../../types/DasErasureCodingProof'
-import { DasErasureCodingScheme } from '../../types/DasErasureCodingScheme'
+import { HotShotLightClient } from './bridges/HotShotLightClient'
 
 export const espressoDA: DaLayer = {
   id: 'espressoDA',
   type: 'DaLayer',
-  kind: 'PublicBlockchain',
+  kind: 'DA Service',
   systemCategory: 'public',
   display: {
     name: 'EspressoDA',
@@ -36,7 +35,9 @@ export const espressoDA: DaLayer = {
 
     ## Consensus
 
-    EspressoDA uses the HotShot consensus protocol, a communication-efficient, permissioned proof-of-stake system that is Byzantine Fault Tolerant (BFT). 
+    Currenly only a fixed set of pre-registered operators can run a node. The Espresso Network will upgrade to proof-of-stake in a later release.
+
+    Espresso uses the HotShot consensus protocol, a communication-efficient, permissioned proof-of-stake system that is Byzantine Fault Tolerant (BFT). 
     Built on HotStuff-2, it achieves linear communication complexity using a pacemaker module to synchronize views and ensures safety and liveness as long as over two-thirds of the stake is controlled by honest nodes.
 
     HotShot operates in a view-by-view manner, where each view designates a leader and an external builder. 
@@ -46,12 +47,13 @@ export const espressoDA: DaLayer = {
 
     Once the proposer sends data to HotShot node operators, they initiate Tiramisu's three layers of data availability:
     
-    - **Savoiardi**: Disperses the data to all nodes.
-    - **Mascarpone**: Uploads the data and commitment to a small DA committee.
-    - **Cocoa**: Uploads the data and commitment to a content delivery network (CDN). 
+    - **Savoiardi**: Disperses erasure-coded data to all nodes. Savoiardi nodes only store chunks of the data.
+    - **Mascarpone**: Uploads the data and commitment to a small DA committee. Every node in the committee stores the full data.
+    - **Cocoa**: Uploads the full data to a content delivery network (CDN). \  
 
-
-    A DA certificate consists of two components: the retrievability certificate and the optimistic DAC certificate:
+      
+    Once nodes receive and store the data, they return votes to the proposer. Strong votes are votes from nodes storing the full data, while normal votes are votes from nodes storing erasure-coded shares of the data.
+    A DA certificate consists of two components, the retrievability certificate and the optimistic DAC certificate:
 
     - **Retrievability Certificate**: Formed when the DA leader collects 1/3 + 1  strong votes either from committee nodes or regular nodes, or 1/3 + m normal votes, where ( m ) is the number of VID shares into which the block was split.
     - **Optimistic DAC Certificate**: Formed when the DA leader gathers 2*(S - T) + 1 strong votes from the DA committee, where S represents the committee size and T the threshold for the committee. 
@@ -60,10 +62,10 @@ export const espressoDA: DaLayer = {
 
     ## L2s Data Availability
   
-    The life cycle of L2 rollup transactions begins with users submitting transactions to the Tiramisu DA mempool through an RPC endpoint, including a namespace ID to indicate the target L2 rollup. 
+    The life cycle of L2 transactions begins with users submitting transactions to the Tiramisu DA mempool through an RPC endpoint, including a namespace ID to indicate the target L2 rollup. 
     A DA leader collects and disperses these transactions across Tiramisu's layers to form a DA certificate. Simultaneously, a leader in the HotShot Consensus layer broadcasts a proposal with a vector commitment for the transactions. 
     The finalization of the block commitment in HotShot establishes data availability for the corresponding transactions.
-    After block finalization in HotShot, the Espresso Sequencer propagates the commitment and quorum certificates to the L1 sequencer contract, which verifies the certificate ZK-proof. 
+    After block finalization in HotShot, the Espresso Sequencer propagates the commitment and quorum certificates to the L1 Light Client contract, which verifies the certificate and the HotShot state SNARK proof via the verifyProof function. 
 
     ![EspressoDA architecture with L2s](/images/da-layer-technology/espressoDA/architectureL2.png#center)
 
@@ -83,19 +85,8 @@ export const espressoDA: DaLayer = {
         description: `No DA bridge is selected. Without a DA bridge, Ethereum has no proof of data availability for this project.\n`,
       },
     }),
+    HotShotLightClient,
   ],
-  consensusAlgorithm: {
-    name: 'HotShot',
-    description: ``,
-    blockTime: 8, // average block time
-    consensusFinality: 1,
-    unbondingPeriod: UnixTime.DAY * 21,
-  },
-  dataAvailabilitySampling: {
-    erasureCodingScheme: DasErasureCodingScheme.TwoDReedSolomon,
-    erasureCodingProof: DasErasureCodingProof.ValidityProofs,
-  },
-  pruningWindow: 0, // does not prune
   risks: {
     economicSecurity: DaEconomicSecurityRisk.OffChainVerifiable,
     fraudDetection: DaFraudDetectionRisk.NoFraudDetection,
