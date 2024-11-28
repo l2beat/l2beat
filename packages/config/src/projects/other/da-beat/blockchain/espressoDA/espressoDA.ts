af35857cb2941f0b8bd78db64810a3c312317663
vincfurc <10850139+vincfurc@users.noreply.github.com>
2024-11-28 16:13:32 +0000

    typo fix, formula

--- packages/config/src/projects/other/da-beat/blockchain/espressoDA/espressoDA.ts
+++ packages/config/src/projects/other/da-beat/blockchain/espressoDA/espressoDA.ts
@@ -35,7 +35,7 @@ export const espressoDA: DaLayer = {
 
     ## Consensus
 
-    Currenly only a fixed set of pre-registered operators can run a node. The Espresso Network will upgrade to proof-of-stake in a later release.
+    Currently only a fixed set of pre-registered operators can run a node. The Espresso Network will upgrade to proof-of-stake in a later release.
 
     Espresso uses the HotShot consensus protocol, a communication-efficient, permissioned proof-of-stake system that is Byzantine Fault Tolerant (BFT). 
     Built on HotStuff-2, it achieves linear communication complexity using a pacemaker module to synchronize views and ensures safety and liveness as long as over two-thirds of the stake is controlled by honest nodes.
@@ -55,8 +55,8 @@ export const espressoDA: DaLayer = {
     Once nodes receive and store the data, they return votes to the proposer. DAVotes are votes from committee nodes storing the full data, while QuorumVotes are votes from nodes storing erasure-coded shares of the data.
     A DA certificate consists of two components, the retrievability certificate and the optimistic DAC certificate:
 
-    - **Retrievability Certificate**: Formed when the DA leader collects 1/3 + 1  DAVotes either from committee nodes, or 1/3 + m QuorumVotes, where ( m ) is the number of VID shares into which the block was split.
-    - **Optimistic DAC Certificate**: Formed when the DA leader gathers 2*(S - T) + 1 DAVotes from the DA committee, where S represents the committee size and T the threshold for the committee. Currenlty, the committee size is 21 members, so the threshold is 15 signatures.
+    - **Retrievability Certificate**: Formed when the DA leader collects 1/3 + m QuorumVotes, where ( m ) is the number of VID shares into which the block was split.
+    - **Optimistic DAC Certificate**: Formed when the DA leader gathers 2/3 + 1 DAVotes from the DA committee. Currently, the committee size is 21 members, so the threshold is 15 signatures.
 
     Once the DAC is formed, the DA leader stops broadcasting data to the nodes.
 
