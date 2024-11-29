824d6a1d856551b8a8e1d8f7a06eec202d0c4ab7
Michał Podsiadły <67391475+sdlyy@users.noreply.github.com>
2024-11-29 10:11:34 +0100

    Test to keep scaling and DA-BEAT projects in sync (#5767)
    Co-authored-by: Mateusz Radomski <33978857+mateuszradomski@users.noreply.github.com>

--- packages/config/src/projects/other/da-beat/index.test.ts
+++ packages/config/src/projects/other/da-beat/index.test.ts
@@ -1,7 +1,10 @@
+import { UnixTime } from '@l2beat/shared-pure'
 import { expect } from 'earl'
 import { daLayers } from '.'
+import { layer2s } from '../../layer2s'
+import { layer3s } from '../../layer3s'
 
-describe('da-beat', () => {
+describe('DA-BEAT', () => {
   daLayers.forEach((layer) => {
     describe(layer.display.name, () => {
       it('should contain description with dot at the end', () => {
@@ -34,4 +37,54 @@ describe('da-beat', () => {
       }
     })
   })
+
+  describe('synchronization with scaling projects - layer2s and layer3s', () => {
+    it('each scaling project should have a corresponding entry in the DA-BEAT', () => {
+      // It can be squashed, but it's more readable this way
+      const target = [...layer2s, ...layer3s]
+        .filter(
+          (project) =>
+            !project.isUpcoming &&
+            !project.isUnderReview &&
+            !project.isArchived,
+        )
+        .filter(
+          (project) =>
+            project.display.category === 'Optimium' ||
+            project.display.category === 'Validium',
+        )
+        .filter(
+          // It makes no sense to list them on the DA-BEAT
+          (scaling) =>
+            getLatestDataAvailabilityEntry(scaling.dataAvailability)?.layer
+              .value !== 'External',
+        )
+
+      const daBeatProjectIds = daLayers.flatMap((daLayer) =>
+        daLayer.bridges.flatMap((bridge) =>
+          bridge.usedIn.map((usedIn) => usedIn.id),
+        ),
+      )
+
+      const scalingProjectIds = target.map((project) => project.id)
+
+      const projectsWithoutDaBeatEntry = scalingProjectIds.filter(
+        (project) => !daBeatProjectIds.includes(project),
+      )
+
+      // Array comparison to have a better error message with actual names
+      expect(projectsWithoutDaBeatEntry).toEqual([])
+    })
+  })
 })
+
+function getLatestDataAvailabilityEntry<
+  T extends { sinceTimestamp?: UnixTime; untilTimestamp?: UnixTime },
+>(entries: T[] | null | undefined) {
+  const now = UnixTime.now()
+  return entries?.find(
+    (entry) =>
+      (!entry.sinceTimestamp || entry.sinceTimestamp.lte(now)) &&
+      (!entry.untilTimestamp || entry.untilTimestamp.gt(now)),
+  )
+}
