94a62ab8eb3c054110bed70c246c8fa34ded9557
torztomasz <tomasz.torz@l2beat.com>
2024-12-01 19:06:06 +0100

    fix

--- packages/frontend/src/components/table/filters/table-filter.tsx
+++ packages/frontend/src/components/table/filters/table-filter.tsx
@@ -1,6 +1,5 @@
 'use client'
-import assert from 'assert'
-import { KeyboardEvent, MouseEvent, useCallback } from 'react'
+import { type KeyboardEvent, type MouseEvent, useCallback } from 'react'
 import {
   Select,
   SelectContent,
@@ -36,23 +35,30 @@ export function TableFilter<T extends string>({
   // that will be handled by the onValueChange handler
   const mappedOptions = replaceUndefined(options)
 
-  const onClick = useCallback((e: MouseEvent) => {
-    if (value !== undefined) {
-      e.preventDefault()
-      onValueChange(undefined)
-    }
-  }, [])
-
-  const onKeyDown = useCallback((e: KeyboardEvent) => {
-    if (value !== undefined) {
-      if (e.code === 'Space' || e.code === 'Enter') {
+  const onClick = useCallback(
+    (e: MouseEvent) => {
+      if (value !== undefined) {
         e.preventDefault()
         onValueChange(undefined)
-      } else if (e.code === 'ArrowDown' || e.code === 'ArrowUp') {
-        e.preventDefault()
       }
-    }
-  }, [])
+    },
+    [value, onValueChange],
+  )
+
+  const onKeyDown = useCallback(
+    (e: KeyboardEvent) => {
+      if (value !== undefined) {
+        if (e.code === 'Space' || e.code === 'Enter') {
+          e.preventDefault()
+          onValueChange(undefined)
+        } else if (e.code === 'ArrowDown' || e.code === 'ArrowUp') {
+          e.preventDefault()
+        }
+      }
+    },
+    [value, onValueChange],
+  )
+
   return (
     <Select
       value={value ?? ''}
@@ -93,29 +99,6 @@ export function TableFilter<T extends string>({
         ))}
       </SelectContent>
     </Select>
-  )
-}
-
-function SelectedValue<T extends string>({
-  options,
-  value,
-  onValueChange,
-}: Props<T>) {
-  const option = options.find((option) => option.value === value)
-  assert(option, 'Option not found')
-  return (
-    <button
-      onClick={() => onValueChange(undefined)}
-      className={cn(
-        'flex h-8 cursor-pointer select-none items-center justify-center gap-1.5 whitespace-pre rounded-lg px-2.5 text-xs font-medium text-brand outline-none transition-colors md:text-sm',
-        'sidebar:bg-surface-primary sidebar:hover:bg-surface-tertiary sidebar:main-page-card:bg-surface-secondary',
-      )}
-    >
-      <span>{option.label}</span>
-      <div className="inline-flex size-3 items-center justify-center rounded-sm bg-current">
-        <CloseIcon className="size-2.5 fill-white dark:fill-black dark:group-hover:fill-gray-950" />
-      </div>
-    </button>
   )
 }
 
