5d2541f9803d38011dc534f319fc9f89272681eb
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2024-12-13 15:50:35 +0100

    Remove hostChain: 'Multiple' (#6128)

--- packages/frontend/src/server/features/scaling/utils/get-host-chain.ts
+++ packages/frontend/src/server/features/scaling/utils/get-host-chain.ts
@@ -1,9 +1,6 @@
 import { type Layer3, layer2s } from '@l2beat/config'
 
 export function getHostChain(project: Layer3) {
-  if (project.hostChain === 'Multiple') {
-    return 'Multiple'
-  }
   const layer2 = layer2s.find((l) => l.id === project.hostChain)
   if (!layer2) {
     throw new Error(
