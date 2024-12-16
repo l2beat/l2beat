051b899b853e423d6d608195a12488c71ecd2502
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2024-12-16 14:06:35 +0100

    Add route to explore filesystem

---
+++ packages/frontend/src/app/api/(public)/walk/route.ts
@@ -0,0 +1,13 @@
+import { readdirSync } from 'fs'
+import { NextRequest, NextResponse } from 'next/server'
+import { join } from 'path'
+
+export async function GET(request: NextRequest) {
+  const path = request.nextUrl.searchParams.get('path') ?? '/'
+  const response = readdirSync(path, { withFileTypes: true }).map((x) => ({
+    name: x.name,
+    path: join(path, x.name),
+    type: x.isFile() ? 'file' : x.isDirectory() ? 'dir' : '?',
+  }))
+  return NextResponse.json(response)
+}
