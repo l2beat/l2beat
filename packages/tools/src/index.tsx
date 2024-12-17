649be1e4cc94f2fd9037f71afe4ef6153e8c6845
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2024-12-17 09:38:30 +0100

    Move files

--- packages/tools/src/index.tsx
+++ packages/tools/src/index.tsx
@@ -1,8 +1,7 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { App } from './App'
+import { App } from './decoder/App'
 
 const queryClient = new QueryClient()
 
