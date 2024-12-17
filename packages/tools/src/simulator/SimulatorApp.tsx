f1b194480d3c3fad9cde03873cac3fcae4df0375
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2024-12-17 10:45:40 +0100

    Add actual simulation

--- packages/tools/src/simulator/SimulatorApp.tsx
+++ packages/tools/src/simulator/SimulatorApp.tsx
@@ -1,11 +1,13 @@
+import { useMutation } from '@tanstack/react-query'
+import clsx from 'clsx'
 import { useState } from 'react'
+import { simulate } from './simulate'
 
 export function SimulatorApp() {
   const [value, setValue] = useState('')
-
-  function simulate() {
-    console.log('Simulating')
-  }
+  const mutation = useMutation({
+    mutationFn: (data: string) => simulate(JSON.parse(data)),
+  })
 
   return (
     <div className="mx-auto max-w-[900px] p-4 pb-20">
@@ -18,11 +20,35 @@ export function SimulatorApp() {
         onChange={(e) => setValue(e.target.value)}
       />
       <button
-        className="mb-8 rounded-sm border-zinc-900 border-b-4 bg-zinc-800 px-2.5 py-1 active:mt-1 active:border-b-0"
-        onClick={simulate}
+        disabled={mutation.isPending}
+        className={clsx(
+          'mb-8 rounded-sm border-zinc-900 border-b-4 bg-zinc-800 px-2.5 py-1 active:mt-1 active:border-b-0',
+          mutation.isPending && 'opacity-60',
+        )}
+        onClick={() => mutation.mutate(value)}
       >
         Simulate
       </button>
+      {mutation.isError && (
+        <p className="text-red-600">{mutation.error.message}</p>
+      )}
+      {mutation.isSuccess && 'link' in mutation.data && (
+        <p>
+          <a
+            className="text-orange-500 underline"
+            href={mutation.data.link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            View simulation on tenderly
+          </a>
+        </p>
+      )}
+      {mutation.isSuccess && 'data' in mutation.data && (
+        <pre className="text-mono text-red-600">
+          <code>{JSON.stringify(mutation.data.data, null, 2)}</code>
+        </pre>
+      )}
     </div>
   )
 }
