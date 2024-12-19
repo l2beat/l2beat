7e145953a387a8483446c601fc67f10b32d78382
torztomasz <tomasz.torz@l2beat.com>
2024-12-19 14:06:07 +0100

    fix misaligned badges

--- packages/frontend/src/components/badge/badge.tsx
+++ packages/frontend/src/components/badge/badge.tsx
@@ -1,51 +1,44 @@
 import { type VariantProps, cva } from 'class-variance-authority'
 import { type ReactNode } from 'react'
-import { cn } from '~/utils/cn'
 
 export interface BadgeProps extends VariantProps<typeof badgeVariants> {
   className?: string
   children: ReactNode
 }
 
-const badgeVariants = cva('rounded text-center font-medium', {
-  variants: {
-    type: {
-      error: 'bg-negative/20 text-negative',
-      gray: 'bg-gray-200 font-medium text-gray-700 sidebar:!bg-surface-tertiary sidebar:!text-secondary dark:bg-zinc-700 dark:text-gray-50',
-      warning: 'bg-yellow-500 text-black',
-      brightYellow: 'bg-yellow-200 text-purple-700',
-      pink: 'bg-pink-900 text-white',
-      purple: 'bg-purple-100',
+const badgeVariants = cva(
+  'rounded px-1.5 py-1 text-center font-medium !leading-none',
+  {
+    variants: {
+      type: {
+        error: 'bg-negative/20 text-negative',
+        gray: 'bg-gray-200 font-medium text-gray-700 sidebar:!bg-surface-tertiary sidebar:!text-secondary dark:bg-zinc-700 dark:text-gray-50',
+        warning: 'bg-yellow-500 text-black',
+        brightYellow: 'bg-yellow-200 text-purple-700',
+        pink: 'bg-pink-900 text-white',
+        purple: 'bg-purple-100',
+      },
+      size: {
+        extraSmall: 'text-3xs',
+        small: 'text-xs',
+        regular: 'text-2xs md:text-xs',
+      },
     },
-    size: {
-      extraSmall: 'text-3xs',
-      small: 'text-xs',
-      regular: 'text-2xs md:text-xs',
-    },
-    padding: {
-      small: 'px-1.5 py-px',
-      regular: 'px-1.5 py-1',
+    defaultVariants: {
+      size: 'regular',
     },
   },
-  defaultVariants: {
-    size: 'regular',
-    padding: 'small',
-  },
-})
+)
 
-export function Badge(props: BadgeProps) {
+export function Badge({ children, className, ...props }: BadgeProps) {
   return (
     <div
-      className={cn(
-        badgeVariants({
-          type: props.type,
-          size: props.size,
-          padding: props.padding,
-        }),
-        props.className,
-      )}
+      className={badgeVariants({
+        ...props,
+        className,
+      })}
     >
-      {props.children}
+      {children}
     </div>
   )
 }
