4e27a5ca057253beab9794f88ac31b4c52f7fb26
maciekzygmunt <maciekzygmunt@interia.pl>
2024-12-19 15:24:03 +0100

    add legend to project stacked chart

--- packages/frontend/src/components/chart/tvl/stacked/project-stacked-tvl-chart.tsx
+++ packages/frontend/src/components/chart/tvl/stacked/project-stacked-tvl-chart.tsx
@@ -53,6 +53,7 @@ export function ProjectStackedTvlChart({
         setUnit={setUnit}
         milestones={milestones}
         projectId={projectId}
+        showStackedChartLegend
       />
     )
   }
