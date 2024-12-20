3a96ee65dae368a47cc75e0b4ad0084589a68ebf
vincfurc <10850139+vincfurc@users.noreply.github.com>
2024-12-20 17:38:23 +0000

    big & clear buttons for old people

--- packages/frontend/src/app/(side-nav)/scaling/summary/_components/chart-tabs.tsx
+++ packages/frontend/src/app/(side-nav)/scaling/summary/_components/chart-tabs.tsx
@@ -26,12 +26,19 @@ export function ChartTabs({ unit, timeRange, className }: Props) {
         </Tabs.Content>
         <Tabs.List className="flex gap-1.5">
           <Tabs.Trigger value="tvl" className="group w-full py-2.5">
-            <div className="my-auto h-2 w-full rounded-full bg-surface-tertiary group-data-[state=active]:bg-brand" />
+            <div className="my-auto h-8 w-full rounded-lg bg-surface-tertiary group-data-[state=active]:bg-brand flex items-center justify-center"
+                 style={{ borderTopRightRadius: '0', borderBottomRightRadius: '0' }}>
+              <span className="text-[20px]">Value</span>
+            </div>
           </Tabs.Trigger>
           <Tabs.Trigger value="activity" className="group w-full py-2.5">
-            <div className="my-auto h-2 w-full rounded-full bg-surface-tertiary group-data-[state=active]:bg-brand" />
+            <div className="my-auto h-8 w-full rounded-lg bg-surface-tertiary group-data-[state=active]:bg-brand flex items-center justify-center"
+                 style={{ borderTopLeftRadius: '0', borderBottomLeftRadius: '0' }}>
+              <span className="text-[20px]">Activity</span>
+            </div>
           </Tabs.Trigger>
         </Tabs.List>
+        
       </Tabs.Root>
     </MainPageCard>
   )
