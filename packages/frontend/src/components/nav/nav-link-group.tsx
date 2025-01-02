3e755bf516f591a495e166c431267f798e703d34
torztomasz <tomasz.torz@l2beat.com>
2025-01-02 10:24:58 +0100

    change side nav behavior

--- packages/frontend/src/components/nav/nav-link-group.tsx
+++ packages/frontend/src/components/nav/nav-link-group.tsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { useState } from 'react'
 import { ChevronIcon } from '~/icons/chevron'
 import { cn } from '~/utils/cn'
 import {
@@ -21,55 +22,71 @@ export interface NavLinkGroupProps {
  * Group of nav links with a title used in the sidenav.
  */
 export function NavLinkGroup({ group }: NavLinkGroupProps) {
-  const pathname = usePathname()
-
   if (group.type === 'single') {
-    const isSelected = group.href === pathname
-    return (
-      <Link
-        href={group.href}
-        className="group flex items-center gap-2 p-1.5"
-        data-active={isSelected}
-      >
-        {group.icon}
-        <span
-          className={cn(
-            'ml-1 text-base font-medium tracking-tight text-primary transition-colors duration-300',
-            isSelected && 'text-brand',
-          )}
-        >
-          {group.title}
-        </span>
-      </Link>
-    )
+    return <NavLinkGroupSingleLink group={group} />
   }
 
-  const isSelected = pathname.startsWith(`/${group.match}`)
+  return <NavLinkMultipleGroup group={group} />
+}
+
+function NavLinkGroupSingleLink({
+  group,
+}: { group: Extract<NavGroup, { type: 'single' }> }) {
+  const pathname = usePathname()
+  const isSelected = group.href === pathname
+  return (
+    <Link
+      href={group.href}
+      className="group flex items-center gap-2 p-1.5"
+      data-active={isSelected}
+    >
+      {group.icon}
+      <span
+        className={cn(
+          'ml-1 text-base font-medium tracking-tight text-primary transition-colors duration-300',
+          isSelected && 'text-brand',
+        )}
+      >
+        {group.title}
+      </span>
+    </Link>
+  )
+}
+
+function NavLinkMultipleGroup({
+  group,
+}: { group: Extract<NavGroup, { type: 'multiple' }> }) {
+  const pathname = usePathname()
+  const [open, setOpen] = useState(pathname.startsWith(`/${group.match}`))
 
   return (
-    <Collapsible className="flex flex-col" defaultOpen={isSelected}>
-      <CollapsibleTrigger
-        className="group flex items-center gap-2 p-1.5"
-        data-active={isSelected}
-      >
-        {group.icon}
-        <div className="flex items-baseline gap-1.5">
+    <Collapsible className="flex flex-col" open={open} onOpenChange={setOpen}>
+      <div className="flex items-center gap-1.5 p-1.5">
+        <Link
+          href={group.links[0]!.href}
+          className="group flex items-center gap-2"
+          onClick={() => setOpen(true)}
+          data-active={open}
+        >
+          <div>{group.icon}</div>
           <span
             className={cn(
               'ml-1 text-base font-medium tracking-tight text-primary transition-colors duration-300 ',
-              isSelected && 'text-brand',
+              open && 'text-brand',
             )}
           >
             {group.title}
           </span>
+        </Link>
+        <CollapsibleTrigger className="group">
           <ChevronIcon
             className={cn(
               'size-3 -rotate-90 fill-primary transition-[transform,_color,_fill] duration-300 group-data-[state=open]:rotate-0',
-              isSelected && 'fill-brand',
+              open && 'fill-brand',
             )}
           />
-        </div>
-      </CollapsibleTrigger>
+        </CollapsibleTrigger>
+      </div>
       <CollapsibleContent>
         <ul className="mb-3 ml-[15px] mt-2 flex flex-col gap-0.5 border-l border-gray-300 pl-[14px] dark:border-gray-850">
           {group.links?.map(
