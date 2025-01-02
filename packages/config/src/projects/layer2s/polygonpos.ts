033ce11925281167c3d82ae49a1d537b21dcf697
maciekop-l2b <maciej.opala@l2beat.com>
2025-01-02 13:45:08 +0100

    [L2B-8619] Add polygon chain config

--- packages/config/src/projects/layer2s/polygonpos.ts
+++ packages/config/src/projects/layer2s/polygonpos.ts
@@ -270,4 +270,22 @@ export const polygonpos: Layer2 = {
       thumbnail: NUGGETS.THUMBNAILS.L2BEAT_03,
     },
   ],
+  chainConfig: {
+    name: 'polygonpos',
+    chainId: 137,
+    blockscoutV2ApiUrl: 'https://polygon.blockscout.com/api/v2',
+    explorerUrl: 'https://polygonscan.com',
+    explorerApi: {
+      url: 'https://api.polygonscan.com/api',
+      type: 'etherscan',
+    },
+    multicallContracts: [
+      {
+        address: EthereumAddress('0xcA11bde05977b3631167028862bE2a173976CA11'),
+        batchSize: 150,
+        sinceBlock: 25770160,
+        version: '3',
+      },
+    ],
+  },
 }
