148997288c8533980be8e66358ab59ade0c90e4c
torztomasz <tomasz.torz@l2beat.com>
2025-01-03 10:19:53 +0100

    fix

--- packages/frontend/src/components/core/tooltip/tooltip.tsx
+++ packages/frontend/src/components/core/tooltip/tooltip.tsx
@@ -63,7 +63,7 @@ const TooltipTrigger = ({
 TooltipTrigger.displayName = TooltipPrimitive.Trigger.displayName
 
 const tooltipContentVariants = cva(
-  'z-110 shadow-popover animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 rounded-lg bg-white px-4 py-3 text-left text-sm font-normal normal-case leading-tight text-gray-700 dark:bg-neutral-700 dark:text-white',
+  'z-110 rounded-lg bg-white px-4 py-3 text-left text-sm font-normal normal-case leading-tight text-gray-700 shadow-popover animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 dark:bg-neutral-700 dark:text-white',
   {
     variants: {
       fitContent: {
