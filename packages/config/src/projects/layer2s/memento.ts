e12446c37088fb7f7553d76416ae97ec73476688
Fabrizio Audisio <fabrizio.audisio@dextf.com>
2025-01-06 17:30:17 +0800

    Added Memento ZKchain
    Added the Memento ZKchain as upcoming project

---
+++ packages/config/src/projects/layer2s/memento.ts
@@ -0,0 +1,28 @@
+import { UnixTime } from '@l2beat/shared-pure';
+import { upcomingL2 } from './templates/upcoming';
+import { Layer2 } from './types';
+
+export const memento: Layer2 = upcomingL2({
+  id: 'memento',
+  createdAt: new UnixTime(1730879100), // 2024-11-6T07:45:00Z
+  display: {
+    name: 'Memento ZK Chain',
+    slug: 'memento',
+    description:
+      'Memento ZKchain is a ZK Rollup dedicated to institutional DeFi and digital asset management, providing a secure, permissioned environment for digital investment funds and asset distribution.',
+    purposes: ['Interoperability', 'Privacy', 'RWA'],
+    category: 'ZK Rollup',
+    provider: 'ZK Stack',
+    links: {
+      websites: ['https://mementoblockchain.com/'],
+      apps: [],
+      documentation: [],
+      explorers: [],
+      repositories: [],
+      socialMedia: [
+        'https://x.com/Memento_Bc',
+        'https://linkedin.com/company/memento-blockchain/',
+      ],
+    },
+  },
+});
