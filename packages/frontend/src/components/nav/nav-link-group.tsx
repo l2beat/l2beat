5c6945fea5edf65c7984b7011d812eedb1811666
torztomasz <tomasz.torz@l2beat.com>
2025-01-07 12:32:41 +0100

    change behavior on mobile

--- packages/frontend/src/components/nav/nav-link-group.tsx
+++ packages/frontend/src/components/nav/nav-link-group.tsx
@@ -71,26 +71,22 @@ function NavLinkMultipleGroup({
 
   return (
     <Collapsible className="flex flex-col" open={open} onOpenChange={setOpen}>
-      <div
-        className="group flex cursor-pointer items-center gap-1.5 p-1.5"
-        data-active={isActive}
-      >
-        <Link
-          href={group.links[0]!.href}
-          className="flex items-center gap-2"
+      {isMobile ? (
+        <CollapsibleTrigger
+          className="group flex cursor-pointer items-center gap-1.5 p-1.5"
           data-active={isActive}
         >
-          <div>{group.icon}</div>
-          <span
-            className={cn(
-              'ml-1 text-base font-medium tracking-tight text-primary transition-colors duration-300 ',
-              isActive && 'text-brand',
-            )}
-          >
-            {group.title}
-          </span>
-        </Link>
-        <CollapsibleTrigger asChild>
+          <div className="flex items-center gap-2" data-active={isActive}>
+            <div>{group.icon}</div>
+            <span
+              className={cn(
+                'ml-1 text-base font-medium tracking-tight text-primary transition-colors duration-300 ',
+                isActive && 'text-brand',
+              )}
+            >
+              {group.title}
+            </span>
+          </div>
           <ChevronIcon
             className={cn(
               'size-3 -rotate-90 fill-primary transition-[transform,_color,_fill] duration-300 data-[state=open]:rotate-0',
@@ -98,7 +94,36 @@ function NavLinkMultipleGroup({
             )}
           />
         </CollapsibleTrigger>
-      </div>
+      ) : (
+        <div
+          className="group flex cursor-pointer items-center gap-1.5 p-1.5"
+          data-active={isActive}
+        >
+          <Link
+            href={group.links[0]!.href}
+            className="flex items-center gap-2"
+            data-active={isActive}
+          >
+            <div>{group.icon}</div>
+            <span
+              className={cn(
+                'ml-1 text-base font-medium tracking-tight text-primary transition-colors duration-300 ',
+                isActive && 'text-brand',
+              )}
+            >
+              {group.title}
+            </span>
+          </Link>
+          <CollapsibleTrigger asChild>
+            <ChevronIcon
+              className={cn(
+                'size-3 -rotate-90 fill-primary transition-[transform,_color,_fill] duration-300 data-[state=open]:rotate-0',
+                isActive && 'fill-brand',
+              )}
+            />
+          </CollapsibleTrigger>
+        </div>
+      )}
       <CollapsibleContent>
         <ul className="mb-3 ml-[15px] mt-2 flex flex-col gap-0.5 border-l border-gray-300 pl-[14px] dark:border-gray-850">
           {group.links?.map(
