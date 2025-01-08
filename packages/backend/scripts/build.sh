3d892c0b4a6252c1fc411122c0465ce97ee41e59
Konrad B <arukopay@gmail.com>
2025-01-08 11:12:24 +0100

    Fix publish path of backend's static artifact (#6293)
    Signed-off-by: Konrad <arukopay@gmail.com>

--- packages/backend/scripts/build.sh
+++ packages/backend/scripts/build.sh
@@ -5,7 +5,7 @@ set -e
 tsc -p tsconfig.build.json
 
 STATIC_SRC="src/static"
-STATIC_DEST="build/src/static"
+STATIC_DEST="build/src"
 
 mkdir -p "$STATIC_DEST"
 cp -R "$STATIC_SRC/" "$STATIC_DEST/"
