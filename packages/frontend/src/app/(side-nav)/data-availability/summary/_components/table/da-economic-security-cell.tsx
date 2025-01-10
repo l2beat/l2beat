34cf143e1304b912e39eaef209bebc6b07062224
maciekzygmunt <maciekzygmunt@interia.pl>
2025-01-10 12:16:36 +0100

    format dabeat numbers

--- packages/frontend/src/app/(side-nav)/data-availability/summary/_components/table/da-economic-security-cell.tsx
+++ packages/frontend/src/app/(side-nav)/data-availability/summary/_components/table/da-economic-security-cell.tsx
@@ -1,15 +1,15 @@
 import { assertUnreachable } from '@l2beat/shared-pure'
 import { type DaSummaryEntry } from '~/server/features/data-availability/summary/get-da-summary-entries'
-import { formatCurrency } from '~/utils/number-format/format-currency'
+import { formatTvlTableNumber } from '~/utils/number-format/format-tvl-number'
 
 export function DaEconomicSecurityCell({
   value,
 }: { value: DaSummaryEntry['economicSecurity'] }) {
-  if (!value) return formatCurrency(0, 'usd')
+  if (!value) return formatTvlTableNumber(0)
 
   switch (value.status) {
     case 'Synced':
-      return formatCurrency(value.economicSecurity, 'usd')
+      return formatTvlTableNumber(value.economicSecurity)
     case 'StakeNotSynced':
       return 'Stake not synced'
     case 'CurrentPriceNotSynced':
