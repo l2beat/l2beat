2afa9db8eedf09b3425f2329eca3d069d3249dde
sekuba <29250140+sekuba@users.noreply.github.com>
2025-01-10 15:10:44 +0100

    move soon to inreview, add ETH escrow

--- packages/config/src/projects/layer2s/soon.ts
+++ packages/config/src/projects/layer2s/soon.ts
@@ -1,8 +1,8 @@
-import { UnixTime } from '@l2beat/shared-pure'
-import { upcomingL2 } from './templates/upcoming'
+import { EthereumAddress, UnixTime } from '@l2beat/shared-pure'
+import { underReviewL2 } from './templates/underReview'
 import { Layer2 } from './types'
 
-export const soon: Layer2 = upcomingL2({
+export const soon: Layer2 = underReviewL2({
   id: 'soon',
   createdAt: new UnixTime(1726836904), // 2024-09-20T12:55:04Z
   display: {
@@ -14,11 +14,24 @@ export const soon: Layer2 = upcomingL2({
     category: 'Optimistic Rollup',
     links: {
       websites: ['https://soo.network/'],
-      apps: [],
-      documentation: ['https://soon-2.gitbook.io/home'],
-      explorers: ['https://explorer.devnet.soo.network'],
+      apps: ['https://bridge.mainnet.soo.network/home'],
+      documentation: ['https://docs.soo.network/introduction/what-is-soon'],
+      explorers: ['https://explorer.soo.network/'],
       repositories: ['https://github.com/soonlabs'],
-      socialMedia: ['https://x.com/soon_svm', 'https://discord.gg/soon-svm'],
+      socialMedia: [
+        'https://x.com/soon_svm',
+        'https://discord.gg/soon-svm',
+        'https://medium.com/@soon_SVM',
+      ],
     },
+    // no activityDataSource due to SVM
   },
+  escrows: [
+    {
+      address: EthereumAddress('0x5a0702c7ebbec83802b35db737fccdc5fc6c5e07'), // optimismPortal
+      sinceTimestamp: new UnixTime(1735877303),
+      tokens: ['ETH'],
+      chain: 'ethereum',
+    },
+  ],
 })
