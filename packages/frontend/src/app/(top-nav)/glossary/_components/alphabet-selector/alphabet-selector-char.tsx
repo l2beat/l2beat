845142a0994c6bda37f927e66609257b52f2711c
torztomasz <tomasz.torz@l2beat.com>
2025-01-13 13:00:17 +0100

    fix alphabet selector char styles

--- packages/frontend/src/app/(top-nav)/glossary/_components/alphabet-selector/alphabet-selector-char.tsx
+++ packages/frontend/src/app/(top-nav)/glossary/_components/alphabet-selector/alphabet-selector-char.tsx
@@ -20,7 +20,7 @@ export function AlphabetSelectorChar({ char, href, selected, ref }: CharProps) {
         className={cn(
           'flex size-[34px] items-center justify-center rounded border transition ease-out',
           'border-gray-300 bg-pure-white dark:border-zinc-700 dark:bg-zinc-900',
-          selected && 'border-brand bg-purple-300 dark:bg-neutral-700',
+          selected && '!border-brand !bg-[#FF5FFB66]',
           href && !selected && 'hover:bg-gray-100 dark:hover:bg-zinc-800',
           !href &&
             'cursor-not-allowed border-gray-300 text-gray-400 dark:border-zinc-800 dark:text-zinc-500',
