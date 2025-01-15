8e3dd7504c6b67e433e5aaa89a6e925d0a7142f6
torztomasz <tomasz.torz@l2beat.com>
2025-01-15 13:05:57 +0100

    adjust tvs-breakdown

--- packages/frontend/src/app/(top-nav)/scaling/projects/[slug]/tvs-breakdown/_components/tvl-breakdown-page-header.tsx
+++ packages/frontend/src/app/(top-nav)/scaling/projects/[slug]/tvs-breakdown/_components/tvl-breakdown-page-header.tsx
@@ -1,20 +1,27 @@
+import { type WarningWithSentiment } from '@l2beat/config'
 import Image from 'next/image'
+import { type ProjectSevenDayTvlBreakdown } from '~/server/features/scaling/tvl/utils/get-7d-tvl-breakdown'
 import { formatTimestampToDateWithHour } from '~/utils/dates'
+import { TvlBreakdownSummaryBox } from './tvl-breakdown-summary-box'
 
 interface Props {
   title: string
   slug: string
+  tvl: ProjectSevenDayTvlBreakdown
+  tvlWarning: WarningWithSentiment | undefined
   tvlBreakdownTimestamp: number
 }
 
 export function TvlBreakdownPageHeader({
   title,
   slug,
+  tvl,
+  tvlWarning,
   tvlBreakdownTimestamp,
 }: Props) {
   return (
-    <div className="mt-11 flex flex-col px-4 md:px-0">
-      <div className={'flex items-center gap-1.5'}>
+    <div className="flex flex-col border-divider px-4 pt-6 max-md:border-b max-md:bg-header-primary md:mt-[38px] md:px-0">
+      <div className="mb-4 flex items-center gap-1.5">
         <Image
           width={16}
           height={16}
@@ -28,15 +35,35 @@ export function TvlBreakdownPageHeader({
         </h1>
       </div>
 
-      <div className="my-4 flex flex-col items-start justify-between gap-[10px] md:mt-[38px] md:flex-row md:items-center">
+      <div className="flex flex-col items-start justify-between gap-[10px] md:mb-4 md:flex-row md:items-center">
         <h2 className="text-[28px] font-bold md:text-3xl">TVS Breakdown</h2>
-        <div className="text-xs font-medium text-gray-500 dark:text-gray-550">
+        <div className="text-xs font-medium text-secondary">
           Timestamp:&nbsp;
           <span className="text-base font-medium text-primary">
             {formatTimestampToDateWithHour(tvlBreakdownTimestamp)}
           </span>
         </div>
       </div>
+
+      <TvlBreakdownSummaryBox
+        total={{
+          value: tvl.breakdown.total,
+          change: tvl.change.total,
+        }}
+        canonical={{
+          value: tvl.breakdown.canonical,
+          change: tvl.change.canonical,
+        }}
+        external={{
+          value: tvl.breakdown.external,
+          change: tvl.change.external,
+        }}
+        native={{
+          value: tvl.breakdown.native,
+          change: tvl.change.native,
+        }}
+        warning={tvlWarning}
+      />
     </div>
   )
 }
