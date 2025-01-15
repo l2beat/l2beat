760d7370277b06c3cb756e9b0896f168f7f85dce
torztomasz <tomasz.torz@l2beat.com>
2025-01-15 09:34:42 +0100

    fix bridges header

--- packages/frontend/src/app/(top-nav)/bridges/projects/[slug]/layout.tsx
+++ packages/frontend/src/app/(top-nav)/bridges/projects/[slug]/layout.tsx
@@ -1,8 +1,10 @@
+import { BridgesMvpWarning } from '~/app/(side-nav)/bridges/_components/bridges-mvp-warning'
 import { ScrollToTopButton } from '~/components/scroll-to-top-button'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <BridgesMvpWarning className="w-full" />
       {children}
       <ScrollToTopButton />
     </>
