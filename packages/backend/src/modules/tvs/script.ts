2be676bfd0d287e36cd8942b83a8c9cce7dccffb
maciekop-l2b <maciej.opala@l2beat.com>
2025-01-15 09:40:28 +0100

    Basic interfaces
    Co-authored-by: Antoni Pawlak <antoni.pawlak@pm.me>

---
+++ packages/backend/src/modules/tvs/script.ts
@@ -0,0 +1,20 @@
+import { layer2s } from "@l2beat/config"
+import { assert, ProjectId, UnixTime } from "@l2beat/shared-pure"
+import { LocalExecutor } from "./LocalExecutor"
+import { mapConfig } from "./mapConfig"
+
+main().catch(() => {
+  process.exit(1)
+})
+
+async function main() {
+  const arbitrum = layer2s.find(l => l.id === ProjectId('arbitrum'))
+  assert(arbitrum)
+  const config = mapConfig(arbitrum)
+
+  const localExecutor = new LocalExecutor()
+  const tvs = await localExecutor.run(config, [UnixTime.now()])
+
+  console.log(tvs)
+  // TODO: sum
+}
