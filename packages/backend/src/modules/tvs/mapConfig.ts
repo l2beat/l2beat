eba675696daeab87fde691b5c9ce95c94261be4a
maciekop-l2b <166010253+maciekop-l2b@users.noreply.github.com>
2025-01-15 11:16:34 +0100

    [L2B-8775] Define basic interfaces for TVL4 (#6365)
    Co-authored-by: Antoni Pawlak <antoni.pawlak@pm.me>

---
+++ packages/backend/src/modules/tvs/mapConfig.ts
@@ -0,0 +1,13 @@
+import { Layer2 } from '@l2beat/config'
+import { AmountConfig, PriceConfig, TvsConfig } from './types'
+
+export function mapConfig(_project: Layer2): TvsConfig {
+  throw new Error('Not implemented')
+}
+
+export function extractPricesAndAmounts(_config: TvsConfig): {
+  prices: PriceConfig[]
+  amounts: AmountConfig[]
+} {
+  throw new Error('Not implemented')
+}
