fbaa8fa2effe4b0bd1192b6dc183d6e6109a7a9c
Mateusz Radomski <radomski.main@protonmail.com>
2025-01-16 11:22:07 +0100

    Wrap

--- packages/tools/src/discolupe/ColumnSelector.tsx
+++ packages/tools/src/discolupe/ColumnSelector.tsx
@@ -20,7 +20,7 @@ export function ColumnSelector({
       <summary className="font-semibold text-lg">Select columns</summary>
 
       <p className="mb-1 font-bold text-xl">Available columns</p>
-      <div className="mb-3 flex gap-2">
+      <div className="mb-3 flex flex-wrap gap-2">
         {notSelected.map((col) => (
           <div
             className="cursor-pointer rounded bg-zinc-700 px-2 py-1 hover:bg-zinc-600 focus:outline-none focus:ring-2 focus:ring-orange-400"
@@ -34,7 +34,7 @@ export function ColumnSelector({
       <hr />
 
       <p className="mb-1 font-bold text-xl">Selected columns</p>
-      <div className="flex gap-2">
+      <div className="flex flex-wrap gap-2">
         {selected.map((col, i) => (
           <div
             className="cursor-pointer rounded bg-zinc-700 px-2 py-1 hover:bg-zinc-600 focus:outline-none focus:ring-2 focus:ring-orange-400"
