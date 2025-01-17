25eadc43c96428a51fe2a23a1149d6ee4235b833
torztomasz <tomasz.torz@l2beat.com>
2025-01-17 16:37:08 +0100

    enable new plausible provider

--- packages/frontend/src/providers/plausible-provider.tsx
+++ packages/frontend/src/providers/plausible-provider.tsx
@@ -2,14 +2,13 @@
 
 import NextPlausibleProvider from 'next-plausible'
 import { useTheme } from 'next-themes'
-import { env } from '~/env'
 
 export function PlausibleProvider({ children }: { children: React.ReactNode }) {
   const { resolvedTheme } = useTheme()
   return (
     <NextPlausibleProvider
       domain="new-analytics.l2beat.com"
-      enabled={env.NEXT_PUBLIC_PLAUSIBLE_ENABLED}
+      enabled={true}
       pageviewProps={{
         theme: resolvedTheme ?? 'light',
       }}
