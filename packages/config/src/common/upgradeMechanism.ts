148d24317a30fd51e89b2ea5c80fe0c2b645a876
Luca Donno <donnoh99@gmail.com>
2025-01-20 21:57:59 +0000

    Fix duplicated risks

--- packages/config/src/common/upgradeMechanism.ts
+++ packages/config/src/common/upgradeMechanism.ts
@@ -1,7 +1,6 @@
 import { formatSeconds } from '@l2beat/shared-pure'
 
 import { ScalingProjectTechnologyChoice } from './ScalingProjectTechnologyChoice'
-import { CONTRACTS } from './contracts'
 
 function ARBITRUM_DAO(
   l1Delay: number,
@@ -20,7 +19,7 @@ function ARBITRUM_DAO(
     )} delay for all L2 --> L1 messages (in total a delay of ${formatSeconds(
       totalDelay,
     )}). The Security Council can upgrade the contracts without any delay. It can also cancel any upgrades initiated by the DAO.`,
-    risks: [CONTRACTS.UPGRADE_WITH_DELAY_RISK(formatSeconds(totalDelay))],
+    risks: [],
     references: [
       {
         text: 'Arbitrum DAO',
