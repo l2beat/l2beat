e3328fc56f5959f7d991eb491cda6923cf116ce5
sekuba <29250140+sekuba@users.noreply.github.com>
2025-01-21 13:02:32 +0100

    remove wrong refs

--- packages/config/src/projects/layer2s/hashkey.ts
+++ packages/config/src/projects/layer2s/hashkey.ts
@@ -162,7 +162,7 @@ export const hashkey: Layer2 = {
       warnings: {
         stateUpdates: OPTIMISTIC_ROLLUP_STATE_UPDATES_WARNING,
       },
-      explanation: `Ink is an Optimistic rollup that posts transaction data to the L1. For a transaction to be considered final, it has to be posted within a tx batch on L1 that links to a previous finalized batch. If the previous batch is missing, transaction finalization can be delayed up to ${formatSeconds(
+      explanation: `Optimistic rollup that posts transaction data to the L1. For a transaction to be considered final, it has to be posted within a tx batch on L1 that links to a previous finalized batch. If the previous batch is missing, transaction finalization can be delayed up to ${formatSeconds(
         HARDCODED.OPTIMISM.SEQUENCING_WINDOW_SECONDS,
       )} or until it gets published. The state root gets confirmed ${formatSeconds(
         maxClockDuration,
@@ -376,7 +376,6 @@ export const hashkey: Layer2 = {
       },
     ],
   },
-  //stateDerivation: DERIVATION.OPSTACK('INK'),
   stateValidation: {
     // NOTE THAT DESCRIPTIONS ARE SLIGHTLY MODIFIED BECAUSE PERMISSIONED
     description:
