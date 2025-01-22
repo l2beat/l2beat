7b7d849a5c3fbf16e916ac640152570d557a5e64
torztomasz <tomasz.torz@l2beat.com>
2025-01-22 21:28:02 +0100

    adjust error state

--- packages/tools/src/common/ErrorState.tsx
+++ packages/tools/src/common/ErrorState.tsx
@@ -1,8 +1,8 @@
 export function ErrorState() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center font-mono text-red-500">
-      <div className="pixelated mb-8 grid h-64 w-64 grid-cols-8 gap-1">
-        {Array.from({ length: 64 }).map((_, index) => (
+      <div className="pixelated mb-8 grid h-64 w-64 grid-cols-10 gap-1">
+        {Array.from({ length: 100 }).map((_, index) => (
           <div key={index} className="h-full w-full bg-red-500" />
         ))}
       </div>
