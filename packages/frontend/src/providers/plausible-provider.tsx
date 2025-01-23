13bdcf6dc86eec7d17e861d01709fe6f10db03bf
torztomasz <tomasz.torz@l2beat.com>
2025-01-23 11:12:35 +0100

    remove recategorisation preview from plausible pageview props

--- packages/frontend/src/providers/plausible-provider.tsx
+++ packages/frontend/src/providers/plausible-provider.tsx
@@ -2,12 +2,10 @@
 
 import NextPlausibleProvider from 'next-plausible'
 import { useTheme } from 'next-themes'
-import { useRecategorisationPreviewContext } from '~/components/recategorisation-preview/recategorisation-preview-provider'
 import { env } from '~/env'
 
 export function PlausibleProvider({ children }: { children: React.ReactNode }) {
   const { resolvedTheme } = useTheme()
-  const { checked } = useRecategorisationPreviewContext()
 
   return (
     <NextPlausibleProvider
@@ -15,7 +13,6 @@ export function PlausibleProvider({ children }: { children: React.ReactNode }) {
       enabled={env.NEXT_PUBLIC_PLAUSIBLE_ENABLED}
       pageviewProps={{
         theme: resolvedTheme ?? 'light',
-        recategorisationPreview: checked.toString(),
       }}
       hash
       taggedEvents
