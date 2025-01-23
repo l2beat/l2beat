c3bbcd0976f03df808fde8765fe222262bb677ec
Michał Podsiadły <67391475+sdlyy@users.noreply.github.com>
2025-01-23 14:26:38 +0100

    Display DA solution's permissions and contracts (#6489)

---
+++ packages/frontend/src/server/features/scaling/project/get-scaling-project-da-solution.ts
@@ -0,0 +1,60 @@
+import {
+  type ScalingProjectContract,
+  type ScalingProjectPermission,
+  daLayers,
+} from '@l2beat/config'
+import type { ScalingProject } from './get-scaling-project-entry'
+
+type Common = {
+  layerName: string
+  bridgeName: string
+  hostChain: string
+}
+
+export type DaSolution = Common & {
+  permissions: ScalingProjectPermission[] | undefined
+  contracts: ScalingProjectContract[] | undefined
+}
+
+export type DaSolutionWith<T> = Common & T
+
+export function getDaSolution(project: ScalingProject): DaSolution | undefined {
+  const layerBridgePairs = daLayers.flatMap((layer) =>
+    layer.bridges.flatMap((bridge) => ({ layer, bridge })),
+  )
+
+  const daSolution = layerBridgePairs.find((pair) =>
+    pair.bridge.usedIn.some((usedIn) => usedIn.id === project.id),
+  )
+
+  const hostChainSelector =
+    project.type === 'layer2' ? 'ethereum' : project.hostChain
+
+  if (
+    !daSolution ||
+    daSolution.bridge.type === 'NoBridge' ||
+    !hostChainSelector
+  ) {
+    return
+  }
+
+  const {
+    permissions: allDaBridgePermissions,
+    contracts: allDaBridgeContracts,
+  } = daSolution.bridge
+
+  const daBridgePermissions =
+    allDaBridgePermissions !== 'UnderReview'
+      ? allDaBridgePermissions?.[hostChainSelector]
+      : undefined
+
+  const daBridgeContracts = allDaBridgeContracts.addresses[hostChainSelector]
+
+  return {
+    layerName: daSolution.layer.display.name,
+    bridgeName: daSolution.bridge.display.name,
+    hostChain: hostChainSelector,
+    permissions: daBridgePermissions,
+    contracts: daBridgeContracts,
+  }
+}
