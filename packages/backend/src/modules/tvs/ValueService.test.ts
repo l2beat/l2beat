2feb3f57e8b31b8a44d2d5d3dcb30f699bc445e0
maciekop-l2b <maciej.opala@l2beat.com>
2025-01-23 09:06:03 +0100

    Add more tests

--- packages/backend/src/modules/tvs/ValueService.test.ts
+++ packages/backend/src/modules/tvs/ValueService.test.ts
@@ -14,7 +14,74 @@ import type {
 
 describe(ValueService.name, () => {
   describe(ValueService.prototype.calculate.name, () => {
-    it('should calculate TVS - wrapped token', async () => {
+    it('should calculate TVS - simple scenario', async () => {
+      const ticker = 'ABCD'
+
+      const amountFormula = {
+        type: 'totalSupply',
+        address: EthereumAddress.random(),
+        chain: 'chain',
+        decimals: 18,
+      } as TotalSupplyAmountFormula
+
+      const amountConfigId = createAmountConfig(amountFormula).id
+
+      const tvsConfig = mockObject<TvsConfig>({
+        projectId: ProjectId('project'),
+        tokens: [
+          mockObject<Token>({
+            id: 'tokeId',
+            ticker,
+            amount: amountFormula,
+            valueForProject: undefined,
+            valueForTotal: undefined,
+          }),
+        ],
+      })
+
+      const priceConfigId = createPriceConfig(
+        mockObject<ValueFormula>({
+          ticker,
+        }),
+      ).id
+
+      const mockTimestamp = UnixTime.now()
+
+      const mockDataStorage = mockObject<DataStorage>({
+        getAmount: mockFn()
+          .given(amountConfigId, mockTimestamp)
+          .resolvesToOnce(10000)
+          .resolvesToOnce(10000),
+        getPrice: mockFn()
+          .given(priceConfigId, mockTimestamp)
+          .resolvesToOnce(200)
+          .resolvesToOnce(200),
+      })
+
+      const valueService = new ValueService(mockDataStorage)
+
+      const result = await valueService.calculate(tvsConfig, [mockTimestamp])
+
+      expect(result).toEqual(
+        new Map<number, TokenValue[]>([
+          [
+            mockTimestamp.toNumber(),
+            [
+              {
+                amount: 10000,
+                projectId: ProjectId('project'),
+                tokenId: 'tokeId',
+                value: 2000000,
+                valueForProject: undefined,
+                valueForTotal: undefined,
+              },
+            ],
+          ],
+        ]),
+      )
+    })
+
+    it('should calculate TVS - wrapped token (solvBTC)', async () => {
       const wBTCContractAddress = EthereumAddress(
         '0x10e4C3460310a2F4b56C8DB0b3806Be29B15c15E',
       )
