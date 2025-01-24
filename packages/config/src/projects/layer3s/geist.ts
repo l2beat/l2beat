a24eb5410ce16a639e0f327e7e096841750874b5
sekuba <29250140+sekuba@users.noreply.github.com>
2025-01-24 10:45:20 +0100

    add Geist L3 (full)

--- packages/config/src/projects/layer3s/geist.ts
+++ packages/config/src/projects/layer3s/geist.ts
@@ -1,24 +1,41 @@
 import { ProjectId, UnixTime } from '@l2beat/shared-pure'
-import { upcomingL3 } from '../layer2s/templates/upcoming'
+import { REASON_FOR_BEING_OTHER } from '../../common'
+import { subtractOne } from '../../common/assessCount'
+import { ProjectDiscovery } from '../../discovery/ProjectDiscovery'
+import { Badge } from '../badges'
+import { AnytrustDAC } from '../da-beat/templates/anytrust-template'
+import { orbitStackL3 } from '../layer2s/templates/orbitStack'
 import type { Layer3 } from './types'
 
-export const geist: Layer3 = upcomingL3({
-  id: 'geist',
+const discovery = new ProjectDiscovery('geist', 'base')
+
+export const geist: Layer3 = orbitStackL3({
   createdAt: new UnixTime(1720191862), // 2024-07-05T15:04:22Z
   hostChain: ProjectId('base'),
+  additionalPurposes: ['Gaming', 'NFT'],
+  additionalBadges: [
+    Badge.DA.DAC,
+    Badge.L3ParentChain.Base,
+    Badge.RaaS.Alchemy,
+  ],
   display: {
+    reasonsForBeingOther: [
+      REASON_FOR_BEING_OTHER.CLOSED_PROOFS,
+      REASON_FOR_BEING_OTHER.SMALL_DAC,
+    ],
     name: 'Geist',
     slug: 'geist',
     description:
-      'Geist is an upcoming Layer 3 on Base, built on the Orbit stack. It is focused on creating a better gaming and metaverse experience.',
-    purposes: ['Gaming'],
+      'Geist is an Orbit stack Optimium on Base. It is focused on creating a better gaming and metaverse experience around AavegotchiDAO and the GHST governance token.',
     category: 'Optimium',
-    provider: 'Arbitrum',
     links: {
       websites: ['https://playongeist.com/'],
-      apps: [],
+      apps: [
+        'https://bridge.arbitrum.io/?destinationChain=geist-mainnet&sourceChain=base',
+        'https://dapp.aavegotchi.com/migrate?type=migrateTokens&fromChain=137&toChain=63157',
+      ],
       documentation: ['https://docs.playongeist.com/'],
-      explorers: [],
+      explorers: ['https://geist-mainnet.explorer.alchemy.com/'],
       repositories: [],
       socialMedia: [
         'https://x.com/aavegotchi',
@@ -26,4 +43,24 @@ export const geist: Layer3 = upcomingL3({
       ],
     },
   },
+  transactionApi: {
+    type: 'rpc',
+    defaultUrl: 'https://geist-mainnet.g.alchemy.com/public',
+    defaultCallsPerMinute: 600,
+    assessCount: subtractOne,
+    startBlock: 1,
+  },
+  bridge: discovery.getContract('ERC20Bridge'),
+  rollupProxy: discovery.getContract('RollupProxy'),
+  sequencerInbox: discovery.getContract('SequencerInbox'),
+  associatedTokens: ['GHST'],
+  gasTokens: ['GHST'],
+  discovery,
+  discoveryDrivenData: true,
+  dataAvailabilitySolution: AnytrustDAC({
+    bridge: {
+      createdAt: new UnixTime(1737711018), // 2025-01-24T09:30:18+00:00
+    },
+    discovery,
+  }),
 })
