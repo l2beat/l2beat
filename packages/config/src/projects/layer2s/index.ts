9a1d0b126e79452807165ea320c853285de32c33
hejpiotrze <150451714+hejpiotrze@users.noreply.github.com>
2025-01-27 15:12:10 +0100

    Add Status as upcoming project (#6261)
    Co-authored-by: Radina Talanova <nt.radina@gmail.com>

--- packages/config/src/projects/layer2s/index.ts
+++ packages/config/src/projects/layer2s/index.ts
@@ -155,6 +155,7 @@ import { sophon } from './sophon'
 import { sorare } from './sorare'
 import { specular } from './specular'
 import { starknet } from './starknet'
+import { status } from './status'
 import { stealthchain } from './stealthchain'
 import { superlumio } from './superlumio'
 import { superseed } from './superseed'
@@ -353,6 +354,7 @@ export const layer2s: Layer2[] = [
   sorare,
   specular,
   starknet,
+  status,
   stealthchain,
   shape,
   superlumio,
