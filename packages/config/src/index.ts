aba1270dbe43d30d93f9c93363e386512cc7d1c5
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2025-01-27 16:58:25 +0100

    Restrict config exports even more

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,10 +1,17 @@
-export * from './chains'
+export { chains } from './chains'
 export { CONTRACTS, HOMEPAGE_MILESTONES, PROJECT_COUNTDOWNS } from './common'
 export { ProjectDiscovery } from './discovery/ProjectDiscovery'
 export * from './projects'
-export * from './tokens/tokens'
+export {
+  tokenList,
+  safeGetTokenByAssetId,
+} from './tokens/tokens'
 export type * from './types'
-export * from './utils/commonContracts'
-export * from './utils/discoveryDriven'
-export * from './verification/getContractsVerificationStatuses'
-export * from './verification/isVerified'
+export { getCommonContractsIn } from './utils/commonContracts'
+export {
+  areContractsDiscoveryDriven,
+  arePermissionsDiscoveryDriven,
+  isDiscoveryDriven,
+} from './utils/discoveryDriven'
+export { getContractsVerificationStatuses } from './verification/getContractsVerificationStatuses'
+export { isDaBridgeVerified, isVerified } from './verification/isVerified'
