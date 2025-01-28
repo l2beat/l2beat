f953299af6d1f673457ebe213b3bb67f411df373
vincfurc <10850139+vincfurc@users.noreply.github.com>
2025-01-28 19:18:24 +0000

    Celestia upgrades to 8MB blocks (#6550)

--- packages/config/src/projects/da-beat/celestia/celestia.ts
+++ packages/config/src/projects/da-beat/celestia/celestia.ts
@@ -143,7 +143,7 @@ export const celestia: BlockchainDaLayer = {
   },
   pruningWindow: 86400 * 30, // 30 days in seconds
   throughput: {
-    size: 2000, // 2 MB
+    size: 8000, // 8 MB
     frequency: 6, // 6 seconds
   },
   risks: {
@@ -158,4 +158,29 @@ export const celestia: BlockchainDaLayer = {
       coingeckoId: 'celestia',
     },
   },
+  milestones: [
+    {
+      title: 'Mainnet launch',
+      url: 'https://blog.celestia.org/celestia-mainnet-is-live/',
+      date: '2023-10-31T14:00:00Z',
+      description: 'Celestia mainnet launches.',
+      type: 'general',
+    },
+    {
+      title: 'Ginger upgrade',
+      url: 'https://blog.celestia.org/ginger/',
+      date: '2024-12-12T00:00:00Z',
+      description:
+        'Celestia Ginger upgrade cuts the block time in half from 12 to 6 seconds.',
+      type: 'general',
+    },
+    {
+      title: 'Block size increase to 8MB',
+      url: 'https://x.com/celestia/status/1881756463317860430/',
+      date: '2025-01-28T00:00:00Z',
+      description:
+        'Celestia onchain governance votes to increase the block size from 2MB to 8MB.',
+      type: 'general',
+    },
+  ],
 }
