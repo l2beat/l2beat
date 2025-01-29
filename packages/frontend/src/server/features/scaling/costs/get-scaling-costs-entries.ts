2fc4b70f0ae8f1c0fce1d9d742427a580c87a844
maciekzygmunt <maciekzygmunt@interia.pl>
2025-01-29 10:18:39 +0100

    update sorting

--- packages/frontend/src/server/features/scaling/costs/get-scaling-costs-entries.ts
+++ packages/frontend/src/server/features/scaling/costs/get-scaling-costs-entries.ts
@@ -3,6 +3,7 @@ import {
   ProjectService,
   type WarningWithSentiment,
 } from '@l2beat/config'
+import { api } from '~/trpc/server'
 import { groupByTabs } from '~/utils/group-by-tabs'
 import {
   type ProjectChanges,
@@ -12,18 +13,18 @@ import {
   type CommonScalingEntry,
   getCommonScalingEntry,
 } from '../get-common-scaling-entry'
-import { getProjectsLatestTvlUsd } from '../tvl/utils/get-latest-tvl-usd'
-import { compareStageAndTvl } from '../utils/compare-stage-and-tvl'
+import type { CostsTableData } from './get-costs-table-data'
+import { compareStageAndCost } from './utils/compare-stage-and-cost'
 
 export async function getScalingCostsEntries() {
-  const [tvl, projectsChangeReport, projects] = await Promise.all([
-    getProjectsLatestTvlUsd(),
+  const [projectsChangeReport, projects, costs] = await Promise.all([
     getProjectsChangeReport(),
     ProjectService.STATIC.getProjects({
       select: ['statuses', 'scalingInfo', 'costsInfo'],
       where: ['isScaling'],
       whereNot: ['isUpcoming', 'isArchived'],
     }),
+    api.costs.table({ range: '30d' }),
   ])
 
   const entries = projects
@@ -31,27 +32,31 @@ export async function getScalingCostsEntries() {
       getScalingCostEntry(
         project,
         projectsChangeReport.getChanges(project.id),
-        tvl[project.id],
+        costs[project.id],
       ),
     )
-    .sort(compareStageAndTvl)
+    .sort(compareStageAndCost)
   return groupByTabs(entries)
 }
 
 export interface ScalingCostsEntry extends CommonScalingEntry {
   costsWarning: WarningWithSentiment | undefined
-  tvlOrder: number
+  costOrder: number
 }
 
 function getScalingCostEntry(
   project: Project<'statuses' | 'scalingInfo' | 'costsInfo'>,
   changes: ProjectChanges,
-  tvl: number | undefined,
+  costs: CostsTableData[string] | undefined,
 ): ScalingCostsEntry {
+  let costPerUop = Infinity
+  if (costs?.uopsCount && costs.usd.total) {
+    costPerUop = costs.usd.total / costs.uopsCount
+  }
   return {
     ...getCommonScalingEntry({ project, changes }),
     href: `/scaling/projects/${project.slug}#onchain-costs`,
     costsWarning: project.costsInfo.warning,
-    tvlOrder: tvl ?? -1,
+    costOrder: costPerUop,
   }
 }
