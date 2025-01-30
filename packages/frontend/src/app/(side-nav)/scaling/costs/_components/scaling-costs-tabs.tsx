fcc0311cf738b9a747fd7e4aedc661ec63f0a1c5
torztomasz <tomasz.torz@l2beat.com>
2025-01-30 14:03:21 +0100

    use compare fns

--- packages/frontend/src/app/(side-nav)/scaling/costs/_components/scaling-costs-tabs.tsx
+++ packages/frontend/src/app/(side-nav)/scaling/costs/_components/scaling-costs-tabs.tsx
@@ -15,6 +15,7 @@ import { useRecategorisationPreviewContext } from '~/components/recategorisation
 import { OthersInfo, RollupsInfo } from '~/components/scaling-tabs-info'
 import { TableSortingProvider } from '~/components/table/sorting/table-sorting-context'
 import type { ScalingCostsEntry } from '~/server/features/scaling/costs/get-scaling-costs-entries'
+import { compareStageAndTvs } from '~/server/features/scaling/utils/compare-stage-and-tvs'
 import type { TabbedScalingEntries } from '~/utils/group-by-tabs'
 import { useScalingFilter } from '../../_components/scaling-filter-context'
 import { ScalingFilters } from '../../_components/scaling-filters'
@@ -36,10 +37,7 @@ export function ScalingCostsTabs(props: Props) {
     others: props.others.filter(includeFilters),
   }
   const entries = checked
-    ? getRecategorisedEntries(
-        filteredEntries,
-        (a, b) => b.tvsOrder - a.tvsOrder,
-      )
+    ? getRecategorisedEntries(filteredEntries, compareStageAndTvs)
     : filteredEntries
 
   const projectToBeMigratedToOthers = useMemo(
