0b8ad7cfdce60cd9ec8223d72c38c9fdc2aa43d3
sekuba <29250140+sekuba@users.noreply.github.com>
2025-01-30 16:56:36 +0100

    init

---
+++ packages/backend/discovery/lumia/ethereum/diffHistory.md
@@ -0,0 +1,36 @@
+Generated with discovered.json: 0xe105f706e2bb89bb43f7820bb1b3f756dba47bd6
+
+# Diff at Thu, 30 Jan 2025 15:33:37 GMT:
+
+- author: sekuba (<29250140+sekuba@users.noreply.github.com>)
+- current block number: 21738270
+
+## Description
+
+Provide description of changes. This section will be preserved.
+
+## Initial discovery
+
+```diff
++   Status: CREATED
+    contract Verifier (0x0775e11309d75aA6b0967917fB0213C5673eDf81)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract LumiaDAC (0x25ba7858b4592b777A2fF3f7da79cB080aAb15c8)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract LumiaValidium (0x92726F7dE49300DBdb60930066bc1d0803c0740B)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract DACProxyAdmin (0xb3F294dAEd917b33FFcC687DFfB8Cd77565FF54a)
+    +++ description: None
+```
--- packages/config/src/projects/layer2s/lumia.ts
+++ packages/config/src/projects/layer2s/lumia.ts
@@ -1,29 +1,162 @@
-import { UnixTime } from '@l2beat/shared-pure'
+import { EthereumAddress, UnixTime } from '@l2beat/shared-pure'
+import {
+  DA_BRIDGES,
+  DA_LAYERS,
+  NEW_CRYPTOGRAPHY,
+  RISK_VIEW,
+} from '../../common'
+import { REASON_FOR_BEING_OTHER } from '../../common'
+import { ProjectDiscovery } from '../../discovery/ProjectDiscovery'
 import type { Layer2 } from '../../types'
 import { Badge } from '../badges'
-import { upcomingL2 } from './templates/upcoming'
+import { PolygoncdkDAC } from '../da-beat/templates/polygoncdk-template'
+import { polygonCDKStack } from './templates/polygonCDKStack'
 
-export const lumia: Layer2 = upcomingL2({
-  id: 'lumia',
-  capability: 'universal',
+const discovery = new ProjectDiscovery('lumia')
+
+const shared = new ProjectDiscovery('shared-polygon-cdk')
+const bridge = shared.getContract('Bridge')
+
+const membersCountDAC = discovery.getContractValue<number>(
+  'LumiaDAC',
+  'getAmountOfMembers',
+)
+
+const requiredSignaturesDAC = discovery.getContractValue<number>(
+  'LumiaDAC',
+  'requiredAmountOfSignatures',
+)
+
+const isForcedBatchDisallowed =
+  discovery.getContractValue<string>('LumiaValidium', 'forceBatchAddress') !==
+  '0x0000000000000000000000000000000000000000'
+
+const upgradeability = {
+  upgradableBy: ['LumiaDAC Upgrader'],
+  upgradeDelay: 'None',
+}
+
+export const lumia: Layer2 = polygonCDKStack({
   addedAt: new UnixTime(1718181773), // 2024-06-12T08:42:53Z
-  badges: [Badge.Infra.AggLayer],
+  additionalBadges: [Badge.DA.DAC],
+  reasonsForBeingOther: [REASON_FOR_BEING_OTHER.SMALL_DAC],
+  additionalPurposes: ['Restaking', 'RWA'],
   display: {
     name: 'Lumia',
     slug: 'lumia',
-    description:
-      'Lumia is a modular L2 aiming at leveraging Polygon CDK, EigenDA, and AggLayer to achieve enhanced functionality in DeFi applications.',
-    purposes: ['Universal', 'Restaking'],
-    category: 'Validium',
-    stack: 'Polygon',
+    description: 'Lumia is a modular L2 aiming at leveraging Polygon CDK, EigenDA, and AggLayer to achieve enhanced functionality in DeFi applications.',
     links: {
       websites: ['https://lumia.org/'],
+      apps: ['https://bridge.lumia.org/'],
+      explorers: ['https://explorer.lumia.org/', 'https://lens.lumia.org/'],
       documentation: ['https://docs.lumia.org/'],
-      repositories: ['https://github.com/orionprotocol'],
+      repositories: ['https://github.com/orionprotocol/cdk-validium-permissionless-node'],
       socialMedia: [
         'https://x.com/BuildOnLumia',
         'https://t.me/lumia_community',
+        'https://discord.gg/Lumia'
       ],
     },
   },
+  rpcUrl: '', // successfully tested at 5k/min
+  discovery,
+  daProvider: {
+    layer: DA_LAYERS.DAC,
+    bridge: DA_BRIDGES.DAC_MEMBERS({
+      requiredSignatures: requiredSignaturesDAC,
+      membersCount: membersCountDAC,
+    }),
+    riskView: RISK_VIEW.DATA_EXTERNAL_DAC({
+      membersCount: membersCountDAC,
+      requiredSignatures: requiredSignaturesDAC,
+    }),
+    technology: {
+      name: 'Data is not stored on chain',
+      description: 'The transaction data is not recorded on the Ethereum main chain. Transaction data is stored off-chain and only the hashes are posted onchain by the Sequencer, after being signed by the DAC members.',
+      risks: [
+        {
+          category: 'Funds can be lost if',
+          text: 'the external data becomes unavailable.',
+          isCritical: true,
+        },
+      ],
+      references: [
+        {
+          title: 'PolygonValidiumEtrog.sol - Etherscan source code, sequenceBatchesValidium function',
+          url: 'https://etherscan.io/address//0x427113ae6F319BfFb4459bfF96eb8B6BDe1A127F#code#F1#L91',
+        },
+      ],
+    },
+  },
+  rollupModuleContract: discovery.getContract('LumiaValidium'),
+  rollupVerifierContract: discovery.getContract('Verifier'),
+  isForcedBatchDisallowed,
+  chainConfig: {
+    name: 'lumia',
+    chainId: 994873017,
+    explorerUrl: '',
+    minTimestampForTvl: new UnixTime(),
+  },
+  nonTemplateEscrows: [
+    shared.getEscrowDetails({
+      address: bridge.address,
+      tokens: '*',
+      sharedEscrow: {
+        type: 'AggLayer',
+        nativeAsset: 'etherWrapped',
+        wethAddress: EthereumAddress(
+          '0x5A77f1443D16ee5761d310e38b62f77f726bC71c'
+        ),
+        tokensToAssignFromL1: ['CAPS'],
+      },
+    }),
+  ],
+  nonTemplateTechnology: {
+    newCryptography: {
+      ...NEW_CRYPTOGRAPHY.ZK_BOTH,
+    },
+  },
+  stateDerivation: {
+    nodeSoftware: 'Node software can be found [here](https://github.com/0xPolygon/cdk-validium-node).',
+    compressionScheme: 'No compression scheme yet.',
+    genesisState: 'The genesis state, whose corresponding root is accessible as Batch 0 root in the `getRollupBatchNumToStateRoot(5,0)` method of PolygonRollupManager, is available [here](https://github.com/0xPolygonHermez/zkevm-contracts/blob/1ad7089d04910c319a257ff4f3674ffd6fc6e64e/tools/addRollupType/genesis.json).',
+    dataFormat: 'The trusted sequencer request signatures from DAC members off-chain, and posts hashed batches with signatures to the WirexPayChainValidium contract.',
+  },
+
+  nonTemplatePermissions: [
+    {
+      name: 'LocalAdmin',
+      accounts: [
+        discovery.formatPermissionedAccount(
+          discovery.getContractValue('LumiaValidium', 'admin')
+        ),
+      ],
+      description: 'Admin and ForceBatcher of the LumiaValidium contract, can set core system parameters like timeouts, sequencer, activate forced transactions, and set the DA committee members in the LumiaDAC contract.',
+    },
+    {
+      name: 'LumiaDAC Upgrader',
+      accounts: [
+        discovery.formatPermissionedAccount(
+          discovery.getContractValue('DACProxyAdmin', 'owner')
+        ),
+      ],
+      description: 'Can upgrade the LumiaDAC contract and thus change the data availability rules any time.',
+    },
+  ],
+  nonTemplateContracts: [
+    discovery.getContractDetails('LumiaDAC', {
+      description: 'Validium committee contract that allows the owner to setup the members of the committee and stores the required amount of signatures threshold.',
+      ...upgradeability,
+    }),
+  ],
+  knowledgeNuggets: [],
+  dataAvailabilitySolution: PolygoncdkDAC({
+    bridge: {
+      addedAt: new UnixTime(1738252392), // 2025-01-30T15:53:12+00:00
+      requiredMembers: requiredSignaturesDAC,
+      membersCount: membersCountDAC,
+      transactionDataType: 'Transaction data',
+    },
+  }),
+  milestones: []
 })
