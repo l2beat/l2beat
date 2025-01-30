3ba006a50ffdfeddb798ee8ece899e3641383f0c
sekuba <29250140+sekuba@users.noreply.github.com>
2025-01-30 17:09:56 +0100

    add chainconf

--- packages/config/src/projects/layer2s/lumia.ts
+++ packages/config/src/projects/layer2s/lumia.ts
@@ -99,7 +99,7 @@ export const lumia: Layer2 = polygonCDKStack({
   chainConfig: {
     name: 'lumia',
     chainId: 994873017,
-    explorerUrl: '',
+    explorerUrl: 'https://explorer.lumia.org/',
     minTimestampForTvl: new UnixTime(1719499031),
   },
   nonTemplateEscrows: [
