f8f0a7e3000006bf86c455ca8053d12e45af3b88
Antoni Pawlak <antoni.pawlak@pm.me>
2025-01-31 09:36:46 +0100

    Prevent BlockTargetIndexer from invalidating on RPC failure (#6613)

--- packages/backend/src/modules/activity/indexers/BlockTargetIndexer.ts
+++ packages/backend/src/modules/activity/indexers/BlockTargetIndexer.ts
@@ -1,4 +1,5 @@
 import type { Logger } from '@l2beat/backend-tools'
+import type { Database } from '@l2beat/database'
 import { assert, type ProjectId } from '@l2beat/shared-pure'
 import { Indexer, RootIndexer } from '@l2beat/uif'
 import type { Clock } from '../../../tools/Clock'
@@ -6,13 +7,14 @@ import type { BlockTimestampProvider } from '../../tvl/services/BlockTimestampPr
 
 export class BlockTargetIndexer extends RootIndexer {
   // used only for runtime invalidation protection
-  blockHeight = 0
+  blockHeight = -1
 
   constructor(
     logger: Logger,
     private readonly clock: Clock,
     private readonly blockTimestampProvider: BlockTimestampProvider,
-    projectId: ProjectId,
+    private readonly db: Database,
+    private projectId: ProjectId,
   ) {
     super(
       logger.tag({
@@ -34,15 +36,33 @@ export class BlockTargetIndexer extends RootIndexer {
   async tick(): Promise<number> {
     const timestamp = this.clock.getLastHour()
     this.logger.info('Getting block number for timestamp', { timestamp })
+
     const blockNumber =
       await this.blockTimestampProvider.getBlockNumberAtOrBefore(timestamp)
+
+    await this.checkBlockNumber(blockNumber)
+
+    this.blockHeight = blockNumber
+    return blockNumber
+  }
+
+  /**
+   * Some RPCs can return smaller block number over time
+   * This check is here to prevent invalidation of our infra in such cases
+   */
+  private async checkBlockNumber(blockNumber: number) {
+    if (this.blockHeight === -1) {
+      const lastProcessedBlock = await this.db.activity.getLatestProcessedBlock(
+        this.projectId,
+      )
+      if (lastProcessedBlock) {
+        this.blockHeight = lastProcessedBlock
+      }
+    }
 
     assert(
       blockNumber >= this.blockHeight,
       `Block number cannot be smaller: ${blockNumber} < ${this.blockHeight}`,
     )
-
-    this.blockHeight = blockNumber
-    return blockNumber
   }
 }
