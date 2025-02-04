41ddf0e11836cd057c48c8bd4b31c0d81aee0442
vincfurc <10850139+vincfurc@users.noreply.github.com>
2025-02-04 21:55:52 +0000

    espresso badge

--- packages/config/src/projects/layer3s/rari.ts
+++ packages/config/src/projects/layer3s/rari.ts
@@ -13,6 +13,7 @@ export const rari: Layer3 = orbitStackL3({
     Badge.DA.CelestiaBlobstream,
     Badge.L3ParentChain.Arbitrum,
     Badge.RaaS.Caldera,
+    Badge.Other.EspressoSequencing,
   ],
   additionalPurposes: ['NFT'],
   discovery,
