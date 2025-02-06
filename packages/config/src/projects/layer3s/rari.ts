5a23d5f1ba762939a9b173bc62edd02b0ea0ab68
vincfurc <10850139+vincfurc@users.noreply.github.com>
2025-02-06 10:59:51 +0000

    update refs

--- packages/config/src/projects/layer3s/rari.ts
+++ packages/config/src/projects/layer3s/rari.ts
@@ -84,8 +84,12 @@ export const rari: Layer3 = orbitStackL3({
         The verifier checks the quote signature originates from inside the TEE and reverts if unsuccessful.`,
       references: [
         {
-          url: 'https://github.com/EspressoSystems/nitro-espresso-integration',
+          url: 'https://github.com/EspressoSystems/nitro-espresso-integration/blob/7ddcc6c036fa05cc47560552c85f30b5adedf32c/arbnode/batch_poster.go#L574',
           title: 'Nitro Espresso Integration',
+        },
+        {
+          url: 'https://gramine.readthedocs.io/en/stable/sgx-intro.html#:~:text=The%20SGX%20quote%20is%20a%20signed%20report%20that%20contains%20the%20enclave%20measurement%20and%20the%20signer%20measurement%20of%20the%20enclave%20and%20the%20signer%20of%20the%20signer%20process%20that%20created%20the%20report.',
+          title: 'SGX Quote',
         },
       ],
       risks: [
