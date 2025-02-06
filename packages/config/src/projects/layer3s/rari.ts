1c85504c0e3d54fb568c7ecd413668eda6967eb4
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2025-02-06 12:50:11 +0100

    Update packages/config/src/projects/layer3s/rari.ts

--- packages/config/src/projects/layer3s/rari.ts
+++ packages/config/src/projects/layer3s/rari.ts
@@ -76,7 +76,7 @@ export const rari: Layer3 = orbitStackL3({
   nonTemplateTechnology: {
     sequencing: {
       name: 'Espresso TEE sequencer',
-      description: `Rari integrates with Espresso sequencing, 
+      description: `Rari integrates with Espresso sequencing. 
         In addition to providing regular pre-confirmations, the sequencer publishes blocks to the Espresso Network.
         The integration expects the transaction batch poster to run inside a Trusted Execution Environment (TEE), and it is programmed to verify batch inclusion in a Espresso Network block before publishing it to the host chain.
         However, the confirmations provided by Espresso Network are additive, and the batch poster can skip Espresso inclusion checks should the Espresso Network be down or unavailable.
