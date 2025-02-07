0bbde256456cb28286185b98faeb1255460c83cb
sekuba <29250140+sekuba@users.noreply.github.com>
2025-02-07 10:29:13 +0100

    Add Hyperlane Nexus (and base for Hyperlane protocol)

---
+++ packages/backend/discovery/hyperlane/ethereum/diffHistory.md
@@ -0,0 +1,42 @@
+Generated with discovered.json: 0xa821f37015f63e96183035cc5f5f06dbb83e397d
+
+# Diff at Fri, 07 Feb 2025 06:49:34 GMT:
+
+- author: sekuba (<29250140+sekuba@users.noreply.github.com>)
+- current block number: 21792734
+
+## Description
+
+Initial discovery: Token bridge with lane-specific security and default security configs.
+
+## Initial discovery
+
+```diff
++   Status: CREATED
+    contract HyperlaneMultisig (0x3965AC3D295641E452E0ea896a086A9cD7C6C5b6)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract ProxyAdmin (0x75EE15Ee1B4A75Fa3e2fDF5DF3253c25599cc659)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract Mailbox (0xc005dc82818d67AF737725bD4bf75435d065D239)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract StaticAggregationIsm (0xd27fe5631533a193776A61B600809a73256eF9a7)
+    +++ description: None
+```
+
+```diff
++   Status: CREATED
+    contract HypERC20Collateral (0xe1De9910fe71cC216490AC7FCF019e13a34481D7)
+    +++ description: None
+```
---
+++ packages/config/src/projects/bridges/hyperlane.ts
@@ -0,0 +1,229 @@
+import { EthereumAddress, ProjectId, UnixTime } from '@l2beat/shared-pure'
+import { CONTRACTS } from '../../common'
+import { ProjectDiscovery } from '../../discovery/ProjectDiscovery'
+import type { Bridge } from '../../types'
+import { RISK_VIEW } from './common'
+
+const PROJECT_ID = ProjectId('hyperlane')
+const discovery = new ProjectDiscovery(PROJECT_ID.toString())
+
+export const hyperlane: Bridge = {
+  type: 'bridge',
+  id: PROJECT_ID,
+  addedAt: new UnixTime(1738918243), // 2025-02-07T
+  display: {
+    name: 'Hyperlane Nexus',
+    slug: 'hyperlane',
+    // nexus-unspecific fields are commented out (will add those later)
+    // warning:
+    //   'The security parameters of each Token Router and non-default ISM module must be individually assessed, and can be changed by their developers.',
+    category: 'Token Bridge',
+    links: {
+      websites: ['https://hyperlane.xyz/'],
+      apps: ['https://www.usenexus.org/'],
+      repositories: ['https://github.com/hyperlane-xyz'],
+      documentation: ['https://docs.hyperlane.xyz/'],
+      socialMedia: [
+        'https://x.com/hyperlane',
+        'https://discord.com/invite/VK9ZUy3aTV',
+      ],
+    },
+    description:
+      'Hyperlane Nexus is a Token Bridge built on the Hyperlane interoperability protocol for cross-chain communication. The Nexus bridge validates token bridging between chains with Multisig threshold security by default, whereas the Hyperlane protocol supports a wide range of endpoint and security configurations.',
+    // detailedDescription: `It allows developers to create interchain token bridges by deploying Hyperlane Warp Route contracts and leveraging the Hyperlane Mailbox infrastructure deployed to each chain.
+    //     Hyperlane deployments can be configured with a custom Interchain Security Module (ISM), which specifies the security model to use. If no custom ISM is configured, the default ISM module will be used.`,
+  },
+  config: {
+    escrows: [
+      discovery.getEscrowDetails({
+        description:
+          'Escrow for USDC that is bridged from Ethereum to Eclipse.',
+        address: EthereumAddress('0xe1De9910fe71cC216490AC7FCF019e13a34481D7'),
+        tokens: ['USDC'],
+      }),
+    ],
+  },
+  riskView: {
+    validatedBy: {
+      value: 'Third Party',
+      description:
+        'The Hyperlane message protocol is used. If the verifier (called ISM, default is a multisig) agrees on a message, it is considered verified and can be executed at the destination.',
+      sentiment: 'bad',
+    },
+    sourceUpgradeability: {
+      value: 'Yes',
+      description:
+        'System and token contracts can be changed arbitrarily and without notice.',
+      sentiment: 'bad',
+    },
+    destinationToken: RISK_VIEW.CANONICAL,
+  },
+  technology: {
+    destination: [
+      'Optimism',
+      'Polygon',
+      'Boba',
+      'Arbitrum',
+      'ZkSync Era',
+      'Linea',
+      'Base',
+      'Aleph Zero EVM',
+      'Ancient8',
+      'ApeChain',
+      'AppChain',
+      'Arbitrum Nova',
+      'Astar',
+      'Astar zkEVM',
+      'Avalanche',
+      'B3',
+      'Bitlayer',
+      'Blast',
+      'BOB',
+      'Boba Mainnet',
+      'Binance Smart Chain',
+      'BSquared Network',
+      'Celo',
+      'CheeseChain',
+      'Chiliz',
+      'Core',
+      'Cyber',
+      'Degen',
+      'Dogechain',
+      'DuckChain',
+      'Eclipse',
+      'Endurance',
+      'Ethereum',
+      'Everclear',
+      'Fantom Opera',
+      'Flame',
+      'Flare',
+      'EVM on Flow',
+      'Fraxtal',
+      'Fuse',
+      'Gnosis',
+      'Gravity Alpha Mainnet',
+      'Harmony One',
+      'Immutable zkEVM',
+      'Injective EVM',
+      'Injective',
+      'Kaia',
+      'Kroma',
+      'Lisk',
+      'LUKSO',
+      'Lumia Prism',
+      'Manta Pacific',
+      'Mantle',
+      'Merlin',
+      'Metal L2',
+      'Metis Andromeda',
+      'Mint',
+      'Mode',
+      'Molten',
+      'Moonbeam',
+      'Morph',
+      'Neutron',
+      'Oort',
+      'Orderly L2',
+      'Polynomial',
+      'Prom',
+      'Proof of Play Apex',
+      'RARI Chain',
+      're.al',
+      'Redstone',
+      'Rootstock',
+      'Sanko',
+      'Scroll',
+      'Sei',
+      'Shibarium',
+      'SnaxChain',
+      'Solana',
+      'Superposition',
+      'Superseed',
+      'Swell',
+      'Taiko',
+      'Tangle',
+      'Treasure',
+      'Unichain',
+      'Vana',
+      'Viction',
+      'World Chain',
+      'Xai',
+      'XLayer',
+      'Zero Network',
+      'ZetaChain',
+      'Zircuit',
+      'zkLink Nova',
+      'Zora',
+    ],
+    principleOfOperation: {
+      name: 'Principle of operation',
+      description: `
+      Hyperlane Nexus is a minimal Token Bridge, simply representing a certain configuration of the modular Hyperlane protocol. 
+      It consists of three main components: A Mailbox contract on each chain, Interchain Security Modules (ISMs), and Relayers. 
+      The Mailbox as the central Endpoint contract on each chain, befitting to its name, is used for dispatching and processing messages. The ISM contract defines the security model (validation logic) for a given message. 
+      Unless overridden with a custom ISM by an application, the default multisig ISM is used. The default ISM validates messages by verifying that a quorum of signatures from a set of configured validators is reached (similar to a Multisig).
+      
+      
+      The Nexus bridge
+      To initiate a token transfer, users call the transferRemote() function of the Nexus bridge token router (e.g. a contract called HypERC20). Depending on the specific token router, users' tokens may be wrapped, burned, or locked.
+      The function call eventually dispatches a message to the origin chain Mailbox, emitting a Dispatch event. Off-chain agents, such as ISM validators and Relayers, monitor the Mailbox contract events and index each dispatched message. 
+      ISM validators sign off on messages by producing attestations (called checkpoints) from the Mailbox, which commit to the Merkle root of all dispatched message IDs. 
+      On the destination chain, Relayers will then call the process() function on the receiving Mailbox, which verifies the relayed message with the ISM module. 
+      The Mailbox processing function will conclude the token transfer by calling the handle() function the destination token router contract, which will contain the logic for asset delivery to the user. 
+      Depending on the application, this can be releasing tokens from an escrow, or minting new tokens previously burnt on the origin chain.`,
+      references: [
+        {
+          title: 'Hyperlane documentation',
+          url: 'https://docs.hyperlane.xyz/',
+        },
+      ],
+      risks: [],
+    },
+    validation: {
+      name: 'Modular ISMs',
+      description:
+        'Each crosschain transaction is emitted on the origin chain and must be picked up and verified by preconfigured verifiers (Hyperlane calls these ISMs). If they agree on a message, it is considered verified and can be executed at the destination.',
+      references: [
+        {
+          title:
+            'Etherscan: Function setInterchainSecurityModule() in HypERC20Collateral (token router)',
+          url: 'https://etherscan.io/address/0xe8cc4ff9203196a90734d2c4328b83775486163c#code#F4#L73',
+        },
+        {
+          title:
+            'Etherscan: Function verify() in StaticAggregationIsm (token router)',
+          url: 'https://etherscan.io/address//0xd27fe5631533a193776A61B600809a73256eF9a7#code#F2#L48',
+        },
+      ],
+      risks: [
+        {
+          category: 'Users can be censored if',
+          text: 'the required verifiers fail to sign the transfer message.',
+        },
+        {
+          category: 'Funds can be stolen if',
+          text: 'there is no custom ISM configured and the default ISM Multisig signers submit a fraudulent transfer message.',
+        },
+        {
+          category: 'Funds can be stolen if',
+          text: 'the token router owner changes the token router or its security stack (ISM) maliciously.',
+        },
+      ],
+    },
+  },
+  contracts: {
+    addresses: {
+      [discovery.chain]: [
+        discovery.getContractDetails(
+          'Mailbox',
+          'The Mailbox contract is deployed on each chain and is used as a central Endpoint of the Hyperlane protocol to dispatch outgoing or process incoming messages.',
+        ),
+        discovery.getContractDetails(
+          'StaticAggregationIsm',
+          'This specific Interchain Security Model (ISM) contract defines the security model for the nexus bridge, unless overridden by a custom ISM.',
+        ),
+      ],
+    },
+    risks: [CONTRACTS.UPGRADE_NO_DELAY_RISK],
+  },
+}
