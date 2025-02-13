48b3aaead4c75e07c6d4aaf84e7507bad4ff589a
Luca Donno <donnoh99@gmail.com>
2025-02-13 13:49:04 +0100

    svur

--- packages/config/src/projects/layer2s/arbitrum.ts
+++ packages/config/src/projects/layer2s/arbitrum.ts
@@ -357,6 +357,7 @@ export const arbitrum: Layer2 = orbitStackL2({
     },
   ),
   stateDerivation: {
+    isUnderReview: true,
     nodeSoftware: `The rollup node (Arbitrum Nitro) consists of four parts. The base layer is the core Geth server (with minor modifications to add hooks) that emulates the execution of EVM contracts and maintains Ethereum's state and [a fork of wasmer](https://github.com/OffchainLabs/wasmer) that is used for native WASM execution. The middle layer, ArbOS, provides additional Layer 2 functionalities such as decompressing data batches, accounting for Layer 1 gas costs, and supporting cross-chain bridge functionalities. The top layer consists of node software, primarily from Geth, that handles client connections (i.e., regular RPC node). [View Code](https://github.com/OffchainLabs/nitro/)`,
     compressionScheme: `The Sequencer's batches are compressed using a general-purpose data compression algorithm known as [Brotli](https://github.com/google/brotli), configured to its highest compression setting.`,
     genesisState:
