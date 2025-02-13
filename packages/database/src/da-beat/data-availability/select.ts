7524a03d47222b7d3447ca409da6707985603b89
Antoni Pawlak <antoni.pawlak@pm.me>
2025-02-13 12:41:17 +0100

    Add daLayer field to DA database table (#6769)

--- packages/database/src/da-beat/data-availability/select.ts
+++ packages/database/src/da-beat/data-availability/select.ts
@@ -2,6 +2,7 @@ import type { DataAvailability } from '../../kysely/generated/types'
 
 export const selectDataAvailability = [
   'projectId',
+  'daLayer',
   'timestamp',
   'totalSize',
 ] as const satisfies (keyof DataAvailability)[]
