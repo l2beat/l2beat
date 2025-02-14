269c552302e80a018d2fb35687a6e6369200f66c
Antoni Pawlak <antoni.pawlak@pm.me>
2025-02-14 11:50:39 +0100

    Add DA configurationId handling to backend logic (#6785)

--- packages/database/src/da-beat/data-availability/repository.test.ts
+++ packages/database/src/da-beat/data-availability/repository.test.ts
@@ -12,37 +12,44 @@ describeDatabase(DataAvailabilityRepository.name, (db) => {
   describe.skip(DataAvailabilityRepository.prototype.upsertMany.name, () => {
     it('adds new rows', async () => {
       await repository.upsertMany([
-        record('a', START, 100n),
-        record('a', START.add(1, 'days'), 200n),
-        record('a', START.add(2, 'days'), 300n),
+        record('a', 'project-a', 'layer-a,', START, 100n),
+        record('b', 'project-b', 'layer-a,', START, 200n),
+        record('c', 'project-c', 'layer-b,', START, 300n),
+        record('c', 'project-c', 'layer-b,', START.add(1, 'days'), 400n),
       ])
 
       const results = await repository.getAll()
       expect(results).toEqualUnsorted([
-        record('a', START, 100n),
-        record('a', START.add(1, 'days'), 200n),
-        record('a', START.add(2, 'days'), 300n),
+        record('a', 'project-a', 'layer-a,', START, 100n),
+        record('b', 'project-b', 'layer-a,', START, 200n),
+        record('c', 'project-c', 'layer-b,', START, 300n),
+        record('c', 'project-c', 'layer-b,', START.add(1, 'days'), 400n),
       ])
     })
 
     it('merges on conflict', async () => {
       await repository.upsertMany([
-        record('a', START, 100n),
-        record('a', START.add(1, 'days'), 200n),
+        record('a', 'project-a', 'layer-a,', START, 100n),
+        record('b', 'project-b', 'layer-a,', START, 200n),
+        record('c', 'project-c', 'layer-b,', START, 300n),
       ])
-      await repository.upsertMany([record('a', START, 150n)])
+      await repository.upsertMany([
+        record('a', 'project-a', 'layer-a,', START, 1_000n),
+        record('b', 'project-b', 'layer-a,', START, 2_000n),
+      ])
 
       const results = await repository.getAll()
       expect(results).toEqualUnsorted([
-        record('a', START, 150n),
-        record('a', START.add(1, 'days'), 200n),
+        record('a', 'project-a', 'layer-a,', START, 1_000n),
+        record('b', 'project-b', 'layer-a,', START, 2_000n),
+        record('c', 'project-c', 'layer-b,', START, 300n),
       ])
     })
 
     it('returns number of processed records', async () => {
       const records = [
-        record('a', START, 100n),
-        record('a', START.add(1, 'days'), 200n),
+        record('a', 'project-a', 'layer-a,', START, 100n),
+        record('b', 'project-b', 'layer-a,', START, 200n),
       ]
       const result = await repository.upsertMany(records)
       expect(result).toEqual(2)
@@ -57,8 +64,8 @@ describeDatabase(DataAvailabilityRepository.name, (db) => {
   describe.skip(DataAvailabilityRepository.prototype.deleteAll.name, () => {
     it('should delete all rows', async () => {
       await repository.upsertMany([
-        record('a', START, 100n),
-        record('a', START.add(1, 'days'), 200n),
+        record('a', 'project-a', 'layer-a,', START, 100n),
+        record('b', 'project-b', 'layer-a,', START, 200n),
       ])
 
       const deleteResult = await repository.deleteAll()
@@ -71,40 +78,26 @@ describeDatabase(DataAvailabilityRepository.name, (db) => {
 
   describe.skip(DataAvailabilityRepository.prototype.getByProjectIdsAndTimeRange
     .name, () => {
-    it('should return the latest record for each project', async () => {
+    it('should return records for projects in given time range', async () => {
       await repository.upsertMany([
-        record('a', START, 100n),
-        record('a', START.add(1, 'days'), 200n),
-        record('b', START, 300n),
-        record('b', START.add(2, 'days'), 300n),
+        record('a', 'project-a', 'layer-a,', START, 100n),
+        record('a', 'project-a', 'layer-a,', START.add(1, 'days'), 1_000n),
+        record('a', 'project-a', 'layer-a,', START.add(2, 'days'), 10_000n),
+        record('b', 'project-b', 'layer-a,', START, 200n),
+        record('b', 'project-b', 'layer-a,', START.add(1, 'days'), 2_000n),
+        record('c', 'project-c', 'layer-a,', START, 300n),
       ])
 
       const results = await repository.getByProjectIdsAndTimeRange(
-        ['a', 'b'],
+        ['project-a', 'project-b'],
         [START, START.add(1, 'days')],
       )
 
       expect(results).toEqualUnsorted([
-        record('a', START, 100n),
-        record('a', START.add(1, 'days'), 200n),
-        record('b', START, 300n),
-      ])
-    })
-
-    it('should return omit project without data', async () => {
-      await repository.upsertMany([
-        record('a', START, 100n),
-        record('a', START.add(1, 'days'), 200n),
-      ])
-
-      const results = await repository.getByProjectIdsAndTimeRange(
-        ['a', 'b'],
-        [START, START.add(1, 'days')],
-      )
-
-      expect(results).toEqualUnsorted([
-        record('a', START, 100n),
-        record('a', START.add(1, 'days'), 200n),
+        record('a', 'project-a', 'layer-a,', START, 100n),
+        record('a', 'project-a', 'layer-a,', START.add(1, 'days'), 1_000n),
+        record('b', 'project-b', 'layer-a,', START, 200n),
+        record('b', 'project-b', 'layer-a,', START.add(1, 'days'), 2_000n),
       ])
     })
   })
@@ -113,28 +106,29 @@ describeDatabase(DataAvailabilityRepository.name, (db) => {
     .getLargestPosterByProjectIdsAndTimestamp.name, () => {
     it('should return the largest poster at a given timestamp', async () => {
       await repository.upsertMany([
-        record('a', START, 100n),
-        record('a', START.add(1, 'days'), 200n),
-        record('b', START, 300n),
-        record('b', START.add(1, 'days'), 400n),
+        record('a', 'project-a', 'layer-a,', START, 100n),
+        record('a', 'project-a', 'layer-a,', START.add(1, 'days'), 1_000n),
+        record('b', 'project-b', 'layer-a,', START, 200n),
+        record('b', 'project-b', 'layer-a,', START.add(1, 'days'), 2_000n),
+        record('c', 'project-c', 'layer-a,', START, 300n),
       ])
 
       const results = await repository.getLargestPosterByProjectIdsAndTimestamp(
-        ['a', 'b'],
+        ['project-a', 'project-b'],
         START,
       )
 
-      expect(results).toEqual(record('b', START, 300n))
+      expect(results).toEqual(record('b', 'project-b', 'layer-a,', START, 200n))
     })
 
     it('should return undefined if no data is available', async () => {
       await repository.upsertMany([
-        record('a', START, 100n),
-        record('b', START, 300n),
+        record('a', 'project-a', 'layer-a,', START, 100n),
+        record('b', 'project-b', 'layer-a,', START, 200n),
       ])
 
       const results = await repository.getLargestPosterByProjectIdsAndTimestamp(
-        ['a', 'b'],
+        ['project-a', 'project-b'],
         START.add(1, 'days'),
       )
 
@@ -146,14 +140,14 @@ describeDatabase(DataAvailabilityRepository.name, (db) => {
     .name, () => {
     it('should delete all rows after a given timestamp for a project', async () => {
       await repository.upsertMany([
-        record('a', START, 100n),
-        record('a', START.add(1, 'days'), 200n),
-        record('a', START.add(2, 'days'), 300n),
-        record('b', START.add(2, 'days'), 400n),
+        record('a', 'project-a', 'layer-a,', START, 100n),
+        record('a', 'project-a', 'layer-a,', START.add(1, 'days'), 100n),
+        record('a', 'project-a', 'layer-a,', START.add(2, 'days'), 100n),
+        record('b', 'project-b', 'layer-a,', START, 200n),
       ])
 
       const deleteResult = await repository.deleteByProjectFrom(
-        'a',
+        'project-a',
         START.add(1, 'days'),
       )
 
@@ -161,8 +155,8 @@ describeDatabase(DataAvailabilityRepository.name, (db) => {
 
       expect(deleteResult).toEqual(2)
       expect(results).toEqualUnsorted([
-        record('a', START, 100n),
-        record('b', START.add(2, 'days'), 400n),
+        record('a', 'project-a', 'layer-a,', START, 100n),
+        record('b', 'project-b', 'layer-a,', START, 200n),
       ])
     })
   })
@@ -171,22 +165,22 @@ describeDatabase(DataAvailabilityRepository.name, (db) => {
     .name, () => {
     it('should return all rows in a given time range for a project', async () => {
       const records = [
-        record('a', START, 100n),
-        record('a', START.add(1, 'days'), 200n),
-        record('a', START.add(2, 'days'), 300n),
-        record('b', START.add(1, 'days'), 400n),
+        record('a', 'project-a', 'layer-a,', START, 100n),
+        record('a', 'project-a', 'layer-a,', START.add(1, 'days'), 100n),
+        record('a', 'project-a', 'layer-a,', START.add(2, 'days'), 100n),
+        record('b', 'project-b', 'layer-a,', START, 200n),
       ]
 
       await repository.upsertMany(records)
 
-      const results = await repository.getByProjectAndTimeRange('a', [
+      const results = await repository.getByProjectAndTimeRange('project-a', [
         START.add(1, 'days'),
         START.add(2, 'days'),
       ])
 
       expect(results).toEqual([
-        record('a', START.add(1, 'days'), 200n),
-        record('a', START.add(2, 'days'), 300n),
+        record('a', 'project-a', 'layer-a,', START.add(1, 'days'), 100n),
+        record('a', 'project-a', 'layer-a,', START.add(2, 'days'), 100n),
       ])
     })
   })
@@ -195,20 +189,20 @@ describeDatabase(DataAvailabilityRepository.name, (db) => {
     .name, () => {
     it('should return all rows from given timestamp for a project', async () => {
       await repository.upsertMany([
-        record('a', START, 100n),
-        record('a', START.add(1, 'days'), 200n),
-        record('a', START.add(2, 'days'), 300n),
-        record('b', START.add(1, 'days'), 400n),
+        record('a', 'project-a', 'layer-a,', START, 100n),
+        record('a', 'project-a', 'layer-a,', START.add(1, 'days'), 100n),
+        record('a', 'project-a', 'layer-a,', START.add(2, 'days'), 100n),
+        record('b', 'project-b', 'layer-a,', START, 200n),
       ])
 
       const results = await repository.getByProjectIdAndFrom(
-        'a',
+        'project-a',
         START.add(1, 'days'),
       )
 
       expect(results).toEqual([
-        record('a', START.add(1, 'days'), 200n),
-        record('a', START.add(2, 'days'), 300n),
+        record('a', 'project-a', 'layer-a,', START.add(1, 'days'), 100n),
+        record('a', 'project-a', 'layer-a,', START.add(2, 'days'), 100n),
       ])
     })
   })
@@ -219,12 +213,14 @@ describeDatabase(DataAvailabilityRepository.name, (db) => {
 })
 
 function record(
+  configurationId: string,
   projectId: string,
+  daLayer: string,
   timestamp: UnixTime,
   totalSize: bigint,
-  daLayer = 'ethereum',
 ): DataAvailabilityRecord {
   return {
+    configurationId: configurationId.repeat(12),
     projectId,
     daLayer,
     timestamp,
