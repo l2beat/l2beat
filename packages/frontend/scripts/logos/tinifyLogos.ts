afbba5e289d2de5e67494a52b258c52374d223ce
HaLambur <190474635+HaLambur@users.noreply.github.com>
2025-02-15 14:49:03 +0800

    feat: sort tinified logos alphabetically to reduce merge conflicts

--- packages/frontend/scripts/logos/tinifyLogos.ts
+++ packages/frontend/scripts/logos/tinifyLogos.ts
@@ -98,9 +98,17 @@ function getTinifiedLogos() {
 function saveToJson(fileName: string, file: Buffer) {
   tinifiedLogos[fileName] = getHash(file)
 
+  // Sort the object by keys before saving
+  const sortedLogos = Object.keys(tinifiedLogos)
+    .sort()
+    .reduce((obj: Record<string, string>, key) => {
+      obj[key] = tinifiedLogos[key]!
+      return obj
+    }, {})
+
   writeFileSync(
     tinifiedLogosFile,
-    JSON.stringify(tinifiedLogos, null, 2) + '\n',
+    JSON.stringify(sortedLogos, null, 2) + '\n',
   )
 }
 
