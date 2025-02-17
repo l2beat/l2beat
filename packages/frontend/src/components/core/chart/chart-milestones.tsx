405d554bdc74f9e4fc03a6f9084001374b6710d0
torztomasz <tomasz.torz@l2beat.com>
2025-02-17 09:23:52 +0100

    fix milestones rendering

--- packages/frontend/src/components/core/chart/chart-milestones.tsx
+++ packages/frontend/src/components/core/chart/chart-milestones.tsx
@@ -47,7 +47,7 @@ export function ChartMilestones<
           <ChartMilestone
             key={data.milestone.date}
             milestone={data.milestone}
-            left={x * (width - 10)}
+            left={x * width - 10}
           />
         )
       })}
@@ -64,27 +64,43 @@ function ChartMilestone({
 }) {
   const isMobile = useIsMobile()
 
-  const icon = (
-    <svg
-      width="20"
-      height="20"
-      viewBox="0 0 20 20"
-      role="img"
-      aria-label="Milestone icon"
-      className="absolute bottom-5 cursor-pointer fill-green-700 stroke-green-500 hover:stroke-green-400 group-has-[.recharts-legend-wrapper]:bottom-9"
-      style={{ left }}
-    >
-      <rect
-        x="9.89941"
-        y="1.41421"
-        width="12"
-        height="12"
-        rx="1"
-        transform="rotate(45 9.89941 1.41421)"
-        strokeWidth="2"
-      ></rect>
-    </svg>
-  )
+  const icon =
+    milestone.type === 'general' ? (
+      <svg
+        width="20"
+        height="20"
+        viewBox="0 0 20 20"
+        role="img"
+        aria-label="Milestone icon"
+        className="absolute bottom-5 cursor-pointer fill-green-700 stroke-green-500 hover:stroke-green-400 group-has-[.recharts-legend-wrapper]:bottom-9"
+        style={{ left }}
+      >
+        <rect
+          x="9.89941"
+          y="1.41421"
+          width="12"
+          height="12"
+          rx="1"
+          transform="rotate(45 9.89941 1.41421)"
+          strokeWidth="2"
+        ></rect>
+      </svg>
+    ) : (
+      <svg
+        width="20"
+        height="20"
+        viewBox="0 0 20 20"
+        role="img"
+        aria-label="Incident icon"
+        className="absolute bottom-5 cursor-pointer fill-red-800 stroke-red-700 hover:stroke-red-600 group-has-[.recharts-legend-wrapper]:bottom-9"
+        style={{ left }}
+      >
+        <path
+          d="M2.11842 14.4966L9.13637 2.46527C9.52224 1.80374 10.4781 1.80375 10.864 2.46528L17.882 14.497C18.2708 15.1637 17.7899 16.0008 17.0182 16.0008L10.0003 16.0008L10.0002 16.0008L2.98214 16.0004C2.21039 16.0004 1.72956 15.1632 2.11842 14.4966Z"
+          strokeWidth="2"
+        ></path>
+      </svg>
+    )
 
   if (isMobile) {
     return (
