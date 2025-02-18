dcb8225f9fdcdad906e99843655d1d795ce46d62
sekuba <29250140+sekuba@users.noreply.github.com>
2025-02-18 11:32:47 +0100

    River: DA switch ethereum blobs -> celestia

--- packages/config/src/projects/layer2s/river.ts
+++ packages/config/src/projects/layer2s/river.ts
@@ -3,20 +3,25 @@ import { REASON_FOR_BEING_OTHER } from '../../common'
 import { ProjectDiscovery } from '../../discovery/ProjectDiscovery'
 import type { Layer2 } from '../../types'
 import { Badge } from '../badges'
-import { opStackL2 } from './templates/opStack'
+import { CELESTIA_DA_PROVIDER, opStackL2 } from './templates/opStack'
 
 const discovery = new ProjectDiscovery('river')
 
 export const river: Layer2 = opStackL2({
   addedAt: new UnixTime(1729867724), // 2024-10-25T17:48:44Z
-  additionalBadges: [Badge.RaaS.Caldera],
+  additionalBadges: [Badge.RaaS.Caldera, Badge.DA.Celestia],
   discovery,
-  reasonsForBeingOther: [REASON_FOR_BEING_OTHER.NO_PROOFS],
+  reasonsForBeingOther: [REASON_FOR_BEING_OTHER.NO_PROOFS, REASON_FOR_BEING_OTHER.NO_DA_ORACLE,],
+  daProvider: CELESTIA_DA_PROVIDER,
+  // celestiaDa: {
+  //   sinceBlock: 0, // Edge Case: config added @ DA Module start
+  //   namespace: 'XXXX', not found on celenium.io
+  // },
   display: {
     name: 'Towns',
     slug: 'towns',
     description:
-      'Towns is a Rollup based on the OP Stack. It is used by the Towns protocol - a protocol for building decentralized real-time messaging apps - acting as its backbone.',
+      'Towns is an Optimium based on the OP Stack. It is used by the Towns protocol - a protocol for building decentralized real-time messaging apps - acting as its backbone.',
     links: {
       websites: ['https://towns.com/'],
       apps: ['https://app.towns.com/'],
