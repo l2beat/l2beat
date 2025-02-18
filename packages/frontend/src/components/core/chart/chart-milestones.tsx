bad7fb3343dd212a009cfe6553a762489b03e13c
torztomasz <tomasz.torz@l2beat.com>
2025-02-18 09:46:36 +0100

    cleanup milestones

--- packages/frontend/src/components/core/chart/chart-milestones.tsx
+++ packages/frontend/src/components/core/chart/chart-milestones.tsx
@@ -4,6 +4,8 @@ import { useEffect, useMemo, useState } from 'react'
 import { CustomLink } from '~/components/link/custom-link'
 import { useIsMobile } from '~/hooks/use-breakpoint'
 import { useEventListener } from '~/hooks/use-event-listener'
+import { IncidentIcon } from '~/icons/incident'
+import { MilestoneIcon } from '~/icons/milestone'
 import { formatDate } from '~/utils/dates'
 import { DialogTitle } from '../dialog'
 import {
@@ -71,70 +73,22 @@ function ChartMilestone({
 }) {
   const isMobile = useIsMobile()
 
-  const icon =
-    milestone.type === 'general' ? (
-      <svg
-        width="20"
-        height="20"
-        viewBox="0 0 20 20"
-        role="img"
-        aria-label="Milestone icon"
-        className="absolute bottom-5 cursor-pointer fill-green-700 stroke-green-500 hover:stroke-green-400 group-has-[.recharts-legend-wrapper]:bottom-9"
-        style={{ left }}
-      >
-        <rect
-          x="9.89941"
-          y="1.41421"
-          width="12"
-          height="12"
-          rx="1"
-          transform="rotate(45 9.89941 1.41421)"
-          strokeWidth="2"
-        ></rect>
-      </svg>
-    ) : (
-      <svg
-        width="20"
-        height="20"
-        viewBox="0 0 20 20"
-        role="img"
-        aria-label="Incident icon"
-        className="absolute bottom-5 cursor-pointer fill-red-800 stroke-red-700 hover:stroke-red-600 group-has-[.recharts-legend-wrapper]:bottom-9"
-        style={{ left }}
-      >
-        <path
-          d="M2.11842 14.4966L9.13637 2.46527C9.52224 1.80374 10.4781 1.80375 10.864 2.46528L17.882 14.497C18.2708 15.1637 17.7899 16.0008 17.0182 16.0008L10.0003 16.0008L10.0002 16.0008L2.98214 16.0004C2.21039 16.0004 1.72956 15.1632 2.11842 14.4966Z"
-          strokeWidth="2"
-        ></path>
-      </svg>
-    )
+  const Icon = milestone.type === 'general' ? MilestoneIcon : IncidentIcon
 
   if (isMobile) {
     return (
       <Drawer>
-        <DrawerTrigger asChild>{icon}</DrawerTrigger>
+        <DrawerTrigger asChild>
+          <Icon
+            className="absolute bottom-5 scale-75 cursor-pointer group-has-[.recharts-legend-wrapper]:bottom-9"
+            style={{ left }}
+          />
+        </DrawerTrigger>
         <DrawerContent>
           <DrawerHeader>
-            <DialogTitle className="flex items-center font-bold">
-              <svg
-                width="20"
-                height="20"
-                viewBox="0 0 20 20"
-                role="img"
-                aria-label="Milestone icon"
-                className="mr-1.5 fill-green-700 stroke-green-500"
-              >
-                <rect
-                  x="9.89941"
-                  y="1.41421"
-                  width="12"
-                  height="12"
-                  rx="1"
-                  transform="rotate(45 9.89941 1.41421)"
-                  strokeWidth="2"
-                ></rect>
-              </svg>
-              {milestone.title}
+            <DialogTitle className="flex gap-1.5 font-bold">
+              <Icon className="size-[18px] shrink-0" />
+              <span>{milestone.title}</span>
             </DialogTitle>
             <DrawerDescription>
               <p className="mb-2 text-xs text-secondary">
@@ -151,29 +105,19 @@ function ChartMilestone({
 
   return (
     <Tooltip delayDuration={0}>
-      <TooltipTrigger asChild>{icon}</TooltipTrigger>
-      <TooltipContent side="top">
+      <TooltipTrigger asChild>
+        <Icon
+          className="absolute bottom-5 cursor-pointer group-has-[.recharts-legend-wrapper]:bottom-9"
+          style={{ left }}
+        />
+      </TooltipTrigger>
+      <TooltipContent side="bottom">
         <div className="mb-1 whitespace-nowrap">
           {formatDate(milestone.date.slice(0, 10))}
         </div>
-        <div className="mb-2 flex max-w-[216px] flex-wrap font-bold">
-          {milestone.type === 'incident' ? (
-            <svg
-              width="14"
-              height="14"
-              viewBox="0 0 24 24"
-              className="absolute mt-[2px] fill-red-800 stroke-red-700 md:mt-1"
-              role="img"
-            >
-              <path
-                d="M2.11842 14.4966L9.13637 2.46527C9.52224 1.80374 10.4781 1.80375 10.864 2.46528L17.882 14.497C18.2708 15.1637 17.7899 16.0008 17.0182 16.0008L10.0003 16.0008L10.0002 16.0008L2.98214 16.0004C2.21039 16.0004 1.72956 15.1632 2.11842 14.4966Z"
-                strokeWidth="2"
-              />
-            </svg>
-          ) : (
-            <div className="absolute mt-[2px] size-2.5 rotate-45 border-2 border-green-500 bg-green-700 md:mt-1"></div>
-          )}
-          <span className="ml-4 text-left">{milestone.title}</span>
+        <div className="mb-2 flex max-w-[216px] font-bold">
+          <Icon className="mt-px size-3.5 shrink-0" />
+          <span className="ml-1.5 text-left">{milestone.title}</span>
         </div>
         <div className="mb-1 max-w-[216px] text-left">
           {milestone.description}
