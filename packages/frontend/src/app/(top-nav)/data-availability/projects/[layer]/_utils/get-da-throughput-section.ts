7290b6acc0fd44131aef1d3a778d415be96623b0
maciekzygmunt <maciekzygmunt@interia.pl>
2025-02-19 11:58:56 +0200

    add details below chart

--- packages/frontend/src/app/(top-nav)/data-availability/projects/[layer]/_utils/get-da-throughput-section.ts
+++ packages/frontend/src/app/(top-nav)/data-availability/projects/[layer]/_utils/get-da-throughput-section.ts
@@ -1,4 +1,7 @@
 import type { Project } from '@l2beat/config'
+import { formatBytes } from '~/server/features/data-availability/throughput/utils/format-bytes'
+import { getDaThroughput } from '~/server/features/data-availability/utils/get-da-throuput'
+import { ps } from '~/server/projects'
 import { api } from '~/trpc/server'
 
 export async function getDaThroughputSection(
@@ -9,16 +12,32 @@ export async function getDaThroughputSection(
     projectId: project.id,
   })
 
-  const throughput = await api.da.projectChart({
+  const throughputChart = await api.da.projectChart({
     range: 'max',
     projectId: project.id,
   })
-  if (throughput.length === 0) {
-    return undefined
-  }
+  if (throughputChart.length === 0) return undefined
+
+  const projectsWithDaTracking = await ps.getProjects({
+    select: ['daTrackingConfig'],
+  })
+
+  const throughputData = await getDaThroughput(
+    [project],
+    projectsWithDaTracking,
+  )
+  const projectData = throughputData[project.id]
+
+  if (!projectData) return undefined
 
   return {
     projectId: project.id,
     throughput: project.daLayer.throughput ?? [],
+    pastDayAvgCapacityUtilization: `${projectData.pastDayAvgCapacityUtilization}%`,
+    pastDayAvgThroughput: `${projectData.pastDayAvgThroughput} MB/s`,
+    largestPoster: projectData.largestPoster
+      ? `${projectData.largestPoster.name} (${projectData.largestPoster.percentage}%)`
+      : undefined,
+    totalPosted: formatBytes(Number(projectData.totalPosted)),
   }
 }
--- packages/frontend/src/components/projects/sections/throughput/throughput-section.tsx
+++ packages/frontend/src/components/projects/sections/throughput/throughput-section.tsx
@@ -1,5 +1,6 @@
 import type { DaLayerThroughput } from '@l2beat/config'
 import type { ProjectId } from '@l2beat/shared-pure'
+import { NoDataBadge } from '~/components/badge/no-data-badge'
 import { ProjectDaThroughputChart } from '~/components/chart/data-availability/project-da-throughput-chart'
 import { ProjectSection } from '../project-section'
 import type { ProjectSectionProps } from '../types'
@@ -7,11 +8,19 @@ import type { ProjectSectionProps } from '../types'
 export interface ThroughputSectionProps extends ProjectSectionProps {
   projectId: ProjectId
   throughput: DaLayerThroughput[]
+  pastDayAvgCapacityUtilization: string
+  pastDayAvgThroughput: string
+  largestPoster: string | undefined
+  totalPosted: string
 }
 
 export function ThroughputSection({
   projectId,
   throughput,
+  pastDayAvgCapacityUtilization,
+  pastDayAvgThroughput,
+  largestPoster,
+  totalPosted,
   ...sectionProps
 }: ThroughputSectionProps) {
   return (
@@ -22,6 +31,31 @@ export function ThroughputSection({
         per day.
       </p>
       <ProjectDaThroughputChart projectId={projectId} throughput={throughput} />
+      <div className="grid grid-cols-1 gap-4 rounded-lg bg-surface-secondary p-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Detail label="Past day avg. throughput" value={pastDayAvgThroughput} />
+        <Detail
+          label="Past day avg. capacity used"
+          value={pastDayAvgCapacityUtilization}
+        />
+        <Detail label="Past day largest poster" value={largestPoster} />
+        <Detail label="Past day total data posted" value={totalPosted} />
+      </div>
     </ProjectSection>
+  )
+}
+
+function Detail({
+  label,
+  value,
+}: { label: string; value: string | undefined }) {
+  return (
+    <div className="flex flex-col items-start gap-2">
+      <span className="text-xs font-medium text-secondary">{label}</span>
+      {value ? (
+        <span className="text-lg font-bold text-primary">{value}</span>
+      ) : (
+        <NoDataBadge />
+      )}
+    </div>
   )
 }
