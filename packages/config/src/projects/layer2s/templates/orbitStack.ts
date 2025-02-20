4632ce09ab63832a20361f63349924e1fd47fab2
sekuba <29250140+sekuba@users.noreply.github.com>
2025-02-20 16:33:58 +0100

    edit and format text

--- packages/config/src/projects/layer2s/templates/orbitStack.ts
+++ packages/config/src/projects/layer2s/templates/orbitStack.ts
@@ -215,11 +215,11 @@ export function getNitroGovernance(
   All critical system smart contracts are upgradeable (can be arbitrarily changed). This permission is governed by the Arbitrum Decentralized Autonomous Organization (DAO)
   and their elected Security Council. The Arbitrum DAO controls Arbitrum One and Arbitrum Nova through upgrades and modifications to their smart contracts on Layer 1 Ethereum and the Layer 2s.
   While the DAO governs through token-weighted governance in their associated ARB token, the Security Council can directly act through
-  the Security Council smart contracts on all three chains. Although these multisigs are technically separate and connect to different target permissions,
-  their member- and threshold configuration is kept in sync by a manager contract on Arbitrum One and crosschain transactions.
+  multisigs on all three chains. Although they are technically separate and connect to different target permissions,
+  their member- and threshold configuration is kept in sync by a manager contract on Arbitrum One sending crosschain transactions.
   
   
-  Regular upgrades, Admin- and Owner actions originate from either the Arbitrum DAO or the non-emergency (proposer-) Security Council on Arbitrum One
+  Regular upgrades, Admin- and Owner actions originate from either the Arbitrum DAO or the non-emergency (Proposer-) Security Council on Arbitrum One
   and pass through multiple delays and timelocks before being executed at their destination. Contrarily, the three Emergency Security Council multisigs
   (one on each chain: Arbitrum One, Ethereum, Arbitrum Nova) can skip delays and directly access all admin- and upgrade functions of all smart contracts.
   These two general paths have the same destination: the respective UpgradeExecutor smart contract.
@@ -234,19 +234,19 @@ export function getNitroGovernance(
   When that has passed, the L1 Timelock delays for additional ${formatSeconds(
     l1TimelockDelay,
   )}. Both timelocks serve as delays during which the transparent transaction contents can be audited,
-  and even cancelled by the Emergency Security Council. Finally, the transaction can be executed, calling Admin- or Owner functions of the respective destination smart contracts
+  and, in the case of the final L1 timelock, cancelled by the Emergency Security Council. Finally, the transaction can be executed, calling Admin- or Owner restricted functions of the respective destination smart contracts
   through the UpgradeExecutor on Ethereum. If the predefined  transaction destination is Arbitrum One or -Nova, this last call is executed on L2 through the canonical bridge and the aliased address of the L1 Timelock.
-
-
+  
+  
   Operator roles like the Sequencers and Validators are managed using the same paths.
-  Sequencer changes can be delegated to a Batch Poster Manager.
-
-
+  Sequencer changes can be delegated to a Batch Poster Manager role.
+  
+  
   Transactions targeting the Arbitrum DAO Treasury can be scheduled in the ${formatSeconds(
     treasuryTimelockDelay,
   )}
   Treasury Timelock by meeting a TreasuryGovernor-enforced ${l2TreasuryQuorumPercent}% threshold of votable ARB tokens. The Security Council cannot regularly cancel
-  these transactions or schedule different ones but can overwrite them anyway by having full admin upgrade permissions for all the underlying smart contracts.`
+  these transactions or schedule different ones but can overwrite them anyway by having upgrade permissions for all the underlying smart contracts.`
 }
 
 function defaultStateValidation(
