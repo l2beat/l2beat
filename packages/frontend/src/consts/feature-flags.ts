bb2a148c1b881301ac75f5dc8ae420915d654a1e
torztomasz <tomasz.torz@l2beat.com>
2025-02-20 12:37:36 +0100

    set feature flag to true

--- packages/frontend/src/consts/feature-flags.ts
+++ packages/frontend/src/consts/feature-flags.ts
@@ -8,5 +8,5 @@ export const featureFlags = {
   stageOneRequirementsChanged: () =>
     PROJECT_COUNTDOWNS.stageChanges.lt(UnixTime.now()),
   stageSorting: env.NEXT_PUBLIC_FEATURE_FLAG_STAGE_SORTING,
-  daThroughput: env.NEXT_PUBLIC_FEATURE_FLAG_DA_THROUGHPUT,
+  daThroughput: true,
 }
