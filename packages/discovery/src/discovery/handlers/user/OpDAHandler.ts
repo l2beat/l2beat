655aea75aa47575ea44fabc65feb178fee6d31cd
Michał Podsiadły <michal.podsiadly@l2beat.com>
2025-02-21 16:41:59 +0100

    feat: rlp + simplify

--- packages/discovery/src/discovery/handlers/user/OpDAHandler.ts
+++ packages/discovery/src/discovery/handlers/user/OpDAHandler.ts
@@ -1,11 +1,10 @@
-import { assert, EthereumAddress, UnixTime } from '@l2beat/shared-pure'
-import { AbiCoder } from 'ethers/lib/utils'
+import { EthereumAddress } from '@l2beat/shared-pure'
+import { RLP } from 'ethers/lib/utils'
 import { z } from 'zod'
 import type { Transaction } from '../../../utils/IEtherscanClient'
 import type { IProvider } from '../../provider/IProvider'
 import type { Handler, HandlerResult } from '../Handler'
 import {
-  type ReferenceInput,
   generateReferenceInput,
   getReferencedName,
   resolveReference,
@@ -50,13 +49,8 @@ const BLOB_TX_TYPE = 3
 const EIGEN_DA_CONSTANTS = {
   COMMITMENT_PREFIX: '0x01',
   COMMITMENT_THIRD_BYTE: '00',
-  VERIFICATION_THRESHOLD: 0.8, // 80% success rate required
-  CONFIRM_BATCH_ADDRESS: EthereumAddress(
-    '0x454Ef2f69f91527856E06659f92a66f464C1ca4e',
-  ),
-  REFERENCE_LENGTH: 1090,
-  BATCH_ROOT_OFFSET: 208,
-  BATCH_ROOT_HASH_LENGTH: 64,
+  VERIFICATION_THRESHOLD: 1.5, // 80% success rate required
+  EIGEN_AVS: EthereumAddress('0x870679e138bcdf293b7ff14dd44b70fc97e12fc0'),
 } as const
 
 /**
@@ -122,11 +116,7 @@ export class OpStackDAHandler implements Handler {
     const isSequencerSendingBlobTx =
       hasTxs && rpcTxs.some((tx) => tx?.type === BLOB_TX_TYPE)
 
-    const isUsingEigenDA = await this.checkForEigenDA(
-      provider,
-      referenceInput,
-      lastTxs,
-    )
+    const isUsingEigenDA = await this.checkForEigenDA(provider, lastTxs)
 
     return {
       field: this.field,
@@ -140,7 +130,6 @@ export class OpStackDAHandler implements Handler {
 
   private async checkForEigenDA(
     provider: IProvider,
-    referenceInput: ReferenceInput,
     sequencerTxs: Transaction[],
   ) {
     const isUsingEigenLikeCommitments =
@@ -161,254 +150,57 @@ export class OpStackDAHandler implements Handler {
       return false
     }
 
-    const resolvedInbox = resolveReference(
-      this.definition.inboxAddress,
-      referenceInput,
+    const outgoingBatchHeaderHashes = sequencerTxs.map((tx) =>
+      decodeCommitmentRLP(tx.input),
     )
-    const inboxAddress = valueToAddress(resolvedInbox)
 
-    const { confirmBatchTxs, inboxTxs } = await fetchEigenDAVerificationData(
-      provider,
-      inboxAddress,
-    )
-    const successfulVerifications = countSuccessfulVerifications(
-      inboxTxs,
-      confirmBatchTxs,
-    )
-    const successRate = (successfulVerifications / inboxTxs.length) * 100
+    const confirmedBatchHeaderHashes =
+      await getConfirmedBatchHeaderHashes(provider)
+
+    const successfulVerifications = outgoingBatchHeaderHashes.filter((hash) =>
+      confirmedBatchHeaderHashes.includes(hash),
+    ).length
+
+    const successRate = (successfulVerifications / sequencerTxs.length) * 100
 
     return successRate >= EIGEN_DA_CONSTANTS.VERIFICATION_THRESHOLD
   }
 }
 
-async function fetchEigenDAVerificationData(
+async function getConfirmedBatchHeaderHashes(
   provider: IProvider,
-  inboxAddress: EthereumAddress,
-) {
-  const latestBlockNumber = provider.blockNumber
-  const latestBlock = await provider.getBlock(latestBlockNumber)
-  assert(latestBlock?.timestamp, 'Missing block timestamp')
-
-  const oneDayBefore = new UnixTime(latestBlock.timestamp).add(-1, 'days')
-
-  const blockNumberDayBefore = await provider.raw(
-    `optimism_eigen_reference_block_${oneDayBefore.toNumber()}_${provider.chain}`,
-    ({ etherscanClient }) =>
-      etherscanClient.getBlockNumberAtOrBefore(oneDayBefore),
+): Promise<string[]> {
+  const logs = await provider.getEvents(
+    EIGEN_DA_CONSTANTS.EIGEN_AVS,
+    'event BatchConfirmed(bytes32 indexed batchHeaderHash, uint32 batchId)',
+    [],
   )
 
-  const fromBlock = {
-    number: blockNumberDayBefore,
-    timestamp: oneDayBefore.toNumber(),
-  }
-  const toBlock = {
-    number: latestBlockNumber,
-    timestamp: latestBlock.timestamp,
-  }
-
-  const confirmBatchTxs = await getBatchConfirmTransactions(
-    provider,
-    fromBlock,
-    toBlock,
-  )
-
-  const inboxTxs = await provider.raw(
-    `optimism_eigen_inbox_tx_${inboxAddress}_${blockNumberDayBefore}_${latestBlockNumber}`,
-    ({ etherscanClient }) =>
-      etherscanClient.getTransactions(
-        inboxAddress,
-        blockNumberDayBefore,
-        latestBlockNumber,
-      ),
-  )
-  return { confirmBatchTxs, inboxTxs }
+  return logs.map((log) => log.event.batchHeaderHash.toLowerCase())
 }
 
-async function getBatchConfirmTransactions(
-  provider: IProvider,
-  fromBlock: {
-    number: number
-    timestamp: number
-  },
-  toBlock: {
-    number: number
-    timestamp: number
-  },
-) {
-  const {
-    fromBlock: fromBlockEthereum,
-    toBlock: toBlockEthereum,
-    provider: ethereumProvider,
-  } = await translateToEthereum(provider, fromBlock, toBlock)
+function decodeCommitmentRLP(inputData: string): string {
+  // strip four commitment type bytes + 0x
+  const eigenCommitment = inputData.slice(5 * 2)
 
-  const confirmBatchTxs = await ethereumProvider.raw(
-    `optimism_eigen_confirm_batch_${EIGEN_DA_CONSTANTS.CONFIRM_BATCH_ADDRESS}_${fromBlockEthereum}_${toBlockEthereum}`,
-    ({ etherscanClient }) =>
-      etherscanClient.getTransactions(
-        EIGEN_DA_CONSTANTS.CONFIRM_BATCH_ADDRESS,
-        fromBlockEthereum,
-        toBlockEthereum,
-      ),
-  )
+  const rlp = RLP.decode('0x' + eigenCommitment)
 
-  return confirmBatchTxs
-}
+  /**
+   * @see https://github.com/Layr-Labs/eigenda/blob/master/api/proto/disperser/disperser.proto
+   * @commit 733bcbe
+   */
+  // see: disperser.BlobInfo.blob_verification_proof
+  const RLP_BLOB_VERIFICATION_HEADER_IDX = 1
+  // see: disperser.BlobVerificationProof.batch_metadata
+  const RLP_BLOB_BATCH_METADATA_IDX = 2
+  // see: disperser.BlobVerificationProof.batch_metadata.batch_header_hash
+  const RLP_BLOB_BATCH_HEADER_HASH_IDX = 4
 
-// We need to align block ranges across different chains is applicable
-// since Eigen AVS lives on Ethereum
-async function translateToEthereum(
-  provider: IProvider,
-  fromBlock: {
-    number: number
-    timestamp: number
-  },
-  toBlock: {
-    number: number
-    timestamp: number
-  },
-) {
-  const ethereumProvider = provider.switchChain('ethereum', fromBlock.number)
+  const blobVerificationProof = rlp[RLP_BLOB_VERIFICATION_HEADER_IDX]
+  const blobBatchHeaderHash =
+    blobVerificationProof[RLP_BLOB_BATCH_METADATA_IDX][
+      RLP_BLOB_BATCH_HEADER_HASH_IDX
+    ]
 
-  const [ethereumFromBlock, ethereumToBlock] = await Promise.all([
-    ethereumProvider.raw(
-      `optimism_eigen_translate_block_${fromBlock.timestamp}_${provider.chain}`,
-      ({ etherscanClient }) =>
-        etherscanClient.getBlockNumberAtOrBefore(
-          new UnixTime(fromBlock.timestamp),
-        ),
-    ),
-    ethereumProvider.raw(
-      `optimism_eigen_translate_block_${toBlock.timestamp}_${provider.chain}`,
-      ({ etherscanClient }) =>
-        etherscanClient.getBlockNumberAtOrBefore(
-          new UnixTime(toBlock.timestamp),
-        ),
-    ),
-  ])
-
-  assert(ethereumFromBlock, 'Missing ethereum from block')
-  assert(ethereumToBlock, 'Missing ethereum to block')
-
-  return {
-    provider: ethereumProvider,
-    fromBlock: ethereumFromBlock,
-    toBlock: ethereumToBlock,
-  }
-}
-
-function countSuccessfulVerifications(
-  inboxTxs: Transaction[],
-  confirmBatchTxs: Transaction[],
-) {
-  let successes = 0
-
-  for (const tx of inboxTxs) {
-    try {
-      const blobInfo = parseEigenDACommitment(tx.input)
-      const hasMatch = findMatchingConfirmation(blobInfo, confirmBatchTxs)
-
-      if (hasMatch) {
-        successes++
-      }
-    } catch {
-      // Invalid commitment format, skip
-    }
-  }
-
-  return successes
-}
-
-function parseStandardCommitment(hexData: string) {
-  const batchRootStart =
-    hexData.length -
-    EIGEN_DA_CONSTANTS.REFERENCE_LENGTH +
-    EIGEN_DA_CONSTANTS.BATCH_ROOT_OFFSET
-
-  return {
-    batchRootHash: `0x${hexData.slice(
-      batchRootStart,
-      batchRootStart + EIGEN_DA_CONSTANTS.BATCH_ROOT_HASH_LENGTH,
-    )}`,
-    possibleBatchRoots: undefined,
-  }
-}
-
-function parseExtendedCommitment(hexData: string) {
-  const possibleBatchRoots = Array.from(
-    { length: hexData.length - EIGEN_DA_CONSTANTS.BATCH_ROOT_HASH_LENGTH - 1 },
-    (_, i) => ({
-      hash: `0x${hexData.slice(i, i + EIGEN_DA_CONSTANTS.BATCH_ROOT_HASH_LENGTH)}`,
-      position: i,
-    }),
-  )
-
-  return {
-    batchRootHash: `0x${hexData.slice(-64)}`,
-    possibleBatchRoots,
-  }
-}
-
-function getConfirmationRoots(confirmBatchTxs: Transaction[]): Set<string> {
-  const roots = new Set<string>()
-  for (const tx of confirmBatchTxs) {
-    try {
-      const blobHeadersRoot = decodeBatchConfirmation(tx.input)
-      if (blobHeadersRoot) {
-        roots.add(blobHeadersRoot.toLowerCase()) // Normalize case
-      }
-    } catch {
-      // Skip invalid confirmations
-    }
-  }
-  return roots
-}
-
-function findMatchingConfirmation(
-  blobInfo: ReturnType<typeof parseEigenDACommitment>,
-  confirmBatchTxs: Transaction[],
-) {
-  const confirmationRoots = getConfirmationRoots(confirmBatchTxs)
-
-  if (blobInfo.possibleBatchRoots?.length) {
-    return blobInfo.possibleBatchRoots.some(({ hash }) =>
-      confirmationRoots.has(hash.toLowerCase()),
-    )
-  }
-
-  // Check standard single root format
-  return confirmationRoots.has(blobInfo.batchRootHash.toLowerCase())
-}
-
-function parseEigenDACommitment(inputData: string) {
-  const hexData = inputData.startsWith('0x') ? inputData.slice(2) : inputData
-
-  // 1150 is arbitrary, we treat anything longer than that as extended
-  if (hexData.length > 1150) {
-    return parseExtendedCommitment(hexData)
-  }
-
-  return parseStandardCommitment(hexData)
-}
-
-function decodeBatchConfirmation(inputData: string): string | undefined {
-  if (!inputData.startsWith('0x7794965a')) {
-    // Skip invalid selectors
-    return
-  }
-
-  // Remove '0x' and function selector
-  const data = inputData.slice(10)
-  const abiCoder = new AbiCoder()
-
-  const batchHeaderType =
-    'tuple(bytes32 blobHeadersRoot, bytes quorumNumbers, bytes signedStakeForQuorums, uint32 referenceBlockNumber)'
-  const nonSignerStakesType =
-    'tuple(uint32[] nonSignerQuorumBitmapIndices, tuple(uint256,uint256)[] nonSignerPubkeys, tuple(uint256,uint256)[] quorumApks, tuple(uint256,uint256,uint256,uint256) apkG2, tuple(uint256,uint256) sigma, uint32[] quorumApkIndices, uint32[] totalStakeIndices, uint32[][] nonSignerStakeIndices)'
-
-  const decoded = abiCoder.decode(
-    [batchHeaderType, nonSignerStakesType],
-    '0x' + data,
-  )
-
-  return decoded[0].blobHeadersRoot
+  return blobBatchHeaderHash.toLowerCase()
 }
