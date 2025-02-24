912d8d5c185713c2dc4d034070fdbc75a2b8f638
Radina Talanova <nt.radina@gmail.com>
2025-02-24 15:14:00 +0200

    move to under review

--- packages/config/src/projects/layer2s/snaxchain.ts
+++ packages/config/src/projects/layer2s/snaxchain.ts
@@ -1,16 +1,18 @@
-import { UnixTime } from '@l2beat/shared-pure'
+import { EthereumAddress, UnixTime } from '@l2beat/shared-pure'
 import type { Layer2 } from '../../types'
-import { upcomingL2 } from './templates/upcoming'
+import { Badge } from '../badges'
+import { underReviewL2 } from './templates/underReview'
 
-export const snaxchain: Layer2 = upcomingL2({
+export const snaxchain: Layer2 = underReviewL2({
   id: 'snaxchain',
   capability: 'universal',
   addedAt: new UnixTime(1740099913),
+  badges: [Badge.Stack.OPStack, Badge.VM.EVM, Badge.RaaS.Conduit],
   display: {
     name: 'SNAXchain',
     slug: 'snaxchain',
     description:
-      'SNAXchain, developed by Synthetix, is a Layer-2 blockchain built on the OP Stack. It joins the Superchain ecosystem to enhance decentralized governance and streamline operations.',
+      "SNAXchain, developed by Synthetix, is a Layer-2 blockchain built on the OP Stack. It aims to enhance Synthetix V3's decentralized governance and streamline operations.",
     purposes: ['Universal'],
     category: 'Optimistic Rollup',
     stack: 'OP Stack',
@@ -19,6 +21,22 @@ export const snaxchain: Layer2 = upcomingL2({
       repositories: ['https://github.com/Synthetixio/snaxchain-config'],
       documentation: ['https://sips.synthetix.io/sips/sip-384/'],
       socialMedia: ['https://x.com/synthetix_io'],
+      explorers: ['https://explorer.snaxchain.io'],
     },
   },
+  transactionApi: {
+    type: 'rpc',
+    defaultUrl: 'https://mainnet.snaxchain.io',
+    defaultCallsPerMinute: 1500,
+    adjustCount: { type: 'SubtractOne' },
+    startBlock: 1,
+  },
+  escrows: [
+    {
+      address: EthereumAddress('0x936D881b4760D5e9b6D55b774f65c509236b4743'), // optiPortal
+      sinceTimestamp: new UnixTime(1723518000),
+      tokens: ['ETH'],
+      chain: 'ethereum',
+    },
+  ],
 })
