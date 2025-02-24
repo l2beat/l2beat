ace45214f5be1e080bf4c1804b72f24fb792a43c
ahmed9202543 <ahmed@clica.tech>
2025-02-24 21:52:05 +0800

    Add SNAXchain as upcomingL2 (#6850)
    Co-authored-by: Ahmed <183810511+ahmed9202543@users.noreply.github.com>
    Co-authored-by: Radina Talanova <nt.radina@gmail.com>

--- packages/config/src/projects/layer2s/index.ts
+++ packages/config/src/projects/layer2s/index.ts
@@ -157,6 +157,7 @@ import { shape } from './shape'
 import { shibarium } from './shibarium'
 import { silicon } from './silicon'
 import { singularityfinance } from './singularityfinance'
+import { snaxchain } from './snaxchain'
 import { socialnetwork } from './socialnetwork'
 import { solo } from './solo'
 import { soneium } from './soneium'
@@ -366,6 +367,7 @@ export const layer2s: Layer2[] = [
   scroll,
   silicon,
   singularityfinance,
+  snaxchain,
   sophon,
   soneium,
   socialnetwork,
