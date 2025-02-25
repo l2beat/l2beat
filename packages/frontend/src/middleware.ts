396bff3cc995f355a7f90c9b0b9f9643ac1c5855
torztomasz <tomasz.torz@l2beat.com>
2025-02-25 11:59:26 +0100

    remove middleware

--- packages/frontend/src/middleware.ts
+++
@@ -1,7 +0,0 @@
-import type { NextRequest } from 'next/server'
-
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  console.log('middleware', request.url)
-  return request
-}
