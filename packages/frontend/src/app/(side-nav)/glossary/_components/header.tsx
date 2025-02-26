3be72ef3f9b430ab63b5fb472a21680a31f55d47
maciekzygmunt <maciekzygmunt@interia.pl>
2025-02-26 13:50:42 +0200

    small fixes

--- packages/frontend/src/app/(side-nav)/glossary/_components/header.tsx
+++ packages/frontend/src/app/(side-nav)/glossary/_components/header.tsx
@@ -1,6 +1,8 @@
 'use client'
 
+import { useEffect, useState } from 'react'
 import { PrimaryCard } from '~/components/primary-card'
+import { useEventListener } from '~/hooks/use-event-listener'
 import { cn } from '~/utils/cn'
 import { AlphabetSelector } from './alphabet-selector/alphabet-selector'
 
@@ -9,9 +11,18 @@ export function Header<T extends { id: string }>({
 }: {
   glossaryEntries: T[]
 }) {
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  const handleScroll = () => {
+    setIsScrolled(window.scrollY > 200)
+  }
+
+  useEventListener('scroll', handleScroll)
+  useEffect(handleScroll, [])
+
   return (
     <>
-      <PrimaryCard className="relative w-full  md:rounded-b-none md:pb-0">
+      <PrimaryCard className="relative z-10 w-full md:rounded-b-none md:pb-0">
         <div className="space-y-4">
           <p className="text-lg font-bold leading-[150%]">
             Explore the L2BEAT Glossary, your comprehensive resource for
@@ -27,16 +38,22 @@ export function Header<T extends { id: string }>({
           </div>
         </div>
       </PrimaryCard>
-
+      {isScrolled && <div className="fixed top-0 h-8 w-full bg-background" />}
       <PrimaryCard
         className={cn(
           'sticky top-0 z-10 w-full bg-surface-primary py-4 transition-all',
           'md:top-6 md:rounded-t-none',
+          isScrolled && 'md:rounded-t-xl',
         )}
       >
         <AlphabetSelector entries={glossaryEntries} />
-        <div className="absolute -bottom-6 left-0 h-6 w-full bg-gradient-to-b from-background to-background/5 md:w-[calc(100%-278px)]" />
       </PrimaryCard>
+      {/* Gradient */}
+      <div
+        className={cn(
+          'sticky top-16 h-10 w-full bg-gradient-to-b from-background to-background/5 md:top-20 md:w-[calc(100%-278px)] md:via-background md:via-40%',
+        )}
+      />
     </>
   )
 }
