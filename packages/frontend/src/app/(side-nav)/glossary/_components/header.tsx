b244572edc3fc8f6eee67567d5fb03cf00ef0131
maciekzygmunt <maciekzygmunt@interia.pl>
2025-02-26 12:39:36 +0200

    use css

--- packages/frontend/src/app/(side-nav)/glossary/_components/header.tsx
+++ packages/frontend/src/app/(side-nav)/glossary/_components/header.tsx
@@ -1,8 +1,6 @@
 'use client'
 
-import { useEffect, useState } from 'react'
 import { PrimaryCard } from '~/components/primary-card'
-import { useEventListener } from '~/hooks/use-event-listener'
 import { cn } from '~/utils/cn'
 import { AlphabetSelector } from './alphabet-selector/alphabet-selector'
 
@@ -11,47 +9,33 @@ export function Header<T extends { id: string }>({
 }: {
   glossaryEntries: T[]
 }) {
-  const [isScrolled, setIsScrolled] = useState(false)
-
-  const handleScroll = () => {
-    const scrollPosition = window.scrollY
-    setIsScrolled(scrollPosition > 182)
-  }
-
-  useEffect(handleScroll, [])
-  useEventListener('scroll', handleScroll)
-
   return (
     <>
-      <PrimaryCard
-        className={cn('w-full md:rounded-b-none md:pb-0', isScrolled && '')}
-      >
-        <p className="text-lg font-bold leading-[150%]">
-          Explore the L2BEAT Glossary, your comprehensive resource for
-          understanding the terms and concepts of the L2 ecosystem. Find here
-          all essential definitions and insights.
-        </p>
-        <div
-          className={`transition-all duration-300 ${
-            isScrolled ? 'h-0 overflow-hidden opacity-0' : 'opacity-100'
-          }`}
-        >
-          <p className="text-lg font-normal leading-[150%]">
-            Designed for everyone from developers to enthusiasts, this resource
-            simplifies the complexities of L2, helping you navigate
-            Ethereum&apos;s advanced landscape with ease.
+      <PrimaryCard className="relative w-full  md:rounded-b-none md:pb-0">
+        <div className="space-y-4">
+          <p className="text-lg font-bold leading-[150%]">
+            Explore the L2BEAT Glossary, your comprehensive resource for
+            understanding the terms and concepts of the L2 ecosystem. Find here
+            all essential definitions and insights.
           </p>
+          <div className="transition-all duration-300">
+            <p className="text-lg font-normal leading-[150%]">
+              Designed for everyone from developers to enthusiasts, this
+              resource simplifies the complexities of L2, helping you navigate
+              Ethereum&apos;s advanced landscape with ease.
+            </p>
+          </div>
         </div>
       </PrimaryCard>
-      {isScrolled && <div className="sticky top-0 h-8 w-full bg-background" />}
+
       <PrimaryCard
         className={cn(
-          'relative w-full bg-surface-primary py-4 md:rounded-t-none',
-          isScrolled && 'sticky top-6 md:rounded-t-xl',
+          'sticky top-0 z-10 w-full bg-surface-primary py-4 transition-all',
+          'md:top-6 md:rounded-t-none',
         )}
       >
         <AlphabetSelector entries={glossaryEntries} />
-        <div className="absolute -bottom-6 left-0 h-6   w-full bg-gradient-to-b from-background to-background/5 md:w-[calc(100%-278px)]" />
+        <div className="absolute -bottom-6 left-0 h-6 w-full bg-gradient-to-b from-background to-background/5 md:w-[calc(100%-278px)]" />
       </PrimaryCard>
     </>
   )
