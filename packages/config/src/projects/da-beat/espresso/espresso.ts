df02d4dd24d55efe3d0f045444fa149291e0f757
vincfurc <10850139+vincfurc@users.noreply.github.com>
2025-02-26 16:25:04 +0000

    Espresso markdown fix  (#6951)

--- packages/config/src/projects/da-beat/espresso/espresso.ts
+++ packages/config/src/projects/da-beat/espresso/espresso.ts
@@ -73,9 +73,8 @@ Once the proposer sends data to HotShot node operators, they initiate Espresso D
 
 - **VID Layer**: Disperses erasure-coded data to all nodes. VID layer nodes only store chunks of the data.
 - **DA Committee Layer**: Uploads the data and commitment to a small DA committee. Every node in the committee stores the full data.
-- **CDN Layer**: Uploads the full data to a content delivery network (CDN). \  
+- **CDN Layer**: Uploads the full data to a content delivery network (CDN). 
 
-  
 Once nodes receive and store the data, they return votes to the proposer. DAVotes are votes from committee nodes storing the full data, while QuorumVotes are votes from nodes storing erasure-coded shares of the data.
 A DA certificate consists of two components, the retrievability certificate and the optimistic DAC certificate:
 
