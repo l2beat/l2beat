5b977ad65e3c0f340d48a670af80cd2d1900d111
Michał Podsiadły <michal.podsiadly@l2beat.com>
2025-02-27 16:17:44 +0100

    feat: use zod for msg events

--- packages/l2b/src/implementations/find-celestia-namespace/extract-namespaces.ts
+++ packages/l2b/src/implementations/find-celestia-namespace/extract-namespaces.ts
@@ -1,3 +1,4 @@
+import { z } from 'zod'
 import type { CelestiaTransactionResult } from './client'
 
 export function extractNamespaces(txs: CelestiaTransactionResult[]) {
@@ -16,10 +17,10 @@ function extractNamespacesFromLog(log: string) {
   }
 
   // Log is a raw string, not a JSON object
-  const json = JSON.parse(log) as MsgEventArray
+  const parsedLog = MsgEventArray.parse(JSON.parse(log))
 
   // Log itself has sub-events, now we need to find the proper ones
-  const payForBlobEvents = json
+  const payForBlobEvents = parsedLog
     .flatMap((msgEvent) => msgEvent.events)
     .filter((event) => event.type === 'celestia.blob.v1.EventPayForBlobs')
 
@@ -38,13 +39,19 @@ function extractNamespacesFromLog(log: string) {
   return namespaces.flat()
 }
 
-type MsgEventArray = {
-  msg_index: number
-  events: {
-    type: string
-    attributes: {
-      key: string
-      value: string
-    }[]
-  }[]
-}[]
+const MsgEventArray = z.array(
+  z.object({
+    msg_index: z.number(),
+    events: z.array(
+      z.object({
+        type: z.string(),
+        attributes: z.array(
+          z.object({
+            key: z.string(),
+            value: z.string(),
+          }),
+        ),
+      }),
+    ),
+  }),
+)
