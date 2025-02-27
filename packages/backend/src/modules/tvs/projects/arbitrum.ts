4ecb6583267951a266eff2de70df9f2b134268a8
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2025-02-27 10:07:42 +0100

    Update comments to not mention layer2s

--- packages/backend/src/modules/tvs/projects/arbitrum.ts
+++ packages/backend/src/modules/tvs/projects/arbitrum.ts
@@ -1,14 +1,9 @@
 import { readFileSync } from 'fs'
 import path from 'path'
-import {} from '@l2beat/shared-pure'
 import type { TvsConfig } from '../types'
 
-// const arbitrum = layer2s.find((l) => l.id === ProjectId('arbitrum'))
-// assert(arbitrum, 'Arbitrum not found')
-// assert(arbitrum.chainConfig, 'Arbitrum chain config not defined')
-// const backendProject = toBackendProject(arbitrum)
-
-// export const arbitrumConfig = mapConfig(backendProject, arbitrum.chainConfig)
+// const arbitrum = await ps.getProject({ id: ProjectId('arbitrum'), select: ['tvlConfig', 'chainConfig'] })
+// export const arbitrumConfig = mapConfig(arbitrum, arbitrum.chainConfig)
 
 const filePath = path.join(__dirname, 'arbitrum-config.json')
 export const arbitrumConfig: TvsConfig = JSON.parse(
