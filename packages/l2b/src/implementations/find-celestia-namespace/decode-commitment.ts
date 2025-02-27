ed093ab2a00dacc33d5b3a86c75fd4f7d25a1e6e
Michał Podsiadły <michal.podsiadly@l2beat.com>
2025-02-27 16:26:48 +0100

    docs: op-go da

--- packages/l2b/src/implementations/find-celestia-namespace/decode-commitment.ts
+++ packages/l2b/src/implementations/find-celestia-namespace/decode-commitment.ts
@@ -1,3 +1,6 @@
+/**
+ * @see https://github.com/celestiaorg/optimism/blob/9931de7ebf78564062383d5d680458e750a0cb52/op-celestia/da.go#L10
+ */
 export function decodeCommitment(commitment: string) {
   const byteDerivationVersion = commitment.slice(2, 4)
   const heightHex = commitment.slice(4, 20)
