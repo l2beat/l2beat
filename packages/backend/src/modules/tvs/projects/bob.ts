4df15ba319cdcf409aa3db711399ab3b05572f96
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2025-02-27 15:18:23 +0100

    Use only ProjectService in backend (#6968)
    Co-authored-by: antooni <antoni.pawlak@protonmail.com>

--- packages/backend/src/modules/tvs/projects/bob.ts
+++ packages/backend/src/modules/tvs/projects/bob.ts
@@ -2,12 +2,8 @@ import { readFileSync } from 'fs'
 import path from 'path'
 import type { TvsConfig } from '../types'
 
-// const bob = layer2s.find((l) => l.id === ProjectId('bob'))
-// assert(bob, 'Arbitrum not found')
-// assert(bob.chainConfig, 'Arbitrum chain config not defined')
-// const backendProject = toBackendProject(bob)
-
-// export const bobConfig = mapConfig(backendProject, bob.chainConfig)
+// const bob = await ps.getProject({ id: ProjectId('bob'), select: ['tvlConfig', 'chainConfig'] })
+// export const bobConfig = mapConfig(bob, bob.chainConfig)
 
 const filePath = path.join(__dirname, 'bob-config.json')
 export const bobConfig: TvsConfig = JSON.parse(readFileSync(filePath, 'utf8'))
--- packages/backend/src/modules/tvs/projects/kinto.ts
+++ packages/backend/src/modules/tvs/projects/kinto.ts
@@ -1,14 +1,9 @@
 import { readFileSync } from 'fs'
 import path from 'path'
-import {} from '@l2beat/shared-pure'
 import type { TvsConfig } from '../types'
 
-// const kinto = layer2s.find((l) => l.id === ProjectId('kinto'))
-// assert(kinto, 'Kinto not found')
-// assert(kinto.chainConfig, 'Kinto chain config not defined')
-// const backendProject = toBackendProject(kinto)
-
-// export const kintoConfig = mapConfig(backendProject, kinto.chainConfig)
+// const kinto = await ps.getProject({ id: ProjectId('kinto'), select: ['tvlConfig', 'chainConfig'] })
+// export const kintoConfig = mapConfig(kinto, kinto.chainConfig)
 
 const filePath = path.join(__dirname, 'kinto-config.json')
 export const kintoConfig: TvsConfig = JSON.parse(readFileSync(filePath, 'utf8'))
