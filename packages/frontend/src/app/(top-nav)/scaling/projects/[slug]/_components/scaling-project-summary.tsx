0e5b33f62ce548f28c15911af69b86a9d44e3695
maciekzygmunt <maciekzygmunt@interia.pl>
2025-02-27 16:20:23 +0200

    update padding

--- packages/frontend/src/app/(top-nav)/scaling/projects/[slug]/_components/scaling-project-summary.tsx
+++ packages/frontend/src/app/(top-nav)/scaling/projects/[slug]/_components/scaling-project-summary.tsx
@@ -21,7 +21,10 @@ interface Props {
 
 export function ScalingProjectSummary({ project }: Props) {
   return (
-    <FullPageHeader className="pb-0 pt-8 md:pb-8 md:pt-12">
+    <FullPageHeader
+      className="pb-0 pt-8 md:pb-8 md:pt-12"
+      contentWrapperClassName="md:px-6"
+    >
       <section id="summary" className="w-full max-md:bg-header-primary">
         <div className="flex justify-between gap-4">
           <div className="w-full space-y-4 md:space-y-6">
