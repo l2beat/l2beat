a27eb2aee38532060f3dfff60e21e7771a9f93eb
torztomasz <tomasz.torz@l2beat.com>
2025-03-05 11:18:38 +0100

    fix

--- packages/frontend/src/components/table/utils/common-project-columns/bridges-common-project-columns.tsx
+++ packages/frontend/src/components/table/utils/common-project-columns/bridges-common-project-columns.tsx
@@ -11,7 +11,7 @@ export function getBridgesCommonProjectColumns<T extends CommonProjectEntry>(
   opts?: CommonProjectColumnsOptions,
 ) {
   return [
-    ...getCommonProjectColumns(columnHelper, opts),
+    ...getCommonProjectColumns(columnHelper, getHref, opts),
     columnHelper.accessor((row) => row.name, {
       id: 'name',
       cell: (ctx) => (
