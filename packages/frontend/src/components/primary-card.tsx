1117154ffef83dc5b58d3c2b67efd4a848e9b36d
torztomasz <tomasz.torz@l2beat.com>
2025-03-05 14:50:02 +0100

    fix

--- packages/frontend/src/components/primary-card.tsx
+++ packages/frontend/src/components/primary-card.tsx
@@ -12,7 +12,7 @@ export function PrimaryCard({
   return (
     <Component
       className={cn(
-        'group/primary-card bg-surface-primary p-4 primary-card md:rounded-xl md:px-6 md:py-5',
+        'group/primary-card relative bg-surface-primary p-4 primary-card md:rounded-xl md:px-6 md:py-5',
         'data-[highlighted]:before:absolute data-[highlighted]:before:inset-0 data-[highlighted]:before:rounded-lg data-[highlighted]:before:border-2 data-[highlighted]:before:border-brand data-[highlighted]:before:shadow-[0px_4px_12px_10px] data-[highlighted]:before:shadow-[#FF5FFB40]',
         className,
       )}
