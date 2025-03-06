17f1d73e25111a4393291b9a76926d78172f5626
torztomasz <tomasz.torz@l2beat.com>
2025-03-06 10:25:29 +0100

    add chart to tvs breakdown

--- packages/frontend/src/app/(top-nav)/scaling/projects/[slug]/tvs-breakdown/page.tsx
+++ packages/frontend/src/app/(top-nav)/scaling/projects/[slug]/tvs-breakdown/page.tsx
@@ -1,9 +1,12 @@
 import { notFound } from 'next/navigation'
+import { ProjectStackedTvsChart } from '~/components/chart/tvs/stacked/project-stacked-tvs-chart'
 import { PrimaryCard } from '~/components/primary-card'
 import { env } from '~/env'
 import { getTvsBreakdownForProject } from '~/server/features/scaling/tvs/breakdown/get-tvs-breakdown-for-project'
+import { getTokensForProject } from '~/server/features/scaling/tvs/tokens/get-tokens-for-project'
 import { get7dTvsBreakdown } from '~/server/features/scaling/tvs/utils/get-7d-tvs-breakdown'
 import { ps } from '~/server/projects'
+import { api } from '~/trpc/server'
 import { getDefaultMetadata } from '~/utils/metadata'
 import { RequestTokenBox } from './_components/request-token-box'
 import { CanonicallyBridgedTable } from './_components/tables/canonically-bridged-table'
@@ -60,34 +63,55 @@ export default async function Page(props: Props) {
     notFound()
   }
 
-  const {
-    dataTimestamp,
-    breakdown: { canonical, native, external },
-  } = await getTvsBreakdownForProject(project)
+  const [
+    {
+      dataTimestamp,
+      breakdown: { canonical, native, external },
+    },
+    tokens,
+    _,
+  ] = await Promise.all([
+    getTvsBreakdownForProject(project),
+    getTokensForProject(project.id),
+    api.tvs.chart.prefetch({
+      filter: { type: 'projects', projectIds: [project.id.toString()] },
+      excludeAssociatedTokens: false,
+      range: '1y',
+    }),
+  ])
 
   return (
     <>
-      <TvsBreakdownPageHeader
-        title={project.name}
-        slug={project.slug}
-        tvs={project7dData}
-        tvsWarning={project.tvlInfo.warnings[0]}
-        tvsBreakdownTimestamp={dataTimestamp}
-      />
       <div className="md:mt-6 md:space-y-6">
+        <TvsBreakdownPageHeader
+          title={project.name}
+          slug={project.slug}
+          tvs={project7dData}
+          tvsWarning={project.tvlInfo.warnings[0]}
+          tvsBreakdownTimestamp={dataTimestamp}
+        />
+        <PrimaryCard>
+          <ProjectStackedTvsChart
+            projectId={project.id}
+            milestones={[]}
+            tokens={tokens}
+            isBridge={false}
+          />
+        </PrimaryCard>
+
         {canonical.length > 0 && (
           <PrimaryCard>
             <CanonicallyBridgedTable tokens={canonical} />
           </PrimaryCard>
         )}
-        {external.length > 0 && (
-          <PrimaryCard>
-            <ExternallyBridgedTable tokens={external} />
-          </PrimaryCard>
-        )}
         {native.length > 0 && (
           <PrimaryCard>
             <NativelyMintedTable tokens={native} />
+          </PrimaryCard>
+        )}
+        {external.length > 0 && (
+          <PrimaryCard>
+            <ExternallyBridgedTable tokens={external} />
           </PrimaryCard>
         )}
       </div>
