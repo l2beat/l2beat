7a44653c9c51fcd185ea3222422e7f1a81ec8251
torztomasz <tomasz.torz@l2beat.com>
2025-03-06 08:24:35 +0100

    handle primary card in primary card

--- packages/frontend/src/components/highlighters/primary-card-highlighter.tsx
+++ packages/frontend/src/components/highlighters/primary-card-highlighter.tsx
@@ -5,22 +5,27 @@ import { useEventListener } from '~/hooks/use-event-listener'
 export function PrimaryCardHighlighter() {
   const handleHashChange = () => {
     const hash = window.location.hash
-    if (hash) {
-      document
-        .querySelectorAll(`.primary-card[data-highlighted]`)
-        .forEach((el) => {
-          el.removeAttribute('data-highlighted')
-        })
-      const element = document.querySelector(hash)
+    if (!hash) return
 
-      if (element) {
-        element.setAttribute('data-highlighted', 'true')
-        setTimeout(() => {
-          element.removeAttribute('data-highlighted')
-        }, 3000)
-      }
-    }
+    document
+      .querySelectorAll(`.primary-card[data-highlighted]`)
+      .forEach((el) => el.removeAttribute('data-highlighted'))
+
+    const element = document.querySelector(hash)
+    if (!element) return
+
+    const parentPrimaryCard = Array.from(
+      document.querySelectorAll('.primary-card'),
+    ).find((card) => card !== element && card.contains(element))
+
+    const toHighlight = parentPrimaryCard ?? element
+    toHighlight.setAttribute('data-highlighted', 'true')
+
+    setTimeout(() => {
+      toHighlight.removeAttribute('data-highlighted')
+    }, 3000)
   }
+
   useEffect(() => {
     handleHashChange()
   }, [])
