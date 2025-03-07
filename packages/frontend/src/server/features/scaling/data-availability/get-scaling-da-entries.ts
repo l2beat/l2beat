6e88bcc0e92037caf803937aa8c9bbf50d457160
torztomasz <tomasz.torz@l2beat.com>
2025-03-07 16:01:03 +0100

    adjust sentiment

--- packages/frontend/src/server/features/scaling/data-availability/get-scaling-da-entries.ts
+++ packages/frontend/src/server/features/scaling/data-availability/get-scaling-da-entries.ts
@@ -4,6 +4,7 @@ import type {
   ProjectScalingDa,
   ProjectScalingStack,
 } from '@l2beat/config'
+import type { ProjectId } from '@l2beat/shared-pure'
 import {
   mapBridgeRisksToRosetteValues,
   mapLayerRisksToRosetteValues,
@@ -11,6 +12,14 @@ import {
 import { groupByScalingTabs } from '~/app/(side-nav)/scaling/_utils/group-by-scaling-tabs'
 import type { RosetteValue } from '~/components/rosette/types'
 import { ps } from '~/server/projects'
+import { getDaLayerRisks } from '../../data-availability/utils/get-da-layer-risks'
+import type { ProjectsEconomicSecurity } from '../../data-availability/utils/get-da-projects-economic-security'
+import { getDaProjectsEconomicSecurity } from '../../data-availability/utils/get-da-projects-economic-security'
+import {
+  getDaProjectsTvs,
+  pickTvsForProjects,
+} from '../../data-availability/utils/get-da-projects-tvs'
+import { getDaUsers } from '../../data-availability/utils/get-da-users'
 import type { ProjectChanges } from '../../projects-change-report/get-projects-change-report'
 import { getProjectsChangeReport } from '../../projects-change-report/get-projects-change-report'
 import type { CommonScalingEntry } from '../get-common-scaling-entry'
@@ -19,26 +28,45 @@ import { getProjectsLatestTvsUsd } from '../tvs/utils/get-latest-tvs-usd'
 import { compareStageAndTvs } from '../utils/compare-stage-and-tvs'
 
 export async function getScalingDaEntries() {
-  const [tvs, projectsChangeReport, projects, daLayers, daBridges] =
-    await Promise.all([
-      getProjectsLatestTvsUsd(),
-      getProjectsChangeReport(),
-      ps.getProjects({
-        select: ['statuses', 'scalingInfo', 'scalingDa', 'display'],
-        optional: ['customDa'],
-        where: ['isScaling'],
-        whereNot: ['isUpcoming', 'isArchived'],
-      }),
-      ps.getProjects({
-        select: ['daLayer'],
-      }),
-      ps.getProjects({
-        select: ['daBridge'],
-      }),
-    ])
+  const [
+    tvs,
+    projectsChangeReport,
+    projects,
+    daLayers,
+    daBridges,
+    projectsEconomicSecurity,
+  ] = await Promise.all([
+    getProjectsLatestTvsUsd(),
+    getProjectsChangeReport(),
+    ps.getProjects({
+      select: ['statuses', 'scalingInfo', 'scalingDa', 'display'],
+      optional: ['customDa'],
+      where: ['isScaling'],
+      whereNot: ['isUpcoming', 'isArchived'],
+    }),
+    ps.getProjects({
+      select: ['daLayer'],
+    }),
+    ps.getProjects({
+      select: ['daBridge'],
+    }),
+    getDaProjectsEconomicSecurity(),
+  ])
+  const dacs = projects.filter((p) => !!p.customDa) as Project<'customDa'>[]
+
+  const uniqueProjectsInUse = getDaUsers(daLayers, daBridges, dacs)
+  const tvsPerProject = await getDaProjectsTvs(uniqueProjectsInUse)
+  const getTvs = pickTvsForProjects(tvsPerProject)
+
   const entries = projects
     .map((project) => {
-      const risks = getRisks(project, daLayers, daBridges)
+      const risks = getRisks(
+        project,
+        daLayers,
+        daBridges,
+        getTvs,
+        projectsEconomicSecurity,
+      )
       return getScalingDaEntry(
         project,
         risks,
@@ -90,6 +118,11 @@ function getRisks(
   >,
   daLayers: Project<'daLayer'>[],
   daBridges: Project<'daBridge'>[],
+  getTvs: (projectIds: ProjectId[]) => {
+    latest: number
+    sevenDaysAgo: number
+  },
+  projectsEconomicSecurity: ProjectsEconomicSecurity,
 ): EntryRisks | undefined {
   if (project.customDa) {
     return {
@@ -108,9 +141,17 @@ function getRisks(
   if (!daLayerProject) {
     return undefined
   }
+  const usedProjectIds = daBridgeProject
+    ? daBridgeProject.daBridge.usedIn.map((project) => project.id)
+    : daLayerProject.daLayer.usedWithoutBridgeIn.map((project) => project.id)
+  const tvs = getTvs(usedProjectIds).latest
+  const economicSecurity = projectsEconomicSecurity[daLayerProject.id]
 
   return {
-    daLayer: mapLayerRisksToRosetteValues(daLayerProject.daLayer.risks),
+    daLayer: mapLayerRisksToRosetteValues(
+      // It's really bad and it will be replaced soon
+      getDaLayerRisks(daLayerProject.daLayer, tvs, economicSecurity),
+    ),
     daBridge: daBridgeProject
       ? mapBridgeRisksToRosetteValues(daBridgeProject.daBridge.risks)
       : mapBridgeRisksToRosetteValues({ isNoBridge: true }),
