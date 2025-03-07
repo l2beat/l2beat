b3832452947c176aa0234dfc8083d0fc6913963a
maciekzygmunt <maciekzygmunt@interia.pl>
2025-03-07 12:26:38 +0100

    merge into one

--- packages/frontend/src/components/show-more-text.tsx
+++ packages/frontend/src/components/show-more-text.tsx
@@ -11,33 +11,50 @@ import {
   DrawerTrigger,
 } from './core/drawer'
 
+interface ShowMoreTextProps {
+  children: ReactNode
+  pageTitle: string
+  textClassName?: string
+  className?: string
+}
+
 export function ShowMoreText({
   children,
   pageTitle,
   textClassName,
   className,
-}: {
-  children: React.ReactElement | string
-  pageTitle: string
-  textClassName?: string
-  className?: string
-}) {
-  const [isOverflowing, setIsOverflowing] = useState(false)
+}: ShowMoreTextProps) {
   const [expanded, setExpanded] = useState(false)
-  const textRef = useRef<HTMLDivElement>(null)
+  const [isTruncatedMobile, setIsTruncatedMobile] = useState(false)
+  const [isOverflowingDesktop, setIsOverflowingDesktop] = useState(false)
+
+  const mobileContentRef = useRef<HTMLDivElement>(null)
+  const desktopContentRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    const checkOverflow = () => {
-      const element = textRef.current
-      if (element) {
-        setIsOverflowing(element.scrollWidth > element.clientWidth)
+    const checkMobileTruncation = () => {
+      if (mobileContentRef.current) {
+        const { scrollHeight, offsetHeight } = mobileContentRef.current
+        setIsTruncatedMobile(scrollHeight > offsetHeight)
       }
     }
 
-    checkOverflow()
+    checkMobileTruncation()
+    window.addEventListener('resize', checkMobileTruncation)
+    return () => window.removeEventListener('resize', checkMobileTruncation)
+  }, [children])
 
-    window.addEventListener('resize', checkOverflow)
-    return () => window.removeEventListener('resize', checkOverflow)
+  useEffect(() => {
+    const checkDesktopOverflow = () => {
+      if (desktopContentRef.current) {
+        const { scrollWidth, clientWidth } = desktopContentRef.current
+        setIsOverflowingDesktop(scrollWidth > clientWidth)
+      }
+    }
+
+    checkDesktopOverflow()
+    window.addEventListener('resize', checkDesktopOverflow)
+    return () => window.removeEventListener('resize', checkDesktopOverflow)
   }, [children])
 
   const toggleExpand = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -63,96 +80,57 @@ export function ShowMoreText({
 
   return (
     <div className={cn('w-full', className)}>
-      <TruncatedText
-        pageTitle={pageTitle}
-        className="md:hidden"
-        textClassName={textClassName}
-      >
-        {children}
-      </TruncatedText>
-      <div
-        ref={textRef}
-        className={cn(
-          'relative hidden truncate pr-[75px] md:block',
-          textClassName,
-        )}
-      >
-        {children}
-        {isOverflowing && (
-          <button
-            className={cn(
-              'absolute right-0 top-0 cursor-pointer pl-1 underline',
-              textClassName,
-            )}
-            onClick={toggleExpand}
-          >
-            Show more
-          </button>
-        )}
+      {/* Mobile  */}
+      <div className="md:hidden">
+        <div
+          ref={mobileContentRef}
+          className={cn('relative line-clamp-3', textClassName)}
+        >
+          {children}
+          {isTruncatedMobile && (
+            <Drawer>
+              <DrawerTrigger className="absolute bottom-0 right-0 inline-flex items-center bg-background pl-1">
+                <span>...</span>
+                <div
+                  className={cn('ml-1 cursor-pointer underline', textClassName)}
+                >
+                  Show more
+                </div>
+              </DrawerTrigger>
+              <DrawerContent className="pb-8">
+                <DrawerHeader>
+                  <DrawerTitle className="text-lg font-semibold text-primary">
+                    {pageTitle}
+                  </DrawerTitle>
+                  <DrawerDescription className={textClassName}>
+                    {children}
+                  </DrawerDescription>
+                </DrawerHeader>
+              </DrawerContent>
+            </Drawer>
+          )}
+        </div>
       </div>
-    </div>
-  )
-}
 
-interface TruncatedTextProps {
-  children: ReactNode
-  pageTitle: string
-  className?: string
-  textClassName?: string
-}
-
-function TruncatedText({
-  children,
-  pageTitle,
-  textClassName,
-  className,
-}: TruncatedTextProps) {
-  const [isTruncated, setIsTruncated] = useState(false)
-  const contentRef = useRef<HTMLDivElement>(null)
-
-  useEffect(() => {
-    const checkTruncation = (): void => {
-      if (contentRef.current) {
-        const { offsetHeight, scrollHeight } = contentRef.current
-        setIsTruncated(scrollHeight > offsetHeight)
-      }
-    }
-
-    checkTruncation()
-
-    window.addEventListener('resize', checkTruncation)
-    return () => window.removeEventListener('resize', checkTruncation)
-  }, [children])
-
-  return (
-    <div className={cn(className)}>
-      <div
-        ref={contentRef}
-        className={cn('relative line-clamp-3', textClassName)}
-      >
-        {children}
-        {isTruncated && (
-          <Drawer>
-            <DrawerTrigger className="absolute bottom-0 right-0 inline-flex items-center bg-background pl-1">
-              <span>...</span>
-              <button
-                className={cn('ml-1 cursor-pointer underline', textClassName)}
-              >
-                Show more
-              </button>
-            </DrawerTrigger>
-            <DrawerContent className="pb-8">
-              <DrawerHeader>
-                <DrawerTitle className="text-lg font-semibold text-primary">
-                  {pageTitle}
-                </DrawerTitle>
-                <DrawerDescription className={textClassName}>
-                  {children}
-                </DrawerDescription>
-              </DrawerHeader>
-            </DrawerContent>
-          </Drawer>
-        )}
+      {/* Desktop  */}
+      <div className="hidden md:block">
+        <div
+          ref={desktopContentRef}
+          className={cn('relative truncate pr-[75px]', textClassName)}
+        >
+          {children}
+          {isOverflowingDesktop && (
+            <button
+              className={cn(
+                'absolute right-0 top-0 cursor-pointer pl-1 underline',
+                textClassName,
+              )}
+              onClick={toggleExpand}
+            >
+              Show more
+            </button>
+          )}
+        </div>
       </div>
     </div>
   )
