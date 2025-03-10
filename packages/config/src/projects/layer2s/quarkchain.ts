43620db1637b6bc84b37368909d05e314f7e6a0a
carl <carl587@users.noreply.github.com>
2025-03-10 20:23:26 +0800

    Add QuarkChain

---
+++ packages/config/src/projects/layer2s/quarkchain.ts
@@ -0,0 +1,32 @@
+import { UnixTime } from '@l2beat/shared-pure'
+import type { Layer2 } from '../../types'
+import { upcomingL2 } from './templates/upcoming'
+
+export const quarkchain: Layer2 = upcomingL2({
+  id: 'quarkchain',
+  capability: 'universal',
+  addedAt: new UnixTime(1741356345),
+  display: {
+    name: 'QuarkChain',
+    slug: 'quarkchain',
+    description:
+      'QuarkChain is a Fully Decentralized L2 Delivering Unmatched Scalability, Security, and On-Chain Verifiable Storage - The Ultimate Infrastructure for AI and dApps.',
+    purposes: ['AI'],
+    category: 'Optimistic Rollup',
+    links: {
+      websites: ['https://quarkchain.io/'],
+      apps: [
+        'https://bridge.beta.testnet.l2.quarkchain.io',
+        'https://qkc-l2-faucet.eth.sep.w3link.io',
+      ],
+      documentation: ['https://docs.quarkchain.io/'],
+      explorers: ['https://explorer.beta.testnet.l2.quarkchain.io/'],
+      socialMedia: [
+        'https://x.com/quark_chain',
+        'https://discord.com/invite/GbkGhY3qkh',
+        'https://quarkchainio.medium.com/',
+        'https://t.me/quarkchainio',
+      ],
+    },
+  },
+})
