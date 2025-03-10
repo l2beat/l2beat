5e2e00d145da2e873ce79b9e116ce1c053ea1742
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2025-03-10 15:27:50 +0100

    Move every project into its own directory

--- packages/config/src/projects/near/near.ts
+++ packages/config/src/projects/near/near.ts
@@ -1,185 +1,170 @@
-import { ProjectId, UnixTime } from '@l2beat/shared-pure'
-import { DaEconomicSecurityRisk, DaFraudDetectionRisk } from '../../common'
-import { linkByDA } from '../../common/linkByDA'
-import type { BaseProject } from '../../types'
+import { EthereumAddress, ProjectId, UnixTime } from '@l2beat/shared-pure'
+import { utils } from 'ethers'
 
-export const near: BaseProject = {
-  id: ProjectId('near-da'), // TODO: merge with near bridge in the future
-  slug: 'near',
-  name: 'NEAR DA',
-  shortName: undefined,
-  addedAt: UnixTime.fromDate(new Date('2024-09-03')),
-  // tags
-  isDaLayer: true,
-  // data
-  statuses: {
-    yellowWarning: undefined,
-    redWarning: undefined,
-    isUnderReview: false,
-    isUnverified: false,
-  },
+import { CONTRACTS } from '../../common'
+import { BRIDGE_RISK_VIEW } from '../../common'
+import { ProjectDiscovery } from '../../discovery/ProjectDiscovery'
+import type { Bridge } from '../../internalTypes'
+
+const discovery = new ProjectDiscovery('near')
+
+const threshold = discovery.getContractValue<number>(
+  'BridgeAdminMultisig',
+  '$threshold',
+)
+
+const owners: string[] = discovery.getContractValue<string[]>(
+  'BridgeAdminMultisig',
+  '$members',
+)
+
+const size = owners.length
+const adminThresholdString = `${threshold} / ${size}`
+
+const lockDurationSeconds = discovery.getContractValue<number>(
+  'NearBridge',
+  'lockDuration',
+)
+
+const lockDurationHours = lockDurationSeconds / 3600
+
+const lockRequirementInWei = discovery.getContractValue<number>(
+  'NearBridge',
+  'lockEthAmount',
+)
+
+export const near: Bridge = {
+  type: 'bridge',
+  id: ProjectId('near'),
+  addedAt: UnixTime(1662628329), // 2022-09-08T09:12:09Z
   display: {
-    description: `NEAR's Data Availability Layer (NEAR DA) leverages the sharded architecture of the NEAR Protocol to provide a modular data availability layer for layer 2 solutions.`,
+    name: 'Rainbow Bridge',
+    slug: 'near',
     links: {
-      websites: ['https://near.org/', 'https://nuff.tech/'],
-      documentation: [
-        'https://docs.near.org/build/chain-abstraction/data-availability',
-      ],
-      repositories: [
-        'https://github.com/near',
-        'https://github.com/Nuffle-Labs/data-availability',
-      ],
-      explorers: ['https://nearblocks.io/'],
-      socialMedia: [
-        'https://x.com/NEARProtocol',
-        'https://discord.com/invite/zfhfRpaM4m',
-      ],
+      websites: ['https://near.org/'],
+      explorers: ['https://explorer.near.org/', 'https://aurorascan.dev/'],
+      apps: ['https://rainbowbridge.app/'],
+      repositories: ['https://github.com/aurora-is-near/rainbow-bridge'],
+      socialMedia: ['https://twitter.com/auroraisnear'],
     },
-    badges: [],
+    description:
+      'Rainbow bridge is a light client based bridge between NEAR/AURORA and Ethereum that allows for asset and data movement between these chains. For better gas efficiency from NEAR to Ethereum, it leverages optimistic validation, which adds some trust assumption and latency.',
+    category: 'Token Bridge',
   },
-  daLayer: {
-    type: 'Public Blockchain',
-    systemCategory: 'public',
-    consensusAlgorithm: {
-      name: 'Nightshade',
-      description: `Nightshade is a sharding-based, Proof-of-Stake (PoS) consensus protocol enabling parallel transaction processing.
-    Near Nightshade has one single main chain producing blocks. Main chain blocks do not contain actual transactions, but they include one chunk header for each shard, where
-    a chunk is the equivalent of a block in a standard, non-shared blockchain. At the beginning of the epoch, both the block and chunk production schedule
-    are randomly generated. For each block on the main chain, and for every shard, one of the assigned chunk producers is responsible to produce the part of the main chain block
-    related to the shard, and share the chunk header with the network. Finality is determined by the NFG (Nightshade finality gadget), and after 2 consecutive blocks are built on the same fork the t-2 block is considered final.
-    Reverting a finalized block will require at least 1/3 of the total stake to be slashed.`, // this is not shown anywhere in the UI, and maybe we don't need it
-      blockTime: 1.2, // seconds average
-      consensusFinality: 2.4, // NFG (Nightshade finality gadget, after 2 consecutive blocks are built on the same fork we consider the t-2 block final, thus transactions belonging to t-2 are final )
-      unbondingPeriod: 86400 * 2, // up to 48 hours
-    },
-    technology: {
-      description: `
-## Architecture
-
-![Near architecture](/images/da-layer-technology/near/architecture.png#center)
-
-## Near Nightshade
-
-### Consensus
-NEAR's Nightshade consensus operates a Proof-of-Stake (PoS) system that enables parallel processing of transactions through a sharded architecture. As with any PoS system, validators are required to lock a stake to be eligible for block production and attestations.
-The main differentiator of the NEAR blockchain is that its blocks do not contain actual transactions but rather block headers of separate blockchains, known as shards.
-The ***main chain*** can contain many shards, and the current NEAR implementation supports 6 shards.
-
-
-![Near Shards](/images/da-layer-technology/near/nearShards.png#center)\n
-
-
-Each shard is composed of chunks, which are equivalent to standard blockchain blocks.
-Essentially, the state of the main chain is split into n shards, and only the shards block headers end up being part of the main chain.
-
-
-![Near Chunks](/images/da-layer-technology/near/nearChunks.png#center)\n
-
-
-Becoming a block producer requires locking (staking) a certain amount of tokens, currently around 11,000 NEAR. Staking operates through a threshold PoS mechanism, where a user’s stake must exceed the protocol's seat price—determined by the total NEAR tokens staked by other validators—in order to become a validator. The largest stakers at the beginning of a particular epoch are selected as block producers for that epoch. Each block producer is randomly assigned a certain number of shards to manage.  
-Before the epoch starts, the block producer downloads the state of the shard(s) they are assigned to (they have 1 epoch to complete this download). Throughout the epoch, they collect transactions that affect their assigned shard(s) and apply them to the state. NEAR nodes have an automatic 'garbage collection' routine that deletes the state of previous shards after five epochs, freeing up unused storage.
-Within an epoch (12 hours), the main chain block and shard block production schedule is determined by a randomness seed generated at the beginning of the epoch. For each block height, a main chain block producer is assigned.  
-Validators participate in several validation rounds within the epoch. For each round, one validator in each shard is chosen to be the chunk producer, and one validator from the entire set is chosen to be the block producer. Validators can serve as both block and chunk producers, but they maintain separate stakes for these roles.
-The shard block producer is responsible for producing the part of the block related to their shard, known as a chunk. The chunk contains the list of transactions for the shard to be included in the block, as well as the Merkle root of the resulting state. Each main chain block contains either one or zero chunks per shard, depending on whether the shard can keep up with the main chain block production speed.
-
-
-![Near Validators](/images/da-layer-technology/near/nearValidators.png#center)\n
-
-
-A block on the main chain will only contain a small header of the chunk, composed of the Merkle root of all applied transactions and the Merkle root of the final state. When a block producer generates a chunk, they also produce an associated state witness, which is necessary for executing the chunk.
-
-Chunk producers maintain the full state of their shard. Validators, on the other hand, do not maintain the state of any shard locally; instead, they download and verify all the block headers. They validate chunks using the state witness created by the chunk producers.
-
-### Data Availability
-Since only shard chunk producers maintain the shard state (while main chain validators do not), and main chain block producers need to attest to the chunk headers, data availability of the shard data must be ensured.
-
-After a block producer creates a chunk, they generate an erasure-coded version of it using Reed-Solomon encoding. The extended chunk is then split into multiple parts, which are distributed to all main chain validators.
-
-![Near Erasure Coding](/images/da-layer-technology/near/nearErasureCoding.png)\n
-
-A block producer will not process a main chain block if they do not have the corresponding chunk sample for at least one chunk included in the block, or if they are unable to reconstruct the entire chunk for any shard for which they maintain the state.
-
-For a particular chunk to be considered available, it is sufficient that [mainChainBlockProducer/nOfShards] + 1 of the block producers possess their respective parts and are able to serve them. As long as the number of malicious actors does not exceed one-third of the validators (block producers), any chain built by more than half of the block producers will have all its chunks available.
-
-![Near Erasure Distribution](/images/da-layer-technology/near/nearErasureDistribution.png)\n
-
-
-### Finality
-Finality is determined by a modified Doomslug finality gadget. A block is considered final after two consecutive blocks are built on the same fork, making the block that is two blocks behind (t-2) final. Reverting a finalized block would require slashing at least one-third of the total stake.
-
-## L2s Data Availability
-A rollup can utilize a dedicated Data Availability (DA) smart contract on a NEAR shard, known as a Blob Store contract, where it posts data as standard NEAR transactions. All transactions are converted into Receipts, and depending on their actions, some receipts may be processed over two blocks.
-Regarding data retrieval, full nodes prune Receipts after 3 epochs (approximately 36 hours). Once the pruning window expires, the data remains accessible only through archive nodes.
-  `,
-      references: [
-        {
-          title: 'Near Nightshade Consensus',
-          url: 'https://pages.near.org/downloads/Nightshade.pdf',
-        },
-        {
-          title: 'Near Doomslug Finality Gadget',
-          url: 'https://discovery-domain.org/papers/doomslug.pdf',
-        },
-        {
-          title: 'Near documentation',
-          url: 'https://dev.near.org/documentation/',
-        },
-        {
-          title: 'Near Core - Architecture',
-          url: 'https://near.github.io/nearcore/',
-        },
-        {
-          title: 'Blob Store contract - Nuffle Labs',
-          url: 'https://github.com/Nuffle-Labs/data-availability/blob/5026b81aa5d941aaf4dd1b23bc219b9150e84405/contracts/blob-store/src/lib.rs',
-        },
-      ],
-      risks: [
-        {
-          category: 'Funds can be lost if',
-          text: `a dishonest majority of Near validators finalizes an unavailable block.`,
-        },
-      ],
-    },
-    usedWithoutBridgeIn: linkByDA({
-      layer: ProjectId('near-da'),
-      bridge: undefined,
-    }),
-    pruningWindow: 43200 * 3, // minimum 3 epochs (12 hours each), claimed in practice around 5 epochs (due to nodes garbage collection)
-    throughput: [
+  config: {
+    associatedTokens: ['AURORA'],
+    escrows: [
       {
-        size: 16777216, // 16 MiB , 4MiB per 4 shard
-        frequency: 1, // 16 MiB/s
-        sinceTimestamp: 1587513600, // 2020-04-22
+        address: EthereumAddress('0x23Ddd3e3692d1861Ed57EDE224608875809e127f'),
+        sinceTimestamp: UnixTime(1615826693),
+        tokens: '*',
+        chain: 'ethereum',
+      },
+      {
+        address: EthereumAddress('0x6BFaD42cFC4EfC96f529D786D643Ff4A8B89FA52'),
+        sinceTimestamp: UnixTime(1624579200),
+        tokens: ['ETH'],
+        chain: 'ethereum',
       },
     ],
-    risks: {
-      economicSecurity: DaEconomicSecurityRisk.OnChainQuantifiable,
-      fraudDetection: DaFraudDetectionRisk.NoFraudDetection,
+  },
+  riskView: {
+    validatedBy: {
+      value: 'Destination Chain',
+      description:
+        'Transfers out of the bridge are validated using Optimistic Light Client of Near Chain on Ethereum. Transfers into NEAR are validated by Ethereum light client on NEAR side.',
+      sentiment: 'warning',
     },
-    economicSecurity: {
-      name: 'Near',
-      token: {
-        symbol: 'NEAR',
-        decimals: 24,
-        coingeckoId: 'near',
-      },
+    sourceUpgradeability: {
+      value: 'Yes',
+      description: `Bridge cannot be upgraded but ${adminThresholdString} Admin Multisig can move all funds out of the bridge via admin functions with no warning.`,
+      sentiment: 'bad',
+    },
+    destinationToken: {
+      ...BRIDGE_RISK_VIEW.CANONICAL_OR_WRAPPED,
+      description:
+        BRIDGE_RISK_VIEW.CANONICAL_OR_WRAPPED.description +
+        ' Tokens minted on AURORA do not appear to be verified on explorer.aurora.dev.', // TBC
+      sentiment: 'bad',
     },
   },
-  milestones: [
-    {
-      title: 'Near mainnet launch',
-      url: 'https://near.org/blog/near-mainnet-genesis',
-      date: '2020-04-22T00:00:00Z',
-      description: 'Near mainnet launches.',
-      type: 'general',
-    },
-    {
-      title: 'Near Foundation launches Near DA',
-      url: 'https://medium.com/nearprotocol/near-foundation-launches-near-da-to-offer-secure-cost-effective-data-availability-for-eth-rollups-2d26beeb4cd8',
-      date: '2023-11-08T00:00:00Z',
+  technology: {
+    canonical: true,
+    destination: ['Near', 'Aurora'],
+    principleOfOperation: {
+      name: 'Principle of operation',
       description:
-        'Near Foundation proposes Near DA as a data availability layer for Ethereum L2s.',
-      type: 'general',
+        'Rainbow is a Token Bridge that allows transferring assets and passing arbitrary messages between Ethereum, NEAR, and Aurora chains. During token transfers, Rainbow Bridge locks tokens in the escrow contracts on Ethereum and mints tokens on the Aurora or NEAR networks. When bridging back to Ethereum, tokens are burned on Aurora / NEAR and then released from the escrow on Ethereum.',
+      references: [],
+      risks: [],
     },
-  ],
+    validation: {
+      name: 'Both inbound and outbound transfers are verified by the light client',
+      description: `Near Rainbow bridge implements a light client for both inbound and outbound transfers. For inbound transfers, checkpoints of NEAR state are submitted every ${lockDurationHours} hours. \
+          These are optimistically assumed to be signed by 2/3 of Near Validators. The signatures are not immediately verified by Ethereum due to a different signature scheme \
+          used on NEAR and - as a result - very high gas cost on Ethereum. Checkpoints relayers are required to lock ${utils.formatEther(
+            lockRequirementInWei,
+          )} ETH in order to submit block headers. If signatures are found to be invalid, checkpoints can be permissionlessly challenged and the relayers' bond is slashed. \
+          Challengers can specify an address to receive half of the slashed bond. \
+          Users can withdraw funds by submitting a Merkle proof of a burn event against the checkpoint. \
+          For outbound transfers, Ethereum light client is implemented on NEAR and a Merkle proof of a lock event must be presented.`,
+      references: [],
+      risks: [
+        {
+          category: 'Funds can be stolen if',
+          text: 'incorrect checkpoint is submitted and nobody challenges it.',
+        },
+        {
+          category: 'Funds can be stolen if',
+          text: 'bridge administrator removes funds from the bridge escrow.',
+        },
+      ],
+      isIncomplete: true,
+    },
+    destinationToken: {
+      name: 'Destination tokens are not verified',
+      description:
+        'Tokens transferred end up as ERC20 tokens on AURORA or NEAR and they do not appear to be verified.',
+      references: [],
+      risks: [
+        {
+          category: 'Funds can be stolen if',
+          text: 'destination token contract is maliciously upgraded.',
+        },
+      ],
+      isIncomplete: true,
+    },
+  },
+  contracts: {
+    addresses: {
+      [discovery.chain]: [
+        discovery.getContractDetails('NearBridge', {
+          description: 'Contract storing Near state checkpoints.',
+        }),
+        discovery.getContractDetails('NearProver', {
+          description:
+            'Contract verifying merkle proofs, used for withdrawals.',
+        }),
+        discovery.getContractDetails(
+          'ERC20Locker',
+          'Escrow contract for ERC20 tokens.',
+        ),
+        discovery.getContractDetails(
+          'EthCustodian',
+          'Escrow contract for ETH tokens.',
+        ),
+      ],
+    },
+    risks: [CONTRACTS.UPGRADE_NO_DELAY_RISK],
+  },
+  permissions: {
+    [discovery.chain]: {
+      actors: [
+        discovery.getMultisigPermission(
+          'BridgeAdminMultisig',
+          'Admin can pause/unpause contracts, modify contracts storage and delegate call to any contract. This allows for any arbitrary action including removal of all tokens from escrows.',
+        ),
+      ],
+    },
+  },
 }
