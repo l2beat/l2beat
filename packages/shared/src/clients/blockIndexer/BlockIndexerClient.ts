0199eb5227b0b0e750f1c2b56e338728b5cb01d8
VolodymyrBg <aqdrgg19@gmail.com>
2025-03-12 15:24:46 +0200

    Update BlockIndexerClient.ts

--- packages/shared/src/clients/blockIndexer/BlockIndexerClient.ts
+++ packages/shared/src/clients/blockIndexer/BlockIndexerClient.ts
@@ -1,7 +1,7 @@
 import type { Logger, RateLimiter } from '@l2beat/backend-tools'
-import { UnixTime } from '@l2beat/shared-pure'
+import { UnixTime, type json } from '@l2beat/shared-pure'
 import type { HttpClient } from '../http/HttpClient'
-import { RetryHandler, type RetryHandlerVariant } from '../../tools'
+import { ClientCore, type ClientCoreDependencies } from '../ClientCore'
 import { BlockTimestampResponse, EtherscanResponse } from './types'
 
 interface EtherscanOptions {
@@ -17,7 +17,11 @@ interface BlockscoutOptions {
   chain: string
 }
 
-export class BlockIndexerClient {
+interface Dependencies extends ClientCoreDependencies {
+  options: EtherscanOptions | BlockscoutOptions
+}
+
+export class BlockIndexerClient extends ClientCore {
   chain: string
 
   // If you ask for a timestamp
@@ -25,27 +29,14 @@ export class BlockIndexerClient {
   // API will return the error
   private readonly binTimeWidth
   private readonly maximumCallsForBlockTimestamp
-  private readonly retryHandler: RetryHandler
+  private readonly options: EtherscanOptions | BlockscoutOptions
 
-  constructor(
-    private readonly httpClient: HttpClient,
-    private readonly rateLimiter: RateLimiter,
-    private readonly options: EtherscanOptions | BlockscoutOptions,
-    logger?: Logger,
-    retryStrategy: RetryHandlerVariant = 'RELIABLE',
-  ) {
-    this.call = this.rateLimiter.wrap(this.call.bind(this))
-    this.binTimeWidth = options.type === 'etherscan' ? 10 : 1
-    this.maximumCallsForBlockTimestamp = options.type === 'etherscan' ? 3 : 10
-    this.chain = options.chain
-    this.retryHandler = logger 
-      ? RetryHandler.create(retryStrategy, logger)
-      : new RetryHandler({
-          logger: { warn: () => {} } as Logger,
-          maxRetries: 3,
-          initialRetryDelayMs: 1000,
-          maxRetryDelayMs: 10000,
-        })
+  constructor(private readonly $: Dependencies) {
+    super($)
+    this.options = $.options
+    this.binTimeWidth = this.options.type === 'etherscan' ? 10 : 1
+    this.maximumCallsForBlockTimestamp = this.options.type === 'etherscan' ? 3 : 10
+    this.chain = this.options.chain
   }
 
   static create(
@@ -53,12 +44,14 @@ export class BlockIndexerClient {
     rateLimiter: RateLimiter,
     options: EtherscanOptions | BlockscoutOptions,
   ) {
-    return new BlockIndexerClient(
-      services.httpClient, 
-      rateLimiter, 
+    return new BlockIndexerClient({
+      http: services.httpClient,
+      logger: services.logger,
+      sourceName: options.chain,
+      callsPerMinute: rateLimiter.callsPerMinute,
+      retryStrategy: 'RELIABLE',
       options,
-      services.logger
-    )
+    })
   }
 
   // There is a case when there is not enough activity on a given chain
@@ -107,26 +100,31 @@ export class BlockIndexerClient {
     }
     const url = `${this.options.url}?${query.toString()}`
 
-    const response = await this.retryHandler.retry(() => 
-      this.httpClient.fetch(url, {})
-    )
+    const response = await this.fetch(url, {})
 
+    return response
+  }
+
+  override validateResponse(response: json): {
+    success: boolean
+    message?: string
+  } {
     const etherscanResponse = EtherscanResponse.safeParse(response)
 
     if (etherscanResponse.success === false) {
-      const message = `Invalid Etherscan response [${JSON.stringify(response)}] for request [${url}].`
-      throw new TypeError(message)
+      return {
+        success: false,
+        message: `Invalid Etherscan response [${JSON.stringify(response)}]`,
+      }
     }
 
     if (etherscanResponse.data.message === 'NOTOK') {
-      throw new Error(JSON.stringify(etherscanResponse.data.result))
+      return {
+        success: false,
+        message: JSON.stringify(etherscanResponse.data.result),
+      }
     }
 
-    return etherscanResponse.data.result
+    return { success: true }
   }
-}
-
-// This is needed for type checking in the constructor
-interface HttpClient {
-  fetch(url: string, init: Record<string, unknown>): Promise<json>
 }
