3692eecaa2ceadaef4312d44d7b75b5ca22fff5d
maciekzygmunt <maciekzygmunt@interia.pl>
2025-03-12 12:59:20 +0200

    small change

--- packages/frontend/src/components/show-more-text.tsx
+++ packages/frontend/src/components/show-more-text.tsx
@@ -53,9 +53,9 @@ export function ShowMoreText({
     setExpanded(!expanded)
   }
 
-  if (expanded) {
-    return (
-      <div className={cn('w-full', className)}>
+  return (
+    <div className={cn('w-full', className)}>
+      {expanded ? (
         <div className={textClassName}>
           {children}
           <button
@@ -65,63 +65,59 @@ export function ShowMoreText({
             Show less
           </button>
         </div>
-      </div>
-    )
-  }
-
-  return (
-    <div className={cn('w-full', className)}>
-      <div className="relative">
-        <div
-          ref={contentRef}
-          className={cn(
-            'md:truncate md:pr-[75px]',
-            'line-clamp-2 md:line-clamp-none',
-            textClassName,
-          )}
-        >
-          {children}
-        </div>
-
-        {/* Desktop Show More */}
-        {isOverflowing && (
-          <button
+      ) : (
+        <div className="relative">
+          <div
+            ref={contentRef}
             className={cn(
-              'absolute right-0 top-0 cursor-pointer pl-1 underline',
-              'hidden md:block',
+              'md:truncate md:pr-[75px]',
+              'line-clamp-2 md:line-clamp-none',
               textClassName,
             )}
-            onClick={toggleExpand}
           >
-            Show more
-          </button>
-        )}
+            {children}
+          </div>
 
-        {/* Mobile Show More */}
-        {isOverflowing && (
-          <Drawer>
-            <DrawerTrigger
+          {/* Desktop Show More */}
+          {isOverflowing && (
+            <button
               className={cn(
-                'cursor-pointer underline',
-                'md:hidden',
+                'absolute right-0 top-0 cursor-pointer pl-1 underline',
+                'hidden md:block',
                 textClassName,
               )}
+              onClick={toggleExpand}
             >
               Show more
-            </DrawerTrigger>
-            <DrawerContent className="pb-8">
-              <DrawerHeader>
-                <DrawerTitle className="mb-2.5 text-lg font-semibold text-primary">
-                  {pageTitle}
-                </DrawerTitle>
-                <DrawerDescription className={textClassName}>
-                  {children}
-                </DrawerDescription>
-              </DrawerHeader>
-            </DrawerContent>
-          </Drawer>
-        )}
-      </div>
+            </button>
+          )}
+
+          {/* Mobile Show More */}
+          {isOverflowing && (
+            <Drawer>
+              <DrawerTrigger
+                className={cn(
+                  'cursor-pointer underline',
+                  'md:hidden',
+                  textClassName,
+                )}
+              >
+                Show more
+              </DrawerTrigger>
+              <DrawerContent className="pb-8">
+                <DrawerHeader>
+                  <DrawerTitle className="mb-2.5 text-lg font-semibold text-primary">
+                    {pageTitle}
+                  </DrawerTitle>
+                  <DrawerDescription className={textClassName}>
+                    {children}
+                  </DrawerDescription>
+                </DrawerHeader>
+              </DrawerContent>
+            </Drawer>
+          )}
+        </div>
+      )}
     </div>
   )
 }
