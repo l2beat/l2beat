13e79ea8e4937cfc04d1c88a98639bf2d05f2df6
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2025-03-12 09:27:35 +0100

    Update frontend

--- packages/frontend/src/server/features/projects-change-report/get-on-disk-data.ts
+++ packages/frontend/src/server/features/projects-change-report/get-on-disk-data.ts
@@ -3,6 +3,9 @@ import { ConfigReader, type DiscoveryOutput } from '@l2beat/discovery'
 
 export function getOnDiskData() {
   const configReader = new ConfigReader(
+    // TODO: This is the only place we are not using getDiscoveryPaths()
+    // This feature is architected incorrectly and instead we should save
+    // the info about implementation/highSeverityField change in the db directly
     path.join(process.cwd(), '../config/discovery'),
   )
   const chains = configReader.readAllChains()
