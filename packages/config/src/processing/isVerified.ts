7df916b34e68f099e1d31b01413791803cbe28ac
Mateusz Radomski <33978857+mateuszradomski@users.noreply.github.com>
2025-03-13 11:05:10 +0100

    Stop skipping checking verification of permissions (#7146)

--- packages/config/src/processing/isVerified.ts
+++ packages/config/src/processing/isVerified.ts
@@ -1,27 +1,37 @@
 import type { Bridge, ScalingProject } from '../internalTypes'
 import type { BaseProject } from '../types'
 
-// TODO(radomski): Permissions
-export function isVerified(project: ScalingProject | Bridge): boolean {
-  const contractsVerification = Object.values(
-    project.contracts?.addresses ?? {},
-  ).every((p) => p.every((c) => c.isVerified))
-  const escrowVerifications =
-    project.config.escrows.every((e) => {
-      if (!('contract' in e)) {
-        return true
-      }
-
-      return e.contract?.isVerified
-    }) ?? true
-
-  const newVerification = escrowVerifications && contractsVerification
-
-  return newVerification
-}
-
-export function isProjectVerified(project: BaseProject): boolean {
+function getContractsVerification(
+  project: ScalingProject | Bridge | BaseProject,
+): boolean {
   return Object.values(project.contracts?.addresses ?? {})
     .flat()
     .every((c) => c.isVerified)
+}
+
+function getPermissionsVerification(
+  project: ScalingProject | Bridge | BaseProject,
+): boolean {
+  return Object.values(project.permissions ?? {})
+    .flat()
+    .flatMap((p) => [...(p.roles ?? []), ...(p.actors ?? [])])
+    .flatMap((p) => p.accounts)
+    .every((a) => a.isVerified)
+}
+
+export function isVerified(project: ScalingProject | Bridge): boolean {
+  const contracts = getContractsVerification(project)
+  const permissions = getPermissionsVerification(project)
+
+  const escrows = project.config.escrows.every(
+    (e) => e.contract?.isVerified ?? true,
+  )
+
+  return escrows && permissions && contracts
+}
+
+export function isProjectVerified(project: BaseProject): boolean {
+  return (
+    getContractsVerification(project) && getPermissionsVerification(project)
+  )
 }
