aa74c85bfb9bfae211a27867919ad0916d5988ef
Radina Talanova <nt.radina@gmail.com>
2025-03-13 16:52:39 +0200

    format

--- packages/config/src/projects/hyperagi/hyperagi.ts
+++ packages/config/src/projects/hyperagi/hyperagi.ts
@@ -11,20 +11,20 @@ export const hyperagi: ScalingProject = upcomingL3({
     name: 'HyperAGI',
     slug: 'hyperagi',
     description:
-        'HyperAGI is a multi-tiered decentralized platform designed to provide powerful computational support and innovative applications for AI.',
+      'HyperAGI is a multi-tiered decentralized platform designed to provide powerful computational support and innovative applications for AI.',
     purposes: ['AI'],
     category: 'Optimium',
     stack: 'Arbitrum',
     links: {
-        websites: ['https://hyperagi.network/'],
-        documentation: ['https://hyperdust-foundation.gitbook.io/moss-ai-docs'],
-        repositories: ['https://github.com/HyperdustLab/HyperdustProtocol'],
-        explorers: ['https://explorer.hyperagi.network/'],
-        socialMedia: [
-          'https://x.com/HyperDustIO',
-          'https://linkedin.com/company/hyperdust-protocol',
-          'https://t.me/hyperdust_io',
-        ],
-      },
+      websites: ['https://hyperagi.network/'],
+      documentation: ['https://hyperdust-foundation.gitbook.io/moss-ai-docs'],
+      repositories: ['https://github.com/HyperdustLab/HyperdustProtocol'],
+      explorers: ['https://explorer.hyperagi.network/'],
+      socialMedia: [
+        'https://x.com/HyperDustIO',
+        'https://linkedin.com/company/hyperdust-protocol',
+        'https://t.me/hyperdust_io',
+      ],
+    },
   },
 })
