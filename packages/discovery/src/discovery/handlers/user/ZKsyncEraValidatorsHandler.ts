54a2f99355fccb043df9f4f87a2a883896e26516
Mateusz Radomski <radomski.main@protonmail.com>
2025-03-14 12:51:30 +0100

    Handle Gateways upgrade post upgrade calldata

--- packages/discovery/src/discovery/handlers/user/ZKsyncEraValidatorsHandler.ts
+++ packages/discovery/src/discovery/handlers/user/ZKsyncEraValidatorsHandler.ts
@@ -11,12 +11,18 @@ export const ZKsyncEraValidatorsHandlerDefinition = z.strictObject({
   type: z.literal('zksynceraValidators'),
 })
 
+const upgradeCompleteSignature1 =
+  'UpgradeComplete(uint256 indexed newProtocolVersion, bytes32 indexed l2UpgradeTxHash, tuple(tuple(uint256 txType, uint256 from, uint256 to, uint256 gasLimit, uint256 gasPerPubdataByteLimit, uint256 maxFeePerGas, uint256 maxPriorityFeePerGas, uint256 paymaster, uint256 nonce, uint256 value, uint256[4] reserved, bytes data, bytes signature, uint256[] factoryDeps, bytes paymasterInput, bytes reservedDynamic) l2ProtocolUpgradeTx, bytes[] factoryDeps, bytes32 bootloaderHash, bytes32 defaultAccountHash, address verifier, tuple(bytes32 recursionNodeLevelVkHash, bytes32 recursionLeafLevelVkHash, bytes32 recursionCircuitsSetVksHash) verifierParams, bytes l1ContractsUpgradeCalldata, bytes postUpgradeCalldata, uint256 upgradeTimestamp, uint256 newProtocolVersion) upgrade)'
+
+// new UpgradeComplete event since 2025-03-12 (emitted [here](https://app.blocksec.com/explorer/tx/eth/0x2c6aa40efd0500a015b036cf03de961b6e2aa2c726d21b5b8cede9a91964c12f?line=28)
+const upgradeCompleteSignature2 =
+  'UpgradeComplete(uint256 indexed newProtocolVersion, bytes32 indexed l2UpgradeTxHash, tuple(tuple(uint256 txType, uint256 from, uint256 to, uint256 gasLimit, uint256 gasPerPubdataByteLimit, uint256 maxFeePerGas, uint256 maxPriorityFeePerGas, uint256 paymaster, uint256 nonce, uint256 value, uint256[4] reserved, bytes data, bytes signature, uint256[] factoryDeps, bytes paymasterInput, bytes reservedDynamic) l2ProtocolUpgradeTx, bytes32 bootloaderHash, bytes32 defaultAccountHash, address verifier, tuple(bytes32 recursionNodeLevelVkHash, bytes32 recursionLeafLevelVkHash, bytes32 recursionCircuitsSetVksHash) verifierParams, bytes l1ContractsUpgradeCalldata, bytes postUpgradeCalldata, uint256 upgradeTimestamp, uint256 newProtocolVersion) upgrade)'
+
 const abi = new utils.Interface([
   'event ValidatorStatusUpdate(address indexed validatorAddress, bool isActive)',
   'event DiamondCut(tuple(address facet, uint8 action, bool isFreezable, bytes4[] selectors)[] facetCuts, address initAddress, bytes initCalldata)',
-  'event UpgradeComplete(uint256 indexed newProtocolVersion, bytes32 indexed l2UpgradeTxHash, tuple(tuple(uint256 txType, uint256 from, uint256 to, uint256 gasLimit, uint256 gasPerPubdataByteLimit, uint256 maxFeePerGas, uint256 maxPriorityFeePerGas, uint256 paymaster, uint256 nonce, uint256 value, uint256[4] reserved, bytes data, bytes signature, uint256[] factoryDeps, bytes paymasterInput, bytes reservedDynamic) l2ProtocolUpgradeTx, bytes[] factoryDeps, bytes32 bootloaderHash, bytes32 defaultAccountHash, address verifier, tuple(bytes32 recursionNodeLevelVkHash, bytes32 recursionLeafLevelVkHash, bytes32 recursionCircuitsSetVksHash) verifierParams, bytes l1ContractsUpgradeCalldata, bytes postUpgradeCalldata, uint256 upgradeTimestamp, uint256 newProtocolVersion) upgrade)',
-  // new UpgradeComplete event since 2025-03-12 (emitted [here](https://app.blocksec.com/explorer/tx/eth/0x2c6aa40efd0500a015b036cf03de961b6e2aa2c726d21b5b8cede9a91964c12f?line=28)
-  'event UpgradeComplete(uint256 indexed newProtocolVersion, bytes32 indexed l2UpgradeTxHash, tuple(tuple(uint256 txType, uint256 from, uint256 to, uint256 gasLimit, uint256 gasPerPubdataByteLimit, uint256 maxFeePerGas, uint256 maxPriorityFeePerGas, uint256 paymaster, uint256 nonce, uint256 value, uint256[4] reserved, bytes data, bytes signature, uint256[] factoryDeps, bytes paymasterInput, bytes reservedDynamic) l2ProtocolUpgradeTx, bytes32 bootloaderHash, bytes32 defaultAccountHash, address verifier, tuple(bytes32 recursionNodeLevelVkHash, bytes32 recursionLeafLevelVkHash, bytes32 recursionCircuitsSetVksHash) verifierParams, bytes l1ContractsUpgradeCalldata, bytes postUpgradeCalldata, uint256 upgradeTimestamp, uint256 newProtocolVersion) upgrade)',
+  `event ${upgradeCompleteSignature1}`,
+  `event ${upgradeCompleteSignature2}`,
 
   'function initialize(tuple(uint256 chainId, address bridgehub, address stateTransitionManager, uint256 protocolVersion, address admin, address validatorTimelock, address baseToken, address baseTokenBridge, bytes32 storedBatchZero, address verifier, tuple(bytes32 recursionNodeLevelVkHash, bytes32 recursionLeafLevelVkHash, bytes32 recursionCircuitsSetVksHash) verifierParams, bytes32 l2BootloaderBytecodeHash, bytes32 l2DefaultAccountBytecodeHash, uint256 priorityTxMaxGasLimit, tuple(uint8 pubdataPricingMode, uint32 batchOverheadL1Gas, uint32 maxPubdataPerBatch, uint32 maxL2GasPerBatch, uint32 priorityTxMaxPubdata, uint64 minimalL2GasPrice) feeParams, address blobVersionedHashRetriever) _initializeData) returns (bytes32)',
 ])
@@ -36,7 +42,8 @@ export class ZKsyncEraValidatorsHandler implements Handler {
     const logs = await provider.getLogs(address, [
       [
         abi.getEventTopic('ValidatorStatusUpdate'),
-        abi.getEventTopic('UpgradeComplete'),
+        abi.getEventTopic(upgradeCompleteSignature1),
+        abi.getEventTopic(upgradeCompleteSignature2),
         abi.getEventTopic('DiamondCut'),
       ],
     ])
@@ -54,7 +61,6 @@ export class ZKsyncEraValidatorsHandler implements Handler {
         }
       } else if (log.name === 'UpgradeComplete') {
         const postUpgradeCalldata = log.args.upgrade.postUpgradeCalldata
-        let validatorTimelock: string | undefined
 
         // NOTE(radomski): Upgrades can go through a contract that will
         // actually execute it. The contract is based on BaseZkSyncUpgrade. At
@@ -71,23 +77,56 @@ export class ZKsyncEraValidatorsHandler implements Handler {
         // NOTE(radomski): Taken from:
         // https://etherscan.io/address/0xd719fca4433646cbd86f6b073ee364d36b856b1d
         const UPGRADE_HYPERCHAINS_POST_UPGRADE_CALLDATA_LENGTH = 386
-        if (
-          postUpgradeCalldata.length ===
-          UPGRADE_HYPERCHAINS_POST_UPGRADE_CALLDATA_LENGTH
-        ) {
-          const decoded = utils.defaultAbiCoder.decode(
-            [
-              'uint256 chainId',
-              'address bridgehubAddress',
-              'address stateTransitionManager',
-              'address sharedBridgeAddress',
-              'address chainAdmin',
-              'address validatorTimelock',
-            ],
-            postUpgradeCalldata,
-          )
-          validatorTimelock = decoded.validatorTimelock as string
-          validators.add(validatorTimelock)
+
+        // NOTE(radomski): Taken from:
+        // https://etherscan.io/address/0x516fa73908963ae2072647b8a0c053327389d7c2
+        const UPGRADE_GATEWAYS_POST_UPGRADE_CALL_DATA_LENGTH = 18498
+
+        switch (postUpgradeCalldata.length) {
+          case UPGRADE_HYPERCHAINS_POST_UPGRADE_CALLDATA_LENGTH: {
+            const decoded = utils.defaultAbiCoder.decode(
+              [
+                'uint256 chainId',
+                'address bridgehubAddress',
+                'address stateTransitionManager',
+                'address sharedBridgeAddress',
+                'address chainAdmin',
+                'address validatorTimelock',
+              ],
+              postUpgradeCalldata,
+            )
+            const validatorTimelock = decoded.validatorTimelock as string
+            validators.add(validatorTimelock)
+            break
+          }
+          case UPGRADE_GATEWAYS_POST_UPGRADE_CALL_DATA_LENGTH: {
+            const decoded = utils.defaultAbiCoder.decode(
+              [
+                [
+                  'tuple(',
+                  'tuple(bytes32,address,bool,uint256,bytes)[] forceDeployments,',
+                  'uint256 l2GatewayUpgradePosition,',
+                  'bytes fixedForceDeploymentsData,',
+                  'address ctmDeployer,',
+                  'address oldValidatorTimelock,',
+                  'address newValidatorTimelock,',
+                  'address wrappedBaseTokenStore',
+                  ')',
+                ].join(''),
+              ],
+              postUpgradeCalldata,
+            )
+
+            const oldValidatorTimelock = decoded[0].oldValidatorTimelock
+            const newValidatorTimelock = decoded[0].newValidatorTimelock
+
+            validators.add(oldValidatorTimelock)
+            validators.add(newValidatorTimelock)
+            break
+          }
+          default: {
+            break
+          }
         }
       } else if (log.name === 'DiamondCut') {
         try {
