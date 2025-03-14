8de483377c9589989aea6a73cbba3c24e0f7c15d
torztomasz <tomasz.torz@l2beat.com>
2025-03-14 10:19:51 +0100

    adjust top for mobile dialog

--- packages/frontend/src/components/core/dialog.tsx
+++ packages/frontend/src/components/core/dialog.tsx
@@ -44,10 +44,9 @@ const DialogContent = ({
       ref={ref}
       className={cn(
         'fixed left-1/2 top-1/2 z-999 grid w-full -translate-x-1/2 -translate-y-1/2 gap-4 border border-surface-tertiary bg-surface-secondary p-6 shadow-popover duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] md:top-1/4 md:max-w-lg md:rounded-lg',
-        fullScreenMobile &&
-          'max-md:left-1/2 max-md:top-1/2 max-md:h-dvh max-md:w-full max-md:rounded-none',
+        fullScreenMobile && 'max-md:h-dvh max-md:w-full max-md:rounded-none',
         !fullScreenMobile &&
-          'max-md:top-1/2 max-md:w-[calc(100%-1rem)] max-md:rounded-md',
+          'max-md:top-1/3 max-md:w-[calc(100%-1rem)] max-md:rounded-md',
         className,
       )}
       {...props}
