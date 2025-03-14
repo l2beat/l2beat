54c6cc59337af0da7d6c561fd65b9ec1b58b1acd
antooni <antoni.pawlak@protonmail.com>
2025-03-14 17:56:00 +0100

    update migrations

--- packages/database/prisma/migrations/20250313190839_tvs_block_timestamp/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "TvsBlockTimestamp" (
-    "chain" VARCHAR(255) NOT NULL,
-    "timestamp" TIMESTAMP(6) NOT NULL,
-    "blockNumber" INTEGER NOT NULL,
-    "configurationId" CHAR(12) NOT NULL,
-
-    CONSTRAINT "TvsBlockTimestamp_pkey" PRIMARY KEY ("chain","timestamp")
-);
--- packages/database/prisma/migrations/20250314165516_tvs_amount_block/migration.sql
+++ packages/database/prisma/migrations/20250314165516_tvs_amount_block/migration.sql
@@ -8,5 +8,15 @@ CREATE TABLE "TvsAmount" (
     CONSTRAINT "TvsAmount_pkey" PRIMARY KEY ("timestamp","configurationId")
 );
 
+-- CreateTable
+CREATE TABLE "TvsBlockTimestamp" (
+    "chain" VARCHAR(255) NOT NULL,
+    "timestamp" TIMESTAMP(6) NOT NULL,
+    "blockNumber" INTEGER NOT NULL,
+    "configurationId" CHAR(12) NOT NULL,
+
+    CONSTRAINT "TvsBlockTimestamp_pkey" PRIMARY KEY ("chain","timestamp")
+);
+
 -- CreateIndex
 CREATE INDEX "TvsAmount_project_idx" ON "TvsAmount"("project");
