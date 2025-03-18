092e36c121b0976c2c8e928c0f5500c87cf4c4a1
torztomasz <tomasz.torz@l2beat.com>
2025-03-18 18:10:54 +0100

    add animations

--- packages/frontend/src/components/table/filters/table-filter.tsx
+++ packages/frontend/src/components/table/filters/table-filter.tsx
@@ -27,7 +27,12 @@ export function TableFilter({ filter, possibleValues }: Props) {
   const { dispatch } = useTableFilterContext()
 
   return (
-    <div className="flex h-8 w-max select-none items-center rounded-lg bg-surface-primary text-xs font-medium leading-none primary-card:bg-surface-secondary md:text-sm">
+    <div
+      className={cn(
+        'flex h-8 w-max select-none items-center rounded-lg bg-surface-primary text-xs font-medium leading-none primary-card:bg-surface-secondary md:text-sm',
+        'animate-in fade-in-0 zoom-in-95 slide-in-from-left-2',
+      )}
+    >
       <div className="flex h-full items-center justify-center pl-2.5 pr-2">
         {filterIdToLabel[filter.id]}
       </div>
