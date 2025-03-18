2140f836bbe5c1d5596297a2aabb64b5d41e722c
Radina Talanova <nt.radina@gmail.com>
2025-03-18 17:31:04 +0200

    Move OpenZK to under review

--- packages/config/src/projects/openzk/openzk.ts
+++ packages/config/src/projects/openzk/openzk.ts
@@ -1,24 +1,33 @@
 import { UnixTime } from '@l2beat/shared-pure'
 import type { ScalingProject } from '../../internalTypes'
-import { upcomingL2 } from '../../templates/upcoming'
+import { underReviewL2 } from '../../templates/underReview'
+import { BADGES } from '../../common/badges'
 
-export const openzk: ScalingProject = upcomingL2({
+
+export const openzk: ScalingProject = underReviewL2({
   id: 'openzk',
   capability: 'universal',
-  addedAt: UnixTime(1739874850),
+  addedAt: UnixTime(1742310729), //18.03.2025 15:12:09 
+  badges: [
+    BADGES.Infra.ElasticChain,
+    BADGES.DA.EthereumBlobs,
+    BADGES.Stack.ZKStack,
+    BADGES.VM.EVM,
+    BADGES.RaaS.Caldera
+  ],
   display: {
     name: 'OpenZK',
     slug: 'openzk',
     description:
-      'OpenZK is the first Layer 2 solution on ZK rollup technology to unite native ETH staking, liquid restaking, and stablecoin staking in one seamless platform.',
+      'OpenZK is a ZK rollup that unites native ETH staking, liquid restaking, and stablecoin staking in one seamless platform.',
     purposes: ['Universal'],
     category: 'ZK Rollup',
     stack: 'ZK Stack',
     links: {
       websites: ['https://openzk.net/'],
-      apps: ['https://portal.openzk.net/bridge'],
+      apps: ['https://openzk-mainnet-bridge.vercel.app/bridge'],
       documentation: ['https://docs.openzk.net/'],
-      explorers: ['https://explorer.openzk.net/'],
+      explorers: ['https://openzk.calderaexplorer.xyz/'],
       repositories: [],
       socialMedia: [
         'https://x.com/OpenZkNetwork',
@@ -26,5 +35,20 @@ export const openzk: ScalingProject = upcomingL2({
         'https://t.me/OpenZkNetwork',
       ],
     },
+  },
+  chainConfig: {
+    name: 'openzk',
+    chainId: 1345,
+    apis: [
+      {
+        type: 'rpc',
+        url: 'https://openzk.calderachain.xyz/http',
+        callsPerMinute: 1500,
+      },
+    ],
+  },
+  activityConfig: {
+    type: 'block',
+    startBlock: 1,
   },
 })
