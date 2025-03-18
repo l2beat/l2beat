a8ad0c5d903c97d017500717e49aa51d838a834e
antooni <antoni.pawlak@protonmail.com>
2025-03-18 14:42:56 +0100

    clear interface

--- packages/backend/src/modules/tvs/tools/DataStorage.ts
+++ packages/backend/src/modules/tvs/tools/DataStorage.ts
@@ -1,12 +1,10 @@
 import type { UnixTime } from '@l2beat/shared-pure'
 
 export interface DataStorage {
-  writePrice(id: string, timestamp: UnixTime, price: number): Promise<void>
   getPrices(
     ids: string[],
     timestamps: UnixTime[],
   ): Promise<Map<UnixTime, Map<string, number>>>
-  writeAmount(id: string, timestamp: UnixTime, amount: bigint): Promise<void>
   getAmounts(
     ids: string[],
     timestamps: UnixTime[],
