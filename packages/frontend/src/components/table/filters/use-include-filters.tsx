31e94cbc14b64ce752f483c27a4cc6ef418c7edc
torztomasz <tomasz.torz@l2beat.com>
2025-03-19 08:33:38 +0100

    fix multiple values for one id filtering

--- packages/frontend/src/components/table/filters/use-include-filters.tsx
+++ packages/frontend/src/components/table/filters/use-include-filters.tsx
@@ -12,12 +12,15 @@ export function useIncludeFilters() {
         if (filterable === undefined) {
           return true
         }
-        const filterableValue = filterable.find((f) => f.id === filter.id)
-        if (!filterableValue) {
+        // You can have multiple filterable values with the same id, e.g. purpose
+        const filterableValues = filterable.filter((f) => f.id === filter.id)
+        if (filterableValues.length === 0) {
           return false
         }
 
-        const valueMatches = filter.values.includes(filterableValue.value)
+        const valueMatches = filterableValues.some((f) =>
+          filter.values.includes(f.value),
+        )
         return filter.inversed ? !valueMatches : valueMatches
       })
     },
