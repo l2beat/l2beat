ca352813ac372ad7d31b9a46aaa8ffccdf40095e
Mateusz Radomski <radomski.main@protonmail.com>
2025-03-19 14:03:12 +0100

    Template hashes use key sorting

--- packages/discovery/src/discovery/analysis/TemplateService.ts
+++ packages/discovery/src/discovery/analysis/TemplateService.ts
@@ -15,6 +15,7 @@ import { DiscoveryContract } from '../config/RawDiscoveryConfig'
 import type { DiscoveryOutput } from '../output/types'
 import type { ContractSources } from '../source/SourceCodeService'
 import { readJsonc } from '../utils/readJsonc'
+import { deepSortByKeys } from '../config/getDiscoveryConfigEntries'
 
 export const TEMPLATES_PATH = path.join('_templates')
 const TEMPLATE_SHAPE_FOLDER = 'shape'
@@ -135,7 +136,7 @@ export class TemplateService {
 
   getTemplateHash(template: string): Hash256 {
     const templateJson = this.loadContractTemplate(template)
-    return hashJson(templateJson as json)
+    return hashJson(deepSortByKeys(templateJson) as json)
   }
 
   getAllShapes(): Record<string, Shape> {
