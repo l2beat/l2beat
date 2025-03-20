c0ad5b53e947bb5b2150614bb07eb8eb5ef28ea0
antooni <antoni.pawlak@protonmail.com>
2025-03-20 11:47:58 +0100

    update log

--- packages/backend/src/modules/tvs/indexers/CirculatingSupplyAmountIndexer.ts
+++ packages/backend/src/modules/tvs/indexers/CirculatingSupplyAmountIndexer.ts
@@ -104,7 +104,7 @@ export class CirculatingSupplyAmountIndexer extends ManagedMultiIndexer<Circulat
     return async () => {
       await this.$.db.tvsAmount.insertMany(records)
 
-      this.logger.info('Saved prices into DB', {
+      this.logger.info('Saved circulating supplies into DB', {
         from,
         to: adjustedTo,
         records: records.length,
