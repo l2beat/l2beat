6dc508bc99db526580de3c4849b1d319c7d1fdce
torztomasz <tomasz.torz@l2beat.com>
2025-03-21 14:14:26 +0100

    cache badge params

--- packages/frontend/src/utils/project/get-badge-with-params.ts
+++ packages/frontend/src/utils/project/get-badge-with-params.ts
@@ -2,8 +2,16 @@ import type { Badge } from '@l2beat/config'
 import { getImageParams } from './get-image-params'
 import type { BadgeWithParams } from '~/components/projects/project-badge'
 
+const badgesParams: Record<string, BadgeWithParams> = {}
+
 export function getBadgeWithParams(badge: Badge): BadgeWithParams | undefined {
+  if (badgesParams[badge.id]) return badgesParams[badge.id]
+
   const imageParams = getImageParams(`/images/badges/${badge.id}.png`)
   if (!imageParams) return undefined
-  return { ...badge, ...imageParams }
+
+  const result = { ...badge, ...imageParams }
+  badgesParams[badge.id] = result
+
+  return result
 }
