a01345769e9206521c33710d700546a7a785e171
maciekzygmunt <maciekzygmunt@interia.pl>
2025-03-27 12:25:08 +0100

    add cookies upport

--- packages/spell2beat/src/index.ts
+++ packages/spell2beat/src/index.ts
@@ -27,9 +27,18 @@ main()
 async function getScraper() {
   const env = getEnv()
   const scraper = new Scraper()
-  await scraper.login(
-    env.string('TWITTER_USERNAME'),
-    env.string('TWITTER_PASSWORD'),
-  )
+
+  const cookies = [
+    env.string('TWITTER_COOKIE_GUEST_ID'),
+    env.string('TWITTER_COOKIE_CF_BM'),
+    env.string('TWITTER_COOKIE_KDT'),
+    env.string('TWITTER_COOKIE_TWID'),
+    env.string('TWITTER_COOKIE_CT0'),
+    env.string('TWITTER_COOKIE_AUTH_TOKEN'),
+    env.string('TWITTER_COOKIE_ATT'),
+  ]
+
+  await scraper.setCookies(cookies)
+
   return scraper
 }
