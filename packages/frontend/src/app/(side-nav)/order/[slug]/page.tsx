591bb81a7cba5020b6c4cf68b88d1baf8a49def7
torztomasz <tomasz.torz@l2beat.com>
2025-03-31 23:54:46 +0200

    fix

--- packages/frontend/src/app/(side-nav)/order/[slug]/page.tsx
+++ packages/frontend/src/app/(side-nav)/order/[slug]/page.tsx
@@ -2,11 +2,21 @@ import { notFound } from 'next/navigation'
 import { PrimaryCard } from '~/components/primary-card/primary-card'
 import { ps } from '~/server/projects'
 import { L2BeatzzaResult } from './_components/result'
+import { env } from '~/env'
+
+type Params = {
+  slug: string
+}
 
 type Props = {
-  params: Promise<{
-    slug: string
-  }>
+  params: Promise<Params>
+}
+export async function generateStaticParams(): Promise<Params[]> {
+  if (env.VERCEL_ENV !== 'production') return []
+  const projects = await ps.getProjects({
+    where: ['scalingRisks'],
+  })
+  return projects.map((p) => ({ slug: p.slug }))
 }
 
 export default async function Page(props: Props) {
