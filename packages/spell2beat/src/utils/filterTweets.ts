4cce384b9c61a6442c4d8e59f3a9f5f636c6c171
maciekzygmunt <maciekzygmunt@interia.pl>
2025-03-31 08:54:16 +0200

    small fixes

--- packages/spell2beat/src/utils/filterTweets.ts
+++ packages/spell2beat/src/utils/filterTweets.ts
@@ -2,10 +2,14 @@ import type { Tweet } from 'agent-twitter-client'
 import { getTweetsInDb } from './db'
 
 export function filterTweets(tweets: Tweet[]) {
-  const tweetsInDb = getTweetsInDb(tweets.map((tweet) => tweet.id))
+  const tweetsInDb = getTweetsInDb(
+    tweets.map((tweet) => tweet.id).filter((tweet) => tweet !== undefined),
+  )
 
   return tweets.filter((tweet) => {
-    if (tweetsInDb.includes(tweet.id)) return false
+    if (!tweet.id || !tweet.text) return false
+    if (tweetsInDb.includes(tweet.id) || tweet.username === 'SPELL2BEAT')
+      return false
 
     let processedText = tweet.text
 
@@ -13,7 +17,7 @@ export function filterTweets(tweets: Tweet[]) {
       /https?:\/\/(?:www\.)?l2beat\.com\S*/gi,
       '',
     )
-    processedText = processedText.replace(/@\s*l2\s*beat\b/gi, '')
+    processedText = processedText.replace(/@\s*l2\s*beat(?:gov)?\b/gi, '')
 
     const l2beatRegex = /l2\s*beat/gi
     const matches = processedText.match(l2beatRegex)
