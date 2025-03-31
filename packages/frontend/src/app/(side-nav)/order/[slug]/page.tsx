fbd2aeb9367a3c8015116e2ebfdfb833320dd4ab
Tomasz Tórz <93620601+torztomasz@users.noreply.github.com>
2025-03-31 23:59:01 +0200

    Fix l2beatzza order page (#7335)

--- packages/frontend/src/app/(side-nav)/order/[slug]/page.tsx
+++ packages/frontend/src/app/(side-nav)/order/[slug]/page.tsx
@@ -1,12 +1,22 @@
 import { notFound } from 'next/navigation'
 import { PrimaryCard } from '~/components/primary-card/primary-card'
+import { env } from '~/env'
 import { ps } from '~/server/projects'
 import { L2BeatzzaResult } from './_components/result'
 
+type Params = {
+  slug: string
+}
+
 type Props = {
-  params: Promise<{
-    slug: string
-  }>
+  params: Promise<Params>
+}
+export async function generateStaticParams(): Promise<Params[]> {
+  if (env.VERCEL_ENV !== 'production') return []
+  const projects = await ps.getProjects({
+    where: ['scalingRisks'],
+  })
+  return projects.map((p) => ({ slug: p.slug }))
 }
 
 export default async function Page(props: Props) {
