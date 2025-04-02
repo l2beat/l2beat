6f5f5f08add24011dca827716362f79740028c1e
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2025-04-02 14:18:33 +0200

    Add config diff step

---
+++ packages/config/scripts/diff/diff.ts
@@ -0,0 +1,37 @@
+import { readFileSync } from 'fs'
+import { exit } from 'process'
+
+const prDb = readFileSync('/tmp/db/pr.sqlite')
+const prCommit = readFileSync('/tmp/db/pr.commit', 'utf-8')
+const mainDb = readFileSync('/tmp/db/main.sqlite')
+const mainCommit = readFileSync('/tmp/db/main.commit', 'utf-8')
+
+const prSize = prDb.length
+const mainSize = mainDb.length
+
+if (prSize === mainSize) {
+  console.log('No changes detected')
+  exit()
+}
+
+const html = `
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <title>Diff</title>
+</head>
+
+<body>
+  <h1>Diff</h1>
+  <h2>Main commit ${mainCommit}</h2>
+  <p>Main size ${mainSize}</p>
+  <h2>PR commit ${prCommit}</h2>
+  <p>PR size ${prSize}</p>
+</body>
+
+</html>
+`
+
+console.log(html)
