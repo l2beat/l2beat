8bf19639c70489991b957cbcdd01bbd07e253295
antooni <antoni.pawlak@protonmail.com>
2025-04-02 11:32:50 +0200

    update token id

--- packages/shared-pure/src/types/TokenId.ts
+++ packages/shared-pure/src/types/TokenId.ts
@@ -6,6 +6,23 @@ export function TokenId(value: string) {
   return value as unknown as TokenId
 }
 
-TokenId.create = function (chain: string, symbol: string) {
-  return TokenId(`${chain}-${symbol}`)
+TokenId.create = function (chain: string, symbol: string, priceId: string) {
+  if (TokenId.SYMBOL_DUPLICATES.includes(symbol)) {
+    return TokenId(`${chain}-${symbol}.${priceId}`)
+  } else {
+    return TokenId(`${chain}-${symbol}`)
+  }
 }
+
+TokenId.SYMBOL_DUPLICATES = [
+  'GAME',
+  'mETH',
+  'MOON',
+  'USDN',
+  'USDT',
+  'USDC',
+  'WETH',
+  'SOL',
+  'USDC.e',
+  'wBTC',
+]
