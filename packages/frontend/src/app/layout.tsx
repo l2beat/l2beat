15666a4d0d4f885d21cf35c4d998a2a43bf5c07b
torztomasz <tomasz.torz@l2beat.com>
2025-04-02 12:58:16 +0200

    wind down l2beatzza

--- packages/frontend/src/app/layout.tsx
+++ packages/frontend/src/app/layout.tsx
@@ -11,10 +11,6 @@ import { GlossaryContextProvider } from '../components/markdown/glossary-context
 import { ProgressBar } from '../components/progress-bar'
 import { roboto } from '../fonts'
 import '../styles/globals.css'
-import {
-  L2BeatzzaDialog,
-  L2BeatzzaDialogProvider,
-} from '~/components/l2beatzza/l2beatzza-dialog'
 import { RecategorisationPreviewContextProvider } from '~/components/recategorisation-preview/recategorisation-preview-provider'
 import { FullGreenPizzaSymbol } from '~/components/rosette/pizza/real-elements/full-green-pizza'
 import { FullRedPizzaSymbol } from '~/components/rosette/pizza/real-elements/full-red-pizza'
@@ -63,15 +59,12 @@ export default async function RootLayout({
                 >
                   <SearchBarContextProvider projects={searchBarProjects}>
                     <RecategorisationPreviewContextProvider>
-                      <L2BeatzzaDialogProvider>
-                        {children}
-                        <div className="fixed size-0">
-                          <FullRedPizzaSymbol />
-                          <FullYellowPizzaSymbol />
-                          <FullGreenPizzaSymbol />
-                        </div>
-                        <L2BeatzzaDialog />
-                      </L2BeatzzaDialogProvider>
+                      {children}
+                      <div className="fixed size-0">
+                        <FullRedPizzaSymbol />
+                        <FullYellowPizzaSymbol />
+                        <FullGreenPizzaSymbol />
+                      </div>
                     </RecategorisationPreviewContextProvider>
                   </SearchBarContextProvider>
                   <ProgressBar />
--- packages/frontend/src/components/l2beatzza/pizza-button.tsx
+++ packages/frontend/src/components/l2beatzza/pizza-button.tsx
@@ -1,10 +1,5 @@
-import Image from 'next/image'
 import { cn } from '~/utils/cn'
 import { Button, type ButtonProps } from '../core/button'
-
-type Props = {
-  onClick: () => void
-}
 
 export function PizzaButton(
   props: ButtonProps & { children: React.ReactNode },
@@ -20,20 +15,5 @@ export function PizzaButton(
     >
       {props.children}
     </Button>
-  )
-}
-
-export function Step1(props: Props) {
-  return (
-    <div className="flex flex-col items-center justify-center gap-7">
-      <Image
-        src="/images/l2beatzza/open-pizza.png"
-        alt="Open pizza"
-        width={1000}
-        height={1000}
-        className="w-full max-w-[360px]"
-      />
-      <PizzaButton onClick={props.onClick}>Start making my pizza</PizzaButton>
-    </div>
   )
 }
