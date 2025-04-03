06e857ecc46172d2e883db0f5440347132201c0e
torztomasz <tomasz.torz@l2beat.com>
2025-04-03 12:46:46 +0200

    add the elastic network

---
+++ packages/config/src/projects/the-elastic-network/the-elestic-network.ts
@@ -0,0 +1,31 @@
+import { ProjectId } from '@l2beat/shared-pure'
+import { BADGES } from '../../common/badges'
+import type { BaseProject } from '../../types'
+
+export const theElasticNetwork: BaseProject = {
+  id: ProjectId('the-elastic-network'),
+  slug: 'the-elastic-network',
+  name: 'The Elastic Network',
+  shortName: undefined,
+  addedAt: 1743677000,
+  display: {
+    description:
+      'An ever-expanding, verifiable network of blockchains, powered by ZKsync',
+    links: {
+      websites: ['https://www.zksync.io/'],
+    },
+    badges: [BADGES.Stack.ZKStack, BADGES.Infra.ElasticChain],
+  },
+  ecosystemConfig: {
+    colors: {
+      primary: '#513BE5',
+      secondary: '#4276FF',
+    },
+    ecosystemToken: {
+      name: 'ZK',
+      chain: 'ethereum',
+      description: 'ZK is the governance token for the Elastic Network.',
+    },
+    governanceReviews: true,
+  },
+}
