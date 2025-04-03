b72602c1931a25a1e6642eeef1b15493fabe17c2
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2025-04-03 10:41:12 +0200

    Improve diff styling

--- packages/config/scripts/diff/html.ts
+++ packages/config/scripts/diff/html.ts
@@ -1,27 +1,90 @@
 import type { Change } from 'diff'
 import type { FieldDiff, ProjectDiff } from './diff'
+import { readFileSync } from 'fs'
 
 export function diffsToHtml(props: {
   diffs: ProjectDiff[]
   commitBefore: string
   commitAfter: string
 }) {
+  const alpineSource = readFileSync(
+    require.resolve('alpinejs/dist/cdn.min.js'),
+    'utf-8',
+  )
+
   return `<!doctype html>
 <html lang="en">
 
 <head>
   <meta charset="UTF-8" />
   <title>Diff</title>
-  <style>
+  <style>${getStyles()}</style>
+</head>
+
+<body>
+  <script>${alpineSource}</script>
+  <h1>
+    Diff between
+    <code class="removed">${props.commitBefore.slice(0, 8)}</code> (main)
+    and
+    <code class="added">${props.commitAfter.slice(0, 8)}</code> (PR)
+  </h1>
+  <ul>${props.diffs.map(diffToHtml).join('')}</ul>
+</body>
+
+</html>`
+}
+
+function diffToHtml(diff: ProjectDiff, _index: number, diffs: ProjectDiff[]) {
+  const initialOpen = diffs.length === 1
+
+  return `<li x-data="{ open: ${initialOpen} }">
+  <h2>
+    <button @click="open = !open" x-text="open ? 'Close' : 'Open'"></button>
+    <span>${diff.id}</span>
+    <span class="${diff.type}">${diff.type}<span>
+  </h2>
+  <ul class="field" x-show="open">${diff.fields.map(fieldDiffToHtml).join('')}</ul>
+</li>`
+}
+
+function fieldDiffToHtml(
+  field: FieldDiff,
+  _index: number,
+  fields: FieldDiff[],
+) {
+  const initialOpen = fields.length === 1
+
+  return `<li x-data="{ open: ${initialOpen} }">
+  <h3>
+    <button @click="open = !open" x-text="open ? 'Close' : 'Open'"></button>
+    <span>${field.field}</span>
+  </h3>
+  <pre x-show="open"><code>${field.diff.map(changeToHtml).join('')}</code></pre>
+</li>`
+}
+
+function changeToHtml(change: Change) {
+  let className = 'unmodified'
+  if (change.added) {
+    className = 'added'
+  }
+  if (change.removed) {
+    className = 'removed'
+  }
+  return `<span class="${className}">${change.value}</span>`
+}
+
+function getStyles() {
+  return `
+:root {
+  --font-mono: ui-monospace, 'Cascadia Code', 'Source Code Pro', Menlo, Consolas, 'DejaVu Sans Mono', monospace;
+  --font-sans: system-ui, sans-serif;
+}
 body {
-  background-color:#222;
-  color:white;
-}
-h1, h2, h3 {
-  font-family: monospace;
-}
-pre {
-  background-color: #111;
+  background-color: #222;
+  color: white;
+  font-family: var(--font-sans);
 }
 .unmodified {
   color: #999;
@@ -37,43 +100,51 @@ pre {
   background-color: #27181d;
   color: #f14236;
 }
-  </style>
-</head>
-
-<body>
-  <h1>
-    Diff between
-    <span class="removed">${props.commitBefore.slice(0, 8)}</span> (main)
-    and
-    <span class="added">${props.commitAfter.slice(0, 8)}</span> (PR)
-  </h1>
-  <ul>${props.diffs.map(diffToHtml).join('')}</ul>
-</body>
-
-</html>`
+button {
+  font-size: 12px;
+  font-family: var(--font-sans);
+  min-width: 60px;
+  background-color: #444;
+  font-family: inherit;
+  color: white;
 }
-
-function diffToHtml(diff: ProjectDiff) {
-  return `<li>
-  <h2>Project: ${diff.id} - <span class="${diff.type}">${diff.type}<span></h2>
-  <ul>${diff.fields.map(fieldDiffToHtml).join('')}</ul>
-</li>`
+pre, code {
+  font-family: var(--font-mono);
 }
-
-function fieldDiffToHtml(field: FieldDiff) {
-  return `<li>
-  <h3>${field.field}</h3>
-  <pre><code>${field.diff.map(changeToHtml).join('')}</code></pre>
-</li>`
+pre {
+  background-color: #111;
+  margin: 0;
+  padding: 8px;
+  font-weight: normal;
+  font-size: 12px;
 }
-
-function changeToHtml(change: Change) {
-  let className = 'unmodified'
-  if (change.added) {
-    className = 'added'
-  }
-  if (change.removed) {
-    className = 'removed'
-  }
-  return `<span class="${className}">${change.value}</span>`
+h1 {
+  font-size: 24px;
+  font-weight: normal;
+  margin: 16px 0 24px 0;
+}
+h2, h3 {
+  font-size: 14px;
+  font-weight: normal;
+  padding: 8px 0;
+  margin: 0;
+  position: sticky;
+}
+h2 {
+  top: 0;
+  background-color: #222;
+}
+h3 {
+  top: 28px;
+  background-color: #222;
+}
+ul {
+  list-style-type: none;
+  padding-left: 0;
+  margin: 0;
+}
+.field {
+  margin-left: 16px;
+}
+`
 }
