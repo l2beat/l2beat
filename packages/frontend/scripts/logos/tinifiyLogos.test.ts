025fc268f4e663bf33885d180b40442083385b64
torztomasz <tomasz.torz@l2beat.com>
2025-04-08 09:35:33 +0200

    add raas icons

--- packages/frontend/scripts/logos/tinifiyLogos.test.ts
+++ packages/frontend/scripts/logos/tinifiyLogos.test.ts
@@ -1,9 +1,9 @@
-import crypto from 'crypto'
-import { readFileSync, readdirSync } from 'fs'
-import path from 'path'
 import { ProjectService } from '@l2beat/config'
 import { assert } from '@l2beat/shared-pure'
+import crypto from 'crypto'
 import { expect } from 'earl'
+import { readFileSync, readdirSync } from 'fs'
+import path from 'path'
 import { getImageDimensions } from '~/utils/project/get-image-params'
 
 describe('icons', () => {
@@ -13,9 +13,9 @@ describe('icons', () => {
     .filter((x) => x.isFile() && x.name.endsWith('.png'))
     .map((x) => x.name.slice(0, -4))
     .sort()
+  const ps = new ProjectService()
 
   it('every project has an icon', async () => {
-    const ps = new ProjectService()
     const projects = await ps.getProjects({
       // We handle ecosystem logos in a different way
       whereNot: ['ecosystemConfig'],
@@ -27,6 +27,26 @@ describe('icons', () => {
     const requiredIcons = icons.filter((x) => uniqueSlugs.includes(x))
 
     expect(requiredIcons).toEqual(uniqueSlugs)
+  })
+
+  it('every raas has an icon', async () => {
+    const projects = await ps.getProjects({
+      select: ['display'],
+    })
+
+    const raasSlugs = projects
+      .flatMap((x) => {
+        const badgeName = x.display?.badges
+          .find((b) => b.type === 'RaaS')
+          ?.name.toLowerCase()
+        if (!badgeName) return []
+        return [badgeName]
+      })
+      .filter((x, i, a) => a.indexOf(x) === i) // unique
+    console.log(raasSlugs)
+    for (const slug of raasSlugs) {
+      expect(icons).toInclude(slug)
+    }
   })
 
   describe('every icon has proper dimensions, size and has been tinified ', () => {
