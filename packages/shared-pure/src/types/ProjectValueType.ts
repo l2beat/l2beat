edd9c86807e321662022a2077a1c8f9bf66465fc
antooni <antoni.pawlak@protonmail.com>
2025-04-09 13:07:13 +0200

    more comments

--- packages/shared-pure/src/types/ProjectValueType.ts
+++ packages/shared-pure/src/types/ProjectValueType.ts
@@ -1,5 +1,5 @@
 export type ProjectValueType =
-  | 'PROJECT'
+  | 'PROJECT' // Sum of all tokens for project
   | 'PROJECT_WA' // Without Associated
-  | 'SUMMARY'
+  | 'SUMMARY' // Sum of tokens but without double-count e.g. L2 tokens in L3 escrow on this L2. Used for calculating aggregate chart for whole scaling ecosystem
   | 'SUMMARY_WA' // Without Associated
