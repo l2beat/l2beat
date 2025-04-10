7f0600016da081278f0079525016ca2b98695cae
maciekop-l2b <maciej.opala@l2beat.com>
2025-04-10 13:46:08 +0200

    Fix rari situation

--- packages/backend/src/modules/tvs/tools/getTokenSyncRange.test.ts
+++ packages/backend/src/modules/tvs/tools/getTokenSyncRange.test.ts
@@ -3,7 +3,7 @@ import { expect, mockObject } from 'earl'
 import { getTokenSyncRange } from './getTokenSyncRange'
 
 describe(getTokenSyncRange.name, () => {
-  it('should return sync range', async () => {
+  it('should return sync range - until undefined', async () => {
     const token: TvsToken = mockObject<TvsToken>({
       amount: mockAmountFormula(20, 40),
       valueForProject: {
@@ -45,6 +45,51 @@ describe(getTokenSyncRange.name, () => {
     expect(result).toEqual({
       sinceTimestamp: 5,
       untilTimestamp: undefined,
+    })
+  })
+
+  it('should return sync range - until defined', async () => {
+    const token: TvsToken = mockObject<TvsToken>({
+      amount: mockAmountFormula(20, 40),
+      valueForProject: {
+        type: 'calculation',
+        operator: 'sum',
+        arguments: [
+          {
+            type: 'value',
+            priceId: 'priceId',
+            amount: mockAmountFormula(10, 20),
+          },
+          {
+            type: 'value',
+            priceId: 'priceId',
+            amount: mockAmountFormula(10, 20),
+          },
+        ],
+      },
+      valueForSummary: {
+        type: 'calculation',
+        operator: 'sum',
+        arguments: [
+          {
+            type: 'value',
+            priceId: 'priceId',
+            amount: mockAmountFormula(5, 15),
+          },
+          {
+            type: 'value',
+            priceId: 'priceId',
+            amount: mockAmountFormula(10, 50),
+          },
+        ],
+      },
+    })
+
+    const result = getTokenSyncRange(token)
+
+    expect(result).toEqual({
+      sinceTimestamp: 5,
+      untilTimestamp: 50,
     })
   })
 })
