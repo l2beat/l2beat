362ac2a7dd9fe6e542a905e60357d6ac8a7adf6e
torztomasz <tomasz.torz@l2beat.com>
2025-04-11 12:27:19 +0200

    add cache to blobs data

--- packages/frontend/src/server/features/ecosystems/get-blobs-data.tsx
+++ packages/frontend/src/server/features/ecosystems/get-blobs-data.tsx
@@ -1,6 +1,8 @@
 import type { Project } from '@l2beat/config'
 import { assert, ProjectId, UnixTime } from '@l2beat/shared-pure'
 import { keyBy } from 'lodash'
+import { unstable_cache as cache } from 'next/cache'
+import { env } from '~/env'
 import { getDb } from '~/server/database'
 
 export interface BlobsData {
@@ -11,35 +13,55 @@ export interface BlobsData {
 export async function getBlobsData(
   projects: Project<'scalingInfo'>[],
 ): Promise<BlobsData> {
-  const now = UnixTime.toStartOf(UnixTime.now(), 'day') - UnixTime.DAY
+  if (env.MOCK) {
+    return getMockBlobsData()
+  }
+  return getCachedBlobsData(projects)
+}
 
-  const db = getDb()
-  const records = await db.dataAvailability.getForDaLayerByTimestamp(
-    ProjectId.ETHEREUM,
-    now,
-  )
+const getCachedBlobsData = cache(
+  async (projects: Project<'scalingInfo'>[]): Promise<BlobsData> => {
+    const now = UnixTime.toStartOf(UnixTime.now(), 'day') - UnixTime.DAY
 
-  const ethereumRecord = records.find(
-    (record) => record.projectId === ProjectId.ETHEREUM,
-  )
-  assert(ethereumRecord, 'Ethereum record not found')
+    const db = getDb()
+    const records = await db.dataAvailability.getForDaLayerByTimestamp(
+      ProjectId.ETHEREUM,
+      now,
+    )
 
-  const recordsByProjectId = keyBy(records, (e) => e.projectId)
+    const ethereumRecord = records.find(
+      (record) => record.projectId === ProjectId.ETHEREUM,
+    )
+    assert(ethereumRecord, 'Ethereum record not found')
 
+    const recordsByProjectId = keyBy(records, (e) => e.projectId)
 
-  const totalData = projects.reduce((acc, project) => {
-    const projectRecord = recordsByProjectId[project.id]
-    if (!projectRecord) {
-      console.info(`No record found for ${project.id}`)
-      return acc
+    const totalData = projects.reduce((acc, project) => {
+      const projectRecord = recordsByProjectId[project.id]
+      if (!projectRecord) {
+        console.info(`No record found for ${project.id}`)
+        return acc
+      }
+      return acc + Number(projectRecord.totalSize)
+    }, 0)
+
+    const blobsShare = totalData / Number(ethereumRecord.totalSize)
+
+    return {
+      totalData,
+      blobsShare,
     }
-    return acc + Number(projectRecord.totalSize)
-  }, 0)
+  },
+  ['blobs-data'],
+  {
+    tags: ['hourly-data'],
+    revalidate: UnixTime.HOUR,
+  },
+)
 
-  const blobsShare = totalData / Number(ethereumRecord.totalSize)
-
+function getMockBlobsData(): BlobsData {
   return {
-    totalData,
-    blobsShare,
+    totalData: 1000000,
+    blobsShare: 0.15,
   }
 }
