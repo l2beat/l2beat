53b553f611a16655a6760db621aa6d432e647593
Maciej Zygmunt <maciekzygmunt@interia.pl>
2025-04-14 14:00:55 +0300

    Introduce tabs on Bridges (#7470)

--- packages/frontend/src/app/(side-nav)/bridges/summary/_components/table/bridges-summary-table.tsx
+++ packages/frontend/src/app/(side-nav)/bridges/summary/_components/table/bridges-summary-table.tsx
@@ -1,30 +1,21 @@
 'use client'
 
 import { getCoreRowModel, getSortedRowModel } from '@tanstack/react-table'
-import { useMemo } from 'react'
 import { BasicTable } from '~/components/table/basic-table'
 import { useTable } from '~/hooks/use-table'
-import { bridgesSummaryActiveColumns } from './columns'
+import { getBridgesSummaryActiveColumns } from './columns'
 
-import { TableFilters } from '~/components/table/filters/table-filters'
-import { useFilterEntries } from '~/components/table/filters/use-filter-entries'
 import type { BridgesSummaryEntry } from '~/server/features/bridges/get-bridges-summary-entries'
 
 export interface Props {
   entries: BridgesSummaryEntry[]
+  isOthers?: boolean
 }
 
-export function BridgesSummaryTable({ entries }: Props) {
-  const filterEntries = useFilterEntries()
-
-  const filteredEntries = useMemo(
-    () => entries.filter(filterEntries),
-    [entries, filterEntries],
-  )
-
+export function BridgesSummaryTable({ entries, isOthers }: Props) {
   const activeTable = useTable({
-    data: filteredEntries,
-    columns: bridgesSummaryActiveColumns,
+    data: entries,
+    columns: getBridgesSummaryActiveColumns(isOthers),
     getCoreRowModel: getCoreRowModel(),
     getSortedRowModel: getSortedRowModel(),
     manualFiltering: true,
@@ -41,10 +32,5 @@ export function BridgesSummaryTable({ entries }: Props) {
     },
   })
 
-  return (
-    <div className="space-y-3 md:space-y-6">
-      <TableFilters entries={entries} />
-      <BasicTable table={activeTable} />
-    </div>
-  )
+  return <BasicTable table={activeTable} />
 }
