3c176ec6de5e9f8b76773325b631684f3b5d32b7
Hubert Kotliński <75430263+Huberacht@users.noreply.github.com>
2025-04-23 15:52:00 +0000

    Add Surge as upcoming

---
+++ packages/config/src/projects/surge/surge.ts
@@ -0,0 +1,23 @@
+import { UnixTime } from '@l2beat/shared-pure'
+import type { ScalingProject } from '../../internalTypes'
+import { upcomingL2 } from '../../templates/upcoming'
+
+export const surge: ScalingProject = upcomingL2({
+  id: 'surge',
+  capability: 'universal',
+  addedAt: UnixTime(1745423262), // 2025-04-23T17:47:35Z
+  display: {
+    name: 'Surge',
+    slug: 'surge',
+    description:
+      'Surge is based rollup showcasing the Nethermind client's Gigagas performance, built on the Taiko stack.',
+    purposes: ['Universal'],
+    category: 'ZK Rollup',
+    stack: 'Surge',
+    links: {
+      websites: ['https://www.surge.wtf/'],
+      repositories: ['https://github.com/nethermindEth/surge'],
+      socialMedia: ['https://x.com/NethermindEth'],
+    },
+  },
+})
