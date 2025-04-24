b4f2ac292237a1ea06587e300060eb245a35c3f2
Radina Talanova <nt.radina@gmail.com>
2025-04-24 15:58:01 +0300

    fixes

---
+++ packages/config/src/projects/spire/spire.ts
@@ -0,0 +1,25 @@
+import { UnixTime } from '@l2beat/shared-pure'
+import type { ScalingProject } from '../../internalTypes'
+import { upcomingL2 } from '../../templates/upcoming'
+
+export const spire: ScalingProject = upcomingL2({
+  id: 'spire',
+  addedAt: UnixTime(1745421141), // '2025-04-23T17:12:38Z'
+  capability: 'universal',
+  display: {
+    name: 'Spire',
+    slug: 'spire',
+    description:
+      'Spire is an upcoming appchain stack, focused on scaling Ethereum while maintaining close integration and alignment with L1.',
+    purposes: ['Universal'],
+    category: 'ZK Rollup',
+    links: {
+      websites: ['https://www.spire.dev/'],
+      apps: ['https://bridge.hoodi.surge.wtf/'],
+      documentation: ['https://docs.spire.dev/'],
+      explorers: ['https://explorer.hoodi.surge.wtf'],
+      repositories: ['https://github.com/spire-labs'],
+      socialMedia: ['https://x.com/Spire_Labs', 'https://paragraph.com/@spire'],
+    },
+  },
+})
