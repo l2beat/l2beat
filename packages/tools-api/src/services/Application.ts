1910997dead64695ea4abaa789aefa92733350f4
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2025-04-24 10:20:22 +0200

    Add SignatureClient

--- packages/tools-api/src/services/Application.ts
+++ packages/tools-api/src/services/Application.ts
@@ -1,6 +1,9 @@
 import { Logger } from '@l2beat/backend-tools'
 import type { Config } from '../config/types'
+import { SignatureClient } from '../domain/SignatureClient'
 import { createHttpServer } from './HttpServer'
+import { FourByteClient } from './api/FourByteClient'
+import { OpenChainClient } from './api/OpenChainClient'
 
 export class Application {
   start: () => Promise<void>
@@ -9,10 +12,19 @@ export class Application {
     const logger = Logger.INFO
     const appLogger = logger.for(this)
 
+    const openChainClient = new OpenChainClient()
+    const fourByteClient = new FourByteClient()
+    const signatureClient = new SignatureClient(
+      openChainClient,
+      fourByteClient,
+      logger,
+    )
+
     const httpServer = createHttpServer(config, logger.for('Router'))
 
     this.start = async () => {
       await httpServer.start()
+      console.log(await signatureClient.lookup('0xa9059cbb'))
       appLogger.info('Started')
     }
   }
