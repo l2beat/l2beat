8fbea3cc5ab3292123c124d7ef8c816b6265acd2
torztomasz <tomasz.torz@l2beat.com>
2025-04-25 10:37:02 +0200

    format fix

--- packages/frontend/src/utils/project/get-image-params.ts
+++ packages/frontend/src/utils/project/get-image-params.ts
@@ -1,6 +1,6 @@
 import { readFileSync } from 'fs'
-import imageSize from 'image-size'
 import path from 'path'
+import imageSize from 'image-size'
 import { env } from '~/env'
 import { getManifest } from '~/utils/Manifest'
 
