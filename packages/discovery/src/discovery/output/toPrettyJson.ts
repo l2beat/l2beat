98696c53b54cd5af57588cb7c9d2da689d38b1ee
Mateusz Radomski <radomski.main@protonmail.com>
2025-05-08 15:58:25 +0200

    Fix linting and formatting mistakes

--- packages/discovery/src/discovery/output/toPrettyJson.ts
+++ packages/discovery/src/discovery/output/toPrettyJson.ts
@@ -1,6 +1,6 @@
-import { format } from 'prettier/standalone'
 import * as parserBabel from 'prettier/plugins/babel'
 import * as parserEstree from 'prettier/plugins/estree'
+import { format } from 'prettier/standalone'
 
 export async function toPrettyJson(value: unknown): Promise<string> {
   const ugly = JSON.stringify(value)
