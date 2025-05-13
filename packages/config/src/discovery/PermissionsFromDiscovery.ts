de5ce50b1c64aff6632aa43a27f0f86d28a1b2cd
Adrian Adamiak <adrian@adamiak.net>
2025-05-13 10:39:27 +0000

    Display legacy roles in permissions

--- packages/config/src/discovery/PermissionsFromDiscovery.ts
+++ packages/config/src/discovery/PermissionsFromDiscovery.ts
@@ -1,27 +1,15 @@
-import type {
-  EntryParameters,
-  Permission,
-  ReceivedPermission,
-  ResolvedPermissionPath,
-} from '@l2beat/discovery'
-import {
-  type EthereumAddress,
-  formatSeconds,
-} from '@l2beat/shared-pure'
+import type { EntryParameters, ReceivedPermission } from '@l2beat/discovery'
+import { type EthereumAddress, formatSeconds } from '@l2beat/shared-pure'
 // biome-ignore lint: chain() can't be imported individually as lodash/chain
 import { chain } from 'lodash'
 import groupBy from 'lodash/groupBy'
 import sum from 'lodash/sum'
 import type { PermissionRegistry } from './PermissionRegistry'
 import type { ProjectDiscovery } from './ProjectDiscovery'
-import {
-  DirectPermissionToPrefix,
-  UltimatePermissionToPrefix,
-} from './descriptions'
+import { UltimatePermissionToPrefix } from './descriptions'
 import {
   formatPermissionCondition,
   formatPermissionDelay,
-  formatPermissionDescription,
   isMultisigLike,
   trimTrailingDots,
 } from './utils'
@@ -119,129 +107,31 @@ export class PermissionsFromDiscovery implements PermissionRegistry {
       .value()
   }
 
+  describeLegacyPermissions(contractOrEoa: EntryParameters) {
+    const excludedPermissions: ReceivedPermission['permission'][] = [
+      'act',
+      'upgrade',
+      'interact',
+      'member',
+    ] satisfies ReceivedPermission['permission'][]
+    return chain(contractOrEoa.receivedPermissions ?? [])
+      .filter((p) => !excludedPermissions.includes(p.permission))
+      .map((p) => {
+        const prefix = UltimatePermissionToPrefix[p.permission]
+        const via = this.formatPermissionVia(p)
+        return `${prefix} ${via}`
+      })
+      .value()
+  }
+
   describePermissions(
     contractOrEoa: EntryParameters,
     _includeDirectPermissions: boolean = true,
   ) {
-    console.log(`> ${contractOrEoa.name} (${contractOrEoa.type})`)
     const upgrade = this.describeUpgradePermissions(contractOrEoa)
-    console.log(upgrade)
     const interact = this.describeInteractPermissions(contractOrEoa)
-    console.log(interact)
-    return [...upgrade, ...interact].filter((s) => s !== '')
-
-    // return [
-    //   ...(includeDirectPermissions
-    //     ? this.describeDirectlyReceivedPermissions(contractOrEoa)
-    //     : []),
-    //   ...this.describeUltimatelyReceivedPermissions(contractOrEoa),
-    // ].filter(notUndefined)
-  }
-
-  describeDirectlyReceivedPermissions(
-    contractOrEoa: EntryParameters,
-  ): string[] {
-    return Object.entries(
-      groupBy(
-        contractOrEoa.directlyReceivedPermissions ?? [],
-        (value: ReceivedPermission) =>
-          [
-            value.permission,
-            value.description ?? '',
-            value.condition ?? '',
-            value.delay?.toString() ?? '',
-          ].join('►'),
-      ),
-    ).map(([key, entries]) => {
-      const permission = key.split('►')[0] as Permission
-      const description = key.split('►')[1] ?? ''
-      const condition = key.split('►')[2] ?? ''
-      const delay = key.split('►')[3] ?? ''
-      const permissionsRequiringTarget: Permission[] = [
-        'interact',
-        'upgrade',
-        'act',
-      ]
-      const showTargets = permissionsRequiringTarget.includes(permission)
-      const addressesString = showTargets
-        ? entries
-            .map(
-              (entry) =>
-                this.projectDiscovery.getContract(entry.from.toString()).name,
-            )
-            .join(', ')
-        : ''
-
-      return `${[
-        DirectPermissionToPrefix[permission],
-        addressesString,
-        formatPermissionDescription(description),
-        formatPermissionCondition(condition),
-        delay === '' ? '' : formatPermissionDelay(Number(delay)),
-      ]
-        .filter((s) => s !== '')
-        .join(' ')
-        .trim()}.`
-    })
-  }
-
-  describeUltimatelyReceivedPermissions(
-    contractOrEoa: EntryParameters,
-  ): string[] {
-    const formatVia = (via: ResolvedPermissionPath[]) =>
-      `- acting via ${via.map((p) => this.projectDiscovery.formatViaPath(p)).join(', ')}`
-
-    return Object.entries(
-      groupBy(
-        contractOrEoa.receivedPermissions ?? [],
-        (value: ReceivedPermission) => {
-          return [
-            value.permission,
-            value.via !== undefined ? formatVia(value.via) : '',
-            value.description ?? '',
-            value.condition ?? '',
-            value.delay?.toString() ?? '',
-          ].join('►')
-        },
-      ),
-    ).map(([key, entries]) => {
-      const permission = key.split('►')[0] as Permission
-      const via = key.split('►')[1] ?? ''
-      const description = key.split('►')[2] ?? ''
-      const condition = key.split('►')[3] ?? ''
-      const delay = key.split('►')[4] ?? ''
-      const prefix = UltimatePermissionToPrefix[permission]
-      if (prefix === undefined) {
-        return ''
-      }
-
-      const permissionsRequiringTarget: Permission[] = [
-        'interact',
-        'upgrade',
-        'act',
-      ]
-      const showTargets = permissionsRequiringTarget.includes(permission)
-      const addressesString = showTargets
-        ? entries
-            .map(
-              (entry) =>
-                this.projectDiscovery.getContract(entry.from.toString()).name,
-            )
-            .join(', ')
-        : ''
-
-      return `${[
-        UltimatePermissionToPrefix[permission as Permission],
-        addressesString,
-        formatPermissionDescription(description),
-        formatPermissionCondition(condition),
-        delay === '' ? '' : formatPermissionDelay(Number(delay)),
-        via,
-      ]
-        .filter((s) => s !== '')
-        .join(' ')
-        .trim()}.`
-    })
+    const legacyRoles = this.describeLegacyPermissions(contractOrEoa)
+    return [...upgrade, ...interact, ...legacyRoles].filter((s) => s !== '')
   }
 
   getUpgradableBy(
