ce93a62f7cb113116ef9cc9e172556fa1959d0af
torztomasz <tomasz.torz@l2beat.com>
2025-05-14 12:42:05 +0200

    remove cache

--- packages/frontend/src/server/features/projects-change-report/get-projects-change-report.ts
+++ packages/frontend/src/server/features/projects-change-report/get-projects-change-report.ts
@@ -1,18 +1,8 @@
 import type { UpdateDiffRecord } from '@l2beat/database'
 import { EthereumAddress } from '@l2beat/shared-pure'
-import { UnixTime } from '@l2beat/shared-pure'
 import groupBy from 'lodash/groupBy'
-import { unstable_cache as cache } from 'next/cache'
 import { env } from '~/env'
 import { getDb } from '~/server/database'
-
-export async function getProjectsChangeReport() {
-  if (env.MOCK) {
-    return getProjectsChangeReportMock()
-  }
-
-  return getProjectsChangeReportWithFns()
-}
 
 export type ProjectsChangeReport = Awaited<
   ReturnType<typeof getProjectsChangeReportWithFns>
@@ -22,8 +12,50 @@ export interface ProjectChanges {
   impactfulChange: boolean
 }
 
+type ProjectChangeReport = Record<
+  string,
+  {
+    implementationChange: EthereumAddress[]
+    highSeverityFieldChange: EthereumAddress[]
+    ultimateUpgraderChange: EthereumAddress[]
+  }
+>
+
+export async function getProjectsChangeReport() {
+  if (env.MOCK) {
+    return getProjectsChangeReportMock()
+  }
+
+  return getProjectsChangeReportWithFns()
+}
+
 async function getProjectsChangeReportWithFns() {
-  const result = await getCachedProjectsChangeReport()
+  const result: Record<string, ProjectChangeReport> = {}
+
+  const db = getDb()
+  const updateDiffs = await db.updateDiff.getAll()
+
+  const byProject = groupBy(updateDiffs, (diff) => diff.projectName)
+  for (const [projectName, diffs] of Object.entries(byProject)) {
+    const byChain = groupBy(diffs, (diff) => diff.chain)
+    for (const [chain, changes] of Object.entries(byChain)) {
+      const changesByType = groupByType(changes)
+
+      result[projectName] ??= {}
+      result[projectName][chain] = {
+        implementationChange: changesByType.implementationChange.map((c) =>
+          EthereumAddress(c.address),
+        ),
+        highSeverityFieldChange: changesByType.highSeverityFieldChange.map(
+          (c) => EthereumAddress(c.address),
+        ),
+        ultimateUpgraderChange: changesByType.ultimateUpgraderChange.map((c) =>
+          EthereumAddress(c.address),
+        ),
+      }
+    }
+  }
+
   return {
     projects: result,
     getChanges: function (projectId: string): ProjectChanges {
@@ -57,49 +89,6 @@ async function getProjectsChangeReportWithFns() {
     },
   }
 }
-
-type ProjectChangeReport = Record<
-  string,
-  {
-    implementationChange: EthereumAddress[]
-    highSeverityFieldChange: EthereumAddress[]
-    ultimateUpgraderChange: EthereumAddress[]
-  }
->
-
-const getCachedProjectsChangeReport = cache(
-  async () => {
-    const result: Record<string, ProjectChangeReport> = {}
-
-    const db = getDb()
-    const updateDiffs = await db.updateDiff.getAll()
-
-    const byProject = groupBy(updateDiffs, (diff) => diff.projectName)
-    for (const [projectName, diffs] of Object.entries(byProject)) {
-      const byChain = groupBy(diffs, (diff) => diff.chain)
-      for (const [chain, changes] of Object.entries(byChain)) {
-        const changesByType = groupByType(changes)
-
-        result[projectName] ??= {}
-        result[projectName][chain] = {
-          implementationChange: changesByType.implementationChange.map((c) =>
-            EthereumAddress(c.address),
-          ),
-          highSeverityFieldChange: changesByType.highSeverityFieldChange.map(
-            (c) => EthereumAddress(c.address),
-          ),
-          ultimateUpgraderChange: changesByType.ultimateUpgraderChange.map(
-            (c) => EthereumAddress(c.address),
-          ),
-        }
-      }
-    }
-
-    return result
-  },
-  [`projectsChangeReport-${env.VERCEL_GIT_COMMIT_SHA}`],
-  { tags: ['hourly-data'], revalidate: UnixTime.HOUR },
-)
 
 function groupByType(changes: UpdateDiffRecord[]) {
   const result: Record<UpdateDiffRecord['type'], UpdateDiffRecord[]> = {
