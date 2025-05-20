43014823721d7dd1e4bd764c1fd183bd838cfa13
Landon Gingerich <landon.ging@gmail.com>
2025-05-20 18:26:38 +0000

    feat: update sxt to live

--- packages/config/src/projects/sxt/sxt.ts
+++ packages/config/src/projects/sxt/sxt.ts
@@ -1,23 +1,24 @@
-import { UnixTime } from '@l2beat/shared-pure'
+import { ProjectId, UnixTime } from '@l2beat/shared-pure'
+import { ProjectDiscovery } from '../../discovery/ProjectDiscovery'
 import type { ScalingProject } from '../../internalTypes'
-import { upcomingL2 } from '../../templates/upcoming'
+import { zkStackL2 } from '../../templates/zkStack'
 
-export const sxt: ScalingProject = upcomingL2({
-  id: 'sxt',
-  capability: 'universal',
+const genesisTimestamp = UnixTime(1744071776) // 2025-04-07T19:22:56Z
+const chainId = 1217
+const discovery = new ProjectDiscovery('sxt')
+
+export const sxt: ScalingProject = zkStackL2({
   addedAt: UnixTime(1716819511), // 2024-05-27T14:18:31Z
   display: {
     name: 'Space and Time',
     slug: 'sxt',
     description:
-      "Space and Time (SxT) is a decentralized data warehouse that aims to provide a zk 'Proof of SQL' to bring offchain data to smart contracts onchain. Built on ZK Stack, the SxT hyperchain will serve as a settlement layer and payment hub for data queries.",
-    purposes: ['AI', 'Information'],
-    category: 'ZK Rollup',
-    stack: 'ZK Stack',
+      "Space and Time (SxT) is a decentralized data warehouse that aims to provide a zk 'Proof of SQL' to bring offchain data to smart contracts onchain. Built on ZK Stack, the SxT chain will serve as a settlement layer and payment hub for data queries.",
     links: {
       websites: ['https://spaceandtime.io'],
       apps: ['https://app.spaceandtime.ai'],
       documentation: ['https://docs.spaceandtime.io'],
+      explorers: ['https://spaceandtime.calderaexplorer.xyz'],
       repositories: ['https://github.com/spaceandtimelabs'],
       socialMedia: [
         'https://x.com/SpaceandTimeDB',
@@ -30,4 +31,35 @@ export const sxt: ScalingProject = upcomingL2({
       ],
     },
   },
+  ecosystemInfo: {
+    id: ProjectId('the-elastic-network'),
+  },
+  chainConfig: {
+    name: 'sxt',
+    chainId,
+    explorerUrl: 'https://spaceandtime.calderaexplorer.xyz',
+    sinceTimestamp: genesisTimestamp,
+    apis: [
+      {
+        type: 'rpc',
+        url: 'https://spaceandtime.calderachain.xyz/http',
+        callsPerMinute: 1500,
+      },
+      {
+        type: 'blockscoutV2',
+        url: 'https://spaceandtime.calderaexplorer.xyz/api/v2',
+      },
+    ],
+  },
+  discovery,
+  diamondContract: discovery.getContract(''), // TODO
+  milestones: [
+    {
+      title: 'Mainnet Launch',
+      url: 'https://x.com/SpaceandTimeDB/status/1920873080471384271',
+      date: '2025-05-09T00:00:00Z',
+      description: 'Space and Time is live on mainnet.',
+      type: 'general',
+    },
+  ],
 })
