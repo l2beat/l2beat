40bde0ac77dbcd115185dfa472bb876b69dc4896
Maciej Zygmunt <maciekzygmunt@interia.pl>
2025-05-21 16:12:47 +0300

    Fix bridges table error (#7847)

--- packages/frontend/src/app/(side-nav)/bridges/summary/_components/table/bridges-summary-singlechain-table.tsx
+++ packages/frontend/src/app/(side-nav)/bridges/summary/_components/table/bridges-summary-singlechain-table.tsx
@@ -21,7 +21,7 @@ export function BridgesSummarySingleChainTable({ entries }: Props) {
     initialState: {
       sorting: [
         {
-          id: 'total',
+          id: 'tvs',
           desc: true,
         },
       ],
