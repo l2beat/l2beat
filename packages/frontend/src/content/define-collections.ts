19cad394c055ea5a198e4968d847ae0d577bda8b
Mateusz Radomski <radomski.main@protonmail.com>
2025-05-22 14:10:33 +0200

    Fix defineCollection function and types

--- packages/frontend/src/content/define-collections.ts
+++ packages/frontend/src/content/define-collections.ts
@@ -1,6 +1,7 @@
+import { z } from 'zod/v4'
 type CollectionType = 'data' | 'content'
 
-interface CollectionInput<T extends CollectionType, S extends Zod.Schema> {
+interface CollectionInput<T extends CollectionType, S extends z.Schema> {
   type: T
   schema: S
 }
@@ -12,7 +13,7 @@ const typeToExtension = {
 
 export function defineCollection<
   T extends CollectionType,
-  S extends Zod.Schema,
+  S extends z.Schema,
 >(input: CollectionInput<T, S>) {
   return {
     type: input.type,
