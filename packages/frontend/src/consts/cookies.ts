fa8c44abc80e6b9e0626e6742dfe2a8d9d889999
torztomasz <tomasz.torz@l2beat.com>
2025-05-22 10:37:00 +0200

    comment out example

--- packages/frontend/src/consts/cookies.ts
+++ packages/frontend/src/consts/cookies.ts
@@ -5,7 +5,7 @@ import { z } from 'zod'
  * Known cookie definitions.
  */
 export const knownCookies = {
-  example: knownCookie('example', z.string(), 'example'),
+  // example: knownCookie('example', z.string(), 'example'),
   recategorisationPreview: knownCookie(
     'recategorisationPreview',
     z.boolean(),
