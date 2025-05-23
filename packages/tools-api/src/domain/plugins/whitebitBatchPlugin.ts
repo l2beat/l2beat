98f5b92fe5cdef1e8cd484a4caece07cb7f30c7e
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2025-05-23 17:20:14 +0200

    Remove extra in plugin

--- packages/tools-api/src/domain/plugins/whitebitBatchPlugin.ts
+++ packages/tools-api/src/domain/plugins/whitebitBatchPlugin.ts
@@ -22,6 +22,7 @@ export function whitebitBatchPlugin(
     try {
       const value = decodeBatch(call.extra, chain, tokens)
       call.arguments = [value]
+      call.extra = undefined
       return []
     } catch {}
   }
