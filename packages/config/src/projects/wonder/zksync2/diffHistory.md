9ecb9942317288697845fb5eb7c80eb567b14958
Landon Gingerich <landon.ging@gmail.com>
2025-05-28 02:19:57 -0500

    Update zk stack (#7838)
    Co-authored-by: sekuba <29250140+sekuba@users.noreply.github.com>
    Co-authored-by: maciekop-l2b <166010253+maciekop-l2b@users.noreply.github.com>
    Co-authored-by: torztomasz <tomasz.torz@l2beat.com>

---
+++ packages/config/src/projects/wonder/zksync2/diffHistory.md
@@ -0,0 +1,10 @@
+Generated with discovered.json: 0x0589eded830caf71df17738ce077ee67f2569fd4
+
+# Diff at Tue, 27 May 2025 15:26:27 GMT:
+
+- author: sekuba (<29250140+sekuba@users.noreply.github.com>)
+- current block number: 60986305
+
+## Description
+
+Provide description of changes. This section will be preserved.
--- packages/l2b/src/implementations/fetchZkStack.ts
+++ packages/l2b/src/implementations/fetchZkStack.ts
@@ -83,11 +83,12 @@ export class ZkStackDataFetcher {
     '325': 'GRVT',
     '61166': 'Treasure Chain',
     '1345': 'OpenZK',
-    '9637': 'Lumoz',
+    '9637': 'Wonder',
     '320': 'ZKcandy',
     '232': 'Lens',
     '2904': 'LaChain',
     '9075': 'Gateway',
+    '1217': 'SxT',
   }
 
   private tableFields: TableField[] = [
