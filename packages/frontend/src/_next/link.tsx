f7a0b0bb8c15d23d07e2f0d1c2545095e484b407
torztomasz <tomasz.torz@l2beat.com>
2025-05-28 13:56:17 +0200

    remove link replacement

--- packages/frontend/src/_next/link.tsx
+++
@@ -1,7 +0,0 @@
-import type { LinkProps } from '~/_next/link'
-
-export default function Link(props: LinkProps & { href: string }) {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { prefetch, ...stripped } = props
-  return <a {...stripped} />
-}
