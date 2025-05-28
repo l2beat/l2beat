3c0f017bf84a594333550b75eb81afaaf55dc541
torztomasz <tomasz.torz@l2beat.com>
2025-05-28 10:29:18 +0200

    glossary

--- packages/frontend/src/pages/glossary/GlossaryPage.tsx
+++ packages/frontend/src/pages/glossary/GlossaryPage.tsx
@@ -1,7 +1,12 @@
-import type { CollectionEntry } from '~/content/get-collection'
 import { SideNavLayout } from '~/app/(side-nav)/side-nav-layout'
 import { AppLayout, type AppLayoutProps } from '~/app/_layout'
-import { GlossaryPage as NextGlossaryPage } from '../../app/(side-nav)/glossary/_page'
+import { MainPageHeader } from '~/components/main-page-header'
+import { PrimaryCard } from '~/components/primary-card/primary-card'
+import { ScrollToTopButton } from '~/components/scroll-to-top-button'
+import type { CollectionEntry } from '~/content/get-collection'
+import { GlossaryItem } from './components/glossary-item'
+import { Header } from './components/header'
+import { GlossarySideNav } from './components/side-nav/glossary-side-nav'
 
 interface Props extends AppLayoutProps {
   glossaryEntries: CollectionEntry<'glossary'>[]
@@ -11,7 +16,17 @@ export function GlossaryPage({ glossaryEntries, ...props }: Props) {
   return (
     <AppLayout {...props}>
       <SideNavLayout>
-        <NextGlossaryPage glossaryEntries={glossaryEntries} />
+        <MainPageHeader className="relative z-10">Glossary</MainPageHeader>
+        <Header glossaryEntries={glossaryEntries} />
+        <main className="mt-0 flex gap-8 border-t border-divider md:-mt-2 md:border-t-0">
+          <PrimaryCard className="">
+            {glossaryEntries.map((entry) => (
+              <GlossaryItem key={entry.id} entry={entry} />
+            ))}
+          </PrimaryCard>
+          <GlossarySideNav entries={glossaryEntries} />
+        </main>
+        <ScrollToTopButton />
       </SideNavLayout>
     </AppLayout>
   )
