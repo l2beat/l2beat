02ba084cf153850ba3302c43e7daa858260b25e2
torztomasz <tomasz.torz@l2beat.com>
2025-05-28 13:18:29 +0200

    bridges project

--- packages/frontend/src/pages/bridges/components/bridges-header.tsx
+++ packages/frontend/src/pages/bridges/components/bridges-header.tsx
@@ -1,5 +1,5 @@
-import { bridgeWarningContext } from '~/app/(top-nav)/bridges/projects/[slug]/_components/bridges-mvp-warning'
 import { MainPageHeader } from '~/components/main-page-header'
+import { bridgeWarningContext } from '~/pages/bridges/project/components/bridges-mvp-warning'
 
 export function BridgesHeader({ children }: { children: string }) {
   return (
