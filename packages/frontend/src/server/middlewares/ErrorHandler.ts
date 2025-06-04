fc304527fc9d10f9325f0973d218ca9c9ed5b07f
Tomasz Tórz <93620601+torztomasz@users.noreply.github.com>
2025-06-04 14:30:38 +0200

    Add UUID to error log (#7983)

--- packages/frontend/src/server/middlewares/ErrorHandler.ts
+++ packages/frontend/src/server/middlewares/ErrorHandler.ts
@@ -1,15 +1,18 @@
+import { randomUUID } from 'crypto'
 import type { Logger } from '@l2beat/backend-tools'
 import type { NextFunction, Request, Response } from 'express'
 
 export function ErrorHandler(appLogger: Logger) {
   return (err: Error, req: Request, res: Response, _: NextFunction) => {
+    const errorId = randomUUID()
     res.status(500)
     appLogger.error('Error processing request', {
       error: err instanceof Error ? err.message : String(err),
       method: req.method,
       url: req.originalUrl,
+      errorId,
       status: res.statusCode,
     })
-    res.send('Internal Server Error')
+    res.send(`Internal Server Error\n\n Error ID: ${errorId}`)
   }
 }
