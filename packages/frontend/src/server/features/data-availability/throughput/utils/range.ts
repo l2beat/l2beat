6bbe5cea371eabe0d487a6619a5338e8b8628fd7
maciekzygmunt <maciekzygmunt@interia.pl>
2025-06-04 14:27:38 +0300

    update main thorughput charts

--- packages/frontend/src/server/features/data-availability/throughput/utils/range.ts
+++ packages/frontend/src/server/features/data-availability/throughput/utils/range.ts
@@ -1,4 +1,19 @@
 import { z } from 'zod'
+import { rangeToDays } from '~/utils/range/rangeToDays'
 
 export type DaThroughputTimeRange = z.infer<typeof DaThroughputTimeRange>
-export const DaThroughputTimeRange = z.enum(['30d', '90d', '180d', '1y', 'max'])
+export const DaThroughputTimeRange = z.enum([
+  '7d',
+  '30d',
+  '90d',
+  '180d',
+  '1y',
+  'max',
+])
+
+export function rangeToResolution(range: DaThroughputTimeRange) {
+  const days = rangeToDays(range)
+  if (days && days <= 7) return 'hourly'
+  if (days && days < 180) return 'sixHourly'
+  return 'daily'
+}
