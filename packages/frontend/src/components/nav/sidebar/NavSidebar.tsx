ee179568839f45e5bc737f7a710715d0b73cc7eb
torztomasz <tomasz.torz@l2beat.com>
2025-06-10 10:44:17 +0200

    fix

--- packages/frontend/src/components/nav/sidebar/NavSidebar.tsx
+++ packages/frontend/src/components/nav/sidebar/NavSidebar.tsx
@@ -208,5 +208,5 @@ function NavCollapsibleItem({
 }
 
 function getIsActive(href: string, pathname: string) {
-  return pathname.startsWith(href)
+  return pathname === href || pathname.startsWith(href + '/')
 }
