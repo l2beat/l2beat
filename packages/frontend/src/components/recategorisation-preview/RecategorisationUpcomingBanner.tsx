7653920085df454c5ab9a69e2f3720519c3f5bf0
maciekzygmunt <maciekzygmunt@interia.pl>
2025-06-11 14:23:09 +0200

    update

--- packages/frontend/src/components/recategorisation-preview/RecategorisationUpcomingBanner.tsx
+++ packages/frontend/src/components/recategorisation-preview/RecategorisationUpcomingBanner.tsx
@@ -1,8 +1,10 @@
 import { PROJECT_COUNTDOWNS } from '@l2beat/config/build/global/countdowns'
 
 import { UnixTime } from '@l2beat/shared-pure'
+import { useEffect, useState } from 'react'
+import { useInterval } from '~/hooks/useInterval'
 import { cn } from '~/utils/cn'
-import { Countdown } from '../Countdown'
+import { MemoizedTimePart, getTimeParts } from '../Countdown'
 import { CustomLink } from '../link/CustomLink'
 
 export function RecategorisationUpcomingBanner({
@@ -20,13 +22,10 @@ export function RecategorisationUpcomingBanner({
         className,
       )}
     >
-      <span className="text-sm">Recategorisation happening in</span>
-      <Countdown
-        expiresAt={PROJECT_COUNTDOWNS.otherMigration}
-        shortSuffix={false}
-        size="xs"
-        timePartClassName="bg-white text-black"
-      />
+      <span className="font-semibold text-base">
+        Recategorisation happening in
+      </span>
+      <BannerCountdown expiresAt={PROJECT_COUNTDOWNS.otherMigration} />
       <BannerActionButton />
     </div>
   )
@@ -44,5 +43,48 @@ function BannerActionButton() {
         <span className="font-medium text-xs">Learn more</span>
       </div>
     </CustomLink>
+  )
+}
+
+function BannerCountdown({ expiresAt }: { expiresAt: number }) {
+  const [secondsLeft, setSecondsLeft] = useState<number | undefined>(undefined)
+
+  useEffect(() => {
+    setSecondsLeft(expiresAt - UnixTime.now())
+  }, [expiresAt])
+
+  useInterval(() => {
+    setSecondsLeft((timeLeft) => (timeLeft ? timeLeft - 1 : 0))
+  }, 1000)
+
+  if (secondsLeft === undefined) {
+    return (
+      <div className="flex gap-1">
+        {Array.from({ length: 4 }).map((_, i) => (
+          <div key={i} className="h-[35px] w-[72px] rounded bg-white" />
+        ))}
+      </div>
+    )
+  }
+
+  const { days, hours, minutes, seconds } = getTimeParts(secondsLeft)
+
+  const timePartClass = 'h-[35px] w-[72px] bg-white text-black'
+
+  return (
+    <div className={cn('flex w-max items-center justify-center gap-x-1')}>
+      <MemoizedTimePart suffix={'days'} size="xs" className={timePartClass}>
+        {days}
+      </MemoizedTimePart>
+      <MemoizedTimePart suffix={'hrs'} size="xs" className={timePartClass}>
+        {hours}
+      </MemoizedTimePart>
+      <MemoizedTimePart suffix={'min'} size="xs" className={timePartClass}>
+        {minutes}
+      </MemoizedTimePart>
+      <MemoizedTimePart suffix={'sec'} size="xs" className={timePartClass}>
+        {seconds}
+      </MemoizedTimePart>
+    </div>
   )
 }
