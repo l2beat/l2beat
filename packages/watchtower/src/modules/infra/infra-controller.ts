2fd8e40992654e7aa11fe75a49adaac4d6886b75
Michał Podsiadły <michal.podsiadly@l2beat.com>
2025-06-12 12:23:55 +0200

    push push push

--- packages/watchtower/src/modules/infra/infra-controller.ts
+++ packages/watchtower/src/modules/infra/infra-controller.ts
@@ -1,4 +1,5 @@
 import type { Logger } from '@l2beat/backend-tools'
+import { exec } from 'child_process'
 
 type InfrastructureControllerDependencies = {
   logger: Logger
@@ -9,7 +10,20 @@ export class InfrastructureController {
     private readonly dependencies: InfrastructureControllerDependencies,
   ) {}
 
-  handleFrontendPreview() {
-    return 'OK'
+  async handleFrontendPreview(prNumber: number) {
+    await execShellCommand(
+      `cat envsubst-template.yaml | PR_ID=${prNumber} envsubst | kubectl apply -n preview-prod -f`,
+    )
   }
+}
+
+async function execShellCommand(command: string) {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error || stderr) {
+        reject(error || stderr)
+      }
+      resolve(stdout)
+    })
+  })
 }
--- packages/watchtower/src/modules/infra/infra-router.ts
+++ packages/watchtower/src/modules/infra/infra-router.ts
@@ -1,14 +1,57 @@
 import express from 'express'
 import type { InfrastructureController } from './infra-controller'
+import { z } from 'zod'
+
+function stringAsInt() {
+  return z.preprocess((s) => {
+    const res = z.string().safeParse(s)
+    const maybeNumber = res.success && s !== '' ? Number(res.data) : null
+
+    if (maybeNumber === null) {
+      throw new Error('Invalid number')
+    }
+
+    const isPositive = maybeNumber > 0
+
+    if (!isPositive) {
+      throw new Error('Number must be positive')
+    }
+
+    return maybeNumber
+  }, z.number().int())
+}
+
+const schema = z.object({
+  prNumber: stringAsInt(),
+})
 
 export function createInfrastructureRouter(
   controller: InfrastructureController,
 ): express.Router {
   const r = express.Router()
 
-  r.post('/frontend-preview', (_req, res) => {
-    res.json({ status: controller.handleFrontendPreview() })
-  })
+  r.post(
+    '/frontend-preview',
+    async (req: express.Request, res: express.Response) => {
+      const parsed = schema.safeParse(req.body)
+
+      if (!parsed.success) {
+        res.status(400).json({ error: parsed.error.message })
+        return
+      }
+
+      const { prNumber } = parsed.data
+
+      try {
+        await controller.handleFrontendPreview(prNumber)
+        res.json({ status: 'OK' })
+      } catch (error) {
+        console.error('Failed to apply preview deployment', error)
+        res.status(500).json({ error: 'Failed to apply preview deployment' })
+        return
+      }
+    },
+  )
 
   return r
 }
