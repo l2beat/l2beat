dfd2570b55d227d465385a6398c833aeb2465c5c
Gabriel Coutinho de Paula <38709327+GCdePaula@users.noreply.github.com>
2025-06-11 23:14:48 -0300

    Update packages/config/src/projects/honeypot-v2/honeypot-v2.ts
    Co-authored-by: Guilherme Dantas <guidanoli@proton.me>

--- packages/config/src/projects/honeypot-v2/honeypot-v2.ts
+++ packages/config/src/projects/honeypot-v2/honeypot-v2.ts
@@ -240,13 +240,6 @@ export const honeypotv2: ScalingProject = {
           description:
             'Contract that allows anyone to perform transfers of ERC-20 tokens to Cartesi DApps.',
         }),
-        discovery.getContractDetails('Authority', {
-          description:
-            'Simple consensus model controlled by a single address, the owner.',
-        }),
-        discovery.getContractDetails('History', {
-          description: 'Contract that stores claims for Cartesi DApps.',
-        }),
       ],
     },
     risks: [],
