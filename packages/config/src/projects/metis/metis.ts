d96748b83be2c310eb3d64e7450f4519359d21b0
vincfurc <10850139+vincfurc@users.noreply.github.com>
2025-06-19 17:09:53 +0100

    challenge section

--- packages/config/src/projects/metis/metis.ts
+++ packages/config/src/projects/metis/metis.ts
@@ -28,6 +28,11 @@ const CHALLENGE_PERIOD_SECONDS = discovery.getContractValue<number>(
   'FRAUD_PROOF_WINDOW',
 )
 
+const DISPUTE_TIMEOUT_PERIOD = discovery.getContractValue<number>(
+  'DisputeGameFactory',
+  'DISPUTE_TIMEOUT_PERIOD',
+)
+
 export const metis: ScalingProject = {
   type: 'layer2',
   id: ProjectId('metis'),
@@ -177,9 +182,16 @@ export const metis: ScalingProject = {
       },
       {
         title: 'Challenges',
-        description:
-          'Games are created on demand by the permissioned `GameCreator` should a dispute arise. Once resolved, disputed state batches can be marked as such in the `StateCommitmentChain`. Then, these flagged batches can be deleted (within the fraud proof window). Batches can only be deleted from the MVM_Verifier contract address, which currently corresponds to the `Metis Multisig`.',
-        risks: [],
+        description: `Games are created on demand by the permissioned GameCreator should a dispute arise. Users can signal the need for a dispute through the dispute() function of the \`DisputeGameFactory\`. Should a game not be created by the \`GameCreator\` within the dispute timeout period of ${formatChallengePeriod(
+          DISPUTE_TIMEOUT_PERIOD,
+        )}, sequencer deposits in the \`FaultProofLockingPool\` can be slashed and transfered to the dispute creator. Should a game be created and resolved, disputed state batches can be marked as such in the \`StateCommitmentChain\`. Then, these flagged batches can be deleted (within the fraud proof window). Batches can only be deleted from the MVM_Verifier contract address, which currently corresponds to the \`Metis Multisig\`.`,
+        risks: [
+          {
+            category: 'Funds can be stolen if',
+            text: 'an invalid state root is submitted to the system and no dispute game is created by the permissioned GameCreator.',
+            isCritical: true,
+          },
+        ],
         references: [
           {
             title:
