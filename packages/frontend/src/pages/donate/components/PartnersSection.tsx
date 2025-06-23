9d028b3f1d3a808950b4434b58d6581896f4dbcd
maciekzygmunt <maciekzygmunt@interia.pl>
2025-06-23 11:03:45 +0200

    update styling

--- packages/frontend/src/pages/donate/components/PartnersSection.tsx
+++ packages/frontend/src/pages/donate/components/PartnersSection.tsx
@@ -1,6 +1,8 @@
 import { HorizontalSeparator } from '~/components/core/HorizontalSeparator'
+import { VerticalSeparator } from '~/components/core/VerticalSeparator'
 import { PrimaryCard } from '~/components/primary-card/PrimaryCard'
 import { ArrowRightIcon } from '~/icons/ArrowRight'
+import { cn } from '~/utils/cn'
 import type { Partners } from '../getDonateData'
 
 interface Props {
@@ -13,7 +15,7 @@ export function PartnersSection({ partners }: Props) {
       <h2 className="font-bold text-xl">Partners</h2>
       <div>
         <h3 className="font-medium text-secondary text-xs">ECOSYSTEMS TIER</h3>
-        <div className="mt-3 grid gap-3 sm:grid-cols-2 [@media(min-width:1440px)]:grid-cols-4">
+        <div className="mt-3 grid gap-3 md:grid-cols-3">
           {partners.ecosystem.map((partner) => (
             <EcosystemTierCard key={partner.slug} partner={partner} />
           ))}
@@ -53,23 +55,33 @@ export function EcosystemTierCard({
     >
       <a href={`/ecosystems/${partner.slug}`} className="block">
         <div
-          className={`relative h-[260px] cursor-pointer overflow-hidden rounded bg-gradient-radial from-[--ecosystem-secondary] to-[--ecosystem-primary] shadow-lg transition-shadow duration-300 group-hover:shadow-xl`}
+          className={`relative h-[135px] cursor-pointer overflow-hidden rounded bg-gradient-radial from-[--ecosystem-secondary] to-[--ecosystem-primary] shadow-lg transition-shadow duration-300 group-hover:shadow-xl md:h-[260px]`}
         >
           {/* Background pattern/texture overlay */}
           <div className="absolute inset-0 bg-black/0 transition-colors duration-300 group-hover:bg-black/10" />
 
           {/* Content */}
-          <div className="group-hover:-translate-y-6 relative z-10 flex h-full flex-col items-center justify-center p-6 text-white transition-transform ease-in-out">
-            <div className="h-[50px]">
+          <div className="max-md:-translate-y-5 md:group-hover:-translate-y-6 relative z-10 h-full items-center justify-center justify-items-center p-6 text-white transition-transform ease-in-out max-md:grid max-md:grid-cols-[1fr_56px_1fr] md:flex md:flex-col">
+            <div className="h-[50px] max-md:h-12">
               <img
                 src={partner.ecosystemLogo.dark}
                 alt={partner.project.name}
                 className="h-full object-contain"
               />
             </div>
-            <HorizontalSeparator className="mt-6 mb-4 border-pure-white/40" />
+            <HorizontalSeparator className="mt-6 mb-4 border-pure-white/40 max-md:hidden" />
+            <div className="flex items-center justify-center md:hidden">
+              <VerticalSeparator className="h-12 border-pure-white/40 border-t-0" />
+            </div>
             <div className="h-16 text-center">
-              <p className="mb-2 text-2xs text-pure-white/70">PARTNER:</p>
+              <p
+                className={cn(
+                  'text-2xs text-pure-white/70',
+                  partner.project.id !== 'arbitrum-orbit' && 'mb-2',
+                )}
+              >
+                PARTNER:
+              </p>
               <img
                 src={partner.mainPartnerLogo.src}
                 width={partner.mainPartnerLogo.width / 2}
@@ -80,7 +92,7 @@ export function EcosystemTierCard({
             <div className="-translate-x-full pointer-events-none absolute inset-0 transform bg-gradient-to-r from-transparent via-white/10 to-transparent transition-transform duration-700 ease-in-out group-hover:translate-x-full" />
           </div>
 
-          <div className="ease absolute inset-x-6 bottom-6 flex translate-y-full items-center justify-center rounded border border-link-stroke bg-surface-primary p-3 font-medium text-link text-xs leading-none opacity-0 transition-[opacity,transform] duration-200 ease-in-out group-hover:translate-y-0 group-hover:opacity-100">
+          <div className="ease absolute inset-x-6 bottom-4 flex translate-y-full items-center justify-center rounded border border-link-stroke bg-surface-primary p-3 font-medium text-link text-xs leading-none transition-[opacity,transform] duration-200 ease-in-out max-md:translate-y-0 md:bottom-6 md:opacity-0 md:group-hover:translate-y-0 md:group-hover:opacity-100">
             Go to ecosystem page
           </div>
 
@@ -103,7 +115,11 @@ export function InnovatorTierCard({
       }}
       className="group relative flex h-14 items-center justify-between rounded border border-divider bg-center bg-cover px-6 max-md:pr-4 md:h-20"
     >
-      <img src={partner.logo.src} alt={partner.slug} className="h-8 w-fit" />
+      <img
+        src={partner.logo.src}
+        alt={partner.slug}
+        className="h-[42px] w-fit md:h-[52px]"
+      />
       <div className="hidden rounded border border-link-stroke bg-surface-primary px-3 py-2 font-medium text-link text-xs leading-none max-md:block">
         Go to Project Details
       </div>
@@ -131,7 +147,7 @@ export function SupporterTierTable({
       <table className="w-full">
         <tbody>
           {partners.map((partner) => (
-            <tr key={partner.slug} className="border-divider border-t">
+            <tr key={partner.slug} className="h-14 border-divider border-t">
               <td className="pl-7">
                 <div className="flex items-center gap-1.5">
                   <img
