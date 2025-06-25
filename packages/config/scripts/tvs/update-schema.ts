daeed2579452e619dba31512f591e5efe6720a65
Mateusz Radomski <radomski.main@protonmail.com>
2025-06-25 09:52:59 +0200

    Migrate config to @l2beat/validate

--- packages/config/scripts/tvs/update-schema.ts
+++ packages/config/scripts/tvs/update-schema.ts
@@ -1,15 +1,10 @@
 import * as fs from 'fs'
-import { z } from 'zod'
-import { zodToJsonSchema } from 'zod-to-json-schema'
+import { toJsonSchema } from '@l2beat/validate'
 import { ProjectTvsConfigSchema } from '../../src/types'
 
 function main() {
   try {
-    const schemaWithMeta = z
-      .object({ $schema: z.string().optional() })
-      .merge(ProjectTvsConfigSchema)
-
-    const schema = zodToJsonSchema(schemaWithMeta)
+    const schema = toJsonSchema(ProjectTvsConfigSchema)
 
     fs.writeFileSync(
       './src/tvs/json/schema/tvs-config-schema.json',
