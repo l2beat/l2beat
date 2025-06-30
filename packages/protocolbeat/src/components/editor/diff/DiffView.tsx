5b5dde300e78ba63cd9fe80c76c4f176a08c8e60
Michał Podsiadły <michal.podsiadly@l2beat.com>
2025-06-30 11:59:47 +0200

    fix

--- packages/protocolbeat/src/components/editor/diff/DiffView.tsx
+++ packages/protocolbeat/src/components/editor/diff/DiffView.tsx
@@ -175,10 +175,7 @@ export function DiffView(props: DiffViewProps) {
               'rounded p-1.5 transition-colors',
               'hover:bg-coffee-700',
             )}
-            onClick={() => {
-              editor?.swapSides()
-              swapSides()
-            }}
+            onClick={swapSides}
             title="Swap sides"
           >
             <IconSwap className="size-4" />
