cb3bfdf037970467f8863f9ece3a159b2fd78ced
torztomasz <tomasz.torz@l2beat.com>
2025-07-01 14:39:07 +0200

    remove liveness schemas

--- packages/frontend/src/server/features/scaling/liveness/types.ts
+++ packages/frontend/src/server/features/scaling/liveness/types.ts
@@ -1,39 +1,32 @@
-import { TrackedTxsConfigSubtype } from '@l2beat/shared-pure'
-import { v } from '@l2beat/validate'
+import type { TrackedTxsConfigSubtype, UnixTime } from '@l2beat/shared-pure'
 
-export const LivenessTimeRange = v.enum(['30d', '90d', 'max'])
-export type LivenessTimeRange = v.infer<typeof LivenessTimeRange>
+export type LivenessTimeRange = '30d' | '90d' | 'max'
 
-export const LivenessDataPoint = v.object({
-  averageInSeconds: v.number().check((v) => Number.isInteger(v) && v > 0),
-  minimumInSeconds: v.number().check((v) => Number.isInteger(v) && v > 0),
-  maximumInSeconds: v.number().check((v) => Number.isInteger(v) && v > 0),
-})
-export type LivenessDataPoint = v.infer<typeof LivenessDataPoint>
+export interface LivenessDataPoint {
+  averageInSeconds: number
+  minimumInSeconds: number
+  maximumInSeconds: number
+}
 
-export const LivenessAnomaly = v.object({
-  timestamp: v.number(),
-  durationInSeconds: v.number().check((v) => Number.isInteger(v) && v > 0),
-  type: TrackedTxsConfigSubtype,
-})
-export type LivenessAnomaly = v.infer<typeof LivenessAnomaly>
+export interface LivenessAnomaly {
+  start: UnixTime
+  end: UnixTime
+  durationInSeconds: number
+  type: TrackedTxsConfigSubtype
+}
 
-const LivenessDetails = v.object({
-  '30d': LivenessDataPoint.optional(),
-  '90d': LivenessDataPoint.optional(),
-  max: LivenessDataPoint.optional(),
-  syncedUntil: v.number(),
-})
+export interface LivenessDetails {
+  '30d'?: LivenessDataPoint
+  '90d'?: LivenessDataPoint
+  max?: LivenessDataPoint
+  syncedUntil: number
+}
 
-export type LivenessDetails = v.infer<typeof LivenessDetails>
+export interface LivenessProject {
+  batchSubmissions?: LivenessDetails
+  stateUpdates?: LivenessDetails
+  proofSubmissions?: LivenessDetails
+  anomalies: LivenessAnomaly[]
+}
 
-export const LivenessProject = v.object({
-  batchSubmissions: LivenessDetails.optional(),
-  stateUpdates: LivenessDetails.optional(),
-  proofSubmissions: LivenessDetails.optional(),
-  anomalies: v.array(LivenessAnomaly),
-})
-export type LivenessProject = v.infer<typeof LivenessProject>
-
-export const LivenessResponse = v.record(v.string(), LivenessProject)
-export type LivenessResponse = v.infer<typeof LivenessResponse>
+export type LivenessResponse = Record<string, LivenessProject>
