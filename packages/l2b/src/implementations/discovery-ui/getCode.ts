37b72f711621e7963fce493864556adda0b1762f
Mateusz Radomski <radomski.main@protonmail.com>
2025-07-02 12:50:14 +0200

    DiscoUI locally won't return the code unless it's up to date

--- packages/l2b/src/implementations/discovery-ui/getCode.ts
+++ packages/l2b/src/implementations/discovery-ui/getCode.ts
@@ -1,8 +1,10 @@
 import { existsSync, readFileSync, readdirSync } from 'fs'
 import { join } from 'path'
+import { isDeepStrictEqual } from 'util'
 import {
   type ConfigReader,
   type DiscoveryPaths,
+  flatteningHash,
   get$Implementations,
   getChainFullName,
   getChainShortName,
@@ -32,11 +34,38 @@ export function addFlattenerNote(code: string): string {
   return note.join('\n') + code
 }
 
+interface CodePathResult {
+  entryName: string | undefined
+  codePaths: { name: string; path: string }[]
+}
+
+function isFlatCodeCurrent(
+  configReader: ConfigReader,
+  project: string,
+  address: string,
+  codePaths: CodePathResult['codePaths'],
+): boolean {
+  const [chainShortName, simpleAddress] = address.split(':')
+  const chain = getChainFullName(chainShortName)
+
+  const discoHashes =
+    configReader
+      .readDiscovery(project, chain)
+      .entries.find((e) => e.address === simpleAddress)?.sourceHashes ?? []
+
+  const flatHashes = codePaths.map(({ path }) =>
+    flatteningHash(readFileSync(path, 'utf-8')),
+  )
+
+  return isDeepStrictEqual(discoHashes.sort(), flatHashes.sort())
+}
+
 export function getCode(
   paths: DiscoveryPaths,
   configReader: ConfigReader,
   project: string,
   address: string,
+  checkFlatCode: boolean = false,
 ): ApiCodeResponse {
   const { entryName, codePaths } = getCodePaths(
     paths,
@@ -44,6 +73,17 @@ export function getCode(
     project,
     address,
   )
+
+  const flatCodeCurrent = isFlatCodeCurrent(
+    configReader,
+    project,
+    address,
+    codePaths,
+  )
+
+  if (checkFlatCode && !flatCodeCurrent) {
+    throw new Error('Flat code is outdated')
+  }
 
   return {
     entryName,
@@ -110,10 +150,7 @@ export function getCodePaths(
   configReader: ConfigReader,
   project: string,
   address: string,
-): {
-  entryName: string | undefined
-  codePaths: { name: string; path: string }[]
-} {
+): CodePathResult {
   const [chainShortName, simpleAddress] = address.split(':')
   const chain = getChainFullName(chainShortName)
   const discoveries = getProjectDiscoveries(configReader, project, chain)
