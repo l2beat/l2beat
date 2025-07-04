1e703cd311fe6f22aebb2a46c8111b04c0212a59
antooni <antoni.pawlak@protonmail.com>
2025-07-04 10:55:34 +0200

    rpcs

---
+++ packages/backend/scripts/bridges/chains.ts
@@ -0,0 +1,34 @@
+export const CHAINS = [
+  {
+    name: 'ethereum',
+    callsPerMinute: 1000,
+  },
+  {
+    name: 'arbitrum',
+    callsPerMinute: 1000,
+  },
+  {
+    name: 'base',
+    callsPerMinute: 1000,
+  },
+  {
+    name: 'unichain',
+    callsPerMinute: 1000,
+  },
+  {
+    name: 'zksync2',
+    callsPerMinute: 1000,
+  },
+  {
+    name: 'linea',
+    callsPerMinute: 1000,
+  },
+  {
+    name: 'scroll',
+    callsPerMinute: 1000,
+  },
+  {
+    name: 'ink',
+    callsPerMinute: 1000,
+  },
+]
--- packages/backend/scripts/bridges/cli.ts
+++ packages/backend/scripts/bridges/cli.ts
@@ -1,4 +1,7 @@
+import { Logger, getEnv } from '@l2beat/backend-tools'
+import { HttpClient, RpcClient } from '@l2beat/shared'
 import { command, option, optional, run, string } from 'cmd-ts'
+import { CHAINS } from './chains'
 
 const args = {
   chains: option({
@@ -20,12 +23,37 @@ const args = {
 const cmd = command({
   name: 'bridges:cli',
   args,
-  handler: (args) => {
-    console.log(args.chains?.split(','))
-    console.log(args.protocols?.split(','))
+  handler: async (args) => {
+    const http = new HttpClient()
+    const logger = Logger.SILENT
+    const env = getEnv()
+
+    const chains = args.chains
+      ? CHAINS.filter((c) => args.chains?.split(',').includes(c.name))
+      : CHAINS
+
+    const rpcs = chains.map(
+      (c) =>
+        new RpcClient({
+          url: env.string(`${c.name.toUpperCase()}_RPC_URL`),
+          sourceName: c.name,
+          http,
+          logger,
+          callsPerMinute: c.callsPerMinute,
+          retryStrategy: 'RELIABLE',
+        }),
+    )
+
+    for (const r of rpcs) {
+      const latest = await r.getLatestBlockNumber()
+      const logs = await r.getLogs(latest - 10, latest)
+      console.log(logs.map((l) => l.transactionHash))
+    }
 
     process.exit(0)
   },
 })
 
 run(cmd, process.argv.slice(2))
+
+// function decode(tx: Transaction) {}
