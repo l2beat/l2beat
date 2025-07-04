aef5e00a77301efe2590729dbd2a8d6cb1aba6b4
torztomasz <tomasz.torz@l2beat.com>
2025-07-04 08:42:49 +0200

    post merge

--- packages/frontend/src/components/projects/ProjectHeader.tsx
+++ packages/frontend/src/components/projects/ProjectHeader.tsx
@@ -1,6 +1,4 @@
-import { ChevronIcon } from '~/icons/Chevron'
 import { cn } from '~/utils/cn'
-import { LiveIndicator } from '../LiveIndicator'
 
 interface Project {
   name: string
@@ -10,42 +8,23 @@ interface Project {
 
 interface Props {
   project: Project
-  ongoingAnomaly?: 'single' | 'multiple'
   className?: string
 }
 
-export function ProjectHeader({ project, ongoingAnomaly, className }: Props) {
+export function ProjectHeader({ project, className }: Props) {
   return (
-    <div
-      className={cn('flex gap-6 max-md:flex-col md:items-center', className)}
-    >
-      <h1 className="flex items-center justify-start gap-3">
-        {project.slug && (
-          <img
-            width={32}
-            height={32}
-            src={project.icon}
-            alt={`${project.name} logo`}
-          />
-        )}
-        <span className="!leading-none font-bold text-[28px]">
-          {project.name}
-        </span>
-      </h1>
-      {!!ongoingAnomaly && (
-        <a
-          href="#liveness"
-          className="flex h-8 items-center justify-center gap-2 rounded border border-negative p-2"
-        >
-          <LiveIndicator />
-          <h3 className="font-medium text-negative text-xs uppercase leading-none">
-            {ongoingAnomaly === 'single'
-              ? 'Ongoing anomaly'
-              : 'Ongoing anomalies'}
-          </h3>
-          <ChevronIcon className="-rotate-90 size-2.5 fill-negative" />
-        </a>
+    <h1 className={cn('flex items-center justify-start gap-3', className)}>
+      {project.slug && (
+        <img
+          width={32}
+          height={32}
+          src={project.icon}
+          alt={`${project.name} logo`}
+        />
       )}
-    </div>
+      <span className="!leading-none font-bold text-[28px]">
+        {project.name}
+      </span>
+    </h1>
   )
 }
