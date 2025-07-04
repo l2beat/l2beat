64c459a074c9180b0515ad464ed0611fb847c873
torztomasz <tomasz.torz@l2beat.com>
2025-07-04 14:04:55 +0200

    adjust styles in ongoing anomalies

--- packages/frontend/src/components/projects/sections/LivenessSection.tsx
+++ packages/frontend/src/components/projects/sections/LivenessSection.tsx
@@ -76,10 +76,10 @@ export function OngoingAnomalies({
       </div>
       <table className="w-full">
         <thead className="text-xs">
-          <tr className="text-left font-medium text-secondary ">
-            <th className="w-1/2 pr-2 lg:w-1/3">Type</th>
-            <th className="w-1/2 pr-2 lg:w-1/3">Duration</th>
-            <th className="w-1/3 max-lg:hidden">Start</th>
+          <tr className="text-left font-medium text-secondary">
+            <th className="w-1/2 pr-2 pb-2 lg:w-1/3">Type</th>
+            <th className="w-1/2 pr-2 pb-2 lg:w-1/3">Duration</th>
+            <th className="w-1/3 pb-2 max-lg:hidden">Start</th>
           </tr>
         </thead>
         <tbody className="!leading-none align-top text-sm md:text-lg">
@@ -92,12 +92,12 @@ export function OngoingAnomalies({
                 key={`${anomaly.subtype}-${anomaly.start}`}
                 className="font-bold"
               >
-                <td className="pr-2">
+                <td className="pr-2 pb-1">
                   {anomalySubtypeToLabel(anomaly.subtype)}
                 </td>
                 <td
                   className={cn(
-                    'pr-2',
+                    'pr-2 pb-1',
                     getDurationColorClassName(anomaly.durationInSeconds),
                   )}
                 >
@@ -106,7 +106,7 @@ export function OngoingAnomalies({
                     {formattedStart}
                   </p>
                 </td>
-                <td className="max-lg:hidden">{formattedStart}</td>
+                <td className="pb-1 max-lg:hidden">{formattedStart}</td>
               </tr>
             )
           })}
