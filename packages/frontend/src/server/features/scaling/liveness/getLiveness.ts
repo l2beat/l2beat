0f36af3416bd86cb0f60143e755c2c6238d33597
torztomasz <tomasz.torz@l2beat.com>
2025-07-09 09:31:32 +0200

    fix condition

--- packages/frontend/src/server/features/scaling/liveness/getLiveness.ts
+++ packages/frontend/src/server/features/scaling/liveness/getLiveness.ts
@@ -233,7 +233,9 @@ function getAnomalies(
   const filteredAnomalies = anomalies.filter((a) => {
     const record = realTimeAnomalies.find((r) => r.start === a.timestamp)
     const alreadyExists =
-      record?.subtype === a.subtype && record.projectId === a.projectId
+      record?.start === a.timestamp &&
+      record?.subtype === a.subtype &&
+      record?.projectId === a.projectId
     return !alreadyExists
   })
 
