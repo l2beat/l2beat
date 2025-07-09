7949e341d764a06dab4ed43cc4142ee874f8ea4b
maciekzygmunt <maciekzygmunt@interia.pl>
2025-07-09 15:57:17 +0200

    rename

--- packages/frontend/src/pages/ecosystems/project/components/charts/EcosystemsProjectsChart.tsx
+++ packages/frontend/src/pages/ecosystems/project/components/charts/EcosystemsProjectsChart.tsx
@@ -104,7 +104,7 @@ function Header({
   marketShare: number
 }) {
   return (
-    <div className="mb-3 flex items-center justify-between">
+    <div className="mb-3 flex items-start justify-between">
       <div>
         <div className="font-bold text-xl">Projects count</div>
         <EcosystemChartTimeRange range={range} />
