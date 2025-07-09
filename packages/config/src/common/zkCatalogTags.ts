8fef45fb496c44c1a1af681f31917a353a9e9c6d
maciekzygmunt <maciekzygmunt@interia.pl>
2025-07-09 17:16:08 +0200

    plff

--- packages/config/src/common/zkCatalogTags.ts
+++ packages/config/src/common/zkCatalogTags.ts
@@ -17,37 +17,43 @@ export const zkCatalogTags = [
     id: 'Boojum',
     type: ZkCatalogTagType.STARK,
     name: 'boojum',
-    description: 'zkVM STARK proving system developed by Matter Labs for proving state transition of ZKsync Era.',
+    description:
+      'zkVM STARK proving system developed by Matter Labs for proving state transition of ZKsync Era.',
   },
   {
     id: 'Plonky3',
     type: ZkCatalogTagType.STARK,
     name: 'Plonky3',
-    description: 'Toolkit that implements a set of primitives for STARK-based zkVMs developed by Polygon Zero team.',
+    description:
+      'Toolkit that implements a set of primitives for STARK-based zkVMs developed by Polygon Zero team.',
   },
   {
     id: 'PIL-STARK',
     type: ZkCatalogTagType.STARK,
     name: 'PIL-STARK',
-    description: 'zkVM STARK proving system that works with Polynomial Identity Langugae (PIL) developed by Polygon Zero team.',
+    description:
+      'zkVM STARK proving system that works with Polynomial Identity Langugae (PIL) developed by Polygon Zero team.',
   },
   {
     id: 'Stone',
     type: ZkCatalogTagType.STARK,
     name: 'Stone',
-    description: 'zkVM STARK proving system developed by Starkware for Cairo programs, including state transition of Starknet.',
+    description:
+      'zkVM STARK proving system developed by Starkware for Cairo programs, including state transition of Starknet.',
   },
   {
     id: 'ZkvmProver',
     type: ZkCatalogTagType.STARK,
     name: 'zkvm-prover',
-    description: 'zkVM STARK proving system developed by Scroll for openvm programs, including state transition of Scroll.',
+    description:
+      'zkVM STARK proving system developed by Scroll for openvm programs, including state transition of Scroll.',
   },
   {
     id: 'RISC0',
     type: ZkCatalogTagType.STARK,
     name: 'Risc0',
-    description: 'zkVM STARK proving system developed by RISC ZERO for RISC-V programs.',
+    description:
+      'zkVM STARK proving system developed by RISC ZERO for RISC-V programs.',
   },
   // ISA
   {
@@ -60,7 +66,8 @@ export const zkCatalogTags = [
     id: 'RISCV',
     type: ZkCatalogTagType.ISA,
     name: 'RISC-V',
-    description: 'Free and open-source universal 32-bit ISA used across a variety of hardware and software.',
+    description:
+      'Free and open-source universal 32-bit ISA used across a variety of hardware and software.',
   },
   {
     id: 'ZkASM',
@@ -91,74 +98,86 @@ export const zkCatalogTags = [
     id: 'PlonkBellman',
     type: ZkCatalogTagType.SNARK,
     name: 'Plonk: bellman',
-    description: 'Bellman Rust library for Plonk proving system, originally developed for ZCash.',
+    description:
+      'Bellman Rust library for Plonk proving system, originally developed for ZCash.',
   },
   {
     id: 'FflonkZksync',
     type: ZkCatalogTagType.SNARK,
     name: 'Fflonk: zksync',
-    description: 'Matter Labs Rust implementation of Fflonk improvement over standard Plonk proving system.',
+    description:
+      'Matter Labs Rust implementation of Fflonk improvement over standard Plonk proving system.',
   },
   {
     id: 'PlonkGnark',
     type: ZkCatalogTagType.SNARK,
     name: 'Plonk: gnark',
-    description: 'Consensys implementation of Plonk proving system written in Go.',
+    description:
+      'Consensys implementation of Plonk proving system written in Go.',
   },
   {
     id: 'Groth16Gnark',
     type: ZkCatalogTagType.SNARK,
     name: 'Groth16: gnark',
-    description: 'Consensys implementation of Groth16 proving system written in Go.',
+    description:
+      'Consensys implementation of Groth16 proving system written in Go.',
   },
   {
     id: 'FflonkSnarkjs',
     type: ZkCatalogTagType.SNARK,
     name: 'Fflonk: snarkjs',
-    description: 'Circom / iden3 implementation of Fflonk improvement over standard Plonk proving system written in JS.',
+    description:
+      'Circom / iden3 implementation of Fflonk improvement over standard Plonk proving system written in JS.',
   },
   {
     id: 'Groth16Snarkjs',
     type: ZkCatalogTagType.SNARK,
     name: 'Groth16: snarkjs',
-    description: 'Circom / iden3 implementation of Groth16 proving system written in JS.',
+    description:
+      'Circom / iden3 implementation of Groth16 proving system written in JS.',
   },
   {
     id: 'PlonkHalo2',
     type: ZkCatalogTagType.SNARK,
     name: 'Plonk: halo2',
-    description: 'Rust implementation of Halo2 SNARK proving system, originally developed for ZCash.',
+    description:
+      'Rust implementation of Halo2 SNARK proving system, originally developed for ZCash.',
   },
   {
     id: 'Groth16EthSnarks',
     type: ZkCatalogTagType.SNARK,
     name: 'Groth16: EthSnarks',
-    description: 'EthSnarks implementation of Groth16 proving system written in C++.',
+    description:
+      'EthSnarks implementation of Groth16 proving system written in C++.',
   },
   {
     id: 'PlonkZksync',
     type: ZkCatalogTagType.SNARK,
     name: 'Plonk: zksync',
-    description: 'Matter Labs Rust implementation of Plonk proving system, originally developed for ZKsync Lite (old ZKsync).',
+    description:
+      'Matter Labs Rust implementation of Plonk proving system, originally developed for ZKsync Lite (old ZKsync).',
   },
   // Curve
   {
     id: 'BN254',
     type: ZkCatalogTagType.Curve,
     name: 'BN254',
-    description: 'BN254, aka BN256, aka alt_bn128 pairing-friendly 254-bit prime field Weierstrass elliptic curve.',
+    description:
+      'BN254, aka BN256, aka alt_bn128 pairing-friendly 254-bit prime field Weierstrass elliptic curve.',
   },
   {
     id: 'BW6-761',
     type: ZkCatalogTagType.Curve,
     name: 'BW6-761',
-    description: 'Pairing-friendly 761-bit prime field elliptic curve introduced by Housni and Guillevic.',
+    description:
+      'Pairing-friendly 761-bit prime field elliptic curve introduced by Housni and Guillevic.',
   },
   {
     id: 'BLS12-377',
     type: ZkCatalogTagType.Curve,
     name: 'BLS12-377',
-    description: 'Pairing-friendly 377-bit prime field Weierstrass elliptic curve.',
+    description:
+      'Pairing-friendly 377-bit prime field Weierstrass elliptic curve.',
   },
   // Field
   {
@@ -184,32 +203,37 @@ export const zkCatalogTags = [
     id: 'KZG',
     type: ZkCatalogTagType.PCS,
     name: 'KZG',
-    description: 'Polynomial commitment scheme that requires a universal trusted setup, introduced by Kate, Zaverucha and Goldberg.',
+    description:
+      'Polynomial commitment scheme that requires a universal trusted setup, introduced by Kate, Zaverucha and Goldberg.',
   },
   // Arithmetization
   {
     id: 'Plonkish',
     type: ZkCatalogTagType.Arithmetization,
     name: 'Plonkish',
-    description: 'General name for PLONK-like arithmetizations, i.e. value matrix with polynomial constraints, custom gates and lookup arguments.',
+    description:
+      'General name for PLONK-like arithmetizations, i.e. value matrix with polynomial constraints, custom gates and lookup arguments.',
   },
   {
     id: 'R1CS',
     type: ZkCatalogTagType.Arithmetization,
     name: 'R1CS',
-    description: 'Rank 1 constraint system defined by a set of quadratic conditions on the circuit input.',
+    description:
+      'Rank 1 constraint system defined by a set of quadratic conditions on the circuit input.',
   },
   {
     id: 'AIR',
     type: ZkCatalogTagType.Arithmetization,
     name: 'AIR',
-    description: 'Algebraic intermediate representation that encodes a valid VM execution trace with polynomials.',
+    description:
+      'Algebraic intermediate representation that encodes a valid VM execution trace with polynomials.',
   },
   {
     id: 'eAIR',
     type: ZkCatalogTagType.Arithmetization,
     name: 'eAIR',
-    description: 'Extended algebraic intermediate representation introduced by Polygon Zero for eSTARK.',
+    description:
+      'Extended algebraic intermediate representation introduced by Polygon Zero for eSTARK.',
   },
 ] as const satisfies ZkCatalogTag[]
 
--- packages/config/src/projects/shared-sp1/sp1.ts
+++ packages/config/src/projects/shared-sp1/sp1.ts
@@ -1,31 +1,41 @@
-import { ProjectId, UnixTime } from "@l2beat/shared-pure";
-import { ZK_CATALOG_TAGS } from "../../common/zkCatalogTags";
-import { BaseProject, ProjectProofSystem } from "../../types";
-import { ZK_CATALOG_ATTESTERS } from "../../common/zkCatalogAttesters";
+import { ProjectId, UnixTime } from '@l2beat/shared-pure'
+import { ZK_CATALOG_ATTESTERS } from '../../common/zkCatalogAttesters'
+import { ZK_CATALOG_TAGS } from '../../common/zkCatalogTags'
+import type { BaseProject } from '../../types'
 
 export const sp1: BaseProject = {
-    id: ProjectId('sp1'),
-    slug: 'sp1',
-    name: 'SP1',
-    shortName: undefined,
-    addedAt: UnixTime.fromDate(new Date('2025-07-08')),
-    display: {
-        description: 'SP1 is a zk proving system for RISC-V programs built by Succinct.',
-        links: {
-            websites: ['https://www.succinct.xyz'],
-            documentation: ['https://docs.succinct.xyz/docs/sp1/introduction'],
-            repositories: ['https://github.com/succinctlabs/sp1'],
-            socialMedia: ['https://x.com/SuccinctLabs']
-        },
-        badges: [],
+  id: ProjectId('sp1'),
+  slug: 'sp1',
+  name: 'SP1',
+  shortName: undefined,
+  addedAt: UnixTime.fromDate(new Date('2025-07-08')),
+  display: {
+    description:
+      'SP1 is a zk proving system for RISC-V programs built by Succinct.',
+    links: {
+      websites: ['https://www.succinct.xyz'],
+      documentation: ['https://docs.succinct.xyz/docs/sp1/introduction'],
+      repositories: ['https://github.com/succinctlabs/sp1'],
+      socialMedia: ['https://x.com/SuccinctLabs'],
     },
-    proofSystem: {
-        creator: 'Succinct',
-        techStack: {
-            zkVM: [ZK_CATALOG_TAGS.STARK.Plonky3, ZK_CATALOG_TAGS.ISA.RISCV, ZK_CATALOG_TAGS.Field.BabyBear],
-            finalWrap: [ZK_CATALOG_TAGS.SNARK.PlonkGnark, ZK_CATALOG_TAGS.SNARK.Groth16Gnark, ZK_CATALOG_TAGS.Curve.BN254, ZK_CATALOG_TAGS.PCS.KZG],
-        },
-        proofSystemInfo: `
+    badges: [],
+  },
+  proofSystem: {
+    creator: 'Succinct',
+    techStack: {
+      zkVM: [
+        ZK_CATALOG_TAGS.STARK.Plonky3,
+        ZK_CATALOG_TAGS.ISA.RISCV,
+        ZK_CATALOG_TAGS.Field.BabyBear,
+      ],
+      finalWrap: [
+        ZK_CATALOG_TAGS.SNARK.PlonkGnark,
+        ZK_CATALOG_TAGS.SNARK.Groth16Gnark,
+        ZK_CATALOG_TAGS.Curve.BN254,
+        ZK_CATALOG_TAGS.PCS.KZG,
+      ],
+    },
+    proofSystemInfo: `
         
         ## Description
 
@@ -69,10 +79,10 @@ export const sp1: BaseProject = {
         The setup data is downloaded from URL [https://aztec-ignition.s3.amazonaws.com/](https://aztec-ignition.s3.amazonaws.com/) in the 
         [DownloadAndSaveAztecIgnitionSrs](https://github.com/succinctlabs/sp1/blob/ad212dd52bdf8f630ea47f2b58aa94d5b6e79904/crates/recursion/gnark-ffi/go/sp1/trusted_setup/trusted_setup.go#L69) function.
         `,
-        trustedSetup: {
-            risk: 'green',
-            shortDescription: 'Aztec Ignition universal setup for KZG commitments',
-            longDescription: `
+    trustedSetup: {
+      risk: 'green',
+      shortDescription: 'Aztec Ignition universal setup for KZG commitments',
+      longDescription: `
             
             ## Aztec Ignition 
 
@@ -83,27 +93,44 @@ export const sp1: BaseProject = {
             - Github repo with instructions for ceremony participants: https://github.com/AztecProtocol/Setup.
             - Ceremony announcement with a call to participate: https://aztec.network/blog/announcing-ignition.
             `,
-        },
-        verifierHashes: [
-            {
-                hash: '0xd4e8ecd2357dd882209800acd6abb443d231cf287d77ba62b732ce937c8b56e7',
-                explorerLink: 'https://etherscan.io/address/0x0459d576A6223fEeA177Fb3DF53C9c77BF84C459',
-                verificationStatus: 'notVerified',
-                usedBy: [ProjectId('vector'), ProjectId('blobstream'), ProjectId('astarzkevm'), ProjectId('gpt'), ProjectId('katana'), ProjectId('kroma'), ProjectId('lumia'), ProjectId('polygonzkevm'), ProjectId('silicon'), ProjectId('ternoa'), ProjectId('wirex'), ProjectId('witness'), ProjectId('xlayer'), ProjectId('taiko')],
-            },
-            {
-                hash: '0x1b34fe11a637737f0c75c88241669dcf9ca3c03713659265b8241f398a2d286d',
-                explorerLink: 'https://etherscan.io/address/0xE00a3cBFC45241b33c0A44C78e26168CBc55EC63',
-                verificationStatus: 'successful',
-                usedBy: [],
-                verificationSteps: `
+    },
+    verifierHashes: [
+      {
+        hash: '0xd4e8ecd2357dd882209800acd6abb443d231cf287d77ba62b732ce937c8b56e7',
+        explorerLink:
+          'https://etherscan.io/address/0x0459d576A6223fEeA177Fb3DF53C9c77BF84C459',
+        verificationStatus: 'notVerified',
+        usedBy: [
+          ProjectId('vector'),
+          ProjectId('blobstream'),
+          ProjectId('astarzkevm'),
+          ProjectId('gpt'),
+          ProjectId('katana'),
+          ProjectId('kroma'),
+          ProjectId('lumia'),
+          ProjectId('polygonzkevm'),
+          ProjectId('silicon'),
+          ProjectId('ternoa'),
+          ProjectId('wirex'),
+          ProjectId('witness'),
+          ProjectId('xlayer'),
+          ProjectId('taiko'),
+        ],
+      },
+      {
+        hash: '0x1b34fe11a637737f0c75c88241669dcf9ca3c03713659265b8241f398a2d286d',
+        explorerLink:
+          'https://etherscan.io/address/0xE00a3cBFC45241b33c0A44C78e26168CBc55EC63',
+        verificationStatus: 'successful',
+        usedBy: [],
+        verificationSteps: `
                 - Check out [sp1 repo](https://github.com/succinctlabs/sp1) at commit \`76c28bf986ba102127788ce081c21fa09cf93b18\`.
                 - Set an environment variable by calling \`export SP1_ALLOW_DEPRECATED_HOOKS=true\`. It is needed for the correct execution of circuit building.
                 - Make sure that you have [go lang installed](https://go.dev/doc/install).
                 - From \`crates/prover\` call \`make build-circuits\`. Note that the execution could take a while.
                 `,
-                attesters: [ZK_CATALOG_ATTESTERS.L2BEAT],
-            },
-        ],
-    }
+        attesters: [ZK_CATALOG_ATTESTERS.L2BEAT],
+      },
+    ],
+  },
 }
