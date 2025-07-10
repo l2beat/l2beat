926061e14dcb7d68c8bcf1d675c6867796615a80
torztomasz <tomasz.torz@l2beat.com>
2025-07-10 11:24:30 +0200

    update cli

--- packages/backend/scripts/anomalies/anomalies.ts
+++ packages/backend/scripts/anomalies/anomalies.ts
@@ -19,6 +19,8 @@ import {
 } from 'cmd-ts'
 import { formatDuration } from '../../src/modules/shared/notifiers/utils/format'
 
+const ps = new ProjectService()
+
 export const AnomalyKey = extendType(string, {
   async from(input) {
     const match = input.match(
@@ -28,8 +30,6 @@ export const AnomalyKey = extendType(string, {
 
     const projectId = match[1]
     const subtype = match[2] as TrackedTxsConfigSubtype
-
-    const ps = new ProjectService()
 
     const project = await ps.getProject({
       id: ProjectId(projectId),
@@ -92,7 +92,10 @@ const cmd = command({
 
     if (args.list) {
       console.log('Fetching ongoing anomalies...')
-      const ongoingAnomalies = await db.realTimeAnomalies.getOngoingAnomalies()
+      const projects = await getLivenessProjects()
+      const ongoingAnomalies = await db.realTimeAnomalies.getOngoingAnomalies(
+        projects.map((p) => p.id),
+      )
 
       if (ongoingAnomalies.length === 0) {
         console.log('No ongoing anomalies found.')
@@ -117,8 +120,8 @@ const cmd = command({
         'duration',
         'projectId',
         'subtype',
-        'status',
         'start',
+        'isApproved',
       ])
     } else if (args.approve) {
       const ongoingAnomalies = await db.realTimeAnomalies.getOngoingAnomalies()
@@ -173,5 +176,20 @@ const cmd = command({
     process.exit(0)
   },
 })
+
+function getLivenessProjects() {
+  return ps.getProjects({
+    select: [
+      'statuses',
+      'scalingInfo',
+      'livenessInfo',
+      'display',
+      'trackedTxsConfig',
+    ],
+    optional: ['scalingDa'],
+    where: ['isScaling'],
+    whereNot: ['isUpcoming', 'archivedAt'],
+  })
+}
 
 run(cmd, process.argv.slice(2))
