04ceab68b93332019973c80e45f3727b38e1b86f
Mateusz Radomski <33978857+mateuszradomski@users.noreply.github.com>
2025-07-10 12:42:54 +0200

    Refresh discover now has a concise mode (#8286)

--- packages/l2b/src/implementations/discovery/discoveryWrapper.ts
+++ packages/l2b/src/implementations/discovery/discoveryWrapper.ts
@@ -1,15 +1,41 @@
+import type { Logger } from '@l2beat/backend-tools'
 import {
+  type ConfigReader,
   type DiscoveryModuleConfig,
+  type DiscoveryPaths,
+  type TemplateService,
   discover,
+  getChainConfigs,
   modelPermissionsCommand,
 } from '@l2beat/discovery'
 import { updateDiffHistory } from './updateDiffHistory'
 
+export interface Options {
+  logger: Logger
+  description?: string
+  configReader?: ConfigReader
+  templateService?: TemplateService
+  paths?: DiscoveryPaths
+  debug?: boolean
+}
+
 export async function discoverAndUpdateDiffHistory(
   config: DiscoveryModuleConfig,
-  description?: string,
+  options: Options,
 ) {
-  await discover(config)
-  await modelPermissionsCommand(config.project)
-  await updateDiffHistory(config.project, description, config.overwriteCache)
+  await discover(config, getChainConfigs(), options.logger)
+  await modelPermissionsCommand(
+    config.project,
+    options.configReader,
+    options.templateService,
+    options.paths,
+    options.debug,
+    options.logger,
+  )
+  await updateDiffHistory(
+    config.project,
+    options.description,
+    config.overwriteCache,
+    options.logger,
+  )
 }
