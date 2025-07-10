bf59bce30a0c542cd74ac14ce7df61b396ec7253
torztomasz <tomasz.torz@l2beat.com>
2025-07-10 14:15:36 +0200

    fix sidebar

--- packages/frontend/src/components/nav/sidebar/NavSidebar.tsx
+++ packages/frontend/src/components/nav/sidebar/NavSidebar.tsx
@@ -144,7 +144,7 @@ function NavCollapsibleItem({
           </div>
           <ChevronIcon
             className={cn(
-              '-rotate-90 size-3 fill-primary transition-[transform,color,fill] duration-300 group-data-[state=open]:rotate-0',
+              '-rotate-90 size-3 fill-primary transition-[rotate,color,fill] duration-300 group-data-[state=open]:rotate-0',
               isActive && 'fill-brand',
             )}
           />
@@ -169,7 +169,7 @@ function NavCollapsibleItem({
           <CollapsibleTrigger className="group size-6">
             <ChevronIcon
               className={cn(
-                '-rotate-90 m-auto size-3 fill-primary transition-[transform,color,fill] duration-300 group-data-[state=open]:rotate-0',
+                '-rotate-90 m-auto size-3 fill-primary transition-[rotate,color,fill] duration-300 group-data-[state=open]:rotate-0',
                 isActive && 'fill-brand',
               )}
             />
