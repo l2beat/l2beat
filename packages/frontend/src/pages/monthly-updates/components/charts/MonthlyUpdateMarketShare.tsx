fd08ec33d3ef124690a0481ff3ad464d7e759708
torztomasz <tomasz.torz@l2beat.com>
2025-07-11 10:32:38 +0200

    fixes

--- packages/frontend/src/pages/monthly-updates/components/charts/MonthlyUpdateMarketShare.tsx
+++ packages/frontend/src/pages/monthly-updates/components/charts/MonthlyUpdateMarketShare.tsx
@@ -1,18 +1,18 @@
-import { Skeleton } from "~/components/core/Skeleton";
-import { formatPercent } from "~/utils/calculatePercentageChange";
+import { Skeleton } from '~/components/core/Skeleton'
+import { formatPercent } from '~/utils/calculatePercentageChange'
 
 export function MarketShare({
-	marketShare,
+  marketShare,
 }: {
-	marketShare: number | undefined;
+  marketShare: number | undefined
 }) {
-	if (marketShare === undefined) {
-		return <Skeleton className="my-[3px] ml-auto h-[14px] w-36" />;
-	}
+  if (marketShare === undefined) {
+    return <Skeleton className="my-[3px] ml-auto h-[14px] w-36" />
+  }
 
-	return (
-		<div className="font-medium text-(--project-primary) text-xs">
-			{formatPercent(marketShare)} scaling market share
-		</div>
-	);
+  return (
+    <div className="font-medium text-(--project-primary) text-xs">
+      {formatPercent(marketShare)} scaling market share
+    </div>
+  )
 }
