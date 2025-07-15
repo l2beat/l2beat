aabd433a6d02002bc986ed2ffb76e47a2fb1e703
torztomasz <tomasz.torz@l2beat.com>
2025-07-15 14:07:21 +0200

    update fonts

--- packages/frontend/src/ssr/head/fonts.ts
+++ packages/frontend/src/ssr/head/fonts.ts
@@ -8,27 +8,35 @@ export const fonts: FontInfo[] = [
     name: 'Roboto',
     src: [
       {
-        path: '/fonts/roboto/roboto-v32-latin-100.woff2',
+        path: '/fonts/roboto/roboto-v48-latin-100.woff2',
         weight: '100',
       },
       {
-        path: '/fonts/roboto/roboto-v32-latin-300.woff2',
+        path: '/fonts/roboto/roboto-v48-latin-200.woff2',
+        weight: '200',
+      },
+      {
+        path: '/fonts/roboto/roboto-v48-latin-300.woff2',
         weight: '300',
       },
       {
-        path: '/fonts/roboto/roboto-v32-latin-400.woff2',
+        path: '/fonts/roboto/roboto-v48-latin-400.woff2',
         weight: '400',
       },
       {
-        path: '/fonts/roboto/roboto-v32-latin-500.woff2',
+        path: '/fonts/roboto/roboto-v48-latin-500.woff2',
         weight: '500',
       },
       {
-        path: '/fonts/roboto/roboto-v32-latin-700.woff2',
+        path: '/fonts/roboto/roboto-v48-latin-600.woff2',
+        weight: '600',
+      },
+      {
+        path: '/fonts/roboto/roboto-v48-latin-700.woff2',
         weight: '700',
       },
       {
-        path: '/fonts/roboto/roboto-v32-latin-900.woff2',
+        path: '/fonts/roboto/roboto-v48-latin-900.woff2',
         weight: '900',
       },
     ],
@@ -37,39 +45,39 @@ export const fonts: FontInfo[] = [
     name: 'Roboto Serif',
     src: [
       {
-        path: '/fonts/roboto-serif/roboto-serif-v13-latin-100.woff2',
+        path: '/fonts/roboto-serif/roboto-serif-v16-latin-100.woff2',
         weight: '100',
       },
       {
-        path: '/fonts/roboto-serif/roboto-serif-v13-latin-200.woff2',
+        path: '/fonts/roboto-serif/roboto-serif-v16-latin-200.woff2',
         weight: '200',
       },
       {
-        path: '/fonts/roboto-serif/roboto-serif-v13-latin-300.woff2',
+        path: '/fonts/roboto-serif/roboto-serif-v16-latin-300.woff2',
         weight: '300',
       },
       {
-        path: '/fonts/roboto-serif/roboto-serif-v13-latin-400.woff2',
+        path: '/fonts/roboto-serif/roboto-serif-v16-latin-400.woff2',
         weight: '400',
       },
       {
-        path: '/fonts/roboto-serif/roboto-serif-v13-latin-500.woff2',
+        path: '/fonts/roboto-serif/roboto-serif-v16-latin-500.woff2',
         weight: '500',
       },
       {
-        path: '/fonts/roboto-serif/roboto-serif-v13-latin-600.woff2',
+        path: '/fonts/roboto-serif/roboto-serif-v16-latin-600.woff2',
         weight: '600',
       },
       {
-        path: '/fonts/roboto-serif/roboto-serif-v13-latin-700.woff2',
+        path: '/fonts/roboto-serif/roboto-serif-v16-latin-700.woff2',
         weight: '700',
       },
       {
-        path: '/fonts/roboto-serif/roboto-serif-v13-latin-800.woff2',
+        path: '/fonts/roboto-serif/roboto-serif-v16-latin-800.woff2',
         weight: '800',
       },
       {
-        path: '/fonts/roboto-serif/roboto-serif-v13-latin-900.woff2',
+        path: '/fonts/roboto-serif/roboto-serif-v16-latin-900.woff2',
         weight: '900',
       },
     ],
@@ -78,31 +86,31 @@ export const fonts: FontInfo[] = [
     name: 'Roboto Serif Ext',
     src: [
       {
-        path: '/fonts/roboto-serif-ext/roboto-serif-v15-latin_latin-ext-100.woff2',
+        path: '/fonts/roboto-serif-ext/roboto-serif-v16-latin_latin-ext-100.woff2',
         weight: '100',
       },
       {
-        path: '/fonts/roboto-serif-ext/roboto-serif-v15-latin_latin-ext-200.woff2',
+        path: '/fonts/roboto-serif-ext/roboto-serif-v16-latin_latin-ext-200.woff2',
         weight: '200',
       },
       {
-        path: '/fonts/roboto-serif-ext/roboto-serif-v15-latin_latin-ext-300.woff2',
+        path: '/fonts/roboto-serif-ext/roboto-serif-v16-latin_latin-ext-300.woff2',
         weight: '300',
       },
       {
-        path: '/fonts/roboto-serif-ext/roboto-serif-v15-latin_latin-ext-400.woff2',
+        path: '/fonts/roboto-serif-ext/roboto-serif-v16-latin_latin-ext-400.woff2',
         weight: '400',
       },
       {
-        path: '/fonts/roboto-serif-ext/roboto-serif-v15-latin_latin-ext-500.woff2',
+        path: '/fonts/roboto-serif-ext/roboto-serif-v16-latin_latin-ext-500.woff2',
         weight: '500',
       },
       {
-        path: '/fonts/roboto-serif-ext/roboto-serif-v15-latin_latin-ext-600.woff2',
+        path: '/fonts/roboto-serif-ext/roboto-serif-v16-latin_latin-ext-600.woff2',
         weight: '600',
       },
       {
-        path: '/fonts/roboto-serif-ext/roboto-serif-v15-latin_latin-ext-700.woff2',
+        path: '/fonts/roboto-serif-ext/roboto-serif-v16-latin_latin-ext-700.woff2',
         weight: '700',
       },
     ],
