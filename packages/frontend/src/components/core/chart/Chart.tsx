8e82e0070d500b38de583ee7b6f49e33aed3a1bf
torztomasz <tomasz.torz@l2beat.com>
2025-07-15 11:32:19 +0200

    fix 2xl

--- packages/frontend/src/components/core/chart/Chart.tsx
+++ packages/frontend/src/components/core/chart/Chart.tsx
@@ -113,7 +113,7 @@ function ChartContainer<T extends { timestamp: number }>({
         ref={ref}
         className={cn(
           chartContainerClassNames,
-          '2xlh-[258px] 2xlmin-h-[258px] h-[188px] min-h-[188px] w-full md:h-[228px] md:min-h-[228px]',
+          'h-[188px] min-h-[188px] w-full md:h-[228px] md:min-h-[228px] 2xl:h-[258px] 2xl:min-h-[258px]',
           className,
         )}
         {...props}
