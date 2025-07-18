691a8c2702ce381eda7d423a03726f6d2e624979
torztomasz <tomasz.torz@l2beat.com>
2025-07-18 12:39:50 +0200

    make charts bigger on project page mobile

--- packages/frontend/src/pages/scaling/project/ScalingProjectPage.tsx
+++ packages/frontend/src/pages/scaling/project/ScalingProjectPage.tsx
@@ -38,7 +38,7 @@ export function ScalingProjectPage({
     <AppLayout {...props}>
       <HydrationBoundary state={queryState}>
         <SideNavLayout childrenWrapperClassName="md:pt-0">
-          <div className="smooth-scroll max-md:bg-surface-primary">
+          <div className="smooth-scroll project-page max-md:bg-surface-primary">
             {!isNavigationEmpty && (
               <div className="md:-mx-6 sticky top-0 z-100 lg:hidden">
                 <MobileProjectNavigation sections={navigationSections} />
