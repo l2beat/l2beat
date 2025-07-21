41fb5e799b76213071151d8bfaf6c4285fb931ea
torztomasz <tomasz.torz@l2beat.com>
2025-07-21 12:39:51 +0200

    fix

--- packages/frontend/src/server/features/scaling/liveness/getApprovedOngoingAnomalies.ts
+++ packages/frontend/src/server/features/scaling/liveness/getApprovedOngoingAnomalies.ts
@@ -1,3 +1,4 @@
+import type { RealTimeAnomalyRecord } from '@l2beat/database'
 import { UnixTime } from '@l2beat/shared-pure'
 import groupBy from 'lodash/groupBy'
 import { env } from '~/env'
@@ -6,12 +7,14 @@ import { getDb } from '~/server/database'
 
 const cache = new InMemoryCache()
 
-export function getApprovedOngoingAnomalies() {
+type GroupedAnomalies = Record<string, RealTimeAnomalyRecord[]>
+
+export async function getApprovedOngoingAnomalies(): Promise<GroupedAnomalies> {
   if (env.MOCK) {
     return getMockedApprovedOngoingAnomalies()
   }
 
-  return cache.get(
+  const data = await cache.get(
     {
       key: ['approved-ongoing-anomalies'],
       ttl: 3 * UnixTime.MINUTE,
@@ -24,9 +27,10 @@ export function getApprovedOngoingAnomalies() {
       return groupBy(anomalies, (anomaly) => anomaly.projectId)
     },
   )
+  return data
 }
 
-function getMockedApprovedOngoingAnomalies() {
+function getMockedApprovedOngoingAnomalies(): GroupedAnomalies {
   return {
     kinto: [],
   }
