449d9aafb6654f10315896e732f300ea75fec701
Antoni Pawlak <antoni.pawlak@pm.me>
2025-07-22 15:50:32 +0200

    Add LayerZero V1 to Bridges CLI (#8467)

--- packages/backend/scripts/bridges/cli.ts
+++ packages/backend/scripts/bridges/cli.ts
@@ -1,30 +1,32 @@
-import { getEnv, Logger, RateLimiter } from '@l2beat/backend-tools'
-import {
-  BlockIndexerClient,
-  BlockProvider,
-  HttpClient,
-  RpcClient,
-} from '@l2beat/shared'
-import { UnixTime } from '@l2beat/shared-pure'
-import { command, option, optional, run, string } from 'cmd-ts'
-import groupBy from 'lodash/groupBy'
-import { CHAINS } from './chains'
+import { getEnv, Logger } from '@l2beat/backend-tools'
+import { HttpClient, RpcClient } from '@l2beat/shared'
+import { assert, formatSeconds } from '@l2beat/shared-pure'
+import { command, number, positional, run, string } from 'cmd-ts'
+import { CHAINS, type Chain } from './chains'
+import { Decoder } from './Decoder'
 import { PROTOCOLS } from './protocols/protocols'
 import type { Message } from './types/Message'
-import { EnvioClient } from './utils/EnvioClient'
 
 const args = {
-  chains: option({
-    type: optional(string),
-    long: 'chains',
-    description:
-      'Comma-separated list of chains, runs script only for them. If not provided will run for all chains. See chains.ts for possible values.',
+  protocol: positional({
+    type: string,
+    displayName: 'protocol',
   }),
-  protocols: option({
-    type: optional(string),
-    long: 'protocols',
-    description:
-      'Comma-separated list of protocols, runs script only for them. If not provided will run for all protocols with decoders configured. See protocols.ts for possible values.',
+  chain1: positional({
+    type: string,
+    displayName: 'chain1',
+  }),
+  block1: positional({
+    type: number,
+    displayName: 'block1',
+  }),
+  chain2: positional({
+    type: string,
+    displayName: 'chain2',
+  }),
+  block2: positional({
+    type: number,
+    displayName: 'block2',
   }),
 }
 
@@ -32,254 +34,82 @@ const cmd = command({
   name: 'bridges:cli',
   args,
   handler: async (args) => {
-    const http = new HttpClient()
     const logger = Logger.INFO
-    const env = getEnv()
-    const envioApiToken = env.string('ENVIO_API_TOKEN')
+    const chains = setupChains(args, logger)
+    const decoder = new Decoder(PROTOCOLS, chains, logger)
 
-    const enabledChains = args.chains
-      ? CHAINS.filter((c) => args.chains?.split(',').includes(c.name))
-      : CHAINS
+    const messages: Message[] = await decoder.execute(args.protocol, [
+      {
+        chain: args.chain1,
+        block: args.block1,
+      },
+      {
+        chain: args.chain2,
+        block: args.block2,
+      },
+    ])
 
-    const chains = enabledChains.map((c) => ({
-      ...c,
-      blockTimestampClient:
-        c.blockProviderConfig.type === 'etherscan'
-          ? new BlockIndexerClient(
-              http,
-              new RateLimiter({ callsPerMinute: 10 }),
-              {
-                type: 'etherscan',
-                url: env.string('ETHERSCAN_API_URL'),
-                apiKey: env.string('ETHERSCAN_API_KEY'),
-                chain: c.name,
-                chainId: c.blockProviderConfig.chainId,
-              },
-            )
-          : new BlockProvider(c.name, [
-              new RpcClient({
-                url: env.string(`${c.name.toUpperCase()}_RPC_URL`),
-                sourceName: c.name,
-                http,
-                logger,
-                callsPerMinute: c.blockProviderConfig.callsPerMinute,
-                retryStrategy: 'SCRIPT',
-              }),
-            ]),
-      envioClient: new EnvioClient({
-        url: c.envioUrl,
-        apiToken: envioApiToken,
-        sourceName: `${c.name} (Envio)`,
-        http,
-        logger,
-        callsPerMinute: c.envioCallsPerMinute,
-        retryStrategy: 'SCRIPT',
-      }),
-      start: -1, // will be filled later
-      end: -1, // will be filled later,
-    }))
+    for (const message of messages) {
+      logger.info(`${message.direction} (${message.type})`, message)
+    }
 
-    const enabledProtocols = args.protocols
-      ? PROTOCOLS.filter((p) => args.protocols?.split(',').includes(p.name))
-      : PROTOCOLS
+    const outbound = messages.find((m) => m.direction === 'outbound')
+    assert(outbound, 'Outbound message not found, update decoder')
+    const inbound = messages.find(
+      (m) => m.direction === 'inbound' && m.matchingId === outbound?.matchingId,
+    )
+    assert(inbound, 'Inbound message not found, update decoder')
 
-    const decoders = enabledProtocols.map((p) => p.decoder)
-
-    logger.info('Running script for last day', {
-      protocols: enabledProtocols.map((p) => p.name),
-      chains: enabledChains.map((c) => c.name),
+    logger.info('Matching', {
+      latency: formatSeconds(inbound.blockTimestamp - outbound.blockTimestamp),
+      origin: chains
+        .find((c) => c.shortName === outbound.origin)
+        ?.getTxUrl(outbound.txHash),
+      destination: chains
+        .find((c) => c.shortName === inbound.destination)
+        ?.getTxUrl(inbound.txHash),
+      id: outbound.matchingId,
     })
-
-    const blockPromises: (() => Promise<void>)[] = []
-    for (const r of chains) {
-      blockPromises.push(async () => {
-        r.start = await r.blockTimestampClient.getBlockNumberAtOrBefore(
-          UnixTime.toStartOf(UnixTime.now(), 'day') - UnixTime.DAY,
-        )
-        r.end = await r.blockTimestampClient.getBlockNumberAtOrBefore(
-          UnixTime.toStartOf(UnixTime.now(), 'day'),
-        )
-      })
-    }
-    logger.info('Fetching block numbers...')
-    await Promise.all(blockPromises.map((fn) => fn()))
-
-    const messages: Message[] = []
-    const promises: (() => Promise<void>)[] = []
-    for (const r of chains) {
-      const totalRange = r.end - r.start
-      const numBatches = Math.ceil(totalRange / r.envioBatchSize)
-
-      for (let batchIndex = 0; batchIndex < numBatches; batchIndex++) {
-        promises.push(async () => {
-          const batchStart = r.start + batchIndex * r.envioBatchSize
-          const batchEnd = Math.min(batchStart + r.envioBatchSize, r.end)
-
-          const transactions = await r.envioClient.getTransactionsWithLogs(
-            batchStart,
-            batchEnd,
-          )
-
-          for (const t of transactions) {
-            for (const decoder of decoders) {
-              const d = decoder(r, t)
-              if (d) {
-                messages.push(d)
-              }
-            }
-          }
-        })
-      }
-    }
-
-    logger.info('Fetching data... (this may take a while)')
-    await Promise.all(promises.map((fn) => fn()))
-
-    const matching: Record<
-      string,
-      Record<string, { send: Message[]; receive: Message[] }>
-    > = {}
-    for (const transfer of messages) {
-      if (!matching[transfer.protocol]) {
-        matching[transfer.protocol] = {}
-      }
-      const protocol = matching[transfer.protocol]
-
-      if (transfer.matchingId) {
-        if (!protocol[transfer.matchingId]) {
-          protocol[transfer.matchingId] = { send: [], receive: [] }
-        }
-        switch (transfer.direction) {
-          case 'outbound':
-            protocol[transfer.matchingId].send.push(transfer)
-            break
-          case 'inbound':
-            protocol[transfer.matchingId].receive.push(transfer)
-            break
-        }
-      } else {
-        if (!protocol['undefined']) {
-          protocol['undefined'] = { send: [], receive: [] }
-        }
-        switch (transfer.direction) {
-          case 'outbound':
-            protocol['undefined'].send.push(transfer)
-            break
-          case 'inbound':
-            protocol['undefined'].receive.push(transfer)
-            break
-        }
-      }
-    }
-
-    for (const t of messages) {
-      logger.info(`${t.direction} via ${t.protocol}`, {
-        ...t,
-      })
-    }
-
-    for (const [protocol, m] of Array.from(Object.entries(matching))) {
-      const send = messages.filter(
-        (t) => t.protocol === protocol && t.direction === 'outbound',
-      )
-
-      const receive = messages.filter(
-        (t) => t.protocol === protocol && t.direction === 'inbound',
-      )
-      const matchedBreakdown: Record<string, number> = {}
-      Object.entries(m).forEach(([id, match]) => {
-        if (
-          id === 'undefined' ||
-          match.send.length === 0 ||
-          match.receive.length === 0
-        )
-          return
-
-        match.send.forEach((_) => {
-          match.receive.forEach((receiveTx) => {
-            const pairKey = `${receiveTx.origin} -> ${receiveTx.destination}`
-
-            matchedBreakdown[pairKey] = (matchedBreakdown[pairKey] || 0) + 1
-          })
-        })
-      })
-
-      const unmatchedBreakdown: Record<string, number> = {}
-
-      Object.entries(m).forEach(([id, match]) => {
-        if (id === 'undefined') return
-        if (match.send.length > 0 && match.receive.length === 0) {
-          match.send.forEach((sendTx) => {
-            const pairKey = `${sendTx.origin} -> X`
-            unmatchedBreakdown[pairKey] = (unmatchedBreakdown[pairKey] || 0) + 1
-          })
-        }
-      })
-
-      Object.entries(m).forEach(([id, match]) => {
-        if (id === 'undefined') return
-        if (match.send.length === 0 && match.receive.length > 0) {
-          match.receive.forEach((receiveTx) => {
-            const pairKey = `X -> ${receiveTx.destination}`
-            unmatchedBreakdown[pairKey] = (unmatchedBreakdown[pairKey] || 0) + 1
-          })
-        }
-      })
-
-      if (m['undefined']) {
-        m['undefined'].send.forEach((sendTx) => {
-          const pairKey = `${sendTx.origin} -> X`
-          unmatchedBreakdown[pairKey] = (unmatchedBreakdown[pairKey] || 0) + 1
-        })
-
-        m['undefined'].receive.forEach((receiveTx) => {
-          const pairKey = `X -> ${receiveTx.destination}`
-          unmatchedBreakdown[pairKey] = (unmatchedBreakdown[pairKey] || 0) + 1
-        })
-      }
-
-      logger.info(protocol, {
-        send: {
-          count: send.length,
-          breakdown: Object.entries(groupBy(send, (s) => s.origin)).reduce(
-            (acc, [chain, items]) => {
-              acc[chain] = items.length
-              return acc
-            },
-            {} as Record<string, number>,
-          ),
-        },
-        receive: {
-          count: receive.length,
-          breakdown: Object.entries(
-            groupBy(receive, (s) => s.destination),
-          ).reduce(
-            (acc, [chain, items]) => {
-              acc[chain] = items.length
-              return acc
-            },
-            {} as Record<string, number>,
-          ),
-        },
-        matched: {
-          count: Object.entries(m).filter(
-            ([_, m]) => m.send.length > 0 && m.receive.length > 0,
-          ).length,
-          breakdown: matchedBreakdown,
-        },
-        unmatched: {
-          count:
-            Object.entries(m).filter(
-              ([_, m]) => m.send.length === 0 || m.receive.length === 0,
-            ).length +
-            (m['undefined']?.send.length ?? 0) +
-            (m['undefined']?.receive.length ?? 0),
-          breakdown: unmatchedBreakdown,
-        },
-      })
-    }
-    process.exit(0)
   },
 })
+
+function setupChains(
+  args: {
+    protocol: string
+    chain1: string
+    block1: number
+    chain2: string
+    block2: number
+  },
+  logger: Logger,
+) {
+  const http = new HttpClient()
+  const env = getEnv()
+
+  const chain1 = CHAINS.find((c) => c.name === args.chain1)
+  assert(chain1, `${args.chain1}: Chain not found`)
+
+  const chain2 = CHAINS.find((c) => c.name === args.chain2)
+  assert(chain2, `${args.chain2}: Chain not found`)
+
+  const chains = (
+    [
+      [chain1, args.block1],
+      [chain2, args.block2],
+    ] as [Chain, number][]
+  ).map(([chain, block]) => ({
+    ...chain,
+    block,
+    rpc: new RpcClient({
+      url: env.string(`${chain.name.toUpperCase()}_RPC_URL`),
+      sourceName: chain.name,
+      http,
+      logger,
+      callsPerMinute: chain.rpcCallsPerMinute,
+      retryStrategy: 'SCRIPT',
+    }),
+  }))
+  return chains
+}
 
 run(cmd, process.argv.slice(2))
