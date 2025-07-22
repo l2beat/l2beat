a02143f0fff6df3e2d74906a59ec73fc6c463943
Radina Talanova <nt.radina@gmail.com>
2025-07-22 15:43:09 +0300

    fix

--- packages/frontend/src/pages/ecosystems/project/components/widgets/EcosystemLearnMoreLink.tsx
+++ packages/frontend/src/pages/ecosystems/project/components/widgets/EcosystemLearnMoreLink.tsx
@@ -20,7 +20,7 @@ export function EcosystemLearnMoreLink({ name, href, className }: Props) {
     >
       <div className="relative flex h-full flex-col justify-center rounded bg-pure-black px-4 text-pure-white">
         <div className="font-medium text-2xs uppercase transition-opacity group-hover:opacity-0">
-          Interested in learning more about the ecosystem??
+          Interested in learning more about the ecosystem?
         </div>
         <div className="group-hover:-translate-y-2 origin-left text-balance font-bold text-lg leading-tight! transition-all ease-in-out will-change-transform group-hover:translate-x-4 group-hover:scale-125 sm:text-xl">
           Learn more about {name}
