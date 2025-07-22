2a320c8b49934d12537bf6473755623d2d49af76
torztomasz <tomasz.torz@l2beat.com>
2025-07-22 13:49:10 +0200

    fix costs flicker

--- packages/frontend/src/server/features/scaling/costs/getProjectCostsChart.ts
+++ packages/frontend/src/server/features/scaling/costs/getProjectCostsChart.ts
@@ -73,7 +73,7 @@ export async function getProjectCostsChart(params: ProjectCostsChartParams) {
   })
 
   const perL2Uop =
-    costsUopsCount !== undefined && resolution === 'daily'
+    costsUopsCount !== undefined && resolution !== 'hourly'
       ? mapToPerL2UopsCost(total, {
           costs: costsUopsCount,
           throughput: throughputUopsCount,
