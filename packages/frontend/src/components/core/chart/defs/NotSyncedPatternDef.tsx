64390972a7461098d56e49d0ec2d115b6bca7c51
torztomasz <tomasz.torz@l2beat.com>
2025-07-25 09:10:58 +0200

    add dark mode pattern

--- packages/frontend/src/components/core/chart/defs/NotSyncedPatternDef.tsx
+++ packages/frontend/src/components/core/chart/defs/NotSyncedPatternDef.tsx
@@ -7,7 +7,20 @@ export function NotSyncedPatternDef() {
       height="20"
       patternTransform="rotate(45)"
     >
-      <rect width="10" height="20" fill="var(--secondary)" fillOpacity={0.25} />
+      <rect
+        width="10"
+        height="20"
+        fill="var(--secondary)"
+        fillOpacity={0.25}
+        className="dark:hidden"
+      />
+      <rect
+        width="10"
+        height="20"
+        fill="var(--secondary)"
+        fillOpacity={0.5}
+        className="not-dark:hidden"
+      />
     </pattern>
   )
 }
