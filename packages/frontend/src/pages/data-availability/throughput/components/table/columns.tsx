76b20d82eaf62332c5d6edda41c1856e36018947
Luca Donno <donnoh99@gmail.com>
2025-07-28 11:33:32 +0200

    fix styling

--- packages/frontend/src/pages/data-availability/throughput/components/table/columns.tsx
+++ packages/frontend/src/pages/data-availability/throughput/components/table/columns.tsx
@@ -157,7 +157,10 @@ export const publicSystemsColumns = [
 
       return (
         <SyncStatusWrapper isSynced={ctx.row.original.isSynced}>
-          <ValueWithPercentageChange change={data.change}>
+          <ValueWithPercentageChange 
+            change={data.change}
+            className="font-medium text-xs md:text-sm"
+          >
             {formatBytes(data.totalPosted)}
           </ValueWithPercentageChange>
         </SyncStatusWrapper>
