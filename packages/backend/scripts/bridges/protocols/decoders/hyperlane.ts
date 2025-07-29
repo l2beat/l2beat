16c56cba8937c36475826e83834e53c347c02fee
antooni <antoni.pawlak@protonmail.com>
2025-07-29 08:42:01 +0200

    hyperlane

--- packages/backend/scripts/bridges/protocols/decoders/hyperlane.ts
+++ packages/backend/scripts/bridges/protocols/decoders/hyperlane.ts
@@ -2,7 +2,7 @@ import { assert, EthereumAddress } from '@l2beat/shared-pure'
 import { decodeEventLog, encodeEventTopics, parseAbi } from 'viem'
 import type { Chain } from '../../chains'
 import type { Asset } from '../../types/Asset'
-import type { TransactionWithLogs } from '../../types/DecoderInput'
+import type { DecoderInput } from '../../types/DecoderInput'
 import type { Message } from '../../types/Message'
 
 export const HYPERLANE = {
@@ -19,98 +19,95 @@ const ABI = parseAbi([
 
 function decoder(
   chain: Chain,
-  transaction: TransactionWithLogs,
-): Partial<{ message: Message; asset: Asset }> | undefined {
-  for (const log of transaction.logs) {
-    const network = NETWORKS.find((b) => b.chainShortName === chain.shortName)
+  input: DecoderInput,
+): Message | Asset | undefined {
+  const network = NETWORKS.find((b) => b.chainShortName === chain.shortName)
 
-    if (!network) continue
+  if (!network) return undefined
 
-    if (
-      EthereumAddress(log.address) === network.mailbox &&
-      log.topics[0] ===
-        encodeEventTopics({ abi: ABI, eventName: 'Dispatch' })[0]
-    ) {
-      const data = decodeEventLog({
-        abi: ABI,
-        data: log.data,
-        topics: log.topics,
-        eventName: 'Dispatch',
-      })
+  if (
+    EthereumAddress(input.log.address) === network.mailbox &&
+    input.log.topics[0] ===
+      encodeEventTopics({ abi: ABI, eventName: 'Dispatch' })[0]
+  ) {
+    const data = decodeEventLog({
+      abi: ABI,
+      data: input.log.data,
+      topics: input.log.topics,
+      eventName: 'Dispatch',
+    })
 
-      const destination = NETWORKS.find(
-        (b) => b.chainId === data.args.destination,
-      )?.chainShortName
+    const destination = NETWORKS.find(
+      (b) => b.chainId === data.args.destination,
+    )?.chainShortName
 
-      const logWithId = transaction.logs.find(
-        (l) =>
-          l.topics[0] ===
-          encodeEventTopics({ abi: ABI, eventName: 'DispatchId' })[0],
-      )
-      assert(logWithId, 'DispatchId not emitted')
+    const logWithId = input.transactionLogs.find(
+      (l) =>
+        l.topics[0] ===
+        encodeEventTopics({ abi: ABI, eventName: 'DispatchId' })[0],
+    )
+    assert(logWithId, 'DispatchId not emitted')
 
-      const id = decodeEventLog({
-        abi: ABI,
-        data: logWithId.data,
-        topics: logWithId.topics,
-        eventName: 'DispatchId',
-      }).args.messageId
+    const id = decodeEventLog({
+      abi: ABI,
+      data: logWithId.data,
+      topics: logWithId.topics,
+      eventName: 'DispatchId',
+    }).args.messageId
 
-      return {
-        message: {
-          direction: 'outbound',
-          protocol: HYPERLANE.name,
-          origin: chain.shortName,
-          destination: destination ?? data.args.destination.toString(),
-          blockTimestamp: transaction.blockTimestamp,
-          txHash: transaction.hash,
-          type: 'Dispatch',
-          matchingId: id,
-        },
-      }
+    return {
+      type: 'message',
+      direction: 'outbound',
+      protocol: HYPERLANE.name,
+      origin: chain.shortName,
+      destination: destination ?? data.args.destination.toString(),
+      blockTimestamp: input.blockTimestamp,
+      txHash: input.transactionHash,
+      customType: 'Dispatch',
+      matchingId: id,
     }
+  }
 
-    if (
-      EthereumAddress(log.address) === network.mailbox &&
-      log.topics[0] === encodeEventTopics({ abi: ABI, eventName: 'Process' })[0]
-    ) {
-      const data = decodeEventLog({
-        abi: ABI,
-        data: log.data,
-        topics: log.topics,
-        eventName: 'Process',
-      })
+  if (
+    EthereumAddress(input.log.address) === network.mailbox &&
+    input.log.topics[0] ===
+      encodeEventTopics({ abi: ABI, eventName: 'Process' })[0]
+  ) {
+    const data = decodeEventLog({
+      abi: ABI,
+      data: input.log.data,
+      topics: input.log.topics,
+      eventName: 'Process',
+    })
 
-      const origin = NETWORKS.find(
-        (c) => c.chainId === data.args.origin,
-      )?.chainShortName
+    const origin = NETWORKS.find(
+      (c) => c.chainId === data.args.origin,
+    )?.chainShortName
 
-      const logWithId = transaction.logs.find(
-        (l) =>
-          l.topics[0] ===
-          encodeEventTopics({ abi: ABI, eventName: 'ProcessId' })[0],
-      )
-      assert(logWithId, 'DispatchId not emitted')
+    const logWithId = input.transactionLogs.find(
+      (l) =>
+        l.topics[0] ===
+        encodeEventTopics({ abi: ABI, eventName: 'ProcessId' })[0],
+    )
+    assert(logWithId, 'DispatchId not emitted')
 
-      const id = decodeEventLog({
-        abi: ABI,
-        data: logWithId.data,
-        topics: logWithId.topics,
-        eventName: 'ProcessId',
-      }).args.messageId
+    const id = decodeEventLog({
+      abi: ABI,
+      data: logWithId.data,
+      topics: logWithId.topics,
+      eventName: 'ProcessId',
+    }).args.messageId
 
-      return {
-        message: {
-          direction: 'inbound',
-          protocol: HYPERLANE.name,
-          origin: origin ?? data.args.origin.toString(),
-          destination: chain.shortName,
-          blockTimestamp: transaction.blockTimestamp,
-          txHash: transaction.hash,
-          type: 'Process',
-          matchingId: id,
-        },
-      }
+    return {
+      type: 'message',
+      direction: 'inbound',
+      protocol: HYPERLANE.name,
+      origin: origin ?? data.args.origin.toString(),
+      destination: chain.shortName,
+      blockTimestamp: input.blockTimestamp,
+      txHash: input.transactionHash,
+      customType: 'Process',
+      matchingId: id,
     }
   }
 
