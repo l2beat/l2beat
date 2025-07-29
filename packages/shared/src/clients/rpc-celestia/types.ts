b831b0629b045d7233b6cda38544f8e52b31cea6
maciekop-l2b <maciej.opala@l2beat.com>
2025-07-29 14:00:48 +0200

    [L2B-11168] Update RPC data structure for Celestia

--- packages/shared/src/clients/rpc-celestia/types.ts
+++ packages/shared/src/clients/rpc-celestia/types.ts
@@ -13,19 +13,6 @@ export const CelestiaBlockResponse = v.object({
   result: CelestiaBlock,
 })
 
-const CelestiaLogs = v.array(
-  v.object({
-    events: v.array(
-      v.object({
-        type: v.string(),
-        attributes: v.array(
-          v.object({ key: v.string(), value: v.string().optional() }),
-        ),
-      }),
-    ),
-  }),
-)
-
 export type CelestiaBlockResult = v.infer<typeof CelestiaBlockResult>
 const CelestiaBlockResult = v.object({
   height: v.string(),
@@ -33,14 +20,14 @@ const CelestiaBlockResult = v.object({
     v.null(),
     v.array(
       v.object({
-        log: v.unknown().transform((val) => {
-          let array = []
-          try {
-            array = JSON.parse(String(val))
-          } catch {}
-
-          return CelestiaLogs.parse(array)
-        }),
+        events: v.array(
+          v.object({
+            type: v.string(),
+            attributes: v.array(
+              v.object({ key: v.string(), value: v.string().optional() }),
+            ),
+          }),
+        ),
       }),
     ),
   ]),
