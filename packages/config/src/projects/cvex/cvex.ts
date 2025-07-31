546582df539e77a40b44ea41e16db8100bc662f4
Alejandro M <183810769+alejandrogogogo@users.noreply.github.com>
2025-07-31 15:35:03 +0800

    Add CVEX chain

---
+++ packages/config/src/projects/cvex/cvex.ts
@@ -0,0 +1,32 @@
+import { ProjectId, UnixTime } from '@l2beat/shared-pure'
+import type { ScalingProject } from '../../internalTypes'
+import { upcomingL3 } from '../../templates/upcoming'
+
+export const cvex: ScalingProject = upcomingL3({
+  id: 'cvex',
+  capability: 'universal',
+  addedAt: UnixTime(1753947052),
+  hostChain: ProjectId('arbitrum'),
+  display: {
+    name: 'CVEX',
+    slug: 'cvex',
+    description:
+      'CVEX is a decentralized trading protocol powered by Arbitrum Orbit, offering ultra-low fees and a smoother trading experience on its own dedicated chain.',
+    purposes: ['Exchange'],
+    category: 'Optimium',
+    stacks: ['Arbitrum'],
+    links: {
+      websites: ['https://jetstream.trade/'],
+      documentation: ['https://docs.jetstream.trade/'],
+      repositories: ['https://github.com/0xSyndr'],
+      socialMedia: [
+        'https://t.me/JetStreamTrade',
+        'https://discord.com/invite/WhYYaHHhy3',
+        'https://x.com/JetstreamTrade',
+      ],
+    },
+  },
+  ecosystemInfo: {
+    id: ProjectId('arbitrum-orbit'),
+  },
+})
