f9f9fc78c2b87aebac0f52c74efe1ade2ad8dda0
torztomasz <tomasz.torz@l2beat.com>
2025-08-01 10:54:16 +0200

    fix tvs breakdown

--- packages/frontend/src/server/features/scaling/tvs/utils/getTvsTargetTimestamp.ts
+++ packages/frontend/src/server/features/scaling/tvs/utils/getTvsTargetTimestamp.ts
@@ -1,9 +1,8 @@
 import { UnixTime } from '@l2beat/shared-pure'
 
 export function getTvsTargetTimestamp() {
-  return (
-    UnixTime.toStartOf(UnixTime.now(), 'hour') -
-    UnixTime.HOUR -
-    15 * UnixTime.MINUTE
+  return UnixTime.toStartOf(
+    UnixTime.now() - UnixTime.HOUR - 15 * UnixTime.MINUTE,
+    'hour',
   )
 }
