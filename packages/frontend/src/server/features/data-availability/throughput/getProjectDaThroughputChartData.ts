66375686569307a3982bbe2092d8a4203a1448e0
anim001k <140460766+anim001k@users.noreply.github.com>
2025-08-01 17:09:41 +0200

    Update getProjectDaThroughputChartData.ts

--- packages/frontend/src/server/features/data-availability/throughput/getProjectDaThroughputChartData.ts
+++ packages/frontend/src/server/features/data-availability/throughput/getProjectDaThroughputChartData.ts
@@ -7,7 +7,7 @@ import { getDaThroughputTable } from './getDaThroughputTable'
 import {
   getProjectDaThroughputChart,
   ProjectDaThroughputChartParams,
-} from './getProjectDaThroughtputChart'
+} from './getProjectDaThroughputChart'
 
 export type ProjectDaThroughputChartData = {
   chart: ProjectDaThroughputChartPoint[]
