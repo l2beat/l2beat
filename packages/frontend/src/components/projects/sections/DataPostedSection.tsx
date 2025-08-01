1eb1494b72f35c63906031615e89a79fe84f7835
torztomasz <tomasz.torz@l2beat.com>
2025-08-01 13:51:50 +0200

    add link to daLayer to data posted section

--- packages/frontend/src/components/projects/sections/DataPostedSection.tsx
+++ packages/frontend/src/components/projects/sections/DataPostedSection.tsx
@@ -1,18 +1,25 @@
 import type { Milestone } from '@l2beat/config'
 import { ProjectDataPostedChart } from '~/components/chart/data-posted/ProjectDataPostedChart'
 import { HorizontalSeparator } from '~/components/core/HorizontalSeparator'
+import { CustomLink } from '~/components/link/CustomLink'
 import type { DataPostedTimeRange } from '~/server/features/scaling/data-posted/range'
 import { ProjectSection } from './ProjectSection'
 import type { ProjectSectionProps } from './types'
 
 export interface DataPostedSectionProps extends ProjectSectionProps {
   projectId: string
+  daLayer: {
+    name: string
+    logo: string
+    href: string
+  }
   milestones: Milestone[]
   defaultRange: DataPostedTimeRange
 }
 
 export function DataPostedSection({
   projectId,
+  daLayer,
   milestones,
   defaultRange,
   ...sectionProps
@@ -20,8 +27,16 @@ export function DataPostedSection({
   return (
     <ProjectSection {...sectionProps}>
       <p className="text-paragraph-15 md:text-paragraph-16">
-        This section shows the amount of data the project has posted to its
-        respective Data Availability (DA) layer.
+        This section shows the amount of data the project has posted to the{' '}
+        <CustomLink href={daLayer.href}>
+          <img
+            src={daLayer.logo}
+            alt={daLayer.name}
+            className="mr-1 inline-block size-6"
+          />
+          <span>{daLayer.name}</span>
+        </CustomLink>
+        .
       </p>
       <HorizontalSeparator className="my-4" />
       <ProjectDataPostedChart
