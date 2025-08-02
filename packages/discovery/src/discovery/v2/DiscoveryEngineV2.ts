02767395d2113c6382214660941db2b1cd181543
Adrian Adamiak <adrian@adamiak.net>
2025-08-02 19:33:59 +0000

    Rethinking DiscoveryEngine

--- packages/discovery/src/discovery/v2/DiscoveryEngineV2.ts
+++ packages/discovery/src/discovery/v2/DiscoveryEngineV2.ts
@@ -9,43 +9,68 @@ interface SourceCodeProvider {
 
 interface FieldHandler {}
 
+interface AddressAnalysisV2 {
+  references: ChainSpecificAddress[]
+}
+
 interface Config {
   timestamp: number
   initialAddresses: ChainSpecificAddress[]
 }
 
-interface TemplateService { 
+interface TemplateService {
   findMatchingTemplate(address: ChainSpecificAddress, sources: string): Config
 }
 
 interface ProxyDetector {}
 
-export class DiscoveryEngineV2 {
+export class AddressAnalyzerV2 {
   constructor(
-    public readonly blockchainReader: BlockchainReader,
     public readonly sourceCodeProvider: SourceCodeProvider,
     public readonly templateManager: TemplateService,
     public readonly fieldHandlers?: FieldHandler[],
-    public readonly proxyDetectors?: ProxyDetector[]
+    public readonly proxyDetectors?: ProxyDetector[],
   ) {}
 
-  discover(config: Config) {
-    const toDiscover = [...config.initialAddresses]
-    for (const address of toDiscover) {
-      // TODO: detect proxy
-      const sources = this.sourceCodeProvider.getSourceCode(address)
-      const template = this.templateManager.findMatchingTemplate(address, sources)
+  analyze(address: ChainSpecificAddress): AddressAnalysisV2 {
+    // TODO: detect proxy
+    const sources = this.sourceCodeProvider.getSourceCode(address)
+    const template = this.templateManager.findMatchingTemplate(address, sources)
+    // TODO: Execute handlers
+    // const functions = [] //sources.abi.functions
+    // for (const function of functions) {
+    //   for (const handler of this.fieldHandlers) {
+    //     ...
+    //   }
+    // }
 
-      // TODO: Execute handlers
-      // const functions = [] //sources.abi.functions
-      // for (const function of functions) {
-      //   for (const handler of this.fieldHandlers) {
-      //     ...
-      //   }
-      // }
+    // gather references
+    return { references: [] }
+  }
+}
+export class DiscoveryEngineV2 {
+  constructor(
+    public readonly blockchainReader: BlockchainReader,
+    public readonly addressAnalyzer: AddressAnalyzerV2,
+  ) {}
 
-      // gather references
-      // toDiscover.concat(references)
+  async discover(config: Config) {
+    const pending = new Set(config.initialAddresses)
+    const analyzed = new Set<ChainSpecificAddress>()
+    while (toAnalyze.size > 0) {
+      const toAnalyze = Array.from(pending)
+      pending.clear()
+      await Promise.all(
+        toAnalyze.map(async (address) => {
+          const analysis = await this.addressAnalyzer.analyze(address)
+          const newAddresses = analysis.references.filter(
+            (a) => !toAnalyze.has(a) && !analyzed.has(a),
+          )
+          pending.add(...newAddresses)
+        }),
+      )
+      const analysis = this.addressAnalyzer.analyze(address)
+      analysis.references.filter((a) => !analyzed.has(a))
     }
   }
 }
