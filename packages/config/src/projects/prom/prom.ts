d71d746742139a2a6c1e0b2bd3003a85b768cb40
Radina Talanova <nt.radina@gmail.com>
2025-08-04 17:17:48 +0300

    format

--- packages/config/src/projects/prom/prom.ts
+++ packages/config/src/projects/prom/prom.ts
@@ -1,4 +1,4 @@
-import { EthereumAddress, ProjectId, UnixTime } from '@l2beat/shared-pure'
+import { EthereumAddress, UnixTime } from '@l2beat/shared-pure'
 import type { ScalingProject } from '../../internalTypes'
 import { underReviewL2 } from '../../templates/underReview'
 
