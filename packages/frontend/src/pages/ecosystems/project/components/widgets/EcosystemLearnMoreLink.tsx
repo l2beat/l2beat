179da87c48afb0422eaaa5fcfadca186d2373499
torztomasz <tomasz.torz@l2beat.com>
2025-08-07 07:55:51 +0200

    fix links

--- packages/frontend/src/pages/ecosystems/project/components/widgets/EcosystemLearnMoreLink.tsx
+++ packages/frontend/src/pages/ecosystems/project/components/widgets/EcosystemLearnMoreLink.tsx
@@ -22,7 +22,7 @@ export function EcosystemLearnMoreLink({ name, href, className }: Props) {
         <div className="font-medium text-2xs uppercase transition-opacity group-hover:opacity-0">
           Interested in learning more about the ecosystem?
         </div>
-        <div className="group-hover:-translate-y-2 origin-left text-balance font-bold text-lg leading-tight! transition-all ease-in-out will-change-transform group-hover:translate-x-4 group-hover:scale-125 sm:text-xl">
+        <div className="group-hover:-translate-y-2 origin-left text-balance font-bold text-lg leading-tight! transition-[translate,scale] ease-in-out group-hover:translate-x-4 group-hover:scale-125 sm:text-xl">
           Learn more about {name}
         </div>
       </div>
