97af613398956390ab6655f4da9fdd849bf2ea10
torztomasz <tomasz.torz@l2beat.com>
2025-08-08 11:12:28 +0200

    x

--- packages/frontend/src/components/core/chart/Chart.tsx
+++ packages/frontend/src/components/core/chart/Chart.tsx
@@ -105,9 +105,9 @@ function ChartContainer<T extends { timestamp: number }>({
   const isClient = useIsClient()
 
   const hasData = data && data.length > 1
-  console.log(Object.keys(meta), interactiveLegend)
+
   const noDataSourcesSelected = Object.keys(meta).every(
-    (key) => interactiveLegend && !interactiveLegend.dataKeys.includes(key),
+    (key) => interactiveLegend && !interactiveLegend?.dataKeys.includes(key),
   )
   return (
     <ChartContext.Provider value={{ meta, interactiveLegend }}>
