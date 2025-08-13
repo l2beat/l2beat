51c9efea11a1c2e7266fa57044c54fcc5849c2f4
torztomasz <tomasz.torz@l2beat.com>
2025-08-13 12:22:17 +0200

    show correct warnings

--- packages/frontend/src/pages/scaling/tvs/components/table/TotalValueSecuredCell.tsx
+++ packages/frontend/src/pages/scaling/tvs/components/table/TotalValueSecuredCell.tsx
@@ -1,7 +1,13 @@
 import type { WarningWithSentiment } from '@l2beat/config'
 import { TokenBreakdown } from '~/components/breakdown/TokenBreakdown'
 import { ValueSecuredBreakdown } from '~/components/breakdown/ValueSecuredBreakdown'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '~/components/core/tooltip/Tooltip'
 import { ValueWithPercentageChange } from '~/components/table/cells/ValueWithPercentageChange'
+import { sentimentToWarningBarColor, WarningBar } from '~/components/WarningBar'
 import { RoundedWarningIcon } from '~/icons/RoundedWarning'
 import { formatDollarValueNumber } from '~/utils/number-format/formatDollarValueNumber'
 import { TableLink } from '../../../../../components/table/TableLink'
@@ -33,37 +39,55 @@ export function TotalValueSecuredCell(props: TotalValueSecuredCellProps) {
   const anyBadWarnings = tvsWarnings.some((w) => w?.sentiment === 'bad')
 
   return (
-    <TableLink href={props.href}>
-      <div className="flex flex-col items-end">
-        <div className="flex items-center gap-1">
-          {tvsWarnings.length ? (
-            <RoundedWarningIcon
-              className="size-4"
-              sentiment={anyBadWarnings ? 'bad' : 'warning'}
-            />
-          ) : null}
-          <ValueWithPercentageChange change={props.change}>
-            {formatDollarValueNumber(props.total)}
-          </ValueWithPercentageChange>
-        </div>
-        {props.breakdown.type === 'source' ? (
-          <ValueSecuredBreakdown
-            canonical={props.breakdown.canonical}
-            external={props.breakdown.external}
-            native={props.breakdown.native}
-            className="h-[3px] w-[180px]"
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <TableLink href={props.href}>
+          <div className="flex flex-col items-end">
+            <div className="flex items-center gap-1">
+              {tvsWarnings.length ? (
+                <RoundedWarningIcon
+                  className="size-4"
+                  sentiment={anyBadWarnings ? 'bad' : 'warning'}
+                />
+              ) : null}
+              <ValueWithPercentageChange change={props.change}>
+                {formatDollarValueNumber(props.total)}
+              </ValueWithPercentageChange>
+            </div>
+            {props.breakdown.type === 'source' ? (
+              <ValueSecuredBreakdown
+                canonical={props.breakdown.canonical}
+                external={props.breakdown.external}
+                native={props.breakdown.native}
+                className="h-[3px] w-[180px]"
+              />
+            ) : (
+              <TokenBreakdown
+                total={props.total}
+                ether={props.breakdown.ether}
+                stablecoin={props.breakdown.stablecoin}
+                btc={props.breakdown.btc}
+                other={props.breakdown.other}
+                className="h-[3px] w-[180px]"
+              />
+            )}
+          </div>
+        </TableLink>
+      </TooltipTrigger>
+      <TooltipContent className="p-0!">
+        {tvsWarnings?.map((warning, i) => (
+          <WarningBar
+            key={`tvs-warning-${i}`}
+            icon={RoundedWarningIcon}
+            text={warning.value}
+            color={sentimentToWarningBarColor(warning.sentiment)}
+            // Cell itself is a href.
+            // Markdown might contain links - nesting them in a tooltip
+            // breaks semantics apart causing significant layout shifts.
+            ignoreMarkdown
           />
-        ) : (
-          <TokenBreakdown
-            total={props.total}
-            ether={props.breakdown.ether}
-            stablecoin={props.breakdown.stablecoin}
-            btc={props.breakdown.btc}
-            other={props.breakdown.other}
-            className="h-[3px] w-[180px]"
-          />
-        )}
-      </div>
-    </TableLink>
+        ))}
+      </TooltipContent>
+    </Tooltip>
   )
 }
--- packages/frontend/src/server/features/scaling/tvs/getScalingTvsEntries.ts
+++ packages/frontend/src/server/features/scaling/tvs/getScalingTvsEntries.ts
@@ -3,6 +3,7 @@ import type {
   ProjectAssociatedToken,
   WarningWithSentiment,
 } from '@l2beat/config'
+import compact from 'lodash/compact'
 import { groupByScalingTabs } from '~/pages/scaling/utils/groupByScalingTabs'
 import { ps } from '~/server/projects'
 import type { ProjectChanges } from '../../projects-change-report/getProjectsChangeReport'
@@ -12,6 +13,7 @@ import { getCommonScalingEntry } from '../getCommonScalingEntry'
 import { compareStageAndTvs } from '../utils/compareStageAndTvs'
 import type { ProjectSevenDayTvsBreakdown } from './get7dTvsBreakdown'
 import { get7dTvsBreakdown } from './get7dTvsBreakdown'
+import { getAssociatedTokenWarning } from './utils/getAssociatedTokenWarning'
 
 export async function getScalingTvsEntries() {
   const [projectsChangeReport, tvs, projects] = await Promise.all([
@@ -52,12 +54,24 @@ function getScalingTvsEntry(
   changes: ProjectChanges,
   data: ProjectSevenDayTvsBreakdown | undefined,
 ): ScalingTvsEntry | undefined {
+  const associatedTokenWarning =
+    data?.breakdown && data.breakdown.total > 0
+      ? getAssociatedTokenWarning({
+          associatedRatio: data.associated.total / data.breakdown.total,
+          name: project.name,
+          associatedTokens: project.tvsInfo?.associatedTokens ?? [],
+        })
+      : undefined
+
   return {
     ...getCommonScalingEntry({ project, changes }),
     tvs: {
       data,
       associatedTokens: project.tvsInfo.associatedTokens,
-      warnings: project.tvsInfo.warnings,
+      warnings: compact([
+        ...project.tvsInfo.warnings,
+        associatedTokenWarning?.sentiment === 'bad' && associatedTokenWarning,
+      ]),
     },
     tvsOrder: data?.breakdown.total ?? -1,
   }
