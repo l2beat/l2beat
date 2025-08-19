44841e75241cdb5f8fe01882aec3adf01d0df05c
torztomasz <tomasz.torz@l2beat.com>
2025-08-19 15:44:53 +0200

    disable animation

--- packages/frontend/src/components/chart/tvs/TokenChart.tsx
+++ packages/frontend/src/components/chart/tvs/TokenChart.tsx
@@ -81,6 +81,7 @@ export function TokenChart({
           fillOpacity={1}
           stroke="url(#stroke)"
           strokeWidth={2}
+          isAnimationActive={false}
         />
         {getCommonChartComponents({
           data,
