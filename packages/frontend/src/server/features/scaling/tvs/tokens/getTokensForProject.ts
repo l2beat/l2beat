1de48076bb4d89bae6c92ef9b8a6f1b42e9b9335
Tomasz Tórz <93620601+torztomasz@users.noreply.github.com>
2025-08-19 11:17:55 +0200

    Remove group split in token selector (#8901)

--- packages/frontend/src/server/features/scaling/tvs/tokens/getTokensForProject.ts
+++ packages/frontend/src/server/features/scaling/tvs/tokens/getTokensForProject.ts
@@ -5,7 +5,6 @@ import { getDb } from '~/server/database'
 import { getStaticAsset } from '~/server/features/utils/getProjectIcon'
 import { getTvsTargetTimestamp } from '../utils/getTvsTargetTimestamp'
 
-export type ProjectTokens = Record<TvsToken['source'], ProjectToken[]>
 export type ProjectToken = TvsToken & {
   usdValue: number
   iconUrl: string
@@ -13,7 +12,7 @@ export type ProjectToken = TvsToken & {
 
 export async function getTokensForProject(
   project: Project<never, 'tvsConfig'>,
-): Promise<ProjectTokens | undefined> {
+): Promise<ProjectToken[] | undefined> {
   if (!project.tvsConfig) return undefined
   if (env.MOCK) {
     return getMockTokensForProject(project)
@@ -26,68 +25,30 @@ export async function getTokensForProject(
   )
 
   const tokenValuesMap = new Map(tokenValues.map((x) => [x.tokenId, x]))
+  const placeholderIcon = getStaticAsset('/images/token-placeholder.png')
 
-  const withUsdValue = project.tvsConfig
+  const withUsdValue: ProjectToken[] = project.tvsConfig
     .map((t) => {
       const tokenValue = tokenValuesMap.get(t.id)
       if (!tokenValue) return undefined
       return {
         ...t,
         usdValue: tokenValue.valueForProject,
+        iconUrl: t.iconUrl ?? placeholderIcon,
       }
     })
     .filter(notUndefined)
 
   withUsdValue.sort((a, b) => b.usdValue - a.usdValue)
 
-  return groupBySource(withUsdValue)
-}
-
-function groupBySource(
-  tokens: (Omit<ProjectToken, 'iconUrl'> & { iconUrl?: string })[],
-) {
-  const canonical: ProjectToken[] = []
-  const native: ProjectToken[] = []
-  const external: ProjectToken[] = []
-
-  const placeholderIcon = getStaticAsset('/images/token-placeholder.png')
-  for (const token of tokens) {
-    switch (token.source) {
-      case 'canonical':
-        canonical.push({
-          ...token,
-          iconUrl: token.iconUrl ?? placeholderIcon,
-        })
-        break
-      case 'native':
-        native.push({
-          ...token,
-          iconUrl: token.iconUrl ?? placeholderIcon,
-        })
-        break
-      case 'external':
-        external.push({
-          ...token,
-          iconUrl: token.iconUrl ?? placeholderIcon,
-        })
-        break
-    }
-  }
-
-  return {
-    canonical,
-    native,
-    external,
-  }
+  return withUsdValue
 }
 
 function getMockTokensForProject(project: Project<never, 'tvsConfig'>) {
   if (!project.tvsConfig) return undefined
-  return groupBySource(
-    project.tvsConfig.map((t) => ({
-      ...t,
-      iconUrl: t.iconUrl ?? getStaticAsset('/images/token-placeholder.png'),
-      usdValue: 1000,
-    })),
-  )
+  return project.tvsConfig.map((t) => ({
+    ...t,
+    iconUrl: t.iconUrl ?? getStaticAsset('/images/token-placeholder.png'),
+    usdValue: 1000,
+  }))
 }
