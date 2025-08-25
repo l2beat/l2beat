c113b65cbf340c16260b4ed135cdb3da4f342b02
Michał Podsiadły <michal.podsiadly@l2beat.com>
2025-08-25 14:43:59 +0200

    simplify

--- packages/l2b/src/commands/Minters.ts
+++ packages/l2b/src/commands/Minters.ts
@@ -1,18 +1,12 @@
-import {
-  type DiscoveryChainConfig,
-  getChainConfig,
-  type IProvider,
-} from '@l2beat/discovery'
-import type {
-  DebugTransactionCall,
-  DebugTransactionCallResponse,
-} from '@l2beat/discovery/dist/discovery/provider/DebugTransactionTrace'
-import { Hash256 } from '@l2beat/shared-pure'
+import { getChainConfig } from '@l2beat/discovery'
 import { command, positional, restPositionals, string } from 'cmd-ts'
-import { utils } from 'ethers'
-import { Listr } from 'listr2'
 import { getProvider } from '../implementations/common/GetProvider'
-import { getMintTransactions } from '../implementations/minters/getMinters'
+import { getPlainLogger } from '../implementations/common/getPlainLogger'
+import {
+  getDebugTraces,
+  getMintTransactions,
+  traverseTraces,
+} from '../implementations/minters/getMinters'
 import { EthereumAddressValue } from './types'
 
 export const Minters = command({
@@ -30,126 +24,34 @@ export const Minters = command({
     }),
   },
   handler: async (args) => {
+    const logger = getPlainLogger()
     const chain = getChainConfig(args.chain)
 
     const provider = await getProvider(chain.rpcUrl, chain.explorer)
     Object.assign(provider, { chain: args.chain })
 
-    interface Ctx {
-      provider: IProvider
-      chain: DiscoveryChainConfig
-      transactions: string[]
-      traces: Record<string, DebugTransactionCallResponse>
-      minters: string[]
+    logger.info('Getting mint transactions...')
+    const transactions = await getMintTransactions(provider, args.address)
+    logger.info(`Done. Got ${transactions.length} transactions`)
+
+    logger.info('Getting debug traces...')
+    let counter = 0
+    const onFetch = (txHash: string) => {
+      counter++
+      logger.info(
+        `Fetched trace for ${txHash} [${counter}/${transactions.length}] `,
+      )
     }
+    const traces = await getDebugTraces(provider, transactions, onFetch)
+    logger.info(`Done. Got ${traces.length} traces`)
 
-    const tasks = new Listr<Ctx>([
-      {
-        title: 'Setting up provider',
-        task: (ctx) => {
-          ctx.provider = provider
-          ctx.chain = chain
-        },
-      },
-      {
-        title: 'Pulling transactions',
-        task: async (ctx, task) => {
-          ctx.transactions = await getMintTransactions(
-            ctx.provider,
-            args.address,
-          )
-          task.title = `Fetched ${ctx.transactions.length} transaction(s)`
+    logger.info('Traversing traces...')
+    const minters = traverseTraces(traces)
+    logger.info(`Done. Got ${minters.length} minters`)
 
-          if (ctx.transactions.length === 0) {
-            throw new Error('No transactions found')
-          }
-        },
-      },
-      {
-        title: 'Pulling debug trace(s)',
-        enabled: (ctx) => (ctx.transactions ?? []).length > 0,
-        task: (ctx, task) => {
-          ctx.traces = {}
-          let readyCounter = 0
-
-          function onFinish(txHash: string) {
-            readyCounter++
-            task.output = txHash
-            task.title = `Pulled ${readyCounter}/${ctx.transactions.length} trace(s)`
-          }
-
-          return task.newListr(
-            (ctx.transactions ?? []).map((txHash) => ({
-              task: async () => {
-                ctx.traces[txHash] = await ctx.provider.getDebugTrace(
-                  Hash256(txHash),
-                )
-
-                onFinish(txHash)
-              },
-            })),
-            {
-              concurrent: true,
-            },
-          )
-        },
-      },
-      {
-        title: 'Traversing traces',
-        enabled: (ctx) => Object.keys(ctx.traces ?? {}).length > 0,
-        task: (ctx, task) => {
-          const sendersSet = new Set<string>()
-
-          for (const trace of Object.values(ctx.traces)) {
-            for (const call of trace.calls ?? []) {
-              for (const { sender } of walk(call, call.from)) {
-                sendersSet.add(sender)
-              }
-            }
-          }
-
-          ctx.minters = [...sendersSet]
-          task.title = `Found ${ctx.minters.length} function caller(s)`
-        },
-      },
-    ])
-
-    const { minters } = await tasks.run()
-
-    printList(minters)
+    logger.info('Minters:')
+    for (const minter of minters) {
+      logger.info(`• ${minter}`)
+    }
   },
 })
-
-function* walk(
-  call: DebugTransactionCall,
-  lastCallSender: string,
-): IterableIterator<{ sender: string }> {
-  const abi = [
-    'event Transfer(address indexed from, address indexed to, uint256 value)',
-  ]
-  const iface = new utils.Interface(abi)
-  const topic0 = iface.getEventTopic('Transfer')
-
-  const isMint = call.logs?.some((log) => {
-    const [eventTopic, from] = log.topics
-
-    return eventTopic === topic0 && from === Hash256.ZERO
-  })
-
-  if (isMint) {
-    const sender = call.type === 'DELEGATECALL' ? lastCallSender : call.from
-    yield { sender: sender.toLowerCase() }
-  }
-
-  const nextLastCallSender = call.type === 'CALL' ? call.from : lastCallSender
-
-  for (const nested of call.calls ?? []) {
-    yield* walk(nested, nextLastCallSender)
-  }
-}
-
-function printList(list: string[]) {
-  for (const item of list) {
-    console.log(`• ${item}`)
-  }
-}
--- packages/l2b/src/implementations/minters/getMinters.ts
+++ packages/l2b/src/implementations/minters/getMinters.ts
@@ -1,16 +1,25 @@
 import type { IProvider } from '@l2beat/discovery'
-import { ChainSpecificAddress, EthereumAddress } from '@l2beat/shared-pure'
+import type {
+  DebugTransactionCall,
+  DebugTransactionCallResponse,
+} from '@l2beat/discovery/dist/discovery/provider/DebugTransactionTrace'
+import {
+  ChainSpecificAddress,
+  EthereumAddress,
+  Hash256,
+} from '@l2beat/shared-pure'
 import { utils } from 'ethers'
+
+const abi = [
+  'event Transfer(address indexed from, address indexed to, uint256 value)',
+]
+const iface = new utils.Interface(abi)
+const topic0 = iface.getEventTopic('Transfer')
 
 export async function getMintTransactions(
   provider: IProvider,
   address: EthereumAddress,
 ) {
-  const abi = [
-    'event Transfer(address indexed from, address indexed to, uint256 value)',
-  ]
-
-  const iface = new utils.Interface(abi)
   const topics = iface.encodeFilterTopics('Transfer', [EthereumAddress.ZERO])
 
   const chainSpecificAddress = ChainSpecificAddress.fromLong(
@@ -21,4 +30,54 @@ export async function getMintTransactions(
   const logs = await provider.getLogs(chainSpecificAddress, topics)
 
   return [...new Set(logs.map((log) => log.transactionHash))]
+}
+
+export function getDebugTraces(
+  provider: IProvider,
+  transactions: string[],
+  onFetch?: (txHash: string) => void,
+) {
+  return Promise.all(
+    transactions.map(async (txHash) => {
+      const result = await provider.getDebugTrace(Hash256(txHash))
+      onFetch?.(txHash)
+      return result
+    }),
+  )
+}
+
+export function traverseTraces(traces: DebugTransactionCallResponse[]) {
+  const sendersSet = new Set<string>()
+
+  for (const trace of traces) {
+    for (const call of trace.calls ?? []) {
+      for (const { sender } of walk(call, call.from)) {
+        sendersSet.add(sender)
+      }
+    }
+  }
+
+  return [...sendersSet]
+}
+
+function* walk(
+  call: DebugTransactionCall,
+  lastCallSender: string,
+): IterableIterator<{ sender: string }> {
+  const isMint = call.logs?.some((log) => {
+    const [eventTopic, from] = log.topics
+
+    return eventTopic === topic0 && from === Hash256.ZERO
+  })
+
+  if (isMint) {
+    const sender = call.type === 'DELEGATECALL' ? lastCallSender : call.from
+    yield { sender: sender.toLowerCase() }
+  }
+
+  const nextLastCallSender = call.type === 'CALL' ? call.from : lastCallSender
+
+  for (const nested of call.calls ?? []) {
+    yield* walk(nested, nextLastCallSender)
+  }
 }
