163eb37ff565da659404ee4270936bed49d50317
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2025-08-26 16:03:32 +0200

    Add Linea on Linea (#8927)

--- packages/config/src/projects/linea/linea.ts
+++ packages/config/src/projects/linea/linea.ts
@@ -36,7 +36,7 @@ import type { ProjectPermissionedAccount } from '../../types'
 const discovery = new ProjectDiscovery('linea')
 
 const timelockDelay = discovery.getContractValue<number>(
-  'L1Timelock',
+  'Timelock',
   'getMinDelay',
 )
 const timelockDelayString = formatSeconds(timelockDelay)
@@ -58,6 +58,40 @@ assert(
   zodiacPausers.length === zodiacPausersHardcoded.length &&
     zodiacPausers[0].address === zodiacPausersHardcoded[0].address,
   'disco config is wrong for the pausers, check hardcoded pausers in the Roles module',
+)
+
+const zodiacL2Roles = discovery.getContractValue<{
+  roles: Record<string, Record<string, boolean>>
+}>('L2Roles', 'roles')
+const zodiacVoyageXpMinterRole = '1'
+const zodiacVoyageXpMinters: ProjectPermissionedAccount[] =
+  discovery.formatPermissionedAccounts(
+    Object.keys(zodiacL2Roles.roles[zodiacVoyageXpMinterRole].members),
+  )
+const zodiacVoyageXpMintersHardcoded = discovery.getPermissionedAccounts(
+  'L2Roles',
+  'voyageXpMinters',
+)
+assert(
+  zodiacVoyageXpMinters.length === zodiacVoyageXpMintersHardcoded.length &&
+    zodiacVoyageXpMinters[0].address ===
+      zodiacVoyageXpMintersHardcoded[0].address,
+  'disco config is wrong for the voyageXpMinters, check hardcoded voyageXpMinters in the L2Roles module',
+)
+
+const zodiacL2PauserRole = '2'
+const zodiacL2Pausers: ProjectPermissionedAccount[] =
+  discovery.formatPermissionedAccounts(
+    Object.keys(zodiacL2Roles.roles[zodiacL2PauserRole].members),
+  )
+const zodiacL2PausersHardcoded = discovery.getPermissionedAccounts(
+  'L2Roles',
+  'pausers',
+)
+assert(
+  zodiacL2Pausers.length === zodiacL2PausersHardcoded.length &&
+    zodiacL2Pausers[0].address === zodiacL2PausersHardcoded[0].address,
+  'disco config is wrong for the L2 pausers, check hardcoded pausers in the L2Roles module',
 )
 
 const periodInSeconds = discovery.getContractValue<number>(
