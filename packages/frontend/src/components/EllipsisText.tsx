f30c78e26bdc923124adf8f394ef6c9057a6d92f
torztomasz <tomasz.torz@l2beat.com>
2025-08-28 11:25:42 +0200

    change design

--- packages/frontend/src/components/EllipsisText.tsx
+++ packages/frontend/src/components/EllipsisText.tsx
@@ -4,9 +4,15 @@ interface Props {
   children: string
   className?: string
   maxChars?: number
+  accessoryRight?: React.ReactNode
 }
 
-export function EllipsisText({ children, className, maxChars = 20 }: Props) {
+export function EllipsisText({
+  children,
+  className,
+  maxChars = 20,
+  accessoryRight,
+}: Props) {
   return (
     // tabIndex is required to make the div focusable
     <span
@@ -16,8 +22,9 @@ export function EllipsisText({ children, className, maxChars = 20 }: Props) {
       {children.slice(0, maxChars).trimEnd()}
       {children.length > maxChars && <span>&hellip;</span>}
       {children.length > maxChars && (
-        <div className="absolute inset-y-0 hidden bg-primary text-primary-invert group-hover/text-ellipsis:block group-focus/text-ellipsis:block">
+        <div className="absolute inset-y-0 z-10 hidden bg-surface-secondary text-primary group-hover/text-ellipsis:block group-focus/text-ellipsis:block">
           {children}
+          {accessoryRight}
         </div>
       )}
     </span>
