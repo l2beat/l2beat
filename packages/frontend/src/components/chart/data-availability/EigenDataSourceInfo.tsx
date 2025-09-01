d7ad65786329deedb0352e691eb347bab7ac5a7e
Radina Talanova <nt.radina@gmail.com>
2025-09-01 10:11:49 +0300

    Update EigenDataSourceInfo.tsx

--- packages/frontend/src/components/chart/data-availability/EigenDataSourceInfo.tsx
+++ packages/frontend/src/components/chart/data-availability/EigenDataSourceInfo.tsx
@@ -12,8 +12,7 @@ export function EigenDataSourceInfo() {
   return (
     <>
       <div className="font-medium text-2xs text-secondary max-sm:hidden">
-        Data source: API provided by EigenLayer. Please note that the maximum
-        throughput claimed of 100 MiB/s has not been verified by L2BEAT.
+        Data source: API provided by EigenLayer.
       </div>
       <Drawer>
         <DrawerTrigger className="flex items-center gap-1 font-medium text-[13px] text-secondary sm:hidden">
