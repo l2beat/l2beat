6b12a233a1680d7047cd898b58966671c366c182
Maciej Zygmunt <maciekzygmunt@interia.pl>
2025-09-01 12:48:41 +0200

    use monthly updates

--- packages/frontend/src/pages/publications/PublicationsRouter.ts
+++ packages/frontend/src/pages/publications/PublicationsRouter.ts
@@ -2,11 +2,12 @@ import { v } from '@l2beat/validate'
 import express from 'express'
 import { getCollectionEntry } from '~/content/getCollection'
 import type { ICache } from '~/server/cache/ICache'
-import type { RenderFunction } from '~/ssr/types'
+import type { RenderData, RenderFunction } from '~/ssr/types'
 import { validateRoute } from '~/utils/validateRoute'
 import type { Manifest } from '../../utils/Manifest'
 import { getPublicationsData } from './getPublicationsData'
 import { getGovernancePublicationData } from './governance/GetGovernancePublicationData'
+import { getMonthlyUpdateData } from './monthly-updates/getMonthlyUpdateData'
 
 export function createPublicationsRouter(
   manifest: Manifest,
@@ -32,21 +33,33 @@ export function createPublicationsRouter(
       params: v.object({ id: v.string() }),
     }),
     async (req, res) => {
-      const isGovernancePublication = getCollectionEntry(
+      const isGovernancePublication = !!getCollectionEntry(
         'governance-publications',
         req.params.id,
       )
 
-      if (!isGovernancePublication) {
-        res.status(404).send('Not found')
-        return
+      const isMonthlyUpdate = !!getCollectionEntry(
+        'monthly-updates',
+        req.params.id,
+      )
+
+      let data: RenderData | undefined
+      if (isGovernancePublication) {
+        data = await getGovernancePublicationData(
+          manifest,
+          req.params.id,
+          req.originalUrl,
+        )
       }
 
-      const data = await getGovernancePublicationData(
-        manifest,
-        req.params.id,
-        req.originalUrl,
-      )
+      if (isMonthlyUpdate) {
+        data = await getMonthlyUpdateData(
+          manifest,
+          req.params.id,
+          req.originalUrl,
+          cache,
+        )
+      }
 
       if (!data) {
         res.status(404).send('Not found')
