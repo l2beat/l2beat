cc19dd2da2121385166aad72b0fe0af137e086c3
Maciej Zygmunt <maciekzygmunt@interia.pl>
2025-09-04 10:26:09 +0200

    use in ethereum connect

--- packages/frontend/src/pages/governance/GovernanceRouter.ts
+++ packages/frontend/src/pages/governance/GovernanceRouter.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import type { RenderFunction } from '~/ssr/types'
 import type { Manifest } from '../../utils/Manifest'
+import { getRedirectData } from '../redirect/getRedirectData'
 import { getGovernanceData } from './GetGovernanceData'
 
 export function createGovernanceRouter(
@@ -15,8 +16,20 @@ export function createGovernanceRouter(
     res.status(200).send(html)
   })
 
-  router.get('/governance/ethereum-connect', (_, res) => {
-    res.redirect('https://form.typeform.com/to/YVrD7Ava')
+  router.get('/governance/ethereum-connect', (req, res) => {
+    const data = getRedirectData(
+      manifest,
+      'https://form.typeform.com/to/YVrD7Ava',
+      req.originalUrl,
+      {
+        image: '/meta-images/governance/ethereum-connect/opengraph-image.png',
+        title: 'Ethereum Connect Survey',
+        description:
+          'Help us understand the needs of professionals in LATAM who want to learn more about crypto and Ethereum by filling this quick survey',
+      },
+    )
+    const html = render(data, req.originalUrl)
+    res.status(200).send(html)
   })
 
   return router
