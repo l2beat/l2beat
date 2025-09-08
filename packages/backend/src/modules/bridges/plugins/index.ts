c3655462e85e881bd7e04baddf998a98f30ba036
antooni <antoni.pawlak@protonmail.com>
2025-09-08 21:58:37 +0200

    cctpv1

--- packages/backend/src/modules/bridges/plugins/index.ts
+++ packages/backend/src/modules/bridges/plugins/index.ts
@@ -1,6 +1,7 @@
+import { CCTPv1Plugin } from './cctpv1'
 import { StargatePlugin } from './stargate'
 import type { BridgePlugin } from './types'
 
 export function createBridgePlugins(): BridgePlugin[] {
-  return [new StargatePlugin()]
+  return [new StargatePlugin(), new CCTPv1Plugin()]
 }
