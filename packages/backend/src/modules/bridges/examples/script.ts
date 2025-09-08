330fa1d4720d5e66c40e7338372209b01ede58d4
antooni <antoni.pawlak@protonmail.com>
2025-09-08 21:37:33 +0200

    example

---
+++ packages/backend/src/modules/bridges/examples/script.ts
@@ -0,0 +1,114 @@
+import { getEnv, Logger } from '@l2beat/backend-tools'
+import { HttpClient, RpcClient } from '@l2beat/shared'
+import { assert, EthereumAddress } from '@l2beat/shared-pure'
+import { command, run } from 'cmd-ts'
+import { logToViemLog } from '../../../../scripts/bridges/utils/viem'
+import { createBridgePlugins } from '../plugins'
+import type { BridgeEvent } from '../plugins/types'
+import * as examples from './examples.json'
+
+const cmd = command({
+  name: 'bridges:example',
+  args: {},
+  handler: async (args) => {
+    const logger = Logger.INFO
+
+    const project = 'stargate'
+
+    const example = examples[project]
+
+    if (!example) {
+      logger.error(`${project}: example not found, see examples.json`)
+      return
+    }
+
+    const chains = initChains([example.outboud, example.inbound], logger)
+    const plugins = createBridgePlugins()
+
+    const events: BridgeEvent[] = []
+    for (const chain of chains) {
+      const block = await chain.rpc.getBlockWithTransactions(chain.block)
+      const transaction = block.transactions.find(
+        (t) => t.hash?.toLowerCase() === chain.tx,
+      )
+      assert(
+        transaction && transaction.hash,
+        `Transaction not found: ${chain.tx} @ ${chain.name}`,
+      )
+      const logs = await chain.rpc.getLogs(chain.block, chain.block)
+      const txLogs = logs.filter((l) => l.transactionHash === chain.tx)
+
+      for (const log of txLogs) {
+        for (const plugin of plugins) {
+          if (!plugin.capture) {
+            continue
+          }
+          const event = await plugin.capture({
+            log: logToViemLog(log),
+            txLogs: txLogs.map(logToViemLog),
+            ctx: {
+              timestamp: block.timestamp,
+              chain: chain.name,
+              blockNumber: block.number,
+              blockHash: block.hash,
+              txHash: transaction.hash,
+              txTo: transaction.to
+                ? EthereumAddress(transaction.to)
+                : undefined,
+              logIndex: log.logIndex,
+            },
+          })
+
+          if (event) {
+            logger.info('Captured', event)
+            events.push(event)
+          }
+        }
+      }
+    }
+
+    for (const event of events) {
+      for (const plugin of plugins) {
+        if (!plugin.match) {
+          continue
+        }
+
+        const matched = plugin.match(event)
+
+        if (matched) {
+          logger.info('Matched', matched)
+        }
+      }
+    }
+  },
+})
+
+function initChains(
+  chains: {
+    chain: string
+    block: number
+    tx: string
+  }[],
+  logger: Logger,
+) {
+  const http = new HttpClient()
+  const env = getEnv()
+
+  return chains.map(({ chain, block, tx }) => {
+    return {
+      block,
+      tx,
+      name: chain,
+      rpc: new RpcClient({
+        url: env.string(`${chain.toUpperCase()}_RPC_URL`),
+        sourceName: chain,
+        http,
+        logger,
+        callsPerMinute: 600,
+        retryStrategy: 'SCRIPT',
+      }),
+    }
+  })
+}
+
+run(cmd, process.argv.slice(2))
