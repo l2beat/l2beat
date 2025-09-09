20f38b22dd3ed5c8e2f00d7afc42c77784f10d05
sekuba <29250140+sekuba@users.noreply.github.com>
2025-09-09 14:38:41 +0200

    layerzerov2 packet matching
    Co-authored-by: Antoni Pawlak <antoni.pawlak@pm.me>

--- packages/backend/src/modules/bridges/plugins/index.ts
+++ packages/backend/src/modules/bridges/plugins/index.ts
@@ -1,15 +1,17 @@
 import type { Logger } from '@l2beat/backend-tools'
 import { CCTPv1Plugin } from './cctpv1'
+import { LayerZeroV2Plugin } from './layerzerov2'
 import { MayanPlugin } from './mayan'
-import { StargatePlugin } from './stargate'
 import type { BridgePlugin } from './types'
 import { WormholePlugin } from './wormhole'
 
 export function createBridgePlugins(logger: Logger): BridgePlugin[] {
   return [
-    new StargatePlugin(),
+    // new StargatePlugin(),
     new MayanPlugin(),
     new CCTPv1Plugin(),
+    // new StargateV2TaxiPlugin(),
+    new LayerZeroV2Plugin(),
     new WormholePlugin(logger),
   ]
 }
