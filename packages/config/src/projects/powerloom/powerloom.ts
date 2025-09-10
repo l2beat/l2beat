cffe6bc47a1fb630909ac62a82607f2fbd85491e
Luca Donno <donnoh99@gmail.com>
2025-09-10 16:21:42 +0200

    plff

--- packages/config/src/projects/powerloom/powerloom.ts
+++ packages/config/src/projects/powerloom/powerloom.ts
@@ -1,9 +1,9 @@
 import { ChainSpecificAddress, UnixTime } from '@l2beat/shared-pure'
 import { REASON_FOR_BEING_OTHER } from '../../common'
+import { BADGES } from '../../common/badges'
 import { ProjectDiscovery } from '../../discovery/ProjectDiscovery'
 import type { ScalingProject } from '../../internalTypes'
 import { orbitStackL2 } from '../../templates/orbitStack'
-import { BADGES } from '../../common/badges'
 
 const discovery = new ProjectDiscovery('powerloom')
 
