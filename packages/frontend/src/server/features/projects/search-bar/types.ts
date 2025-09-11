f27c776a6678ddf9e850ad7a07db594fb6767310
Tomasz Tórz <93620601+torztomasz@users.noreply.github.com>
2025-09-11 12:37:38 +0200

    Dynamic search bar items (#9196)

--- packages/frontend/src/server/features/projects/search-bar/types.ts
+++ packages/frontend/src/server/features/projects/search-bar/types.ts
@@ -1,26 +1,12 @@
 import type { ProjectScalingCategory } from '@l2beat/config'
-import type { SearchBarCategory } from './searchBarCategories'
-
-interface SearchBarEntry {
-  category: SearchBarCategory
-  name: string
-  tags?: string[]
-  href: string
-}
-
-export type SearchBarPage = SearchBarEntry & {
-  type: 'page'
-}
+import type { SearchBarEntry } from '~/components/search-bar/types'
 
 export type SearchBarProject = SearchBarEntry & {
   type: 'project'
   id: string
   isUpcoming: boolean
   iconUrl: string
-  addedAt: number
   kind: 'layer2' | 'layer3' | 'bridge' | 'da' | 'zkCatalog'
   scalingCategory?: ProjectScalingCategory
   filePrepared?: Fuzzysort.Prepared
 }
-
-export type AnySearchBarEntry = SearchBarPage | SearchBarProject
