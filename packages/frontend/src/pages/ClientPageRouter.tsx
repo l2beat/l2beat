00e9d749058d52160c993b36d774b4e51a6a2ebf
Maciej Zygmunt <maciekzygmunt@interia.pl>
2025-09-12 14:07:18 +0200

    embed form

--- packages/frontend/src/pages/ClientPageRouter.tsx
+++ packages/frontend/src/pages/ClientPageRouter.tsx
@@ -12,6 +12,7 @@ import { DonatePage } from './donate/DonatePage'
 import { EcosystemProjectPage } from './ecosystems/project/EcosystemProjectPage'
 import { FaqPage } from './faq/FaqPage'
 import { GlossaryPage } from './glossary/GlossaryPage'
+import { EthereumConnectPage } from './governance/ethereum-connect/EthereumConnectPage'
 import { GovernancePage } from './governance/GovernancePage'
 import { MultisigReportPage } from './multisig-report/MultisigReportPage'
 import { GovernancePublicationPage } from './publications/governance/GovernancePublicationPage'
@@ -79,6 +80,7 @@ const pages = {
   EcosystemProjectPage,
   // Governance
   GovernancePage,
+  EthereumConnectPage,
   GovernancePublicationPage,
   // Side pages
   FaqPage,
---
+++ packages/frontend/src/pages/governance/ethereum-connect/EthereumConnectPage.tsx
@@ -0,0 +1,12 @@
+import type { AppLayoutProps } from '~/layouts/AppLayout'
+
+interface Props extends AppLayoutProps {}
+
+export function EthereumConnectPage(props: Props) {
+  return (
+    <div>
+      <div data-tf-live="01K4YW0GP4KT7KB36586RAQ969" />
+      <script src="//embed.typeform.com/next/embed.js" async />
+    </div>
+  )
+}
