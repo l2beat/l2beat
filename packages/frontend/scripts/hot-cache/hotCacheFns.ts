5f7b7d521e75098b22775bdf36b859cc1ba624b0
torztomasz <tomasz.torz@l2beat.com>
2025-09-12 12:47:39 +0200

    add crone scheduler on prod

--- packages/frontend/scripts/hot-cache/hotCacheFns.ts
+++ packages/frontend/scripts/hot-cache/hotCacheFns.ts
@@ -20,6 +20,15 @@ class FakeCache implements ICache {
 }
 const cache = new FakeCache()
 
+/*
+ * Hot cache functions
+ *
+ * This is a list of functions that are used to warm up the cache.
+ * It is used in CI to warm up the cache before server starts.
+ * Also, it is used on production server each 30 minute of every hour.
+ * Do not add new functions here without a good reason.
+ * I.e. getScalingTvsData is a database heavy function resulting in >3s of execution time.
+ */
 export const hotCacheFns: Record<string, () => Promise<unknown>> = {
   scalingTvs: () =>
     getScalingTvsData(
