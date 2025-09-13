e98b62687c455dddc4e4d67fcdd9195d1e9f6735
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2025-09-13 08:09:14 +0200

    Add Orbit stack plugin (weak version but good enough) (#9233)
    Co-authored-by: sekuba <29250140+sekuba@users.noreply.github.com>

--- packages/backend/src/modules/bridges/plugins/index.ts
+++ packages/backend/src/modules/bridges/plugins/index.ts
@@ -7,6 +7,7 @@ import { MayanMctpPlugin } from './mayanmctp'
 import { MayanMctpFastPlugin } from './mayanmctp-fast'
 import { MayanSwiftPlugin } from './mayanswift'
 import { OpStackPlugin } from './opstack'
+import { OrbitStackPlugin } from './orbitstack'
 import { StargatePlugin } from './stargate'
 import { StargateV2BusPlugin } from './stargate-v2-bus'
 import { StargateV2TaxiPlugin } from './stargate-v2-taxi'
@@ -33,6 +34,7 @@ export function createBridgePlugins(logger: Logger): BridgePlugin[] {
     new StargatePlugin(logger),
     new AxelarPlugin(),
     new AcrossPlugin(),
+    new OrbitStackPlugin(),
     new OpStackPlugin(),
   ]
 }
