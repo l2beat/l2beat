65ca346a30b0f7a51c502ed5aab946c949f406e6
Adrian Adamiak <adrian@adamiak.net>
2025-09-15 11:28:46 +0000

    UM: Use entrypoint references to show dependent projects

--- packages/backend/src/modules/update-monitor/utils/findDependents.ts
+++ packages/backend/src/modules/update-monitor/utils/findDependents.ts
@@ -1,16 +1,19 @@
 import type { ConfigReader } from '@l2beat/discovery'
 
 export function findDependents(name: string, configReader: ConfigReader) {
-  if (!name.startsWith('shared-')) return []
-
-  const configs = configReader
-    .readAllDiscoveredProjects()
-    .map((project) => configReader.readConfig(project))
+  const allProjects = configReader.readAllDiscoveredProjects()
   const dependents: string[] = []
-  for (const config of configs) {
+
+  for (const project of allProjects) {
+    const config = configReader.readConfig(project)
     if (config.structure.sharedModules.includes(name)) {
       dependents.push(config.name)
     }
+    const discovery = configReader.readDiscovery(project)
+    if (discovery.entries.some((e) => e.targetProject === name)) {
+      dependents.push(config.name)
+    }
   }
+
   return dependents
 }
