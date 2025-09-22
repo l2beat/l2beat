cc8f5c670865b920c9476c96f1bc2374454c4bff
Maciej Zygmunt <maciekzygmunt@interia.pl>
2025-09-22 09:31:26 +0200

    Fix donate animations & colors (#9352)
    Co-authored-by: torztomasz <tomasz.torz@l2beat.com>

--- packages/frontend/src/components/CssVariables.tsx
+++ packages/frontend/src/components/CssVariables.tsx
@@ -1,31 +1,53 @@
+import { useId } from 'react'
+
 const THEMES = { light: '', dark: '.dark' } as const
 
 export function CssVariables({
   variables,
+  children,
 }: {
   variables: Record<string, string | { light: string; dark?: string }>
+  children?: React.ReactNode
 }) {
-  return (
+  const uniqueId = useId()
+  const isLocalScope = !!children
+
+  const styleElement = (
     <style
       dangerouslySetInnerHTML={{
         __html: Object.entries(THEMES)
-          .map(
-            ([theme, prefix]) => `
-html${prefix} {
-${Object.entries(variables)
-  .map(([key, itemConfig]) => {
-    const color =
-      typeof itemConfig === 'string' && theme === 'light'
-        ? itemConfig
-        : itemConfig[theme as keyof typeof itemConfig]
-    return color ? `  --${key}: ${color};` : null
-  })
-  .join('\n')}
-}
-`,
-          )
+          .map(([theme, prefix]) => {
+            const selector = isLocalScope
+              ? `[data-css-vars="${uniqueId}"]${prefix}`
+              : `html${prefix}`
+
+            return `
+  ${selector} {
+  ${Object.entries(variables)
+    .map(([key, itemConfig]) => {
+      const color =
+        typeof itemConfig === 'string' && theme === 'light'
+          ? itemConfig
+          : itemConfig[theme as keyof typeof itemConfig]
+      return color ? `  --${key}: ${color};` : null
+    })
+    .join('\n')}
+  }
+  `
+          })
           .join('\n'),
       }}
     />
+  )
+
+  if (!isLocalScope) {
+    return styleElement
+  }
+
+  return (
+    <div data-css-vars={uniqueId}>
+      {styleElement}
+      {children}
+    </div>
   )
 }
--- packages/frontend/src/pages/publications/monthly-updates/components/ProjectUpdateSection.tsx
+++ packages/frontend/src/pages/publications/monthly-updates/components/ProjectUpdateSection.tsx
@@ -16,19 +16,22 @@ export function ProjectUpdateSection({
 }: Props) {
   return (
     <div id={id} className="mb-8 scroll-mt-8 md:mb-16">
-      {colors && (
-        <CssVariables
-          variables={{
-            [`${id}-primary`]: colors.primary,
-            [`${id}-secondary`]: colors.secondary,
-          }}
-        />
-      )}
       <img
         src={bannerImg}
         className="mb-4 min-h-[70px] w-full rounded-lg object-cover md:mb-8"
       />
-      {children}
+      {colors ? (
+        <CssVariables
+          variables={{
+            'project-primary': colors.primary,
+            'project-secondary': colors.secondary,
+          }}
+        >
+          {children}
+        </CssVariables>
+      ) : (
+        children
+      )}
     </div>
   )
 }
