6aa0c24caa96faede082769dd518ba2aa97d3db4
Michał Podsiadły <michal.podsiadly@l2beat.com>
2025-09-23 14:21:43 +0200

    review fixes

--- packages/l2b/src/implementations/discovery-ui/configs/router.ts
+++ packages/l2b/src/implementations/discovery-ui/configs/router.ts
@@ -1,12 +1,7 @@
-import type {
-  ConfigReader,
-  ConfigWriter,
-  DiscoveryPaths,
-} from '@l2beat/discovery'
-import { ChainSpecificAddress, formatJson } from '@l2beat/shared-pure'
+import type { ConfigReader, ConfigWriter } from '@l2beat/discovery'
+import { ChainSpecificAddress } from '@l2beat/shared-pure'
 import { v as z } from '@l2beat/validate'
 import type { Express } from 'express'
-import path from 'path'
 import { projectParamsSchema } from '../main'
 
 const updateConfigFileSchema = z.object({
@@ -29,7 +24,6 @@ export function attachConfigRouter(
   app: Express,
   configReader: ConfigReader,
   configWriter: ConfigWriter,
-  paths: DiscoveryPaths,
 ) {
   app.put('/api/config-files/:project', (req, res) => {
     const query = projectParamsSchema.safeParse(req.params)
@@ -45,7 +39,7 @@ export function attachConfigRouter(
       return
     }
 
-    configWriter.writeConfigFile(query.data.project, data.data.content)
+    configWriter.updateRawConfigFile(query.data.project, data.data.content)
 
     res.json({ success: true })
   })
@@ -58,102 +52,29 @@ export function attachConfigRouter(
       return
     }
 
-    if (configExists(configReader, body.data.project) && !body.data.overwrite) {
+    if (
+      configReader.projectConfigExists(body.data.project) &&
+      !body.data.overwrite
+    ) {
       res
         .status(400)
         .json({ success: false, error: 'Config file already exists' })
       return
     }
 
-    const { getProjectPath, createEmptyConfig } =
-      body.data.type === 'project' ? ProjectConfigTemplate : TokenConfigTemplate
-
-    const projectPath = getProjectPath(paths, body.data.project)
-
-    const contents = createEmptyConfig({
+    const templateValues = {
       name: body.data.project,
       initialAddresses: body.data.initialAddresses.map(ChainSpecificAddress),
       maxDepth: body.data.maxDepth,
       maxAddresses: body.data.maxAddresses,
-    })
+    }
 
-    configWriter.createConfigFile(projectPath, formatJson(contents))
+    if (body.data.type === 'project') {
+      configWriter.createProjectConfigFile(body.data.project, templateValues)
+    } else {
+      configWriter.createTokenConfigFile(body.data.project, templateValues)
+    }
 
     res.json({ success: true })
   })
-}
-
-function configExists(configReader: ConfigReader, project: string) {
-  try {
-    configReader.getProjectPath(project)
-    return true
-  } catch {
-    return false
-  }
-}
-
-type IncomingConfigSkeleton = {
-  name: string
-  initialAddresses: ChainSpecificAddress[]
-  maxDepth?: number
-  maxAddresses?: number
-}
-
-type ConfigTemplate = {
-  getProjectPath(paths: DiscoveryPaths, name: string): string
-  createEmptyConfig(
-    incomingValues: IncomingConfigSkeleton,
-  ): Record<string, unknown>
-}
-
-const ProjectConfigTemplate: ConfigTemplate = {
-  getProjectPath: (paths, name) => path.join(paths.discovery, name),
-  createEmptyConfig: (incomingValues) => {
-    const { name, initialAddresses, maxAddresses, maxDepth } = incomingValues
-
-    const config = {
-      $schema: '../../../../discovery/schemas/config.v2.schema.json',
-      name,
-      import: ['../globalConfig.jsonc'],
-      ...(maxDepth ? { maxDepth } : {}),
-      ...(maxAddresses ? { maxAddresses } : {}),
-      initialAddresses,
-    }
-
-    return config
-  },
-}
-
-const TokenConfigTemplate: ConfigTemplate = {
-  getProjectPath: (paths, name) => path.join(paths.discovery, '(tokens)', name),
-  createEmptyConfig: (incomingValues) => {
-    const { name, initialAddresses, maxDepth, maxAddresses } = incomingValues
-
-    const overrides: Record<string, unknown> = {}
-
-    for (const address of initialAddresses) {
-      overrides[address.toString()] = {
-        fields: {
-          $tokenData: {
-            handler: {
-              type: 'ERC20Data',
-            },
-          },
-        },
-      }
-    }
-
-    const projectConfig = ProjectConfigTemplate.createEmptyConfig({
-      name,
-      initialAddresses,
-      maxDepth,
-      maxAddresses,
-    })
-
-    return {
-      ...projectConfig,
-      import: ['../../globalConfig.jsonc'],
-      overrides,
-    }
-  },
 }
