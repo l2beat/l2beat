5ac2b9eee09648d6aa36ddf4693a69b84417892a
Maciej Zygmunt <maciekzygmunt@interia.pl>
2025-09-24 09:59:26 +0300

    add celestia

--- packages/backend/src/providers/Providers.ts
+++ packages/backend/src/providers/Providers.ts
@@ -105,6 +105,7 @@ export class Providers {
     this.daBeatStats = new DaBeatStatsProvider(
       this.clients.beacon,
       this.clients.near,
+      this.clients.celestia,
     )
   }
 
--- packages/shared/src/providers/dabeat/DaBeatStatsProvider.ts
+++ packages/shared/src/providers/dabeat/DaBeatStatsProvider.ts
@@ -1,5 +1,9 @@
 import { assert } from '@l2beat/shared-pure'
-import type { BeaconChainClient, NearClient } from '../../clients'
+import type {
+  BeaconChainClient,
+  CelestiaRpcClient,
+  NearClient,
+} from '../../clients'
 
 export interface DaBeatStats {
   totalStake: bigint
@@ -11,6 +15,7 @@ export class DaBeatStatsProvider {
   constructor(
     private readonly beaconChainClient: BeaconChainClient | undefined,
     private readonly nearClient: NearClient | undefined,
+    private readonly celestiaClient: CelestiaRpcClient | undefined,
   ) {}
 
   async getStats(projectId: string): Promise<DaBeatStats | undefined> {
@@ -19,6 +24,8 @@ export class DaBeatStatsProvider {
         return await this.getEthereumStats()
       case 'near-da':
         return await this.getNearStats()
+      case 'celestia':
+        return await this.getCelestiaStats()
       default:
         throw new Error(`Stats provider not implemented for: ${projectId}`)
     }
@@ -27,7 +34,7 @@ export class DaBeatStatsProvider {
   async getEthereumStats(): Promise<DaBeatStats | undefined> {
     assert(this.beaconChainClient, 'Beacon chain client not found')
 
-    const stats = await this.beaconChainClient.getValidatorsStake({
+    const stats = await this.beaconChainClient.getValidatorsInfo({
       stateId: 'head',
       status: ['active'],
     })
@@ -42,6 +49,30 @@ export class DaBeatStatsProvider {
     assert(this.nearClient, 'Near client not found')
 
     const { totalStake } = await this.nearClient.getValidatorsInfo()
+
+    return {
+      totalStake,
+      thresholdStake: (totalStake * 200n) / 300n,
+      numberOfValidators: null,
+    }
+  }
+
+  async getCelestiaStats(): Promise<DaBeatStats | undefined> {
+    assert(this.celestiaClient, 'Celestia client not found')
+
+    const perPage = 100
+    let pageCount = 1
+    let totalStake = 0n
+    for (let page = 1; page <= pageCount; page++) {
+      const res = await this.celestiaClient.getValidatorsInfo({ page, perPage })
+      pageCount = Math.ceil(res.total / perPage)
+      totalStake += res.validators.reduce((acc, v) => {
+        // We store the stake in the smallest denomination
+        // but API returns it in the full TIA
+        const units = BigInt(v.voting_power) * 10n ** 6n
+        return acc + BigInt(units)
+      }, 0n)
+    }
 
     return {
       totalStake,
