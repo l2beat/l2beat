506cf4db73f4a3f49319300c9ced62663719e1a4
Antoni Pawlak <antoni.pawlak@pm.me>
2025-09-24 21:01:55 +0200

    Upgrade Bridges Dashboard (#9400)

---
+++ packages/backend/src/modules/bridges/dashboard/EventsPage.tsx
@@ -0,0 +1,83 @@
+import type { BridgeEventRecord } from '@l2beat/database'
+import React from 'react'
+import { renderToStaticMarkup } from 'react-dom/server'
+import { DataTablePage } from './DataTablePage'
+
+function EventsTable(props: {
+  events: BridgeEventRecord[]
+  getExplorerUrl: (chain: string) => string | undefined
+}) {
+  return (
+    <table id="myTable" className="display">
+      <thead>
+        <tr>
+          <th>Timestamp</th>
+          <th>Chain</th>
+          <th>Tx Hash</th>
+          <th>Args</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.events.map((e) => {
+          const explorerUrl = props.getExplorerUrl(e.chain)
+
+          return (
+            <tr>
+              <td data-order={e.timestamp}>
+                {new Date(e.timestamp * 1000).toLocaleString()}
+              </td>
+              <td>{e.chain}</td>
+              <td>
+                {explorerUrl ? (
+                  <a target="_blank" href={`${explorerUrl}/tx/${e.txHash}`}>
+                    {e.txHash}
+                  </a>
+                ) : (
+                  e.txHash
+                )}
+              </td>
+              <td>{JSON.stringify(e.args)}</td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
+
+function EventsPageLayout(props: {
+  events: BridgeEventRecord[]
+  getExplorerUrl: (chain: string) => string | undefined
+}) {
+  const eventsTable = <EventsTable {...props} />
+  const title = `Bridge Events: ${props.events[0]?.type ?? ''}`
+
+  const dataTableOptions = {
+    pageLength: 25,
+    order: [[0, 'desc']], // Sort by timestamp descending
+    columnDefs: [
+      {
+        targets: 0, // Timestamp column
+        type: 'num', // Ensure numeric sorting
+      },
+    ],
+  }
+
+  return (
+    <DataTablePage
+      title={title}
+      table={eventsTable}
+      tableId="myTable"
+      dataTableOptions={dataTableOptions}
+    />
+  )
+}
+
+export function renderEventsPage(props: {
+  events: BridgeEventRecord[]
+  getExplorerUrl: (chain: string) => string | undefined
+}) {
+  return (
+    '<!DOCTYPE html>' + renderToStaticMarkup(<EventsPageLayout {...props} />)
+  )
+}
---
+++ packages/backend/src/modules/bridges/dashboard/MessagesPage.tsx
@@ -0,0 +1,103 @@
+import type { BridgeMessageRecord } from '@l2beat/database'
+import { formatSeconds } from '@l2beat/shared-pure'
+import React from 'react'
+import { renderToStaticMarkup } from 'react-dom/server'
+import { DataTablePage } from './DataTablePage'
+
+function MessagesTable(props: {
+  messages: BridgeMessageRecord[]
+  getExplorerUrl: (chain: string) => string | undefined
+}) {
+  return (
+    <table id="myTable" className="display">
+      <thead>
+        <tr>
+          <th>Timestamp</th>
+          <th>Duration</th>
+          <th>srcChain</th>
+          <th>srcTx</th>
+          <th>dstChain</th>
+          <th>dstTx</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.messages.map((e) => {
+          const srcExplorerUrl = e.srcChain && props.getExplorerUrl(e.srcChain)
+          const dstExplorerUrl = e.dstChain && props.getExplorerUrl(e.dstChain)
+
+          return (
+            <tr>
+              <td data-order={e.timestamp}>
+                {new Date(e.timestamp * 1000).toLocaleString()}
+              </td>
+              <td>{e.duration && formatSeconds(e.duration)}</td>
+              <td>{e.srcChain}</td>
+              <td>
+                {srcExplorerUrl ? (
+                  <a
+                    target="_blank"
+                    href={`${srcExplorerUrl}/tx/${e.srcTxHash}`}
+                  >
+                    {e.srcTxHash}
+                  </a>
+                ) : (
+                  e.srcTxHash
+                )}
+              </td>
+              <td>{e.dstChain}</td>
+              <td>
+                {dstExplorerUrl ? (
+                  <a
+                    target="_blank"
+                    href={`${dstExplorerUrl}/tx/${e.dstTxHash}`}
+                  >
+                    {e.dstTxHash}
+                  </a>
+                ) : (
+                  e.dstTxHash
+                )}
+              </td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
+
+function MessagesPageLayout(props: {
+  messages: BridgeMessageRecord[]
+  getExplorerUrl: (chain: string) => string | undefined
+}) {
+  const eventsTable = <MessagesTable {...props} />
+  const title = `Bridge Events: ${props.messages[0]?.type ?? ''}`
+
+  const dataTableOptions = {
+    pageLength: 25,
+    order: [[0, 'desc']], // Sort by timestamp descending
+    columnDefs: [
+      {
+        targets: 0, // Timestamp column
+        type: 'num', // Ensure numeric sorting
+      },
+    ],
+  }
+
+  return (
+    <DataTablePage
+      title={title}
+      table={eventsTable}
+      tableId="myTable"
+      dataTableOptions={dataTableOptions}
+    />
+  )
+}
+
+export function renderMessagesPage(props: {
+  messages: BridgeMessageRecord[]
+  getExplorerUrl: (chain: string) => string | undefined
+}) {
+  return (
+    '<!DOCTYPE html>' + renderToStaticMarkup(<MessagesPageLayout {...props} />)
+  )
+}
