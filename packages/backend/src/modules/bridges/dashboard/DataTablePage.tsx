3a451e44b396f977d2e683ea2a62a0d06ce36e20
antooni <antoni.pawlak@protonmail.com>
2025-09-24 16:50:13 +0200

    buttons

--- packages/backend/src/modules/bridges/dashboard/DataTablePage.tsx
+++ packages/backend/src/modules/bridges/dashboard/DataTablePage.tsx
@@ -13,6 +13,12 @@ export function DataTablePage(props: DataTablePageProps) {
   const defaultOptions = {
     pageLength: 25,
     order: [[0, 'desc']], // Default sort by first column
+    dom: 'Bfrtip', // B = Buttons, f = filter, r = processing, t = table, i = info, p = pagination
+    buttons: [
+      'colvis', // Column visibility button
+      'pageLength',
+      'csv',
+    ],
     ...dataTableOptions,
   }
 
@@ -22,12 +28,25 @@ export function DataTablePage(props: DataTablePageProps) {
         <title>{title}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        {/* jQuery (DataTable dependency)*/}
         <script src="https://code.jquery.com/jquery-3.7.1.min.js" />
+
+        {/* DataTables Core */}
         <link
           rel="stylesheet"
           href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.min.css"
         />
         <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js" />
+
+        {/* DataTables Buttons Extension */}
+        <link
+          rel="stylesheet"
+          href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.dataTables.min.css"
+        />
+        <script src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js" />
+        <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.colVis.min.js" />
+        <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js" />
 
         <style>{`
           body {
