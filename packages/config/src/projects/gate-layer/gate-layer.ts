63fadb30f2b4aa84eb19edd64c7c91c87a88bedf
CodePulse <231531436+codepulse21@users.noreply.github.com>
2025-09-27 16:51:12 +0000

    Add GateLayer config

---
+++ packages/config/src/projects/gate-layer/gate-layer.ts
@@ -0,0 +1,29 @@
+import { ProjectId, UnixTime } from '@l2beat/shared-pure'
+import type { ScalingProject } from '../../internalTypes'
+import { upcomingL2 } from '../../templates/upcoming'
+
+export const gateLayer: ScalingProject = upcomingL2({
+  id: 'gate-layer',
+  capability: 'universal',
+  addedAt: UnixTime(1754639625),
+  display: {
+    name: 'Gate Layer',
+    slug: 'gate-layer',
+    description:
+      'Gate Layer is an OP Stack L2, built to deliver exchange-grade throughput and ultra-low fees for EVM-compatible Web3 apps',
+    purposes: ['Universal'],
+    stacks: ['OP Stack'],
+    links: {
+      websites: ['https://www.gate.com/'],
+      documentation: ['https://www.gatechain.io/docs/GateLayer/introduction'],
+      explorers: ['https://gatescan.org/gatelayer'],
+      socialMedia: ['https://x.com/gatechain_io'],
+    },
+  },
+  ecosystemInfo: {
+    id: ProjectId('superchain'),
+  },
+  proofSystem: {
+    type: 'Optimistic',
+  },
+})
