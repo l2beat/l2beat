f3537faf2f6acb084a32db5fcf1f662a5c674c36
Radina Talanova <nt.radina@gmail.com>
2025-09-30 11:38:15 +0300

    Project updates (#9467)

---
+++ packages/config/src/projects/adi/adi.ts
@@ -0,0 +1,32 @@
+import { ProjectId, UnixTime } from '@l2beat/shared-pure'
+import type { ScalingProject } from '../../internalTypes'
+import { upcomingL2 } from '../../templates/upcoming'
+
+export const adi: ScalingProject = upcomingL2({
+  id: 'adi',
+  capability: 'universal',
+  addedAt: UnixTime(1759216045), // 2025-09-30
+  display: {
+    name: 'ADI Chain',
+    slug: 'adi',
+    description:
+      'ADI Chain is a zk rollup built for scale and policy alignment.',
+    purposes: ['Universal'],
+    stacks: ['ZK Stack'],
+    links: {
+      websites: ['https://adi.foundation/'],
+      explorers: ['https://explorer.testnet.adifoundation.ai/'],
+      repositories: ['https://github.com/orgs/ADI-Foundation-Labs/'],
+      socialMedia: [
+        'https://x.com/ADIChain_',
+        'https://discord.com/invite/adi-foundation',
+      ],
+    },
+  },
+  ecosystemInfo: {
+    id: ProjectId('the-elastic-network'),
+  },
+  proofSystem: {
+    type: 'Validity',
+  },
+})
