9bf0af35229663a26441c5d4d701821bda7fd1aa
torztomasz <tomasz.torz@l2beat.com>
2025-10-01 15:07:00 +0200

    deps

--- packages/token-ui/src/components/core/Separator.tsx
+++ packages/token-ui/src/components/core/Separator.tsx
@@ -1,4 +1,3 @@
-import * as SeparatorPrimitive from '@radix-ui/react-separator'
 import * as React from 'react'
 
 import { cn } from '~/utils/cn'
@@ -6,14 +5,14 @@ import { cn } from '~/utils/cn'
 function Separator({
   className,
   orientation = 'horizontal',
-  decorative = true,
   ...props
-}: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
+}: React.ComponentProps<'div'> & {
+  orientation?: 'horizontal' | 'vertical'
+}) {
   return (
-    <SeparatorPrimitive.Root
+    <div
       data-slot="separator"
-      decorative={decorative}
-      orientation={orientation}
+      data-orientation={orientation}
       className={cn(
         'shrink-0 bg-border data-[orientation=horizontal]:h-px data-[orientation=vertical]:h-full data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-px',
         className,
