9595f836da6b1c2247eab0e7e0c8038dd746c730
torztomasz <tomasz.torz@l2beat.com>
2025-10-01 12:27:54 +0200

    scaffold

---
+++ packages/token-ui/src/App.tsx
@@ -0,0 +1,9 @@
+import { AppLayout } from './layouts/AppLayout'
+
+export function App() {
+  return (
+    <AppLayout>
+      <h1>Token UI</h1>
+    </AppLayout>
+  )
+}
---
+++ packages/token-ui/src/components/AppSidebar.tsx
@@ -0,0 +1,47 @@
+import { Home } from 'lucide-react'
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '~/components/core/Sidebar'
+
+// Menu items.
+const items = [
+  {
+    title: 'Home',
+    url: '#',
+    icon: Home,
+  },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar variant="floating" collapsible="icon">
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Token UI</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
