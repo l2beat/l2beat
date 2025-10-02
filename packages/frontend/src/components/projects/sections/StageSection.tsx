c1ea5904192c6be2fb2b0a44bdd3ff2faf72bcdd
Maciej Zygmunt <maciekzygmunt@interia.pl>
2025-10-02 16:11:25 +0300

    rewrite accordion to collapsible

--- packages/frontend/src/components/projects/sections/StageSection.tsx
+++ packages/frontend/src/components/projects/sections/StageSection.tsx
@@ -6,11 +6,10 @@ import type {
 } from '@l2beat/config'
 import { UnderReviewBadge } from '~/components/badge/UnderReviewBadge'
 import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from '~/components/core/Accordion'
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from '~/components/core/Collapsible'
 import { HorizontalSeparator } from '~/components/core/HorizontalSeparator'
 import {
   Tooltip,
@@ -26,6 +25,7 @@ import { MissingIcon } from '~/icons/Missing'
 import { RoundedWarningIcon } from '~/icons/RoundedWarning'
 import { SatisfiedIcon } from '~/icons/Satisfied'
 import { UnderReviewIcon } from '~/icons/UnderReview'
+import { ChevronIcon } from '../../../icons/Chevron'
 import { StageBadge } from '../../badge/StageBadge'
 import { Callout } from '../../Callout'
 import { Markdown } from '../../markdown/Markdown'
@@ -157,7 +157,7 @@ export function StageSection({
         />
       )}
       <HorizontalSeparator className="my-4" />
-      <Accordion type="multiple">
+      <div className="space-y-2">
         {stageConfig.summary.map((stage) => {
           const requirementsForLabel =
             stage.principle && featureFlags.stageOneRequirementsChanged()
@@ -184,127 +184,131 @@ export function StageSection({
           )
 
           return (
-            <AccordionItem key={stage.stage} value={stage.stage}>
-              <AccordionTrigger className="py-4 font-normal text-lg md:px-6">
-                <div className="flex select-none items-center justify-start gap-3 max-md:text-base">
+            <Collapsible
+              key={stage.stage}
+              className="group rounded-lg border border-divider"
+            >
+              <CollapsibleTrigger className="flex w-full items-center justify-between px-4 py-3 font-bold md:px-6">
+                <div className="flex select-none items-center justify-start gap-4 max-md:text-base">
                   <StageBadge stage={stage.stage} isAppchain={false} />
                   {missingForLabel.length === 0 ? (
                     <div className="flex flex-col gap-3 md:flex-row">
                       <div className="flex items-center gap-2 font-bold">
                         <SatisfiedIcon className="size-4 shrink-0 fill-positive" />
-                        <span>
+                        <span className="text-label-value-16 md:text-label-value-18">
                           {reqTextSatisfied(satisfiedForLabel.length)}
                         </span>
                       </div>
                       {underReviewForLabel.length > 0 && (
                         <div className="flex items-center gap-2">
                           <UnderReviewIcon className="size-4 shrink-0" />
-                          <span>{underReviewForLabel.length} under review</span>
+                          <span className="text-label-value-16 md:text-label-value-18">
+                            {underReviewForLabel.length} under review
+                          </span>
                         </div>
                       )}
                     </div>
                   ) : (
                     <div className="flex items-center gap-2 font-bold">
                       <MissingIcon className="size-4 shrink-0 fill-negative" />
-                      <span>{reqTextMissing(missingForLabel.length)}</span>
+                      <span className="text-label-value-16 md:text-label-value-18">
+                        {reqTextMissing(missingForLabel.length)}
+                      </span>
                     </div>
                   )}
                 </div>
-              </AccordionTrigger>
-              <AccordionContent className="space-y-1 text-lg md:mx-6 md:space-y-2">
-                {stage.principle && (
-                  <>
-                    <div className="flex items-center gap-1">
-                      <span className="text-[13px] uppercase leading-normal">
-                        {featureFlags.stageOneRequirementsChanged()
-                          ? 'Principle'
-                          : 'Upcoming principle'}
-                      </span>
-                      <Tooltip>
-                        <TooltipTrigger className="-mt-0.5">
-                          <InfoIcon className="size-3" />
-                        </TooltipTrigger>
-                        <TooltipContent>
-                          A Principle is the main requirement you need to
-                          fulfill in order to receive this stage. The guidelines
-                          below serve to help the project meet this goal.
-                        </TooltipContent>
-                      </Tooltip>
-                    </div>
-                    <div className="rounded-lg border border-brand px-[15px] py-3">
-                      <div className="flex">
-                        {stage.principle.satisfied === 'UnderReview' ? (
-                          <UnderReviewIcon className="mt-0.5 size-4 shrink-0" />
-                        ) : stage.principle.satisfied === true ? (
-                          <SatisfiedIcon className="mt-0.5 size-4 shrink-0 fill-positive" />
-                        ) : (
-                          <MissingIcon className="mt-0.5 size-4 shrink-0 fill-negative" />
-                        )}
-                        <Markdown className="ml-2 font-medium leading-tight max-md:text-base">
-                          {stage.principle.description}
-                        </Markdown>
+                <ChevronIcon className="group-data-[state=open]:-rotate-180 size-4 fill-secondary transition-transform duration-300" />
+              </CollapsibleTrigger>
+              <CollapsibleContent>
+                <div className="space-y-3 px-4 pt-1 pb-6 text-lg md:px-6">
+                  {stage.principle && (
+                    <div className="space-y-2">
+                      <div className="flex items-center gap-1">
+                        <span className="font-bold text-brand text-label-value-14">
+                          {featureFlags.stageOneRequirementsChanged()
+                            ? 'Principle'
+                            : 'Upcoming Principle'}
+                        </span>
+                        <Tooltip>
+                          <TooltipTrigger>
+                            <InfoIcon className="size-3 fill-brand" />
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            A Principle is the main requirement you need to
+                            fulfill in order to receive this stage. The
+                            guidelines below serve to help the project meet this
+                            goal.
+                          </TooltipContent>
+                        </Tooltip>
                       </div>
-                      {stageConfig.stage1PrincipleDescription && (
-                        <p className="mt-2 pl-6 leading-[125%] max-md:text-base">
-                          {stageConfig.stage1PrincipleDescription}
-                        </p>
-                      )}
+                      <div className="rounded-lg border border-brand px-[15px] py-2.5">
+                        <div className="flex">
+                          {stage.principle.satisfied === 'UnderReview' ? (
+                            <UnderReviewIcon className="mt-0.5 size-4 shrink-0" />
+                          ) : stage.principle.satisfied === true ? (
+                            <SatisfiedIcon className="mt-0.5 size-4 shrink-0 fill-positive" />
+                          ) : (
+                            <MissingIcon className="mt-0.5 size-4 shrink-0 fill-negative" />
+                          )}
+                          <Markdown className="ml-2 font-medium text-paragraph-14 md:text-paragraph-16">
+                            {stage.principle.description}
+                          </Markdown>
+                        </div>
+                        {stageConfig.stage1PrincipleDescription && (
+                          <p className="mt-2 pl-6 font-medium text-paragraph-14 text-secondary">
+                            {stageConfig.stage1PrincipleDescription}
+                          </p>
+                        )}
+                      </div>
                     </div>
-                  </>
-                )}
-
-                {featureFlags.stageOneRequirementsChanged() &&
-                  stage.principle && (
-                    <p className="ml-8 text-[13px] uppercase">Guidelines</p>
                   )}
-                <ul className="space-y-1 px-4 md:space-y-2">
-                  {satisfiedRequirements.map((req, i) => (
-                    <li key={i} className="flex">
-                      <SatisfiedIcon className="relative top-0.5 size-4 shrink-0 fill-positive" />
-                      <Markdown
-                        className="ml-2 leading-none max-md:text-base"
-                        inline
-                      >
-                        {req.description}
-                      </Markdown>
-                    </li>
-                  ))}
-                  {underReviewRequirements.map((req, i) => (
-                    <li key={i} className="flex">
-                      <UnderReviewIcon className="relative top-0.5 size-4 shrink-0" />
-                      <Markdown
-                        className="ml-2 leading-none max-md:text-base"
-                        inline
-                      >
-                        {req.description}
-                      </Markdown>
-                    </li>
-                  ))}
-                  {missingRequirements.map((req, i) => (
-                    <li key={i} className="flex">
-                      <MissingIcon className="relative top-0.5 size-4 shrink-0 fill-negative" />
-                      <Markdown
-                        className="ml-2 leading-none max-md:text-base"
-                        inline
-                      >
-                        {req.description}
-                      </Markdown>
-                    </li>
-                  ))}
-                </ul>
-              </AccordionContent>
-            </AccordionItem>
+
+                  {featureFlags.stageOneRequirementsChanged() &&
+                    stage.principle && (
+                      <p className="font-bold text-label-value-14 text-secondary">
+                        Guidelines
+                      </p>
+                    )}
+                  <ul className="space-y-1 md:space-y-2">
+                    {satisfiedRequirements.map((req, i) => (
+                      <li key={i} className="flex">
+                        <SatisfiedIcon className="relative top-0.5 size-4 shrink-0 fill-positive" />
+                        <Markdown className="ml-2 font-medium text-paragraph-14 md:text-paragraph-16">
+                          {req.description}
+                        </Markdown>
+                      </li>
+                    ))}
+                    {underReviewRequirements.map((req, i) => (
+                      <li key={i} className="flex">
+                        <UnderReviewIcon className="relative top-0.5 size-4 shrink-0" />
+                        <Markdown className="ml-2 font-medium text-paragraph-14 md:text-paragraph-16">
+                          {req.description}
+                        </Markdown>
+                      </li>
+                    ))}
+                    {missingRequirements.map((req, i) => (
+                      <li key={i} className="flex">
+                        <MissingIcon className="relative top-0.5 size-4 shrink-0 fill-negative" />
+                        <Markdown className="ml-2 font-medium text-paragraph-14 md:text-paragraph-16">
+                          {req.description}
+                        </Markdown>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </CollapsibleContent>
+            </Collapsible>
           )
         })}
-      </Accordion>
-      <CustomLink href="/stages" className="mt-3 block text-sm">
+      </div>
+      <CustomLink href="/stages" className="mt-3 block text-label-value-14">
         Learn more about Rollup stages
       </CustomLink>
       <Callout
         color="blue"
         body="Please keep in mind that these stages do not reflect rollup security, this is an opinionated assessment of rollup maturity based on subjective criteria, created with a goal of incentivizing projects to push toward better decentralization. Each team may have taken different paths to achieve this goal."
         icon={<InfoIcon className="size-4 md:size-5" variant="blue" />}
-        className="mt-6 p-4 font-medium text-paragraph-15 md:text-paragraph-16"
+        className="mt-6 p-4 font-normal text-paragraph-15 md:text-paragraph-16"
       />
     </ProjectSection>
   )
