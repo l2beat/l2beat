7ce3c7a5da4eb0044e6c3556f40b60f22971cf52
torztomasz <tomasz.torz@l2beat.com>
2025-10-09 22:05:05 +0200

    fix

--- packages/token-ui/src/App.tsx
+++ packages/token-ui/src/App.tsx
@@ -1,17 +1,28 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { SidebarProvider } from './components/core/Sidebar'
 import { Toaster } from './components/core/Sonner'
+import { MainPage } from './pages/MainPage'
+import { NotFoundPage } from './pages/NotFoundPage'
+import { SearchPage } from './pages/search/SearchPage'
+import { AddTokensPage } from './pages/tokens/add-tokens/AddTokensPage'
+import { TokensPage } from './pages/tokens/TokensPage'
 import { TRPCReactProvider } from './react-query/trpc'
-import { routes } from './routes'
-
-const router = createBrowserRouter(routes)
 
 export function App() {
   return (
     <TRPCReactProvider>
       <SidebarProvider>
-        <Toaster />
-        <RouterProvider router={router} />
+        <BrowserRouter>
+          <Toaster />
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/search/:search" element={<SearchPage />} />
+            <Route path="/tokens/new" element={<AddTokensPage />} />
+            <Route path="/tokens/:id" element={<TokensPage />} />
+            <Route path="/not-found" element={<NotFoundPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </BrowserRouter>
       </SidebarProvider>
     </TRPCReactProvider>
   )
