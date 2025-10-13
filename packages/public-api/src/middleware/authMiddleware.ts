7b616af8e4caefa43c997fd85cdad1ad2d954bb8
torztomasz <tomasz.torz@l2beat.com>
2025-10-13 16:06:41 +0200

    simplify

--- packages/public-api/src/middleware/authMiddleware.ts
+++ packages/public-api/src/middleware/authMiddleware.ts
@@ -1,4 +1,3 @@
-import { assert } from '@l2beat/shared-pure'
 import type { NextFunction, Request, Response } from 'express'
 import type { AuthConfig } from '../config/Config'
 import { requestContext } from '../context/context'
@@ -6,17 +5,17 @@ import { requestContext } from '../context/context'
 export function authMiddleware(config: AuthConfig) {
   return (req: Request, res: Response, next: NextFunction) => {
     const apiKey = req.query.apiKey
-    if (!apiKey || !Object.values(config.apiKeys).includes(apiKey.toString())) {
+
+    const user = Object.entries(config.apiKeys).find(
+      ([_, value]) => value === apiKey?.toString(),
+    )?.[0]
+
+    if (!user) {
       res.status(401).json({
         message: 'Unauthorized. Use apiKey query parameter with valid API key.',
       })
       return
     }
-
-    const user = Object.entries(config.apiKeys).find(
-      ([_, value]) => value === apiKey.toString(),
-    )?.[0]
-    assert(user, 'User not found')
 
     requestContext.run({ user }, next)
   }
