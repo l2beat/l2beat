6ac05bca8fde475b9b2fed4ee586dd2c0368d64a
torztomasz <tomasz.torz@l2beat.com>
2025-10-15 13:45:13 +0200

    testtt

--- packages/frontend/scripts/hot-cache/index.ts
+++ packages/frontend/scripts/hot-cache/index.ts
@@ -1,6 +1,9 @@
+import { getEnv } from '@l2beat/backend-tools'
 import { hotCacheFns } from './hotCacheFns'
 
 async function main() {
+  const env = getEnv()
+  console.log(env)
   await Promise.all(
     Object.entries(hotCacheFns).map(async ([key, fn]) => {
       console.time(key)
