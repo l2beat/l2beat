8bbc269505440dfbc0095f4f247460454d981f6d
sashaodessa <140454972+sashaodessa@users.noreply.github.com>
2025-10-20 13:12:32 +0200

    Update generateId.ts

--- packages/shared/src/tools/generateId.ts
+++ packages/shared/src/tools/generateId.ts
@@ -1,5 +1,5 @@
-import { v4 as uuidv4 } from 'uuid'
-import { randomInt } from 'crypto'
+import·{·randomInt·}·from·'crypto'
+import·{·v4·as·uuidv4·}·from·'uuid'
 
 export function generateId() {
   return uuidv4()
