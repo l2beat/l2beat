cdb1bfd9bfc4378654266a49f8ad81339a7fde90
antooni <antoni.pawlak@protonmail.com>
2025-10-20 16:55:26 +0200

    restore compare

--- packages/backend/src/modules/interop/compare/index.ts
+++ packages/backend/src/modules/interop/compare/index.ts
@@ -1,13 +1,5 @@
-import type { Logger } from '@l2beat/backend-tools'
-import type { Database } from '@l2beat/database'
-import type { RpcClient } from '@l2beat/shared'
-import { AcrossConfigPlugin } from '../config/across'
+import { AcrossComparePlugin } from './across'
 
-export function createInteropComparePlugins(
-  chains: { id: number; name: string }[],
-  db: Database,
-  logger: Logger,
-  ethereumRpc: RpcClient,
-) {
-  return [new AcrossConfigPlugin(chains, db, logger, ethereumRpc)]
+export function createInteropConfigPlugins() {
+  return [new AcrossComparePlugin()]
 }
--- packages/backend/src/modules/interop/config/index.ts
+++ packages/backend/src/modules/interop/config/index.ts
@@ -1,3 +1,13 @@
-export function createInteropConfigPlugins() {
-  return [new AcrossComparePlugin()]
+import type { Logger } from '@l2beat/backend-tools'
+import type { Database } from '@l2beat/database'
+import type { RpcClient } from '@l2beat/shared'
+import { AcrossConfigPlugin } from './across'
+
+export function createInteropComparePlugins(
+  chains: { id: number; name: string }[],
+  db: Database,
+  logger: Logger,
+  ethereumRpc: RpcClient,
+) {
+  return [new AcrossConfigPlugin(chains, db, logger, ethereumRpc)]
 }
