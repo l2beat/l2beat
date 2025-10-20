2942a732e6abe12683f886c05d20ec272f0d1807
Michał Podsiadły <michal.podsiadly@l2beat.com>
2025-10-20 18:54:45 +0200

    feat: add initial logic

--- packages/protocolbeat/src/apps/discovery/panel-list/ListPanel.tsx
+++ packages/protocolbeat/src/apps/discovery/panel-list/ListPanel.tsx
@@ -154,14 +154,23 @@ function ListItemContracts(props: {
 function AddressEntry({ entry }: { entry: ApiAddressEntry }) {
   const isSelected = usePanelStore((state) => state.selected === entry.address)
   const select = usePanelStore((state) => state.select)
+  const markUnreachableEntries = usePanelStore(
+    ({ userSettings }) => userSettings.markUnreachableEntries,
+  )
+  const isGrayedOut = markUnreachableEntries && !entry.isReachable
+
   return (
     <li
       className={clsx(
-        'flex min-h-[22px] cursor-pointer select-none items-center gap-1 whitespace-pre pl-4 text-sm',
+        'relative flex min-h-[22px] cursor-pointer select-none items-center gap-1 whitespace-pre pl-4 text-sm',
         isSelected && 'bg-autumn-300 text-black',
         !isSelected && 'bg-coffee-800 hover:bg-aux-brown',
       )}
       onClick={() => select(entry.address)}
+      style={{
+        opacity: isGrayedOut ? 0.2 : 1,
+        filter: isGrayedOut ? 'grayscale(100%)' : 'none',
+      }}
     >
       <div className="mr-[7px] min-h-[22px] border-coffee-600 border-l" />
       <AddressIcon type={entry.type} />
