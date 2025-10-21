675a588fcd48ac652d49950ea4e48b2a6f78165a
Maciej Zygmunt <maciekzygmunt@interia.pl>
2025-10-21 10:56:47 +0300

    add tooltip & change label

--- packages/frontend/src/pages/scaling/components/IncludeRwaRestrictedTokensCheckbox.tsx
+++ packages/frontend/src/pages/scaling/components/IncludeRwaRestrictedTokensCheckbox.tsx
@@ -1,4 +1,10 @@
 import { Checkbox } from '~/components/core/Checkbox'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '~/components/core/tooltip/Tooltip'
+import { InfoIcon } from '~/icons/Info'
 import { useScalingRwaRestrictedTokensContext } from './ScalingRwaRestrictedTokensContext'
 
 export function IncludeRwaRestrictedTokensCheckbox() {
@@ -6,12 +12,24 @@ export function IncludeRwaRestrictedTokensCheckbox() {
     useScalingRwaRestrictedTokensContext()
 
   return (
-    <Checkbox
-      name="includeRwaRestrictedTokens"
-      checked={includeRwaRestrictedTokens}
-      onCheckedChange={(checked) => setIncludeRwaRestrictedTokens(!!checked)}
-    >
-      Include RWA restricted tokens
-    </Checkbox>
+    <Tooltip>
+      <Checkbox
+        name="includeRwaRestrictedTokens"
+        checked={includeRwaRestrictedTokens}
+        onCheckedChange={(checked) => setIncludeRwaRestrictedTokens(!!checked)}
+      >
+        <div className="flex items-center gap-1">
+          <div>Include restricted RWAs tokens</div>
+          <TooltipTrigger asChild>
+            <InfoIcon />
+          </TooltipTrigger>
+        </div>
+      </Checkbox>
+      <TooltipContent>
+        Centralized RWAs with access, transfer, transparency or onchain
+        liquidity restrictions. A more formal framework for RWAs is in the
+        works!
+      </TooltipContent>
+    </Tooltip>
   )
 }
