ddeb75d43b5ff999c9f9421aebf3f3886194fbfa
Adrian Adamiak <adrian@adamiak.net>
2025-10-21 10:49:08 +0000

    Add bloom filter on address

--- packages/eventdb/schema/1.sql
+++ packages/eventdb/schema/1.sql
@@ -16,6 +16,7 @@ PARTITION BY intDiv(block_number, 1000000)          -- or toYYYYMM(block_time)
 ORDER BY (address_hex, topic0_hex, block_number, log_index);
 
 ALTER TABLE logs_eth
+  ADD INDEX bf_addr address_hex TYPE bloom_filter GRANULARITY 4; -- without it queries on HDD take much much longer (0.7s vs 0.15s), even with ORDER BY (address_hex, ...
   ADD INDEX bf_topic1 topic1_hex TYPE bloom_filter GRANULARITY 4,
   ADD INDEX bf_topic2 topic2_hex TYPE bloom_filter GRANULARITY 4,
   ADD INDEX bf_topic3 topic3_hex TYPE bloom_filter GRANULARITY 4;
