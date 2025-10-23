7c820c1b5728d20977b1f16aed94bc93742a91e2
torztomasz <tomasz.torz@l2beat.com>
2025-10-23 09:03:36 +0200

    better url log

--- packages/public-api/src/middleware/loggerMiddleware.ts
+++ packages/public-api/src/middleware/loggerMiddleware.ts
@@ -5,9 +5,11 @@ import { getContext } from '../context/context'
 export function loggerMiddleware(logger: Logger) {
   return (req: Request, res: Response, next: NextFunction) => {
     const context = getContext()
+    const { queryParams, url } = getParamsWithoutApiKey(req.originalUrl)
     logger.info('Processing request', {
       method: req.method,
-      url: req.originalUrl,
+      url,
+      queryParams,
       referer: req.headers.referer ?? 'unknown',
       userAgent: req.headers['user-agent'] ?? 'unknown',
       user: context.user,
@@ -18,9 +20,11 @@ export function loggerMiddleware(logger: Logger) {
       const end = process.hrtime.bigint()
       const durationMs = Number(end - start) / 1_000_000
       const contentLength = res.getHeader('Content-Length')
+      const { queryParams, url } = getParamsWithoutApiKey(req.originalUrl)
       logger.info('Request processed', {
         method: req.method,
-        url: req.originalUrl,
+        url,
+        queryParams,
         status: res.statusCode,
         duration: Math.round(durationMs),
         size: contentLength ? Number(contentLength) : undefined,
@@ -31,5 +35,25 @@ export function loggerMiddleware(logger: Logger) {
     })
 
     next()
+  }
+}
+
+function getParamsWithoutApiKey(url: string) {
+  const [urlWithoutParams, ...queryParamsString] = url.split('?')
+  const params = queryParamsString.join('?').split('&')
+  const result: Record<string, string> = {}
+  for (const param of params) {
+    const [key, value] = param.split('=')
+    if (!key || !value) {
+      continue
+    }
+    if (key === 'apiKey') {
+      continue
+    }
+    result[key] = value
+  }
+  return {
+    url: urlWithoutParams,
+    queryParams: Object.keys(result).length > 0 ? result : undefined,
   }
 }
