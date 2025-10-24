5dc7b1031450244fd15fe68a640997269012dbe3
antooni <antoni.pawlak@protonmail.com>
2025-10-24 14:52:05 +0200

    add solana to layer zero

--- packages/backend/src/modules/interop/plugins/layerzero/layerzero.config.ts
+++ packages/backend/src/modules/interop/plugins/layerzero/layerzero.config.ts
@@ -78,7 +78,16 @@ const DocsResult = v.record(
   }),
 )
 
-const OVERRIDES: { id: number; name: string }[] = [] //TODO: fill
+const OVERRIDES = {
+  v2: [
+    {
+      eid: 30168,
+      chain: 'solana',
+      chainId: -1,
+      endpointV2: EthereumAddress.ZERO,
+    },
+  ],
+}
 
 export class LayerZeroConfigPlugin
   extends TimeLoop
@@ -141,14 +150,12 @@ export class LayerZeroConfigPlugin
     const response = await this.http.fetch(DOCS_URL, { timeout: 10_000 })
     const docs = DocsResult.parse(response)
 
-    const chains = [...this.chains, ...OVERRIDES]
-
     const config: {
       v1: LayerZeroV1Network[]
       v2: LayerZeroV2Network[]
     } = {
       v1: [],
-      v2: [],
+      v2: [...OVERRIDES.v2],
     }
     for (const [_, value] of Object.entries(docs)) {
       if (
@@ -164,7 +171,7 @@ export class LayerZeroConfigPlugin
         continue
       }
 
-      const chain = chains.find(
+      const chain = this.chains.find(
         (c) => c.id === value.chainDetails?.nativeChainId,
       )
 
