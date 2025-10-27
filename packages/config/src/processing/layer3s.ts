5f9657846ce7e808c3da5efbbb1dbfa2523d051c
sebastien <238387108+sebastien-byte@users.noreply.github.com>
2025-10-27 19:05:09 +0800

    Remove Syndr

--- packages/config/src/processing/layer3s.ts
+++ packages/config/src/processing/layer3s.ts
@@ -59,7 +59,6 @@ import { spheron } from '../projects/spheron/spheron'
 import { stack } from '../projects/stack/stack'
 import { superposition } from '../projects/superposition/superposition'
 import { syndicateframe } from '../projects/syndicateframe/syndicateframe'
-import { syndr } from '../projects/syndr/syndr'
 import { t3rn } from '../projects/t3rn/t3rn'
 import { toy } from '../projects/toy/toy'
 import { unite } from '../projects/unite/unite'
@@ -135,7 +134,6 @@ export const layer3s: ScalingProject[] = [
   playblock,
   spheron,
   superposition,
-  syndr,
   t3rn,
   toy,
   winr,
