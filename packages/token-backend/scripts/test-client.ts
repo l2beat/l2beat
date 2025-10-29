b2a2565b98ac2909a7015eea5b9c213a585092e6
antooni <antoni.pawlak@protonmail.com>
2025-10-29 15:23:23 +0100

    fix test

--- packages/token-backend/scripts/test-client.ts
+++ packages/token-backend/scripts/test-client.ts
@@ -16,10 +16,12 @@ async function main() {
     callSource: 'test-script',
   })
   // const result = await client.tokens.getAllAbstractTokens.query()
-  const result = await client.tokens.getByChainAndAddress.query({
-    chain: 'celo',
-    address: '0xcebA9300f2b948710d2653dD7B07f33A8B32118C',
-  })
+  const result = await client.tokens.getByChainAndAddress.query([
+    {
+      chain: 'celo',
+      address: '0xcebA9300f2b948710d2653dD7B07f33A8B32118C',
+    },
+  ])
   console.log(JSON.stringify(result, null, 2))
 }
 
