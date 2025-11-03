31e3bd28a0a860a86adc5ac86fae0ccf74dd204a
Forostovec <ilonaforostovec22@gmail.com>
2025-11-03 15:31:18 +0200

    docs: fix broken link to boundless docs (#9787)

--- packages/config/src/templates/opStack.ts
+++ packages/config/src/templates/opStack.ts
@@ -806,7 +806,7 @@ Proving any of the ${proposalOutputCount} intermediate state commitments in a pr
 A single remaining child in a tournament can be 'resolved' and will be finalized and usable for withdrawals after an execution delay of ${formatSeconds(disputeGameFinalityDelaySeconds)} (time for the Guardian to manually blacklist malicious state roots).`,
             references: [
               {
-                url: 'https://docs.boundless.network/developers/kailua/how',
+                url: 'https://docs.boundless.network/developers/kailua/quick-start',
                 title: 'Disputes - Kailua Docs',
               },
             ],
