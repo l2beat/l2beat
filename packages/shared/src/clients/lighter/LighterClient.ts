9e1f4f3b0b3659e9c88c33723bca155c279fcada
torztomasz <tomasz.torz@l2beat.com>
2025-11-03 08:46:44 +0100

    add user agent

--- packages/shared/src/clients/lighter/LighterClient.ts
+++ packages/shared/src/clients/lighter/LighterClient.ts
@@ -33,6 +33,10 @@ export class LighterClient extends ClientCore {
       url += `?${query}`
     }
     return await this.fetch(url, {
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+      },
       timeout: this.timeoutMs,
     })
   }
