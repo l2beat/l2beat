191f63b14d8fbda2ee8edf4b8e9cf6738861d94c
Lorden <coccus.hapless.1w@icloud.com>
2025-11-05 23:08:05 +0100

    Update ChildIndexer.ts

--- packages/uif/src/indexers/ChildIndexer.ts
+++ packages/uif/src/indexers/ChildIndexer.ts
@@ -1,7 +1,7 @@
 import { Indexer } from '../Indexer'
 
 export abstract class ChildIndexer extends Indexer {
-  override async tick(): Promise<number> {
-    return await Promise.reject(new Error('ChildIndexer cannot tick'))
+  override tick(): Promise<number> {
+    return Promise.reject(new Error('ChildIndexer cannot tick'))
   }
 }
