c32a85959b8f549390056353a0966ebf8f7f6531
torztomasz <tomasz.torz@l2beat.com>
2025-11-06 12:02:48 +0100

    add chains repository

---
+++ packages/database/src/repositories/ChainRepository.ts
@@ -0,0 +1,69 @@
+import type { Insertable } from 'kysely'
+import { BaseRepository } from '../BaseRepository'
+import type { Chain } from '../kysely/generated/types'
+
+type ChainApi = {
+  callsPerMinute?: number
+} & (RpcChainApi | EtherscanChainApi | BlockscoutChainApi | RoutescanChainApi)
+
+type EtherscanChainApi = {
+  type: 'etherscan'
+}
+
+type BlockscoutChainApi = {
+  type: 'blockscout'
+  url: string
+}
+
+type RoutescanChainApi = {
+  type: 'routescan'
+  url: string
+}
+
+type RpcChainApi = {
+  type: 'rpc'
+  url: string
+}
+
+export type ChainRecord = {
+  name: string
+  chainId: number
+  aliases: string[] | null
+  apis: ChainApi[] | null
+}
+
+const toRecord = (record: Chain): ChainRecord => {
+  return {
+    ...record,
+    aliases: record.aliases as string[] | null,
+    apis: record.apis as ChainApi[] | null,
+  }
+}
+
+const toRow = (record: ChainRecord): Insertable<Chain> => {
+  return {
+    ...record,
+    aliases: JSON.stringify(record.aliases),
+    apis: JSON.stringify(record.apis),
+  }
+}
+
+export class ChainRepository extends BaseRepository {
+  async insert(record: ChainRecord): Promise<void> {
+    await this.db.insertInto('Chain').values(toRow(record)).execute()
+  }
+
+  async insertMany(records: ChainRecord[]): Promise<void> {
+    await this.db.insertInto('Chain').values(records.map(toRow)).execute()
+  }
+
+  async getAll(): Promise<ChainRecord[]> {
+    const rows = await this.db.selectFrom('Chain').selectAll().execute()
+    return rows.map(toRecord)
+  }
+
+  async deleteAll(): Promise<number> {
+    const result = await this.db.deleteFrom('Chain').executeTakeFirst()
+    return Number(result.numDeletedRows)
+  }
+}
