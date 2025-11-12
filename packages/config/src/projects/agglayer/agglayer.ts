e224125568252d929ba63e63e9c064b4fbaf64bf
GarmashAlex <garmasholeksii@gmail.com>
2025-11-13 02:06:42 +0300

    fix(agglayer): wrap addedAt with UnixTime

--- packages/config/src/projects/agglayer/agglayer.ts
+++ packages/config/src/projects/agglayer/agglayer.ts
@@ -1,4 +1,4 @@
-import { CoingeckoId, ProjectId } from '@l2beat/shared-pure'
+import { CoingeckoId, ProjectId, UnixTime } from '@l2beat/shared-pure'
 import { BADGES } from '../../common/badges'
 import type { BaseProject } from '../../types'
 
@@ -7,7 +7,7 @@ export const agglayer: BaseProject = {
   slug: 'agglayer',
   name: 'Agglayer',
   shortName: undefined,
-  addedAt: 1743677000,
+  addedAt: UnixTime(1743677000),
   display: {
     description:
       'Agglayer is a cross-chain settlement layer that connects the liquidity and users of any blockchain for fast, low cost interoperability and growth.',
