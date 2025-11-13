828668bd9e24091fb126c3d4b61dd12b442bcb8f
Maciej Zygmunt <maciekzygmunt@interia.pl>
2025-11-13 10:08:36 +0200

    update tvs logic

--- packages/frontend/src/server/features/zk-catalog/utils/getZkCatalogProjectTvs.ts
+++ packages/frontend/src/server/features/zk-catalog/utils/getZkCatalogProjectTvs.ts
@@ -1,8 +1,10 @@
 import type { Project } from '@l2beat/config'
+import { type ProjectId, UnixTime } from '@l2beat/shared-pure'
 import uniq from 'lodash/uniq'
 import { getLogger } from '~/server/utils/logger'
 import { calculatePercentageChange } from '~/utils/calculatePercentageChange'
 import type { SevenDayTvsBreakdown } from '../../scaling/tvs/get7dTvsBreakdown'
+import { getTvsTargetTimestamp } from '../../scaling/tvs/utils/getTvsTargetTimestamp'
 
 export function getZkCatalogProjectTvs(
   project: Project<'zkCatalogInfo'>,
@@ -12,13 +14,65 @@ export function getZkCatalogProjectTvs(
   >[],
   tvs: SevenDayTvsBreakdown,
 ) {
-  const liveTvsProjects =
-    project.zkCatalogInfo.projectsForTvs
-      ?.filter((p) => !p.untilTimestamp)
-      .map((p) => p.projectId) ?? []
+  const target = getTvsTargetTimestamp()
+  const todayTvsProjects = getLiveProjectsAtTimestamp(
+    project.zkCatalogInfo.projectsForTvs,
+    target,
+  )
+  const sevenDaysAgoTvsProjects = getLiveProjectsAtTimestamp(
+    project.zkCatalogInfo.projectsForTvs,
+    target - 7 * UnixTime.DAY,
+  )
 
-  const projectsForTvs = uniq(
-    liveTvsProjects.flatMap((tp) => {
+  const projectsForTodayTvs = getProjectsForTvs(todayTvsProjects, allProjects)
+  const projectsForSevenDaysAgoTvs = getProjectsForTvs(
+    sevenDaysAgoTvsProjects,
+    allProjects,
+  )
+
+  const projectTvs = projectsForTodayTvs.reduce((acc, projectId) => {
+    return acc + (tvs.projects[projectId]?.breakdown.total ?? 0)
+  }, 0)
+  const tvs7d = projectsForSevenDaysAgoTvs.reduce((acc, p) => {
+    return acc + (tvs.projects[p]?.breakdown7d.total ?? 0)
+  }, 0)
+
+  return {
+    tvs: projectTvs,
+    change: calculatePercentageChange(projectTvs, tvs7d),
+  }
+}
+
+function getLiveProjectsAtTimestamp(
+  projects:
+    | {
+        projectId: ProjectId
+        sinceTimestamp: UnixTime
+        untilTimestamp?: UnixTime
+      }[]
+    | undefined,
+  timestamp: UnixTime,
+): ProjectId[] {
+  if (!projects) return []
+  return projects
+    .filter((p) => {
+      return (
+        p.sinceTimestamp <= timestamp &&
+        (p.untilTimestamp === undefined || p.untilTimestamp >= timestamp)
+      )
+    })
+    .map((p) => p.projectId)
+}
+
+function getProjectsForTvs(
+  projectIds: ProjectId[],
+  allProjects: Project<
+    never,
+    'daBridge' | 'isBridge' | 'isScaling' | 'isDaLayer'
+  >[],
+) {
+  return uniq(
+    projectIds.flatMap((tp) => {
       const project = allProjects.find((p) => p.id === tp)
       if (!project) {
         const logger = getLogger().for('getZkCatalogEntry')
@@ -33,21 +87,4 @@ export function getZkCatalogProjectTvs(
       return tp
     }),
   )
-
-  const projectTvs = projectsForTvs.reduce((acc, projectId) => {
-    return acc + (tvs.projects[projectId]?.breakdown.total ?? 0)
-  }, 0)
-
-  const tvs7d = projectsForTvs.reduce((acc, p) => {
-    const projectTvs = tvs.projects[p]?.breakdown7d.total
-    if (!projectTvs) {
-      return acc
-    }
-    return acc + projectTvs
-  }, 0)
-
-  return {
-    tvs: projectTvs,
-    change: calculatePercentageChange(projectTvs, tvs7d),
-  }
 }
