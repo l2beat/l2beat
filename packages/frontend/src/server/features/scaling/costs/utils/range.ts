e7f1e2ea9684bde0030202ee6a53395fdd4fc522
torztomasz <tomasz.torz@l2beat.com>
2025-11-27 09:33:29 +0100

    revert

--- packages/frontend/src/server/features/scaling/costs/utils/range.ts
+++ packages/frontend/src/server/features/scaling/costs/utils/range.ts
@@ -1,9 +1,11 @@
-import type { ChartRange, ChartResolution } from '~/utils/range/range'
-import { rangeToDays } from '~/utils/range/rangeToDays'
+import { UnixTime } from '@l2beat/shared-pure'
+import type { ChartRange } from '~/utils/range/range'
 
-export function rangeToResolution(range: ChartRange): ChartResolution {
-  const days = rangeToDays(range)
-  if (days && days <= 7) return 'hourly'
-  if (days && days <= 90) return 'sixHourly'
+export function rangeToResolution(range: ChartRange) {
+  if (range[0] === null) return 'daily'
+  if (range[0] > UnixTime.toStartOf(UnixTime.now(), 'day') - 30 * UnixTime.DAY)
+    return 'hourly'
+  if (range[0] > UnixTime.toStartOf(UnixTime.now(), 'day') - 90 * UnixTime.DAY)
+    return 'sixHourly'
   return 'daily'
 }
