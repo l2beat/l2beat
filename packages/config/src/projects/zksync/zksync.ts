9eb2b44b274cf886f3fa47eea8ad7abff96228cc
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2025-12-09 12:49:25 +0100

    Update packages/config/src/projects/zksync/zksync.ts

--- packages/config/src/projects/zksync/zksync.ts
+++ packages/config/src/projects/zksync/zksync.ts
@@ -71,7 +71,7 @@ export const zksync: ScalingProject = {
     description:
       'ZKsync Lite (formerly ZKsync) is a ZK Rollup platform that supports payments, token swaps and NFT minting.',
     headerWarning:
-      '[ZKsync Lite will be deprecated in 2026, and the future is firmly focused on the ZKsync network.](https://x.com/zksync/status/1997776813708242975?s=20).',
+      '[ZKsync Lite will be deprecated in 2026.](https://x.com/zksync/status/1997776813708242975?s=20).',
     purposes: ['Payments', 'Exchange', 'NFT'],
     stacks: ['ZKsync Lite'],
     links: {
