fe5130b0f4ae1580a96ecd1ad433055047a19e26
John John <spotter-spinel.4w@icloud.com>
2025-12-18 08:13:28 +0100

    Update hermez.ts

--- packages/config/src/projects/hermez/hermez.ts
+++ packages/config/src/projects/hermez/hermez.ts
@@ -46,8 +46,9 @@ export const hermez: ScalingProject = {
       socialMedia: [
         'https://blog.hermez.io/',
         'https://t.me/PolygonHermez',
-        'https://discord.gg/AczuUXDA2N',
+        'https://discord.com/invite/polygon',
         'https://twitter.com/0xPolygonZK',
+        'https://www.linkedin.com/company/polygonhermez',
       ],
     },
   },
