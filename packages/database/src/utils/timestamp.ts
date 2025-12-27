f64d61245e95cc71fdd9da11feb8d81014c57003
Adrian Adamiak <adrian@adamiak.net>
2025-12-27 13:52:51 +0000

    Update timestamp.ts type signatures

--- packages/database/src/utils/timestamp.ts
+++ packages/database/src/utils/timestamp.ts
@@ -1,10 +1,19 @@
 import { UnixTime } from '@l2beat/shared-pure'
 import isNil from 'lodash/isNil'
 
+export function fromTimestamp(timestamp: UnixTime): Date
+export function fromTimestamp(timestamp: null): null
+export function fromTimestamp(timestamp: undefined): undefined
+export function fromTimestamp(
+  timestamp: UnixTime | null | undefined,
+): Date | null | undefined
 export function fromTimestamp(timestamp: UnixTime | null | undefined) {
   return isNil(timestamp) ? timestamp : UnixTime.toDate(timestamp)
 }
 
+export function toTimestamp(timestamp: Date): number
+export function toTimestamp(timestamp: null): null
+export function toTimestamp(timestamp: Date | null): number | null
 export function toTimestamp(timestamp: Date | null) {
   return isNil(timestamp) ? timestamp : UnixTime.fromDate(timestamp)
 }
