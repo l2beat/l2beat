99cae666443fb1d3df31cf7ab3d52f8e23b7648c
Luca Donno <donnoh99@gmail.com>
2026-01-02 13:02:20 +0100

    Add LIT to Lighter

--- packages/config/src/projects/lighter/lighter.ts
+++ packages/config/src/projects/lighter/lighter.ts
@@ -84,7 +84,7 @@ export const lighter: ScalingProject = {
         address: ChainSpecificAddress(
           'eth:0x3B4D794a66304F130a4Db8F2551B0070dfCf5ca7',
         ),
-        tokens: ['USDC', 'ETH'],
+        tokens: ['USDC', 'ETH', 'LIT'],
       }),
     ],
     daTracking: [
