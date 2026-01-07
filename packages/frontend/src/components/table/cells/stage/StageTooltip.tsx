c8212ea8898497d6919c92bda344e764d84fff43
torztomasz <tomasz.torz@l2beat.com>
2026-01-07 11:16:39 +0100

    fix

--- packages/frontend/src/components/table/cells/stage/StageTooltip.tsx
+++ packages/frontend/src/components/table/cells/stage/StageTooltip.tsx
@@ -147,7 +147,7 @@ export function StageTooltip({
       <Callout
         color="blue"
         body="Please mind, stages do not reflect rollup security"
-        icon={<InfoIcon className="size-4 fill-blue-600" />}
+        icon={<InfoIcon className="mt-px size-4 fill-blue-600" />}
         className={cn(
           'gap-1! px-3 py-2',
           stageConfig.stage !== 'Stage 2' && 'mt-3',
