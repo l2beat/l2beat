b524f6e9069110b64bb87a38f65c40bdaff78c18
Maciej Zygmunt <77489537+maciekzygmunt@users.noreply.github.com>
2026-01-08 12:30:08 +0200

    add auto recovery

--- packages/backend/src/modules/anomalies/AnomalyNotifier.ts
+++ packages/backend/src/modules/anomalies/AnomalyNotifier.ts
@@ -168,6 +168,37 @@ export class AnomalyNotifier {
     )
   }
 
+  async anomalyAutoRecovered(
+    ongoingAnomaly: RealTimeAnomalyRecord,
+    block: Block,
+  ) {
+    const relatedEntityId = this.generateRelatedEntityId(ongoingAnomaly)
+    const notifications =
+      await this.db.notifications.getByRelatedEntityId(relatedEntityId)
+
+    // we only want to send notification if we previously sent a notification about the detected anomaly
+    if (notifications.length === 0) {
+      return
+    }
+
+    const message =
+      `🚧 AUTO RECOVERY: **${ongoingAnomaly.projectId}** recovered from **${formatSubtype(ongoingAnomaly.subtype)}**\n` +
+      `We don't have all data to calculate the anomaly, so we recovered it automatically. Probably by mistake we removed some configuration and now we don't have the whole range (60 days) of data to calculate the anomaly.\n\n` +
+      `- recovered at time: \`${block.timestamp}\`\n` +
+      `- recovered on block: \`${block.number}\``
+
+    const id = await this.sendDiscordNotification(message)
+
+    if (!id) return
+
+    await this.saveNotification(
+      id,
+      'anomaly-recovered',
+      this.generateRelatedEntityId(ongoingAnomaly),
+      block.timestamp,
+    )
+  }
+
   async dailyReport() {
     this.logger.info('Sending daily report')
 
