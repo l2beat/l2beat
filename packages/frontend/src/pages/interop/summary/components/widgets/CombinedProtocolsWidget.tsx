7e4a75b914eddf2bcb3424f1c88ceddb499ab138
Maciej Zygmunt <77489537+maciekzygmunt@users.noreply.github.com>
2026-01-14 09:56:28 +0200

    make clickable area bigger

--- packages/frontend/src/pages/interop/summary/components/widgets/CombinedProtocolsWidget.tsx
+++ packages/frontend/src/pages/interop/summary/components/widgets/CombinedProtocolsWidget.tsx
@@ -12,21 +12,29 @@ export function CombinedProtocolsWidget() {
       ) : (
         <TopProtocolsByTransfers />
       )}
-      <div className="absolute right-[50%] bottom-3 flex gap-1">
+      <div className="absolute right-[50%] bottom-2 flex">
         <div
-          className={cn(
-            'size-3 cursor-pointer rounded-xs',
-            view === 'volume' ? 'bg-brand' : 'bg-secondary',
-          )}
+          className="flex size-4 cursor-pointer items-center justify-center"
           onClick={() => setView('volume')}
-        />
+        >
+          <div
+            className={cn(
+              'size-2 rounded-xs',
+              view === 'volume' ? 'bg-brand' : 'bg-secondary',
+            )}
+          />
+        </div>
         <div
-          className={cn(
-            'size-3 cursor-pointer rounded-xs',
-            view === 'transfers' ? 'bg-brand' : 'bg-secondary',
-          )}
+          className="flex size-4 cursor-pointer items-center justify-center"
           onClick={() => setView('transfers')}
-        />
+        >
+          <div
+            className={cn(
+              'size-2 rounded-xs',
+              view === 'transfers' ? 'bg-brand' : 'bg-secondary',
+            )}
+          />
+        </div>
       </div>
     </div>
   )
