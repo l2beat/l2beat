fe55451371b79027b1c63f457a5ff3eef4f6545e
Adrian Adamiak <adrian@adamiak.net>
2026-01-16 15:11:11 +0000

    Add megaeth to ChainSpecificAddress

--- packages/shared-pure/src/types/ChainSpecificAddress.ts
+++ packages/shared-pure/src/types/ChainSpecificAddress.ts
@@ -52,6 +52,7 @@ const SHORT_TO_LONG_CHAIN_NAMES = {
   soneium: 'soneium',
   wc: 'worldchain',
   'hype-evm': 'hyperevm',
+  megaeth: 'megaeth'
 } as const
 
 const LONG_TO_SHORT_CHAIN_NAMES = Object.fromEntries(
