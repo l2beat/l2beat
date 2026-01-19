abf7d25eb378bd4d9ef77e6e1d24ddddde3e9b92
Maciej Zygmunt <77489537+maciekzygmunt@users.noreply.github.com>
2026-01-19 14:51:29 +0200

    sticky chain selector

--- packages/frontend/src/pages/interop/summary/components/ChainSelector.tsx
+++ packages/frontend/src/pages/interop/summary/components/ChainSelector.tsx
@@ -17,35 +17,38 @@ export function ChainSelector({ chains }: Props) {
     useInteropSelectedChains()
 
   return (
-    <div className="flex items-center justify-between bg-[#ECB2FF] px-4 py-3 max-md:border-brand max-md:border-b md:rounded-lg md:py-2 min-[1024px]:px-6 dark:bg-pink-900">
-      <div className="flex items-center gap-[17px]">
-        <div className="font-semibold text-xs uppercase max-[1024px]:hidden">
-          Chain Selector
+    <div className="sticky top-0 z-30 md:pt-4">
+      <div className="-z-10 absolute top-0 h-22 w-full bg-gradient-to-b from-surface-secondary via-60% via-surface-secondary to-transparent max-md:hidden dark:from-background dark:via-background" />
+      <div className="flex items-center justify-between bg-[#ECB2FF] px-4 py-3 max-md:border-brand max-md:border-b md:rounded-lg md:py-2 min-[1024px]:px-6 dark:bg-pink-900">
+        <div className="flex items-center gap-[17px]">
+          <div className="font-semibold text-xs uppercase max-[1024px]:hidden">
+            Chain Selector
+          </div>
+          <div className="h-10 w-px bg-black/16 max-[1024px]:hidden" />
+          <div className="flex items-center gap-3">
+            <ChainSelectorButton
+              selectedChains={selectedChains.from}
+              allChains={chains}
+              toggleSelected={toggleFrom}
+              type="From"
+            />
+            <ChainSelectorButton
+              selectedChains={selectedChains.to}
+              allChains={chains}
+              toggleSelected={toggleTo}
+              type="To"
+            />
+          </div>
         </div>
-        <div className="h-10 w-px bg-black/16 max-[1024px]:hidden" />
-        <div className="flex items-center gap-3">
-          <ChainSelectorButton
-            selectedChains={selectedChains.from}
-            allChains={chains}
-            toggleSelected={toggleFrom}
-            type="From"
-          />
-          <ChainSelectorButton
-            selectedChains={selectedChains.to}
-            allChains={chains}
-            toggleSelected={toggleTo}
-            type="To"
-          />
-        </div>
+        {isDirty && (
+          <button
+            className="font-semibold text-base leading-[115%] underline"
+            onClick={reset}
+          >
+            Reset to default
+          </button>
+        )}
       </div>
-      {isDirty && (
-        <button
-          className="font-semibold text-base leading-[115%] underline"
-          onClick={reset}
-        >
-          Reset to default
-        </button>
-      )}
     </div>
   )
 }
