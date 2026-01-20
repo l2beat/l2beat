6ec3135a32ab2d88cc7969ad67f1c0815670b7dc
Maciej Zygmunt <77489537+maciekzygmunt@users.noreply.github.com>
2026-01-20 10:33:23 +0100

    Fix Lock mint protocls (#10717)

--- packages/frontend/src/server/features/scaling/interop/utils/getProtocolsByType.ts
+++ packages/frontend/src/server/features/scaling/interop/utils/getProtocolsByType.ts
@@ -19,7 +19,7 @@ export type NonMintingProtocolEntry = CommonProtocolEntry & {
   volume: number
 }
 
-export type LockMintProtocolEntry = CommonProtocolEntry & {
+export type LockAndMintProtocolEntry = CommonProtocolEntry & {
   volume: number
   tokens: TokenData[]
   averageDuration: number
@@ -32,7 +32,7 @@ export type OmniChainProtocolEntry = CommonProtocolEntry & {
 
 export type ProtocolsByType = {
   nonMinting: NonMintingProtocolEntry[]
-  lockMint: LockMintProtocolEntry[]
+  lockAndMint: LockAndMintProtocolEntry[]
   omniChain: OmniChainProtocolEntry[]
 }
 
@@ -84,8 +84,8 @@ export function getProtocolsByType(
   const nonMintingData = protocolsData.filter(([key]) =>
     protocolsByType.nonMinting?.some((p) => p.id === key),
   )
-  const mintLockData = protocolsData.filter(([key]) =>
-    protocolsByType.canonical?.some((p) => p.id === key),
+  const lockAndMintData = protocolsData.filter(([key]) =>
+    protocolsByType.lockAndMint?.some((p) => p.id === key),
   )
   const omniChainData = protocolsData.filter(([key]) =>
     protocolsByType.omnichain?.some((p) => p.id === key),
@@ -122,7 +122,7 @@ export function getProtocolsByType(
         volume,
       }
     }),
-    lockMint: mintLockData.map(
+    lockAndMint: lockAndMintData.map(
       ([key, { volume, tokens, transferCount, totalDurationSum }]) => {
         return {
           ...getProjectCommon(key),
