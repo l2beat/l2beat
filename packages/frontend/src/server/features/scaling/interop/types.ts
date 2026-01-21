f133bd421752b583b800bd6400f129a9a853eacd
Maciej Zygmunt <77489537+maciekzygmunt@users.noreply.github.com>
2026-01-21 16:25:56 +0200

    move type to shared pure

--- packages/frontend/src/server/features/scaling/interop/types.ts
+++ packages/frontend/src/server/features/scaling/interop/types.ts
@@ -1,8 +1,9 @@
+import { InteropBridgeType } from '@l2beat/shared-pure'
 import { v } from '@l2beat/validate'
 
 export type InteropDashboardParams = v.infer<typeof InteropDashboardParams>
 export const InteropDashboardParams = v.object({
   from: v.array(v.string()),
   to: v.array(v.string()),
-  type: v.enum(['nonMinting', 'lockAndMint', 'omnichain']).optional(),
+  type: InteropBridgeType.optional(),
 })
