9ebcba2dbcae7a71d8dd16792b4d818f373ce6c4
Maciej Zygmunt <77489537+maciekzygmunt@users.noreply.github.com>
2026-01-21 15:36:32 +0200

    add link to summary

--- packages/frontend/src/pages/interop/summary/components/table-widgets/NonMintingCard.tsx
+++ packages/frontend/src/pages/interop/summary/components/table-widgets/NonMintingCard.tsx
@@ -1,3 +1,4 @@
+import { Button } from '~/components/core/Button'
 import { Skeleton } from '~/components/core/Skeleton'
 import { PrimaryCard } from '~/components/primary-card/PrimaryCard'
 import { api } from '~/trpc/React'
@@ -15,9 +16,20 @@ export function NonMintingCard() {
 
   return (
     <PrimaryCard className="flex flex-col max-md:border-divider max-md:border-b">
-      <h2 className="font-bold text-heading-20 md:text-heading-24">
-        Non-minting
-      </h2>
+      <div className="flex items-center justify-between">
+        <h2 className="font-bold text-heading-20 md:text-heading-24">
+          Non-minting
+        </h2>
+        <a href="/interop/non-minting">
+          <Button
+            variant="outline"
+            size="sm"
+            className="px-4 py-[9px] leading-none"
+          >
+            View all
+          </Button>
+        </a>
+      </div>
       <div className="mt-2.5 text-paragraph-12 text-secondary md:text-paragraph-13">
         In-light risk only. Tokens are therefore first bridged using a different
         minting bridge that needs to be separately assessed.
