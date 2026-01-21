c2fb95eda8af19bf7d445bedca092189b51c7254
Maciej Zygmunt <77489537+maciekzygmunt@users.noreply.github.com>
2026-01-21 12:30:43 +0200

    add config to base

--- packages/config/src/projects/base/base.ts
+++ packages/config/src/projects/base/base.ts
@@ -51,6 +51,18 @@ export const base: ScalingProject = opStackL2({
   interopConfig: {
     name: 'Base Canonical',
     bridgeType: 'lockAndMint',
+    durationSplit: {
+      in: {
+        label: 'L1 -> L2',
+        from: 'ethereum',
+        to: 'optimism',
+      },
+      out: {
+        label: 'L2 -> L1',
+        from: 'optimism',
+        to: 'ethereum',
+      },
+    },
     plugins: [
       {
         chain: 'base',
