cee8f63a4e1f36d3897460bdcf9b3535d4ec117c
Maciej Zygmunt <77489537+maciekzygmunt@users.noreply.github.com>
2026-01-22 14:11:47 +0200

    add link

--- packages/frontend/src/pages/interop/summary/components/table-widgets/LockAndMintCard.tsx
+++ packages/frontend/src/pages/interop/summary/components/table-widgets/LockAndMintCard.tsx
@@ -1,3 +1,4 @@
+import { Button } from '~/components/core/Button'
 import { Skeleton } from '~/components/core/Skeleton'
 import { PrimaryCard } from '~/components/primary-card/PrimaryCard'
 import type { InteropDashboardData } from '~/server/features/scaling/interop/getInteropDashboardData'
@@ -13,9 +14,20 @@ export function LockAndMintCard({
 }) {
   return (
     <PrimaryCard className="flex flex-col max-md:border-divider max-md:border-b">
-      <h2 className="font-bold text-heading-20 md:text-heading-24">
-        Lock & Mint
-      </h2>
+      <div className="flex items-center justify-between">
+        <h2 className="font-bold text-heading-20 md:text-heading-24">
+          Lock & Mint
+        </h2>
+        <a href="/interop/lock-and-mint">
+          <Button
+            variant="outline"
+            size="sm"
+            className="px-4 py-[9px] leading-none"
+          >
+            View all
+          </Button>
+        </a>
+      </div>
       <div className="mt-2.5 text-paragraph-12 text-secondary md:text-paragraph-13">
         One-sided risk. If user bridge back, the original tokens are unlocked
         and the bridge risk is removed.
