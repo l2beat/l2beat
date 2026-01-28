2f51f28206504bd5ab69715972d230d014fcd77a
marukai67 <miserly.pendant0p@icloud.com>
2026-01-28 21:35:37 +0100

    Update chains.ts

--- packages/config/src/processing/chains.ts
+++ packages/config/src/processing/chains.ts
@@ -1,12 +1,16 @@
+import type { ChainConfig } from '../types'
 import { bridges } from './bridges'
 import { layer2s } from './layer2s'
 import { layer3s } from './layer3s'
 import { refactored } from './refactored'
 
+const getChainConfigs = (projects: Array<{ chainConfig?: ChainConfig }>) =>
+  projects.flatMap(({ chainConfig }) => (chainConfig ? [chainConfig] : []))
+
 /** @deprecated */
 export const chains = [
-  ...layer2s.map((p) => p.chainConfig).filter((x) => x !== undefined),
-  ...layer3s.map((p) => p.chainConfig).filter((x) => x !== undefined),
-  ...bridges.map((p) => p.chainConfig).filter((x) => x !== undefined),
-  ...refactored.map((p) => p.chainConfig).filter((x) => x !== undefined),
+  ...getChainConfigs(layer2s),
+  ...getChainConfigs(layer3s),
+  ...getChainConfigs(bridges),
+  ...getChainConfigs(refactored),
 ]
