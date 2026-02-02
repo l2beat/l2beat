c9ecde2a73e2afe57792b3268fda70533b98273e
sekuba <29250140+sekuba@users.noreply.github.com>
2026-02-02 08:41:15 +0100

    polygon duration split

--- packages/config/src/projects/polygon-pos/polygon-pos.ts
+++ packages/config/src/projects/polygon-pos/polygon-pos.ts
@@ -86,6 +86,18 @@ export const polygonpos: ScalingProject = {
         plugin: 'polygon',
       },
     ],
+    durationSplit: {
+      in: {
+        label: 'L1 -> L2',
+        from: 'ethereum',
+        to: 'polygonpos',
+      },
+      out: {
+        label: 'L2 -> L1',
+        from: 'polygonpos',
+        to: 'ethereum',
+      },
+    },
   },
   config: {
     associatedTokens: ['POL', 'MATIC'],
