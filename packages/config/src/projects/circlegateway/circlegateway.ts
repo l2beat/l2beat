70ef323851d97d01be1259693c7fd0786cfe3780
sekuba <29250140+sekuba@users.noreply.github.com>
2026-02-03 11:56:54 +0100

    add circle gateway

---
+++ packages/config/src/projects/circlegateway/circlegateway.ts
@@ -0,0 +1,19 @@
+import { ProjectId, UnixTime } from '@l2beat/shared-pure'
+import type { BaseProject } from '../../types'
+
+export const circlegateway: BaseProject = {
+  id: ProjectId('circlegateway'),
+  slug: 'circlegateway',
+  name: 'Circle Gateway',
+  shortName: 'Gateway',
+  addedAt: UnixTime(1770115878),
+  interopConfig: {
+    bridgeType: 'omnichain',
+    plugins: [
+      {
+        plugin: 'circle-gateway',
+      },
+    ],
+  },
+  isInteropProtocol: true,
+}
