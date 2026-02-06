2c8263c18bc497613411230829dc66d09c881bc5
Radina Talanova <nt.radina@gmail.com>
2026-02-06 11:16:53 +0200

    format

--- packages/frontend/src/components/projects/sections/StageSection.tsx
+++ packages/frontend/src/components/projects/sections/StageSection.tsx
@@ -309,10 +309,10 @@ export function StageSection({
                   )}
 
                   {stage.principle && (
-                      <p className="font-bold text-label-value-14 text-secondary">
-                        Guidelines
-                      </p>
-                    )}
+                    <p className="font-bold text-label-value-14 text-secondary">
+                      Guidelines
+                    </p>
+                  )}
                   <ul className="space-y-1 md:space-y-2">
                     {satisfiedRequirements.map((req, i) => (
                       <li key={i} className="flex">
@@ -339,29 +339,30 @@ export function StageSection({
                       </li>
                     ))}
                   </ul>
-                  {showUpcomingGuidelines && upcomingRequirements.length > 0 && (
-                    <div className="mt-3 rounded-lg bg-brand/20 p-4">
-                      <p className="mb-2 font-bold text-label-value-14 text-secondary">
-                        Upcoming guidelines
-                      </p>
-                      <ul className="space-y-1 md:space-y-2">
-                        {upcomingRequirements.map((req, i) => (
-                          <li key={i} className="flex">
-                            {req.satisfied === 'UnderReview' ? (
-                              <UnderReviewIcon className="relative top-0.5 size-4 shrink-0 md:top-[3px]" />
-                            ) : req.satisfied === true ? (
-                              <SatisfiedIcon className="relative top-0.5 size-4 shrink-0 fill-positive md:top-[3px]" />
-                            ) : (
-                              <MissingIcon className="relative top-0.5 size-4 shrink-0 fill-negative md:top-[3px]" />
-                            )}
-                            <Markdown className="ml-2 font-medium text-paragraph-14 md:text-paragraph-16">
-                              {req.description}
-                            </Markdown>
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                  )}
+                  {showUpcomingGuidelines &&
+                    upcomingRequirements.length > 0 && (
+                      <div className="mt-3 rounded-lg bg-brand/20 p-4">
+                        <p className="mb-2 font-bold text-label-value-14 text-secondary">
+                          Upcoming guidelines
+                        </p>
+                        <ul className="space-y-1 md:space-y-2">
+                          {upcomingRequirements.map((req, i) => (
+                            <li key={i} className="flex">
+                              {req.satisfied === 'UnderReview' ? (
+                                <UnderReviewIcon className="relative top-0.5 size-4 shrink-0 md:top-[3px]" />
+                              ) : req.satisfied === true ? (
+                                <SatisfiedIcon className="relative top-0.5 size-4 shrink-0 fill-positive md:top-[3px]" />
+                              ) : (
+                                <MissingIcon className="relative top-0.5 size-4 shrink-0 fill-negative md:top-[3px]" />
+                              )}
+                              <Markdown className="ml-2 font-medium text-paragraph-14 md:text-paragraph-16">
+                                {req.description}
+                              </Markdown>
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    )}
                 </div>
               </CollapsibleContent>
             </Collapsible>
