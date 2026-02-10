561853ea12720a923517f18a58385ba9f9892b58
Radina Talanova <nt.radina@gmail.com>
2026-02-10 13:18:40 +0200

    Update billions.ts

--- packages/config/src/projects/billions/billions.ts
+++ packages/config/src/projects/billions/billions.ts
@@ -19,7 +19,7 @@ export const billions: ScalingProject = upcomingL2({
       socialMedia: [
         'https://x.com/billions_ntwk',
         'https://discord.com/invite/billions-ntwk',
-        'https://www.linkedin.com/company/billions-ntwk',
+        'https://linkedin.com/company/billions-ntwk',
       ],
       repositories: ['https://github.com/0xPolygonID'],
     },
