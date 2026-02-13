858eb1d186ebe511167e38787c16249af1dcdf58
Maciej Zygmunt <77489537+maciekzygmunt@users.noreply.github.com>
2026-02-13 14:20:30 +0200

    sort by percentage

--- packages/frontend/src/server/features/scaling/interop/utils/getTransferSizeChartData.ts
+++ packages/frontend/src/server/features/scaling/interop/utils/getTransferSizeChartData.ts
@@ -1,25 +1,29 @@
 import type { Project } from '@l2beat/config'
 import type { AggregatedInteropTransferRecord } from '@l2beat/database'
 import { assert } from '@l2beat/shared-pure'
+import round from 'lodash/round'
 import { manifest } from '~/utils/Manifest'
 
-export type TransferSizeChartData = Record<string, TransferSizeData>
-
-type TransferSizeData = {
+export type TransferSizeDataPoint = {
   name: string
   iconUrl: string
   countUnder100: number
+  percentageUnder100: number
   count100To1K: number
+  percentage100To1K: number
   count1KTo10K: number
+  percentage1KTo10K: number
   count10KTo100K: number
+  percentage10KTo100K: number
   countOver100K: number
+  percentageOver100K: number
 }
 
 export function getTransferSizeChartData(
   records: AggregatedInteropTransferRecord[],
   interopProjects: Project<'interopConfig'>[],
-): TransferSizeChartData | undefined {
-  const data = new Map<string, TransferSizeData & { volume: number }>()
+): TransferSizeDataPoint[] | undefined {
+  const data = new Map<string, TransferSizeDataPoint>()
 
   if (records.length === 0) {
     return undefined
@@ -36,34 +40,55 @@ export function getTransferSizeChartData(
       count1KTo10K: 0,
       count10KTo100K: 0,
       countOver100K: 0,
-      volume: 0,
     }
+
+    const countUnder100 = current.countUnder100 + record.countUnder100
+    const count100To1K = current.count100To1K + record.count100To1K
+    const count1KTo10K = current.count1KTo10K + record.count1KTo10K
+    const count10KTo100K = current.count10KTo100K + record.count10KTo100K
+    const countOver100K = current.countOver100K + record.countOver100K
+
+    const total =
+      countUnder100 +
+      count100To1K +
+      count1KTo10K +
+      count10KTo100K +
+      countOver100K
+
     data.set(record.id, {
-      ...current,
-      volume: current.volume + (record.srcValueUsd ?? record.dstValueUsd ?? 0),
-      countUnder100: current.countUnder100 + record.countUnder100,
-      count100To1K: current.count100To1K + record.count100To1K,
-      count1KTo10K: current.count1KTo10K + record.count1KTo10K,
-      count10KTo100K: current.count10KTo100K + record.count10KTo100K,
-      countOver100K: current.countOver100K + record.countOver100K,
+      name: current.name,
+      iconUrl: current.iconUrl,
+      countUnder100,
+      percentageUnder100:
+        total > 0 ? round((countUnder100 / total) * 100, 2) : 0,
+      count100To1K,
+      percentage100To1K: total > 0 ? round((count100To1K / total) * 100, 2) : 0,
+      count1KTo10K,
+      percentage1KTo10K: total > 0 ? round((count1KTo10K / total) * 100, 2) : 0,
+      count10KTo100K,
+      percentage10KTo100K:
+        total > 0 ? round((count10KTo100K / total) * 100, 2) : 0,
+      countOver100K,
+      percentageOver100K:
+        total > 0 ? round((countOver100K / total) * 100, 2) : 0,
     })
   }
 
-  return Object.fromEntries(
-    Array.from(data.entries())
-      .sort((a, b) => b[1].volume - a[1].volume)
-      .slice(0, 15)
-      .map(([key, value]) => [
-        key,
-        {
-          name: value.name,
-          iconUrl: value.iconUrl,
-          countUnder100: value.countUnder100,
-          count100To1K: value.count100To1K,
-          count1KTo10K: value.count1KTo10K,
-          count10KTo100K: value.count10KTo100K,
-          countOver100K: value.countOver100K,
-        },
-      ]),
-  )
+  return Array.from(data.values())
+    .sort((a, b) => b.percentageUnder100 - a.percentageUnder100)
+    .slice(0, 15)
+    .map((value) => ({
+      name: value.name,
+      iconUrl: value.iconUrl,
+      countUnder100: value.countUnder100,
+      percentageUnder100: value.percentageUnder100,
+      count100To1K: value.count100To1K,
+      percentage100To1K: value.percentage100To1K,
+      count1KTo10K: value.count1KTo10K,
+      percentage1KTo10K: value.percentage1KTo10K,
+      count10KTo100K: value.count10KTo100K,
+      percentage10KTo100K: value.percentage10KTo100K,
+      countOver100K: value.countOver100K,
+      percentageOver100K: value.percentageOver100K,
+    }))
 }
