cb844278e68adf0053353b1de837f231d9000792
Radina Talanova <nt.radina@gmail.com>
2026-02-16 16:33:12 +0200

    Change countdown time

--- packages/config/src/global/countdowns.ts
+++ packages/config/src/global/countdowns.ts
@@ -1,5 +1,5 @@
 import { UnixTime } from '@l2beat/shared-pure'
 
 export const PROJECT_COUNTDOWNS = {
-  stageChanges: UnixTime.fromDate(new Date('2026-07-01T00:00:00Z')),
+  stageChanges: UnixTime.fromDate(new Date('2026-08-17T12:00:00Z')),
 }
