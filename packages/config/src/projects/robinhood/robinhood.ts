32bba0a061f4995dc6d376ebc22e68d643812466
Radina Talanova <nt.radina@gmail.com>
2026-02-17 16:11:40 +0100

    Add Robinhood (#11069)

---
+++ packages/config/src/projects/robinhood/robinhood.ts
@@ -0,0 +1,32 @@
+import { ProjectId, UnixTime } from '@l2beat/shared-pure'
+import type { ScalingProject } from '../../internalTypes'
+import { upcomingL2 } from '../../templates/upcoming'
+
+export const robinhood: ScalingProject = upcomingL2({
+  id: 'robinhood',
+  capability: 'universal',
+  addedAt: UnixTime(1771258645), //16.02.2026
+  hasTestnet: true,
+  display: {
+    name: 'Robinhood Chain',
+    slug: 'robinhood',
+    description:
+      'Robinhood Chain is a Layer 2 blockchain built for financial services and tokenized real-world assets. It uses the Arbitrum Orbit stack.',
+    purposes: ['Universal'],
+    stacks: ['Arbitrum'],
+    links: {
+      websites: ['https://robinhood.com/eu/en/chain/'],
+      bridges: [],
+      documentation: ['https://docs.robinhood.com/chain'],
+      repositories: [],
+      explorers: ['https://explorer.testnet.chain.robinhood.com/stats'],
+      socialMedia: ['https://x.com/RobinhoodApp'],
+    },
+  },
+  ecosystemInfo: {
+    id: ProjectId('arbitrum-orbit'),
+  },
+  proofSystem: {
+    type: 'Optimistic',
+  },
+})
