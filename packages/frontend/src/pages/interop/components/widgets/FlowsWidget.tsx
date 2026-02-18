08b82a4519d2ed58c8dd7e39d0c80e1416a0441c
Maciej Zygmunt <77489537+maciekzygmunt@users.noreply.github.com>
2026-02-18 12:02:07 +0100

    update flows widget

--- packages/frontend/src/pages/interop/components/widgets/FlowsWidget.tsx
+++ packages/frontend/src/pages/interop/components/widgets/FlowsWidget.tsx
@@ -1,5 +1,6 @@
 import { assert } from '@l2beat/shared-pure'
 import times from 'lodash/times'
+import { Breakdown } from '~/components/breakdown/Breakdown'
 import { Skeleton } from '~/components/core/Skeleton'
 import { PrimaryCard } from '~/components/primary-card/PrimaryCard'
 import { ArrowRightIcon } from '~/icons/ArrowRight'
@@ -7,7 +8,6 @@ import type { InteropChainWithIcon } from '~/pages/interop/components/chain-sele
 import type { InteropDashboardData } from '~/server/features/scaling/interop/getInteropDashboardData'
 import { cn } from '~/utils/cn'
 import { formatCurrency } from '~/utils/number-format/formatCurrency'
-import { BetweenChainsInfo } from '../BetweenChainsInfo'
 
 export function FlowsWidget({
   interopChains,
@@ -25,7 +25,6 @@ export function FlowsWidget({
     assert(chain, `Chain not found: ${id}`)
     return {
       id: chain.id,
-      name: chain.name,
       iconUrl: chain.iconUrl,
     }
   }
@@ -38,69 +37,56 @@ export function FlowsWidget({
         <h2 className="font-bold text-heading-16 md:text-heading-20">
           Last 24 hours flows
         </h2>
-        <BetweenChainsInfo className="mt-0.5" />
-        <table className="-mb-1.5 mt-0.5 w-full border-separate border-spacing-y-1.5">
-          <tbody>
-            {isLoading &&
-              times(4).map((index) => (
-                <tr key={index}>
-                  <td>
-                    <Skeleton className="h-9.5 w-full" />
-                  </td>
-                </tr>
-              ))}
-            {flows?.map((flow) => (
-              <PathItem
-                key={flow.srcChain + flow.dstChain}
-                from={getChainDetails(flow.srcChain)}
-                to={getChainDetails(flow.dstChain)}
-                volume={flow.volume}
-              />
+        <div className="mt-7 grid grid-cols-2 gap-3">
+          {isLoading &&
+            times(2).map((index) => (
+              <Skeleton key={index} className="h-20 w-full" />
             ))}
-          </tbody>
-        </table>
+          {flows?.map((flow) => (
+            <FlowItem
+              key={flow.srcChain + flow.dstChain}
+              from={getChainDetails(flow.srcChain)}
+              to={getChainDetails(flow.dstChain)}
+              volume={flow.volume}
+            />
+          ))}
+        </div>
+        {flows && (
+          <Breakdown
+            className="mt-2! h-1 w-full"
+            values={flows?.map((flow, i) => ({
+              value: flow.volume,
+              className: i === 0 ? 'bg-purple-100' : 'bg-[#6AC9FF]',
+            }))}
+          />
+        )}
       </div>
     </PrimaryCard>
   )
 }
 
-function PathItem({
+function FlowItem({
   from,
   to,
   volume,
 }: {
-  from: { id: string; name: string; iconUrl: string }
-  to: { id: string; name: string; iconUrl: string }
+  from: { id: string; iconUrl: string }
+  to: { id: string; iconUrl: string }
   volume: number
 }) {
   return (
-    <tr>
-      <td className="rounded-l-lg border-divider border-t border-b border-l py-2 pl-2.5 leading-none">
-        <div className="flex items-center gap-1.5">
-          <img src={from.iconUrl} alt={from.name} className="size-5" />
-          <div className="@max-[472px]:hidden font-medium text-label-value-15">
-            {from.name}
-          </div>
-          <ArrowRightIcon className="size-5 fill-brand" />
-          <img src={to.iconUrl} alt={to.name} className="size-5" />
-          <div className="@max-[472px]:hidden font-medium text-label-value-15">
-            {to.name}
-          </div>
-        </div>
-      </td>
-      <td
-        align="right"
-        className="w-0 rounded-r-lg border-divider border-t border-r border-b py-2 pr-6 text-left leading-none"
-      >
-        <div className="flex items-center justify-start gap-1">
-          <div className="font-medium text-[13px] text-secondary leading-none">
-            Volume:
-          </div>
-          <div className="whitespace-nowrap font-bold text-label-value-15">
-            {formatCurrency(volume, 'usd')}
-          </div>
-        </div>
-      </td>
-    </tr>
+    <div className="flex flex-col items-center rounded-lg border border-divider px-4 py-3">
+      <div className="flex gap-1.5">
+        <img src={from.iconUrl} alt={from.id} className="size-5" />
+        <ArrowRightIcon className="size-5 fill-brand" />
+        <img src={to.iconUrl} alt={to.id} className="size-5" />
+      </div>
+      <span className="mt-2 font-medium text-label-value-12 text-secondary">
+        Volume
+      </span>
+      <div className="mt-0.5 font-bold text-label-value-20">
+        {formatCurrency(volume, 'usd')}
+      </div>
+    </div>
   )
 }
