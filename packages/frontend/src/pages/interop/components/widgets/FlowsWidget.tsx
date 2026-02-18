056a082601fe3853a631ab17d5ddd178d9dec020
Maciej Zygmunt <77489537+maciekzygmunt@users.noreply.github.com>
2026-02-18 15:34:12 +0100

    add tooltip

--- packages/frontend/src/pages/interop/components/widgets/FlowsWidget.tsx
+++ packages/frontend/src/pages/interop/components/widgets/FlowsWidget.tsx
@@ -2,6 +2,12 @@ import { assert } from '@l2beat/shared-pure'
 import times from 'lodash/times'
 import { Breakdown } from '~/components/breakdown/Breakdown'
 import { Skeleton } from '~/components/core/Skeleton'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipPortal,
+  TooltipTrigger,
+} from '~/components/core/tooltip/Tooltip'
 import { PrimaryCard } from '~/components/primary-card/PrimaryCard'
 import { ArrowRightIcon } from '~/icons/ArrowRight'
 import type { InteropChainWithIcon } from '~/pages/interop/components/chain-selector/types'
@@ -26,8 +32,11 @@ export function FlowsWidget({
     return {
       id: chain.id,
       iconUrl: chain.iconUrl,
+      name: chain.name,
     }
   }
+
+  const totalVolume = flows?.reduce((acc, flow) => acc + flow.volume, 0)
 
   return (
     <PrimaryCard
@@ -37,7 +46,10 @@ export function FlowsWidget({
         <h2 className="font-bold text-heading-16 md:text-heading-20">
           Last 24 hours flows
         </h2>
-        <div className="mt-7 grid grid-cols-2 gap-3">
+        <div className="mt-1 font-medium text-label-value-12 text-secondary md:text-label-value-14">
+          By volume
+        </div>
+        <div className="mt-4 grid grid-cols-2 gap-3">
           {isLoading &&
             times(2).map((index) => (
               <Skeleton key={index} className="h-20 w-full" />
@@ -52,16 +64,65 @@ export function FlowsWidget({
           ))}
         </div>
         {flows && (
-          <Breakdown
-            className="mt-2! h-1 w-full"
-            values={flows?.map((flow, i) => ({
-              value: flow.volume,
-              className: i === 0 ? 'bg-purple-100' : 'bg-n-pink-350',
-            }))}
-          />
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <div className="cursor-pointer">
+                <Breakdown
+                  className="mt-4! h-1.5 w-full"
+                  values={flows?.map((flow, i) => ({
+                    value: flow.volume,
+                    className: i === 0 ? 'bg-purple-100' : 'bg-n-pink-350',
+                  }))}
+                />
+              </div>
+            </TooltipTrigger>
+            <TooltipPortal>
+              <TooltipContent className="flex flex-col gap-1.5">
+                {flows.map((flow) => (
+                  <FlowTooltipItem
+                    key={flow.srcChain + flow.dstChain}
+                    flow={flow}
+                    from={getChainDetails(flow.srcChain)}
+                    to={getChainDetails(flow.dstChain)}
+                    totalVolume={totalVolume ?? 0}
+                  />
+                ))}
+              </TooltipContent>
+            </TooltipPortal>
+          </Tooltip>
         )}
       </div>
     </PrimaryCard>
+  )
+}
+
+function FlowTooltipItem({
+  flow,
+  from,
+  to,
+  totalVolume,
+}: {
+  flow: InteropDashboardData['flows'][number]
+  from: { id: string; iconUrl: string; name: string }
+  to: { id: string; iconUrl: string; name: string }
+  totalVolume: number
+}) {
+  return (
+    <div
+      key={flow.srcChain + flow.dstChain}
+      className="flex items-center gap-2.5"
+    >
+      <div className="flex items-center gap-1.5 font-medium text-label-value-15">
+        <img src={from.iconUrl} alt={from.id} className="size-5" />
+        <span>{from.name}</span>
+        <ArrowRightIcon className="size-5 fill-brand" />
+        <img src={to.iconUrl} alt={to.id} className="size-5" />
+        <span>{to.name}</span>
+      </div>
+      <div className="font-bold text-label-value-15">
+        {Math.round((flow.volume / totalVolume) * 100)}%
+      </div>
+    </div>
   )
 }
 
@@ -75,16 +136,16 @@ function FlowItem({
   volume: number
 }) {
   return (
-    <div className="flex flex-col items-center rounded-lg border border-divider px-4 py-3">
+    <div className="flex flex-col items-center rounded-lg border border-divider px-4 py-7">
       <div className="flex gap-1.5">
         <img src={from.iconUrl} alt={from.id} className="size-5" />
         <ArrowRightIcon className="size-5 fill-brand" />
         <img src={to.iconUrl} alt={to.id} className="size-5" />
       </div>
-      <span className="mt-2 font-medium text-label-value-12 text-secondary">
+      <span className="mt-2 font-medium text-label-value-14 text-secondary">
         Volume
       </span>
-      <div className="mt-0.5 font-bold text-label-value-20">
+      <div className="mt-0.5 font-bold text-label-value-24">
         {formatCurrency(volume, 'usd')}
       </div>
     </div>
