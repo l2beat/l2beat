4481aa798631532dba027db87202adac23e23678
Maciej Zygmunt <77489537+maciekzygmunt@users.noreply.github.com>
2026-02-18 12:21:36 +0100

    update colors

--- packages/frontend/src/components/primary-card/HighlightablePrimaryCard.tsx
+++ packages/frontend/src/components/primary-card/HighlightablePrimaryCard.tsx
@@ -28,7 +28,7 @@ function Content({
         'relative transition-shadow duration-300',
         highlightedId &&
           props.id === highlightedId &&
-          'md:z-10 md:shadow-[#FF5FFB40] md:shadow-[0px_4px_12px_10px] md:ring-2 md:ring-brand',
+          'md:z-10 md:shadow-[0px_4px_12px_10px] md:shadow-n-pink-350 md:ring-2 md:ring-brand',
         className,
       )}
       {...props}
