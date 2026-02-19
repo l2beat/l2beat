df16ea1872a8177c826f39d8de19e0e1af234578
Maciej Zygmunt <77489537+maciekzygmunt@users.noreply.github.com>
2026-02-19 15:39:06 +0100

    Move flows widget out of carousel on mobile (#11109)

--- packages/frontend/src/pages/interop/summary/InteropSummaryPage.tsx
+++ packages/frontend/src/pages/interop/summary/InteropSummaryPage.tsx
@@ -110,10 +110,10 @@ function Widgets({ interopChains }: { interopChains: InteropChainWithIcon[] }) {
 
   return (
     <div
-      className="mt-5 grid grid-cols-1 md:grid-cols-2 min-[1600px]:grid-cols-3 min-md:gap-5"
+      className="grid grid-cols-1 md:mt-5 md:grid-cols-2 min-[1600px]:grid-cols-3 min-md:gap-5"
       data-hide-overflow-x
     >
-      <div className="z-10 max-md:hidden">
+      <div className="z-10">
         <FlowsWidget
           interopChains={interopChains}
           isLoading={isLoading}
