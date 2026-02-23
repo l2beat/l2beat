7aff196ac403e3ac4d06f933ae7ac9c2201a7f27
torztomasz <tomasz.torz@l2beat.com>
2026-02-23 14:19:51 +0100

    add comment

--- packages/frontend/src/server/features/scaling/interop/utils/resolveInteropSelection.ts
+++ packages/frontend/src/server/features/scaling/interop/utils/resolveInteropSelection.ts
@@ -16,6 +16,17 @@ export function normalizeInteropSelection(
     return undefined
   }
 
+  // I know it's ugly but for the sake of simplicity and not passing any `mode: 'mutli-chain' | 'pair'` to trpc calls
+  // we will assume that if there is only one chain in from and one in to, it's a pair selection
+  if (from.length === 1 && to.length === 1) {
+    return {
+      // biome-ignore lint/style/noNonNullAssertion: temp fix
+      from: [from[0]!, to[0]!],
+      // biome-ignore lint/style/noNonNullAssertion: temp fix
+      to: [to[0]!, from[0]!],
+    }
+  }
+
   return {
     from,
     to,
