ea3a51f9555cc294f6f022bec4cf02f4949669fb
Maciej Zygmunt <77489537+maciekzygmunt@users.noreply.github.com>
2026-02-24 09:42:44 +0100

    Fix weird swipe in two directions on tokens view (#11149)

--- packages/frontend/src/pages/interop/components/top-items/TopChainsCell.tsx
+++ packages/frontend/src/pages/interop/components/top-items/TopChainsCell.tsx
@@ -155,7 +155,7 @@ function TopChainsContent({
               <span>{protocol.name}</span>
             </DrawerTitle>
           </DrawerHeader>
-          <div className="max-h-[60vh] overflow-auto">
+          <div className="max-h-[60vh] overflow-y-auto overflow-x-hidden">
             <BasicTable
               skeletonCount={6}
               table={table}
