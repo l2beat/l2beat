7a3bed3b1a94a716fa77b5635e0a1b157cc2296a
sekuba <29250140+sekuba@users.noreply.github.com>
2026-02-26 11:51:40 +0100

    update types

--- packages/backend/src/modules/interop/engine/aggregation/InteropAggregatingIndexer.test.ts
+++ packages/backend/src/modules/interop/engine/aggregation/InteropAggregatingIndexer.test.ts
@@ -39,6 +39,7 @@ describe(InteropAggregatingIndexer.name, () => {
         {
           id: 'config1',
           plugins: [{ plugin: 'across', bridgeType: 'lockAndMint' }],
+          type: 'other',
         },
       ]
 
@@ -164,6 +165,7 @@ describe(InteropAggregatingIndexer.name, () => {
         {
           id: 'config1',
           plugins: [{ plugin: 'across', bridgeType: 'lockAndMint' }],
+          type: 'other',
         },
       ]
 
