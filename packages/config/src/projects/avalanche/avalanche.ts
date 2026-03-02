9d36a90d484ef75fcb45cffe9d79f90479d92c56
sekuba <29250140+sekuba@users.noreply.github.com>
2026-03-02 12:54:11 +0100

    re-add avalanche

---
+++ packages/config/src/projects/avalanche/avalanche.ts
@@ -0,0 +1,29 @@
+import { EthereumAddress, ProjectId, UnixTime } from '@l2beat/shared-pure'
+import type { BaseProject } from '../../types'
+
+export const avalanche: BaseProject = {
+  id: ProjectId('avalanche'),
+  slug: 'avalanche',
+  name: 'Avalanche',
+  shortName: undefined,
+  addedAt: UnixTime(1662628329),
+  chainConfig: {
+    name: 'avalanche',
+    chainId: 43114,
+    explorerUrl: 'https://snowtrace.io',
+    multicallContracts: [
+      {
+        address: EthereumAddress('0xcA11bde05977b3631167028862bE2a173976CA11'),
+        batchSize: 150,
+        sinceBlock: 11907934,
+        version: '3',
+      },
+    ],
+    apis: [
+      {
+        type: 'rpc',
+        url: 'https://avalanche.drpc.org',
+      },
+    ],
+  },
+}
