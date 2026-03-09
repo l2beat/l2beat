1bf58d97753adc3f04e3d4051aaebdc3c6eb9cfe
Michał Podsiadły <67391475+sdlyy@users.noreply.github.com>
2026-03-09 11:17:41 +0100

    Convert errors to infos when logging about removed networks (#11283)

--- packages/backend/src/modules/interop/plugins/cctp/cctp.config.ts
+++ packages/backend/src/modules/interop/plugins/cctp/cctp.config.ts
@@ -117,7 +117,7 @@ export class CCTPConfigPlugin extends TimeLoop implements InteropConfigPlugin {
     const reconciledV1 = reconcileNetworks(previousV1, latest.v1)
 
     if (reconciledV1.removed.length > 0) {
-      this.logger.error('Networks removed', {
+      this.logger.info('Upstream networks removed', {
         plugin: CCTPV1Config.key,
         removed: reconciledV1.removed,
       })
@@ -134,7 +134,7 @@ export class CCTPConfigPlugin extends TimeLoop implements InteropConfigPlugin {
     const reconciledV2 = reconcileNetworks(previousV2, latest.v2)
 
     if (reconciledV2.removed.length > 0) {
-      this.logger.error('Networks removed', {
+      this.logger.info('Upstream networks removed', {
         plugin: CCTPV2Config.key,
         removed: reconciledV2.removed,
       })
