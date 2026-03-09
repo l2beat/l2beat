b1d6f38931b998711a4d6384a4ca69528fe085b4
torztomasz <tomasz.torz@l2beat.com>
2026-03-09 15:10:35 +0100

    fix

--- packages/frontend/src/server/server.ts
+++ packages/frontend/src/server/server.ts
@@ -7,12 +7,12 @@ import express from 'express'
 import sirv from 'sirv'
 import type { ViteDevServer } from 'vite'
 import { rawEnv } from '~/env'
+import { createServerPageRouter } from '../pages/ServerPageRouter'
 import {
   CLIENT_ASSETS_OUTPUT_DIR,
   CLIENT_ASSETS_PATH,
   CLIENT_TEMPLATE_PATH,
 } from '../paths'
-import { createServerPageRouter } from '../pages/ServerPageRouter'
 import type { RenderData, ServerRenderFunction } from '../ssr/types'
 import { type Manifest, manifest } from '../utils/Manifest'
 import { ErrorHandler } from './middlewares/ErrorHandler'
