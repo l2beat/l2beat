0a3c8ffda6ecd4bc0b9e590a212dfe9f2b59eafe
sekuba <29250140+sekuba@users.noreply.github.com>
2026-03-09 10:32:28 +0100

    use custom bridgetype func for stargate

--- packages/backend/src/modules/interop/plugins/stargate.ts
+++ packages/backend/src/modules/interop/plugins/stargate.ts
@@ -4,7 +4,6 @@ import type { InteropConfigStore } from '../engine/config/InteropConfigStore'
 import type { TokenMap } from '../engine/match/TokenMap'
 import { PacketDelivered, PacketSent } from './layerzero/layerzero-v2.plugin'
 import {
-  getBridgeType,
   parseOFTReceived,
   parseOFTSent,
 } from './layerzero/layerzero-v2-ofts.plugin'
@@ -300,6 +299,18 @@ const StargateV2CreditsReceived = createInteropEventType<{
 const GUID_ZERO =
   '0x0000000000000000000000000000000000000000000000000000000000000000'
 
+function getStargateBridgeType(srcWasBurned: boolean, dstWasMinted: boolean) {
+  if (!srcWasBurned && !dstWasMinted) {
+    return 'nonMinting' // full hydra
+  }
+
+  if (srcWasBurned && dstWasMinted) {
+    return 'burnAndMint'
+  }
+
+  return 'lockAndMint' // normal case
+}
+
 export class StargatePlugin implements InteropPlugin {
   readonly name = 'stargate'
 
@@ -445,7 +456,7 @@ export class StargatePlugin implements InteropPlugin {
   match(
     packetDelivered: InteropEvent,
     db: InteropEventDb,
-    tokenMap: TokenMap,
+    _tokenMap: TokenMap,
   ): MatchResult | undefined {
     if (!PacketDelivered.checkType(packetDelivered)) return
 
@@ -506,16 +517,10 @@ export class StargatePlugin implements InteropPlugin {
             dstAmount: matchedOftReceived.args.amountReceivedLD,
             srcWasBurned: oftSentBusRode.args.hydra,
             dstWasMinted: matchedOftReceived.args.hydra,
-            bridgeType: getBridgeType({
-              srcTokenAddress: oftSentBusRode.args.tokenAddress,
-              dstTokenAddress: matchedOftReceived.args.tokenAddress,
-              srcWasBurned: oftSentBusRode.args.hydra,
-              dstWasMinted: matchedOftReceived.args.hydra,
-              srcChain: oftSentBusRode.ctx.chain,
-              dstChain: matchedOftReceived.ctx.chain,
-              tokenMap,
-              defaultBridgeType: 'nonMinting',
-            }),
+            bridgeType: getStargateBridgeType(
+              oftSentBusRode.args.hydra,
+              matchedOftReceived.args.hydra,
+            ),
           }),
         )
       }
@@ -543,16 +548,10 @@ export class StargatePlugin implements InteropPlugin {
           dstAmount: oftReceived.args.amountReceivedLD,
           srcWasBurned: oftSentTaxi.args.hydra,
           dstWasMinted: oftReceived.args.hydra,
-          bridgeType: getBridgeType({
-            srcTokenAddress: oftSentTaxi.args.tokenAddress,
-            dstTokenAddress: oftReceived.args.tokenAddress,
-            srcWasBurned: oftSentTaxi.args.hydra,
-            dstWasMinted: oftReceived.args.hydra,
-            srcChain: oftSentTaxi.ctx.chain,
-            dstChain: oftReceived.ctx.chain,
-            tokenMap,
-            defaultBridgeType: 'nonMinting',
-          }),
+          bridgeType: getStargateBridgeType(
+            oftSentTaxi.args.hydra,
+            oftReceived.args.hydra,
+          ),
         }),
       ]
     }
