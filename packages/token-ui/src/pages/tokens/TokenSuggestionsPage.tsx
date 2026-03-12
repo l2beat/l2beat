910c78296805f829ec0188b9d5a054aea168de5d
torztomasz <tomasz.torz@l2beat.com>
2026-03-12 13:31:29 +0100

    add basic page routing

---
+++ packages/token-ui/src/pages/tokens/TokenSuggestionsPage.tsx
@@ -0,0 +1,11 @@
+import { AppLayout } from '~/layouts/AppLayout'
+
+export function TokenSuggestionsPage() {
+  return (
+    <AppLayout>
+      <div>
+        <h1>Token Suggestions</h1>
+      </div>
+    </AppLayout>
+  )
+}
