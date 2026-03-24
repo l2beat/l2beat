0071a9972d2e979ef3274334a6882714b07d5bff
torztomasz <tomasz.torz@l2beat.com>
2026-03-24 10:40:19 +0100

    log fn per code

--- packages/frontend/src/server/routers/TrpcRouter.ts
+++ packages/frontend/src/server/routers/TrpcRouter.ts
@@ -1,3 +1,5 @@
+import { assertUnreachable } from '@l2beat/shared-pure'
+import type { TRPCError } from '@trpc/server'
 import * as trpcExpress from '@trpc/server/adapters/express'
 import express from 'express'
 import { appRouter } from '~/server/trpc/root'
@@ -18,7 +20,8 @@ export function createTrpcRouter() {
       router: appRouter,
       createContext,
       onError: (opts) => {
-        logger.error(opts.error.message, {
+        const logFn = getLogFn(opts.error)
+        logFn(opts.error.message, {
           path: opts.path,
           stack: opts.error.stack,
           code: opts.error.code,
@@ -30,4 +33,32 @@ export function createTrpcRouter() {
   )
 
   return router
+}
+
+function getLogFn(error: TRPCError) {
+  switch (error.code) {
+    case 'UNAUTHORIZED':
+    case 'BAD_REQUEST':
+      return logger.warn
+    case 'PARSE_ERROR':
+    case 'INTERNAL_SERVER_ERROR':
+    case 'NOT_IMPLEMENTED':
+    case 'BAD_GATEWAY':
+    case 'SERVICE_UNAVAILABLE':
+    case 'GATEWAY_TIMEOUT':
+    case 'FORBIDDEN':
+    case 'NOT_FOUND':
+    case 'METHOD_NOT_SUPPORTED':
+    case 'TIMEOUT':
+    case 'CONFLICT':
+    case 'PRECONDITION_FAILED':
+    case 'PAYLOAD_TOO_LARGE':
+    case 'UNSUPPORTED_MEDIA_TYPE':
+    case 'UNPROCESSABLE_CONTENT':
+    case 'TOO_MANY_REQUESTS':
+    case 'CLIENT_CLOSED_REQUEST':
+      return logger.error
+    default:
+      assertUnreachable(error.code)
+  }
 }
