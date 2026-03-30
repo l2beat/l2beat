710f4bb9d92551941d7c381334bf77369ad387d3
vilcht <dawdrobny@gmail.com>
2026-03-30 12:14:57 +0200

    Refactor StatCard to simplify its interface

--- packages/frontend/src/components/chart/stats/StatCard.tsx
+++ packages/frontend/src/components/chart/stats/StatCard.tsx
@@ -3,43 +3,33 @@ import { CssVariables } from '~/components/CssVariables'
 import { Skeleton } from '~/components/core/Skeleton'
 import { cn } from '~/utils/cn'
 
-export function StatCard({
-  color,
-  className,
-  title,
-  titleClassName,
-  contentClassName,
-  isLoading,
-  loadingContent,
-  children,
-  footer,
-}: {
+interface StatCardProps {
   color?: string
   className?: string
   title: ReactNode
-  titleClassName?: string
-  contentClassName?: string
   isLoading: boolean
   loadingContent?: ReactNode
   children?: ReactNode
   footer?: ReactNode
-}) {
+}
+
+export function StatCard({
+  color,
+  className,
+  title,
+  isLoading,
+  loadingContent,
+  children,
+  footer,
+}: StatCardProps) {
   const body = (
     <>
       <div
-        className={cn(
-          'text-center font-medium text-label-value-13 md:text-label-value-14 lg:text-label-value-16',
-          titleClassName,
-        )}
+        className="text-center font-medium text-label-value-13 md:text-label-value-14 lg:text-label-value-16"
       >
         {title}
       </div>
-      <div
-        className={cn(
-          'flex flex-col items-center justify-center pt-2',
-          contentClassName,
-        )}
-      >
+      <div className="flex flex-col items-center justify-center pt-2">
         {isLoading
           ? (loadingContent ?? (
               <Skeleton className="min-h-[23px] w-[4.75rem] shrink-0 md:min-h-7 md:w-[6rem]" />
