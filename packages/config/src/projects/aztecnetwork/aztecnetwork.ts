9803b98c973e5ef7158712206f2f607839c11691
sekuba <29250140+sekuba@users.noreply.github.com>
2026-03-31 11:14:33 +0200

    fix trustedsetup

--- packages/config/src/projects/aztecnetwork/aztecnetwork.ts
+++ packages/config/src/projects/aztecnetwork/aztecnetwork.ts
@@ -313,7 +313,6 @@ export const aztecnetwork: ScalingProject = {
     notInScope: [
       SOA.l2Contracts,
       SOA.nonGasTokens,
-      SOA.trustedSetup,
       SOA.specToSourceCode,
     ],
   },
