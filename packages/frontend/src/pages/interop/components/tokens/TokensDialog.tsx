44c2088d0969a476f85ccfcf31839613781fbdde
vilcht <dawdrobny@gmail.com>
2026-04-13 14:48:01 +0200

    widen the dialog to prevent sidescroll

--- packages/frontend/src/pages/interop/components/tokens/TokensDialog.tsx
+++ packages/frontend/src/pages/interop/components/tokens/TokensDialog.tsx
@@ -123,7 +123,7 @@ export function TokensDialog({
 
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
-      <DialogContent className="primary-card flex max-h-3/5 w-[1040px] max-w-[calc(100vw-1rem)] flex-col gap-0 overflow-hidden bg-surface-primary px-0 pt-0 pb-0">
+      <DialogContent className="primary-card flex max-h-3/5 w-[1120px] max-w-[calc(100vw-1rem)] flex-col gap-0 overflow-hidden bg-surface-primary px-0 pt-0 pb-0">
         <DialogClose />
         <Tabs
           name="tokens"
