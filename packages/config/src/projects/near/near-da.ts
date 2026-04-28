8118e07846da392a3bed744b6234f4fbeef80d12
vincfurc <vincfurc@users.noreply.github.com>
2026-04-28 16:46:09 +0100

    Archive NEAR DA

--- packages/config/src/projects/near/near-da.ts
+++ packages/config/src/projects/near/near-da.ts
@@ -8,6 +8,7 @@ export const near: BaseProject = {
   slug: 'near',
   name: 'NEAR DA',
   shortName: undefined,
+  archivedAt: UnixTime.fromDate(new Date('2026-04-28')),
   addedAt: UnixTime.fromDate(new Date('2024-09-03')),
   // data
   statuses: {
