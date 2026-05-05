dc7a70789f1f8e917a2869f65c63700e84299529
Maciej Zygmunt <77489537+maciekzygmunt@users.noreply.github.com>
2026-05-05 14:16:38 +0200

    remove plausible

--- packages/frontend/types.d.ts
+++ packages/frontend/types.d.ts
@@ -36,7 +36,6 @@ declare global {
   interface Window {
     __SSR_DATA__: SsrData
     __ENV__: Record<string, string>
-    plausible: Plausible | undefined
     op: {
       q?: string[][]
       (
