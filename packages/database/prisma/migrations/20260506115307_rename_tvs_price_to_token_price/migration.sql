e373277927cddf38ed89c822f8671645787cf2db
torztomasz <tomasz.torz@l2beat.com>
2026-05-06 12:18:23 +0200

    rename constraint

--- packages/database/prisma/migrations/20260506115307_rename_tvs_price_to_token_price/migration.sql
+++ packages/database/prisma/migrations/20260506115307_rename_tvs_price_to_token_price/migration.sql
@@ -4,6 +4,9 @@ ALTER TABLE "TvsPrice" RENAME TO "TokenPrice";
 -- RenameIndex
 ALTER INDEX "TvsPrice_priceId_idx" RENAME TO "TokenPrice_priceId_idx";
 
+-- RenameConstraint
+ALTER TABLE "TokenPrice" RENAME CONSTRAINT "TvsPrice_pkey" TO "TokenPrice_pkey";
+
 -- UpdateIndexerState
 UPDATE "IndexerState" SET "indexerId" = 'token_price_indexer' WHERE "indexerId" = 'tvs_price_indexer';
 
