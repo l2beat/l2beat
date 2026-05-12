252cae0946aad67d80079906b3f82611b8036d23
Maciej Zygmunt <77489537+maciekzygmunt@users.noreply.github.com>
2026-05-12 10:22:10 +0200

    update badge

--- packages/frontend/src/pages/interop/token-frameworks/components/Last24HoursBadge.tsx
+++ packages/frontend/src/pages/interop/token-frameworks/components/Last24HoursBadge.tsx
@@ -4,7 +4,7 @@ export function Last24HoursBadge({ className }: { className?: string }) {
   return (
     <div
       className={cn(
-        'rounded bg-n-blue-700 px-1.5 py-[3px] font-bold text-sm text-white leading-[1.15]',
+        'whitespace-nowrap rounded bg-n-blue-700 px-1.5 py-[3px] font-bold text-sm text-white leading-[1.15]',
         className,
       )}
     >
