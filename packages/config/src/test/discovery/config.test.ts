36e95eb25a90030d7a8c2ababe79e1d768e892f5
Mateusz Radomski <radomski.main@protonmail.com>
2026-05-19 13:31:36 +0200

    Test

--- packages/config/src/test/discovery/config.test.ts
+++ packages/config/src/test/discovery/config.test.ts
@@ -9,6 +9,7 @@ import {
   getHashToBeMatched,
   makeEntryStructureConfig,
   TemplateService,
+  get$Implementations,
 } from '@l2beat/discovery'
 import { assert, ChainSpecificAddress, unique } from '@l2beat/shared-pure'
 import { expect } from 'earl'
@@ -150,6 +151,31 @@ describe('discovery config.jsonc', () => {
 
         const uniqueAddresses = unique(addresses, asKey)
         expect(addresses).toHaveLength(uniqueAddresses.length)
+      })
+    }
+  })
+
+  describe('shape addresses are not proxies', () => {
+    const proxies: Set<ChainSpecificAddress> = new Set()
+
+    for (const c of configs.filter((c) => !c.archived)) {
+      const discovery = configReader.readDiscovery(c.name)
+      const addresses = discovery.entries
+        .filter((e) => get$Implementations(e.values).length > 0)
+        .map((e) => e.address)
+
+      for (const a of addresses) proxies.add(a)
+    }
+
+    const shapes = templateService.listAllTemplates()
+    for (const [templateId, { shapePath }] of Object.entries(shapes)) {
+      it(`shape ${templateId}:${shapePath} addresses are not proxies`, () => {
+        const shape = templateService.readShapeSchema(shapePath)
+        const addresses = Object.values(shape).flatMap((x) =>
+          Array.isArray(x.address) ? x.address : [x.address],
+        )
+
+        expect(addresses.every((a) => !proxies.has(a))).toBeTruthy()
       })
     }
   })
