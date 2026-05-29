b719614cc3ca85b30e11cf4e05d957d9913a2cf7
Mateusz Radomski <33978857+mateuszradomski@users.noreply.github.com>
2026-05-29 09:49:27 +0200

    Optimize SVG paths (#11983)

--- packages/frontend/src/icons/pages/Bridges.tsx
+++ packages/frontend/src/icons/pages/Bridges.tsx
@@ -14,15 +14,15 @@ export function BridgesIcon({ className, ...props }: SvgIconProps) {
       {...props}
     >
       <g clipPath="url(#clip0_4632_30109)">
-        <path d="M2 13H18" strokeWidth="1.8" strokeLinecap="round" />
-        <path d="M6 13V17" strokeWidth="1.8" strokeLinecap="round" />
+        <path d="M2 13h16" strokeWidth="1.8" strokeLinecap="round" />
+        <path d="M6 13v4" strokeWidth="1.8" strokeLinecap="round" />
         <path
-          d="M2.00012 8.99992C4.00012 8.99992 6.00011 6.99992 6.00011 2.99992C8 11 12 11 14.0001 2.9999C14.0001 6.9999 16 9.00006 18 9.00006"
+          d="M2 9c2 0 4-2 4-6 2 8 6 8 8 0 0 4 2 6 4 6"
           strokeWidth="1.8"
           strokeLinecap="round"
           strokeLinejoin="round"
         />
-        <path d="M14 13V17" strokeWidth="1.8" strokeLinecap="round" />
+        <path d="M14 13v4" strokeWidth="1.8" strokeLinecap="round" />
       </g>
       <defs>
         <clipPath id="clip0_4632_30109">
