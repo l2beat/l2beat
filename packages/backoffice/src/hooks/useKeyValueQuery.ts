9adbdff516bebf92bfadf666ffa47cdf48f943d8
torztomasz <tomasz.torz@l2beat.com>
2026-06-01 12:03:38 +0200

    fix

--- packages/backoffice/src/hooks/useKeyValueQuery.ts
+++ packages/backoffice/src/hooks/useKeyValueQuery.ts
@@ -1,7 +1,12 @@
 import type { KeyValueRecord } from '@l2beat/backend/trpc'
 import { useBackendApi } from '~/react-query/trpc'
 
-export function useKeyValueQuery<K extends KeyValueRecord['key']>(key: K) {
+export function useKeyValueQuery<K extends KeyValueRecord['key']>(
+  key: K,
+): Omit<
+  ReturnType<typeof useBackendApi>['keyValue']['get']['useQuery'],
+  'data'
+> & { data: KeyValueRecord<K> } {
   const api = useBackendApi()
   const { data, ...rest } = api.keyValue.get.useQuery(key)
 
