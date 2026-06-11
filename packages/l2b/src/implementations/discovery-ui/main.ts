8440f4a1dc64ad0a3e00c7d0ac9c0fc50cb5c951
Dawid Drobny <dawdrobny@gmail.com>
2026-06-11 11:08:50 +0200

    add dotenv read for disco server process

--- packages/l2b/src/implementations/discovery-ui/main.ts
+++ packages/l2b/src/implementations/discovery-ui/main.ts
@@ -9,6 +9,7 @@ import {
 import { DiffHistoryParser } from '@l2beat/shared'
 import { ChainSpecificAddress } from '@l2beat/shared-pure'
 import { toJsonSchema, v as z } from '@l2beat/validate'
+import { config as dotenv } from 'dotenv'
 import express from 'express'
 import { existsSync, readFileSync } from 'fs'
 import type { Server } from 'http'
@@ -94,6 +95,7 @@ const diffHistoryQuerySchema = z.object({
 })
 
 export function runDiscoveryUi({ readonly }: { readonly: boolean }) {
+  dotenv()
   const app = express()
   const port = process.env.PORT ?? 2021
 
