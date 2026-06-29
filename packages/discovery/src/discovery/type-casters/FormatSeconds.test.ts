3f107f7220ada58e0c572365b69b18b37c0481e3
Mateusz Radomski <radomski.main@protonmail.com>
2026-06-29 16:03:05 +0200

    Review fixes

--- packages/discovery/src/discovery/type-casters/FormatSeconds.test.ts
+++ packages/discovery/src/discovery/type-casters/FormatSeconds.test.ts
@@ -1,8 +1,9 @@
 import { expect } from 'earl'
+import type { ContractValue } from '../output/types'
 import { FormatSeconds } from './FormatSeconds'
 
 interface TestData {
-  incomingValue: number | bigint
+  incomingValue: ContractValue | bigint
   expected: string
 }
 
@@ -10,27 +11,21 @@ const testData: TestData[] = [
   { incomingValue: 0, expected: '0s' },
   { incomingValue: 3600, expected: '1h' },
   { incomingValue: 90061, expected: '1d 1h' },
+  { incomingValue: '123456789', expected: '3y 11mo' },
+  { incomingValue: '9007199254740991', expected: '285616414y 8mo' },
+  { incomingValue: '9007199254740992', expected: 'more than 285616414y 8mo' },
+  {
+    incomingValue: '1152921504606846975',
+    expected: 'more than 285616414y 8mo',
+  },
   { incomingValue: 123456789n, expected: '3y 11mo' },
-  { incomingValue: BigInt(Number.MAX_SAFE_INTEGER), expected: '285616414y 8mo' },
-  {
-    incomingValue: BigInt(Number.MAX_SAFE_INTEGER) + 1n,
-    expected: 'more than 285616414y 8mo',
-  },
-  {
-    incomingValue: 0x0fffffffffffffffn,
-    expected: 'more than 285616414y 8mo',
-  },
+  { incomingValue: 0x0fffffffffffffffn, expected: 'more than 285616414y 8mo' },
 ]
 
 describe('FormatSeconds', () => {
   for (const data of testData) {
     it(`casts ${data.incomingValue} to "${data.expected}"`, () => {
-      const result = FormatSeconds.cast(
-        {},
-        data.incomingValue as unknown as Parameters<
-          typeof FormatSeconds.cast
-        >[1],
-      )
+      const result = FormatSeconds.cast({}, data.incomingValue)
       expect(result).toEqual(data.expected)
     })
   }
--- packages/discovery/src/discovery/type-casters/FormatSeconds.ts
+++ packages/discovery/src/discovery/type-casters/FormatSeconds.ts
@@ -1,17 +1,20 @@
-import { assert, formatSeconds } from '@l2beat/shared-pure'
+import { formatSeconds } from '@l2beat/shared-pure'
+import { valueToBigInt } from '../handlers/utils/valueToBigInt'
 import type { ContractValue } from '../output/types'
 import type { ArgType, BaseTypeCaster } from './BaseTypeCaster'
 
-export const FormatSeconds: BaseTypeCaster = {
-  cast: function (_arg: ArgType, incomingValue: ContractValue): ContractValue {
-    const value = incomingValue as ContractValue | bigint
-    if (typeof value === 'bigint') {
-      if (value > BigInt(Number.MAX_SAFE_INTEGER)) {
-        return `more than ${formatSeconds(Number.MAX_SAFE_INTEGER)}`
-      }
-      return formatSeconds(Number(value))
+export const FormatSeconds = {
+  cast: function (
+    _arg: ArgType,
+    incomingValue: ContractValue | bigint,
+  ): ContractValue {
+    if (typeof incomingValue === 'number') {
+      return formatSeconds(incomingValue)
     }
-    assert(typeof value === 'number')
-    return formatSeconds(value)
+    const seconds = valueToBigInt(incomingValue)
+    if (seconds > BigInt(Number.MAX_SAFE_INTEGER)) {
+      return `more than ${formatSeconds(Number.MAX_SAFE_INTEGER)}`
+    }
+    return formatSeconds(Number(seconds))
   },
-}
+} satisfies BaseTypeCaster
