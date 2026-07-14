b1480da5163db640a48b902a91fa9553f9b65446
Radina Talanova <radina.talanova@l2beat.com>
2026-07-14 17:06:36 +0300

    replace link

--- packages/frontend/src/pages/native-rollups/consts.ts
+++ packages/frontend/src/pages/native-rollups/consts.ts
@@ -17,7 +17,7 @@ export const NATIVE_ROLLUPS_REPO_URL =
   'https://github.com/l2beat/native-rollups'
 export const FOUNDING_POST_URL =
   'https://ethresear.ch/t/native-rollups-superpowers-from-l1-execution/21517'
-export const ETHREX_POC_URL = 'https://github.com/lambdaclass/ethrex/pull/6186'
+export const ETHREX_POC_URL = 'https://github.com/lambdaclass/ethrex/pull/6418'
 export const L2_FOCIL_URL = 'https://github.com/l2beat/native-rollups/pull/4'
 export const L2_FOCIL_RESEARCH_URL =
   'https://ethresear.ch/t/repurposing-focil-as-an-l2-forced-transaction-mechanism/25233'
