668bfc6e65fcb7a2a113cb1bf8ded71df478a15e
sekuba <29250140+sekuba@users.noreply.github.com>
2026-07-29 08:32:37 +0200

    block

--- packages/config/src/projects/aztecnetwork/aztecnetwork.ts
+++ packages/config/src/projects/aztecnetwork/aztecnetwork.ts
@@ -173,6 +173,7 @@ const registryAddress = ChainSpecificAddress.address(registry.address)
 const escapeHatchAddress = ChainSpecificAddress.address(escapeHatch.address)
 
 const v5ActivationTimestamp = UnixTime(1784060567) // 14 July 2026 20:22:47 UTC
+const v5ActivationBlock = 25533241 // https://etherscan.io/tx/0xff2db4e4bba583f2451478bfe4703e16afc79f0b463fb60615ebe3494142437b
 
 function formatAztecAmount(amount: bigint): string {
   return `${formatLargeNumber(Number(amount / 10n ** 18n))} AZTEC`
@@ -349,7 +350,7 @@ export const aztecnetwork: ScalingProject = {
       {
         type: 'ethereum',
         daLayer: ProjectId('ethereum'),
-        sinceBlock: v5ActivationTimestamp,
+        sinceBlock: v5ActivationBlock,
         inbox: EthereumAddress.ZERO, // Event-only tracking
         topics: [
           '0x6ff492bf2b4ca1b93a175167d14b3e46085b935cab3f39ca94013000799b93a0', // CheckpointProposed
