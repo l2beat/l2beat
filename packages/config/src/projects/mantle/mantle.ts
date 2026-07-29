41eaff7c22a35224e2689e8be2d548be5c5bb0cc
torztomasz <tomasz.torz@l2beat.com>
2026-07-29 08:42:07 +0200

    order configs

--- packages/config/src/projects/mantle/mantle.ts
+++ packages/config/src/projects/mantle/mantle.ts
@@ -126,6 +126,13 @@ export const mantle: ScalingProject = opStackL2({
   ],
   nonTemplateDaTracking: [
     {
+      type: 'eigen-da',
+      customerId: '0x24f0a3716805e8973bf48eb908d6d4a2f34af785',
+      daLayer: ProjectId('eigenda'),
+      sinceTimestamp: UnixTime(1738821600),
+      untilTimestamp: UnixTime(1776322715), // Arsia upgrade: EigenDA code path removed, DA is Ethereum only
+    },
+    {
       type: 'ethereum',
       daLayer: ProjectId('ethereum'),
       sinceBlock: discovery.getContract('SystemConfig').sinceBlock ?? 0,
@@ -137,13 +144,6 @@ export const mantle: ScalingProject = opStackL2({
           discovery.getContractValue('SystemConfig', 'batcherHash'),
         ),
       ],
-    },
-    {
-      type: 'eigen-da',
-      customerId: '0x24f0a3716805e8973bf48eb908d6d4a2f34af785',
-      daLayer: ProjectId('eigenda'),
-      sinceTimestamp: UnixTime(1738821600),
-      untilTimestamp: UnixTime(1776322715), // Arsia upgrade: EigenDA code path removed, DA is Ethereum only
     },
   ],
   associatedTokens: ['MNT'],
