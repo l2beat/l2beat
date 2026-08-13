eb3a0b230e3db4177248aa543d4b0a8890207724
sekuba <29250140+sekuba@users.noreply.github.com>
2026-08-13 11:23:35 +0200

    update starknet sc link

--- packages/config/src/projects/starknet/starknet.ts
+++ packages/config/src/projects/starknet/starknet.ts
@@ -385,7 +385,7 @@ export const starknet: ScalingProject = {
     {
       rollupNodeLink: 'https://github.com/eqlabs/pathfinder',
       securityCouncilReference:
-        'https://governance.starknet.io/learn/security_council',
+        'https://docs.starknet.io/learn/protocol/security-council',
       stage1PrincipleDescription:
         'While Starknet is considered Stage 1, the Security Council minority is employed to enforce censorship resistance in case the permissioned operator fails to include transactions. The process through which a censored user can contact the Security Council is not defined and currently unclear.',
     },
