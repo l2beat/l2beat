79c50c213e01f27c4fdbc936c843748e9013cda5
torztomasz <tomasz.torz@l2beat.com>
2026-08-26 10:43:20 +0200

    make the bubble radius arbitrary

--- packages/frontend/src/pages/interop/components/flows/graph/BubbleHolesClip.tsx
+++ packages/frontend/src/pages/interop/components/flows/graph/BubbleHolesClip.tsx
@@ -1,6 +1,8 @@
 import { useMemo } from 'react'
 import type { FlowsGraphLayout } from './utils/computeGraphLayout'
 
+const BUBBLE_RADIUS = 4
+
 interface Props {
   id: string
   chainIds: string[]
@@ -19,8 +21,8 @@ export function BubbleHolesClip({ id, chainIds, layout }: Props) {
       .map((chainId) => layout.get(chainId))
       .filter((node) => node !== undefined)
       .map(
-        ({ x, y, radius: r }) =>
-          `M ${x - r} ${y} a ${r} ${r} 0 1 0 ${2 * r} 0 a ${r} ${r} 0 1 0 ${-2 * r} 0 Z`,
+        ({ x, y }) =>
+          `M ${x - BUBBLE_RADIUS} ${y} a ${BUBBLE_RADIUS} ${BUBBLE_RADIUS} 0 1 0 ${2 * BUBBLE_RADIUS} 0 a ${BUBBLE_RADIUS} ${BUBBLE_RADIUS} 0 1 0 ${-2 * BUBBLE_RADIUS} 0 Z`,
       )
       .join(' ')
     return `M -1e4 -1e4 H 1e4 V 1e4 H -1e4 Z ${bubbleHoles}`
