3590344cf5ee64122147d01b0e68130dce31b62d
vilcht <dawdrobny@gmail.com>
2026-09-07 09:23:22 +0200

    simplify constants

--- packages/backend/src/modules/interop/engine/cleaner/InteropCleanerLoop.ts
+++ packages/backend/src/modules/interop/engine/cleaner/InteropCleanerLoop.ts
@@ -1,6 +1,6 @@
 import type { Logger } from '@l2beat/backend-tools'
 import type { Database } from '@l2beat/database'
-import { INTEROP_TRANSFER_RETENTION, UnixTime } from '@l2beat/shared-pure'
+import { UnixTime } from '@l2beat/shared-pure'
 import { TimeLoop } from '../../../../tools/TimeLoop'
 import { type InteropPlugins, pluginsAsClusters } from '../../plugins'
 import type { InteropEventStore } from '../capture/InteropEventStore'
@@ -27,7 +27,7 @@ export class InteropCleanerLoop extends TimeLoop {
       now - 1 * UnixTime.DAY,
     )
     const expiredTransfers = await this.db.interopTransfer.deleteBefore(
-      now - INTEROP_TRANSFER_RETENTION,
+      now - 7 * UnixTime.DAY,
     )
     const expiredPrices = await this.db.interopRecentPrices.deleteBefore(
       now - 7 * UnixTime.DAY,
