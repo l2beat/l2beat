3ffbb86b1c4548a3b7b8e1846c5904dd5d09fbda
sergeyshemyakov <98955965+sergeyshemyakov@users.noreply.github.com>
2026-09-11 11:30:37 +0000

    Disco for railgun, cleanup for privacy projects zk catalog (#12820)
    Co-authored-by: Sergey Shemyakov <sergey.shemyakov@l2beat.com>

--- packages/config/src/projects/railgun/railgun.ts
+++ packages/config/src/projects/railgun/railgun.ts
@@ -299,5 +299,6 @@ export const railgun: BaseProject = {
   contracts: {
     addresses: generateDiscoveryDrivenContracts([discovery]),
     risks: [],
+    zkVerifiers: [discovery.getContract('RailgunSmartWallet').address],
   },
 }
