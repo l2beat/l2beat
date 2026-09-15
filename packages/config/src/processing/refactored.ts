c39c1910ec3a3450ebe6527e78bda4d13cbee00b
sekuba <29250140+sekuba@users.noreply.github.com>
2026-09-15 16:15:28 +0200

    review zcash-nearintents-zodl

--- packages/config/src/processing/refactored.ts
+++ packages/config/src/processing/refactored.ts
@@ -72,6 +72,7 @@ import { wormholeNtt } from '../projects/wormhole-ntt/wormhole-ntt'
 import { wormholeWtt } from '../projects/wormhole-wtt/wormhole-wtt'
 import { zama } from '../projects/zama/zama'
 import { zamaCw } from '../projects/zama-cw/zama-cw'
+import { zcashNearIntents } from '../projects/zcash-near-intents/zcash-near-intents'
 import { zkprover } from '../projects/zkprover/zkprover'
 import { zksyncprover } from '../projects/zksyncprover/zksyncprover'
 import type { BaseProject } from '../types'
@@ -159,5 +160,6 @@ export const refactored: BaseProject[] = [
   tornadoCash,
   umbra,
   zamaCw,
+  zcashNearIntents,
   privacyBoost,
 ]
